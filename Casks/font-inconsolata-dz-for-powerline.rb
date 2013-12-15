52a31f1d9a906ba123e1138cf70a3660c00c770f
Julien M <jmarchand@leadformance.com>
2013-12-16 00:31:36 +0100

    Add Font Inconsolata DZ, patched for Powerline
    This is the Inconsolata DZ font (http://cl.ly/2Y322V3n1k0D), a
    variation of the original Inconsolata font.
    It is patched for use with Powerline shell
    (http://cl.ly/1T1L0f3z2N2j).

---
+++ Casks/font-inconsolata-dz-for-powerline.rb
@@ -0,0 +1,7 @@
+class FontInconsolataDzForPowerline < Cask
+  url 'https://raw.github.com/Lokaltog/powerline-fonts/master/InconsolataDz/Inconsolata-dz%20for%20Powerline.otf'
+  homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/InconsolataDz'
+  version '001.010'
+  no_checksum
+  font 'Inconsolata-dz for Powerline.otf'
+end
