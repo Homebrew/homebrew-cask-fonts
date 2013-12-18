f96c34f04cd62daa5b0bbb096a0ae96b2fc86de9
Beng Hee Eu <beng@benghee.eu>
2013-12-18 22:05:43 +0800

    Roboto 1.200311

---
+++ Casks/font-roboto.rb
@@ -0,0 +1,24 @@
+class FontRoboto < Cask
+  url 'http://developer.android.com/downloads/design/roboto-1.2.zip'
+  homepage 'http://developer.android.com/design/style/typography.html'
+  version '1.200311'
+  sha1 '87ac584379974261d2483f459a388790d9d817ed'
+  font 'Roboto_v1.2/Roboto/Roboto-Black.ttf',
+       'Roboto_v1.2/Roboto/Roboto-BlackItalic.ttf',
+       'Roboto_v1.2/Roboto/Roboto-Bold.ttf',
+       'Roboto_v1.2/Roboto/Roboto-BoldItalic.ttf',
+       'Roboto_v1.2/Roboto/Roboto-Italic.ttf',
+       'Roboto_v1.2/Roboto/Roboto-Light.ttf',
+       'Roboto_v1.2/Roboto/Roboto-LightItalic.ttf',
+       'Roboto_v1.2/Roboto/Roboto-Medium.ttf',
+       'Roboto_v1.2/Roboto/Roboto-MediumItalic.ttf',
+       'Roboto_v1.2/Roboto/Roboto-Regular.ttf',
+       'Roboto_v1.2/Roboto/Roboto-Thin.ttf',
+       'Roboto_v1.2/Roboto/Roboto-ThinItalic.ttf',
+       'Roboto_v1.2/RobotoCondensed/RobotoCondensed-Bold.ttf',
+       'Roboto_v1.2/RobotoCondensed/RobotoCondensed-BoldItalic.ttf',
+       'Roboto_v1.2/RobotoCondensed/RobotoCondensed-Italic.ttf',
+       'Roboto_v1.2/RobotoCondensed/RobotoCondensed-Light.ttf',
+       'Roboto_v1.2/RobotoCondensed/RobotoCondensed-LightItalic.ttf',
+       'Roboto_v1.2/RobotoCondensed/RobotoCondensed-Regular.ttf'
+end
