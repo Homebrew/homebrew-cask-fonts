66a19944043a578ddae9d5ea916e0b1e8350759f
Beng Hee Eu <beng@benghee.eu>
2013-12-18 22:53:51 +0800

    Open Sans 1.10

---
+++ Casks/font-open-sans.rb
@@ -0,0 +1,16 @@
+class FontOpenSans < Cask
+  url 'http://www.google.com/fonts/download?kit=3hvsV99qyKCBS55e5pvb3ltkqrIMaAZWyLYEoB48lSQ'
+  homepage 'http://www.google.com/fonts/specimen/Open+Sans'
+  version '1.10'
+  sha1 'fec8832e4c9e28dd8aeca55dc7f15cb4accab915'
+  font 'OpenSans-Bold.ttf',
+       'OpenSans-BoldItalic.ttf',
+       'OpenSans-ExtraBold.ttf',
+       'OpenSans-ExtraBoldItalic.ttf',
+       'OpenSans-Italic.ttf',
+       'OpenSans-Light.ttf',
+       'OpenSans-LightItalic.ttf',
+       'OpenSans-Regular.ttf',
+       'OpenSans-Semibold.ttf',
+       'OpenSans-SemiboldItalic.ttf'
+end
