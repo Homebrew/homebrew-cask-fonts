c745bb1fc9f959a357b8ffdd4aa0ece3c8b6029d
Tom Vincent <git@tlvince.com>
2013-12-26 10:48:36 +0000

    Add Cardo

---
+++ Casks/font-cardo.rb
@@ -0,0 +1,9 @@
+class FontCardo < Cask
+  url 'http://scholarsfonts.net/cardo104.zip'
+  homepage 'http://scholarsfonts.net/cardofnt.html'
+  version '1.04'
+  sha1 'fb156361986052071fa8df09ab6c95749c5dad07'
+  font 'Cardo104s.ttf',
+       'Cardoi99.ttf',
+       'Cardob101.ttf'
+end
