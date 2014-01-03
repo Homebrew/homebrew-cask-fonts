252d6fecd88fa4a8bcb378f68e93f676e36e6480
Roland Walker <walker@pobox.com>
2014-01-03 12:16:20 -0500

    merge Fira Cask files per guidelines in CONTRIBUTING.md. Fixes phinze/homebrew-cask#2285

--- Casks/font-fira-mono.rb
+++
@@ -1,8 +0,0 @@
-class FontFiraMono < Cask
-  url 'https://github.com/mozilla/Fira/archive/master.zip'
-  homepage 'https://github.com/mozilla/Fira'
-  version 'latest'
-  sha1 '1935db578b416fae4b4f92526cf2da19539fff19'
-  font 'Fira-master/otf/FiraMonoOT-Bold.otf'
-       'Fira-master/otf/FiraMonoOT-Regular.otf'
-end
--- Casks/font-fira-sans.rb
+++ Casks/font-fira-sans.rb
@@ -3,12 +3,14 @@ class FontFiraSans < Cask
   homepage 'https://github.com/mozilla/Fira'
   version 'latest'
   sha1 '1935db578b416fae4b4f92526cf2da19539fff19'
-  font 'Fira-master/otf/FiraSansOT-Bold.otf'
-       'Fira-master/otf/FiraSansOT-BoldItalic.otf'
-       'Fira-master/otf/FiraSansOT-Regular.otf'
-       'Fira-master/otf/FiraSansOT-RegularItalic.otf'
-       'Fira-master/otf/FiraSansOT-Light.otf'
-       'Fira-master/otf/FiraSansOT-LightItalic.otf'
-       'Fira-master/otf/FiraSansOT-Medium.otf'
-       'Fira-master/otf/FiraSansOT-MediumItalic.otf'
+  font 'Fira-master/otf/FiraSansOT-Bold.otf',
+       'Fira-master/otf/FiraSansOT-BoldItalic.otf',
+       'Fira-master/otf/FiraSansOT-Regular.otf',
+       'Fira-master/otf/FiraSansOT-RegularItalic.otf',
+       'Fira-master/otf/FiraSansOT-Light.otf',
+       'Fira-master/otf/FiraSansOT-LightItalic.otf',
+       'Fira-master/otf/FiraSansOT-Medium.otf',
+       'Fira-master/otf/FiraSansOT-MediumItalic.otf',
+       'Fira-master/otf/FiraMonoOT-Bold.otf',
+       'Fira-master/otf/FiraMonoOT-Regular.otf'
 end
