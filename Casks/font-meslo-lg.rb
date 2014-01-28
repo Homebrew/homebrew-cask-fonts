fc19521b35740a00c44882fd4266a218abb6c729
Matt Wean <matthew.wean@gmail.com>
2014-01-27 19:04:47 -0800

    Add Meslo LG font

---
+++ Casks/font-meslo-lg.rb
@@ -0,0 +1,18 @@
+class FontMesloLg < Cask
+  url 'https://github.com/andreberg/Meslo-Font/raw/master/dist/v1.2.1/Meslo%20LG%20v1.2.1.zip'
+  homepage 'https://github.com/andreberg/Meslo-Font'
+  version '1.21'
+  sha1 '23cc2b1f4ed16f82054b127a8d3653516731f919'
+  font 'Meslo LG v1.2.1/MesloLGL-Bold.ttf',
+       'Meslo LG v1.2.1/MesloLGL-BoldItalic.ttf',
+       'Meslo LG v1.2.1/MesloLGL-Italic.ttf',
+       'Meslo LG v1.2.1/MesloLGL-Regular.ttf',
+       'Meslo LG v1.2.1/MesloLGM-Bold.ttf',
+       'Meslo LG v1.2.1/MesloLGM-BoldItalic.ttf',
+       'Meslo LG v1.2.1/MesloLGM-Italic.ttf',
+       'Meslo LG v1.2.1/MesloLGM-Regular.ttf',
+       'Meslo LG v1.2.1/MesloLGS-Bold.ttf',
+       'Meslo LG v1.2.1/MesloLGS-BoldItalic.ttf',
+       'Meslo LG v1.2.1/MesloLGS-Italic.ttf',
+       'Meslo LG v1.2.1/MesloLGS-Regular.ttf'
+end
