8a781c6a0c58e4b3135370f48c9637af3632cd7b
Alpha Chen <alpha.chen@gmail.com>
2014-01-28 17:32:46 -0800

    Add Sauce Code Powerline

---
+++ Casks/font-sauce-code-powerline-extralight.rb
@@ -0,0 +1,7 @@
+class FontSauceCodePowerlineExtralight < Cask
+  url 'https://github.com/Lokaltog/powerline-fonts/raw/master/SourceCodePro/Sauce%20Code%20Powerline%20ExtraLight.otf'
+  homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/SourceCodePro'
+  version 'latest'
+  no_checksum
+  font 'Sauce Code Powerline ExtraLight.otf'
+end
---
+++ Casks/font-sauce-code-powerline-medium.rb
@@ -0,0 +1,7 @@
+class FontSauceCodePowerlineMedium < Cask
+  url 'https://github.com/Lokaltog/powerline-fonts/raw/master/SourceCodePro/Sauce%20Code%20Powerline%20Medium.otf'
+  homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/SourceCodePro'
+  version 'latest'
+  no_checksum
+  font 'Sauce Code Powerline Medium.otf'
+end
