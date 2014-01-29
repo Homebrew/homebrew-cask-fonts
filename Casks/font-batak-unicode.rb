f40a08388331902c1aa5279ef0ffea96d222d73e
Roland Walker <walker@pobox.com>
2014-01-28 19:45:45 -0500

    add font Batak Unicode

---
+++ Casks/font-batak-unicode.rb
@@ -0,0 +1,7 @@
+class FontBatakUnicode < Cask
+  url 'http://ulikozok.com/fontfiles/Batak-Unicode-Regular.otf'
+  homepage 'http://ulikozok.com/aksara-batak/batak-font/'
+  version 'latest'
+  no_checksum
+  font 'Batak-Unicode-Regular.otf'
+end
