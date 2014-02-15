0ea830003062776eb5e134596aa904f5d89c33bf
Roland Walker <walker@pobox.com>
2014-02-15 17:18:49 -0500

    add font-concert-one.rb, source: Google

---
+++ Casks/font-concert-one.rb
@@ -0,0 +1,7 @@
+class FontConcertOne < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/concertone/ConcertOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Concert%20One'
+  version '1.003'
+  sha256 '1651e05fba9a75b10a5c5c5bd83de642cbd47e820a9639a3f1592c8eb5d0342c'
+  font 'ConcertOne-Regular.ttf'
+end
