ae55e99be4ed74d0af3b445498003e4b33e72f70
Roland Walker <walker@pobox.com>
2014-02-15 17:18:48 -0500

    add font-bilbo-swash-caps.rb, source: Google

---
+++ Casks/font-bilbo-swash-caps.rb
@@ -0,0 +1,7 @@
+class FontBilboSwashCaps < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/bilboswashcaps/BilboSwashCaps-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Bilbo%20Swash%20Caps'
+  version '1.002'
+  sha256 '882e19185209074faaa7d4ba4dcec492630c1744ea82277e3f3611b29de21ec9'
+  font 'BilboSwashCaps-Regular.ttf'
+end
