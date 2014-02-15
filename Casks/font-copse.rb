9dd7fe6c4ddaf9c43c248f28163e38ec2b3db6ae
Roland Walker <walker@pobox.com>
2014-02-15 17:18:49 -0500

    add font-copse.rb, source: Google

---
+++ Casks/font-copse.rb
@@ -0,0 +1,7 @@
+class FontCopse < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/copse/Copse-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Copse'
+  version '1.000'
+  sha256 'b852e682f0c66de4db1835f8545ff2e94761549987a4607447b069e973f50b1d'
+  font 'Copse-Regular.ttf'
+end
