c20040c79f9757c483c323f4f11005f837eb82a4
Roland Walker <walker@pobox.com>
2014-02-15 17:18:51 -0500

    add font-gabriela.rb, source: Google

---
+++ Casks/font-gabriela.rb
@@ -0,0 +1,7 @@
+class FontGabriela < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/gabriela/Gabriela-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Gabriela'
+  version '1.003'
+  sha256 'f7e3d6ebae857c32722d36aec664ade0471a24f57d522e5d4d907d9f81e2f92f'
+  font 'Gabriela-Regular.ttf'
+end
