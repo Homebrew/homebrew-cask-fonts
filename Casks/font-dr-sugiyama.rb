6d496863c89f41eb7c1b06e7907c07e8738887fc
Roland Walker <walker@pobox.com>
2014-02-15 17:18:50 -0500

    add font-dr-sugiyama.rb, source: Google

---
+++ Casks/font-dr-sugiyama.rb
@@ -0,0 +1,7 @@
+class FontDrSugiyama < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/drsugiyama/DrSugiyama-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Dr%20Sugiyama'
+  version '1.000'
+  sha256 '8436c1eae3a06126a6e0aa85b1dffb1b507d39eab7b6512a9bb1adc5d514d9b9'
+  font 'DrSugiyama-Regular.ttf'
+end
