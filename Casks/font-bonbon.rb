395a46e38195b36a42c672543e83f606cf065990
Roland Walker <walker@pobox.com>
2014-02-15 17:18:48 -0500

    add font-bonbon.rb, source: Google

---
+++ Casks/font-bonbon.rb
@@ -0,0 +1,7 @@
+class FontBonbon < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/bonbon/Bonbon-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Bonbon'
+  version '1.000'
+  sha256 'eb1fd687b29fc686447a8985160bb0743078ae3ed8d8bd5b592479d82aac035d'
+  font 'Bonbon-Regular.ttf'
+end
