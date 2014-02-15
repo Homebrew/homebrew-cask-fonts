9d89444ba94edd395238ac5b5c9455b1f4f1628c
Roland Walker <walker@pobox.com>
2014-02-15 17:18:51 -0500

    add font-fredericka-the-great.rb, source: Google

---
+++ Casks/font-fredericka-the-great.rb
@@ -0,0 +1,7 @@
+class FontFrederickaTheGreat < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/frederickathegreat/FrederickatheGreat-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Fredericka%20the%20Great'
+  version '1.000'
+  sha256 '990272dea68d64b9a819554b57e80ceb5276e3df1eaa32ac78190c6232a07132'
+  font 'FrederickatheGreat-Regular.ttf'
+end
