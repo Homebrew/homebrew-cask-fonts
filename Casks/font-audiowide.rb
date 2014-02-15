63f8004e3015dce25dd3f30f425448db6244248e
Roland Walker <walker@pobox.com>
2014-02-15 17:18:48 -0500

    add font-audiowide.rb, source: Google

---
+++ Casks/font-audiowide.rb
@@ -0,0 +1,7 @@
+class FontAudiowide < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/audiowide/Audiowide-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Audiowide'
+  version '1.002'
+  sha256 '977421788d493b4fb87fc25e66a5a4ef98b532b2fa7d35a736cc453d0e038c92'
+  font 'Audiowide-Regular.ttf'
+end
