309d0f728a9b9a4bbc2b4a10e0a3de10f738b697
Roland Walker <walker@pobox.com>
2014-02-15 17:18:54 -0500

    add font-maiden-orange.rb, source: Google

---
+++ Casks/font-maiden-orange.rb
@@ -0,0 +1,7 @@
+class FontMaidenOrange < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/maidenorange/MaidenOrange.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Maiden%20Orange'
+  version '1.000'
+  sha256 'c0bddef94723070ebf184bee678f21f2edf1e7befaddb3d8495425629f237fc7'
+  font 'MaidenOrange.ttf'
+end
