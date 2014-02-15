3627180bc01f69089eb58ac69c1a3c4417b4da94
Roland Walker <walker@pobox.com>
2014-02-15 17:18:48 -0500

    add font-artifika.rb, source: Google

---
+++ Casks/font-artifika.rb
@@ -0,0 +1,7 @@
+class FontArtifika < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/artifika/Artifika-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Artifika'
+  version '1.000'
+  sha256 'f07035c2a1df5a5e34dcb98005c8e9d957afdc558eaa026ef06644f3793e6965'
+  font 'Artifika-Regular.ttf'
+end
