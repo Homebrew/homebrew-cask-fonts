e761d0299ec13e44e5d4d4319a630897df592a6c
Roland Walker <walker@pobox.com>
2014-02-15 17:18:50 -0500

    add font-croissant-one.rb, source: Google

---
+++ Casks/font-croissant-one.rb
@@ -0,0 +1,7 @@
+class FontCroissantOne < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/croissantone/CroissantOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Croissant%20One'
+  version '1.001'
+  sha256 '55ccb462d07bab853281e9fd9646f0cc258bfb1ae8ff645f857ba1f921f7f69e'
+  font 'CroissantOne-Regular.ttf'
+end
