8cbd3b7c9b4ff4d79b957e8acf6a56219fb0b477
Roland Walker <walker@pobox.com>
2014-02-15 17:18:49 -0500

    add font-cherry-cream-soda.rb, source: Google

---
+++ Casks/font-cherry-cream-soda.rb
@@ -0,0 +1,7 @@
+class FontCherryCreamSoda < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/cherrycreamsoda/CherryCreamSoda.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Cherry%20Cream%20Soda'
+  version '1.000'
+  sha256 'b73095102defba4686605019a0d05444ce922b2db6338963f047de3541b16e86'
+  font 'CherryCreamSoda.ttf'
+end
