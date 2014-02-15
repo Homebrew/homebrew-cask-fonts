343c1e5e8bc3f86f328e6768a66f7281b9ec4b73
Roland Walker <walker@pobox.com>
2014-02-15 17:18:59 -0500

    add font-stalinist-one.rb, source: Google

---
+++ Casks/font-stalinist-one.rb
@@ -0,0 +1,7 @@
+class FontStalinistOne < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/stalinistone/StalinistOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Stalinist%20One'
+  version '3.002'
+  sha256 '186cd0cb9aeb4dd1ae07b54d3a6f14410f58b7a8f3933de5ceb857ad1db6184c'
+  font 'StalinistOne-Regular.ttf'
+end
