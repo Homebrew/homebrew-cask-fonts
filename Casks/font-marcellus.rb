76bdfd479086c55cdc992a9932182eab5998431c
Roland Walker <walker@pobox.com>
2014-02-15 17:18:54 -0500

    add font-marcellus.rb, source: Google

---
+++ Casks/font-marcellus.rb
@@ -0,0 +1,7 @@
+class FontMarcellus < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/marcellus/Marcellus-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Marcellus'
+  version '1.000'
+  sha256 '1cf0cd10b17d35e852729962cc1ffaffed94514895972458345e2df34abb2f81'
+  font 'Marcellus-Regular.ttf'
+end
