ea68a0e305a27b350c60ce73128a9efecb2832e6
Roland Walker <walker@pobox.com>
2014-02-15 17:18:53 -0500

    add font-keania-one.rb, source: Google

---
+++ Casks/font-keania-one.rb
@@ -0,0 +1,7 @@
+class FontKeaniaOne < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/keaniaone/KeaniaOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Keania%20One'
+  version '1.003'
+  sha256 '55c6be3d0cdc816c462bccbcc935ef20f560bd7aecbf1b38caed4752b52c14c0'
+  font 'KeaniaOne-Regular.ttf'
+end
