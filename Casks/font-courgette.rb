791082cd3deb879dd8f39249bedf8b4d14f978a3
Roland Walker <walker@pobox.com>
2014-02-15 17:18:49 -0500

    add font-courgette.rb, source: Google

---
+++ Casks/font-courgette.rb
@@ -0,0 +1,7 @@
+class FontCourgette < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/courgette/Courgette-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Courgette'
+  version '1.002'
+  sha256 '099739d22e66ee2b80656bba63847f9ed419ad7e3969ba2c1d3c437c0d08f3e3'
+  font 'Courgette-Regular.ttf'
+end
