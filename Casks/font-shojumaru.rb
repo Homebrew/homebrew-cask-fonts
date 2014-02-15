7d77107b86a8b503daeef1ccdc50b2fe7d4fe148
Roland Walker <walker@pobox.com>
2014-02-15 17:18:59 -0500

    add font-shojumaru.rb, source: Google

---
+++ Casks/font-shojumaru.rb
@@ -0,0 +1,7 @@
+class FontShojumaru < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/shojumaru/Shojumaru-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Shojumaru'
+  version '1.001'
+  sha256 '8607110ef45da664861c72d95fb804c3609169ebfd2a380cb47b353a7b241c75'
+  font 'Shojumaru-Regular.ttf'
+end
