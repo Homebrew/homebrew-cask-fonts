245e4d82ac19915e393f128fd6bff78951ba12ab
Roland Walker <walker@pobox.com>
2014-02-15 17:18:56 -0500

    add font-petrona.rb, source: Google

---
+++ Casks/font-petrona.rb
@@ -0,0 +1,7 @@
+class FontPetrona < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/petrona/Petrona-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Petrona'
+  version '1.001'
+  sha256 '5ff9703955f6059f123d3272589b03bb95238a1d05a4efc1429e2347b42521e7'
+  font 'Petrona-Regular.ttf'
+end
