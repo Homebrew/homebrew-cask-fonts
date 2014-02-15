b75d9bc830de593c66d1c8e6f0fd7c9d0885705f
Roland Walker <walker@pobox.com>
2014-02-15 17:18:49 -0500

    add font-buda.rb, source: Google

---
+++ Casks/font-buda.rb
@@ -0,0 +1,7 @@
+class FontBuda < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/buda/Buda-Light.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Buda'
+  version '1.002'
+  sha256 'e6ec5aff205dc2109d56a729b2f864ab6578b836715614c228c2bb0280988d81'
+  font 'Buda-Light.ttf'
+end
