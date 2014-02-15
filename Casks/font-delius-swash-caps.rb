6363e0b0998b12f2ac0f077ff0f126b6d4480867
Roland Walker <walker@pobox.com>
2014-02-15 17:18:50 -0500

    add font-delius-swash-caps.rb, source: Google

---
+++ Casks/font-delius-swash-caps.rb
@@ -0,0 +1,7 @@
+class FontDeliusSwashCaps < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/deliusswashcaps/DeliusSwashCaps-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Delius%20Swash%20Caps'
+  version '1.002'
+  sha256 '04c102448007265b8a0672590900919d35dfd26c624e543c8c3bfbab9535b642'
+  font 'DeliusSwashCaps-Regular.ttf'
+end
