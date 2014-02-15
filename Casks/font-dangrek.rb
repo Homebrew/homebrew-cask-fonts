f13047bd0db7accc56bdc715137f31f0f6f7714a
Roland Walker <walker@pobox.com>
2014-02-15 17:18:50 -0500

    add font-dangrek.rb, source: Google

---
+++ Casks/font-dangrek.rb
@@ -0,0 +1,7 @@
+class FontDangrek < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/dangrek/Dangrek.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Dangrek'
+  version '6.00'
+  sha256 '2148e6eaa36456d6a9cdafd19aad553366ac78a2e720556a5710812a57b47782'
+  font 'Dangrek.ttf'
+end
