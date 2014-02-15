0e2ffe50e1991a4d1826c5084cf355857e2f8fee
Roland Walker <walker@pobox.com>
2014-02-15 17:19:00 -0500

    add font-viga.rb, source: Google

---
+++ Casks/font-viga.rb
@@ -0,0 +1,7 @@
+class FontViga < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/viga/Viga-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Viga'
+  version '1.001'
+  sha256 '730ad2a38691ce19ee5aa9dd9ec84ae8737b2084b06eef13435a4facad9b063c'
+  font 'Viga-Regular.ttf'
+end
