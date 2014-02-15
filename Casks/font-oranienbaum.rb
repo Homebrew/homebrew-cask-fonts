1ad3a63bf5e2316feda78610a1de922a715f4b00
Roland Walker <walker@pobox.com>
2014-02-15 17:18:56 -0500

    add font-oranienbaum.rb, source: Google

---
+++ Casks/font-oranienbaum.rb
@@ -0,0 +1,7 @@
+class FontOranienbaum < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/oranienbaum/Oranienbaum-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Oranienbaum'
+  version '1.001'
+  sha256 '6f884e8b6c0d57180016a50d1cbb4e58ea136250dfbb8617dac393e338e2209d'
+  font 'Oranienbaum-Regular.ttf'
+end
