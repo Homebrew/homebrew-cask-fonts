ddea3732aadd9924a3ed98e5788ca9192ac49a81
Roland Walker <walker@pobox.com>
2014-02-15 17:19:01 -0500

    add font-zeyada.rb, source: Google

---
+++ Casks/font-zeyada.rb
@@ -0,0 +1,7 @@
+class FontZeyada < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/zeyada/Zeyada.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Zeyada'
+  version '1.002'
+  sha256 '09f23d0d78b6e166ddb8480793bb550ab4c2aaf6602eda47a394fee93d2a9667'
+  font 'Zeyada.ttf'
+end
