c97e98c39ae58565d1264d2c64a62675ff67f129
Roland Walker <walker@pobox.com>
2014-02-15 17:18:55 -0500

    add font-monofett.rb, source: Google

---
+++ Casks/font-monofett.rb
@@ -0,0 +1,7 @@
+class FontMonofett < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/monofett/Monofett.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Monofett'
+  version '1.000'
+  sha256 '05c225ab2653d62e9aeef6a78693d6c990383d3253daece8bedc5964231d5c52'
+  font 'Monofett.ttf'
+end
