23c1ad169f2c55ea234e44d1aec62d28aa438f70
Roland Walker <walker@pobox.com>
2014-02-15 17:18:49 -0500

    add font-chango.rb, source: Google

---
+++ Casks/font-chango.rb
@@ -0,0 +1,7 @@
+class FontChango < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/chango/Chango-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Chango'
+  version '1.001'
+  sha256 '3e566f4b881f24198fe1b6525aae970f9200078136e3416abc7ba0272d2f80d9'
+  font 'Chango-Regular.ttf'
+end
