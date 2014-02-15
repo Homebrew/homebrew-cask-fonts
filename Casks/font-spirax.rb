b43248325252a860281b7fa89e3cf43d3eb3cc1c
Roland Walker <walker@pobox.com>
2014-02-15 17:18:59 -0500

    add font-spirax.rb, source: Google

---
+++ Casks/font-spirax.rb
@@ -0,0 +1,7 @@
+class FontSpirax < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/spirax/Spirax-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Spirax'
+  version '1.002'
+  sha256 'e509975c0680e447e6be89467f7f0bebec6d4cb0a8b75d7b6bb6d6fd7e2d5b65'
+  font 'Spirax-Regular.ttf'
+end
