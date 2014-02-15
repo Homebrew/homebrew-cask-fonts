7d1e294b7b6ebf4f00768c3a1555befdf56b78d5
Roland Walker <walker@pobox.com>
2014-02-15 17:18:58 -0500

    add font-schoolbell.rb, source: Google

---
+++ Casks/font-schoolbell.rb
@@ -0,0 +1,7 @@
+class FontSchoolbell < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/schoolbell/Schoolbell.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Schoolbell'
+  version '1.000'
+  sha256 '1a3ae62329871c20600217f19f3472ba8f253c11a7df6cb9a9e6e88ced95ee0a'
+  font 'Schoolbell.ttf'
+end
