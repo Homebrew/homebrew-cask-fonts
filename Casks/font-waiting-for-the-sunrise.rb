75eb1166217532e0d9c0e4c29765d06125865eb5
Roland Walker <walker@pobox.com>
2014-02-15 17:19:01 -0500

    add font-waiting-for-the-sunrise.rb, source: Google

---
+++ Casks/font-waiting-for-the-sunrise.rb
@@ -0,0 +1,7 @@
+class FontWaitingForTheSunrise < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/waitingforthesunrise/WaitingfortheSunrise.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Waiting%20for%20the%20Sunrise'
+  version '1.001'
+  sha256 'ac74e70390f5b1be6927c1e079bd6143821a362550b264c5eb7edde34f0899f4'
+  font 'WaitingfortheSunrise.ttf'
+end
