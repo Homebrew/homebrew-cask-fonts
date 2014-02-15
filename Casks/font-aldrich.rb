69a816fa1ff5629f72566cdb5c4b95b56487d7b3
Roland Walker <walker@pobox.com>
2014-02-15 17:18:47 -0500

    add font-aldrich.rb, source: Google

---
+++ Casks/font-aldrich.rb
@@ -0,0 +1,7 @@
+class FontAldrich < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/aldrich/Aldrich-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Aldrich'
+  version '1.001'
+  sha256 '38c2138e642f70184a29a02a58c362a9fb12b5ef6e7716bf4b685197b4399c75'
+  font 'Aldrich-Regular.ttf'
+end
