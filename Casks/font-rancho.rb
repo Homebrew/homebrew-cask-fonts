1b2013224c030274b9ab3b106cf28e78b9b5f9a4
Roland Walker <walker@pobox.com>
2014-02-15 17:18:57 -0500

    add font-rancho.rb, source: Google

---
+++ Casks/font-rancho.rb
@@ -0,0 +1,7 @@
+class FontRancho < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/rancho/Rancho-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Rancho'
+  version '1.000'
+  sha256 'b743fa9fef7c558762bd10d637794666bcf2bdff0249a2ee6fabfa8991347f7a'
+  font 'Rancho-Regular.ttf'
+end
