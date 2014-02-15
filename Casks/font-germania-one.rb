a5176c044770f284c9a5382dc8e6c7eb1d8ec45e
Roland Walker <walker@pobox.com>
2014-02-15 17:18:51 -0500

    add font-germania-one.rb, source: Google

---
+++ Casks/font-germania-one.rb
@@ -0,0 +1,7 @@
+class FontGermaniaOne < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/germaniaone/GermaniaOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Germania%20One'
+  version '1.001'
+  sha256 '3ee3d56584bab8c9e3f59cc3360aa8a36e219dcf0a9f3c8928e3da5a19277e3f'
+  font 'GermaniaOne-Regular.ttf'
+end
