d32e9ae05315f24bc2d35889543340adbba8ce61
Roland Walker <walker@pobox.com>
2014-02-15 17:18:52 -0500

    add font-holtwood-one-sc.rb, source: Google

---
+++ Casks/font-holtwood-one-sc.rb
@@ -0,0 +1,7 @@
+class FontHoltwoodOneSc < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/holtwoodonesc/HoltwoodOneSC.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Holtwood%20One%20SC'
+  version '1.000'
+  sha256 'd733fb5df2cfa8bca5b1d9bdb258c7a772f77a9ee5b209573402ad1dfd8597aa'
+  font 'HoltwoodOneSC.ttf'
+end
