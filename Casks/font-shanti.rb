5e28264d633fc1ec307c189a15a376f6fc9af36f
Roland Walker <walker@pobox.com>
2014-02-15 17:18:58 -0500

    add font-shanti.rb, source: Google

---
+++ Casks/font-shanti.rb
@@ -0,0 +1,7 @@
+class FontShanti < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/shanti/Shanti-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Shanti'
+  version '1.000'
+  sha256 'c08c43d1d03cc84ee4151b239f43fcda0395964b34b7ac5cf9197bfd3c17854d'
+  font 'Shanti-Regular.ttf'
+end
