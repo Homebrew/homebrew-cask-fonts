1338ac8827dbef3960bd3725d57da7ca95bf1dab
Roland Walker <walker@pobox.com>
2014-02-15 17:18:47 -0500

    add font-arbutus-slab.rb, source: Google

---
+++ Casks/font-arbutus-slab.rb
@@ -0,0 +1,7 @@
+class FontArbutusSlab < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/arbutusslab/ArbutusSlab-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Arbutus%20Slab'
+  version '1.001'
+  sha256 'c17514a190509a94d74c9e99427c44527ff8c7d7dbba5d3e67a35ebf0093bd89'
+  font 'ArbutusSlab-Regular.ttf'
+end
