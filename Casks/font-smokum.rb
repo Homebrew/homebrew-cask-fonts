cadb78277f2078a8b80437e24209acc08808352b
Roland Walker <walker@pobox.com>
2014-02-15 17:18:59 -0500

    add font-smokum.rb, source: Google

---
+++ Casks/font-smokum.rb
@@ -0,0 +1,7 @@
+class FontSmokum < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/smokum/Smokum-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Smokum'
+  version '1.000'
+  sha256 'bd8e372e289df15eb499d77a6334a5933ff2225ac6f5020707a61d070f147c2d'
+  font 'Smokum-Regular.ttf'
+end
