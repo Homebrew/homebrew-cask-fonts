48546485cd69da2c17a24786462bf372af9544c0
Roland Walker <walker@pobox.com>
2014-02-15 17:18:50 -0500

    add font-didact-gothic.rb, source: Google

---
+++ Casks/font-didact-gothic.rb
@@ -0,0 +1,7 @@
+class FontDidactGothic < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/didactgothic/DidactGothic.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Didact%20Gothic'
+  version '20110429'
+  sha256 '01c9c7791a161a9022eee3b172ebfa33300891557471f94f488fb7b8442630d8'
+  font 'DidactGothic.ttf'
+end
