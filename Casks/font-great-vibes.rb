629d3d68510036c3bae093f1a47c88c55f06f1ab
Roland Walker <walker@pobox.com>
2014-02-15 17:18:52 -0500

    add font-great-vibes.rb, source: Google

---
+++ Casks/font-great-vibes.rb
@@ -0,0 +1,7 @@
+class FontGreatVibes < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/greatvibes/GreatVibes-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Great%20Vibes'
+  version '1.001'
+  sha256 '9b4de643c541cc8f85b8be33ea93f6131a7911bac9cb44015b49552e402b1b55'
+  font 'GreatVibes-Regular.ttf'
+end
