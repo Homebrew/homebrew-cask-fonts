4ab0984159c8416e9e0647810a7e540ad65041b2
Roland Walker <walker@pobox.com>
2014-02-15 17:18:54 -0500

    add font-loved-by-the-king.rb, source: Google

---
+++ Casks/font-loved-by-the-king.rb
@@ -0,0 +1,7 @@
+class FontLovedByTheKing < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/lovedbytheking/LovedbytheKing.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Loved%20by%20the%20King'
+  version '1.002'
+  sha256 '1d2fce705c902a0c47264992e698ce76336013458f0612cc68917025ca6fcfb3'
+  font 'LovedbytheKing.ttf'
+end
