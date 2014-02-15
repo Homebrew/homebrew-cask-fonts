b18acb5063b1801756821f850118086231802ac2
Roland Walker <walker@pobox.com>
2014-02-15 17:18:55 -0500

    add font-miltonian-tattoo.rb, source: Google

---
+++ Casks/font-miltonian-tattoo.rb
@@ -0,0 +1,7 @@
+class FontMiltonianTattoo < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/miltoniantattoo/MiltonianTattoo-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Miltonian%20Tattoo'
+  version '1.005'
+  sha256 '85f6f41a5158294b195613f7e9977f3edc7a8e1abb9666a288591e01f0051ed0'
+  font 'MiltonianTattoo-Regular.ttf'
+end
