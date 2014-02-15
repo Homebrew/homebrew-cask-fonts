48b0189874459c7691506cdce0b0b40931100ebd
Roland Walker <walker@pobox.com>
2014-02-15 17:18:51 -0500

    add font-gfs-didot.rb, source: Google

---
+++ Casks/font-gfs-didot.rb
@@ -0,0 +1,7 @@
+class FontGfsDidot < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/gfsdidot/GFSDidot-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/GFS%20Didot'
+  version '1.0'
+  sha256 '4fc784ab824e8866eeef8ad55976b93e7e170fb1b8237fd4f8781b5b4dd297f9'
+  font 'GFSDidot-Regular.ttf'
+end
