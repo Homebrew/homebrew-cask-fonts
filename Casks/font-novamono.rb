dae3ec70bd79ad192920d21d2d546e929f87eb74
Roland Walker <walker@pobox.com>
2014-02-15 17:18:56 -0500

    add font-novamono.rb, source: Google

---
+++ Casks/font-novamono.rb
@@ -0,0 +1,7 @@
+class FontNovamono < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/novamono/NovaMono.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Nova%20Mono'
+  version '1.2'
+  sha256 '648eadb6648c0801b186d3dcef60ee6aa84a791b1e09c726935c0712508b4807'
+  font 'NovaMono.ttf'
+end
