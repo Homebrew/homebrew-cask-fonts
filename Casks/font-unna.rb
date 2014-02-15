a14990351ec29d439cba8df7742188ec406d8341
Roland Walker <walker@pobox.com>
2014-02-15 17:19:00 -0500

    add font-unna.rb, source: Google

---
+++ Casks/font-unna.rb
@@ -0,0 +1,7 @@
+class FontUnna < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/unna/Unna-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Unna'
+  version '2.003'
+  sha256 'ac424ce81a707d98a87bbb304273f35d29018e9abb199af0fb76f8989f5ae8a8'
+  font 'Unna-Regular.ttf'
+end
