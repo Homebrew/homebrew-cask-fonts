1fdcee9a01e14798ba4171ba711e4e8265fa9fab
Roland Walker <walker@pobox.com>
2014-02-15 17:18:56 -0500

    add font-oxygen-mono.rb, source: Google

---
+++ Casks/font-oxygen-mono.rb
@@ -0,0 +1,7 @@
+class FontOxygenMono < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/oxygenmono/OxygenMono-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Oxygen%20Mono'
+  version '0.201'
+  sha256 'dcc671da93f7fc31f2e7a4a7c058c815c1eaec6a3d27792e18edff4d3bf539f0'
+  font 'OxygenMono-Regular.ttf'
+end
