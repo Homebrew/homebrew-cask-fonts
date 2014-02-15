2eb2973eb90db4ee7975ef431ec8499f2ec64973
Roland Walker <walker@pobox.com>
2014-02-15 17:18:52 -0500

    add font-homemade-apple.rb, source: Google

---
+++ Casks/font-homemade-apple.rb
@@ -0,0 +1,7 @@
+class FontHomemadeApple < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/homemadeapple/HomemadeApple.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Homemade%20Apple'
+  version '1.000'
+  sha256 'b311f042e9ae6dbb327b23ac0476b16b2e98a041754541a7773fbab3b3279906'
+  font 'HomemadeApple.ttf'
+end
