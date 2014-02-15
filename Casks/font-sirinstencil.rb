61ae17e6b69c3fac46101f1362a844650c443617
Roland Walker <walker@pobox.com>
2014-02-15 17:18:59 -0500

    add font-sirinstencil.rb, source: Google

---
+++ Casks/font-sirinstencil.rb
@@ -0,0 +1,7 @@
+class FontSirinstencil < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/sirinstencil/SirinStencil-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Sirin%20Stencil'
+  version '1.002'
+  sha256 '0e231c6220787179588fe8b1c8921bff70207f1f556a6ef7e35e46ae04352b2d'
+  font 'SirinStencil-Regular.ttf'
+end
