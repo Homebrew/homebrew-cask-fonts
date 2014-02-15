66629769ca8f566fa8e404894301e7935e9617a7
Roland Walker <walker@pobox.com>
2014-02-15 17:18:53 -0500

    add font-just-me-again-down-here.rb, source: Google

---
+++ Casks/font-just-me-again-down-here.rb
@@ -0,0 +1,7 @@
+class FontJustMeAgainDownHere < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/justmeagaindownhere/JustMeAgainDownHere.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Just%20Me%20Again%20Down%20Here'
+  version '1.002'
+  sha256 '0412aa1e460666d339738991b48e9f4bd51e10b6f04e2e1341fce4d2b3244c31'
+  font 'JustMeAgainDownHere.ttf'
+end
