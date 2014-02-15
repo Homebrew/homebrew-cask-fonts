f8bb948a7f2ec1f71b572657006000bbf6a68054
Roland Walker <walker@pobox.com>
2014-02-15 17:18:53 -0500

    add font-kavoon.rb, source: Google

---
+++ Casks/font-kavoon.rb
@@ -0,0 +1,7 @@
+class FontKavoon < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/kavoon/Kavoon-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Kavoon'
+  version '1.002'
+  sha256 'ca39db198843f47ec04b47de4482a76e98430a974a17a958991d9cff1d0b2b35'
+  font 'Kavoon-Regular.ttf'
+end
