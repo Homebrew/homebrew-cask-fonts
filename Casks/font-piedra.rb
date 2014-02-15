82a7e135778615007bc2fb8a913fee28b0963a31
Roland Walker <walker@pobox.com>
2014-02-15 17:18:56 -0500

    add font-piedra.rb, source: Google

---
+++ Casks/font-piedra.rb
@@ -0,0 +1,7 @@
+class FontPiedra < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/piedra/Piedra-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Piedra'
+  version '1.000'
+  sha256 '2e9d00f94d3d78f38618250d03d3e8bccca541c956d5e0a437c62d708ef6c1da'
+  font 'Piedra-Regular.ttf'
+end
