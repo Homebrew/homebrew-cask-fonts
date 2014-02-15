d4e05ab3ca087e6d1cfdb878bdaa20fa41c614bf
Roland Walker <walker@pobox.com>
2014-02-15 17:18:48 -0500

    add font-average-sans.rb, source: Google

---
+++ Casks/font-average-sans.rb
@@ -0,0 +1,7 @@
+class FontAverageSans < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/averagesans/AverageSans-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Average%20Sans'
+  version '1.001'
+  sha256 '80e216cd92e30f9f2fe73d6bd3a80dfd2105626929b7d037d473831c25782e38'
+  font 'AverageSans-Regular.ttf'
+end
