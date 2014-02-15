66f1bfe43fd251d6f0cb658ad9118c185a74f298
Roland Walker <walker@pobox.com>
2014-02-15 17:19:00 -0500

    add font-vt323.rb, source: Google

---
+++ Casks/font-vt323.rb
@@ -0,0 +1,7 @@
+class FontVt323 < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/vt323/VT323-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/VT323'
+  version '001.002'
+  sha256 '214c2bbd6f7d1a9787413a01dccdf6134fd1ed7934e425d07c2ad32b1568979a'
+  font 'VT323-Regular.ttf'
+end
