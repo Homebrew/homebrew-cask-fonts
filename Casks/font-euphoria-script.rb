5e8aa11a962504c7d84a1832e3f7bd0556ab2c7e
Roland Walker <walker@pobox.com>
2014-02-15 17:18:50 -0500

    add font-euphoria-script.rb, source: Google

---
+++ Casks/font-euphoria-script.rb
@@ -0,0 +1,7 @@
+class FontEuphoriaScript < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/euphoriascript/EuphoriaScript-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Euphoria%20Script'
+  version '1.002'
+  sha256 '1ca81517a450506290e2be25648e153ee4c9cf6acc44c1d610b1e6d7e0e67484'
+  font 'EuphoriaScript-Regular.ttf'
+end
