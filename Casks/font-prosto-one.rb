02e6afdec65a00818d0e78773bd1c0bac18365c2
Roland Walker <walker@pobox.com>
2014-02-15 17:18:57 -0500

    add font-prosto-one.rb, source: Google

---
+++ Casks/font-prosto-one.rb
@@ -0,0 +1,7 @@
+class FontProstoOne < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/prostoone/ProstoOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Prosto%20One'
+  version '1.001'
+  sha256 'bc6ffae8b9a4115461767edfc40ef5702c7c73370b333d9d24f9651c52d8d8b5'
+  font 'ProstoOne-Regular.ttf'
+end
