b481b2603453051381168612f57844b859bca7d4
Roland Walker <walker@pobox.com>
2014-02-15 17:18:55 -0500

    add font-montez.rb, source: Google

---
+++ Casks/font-montez.rb
@@ -0,0 +1,7 @@
+class FontMontez < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/montez/Montez-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Montez'
+  version '1.000'
+  sha256 '1d5dedd2803404c4571d8d46c76bc31030aeaffa1fb45b265b29053aa2d6c68f'
+  font 'Montez-Regular.ttf'
+end
