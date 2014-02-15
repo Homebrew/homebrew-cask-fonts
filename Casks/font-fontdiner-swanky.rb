ba9a3b4e70d6a68646f7959d9deb332925164879
Roland Walker <walker@pobox.com>
2014-02-15 17:18:51 -0500

    add font-fontdiner-swanky.rb, source: Google

---
+++ Casks/font-fontdiner-swanky.rb
@@ -0,0 +1,7 @@
+class FontFontdinerSwanky < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/fontdinerswanky/FontdinerSwanky.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Fontdiner%20Swanky'
+  version '1.000'
+  sha256 'bc56285ce814a8d119e89d1f669b743702088e816b274a830f411b358d4fd10b'
+  font 'FontdinerSwanky.ttf'
+end
