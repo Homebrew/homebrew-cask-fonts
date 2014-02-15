b162c1aadb7babc0256ad3e226490fa181cf03f8
Roland Walker <walker@pobox.com>
2014-02-15 17:18:55 -0500

    add font-niconne.rb, source: Google

---
+++ Casks/font-niconne.rb
@@ -0,0 +1,7 @@
+class FontNiconne < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/niconne/Niconne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Niconne'
+  version '1.002'
+  sha256 'dc70580a25914aeec39a9460b7858ee08eb4ad0bbbcd1d7c9cb888a6ea989a27'
+  font 'Niconne-Regular.ttf'
+end
