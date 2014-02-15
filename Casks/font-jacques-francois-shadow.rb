3809109e22f8706842a907684859221ef85e4da9
Roland Walker <walker@pobox.com>
2014-02-15 17:18:52 -0500

    add font-jacques-francois-shadow.rb, source: Google

---
+++ Casks/font-jacques-francois-shadow.rb
@@ -0,0 +1,7 @@
+class FontJacquesFrancoisShadow < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/jacquesfrancoisshadow/JacquesFrancoisShadow-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Jacques%20Francois%20Shadow'
+  version '1.003'
+  sha256 '3947827a196a3c12ceb471ed589168a9b57285db7258eb468a83f4c17945784c'
+  font 'JacquesFrancoisShadow-Regular.ttf'
+end
