39cd45030ab4aab627fcf0e6b29ff48157fe7b2c
Roland Walker <walker@pobox.com>
2014-02-15 17:18:54 -0500

    add font-metal.rb, source: Google

---
+++ Casks/font-metal.rb
@@ -0,0 +1,7 @@
+class FontMetal < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/metal/Metal.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Metal'
+  version '6.00'
+  sha256 '270a7045adfbcc335e50b4fbb4c24f1c58f15393ded13a1d87bfbbbe57bfb8ae'
+  font 'Metal.ttf'
+end
