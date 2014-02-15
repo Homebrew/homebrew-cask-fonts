db97e4cce23cd88dd98058dd3bcc8ec4cbd4b496
Roland Walker <walker@pobox.com>
2014-02-15 17:18:52 -0500

    add font-im-fell-french-canon-sc.rb, source: Google

---
+++ Casks/font-im-fell-french-canon-sc.rb
@@ -0,0 +1,7 @@
+class FontImFellFrenchCanonSc < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/imfellfrenchcanonsc/IMFeFCsc28P.ttf'
+  homepage 'http://www.google.com/fonts/specimen/IM%20Fell%20French%20Canon%20SC'
+  version '3.00'
+  sha256 '2eeab14e384b07fc27439fb992ba16c77a2996e2227e67e7627ec07c6fd01d9f'
+  font 'IMFeFCsc28P.ttf'
+end
