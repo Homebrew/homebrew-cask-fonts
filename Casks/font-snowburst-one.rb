767f6dd07eaef4ce9b613593ca448dc90a542adc
Roland Walker <walker@pobox.com>
2014-02-15 17:18:59 -0500

    add font-snowburst-one.rb, source: Google

---
+++ Casks/font-snowburst-one.rb
@@ -0,0 +1,7 @@
+class FontSnowburstOne < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/snowburstone/SnowburstOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Snowburst%20One'
+  version '1.001'
+  sha256 'ede8110b5922b718c9b80717a4c5dcc2b4a862767d8319877983773c900bb00f'
+  font 'SnowburstOne-Regular.ttf'
+end
