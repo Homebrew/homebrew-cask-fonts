3023edf01ef8c2ee702a7f5071cd9964a609f955
Roland Walker <walker@pobox.com>
2014-02-15 17:18:53 -0500

    add font-lobster.rb, source: Google

---
+++ Casks/font-lobster.rb
@@ -0,0 +1,7 @@
+class FontLobster < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/lobster/Lobster.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Lobster'
+  version '1.004'
+  sha256 '3214c9d6b387bbf241763649b0e0201f86895ff47d64e1a314a94725a687187d'
+  font 'Lobster.ttf'
+end
