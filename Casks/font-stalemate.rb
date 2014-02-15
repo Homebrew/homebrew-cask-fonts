2c1e29fce3c1073610eb7530b6395080f6a1db46
Roland Walker <walker@pobox.com>
2014-02-15 17:18:59 -0500

    add font-stalemate.rb, source: Google

---
+++ Casks/font-stalemate.rb
@@ -0,0 +1,7 @@
+class FontStalemate < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/stalemate/Stalemate-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Stalemate'
+  version '001.000'
+  sha256 '36b8c2f2777cfcca17815e63636f749b398062310c089e6c9fe2a65290c7e579'
+  font 'Stalemate-Regular.ttf'
+end
