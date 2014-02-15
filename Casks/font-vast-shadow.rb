92a86c628045515cfbff0c99d455ac94a8ace16a
Roland Walker <walker@pobox.com>
2014-02-15 17:19:00 -0500

    add font-vast-shadow.rb, source: Google

---
+++ Casks/font-vast-shadow.rb
@@ -0,0 +1,7 @@
+class FontVastShadow < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/vastshadow/VastShadow-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Vast%20Shadow'
+  version '1.002'
+  sha256 '98bdf7f93c2330697078ece27e48dea7d97b253a547d699bf7afc4bdfa641a93'
+  font 'VastShadow-Regular.ttf'
+end
