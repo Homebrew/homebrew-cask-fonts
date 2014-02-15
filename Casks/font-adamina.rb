38296cd2c225717beacf0278a219459ca1b1dc76
Roland Walker <walker@pobox.com>
2014-02-15 17:18:47 -0500

    add font-adamina.rb, source: Google

---
+++ Casks/font-adamina.rb
@@ -0,0 +1,7 @@
+class FontAdamina < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/adamina/Adamina-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Adamina'
+  version '1.010'
+  sha256 '9617e85f5b40f4a45b5fc77e74fe83197bd2805fd1f129420af4cdf669b3b436'
+  font 'Adamina-Regular.ttf'
+end
