b71622ce090f8a283d069c0f21bea8d3cd22596c
Roland Walker <walker@pobox.com>
2014-02-15 17:18:54 -0500

    add font-londrina-solid.rb, source: Google

---
+++ Casks/font-londrina-solid.rb
@@ -0,0 +1,7 @@
+class FontLondrinaSolid < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/londrinasolid/LondrinaSolid-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Londrina%20Solid'
+  version '1.001'
+  sha256 '0bec2bdceb575d7568ef18ceab91bd3fe3b574a63bba037dec559022abf6b1fd'
+  font 'LondrinaSolid-Regular.ttf'
+end
