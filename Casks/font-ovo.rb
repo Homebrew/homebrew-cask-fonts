b479ec315210a6c187e2ca173f5ec1e2c9e7dd74
Roland Walker <walker@pobox.com>
2014-02-15 17:18:56 -0500

    add font-ovo.rb, source: Google

---
+++ Casks/font-ovo.rb
@@ -0,0 +1,7 @@
+class FontOvo < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/ovo/Ovo-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Ovo'
+  version '1.001'
+  sha256 '8d4f113ada748f44d005371bee5692e58950a84e6068b74f54710e280a29f389'
+  font 'Ovo-Regular.ttf'
+end
