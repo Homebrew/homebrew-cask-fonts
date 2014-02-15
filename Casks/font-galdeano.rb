af008040dbbc098752abe14093d9ba82cd0487ff
Roland Walker <walker@pobox.com>
2014-02-15 17:18:51 -0500

    add font-galdeano.rb, source: Google

---
+++ Casks/font-galdeano.rb
@@ -0,0 +1,7 @@
+class FontGaldeano < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/galdeano/Galdeano-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Galdeano'
+  version '1.001'
+  sha256 'ec2379e6ae1192420eb7a633c170890cf85944abc73e6ef6c68371d1c05c6652'
+  font 'Galdeano-Regular.ttf'
+end
