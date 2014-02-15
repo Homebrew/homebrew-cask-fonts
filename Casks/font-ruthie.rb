b63a624bccd40116ba906d986e7f5e91ca65bb02
Roland Walker <walker@pobox.com>
2014-02-15 17:18:58 -0500

    add font-ruthie.rb, source: Google

---
+++ Casks/font-ruthie.rb
@@ -0,0 +1,7 @@
+class FontRuthie < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/ruthie/Ruthie-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Ruthie'
+  version '1.003'
+  sha256 '7c5a1bfbcd9b0bf6f709686bdcefa4b06305f60fecdd290c1c2782de31559232'
+  font 'Ruthie-Regular.ttf'
+end
