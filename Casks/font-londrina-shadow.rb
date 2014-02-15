67c73658354e7608f11cc257fe373f4fa2e04dbc
Roland Walker <walker@pobox.com>
2014-02-15 17:18:53 -0500

    add font-londrina-shadow.rb, source: Google

---
+++ Casks/font-londrina-shadow.rb
@@ -0,0 +1,7 @@
+class FontLondrinaShadow < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/londrinashadow/LondrinaShadow-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Londrina%20Shadow'
+  version '1.001'
+  sha256 '8e8fcd9dfb9dccc934ac9930ebc83a452c3c2049ffbce351b68622fe8308b10a'
+  font 'LondrinaShadow-Regular.ttf'
+end
