25b4f39c4a5db639b63e3d646b87475c613952b4
Roland Walker <walker@pobox.com>
2014-02-15 17:18:56 -0500

    add font-pinyon-script.rb, source: Google

---
+++ Casks/font-pinyon-script.rb
@@ -0,0 +1,7 @@
+class FontPinyonScript < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/pinyonscript/PinyonScript-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Pinyon%20Script'
+  version '1.005'
+  sha256 '26507b30d76ebdd4d2396c068c0ba1aec8c6d162dc10e2f1d21e57c0da0d36d1'
+  font 'PinyonScript-Regular.ttf'
+end
