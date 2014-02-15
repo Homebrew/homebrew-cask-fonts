98e49e531a6d2c61e9d439aa42b6f5d03d6abbe7
Roland Walker <walker@pobox.com>
2014-02-15 17:18:57 -0500

    add font-playball.rb, source: Google

---
+++ Casks/font-playball.rb
@@ -0,0 +1,7 @@
+class FontPlayball < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/playball/Playball-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Playball'
+  version '1.001'
+  sha256 '3985700143687aa5d575b736abec8ef82170127494760393d826d7490f25e46b'
+  font 'Playball-Regular.ttf'
+end
