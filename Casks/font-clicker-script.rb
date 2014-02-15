b112599e4b33dbedfcaa5f9ece6a68219da26d29
Roland Walker <walker@pobox.com>
2014-02-15 17:18:49 -0500

    add font-clicker-script.rb, source: Google

---
+++ Casks/font-clicker-script.rb
@@ -0,0 +1,7 @@
+class FontClickerScript < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/clickerscript/ClickerScript-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Clicker%20Script'
+  version '1.000'
+  sha256 '0221d4376fc0a25795a32d7e309798b6693b56f65c5851f92529043b230a9117'
+  font 'ClickerScript-Regular.ttf'
+end
