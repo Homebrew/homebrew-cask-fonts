1a73553b3d2b8b383bc001b64f30d9087c9aec69
Roland Walker <walker@pobox.com>
2014-02-15 17:18:51 -0500

    add font-gloria-hallelujah.rb, source: Google

---
+++ Casks/font-gloria-hallelujah.rb
@@ -0,0 +1,7 @@
+class FontGloriaHallelujah < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/gloriahallelujah/GloriaHallelujah.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Gloria%20Hallelujah'
+  version '1.004'
+  sha256 'f218055f0d440cb5150babf3d80e79a1b51b6058f41254b518a9b6f7f6960bc7'
+  font 'GloriaHallelujah.ttf'
+end
