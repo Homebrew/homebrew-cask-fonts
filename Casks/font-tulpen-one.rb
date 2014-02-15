bc2edf084d22d66226653d84b5ed6b7c50b267f4
Roland Walker <walker@pobox.com>
2014-02-15 17:19:00 -0500

    add font-tulpen-one.rb, source: Google

---
+++ Casks/font-tulpen-one.rb
@@ -0,0 +1,7 @@
+class FontTulpenOne < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/tulpenone/TulpenOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Tulpen%20One'
+  version '1.002'
+  sha256 'b773d2fd30f5ef174560ce0fe05b05c6fa901e710236c4e9eea4c3f669580005'
+  font 'TulpenOne-Regular.ttf'
+end
