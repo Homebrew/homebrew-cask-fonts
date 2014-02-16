98e0e6e6759c9056333c2db19320287f36f8bbb1
Roland Walker <walker@pobox.com>
2014-02-15 19:43:09 -0500

    add font-lohit-malayalam.rb, source: Fedora

---
+++ Casks/font-lohit-malayalam.rb
@@ -0,0 +1,7 @@
+class FontLohitMalayalam < Cask
+  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-malayalam-ttf-2.5.4.tar.gz'
+  homepage 'https://fedorahosted.org/lohit/'
+  version '2.5.4'
+  sha256 '8c230bbe4cee42d55caa3e548f52dc5f54bae42a35b8eefe575297bd23b2fde4'
+  font 'lohit-malayalam-ttf-2.5.4/Lohit-Malayalam.ttf'
+end
