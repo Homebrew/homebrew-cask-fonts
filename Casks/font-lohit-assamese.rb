d23ee8ecab11a36e86db49d32245378a9051b7ab
Roland Walker <walker@pobox.com>
2014-02-15 19:43:08 -0500

    add font-lohit-assamese.rb, source: Fedora

---
+++ Casks/font-lohit-assamese.rb
@@ -0,0 +1,7 @@
+class FontLohitAssamese < Cask
+  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-assamese-ttf-2.5.3.tar.gz'
+  homepage 'https://fedorahosted.org/lohit/'
+  version '2.5.3'
+  sha256 'ce8e510f4e78527eb4c514bf49ed69389b983cd8c4990f0a7b9b9f43b6bef9ce'
+  font 'lohit-assamese-ttf-2.5.3/Lohit-Assamese.ttf'
+end
