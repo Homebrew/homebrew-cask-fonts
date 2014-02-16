f97635e441e875705d83d712314febf53783b0f1
Roland Walker <walker@pobox.com>
2014-02-15 19:43:09 -0500

    add font-lohit-tamil.rb, source: Fedora

---
+++ Casks/font-lohit-tamil.rb
@@ -0,0 +1,7 @@
+class FontLohitTamil < Cask
+  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-tamil-ttf-2.5.3.tar.gz'
+  homepage 'https://fedorahosted.org/lohit/'
+  version '2.5.3'
+  sha256 '8511a31880f5e00fa19e8c8b1029943a99d17c45b3b7c186a391d8cc8edaa915'
+  font 'lohit-tamil-ttf-2.5.3/Lohit-Tamil.ttf'
+end
