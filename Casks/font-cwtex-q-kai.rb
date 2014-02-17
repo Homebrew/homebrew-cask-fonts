9837a464a32245c9c35e31a91032a97eefa02bdc
Roland Walker <walker@pobox.com>
2014-02-17 10:41:59 -0500

    add font cwTeX Q Kai

---
+++ Casks/font-cwtex-q-kai.rb
@@ -0,0 +1,7 @@
+class FontCwtexQKai < Cask
+  url 'https://cwtex-q-fonts.googlecode.com/svn-history/r37/trunk/ttf/cwTeXQKai-Medium.ttf'
+  homepage 'https://code.google.com/p/cwtex-q-fonts/'
+  version '0.1'
+  sha256 'c48063a3ff6834a8e68167438f560ce8cfd73fcd8c350addec0fdcabad2df6d5'
+  font 'cwTeXQKai-Medium.ttf'
+end
