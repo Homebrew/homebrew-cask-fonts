628ec98981bf6f1704dbdbe70c9086bd6810ec86
Roland Walker <walker@pobox.com>
2014-02-17 10:46:38 -0500

    add font-exo2.rb, source: Google

---
+++ Casks/font-exo2.rb
@@ -0,0 +1,27 @@
+class FontExo2 < Cask
+  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/exo2',
+      :using      => :svn,
+      :revision   => '50',
+      :trust_cert => true
+  homepage 'http://www.google.com/fonts/specimen/Exo%202'
+  version '1.001'
+  sha256 'df20a984b3f6e96dbfda8eb80ae7117c40d824a755983bb478524a9140731a47'
+  font 'Exo2-Black.ttf'
+  font 'Exo2-BlackItalic.ttf'
+  font 'Exo2-Bold.ttf'
+  font 'Exo2-BoldItalic.ttf'
+  font 'Exo2-ExtraBold.ttf'
+  font 'Exo2-ExtraBoldItalic.ttf'
+  font 'Exo2-ExtraLight.ttf'
+  font 'Exo2-ExtraLightItalic.ttf'
+  font 'Exo2-Italic.ttf'
+  font 'Exo2-Light.ttf'
+  font 'Exo2-LightItalic.ttf'
+  font 'Exo2-Medium.ttf'
+  font 'Exo2-MediumItalic.ttf'
+  font 'Exo2-Regular.ttf'
+  font 'Exo2-SemiBold.ttf'
+  font 'Exo2-SemiBoldItalic.ttf'
+  font 'Exo2-Thin.ttf'
+  font 'Exo2-ThinItalic.ttf'
+end
