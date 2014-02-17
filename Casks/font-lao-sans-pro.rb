1a19c94dd1d4b730c2fedc269f4fe73c74084802
Roland Walker <walker@pobox.com>
2014-02-17 11:02:30 -0500

    add font-lao-sans-pro.rb, source: Google

---
+++ Casks/font-lao-sans-pro.rb
@@ -0,0 +1,7 @@
+class FontLaoSansPro < Cask
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/laosanspro/LaoSansPro-Regular.ttf'
+  homepage 'https://www.google.com/fonts/earlyaccess'
+  version '1.0'
+  sha256 '1b9a64e11d0812c8641e23062ef0805df9a1a4ae8db1414596c82fc41f8e59ea'
+  font 'LaoSansPro-Regular.ttf'
+end
