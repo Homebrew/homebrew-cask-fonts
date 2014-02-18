ab51d12923a87e02b8bf6e0fbbf85d981ee18116
Roland Walker <walker@pobox.com>
2014-02-18 10:10:14 -0500

    add font Tuffy

---
+++ Casks/font-tuffy.rb
@@ -0,0 +1,10 @@
+class FontTuffy < Cask
+  url 'http://tulrich.com/fonts/tuffy-20120614.tar.gz'
+  homepage 'http://tulrich.com/fonts/'
+  version '1.28'
+  sha256 'c6f9eb7a878e270b96fbb5dae7783be29710b4405c76e856de60dcfc96386e0b'
+  font 'tuffy-20120614/Tuffy.otf'
+  font 'tuffy-20120614/Tuffy_Bold.otf'
+  font 'tuffy-20120614/Tuffy_Bold_Italic.otf'
+  font 'tuffy-20120614/Tuffy_Italic.otf'
+end
