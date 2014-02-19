636152af364adcb9bf0a2e4b3f6eea47dfa7c68c
Roland Walker <walker@pobox.com>
2014-02-19 08:59:47 -0500

    add font Libre Caslon Text

---
+++ Casks/font-libre-caslon-text.rb
@@ -0,0 +1,9 @@
+class FontLibreCaslonText < Cask
+  url 'http://www.impallari.com/media/uploads/prosources/update-94-source.zip'
+  homepage 'http://www.impallari.com/projects/overview/libre-caslon-display-and-text'
+  version '1.000'
+  sha256 '491f478905d9591f6858d865bdc653798c9204d01ab1fbc03477e5f7adc09a74'
+  font 'Libre Caslon Text v1.0/LibreCaslonText-Bold.ttf'
+  font 'Libre Caslon Text v1.0/LibreCaslonText-Italic.ttf'
+  font 'Libre Caslon Text v1.0/LibreCaslonText-Regular.ttf'
+end
