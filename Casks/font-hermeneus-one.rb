0bb50b2fd3f50588bca2bb732695b8ecbc2425fc
Roland Walker <walker@pobox.com>
2014-02-19 09:07:41 -0500

    add font Hermeneus One

---
+++ Casks/font-hermeneus-one.rb
@@ -0,0 +1,7 @@
+class FontHermeneusOne < Cask
+  url 'http://www.impallari.com/media/uploads/commentsources/comment-239-source.zip'
+  homepage 'http://www.impallari.com/projects/overview/hermeneus'
+  version '1.002'
+  sha256 'c76245c77bcbe570e9146352de5bf6ae9a3c1c475ddd36cb6c50272e4000e4c1'
+  font 'HermeneusOne-v1.2/HermeneusOne-Regular.ttf'
+end
