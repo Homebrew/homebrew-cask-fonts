eaa4444fe23366cbaef20935fc1f53f20973c122
David Dinh <david@dinh.io>
2014-02-26 12:53:02 -0800

    Add Fantasque Sans Mono

---
+++ Casks/font-fantasque-sans-mono.rb
@@ -0,0 +1,10 @@
+class FontFantasqueSansMono < Cask
+    url 'https://github.com/belluzj/fantasque-sans/releases/download/v1.6.2/FantasqueSansMono.zip'
+    homepage 'https://github.com/belluzj/fantasque-sans'
+    version '1.6.2'
+    sha256 '06262d8e0d30ec85cebd3a2cd69c041fb35d48c159ef124379cf03d5c99a215c'
+    font 'OTF/FantasqueSansMono-Bold.otf'
+    font 'OTF/FantasqueSansMono-BoldItalic.otf'
+    font 'OTF/FantasqueSansMono-RegItalic.otf'
+    font 'OTF/FantasqueSansMono-Regular.otf'
+end
