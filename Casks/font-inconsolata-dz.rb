e164642154ab7b317caa5c636c1b463cc9da182a
Amiel Martin <amiel@carnesmedia.com>
2014-02-26 16:41:02 -0800

    Add Inconsolata-dz

---
+++ Casks/font-inconsolata-dz.rb
@@ -0,0 +1,7 @@
+class FontInconsolataDz < Cask
+  url 'http://media.nodnod.net/Inconsolata-dz.otf.zip'
+  homepage 'http://nodnod.net/2009/feb/12/adding-straight-single-and-double-quotes-inconsola/'
+  version 'latest'
+  no_checksum
+  font 'Inconsolata-dz.otf'
+end
