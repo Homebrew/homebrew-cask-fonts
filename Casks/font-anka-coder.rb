c2882fa27e8dd921104405ac37bcc394ac416e1c
aereal <aereal@aereal.org>
2014-03-03 01:41:13 +0900

    Add Anka/Coder font

---
+++ Casks/font-anka-coder.rb
@@ -0,0 +1,10 @@
+class FontAnkaCoder < Cask
+  url 'https://anka-coder-fonts.googlecode.com/files/AnkaCoder.1.100.zip'
+  homepage 'https://code.google.com/p/anka-coder-fonts/'
+  version '1.100'
+  sha256 '3c3cd48ed57796cd490a2c22614c73e32ad202790d10e9af3d632cd985c0e172'
+  font 'AnkaCoder-b.ttf'
+  font 'AnkaCoder-bi.ttf'
+  font 'AnkaCoder-i.ttf'
+  font 'AnkaCoder-r.ttf'
+end
