e4a0dcf3001aea9b767f079b642f887395be8ef4
Roland Walker <walker@pobox.com>
2014-03-14 09:53:03 -0400

    Update Vollkorn to sha256 checksums

--- Casks/font-vollkorn.rb
+++ Casks/font-vollkorn.rb
@@ -2,7 +2,7 @@ class FontVollkorn < Cask
   url 'http://vollkorn-typeface.com/download/vollkorn-3-003.zip'
   homepage 'http://vollkorn-typeface.com/'
   version '3.003'
-  sha1 '13a1f71fe1fa1fbf4c89641e390fd3eb146a9dbb'
+  sha256 'dcf7913f1f29f044950984276e1119ef64e8a158f48e3ae159b2e152ce2b3e5f'
   font 'vollkorn-3-003/PS-OTF/Vollkorn-Bold.otf'
   font 'vollkorn-3-003/PS-OTF/Vollkorn-BoldItalic.otf'
   font 'vollkorn-3-003/PS-OTF/Vollkorn-Italic.otf'
