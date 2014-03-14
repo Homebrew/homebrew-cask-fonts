2991dfeceaf7bc15c179ae8b25fd4602b02a89bf
Roland Walker <walker@pobox.com>
2014-03-14 09:52:53 -0400

    Update PT mono to sha256 checksums

--- Casks/font-pt-mono.rb
+++ Casks/font-pt-mono.rb
@@ -2,7 +2,7 @@ class FontPtMono < Cask
   url 'http://www.fontstock.com/public/PTMono.zip'
   homepage 'http://www.paratype.com/public/'
   version '1.002'
-  sha1 '1e06f32df070d93adb1b5f5b515d8af770b6cd28'
+  sha256 '93c0ca1d9873ecc7c70131fa763aaaaa24345fc8b8f193ea2175b0857823d58a'
   font 'PTM55F.ttf'
   font 'PTM75F.ttf'
 end
