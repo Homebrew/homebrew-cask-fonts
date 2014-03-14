888e7bb6c5c08c738043ef52d97efaa0d99c252e
Roland Walker <walker@pobox.com>
2014-03-14 09:52:59 -0400

    Update Source Code Pro to sha256 checksums

--- Casks/font-source-code-pro.rb
+++ Casks/font-source-code-pro.rb
@@ -2,7 +2,7 @@ class FontSourceCodePro < Cask
   url 'http://downloads.sourceforge.net/sourceforge/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip'
   homepage 'http://store1.adobe.com/cfusion/store/html/index.cfm?store=OLS-US&event=displayFontPackage&code=1960'
   version '1.017'
-  sha1 'c0e3f6f8e25b434c0e28a817539632f8a5ecb9e5'
+  sha256 '8136b4686309c428ef073356ab178c2f7e8f7b6fadd5a6c61b6a20646377b21f'
   font 'SourceCodePro_FontsOnly-1.017/OTF/SourceCodePro-Black.otf'
   font 'SourceCodePro_FontsOnly-1.017/OTF/SourceCodePro-Bold.otf'
   font 'SourceCodePro_FontsOnly-1.017/OTF/SourceCodePro-ExtraLight.otf'
