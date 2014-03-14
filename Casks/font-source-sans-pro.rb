35504e9782749bd9d8e743a745b06d90c06e02c5
Roland Walker <walker@pobox.com>
2014-03-14 09:53:00 -0400

    Update Source Sans Pro to sha256 checksums

--- Casks/font-source-sans-pro.rb
+++ Casks/font-source-sans-pro.rb
@@ -2,7 +2,7 @@ class FontSourceSansPro < Cask
   url 'http://downloads.sourceforge.net/sourceforge/sourcesans.adobe/SourceSansPro_FontsOnly-1.050.zip'
   homepage 'http://store1.adobe.com/cfusion/store/html/index.cfm?store=OLS-US&event=displayFontPackage&code=1959'
   version '1.050'
-  sha1 'f3af1201c80a86f1dd6e045a565c96bdd12d445f'
+  sha256 'c379f5ef53981757b9aa115cfd0775e9fa92175f5d611ab3bbe57689fa3c5f00'
   font 'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-Black.otf'
   font 'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-BlackIt.otf'
   font 'SourceSansPro_FontsOnly-1.050/OTF/SourceSansPro-Bold.otf'
