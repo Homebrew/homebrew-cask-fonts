5b9c47d4a2885d28b918c51fe555705c3d61a47e
Jarin Udom <jarin@robotmodehq.com>
2014-04-11 11:28:02 -0700

    Add the Comic Neue font
    Public domain, available at http://comicneue.com/

---
+++ Casks/font-comic-neue.rb
@@ -0,0 +1,18 @@
+class FontComicNeue < Cask
+  url 'http://comicneue.com/comic-neue-1.1.zip'
+  homepage 'http://comicneue.com/'
+  version '1.1'
+  sha256 'e4c2bd402348d73d86f87d930670748e2963b6c29be2ebf6df407de1425ff6b6'
+  font 'comic-neue-1.1/ComicNeue-Angular-Bold-Oblique.ttf'
+  font 'comic-neue-1.1/ComicNeue-Angular-Bold.ttf'
+  font 'comic-neue-1.1/ComicNeue-Angular-Light-Oblique.ttf'
+  font 'comic-neue-1.1/ComicNeue-Angular-Light.ttf'
+  font 'comic-neue-1.1/ComicNeue-Angular-Regular-Oblique.ttf'
+  font 'comic-neue-1.1/ComicNeue-Angular-Regular.ttf'
+  font 'comic-neue-1.1/ComicNeue-Bold-Oblique.ttf'
+  font 'comic-neue-1.1/ComicNeue-Bold.ttf'
+  font 'comic-neue-1.1/ComicNeue-Light-Oblique.ttf'
+  font 'comic-neue-1.1/ComicNeue-Light.ttf'
+  font 'comic-neue-1.1/ComicNeue-Regular-Oblique.ttf'
+  font 'comic-neue-1.1/ComicNeue-Regular.ttf'
+end
