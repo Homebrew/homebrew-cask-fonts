396a208a04e252339eee98da5b852d2287d477bc
ndr <ndr@qef.io>
2014-04-27 22:11:01 +0200

    Add font Disclaimer by FontFabric

---
+++ Casks/font-disclaimer.rb
@@ -0,0 +1,8 @@
+class FontDisclaimer < Cask
+  url 'http://fontfabric.com/downfont/disclaimer.zip'
+  homepage 'http://fontfabric.com/disclaimer-free-font/'
+  version 'latest'
+  no_checksum
+  font 'Disclaimer-Plain.otf'
+  font 'Disclaimer-Classic.otf'
+end
