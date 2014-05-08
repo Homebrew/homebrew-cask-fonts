cd0952c226b7db22fb5ee362cd5123de1147a8f2
Roland Walker <walker@pobox.com>
2014-05-08 09:13:36 -0400

    update Meslo to use sha256 checksum
    sha1 is deprecated

--- Casks/font-meslo-lg.rb
+++ Casks/font-meslo-lg.rb
@@ -2,7 +2,7 @@ class FontMesloLg < Cask
   url 'https://github.com/andreberg/Meslo-Font/raw/master/dist/v1.2.1/Meslo%20LG%20v1.2.1.zip'
   homepage 'https://github.com/andreberg/Meslo-Font'
   version '1.21'
-  sha1 '23cc2b1f4ed16f82054b127a8d3653516731f919'
+  sha256 'd0bcb7668dda8fa1a0f8162d626adb434c32854e243b5bd52a717cf569af08d0'
   font 'Meslo LG v1.2.1/MesloLGL-Bold.ttf'
   font 'Meslo LG v1.2.1/MesloLGL-BoldItalic.ttf'
   font 'Meslo LG v1.2.1/MesloLGL-Italic.ttf'
