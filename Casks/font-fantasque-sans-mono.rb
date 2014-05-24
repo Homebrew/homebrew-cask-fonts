cb772a214432df18fd28059909cbb023c6d75441
ndr <ndr@qef.io>
2014-05-24 19:27:58 +0200

    Update Fantasque Sans Mono to 1.6.3

--- Casks/font-fantasque-sans-mono.rb
+++ Casks/font-fantasque-sans-mono.rb
@@ -1,8 +1,8 @@
 class FontFantasqueSansMono < Cask
-    url 'https://github.com/belluzj/fantasque-sans/releases/download/v1.6.2/FantasqueSansMono.zip'
+    url 'https://github.com/belluzj/fantasque-sans/releases/download/v1.6.3/FantasqueSansMono.tar.gz'
     homepage 'https://github.com/belluzj/fantasque-sans'
-    version '1.6.2'
-    sha256 '06262d8e0d30ec85cebd3a2cd69c041fb35d48c159ef124379cf03d5c99a215c'
+    version '1.6.3'
+    sha256 '1dce16461d84fbd9af37c2a8b36ccd7534e3ed70415f1398071aa5a4a16e907b'
     font 'OTF/FantasqueSansMono-Bold.otf'
     font 'OTF/FantasqueSansMono-BoldItalic.otf'
     font 'OTF/FantasqueSansMono-RegItalic.otf'
