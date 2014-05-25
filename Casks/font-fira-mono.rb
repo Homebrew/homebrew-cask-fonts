ada508d74001bb36bc0965fcc626831e8610b573
Dan Barber <hello@danbarber.me>
2014-05-25 10:25:06 +0100

    Add Fira Mono.

---
+++ Casks/font-fira-mono.rb
@@ -0,0 +1,8 @@
+class FontFiraMono < Cask
+  url 'http://dev.carrois.com/wordpress/wp-content/uploads/downloads/fira_3105/FiraMono3105.zip'
+  homepage 'http://dev.carrois.com/fira-3-1/'
+  version '3.105'
+  sha256 '13e70891bbf2e479b9d379539b780224c8214362d9cb18c5618ea313a19a498d'
+  font 'FiraMono3105/OTF/FiraMono-Regular.otf'
+  font 'FiraMono3105/OTF/FiraMono-Bold.otf'
+end
