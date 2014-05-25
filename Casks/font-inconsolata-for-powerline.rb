13bbfa3ef205b93bf1d4e6e5dbf7641d6b92c671
Paul Williamson <squarefrog@gmail.com>
2014-05-25 09:41:55 +0100

    Added Inconsolata for Powerline

---
+++ Casks/font-inconsolata-for-powerline.rb
@@ -0,0 +1,7 @@
+class FontInconsolataForPowerline < Cask
+  url 'https://github.com/Lokaltog/powerline-fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf'
+  homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/Inconsolata'
+  version 'latest'
+  sha256 :no_check
+  font 'Inconsolata for Powerline.otf'
+end
