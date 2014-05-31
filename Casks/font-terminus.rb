a166fd857e6a32d2f562c0272118433e540e10a7
Beng Hee Eu <beng@benghee.eu>
2014-05-31 14:02:17 +0800

    Terminus TTF 4.39

--- Casks/font-terminus.rb
+++ Casks/font-terminus.rb
@@ -1,7 +1,9 @@
 class FontTerminus < Cask
-  url 'http://misc.nybergh.net/pub/fonts/terminus/ttf/Terminus.ttf'
-  homepage 'http://misc.nybergh.net/pub/fonts/terminus/ttf/'
-  version 'latest'
-  sha256 :no_check
-  font 'Terminus.ttf'
+  url 'http://files.ax86.net/terminus-ttf/files/4.39/terminus-ttf-4.39.zip'
+  homepage 'http://files.ax86.net/terminus-ttf/'
+  version '4.39'
+  sha256 '5d8a7075757fd1a66012fafc6bd761e5b758573ea66bc1197025f1d4eb343383' 
+  font 'terminus-ttf-4.39/TerminusTTF-4.39.ttf'
+  font 'terminus-ttf-4.39/TerminusTTF-Bold-4.39.ttf'
+  font 'terminus-ttf-4.39/TerminusTTF-Italic-4.39.ttf'
 end
