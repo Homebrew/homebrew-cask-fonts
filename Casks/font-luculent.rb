c4835c787e3a8ecf0d50826b1a2e9a912db271bf
Beng Hee Eu <beng@benghee.eu>
2014-05-31 13:30:00 +0800

    Luculent 1.0.0-845fa02f9341

---
+++ Casks/font-luculent.rb
@@ -0,0 +1,10 @@
+class FontLuculent < Cask
+  url 'http://eastfarthing.com/luculent/luculent.tar.xz'
+  homepage 'http://eastfarthing.com/luculent/'
+  version '1.0.0-845fa02f9341'
+  sha256 'ebb1cab6fef7ec6dc27a12be48c537ab83f95928503553acef3798c9e608566a'
+  font 'luculent/luculent.ttf'
+  font 'luculent/luculentb.ttf'
+  font 'luculent/luculentbi.ttf'
+  font 'luculent/luculenti.ttf'
+end
