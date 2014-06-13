dbe7b77b645c9b88c4b26c8a90c278802e7d18f0
ndr <ndr@qef.io>
2014-06-13 19:24:21 +0200

    Merge Fira Sans and Mono as Fira Sans v3.108
    Separate packages no longer available upstream.

--- Casks/font-fira-mono.rb
+++
@@ -1,8 +0,0 @@
-class FontFiraMono < Cask
-  url 'http://dev.carrois.com/wordpress/wp-content/uploads/downloads/fira_3_1/FiraMono3106.zip'
-  homepage 'http://dev.carrois.com/fira-3-1/'
-  version '3.106'
-  sha256 '1c0aca0b346f3353c8cbb52dcbe0e98986ab76e3b50c9021547d328c4320803a'
-  font 'FiraMono3106/OTF/FiraMono-Bold.otf'
-  font 'FiraMono3106/OTF/FiraMono-Regular.otf'
-end
