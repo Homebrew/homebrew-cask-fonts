99057badae0bb23ee4595435e08804b7d26b276d
Mathieu Legrand <mathieu@legrand.im>
2014-06-26 10:10:27 +0800

    Update link to FreeBSD distribution

--- Casks/font-code2000.rb
+++ Casks/font-code2000.rb
@@ -1,6 +1,6 @@
 class FontCode2000 < Cask
-  url 'http://ftp.freebsd.org/pub/FreeBSD/ports/local-distfiles/thierry/code2000-1.171/CODE2000.ZIP'
-  homepage 'http://ftp.freebsd.org/pub/FreeBSD/ports/local-distfiles/thierry/code2000-1.171/'
+  url 'http://distcache.freebsd.org/ports-distfiles/code2000-1.171/CODE2000.ZIP'
+  homepage 'http://ftp-archive.freebsd.org/pub/FreeBSD/'
   version '1.171'
   sha256 'af00db49179aaea66494a6b9cad3ae7f2b822bf650df3133c07f9c3209cbc8e2'
   font 'CODE2000.TTF'
