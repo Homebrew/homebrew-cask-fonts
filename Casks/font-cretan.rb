16d7ace0fbcdea988b2693ed678ba8de88658e04
Roland Walker <walker@pobox.com>
2014-06-26 08:14:22 -0400

    add font Cretan

---
+++ Casks/font-cretan.rb
@@ -0,0 +1,7 @@
+class FontCretan < Cask
+  url 'http://users.teilar.gr/~g1951d/Cretan.zip'
+  homepage 'http://users.teilar.gr/~g1951d/'
+  version 'latest'
+  sha256 :no_check
+  font 'Cretan_hint.ttf'
+end
