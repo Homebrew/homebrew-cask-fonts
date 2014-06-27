42918a38e511fe046757859ab13bb06303b74fa7
Yusuke Nakamura <yusuke1994525@gmail.com>
2014-06-27 17:33:12 +0900

    fix version

--- Casks/font-kibitaki.rb
+++ Casks/font-kibitaki.rb
@@ -1,7 +1,7 @@
 class FontKibitaki < Cask
   url 'https://github.com/Koruri/kibitaki/archive/master.zip'
   homepage 'http://kibitaki.lindwurm.biz/'
-  version 'Pre-release'
+  version 'latest'
   sha256 '142c68ae3e322e866577164113b532f648095a218ab284c2ab4ca76ca4888ddc'
   font 'kibitaki-master/Kibitaki-Bold.ttf'
   font 'kibitaki-master/Kibitaki-Light.ttf'
