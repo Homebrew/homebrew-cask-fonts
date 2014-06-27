663addf2a8a2dd06c13d9dd08c5704ab51fbcb3b
Yusuke Nakamura <yusuke1994525@gmail.com>
2014-06-27 17:11:51 +0900

    add font Kibitaki

---
+++ Casks/font-kibitaki.rb
@@ -0,0 +1,10 @@
+class FontKibitaki < Cask
+  url 'https://github.com/Koruri/kibitaki/archive/master.zip'
+  homepage 'http://kibitaki.lindwurm.biz/'
+  version 'Pre-release'
+  sha256 '142c68ae3e322e866577164113b532f648095a218ab284c2ab4ca76ca4888ddc'
+  font 'Kibitaki-Bold.ttf'
+  font 'Kibitaki-Light.ttf'
+  font 'Kibitaki-Medium.ttf'
+  font 'Kibitaki-Regular.ttf'
+end
