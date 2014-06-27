90ba37a5b74c31a79344761b6f3fcce0435a3783
Yusuke Nakamura <yusuke1994525@gmail.com>
2014-06-27 17:22:25 +0900

    fix font path

--- Casks/font-kibitaki.rb
+++ Casks/font-kibitaki.rb
@@ -3,8 +3,8 @@ class FontKibitaki < Cask
   homepage 'http://kibitaki.lindwurm.biz/'
   version 'Pre-release'
   sha256 '142c68ae3e322e866577164113b532f648095a218ab284c2ab4ca76ca4888ddc'
-  font 'Kibitaki-Bold.ttf'
-  font 'Kibitaki-Light.ttf'
-  font 'Kibitaki-Medium.ttf'
-  font 'Kibitaki-Regular.ttf'
+  font 'kibitaki-master/Kibitaki-Bold.ttf'
+  font 'kibitaki-master/Kibitaki-Light.ttf'
+  font 'kibitaki-master/Kibitaki-Medium.ttf'
+  font 'kibitaki-master/Kibitaki-Regular.ttf'
 end
