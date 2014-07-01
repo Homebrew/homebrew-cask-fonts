a47abcd4c9ca8991ac98e116a560d1456f8f174c
Nicolas Brousse <nicolas-brousse@users.noreply.github.com>
2014-07-01 18:47:58 +0200

    Create font-open-iconic.rb

---
+++ Casks/font-open-iconic.rb
@@ -0,0 +1,8 @@
+class FontOpenIconic < Cask
+  url 'https://github.com/iconic/open-iconic/archive/1.1.0.zip'
+  homepage 'https://useiconic.com/open/'
+  version '1.1.0'
+  sha256 :no_check
+
+  font 'open-iconic-1.1.0/font/fonts/open-iconic.ttf'
+end
