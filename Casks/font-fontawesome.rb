a0e54749c929343934aabd8d16c6dbaff7f05f11
Nicolas Brousse <nicolas-brousse@users.noreply.github.com>
2014-07-01 23:47:53 +0200

    Debug sha256 for font-fontawesome.rb

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -2,6 +2,6 @@ class FontFontawesome < Cask
   url 'http://fortawesome.github.io/Font-Awesome/assets/font-awesome-4.1.0.zip'
   homepage 'http://fortawesome.github.io/Font-Awesome/'
   version '4.1.0'
-  sha256 '4980e584bd84437f25788ef45f79fca46dc253b542360f12b1f3b35824a0e27f'
+  sha256 '76b0117979df00caa71c4a1ae3fb41326590ea303747d1f48d611735e749657d'
   font 'font-awesome-4.1.0/fonts/FontAwesome.otf'
 end
