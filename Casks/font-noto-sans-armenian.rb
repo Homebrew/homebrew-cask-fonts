579213fa5da2e80f30006d359df977be0cf7632a
Roland Walker <walker@pobox.com>
2014-07-16 09:52:37 -0400

    add font Font Noto Sans Armenian

---
+++ Casks/font-noto-sans-armenian.rb
@@ -0,0 +1,10 @@
+class FontNotoSansArmenian < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansArmenian-hinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansArmenian-Regular.ttf'
+  font 'NotoSansArmenian-Bold.ttf'
+end
