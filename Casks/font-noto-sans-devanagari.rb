f43635c29275f0c646beee2e66e0bcc591270cfd
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Devanagari

---
+++ Casks/font-noto-sans-devanagari.rb
@@ -0,0 +1,10 @@
+class FontNotoSansDevanagari < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansDevanagari-hinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansDevanagari-Bold.ttf'
+  font 'NotoSansDevanagari-Regular.ttf'
+end
