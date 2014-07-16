8eea74346746f220b6c78c1570f618145aecc5f8
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Myanmar

---
+++ Casks/font-noto-sans-myanmar.rb
@@ -0,0 +1,10 @@
+class FontNotoSansMyanmar < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansMyanmar-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansMyanmar-Bold.ttf'
+  font 'NotoSansMyanmar-Regular.ttf'
+end
