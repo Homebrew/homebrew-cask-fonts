b4987bac0f2c6273e3bf5a931ee2fcc7a99515f6
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Vai

---
+++ Casks/font-noto-sans-vai.rb
@@ -0,0 +1,9 @@
+class FontNotoSansVai < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansVai-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansVai-Regular.ttf'
+end
