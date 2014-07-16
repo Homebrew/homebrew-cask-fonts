a6ef672b2e14b75f143a635bb0135b5b3da4cf5b
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Sundanese

---
+++ Casks/font-noto-sans-sundanese.rb
@@ -0,0 +1,9 @@
+class FontNotoSansSundanese < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansSundanese-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansSundanese-Regular.ttf'
+end
