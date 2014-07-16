00ed775afbc3e45752d751e880dbd7ec9f331649
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Osmanya

---
+++ Casks/font-noto-sans-osmanya.rb
@@ -0,0 +1,9 @@
+class FontNotoSansOsmanya < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansOsmanya-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansOsmanya-Regular.ttf'
+end
