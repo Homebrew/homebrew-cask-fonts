a99597d32a96295a9dd165ffce85096000baf951
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Tagbanwa

---
+++ Casks/font-noto-sans-tagbanwa.rb
@@ -0,0 +1,9 @@
+class FontNotoSansTagbanwa < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansTagbanwa-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansTagbanwa-Regular.ttf'
+end
