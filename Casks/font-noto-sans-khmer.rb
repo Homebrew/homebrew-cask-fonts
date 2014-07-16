822df047a1823cafbb249577d86a34077337157b
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Khmer

---
+++ Casks/font-noto-sans-khmer.rb
@@ -0,0 +1,10 @@
+class FontNotoSansKhmer < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansKhmer-hinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansKhmer-Regular.ttf'
+  font 'NotoSansKhmer-Bold.ttf'
+end
