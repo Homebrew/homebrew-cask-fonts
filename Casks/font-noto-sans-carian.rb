0e68c2133dfd65ec6ab7145de9837a5dde0881c5
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Carian

---
+++ Casks/font-noto-sans-carian.rb
@@ -0,0 +1,9 @@
+class FontNotoSansCarian < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansCarian-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansCarian-Regular.ttf'
+end
