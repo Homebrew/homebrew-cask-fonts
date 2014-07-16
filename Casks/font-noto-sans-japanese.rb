42fe17c4a2f1f3e9ec7f98731c87a338c23c1ef7
n0ts <me@n0ts.org>
2014-07-16 16:55:11 +0900

    Add new Google Noto Fonts

---
+++ Casks/font-noto-sans-japanese.rb
@@ -0,0 +1,13 @@
+class FontNotoSansJapanese < Cask
+  url 'http://www.google.com/get/noto/pkgs/NotoSansJapanese-hinted.zip'
+  homepage 'http://www.google.com/get/noto/#/family/noto-sans-jpan'
+  version 'latest'
+  sha256 :no_check
+  font 'NotoSansJP-Black.otf'
+  font 'NotoSansJP-Bold.otf'
+  font 'NotoSansJP-DemiLight.otf'
+  font 'NotoSansJP-Light.otf'
+  font 'NotoSansJP-Medium.otf'
+  font 'NotoSansJP-Regular.otf'
+  font 'NotoSansJP-Thin.otf'
+end
