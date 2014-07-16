44ed782e5f8edef686e526cb2d5c3d32f977d432
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Malayalam

---
+++ Casks/font-noto-sans-malayalam.rb
@@ -0,0 +1,10 @@
+class FontNotoSansMalayalam < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansMalayalam-hinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansMalayalam-Regular.ttf'
+  font 'NotoSansMalayalam-Bold.ttf'
+end
