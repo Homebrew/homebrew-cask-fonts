b6071786b5a91731e51d3c1bfee02bd3cdadc0c0
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Samaritan

---
+++ Casks/font-noto-sans-samaritan.rb
@@ -0,0 +1,9 @@
+class FontNotoSansSamaritan < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansSamaritan-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansSamaritan-Regular.ttf'
+end
