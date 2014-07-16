53b314d082e964b93f00159c905ae9a369328903
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Phoenician

---
+++ Casks/font-noto-sans-phoenician.rb
@@ -0,0 +1,9 @@
+class FontNotoSansPhoenician < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansPhoenician-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansPhoenician-Regular.ttf'
+end
