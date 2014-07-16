92d8c95b7bcc1ff57c1f2c00e3eb0fdcbd762ae6
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Tagalog

---
+++ Casks/font-noto-sans-tagalog.rb
@@ -0,0 +1,9 @@
+class FontNotoSansTagalog < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansTagalog-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansTagalog-Regular.ttf'
+end
