17d6532dce975fb062d74480354b969c71173014
Roland Walker <walker@pobox.com>
2014-07-16 09:52:37 -0400

    add font Font Noto Sans Batak

---
+++ Casks/font-noto-sans-batak.rb
@@ -0,0 +1,9 @@
+class FontNotoSansBatak < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansBatak-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansBatak-Regular.ttf'
+end
