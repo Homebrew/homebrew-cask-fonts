2f32bbe11aa1599695ec909d4bb7150a2cd36996
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Kayah Li

---
+++ Casks/font-noto-sans-kayah-li.rb
@@ -0,0 +1,9 @@
+class FontNotoSansKayahLi < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansKayahLi-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansKayahLi-Regular.ttf'
+end
