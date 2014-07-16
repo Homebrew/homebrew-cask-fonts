873de90748827042b604232f67beca9abfa1fd89
Roland Walker <walker@pobox.com>
2014-07-16 09:52:37 -0400

    add font Font Noto Sans Buhid

---
+++ Casks/font-noto-sans-buhid.rb
@@ -0,0 +1,9 @@
+class FontNotoSansBuhid < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansBuhid-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansBuhid-Regular.ttf'
+end
