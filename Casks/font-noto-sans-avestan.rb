f136a9c903b99851e9bba28c58a09a9aa1ccddc9
Roland Walker <walker@pobox.com>
2014-07-16 09:52:37 -0400

    add font Font Noto Sans Avestan

---
+++ Casks/font-noto-sans-avestan.rb
@@ -0,0 +1,9 @@
+class FontNotoSansAvestan < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansAvestan-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansAvestan-Regular.ttf'
+end
