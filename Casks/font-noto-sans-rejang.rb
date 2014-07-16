8708cd56ebfe95ac5503732f67249c58df170e8f
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Rejang

---
+++ Casks/font-noto-sans-rejang.rb
@@ -0,0 +1,9 @@
+class FontNotoSansRejang < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansRejang-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansRejang-Regular.ttf'
+end
