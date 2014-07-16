4756fb40815ff1786eff77bad0ab518adde3fbf5
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Gurmukhi

---
+++ Casks/font-noto-sans-gurmukhi.rb
@@ -0,0 +1,10 @@
+class FontNotoSansGurmukhi < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansGurmukhi-hinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansGurmukhi-Bold.ttf'
+  font 'NotoSansGurmukhi-Regular.ttf'
+end
