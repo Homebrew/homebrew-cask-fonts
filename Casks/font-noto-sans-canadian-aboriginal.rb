bbed2ff45e554ebb297f37e5da40683e9ef985b0
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Canadian Aboriginal

---
+++ Casks/font-noto-sans-canadian-aboriginal.rb
@@ -0,0 +1,9 @@
+class FontNotoSansCanadianAboriginal < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansCanadianAboriginal-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansCanadianAboriginal-Regular.ttf'
+end
