b3376982f067fc0c478e89ea9adcdf7a6fc69270
Roland Walker <walker@pobox.com>
2014-07-16 09:52:37 -0400

    add font Font Noto Sans Brahmi

---
+++ Casks/font-noto-sans-brahmi.rb
@@ -0,0 +1,9 @@
+class FontNotoSansBrahmi < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansBrahmi-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansBrahmi-Regular.ttf'
+end
