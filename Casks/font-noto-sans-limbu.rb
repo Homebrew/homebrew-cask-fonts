656acc9a7aff3b5f2462feda0bb00c72b2b35723
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Limbu

---
+++ Casks/font-noto-sans-limbu.rb
@@ -0,0 +1,9 @@
+class FontNotoSansLimbu < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansLimbu-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansLimbu-Regular.ttf'
+end
