b7bedec0be3fd84c7a1d475d81ab34e7d3a0ba3d
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Old Persian

---
+++ Casks/font-noto-sans-old-persian.rb
@@ -0,0 +1,9 @@
+class FontNotoSansOldPersian < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansOldPersian-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansOldPersian-Regular.ttf'
+end
