5103b01905ca26519f1554e6aa3114b35d979eb3
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Ogham

---
+++ Casks/font-noto-sans-ogham.rb
@@ -0,0 +1,9 @@
+class FontNotoSansOgham < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansOgham-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansOgham-Regular.ttf'
+end
