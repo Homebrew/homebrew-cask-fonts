8bee6b2aecb8a0e2ae1f955560f28e8836a656b9
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Mandaic

---
+++ Casks/font-noto-sans-mandaic.rb
@@ -0,0 +1,9 @@
+class FontNotoSansMandaic < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansMandaic-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansMandaic-Regular.ttf'
+end
