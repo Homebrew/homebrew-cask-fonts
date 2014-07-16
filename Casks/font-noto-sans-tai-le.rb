216c623716c73ca9e8159112fb940bd9c18b102b
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Tai Le

---
+++ Casks/font-noto-sans-tai-le.rb
@@ -0,0 +1,9 @@
+class FontNotoSansTaiLe < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansTaiLe-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansTaiLe-Regular.ttf'
+end
