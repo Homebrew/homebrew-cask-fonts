004f77c174454f103cb7bd22e4ae5b0174c54fa1
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Yi

---
+++ Casks/font-noto-sans-yi.rb
@@ -0,0 +1,9 @@
+class FontNotoSansYi < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansYi-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansYi-Regular.ttf'
+end
