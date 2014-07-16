b525bd200c848e65f54af35f18704d50d1085ecd
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Linear B

---
+++ Casks/font-noto-sans-linear-b.rb
@@ -0,0 +1,9 @@
+class FontNotoSansLinearB < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansLinearB-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansLinearB-Regular.ttf'
+end
