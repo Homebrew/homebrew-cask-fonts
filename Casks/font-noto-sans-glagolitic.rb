098d3964cd323b73121c108487fa65c5028a30d1
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Glagolitic

---
+++ Casks/font-noto-sans-glagolitic.rb
@@ -0,0 +1,9 @@
+class FontNotoSansGlagolitic < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansGlagolitic-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansGlagolitic-Regular.ttf'
+end
