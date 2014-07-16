9efb99a1ad4217ba92c7e4bdaae4ae0fb364845c
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Hanunoo

---
+++ Casks/font-noto-sans-hanunoo.rb
@@ -0,0 +1,9 @@
+class FontNotoSansHanunoo < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansHanunoo-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansHanunoo-Regular.ttf'
+end
