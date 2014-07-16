10772604958fe15f6bef6be1dae5b69bdbd19dba
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Lepcha

---
+++ Casks/font-noto-sans-lepcha.rb
@@ -0,0 +1,9 @@
+class FontNotoSansLepcha < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansLepcha-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansLepcha-Regular.ttf'
+end
