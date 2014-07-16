5e94c93f49f3630c5b4068a06bbb93062d5590ab
Roland Walker <walker@pobox.com>
2014-07-16 09:52:39 -0400

    add font Font Noto Sans Sumero- Akkadian Cuneiform

---
+++ Casks/font-noto-sans-sumero--akkadian-cuneiform.rb
@@ -0,0 +1,9 @@
+class FontNotoSansSumero-AkkadianCuneiform < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansSumero-AkkadianCuneiform-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansSumeroAkkadianCuneiform-Regular.ttf'
+end
