78a562b7f51e99a2fc2ea5552837a573e763149f
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Kannada

---
+++ Casks/font-noto-sans-kannada.rb
@@ -0,0 +1,10 @@
+class FontNotoSansKannada < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansKannada-hinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansKannada-Bold.ttf'
+  font 'NotoSansKannada-Regular.ttf'
+end
