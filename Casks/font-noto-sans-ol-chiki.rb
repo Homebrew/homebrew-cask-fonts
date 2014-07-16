5098eaffd575b72fbf51d774c2d30a588a37b6a4
Roland Walker <walker@pobox.com>
2014-07-16 09:52:38 -0400

    add font Font Noto Sans Ol Chiki

---
+++ Casks/font-noto-sans-ol-chiki.rb
@@ -0,0 +1,9 @@
+class FontNotoSansOlChiki < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://www.google.com/get/noto/pkgs/NotoSansOlChiki-unhinted.zip'
+  homepage 'http://www.google.com/get/noto'
+
+  font 'NotoSansOlChiki-Regular.ttf'
+end
