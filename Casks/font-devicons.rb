eafc28eb872ae3770cd1bb71442fcc2809e1176b
Nicolas Brousse <nicolas-brousse@users.noreply.github.com>
2014-07-18 14:49:50 +0200

    Create font-devicons.rb

---
+++ Casks/font-devicons.rb
@@ -0,0 +1,8 @@
+class FontOpenIconic < Cask
+   url 'https://github.com/vorillaz/devicons/archive/v1.1.zip'
+   homepage 'http://vorillaz.github.io/devicons/'
+   version '1.1.0'
+   sha256 :no_check
+ 
+   font 'devicons-1.1/fonts/devicons.ttf'
+ end
