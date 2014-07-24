5bb41ae9bc04924acee62d68e93826615d0f3e99
ndr <ndr@qef.io>
2014-07-24 22:54:04 +0200

    Fix CamingoCode shasum

--- Casks/font-camingocode.rb
+++ Casks/font-camingocode.rb
@@ -2,7 +2,7 @@ class FontCamingocode < Cask
   url 'http://janfromm.de/_data/downloads/CamingoCode-v1.0.zip'
   homepage 'http://www.janfromm.de/typefaces/camingomono/camingocode/'
   version '1.000'
-  sha256 '54140a57d6d23f16404a9b5cd32cc9307810ae7c6fc47725e55dbac11465160a'
+  sha256 '13b188ca8d1b5fe2c3ba880771f20a6faac550405ae98d12edd46240f8896714'
   font 'CamingoCode-Bold.ttf'
   font 'CamingoCode-BoldItalic.ttf'
   font 'CamingoCode-Regular.ttf'
