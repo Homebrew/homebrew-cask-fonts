9a353df96862c2bf6b846a19ab59999efb28004b
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-cutive-mono.rb

--- Casks/font-cutive-mono.rb
+++ Casks/font-cutive-mono.rb
@@ -1,7 +1,9 @@
 class FontCutiveMono < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/cutivemono/CutiveMono-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Cutive%20Mono'
   version '1.002'
   sha256 'ef9633aae944f29d936f5da3d757fa6b00cad4948fe8891a093788c2f3524bba'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/cutivemono/CutiveMono-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Cutive%20Mono'
+
   font 'CutiveMono-Regular.ttf'
 end
