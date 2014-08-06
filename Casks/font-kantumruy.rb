a82a6a60519e53d4bc137f2deac6daa50f0a130b
Roland Walker <walker@pobox.com>
2014-08-06 10:11:23 -0400

    standard Cask layout for font-kantumruy.rb

--- Casks/font-kantumruy.rb
+++ Casks/font-kantumruy.rb
@@ -1,11 +1,13 @@
 class FontKantumruy < Cask
+  version '1.20'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/kantumruy',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Kantumruy'
-  version '1.20'
-  sha256 :no_check
+
   font 'Kantumruy-Bold.ttf'
   font 'Kantumruy-Light.ttf'
   font 'Kantumruy-Regular.ttf'
