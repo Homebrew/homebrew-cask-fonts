2d12f02ced1d835ca932c1143f1e8e56542ffe94
Roland Walker <walker@pobox.com>
2014-08-06 10:11:15 -0400

    standard Cask layout for font-berkshire-swash.rb

--- Casks/font-berkshire-swash.rb
+++ Casks/font-berkshire-swash.rb
@@ -1,7 +1,9 @@
 class FontBerkshireSwash < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/berkshireswash/BerkshireSwash-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Berkshire%20Swash'
   version '1.000'
   sha256 'f92ce92f2c7c529c748ef969c2e0fa0a12f3abd718779a31965a10d340bdf563'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/berkshireswash/BerkshireSwash-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Berkshire%20Swash'
+
   font 'BerkshireSwash-Regular.ttf'
 end
