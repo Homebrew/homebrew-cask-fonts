99ed6e4defbb7587d369b7576dc23fad87530c63
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-archivo-narrow.rb

--- Casks/font-archivo-narrow.rb
+++ Casks/font-archivo-narrow.rb
@@ -1,11 +1,13 @@
 class FontArchivoNarrow < Cask
+  version '1.002'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/archivonarrow',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Archivo%20Narrow'
-  version '1.002'
-  sha256 :no_check
+
   font 'ArchivoNarrow-Bold.ttf'
   font 'ArchivoNarrow-BoldItalic.ttf'
   font 'ArchivoNarrow-Italic.ttf'
