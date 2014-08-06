59c983cfde38c569777eb9321d135f26094ab7a0
Roland Walker <walker@pobox.com>
2014-08-06 10:11:32 -0400

    standard Cask layout for font-rosario.rb

--- Casks/font-rosario.rb
+++ Casks/font-rosario.rb
@@ -1,11 +1,13 @@
 class FontRosario < Cask
+  version '1.003'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/rosario',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Rosario'
-  version '1.003'
-  sha256 :no_check
+
   font 'Rosario-Bold.ttf'
   font 'Rosario-BoldItalic.ttf'
   font 'Rosario-Italic.ttf'
