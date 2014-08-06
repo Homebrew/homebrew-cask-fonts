ac33f058f8ae44411541b04e33abda7dd3a35b07
Roland Walker <walker@pobox.com>
2014-08-06 10:11:33 -0400

    standard Cask layout for font-sauce-code-powerline.rb

--- Casks/font-sauce-code-powerline.rb
+++ Casks/font-sauce-code-powerline.rb
@@ -1,11 +1,13 @@
 class FontSauceCodePowerline < Cask
+  version '1.017'
+  sha256 :no_check
+
   url 'https://github.com/Lokaltog/powerline-fonts/trunk/SourceCodePro',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/SourceCodePro'
-  version '1.017'
-  sha256 :no_check
+
   font 'Sauce Code Powerline Black.otf'
   font 'Sauce Code Powerline Bold.otf'
   font 'Sauce Code Powerline ExtraLight.otf'
