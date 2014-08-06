20465b0b4670ecc60dc87a53af59ca8bf814f91a
Roland Walker <walker@pobox.com>
2014-08-06 10:11:13 -0400

    standard Cask layout for font-alegreya-sans.rb

--- Casks/font-alegreya-sans.rb
+++ Casks/font-alegreya-sans.rb
@@ -1,11 +1,13 @@
 class FontAlegreyaSans < Cask
+  version '1.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/alegreyasans',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Alegreya%20Sans'
-  version '1.000'
-  sha256 :no_check
+
   font 'AlegreyaSans-Black.ttf'
   font 'AlegreyaSans-BlackItalic.ttf'
   font 'AlegreyaSans-Bold.ttf'
