64cda49a91cf3f873c6c5e34ae99964d57c2b6e0
Roland Walker <walker@pobox.com>
2014-08-06 10:11:21 -0400

    standard Cask layout for font-hermeneus-one.rb

--- Casks/font-hermeneus-one.rb
+++ Casks/font-hermeneus-one.rb
@@ -1,7 +1,9 @@
 class FontHermeneusOne < Cask
-  url 'http://www.impallari.com/media/uploads/commentsources/comment-239-source.zip'
-  homepage 'http://www.impallari.com/projects/overview/hermeneus'
   version '1.002'
   sha256 'c76245c77bcbe570e9146352de5bf6ae9a3c1c475ddd36cb6c50272e4000e4c1'
+
+  url 'http://www.impallari.com/media/uploads/commentsources/comment-239-source.zip'
+  homepage 'http://www.impallari.com/projects/overview/hermeneus'
+
   font 'HermeneusOne-v1.2/HermeneusOne-Regular.ttf'
 end
