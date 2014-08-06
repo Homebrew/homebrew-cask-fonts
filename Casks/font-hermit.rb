dda10a62058e9be08646805109f4be63b0c28428
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-hermit.rb

--- Casks/font-hermit.rb
+++ Casks/font-hermit.rb
@@ -1,8 +1,10 @@
 class FontHermit < Cask
-  url 'https://pcaro.es/d/otf-hermit-1.21.tar.gz'
-  homepage 'https://pcaro.es/p/hermit/'
   version '1.21'
   sha256 '2966e83f012e6a31a861e3afdcb89d157f12b307f2367303d58be364d4308b85'
+
+  url 'https://pcaro.es/d/otf-hermit-1.21.tar.gz'
+  homepage 'https://pcaro.es/p/hermit/'
+
   font 'Hermit-bold.otf'
   font 'Hermit-light.otf'
   font 'Hermit-medium.otf'
