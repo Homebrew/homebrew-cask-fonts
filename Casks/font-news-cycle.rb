d803b2c83039e13d33dcd63812acde25d7f96e4b
Roland Walker <walker@pobox.com>
2014-08-06 10:11:27 -0400

    standard Cask layout for font-news-cycle.rb

--- Casks/font-news-cycle.rb
+++ Casks/font-news-cycle.rb
@@ -1,11 +1,13 @@
 class FontNewsCycle < Cask
+  version '0.5'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/newscycle',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/News%20Cycle'
-  version '0.5'
-  sha256 :no_check
+
   font 'NewsCycle-Bold.ttf'
   font 'NewsCycle-Regular.ttf'
 end
