06968bef50d11fb11699181b1dcc1d748c97bb6a
Roland Walker <walker@pobox.com>
2014-08-06 10:11:12 -0400

    standard Cask layout for font-acme.rb

--- Casks/font-acme.rb
+++ Casks/font-acme.rb
@@ -1,7 +1,9 @@
 class FontAcme < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/acme/Acme-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Acme'
   version '1.001'
   sha256 'c51ba84f4dfd70f0f7c212888121c0785db3ce130ff5cec4360014215852383b'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/acme/Acme-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Acme'
+
   font 'Acme-Regular.ttf'
 end
