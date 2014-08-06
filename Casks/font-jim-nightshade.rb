1f1e6eb478547d2b29cb6258f9e5a8739fa604d9
Roland Walker <walker@pobox.com>
2014-08-06 10:11:23 -0400

    standard Cask layout for font-jim-nightshade.rb

--- Casks/font-jim-nightshade.rb
+++ Casks/font-jim-nightshade.rb
@@ -1,7 +1,9 @@
 class FontJimNightshade < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/jimnightshade/JimNightshade-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Jim%20Nightshade'
   version '1.000'
   sha256 'ad49a9f652cb1d7ce711d7ccdfb5922e3961f871705e88fb0e5d033e0e707103'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/jimnightshade/JimNightshade-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Jim%20Nightshade'
+
   font 'JimNightshade-Regular.ttf'
 end
