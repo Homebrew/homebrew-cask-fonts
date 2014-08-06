a1768879ebf04793dfbdc764451e4957db7555c0
Roland Walker <walker@pobox.com>
2014-08-06 10:11:27 -0400

    standard Cask layout for font-monoone.rb

--- Casks/font-monoone.rb
+++ Casks/font-monoone.rb
@@ -1,7 +1,9 @@
 class FontMonoone < Cask
-  url 'https://github.com/madmalik/monoOne/blob/master/monoOne.otf'
-  homepage 'https://github.com/madmalik/monoOne'
   version '1.000'
   sha256 :no_check
+
+  url 'https://github.com/madmalik/monoOne/blob/master/monoOne.otf'
+  homepage 'https://github.com/madmalik/monoOne'
+
   font 'monoOne.otf'
 end
