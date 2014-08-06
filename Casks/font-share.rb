2c914cfd44838d4d5338386c74262ea3e34e87e8
Roland Walker <walker@pobox.com>
2014-08-06 10:11:34 -0400

    standard Cask layout for font-share.rb

--- Casks/font-share.rb
+++ Casks/font-share.rb
@@ -1,11 +1,13 @@
 class FontShare < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/share',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Share'
-  version '1.001'
-  sha256 :no_check
+
   font 'Share-Bold.ttf'
   font 'Share-BoldItalic.ttf'
   font 'Share-Italic.ttf'
