41a356665905c9a3be43ad1bce736e6855b8781e
Roland Walker <walker@pobox.com>
2014-08-06 10:11:32 -0400

    standard Cask layout for font-redacted.rb

--- Casks/font-redacted.rb
+++ Casks/font-redacted.rb
@@ -1,11 +1,13 @@
 class FontRedacted < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/christiannaths/Redacted-Font/trunk/fonts',
       :using => :svn,
       :revision   => '41',
       :trust_cert => true
   homepage 'https://github.com/christiannaths/Redacted-Font'
-  version '1.001'
-  sha256 :no_check
+
   font 'redacted-regular.ttf'
   font 'redacted-script-bold.ttf'
   font 'redacted-script-light.ttf'
