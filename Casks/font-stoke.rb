ac9c1f741518752cf62cbe8b2679fd4deef1f020
Roland Walker <walker@pobox.com>
2014-08-06 10:11:35 -0400

    standard Cask layout for font-stoke.rb

--- Casks/font-stoke.rb
+++ Casks/font-stoke.rb
@@ -1,11 +1,13 @@
 class FontStoke < Cask
+  version '1.002'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/stoke',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Stoke'
-  version '1.002'
-  sha256 :no_check
+
   font 'Stoke-Light.ttf'
   font 'Stoke-Regular.ttf'
 end
