1adfbd92010ab389675194fe2e5b77b22181d4bc
Roland Walker <walker@pobox.com>
2014-08-06 10:11:16 -0400

    standard Cask layout for font-carrois-gothic-sc.rb

--- Casks/font-carrois-gothic-sc.rb
+++ Casks/font-carrois-gothic-sc.rb
@@ -1,7 +1,9 @@
 class FontCarroisGothicSc < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/carroisgothicsc/CarroisGothicSC-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Carrois%20Gothic%20SC'
   version '1.001'
   sha256 'a101d90b51a9e23e4cc2b8084a74e7755df47066bfe929c7cbfe21132fbcb76d'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/carroisgothicsc/CarroisGothicSC-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Carrois%20Gothic%20SC'
+
   font 'CarroisGothicSC-Regular.ttf'
 end
