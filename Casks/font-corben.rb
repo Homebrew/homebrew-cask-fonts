b0ce3339d0acdb3016848b546ed2b93d80872019
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-corben.rb

--- Casks/font-corben.rb
+++ Casks/font-corben.rb
@@ -1,11 +1,13 @@
 class FontCorben < Cask
+  version '1.100'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/corben',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Corben'
-  version '1.100'
-  sha256 :no_check
+
   font 'Corben-Bold.ttf'
   font 'Corben-Regular.ttf'
 end
