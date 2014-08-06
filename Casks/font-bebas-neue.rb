a61db00317c3f5b93b0f7cfe6716102a16042b78
Roland Walker <walker@pobox.com>
2014-08-06 10:11:15 -0400

    standard Cask layout for font-bebas-neue.rb

--- Casks/font-bebas-neue.rb
+++ Casks/font-bebas-neue.rb
@@ -1,8 +1,10 @@
 class FontBebasNeue < Cask
-  url 'http://fontfabric.com/downfont/bebas.zip'
-  homepage 'http://fontfabric.com/bebas-neue/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://fontfabric.com/downfont/bebas.zip'
+  homepage 'http://fontfabric.com/bebas-neue/'
+
   font 'BebasNeue Bold.otf'
   font 'BebasNeue Book.otf'
   font 'BebasNeue Light.otf'
