cef439c224c52261dc379632770d6137f751ebb1
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-cutive.rb

--- Casks/font-cutive.rb
+++ Casks/font-cutive.rb
@@ -1,7 +1,9 @@
 class FontCutive < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/cutive/Cutive-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Cutive'
   version '1.002'
   sha256 '56a2225b8562fe2ef80fa43018d7ffeea9c063fdf6d7e8d2a7eb92c67440737c'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/cutive/Cutive-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Cutive'
+
   font 'Cutive-Regular.ttf'
 end
