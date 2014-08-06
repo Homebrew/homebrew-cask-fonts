68baaa73b4caef5dcf031f9e73c6b596a7eb0e27
Roland Walker <walker@pobox.com>
2014-08-06 10:11:20 -0400

    standard Cask layout for font-freesans.rb

--- Casks/font-freesans.rb
+++ Casks/font-freesans.rb
@@ -1,8 +1,10 @@
 class FontFreesans < Cask
-  url 'http://ftp.gnu.org/gnu/freefont/freefont-otf-20120503.tar.gz'
-  homepage 'http://ftp.gnu.org/gnu/freefont/'
   version '0412.2268'
   sha256 '3a6c51868c71b006c33c4bcde63d90927e6fcca8f51c965b8ad62d021614a860'
+
+  url 'http://ftp.gnu.org/gnu/freefont/freefont-otf-20120503.tar.gz'
+  homepage 'http://ftp.gnu.org/gnu/freefont/'
+
   font 'freefont-20120503/FreeMono.otf'
   font 'freefont-20120503/FreeMonoBold.otf'
   font 'freefont-20120503/FreeMonoBoldOblique.otf'
