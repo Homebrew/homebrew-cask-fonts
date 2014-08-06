25778638264f899581dff56957b492b543f1637a
Roland Walker <walker@pobox.com>
2014-08-06 10:11:29 -0400

    standard Cask layout for font-nunito.rb

--- Casks/font-nunito.rb
+++ Casks/font-nunito.rb
@@ -1,11 +1,13 @@
 class FontNunito < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/nunito',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Nunito'
-  version '1.001'
-  sha256 :no_check
+
   font 'Nunito-Bold.ttf'
   font 'Nunito-Light.ttf'
   font 'Nunito-Regular.ttf'
