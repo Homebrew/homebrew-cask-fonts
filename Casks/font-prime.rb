ec9408217b7952f92a3329af7146fca2ced85775
Roland Walker <walker@pobox.com>
2014-08-06 10:11:31 -0400

    standard Cask layout for font-prime.rb

--- Casks/font-prime.rb
+++ Casks/font-prime.rb
@@ -1,8 +1,10 @@
 class FontPrime < Cask
-  url 'http://fontfabric.com/downfont/prime.zip'
-  homepage 'http://fontfabric.com/prime-free-font/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://fontfabric.com/downfont/prime.zip'
+  homepage 'http://fontfabric.com/prime-free-font/'
+
   font 'Prime Light.otf'
   font 'Prime Regular.otf'
 end
