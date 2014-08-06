2f499475546f195644c3e773106cfaa88a73663e
Roland Walker <walker@pobox.com>
2014-08-06 10:11:33 -0400

    standard Cask layout for font-seoulhangang.rb

--- Casks/font-seoulhangang.rb
+++ Casks/font-seoulhangang.rb
@@ -1,8 +1,10 @@
 class FontSeoulhangang < Cask
-  url 'http://www.seoul.go.kr/v2012/seoul/symbol/download.php?div=Zm9udDA0'
-  homepage 'http://www.seoul.go.kr/v2012/seoul/symbol/font.html'
   version '1.06'
   sha256 '8b8a10a28dc628e9812fafb0533d662565ffc14387e6852da4c653ff7c11ce64'
+
+  url 'http://www.seoul.go.kr/v2012/seoul/symbol/download.php?div=Zm9udDA0'
+  homepage 'http://www.seoul.go.kr/v2012/seoul/symbol/font.html'
+
   font 'SeoulHangangB.otf'
   font 'SeoulHangangEB.otf'
   font 'SeoulHangangL.otf'
