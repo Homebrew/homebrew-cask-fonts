cb28c3ea5f14e5fc77cf95a1cb5a8908f1ee4cee
Roland Walker <walker@pobox.com>
2014-08-06 10:11:28 -0400

    standard Cask layout for font-noto-sans-s-chinese.rb

--- Casks/font-noto-sans-s-chinese.rb
+++ Casks/font-noto-sans-s-chinese.rb
@@ -1,8 +1,10 @@
 class FontNotoSansSChinese < Cask
-  url 'http://www.google.com/get/noto/pkgs/NotoSansSChinese-hinted.zip'
-  homepage 'http://www.google.com/get/noto/#/family/noto-sans-hans'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.google.com/get/noto/pkgs/NotoSansSChinese-hinted.zip'
+  homepage 'http://www.google.com/get/noto/#/family/noto-sans-hans'
+
   font 'NotoSansHans-Black.otf'
   font 'NotoSansHans-Bold.otf'
   font 'NotoSansHans-DemiLight.otf'
