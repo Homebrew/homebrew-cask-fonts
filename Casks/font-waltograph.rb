1e5cfee57e40bc112b17412f602b6921432c0dfe
Roland Walker <walker@pobox.com>
2014-08-06 10:11:37 -0400

    standard Cask layout for font-waltograph.rb

--- Casks/font-waltograph.rb
+++ Casks/font-waltograph.rb
@@ -1,7 +1,9 @@
 class FontWaltograph < Cask
-  url 'http://mickeyavenue.com/fonts/download/waltograph.zip'
-  homepage 'http://mickeyavenue.com/fonts/waltograph/'
   version '4.2'
   sha256 :no_check
+
+  url 'http://mickeyavenue.com/fonts/download/waltograph.zip'
+  homepage 'http://mickeyavenue.com/fonts/waltograph/'
+
   font 'Waltograph/waltograph42.otf'
 end
