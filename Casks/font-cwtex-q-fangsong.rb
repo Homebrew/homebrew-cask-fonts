f97278b5e7c05b2f633571bf504bbe65b54b031a
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-cwtex-q-fangsong.rb

--- Casks/font-cwtex-q-fangsong.rb
+++ Casks/font-cwtex-q-fangsong.rb
@@ -1,7 +1,9 @@
 class FontCwtexQFangsong < Cask
-  url 'https://cwtex-q-fonts.googlecode.com/svn-history/r37/trunk/ttf/cwTeXQFangsong-Medium.ttf'
-  homepage 'https://code.google.com/p/cwtex-q-fonts/'
   version '0.1'
   sha256 '58f66307644b13f527138049624ce9d24ce1993cb9b0e95e58cb3e1d9513c22d'
+
+  url 'https://cwtex-q-fonts.googlecode.com/svn-history/r37/trunk/ttf/cwTeXQFangsong-Medium.ttf'
+  homepage 'https://code.google.com/p/cwtex-q-fonts/'
+
   font 'cwTeXQFangsong-Medium.ttf'
 end
