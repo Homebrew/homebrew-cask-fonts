dd54142e72a57659e2cec3a3af8c9696e95c6a84
Roland Walker <walker@pobox.com>
2014-08-06 10:11:36 -0400

    standard Cask layout for font-trochut.rb

--- Casks/font-trochut.rb
+++ Casks/font-trochut.rb
@@ -1,11 +1,13 @@
 class FontTrochut < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/trochut',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Trochut'
-  version '1.001'
-  sha256 :no_check
+
   font 'Trochut-Bold.ttf'
   font 'Trochut-Italic.ttf'
   font 'Trochut-Regular.ttf'
