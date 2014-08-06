fc7a04c01b4e2d7d4033da0f71d748a8d71865e0
Roland Walker <walker@pobox.com>
2014-08-06 10:11:13 -0400

    standard Cask layout for font-alegreya-sc.rb

--- Casks/font-alegreya-sc.rb
+++ Casks/font-alegreya-sc.rb
@@ -1,11 +1,13 @@
 class FontAlegreyaSc < Cask
+  version '1.003'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/alegreyasc',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Alegreya%20SC'
-  version '1.003'
-  sha256 :no_check
+
   font 'AlegreyaSC-Black.ttf'
   font 'AlegreyaSC-BlackItalic.ttf'
   font 'AlegreyaSC-Bold.ttf'
