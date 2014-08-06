a95ea9f135cc6fdbf5a5dc218de9ee0cece6de06
Roland Walker <walker@pobox.com>
2014-08-06 10:11:13 -0400

    standard Cask layout for font-almendra-display.rb

--- Casks/font-almendra-display.rb
+++ Casks/font-almendra-display.rb
@@ -1,7 +1,9 @@
 class FontAlmendraDisplay < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/almendradisplay/AlmendraDisplay-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Almendra%20Display'
   version '1.003'
   sha256 '787aab8504db249dba5e3efacc10781c7203309bab59edeb34792899a2b56590'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/almendradisplay/AlmendraDisplay-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Almendra%20Display'
+
   font 'AlmendraDisplay-Regular.ttf'
 end
