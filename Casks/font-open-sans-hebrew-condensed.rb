38e576fbd8506167ee00001d75bf038e7c95c55f
Roland Walker <walker@pobox.com>
2014-08-06 10:11:29 -0400

    standard Cask layout for font-open-sans-hebrew-condensed.rb

--- Casks/font-open-sans-hebrew-condensed.rb
+++ Casks/font-open-sans-hebrew-condensed.rb
@@ -1,11 +1,13 @@
 class FontOpenSansHebrewCondensed < Cask
+  version '2.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/opensanshebrewcondensed',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'https://www.google.com/fonts/earlyaccess'
-  version '2.001'
-  sha256 :no_check
+
   font 'OpenSansHebrewCondensed-Bold.ttf'
   font 'OpenSansHebrewCondensed-BoldItalic.ttf'
   font 'OpenSansHebrewCondensed-ExtraBold.ttf'
