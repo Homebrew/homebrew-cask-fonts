2e7f03299c597117dd62fb75fdedc0ca392b94f1
Roland Walker <walker@pobox.com>
2014-08-06 10:11:34 -0400

    standard Cask layout for font-snippet.rb

--- Casks/font-snippet.rb
+++ Casks/font-snippet.rb
@@ -1,7 +1,9 @@
 class FontSnippet < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/snippet/Snippet.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Snippet'
   version '1.000'
   sha256 '66acef67275368057085d747e688e5dcd48807d98fb6a3ca9a9424734bda1353'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/snippet/Snippet.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Snippet'
+
   font 'Snippet.ttf'
 end
