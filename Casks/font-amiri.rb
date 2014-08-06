4f80e1e3509d36dcc168de9d2e96f962a22855e3
Roland Walker <walker@pobox.com>
2014-08-06 10:11:13 -0400

    standard Cask layout for font-amiri.rb

--- Casks/font-amiri.rb
+++ Casks/font-amiri.rb
@@ -1,11 +1,13 @@
 class FontAmiri < Cask
+  version '000.104'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/amiri',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'https://www.google.com/fonts/earlyaccess'
-  version '000.104'
-  sha256 :no_check
+
   font 'Amiri-Bold.ttf'
   font 'Amiri-BoldSlanted.ttf'
   font 'Amiri-Regular.ttf'
