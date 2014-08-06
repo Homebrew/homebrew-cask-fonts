ed18f198fec5b7ea3150227f0c67af0a9e844d35
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-inconsolata.rb

--- Casks/font-inconsolata.rb
+++ Casks/font-inconsolata.rb
@@ -1,7 +1,9 @@
 class FontInconsolata < Cask
-  url 'http://levien.com/type/myfonts/Inconsolata.otf'
-  homepage 'http://levien.com/type/myfonts/inconsolata.html'
   version '001.010'
   sha256 :no_check
+
+  url 'http://levien.com/type/myfonts/Inconsolata.otf'
+  homepage 'http://levien.com/type/myfonts/inconsolata.html'
+
   font 'Inconsolata.otf'
 end
