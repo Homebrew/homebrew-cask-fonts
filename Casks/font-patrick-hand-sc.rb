512b520c8ca5abc2c0d377a07c10833a69d120fb
Roland Walker <walker@pobox.com>
2014-08-06 10:11:30 -0400

    standard Cask layout for font-patrick-hand-sc.rb

--- Casks/font-patrick-hand-sc.rb
+++ Casks/font-patrick-hand-sc.rb
@@ -1,7 +1,9 @@
 class FontPatrickHandSc < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/patrickhandsc/PatrickHandSC-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Patrick%20Hand%20SC'
   version '1.003'
   sha256 '6906ac992051812f7fe1562a14f0f45f4027c169c2120a5af383c81e514daebd'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/patrickhandsc/PatrickHandSC-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Patrick%20Hand%20SC'
+
   font 'PatrickHandSC-Regular.ttf'
 end
