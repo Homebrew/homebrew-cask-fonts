efa076d1abfe8c2050575664bdd455833d2034ad
Roland Walker <walker@pobox.com>
2014-08-06 10:11:21 -0400

    standard Cask layout for font-geostar.rb

--- Casks/font-geostar.rb
+++ Casks/font-geostar.rb
@@ -1,7 +1,9 @@
 class FontGeostar < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/geostar/Geostar-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Geostar'
   version '1.002'
   sha256 '891b92c124bbe4ac006c0b8b7402215486b7cb7951488178f415694a5898d12b'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/geostar/Geostar-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Geostar'
+
   font 'Geostar-Regular.ttf'
 end
