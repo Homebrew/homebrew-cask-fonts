c9a3f6f26cdd1706b5ed22bc11d85cab944bcea3
Roland Walker <walker@pobox.com>
2014-08-06 10:11:34 -0400

    standard Cask layout for font-silent-lips.rb

--- Casks/font-silent-lips.rb
+++ Casks/font-silent-lips.rb
@@ -1,8 +1,10 @@
 class FontSilentLips < Cask
-  url 'http://www.ffonts.net/Silent_Lips.font.zip'
-  homepage 'https://www.behance.net/gallery/18281129/Silent-Lips-Experimental-Free-Font'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.ffonts.net/Silent_Lips.font.zip'
+  homepage 'https://www.behance.net/gallery/18281129/Silent-Lips-Experimental-Free-Font'
+
   font 'Silent Lips/Silent Lips.ttf'
   font 'Silent Lips/Silent_Lips-Bold.ttf'
 end
