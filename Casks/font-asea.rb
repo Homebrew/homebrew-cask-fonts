ae98c2d90f4fae2364890d1aa953ceb8dce43e5f
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-asea.rb

--- Casks/font-asea.rb
+++ Casks/font-asea.rb
@@ -1,8 +1,10 @@
 class FontAsea < Cask
-  url 'http://users.teilar.gr/~g1951d/Asea.zip'
-  homepage 'http://users.teilar.gr/~g1951d/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://users.teilar.gr/~g1951d/Asea.zip'
+  homepage 'http://users.teilar.gr/~g1951d/'
+
   font 'AseaB_hint.ttf'
   font 'AseaI_hint.ttf'
   font 'AseaR_hint.ttf'
