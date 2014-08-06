8ed3afef0bcd577204f3135ca066cbe98eef772c
Roland Walker <walker@pobox.com>
2014-08-06 10:11:36 -0400

    standard Cask layout for font-titillium-web.rb

--- Casks/font-titillium-web.rb
+++ Casks/font-titillium-web.rb
@@ -1,11 +1,13 @@
 class FontTitilliumWeb < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/titilliumweb',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Titillium%20Web'
-  version '1.001'
-  sha256 :no_check
+
   font 'TitilliumWeb-Black.ttf'
   font 'TitilliumWeb-Bold.ttf'
   font 'TitilliumWeb-BoldItalic.ttf'
