b3542e8daebf01472c569247ed7c97264af541db
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-cwtex-q-kai.rb

--- Casks/font-cwtex-q-kai.rb
+++ Casks/font-cwtex-q-kai.rb
@@ -1,7 +1,9 @@
 class FontCwtexQKai < Cask
-  url 'https://cwtex-q-fonts.googlecode.com/svn-history/r37/trunk/ttf/cwTeXQKai-Medium.ttf'
-  homepage 'https://code.google.com/p/cwtex-q-fonts/'
   version '0.1'
   sha256 'c48063a3ff6834a8e68167438f560ce8cfd73fcd8c350addec0fdcabad2df6d5'
+
+  url 'https://cwtex-q-fonts.googlecode.com/svn-history/r37/trunk/ttf/cwTeXQKai-Medium.ttf'
+  homepage 'https://code.google.com/p/cwtex-q-fonts/'
+
   font 'cwTeXQKai-Medium.ttf'
 end
