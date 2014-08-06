d0eeb4fdc57fabe7fcb614a0a47ae1065baeceff
Roland Walker <walker@pobox.com>
2014-08-06 10:11:20 -0400

    standard Cask layout for font-faster-one.rb

--- Casks/font-faster-one.rb
+++ Casks/font-faster-one.rb
@@ -1,7 +1,9 @@
 class FontFasterOne < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/fasterone/FasterOne-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Faster%20One'
   version '1.001'
   sha256 'bf1fc9be65aad5c5cb3fe2ac8617897d6d9d5a449748cc7a2460c200998f4ef1'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/fasterone/FasterOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Faster%20One'
+
   font 'FasterOne-Regular.ttf'
 end
