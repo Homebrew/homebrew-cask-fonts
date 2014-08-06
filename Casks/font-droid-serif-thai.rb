5dc7381be15e4daa37e8fa751a31ccca369a871a
Roland Walker <walker@pobox.com>
2014-08-06 10:11:19 -0400

    standard Cask layout for font-droid-serif-thai.rb

--- Casks/font-droid-serif-thai.rb
+++ Casks/font-droid-serif-thai.rb
@@ -1,11 +1,13 @@
 class FontDroidSerifThai < Cask
+  version '1.00'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/droidserifthai',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'https://www.google.com/fonts/earlyaccess'
-  version '1.00'
-  sha256 :no_check
+
   font 'DroidSerifThai-Bold.ttf'
   font 'DroidSerifThai-Regular.ttf'
 end
