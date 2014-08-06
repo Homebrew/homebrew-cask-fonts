19748039265b438641e9e7563fad2f6d8042c350
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-cwtex-q-hei.rb

--- Casks/font-cwtex-q-hei.rb
+++ Casks/font-cwtex-q-hei.rb
@@ -1,7 +1,9 @@
 class FontCwtexQHei < Cask
-  url 'https://cwtex-q-fonts.googlecode.com/svn-history/r37/trunk/ttf/cwTeXQHei-Bold.ttf'
-  homepage 'https://code.google.com/p/cwtex-q-fonts/'
   version '0.2'
   sha256 '3909b65b66f3099768bb08513db9efe2e90cb21575f5b5398817a29bd12474f6'
+
+  url 'https://cwtex-q-fonts.googlecode.com/svn-history/r37/trunk/ttf/cwTeXQHei-Bold.ttf'
+  homepage 'https://code.google.com/p/cwtex-q-fonts/'
+
   font 'cwTeXQHei-Bold.ttf'
 end
