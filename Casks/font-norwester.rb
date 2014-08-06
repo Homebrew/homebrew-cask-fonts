0224364172e3f30c49b3b448045ffee41af2e778
Roland Walker <walker@pobox.com>
2014-08-06 10:11:28 -0400

    standard Cask layout for font-norwester.rb

--- Casks/font-norwester.rb
+++ Casks/font-norwester.rb
@@ -1,7 +1,9 @@
 class FontNorwester < Cask
-  url 'http://jamiewilson.io/norwester/assets/norwester.zip'
-  homepage 'http://jamiewilson.io/norwester/'
   version '1.002'
   sha256 '4da457006bd6f058cca990f1b15d130b2e9af16974c0dd172391de7ab446cf6b'
+
+  url 'http://jamiewilson.io/norwester/assets/norwester.zip'
+  homepage 'http://jamiewilson.io/norwester/'
+
   font 'norwester-v1.2/norwester.otf'
 end
