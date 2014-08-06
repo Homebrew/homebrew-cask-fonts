aa918bab9fa55d818d33e5c39dd821281b933fce
Roland Walker <walker@pobox.com>
2014-08-06 10:11:21 -0400

    standard Cask layout for font-gfs-neohellenic.rb

--- Casks/font-gfs-neohellenic.rb
+++ Casks/font-gfs-neohellenic.rb
@@ -1,11 +1,13 @@
 class FontGfsNeohellenic < Cask
+  version '1.0'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/gfsneohellenic',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/GFS%20Neohellenic'
-  version '1.0'
-  sha256 :no_check
+
   font 'GFSNeohellenic.ttf'
   font 'GFSNeohellenicBold.ttf'
   font 'GFSNeohellenicBoldItalic.ttf'
