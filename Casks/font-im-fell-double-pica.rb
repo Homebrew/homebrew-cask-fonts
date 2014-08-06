3ffe3bd17d9c9e025c2f656190b2382611917568
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-im-fell-double-pica.rb

--- Casks/font-im-fell-double-pica.rb
+++ Casks/font-im-fell-double-pica.rb
@@ -1,11 +1,13 @@
 class FontImFellDoublePica < Cask
+  version '3.00'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/imfelldoublepica',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/IM%20Fell%20Double%20Pica'
-  version '3.00'
-  sha256 :no_check
+
   font 'IMFeDPit28P.ttf'
   font 'IMFeDPrm28P.ttf'
 end
