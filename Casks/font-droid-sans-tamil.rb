26c3ae4ddb307dfade6ec552f1902f2bc05a5fca
Roland Walker <walker@pobox.com>
2014-08-06 10:11:19 -0400

    standard Cask layout for font-droid-sans-tamil.rb

--- Casks/font-droid-sans-tamil.rb
+++ Casks/font-droid-sans-tamil.rb
@@ -1,11 +1,13 @@
 class FontDroidSansTamil < Cask
+  version '1.02'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/droidsanstamil',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'https://www.google.com/fonts/earlyaccess'
-  version '1.02'
-  sha256 :no_check
+
   font 'DroidSansTamil-Bold.ttf'
   font 'DroidSansTamil-Regular.ttf'
 end
