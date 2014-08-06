f3011c2a34c3092af72e61f1569e3c53deec8b63
Roland Walker <walker@pobox.com>
2014-08-06 10:11:32 -0400

    standard Cask layout for font-quicksand.rb

--- Casks/font-quicksand.rb
+++ Casks/font-quicksand.rb
@@ -1,11 +1,13 @@
 class FontQuicksand < Cask
+  version '001.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/quicksand',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Quicksand'
-  version '001.001'
-  sha256 :no_check
+
   font 'Quicksand-Bold.ttf'
   font 'Quicksand-BoldItalic.ttf'
   font 'Quicksand-Italic.ttf'
