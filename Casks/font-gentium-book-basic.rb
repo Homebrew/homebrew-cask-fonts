d75251bee79d25970996cfabaaf4fdb4914dc336
Roland Walker <walker@pobox.com>
2014-08-06 10:11:20 -0400

    standard Cask layout for font-gentium-book-basic.rb

--- Casks/font-gentium-book-basic.rb
+++ Casks/font-gentium-book-basic.rb
@@ -1,11 +1,13 @@
 class FontGentiumBookBasic < Cask
+  version '1.100'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/gentiumbookbasic',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Gentium%20Book%20Basic'
-  version '1.100'
-  sha256 :no_check
+
   font 'GenBkBasB.ttf'
   font 'GenBkBasBI.ttf'
   font 'GenBkBasI.ttf'
