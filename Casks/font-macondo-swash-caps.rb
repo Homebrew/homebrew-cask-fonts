f33d6de153f4ff2f14204246e6a1cee5a4b114cc
Roland Walker <walker@pobox.com>
2014-08-06 10:11:25 -0400

    standard Cask layout for font-macondo-swash-caps.rb

--- Casks/font-macondo-swash-caps.rb
+++ Casks/font-macondo-swash-caps.rb
@@ -1,7 +1,9 @@
 class FontMacondoSwashCaps < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/macondoswashcaps/MacondoSwashCaps-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Macondo%20Swash%20Caps'
   version '2.001'
   sha256 '776c95ad520e4776a45add8d8693e693b566be9f26cd20505256c8e944dc07ea'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/macondoswashcaps/MacondoSwashCaps-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Macondo%20Swash%20Caps'
+
   font 'MacondoSwashCaps-Regular.ttf'
 end
