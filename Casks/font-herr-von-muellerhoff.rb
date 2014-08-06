8158e893ffbb5dc4f4622d32ebcf8169f83c93d4
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-herr-von-muellerhoff.rb

--- Casks/font-herr-von-muellerhoff.rb
+++ Casks/font-herr-von-muellerhoff.rb
@@ -1,7 +1,9 @@
 class FontHerrVonMuellerhoff < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/herrvonmuellerhoff/HerrVonMuellerhoff-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Herr%20Von%20Muellerhoff'
   version '1.000'
   sha256 'ba8ac10807a79462b7c8265b2eebb8419e5017fdc1a5828b1a071d9e8478772e'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/herrvonmuellerhoff/HerrVonMuellerhoff-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Herr%20Von%20Muellerhoff'
+
   font 'HerrVonMuellerhoff-Regular.ttf'
 end
