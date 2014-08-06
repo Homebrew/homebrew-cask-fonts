e66e99c7c2184aab66ca6b6a911f39d8e41b23c3
Roland Walker <walker@pobox.com>
2014-08-06 10:11:19 -0400

    standard Cask layout for font-droid-sans-mono.rb

--- Casks/font-droid-sans-mono.rb
+++ Casks/font-droid-sans-mono.rb
@@ -1,10 +1,12 @@
 class FontDroidSansMono < Cask
+  version '1.00'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/droidsansmono',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Droid%20Sans%20Mono'
-  version '1.00'
-  sha256 :no_check
+
   font 'DroidSansMono.ttf'
 end
