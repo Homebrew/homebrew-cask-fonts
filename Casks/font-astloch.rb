293a23f36b712024b5ea80bb05734632cbf08db2
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-astloch.rb

--- Casks/font-astloch.rb
+++ Casks/font-astloch.rb
@@ -1,11 +1,13 @@
 class FontAstloch < Cask
+  version '1.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/astloch',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Astloch'
-  version '1.000'
-  sha256 :no_check
+
   font 'Astloch-Bold.ttf'
   font 'Astloch-Regular.ttf'
 end
