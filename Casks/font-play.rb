214dfa41170fc2f36d43f0969f08205ecbed496c
Roland Walker <walker@pobox.com>
2014-08-06 10:11:31 -0400

    standard Cask layout for font-play.rb

--- Casks/font-play.rb
+++ Casks/font-play.rb
@@ -1,11 +1,13 @@
 class FontPlay < Cask
+  version '1.002'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/play',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Play'
-  version '1.002'
-  sha256 :no_check
+
   font 'Play-Bold.ttf'
   font 'Play-Regular.ttf'
 end
