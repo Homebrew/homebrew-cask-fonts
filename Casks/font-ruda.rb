b804059b6b1fdfb31e99153adb5303bb08297052
Roland Walker <walker@pobox.com>
2014-08-06 10:11:33 -0400

    standard Cask layout for font-ruda.rb

--- Casks/font-ruda.rb
+++ Casks/font-ruda.rb
@@ -1,11 +1,13 @@
 class FontRuda < Cask
+  version '1.002'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/ruda',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Ruda'
-  version '1.002'
-  sha256 :no_check
+
   font 'Ruda-Black.ttf'
   font 'Ruda-Bold.ttf'
   font 'Ruda-Regular.ttf'
