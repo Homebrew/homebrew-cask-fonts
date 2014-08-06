08744d331ea61d5f2fdaf62d5a18c1c36b297926
Roland Walker <walker@pobox.com>
2014-08-06 10:11:26 -0400

    standard Cask layout for font-miao-unicode.rb

--- Casks/font-miao-unicode.rb
+++ Casks/font-miao-unicode.rb
@@ -1,7 +1,9 @@
 class FontMiaoUnicode < Cask
-  url 'https://github.com/phjamr/MiaoUnicode/blob/master/MiaoUnicode-Regular.ttf?raw=true'
-  homepage 'http://phjamr.github.io/miao.html'
   version 'latest'
   sha256 :no_check
+
+  url 'https://github.com/phjamr/MiaoUnicode/blob/master/MiaoUnicode-Regular.ttf?raw=true'
+  homepage 'http://phjamr.github.io/miao.html'
+
   font 'MiaoUnicode-Regular.ttf'
 end
