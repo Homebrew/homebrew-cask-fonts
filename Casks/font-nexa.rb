68a8227f269805bd8c493bcf6ceacbd1772fa207
Roland Walker <walker@pobox.com>
2014-08-06 10:11:27 -0400

    standard Cask layout for font-nexa.rb

--- Casks/font-nexa.rb
+++ Casks/font-nexa.rb
@@ -1,8 +1,10 @@
 class FontNexa < Cask
-  url 'http://fontfabric.com/downfont/nexa.zip'
-  homepage 'http://fontfabric.com/nexa-font/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://fontfabric.com/downfont/nexa.zip'
+  homepage 'http://fontfabric.com/nexa-font/'
+
   font 'Nexa Bold.otf'
   font 'Nexa Light.otf'
 end
