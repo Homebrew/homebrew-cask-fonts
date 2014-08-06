19fa8b79e88fbe9608d4fe7d718a6e45991a1af6
Roland Walker <walker@pobox.com>
2014-08-06 10:11:12 -0400

    standard Cask layout for font-aclonica.rb

--- Casks/font-aclonica.rb
+++ Casks/font-aclonica.rb
@@ -1,7 +1,9 @@
 class FontAclonica < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/aclonica/Aclonica.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Aclonica'
   version '1.000'
   sha256 'aec4ac0902f0d9597761557c59e4e0d5c9ac117c70d106ca1ae806082f6b234d'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/aclonica/Aclonica.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Aclonica'
+
   font 'Aclonica.ttf'
 end
