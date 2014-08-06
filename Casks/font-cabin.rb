7cf827134235eacfa598443c3efde001b57eabf0
Roland Walker <walker@pobox.com>
2014-08-06 10:11:16 -0400

    standard Cask layout for font-cabin.rb

--- Casks/font-cabin.rb
+++ Casks/font-cabin.rb
@@ -1,11 +1,13 @@
 class FontCabin < Cask
+  version '1.006'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/cabin',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Cabin'
-  version '1.006'
-  sha256 :no_check
+
   font 'Cabin-Bold.ttf'
   font 'Cabin-BoldItalic.ttf'
   font 'Cabin-Italic.ttf'
