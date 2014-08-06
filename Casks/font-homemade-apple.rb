ab44310e0f76c927141d2bf7c7bf80bdd75a187b
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-homemade-apple.rb

--- Casks/font-homemade-apple.rb
+++ Casks/font-homemade-apple.rb
@@ -1,7 +1,9 @@
 class FontHomemadeApple < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/homemadeapple/HomemadeApple.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Homemade%20Apple'
   version '1.000'
   sha256 'b311f042e9ae6dbb327b23ac0476b16b2e98a041754541a7773fbab3b3279906'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/homemadeapple/HomemadeApple.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Homemade%20Apple'
+
   font 'HomemadeApple.ttf'
 end
