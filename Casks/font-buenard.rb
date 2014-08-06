a9b077c6a5f3652c1bb8219448d3ee4e6e09286f
Roland Walker <walker@pobox.com>
2014-08-06 10:11:15 -0400

    standard Cask layout for font-buenard.rb

--- Casks/font-buenard.rb
+++ Casks/font-buenard.rb
@@ -1,11 +1,13 @@
 class FontBuenard < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/buenard',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Buenard'
-  version '1.001'
-  sha256 :no_check
+
   font 'Buenard-Bold.ttf'
   font 'Buenard-Regular.ttf'
 end
