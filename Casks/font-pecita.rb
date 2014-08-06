8e78d1477ade901822948190ccb3b01bb0dd5071
Roland Walker <walker@pobox.com>
2014-08-06 10:11:30 -0400

    standard Cask layout for font-pecita.rb

--- Casks/font-pecita.rb
+++ Casks/font-pecita.rb
@@ -1,7 +1,9 @@
 class FontPecita < Cask
-  url 'http://pecita.eu/b/Pecita.otf'
-  homepage 'http://pecita.eu/police-en.php'
   version '4.2'
   sha256 '942bfcd5a69abd22443c5ec099316314589e7fb8c343972f60fad2b9a1ef7f7e'
+
+  url 'http://pecita.eu/b/Pecita.otf'
+  homepage 'http://pecita.eu/police-en.php'
+
   font 'Pecita.otf'
 end
