ebc352a01a2432a52442bb410fe26e2163de805d
Roland Walker <walker@pobox.com>
2014-08-06 10:11:24 -0400

    standard Cask layout for font-kreon.rb

--- Casks/font-kreon.rb
+++ Casks/font-kreon.rb
@@ -1,11 +1,13 @@
 class FontKreon < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/kreon',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Kreon'
-  version '1.001'
-  sha256 :no_check
+
   font 'Kreon-Bold.ttf'
   font 'Kreon-Light.ttf'
   font 'Kreon-Regular.ttf'
