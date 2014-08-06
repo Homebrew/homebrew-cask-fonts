35ef309255ac44b7105de680a81931fd006c9071
Roland Walker <walker@pobox.com>
2014-08-06 10:11:32 -0400

    standard Cask layout for font-ribeye.rb

--- Casks/font-ribeye.rb
+++ Casks/font-ribeye.rb
@@ -1,7 +1,9 @@
 class FontRibeye < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/ribeye/Ribeye-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Ribeye'
   version '1.000'
   sha256 'c384b009dba7b9c973402427716ccf2a57dc5e44b30cfda45fc0ecc5cf238bcb'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/ribeye/Ribeye-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Ribeye'
+
   font 'Ribeye-Regular.ttf'
 end
