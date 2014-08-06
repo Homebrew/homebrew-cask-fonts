b8df13903751a76ce06a344aa6a2d45dcb4bd7ea
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-devonshire.rb

--- Casks/font-devonshire.rb
+++ Casks/font-devonshire.rb
@@ -1,7 +1,9 @@
 class FontDevonshire < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/devonshire/Devonshire-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Devonshire'
   version '1.001'
   sha256 '4c00f003570e5a99361cc83017cb1314aa3170cfaa53be29a08c555bbb434657'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/devonshire/Devonshire-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Devonshire'
+
   font 'Devonshire-Regular.ttf'
 end
