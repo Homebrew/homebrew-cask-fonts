c27ac211cf66a8ea27994a27162d1d91cd55afae
Roland Walker <walker@pobox.com>
2014-08-06 10:11:31 -0400

    standard Cask layout for font-poetsenone.rb

--- Casks/font-poetsenone.rb
+++ Casks/font-poetsenone.rb
@@ -1,7 +1,9 @@
 class FontPoetsenone < Cask
-  url 'http://www.impallari.com/media/uploads/commentsources/comment-241-source.zip'
-  homepage 'http://www.impallari.com/projects/overview/poetsen'
   version '1.001'
   sha256 '0f093de9c0fe493d99609f2dfdde6226f2f1373d5117179fab7a33588341dc5f'
+
+  url 'http://www.impallari.com/media/uploads/commentsources/comment-241-source.zip'
+  homepage 'http://www.impallari.com/projects/overview/poetsen'
+
   font 'PoetsenOne-v1.1/PoetsenOne-Regular.ttf'
 end
