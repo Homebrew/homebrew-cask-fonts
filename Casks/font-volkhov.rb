6bf83c958b835b70cc639689d48262c062686d19
Roland Walker <walker@pobox.com>
2014-08-06 10:11:36 -0400

    standard Cask layout for font-volkhov.rb

--- Casks/font-volkhov.rb
+++ Casks/font-volkhov.rb
@@ -1,11 +1,13 @@
 class FontVolkhov < Cask
+  version '1.010'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/volkhov',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Volkhov'
-  version '1.010'
-  sha256 :no_check
+
   font 'Volkhov-Bold.ttf'
   font 'Volkhov-BoldItalic.ttf'
   font 'Volkhov-Italic.ttf'
