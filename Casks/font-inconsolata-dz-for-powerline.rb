6062daf2909132bb1bdf058ceae61fc4ac2c8d1b
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-inconsolata-dz-for-powerline.rb

--- Casks/font-inconsolata-dz-for-powerline.rb
+++ Casks/font-inconsolata-dz-for-powerline.rb
@@ -1,7 +1,9 @@
 class FontInconsolataDzForPowerline < Cask
-  url 'https://raw.github.com/Lokaltog/powerline-fonts/master/InconsolataDz/Inconsolata-dz%20for%20Powerline.otf'
-  homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/InconsolataDz'
   version 'latest'
   sha256 :no_check
+
+  url 'https://raw.github.com/Lokaltog/powerline-fonts/master/InconsolataDz/Inconsolata-dz%20for%20Powerline.otf'
+  homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/InconsolataDz'
+
   font 'Inconsolata-dz for Powerline.otf'
 end
