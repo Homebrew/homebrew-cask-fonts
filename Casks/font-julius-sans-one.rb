5413e5987ea7a5ce6d4de89c1af8946f7a509902
Roland Walker <walker@pobox.com>
2014-08-06 10:11:23 -0400

    standard Cask layout for font-julius-sans-one.rb

--- Casks/font-julius-sans-one.rb
+++ Casks/font-julius-sans-one.rb
@@ -1,7 +1,9 @@
 class FontJuliusSansOne < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/juliussansone/JuliusSansOne-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Julius%20Sans%20One'
   version '1.001'
   sha256 'b9e0d5c8111315894a7d200686c64a0ee0e2670030efa3c4b5b099a6f79f5f1c'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/juliussansone/JuliusSansOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Julius%20Sans%20One'
+
   font 'JuliusSansOne-Regular.ttf'
 end
