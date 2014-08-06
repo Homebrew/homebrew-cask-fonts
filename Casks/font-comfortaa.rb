e4d4a3fef0cb4b06ad77cc8ffb9d1a987509eb12
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-comfortaa.rb

--- Casks/font-comfortaa.rb
+++ Casks/font-comfortaa.rb
@@ -1,11 +1,13 @@
 class FontComfortaa < Cask
+  version '2.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/comfortaa',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Comfortaa'
-  version '2.001'
-  sha256 :no_check
+
   font 'Comfortaa-Bold.ttf'
   font 'Comfortaa-Light.ttf'
   font 'Comfortaa-Regular.ttf'
