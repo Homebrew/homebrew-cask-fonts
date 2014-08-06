35bd0612fe24382112af5d548e6a0d8c639b5fe3
Roland Walker <walker@pobox.com>
2014-08-06 10:11:30 -0400

    standard Cask layout for font-oxygen-mono.rb

--- Casks/font-oxygen-mono.rb
+++ Casks/font-oxygen-mono.rb
@@ -1,7 +1,9 @@
 class FontOxygenMono < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/oxygenmono/OxygenMono-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Oxygen%20Mono'
   version '0.201'
   sha256 'dcc671da93f7fc31f2e7a4a7c058c815c1eaec6a3d27792e18edff4d3bf539f0'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/oxygenmono/OxygenMono-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Oxygen%20Mono'
+
   font 'OxygenMono-Regular.ttf'
 end
