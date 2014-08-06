5c93cbbeffebce7cf3fb75a99f41707a52cd1376
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-della-respira.rb

--- Casks/font-della-respira.rb
+++ Casks/font-della-respira.rb
@@ -1,7 +1,9 @@
 class FontDellaRespira < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/dellarespira/DellaRespira-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Della%20Respira'
   version '0.201'
   sha256 '81518be5b7baf69db5b8e4cff1a447ac2f6bf7a6029ca4963a67e1fb1b0b9218'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/dellarespira/DellaRespira-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Della%20Respira'
+
   font 'DellaRespira-Regular.ttf'
 end
