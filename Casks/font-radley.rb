a581c2062583d17c42309e4d0cda894c9d29bb5b
Roland Walker <walker@pobox.com>
2014-08-06 10:11:32 -0400

    standard Cask layout for font-radley.rb

--- Casks/font-radley.rb
+++ Casks/font-radley.rb
@@ -1,11 +1,13 @@
 class FontRadley < Cask
+  version '1.002'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/radley',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Radley'
-  version '1.002'
-  sha256 :no_check
+
   font 'Radley-Italic.ttf'
   font 'Radley-Regular.ttf'
 end
