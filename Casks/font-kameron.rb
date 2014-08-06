aaf205d8d375e60dcc18295f1ca0af6f54aaaf97
Roland Walker <walker@pobox.com>
2014-08-06 10:11:23 -0400

    standard Cask layout for font-kameron.rb

--- Casks/font-kameron.rb
+++ Casks/font-kameron.rb
@@ -1,11 +1,13 @@
 class FontKameron < Cask
+  version '1.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/kameron',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Kameron'
-  version '1.000'
-  sha256 :no_check
+
   font 'Kameron-Bold.ttf'
   font 'Kameron-Regular.ttf'
 end
