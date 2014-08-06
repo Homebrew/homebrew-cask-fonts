1f912bdbbb5af152eb5227cd8d77ef30c204da4d
Roland Walker <walker@pobox.com>
2014-08-06 10:11:36 -0400

    standard Cask layout for font-trebuchet-ms.rb

--- Casks/font-trebuchet-ms.rb
+++ Casks/font-trebuchet-ms.rb
@@ -1,9 +1,11 @@
 class FontTrebuchetMs < Cask
+  version '1.22'
+  sha256 '5a690d9bb8510be1b8b4fe49f1f2319651fe51bbe54775ddddd8ef0bd07fdac9'
+
   url 'http://downloads.sourceforge.net/sourceforge/corefonts/trebuc32.exe'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
-  version '1.22'
   depends_on_formula 'cabextract'
-  sha256 '5a690d9bb8510be1b8b4fe49f1f2319651fe51bbe54775ddddd8ef0bd07fdac9'
+
   font 'trebuc.ttf'
   font 'Trebucbd.ttf'
   font 'trebucbi.ttf'
