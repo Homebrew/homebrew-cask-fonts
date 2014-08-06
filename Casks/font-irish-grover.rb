d8fd353cf38a00b3f7f667cf46f6ed3941a7a66a
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-irish-grover.rb

--- Casks/font-irish-grover.rb
+++ Casks/font-irish-grover.rb
@@ -1,7 +1,9 @@
 class FontIrishGrover < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/irishgrover/IrishGrover.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Irish%20Grover'
   version '1.000'
   sha256 'b69769714c9479443e794877117d4888dd6fe71e3a8e58184c7f4550571eef73'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/irishgrover/IrishGrover.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Irish%20Grover'
+
   font 'IrishGrover.ttf'
 end
