f1c475fb652e50995cbf72248b1226bb4e2ce0ae
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-combo.rb

--- Casks/font-combo.rb
+++ Casks/font-combo.rb
@@ -1,7 +1,9 @@
 class FontCombo < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/combo/Combo-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Combo'
   version '1.001'
   sha256 'e714674117c2864bf7407e2ebfafb11e581e97480286fa01b419adda94d08686'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/combo/Combo-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Combo'
+
   font 'Combo-Regular.ttf'
 end
