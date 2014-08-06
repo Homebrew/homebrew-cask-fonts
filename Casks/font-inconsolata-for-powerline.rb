4a5e4b43649ad6fef05dfb8a2d106c1ade5a6b01
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-inconsolata-for-powerline.rb

--- Casks/font-inconsolata-for-powerline.rb
+++ Casks/font-inconsolata-for-powerline.rb
@@ -1,7 +1,9 @@
 class FontInconsolataForPowerline < Cask
-  url 'https://github.com/Lokaltog/powerline-fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf'
-  homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/Inconsolata'
   version 'latest'
   sha256 :no_check
+
+  url 'https://github.com/Lokaltog/powerline-fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf'
+  homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/Inconsolata'
+
   font 'Inconsolata for Powerline.otf'
 end
