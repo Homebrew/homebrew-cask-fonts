b67f3bf79acdd5c0e7a8d778889e76f4fa58de89
Roland Walker <walker@pobox.com>
2014-08-06 10:11:15 -0400

    standard Cask layout for font-batak-unicode.rb

--- Casks/font-batak-unicode.rb
+++ Casks/font-batak-unicode.rb
@@ -1,7 +1,9 @@
 class FontBatakUnicode < Cask
-  url 'http://ulikozok.com/fontfiles/Batak-Unicode-Regular.otf'
-  homepage 'http://ulikozok.com/aksara-batak/batak-font/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://ulikozok.com/fontfiles/Batak-Unicode-Regular.otf'
+  homepage 'http://ulikozok.com/aksara-batak/batak-font/'
+
   font 'Batak-Unicode-Regular.otf'
 end
