9351f05d0a0f5e09fd5695043678d2da1c93ae91
Roland Walker <walker@pobox.com>
2014-08-06 10:11:20 -0400

    standard Cask layout for font-fredericka-the-great.rb

--- Casks/font-fredericka-the-great.rb
+++ Casks/font-fredericka-the-great.rb
@@ -1,7 +1,9 @@
 class FontFrederickaTheGreat < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/frederickathegreat/FrederickatheGreat-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Fredericka%20the%20Great'
   version '1.000'
   sha256 '990272dea68d64b9a819554b57e80ceb5276e3df1eaa32ac78190c6232a07132'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/frederickathegreat/FrederickatheGreat-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Fredericka%20the%20Great'
+
   font 'FrederickatheGreat-Regular.ttf'
 end
