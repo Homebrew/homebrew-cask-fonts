8665d6c56e072914b094462c29d7b08e25df02aa
Roland Walker <walker@pobox.com>
2014-08-06 10:11:32 -0400

    standard Cask layout for font-rokkitt.rb

--- Casks/font-rokkitt.rb
+++ Casks/font-rokkitt.rb
@@ -1,11 +1,13 @@
 class FontRokkitt < Cask
+  version '1.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/rokkitt',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Rokkitt'
-  version '1.000'
-  sha256 :no_check
+
   font 'Rokkitt-Bold.ttf'
   font 'Rokkitt-Light.ttf'
   font 'Rokkitt-Regular.ttf'
