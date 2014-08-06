94e804901183be1543bf60babe51ad3348f6cfc5
Roland Walker <walker@pobox.com>
2014-08-06 10:11:13 -0400

    standard Cask layout for font-aleo.rb

--- Casks/font-aleo.rb
+++ Casks/font-aleo.rb
@@ -1,8 +1,10 @@
 class FontAleo < Cask
-  url 'http://fontfabric.com/downfont/aleo.zip'
-  homepage 'http://fontfabric.com/aleo-free-font/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://fontfabric.com/downfont/aleo.zip'
+  homepage 'http://fontfabric.com/aleo-free-font/'
+
   font 'Aleo-Bold.otf'
   font 'Aleo-BoldItalic.otf'
   font 'Aleo-Italic.otf'
