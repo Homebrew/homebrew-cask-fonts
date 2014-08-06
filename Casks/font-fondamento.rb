37712956788f8934efec7cd28e4df233e8027946
Roland Walker <walker@pobox.com>
2014-08-06 10:11:20 -0400

    standard Cask layout for font-fondamento.rb

--- Casks/font-fondamento.rb
+++ Casks/font-fondamento.rb
@@ -1,11 +1,13 @@
 class FontFondamento < Cask
+  version '1.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/fondamento',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Fondamento'
-  version '1.000'
-  sha256 :no_check
+
   font 'Fondamento-Italic.ttf'
   font 'Fondamento-Regular.ttf'
 end
