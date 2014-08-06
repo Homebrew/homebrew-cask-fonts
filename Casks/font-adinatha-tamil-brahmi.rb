96996bfdb20a2bb77742be21000f74dae8a5b199
Roland Walker <walker@pobox.com>
2014-08-06 10:11:12 -0400

    standard Cask layout for font-adinatha-tamil-brahmi.rb

--- Casks/font-adinatha-tamil-brahmi.rb
+++ Casks/font-adinatha-tamil-brahmi.rb
@@ -1,7 +1,9 @@
 class FontAdinathaTamilBrahmi < Cask
-  url 'http://www.virtualvinodh.com/download/Adinatha-Tamil-Brahmi.zip'
-  homepage 'http://www.virtualvinodh.com/downloads'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.virtualvinodh.com/download/Adinatha-Tamil-Brahmi.zip'
+  homepage 'http://www.virtualvinodh.com/downloads'
+
   font 'Adinatha-Tamil-Brahmi.otf'
 end
