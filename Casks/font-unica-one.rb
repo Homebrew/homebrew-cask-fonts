5d7b71b44e837e85662d9de0fc771347ad164ebb
Roland Walker <walker@pobox.com>
2014-08-06 10:11:36 -0400

    standard Cask layout for font-unica-one.rb

--- Casks/font-unica-one.rb
+++ Casks/font-unica-one.rb
@@ -1,7 +1,9 @@
 class FontUnicaOne < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/unicaone/UnicaOne-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Unica%20One'
   version '1.001'
   sha256 'b61df83878f3bb84956eae118c870aea30b88e7508ce419c00f79e65e4da1add'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/unicaone/UnicaOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Unica%20One'
+
   font 'UnicaOne-Regular.ttf'
 end
