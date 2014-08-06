ca393d74a0b0861f0116f0607382835749265e08
Roland Walker <walker@pobox.com>
2014-08-06 10:11:31 -0400

    standard Cask layout for font-preah-vihear.rb

--- Casks/font-preah-vihear.rb
+++ Casks/font-preah-vihear.rb
@@ -1,7 +1,9 @@
 class FontPreahVihear < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/preahvihear/Preahvihear.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Preahvihear'
   version '6.00'
   sha256 'c4b725aa149e48d130cf6de5a64ef78d7a1309832b7425f6f9c4a888a0b65482'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/preahvihear/Preahvihear.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Preahvihear'
+
   font 'Preahvihear.ttf'
 end
