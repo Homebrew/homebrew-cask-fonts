173e79efdf57ce3394c2bc2f1fd9d4c867038426
Roland Walker <walker@pobox.com>
2014-08-06 10:11:25 -0400

    standard Cask layout for font-love-ya-like-a-sister.rb

--- Casks/font-love-ya-like-a-sister.rb
+++ Casks/font-love-ya-like-a-sister.rb
@@ -1,7 +1,9 @@
 class FontLoveYaLikeASister < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/loveyalikeasister/LoveYaLikeASister.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Love%20Ya%20Like%20A%20Sister'
   version '1.002'
   sha256 '4fbe2c1fa647de5a415acac7b2b6491542fe9767b6199719a3bd77a7cf35eb0d'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/loveyalikeasister/LoveYaLikeASister.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Love%20Ya%20Like%20A%20Sister'
+
   font 'LoveYaLikeASister.ttf'
 end
