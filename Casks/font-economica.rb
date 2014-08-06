adfccd2577584ff800282e5c6afc5a3b1039a7ac
Roland Walker <walker@pobox.com>
2014-08-06 10:11:19 -0400

    standard Cask layout for font-economica.rb

--- Casks/font-economica.rb
+++ Casks/font-economica.rb
@@ -1,11 +1,13 @@
 class FontEconomica < Cask
+  version '1.101'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/economica',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Economica'
-  version '1.101'
-  sha256 :no_check
+
   font 'Economica-Bold.ttf'
   font 'Economica-BoldItalic.ttf'
   font 'Economica-Italic.ttf'
