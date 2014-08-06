89454b9201ea135005ef32ed23e7551253a21d0d
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-aubrey.rb

--- Casks/font-aubrey.rb
+++ Casks/font-aubrey.rb
@@ -1,7 +1,9 @@
 class FontAubrey < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/aubrey/Aubrey-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Aubrey'
   version '1.000'
   sha256 '0ec62a132f0f6617a75ff345b23f9741ec1f66bfd0a35694896b9a982943d74c'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/aubrey/Aubrey-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Aubrey'
+
   font 'Aubrey-Regular.ttf'
 end
