8f5ab4a3329b0efe0c57ba4a54cc0e57f5a46c1c
Roland Walker <walker@pobox.com>
2014-08-06 10:11:19 -0400

    standard Cask layout for font-elsie.rb

--- Casks/font-elsie.rb
+++ Casks/font-elsie.rb
@@ -1,11 +1,13 @@
 class FontElsie < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/elsie',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Elsie'
-  version '1.001'
-  sha256 :no_check
+
   font 'Elsie-Black.ttf'
   font 'Elsie-Regular.ttf'
 end
