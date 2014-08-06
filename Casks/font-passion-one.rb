d8f73b1fdd689f6aa7b7b29776a1f12be40689bb
Roland Walker <walker@pobox.com>
2014-08-06 10:11:30 -0400

    standard Cask layout for font-passion-one.rb

--- Casks/font-passion-one.rb
+++ Casks/font-passion-one.rb
@@ -1,11 +1,13 @@
 class FontPassionOne < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/passionone',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Passion%20One'
-  version '1.001'
-  sha256 :no_check
+
   font 'PassionOne-Black.ttf'
   font 'PassionOne-Bold.ttf'
   font 'PassionOne-Regular.ttf'
