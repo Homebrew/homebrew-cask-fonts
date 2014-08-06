080a6ea4c1cd3baba6ab965711515ecee8aca48e
Roland Walker <walker@pobox.com>
2014-08-06 10:11:19 -0400

    standard Cask layout for font-englebert.rb

--- Casks/font-englebert.rb
+++ Casks/font-englebert.rb
@@ -1,7 +1,9 @@
 class FontEnglebert < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/englebert/Englebert-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Englebert'
   version '1.000'
   sha256 'e7c5cf4ffa49ba93289a0434f7db8bf784983a97734588871cd93965ce5efd33'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/englebert/Englebert-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Englebert'
+
   font 'Englebert-Regular.ttf'
 end
