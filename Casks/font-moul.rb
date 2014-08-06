616448117167b99db3d4c86109721ceb3e4c96dc
Roland Walker <walker@pobox.com>
2014-08-06 10:11:27 -0400

    standard Cask layout for font-moul.rb

--- Casks/font-moul.rb
+++ Casks/font-moul.rb
@@ -1,7 +1,9 @@
 class FontMoul < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/moul/Moul.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Moul'
   version '6.00'
   sha256 'e5020ad44384950707915226c0bf70adf865fb7631ae5b19ba2ed3c41402ddc1'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/moul/Moul.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Moul'
+
   font 'Moul.ttf'
 end
