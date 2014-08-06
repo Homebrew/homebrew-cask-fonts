9d1e544bad4ac7562b5227bc860d779abdae0693
Roland Walker <walker@pobox.com>
2014-08-06 10:11:31 -0400

    standard Cask layout for font-profontx.rb

--- Casks/font-profontx.rb
+++ Casks/font-profontx.rb
@@ -1,7 +1,9 @@
 class FontProfontx < Cask
-  url 'http://faisal.com/software/profontx/ProFontX.zip'
-  homepage 'http://faisal.com/software/profontx/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://faisal.com/software/profontx/ProFontX.zip'
+  homepage 'http://faisal.com/software/profontx/'
+
   font 'ProFontX/ProFontX'
 end
