23c13e7f82f41059a94cb32b99987cc8cabd3139
Roland Walker <walker@pobox.com>
2014-08-06 10:11:13 -0400

    standard Cask layout for font-akkadian.rb

--- Casks/font-akkadian.rb
+++ Casks/font-akkadian.rb
@@ -1,7 +1,9 @@
 class FontAkkadian < Cask
-  url 'http://users.teilar.gr/~g1951d/Akkadian.zip'
-  homepage 'http://users.teilar.gr/~g1951d/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://users.teilar.gr/~g1951d/Akkadian.zip'
+  homepage 'http://users.teilar.gr/~g1951d/'
+
   font 'Akkadian_hint.ttf'
 end
