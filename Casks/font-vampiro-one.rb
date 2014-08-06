168b927246ad1bc878c1b2e57f38fe9f93545e67
Roland Walker <walker@pobox.com>
2014-08-06 10:11:36 -0400

    standard Cask layout for font-vampiro-one.rb

--- Casks/font-vampiro-one.rb
+++ Casks/font-vampiro-one.rb
@@ -1,7 +1,9 @@
 class FontVampiroOne < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/vampiroone/VampiroOne-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Vampiro%20One'
   version '1.000'
   sha256 '2a1941f6a2e63c8399cfeb9bea82bfb23caf632636d46552e651ba5803f6cd62'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/vampiroone/VampiroOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Vampiro%20One'
+
   font 'VampiroOne-Regular.ttf'
 end
