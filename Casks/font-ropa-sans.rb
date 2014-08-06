4a39371d35ee238d30fdb8ab35cc75114ada088f
Roland Walker <walker@pobox.com>
2014-08-06 10:11:32 -0400

    standard Cask layout for font-ropa-sans.rb

--- Casks/font-ropa-sans.rb
+++ Casks/font-ropa-sans.rb
@@ -1,11 +1,13 @@
 class FontRopaSans < Cask
+  version '1.002'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/ropasans',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Ropa%20Sans'
-  version '1.002'
-  sha256 :no_check
+
   font 'RopaSans-Italic.ttf'
   font 'RopaSans-Regular.ttf'
 end
