a122aecf8d42f04919695dc85eb26b692224e975
Roland Walker <walker@pobox.com>
2014-08-06 10:11:26 -0400

    standard Cask layout for font-metal.rb

--- Casks/font-metal.rb
+++ Casks/font-metal.rb
@@ -1,7 +1,9 @@
 class FontMetal < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/metal/Metal.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Metal'
   version '6.00'
   sha256 '270a7045adfbcc335e50b4fbb4c24f1c58f15393ded13a1d87bfbbbe57bfb8ae'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/metal/Metal.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Metal'
+
   font 'Metal.ttf'
 end
