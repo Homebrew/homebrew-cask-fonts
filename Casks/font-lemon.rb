716a49f57acb76ff2fb14d29157b1e333806eac6
Roland Walker <walker@pobox.com>
2014-08-06 10:11:24 -0400

    standard Cask layout for font-lemon.rb

--- Casks/font-lemon.rb
+++ Casks/font-lemon.rb
@@ -1,7 +1,9 @@
 class FontLemon < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/lemon/Lemon-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Lemon'
   version '1.002'
   sha256 '7ded93fde4589c3cc32052da187d0bcbf023dfb53af1a8b4ce847d47ad9add33'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/lemon/Lemon-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Lemon'
+
   font 'Lemon-Regular.ttf'
 end
