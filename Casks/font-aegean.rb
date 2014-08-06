ba120a6ef6b53a6408bf94c2747789e74e824b18
Roland Walker <walker@pobox.com>
2014-08-06 10:11:12 -0400

    standard Cask layout for font-aegean.rb

--- Casks/font-aegean.rb
+++ Casks/font-aegean.rb
@@ -1,7 +1,9 @@
 class FontAegean < Cask
-  url 'http://users.teilar.gr/~g1951d/Aegean.zip'
-  homepage 'http://users.teilar.gr/~g1951d/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://users.teilar.gr/~g1951d/Aegean.zip'
+  homepage 'http://users.teilar.gr/~g1951d/'
+
   font 'Aegean_hint.ttf'
 end
