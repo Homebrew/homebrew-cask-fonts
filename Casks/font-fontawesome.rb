de756c27026f6cb3804e3f47696f726a6aa572ef
Roland Walker <walker@pobox.com>
2014-08-06 10:11:20 -0400

    standard Cask layout for font-fontawesome.rb

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,7 +1,9 @@
 class FontFontawesome < Cask
-  url 'http://fortawesome.github.io/Font-Awesome/assets/font-awesome-4.1.0.zip'
-  homepage 'http://fortawesome.github.io/Font-Awesome/'
   version '4.1.0'
   sha256 '76b0117979df00caa71c4a1ae3fb41326590ea303747d1f48d611735e749657d'
+
+  url 'http://fortawesome.github.io/Font-Awesome/assets/font-awesome-4.1.0.zip'
+  homepage 'http://fortawesome.github.io/Font-Awesome/'
+
   font 'font-awesome-4.1.0/fonts/FontAwesome.otf'
 end
