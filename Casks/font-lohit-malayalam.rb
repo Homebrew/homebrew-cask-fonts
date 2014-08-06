c3fd5807fa81f871d10608a5fc60731c88975a37
Roland Walker <walker@pobox.com>
2014-08-06 10:11:25 -0400

    standard Cask layout for font-lohit-malayalam.rb

--- Casks/font-lohit-malayalam.rb
+++ Casks/font-lohit-malayalam.rb
@@ -1,7 +1,9 @@
 class FontLohitMalayalam < Cask
-  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-malayalam-ttf-2.5.4.tar.gz'
-  homepage 'https://fedorahosted.org/lohit/'
   version '2.5.4'
   sha256 '8c230bbe4cee42d55caa3e548f52dc5f54bae42a35b8eefe575297bd23b2fde4'
+
+  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-malayalam-ttf-2.5.4.tar.gz'
+  homepage 'https://fedorahosted.org/lohit/'
+
   font 'lohit-malayalam-ttf-2.5.4/Lohit-Malayalam.ttf'
 end
