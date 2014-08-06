02f0a3ea4ee2e0092e22aa0fcbf1ed4aece772a6
Roland Walker <walker@pobox.com>
2014-08-06 10:11:33 -0400

    standard Cask layout for font-sansita-one.rb

--- Casks/font-sansita-one.rb
+++ Casks/font-sansita-one.rb
@@ -1,7 +1,9 @@
 class FontSansitaOne < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/sansitaone/SansitaOne.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Sansita%20One'
   version '1.001'
   sha256 '54348a413f7689d40f4505daa093fc8ef1bba9bede83e95760631f72ed33c253'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/sansitaone/SansitaOne.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Sansita%20One'
+
   font 'SansitaOne.ttf'
 end
