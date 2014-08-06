69645dca89ab546a2bca2dbaed0e51fc8dc78210
Roland Walker <walker@pobox.com>
2014-08-06 10:11:30 -0400

    standard Cask layout for font-oregano.rb

--- Casks/font-oregano.rb
+++ Casks/font-oregano.rb
@@ -1,11 +1,13 @@
 class FontOregano < Cask
+  version '1.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/oregano',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Oregano'
-  version '1.000'
-  sha256 :no_check
+
   font 'Oregano-Italic.ttf'
   font 'Oregano-Regular.ttf'
 end
