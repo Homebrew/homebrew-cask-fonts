6a3ae9062e43d45f6af64e6b463a9aa166bcebef
Roland Walker <walker@pobox.com>
2014-08-06 10:11:31 -0400

    standard Cask layout for font-quattrocento.rb

--- Casks/font-quattrocento.rb
+++ Casks/font-quattrocento.rb
@@ -1,11 +1,13 @@
 class FontQuattrocento < Cask
+  version '2.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/quattrocento',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Quattrocento'
-  version '2.000'
-  sha256 :no_check
+
   font 'Quattrocento-Bold.ttf'
   font 'Quattrocento-Regular.ttf'
 end
