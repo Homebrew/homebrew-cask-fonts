b7488e8d9c2822883d87fe02ac8c0c07e8e8a131
Roland Walker <walker@pobox.com>
2014-08-06 10:11:25 -0400

    standard Cask layout for font-lohit-marathi.rb

--- Casks/font-lohit-marathi.rb
+++ Casks/font-lohit-marathi.rb
@@ -1,7 +1,9 @@
 class FontLohitMarathi < Cask
-  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-marathi-ttf-2.93.0.tar.gz'
-  homepage 'https://fedorahosted.org/lohit/'
   version '2.93.0'
   sha256 '4b45a36995f1df69a5d80ab8acca31e0e36ae8ed5a33f73690d5be959a6aa543'
+
+  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-marathi-ttf-2.93.0.tar.gz'
+  homepage 'https://fedorahosted.org/lohit/'
+
   font 'lohit-marathi-ttf-2.93.0/Lohit-Marathi.ttf'
 end
