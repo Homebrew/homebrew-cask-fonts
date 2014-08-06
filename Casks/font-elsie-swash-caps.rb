76abe484a360323436d94f6f7c8464c371e6c6e5
Roland Walker <walker@pobox.com>
2014-08-06 10:11:19 -0400

    standard Cask layout for font-elsie-swash-caps.rb

--- Casks/font-elsie-swash-caps.rb
+++ Casks/font-elsie-swash-caps.rb
@@ -1,11 +1,13 @@
 class FontElsieSwashCaps < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/elsieswashcaps',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Elsie%20Swash%20Caps'
-  version '1.001'
-  sha256 :no_check
+
   font 'ElsieSwashCaps-Black.ttf'
   font 'ElsieSwashCaps-Regular.ttf'
 end
