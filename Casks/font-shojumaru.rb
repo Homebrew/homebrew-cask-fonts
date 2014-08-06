76cf32e1ec6365c17e70c731aa953e10dd7a81a6
Roland Walker <walker@pobox.com>
2014-08-06 10:11:34 -0400

    standard Cask layout for font-shojumaru.rb

--- Casks/font-shojumaru.rb
+++ Casks/font-shojumaru.rb
@@ -1,7 +1,9 @@
 class FontShojumaru < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/shojumaru/Shojumaru-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Shojumaru'
   version '1.001'
   sha256 '8607110ef45da664861c72d95fb804c3609169ebfd2a380cb47b353a7b241c75'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/shojumaru/Shojumaru-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Shojumaru'
+
   font 'Shojumaru-Regular.ttf'
 end
