98e99b2b6cb14cef1c9df0701100d46f63f6e0c6
Roland Walker <walker@pobox.com>
2014-08-06 10:11:37 -0400

    standard Cask layout for font-voltaire.rb

--- Casks/font-voltaire.rb
+++ Casks/font-voltaire.rb
@@ -1,7 +1,9 @@
 class FontVoltaire < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/voltaire/Voltaire-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Voltaire'
   version '1.003'
   sha256 '5764e3b5853a61ab69005f24e7105adccfb77b8b7dee115ca2cf34042916ef32'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/voltaire/Voltaire-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Voltaire'
+
   font 'Voltaire-Regular.ttf'
 end
