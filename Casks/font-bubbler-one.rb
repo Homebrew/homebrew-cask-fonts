41e790599243978340fe9b19e3bcd603c7f2a74c
Roland Walker <walker@pobox.com>
2014-08-06 10:11:15 -0400

    standard Cask layout for font-bubbler-one.rb

--- Casks/font-bubbler-one.rb
+++ Casks/font-bubbler-one.rb
@@ -1,7 +1,9 @@
 class FontBubblerOne < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/bubblerone/BubblerOne-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Bubbler%20One'
   version '1.001'
   sha256 'edac030414f2886208051882025f32327de07620552239c3e5e8cbe7128d876b'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/bubblerone/BubblerOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Bubbler%20One'
+
   font 'BubblerOne-Regular.ttf'
 end
