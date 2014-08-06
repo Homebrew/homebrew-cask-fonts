fe899d274f7d5b669e2846dfb21e82dc9353797d
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-coustard.rb

--- Casks/font-coustard.rb
+++ Casks/font-coustard.rb
@@ -1,11 +1,13 @@
 class FontCoustard < Cask
+  version '1.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/coustard',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Coustard'
-  version '1.000'
-  sha256 :no_check
+
   font 'Coustard-Black.ttf'
   font 'Coustard-Regular.ttf'
 end
