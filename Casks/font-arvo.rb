4275c596de6a5d7f2a909c98201f18189ab626d9
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-arvo.rb

--- Casks/font-arvo.rb
+++ Casks/font-arvo.rb
@@ -1,11 +1,13 @@
 class FontArvo < Cask
+  version '2.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/arvo',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Arvo'
-  version '2.001'
-  sha256 :no_check
+
   font 'Arvo-Bold.ttf'
   font 'Arvo-BoldItalic.ttf'
   font 'Arvo-Italic.ttf'
