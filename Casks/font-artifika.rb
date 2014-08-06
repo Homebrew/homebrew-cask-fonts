5a856d12babdd21e7d6f40006af6881f3bbda139
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-artifika.rb

--- Casks/font-artifika.rb
+++ Casks/font-artifika.rb
@@ -1,7 +1,9 @@
 class FontArtifika < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/artifika/Artifika-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Artifika'
   version '1.000'
   sha256 'f07035c2a1df5a5e34dcb98005c8e9d957afdc558eaa026ef06644f3793e6965'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/artifika/Artifika-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Artifika'
+
   font 'Artifika-Regular.ttf'
 end
