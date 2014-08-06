680efe9aa3ac9a0ab758949347dbc57626ed1f6a
Roland Walker <walker@pobox.com>
2014-08-06 10:11:16 -0400

    standard Cask layout for font-cantata-one.rb

--- Casks/font-cantata-one.rb
+++ Casks/font-cantata-one.rb
@@ -1,7 +1,9 @@
 class FontCantataOne < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/cantataone/CantataOne-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Cantata%20One'
   version '1.002'
   sha256 'e12df35ae88a3e0898422110301e0b7ff4c54a4c129b92077ae7f456f45ba3db'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/cantataone/CantataOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Cantata%20One'
+
   font 'CantataOne-Regular.ttf'
 end
