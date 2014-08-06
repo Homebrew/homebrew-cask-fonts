9b31c63c644c6e183b8ae0dd77345a50357dbbeb
Roland Walker <walker@pobox.com>
2014-08-06 10:11:19 -0400

    standard Cask layout for font-droid-arabic-naskh.rb

--- Casks/font-droid-arabic-naskh.rb
+++ Casks/font-droid-arabic-naskh.rb
@@ -1,11 +1,13 @@
 class FontDroidArabicNaskh < Cask
+  version '1.00'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/droidarabicnaskh',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'https://www.google.com/fonts/earlyaccess'
-  version '1.00'
-  sha256 :no_check
+
   font 'DroidNaskh-Bold.ttf'
   font 'DroidNaskh-Regular.ttf'
 end
