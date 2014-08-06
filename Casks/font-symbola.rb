4b1bee135364e417f2a0795260130f187c29effd
Roland Walker <walker@pobox.com>
2014-08-06 10:11:35 -0400

    standard Cask layout for font-symbola.rb

--- Casks/font-symbola.rb
+++ Casks/font-symbola.rb
@@ -1,7 +1,9 @@
 class FontSymbola < Cask
-  url 'http://users.teilar.gr/~g1951d/Symbola.zip'
-  homepage 'http://users.teilar.gr/~g1951d/'
   version '7.17'
   sha256 :no_check
+
+  url 'http://users.teilar.gr/~g1951d/Symbola.zip'
+  homepage 'http://users.teilar.gr/~g1951d/'
+
   font 'Symbola_hint.ttf'
 end
