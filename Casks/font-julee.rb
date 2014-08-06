eeab87411809bad491a62805111599a90a3837b9
Roland Walker <walker@pobox.com>
2014-08-06 10:11:23 -0400

    standard Cask layout for font-julee.rb

--- Casks/font-julee.rb
+++ Casks/font-julee.rb
@@ -1,7 +1,9 @@
 class FontJulee < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/julee/Julee-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Julee'
   version '1.001'
   sha256 '788f6a017f798eda1ee65420ad93b7e0966953ebd585c53b98889b61826268de'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/julee/Julee-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Julee'
+
   font 'Julee-Regular.ttf'
 end
