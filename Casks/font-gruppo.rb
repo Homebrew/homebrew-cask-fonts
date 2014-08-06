c48a6c851862d839ecf1cde71e45fa16b4eadb63
Roland Walker <walker@pobox.com>
2014-08-06 10:11:21 -0400

    standard Cask layout for font-gruppo.rb

--- Casks/font-gruppo.rb
+++ Casks/font-gruppo.rb
@@ -1,7 +1,9 @@
 class FontGruppo < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/gruppo/Gruppo-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Gruppo'
   version '1.000'
   sha256 '2bf89b4b10d8d4a08aec9f6ed124bca48adb10a8bba02d080559195e39dffb80'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/gruppo/Gruppo-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Gruppo'
+
   font 'Gruppo-Regular.ttf'
 end
