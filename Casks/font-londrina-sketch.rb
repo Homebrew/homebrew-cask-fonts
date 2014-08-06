9d6e144beb662c5ac5914f76ec228895d6a05d77
Roland Walker <walker@pobox.com>
2014-08-06 10:11:25 -0400

    standard Cask layout for font-londrina-sketch.rb

--- Casks/font-londrina-sketch.rb
+++ Casks/font-londrina-sketch.rb
@@ -1,7 +1,9 @@
 class FontLondrinaSketch < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/londrinasketch/LondrinaSketch-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Londrina%20Sketch'
   version '1.001'
   sha256 'a95257601f3bac1bcfe7566183aaa079911f12daabd95a3bc584b4004ec92511'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/londrinasketch/LondrinaSketch-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Londrina%20Sketch'
+
   font 'LondrinaSketch-Regular.ttf'
 end
