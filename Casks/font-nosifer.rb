ef6e6135bdc045ee212c6aee239e0a3aa78874f5
Roland Walker <walker@pobox.com>
2014-08-06 10:11:28 -0400

    standard Cask layout for font-nosifer.rb

--- Casks/font-nosifer.rb
+++ Casks/font-nosifer.rb
@@ -1,7 +1,9 @@
 class FontNosifer < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/nosifer/Nosifer-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Nosifer'
   version '001.002'
   sha256 '09c5ac35e34dc7a397d5f698b703c5640b59cf93d4d343ebae4f16f3859d09b8'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/nosifer/Nosifer-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Nosifer'
+
   font 'Nosifer-Regular.ttf'
 end
