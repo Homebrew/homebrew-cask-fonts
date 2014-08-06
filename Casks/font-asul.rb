f1e4e0d4e2fcced10743bf38ceaaa4eb565a955b
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-asul.rb

--- Casks/font-asul.rb
+++ Casks/font-asul.rb
@@ -1,11 +1,13 @@
 class FontAsul < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/asul',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Asul'
-  version '1.001'
-  sha256 :no_check
+
   font 'Asul-Bold.ttf'
   font 'Asul-Regular.ttf'
 end
