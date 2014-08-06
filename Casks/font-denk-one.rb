4b7b56ce25f307a0702d61645926c97c71f8dcd5
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-denk-one.rb

--- Casks/font-denk-one.rb
+++ Casks/font-denk-one.rb
@@ -1,7 +1,9 @@
 class FontDenkOne < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/denkone/DenkOne-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Denk%20One'
   version '1.002'
   sha256 '0d2efbe8916c14f7ce30afed5cd09a503fd6b3b1498bfaad436ee47d3bde8ec0'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/denkone/DenkOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Denk%20One'
+
   font 'DenkOne-Regular.ttf'
 end
