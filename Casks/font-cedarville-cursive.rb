b3c461b20a5bb0aa8d1549da65229a019f50566a
Roland Walker <walker@pobox.com>
2014-08-06 10:11:16 -0400

    standard Cask layout for font-cedarville-cursive.rb

--- Casks/font-cedarville-cursive.rb
+++ Casks/font-cedarville-cursive.rb
@@ -1,7 +1,9 @@
 class FontCedarvilleCursive < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/cedarvillecursive/Cedarville-Cursive.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Cedarville%20Cursive'
   version '1.001'
   sha256 '9c4bcd3d515b5325bc922df3ae2932a58558a36e88818ac0297d46c51772df46'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/cedarvillecursive/Cedarville-Cursive.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Cedarville%20Cursive'
+
   font 'Cedarville-Cursive.ttf'
 end
