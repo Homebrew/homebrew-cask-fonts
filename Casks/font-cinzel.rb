0c256900d1cf42e6d5029e45706eee25d0b69a17
Roland Walker <walker@pobox.com>
2014-08-06 10:11:16 -0400

    standard Cask layout for font-cinzel.rb

--- Casks/font-cinzel.rb
+++ Casks/font-cinzel.rb
@@ -1,11 +1,13 @@
 class FontCinzel < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/cinzel',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Cinzel'
-  version '1.001'
-  sha256 :no_check
+
   font 'Cinzel-Black.ttf'
   font 'Cinzel-Bold.ttf'
   font 'Cinzel-Regular.ttf'
