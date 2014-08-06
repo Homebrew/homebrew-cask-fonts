9ebdd7c109b838cdb1c19a07e1dba07c9ec228f4
Roland Walker <walker@pobox.com>
2014-08-06 10:11:37 -0400

    standard Cask layout for font-wallpoet.rb

--- Casks/font-wallpoet.rb
+++ Casks/font-wallpoet.rb
@@ -1,7 +1,9 @@
 class FontWallpoet < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/wallpoet/Wallpoet-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Wallpoet'
   version '1.000'
   sha256 'a49f7ea2459103a76f5437944a0148566b1ed3776513de94c3293b586610ec0c'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/wallpoet/Wallpoet-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Wallpoet'
+
   font 'Wallpoet-Regular.ttf'
 end
