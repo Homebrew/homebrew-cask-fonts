36bff01dbab60ef8712c8bbb8f4d2cd52767c9a2
Roland Walker <walker@pobox.com>
2014-08-06 10:11:15 -0400

    standard Cask layout for font-bubblegum-sans.rb

--- Casks/font-bubblegum-sans.rb
+++ Casks/font-bubblegum-sans.rb
@@ -1,7 +1,9 @@
 class FontBubblegumSans < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/bubblegumsans/BubblegumSans-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Bubblegum%20Sans'
   version '1.001'
   sha256 '36998d80f145a3c2949c042ba7e66813cda11a339f33b956d5ae195850afd924'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/bubblegumsans/BubblegumSans-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Bubblegum%20Sans'
+
   font 'BubblegumSans-Regular.ttf'
 end
