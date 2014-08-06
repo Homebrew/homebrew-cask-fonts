e12c287eeb6592339fe59e34b73807c728968da6
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-comic-sans-ms.rb

--- Casks/font-comic-sans-ms.rb
+++ Casks/font-comic-sans-ms.rb
@@ -1,9 +1,11 @@
 class FontComicSansMs < Cask
+  version '2.10'
+  sha256 '9c6df3feefde26d4e41d4a4fe5db2a89f9123a772594d7f59afd062625cd204e'
+
   url 'http://downloads.sourceforge.net/sourceforge/corefonts/comic32.exe'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
-  version '2.10'
   depends_on_formula 'cabextract'
-  sha256 '9c6df3feefde26d4e41d4a4fe5db2a89f9123a772594d7f59afd062625cd204e'
+
   font 'Comicbd.TTF'
   font 'Comic.TTF'
 end
