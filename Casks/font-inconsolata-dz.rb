f0e0d1277b396193f91e27d958d23bf001b41219
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-inconsolata-dz.rb

--- Casks/font-inconsolata-dz.rb
+++ Casks/font-inconsolata-dz.rb
@@ -1,7 +1,9 @@
 class FontInconsolataDz < Cask
-  url 'http://media.nodnod.net/Inconsolata-dz.otf.zip'
-  homepage 'http://nodnod.net/2009/feb/12/adding-straight-single-and-double-quotes-inconsola/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://media.nodnod.net/Inconsolata-dz.otf.zip'
+  homepage 'http://nodnod.net/2009/feb/12/adding-straight-single-and-double-quotes-inconsola/'
+
   font 'Inconsolata-dz.otf'
 end
