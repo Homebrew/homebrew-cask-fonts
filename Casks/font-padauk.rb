5e87d9146b92343201074cdcf9d3d1c5c98b8439
Roland Walker <walker@pobox.com>
2014-08-06 10:11:30 -0400

    standard Cask layout for font-padauk.rb

--- Casks/font-padauk.rb
+++ Casks/font-padauk.rb
@@ -1,11 +1,13 @@
 class FontPadauk < Cask
+  version '2.8'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/padauk',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'https://www.google.com/fonts/earlyaccess'
-  version '2.8'
-  sha256 :no_check
+
   font 'Padauk-Bold.ttf'
   font 'Padauk-Regular.ttf'
 end
