f499539984d0f6fc418e309075eb1e92f9a81929
Roland Walker <walker@pobox.com>
2014-08-06 10:11:27 -0400

    standard Cask layout for font-montserrat-subrayada.rb

--- Casks/font-montserrat-subrayada.rb
+++ Casks/font-montserrat-subrayada.rb
@@ -1,11 +1,13 @@
 class FontMontserratSubrayada < Cask
+  version '2.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/montserratsubrayada',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Montserrat%20Subrayada'
-  version '2.001'
-  sha256 :no_check
+
   font 'MontserratSubrayada-Bold.ttf'
   font 'MontserratSubrayada-Regular.ttf'
 end
