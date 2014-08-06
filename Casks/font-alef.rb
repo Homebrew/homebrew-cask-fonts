b3b68905d1df78d6371a47cb1379bd69e7b678c3
Roland Walker <walker@pobox.com>
2014-08-06 10:11:13 -0400

    standard Cask layout for font-alef.rb

--- Casks/font-alef.rb
+++ Casks/font-alef.rb
@@ -1,11 +1,13 @@
 class FontAlef < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/alef',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Alef'
-  version '1.001'
-  sha256 :no_check
+
   font 'Alef-Bold.ttf'
   font 'Alef-Regular.ttf'
 end
