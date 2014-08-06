25143ca22c6ddfd2ae5c4ac2f6cb5da9f138f632
Roland Walker <walker@pobox.com>
2014-08-06 10:11:32 -0400

    standard Cask layout for font-rosarivo.rb

--- Casks/font-rosarivo.rb
+++ Casks/font-rosarivo.rb
@@ -1,11 +1,13 @@
 class FontRosarivo < Cask
+  version '1.003'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/rosarivo',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Rosarivo'
-  version '1.003'
-  sha256 :no_check
+
   font 'Rosarivo-Italic.ttf'
   font 'Rosarivo-Regular.ttf'
 end
