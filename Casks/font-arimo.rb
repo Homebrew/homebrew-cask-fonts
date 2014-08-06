ff3d4f9e9d3b584b819ff124ec2c3e8960025197
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-arimo.rb

--- Casks/font-arimo.rb
+++ Casks/font-arimo.rb
@@ -1,11 +1,13 @@
 class FontArimo < Cask
+  version '1.23'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/arimo',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Arimo'
-  version '1.23'
-  sha256 :no_check
+
   font 'Arimo-Bold.ttf'
   font 'Arimo-BoldItalic.ttf'
   font 'Arimo-Italic.ttf'
