1d4cca112615dad34ebec15d77d880ddc45e735d
Roland Walker <walker@pobox.com>
2014-08-06 10:11:24 -0400

    standard Cask layout for font-lekton.rb

--- Casks/font-lekton.rb
+++ Casks/font-lekton.rb
@@ -1,11 +1,13 @@
 class FontLekton < Cask
+  version '34.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/lekton',
       :using      => :svn,
       :revision   => '47',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Lekton'
-  version '34.000'
-  sha256 :no_check
+
   font 'Lekton-Bold.ttf'
   font 'Lekton-Italic.ttf'
   font 'Lekton-Regular.ttf'
