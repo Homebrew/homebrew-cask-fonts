7749fdc043b081ec42dad240f313ff7f84d5a709
Roland Walker <walker@pobox.com>
2014-08-06 10:11:19 -0400

    standard Cask layout for font-eeyek-unicode.rb

--- Casks/font-eeyek-unicode.rb
+++ Casks/font-eeyek-unicode.rb
@@ -1,7 +1,9 @@
 class FontEeyekUnicode < Cask
-  url 'http://tabish.freeshell.org/eeyek/eeyek.zip'
-  homepage 'http://tabish.freeshell.org/eeyek/download.html'
   version 'latest'
   sha256 :no_check
+
+  url 'http://tabish.freeshell.org/eeyek/eeyek.zip'
+  homepage 'http://tabish.freeshell.org/eeyek/download.html'
+
   font 'Eeyek_Unicode/Eeyek.ttf'
 end
