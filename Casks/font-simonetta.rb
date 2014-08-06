89f2288d2e03b860c8d0fd84230af051646233af
Roland Walker <walker@pobox.com>
2014-08-06 10:11:34 -0400

    standard Cask layout for font-simonetta.rb

--- Casks/font-simonetta.rb
+++ Casks/font-simonetta.rb
@@ -1,11 +1,13 @@
 class FontSimonetta < Cask
+  version '1.002'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/simonetta',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Simonetta'
-  version '1.002'
-  sha256 :no_check
+
   font 'Simonetta-Black.ttf'
   font 'Simonetta-BlackItalic.ttf'
   font 'Simonetta-Italic.ttf'
