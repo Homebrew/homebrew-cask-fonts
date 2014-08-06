508b091e7265f41a75b75b59d9033e8d7e88fe3c
Roland Walker <walker@pobox.com>
2014-08-06 10:53:03 -0400

    version 'latest' for font-fondamento.rb
    preserve known version in comment for later use

--- Casks/font-fondamento.rb
+++ Casks/font-fondamento.rb
@@ -1,5 +1,6 @@
 class FontFondamento < Cask
-  version '1.000'
+  # version '1.000'
+  version 'latest'
   sha256 :no_check
 
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/fondamento',
