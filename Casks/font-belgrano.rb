530a2d5e13593132711b9fd29a462b6902444230
Roland Walker <walker@pobox.com>
2014-08-06 10:11:15 -0400

    standard Cask layout for font-belgrano.rb

--- Casks/font-belgrano.rb
+++ Casks/font-belgrano.rb
@@ -1,7 +1,9 @@
 class FontBelgrano < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/belgrano/Belgrano-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Belgrano'
   version '1.002'
   sha256 '0e33cdbcc7c8f3431301ded4bceca1d6c3fe458a9b0b08505ce4e372dfadd669'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/belgrano/Belgrano-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Belgrano'
+
   font 'Belgrano-Regular.ttf'
 end
