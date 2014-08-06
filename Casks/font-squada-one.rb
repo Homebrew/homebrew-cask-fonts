d29f7149ad1c4eb8b7ba0ca57784fc33e9f18422
Roland Walker <walker@pobox.com>
2014-08-06 10:11:35 -0400

    standard Cask layout for font-squada-one.rb

--- Casks/font-squada-one.rb
+++ Casks/font-squada-one.rb
@@ -1,7 +1,9 @@
 class FontSquadaOne < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/squadaone/SquadaOne-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Squada%20One'
   version '1.001'
   sha256 '5b34a1633dbc48db3835d8a847bc475e6e0267121d3435854729ba4b407a05df'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/squadaone/SquadaOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Squada%20One'
+
   font 'SquadaOne-Regular.ttf'
 end
