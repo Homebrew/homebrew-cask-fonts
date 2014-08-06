4bae18f3fb8958d3f33a32dfffe452c362d16978
Roland Walker <walker@pobox.com>
2014-08-06 10:11:24 -0400

    standard Cask layout for font-linden-hill.rb

--- Casks/font-linden-hill.rb
+++ Casks/font-linden-hill.rb
@@ -1,11 +1,13 @@
 class FontLindenHill < Cask
+  version '1.202'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/lindenhill',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Linden%20Hill'
-  version '1.202'
-  sha256 :no_check
+
   font 'LindenHill-Italic.ttf'
   font 'LindenHill-Regular.ttf'
 end
