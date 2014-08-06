d84895dbac7a881a18a07d2153b87cf5b7048c86
Roland Walker <walker@pobox.com>
2014-08-06 10:11:13 -0400

    standard Cask layout for font-almendra-sc.rb

--- Casks/font-almendra-sc.rb
+++ Casks/font-almendra-sc.rb
@@ -1,11 +1,13 @@
 class FontAlmendraSc < Cask
+  version '1.003'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/almendrasc',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Almendra%20SC'
-  version '1.003'
-  sha256 :no_check
+
   font 'AlmendraSC-Bold.ttf'
   font 'AlmendraSC-BoldItalic.ttf'
   font 'AlmendraSC-Italic.ttf'
