a5f5f0a469926acb216bf92898f71398fb59db01
Roland Walker <walker@pobox.com>
2014-08-06 10:11:19 -0400

    standard Cask layout for font-fanwood-text.rb

--- Casks/font-fanwood-text.rb
+++ Casks/font-fanwood-text.rb
@@ -1,11 +1,13 @@
 class FontFanwoodText < Cask
+  version '1.101'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/fanwoodtext',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Fanwood%20Text'
-  version '1.101'
-  sha256 :no_check
+
   font 'FanwoodText-Italic.ttf'
   font 'FanwoodText-Regular.ttf'
 end
