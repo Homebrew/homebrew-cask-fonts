206871470af25bfafd8969a4c01750a6afe1d6f9
Roland Walker <walker@pobox.com>
2014-08-06 10:11:29 -0400

    standard Cask layout for font-oleo-script.rb

--- Casks/font-oleo-script.rb
+++ Casks/font-oleo-script.rb
@@ -1,11 +1,13 @@
 class FontOleoScript < Cask
+  version '1.002'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/oleoscript',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Oleo%20Script'
-  version '1.002'
-  sha256 :no_check
+
   font 'OleoScript-Bold.ttf'
   font 'OleoScript-Regular.ttf'
 end
