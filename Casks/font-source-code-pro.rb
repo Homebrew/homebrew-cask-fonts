39fb36e690a3c5f557e95ee838a9131cf1150280
Roland Walker <walker@pobox.com>
2014-08-06 10:11:34 -0400

    standard Cask layout for font-source-code-pro.rb

--- Casks/font-source-code-pro.rb
+++ Casks/font-source-code-pro.rb
@@ -1,8 +1,10 @@
 class FontSourceCodePro < Cask
-  url 'http://downloads.sourceforge.net/sourceforge/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip'
-  homepage 'http://store1.adobe.com/cfusion/store/html/index.cfm?store=OLS-US&event=displayFontPackage&code=1960'
   version '1.017'
   sha256 '8136b4686309c428ef073356ab178c2f7e8f7b6fadd5a6c61b6a20646377b21f'
+
+  url 'http://downloads.sourceforge.net/sourceforge/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip'
+  homepage 'http://store1.adobe.com/cfusion/store/html/index.cfm?store=OLS-US&event=displayFontPackage&code=1960'
+
   font 'SourceCodePro_FontsOnly-1.017/OTF/SourceCodePro-Black.otf'
   font 'SourceCodePro_FontsOnly-1.017/OTF/SourceCodePro-Bold.otf'
   font 'SourceCodePro_FontsOnly-1.017/OTF/SourceCodePro-ExtraLight.otf'
