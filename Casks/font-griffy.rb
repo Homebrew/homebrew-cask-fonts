0fcaf5e4ac2c39765c82ae37bcb57e80cf34742e
Roland Walker <walker@pobox.com>
2014-08-06 10:11:21 -0400

    standard Cask layout for font-griffy.rb

--- Casks/font-griffy.rb
+++ Casks/font-griffy.rb
@@ -1,7 +1,9 @@
 class FontGriffy < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/griffy/Griffy-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Griffy'
   version '1.000'
   sha256 'c889c3f8d169631386a297eae8b5bdefbf8d06aa9f4f325faec31b9b5f38eeb5'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/griffy/Griffy-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Griffy'
+
   font 'Griffy-Regular.ttf'
 end
