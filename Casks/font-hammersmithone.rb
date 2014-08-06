bc25f86c4278f9b240b692576ae8c888ce81058b
Roland Walker <walker@pobox.com>
2014-08-06 10:11:21 -0400

    standard Cask layout for font-hammersmithone.rb

--- Casks/font-hammersmithone.rb
+++ Casks/font-hammersmithone.rb
@@ -1,7 +1,9 @@
 class FontHammersmithone < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/hammersmithone/HammersmithOne-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Hammersmith%20One'
   version '1.003'
   sha256 '5c8da79fd5bd09fc38c391c4026f0bd4032d2f88f14c523b265a21c96c2cec64'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/hammersmithone/HammersmithOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Hammersmith%20One'
+
   font 'HammersmithOne-Regular.ttf'
 end
