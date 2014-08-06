8687b64688d5176008b2bae283561a376939a511
Roland Walker <walker@pobox.com>
2014-08-06 10:11:31 -0400

    standard Cask layout for font-pt-mono.rb

--- Casks/font-pt-mono.rb
+++ Casks/font-pt-mono.rb
@@ -1,8 +1,10 @@
 class FontPtMono < Cask
-  url 'http://www.fontstock.com/public/PTMono.zip'
-  homepage 'http://www.paratype.com/public/'
   version '1.002'
   sha256 '93c0ca1d9873ecc7c70131fa763aaaaa24345fc8b8f193ea2175b0857823d58a'
+
+  url 'http://www.fontstock.com/public/PTMono.zip'
+  homepage 'http://www.paratype.com/public/'
+
   font 'PTM55F.ttf'
   font 'PTM75F.ttf'
 end
