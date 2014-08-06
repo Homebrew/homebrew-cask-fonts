eae5b2acfb15b40c9f92745366d4c1154cf12b07
Roland Walker <walker@pobox.com>
2014-08-06 10:11:24 -0400

    standard Cask layout for font-lohit-assamese.rb

--- Casks/font-lohit-assamese.rb
+++ Casks/font-lohit-assamese.rb
@@ -1,7 +1,9 @@
 class FontLohitAssamese < Cask
-  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-assamese-ttf-2.5.3.tar.gz'
-  homepage 'https://fedorahosted.org/lohit/'
   version '2.5.3'
   sha256 'ce8e510f4e78527eb4c514bf49ed69389b983cd8c4990f0a7b9b9f43b6bef9ce'
+
+  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-assamese-ttf-2.5.3.tar.gz'
+  homepage 'https://fedorahosted.org/lohit/'
+
   font 'lohit-assamese-ttf-2.5.3/Lohit-Assamese.ttf'
 end
