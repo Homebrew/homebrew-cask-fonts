4d4a6d55c9e8b9f1fa12fcc6436a3167b1642a92
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-droid-arabic-kufi.rb

--- Casks/font-droid-arabic-kufi.rb
+++ Casks/font-droid-arabic-kufi.rb
@@ -1,11 +1,13 @@
 class FontDroidArabicKufi < Cask
+  version '1.00'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/droidarabickufi',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'https://www.google.com/fonts/earlyaccess'
-  version '1.00'
-  sha256 :no_check
+
   font 'DroidKufi-Bold.ttf'
   font 'DroidKufi-Regular.ttf'
 end
