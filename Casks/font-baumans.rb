ac144c66115e4411ef656d9eb187c44909e279b6
Roland Walker <walker@pobox.com>
2014-08-06 10:11:15 -0400

    standard Cask layout for font-baumans.rb

--- Casks/font-baumans.rb
+++ Casks/font-baumans.rb
@@ -1,7 +1,9 @@
 class FontBaumans < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/baumans/Baumans-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Baumans'
   version '001.001'
   sha256 '46d4196124065aadec02c628583fbca1abb28ab00255dbe9e5b28437bd85c797'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/baumans/Baumans-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Baumans'
+
   font 'Baumans-Regular.ttf'
 end
