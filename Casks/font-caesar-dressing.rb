49ad689ad9553e7409c6cbe20dc217e3a023028e
Roland Walker <walker@pobox.com>
2014-08-06 10:11:16 -0400

    standard Cask layout for font-caesar-dressing.rb

--- Casks/font-caesar-dressing.rb
+++ Casks/font-caesar-dressing.rb
@@ -1,7 +1,9 @@
 class FontCaesarDressing < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/caesardressing/CaesarDressing-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Caesar%20Dressing'
   version '1.000'
   sha256 '009dea24a4d4a3a7e02d683087babb1738acc0b949cce38c485fab325c9e9295'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/caesardressing/CaesarDressing-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Caesar%20Dressing'
+
   font 'CaesarDressing-Regular.ttf'
 end
