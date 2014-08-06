6501737a8e85a68f4dc16d6555e2e6d667f5c8a8
Roland Walker <walker@pobox.com>
2014-08-06 10:11:16 -0400

    standard Cask layout for font-cinzel-decorative.rb

--- Casks/font-cinzel-decorative.rb
+++ Casks/font-cinzel-decorative.rb
@@ -1,11 +1,13 @@
 class FontCinzelDecorative < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/cinzeldecorative',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Cinzel%20Decorative'
-  version '1.001'
-  sha256 :no_check
+
   font 'CinzelDecorative-Black.ttf'
   font 'CinzelDecorative-Bold.ttf'
   font 'CinzelDecorative-Regular.ttf'
