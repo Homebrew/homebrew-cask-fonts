34c853bb71ee4986b88dd980c79daacb9ea68fbf
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-dhyana.rb

--- Casks/font-dhyana.rb
+++ Casks/font-dhyana.rb
@@ -1,11 +1,13 @@
 class FontDhyana < Cask
+  version '1.002'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/dhyana',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'https://www.google.com/fonts/earlyaccess'
-  version '1.002'
-  sha256 :no_check
+
   font 'Dhyana-Bold.ttf'
   font 'Dhyana-Regular.ttf'
 end
