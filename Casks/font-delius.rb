1a858350300a35814a28b9047f22121ff29860fc
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-delius.rb

--- Casks/font-delius.rb
+++ Casks/font-delius.rb
@@ -1,7 +1,9 @@
 class FontDelius < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/delius/Delius-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Delius'
   version '1.001'
   sha256 'c0fd66626926b637d64eb13de013b3318dbd72cdf92c33e41a29a03828bab6f6'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/delius/Delius-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Delius'
+
   font 'Delius-Regular.ttf'
 end
