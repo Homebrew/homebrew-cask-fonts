adb9dd6f5ee6a7501618c4cda5b3ba4013d1b280
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-italiana.rb

--- Casks/font-italiana.rb
+++ Casks/font-italiana.rb
@@ -1,7 +1,9 @@
 class FontItaliana < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/italiana/Italiana-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Italiana'
   version '001.001'
   sha256 '0ad271d9cd42a57cd5c5fa483ae2406a8138b95acb2a43eb73a8d37b7291fee9'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/italiana/Italiana-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Italiana'
+
   font 'Italiana-Regular.ttf'
 end
