afeef25617c6d9066dd1359be7c1d46266888ad5
Roland Walker <walker@pobox.com>
2014-08-06 10:11:35 -0400

    standard Cask layout for font-the-girl-next-door.rb

--- Casks/font-the-girl-next-door.rb
+++ Casks/font-the-girl-next-door.rb
@@ -1,7 +1,9 @@
 class FontTheGirlNextDoor < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/thegirlnextdoor/TheGirlNextDoor.ttf'
-  homepage 'http://www.google.com/fonts/specimen/The%20Girl%20Next%20Door'
   version '1.002'
   sha256 '35329e71258e48398643f1f94175aa0c9a8d18bd179918d9ac4f3d497b6557a1'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/thegirlnextdoor/TheGirlNextDoor.ttf'
+  homepage 'http://www.google.com/fonts/specimen/The%20Girl%20Next%20Door'
+
   font 'TheGirlNextDoor.ttf'
 end
