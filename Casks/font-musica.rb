a50fa67bcfd224cb305651b06d55833ccf0d88d9
Roland Walker <walker@pobox.com>
2014-08-06 10:11:27 -0400

    standard Cask layout for font-musica.rb

--- Casks/font-musica.rb
+++ Casks/font-musica.rb
@@ -1,7 +1,9 @@
 class FontMusica < Cask
-  url 'http://users.teilar.gr/~g1951d/Musica.zip'
-  homepage 'http://users.teilar.gr/~g1951d/'
   version '3.12'
   sha256 :no_check
+
+  url 'http://users.teilar.gr/~g1951d/Musica.zip'
+  homepage 'http://users.teilar.gr/~g1951d/'
+
   font 'Musica_hint.ttf'
 end
