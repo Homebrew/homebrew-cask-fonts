51785b3dc8fbae3cc25fe5da8acdac4a887d1de0
Roland Walker <walker@pobox.com>
2014-08-06 10:53:02 -0400

    version 'latest' for font-droid-sans-tamil.rb
    preserve known version in comment for later use

--- Casks/font-droid-sans-tamil.rb
+++ Casks/font-droid-sans-tamil.rb
@@ -1,5 +1,6 @@
 class FontDroidSansTamil < Cask
-  version '1.02'
+  # version '1.02'
+  version 'latest'
   sha256 :no_check
 
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/droidsanstamil',
