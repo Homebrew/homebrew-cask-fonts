3f3685d5796569417b04c820abc6e4dd89ba6db3
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-istok-web.rb

--- Casks/font-istok-web.rb
+++ Casks/font-istok-web.rb
@@ -1,11 +1,13 @@
 class FontIstokWeb < Cask
+  version '1.0'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/istokweb',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Istok%20Web'
-  version '1.0'
-  sha256 :no_check
+
   font 'IstokWeb-Bold.ttf'
   font 'IstokWeb-BoldItalic.ttf'
   font 'IstokWeb-Italic.ttf'
