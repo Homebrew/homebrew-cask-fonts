ec114fd15e6929329009ccaf6000424f75cbe878
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-cousine.rb

--- Casks/font-cousine.rb
+++ Casks/font-cousine.rb
@@ -1,11 +1,13 @@
 class FontCousine < Cask
+  version '1.21'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/cousine',
       :using      => :svn,
       :revision   => '47',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Cousine'
-  version '1.21'
-  sha256 :no_check
+
   font 'Cousine-Regular.ttf'
   font 'Cousine-Bold.ttf'
   font 'Cousine-BoldItalic.ttf'
