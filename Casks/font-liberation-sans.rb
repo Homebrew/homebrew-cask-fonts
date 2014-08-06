47c1dfbc5e4f8ddfff67a178b837547e7db54d59
Roland Walker <walker@pobox.com>
2014-08-06 10:11:24 -0400

    standard Cask layout for font-liberation-sans.rb

--- Casks/font-liberation-sans.rb
+++ Casks/font-liberation-sans.rb
@@ -1,8 +1,10 @@
 class FontLiberationSans < Cask
-  url 'https://fedorahosted.org/releases/l/i/liberation-fonts/liberation-fonts-ttf-2.00.1.tar.gz'
-  homepage 'https://fedorahosted.org/liberation-fonts/'
   version '2.00.1'
   sha256 '7890278a6cd17873c57d9cd785c2d230d9abdea837e96516019c5885dd271504'
+
+  url 'https://fedorahosted.org/releases/l/i/liberation-fonts/liberation-fonts-ttf-2.00.1.tar.gz'
+  homepage 'https://fedorahosted.org/liberation-fonts/'
+
   font 'liberation-fonts-ttf-2.00.1/LiberationMono-Bold.ttf'
   font 'liberation-fonts-ttf-2.00.1/LiberationMono-BoldItalic.ttf'
   font 'liberation-fonts-ttf-2.00.1/LiberationMono-Italic.ttf'
