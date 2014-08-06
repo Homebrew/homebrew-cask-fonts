f74a017a2318b3ab0e72579eb875b94af0004307
Roland Walker <walker@pobox.com>
2014-08-06 10:11:36 -0400

    standard Cask layout for font-vollkorn.rb

--- Casks/font-vollkorn.rb
+++ Casks/font-vollkorn.rb
@@ -1,8 +1,10 @@
 class FontVollkorn < Cask
-  url 'http://vollkorn-typeface.com/download/vollkorn-3-005.zip'
-  homepage 'http://vollkorn-typeface.com/'
   version '3.005'
   sha256 '2ba29d185bcf943f844a88546fbc4ec668d53f2564d8aa1c5837ffb9a45f4fab'
+
+  url 'http://vollkorn-typeface.com/download/vollkorn-3-005.zip'
+  homepage 'http://vollkorn-typeface.com/'
+
   font 'PS-OTF/Vollkorn-Bold.otf'
   font 'PS-OTF/Vollkorn-BoldItalic.otf'
   font 'PS-OTF/Vollkorn-Italic.otf'
