f4b259105fcd3555a6fb8aa27a4bae8e6c2ad93f
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-arbutus.rb

--- Casks/font-arbutus.rb
+++ Casks/font-arbutus.rb
@@ -1,7 +1,9 @@
 class FontArbutus < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/arbutus/Arbutus-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Arbutus'
   version '1.002'
   sha256 'bb0365451e8d111edc1466bab7cfc8decbdfe9c583ba021718bbae8bd06bc930'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/arbutus/Arbutus-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Arbutus'
+
   font 'Arbutus-Regular.ttf'
 end
