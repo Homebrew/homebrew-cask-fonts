fd657693e2256d5c01d0b7274d026927f77400ae
Roland Walker <walker@pobox.com>
2014-08-06 10:11:22 -0400

    standard Cask layout for font-impact.rb

--- Casks/font-impact.rb
+++ Casks/font-impact.rb
@@ -1,8 +1,10 @@
 class FontImpact < Cask
+  version '2.35'
+  sha256 '6061ef3b7401d9642f5dfdb5f2b376aa14663f6275e60a51207ad4facf2fccfb'
+
   url 'http://downloads.sourceforge.net/sourceforge/corefonts/impact32.exe'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
-  version '2.35'
   depends_on_formula 'cabextract'
-  sha256 '6061ef3b7401d9642f5dfdb5f2b376aa14663f6275e60a51207ad4facf2fccfb'
+
   font 'Impact.TTF'
 end
