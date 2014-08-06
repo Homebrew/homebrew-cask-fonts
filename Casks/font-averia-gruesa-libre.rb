25131b47b7fdefc571cf52845c337bb3fe2439b7
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-averia-gruesa-libre.rb

--- Casks/font-averia-gruesa-libre.rb
+++ Casks/font-averia-gruesa-libre.rb
@@ -1,7 +1,9 @@
 class FontAveriaGruesaLibre < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/averiagruesalibre/AveriaGruesaLibre-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Averia%20Gruesa%20Libre'
   version '1.001'
   sha256 '3b7a37bb73f85750b015358593baefb50a95d24635b8ca3201d018990658ce6d'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/averiagruesalibre/AveriaGruesaLibre-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Averia%20Gruesa%20Libre'
+
   font 'AveriaGruesaLibre-Regular.ttf'
 end
