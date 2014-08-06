5e4919ff715e4e6547e54e53dc242d286df6cc20
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-coda.rb

--- Casks/font-coda.rb
+++ Casks/font-coda.rb
@@ -1,11 +1,13 @@
 class FontCoda < Cask
+  version '2.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/coda',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Coda'
-  version '2.000'
-  sha256 :no_check
+
   font 'Coda-Heavy.ttf'
   font 'Coda-Regular.ttf'
 end
