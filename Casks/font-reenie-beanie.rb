093252f06f2b99127c77aadd04273fe98834c8fe
Roland Walker <walker@pobox.com>
2014-08-06 10:11:32 -0400

    standard Cask layout for font-reenie-beanie.rb

--- Casks/font-reenie-beanie.rb
+++ Casks/font-reenie-beanie.rb
@@ -1,7 +1,9 @@
 class FontReenieBeanie < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/reeniebeanie/ReenieBeanie.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Reenie%20Beanie'
   version '1.000'
   sha256 '9bdbb4afa5560a7e386de9fd25f7cc172fa1c275f7f0d59699dc60841677ced9'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/reeniebeanie/ReenieBeanie.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Reenie%20Beanie'
+
   font 'ReenieBeanie.ttf'
 end
