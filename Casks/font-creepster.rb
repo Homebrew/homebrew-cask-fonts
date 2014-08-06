0ee4095067d40b603f388f7c844b6b63236e4ae3
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-creepster.rb

--- Casks/font-creepster.rb
+++ Casks/font-creepster.rb
@@ -1,7 +1,9 @@
 class FontCreepster < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/creepster/Creepster-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Creepster'
   version '1.002'
   sha256 '402aeb734586c74aecd3dbdc454589b1fb12e2e1c71f782fd019ae68066d9f44'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/creepster/Creepster-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Creepster'
+
   font 'Creepster-Regular.ttf'
 end
