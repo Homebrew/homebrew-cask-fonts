6507e5263358d4687f171adfcd2f8f5a383d73c7
Roland Walker <walker@pobox.com>
2014-08-06 10:11:23 -0400

    standard Cask layout for font-koruri.rb

--- Casks/font-koruri.rb
+++ Casks/font-koruri.rb
@@ -1,8 +1,10 @@
 class FontKoruri < Cask
-  url 'https://github.com/Koruri/Koruri/archive/master.zip'
-  homepage 'http://koruri.lindwurm.biz/'
   version 'latest'
   sha256 '36314b2b9affad1f04e552d29a8d3df8f6def9ea3888bdef71fce1891bdd0a6e'
+
+  url 'https://github.com/Koruri/Koruri/archive/master.zip'
+  homepage 'http://koruri.lindwurm.biz/'
+
   font 'Koruri-master/Koruri-Bold.ttf'
   font 'Koruri-master/Koruri-Extrabold.ttf'
   font 'Koruri-master/Koruri-Light.ttf'
