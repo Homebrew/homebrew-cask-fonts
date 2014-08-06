d31fe2df0b200982097bddc8d3ca13079c67683e
Roland Walker <walker@pobox.com>
2014-08-06 10:11:34 -0400

    standard Cask layout for font-sonsie-one.rb

--- Casks/font-sonsie-one.rb
+++ Casks/font-sonsie-one.rb
@@ -1,7 +1,9 @@
 class FontSonsieOne < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/sonsieone/SonsieOne-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Sonsie%20One'
   version '1.003'
   sha256 'af927e627f54066390d6433631106defec38541255083ea36e4c4da27dad7525'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/sonsieone/SonsieOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Sonsie%20One'
+
   font 'SonsieOne-Regular.ttf'
 end
