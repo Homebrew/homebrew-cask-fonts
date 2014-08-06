74afed79448d60ca03d75bffa51c44d1df6bdc05
Roland Walker <walker@pobox.com>
2014-08-06 10:11:23 -0400

    standard Cask layout for font-knewave.rb

--- Casks/font-knewave.rb
+++ Casks/font-knewave.rb
@@ -1,7 +1,9 @@
 class FontKnewave < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/knewave/Knewave-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Knewave'
   version '1.001'
   sha256 'ed3bac761d755b89ab3082c844d4a623d63c7d6eef85d22ba1fb6c680e6a4436'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/knewave/Knewave-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Knewave'
+
   font 'Knewave-Regular.ttf'
 end
