744545a1025847b1f8c240472fe1b06877d816c6
Roland Walker <walker@pobox.com>
2014-08-06 10:11:24 -0400

    standard Cask layout for font-lobster-two.rb

--- Casks/font-lobster-two.rb
+++ Casks/font-lobster-two.rb
@@ -1,11 +1,13 @@
 class FontLobsterTwo < Cask
+  version '1.006'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/lobstertwo',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Lobster%20Two'
-  version '1.006'
-  sha256 :no_check
+
   font 'LobsterTwo-Bold.ttf'
   font 'LobsterTwo-BoldItalic.ttf'
   font 'LobsterTwo-Italic.ttf'
