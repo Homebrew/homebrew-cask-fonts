3881bb7cf176e333382d043cfed703c453364886
Roland Walker <walker@pobox.com>
2014-08-06 10:11:31 -0400

    standard Cask layout for font-playfair-display.rb

--- Casks/font-playfair-display.rb
+++ Casks/font-playfair-display.rb
@@ -1,11 +1,13 @@
 class FontPlayfairDisplay < Cask
+  version '1.004'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/playfairdisplay',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Playfair%20Display'
-  version '1.004'
-  sha256 :no_check
+
   font 'PlayfairDisplay-Black.ttf'
   font 'PlayfairDisplay-BlackItalic.ttf'
   font 'PlayfairDisplay-Bold.ttf'
