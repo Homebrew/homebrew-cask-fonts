52b8b8b4a1c244b92e3f4f78f88a85e49969e2f3
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-arapey.rb

--- Casks/font-arapey.rb
+++ Casks/font-arapey.rb
@@ -1,11 +1,13 @@
 class FontArapey < Cask
+  version '1.002'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/arapey',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Arapey'
-  version '1.002'
-  sha256 :no_check
+
   font 'Arapey-Italic.ttf'
   font 'Arapey-Regular.ttf'
 end
