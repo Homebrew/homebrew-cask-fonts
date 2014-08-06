da8812a35ee35e21dfddf717fa5506baeed6f2a7
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-crafty-girls.rb

--- Casks/font-crafty-girls.rb
+++ Casks/font-crafty-girls.rb
@@ -1,7 +1,9 @@
 class FontCraftyGirls < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/craftygirls/CraftyGirls.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Crafty%20Girls'
   version '1.000'
   sha256 '1e7e4897ac7aacf7d00be3651fa620840fda0ff2779f990a22bb1336834deb05'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/craftygirls/CraftyGirls.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Crafty%20Girls'
+
   font 'CraftyGirls.ttf'
 end
