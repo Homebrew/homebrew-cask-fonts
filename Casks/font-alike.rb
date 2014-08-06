37572a6bced2d181631408a630e65f7e19b81f41
Roland Walker <walker@pobox.com>
2014-08-06 10:11:13 -0400

    standard Cask layout for font-alike.rb

--- Casks/font-alike.rb
+++ Casks/font-alike.rb
@@ -1,7 +1,9 @@
 class FontAlike < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/alike/Alike-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Alike'
   version '1.210'
   sha256 'c9f714870dbc29930a718b68961a4cb9d930b3f5b180343ba4b7ab0bdb3a1d59'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/alike/Alike-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Alike'
+
   font 'Alike-Regular.ttf'
 end
