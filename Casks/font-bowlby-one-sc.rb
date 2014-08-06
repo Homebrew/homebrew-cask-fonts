b82cfb585281bfa7168ade24fb4df7b62a5b821d
Roland Walker <walker@pobox.com>
2014-08-06 10:11:15 -0400

    standard Cask layout for font-bowlby-one-sc.rb

--- Casks/font-bowlby-one-sc.rb
+++ Casks/font-bowlby-one-sc.rb
@@ -1,7 +1,9 @@
 class FontBowlbyOneSc < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/bowlbyonesc/BowlbyOneSC-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Bowlby%20One%20SC'
   version '1.2'
   sha256 'a6860487ab6b1229bb463a289890aa6e75fa12517ac035e2d8f00a1d534030d1'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/bowlbyonesc/BowlbyOneSC-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Bowlby%20One%20SC'
+
   font 'BowlbyOneSC-Regular.ttf'
 end
