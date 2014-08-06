9a37fe9c63ba88fb35f0f0ab8348de8e1f10f2ef
Roland Walker <walker@pobox.com>
2014-08-06 10:11:31 -0400

    standard Cask layout for font-pt-serif.rb

--- Casks/font-pt-serif.rb
+++ Casks/font-pt-serif.rb
@@ -1,8 +1,10 @@
 class FontPtSerif < Cask
-  url 'http://www.fontstock.com/public/PTSerif.zip'
-  homepage 'http://www.paratype.com/public/'
   version '1.001'
   sha256 '74eec886edb58899018448e69cfab7032dd2ce8ff69a44c18f88a7d2a7bf3b26'
+
+  url 'http://www.fontstock.com/public/PTSerif.zip'
+  homepage 'http://www.paratype.com/public/'
+
   font 'PTF55F.ttf'
   font 'PTF56F.ttf'
   font 'PTF75F.ttf'
