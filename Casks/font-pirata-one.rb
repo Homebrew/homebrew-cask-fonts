f15215a9fbf03732a0cfb9dc2c0dc569a87770ef
Roland Walker <walker@pobox.com>
2014-08-06 10:11:30 -0400

    standard Cask layout for font-pirata-one.rb

--- Casks/font-pirata-one.rb
+++ Casks/font-pirata-one.rb
@@ -1,7 +1,9 @@
 class FontPirataOne < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/pirataone/PirataOne-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Pirata%20One'
   version '1.001'
   sha256 '5347a2e155589ecf667d4b766613c8ee003edde9f83717fd24c09599a4b1ecc0'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/pirataone/PirataOne-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Pirata%20One'
+
   font 'PirataOne-Regular.ttf'
 end
