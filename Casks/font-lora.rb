2eaf2d244f040450386695ac5bda4f2141a565c5
Roland Walker <walker@pobox.com>
2014-08-06 10:11:25 -0400

    standard Cask layout for font-lora.rb

--- Casks/font-lora.rb
+++ Casks/font-lora.rb
@@ -1,11 +1,13 @@
 class FontLora < Cask
+  version '1.014'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/lora',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Lora'
-  version '1.014'
-  sha256 :no_check
+
   font 'Lora-Bold.ttf'
   font 'Lora-BoldItalic.ttf'
   font 'Lora-Italic.ttf'
