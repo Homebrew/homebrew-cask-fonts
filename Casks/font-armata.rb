2b6817296fef2a4a4f2c0028547c5d783d6d9925
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-armata.rb

--- Casks/font-armata.rb
+++ Casks/font-armata.rb
@@ -1,7 +1,9 @@
 class FontArmata < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/armata/Armata-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Armata'
   version '1.002'
   sha256 '3b2b6e3bf1257ba4f9f36b5153f2ac6b036553eda2244883dabafaa5628d4187'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/armata/Armata-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Armata'
+
   font 'Armata-Regular.ttf'
 end
