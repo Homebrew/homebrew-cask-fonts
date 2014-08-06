1ce2b6bd1bd76ebb62926a65c582cb3d397751a0
Roland Walker <walker@pobox.com>
2014-08-06 10:11:28 -0400

    standard Cask layout for font-nokora.rb

--- Casks/font-nokora.rb
+++ Casks/font-nokora.rb
@@ -1,11 +1,13 @@
 class FontNokora < Cask
+  version '1.3'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/nokora',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Nokora'
-  version '1.3'
-  sha256 :no_check
+
   font 'Nokora-Bold.ttf'
   font 'Nokora-Regular.ttf'
 end
