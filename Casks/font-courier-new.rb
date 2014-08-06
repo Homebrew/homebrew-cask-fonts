17ca5c35079478f5774353108952d3653111ba80
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-courier-new.rb

--- Casks/font-courier-new.rb
+++ Casks/font-courier-new.rb
@@ -1,9 +1,11 @@
 class FontCourierNew < Cask
+  version '2.82'
+  sha256 'bb511d861655dde879ae552eb86b134d6fae67cb58502e6ff73ec5d9151f3384'
+
   url 'http://downloads.sourceforge.net/sourceforge/corefonts/courie32.exe'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
-  version '2.82'
   depends_on_formula 'cabextract'
-  sha256 'bb511d861655dde879ae552eb86b134d6fae67cb58502e6ff73ec5d9151f3384'
+
   font 'cour.ttf'
   font 'courbd.ttf'
   font 'courbi.ttf'
