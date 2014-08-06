65dda1319b3fb2d2c6f18659823e12001be3482a
Roland Walker <walker@pobox.com>
2014-08-06 10:11:35 -0400

    standard Cask layout for font-tangerine.rb

--- Casks/font-tangerine.rb
+++ Casks/font-tangerine.rb
@@ -1,11 +1,13 @@
 class FontTangerine < Cask
+  version '1.3'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/tangerine',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Tangerine'
-  version '1.3'
-  sha256 :no_check
+
   font 'Tangerine_Bold.ttf'
   font 'Tangerine_Regular.ttf'
 end
