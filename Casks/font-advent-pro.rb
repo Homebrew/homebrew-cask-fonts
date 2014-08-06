168a8f22c77c6809209b1f100c75fc62bb0c2729
Roland Walker <walker@pobox.com>
2014-08-06 10:53:01 -0400

    version 'latest' for font-advent-pro.rb
    preserve known version in comment for later use

--- Casks/font-advent-pro.rb
+++ Casks/font-advent-pro.rb
@@ -1,5 +1,6 @@
 class FontAdventPro < Cask
-  version '2.003'
+  # version '2.003'
+  version 'latest'
   sha256 :no_check
 
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/adventpro',
