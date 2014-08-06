11cac215ea2a8f4dec5f8affc84f2846ee928958
Roland Walker <walker@pobox.com>
2014-08-06 10:11:21 -0400

    standard Cask layout for font-great-vibes.rb

--- Casks/font-great-vibes.rb
+++ Casks/font-great-vibes.rb
@@ -1,7 +1,9 @@
 class FontGreatVibes < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/greatvibes/GreatVibes-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Great%20Vibes'
   version '1.001'
   sha256 '9b4de643c541cc8f85b8be33ea93f6131a7911bac9cb44015b49552e402b1b55'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/greatvibes/GreatVibes-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Great%20Vibes'
+
   font 'GreatVibes-Regular.ttf'
 end
