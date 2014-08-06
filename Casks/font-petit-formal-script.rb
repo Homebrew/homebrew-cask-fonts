363069838070b9cddbeda4fad4deb085d39a0429
Roland Walker <walker@pobox.com>
2014-08-06 10:11:30 -0400

    standard Cask layout for font-petit-formal-script.rb

--- Casks/font-petit-formal-script.rb
+++ Casks/font-petit-formal-script.rb
@@ -1,7 +1,9 @@
 class FontPetitFormalScript < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/petitformalscript/PetitFormalScript-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Petit%20Formal%20Script'
   version '1.001'
   sha256 '9b80a05170bac7f372b7e00bc762fe14bafd4abdeb013747b189f66525cffe89'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/petitformalscript/PetitFormalScript-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Petit%20Formal%20Script'
+
   font 'PetitFormalScript-Regular.ttf'
 end
