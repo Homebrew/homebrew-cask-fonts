f4c366d1b6b78f2b3a18258791fbed6a72db92be
Roland Walker <walker@pobox.com>
2014-08-06 10:11:14 -0400

    standard Cask layout for font-averia-libre.rb

--- Casks/font-averia-libre.rb
+++ Casks/font-averia-libre.rb
@@ -1,11 +1,13 @@
 class FontAveriaLibre < Cask
+  version '1.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/averialibre',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Averia%20Libre'
-  version '1.001'
-  sha256 :no_check
+
   font 'AveriaLibre-Bold.ttf'
   font 'AveriaLibre-BoldItalic.ttf'
   font 'AveriaLibre-Italic.ttf'
