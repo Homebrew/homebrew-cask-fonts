4cd5bf302838a53bdd41d8b11359020c84a8b748
Roland Walker <walker@pobox.com>
2014-08-06 10:11:29 -0400

    standard Cask layout for font-old-standard-tt.rb

--- Casks/font-old-standard-tt.rb
+++ Casks/font-old-standard-tt.rb
@@ -1,11 +1,13 @@
 class FontOldStandardTt < Cask
+  version '2.0.2'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/oldstandardtt',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Old%20Standard%20TT'
-  version '2.0.2'
-  sha256 :no_check
+
   font 'OldStandard-Bold.ttf'
   font 'OldStandard-Italic.ttf'
   font 'OldStandard-Regular.ttf'
