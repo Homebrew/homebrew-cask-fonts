36aee1c0cf176380cbb7b8da8adb5262da0f6ef7
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-cretan.rb

--- Casks/font-cretan.rb
+++ Casks/font-cretan.rb
@@ -1,7 +1,9 @@
 class FontCretan < Cask
-  url 'http://users.teilar.gr/~g1951d/Cretan.zip'
-  homepage 'http://users.teilar.gr/~g1951d/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://users.teilar.gr/~g1951d/Cretan.zip'
+  homepage 'http://users.teilar.gr/~g1951d/'
+
   font 'Cretan_hint.ttf'
 end
