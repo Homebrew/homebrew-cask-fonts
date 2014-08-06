15206845d3fb0b35653ed6e9dbb96d0de864ff06
Roland Walker <walker@pobox.com>
2014-08-06 10:11:21 -0400

    standard Cask layout for font-georgia.rb

--- Casks/font-georgia.rb
+++ Casks/font-georgia.rb
@@ -1,9 +1,11 @@
 class FontGeorgia < Cask
+  version '2.05'
+  sha256 '2c2c7dcda6606ea5cf08918fb7cd3f3359e9e84338dc690013f20cd42e930301'
+
   url 'http://downloads.sourceforge.net/sourceforge/corefonts/georgi32.exe'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
-  version '2.05'
   depends_on_formula 'cabextract'
-  sha256 '2c2c7dcda6606ea5cf08918fb7cd3f3359e9e84338dc690013f20cd42e930301'
+
   font 'Georgiaz.TTF'
   font 'Georgiab.TTF'
   font 'Georgiai.TTF'
