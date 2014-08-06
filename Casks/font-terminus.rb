98f6d1cdd290159f1699a8e1f57e92f32cf6dd21
Roland Walker <walker@pobox.com>
2014-08-06 10:11:35 -0400

    standard Cask layout for font-terminus.rb

--- Casks/font-terminus.rb
+++ Casks/font-terminus.rb
@@ -1,8 +1,10 @@
 class FontTerminus < Cask
-  url 'http://files.ax86.net/terminus-ttf/files/4.39/terminus-ttf-4.39.zip'
-  homepage 'http://files.ax86.net/terminus-ttf/'
   version '4.39'
   sha256 '5d8a7075757fd1a66012fafc6bd761e5b758573ea66bc1197025f1d4eb343383' 
+
+  url 'http://files.ax86.net/terminus-ttf/files/4.39/terminus-ttf-4.39.zip'
+  homepage 'http://files.ax86.net/terminus-ttf/'
+
   font 'terminus-ttf-4.39/TerminusTTF-4.39.ttf'
   font 'terminus-ttf-4.39/TerminusTTF-Bold-4.39.ttf'
   font 'terminus-ttf-4.39/TerminusTTF-Italic-4.39.ttf'
