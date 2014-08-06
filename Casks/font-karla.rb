e20ac2ce40198b1ccd7ec5e3b000a45b2cae86e8
Roland Walker <walker@pobox.com>
2014-08-06 10:11:23 -0400

    standard Cask layout for font-karla.rb

--- Casks/font-karla.rb
+++ Casks/font-karla.rb
@@ -1,11 +1,13 @@
 class FontKarla < Cask
+  version '1.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/karla',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Karla'
-  version '1.000'
-  sha256 :no_check
+
   font 'Karla-Bold.ttf'
   font 'Karla-BoldItalic.ttf'
   font 'Karla-Italic.ttf'
