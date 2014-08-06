fa5e4420e5558f03c6cec9ca46b39fc7043ff6b8
Roland Walker <walker@pobox.com>
2014-08-06 10:11:33 -0400

    standard Cask layout for font-scheherazade.rb

--- Casks/font-scheherazade.rb
+++ Casks/font-scheherazade.rb
@@ -1,7 +1,9 @@
 class FontScheherazade < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/scheherazade/ScheherazadeRegOT.ttf'
-  homepage 'https://www.google.com/fonts/earlyaccess'
   version '1.005'
   sha256 'e7a6ac89d6049175ada1d2de1c11fc20259deb26461387e6e69b38e878491579'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/scheherazade/ScheherazadeRegOT.ttf'
+  homepage 'https://www.google.com/fonts/earlyaccess'
+
   font 'ScheherazadeRegOT.ttf'
 end
