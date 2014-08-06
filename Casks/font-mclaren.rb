b6ad1a487979f9d929228df81cc569b06a487aad
Roland Walker <walker@pobox.com>
2014-08-06 10:11:26 -0400

    standard Cask layout for font-mclaren.rb

--- Casks/font-mclaren.rb
+++ Casks/font-mclaren.rb
@@ -1,7 +1,9 @@
 class FontMclaren < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/mclaren/McLaren-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/McLaren'
   version '1.000'
   sha256 '3164e0dbdaa761e9a6bc1dff3532a3b3f631b6f2137c09847b2a7514e4bdc4ef'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/mclaren/McLaren-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/McLaren'
+
   font 'McLaren-Regular.ttf'
 end
