eb7d70a0e1dbe69a771d28422d44202801f1baeb
Roland Walker <walker@pobox.com>
2014-08-06 10:11:21 -0400

    standard Cask layout for font-hanuman.rb

--- Casks/font-hanuman.rb
+++ Casks/font-hanuman.rb
@@ -1,11 +1,13 @@
 class FontHanuman < Cask
+  version '2.00'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/hanuman',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Hanuman'
-  version '2.00'
-  sha256 :no_check
+
   font 'Hanuman.ttf'
   font 'Hanumanb.ttf'
 end
