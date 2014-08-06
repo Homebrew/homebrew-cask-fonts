80a159bb64d0b98a54317ed6a6e4dccf9da04bcb
Roland Walker <walker@pobox.com>
2014-08-06 10:11:29 -0400

    standard Cask layout for font-orbitron.rb

--- Casks/font-orbitron.rb
+++ Casks/font-orbitron.rb
@@ -1,11 +1,13 @@
 class FontOrbitron < Cask
+  version '001.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/orbitron',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Orbitron'
-  version '001.001'
-  sha256 :no_check
+
   font 'Orbitron-Black.ttf'
   font 'Orbitron-Bold.ttf'
   font 'Orbitron-Medium.ttf'
