a62a6b8f4324d99fc3be9f2b30d0ee7cb0ba4500
Roland Walker <walker@pobox.com>
2014-08-06 10:11:19 -0400

    standard Cask layout for font-electrolize.rb

--- Casks/font-electrolize.rb
+++ Casks/font-electrolize.rb
@@ -1,7 +1,9 @@
 class FontElectrolize < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/electrolize/Electrolize-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Electrolize'
   version '1.002'
   sha256 'aa81ab9015fc0190bb5e68c50290565c7a4a723ddf32b88774aaff05cdd66bca'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/electrolize/Electrolize-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Electrolize'
+
   font 'Electrolize-Regular.ttf'
 end
