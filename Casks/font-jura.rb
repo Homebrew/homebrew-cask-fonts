ca9d7243d87c9e2590fc6dec9f2dbb17cc3a98d3
Roland Walker <walker@pobox.com>
2014-08-06 10:11:23 -0400

    standard Cask layout for font-jura.rb

--- Casks/font-jura.rb
+++ Casks/font-jura.rb
@@ -1,11 +1,13 @@
 class FontJura < Cask
+  version '2.5.1'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/jura',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Jura'
-  version '2.5.1'
-  sha256 :no_check
+
   font 'Jura-DemiBold.ttf'
   font 'Jura-Light.ttf'
   font 'Jura-Medium.ttf'
