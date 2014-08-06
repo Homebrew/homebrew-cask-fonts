dfa3cfe0de67128d13d4fe0869427484dae0b254
Roland Walker <walker@pobox.com>
2014-08-06 10:11:35 -0400

    standard Cask layout for font-thabit.rb

--- Casks/font-thabit.rb
+++ Casks/font-thabit.rb
@@ -1,11 +1,13 @@
 class FontThabit < Cask
+  version '0.01'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/thabit',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://projects.arabeyes.org/project.php?proj=Khotot'
-  version '0.01'
-  sha256 :no_check
+
   font 'Thabit-Bold.ttf'
   font 'Thabit-BoldOblique.ttf'
   font 'Thabit-Oblique.ttf'
