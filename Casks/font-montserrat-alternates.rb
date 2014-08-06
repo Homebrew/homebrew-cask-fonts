646967666aa00d49ba2122d84b6cdece3453b1ed
Roland Walker <walker@pobox.com>
2014-08-06 10:11:27 -0400

    standard Cask layout for font-montserrat-alternates.rb

--- Casks/font-montserrat-alternates.rb
+++ Casks/font-montserrat-alternates.rb
@@ -1,11 +1,13 @@
 class FontMontserratAlternates < Cask
+  version '2.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/montserratalternates',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Montserrat%20Alternates'
-  version '2.001'
-  sha256 :no_check
+
   font 'MontserratAlternates-Bold.ttf'
   font 'MontserratAlternates-Regular.ttf'
 end
