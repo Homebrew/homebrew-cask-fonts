4d7f8f5ad1f02aa2e602a893a188c142abb5c52a
Roland Walker <walker@pobox.com>
2014-08-06 10:11:26 -0400

    standard Cask layout for font-merriweather-sans.rb

--- Casks/font-merriweather-sans.rb
+++ Casks/font-merriweather-sans.rb
@@ -1,11 +1,13 @@
 class FontMerriweatherSans < Cask
+  version '1.003'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/merriweathersans',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Merriweather%20Sans'
-  version '1.003'
-  sha256 :no_check
+
   font 'MerriweatherSans-Bold.ttf'
   font 'MerriweatherSans-BoldItalic.ttf'
   font 'MerriweatherSans-ExtraBold.ttf'
