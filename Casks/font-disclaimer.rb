72facec97ee5fa538f4cc16598dd1369470015a0
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-disclaimer.rb

--- Casks/font-disclaimer.rb
+++ Casks/font-disclaimer.rb
@@ -1,8 +1,10 @@
 class FontDisclaimer < Cask
-  url 'http://fontfabric.com/downfont/disclaimer.zip'
-  homepage 'http://fontfabric.com/disclaimer-free-font/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://fontfabric.com/downfont/disclaimer.zip'
+  homepage 'http://fontfabric.com/disclaimer-free-font/'
+
   font 'Disclaimer-Plain.otf'
   font 'Disclaimer-Classic.otf'
 end
