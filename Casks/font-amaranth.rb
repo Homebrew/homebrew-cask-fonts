0331e1cf47cfd5756cb1279b0ffd5abcd0831883
Roland Walker <walker@pobox.com>
2014-08-06 10:11:13 -0400

    standard Cask layout for font-amaranth.rb

--- Casks/font-amaranth.rb
+++ Casks/font-amaranth.rb
@@ -1,11 +1,13 @@
 class FontAmaranth < Cask
+  version '1.000'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/amaranth',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Amaranth'
-  version '1.000'
-  sha256 :no_check
+
   font 'Amaranth-Bold.ttf'
   font 'Amaranth-BoldItalic.ttf'
   font 'Amaranth-Italic.ttf'
