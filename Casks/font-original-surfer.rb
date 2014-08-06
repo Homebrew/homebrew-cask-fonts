a5d558ab8091097bb96085eb91fdef3c1353de52
Roland Walker <walker@pobox.com>
2014-08-06 10:11:30 -0400

    standard Cask layout for font-original-surfer.rb

--- Casks/font-original-surfer.rb
+++ Casks/font-original-surfer.rb
@@ -1,7 +1,9 @@
 class FontOriginalSurfer < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/originalsurfer/OriginalSurfer-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Original%20Surfer'
   version '1.001'
   sha256 'ee4a42820b224a1934f2ba0997958d7965ad34762d88244fbffddb0b0ae488e4'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/originalsurfer/OriginalSurfer-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Original%20Surfer'
+
   font 'OriginalSurfer-Regular.ttf'
 end
