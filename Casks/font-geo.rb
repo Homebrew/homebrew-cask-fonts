2a45da385d7993d90cd1fe3fdf891ea5a06b3659
Roland Walker <walker@pobox.com>
2014-08-06 10:11:21 -0400

    standard Cask layout for font-geo.rb

--- Casks/font-geo.rb
+++ Casks/font-geo.rb
@@ -1,11 +1,13 @@
 class FontGeo < Cask
+  version '001.2'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/geo',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Geo'
-  version '001.2'
-  sha256 :no_check
+
   font 'Geo-Oblique.ttf'
   font 'Geo-Regular.ttf'
 end
