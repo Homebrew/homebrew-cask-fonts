db916eb9d5dca3c2fc8f24ac5f6016301919b176
Roland Walker <walker@pobox.com>
2014-08-06 10:11:18 -0400

    standard Cask layout for font-delius-unicase.rb

--- Casks/font-delius-unicase.rb
+++ Casks/font-delius-unicase.rb
@@ -1,11 +1,13 @@
 class FontDeliusUnicase < Cask
+  version '1.002'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/deliusunicase',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Delius%20Unicase'
-  version '1.002'
-  sha256 :no_check
+
   font 'DeliusUnicase-Bold.ttf'
   font 'DeliusUnicase-Regular.ttf'
 end
