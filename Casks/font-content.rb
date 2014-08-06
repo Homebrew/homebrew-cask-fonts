b3405351052c68ee6b2f884015af3478db8646c2
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-content.rb

--- Casks/font-content.rb
+++ Casks/font-content.rb
@@ -1,11 +1,13 @@
 class FontContent < Cask
+  version '6.00'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/content',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Content'
-  version '6.00'
-  sha256 :no_check
+
   font 'Content-Bold.ttf'
   font 'Content-Regular.ttf'
 end
