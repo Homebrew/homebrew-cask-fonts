c8cd163b14d989ba4adf5bc20cbc3d860784c664
Roland Walker <walker@pobox.com>
2014-08-06 10:11:16 -0400

    standard Cask layout for font-camingocode.rb

--- Casks/font-camingocode.rb
+++ Casks/font-camingocode.rb
@@ -1,8 +1,10 @@
 class FontCamingocode < Cask
-  url 'http://janfromm.de/_data/downloads/CamingoCode-v1.0.zip'
-  homepage 'http://www.janfromm.de/typefaces/camingomono/camingocode/'
   version '1.000'
   sha256 '13b188ca8d1b5fe2c3ba880771f20a6faac550405ae98d12edd46240f8896714'
+
+  url 'http://janfromm.de/_data/downloads/CamingoCode-v1.0.zip'
+  homepage 'http://www.janfromm.de/typefaces/camingomono/camingocode/'
+
   font 'CamingoCode-Bold.ttf'
   font 'CamingoCode-BoldItalic.ttf'
   font 'CamingoCode-Regular.ttf'
