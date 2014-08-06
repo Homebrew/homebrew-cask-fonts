89e6de778ddb96e80bd5b03a94af0c05b973f981
Roland Walker <walker@pobox.com>
2014-08-06 10:11:17 -0400

    standard Cask layout for font-coming-soon.rb

--- Casks/font-coming-soon.rb
+++ Casks/font-coming-soon.rb
@@ -1,7 +1,9 @@
 class FontComingSoon < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/comingsoon/ComingSoon.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Coming%20Soon'
   version '1.000'
   sha256 '1c7a44d75a7e130a89ca46bf6ece6d34b0085cd82291d745fae5a9844bd9a615'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/comingsoon/ComingSoon.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Coming%20Soon'
+
   font 'ComingSoon.ttf'
 end
