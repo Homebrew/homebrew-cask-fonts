1094731339ae500ab5c537462a419875d31dd092
Roland Walker <walker@pobox.com>
2014-08-06 10:11:27 -0400

    standard Cask layout for font-nanum-brush-script.rb

--- Casks/font-nanum-brush-script.rb
+++ Casks/font-nanum-brush-script.rb
@@ -1,7 +1,9 @@
 class FontNanumBrushScript < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/nanumbrushscript/NanumBrushScript-Regular.ttf'
-  homepage 'https://www.google.com/fonts/earlyaccess'
   version '1.100'
   sha256 '5fe0e02f74c58aa25ba49908fb6775bcb7d00d19ddd68b480b51a926f5ca12ec'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/nanumbrushscript/NanumBrushScript-Regular.ttf'
+  homepage 'https://www.google.com/fonts/earlyaccess'
+
   font 'NanumBrushScript-Regular.ttf'
 end
