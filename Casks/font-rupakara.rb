817f9ffe5e6dc60777bb132c6866d48c987f0068
Roland Walker <walker@pobox.com>
2014-08-06 10:11:33 -0400

    standard Cask layout for font-rupakara.rb

--- Casks/font-rupakara.rb
+++ Casks/font-rupakara.rb
@@ -1,8 +1,10 @@
 class FontRupakara < Cask
-  url 'http://www.evertype.com/fonts/rupakara/rupakara.zip'
-  homepage 'http://www.evertype.com/fonts/rupakara/'
   version '1.004'
   sha256 'd6ad49842444846ad0ef2ea40d471533993a3137a7aceacbb28b5a8d89aaddc0'
+
+  url 'http://www.evertype.com/fonts/rupakara/rupakara.zip'
+  homepage 'http://www.evertype.com/fonts/rupakara/'
+
   font 'rupakara-1.0.4/Rupakara-Bold.ttf'
   font 'rupakara-1.0.4/Rupakara-BoldOblique.ttf'
   font 'rupakara-1.0.4/Rupakara-Oblique.ttf'
