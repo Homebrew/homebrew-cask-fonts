e127f9fda1d021867b33e7189cd153c4013ad26b
Roland Walker <walker@pobox.com>
2014-08-06 10:11:24 -0400

    standard Cask layout for font-life-savers.rb

--- Casks/font-life-savers.rb
+++ Casks/font-life-savers.rb
@@ -1,11 +1,13 @@
 class FontLifeSavers < Cask
+  version '2.001'
+  sha256 :no_check
+
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/lifesavers',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Life%20Savers'
-  version '2.001'
-  sha256 :no_check
+
   font 'LifeSavers-Bold.ttf'
   font 'LifeSavers-Regular.ttf'
 end
