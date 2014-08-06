939cf15ddf7e86f23ec6101c1e59df9a13b2f2b5
Roland Walker <walker@pobox.com>
2014-08-06 10:11:27 -0400

    standard Cask layout for font-mystery-quest.rb

--- Casks/font-mystery-quest.rb
+++ Casks/font-mystery-quest.rb
@@ -1,7 +1,9 @@
 class FontMysteryQuest < Cask
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/mysteryquest/MysteryQuest-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Mystery%20Quest'
   version '1.000'
   sha256 'fc3fba61be98d7de03964b41175ee5629bcc6958b251f8037ef13eabb5431eb2'
+
+  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/mysteryquest/MysteryQuest-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Mystery%20Quest'
+
   font 'MysteryQuest-Regular.ttf'
 end
