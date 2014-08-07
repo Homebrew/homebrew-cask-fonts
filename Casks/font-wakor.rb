059ffbd11c0acc15eae1459d4313fbceb96e9d13
Roland Walker <walker@pobox.com>
2014-08-07 14:28:39 -0400

    add font Wakor

---
+++ Casks/font-wakor.rb
@@ -0,0 +1,10 @@
+class FontWakor < Cask
+  version '4.007'
+  sha256 '65bee72a3b3433492de20360532b35f17421b601d1cde52b116e666cd6924504'
+
+  url 'http://www.evertype.com/fonts/vai/wakorfont.zip'
+  homepage 'http://www.evertype.com/fonts/vai/'
+
+  font 'Wakor-4.0.7/Wakor.ttf'
+  font 'Wakor-4.0.7/WakorOblique.ttf'
+end
