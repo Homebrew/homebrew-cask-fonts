2b7a5f0227644c0b77d5abd3b08a133980b16d41
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-08-07 08:49:14 +0800

    Fix font-fontawesome sha256

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,6 +1,6 @@
 class FontFontawesome < Cask
   version '4.1.0'
-  sha256 '76b0117979df00caa71c4a1ae3fb41326590ea303747d1f48d611735e749657d'
+  sha256 '9914694f33866ea8703ba9e8df6bc3a81fae4b084dfd097d2e898f2f93b1a3ef'
 
   url 'http://fortawesome.github.io/Font-Awesome/assets/font-awesome-4.1.0.zip'
   homepage 'http://fortawesome.github.io/Font-Awesome/'
