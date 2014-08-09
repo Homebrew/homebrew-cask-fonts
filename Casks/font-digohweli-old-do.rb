79cd12524a4a255e7d784676e2615d633cac797d
Roland Walker <walker@pobox.com>
2014-08-09 11:21:28 -0400

    add font Digohweli

---
+++ Casks/font-digohweli-old-do.rb
@@ -0,0 +1,11 @@
+class FontDigohweliOldDo < Cask
+  version 'latest'
+  sha256 :no_check
+  # version '1.615'
+  # sha256 '3946991311f4fedac9f3cd44fe27c8beaa5c24c5ec8b2a3644ab3b7d1ff7ba6e'
+
+  url 'http://www.languagegeek.com/font/OldDODigohweli.zip'
+  homepage 'http://www.languagegeek.com/font/fontdownload.html'
+
+  font 'OldDODigohweli.ttf'
+end
