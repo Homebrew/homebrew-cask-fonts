235a5c08fce8b66e4fb8cc3d0d2f2226acca2fcf
Roland Walker <walker@pobox.com>
2014-08-09 11:21:28 -0400

    add font Digohweli Old DO

---
+++ Casks/font-masinahikan.rb
@@ -0,0 +1,13 @@
+class FontMasinahikan < Cask
+  version 'latest'
+  sha256 :no_check
+  # version '1.000'
+  # sha256 '15b64154923017c85c0da6563479b1bd5bc6fe78d729ba085168ca3f208ccd4c'
+
+  url 'http://www.languagegeek.com/font/Masinahikan.zip'
+  homepage 'http://www.languagegeek.com/font/fontdownload.html'
+
+  font 'Masinahikan_h.ttf'
+  font 'Masinahikan_h_Bold.ttf'
+  font 'Masinahikan_h_Semi.ttf'
+end
