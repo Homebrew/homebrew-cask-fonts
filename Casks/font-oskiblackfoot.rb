732661e35d0a97ddda11bf175759a42012c6a4e8
Roland Walker <walker@pobox.com>
2014-08-09 11:21:28 -0400

    add font OskiBlackfoot

---
+++ Casks/font-oskiblackfoot.rb
@@ -0,0 +1,14 @@
+class FontOskiblackfoot < Cask
+  version 'latest'
+  sha256 :no_check
+  # version '1.000'
+  # sha256 'b1d86bfaf02d1d61b1b32b204ee3ae57d9f7d2eccd7206596d42aec96e851089'
+
+  url 'http://www.languagegeek.com/font/oskiblackfoot.zip'
+  homepage 'http://www.languagegeek.com/font/fontdownload.html'
+
+  font 'oskiblackfoot5.ttf'
+  font 'oskiblackfootbold5.ttf'
+  font 'oskiblackfootbolditalic5.ttf'
+  font 'oskiblackfootitalic5.ttf'
+end
