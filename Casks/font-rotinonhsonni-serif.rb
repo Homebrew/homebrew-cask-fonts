24eea6889d6d875ac486143e3d23e89ab8ac7937
Roland Walker <walker@pobox.com>
2014-08-09 11:21:28 -0400

    add font Rotinonhsonni Serif

---
+++ Casks/font-rotinonhsonni-serif.rb
@@ -0,0 +1,12 @@
+class FontRotinonhsonniSerif < Cask
+  version '4.002'
+  sha256 'fd7f76b1043ec817b3fd109130eafd1db85a3249ca7d98014a16bd8c207488d2'
+
+  url 'http://www.languagegeek.com/font/RotinonhSerif.zip'
+  homepage 'http://www.languagegeek.com/font/fontdownload.html'
+
+  font 'rotinonhSerif_4_2.ttf'
+  font 'rotinonhSerifb_4_2.ttf'
+  font 'rotinonhSerifbi_4_2.ttf'
+  font 'rotinonhSerifi_4_2.ttf'
+end
