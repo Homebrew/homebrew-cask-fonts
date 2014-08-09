c1d51f94492b3e4c4d8504796ceb349f8d6fca71
Roland Walker <walker@pobox.com>
2014-08-09 11:21:28 -0400

    add font Masinahikan

---
+++ Casks/font-kisiska.rb
@@ -0,0 +1,9 @@
+class FontKisiska < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://www.languagegeek.com/font/kisiska.zip'
+  homepage 'http://www.languagegeek.com/font/fontdownload.html'
+
+  font 'kisiska.otf'
+end
