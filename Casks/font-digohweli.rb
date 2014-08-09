2ff3a05d80d5095b59377496e0e0425c97fe953d
Roland Walker <walker@pobox.com>
2014-08-09 11:21:28 -0400

    add font Kayases

---
+++ Casks/font-digohweli.rb
@@ -0,0 +1,9 @@
+class FontDigohweli < Cask
+  version '1.700'
+  sha256 'a17b6af34e0d96daf933b42f07fc382ae49f8abf66ccd2f397088c2d6672b77f'
+
+  url 'http://www.languagegeek.com/font/Digohweli.zip'
+  homepage 'http://www.languagegeek.com/font/fontdownload.html'
+
+  font 'Digohweli_1_7.ttf'
+end
