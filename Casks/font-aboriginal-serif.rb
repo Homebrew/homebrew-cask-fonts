10dee0ca9c1a15f640b1ec39b955f0a386e4c9d7
Roland Walker <walker@pobox.com>
2014-08-09 11:21:28 -0400

    add font Aboriginal Serif

---
+++ Casks/font-aboriginal-serif.rb
@@ -0,0 +1,14 @@
+class FontAboriginalSerif < Cask
+  version 'latest'
+  sha256 :no_check
+  # version '9.606'
+  # sha256 '04e8548af9a7633f26439be61ba7cf04577361537ca4eaa7471279325680c7dd'
+
+  url 'http://www.languagegeek.com/font/AboriginalSerif.zip'
+  homepage 'http://www.languagegeek.com/font/fontdownload.html'
+
+  font 'AboriginalSerifREGULAR.ttf'
+  font 'AboriginalSerifBOLD.ttf'
+  font 'AboriginalSerifBOLDITALIC.ttf'
+  font 'AboriginalSerifITALIC.ttf'
+end
