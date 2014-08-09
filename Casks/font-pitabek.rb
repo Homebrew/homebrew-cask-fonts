9c8174ddece8aac9875e0052037d36277b0ae270
Roland Walker <walker@pobox.com>
2014-08-09 11:21:28 -0400

    add font Pitabek

---
+++ Casks/font-pitabek.rb
@@ -0,0 +1,11 @@
+class FontPitabek < Cask
+  version 'latest'
+  sha256 :no_check
+  # version '1.000'
+  # sha256 '0ee433091364fc10c9bbcc77fd4007e2d8aa4901e926741256be6b37b848cd6a'
+
+  url 'http://www.languagegeek.com/font/pitabek.zip'
+  homepage 'http://www.languagegeek.com/font/fontdownload.html'
+
+  font 'pitabek.ttf'
+end
