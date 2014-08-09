59d09a69d2d7ad335ee4ef3c62a8dce155fe5501
Roland Walker <walker@pobox.com>
2014-08-09 11:21:28 -0400

    add font OskiDeneC

---
+++ Casks/font-oskidenec.rb
@@ -0,0 +1,14 @@
+class FontOskidenec < Cask
+  version 'latest'
+  sha256 :no_check
+  # version '1.000'
+  # sha256 '6bae2b488e9f29b50c950aa49298fe6ddf50a35a5d40a740e59f49e0096dec26'
+
+  url 'http://www.languagegeek.com/font/OskiDeneC.zip'
+  homepage 'http://www.languagegeek.com/font/fontdownload.html'
+
+  font 'oskidene_c.ttf'
+  font 'oskidenebold_c.ttf'
+  font 'oskidenebolditalic_c.ttf'
+  font 'oskideneitalic_c.ttf'
+end
