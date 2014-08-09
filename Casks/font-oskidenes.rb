c41b99ab75b2b6181b2dd8e7c852f9cf2f8eb3da
Roland Walker <walker@pobox.com>
2014-08-09 11:21:28 -0400

    add font OskiDeneS

---
+++ Casks/font-oskidenes.rb
@@ -0,0 +1,14 @@
+class FontOskidenes < Cask
+  version 'latest'
+  sha256 :no_check
+  # version '1.000'
+  # sha256 '4f951d09daf71c412e1335da0a501a7bab785bcae83f77637ca5edf91c3584b0'
+
+  url 'http://www.languagegeek.com/font/OskiDeneS.zip'
+  homepage 'http://www.languagegeek.com/font/fontdownload.html'
+
+  font 'OskiDeneS.ttf'
+  font 'OskiDeneS_b.ttf'
+  font 'OskiDeneS_bi.ttf'
+  font 'OskiDeneS_i.ttf'
+end
