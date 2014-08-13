ed80689f91ff1f6d00ecc26c906ffa59aa21d067
Roland Walker <walker@pobox.com>
2014-08-13 10:36:20 -0400

    add font-new-athena-unicode.rb

---
+++ Casks/font-new-athena-unicode.rb
@@ -0,0 +1,12 @@
+class FontNewAthenaUnicode < Cask
+  version '4.050'
+  sha256 'ef7cdccec31ea61391333762dd5542994189585f0f5384dcb1194701a2a9a3cf'
+
+  url 'http://apagreekkeys.org/fonts/NAU405styles.zip'
+  homepage 'http://apagreekkeys.org/NAUdownload.html'
+
+  font 'NAU405styles/newathu405.ttf'
+  font 'NAU405styles/newathuBold405.ttf'
+  font 'NAU405styles/newathuBoldItalic405.ttf'
+  font 'NAU405styles/newathuItalic405.ttf'
+end
