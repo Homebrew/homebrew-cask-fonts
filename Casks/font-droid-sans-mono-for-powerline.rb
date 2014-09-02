f6960f598398fbfc8545e410ab1068a17b6d2b7c
Jon Chen <bsd@voltaire.sh>
2014-09-01 21:50:55 -0400

    add droid sans mono for powerline

---
+++ Casks/font-droid-sans-mono-for-powerline.rb
@@ -0,0 +1,11 @@
+class FontDroidSansMonoForPowerline < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'https://github.com/Lokaltog/powerline-fonts/trunk/DroidSansMono',
+      :using => :svn,
+      :trust_cert => true
+  homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/DroidSansMono'
+
+  font 'Droid Sans Mono for Powerline.ttf'
+end
