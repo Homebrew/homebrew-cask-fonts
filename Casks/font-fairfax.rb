263974cecc16a0ed09ea826fc9ec9d4f77cc71ba
Matthew Avant <mavant@palantir.com>
2014-09-05 18:32:15 -0700

    Add Constructium and Fairfax fonts from KreativeKorp.

---
+++ Casks/font-fairfax.rb
@@ -0,0 +1,12 @@
+class FontFairfax < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://www.kreativekorp.com/swdownload/fonts/xlang/fairfax.zip'
+  homepage 'http://www.kreativekorp.com/software/fonts/constructium.shtml'
+
+  font 'Fairfax.ttf'
+  font 'FairfaxBold.ttf'
+  font 'FairfaxItalic.ttf'
+  font 'FairfaxSerif.ttf'
+end
