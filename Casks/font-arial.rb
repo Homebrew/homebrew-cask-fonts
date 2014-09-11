4ba54c6d0cdf38185b1cf5de23eaae619095f138
Roland Walker <walker@pobox.com>
2014-09-11 11:05:16 -0400

    depends_on :formula in font-arial.rb

--- Casks/font-arial.rb
+++ Casks/font-arial.rb
@@ -4,7 +4,7 @@ class FontArial < Cask
 
   url 'http://downloads.sourceforge.net/sourceforge/corefonts/arial32.exe'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
-  depends_on_formula 'cabextract'
+  depends_on :formula => 'cabextract'
 
   font 'Ariali.TTF'
   font 'Arialbd.TTF'
