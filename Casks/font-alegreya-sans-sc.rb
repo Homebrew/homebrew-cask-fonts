be686cfc27a2d8c8673a479d894fd85da4bfac76
Roland Walker <walker@pobox.com>
2014-09-17 07:50:41 -0400

    :latest as sym font-alegreya-sans-sc

--- Casks/font-alegreya-sans-sc.rb
+++ Casks/font-alegreya-sans-sc.rb
@@ -1,6 +1,6 @@
 class FontAlegreyaSansSc < Cask
   # version '1.000'
-  version 'latest'
+  version :latest
   sha256 :no_check
 
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/alegreyasanssc',
