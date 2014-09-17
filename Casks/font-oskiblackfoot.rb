68636d1ef89c348cc85aaf26ac84e7c7915e453b
Roland Walker <walker@pobox.com>
2014-09-17 07:50:50 -0400

    :latest as sym font-oskiblackfoot

--- Casks/font-oskiblackfoot.rb
+++ Casks/font-oskiblackfoot.rb
@@ -1,5 +1,5 @@
 class FontOskiblackfoot < Cask
-  version 'latest'
+  version :latest
   sha256 :no_check
   # version '1.000'
   # sha256 'b1d86bfaf02d1d61b1b32b204ee3ae57d9f7d2eccd7206596d42aec96e851089'
