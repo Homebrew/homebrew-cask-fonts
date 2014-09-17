5df75fb739420d361b7293cecb8a44ea1816b14f
Roland Walker <walker@pobox.com>
2014-09-17 07:50:50 -0400

    :latest as sym font-open-sans-hebrew

--- Casks/font-open-sans-hebrew.rb
+++ Casks/font-open-sans-hebrew.rb
@@ -1,6 +1,6 @@
 class FontOpenSansHebrew < Cask
   # version '2.001'
-  version 'latest'
+  version :latest
   sha256 :no_check
 
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/opensanshebrew',
