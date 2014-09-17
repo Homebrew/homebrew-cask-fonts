195d2079fa64d16722a725f75074d48f55e3e385
Roland Walker <walker@pobox.com>
2014-09-17 07:50:46 -0400

    :latest as sym font-montserrat-alternates

--- Casks/font-montserrat-alternates.rb
+++ Casks/font-montserrat-alternates.rb
@@ -1,6 +1,6 @@
 class FontMontserratAlternates < Cask
   # version '2.001'
-  version 'latest'
+  version :latest
   sha256 :no_check
 
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/montserratalternates',
