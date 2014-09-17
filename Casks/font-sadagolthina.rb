dc48a2aea5e81ec920e26f1d85d7e05cda97feca
Roland Walker <walker@pobox.com>
2014-09-17 07:50:52 -0400

    :latest as sym font-sadagolthina

--- Casks/font-sadagolthina.rb
+++ Casks/font-sadagolthina.rb
@@ -1,5 +1,5 @@
 class FontSadagolthina < Cask
-  version 'latest'
+  version :latest
   sha256 :no_check
   # version '1.002'
   # sha256 'aa8b42ccf0cc74a72bb50ab95a45904e6e032e000e09d0c07d93fbee3870c112'
