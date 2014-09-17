8d704b1757a9e907440861bd2a10e1eea60bd24b
Roland Walker <walker@pobox.com>
2014-09-17 07:50:51 -0400

    :latest as sym font-philosopher

--- Casks/font-philosopher.rb
+++ Casks/font-philosopher.rb
@@ -1,6 +1,6 @@
 class FontPhilosopher < Cask
   # version '1.000'
-  version 'latest'
+  version :latest
   sha256 :no_check
 
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/philosopher',
