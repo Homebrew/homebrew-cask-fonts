7bafbceb19e621c104abbec367c8c6b9e69cef1a
Roland Walker <walker@pobox.com>
2014-09-17 07:50:52 -0400

    :latest as sym font-tangerine

--- Casks/font-tangerine.rb
+++ Casks/font-tangerine.rb
@@ -1,6 +1,6 @@
 class FontTangerine < Cask
   # version '1.3'
-  version 'latest'
+  version :latest
   sha256 :no_check
 
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/tangerine',
