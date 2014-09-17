25a8912a42b86258f4864ffeb0866dd55e7fb7db
Roland Walker <walker@pobox.com>
2014-09-17 07:50:44 -0400

    :latest as sym font-gentium-basic

--- Casks/font-gentium-basic.rb
+++ Casks/font-gentium-basic.rb
@@ -1,6 +1,6 @@
 class FontGentiumBasic < Cask
   # version '1.100'
-  version 'latest'
+  version :latest
   sha256 :no_check
 
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/gentiumbasic',
