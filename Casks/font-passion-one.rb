521faf0fbfc3a8f5968bfdccdac894ac144421b3
Roland Walker <walker@pobox.com>
2014-09-17 07:50:51 -0400

    :latest as sym font-passion-one

--- Casks/font-passion-one.rb
+++ Casks/font-passion-one.rb
@@ -1,6 +1,6 @@
 class FontPassionOne < Cask
   # version '1.001'
-  version 'latest'
+  version :latest
   sha256 :no_check
 
   url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/passionone',
