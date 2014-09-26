7fc0614fee057405cc8e2da3e69985e883d6d21f
Roland Walker <walker@pobox.com>
2014-09-26 08:25:28 -0400

    re-use version in font-lohit-marathi

--- Casks/font-lohit-marathi.rb
+++ Casks/font-lohit-marathi.rb
@@ -2,8 +2,8 @@ class FontLohitMarathi < Cask
   version '2.93.0'
   sha256 '4b45a36995f1df69a5d80ab8acca31e0e36ae8ed5a33f73690d5be959a6aa543'
 
-  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-marathi-ttf-2.93.0.tar.gz'
+  url "https://fedorahosted.org/releases/l/o/lohit/lohit-marathi-ttf-#{version}.tar.gz"
   homepage 'https://fedorahosted.org/lohit/'
 
-  font 'lohit-marathi-ttf-2.93.0/Lohit-Marathi.ttf'
+  font "lohit-marathi-ttf-#{version}/Lohit-Marathi.ttf"
 end
