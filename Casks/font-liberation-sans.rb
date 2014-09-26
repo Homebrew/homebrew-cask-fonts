3b584b59dd9d60253d2087ef56ee99112c34ca29
Roland Walker <walker@pobox.com>
2014-09-26 08:25:28 -0400

    re-use version in font-liberation-sans

--- Casks/font-liberation-sans.rb
+++ Casks/font-liberation-sans.rb
@@ -2,19 +2,19 @@ class FontLiberationSans < Cask
   version '2.00.1'
   sha256 '7890278a6cd17873c57d9cd785c2d230d9abdea837e96516019c5885dd271504'
 
-  url 'https://fedorahosted.org/releases/l/i/liberation-fonts/liberation-fonts-ttf-2.00.1.tar.gz'
+  url "https://fedorahosted.org/releases/l/i/liberation-fonts/liberation-fonts-ttf-#{version}.tar.gz"
   homepage 'https://fedorahosted.org/liberation-fonts/'
 
-  font 'liberation-fonts-ttf-2.00.1/LiberationMono-Bold.ttf'
-  font 'liberation-fonts-ttf-2.00.1/LiberationMono-BoldItalic.ttf'
-  font 'liberation-fonts-ttf-2.00.1/LiberationMono-Italic.ttf'
-  font 'liberation-fonts-ttf-2.00.1/LiberationMono-Regular.ttf'
-  font 'liberation-fonts-ttf-2.00.1/LiberationSans-Bold.ttf'
-  font 'liberation-fonts-ttf-2.00.1/LiberationSans-BoldItalic.ttf'
-  font 'liberation-fonts-ttf-2.00.1/LiberationSans-Italic.ttf'
-  font 'liberation-fonts-ttf-2.00.1/LiberationSans-Regular.ttf'
-  font 'liberation-fonts-ttf-2.00.1/LiberationSerif-Bold.ttf'
-  font 'liberation-fonts-ttf-2.00.1/LiberationSerif-BoldItalic.ttf'
-  font 'liberation-fonts-ttf-2.00.1/LiberationSerif-Italic.ttf'
-  font 'liberation-fonts-ttf-2.00.1/LiberationSerif-Regular.ttf'
+  font "liberation-fonts-ttf-#{version}/LiberationMono-Bold.ttf"
+  font "liberation-fonts-ttf-#{version}/LiberationMono-BoldItalic.ttf"
+  font "liberation-fonts-ttf-#{version}/LiberationMono-Italic.ttf"
+  font "liberation-fonts-ttf-#{version}/LiberationMono-Regular.ttf"
+  font "liberation-fonts-ttf-#{version}/LiberationSans-Bold.ttf"
+  font "liberation-fonts-ttf-#{version}/LiberationSans-BoldItalic.ttf"
+  font "liberation-fonts-ttf-#{version}/LiberationSans-Italic.ttf"
+  font "liberation-fonts-ttf-#{version}/LiberationSans-Regular.ttf"
+  font "liberation-fonts-ttf-#{version}/LiberationSerif-Bold.ttf"
+  font "liberation-fonts-ttf-#{version}/LiberationSerif-BoldItalic.ttf"
+  font "liberation-fonts-ttf-#{version}/LiberationSerif-Italic.ttf"
+  font "liberation-fonts-ttf-#{version}/LiberationSerif-Regular.ttf"
 end
