cdeed266a4237bb1a50ce9c33c384d64a88ed76e
Roland Walker <walker@pobox.com>
2014-09-26 08:25:28 -0400

    re-use version in font-lohit-punjabi

--- Casks/font-lohit-punjabi.rb
+++ Casks/font-lohit-punjabi.rb
@@ -2,8 +2,8 @@ class FontLohitPunjabi < Cask
   version '2.5.3'
   sha256 'd14143d1cbb9d6a72385251875b5b903f95dfbfed9f69e93568f6ab6dfd557bd'
 
-  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-punjabi-ttf-2.5.3.tar.gz'
+  url "https://fedorahosted.org/releases/l/o/lohit/lohit-punjabi-ttf-#{version}.tar.gz"
   homepage 'https://fedorahosted.org/lohit/'
 
-  font 'lohit-punjabi-ttf-2.5.3/Lohit-Punjabi.ttf'
+  font "lohit-punjabi-ttf-#{version}/Lohit-Punjabi.ttf"
 end
