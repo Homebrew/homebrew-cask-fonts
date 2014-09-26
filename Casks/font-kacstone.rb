291e5332ff7fd357ee8069a3921cd0075ed0dd8d
Roland Walker <walker@pobox.com>
2014-09-26 08:25:28 -0400

    re-use version in font-kacstone

--- Casks/font-kacstone.rb
+++ Casks/font-kacstone.rb
@@ -2,7 +2,7 @@ class FontKacstone < Cask
   version '5.0'
   sha256 '1b016f49f99de16a65dcd990f229e729e6c4c6df02b23409771f6e27b69186a7'
 
-  url 'http://downloads.sourceforge.net/project/arabeyes/kacst_fonts/kacst_one_5.0.tar.bz2'
+  url "http://downloads.sourceforge.net/project/arabeyes/kacst_fonts/kacst_one_#{version}.tar.bz2"
   homepage 'http://projects.arabeyes.org/project.php?proj=Khotot'
 
   font 'KacstOne.ttf'
