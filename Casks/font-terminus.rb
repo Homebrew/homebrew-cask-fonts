35f61af075063846b7256ddbc890aa51e475f662
Roland Walker <walker@pobox.com>
2014-09-26 10:33:44 -0400

    whitespace nits in font-terminus

--- Casks/font-terminus.rb
+++ Casks/font-terminus.rb
@@ -1,6 +1,6 @@
 class FontTerminus < Cask
   version '4.39'
-  sha256 '5d8a7075757fd1a66012fafc6bd761e5b758573ea66bc1197025f1d4eb343383' 
+  sha256 '5d8a7075757fd1a66012fafc6bd761e5b758573ea66bc1197025f1d4eb343383'
 
   url "http://files.ax86.net/terminus-ttf/files/#{version}/terminus-ttf-#{version}.zip"
   homepage 'http://files.ax86.net/terminus-ttf/'
