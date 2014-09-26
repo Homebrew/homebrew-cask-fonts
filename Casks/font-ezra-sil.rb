1e244d1e93c1d87ae6bb8e9edaaae6971f12d7d9
Roland Walker <walker@pobox.com>
2014-09-26 08:25:28 -0400

    re-use version in font-ezra-sil

--- Casks/font-ezra-sil.rb
+++ Casks/font-ezra-sil.rb
@@ -2,9 +2,9 @@ class FontEzraSil < Cask
   version '2.51'
   sha256 'fd2894531aab32ff7d6845e3d0b4c0401c91f64ba637d13624dfc17a3e815831'
 
-  url 'https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=EzraSIL251.zip&filename=EzraSIL251.zip'
+  url "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=EzraSIL#{version.gsub('.', '')}.zip&filename=EzraSIL#{version.gsub('.', '')}.zip"
   homepage 'https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=EzraSIL_Home'
 
-  font 'EzraSIL2.51/SILEOT.ttf'
-  font 'EzraSIL2.51/SILEOTSR.ttf'
+  font "EzraSIL#{version}/SILEOT.ttf"
+  font "EzraSIL#{version}/SILEOTSR.ttf"
 end
