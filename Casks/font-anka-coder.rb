5ea92e1b65cb729175a015448084638c32cfe49c
Roland Walker <walker@pobox.com>
2014-09-26 08:25:28 -0400

    re-use version in font-anka-coder

--- Casks/font-anka-coder.rb
+++ Casks/font-anka-coder.rb
@@ -2,7 +2,7 @@ class FontAnkaCoder < Cask
   version '1.100'
   sha256 '3c3cd48ed57796cd490a2c22614c73e32ad202790d10e9af3d632cd985c0e172'
 
-  url 'https://anka-coder-fonts.googlecode.com/files/AnkaCoder.1.100.zip'
+  url "https://anka-coder-fonts.googlecode.com/files/AnkaCoder.#{version}.zip"
   homepage 'https://code.google.com/p/anka-coder-fonts/'
 
   font 'AnkaCoder-b.ttf'
