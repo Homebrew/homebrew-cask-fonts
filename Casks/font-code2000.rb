d09710c4989fe23a75031938e88d142e2ae3aa62
Roland Walker <walker@pobox.com>
2014-09-26 08:25:28 -0400

    re-use version in font-code2000

--- Casks/font-code2000.rb
+++ Casks/font-code2000.rb
@@ -2,7 +2,7 @@ class FontCode2000 < Cask
   version '1.171'
   sha256 'af00db49179aaea66494a6b9cad3ae7f2b822bf650df3133c07f9c3209cbc8e2'
 
-  url 'http://distcache.freebsd.org/ports-distfiles/code2000-1.171/CODE2000.ZIP'
+  url "http://distcache.freebsd.org/ports-distfiles/code2000-#{version}/CODE2000.ZIP"
   homepage 'http://ftp-archive.freebsd.org/pub/FreeBSD/'
 
   font 'CODE2000.TTF'
