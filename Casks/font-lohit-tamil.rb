cbfef332646105c752bb3fe8f27bb436e28a2a3c
Roland Walker <walker@pobox.com>
2014-09-26 08:25:28 -0400

    re-use version in font-lohit-tamil

--- Casks/font-lohit-tamil.rb
+++ Casks/font-lohit-tamil.rb
@@ -2,8 +2,8 @@ class FontLohitTamil < Cask
   version '2.5.3'
   sha256 '8511a31880f5e00fa19e8c8b1029943a99d17c45b3b7c186a391d8cc8edaa915'
 
-  url 'https://fedorahosted.org/releases/l/o/lohit/lohit-tamil-ttf-2.5.3.tar.gz'
+  url "https://fedorahosted.org/releases/l/o/lohit/lohit-tamil-ttf-#{version}.tar.gz"
   homepage 'https://fedorahosted.org/lohit/'
 
-  font 'lohit-tamil-ttf-2.5.3/Lohit-Tamil.ttf'
+  font "lohit-tamil-ttf-#{version}/Lohit-Tamil.ttf"
 end
