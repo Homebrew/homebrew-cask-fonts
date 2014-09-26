49bd434de65d8706fe19fa51dc593a9e1ab043ac
Roland Walker <walker@pobox.com>
2014-09-26 08:25:28 -0400

    re-use version in font-charis-sil

--- Casks/font-charis-sil.rb
+++ Casks/font-charis-sil.rb
@@ -2,11 +2,11 @@ class FontCharisSil < Cask
   version '4.114'
   sha256 '92ea75512a0bee4d491ba23fb295fd410707f6e8dedacd1af81e222a3081dd56'
 
-  url 'https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=CharisSIL-4.114.zip&filename=CharisSIL-4.114.zip'
+  url "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=CharisSIL-#{version}.zip&filename=CharisSIL-#{version}.zip"
   homepage 'https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=CharisSILfont'
 
-  font 'CharisSIL-4.114/CharisSIL-R.ttf'
-  font 'CharisSIL-4.114/CharisSIL-B.ttf'
-  font 'CharisSIL-4.114/CharisSIL-BI.ttf'
-  font 'CharisSIL-4.114/CharisSIL-I.ttf'
+  font "CharisSIL-#{version}/CharisSIL-R.ttf"
+  font "CharisSIL-#{version}/CharisSIL-B.ttf"
+  font "CharisSIL-#{version}/CharisSIL-BI.ttf"
+  font "CharisSIL-#{version}/CharisSIL-I.ttf"
 end
