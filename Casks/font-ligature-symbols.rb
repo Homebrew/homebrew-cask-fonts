8f3d58a7fee0a9e8f7b4ca6f4b129b2de14860a0
Mario Uher <uher.mario@gmail.com>
2014-09-29 16:10:58 +0200

    Adds Ligature Symbols v2.11.

---
+++ Casks/font-ligature-symbols.rb
@@ -0,0 +1,9 @@
+class FontLigatureSymbols < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://kudakurage.com/ligature_symbols/LigatureSymbols.zip'
+  homepage 'http://kudakurage.com/ligature_symbols/'
+
+  font 'LigatureSymbols/LigatureSymbols-2.11.otf'
+end
