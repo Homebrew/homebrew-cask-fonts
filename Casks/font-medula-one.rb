ea1ce4b07e870e8322bed9b066c0f96639ecb714
Roland Walker <walker@pobox.com>
2014-10-01 09:58:20 -0400

    add license stanza, font-medula-one

--- Casks/font-medula-one.rb
+++ Casks/font-medula-one.rb
@@ -4,6 +4,7 @@ class FontMedulaOne < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/medulaone/MedulaOne-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Medula%20One'
+  license :ofl
 
   font 'MedulaOne-Regular.ttf'
 end
