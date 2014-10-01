347ec0482c039e07c8323eeae252025e699478fe
Roland Walker <walker@pobox.com>
2014-10-01 09:58:21 -0400

    add license stanza, font-miniver

--- Casks/font-miniver.rb
+++ Casks/font-miniver.rb
@@ -4,6 +4,7 @@ class FontMiniver < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/miniver/Miniver-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Miniver'
+  license :ofl
 
   font 'Miniver-Regular.ttf'
 end
