e53fd0b4314b87b161e693c63967cc5444a84939
Roland Walker <walker@pobox.com>
2014-10-01 09:58:16 -0400

    add license stanza, font-impact

--- Casks/font-impact.rb
+++ Casks/font-impact.rb
@@ -4,6 +4,7 @@ class FontImpact < Cask
 
   url 'http://downloads.sourceforge.net/sourceforge/corefonts/impact32.exe'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  license :oss
   depends_on :formula => 'cabextract'
 
   font 'Impact.TTF'
