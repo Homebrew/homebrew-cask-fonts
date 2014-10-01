5a7d8ec7e60b774be104d883ed9cc9493b177660
Roland Walker <walker@pobox.com>
2014-10-01 09:58:31 -0400

    add license stanza, font-satisfy

--- Casks/font-satisfy.rb
+++ Casks/font-satisfy.rb
@@ -4,6 +4,7 @@ class FontSatisfy < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/satisfy/Satisfy-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Satisfy'
+  license :apache
 
   font 'Satisfy-Regular.ttf'
 end
