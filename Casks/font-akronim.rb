1faec34ede3ab7bb8c7609acc80cbe5924459bed
Roland Walker <walker@pobox.com>
2014-10-01 09:58:04 -0400

    add license stanza, font-akronim

--- Casks/font-akronim.rb
+++ Casks/font-akronim.rb
@@ -4,6 +4,7 @@ class FontAkronim < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/akronim/Akronim-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Akronim'
+  license :ofl
 
   font 'Akronim-Regular.ttf'
 end
