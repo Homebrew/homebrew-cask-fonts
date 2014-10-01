b4d7cdade4657d79d3cf4b16e356a0089d861618
Roland Walker <walker@pobox.com>
2014-10-01 09:58:05 -0400

    add license stanza, font-archivo-black

--- Casks/font-archivo-black.rb
+++ Casks/font-archivo-black.rb
@@ -4,6 +4,7 @@ class FontArchivoBlack < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/archivoblack/ArchivoBlack-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Archivo%20Black'
+  license :ofl
 
   font 'ArchivoBlack-Regular.ttf'
 end
