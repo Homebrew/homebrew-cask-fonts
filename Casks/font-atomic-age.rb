b017acf0a79e0f9d17092702935120cb5aa1c32c
Roland Walker <walker@pobox.com>
2014-10-01 09:58:06 -0400

    add license stanza, font-atomic-age

--- Casks/font-atomic-age.rb
+++ Casks/font-atomic-age.rb
@@ -4,6 +4,7 @@ class FontAtomicAge < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/atomicage/AtomicAge-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Atomic%20Age'
+  license :ofl
 
   font 'AtomicAge-Regular.ttf'
 end
