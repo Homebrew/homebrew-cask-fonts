736aea40e48d6f34d600d45412c9efd3a2786595
Roland Walker <walker@pobox.com>
2014-10-01 09:58:23 -0400

    add license stanza, font-noto-sans-cherokee

--- Casks/font-noto-sans-cherokee.rb
+++ Casks/font-noto-sans-cherokee.rb
@@ -4,6 +4,7 @@ class FontNotoSansCherokee < Cask
 
   url 'https://www.google.com/get/noto/pkgs/NotoSansCherokee-unhinted.zip'
   homepage 'http://www.google.com/get/noto'
+  license :apache
 
   font 'NotoSansCherokee-Regular.ttf'
 end
