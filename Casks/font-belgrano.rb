7ab6b1762e655834109fbe9a45be8377c5e30501
Roland Walker <walker@pobox.com>
2014-10-01 09:58:06 -0400

    add license stanza, font-belgrano

--- Casks/font-belgrano.rb
+++ Casks/font-belgrano.rb
@@ -4,6 +4,7 @@ class FontBelgrano < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/belgrano/Belgrano-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Belgrano'
+  license :ofl
 
   font 'Belgrano-Regular.ttf'
 end
