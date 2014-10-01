d9ef033f0ab16109d59da54c3b6d2a0fcd9956a4
Roland Walker <walker@pobox.com>
2014-10-01 09:58:20 -0400

    add license stanza, font-margarine

--- Casks/font-margarine.rb
+++ Casks/font-margarine.rb
@@ -4,6 +4,7 @@ class FontMargarine < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/margarine/Margarine-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Margarine'
+  license :ofl
 
   font 'Margarine-Regular.ttf'
 end
