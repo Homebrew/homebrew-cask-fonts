8e97e7147648cdfdf8c9d960cd441a280a9700a9
Roland Walker <walker@pobox.com>
2014-10-01 09:58:13 -0400

    add license stanza, font-galdeano

--- Casks/font-galdeano.rb
+++ Casks/font-galdeano.rb
@@ -4,6 +4,7 @@ class FontGaldeano < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/galdeano/Galdeano-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Galdeano'
+  license :ofl
 
   font 'Galdeano-Regular.ttf'
 end
