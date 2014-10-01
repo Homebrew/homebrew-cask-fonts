2207e9dace164553312f6d51a1aa0437f13b9817
Roland Walker <walker@pobox.com>
2014-10-01 09:58:05 -0400

    add license stanza, font-artifika

--- Casks/font-artifika.rb
+++ Casks/font-artifika.rb
@@ -4,6 +4,7 @@ class FontArtifika < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/artifika/Artifika-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Artifika'
+  license :ofl
 
   font 'Artifika-Regular.ttf'
 end
