a9651497518b65650c23821f6b60598e8388d56c
Roland Walker <walker@pobox.com>
2014-10-01 09:58:34 -0400

    add license stanza, font-titan-one

--- Casks/font-titan-one.rb
+++ Casks/font-titan-one.rb
@@ -4,6 +4,7 @@ class FontTitanOne < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/titanone/TitanOne-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Titan%20One'
+  license :ofl
 
   font 'TitanOne-Regular.ttf'
 end
