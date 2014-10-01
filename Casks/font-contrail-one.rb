524f1e22436864fc3bab5af5bfa464a47ebb11c4
Roland Walker <walker@pobox.com>
2014-10-01 09:58:09 -0400

    add license stanza, font-contrail-one

--- Casks/font-contrail-one.rb
+++ Casks/font-contrail-one.rb
@@ -4,6 +4,7 @@ class FontContrailOne < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/contrailone/ContrailOne-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Contrail%20One'
+  license :ofl
 
   font 'ContrailOne-Regular.ttf'
 end
