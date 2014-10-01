bb23fcdcf6d475e28be6f6be32ac84ee3aafc199
Roland Walker <walker@pobox.com>
2014-10-01 09:58:31 -0400

    add license stanza, font-schoolbell

--- Casks/font-schoolbell.rb
+++ Casks/font-schoolbell.rb
@@ -4,6 +4,7 @@ class FontSchoolbell < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/schoolbell/Schoolbell.ttf'
   homepage 'http://www.google.com/fonts/specimen/Schoolbell'
+  license :apache
 
   font 'Schoolbell.ttf'
 end
