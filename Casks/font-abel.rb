f5838506949c493bd2e3523e8903f38d7ad94859
Roland Walker <walker@pobox.com>
2014-10-01 09:58:03 -0400

    add license stanza, font-abel

--- Casks/font-abel.rb
+++ Casks/font-abel.rb
@@ -4,6 +4,7 @@ class FontAbel < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/abel/Abel-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Abel'
+  license :ofl
 
   font 'Abel-Regular.ttf'
 end
