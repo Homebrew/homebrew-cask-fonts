3679bd23820b3a0fa275f87f92609db303361ec3
Roland Walker <walker@pobox.com>
2014-10-01 09:58:27 -0400

    add license stanza, font-ovo

--- Casks/font-ovo.rb
+++ Casks/font-ovo.rb
@@ -4,6 +4,7 @@ class FontOvo < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/ovo/Ovo-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Ovo'
+  license :ofl
 
   font 'Ovo-Regular.ttf'
 end
