937dfbc6030127f6a2ad39148e55927686e58c9e
Roland Walker <walker@pobox.com>
2014-10-01 09:58:07 -0400

    add license stanza, font-bubbler-one

--- Casks/font-bubbler-one.rb
+++ Casks/font-bubbler-one.rb
@@ -4,6 +4,7 @@ class FontBubblerOne < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/bubblerone/BubblerOne-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Bubbler%20One'
+  license :ofl
 
   font 'BubblerOne-Regular.ttf'
 end
