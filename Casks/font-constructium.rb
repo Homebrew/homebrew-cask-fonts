383a0caa70a7f4b1fed9e34a4e854a977d2239c8
Roland Walker <walker@pobox.com>
2014-10-01 09:58:09 -0400

    add license stanza, font-constructium

--- Casks/font-constructium.rb
+++ Casks/font-constructium.rb
@@ -4,6 +4,7 @@ class FontConstructium < Cask
 
   url 'http://www.kreativekorp.com/swdownload/fonts/xlang/constructium.zip'
   homepage 'http://www.kreativekorp.com/software/fonts/constructium.shtml'
+  license :unknown
 
   font 'Constructium.ttf'
 end
