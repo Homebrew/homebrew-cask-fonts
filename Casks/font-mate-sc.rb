fddaa045d1d24636f1de6c25799dea9ebf2d017e
Roland Walker <walker@pobox.com>
2014-10-01 09:58:20 -0400

    add license stanza, font-mate-sc

--- Casks/font-mate-sc.rb
+++ Casks/font-mate-sc.rb
@@ -4,6 +4,7 @@ class FontMateSc < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/matesc/MateSC-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Mate%20SC'
+  license :ofl
 
   font 'MateSC-Regular.ttf'
 end
