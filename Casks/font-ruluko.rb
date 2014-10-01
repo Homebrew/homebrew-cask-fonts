b61b93a293e8311909f469f18bd98bb9badee174
Roland Walker <walker@pobox.com>
2014-10-01 09:58:31 -0400

    add license stanza, font-ruluko

--- Casks/font-ruluko.rb
+++ Casks/font-ruluko.rb
@@ -4,6 +4,7 @@ class FontRuluko < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/ruluko/Ruluko-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Ruluko'
+  license :ofl
 
   font 'Ruluko-Regular.ttf'
 end
