385b62d5d93c3be680881a432a30157cbc9d47a2
Roland Walker <walker@pobox.com>
2014-10-01 09:58:05 -0400

    add license stanza, font-asset

--- Casks/font-asset.rb
+++ Casks/font-asset.rb
@@ -4,6 +4,7 @@ class FontAsset < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/asset/Asset.ttf'
   homepage 'http://www.google.com/fonts/specimen/Asset'
+  license :ofl
 
   font 'Asset.ttf'
 end
