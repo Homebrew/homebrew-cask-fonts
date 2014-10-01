834f67c413e08919fbce686ab1d3c135dfae9b9c
Roland Walker <walker@pobox.com>
2014-10-01 09:58:13 -0400

    add license stanza, font-fredericka-the-great

--- Casks/font-fredericka-the-great.rb
+++ Casks/font-fredericka-the-great.rb
@@ -4,6 +4,7 @@ class FontFrederickaTheGreat < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/frederickathegreat/FrederickatheGreat-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Fredericka%20the%20Great'
+  license :ofl
 
   font 'FrederickatheGreat-Regular.ttf'
 end
