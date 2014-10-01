9bca2f80071238691a0c74f5ca9e4d34fc904fa4
Roland Walker <walker@pobox.com>
2014-10-01 09:58:25 -0400

    add license stanza, font-noto-sans-phags-pa

--- Casks/font-noto-sans-phags-pa.rb
+++ Casks/font-noto-sans-phags-pa.rb
@@ -4,6 +4,7 @@ class FontNotoSansPhagsPa < Cask
 
   url 'https://www.google.com/get/noto/pkgs/NotoSansPhags-pa-unhinted.zip'
   homepage 'http://www.google.com/get/noto'
+  license :apache
 
   font 'NotoSansPhagsPa-Regular.ttf'
 end
