0b8ccd5c54043472eb3215718a8457125aa5a428
Roland Walker <walker@pobox.com>
2014-10-01 09:58:35 -0400

    add license stanza, font-vibur

--- Casks/font-vibur.rb
+++ Casks/font-vibur.rb
@@ -4,6 +4,7 @@ class FontVibur < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/vibur/Vibur-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Vibur'
+  license :ofl
 
   font 'Vibur-Regular.ttf'
 end
