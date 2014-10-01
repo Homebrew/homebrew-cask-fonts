e3f4bad2ea438cc432230db2656360e8a6905995
Roland Walker <walker@pobox.com>
2014-10-01 09:58:22 -0400

    add license stanza, font-nexa

--- Casks/font-nexa.rb
+++ Casks/font-nexa.rb
@@ -4,6 +4,7 @@ class FontNexa < Cask
 
   url 'http://fontfabric.com/downfont/nexa.zip'
   homepage 'http://fontfabric.com/nexa-font/'
+  license :unknown
 
   font 'Nexa Bold.otf'
   font 'Nexa Light.otf'
