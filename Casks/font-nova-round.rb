180d7ea602ba9536541a2e23e6fe6342e1e83669
Roland Walker <walker@pobox.com>
2014-10-01 09:58:26 -0400

    add license stanza, font-nova-round

--- Casks/font-nova-round.rb
+++ Casks/font-nova-round.rb
@@ -4,6 +4,7 @@ class FontNovaRound < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/novaround/NovaRound.ttf'
   homepage 'http://www.google.com/fonts/specimen/Nova%20Round'
+  license :ofl
 
   font 'NovaRound.ttf'
 end
