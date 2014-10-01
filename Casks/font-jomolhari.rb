904801300f7dffe65037df35b20d6bedf7f41490
Roland Walker <walker@pobox.com>
2014-10-01 09:58:16 -0400

    add license stanza, font-jomolhari

--- Casks/font-jomolhari.rb
+++ Casks/font-jomolhari.rb
@@ -4,6 +4,7 @@ class FontJomolhari < Cask
 
   url 'http://www.library.gov.bt/IT/download/Jomolhari.zip'
   homepage 'http://www.library.gov.bt/IT/fonts.html'
+  license :unknown
 
   font 'Jomolhari-alpha3c-0605331.ttf'
 end
