e698bea20cfa58e7c65104b9bd699e78cdf67577
Roland Walker <walker@pobox.com>
2014-10-01 09:58:07 -0400

    add license stanza, font-brawler

--- Casks/font-brawler.rb
+++ Casks/font-brawler.rb
@@ -4,6 +4,7 @@ class FontBrawler < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/brawler/Brawler-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Brawler'
+  license :ofl
 
   font 'Brawler-Regular.ttf'
 end
