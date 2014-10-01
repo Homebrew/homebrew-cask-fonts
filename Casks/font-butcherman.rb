0d37ca79d4238fd1fd0d93a3f63ec7bec7a95224
Roland Walker <walker@pobox.com>
2014-10-01 09:58:07 -0400

    add license stanza, font-butcherman

--- Casks/font-butcherman.rb
+++ Casks/font-butcherman.rb
@@ -4,6 +4,7 @@ class FontButcherman < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/butcherman/Butcherman-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Butcherman'
+  license :ofl
 
   font 'Butcherman-Regular.ttf'
 end
