2f662d1886bf05427ff7784e0fd9cefc27769173
Roland Walker <walker@pobox.com>
2014-10-01 09:58:12 -0400

    add license stanza, font-fanwood-text

--- Casks/font-fanwood-text.rb
+++ Casks/font-fanwood-text.rb
@@ -8,6 +8,7 @@ class FontFanwoodText < Cask
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Fanwood%20Text'
+  license :ofl
 
   font 'FanwoodText-Italic.ttf'
   font 'FanwoodText-Regular.ttf'
