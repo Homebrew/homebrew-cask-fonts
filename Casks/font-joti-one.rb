c0e9f78ffb3be3018edacc91bf1fc2e019053222
Roland Walker <walker@pobox.com>
2014-10-01 09:58:16 -0400

    add license stanza, font-joti-one

--- Casks/font-joti-one.rb
+++ Casks/font-joti-one.rb
@@ -4,6 +4,7 @@ class FontJotiOne < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/jotione/JotiOne-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Joti%20One'
+  license :ofl
 
   font 'JotiOne-Regular.ttf'
 end
