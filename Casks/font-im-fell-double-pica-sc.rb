4568f13e6c8859caa6be4f374c38587118b0987a
Roland Walker <walker@pobox.com>
2014-10-01 09:58:15 -0400

    add license stanza, font-im-fell-double-pica-sc

--- Casks/font-im-fell-double-pica-sc.rb
+++ Casks/font-im-fell-double-pica-sc.rb
@@ -4,6 +4,7 @@ class FontImFellDoublePicaSc < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/imfelldoublepicasc/IMFeDPsc28P.ttf'
   homepage 'http://www.google.com/fonts/specimen/IM%20Fell%20Double%20Pica%20SC'
+  license :ofl
 
   font 'IMFeDPsc28P.ttf'
 end
