389041ef295241d08c1b57b3891c7346aec89360
Roland Walker <walker@pobox.com>
2014-10-01 09:58:23 -0400

    add license stanza, font-noto-sans-georgian

--- Casks/font-noto-sans-georgian.rb
+++ Casks/font-noto-sans-georgian.rb
@@ -4,6 +4,7 @@ class FontNotoSansGeorgian < Cask
 
   url 'https://www.google.com/get/noto/pkgs/NotoSansGeorgian-hinted.zip'
   homepage 'http://www.google.com/get/noto'
+  license :apache
 
   font 'NotoSansGeorgian-Bold.ttf'
   font 'NotoSansGeorgian-Regular.ttf'
