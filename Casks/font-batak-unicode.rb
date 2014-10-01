4990271f2dc1babcb42bad2d8606bf65d30de8b6
Roland Walker <walker@pobox.com>
2014-10-01 09:58:06 -0400

    add license stanza, font-batak-unicode

--- Casks/font-batak-unicode.rb
+++ Casks/font-batak-unicode.rb
@@ -4,6 +4,7 @@ class FontBatakUnicode < Cask
 
   url 'http://ulikozok.com/fontfiles/Batak-Unicode-Regular.otf'
   homepage 'http://ulikozok.com/aksara-batak/batak-font/'
+  license :unknown
 
   font 'Batak-Unicode-Regular.otf'
 end
