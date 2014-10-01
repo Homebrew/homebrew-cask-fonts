00ef97ef760220338b78e6ad21b2539e9c7e1a65
Roland Walker <walker@pobox.com>
2014-10-01 09:58:34 -0400

    add license stanza, font-trocchi

--- Casks/font-trocchi.rb
+++ Casks/font-trocchi.rb
@@ -4,6 +4,7 @@ class FontTrocchi < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/trocchi/Trocchi-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Trocchi'
+  license :ofl
 
   font 'Trocchi-Regular.ttf'
 end
