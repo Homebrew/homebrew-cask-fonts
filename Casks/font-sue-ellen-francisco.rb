56a8e21f8661e685b3c84c0c68face71dc6d8203
Roland Walker <walker@pobox.com>
2014-10-01 09:58:34 -0400

    add license stanza, font-sue-ellen-francisco

--- Casks/font-sue-ellen-francisco.rb
+++ Casks/font-sue-ellen-francisco.rb
@@ -4,6 +4,7 @@ class FontSueEllenFrancisco < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/sueellenfrancisco/SueEllenFrancisco.ttf'
   homepage 'http://www.google.com/fonts/specimen/Sue%20Ellen%20Francisco'
+  license :ofl
 
   font 'SueEllenFrancisco.ttf'
 end
