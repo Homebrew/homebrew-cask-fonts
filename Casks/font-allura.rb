9358d44f70685681e911e6cefa8a5f22eb3082cb
Roland Walker <walker@pobox.com>
2014-10-01 09:58:04 -0400

    add license stanza, font-allura

--- Casks/font-allura.rb
+++ Casks/font-allura.rb
@@ -4,6 +4,7 @@ class FontAllura < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/allura/Allura-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Allura'
+  license :ofl
 
   font 'Allura-Regular.ttf'
 end
