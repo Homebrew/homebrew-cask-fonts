9c4b189bcca1525979ef58bc0ea4b6114509fd5d
Roland Walker <walker@pobox.com>
2014-10-01 09:58:07 -0400

    add license stanza, font-cagliostro

--- Casks/font-cagliostro.rb
+++ Casks/font-cagliostro.rb
@@ -4,6 +4,7 @@ class FontCagliostro < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/cagliostro/Cagliostro-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Cagliostro'
+  license :ofl
 
   font 'Cagliostro-Regular.ttf'
 end
