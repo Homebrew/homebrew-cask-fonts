c3f683983d8375a126f44157b407ea98b8a19915
Roland Walker <walker@pobox.com>
2014-10-01 09:58:26 -0400

    add license stanza, font-nova-oval

--- Casks/font-nova-oval.rb
+++ Casks/font-nova-oval.rb
@@ -4,6 +4,7 @@ class FontNovaOval < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/novaoval/NovaOval.ttf'
   homepage 'http://www.google.com/fonts/specimen/Nova%20Oval'
+  license :ofl
 
   font 'NovaOval.ttf'
 end
