008985c27b785931f6adb5c9ada5573c335a32ed
Roland Walker <walker@pobox.com>
2014-10-01 09:58:07 -0400

    add license stanza, font-candal

--- Casks/font-candal.rb
+++ Casks/font-candal.rb
@@ -4,6 +4,7 @@ class FontCandal < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/candal/Candal.ttf'
   homepage 'http://www.google.com/fonts/specimen/Candal'
+  license :ofl
 
   font 'Candal.ttf'
 end
