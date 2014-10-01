e77ad02664d8e2d412af74d4f857b9df7fd4bdeb
Roland Walker <walker@pobox.com>
2014-10-01 09:58:07 -0400

    add license stanza, font-bree-serif

--- Casks/font-bree-serif.rb
+++ Casks/font-bree-serif.rb
@@ -4,6 +4,7 @@ class FontBreeSerif < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/breeserif/BreeSerif-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Bree%20Serif'
+  license :ofl
 
   font 'BreeSerif-Regular.ttf'
 end
