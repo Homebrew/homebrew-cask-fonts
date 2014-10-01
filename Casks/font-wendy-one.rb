a8172d944a3fc8284d2ac9e543f2e7d0a88f3d2a
Roland Walker <walker@pobox.com>
2014-10-01 09:58:36 -0400

    add license stanza, font-wendy-one

--- Casks/font-wendy-one.rb
+++ Casks/font-wendy-one.rb
@@ -4,6 +4,7 @@ class FontWendyOne < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/wendyone/WendyOne-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Wendy%20One'
+  license :ofl
 
   font 'WendyOne-Regular.ttf'
 end
