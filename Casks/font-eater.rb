9337973405e7593b41a1909e48bfe598580a0729
Roland Walker <walker@pobox.com>
2014-10-01 09:58:11 -0400

    add license stanza, font-eater

--- Casks/font-eater.rb
+++ Casks/font-eater.rb
@@ -4,6 +4,7 @@ class FontEater < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/eater/Eater-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Eater'
+  license :ofl
 
   font 'Eater-Regular.ttf'
 end
