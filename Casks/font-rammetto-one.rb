21b156e6e252cda191f801afd9c7a76acb3833be
Roland Walker <walker@pobox.com>
2014-10-01 09:58:30 -0400

    add license stanza, font-rammetto-one

--- Casks/font-rammetto-one.rb
+++ Casks/font-rammetto-one.rb
@@ -4,6 +4,7 @@ class FontRammettoOne < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/rammettoone/RammettoOne-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Rammetto%20One'
+  license :ofl
 
   font 'RammettoOne-Regular.ttf'
 end
