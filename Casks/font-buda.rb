2fbafea8d275012a8588c1bce418f7f4aab1af92
Roland Walker <walker@pobox.com>
2014-10-01 09:58:07 -0400

    add license stanza, font-buda

--- Casks/font-buda.rb
+++ Casks/font-buda.rb
@@ -4,6 +4,7 @@ class FontBuda < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/buda/Buda-Light.ttf'
   homepage 'http://www.google.com/fonts/specimen/Buda'
+  license :ofl
 
   font 'Buda-Light.ttf'
 end
