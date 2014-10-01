98b962750b80f2892b159b6dc119343398f9b403
Roland Walker <walker@pobox.com>
2014-10-01 09:58:03 -0400

    add license stanza, font-aguafina-script

--- Casks/font-aguafina-script.rb
+++ Casks/font-aguafina-script.rb
@@ -4,6 +4,7 @@ class FontAguafinaScript < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/aguafinascript/AguafinaScript-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Aguafina%20Script'
+  license :ofl
 
   font 'AguafinaScript-Regular.ttf'
 end
