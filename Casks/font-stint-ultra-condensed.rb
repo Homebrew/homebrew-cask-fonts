de7456ce41ffc33da540213580542c5b2837541a
Roland Walker <walker@pobox.com>
2014-10-01 09:58:33 -0400

    add license stanza, font-stint-ultra-condensed

--- Casks/font-stint-ultra-condensed.rb
+++ Casks/font-stint-ultra-condensed.rb
@@ -4,6 +4,7 @@ class FontStintUltraCondensed < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/stintultracondensed/StintUltraCondensed-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Stint%20Ultra%20Condensed'
+  license :ofl
 
   font 'StintUltraCondensed-Regular.ttf'
 end
