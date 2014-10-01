c4d296496d63455af9da12b817cbcb2fc54f5785
Roland Walker <walker@pobox.com>
2014-10-01 09:58:05 -0400

    add license stanza, font-arial

--- Casks/font-arial.rb
+++ Casks/font-arial.rb
@@ -4,6 +4,7 @@ class FontArial < Cask
 
   url 'http://downloads.sourceforge.net/sourceforge/corefonts/arial32.exe'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  license :oss
   depends_on :formula => 'cabextract'
 
   font 'Ariali.TTF'
