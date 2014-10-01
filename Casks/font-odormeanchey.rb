55ab1f7216b33f45b843e1b6a32326e806ba5372
Roland Walker <walker@pobox.com>
2014-10-01 09:58:26 -0400

    add license stanza, font-odormeanchey

--- Casks/font-odormeanchey.rb
+++ Casks/font-odormeanchey.rb
@@ -4,6 +4,7 @@ class FontOdormeanchey < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/odormeanchey/OdorMeanChey.ttf'
   homepage 'http://www.google.com/fonts/specimen/Odor%20Mean%20Chey'
+  license :oss
 
   font 'OdorMeanChey.ttf'
 end
