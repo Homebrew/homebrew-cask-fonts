ceec40a5f79641159320716a5e50b175ee963c25
Roland Walker <walker@pobox.com>
2014-10-01 09:58:25 -0400

    add license stanza, font-noto-sans-thai

--- Casks/font-noto-sans-thai.rb
+++ Casks/font-noto-sans-thai.rb
@@ -4,6 +4,7 @@ class FontNotoSansThai < Cask
 
   url 'https://www.google.com/get/noto/pkgs/NotoSansThai-hinted.zip'
   homepage 'http://www.google.com/get/noto'
+  license :apache
 
   font 'NotoSansThai-Bold.ttf'
   font 'NotoSansThai-Regular.ttf'
