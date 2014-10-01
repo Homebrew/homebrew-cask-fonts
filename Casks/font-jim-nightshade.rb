006886799392be107eccc66e966d99c945baa991
Roland Walker <walker@pobox.com>
2014-10-01 09:58:16 -0400

    add license stanza, font-jim-nightshade

--- Casks/font-jim-nightshade.rb
+++ Casks/font-jim-nightshade.rb
@@ -4,6 +4,7 @@ class FontJimNightshade < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/jimnightshade/JimNightshade-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Jim%20Nightshade'
+  license :ofl
 
   font 'JimNightshade-Regular.ttf'
 end
