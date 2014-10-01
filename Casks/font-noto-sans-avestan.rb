2520842cdbadb6771c11dc0bcdd1d1711475e487
Roland Walker <walker@pobox.com>
2014-10-01 09:58:23 -0400

    add license stanza, font-noto-sans-avestan

--- Casks/font-noto-sans-avestan.rb
+++ Casks/font-noto-sans-avestan.rb
@@ -4,6 +4,7 @@ class FontNotoSansAvestan < Cask
 
   url 'https://www.google.com/get/noto/pkgs/NotoSansAvestan-unhinted.zip'
   homepage 'http://www.google.com/get/noto'
+  license :apache
 
   font 'NotoSansAvestan-Regular.ttf'
 end
