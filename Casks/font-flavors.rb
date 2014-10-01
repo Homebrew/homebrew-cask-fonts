3bfd2e0cba4e01ba80df70b80bf7e3d64a3e3c7a
Roland Walker <walker@pobox.com>
2014-10-01 09:58:13 -0400

    add license stanza, font-flavors

--- Casks/font-flavors.rb
+++ Casks/font-flavors.rb
@@ -4,6 +4,7 @@ class FontFlavors < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/flavors/Flavors-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Flavors'
+  license :ofl
 
   font 'Flavors-Regular.ttf'
 end
