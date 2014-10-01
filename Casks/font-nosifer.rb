13d744d5ae4f6ec274053d6cf0f78b0be98a21d9
Roland Walker <walker@pobox.com>
2014-10-01 09:58:22 -0400

    add license stanza, font-nosifer

--- Casks/font-nosifer.rb
+++ Casks/font-nosifer.rb
@@ -4,6 +4,7 @@ class FontNosifer < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/nosifer/Nosifer-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Nosifer'
+  license :ofl
 
   font 'Nosifer-Regular.ttf'
 end
