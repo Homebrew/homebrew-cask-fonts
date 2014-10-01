4cfec068957b2c8d746e03194e39736c784a86b8
Roland Walker <walker@pobox.com>
2014-10-01 09:58:26 -0400

    add license stanza, font-open-sans

--- Casks/font-open-sans.rb
+++ Casks/font-open-sans.rb
@@ -8,6 +8,7 @@ class FontOpenSans < Cask
       :revision   => '48',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Open+Sans'
+  license :apache
 
   font 'OpenSans-Bold.ttf'
   font 'OpenSans-BoldItalic.ttf'
