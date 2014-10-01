1081839d83d41fc33c8102bcc65891811a392a90
Roland Walker <walker@pobox.com>
2014-10-01 09:58:35 -0400

    add license stanza, font-varela

--- Casks/font-varela.rb
+++ Casks/font-varela.rb
@@ -4,6 +4,7 @@ class FontVarela < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/varela/Varela-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Varela'
+  license :ofl
 
   font 'Varela-Regular.ttf'
 end
