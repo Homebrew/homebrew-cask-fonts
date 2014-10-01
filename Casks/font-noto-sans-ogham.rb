07ff22daae110b0c3800c75f9d75752d268612bb
Roland Walker <walker@pobox.com>
2014-10-01 09:58:24 -0400

    add license stanza, font-noto-sans-ogham

--- Casks/font-noto-sans-ogham.rb
+++ Casks/font-noto-sans-ogham.rb
@@ -4,6 +4,7 @@ class FontNotoSansOgham < Cask
 
   url 'https://www.google.com/get/noto/pkgs/NotoSansOgham-unhinted.zip'
   homepage 'http://www.google.com/get/noto'
+  license :apache
 
   font 'NotoSansOgham-Regular.ttf'
 end
