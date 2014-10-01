b0370c4cc1be6912222ed9162d6eb045fa73009d
Roland Walker <walker@pobox.com>
2014-10-01 09:58:35 -0400

    add license stanza, font-voces

--- Casks/font-voces.rb
+++ Casks/font-voces.rb
@@ -4,6 +4,7 @@ class FontVoces < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/voces/Voces-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Voces'
+  license :ofl
 
   font 'Voces-Regular.ttf'
 end
