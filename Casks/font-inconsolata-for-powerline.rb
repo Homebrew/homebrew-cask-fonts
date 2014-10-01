b4ffd3f18a48a360fb7f679f99193377fcf443b3
Roland Walker <walker@pobox.com>
2014-10-01 09:58:16 -0400

    add license stanza, font-inconsolata-for-powerline

--- Casks/font-inconsolata-for-powerline.rb
+++ Casks/font-inconsolata-for-powerline.rb
@@ -4,6 +4,7 @@ class FontInconsolataForPowerline < Cask
 
   url 'https://github.com/Lokaltog/powerline-fonts/raw/master/Inconsolata/Inconsolata%20for%20Powerline.otf'
   homepage 'https://github.com/Lokaltog/powerline-fonts/tree/master/Inconsolata'
+  license :ofl
 
   font 'Inconsolata for Powerline.otf'
 end
