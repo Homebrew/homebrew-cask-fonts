e1aba56b8e49f38ea1d4225388b7ea055c7f4043
Roland Walker <walker@pobox.com>
2014-10-01 09:58:14 -0400

    add license stanza, font-glegoo

--- Casks/font-glegoo.rb
+++ Casks/font-glegoo.rb
@@ -4,6 +4,7 @@ class FontGlegoo < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/glegoo/Glegoo-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Glegoo'
+  license :ofl
 
   font 'Glegoo-Regular.ttf'
 end
