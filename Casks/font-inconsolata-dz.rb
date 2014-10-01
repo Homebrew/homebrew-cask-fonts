ed5eb8d9bb5d40a4bf970302cbd79eaaadba21ca
Roland Walker <walker@pobox.com>
2014-10-01 09:58:16 -0400

    add license stanza, font-inconsolata-dz

--- Casks/font-inconsolata-dz.rb
+++ Casks/font-inconsolata-dz.rb
@@ -4,6 +4,7 @@ class FontInconsolataDz < Cask
 
   url 'http://media.nodnod.net/Inconsolata-dz.otf.zip'
   homepage 'http://nodnod.net/2009/feb/12/adding-straight-single-and-double-quotes-inconsola/'
+  license :ofl
 
   font 'Inconsolata-dz.otf'
 end
