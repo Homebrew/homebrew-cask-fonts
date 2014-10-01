ffb608c3a034ace774e54ec8e31165920c76f48e
Roland Walker <walker@pobox.com>
2014-10-01 09:58:17 -0400

    add license stanza, font-kranky

--- Casks/font-kranky.rb
+++ Casks/font-kranky.rb
@@ -4,6 +4,7 @@ class FontKranky < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/kranky/Kranky.ttf'
   homepage 'http://www.google.com/fonts/specimen/Kranky'
+  license :apache
 
   font 'Kranky.ttf'
 end
