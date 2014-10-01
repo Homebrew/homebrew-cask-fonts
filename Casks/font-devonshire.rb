075bd5f2228c0bf247ea616acadc74eb3763ce32
Roland Walker <walker@pobox.com>
2014-10-01 09:58:11 -0400

    add license stanza, font-devonshire

--- Casks/font-devonshire.rb
+++ Casks/font-devonshire.rb
@@ -4,6 +4,7 @@ class FontDevonshire < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/devonshire/Devonshire-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Devonshire'
+  license :ofl
 
   font 'Devonshire-Regular.ttf'
 end
