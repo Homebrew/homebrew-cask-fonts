b6c04a4f05da8f7a95da56b972a09599070ea7eb
Roland Walker <walker@pobox.com>
2014-10-01 09:58:08 -0400

    add license stanza, font-carrois-gothic

--- Casks/font-carrois-gothic.rb
+++ Casks/font-carrois-gothic.rb
@@ -4,6 +4,7 @@ class FontCarroisGothic < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/carroisgothic/CarroisGothic-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Carrois%20Gothic'
+  license :ofl
 
   font 'CarroisGothic-Regular.ttf'
 end
