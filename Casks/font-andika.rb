9cfc217b15226dcf03ac057a16e347eff21dc4ca
Roland Walker <walker@pobox.com>
2014-10-01 09:58:05 -0400

    add license stanza, font-andika

--- Casks/font-andika.rb
+++ Casks/font-andika.rb
@@ -4,6 +4,7 @@ class FontAndika < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/andika/Andika-R.ttf'
   homepage 'http://www.google.com/fonts/specimen/Andika'
+  license :ofl
 
   font 'Andika-R.ttf'
 end
