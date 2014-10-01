bbd3c5a22c043e6f57179b2074e4525055e8d305
Roland Walker <walker@pobox.com>
2014-10-01 09:58:13 -0400

    add license stanza, font-forum

--- Casks/font-forum.rb
+++ Casks/font-forum.rb
@@ -4,6 +4,7 @@ class FontForum < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/forum/Forum-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Forum'
+  license :ofl
 
   font 'Forum-Regular.ttf'
 end
