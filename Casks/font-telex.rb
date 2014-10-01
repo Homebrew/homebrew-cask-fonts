b7e0f6cc6dd835179798decaead712e16448a8dc
Roland Walker <walker@pobox.com>
2014-10-01 09:58:34 -0400

    add license stanza, font-telex

--- Casks/font-telex.rb
+++ Casks/font-telex.rb
@@ -4,6 +4,7 @@ class FontTelex < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/telex/Telex-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Telex'
+  license :ofl
 
   font 'Telex-Regular.ttf'
 end
