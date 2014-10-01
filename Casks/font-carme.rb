16155c901cc68e7dda97deb0b89153921af98148
Roland Walker <walker@pobox.com>
2014-10-01 09:58:08 -0400

    add license stanza, font-carme

--- Casks/font-carme.rb
+++ Casks/font-carme.rb
@@ -4,6 +4,7 @@ class FontCarme < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/carme/Carme-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Carme'
+  license :ofl
 
   font 'Carme-Regular.ttf'
 end
