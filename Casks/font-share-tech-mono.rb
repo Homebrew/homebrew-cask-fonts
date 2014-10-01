59a83a1fa42748ae72d45def1a129fdb2dee0cad
Roland Walker <walker@pobox.com>
2014-10-01 09:58:32 -0400

    add license stanza, font-share-tech-mono

--- Casks/font-share-tech-mono.rb
+++ Casks/font-share-tech-mono.rb
@@ -4,6 +4,7 @@ class FontShareTechMono < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/sharetechmono/ShareTechMono-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Share%20Tech%20Mono'
+  license :ofl
 
   font 'ShareTechMono-Regular.ttf'
 end
