77aab99f5fe00bfad167113d29b3ebb003c9bb2b
Roland Walker <walker@pobox.com>
2014-10-01 09:58:17 -0400

    add license stanza, font-karla-tamil-inclined

--- Casks/font-karla-tamil-inclined.rb
+++ Casks/font-karla-tamil-inclined.rb
@@ -8,6 +8,7 @@ class FontKarlaTamilInclined < Cask
       :revision   => '50',
       :trust_cert => true
   homepage 'https://www.google.com/fonts/earlyaccess'
+  license :ofl
 
   font 'KarlaTamilInclined-Bold.ttf'
   font 'KarlaTamilInclined-Regular.ttf'
