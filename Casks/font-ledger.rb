9859d9486d4bad2df07000391a80f0e2884362d0
Roland Walker <walker@pobox.com>
2014-10-01 09:58:18 -0400

    add license stanza, font-ledger

--- Casks/font-ledger.rb
+++ Casks/font-ledger.rb
@@ -4,6 +4,7 @@ class FontLedger < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/ledger/Ledger-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Ledger'
+  license :ofl
 
   font 'Ledger-Regular.ttf'
 end
