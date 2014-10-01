c9e7adb0e7e3c5f177989e7046c5052fc0f48c53
Roland Walker <walker@pobox.com>
2014-10-01 09:58:29 -0400

    add license stanza, font-port-lligat-sans

--- Casks/font-port-lligat-sans.rb
+++ Casks/font-port-lligat-sans.rb
@@ -4,6 +4,7 @@ class FontPortLligatSans < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/portlligatsans/PortLligatSans-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Port%20Lligat%20Sans'
+  license :ofl
 
   font 'PortLligatSans-Regular.ttf'
 end
