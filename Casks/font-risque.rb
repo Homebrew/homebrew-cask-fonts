b1c61cda2bd04e7be7febb46aa0f9411e7e4be0e
Roland Walker <walker@pobox.com>
2014-10-01 09:58:30 -0400

    add license stanza, font-risque

--- Casks/font-risque.rb
+++ Casks/font-risque.rb
@@ -4,6 +4,7 @@ class FontRisque < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/risque/Risque-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Risque'
+  license :ofl
 
   font 'Risque-Regular.ttf'
 end
