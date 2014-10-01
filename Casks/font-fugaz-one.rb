12f5a8414ece40d044bd1997befa8a101970d8d3
Roland Walker <walker@pobox.com>
2014-10-01 09:58:13 -0400

    add license stanza, font-fugaz-one

--- Casks/font-fugaz-one.rb
+++ Casks/font-fugaz-one.rb
@@ -4,6 +4,7 @@ class FontFugazOne < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/fugazone/FugazOne-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Fugaz%20One'
+  license :ofl
 
   font 'FugazOne-Regular.ttf'
 end
