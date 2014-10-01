3bb0d2a6bbea5b118a78c166c695a00ea89a3c16
Roland Walker <walker@pobox.com>
2014-10-01 09:58:26 -0400

    add license stanza, font-offside

--- Casks/font-offside.rb
+++ Casks/font-offside.rb
@@ -4,6 +4,7 @@ class FontOffside < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/offside/Offside-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Offside'
+  license :ofl
 
   font 'Offside-Regular.ttf'
 end
