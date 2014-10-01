f58caecd81a8a37a697024eaea3f090298b6b492
Roland Walker <walker@pobox.com>
2014-10-01 09:58:32 -0400

    add license stanza, font-slackey

--- Casks/font-slackey.rb
+++ Casks/font-slackey.rb
@@ -4,6 +4,7 @@ class FontSlackey < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/slackey/Slackey.ttf'
   homepage 'http://www.google.com/fonts/specimen/Slackey'
+  license :apache
 
   font 'Slackey.ttf'
 end
