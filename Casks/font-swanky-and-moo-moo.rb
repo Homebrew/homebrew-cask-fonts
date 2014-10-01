d7e0b08f0b784813162bea32f9b6238a0ab03b4f
Roland Walker <walker@pobox.com>
2014-10-01 09:58:34 -0400

    add license stanza, font-swanky-and-moo-moo

--- Casks/font-swanky-and-moo-moo.rb
+++ Casks/font-swanky-and-moo-moo.rb
@@ -4,6 +4,7 @@ class FontSwankyAndMooMoo < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/swankyandmoomoo/SwankyandMooMoo.ttf'
   homepage 'http://www.google.com/fonts/specimen/Swanky%20and%20Moo%20Moo'
+  license :ofl
 
   font 'SwankyandMooMoo.ttf'
 end
