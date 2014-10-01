d8ca8ed53c0f262c16a04cbed1cd8560061343c8
Roland Walker <walker@pobox.com>
2014-10-01 09:58:32 -0400

    add license stanza, font-snippet

--- Casks/font-snippet.rb
+++ Casks/font-snippet.rb
@@ -4,6 +4,7 @@ class FontSnippet < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/snippet/Snippet.ttf'
   homepage 'http://www.google.com/fonts/specimen/Snippet'
+  license :ofl
 
   font 'Snippet.ttf'
 end
