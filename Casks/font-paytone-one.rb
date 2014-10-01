f4efa2de3065655ef62a2e275683d5fd4c536366
Roland Walker <walker@pobox.com>
2014-10-01 09:58:28 -0400

    add license stanza, font-paytone-one

--- Casks/font-paytone-one.rb
+++ Casks/font-paytone-one.rb
@@ -4,6 +4,7 @@ class FontPaytoneOne < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/paytoneone/PaytoneOne.ttf'
   homepage 'http://www.google.com/fonts/specimen/Paytone%20One'
+  license :ofl
 
   font 'PaytoneOne.ttf'
 end
