2acf7e4211e7af46e57936bcf406c2965b9dd7ed
Roland Walker <walker@pobox.com>
2014-10-01 09:58:14 -0400

    add license stanza, font-georgia

--- Casks/font-georgia.rb
+++ Casks/font-georgia.rb
@@ -4,6 +4,7 @@ class FontGeorgia < Cask
 
   url 'http://downloads.sourceforge.net/sourceforge/corefonts/georgi32.exe'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  license :oss
   depends_on :formula => 'cabextract'
 
   font 'Georgiaz.TTF'
