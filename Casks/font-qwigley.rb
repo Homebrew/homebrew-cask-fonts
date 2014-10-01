eae62409dcad669a7cb6adce9463daf4299b5559
Roland Walker <walker@pobox.com>
2014-10-01 09:58:29 -0400

    add license stanza, font-qwigley

--- Casks/font-qwigley.rb
+++ Casks/font-qwigley.rb
@@ -4,6 +4,7 @@ class FontQwigley < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/qwigley/Qwigley-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Qwigley'
+  license :ofl
 
   font 'Qwigley-Regular.ttf'
 end
