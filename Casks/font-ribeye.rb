7c2471bf7bf9ff00185b4920cd24892e1b04114e
Roland Walker <walker@pobox.com>
2014-10-01 09:58:30 -0400

    add license stanza, font-ribeye

--- Casks/font-ribeye.rb
+++ Casks/font-ribeye.rb
@@ -4,6 +4,7 @@ class FontRibeye < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/ribeye/Ribeye-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Ribeye'
+  license :ofl
 
   font 'Ribeye-Regular.ttf'
 end
