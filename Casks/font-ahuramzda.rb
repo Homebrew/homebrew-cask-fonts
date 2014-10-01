905868313fbfc96a0d3261bd9573f3c999f0b858
Roland Walker <walker@pobox.com>
2014-10-01 09:58:03 -0400

    add license stanza, font-ahuramzda

--- Casks/font-ahuramzda.rb
+++ Casks/font-ahuramzda.rb
@@ -4,6 +4,7 @@ class FontAhuramzda < Cask
 
   url 'http://openfontlibrary.org/assets/downloads/ahuramazda/b2c0eeb9186f389749746f075b5a1abf/ahuramazda.zip'
   homepage 'http://openfontlibrary.org/font/ahuramazda/'
+  license :unknown
 
   font 'Ahuramazda-Avestan-Font-1.0/ahuramazda.ttf'
 end
