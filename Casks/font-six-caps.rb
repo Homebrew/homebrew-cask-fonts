9500563cd2764743da037b54b685e3e6624865b2
Roland Walker <walker@pobox.com>
2014-10-01 09:58:32 -0400

    add license stanza, font-six-caps

--- Casks/font-six-caps.rb
+++ Casks/font-six-caps.rb
@@ -4,6 +4,7 @@ class FontSixCaps < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/sixcaps/SixCaps.ttf'
   homepage 'http://www.google.com/fonts/specimen/Six%20Caps'
+  license :ofl
 
   font 'SixCaps.ttf'
 end
