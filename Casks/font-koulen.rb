2ca3b7551c3f6a45baf56940c64dcc372a0ef890
Roland Walker <walker@pobox.com>
2014-10-01 09:58:17 -0400

    add license stanza, font-koulen

--- Casks/font-koulen.rb
+++ Casks/font-koulen.rb
@@ -4,6 +4,7 @@ class FontKoulen < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/koulen/Koulen.ttf'
   homepage 'http://www.google.com/fonts/specimen/Koulen'
+  license :ofl
 
   font 'Koulen.ttf'
 end
