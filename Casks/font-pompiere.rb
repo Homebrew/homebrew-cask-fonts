c5ec1f901334cbb0b3d1d42d075888bc74623893
Roland Walker <walker@pobox.com>
2014-10-01 09:58:28 -0400

    add license stanza, font-pompiere

--- Casks/font-pompiere.rb
+++ Casks/font-pompiere.rb
@@ -4,6 +4,7 @@ class FontPompiere < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/pompiere/Pompiere-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Pompiere'
+  license :ofl
 
   font 'Pompiere-Regular.ttf'
 end
