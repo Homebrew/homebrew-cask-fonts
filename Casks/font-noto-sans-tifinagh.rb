8574332bddc73abdc7540a629d6b0b609f4de6fc
Roland Walker <walker@pobox.com>
2014-10-01 09:58:25 -0400

    add license stanza, font-noto-sans-tifinagh

--- Casks/font-noto-sans-tifinagh.rb
+++ Casks/font-noto-sans-tifinagh.rb
@@ -4,6 +4,7 @@ class FontNotoSansTifinagh < Cask
 
   url 'https://www.google.com/get/noto/pkgs/NotoSansTifinagh-unhinted.zip'
   homepage 'http://www.google.com/get/noto'
+  license :apache
 
   font 'NotoSansTifinagh-Regular.ttf'
 end
