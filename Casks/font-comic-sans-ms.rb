95c139c47ca16d1493bafb17142dcac997c45da6
Roland Walker <walker@pobox.com>
2014-10-01 09:58:09 -0400

    add license stanza, font-comic-sans-ms

--- Casks/font-comic-sans-ms.rb
+++ Casks/font-comic-sans-ms.rb
@@ -4,6 +4,7 @@ class FontComicSansMs < Cask
 
   url 'http://downloads.sourceforge.net/sourceforge/corefonts/comic32.exe'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  license :oss
   depends_on :formula => 'cabextract'
 
   font 'Comicbd.TTF'
