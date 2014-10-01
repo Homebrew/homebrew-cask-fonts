1ad6acd57fc60db73ed9f54da109588e5a62a465
Roland Walker <walker@pobox.com>
2014-10-01 09:58:32 -0400

    add license stanza, font-share-tech

--- Casks/font-share-tech.rb
+++ Casks/font-share-tech.rb
@@ -4,6 +4,7 @@ class FontShareTech < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/sharetech/ShareTech-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Share%20Tech'
+  license :ofl
 
   font 'ShareTech-Regular.ttf'
 end
