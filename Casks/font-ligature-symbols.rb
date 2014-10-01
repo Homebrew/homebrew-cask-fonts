fe479b5c16e53647d5003a4d036c397dfaecd073
Roland Walker <walker@pobox.com>
2014-10-01 09:58:18 -0400

    add license stanza, font-ligature-symbols

--- Casks/font-ligature-symbols.rb
+++ Casks/font-ligature-symbols.rb
@@ -4,6 +4,7 @@ class FontLigatureSymbols < Cask
 
   url 'http://kudakurage.com/ligature_symbols/LigatureSymbols.zip'
   homepage 'http://kudakurage.com/ligature_symbols/'
+  license :unknown
 
   font 'LigatureSymbols/LigatureSymbols-2.11.otf'
 end
