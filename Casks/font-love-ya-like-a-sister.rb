d7192a61861ac4f4e1b151318aa0f98db5bab3f2
Roland Walker <walker@pobox.com>
2014-10-01 09:58:19 -0400

    add license stanza, font-love-ya-like-a-sister

--- Casks/font-love-ya-like-a-sister.rb
+++ Casks/font-love-ya-like-a-sister.rb
@@ -4,6 +4,7 @@ class FontLoveYaLikeASister < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/loveyalikeasister/LoveYaLikeASister.ttf'
   homepage 'http://www.google.com/fonts/specimen/Love%20Ya%20Like%20A%20Sister'
+  license :ofl
 
   font 'LoveYaLikeASister.ttf'
 end
