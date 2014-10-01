0d9232a462ae50161e0e8714db29697e7bf4f365
Roland Walker <walker@pobox.com>
2014-10-01 09:58:21 -0400

    add license stanza, font-moul-pali

--- Casks/font-moul-pali.rb
+++ Casks/font-moul-pali.rb
@@ -4,6 +4,7 @@ class FontMoulPali < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/moulpali/Moulpali.ttf'
   homepage 'http://www.google.com/fonts/specimen/Moulpali'
+  license :oss
 
   font 'Moulpali.ttf'
 end
