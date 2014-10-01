6fc316f81555b2662f280628fed68a7265177564
Roland Walker <walker@pobox.com>
2014-10-01 09:58:13 -0400

    add license stanza, font-fjord

--- Casks/font-fjord.rb
+++ Casks/font-fjord.rb
@@ -4,6 +4,7 @@ class FontFjord < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/fjordone/FjordOne-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Fjord%20One'
+  license :oss
 
   font 'FjordOne-Regular.ttf'
 end
