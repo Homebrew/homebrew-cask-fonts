a3c5a9986cabb38dcf08b9f3465361074feff75e
Roland Walker <walker@pobox.com>
2014-10-01 09:58:23 -0400

    add license stanza, font-nothing-you-could-do

--- Casks/font-nothing-you-could-do.rb
+++ Casks/font-nothing-you-could-do.rb
@@ -4,6 +4,7 @@ class FontNothingYouCouldDo < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/nothingyoucoulddo/NothingYouCouldDo.ttf'
   homepage 'http://www.google.com/fonts/specimen/Nothing%20You%20Could%20Do'
+  license :ofl
 
   font 'NothingYouCouldDo.ttf'
 end
