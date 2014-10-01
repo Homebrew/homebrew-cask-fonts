b08c23b45030378131a3bf8a01dcf5f8d2e197fe
Roland Walker <walker@pobox.com>
2014-10-01 09:58:16 -0400

    add license stanza, font-jacques-francois-shadow

--- Casks/font-jacques-francois-shadow.rb
+++ Casks/font-jacques-francois-shadow.rb
@@ -4,6 +4,7 @@ class FontJacquesFrancoisShadow < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/jacquesfrancoisshadow/JacquesFrancoisShadow-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Jacques%20Francois%20Shadow'
+  license :ofl
 
   font 'JacquesFrancoisShadow-Regular.ttf'
 end
