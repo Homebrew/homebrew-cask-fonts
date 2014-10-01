803e2c62d10487210c1c4c0aa4ff4139c33381cb
Roland Walker <walker@pobox.com>
2014-10-01 09:58:12 -0400

    add license stanza, font-fascinate-inline

--- Casks/font-fascinate-inline.rb
+++ Casks/font-fascinate-inline.rb
@@ -4,6 +4,7 @@ class FontFascinateInline < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/fascinateinline/FascinateInline-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Fascinate%20Inline'
+  license :ofl
 
   font 'FascinateInline-Regular.ttf'
 end
