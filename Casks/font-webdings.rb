f0e6d7220ce99d8eb3a51ac375964e97a787d88b
Roland Walker <walker@pobox.com>
2014-10-01 09:58:36 -0400

    add license stanza, font-webdings

--- Casks/font-webdings.rb
+++ Casks/font-webdings.rb
@@ -4,6 +4,7 @@ class FontWebdings < Cask
 
   url 'http://downloads.sourceforge.net/sourceforge/corefonts/webdin32.exe'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  license :oss
   depends_on :formula => 'cabextract'
 
   font 'Webdings.TTF'
