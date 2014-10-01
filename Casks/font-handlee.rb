e7680adc9839c71eb38e1b07c200992dc9abecf3
Roland Walker <walker@pobox.com>
2014-10-01 09:58:15 -0400

    add license stanza, font-handlee

--- Casks/font-handlee.rb
+++ Casks/font-handlee.rb
@@ -4,6 +4,7 @@ class FontHandlee < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/handlee/Handlee-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Handlee'
+  license :ofl
 
   font 'Handlee-Regular.ttf'
 end
