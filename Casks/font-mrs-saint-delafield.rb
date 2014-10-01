ca4ce588c88b1668be51d0842755182a2fde904d
Roland Walker <walker@pobox.com>
2014-10-01 09:58:22 -0400

    add license stanza, font-mrs-saint-delafield

--- Casks/font-mrs-saint-delafield.rb
+++ Casks/font-mrs-saint-delafield.rb
@@ -4,6 +4,7 @@ class FontMrsSaintDelafield < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/mrssaintdelafield/MrsSaintDelafield-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Mrs%20Saint%20Delafield'
+  license :ofl
 
   font 'MrsSaintDelafield-Regular.ttf'
 end
