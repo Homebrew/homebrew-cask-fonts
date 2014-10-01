8ec9e1de094f367e9d01e65925a86e4c41ab93e1
Roland Walker <walker@pobox.com>
2014-10-01 09:58:21 -0400

    add license stanza, font-miltonian

--- Casks/font-miltonian.rb
+++ Casks/font-miltonian.rb
@@ -4,6 +4,7 @@ class FontMiltonian < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/miltonian/Miltonian-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Miltonian'
+  license :ofl
 
   font 'Miltonian-Regular.ttf'
 end
