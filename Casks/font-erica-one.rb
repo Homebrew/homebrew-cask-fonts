5ad4713884ac589c8587b0b857ec9508029173c0
Roland Walker <walker@pobox.com>
2014-10-01 09:58:12 -0400

    add license stanza, font-erica-one

--- Casks/font-erica-one.rb
+++ Casks/font-erica-one.rb
@@ -4,6 +4,7 @@ class FontEricaOne < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/ericaone/EricaOne-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Erica%20One'
+  license :ofl
 
   font 'EricaOne-Regular.ttf'
 end
