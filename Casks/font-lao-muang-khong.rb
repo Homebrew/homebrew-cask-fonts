0574b570589c02b95e901e79469ed6253b1ddd9d
Roland Walker <walker@pobox.com>
2014-10-01 09:58:18 -0400

    add license stanza, font-lao-muang-khong

--- Casks/font-lao-muang-khong.rb
+++ Casks/font-lao-muang-khong.rb
@@ -4,6 +4,7 @@ class FontLaoMuangKhong < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/laomuangkhong/LaoMuangKhong-Regular.ttf'
   homepage 'https://www.google.com/fonts/earlyaccess'
+  license :oss
 
   font 'LaoMuangKhong-Regular.ttf'
 end
