796dd1e323742da6cae7e0816c2473578e4a3239
Roland Walker <walker@pobox.com>
2014-10-01 09:58:09 -0400

    add license stanza, font-condiment

--- Casks/font-condiment.rb
+++ Casks/font-condiment.rb
@@ -4,6 +4,7 @@ class FontCondiment < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/condiment/Condiment-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Condiment'
+  license :ofl
 
   font 'Condiment-Regular.ttf'
 end
