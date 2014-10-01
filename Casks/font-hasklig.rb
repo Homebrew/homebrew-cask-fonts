ca54ef72c8fb116a780d4d26c43fdeb0876ef537
ndr <ndr@qef.io>
2014-10-01 19:25:33 +0200

    Add Hasklig, version 0.4

---
+++ Casks/font-hasklig.rb
@@ -0,0 +1,15 @@
+class FontHasklig < Cask
+  version '0.4'
+  sha256 '2876403c16225f0f3f74cff127c3e2ade754a2e83b87ab6fe9461b7e95fb4092'
+
+  url "https://github.com/i-tu/Hasklig/releases/download/#{version}/Hasklig-#{version}.zip"
+  homepage 'https://github.com/i-tu/Hasklig'
+
+  font 'Hasklig-Black.otf'
+  font 'Hasklig-Bold.otf'
+  font 'Hasklig-ExtraLight.otf'
+  font 'Hasklig-Light.otf'
+  font 'Hasklig-Medium.otf'
+  font 'Hasklig-Regular.otf'
+  font 'Hasklig-Semibold.otf'
+end
