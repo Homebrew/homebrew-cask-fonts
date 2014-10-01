a0654292c8e30179fb6fe24b93feb6c077bfd91f
Roland Walker <walker@pobox.com>
2014-10-01 09:58:21 -0400

    add license stanza, font-miltonian-tattoo

--- Casks/font-miltonian-tattoo.rb
+++ Casks/font-miltonian-tattoo.rb
@@ -4,6 +4,7 @@ class FontMiltonianTattoo < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/miltoniantattoo/MiltonianTattoo-Regular.ttf'
   homepage 'http://www.google.com/fonts/specimen/Miltonian%20Tattoo'
+  license :ofl
 
   font 'MiltonianTattoo-Regular.ttf'
 end
