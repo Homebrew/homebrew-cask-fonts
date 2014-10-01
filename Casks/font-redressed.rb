46a6ce11cae73443082c0a3552f52c7859753198
Roland Walker <walker@pobox.com>
2014-10-01 09:58:30 -0400

    add license stanza, font-redressed

--- Casks/font-redressed.rb
+++ Casks/font-redressed.rb
@@ -4,6 +4,7 @@ class FontRedressed < Cask
 
   url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/redressed/Redressed.ttf'
   homepage 'http://www.google.com/fonts/specimen/Redressed'
+  license :apache
 
   font 'Redressed.ttf'
 end
