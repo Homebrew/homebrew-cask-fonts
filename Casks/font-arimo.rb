58a65af9fa025a4229f242b620857e370eb2bf43
Roland Walker <walker@pobox.com>
2014-10-01 09:58:05 -0400

    add license stanza, font-arimo

--- Casks/font-arimo.rb
+++ Casks/font-arimo.rb
@@ -8,6 +8,7 @@ class FontArimo < Cask
       :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Arimo'
+  license :apache
 
   font 'Arimo-Bold.ttf'
   font 'Arimo-BoldItalic.ttf'
