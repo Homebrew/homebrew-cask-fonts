a1e628f520f6089b2dfac4235abca94e3fd1367d
ndr <ndr@qef.io>
2014-10-20 17:44:28 +0100

    Update PT Mono to 1.003

--- Casks/font-pt-mono.rb
+++ Casks/font-pt-mono.rb
@@ -1,8 +1,8 @@
 class FontPtMono < Cask
-  version '1.002'
-  sha256 '93c0ca1d9873ecc7c70131fa763aaaaa24345fc8b8f193ea2175b0857823d58a'
+  version '1.003'
+  sha256 '741b1457066d69f045ac5196a512c2c1ca7900645b97bcb7280576d34e540af3'
 
-  url 'http://www.fontstock.com/public/PTMono.zip'
+  url 'http://www.paratype.com/uni/public/PTMono.zip'
   homepage 'http://www.paratype.com/public/'
   license :ofl
 
