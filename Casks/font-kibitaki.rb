51873ffe01e905bbe78e7abc29e264120cd3e4a0
Yusuke Nakamura <yusuke1994525@gmail.com>
2014-11-15 22:11:21 +0900

    update font kibitaki
    Closes #254.

--- Casks/font-kibitaki.rb
+++ Casks/font-kibitaki.rb
@@ -1,13 +1,13 @@
 cask :v1 => 'font-kibitaki' do
-  version :latest
-  sha256 :no_check
+  version '20140621'
+  sha256 '8766502f4f6547c2373bb7b54a0395e6e2da354a21fe4797cbeb795d2d10ddb4'
 
-  url 'https://github.com/Koruri/kibitaki/archive/master.zip'
+  url "https://github.com/Koruri/kibitaki/archive/Kibitaki-#{version}.zip"
   homepage 'http://kibitaki.lindwurm.biz/'
-  license :oss
+  license :ofl
 
-  font 'kibitaki-master/Kibitaki-Bold.ttf'
-  font 'kibitaki-master/Kibitaki-Light.ttf'
-  font 'kibitaki-master/Kibitaki-Medium.ttf'
-  font 'kibitaki-master/Kibitaki-Regular.ttf'
+  font "kibitaki-Kibitaki-#{version}/Kibitaki-Bold.ttf"
+  font "kibitaki-Kibitaki-#{version}/Kibitaki-Light.ttf"
+  font "kibitaki-Kibitaki-#{version}/Kibitaki-Medium.ttf"
+  font "kibitaki-Kibitaki-#{version}/Kibitaki-Regular.ttf"
 end
