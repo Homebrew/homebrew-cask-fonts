0cca79e3b3d19359dde0d4411ebb37588eee1ee7
Yusuke Nakamura <yusuke1994525@gmail.com>
2014-11-15 22:07:05 +0900

    update font Koruri
    Closes #253.

--- Casks/font-koruri.rb
+++ Casks/font-koruri.rb
@@ -1,14 +1,15 @@
 cask :v1 => 'font-koruri' do
-  version :latest
-  sha256 :no_check
+  version '20140904'
+  sha256 '312cf04bfb79e4e0d24275ea9a0e1ba8edaf6e68315be8be3acb9ee3477c4c60'
 
-  url 'https://github.com/Koruri/Koruri/archive/master.zip'
+  # sourceforge.jp is the official download host per the vendor homepage
+  url "http://dl.sourceforge.jp/koruri/61919/Koruri-#{version}.tar.xz"
   homepage 'http://koruri.lindwurm.biz/'
-  license :oss
+  license :apache
 
-  font 'Koruri-master/Koruri-Bold.ttf'
-  font 'Koruri-master/Koruri-Extrabold.ttf'
-  font 'Koruri-master/Koruri-Light.ttf'
-  font 'Koruri-master/Koruri-Regular.ttf'
-  font 'Koruri-master/Koruri-Semibold.ttf'
+  font "Koruri-#{version}/Koruri-Bold.ttf"
+  font "Koruri-#{version}/Koruri-Extrabold.ttf"
+  font "Koruri-#{version}/Koruri-Light.ttf"
+  font "Koruri-#{version}/Koruri-Regular.ttf"
+  font "Koruri-#{version}/Koruri-Semibold.ttf"
 end
