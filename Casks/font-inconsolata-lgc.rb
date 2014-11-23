861990cdd1f558bcbb5fbc8b8ff7680a86939fe4
marvell <alex@ndr.su>
2014-11-23 03:25:36 +0300

    Add Inconsolata-LGC Latest

---
+++ Casks/font-inconsolata-lgc.rb
@@ -0,0 +1,16 @@
+cask :v1 => 'font-inconsolata-lgc' do
+  # version '1.1.0'
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/DeLaGuardo/Inconsolata-LGC/trunk',
+      :using      => :svn,
+      :trust_cert => true
+  homepage 'https://github.com/DeLaGuardo/Inconsolata-LGC'
+  license :ofl
+
+  font 'inconsolatalgc.ttf'
+  font 'inconsolatalgcbold.ttf'
+  font 'inconsolatalgcbolditalic.ttf'
+  font 'inconsolatalgcitalic.ttf'
+end
