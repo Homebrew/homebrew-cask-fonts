5f138ced34d0b57c1647a3054acf62b558d9a72c
Roland Walker <walker@pobox.com>
2014-12-20 11:14:33 -0500

    missing license in font-hasklig

--- Casks/font-hasklig.rb
+++ Casks/font-hasklig.rb
@@ -4,6 +4,7 @@ cask :v1 => 'font-hasklig' do
 
   url "https://github.com/i-tu/Hasklig/releases/download/#{version}/Hasklig-#{version}.zip"
   homepage 'https://github.com/i-tu/Hasklig'
+  license :unknown # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   font 'Hasklig-Black.otf'
   font 'Hasklig-Bold.otf'
