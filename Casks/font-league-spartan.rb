f1bae4b84dc531e888e171d399d8b93150f12421
Roland Walker <walker@pobox.com>
2014-12-20 11:53:20 -0500

    use double-quotes in font-league-spartan
    interpolation is needed

--- Casks/font-league-spartan.rb
+++ Casks/font-league-spartan.rb
@@ -2,7 +2,7 @@ cask :v1 => 'font-league-spartan' do
   version 'c350408b07'
   sha256 'a166294a7e156a0eb14df5714acfeeb3ad4db01eab2bc88f961695da917337fd'
 
-  url 'http://files.theleagueofmoveabletype.com/downloads/league-spartan-#{version}.zip'
+  url "http://files.theleagueofmoveabletype.com/downloads/league-spartan-#{version}.zip"
   homepage 'https://www.theleagueofmoveabletype.com/league-spartan'
   license :ofl
 
