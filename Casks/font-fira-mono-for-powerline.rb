8090e4c8f122b670d20c855b13d039a574e77cd8
Ahmed El Gabri <ahmed@gabri.me>
2015-01-04 23:37:22 +0400

    Add Fira Mono & Source code pro for powerline fonts

---
+++ Casks/font-fira-mono-for-powerline.rb
@@ -0,0 +1,14 @@
+cask :v1 => 'font-fira-mono-for-powerline' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/powerline/fonts/trunk/FiraMono',
+      :using => :svn,
+      :trust_cert => true
+  homepage 'https://github.com/powerline/fonts/tree/master/FiraMono'
+  license :oss
+
+  font 'FuraMono-Bold Powerline.otf'
+  font 'FuraMono-Medium Powerline.otf'
+  font 'FuraMono-Regular Powerline.otf'
+end
