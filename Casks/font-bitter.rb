927000602d399485f065fb5c6c8d7f0893a0cf82
Vítor Galvão <info@vitorgalvao.com>
2015-01-24 23:35:55 +0000

    Update font-bitter.rb

--- Casks/font-bitter.rb
+++ Casks/font-bitter.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'font-bitter' do
-  version :latest
-  sha256 :no_check
+  version '48'
+  sha256 '64da7417f87835e166879af74ddb733d67d8a834cc3fe96ac303e97e5f6b86ea'
 
-  url 'http://www.huertatipografica.com/free_download/36'
+  url "http://www.huertatipografica.com/free_download/#{version}"
   homepage 'http://www.huertatipografica.com/fonts/bitter-ht'
   license :ofl
 
