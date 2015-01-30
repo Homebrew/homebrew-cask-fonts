d810ab6449c40dcc7f85eea6737d5029bac921c2
Andres Torres <andres.torres.marroquin@gmail.com>
2015-01-29 18:45:25 -0600

    Updating Font Awesome Cask

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'font-fontawesome' do
   version '4.2.0'
-  sha256 'b0f2792a2a096ccebe72cfcf1880671658a1f18d5356c5f991b2b54d8f6bf704'
+  sha256 'b68b47c5e6da744fc78393eeae0cb16e3844a76b505855c99ff50e99c680b5c0'
 
-  url "http://fontawesome.io/assets/font-awesome-#{version}.zip"
+  url "https://github.com/FortAwesome/Font-Awesome/archive/v4.2.0.zip"
   homepage 'http://fortawesome.github.io/Font-Awesome/'
   license :oss
 
