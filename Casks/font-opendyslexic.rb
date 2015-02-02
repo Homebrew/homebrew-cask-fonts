41e8de3215063092f19c45829f9bf4f33dee80c3
Benjamin Lupton <b@lupton.cc>
2015-02-02 09:05:43 +0800

    Added the opendyslexic font

---
+++ Casks/font-opendyslexic.rb
@@ -0,0 +1,28 @@
+FAMILY: OpenDyslexic
+cask :v1 => 'font-opendyslexic' do
+  version 'f4b5ba89018b44d633608907e15f93fb3fabbabc'
+  sha256 '579285694acc7dbe007faf7b54e02b4be3f93837c37f5304e0c090e0d36178d1'
+
+  url 'https://github.com/antijingoist/open-dyslexic/archive/f4b5ba89018b44d633608907e15f93fb3fabbabc.zip'
+  homepage 'https://github.com/antijingoist/open-dyslexic'
+  license :bvl
+
+  font 'open-dyslexic-master/otf/OpenDyslexic-Bold.otf'
+  font 'open-dyslexic-master/otf/OpenDyslexic-BoldItalic.otf'
+  font 'open-dyslexic-master/otf/OpenDyslexic-Italic.otf'
+  font 'open-dyslexic-master/otf/OpenDyslexic-Regular.otf'
+  font 'open-dyslexic-master/otf/OpenDyslexicAlta-Bold.otf'
+  font 'open-dyslexic-master/otf/OpenDyslexicAlta-BoldItalic.otf'
+  font 'open-dyslexic-master/otf/OpenDyslexicAlta-Italic.otf'
+  font 'open-dyslexic-master/otf/OpenDyslexicAlta-Regular.otf'
+  font 'open-dyslexic-master/otf/OpenDyslexicMono-Regular.otf'
+  font 'open-dyslexic-master/ttf/OpenDyslexic-Bold.ttf'
+  font 'open-dyslexic-master/ttf/OpenDyslexic-BoldItalic.ttf'
+  font 'open-dyslexic-master/ttf/OpenDyslexic-Italic.ttf'
+  font 'open-dyslexic-master/ttf/OpenDyslexic-Regular.ttf'
+  font 'open-dyslexic-master/ttf/OpenDyslexicAlta-Bold.ttf'
+  font 'open-dyslexic-master/ttf/OpenDyslexicAlta-BoldItalic.ttf'
+  font 'open-dyslexic-master/ttf/OpenDyslexicAlta-Italic.ttf'
+  font 'open-dyslexic-master/ttf/OpenDyslexicAlta-Regular.ttf'
+  font 'open-dyslexic-master/ttf/OpenDyslexicMono-Regular.ttf'
+end
