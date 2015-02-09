3b2f3ab96262a71c720fd046f2451a42c5260901
Vincent Klaiber <vincentklaiber@gmail.com>
2015-02-09 08:52:40 +0100

    Add SkipLegDay.tff latest version.

---
+++ Casks/font-skiplegday.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'font-skiplegday' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://dl.dafont.com/dl/?f=skiplegday'
+  homepage 'https://www.behance.net/gallery/18515345/Skip-Leg-Day-Typeface'
+  license :unknown
+
+  font 'SkipLegDay.ttf'
+end
