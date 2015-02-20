24a18b31b3c06d82d37821c9759910821448be2f
zchee <zcheeee@gmail.com>
2015-02-21 00:44:31 +0900

    Add Myrica fonts cask

---
+++ Casks/font-myrica.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'font-myrica' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/tomokuni/Myrica/archive/master.zip'
+  homepage 'https://github.com/tomokuni/Myrica'
+
+  font 'Myrica-master/Work/MyricaM.ttf'
+  font 'Myrica-master/Work/MyricaM_NoHint.ttf'
+  font 'Myrica-master/Work/MyricaN.ttf'
+  font 'Myrica-master/Work/MyricaP.ttf'
+end
