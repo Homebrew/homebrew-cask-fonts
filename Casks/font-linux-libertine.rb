233e0a70b911cfbd71c483005c9b5e53e82f080d
flother <matt.riggott@gmail.com>
2015-03-06 11:52:41 +0000

    Add Linux Libertine v5.3.0
    Cask for installing the open-source Linux Libertine font, licensed
    under the OFL.

---
+++ Casks/font-linux-libertine.rb
@@ -0,0 +1,18 @@
+cask :v1 => 'font-linux-libertine' do
+  version '5.3.0'
+  sha256 '24a593a949808d976850131a953c0c0d7a72299531dfbb348191964cc038d75d'
+
+  url 'http://downloads.sourceforge.net/project/linuxlibertine/linuxlibertine/5.3.0/LinLibertineTTF_5.3.0_2012_07_02.tgz'
+  homepage 'http://linuxlibertine.org'
+  license :ofl
+
+  font 'LinLibertine_DRah.ttf'
+  font 'LinLibertine_I.ttf'
+  font 'LinLibertine_Mah.ttf'
+  font 'LinLibertine_RBIah.ttf'
+  font 'LinLibertine_RBah.ttf'
+  font 'LinLibertine_RIah.ttf'
+  font 'LinLibertine_RZIah.ttf'
+  font 'LinLibertine_RZah.ttf'
+  font 'LinLibertine_Rah.ttf'
+end
