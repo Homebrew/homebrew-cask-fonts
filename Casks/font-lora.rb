e6ecb0408d3e623b856a3afa684333384164cc26
ndr <ndr@qef.io>
2015-03-23 12:23:16 +0000

    font-lora: download from official github.com/google/fonts repository

--- Casks/font-lora.rb
+++ Casks/font-lora.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-lora' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/lora',
+  url 'https://github.com/google/fonts/trunk/ofl/lora',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
