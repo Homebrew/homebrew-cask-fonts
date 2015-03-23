bb2e284e789c1e0ae9ad883fc846f0f416f3dc1e
ndr <ndr@qef.io>
2015-03-23 12:23:16 +0000

    font-im-fell-great-primer: download from official github.com/google/fonts repository

--- Casks/font-im-fell-great-primer.rb
+++ Casks/font-im-fell-great-primer.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-im-fell-great-primer' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/imfellgreatprimer',
+  url 'https://github.com/google/fonts/trunk/ofl/imfellgreatprimer',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
