073f2e660dc571dd87e4482adb5d173fbb736076
ndr <ndr@qef.io>
2015-03-23 12:23:16 +0000

    font-kreon: download from official github.com/google/fonts repository

--- Casks/font-kreon.rb
+++ Casks/font-kreon.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-kreon' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/kreon',
+  url 'https://github.com/google/fonts/trunk/ofl/kreon',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
