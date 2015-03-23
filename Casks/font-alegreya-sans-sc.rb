d3eb33118528be28420f3de7a64a650870f2f806
ndr <ndr@qef.io>
2015-03-23 12:23:14 +0000

    font-alegreya-sans-sc: download from official github.com/google/fonts repository

--- Casks/font-alegreya-sans-sc.rb
+++ Casks/font-alegreya-sans-sc.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-alegreya-sans-sc' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/alegreyasanssc',
+  url 'https://github.com/google/fonts/trunk/ofl/alegreyasanssc',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
