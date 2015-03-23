3dbe4407da7b33f99b9f4c4ca54dea97ccfc81c1
ndr <ndr@qef.io>
2015-03-23 12:23:17 +0000

    font-podkova: download from official github.com/google/fonts repository

--- Casks/font-podkova.rb
+++ Casks/font-podkova.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-podkova' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/podkova',
+  url 'https://github.com/google/fonts/trunk/ofl/podkova',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
