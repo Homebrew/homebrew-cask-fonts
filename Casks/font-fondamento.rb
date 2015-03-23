70447f7a1f3279f05cf692958b36829fa7ccb5d9
ndr <ndr@qef.io>
2015-03-23 12:23:16 +0000

    font-fondamento: download from official github.com/google/fonts repository

--- Casks/font-fondamento.rb
+++ Casks/font-fondamento.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-fondamento' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/fondamento',
+  url 'https://github.com/google/fonts/trunk/ofl/fondamento',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
