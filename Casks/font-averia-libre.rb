5214b24fc3748ea126666fee65736892daecccdd
ndr <ndr@qef.io>
2015-03-23 12:23:14 +0000

    font-averia-libre: download from official github.com/google/fonts repository

--- Casks/font-averia-libre.rb
+++ Casks/font-averia-libre.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-averia-libre' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/averialibre',
+  url 'https://github.com/google/fonts/trunk/ofl/averialibre',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
