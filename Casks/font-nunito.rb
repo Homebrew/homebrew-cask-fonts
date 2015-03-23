65fae88b11626da3ac996e2519a50353bf2bf44f
ndr <ndr@qef.io>
2015-03-23 12:23:17 +0000

    font-nunito: download from official github.com/google/fonts repository

--- Casks/font-nunito.rb
+++ Casks/font-nunito.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-nunito' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/nunito',
+  url 'https://github.com/google/fonts/trunk/ofl/nunito',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
