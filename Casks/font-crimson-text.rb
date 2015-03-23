0c911eabba3b1b45fed26c5319d62c8b9465010d
ndr <ndr@qef.io>
2015-03-23 12:23:15 +0000

    font-crimson-text: download from official github.com/google/fonts repository

--- Casks/font-crimson-text.rb
+++ Casks/font-crimson-text.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-crimson-text' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/crimsontext',
+  url 'https://github.com/google/fonts/trunk/ofl/crimsontext',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
