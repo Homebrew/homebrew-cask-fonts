6032a8a9af40294d0311ac5008143263be2f2a7e
ndr <ndr@qef.io>
2015-03-23 12:23:16 +0000

    font-merriweather: download from official github.com/google/fonts repository

--- Casks/font-merriweather.rb
+++ Casks/font-merriweather.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-merriweather' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/merriweather',
+  url 'https://github.com/google/fonts/trunk/ofl/merriweather',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
