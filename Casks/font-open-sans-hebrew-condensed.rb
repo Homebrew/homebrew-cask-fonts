ae04781b99c67d04f788a8ffe0471efbdbd68889
ndr <ndr@qef.io>
2015-03-23 12:23:17 +0000

    font-open-sans-hebrew-condensed: download from official github.com/google/fonts repository

--- Casks/font-open-sans-hebrew-condensed.rb
+++ Casks/font-open-sans-hebrew-condensed.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-open-sans-hebrew-condensed' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/opensanshebrewcondensed',
+  url 'https://github.com/google/fonts/trunk/apache/opensanshebrewcondensed',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
