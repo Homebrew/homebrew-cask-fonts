e913f9dd30f6f9d3902f5b664d440210bfc95cb0
ndr <ndr@qef.io>
2015-03-23 12:23:15 +0000

    font-cabin-condensed: download from official github.com/google/fonts repository

--- Casks/font-cabin-condensed.rb
+++ Casks/font-cabin-condensed.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-cabin-condensed' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/cabincondensed',
+  url 'https://github.com/google/fonts/trunk/ofl/cabincondensed',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
