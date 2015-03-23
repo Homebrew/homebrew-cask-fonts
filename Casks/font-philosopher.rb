e751c9f30a66e86aa7ceae813d152895cef2d20d
ndr <ndr@qef.io>
2015-03-23 12:23:17 +0000

    font-philosopher: download from official github.com/google/fonts repository

--- Casks/font-philosopher.rb
+++ Casks/font-philosopher.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-philosopher' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/philosopher',
+  url 'https://github.com/google/fonts/trunk/ofl/philosopher',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
