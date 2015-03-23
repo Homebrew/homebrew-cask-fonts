ca28a2cef6eca3ceae04f2d8c850ddb9639437ee
ndr <ndr@qef.io>
2015-03-23 12:23:15 +0000

    font-elsie: download from official github.com/google/fonts repository

--- Casks/font-elsie.rb
+++ Casks/font-elsie.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-elsie' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/elsie',
+  url 'https://github.com/google/fonts/trunk/ofl/elsie',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
