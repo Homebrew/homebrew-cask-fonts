d52f56ca2316c2761ceb80dd95f709fefb9a8353
ndr <ndr@qef.io>
2015-03-23 12:23:15 +0000

    font-dancing-script: download from official github.com/google/fonts repository

--- Casks/font-dancing-script.rb
+++ Casks/font-dancing-script.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-dancing-script' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/dancingscript',
+  url 'https://github.com/google/fonts/trunk/ofl/dancingscript',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
