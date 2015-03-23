1e0a6bf98217ffb85343c1a84b3087e250cc2e86
ndr <ndr@qef.io>
2015-03-23 12:23:18 +0000

    font-share: download from official github.com/google/fonts repository

--- Casks/font-share.rb
+++ Casks/font-share.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-share' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/share',
+  url 'https://github.com/google/fonts/trunk/ofl/share',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
