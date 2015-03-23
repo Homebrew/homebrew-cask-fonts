3a691df9383dddaa6a39fce7746a21fc754ad0d3
ndr <ndr@qef.io>
2015-03-23 12:23:14 +0000

    font-allan: download from official github.com/google/fonts repository

--- Casks/font-allan.rb
+++ Casks/font-allan.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-allan' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/allan',
+  url 'https://github.com/google/fonts/trunk/ofl/allan',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
