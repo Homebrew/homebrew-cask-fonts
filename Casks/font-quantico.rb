8c5cba6a037693b12fd4c3c3cd18c32446b3c88c
ndr <ndr@qef.io>
2015-03-23 12:23:17 +0000

    font-quantico: download from official github.com/google/fonts repository

--- Casks/font-quantico.rb
+++ Casks/font-quantico.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-quantico' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/quantico',
+  url 'https://github.com/google/fonts/trunk/ofl/quantico',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
