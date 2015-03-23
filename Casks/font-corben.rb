794ae73c8e807bcbde4b9bde6a2d2b1ad683f8b4
ndr <ndr@qef.io>
2015-03-23 12:23:15 +0000

    font-corben: download from official github.com/google/fonts repository

--- Casks/font-corben.rb
+++ Casks/font-corben.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-corben' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/corben',
+  url 'https://github.com/google/fonts/trunk/ofl/corben',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
