c56902285c6bab22b566c1f933d59c4ae492f20e
ndr <ndr@qef.io>
2015-03-23 12:23:16 +0000

    font-kantumruy: download from official github.com/google/fonts repository

--- Casks/font-kantumruy.rb
+++ Casks/font-kantumruy.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-kantumruy' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/kantumruy',
+  url 'https://github.com/google/fonts/trunk/ofl/kantumruy',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
