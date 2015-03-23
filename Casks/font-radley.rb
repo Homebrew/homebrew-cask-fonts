44c6d1382fef1257eaa3b520783b93248e859b69
ndr <ndr@qef.io>
2015-03-23 12:23:17 +0000

    font-radley: download from official github.com/google/fonts repository

--- Casks/font-radley.rb
+++ Casks/font-radley.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-radley' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/radley',
+  url 'https://github.com/google/fonts/trunk/ofl/radley',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
