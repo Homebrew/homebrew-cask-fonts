d172ba4e6a6b7bb56301ae84c6cef26b5e06507b
ndr <ndr@qef.io>
2015-03-23 12:23:17 +0000

    font-muli: download from official github.com/google/fonts repository

--- Casks/font-muli.rb
+++ Casks/font-muli.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-muli' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/muli',
+  url 'https://github.com/google/fonts/trunk/ofl/muli',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
