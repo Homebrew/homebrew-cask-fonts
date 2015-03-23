1026a7ef7170744f5facea84a660db8cddcaf1d6
ndr <ndr@qef.io>
2015-03-23 12:23:17 +0000

    font-montserrat-subrayada: download from official github.com/google/fonts repository

--- Casks/font-montserrat-subrayada.rb
+++ Casks/font-montserrat-subrayada.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-montserrat-subrayada' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/montserratsubrayada',
+  url 'https://github.com/google/fonts/trunk/ofl/montserratsubrayada',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
