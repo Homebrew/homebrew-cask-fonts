d60cf6914b52123fb828eed5a86877caf692a4ef
ndr <ndr@qef.io>
2015-03-23 12:23:16 +0000

    font-josefin-slab: download from official github.com/google/fonts repository

--- Casks/font-josefin-slab.rb
+++ Casks/font-josefin-slab.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-josefin-slab' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/josefinslab',
+  url 'https://github.com/google/fonts/trunk/ofl/josefinslab',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
