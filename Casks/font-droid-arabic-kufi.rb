179f9cd5fd58e4df98958b280d7a6a06ed4caee4
ndr <ndr@qef.io>
2015-03-23 12:23:15 +0000

    font-droid-arabic-kufi: download from official github.com/google/fonts repository

--- Casks/font-droid-arabic-kufi.rb
+++ Casks/font-droid-arabic-kufi.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-droid-arabic-kufi' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/droidarabickufi',
+  url 'https://github.com/google/fonts/trunk/apache/droidarabickufi',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
