df4b18c35022305410ed95f1f0c9a8767e9271f8
ndr <ndr@qef.io>
2015-03-23 12:23:16 +0000

    font-inika: download from official github.com/google/fonts repository

--- Casks/font-inika.rb
+++ Casks/font-inika.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-inika' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/inika',
+  url 'https://github.com/google/fonts/trunk/ofl/inika',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
