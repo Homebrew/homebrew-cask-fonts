b9f877afca1b97684eaea70d74adbe3fc7ad59a4
ndr <ndr@qef.io>
2015-03-23 12:23:14 +0000

    font-andada: download from official github.com/google/fonts repository

--- Casks/font-andada.rb
+++ Casks/font-andada.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-andada' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/andada',
+  url 'https://github.com/google/fonts/trunk/ofl/andada',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
