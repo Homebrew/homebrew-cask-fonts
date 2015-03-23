ae18fa51989c06db4d695134def839f2353aa698
ndr <ndr@qef.io>
2015-03-23 12:23:14 +0000

    font-alef: download from official github.com/google/fonts repository

--- Casks/font-alef.rb
+++ Casks/font-alef.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-alef' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/alef',
+  url 'https://github.com/google/fonts/trunk/ofl/alef',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
