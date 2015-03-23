b533b6abb96c5db36db5e4d6664031c4807f7c8e
ndr <ndr@qef.io>
2015-03-23 12:23:17 +0000

    font-overlock: download from official github.com/google/fonts repository

--- Casks/font-overlock.rb
+++ Casks/font-overlock.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-overlock' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/overlock',
+  url 'https://github.com/google/fonts/trunk/ofl/overlock',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
