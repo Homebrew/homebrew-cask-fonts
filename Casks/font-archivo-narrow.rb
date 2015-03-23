7afb0fa40a6cb1d09b0043274181985190788c91
ndr <ndr@qef.io>
2015-03-23 12:23:14 +0000

    font-archivo-narrow: download from official github.com/google/fonts repository

--- Casks/font-archivo-narrow.rb
+++ Casks/font-archivo-narrow.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-archivo-narrow' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/archivonarrow',
+  url 'https://github.com/google/fonts/trunk/ofl/archivonarrow',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
