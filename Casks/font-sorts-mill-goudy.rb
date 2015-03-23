eee7ef9b6d44c1efc1f80ac089cc215aa357e984
ndr <ndr@qef.io>
2015-03-23 12:23:18 +0000

    font-sorts-mill-goudy: download from official github.com/google/fonts repository

--- Casks/font-sorts-mill-goudy.rb
+++ Casks/font-sorts-mill-goudy.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-sorts-mill-goudy' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/sortsmillgoudy',
+  url 'https://github.com/google/fonts/trunk/ofl/sortsmillgoudy',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
