c6afa471f070cb447128324bdea8472f4af7fd0e
ndr <ndr@qef.io>
2015-03-23 12:23:18 +0000

    font-titillium-web: download from official github.com/google/fonts repository

--- Casks/font-titillium-web.rb
+++ Casks/font-titillium-web.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-titillium-web' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/titilliumweb',
+  url 'https://github.com/google/fonts/trunk/ofl/titilliumweb',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
