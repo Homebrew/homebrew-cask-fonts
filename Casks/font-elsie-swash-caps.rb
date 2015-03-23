0d7927ca66d8549e83ffd1789b0773b0323e897d
ndr <ndr@qef.io>
2015-03-23 12:23:15 +0000

    font-elsie-swash-caps: download from official github.com/google/fonts repository

--- Casks/font-elsie-swash-caps.rb
+++ Casks/font-elsie-swash-caps.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-elsie-swash-caps' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/elsieswashcaps',
+  url 'https://github.com/google/fonts/trunk/ofl/elsieswashcaps',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
