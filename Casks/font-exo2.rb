8aab2f1ba5e6eb03c90928274952a6242572271c
ndr <ndr@qef.io>
2015-03-23 12:23:16 +0000

    font-exo2: download from official github.com/google/fonts repository

--- Casks/font-exo2.rb
+++ Casks/font-exo2.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-exo2' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/exo2',
+  url 'https://github.com/google/fonts/trunk/apache/exo2',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
