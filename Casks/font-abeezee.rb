1741c69cac3e30ceb0e656acd930f0a8cc630efe
ndr <ndr@qef.io>
2015-03-23 12:23:14 +0000

    font-abeezee: download from official github.com/google/fonts repository

--- Casks/font-abeezee.rb
+++ Casks/font-abeezee.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-abeezee' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/abeezee',
+  url 'https://github.com/google/fonts/trunk/ofl/abeezee',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
