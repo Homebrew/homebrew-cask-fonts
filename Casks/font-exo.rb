ec14621e0ed82407d4a94c65ea6ae94761942abb
ndr <ndr@qef.io>
2015-03-23 12:23:15 +0000

    font-exo: download from official github.com/google/fonts repository

--- Casks/font-exo.rb
+++ Casks/font-exo.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-exo' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/exo',
+  url 'https://github.com/google/fonts/trunk/ofl/exo',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
