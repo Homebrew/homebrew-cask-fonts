e99a789c8f2254161df04f059b8ecff647ee63fb
ndr <ndr@qef.io>
2015-03-23 12:23:15 +0000

    font-domine: download from official github.com/google/fonts repository

--- Casks/font-domine.rb
+++ Casks/font-domine.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-domine' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/domine',
+  url 'https://github.com/google/fonts/trunk/ofl/domine',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
