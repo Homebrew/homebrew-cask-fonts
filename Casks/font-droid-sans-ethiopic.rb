bea6236de34d3cfd2efe5fbeafd46b42554d27d7
ndr <ndr@qef.io>
2015-03-23 12:23:15 +0000

    font-droid-sans-ethiopic: download from official github.com/google/fonts repository

--- Casks/font-droid-sans-ethiopic.rb
+++ Casks/font-droid-sans-ethiopic.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-droid-sans-ethiopic' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/droidsansethiopic',
+  url 'https://github.com/google/fonts/trunk/apache/droidsansethiopic',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
