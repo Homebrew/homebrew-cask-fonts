c051577fe3fdd5121005de4726c2074cb0884e2a
ndr <ndr@qef.io>
2015-03-23 12:23:17 +0000

    font-nobile: download from official github.com/google/fonts repository

--- Casks/font-nobile.rb
+++ Casks/font-nobile.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-nobile' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/nobile',
+  url 'https://github.com/google/fonts/trunk/ofl/nobile',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
