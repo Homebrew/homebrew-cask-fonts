3d52e8a10cda9e05f59e1702e24540e101419abd
ndr <ndr@qef.io>
2015-03-23 12:23:16 +0000

    font-jura: download from official github.com/google/fonts repository

--- Casks/font-jura.rb
+++ Casks/font-jura.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-jura' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/jura',
+  url 'https://github.com/google/fonts/trunk/ofl/jura',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
