a5e7a361e9ebf1781ba00114a350aacb24fd3dcb
ndr <ndr@qef.io>
2015-03-23 12:23:18 +0000

    font-yanone-kaffeesatz: download from official github.com/google/fonts repository

--- Casks/font-yanone-kaffeesatz.rb
+++ Casks/font-yanone-kaffeesatz.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-yanone-kaffeesatz' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/yanonekaffeesatz',
+  url 'https://github.com/google/fonts/trunk/ofl/fonts/yanonekaffeesatz',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
