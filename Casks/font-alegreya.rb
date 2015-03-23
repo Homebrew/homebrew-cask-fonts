7b200a9dd5bcdde8b288d70550aab1f505e9b3f0
ndr <ndr@qef.io>
2015-03-23 12:23:14 +0000

    font-alegreya: download from official github.com/google/fonts repository

--- Casks/font-alegreya.rb
+++ Casks/font-alegreya.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-alegreya' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/alegreya',
+  url 'https://github.com/google/fonts/trunk/ofl/alegreya',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
