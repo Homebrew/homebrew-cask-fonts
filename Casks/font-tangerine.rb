4ce91e8ac01742aa7129bf049ef58d20046902c4
ndr <ndr@qef.io>
2015-03-23 12:23:18 +0000

    font-tangerine: download from official github.com/google/fonts repository

--- Casks/font-tangerine.rb
+++ Casks/font-tangerine.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-tangerine' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/tangerine',
+  url 'https://github.com/google/fonts/trunk/ofl/tangerine',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
