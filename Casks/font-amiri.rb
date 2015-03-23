13e36e58e4fd95d5dc2dee6b09cc876693c1af8c
ndr <ndr@qef.io>
2015-03-23 12:23:14 +0000

    font-amiri: download from official github.com/google/fonts repository

--- Casks/font-amiri.rb
+++ Casks/font-amiri.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-amiri' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/amiri',
+  url 'https://github.com/google/fonts/trunk/ofl/amiri',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
