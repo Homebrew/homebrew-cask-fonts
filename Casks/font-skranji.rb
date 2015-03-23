0c97f9392d3971348612252757125c33db9568b8
ndr <ndr@qef.io>
2015-03-23 12:23:18 +0000

    font-skranji: download from official github.com/google/fonts repository

--- Casks/font-skranji.rb
+++ Casks/font-skranji.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-skranji' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/skranji',
+  url 'https://github.com/google/fonts/trunk/ofl/skranji',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
