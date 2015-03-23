b31b5f6aed752fa269493f2b7a385ca13e14e569
ndr <ndr@qef.io>
2015-03-23 12:23:18 +0000

    font-rufina: download from official github.com/google/fonts repository

--- Casks/font-rufina.rb
+++ Casks/font-rufina.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-rufina' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/rufina',
+  url 'https://github.com/google/fonts/trunk/ofl/rufina',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
