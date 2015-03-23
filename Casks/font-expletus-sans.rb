10f610b72c4d2120460c261cfb994f5f9df0c0ad
ndr <ndr@qef.io>
2015-03-23 12:23:16 +0000

    font-expletus-sans: download from official github.com/google/fonts repository

--- Casks/font-expletus-sans.rb
+++ Casks/font-expletus-sans.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-expletus-sans' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/w0ng/googlefontdirectory/trunk/fonts/expletussans',
+  url 'https://github.com/google/fonts/trunk/ofl/expletussans',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
