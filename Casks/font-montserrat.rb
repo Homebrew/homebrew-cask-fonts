1bcb7fd0d35e34fdc91c68f860f8785d455a4a58
ndr <ndr@qef.io>
2015-03-24 11:30:30 +0000

    font-montserrat: use upstream repository

--- Casks/font-montserrat.rb
+++ Casks/font-montserrat.rb
@@ -3,13 +3,21 @@ cask :v1 => 'font-montserrat' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/google/fonts/trunk/ofl/montserrat',
+  url 'https://github.com/JulietaUla/Montserrat/trunk/otf',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
-  homepage 'http://www.google.com/fonts/specimen/Montserrat'
+  homepage 'https://github.com/JulietaUla/Montserrat'
   license :ofl
 
-  font 'Montserrat-Bold.ttf'
-  font 'Montserrat-Regular.ttf'
+  font 'Montserrat-Black.otf'
+  font 'Montserrat-Bold.otf'
+  font 'Montserrat-Hairline.otf'
+  font 'Montserrat-Light.otf'
+  font 'Montserrat-Regular.otf'
+  font 'MontserratAlternates-Black.otf'
+  font 'MontserratAlternates-Bold.otf'
+  font 'MontserratAlternates-Hairline.otf'
+  font 'MontserratAlternates-Light.otf'
+  font 'MontserratAlternates-Regular.otf'
 end
