efbd13918d558d9d9b6713aeb19eb820d1909212
John Sandall <john.sandall@gmail.com>
2015-04-22 00:23:48 +0100

    Migrate broken font-press-start2p url to official GitHub repo

--- Casks/font-press-start2p.rb
+++ Casks/font-press-start2p.rb
@@ -2,9 +2,9 @@ cask :v1 => 'font-press-start2p' do
   version '2.14'
   sha256 '17ec7d250ff590971a6d966b4fdc5aa04d5e39a7694f4a0becb515b6a70a7228'
 
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/pressstart2p/PressStart2P-Regular.ttf'
-  homepage 'http://www.google.com/fonts/specimen/Press%20Start%202P'
-  license :oss
+  url 'https://raw.github.com/google/fonts/master/ofl/pressstart2p/PressStart2P-Regular.ttf'
+  homepage 'http://www.google.com/fonts/specimen/Press+Start+2P'
+  license :ofl
 
   font 'PressStart2P-Regular.ttf'
 end
