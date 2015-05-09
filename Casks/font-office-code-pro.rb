94356da25dc0e5eb6119df56f660f512d6dfa59b
Jeremy Seitz <jeremy@somebox.com>
2015-05-10 01:42:39 +0200

    Add office-code-pro : https://github.com/nathco/Office-Code-Pro

---
+++ Casks/font-office-code-pro.rb
@@ -0,0 +1,25 @@
+cask :v1 => 'font-office-code-pro' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/nathco/Office-Code-Pro/archive/master.zip'
+  homepage 'https://github.com/nathco/Office-Code-Pro'
+  license :ofl
+
+  font 'Office-Code-Pro-master/Fonts/OTF/OfficeCodePro-Bold.otf'
+  font 'Office-Code-Pro-master/Fonts/OTF/OfficeCodePro-BoldD.otf'
+  font 'Office-Code-Pro-master/Fonts/OTF/OfficeCodePro-Light.otf'
+  font 'Office-Code-Pro-master/Fonts/OTF/OfficeCodePro-LightD.otf'
+  font 'Office-Code-Pro-master/Fonts/OTF/OfficeCodePro-Medium.otf'
+  font 'Office-Code-Pro-master/Fonts/OTF/OfficeCodePro-MediumD.otf'
+  font 'Office-Code-Pro-master/Fonts/OTF/OfficeCodePro-Regular.otf'
+  font 'Office-Code-Pro-master/Fonts/OTF/OfficeCodePro-RegularD.otf'
+  font 'Office-Code-Pro-master/Fonts/TTF/OfficeCodePro-Bold.ttf'
+  font 'Office-Code-Pro-master/Fonts/TTF/OfficeCodePro-BoldD.ttf'
+  font 'Office-Code-Pro-master/Fonts/TTF/OfficeCodePro-Light.ttf'
+  font 'Office-Code-Pro-master/Fonts/TTF/OfficeCodePro-LightD.ttf'
+  font 'Office-Code-Pro-master/Fonts/TTF/OfficeCodePro-Medium.ttf'
+  font 'Office-Code-Pro-master/Fonts/TTF/OfficeCodePro-MediumD.ttf'
+  font 'Office-Code-Pro-master/Fonts/TTF/OfficeCodePro-Regular.ttf'
+  font 'Office-Code-Pro-master/Fonts/TTF/OfficeCodePro-RegularD.ttf'
+end
