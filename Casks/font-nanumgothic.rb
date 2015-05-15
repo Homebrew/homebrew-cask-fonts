06d66ea8e92ea119103b509da1c32797bff27d73
Rocky Madden <git@rockymadden.com>
2015-05-15 12:43:18 -0600

    Updated url for font-nanumgothic.

--- Casks/font-nanumgothic.rb
+++ Casks/font-nanumgothic.rb
@@ -3,7 +3,7 @@ cask :v1 => 'font-nanumgothic' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/google/fonts/trunk/apache/nanumgothic',
+  url 'https://github.com/google/fonts/trunk/ofl/nanumgothic',
       :using      => :svn,
       :revision   => '50',
       :trust_cert => true
