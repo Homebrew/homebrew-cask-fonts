1cbeb5239a071dd5638ccdcf49f2ae24d4d93e0a
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-05-19 13:03:21 -0700

    Add cask for Fira Code v0.3

---
+++ font-fira-code.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'font-fira-code' do
+  version '0.3'
+  sha256 'a90721b6b1de4640ace744657432a990aae4c8e403b78141a50d0a7abd305db7'
+
+  url 'https://github.com/tonsky/FiraCode/releases/download/0.3/FiraCode-Regular.otf'
+  homepage 'https://github.com/tonsky/FiraCode'
+  license :ofl
+
+  font 'FiraCode-Regular.otf'
+end
