f612fb1229026707c94a2628fa34a42206fc114b
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-05-21 14:52:37 +0800

    Update font-fontawesome sha256.

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'font-fontawesome' do
   version '4.3.0'
-  sha256 '8c3a524adc9a57b4d545fb3565d94cdb956dc13306ff94ba1c3fee6fffc7a60e'
+  sha256 '514285209133c87a1dfd0063cca7fe411b4b37ca6e2b96fa314f730c7350f203'
 
   url "https://github.com/FortAwesome/Font-Awesome/archive/v#{version}.zip"
   homepage 'http://fortawesome.github.io/Font-Awesome/'
