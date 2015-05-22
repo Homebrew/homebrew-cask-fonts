f4c13a863f06302b7e67040e23b5dd50ff00c4da
Kunio Murasawa <kunio.murasawa@gmail.com>
2015-05-22 23:27:25 +0900

    Update font-ricty-diminished 3.2.4 sha256

--- Casks/font-ricty-diminished.rb
+++ Casks/font-ricty-diminished.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'font-ricty-diminished' do
   version '3.2.4'
-  sha256 'd8ae378f6333080da83ba1ab3a7cb5f3ed0982a2e633de651b9510cb7dcd3bfc'
+  sha256 '028f2b40faed8417fcc80a7b51592e9115a2c5af901aced833032df2983fdcf4'
 
   url 'https://github.com/yascentur/RictyDiminished/archive/3.2.4.zip'
   homepage 'https://github.com/yascentur/RictyDiminished'
