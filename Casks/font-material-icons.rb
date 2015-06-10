56c9e556fabf16b3310d07450519a91e4bd1edb2
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-06-10 09:24:23 +0800

    font-material-icons update version number.

--- Casks/font-material-icons.rb
+++ Casks/font-material-icons.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'font-material-icons' do
-  version '2.0'
+  version '2.0.0'
   sha256 '2e75ad776d1d9251215c14f887d55c1f7649561eed65928a42aeacdccc62322d'
 
   url "https://github.com/google/material-design-icons/releases/download/#{version}/material-design-icons-#{version}.zip"
