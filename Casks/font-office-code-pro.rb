33b6630128cbf14ce623d7006149ca2c2b5b5841
Beng Hee Eu <beng@benghee.eu>
2015-07-02 20:43:33 -0400

    Office Code Pro 1.004

--- Casks/font-office-code-pro.rb
+++ Casks/font-office-code-pro.rb
@@ -1,17 +1,25 @@
 cask :v1 => 'font-office-code-pro' do
-  version :latest
-  sha256 :no_check
+  version '1.004'
+  sha256 '9e24d15309ead8c523ec3f0444ed9c171bba535e109c43b1dde8abfa9d359150'
 
-  url 'https://github.com/nathco/Office-Code-Pro/archive/master.zip'
+  url 'https://github.com/nathco/Office-Code-Pro/archive/1.004.zip'
   homepage 'https://github.com/nathco/Office-Code-Pro'
   license :ofl
 
-  font 'Office-Code-Pro-master/Fonts/Office Code Pro/OTF/OfficeCodePro-Bold.otf'
-  font 'Office-Code-Pro-master/Fonts/Office Code Pro/OTF/OfficeCodePro-BoldD.otf'
-  font 'Office-Code-Pro-master/Fonts/Office Code Pro/OTF/OfficeCodePro-Light.otf'
-  font 'Office-Code-Pro-master/Fonts/Office Code Pro/OTF/OfficeCodePro-LightD.otf'
-  font 'Office-Code-Pro-master/Fonts/Office Code Pro/OTF/OfficeCodePro-Medium.otf'
-  font 'Office-Code-Pro-master/Fonts/Office Code Pro/OTF/OfficeCodePro-MediumD.otf'
-  font 'Office-Code-Pro-master/Fonts/Office Code Pro/OTF/OfficeCodePro-Regular.otf'
-  font 'Office-Code-Pro-master/Fonts/Office Code Pro/OTF/OfficeCodePro-RegularD.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro D/OTF/OfficeCodeProD-Bold.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro D/OTF/OfficeCodeProD-BoldItalic.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro D/OTF/OfficeCodeProD-Light.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro D/OTF/OfficeCodeProD-LightItalic.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro D/OTF/OfficeCodeProD-Medium.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro D/OTF/OfficeCodeProD-MediumItalic.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro D/OTF/OfficeCodeProD-Regular.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro D/OTF/OfficeCodeProD-RegularItalic.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro/OTF/OfficeCodePro-Bold.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro/OTF/OfficeCodePro-BoldItalic.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro/OTF/OfficeCodePro-Light.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro/OTF/OfficeCodePro-LightItalic.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro/OTF/OfficeCodePro-Medium.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro/OTF/OfficeCodePro-MediumItalic.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro/OTF/OfficeCodePro-Regular.otf'
+  font 'Office-Code-Pro-1.004/Fonts/Office Code Pro/OTF/OfficeCodePro-RegularItalic.otf'
 end
