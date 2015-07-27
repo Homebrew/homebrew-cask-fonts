9bd38499165870ae040cfa6a7ac4f9442efe2d78
Scott Humphries <scott@sscotth.io>
2015-07-27 12:09:55 -0500

    Add caveat and official download mention

--- Casks/font-monoid.rb
+++ Casks/font-monoid.rb
@@ -2,6 +2,7 @@ cask :v1 => 'font-monoid' do
   version :latest
   sha256 :no_check
 
+  # github.com is the official download host per the vendor homepage
   url 'https://github.com/larsenwork/monoid/blob/release/Monoid.zip?raw=true'
   homepage 'http://larsenwork.com/monoid/'
   license :ofl
@@ -10,4 +11,10 @@ cask :v1 => 'font-monoid' do
   font 'Monoid-Oblique.ttf'
   font 'Monoid-Regular.ttf'
   font 'Monoid-Retina.ttf'
+
+  caveats <<-EOS.undent
+  #{token} only installs the Normal Weight, Medium LineHeight, with Ligatures variant.
+  To get other styles, please tap the larsenwork/monoid repo
+    brew tap larsenwork/monoid
+  EOS
 end
