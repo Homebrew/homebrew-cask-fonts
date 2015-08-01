3e4ee21e8cc1499fdb47a8fc90e2321f5f9b033e
Scott Humphries <scott@sscotth.io>
2015-07-31 19:16:57 -0500

    Add monoisome font

---
+++ Casks/font-monoisome.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'font-monoisome' do
+  version :latest
+  sha256 :no_check
+
+  # github.com is the official download host per the vendor homepage
+  url 'https://github.com/larsenwork/monoid/blob/master/Monoisome/Monoisome-Regular.ttf?raw=true'
+  homepage 'http://larsenwork.com/monoid/'
+  license :ofl
+
+  font 'Monoisome-Regular.ttf'
+end
