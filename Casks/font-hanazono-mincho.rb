ef7d9678d4d9bd0e0215eab43325f5fd5cb83f6a
mashabow <mashabow@shiromoji.net>
2015-08-06 00:52:57 +0900

    Add font-hanazono-mincho

---
+++ Casks/font-hanazono-mincho.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'font-hanazono-mincho' do
+  version '2014-10-12'
+  sha256 '06a7285dcc5c8a22aa3a1b319f965d9d336b84af61fabd5d9d5ba6fa2d861208'
+
+  url 'http://dl.osdn.jp/hanazono-font/62072/hanazono-20141012.zip'
+  homepage 'http://fonts.jp/hanazono/'
+  license :oss
+
+  font 'HanaMinA.ttf'
+  font 'HanaMinB.ttf'
+end
