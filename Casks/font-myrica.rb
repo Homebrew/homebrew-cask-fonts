7feb9c5c17b942ff64a3844aabd643e5e3163e5c
ryunix <ryunix.net@gmail.com>
2015-08-11 07:16:32 +0900

    Update font-myrica.rb

--- Casks/font-myrica.rb
+++ Casks/font-myrica.rb
@@ -2,12 +2,9 @@ cask :v1 => 'font-myrica' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/tomokuni/Myrica/archive/master.zip'
-  homepage 'https://github.com/tomokuni/Myrica'
+  url 'https://github.com/tomokuni/Myrica/raw/master/product/Myrica.zip'
+  homepage 'http://myrica.estable.jp/'
   license :oss
 
-  font 'Myrica-master/Work/MyricaM.ttf'
-  font 'Myrica-master/Work/MyricaM_NoHint.ttf'
-  font 'Myrica-master/Work/MyricaN.ttf'
-  font 'Myrica-master/Work/MyricaP.ttf'
+  font 'Myrica.TTC'
 end
