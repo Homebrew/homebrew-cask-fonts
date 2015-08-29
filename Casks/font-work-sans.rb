42aee3ad2bb05209cf50793a9e326c38b682b373
Andrew Sardone <andrew@andrewsardone.com>
2015-08-29 14:24:28 -0400

    Add font-work-sans
    - GitHub repo: https://github.com/weiweihuanghuang/Work-Sans
    - Homepage: http://weiweihuanghuang.github.io/Work-Sans/

---
+++ Casks/font-work-sans.rb
@@ -0,0 +1,19 @@
+cask :v1 => 'font-work-sans' do
+  version '1.32'
+  sha256 'fae62ccbed372d1e1834fbc12644f2b8db6e5e6dee8612fd17191967ac68ef9b'
+
+  url "https://github.com/weiweihuanghuang/Work-Sans/archive/v#{version}.zip"
+  homepage 'http://weiweihuanghuang.github.io/Work-Sans/'
+  license :ofl
+
+  font "Work-Sans-#{version}/fonts/desktop/WorkSans-Black.otf"
+  font "Work-Sans-#{version}/fonts/desktop/WorkSans-Bold.otf"
+  font "Work-Sans-#{version}/fonts/desktop/WorkSans-ExtraBold.otf"
+  font "Work-Sans-#{version}/fonts/desktop/WorkSans-ExtraLight.otf"
+  font "Work-Sans-#{version}/fonts/desktop/WorkSans-Hairline.otf"
+  font "Work-Sans-#{version}/fonts/desktop/WorkSans-Light.otf"
+  font "Work-Sans-#{version}/fonts/desktop/WorkSans-Medium.otf"
+  font "Work-Sans-#{version}/fonts/desktop/WorkSans-Regular.otf"
+  font "Work-Sans-#{version}/fonts/desktop/WorkSans-SemiBold.otf"
+  font "Work-Sans-#{version}/fonts/desktop/WorkSans-Thin.otf"
+end
