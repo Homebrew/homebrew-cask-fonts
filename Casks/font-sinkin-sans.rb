7816a8c6d6737b3aec60e546906ca6934dc42127
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-09-08 07:36:07 +0800

    Add font-sinkin-sans
    http://www.fontsquirrel.com/fonts/sinkin-sans

---
+++ Casks/font-sinkin-sans.rb
@@ -0,0 +1,27 @@
+cask :v1 => 'font-sinkin-sans' do
+  version '1.000'
+  sha256 '65a681a573b6af4d173466a14a9b39c93eb45f6878e1c1132b861b12f6166135'
+
+  url 'http://www.fontsquirrel.com/fonts/download/sinkin-sans'
+  homepage 'http://www.fontsquirrel.com/fonts/sinkin-sans'
+  license :apache
+
+  font 'SinkinSans-100Thin.otf'
+  font 'SinkinSans-100ThinItalic.otf'
+  font 'SinkinSans-200XLight.otf'
+  font 'SinkinSans-200XLightItalic.otf'
+  font 'SinkinSans-300Light.otf'
+  font 'SinkinSans-300LightItalic.otf'
+  font 'SinkinSans-400Regular.otf'
+  font 'SinkinSans-400Italic.otf'
+  font 'SinkinSans-500Medium.otf'
+  font 'SinkinSans-500MediumItalic.otf'
+  font 'SinkinSans-600SemiBold.otf'
+  font 'SinkinSans-600SemiBoldItali.otf'
+  font 'SinkinSans-700Bold.otf'
+  font 'SinkinSans-700BoldItalic.otf'
+  font 'SinkinSans-800Black.otf'
+  font 'SinkinSans-800BlackItalic.otf'
+  font 'SinkinSans-900XBlack.otf'
+  font 'SinkinSans-900XBlackItalic.otf'
+end
