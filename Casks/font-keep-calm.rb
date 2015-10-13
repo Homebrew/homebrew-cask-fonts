1874c9db80793259c3d0fdf273c4e6123d1bd76e
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-10-13 16:04:36 +0800

    Add font-keep-calm

---
+++ Casks/font-keep-calm.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'font-keep-calm' do
+  version :latest
+  sha256 '11a6079a98b53a67cd683d2b089014d385a3ea04a888d9d27408e73578a383d7'
+
+  url 'http://dl.1001fonts.com/keep-calm.zip'
+  homepage 'http://www.1001fonts.com/keep-calm-font.html'
+  license :commercial # free for personal use
+
+  font 'Keep Calm Medium – Personal Use/KeepCalm-Medium.ttf'
+end
