9712578c45a49eb039525d9ea5226edf6c02e31d
George Lifchits <george.lifchits@gmail.com>
2015-11-01 22:48:55 -0500

    update sha256 for font-fontawesome

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'font-fontawesome' do
   version '4.4.0'
-  sha256 '715c97f01478a4853532b371547252d59f6ad293f97b22b1cd585c76c371eecc'
+  sha256 'b47ed209dabfedc58503b8137d8ee65639e5c1bbee77a9e8e03f717ab19ea189'
 
   url "https://github.com/FortAwesome/Font-Awesome/archive/v#{version}.zip"
   homepage 'http://fortawesome.github.io/Font-Awesome/'
