6fbbd0ad87ce56b9da80bf85146824eadbb1d3ea
Alex Semyonov <al@semyonov.us>
2015-11-27 12:55:56 +0300

    Update font-fontawesome to 4.5.0

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'font-fontawesome' do
-  version '4.4.0'
-  sha256 'b47ed209dabfedc58503b8137d8ee65639e5c1bbee77a9e8e03f717ab19ea189'
+  version '4.5.0'
+  sha256 '59cbc93fcc73db823b2176c62b6a8957c014e2ba78cbedd2f368182d95bb9d22'
 
   url "https://github.com/FortAwesome/Font-Awesome/archive/v#{version}.zip"
   homepage 'http://fortawesome.github.io/Font-Awesome/'
