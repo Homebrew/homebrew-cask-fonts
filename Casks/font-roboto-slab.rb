537d8b941e00f530d8f2fa980a8b8813cd941a87
Jonathan Rouleau <jr@rlabs.io>
2015-11-28 16:35:29 -0400

    font-roboto-slab: download from official github.com/google/fonts repository

--- Casks/font-roboto-slab.rb
+++ Casks/font-roboto-slab.rb
@@ -1,11 +1,9 @@
 cask :v1 => 'font-roboto-slab' do
-  # version '1.100263'
   version :latest
   sha256 :no_check
 
   url 'https://github.com/google/fonts/trunk/apache/robotoslab',
       :using      => :svn,
-      :revision   => '50',
       :trust_cert => true
   homepage 'http://www.google.com/fonts/specimen/Roboto%20Slab'
   license :apache
