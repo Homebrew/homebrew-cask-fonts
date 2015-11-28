e7684e7f8904e1f241c99fbc0c0ec7fd8e19d8ee
Jonathan Rouleau <jr@rlabs.io>
2015-11-28 16:35:15 -0400

    font-roboto-mono: download from official github.com/google/fonts repository

--- Casks/font-roboto-mono.rb
+++ Casks/font-roboto-mono.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'font-roboto-mono' do
-  # version '2.000985'
   version :latest
   sha256 :no_check
-  
-  url 'https://www.google.com/fonts/download?kit=rqQ1zSE-ZGCKVZgew-A9dofD-WQWLbF4rYwcBGowFYY'
-  homepage 'https://www.google.com/fonts/specimen/Roboto+Mono'
+
+  url 'https://github.com/google/fonts/trunk/apache/robotomono',
+      :using      => :svn,
+      :trust_cert => true
+  homepage 'http://www.google.com/fonts/specimen/Roboto%20Mono'
   license :apache
 
   font 'RobotoMono-Bold.ttf'
