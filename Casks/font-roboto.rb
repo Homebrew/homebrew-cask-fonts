f08c6646e6bc9ae47664328762bd90be31045a5d
Jonathan Rouleau <jr@rlabs.io>
2015-11-28 16:34:32 -0400

    font-roboto: download from official github.com/google/fonts repository

--- Casks/font-roboto.rb
+++ Casks/font-roboto.rb
@@ -1,27 +1,23 @@
 cask :v1 => 'font-roboto' do
-  version '1.200311'
-  sha256 '5101b5bdb3b7b14cc922a771ba068bfcae8634467c1f446bc89bc2b4f8b4f1b5'
+  version :latest
+  sha256 :no_check
 
-  url 'http://developer.android.com/downloads/design/roboto-1.2.zip'
-  homepage 'http://developer.android.com/design/style/typography.html'
+  url 'https://github.com/google/fonts/trunk/apache/roboto',
+      :using      => :svn,
+      :trust_cert => true
+  homepage 'http://www.google.com/fonts/specimen/Roboto'
   license :apache
 
-  font 'Roboto_v1.2/Roboto/Roboto-Black.ttf'
-  font 'Roboto_v1.2/Roboto/Roboto-BlackItalic.ttf'
-  font 'Roboto_v1.2/Roboto/Roboto-Bold.ttf'
-  font 'Roboto_v1.2/Roboto/Roboto-BoldItalic.ttf'
-  font 'Roboto_v1.2/Roboto/Roboto-Italic.ttf'
-  font 'Roboto_v1.2/Roboto/Roboto-Light.ttf'
-  font 'Roboto_v1.2/Roboto/Roboto-LightItalic.ttf'
-  font 'Roboto_v1.2/Roboto/Roboto-Medium.ttf'
-  font 'Roboto_v1.2/Roboto/Roboto-MediumItalic.ttf'
-  font 'Roboto_v1.2/Roboto/Roboto-Regular.ttf'
-  font 'Roboto_v1.2/Roboto/Roboto-Thin.ttf'
-  font 'Roboto_v1.2/Roboto/Roboto-ThinItalic.ttf'
-  font 'Roboto_v1.2/RobotoCondensed/RobotoCondensed-Bold.ttf'
-  font 'Roboto_v1.2/RobotoCondensed/RobotoCondensed-BoldItalic.ttf'
-  font 'Roboto_v1.2/RobotoCondensed/RobotoCondensed-Italic.ttf'
-  font 'Roboto_v1.2/RobotoCondensed/RobotoCondensed-Light.ttf'
-  font 'Roboto_v1.2/RobotoCondensed/RobotoCondensed-LightItalic.ttf'
-  font 'Roboto_v1.2/RobotoCondensed/RobotoCondensed-Regular.ttf'
+  font 'Roboto-Black.ttf'
+  font 'Roboto-BlackItalic.ttf'
+  font 'Roboto-Bold.ttf'
+  font 'Roboto-BoldItalic.ttf'
+  font 'Roboto-Italic.ttf'
+  font 'Roboto-Light.ttf'
+  font 'Roboto-LightItalic.ttf'
+  font 'Roboto-Medium.ttf'
+  font 'Roboto-MediumItalic.ttf'
+  font 'Roboto-Regular.ttf'
+  font 'Roboto-Thin.ttf'
+  font 'Roboto-ThinItalic.ttf'
 end
