7e8a3cd2dc580882339ed29e11b453e702250dc9
Jonathan Rouleau <jr@rlabs.io>
2015-11-28 16:34:45 -0400

    font-roboto-condensed: download from official github.com/google/fonts repository

---
+++ Casks/font-roboto-condensed.rb
@@ -0,0 +1,17 @@
+cask :v1 => 'font-roboto-condensed' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/google/fonts/trunk/apache/robotocondensed',
+      :using      => :svn,
+      :trust_cert => true
+  homepage 'http://www.google.com/fonts/specimen/Roboto%20Condensed'
+  license :apache
+
+  font 'RobotoCondensed-Bold.ttf'
+  font 'RobotoCondensed-BoldItalic.ttf'
+  font 'RobotoCondensed-Italic.ttf'
+  font 'RobotoCondensed-Light.ttf'
+  font 'RobotoCondensed-LightItalic.ttf'
+  font 'RobotoCondensed-Regular.ttf'
+end
