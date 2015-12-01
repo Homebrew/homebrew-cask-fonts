0f36335b1654225a7f3efff5c407f6739712a41f
chuchana <chuchana@gmx.de>
2015-12-01 23:56:10 +0100

    Added iosevka
    http://be5invis.github.io/Iosevka/

---
+++ Casks/font-iosevka.rb
@@ -0,0 +1,15 @@
+cask :v1 => 'font-iosevka' do
+  version '1.3.0'
+  sha256 '7e80d71207449d9316650424362441aea6c99367f6915d7f55ced3e7d7172755'
+
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/iosevka-#{version}.zip"
+  appcast 'https://github.com/be5invis/Iosevka/releases.atom'
+  name 'Iosevka'
+  homepage 'https://github.com/be5invis/Iosevka/'
+  license :ofl
+
+  font 'iosevka-bolditalic.ttf'
+  font 'iosevka-regular.ttf'
+  font 'iosevka-bold.ttf'
+  font 'iosevka-italic.ttf'
+end
