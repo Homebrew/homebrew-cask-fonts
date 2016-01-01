1be5ad1f1d8a5462a6eb26d827b23a5ef4b8c2e1
Grant Rettke <gcr@wisdomandwonder.com>
2016-01-01 13:44:47 -0600

    Restored font-symbola.rb from bcde049b0993c74861f67e29bc83db792e0fcf13
    The font is again available on the author's home page.
    
    Updated version number to 8.00.
    
    Verified download link.
    
    Verified home page link.
    
    License unchanged.
    
    No versioned download.
    
    Correction of cask definition.
    
    Via vitorgalvao the cask definition was incorrect.
    
    Corrected.

---
+++ Casks/font-symbola.rb
@@ -0,0 +1,11 @@
+cask 'font-symbola' do
+  # version '8.00'
+  version :latest
+  sha256 :no_check
+
+  url 'http://users.teilar.gr/~g1951d/Symbola.zip'
+  homepage 'http://users.teilar.gr/~g1951d/'
+  license :gratis
+
+  font 'Symbola_hint.ttf'
+end
