13ff3adfbb90d26db811d8ca9074407e8e124916
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-01-10 10:48:15 +0800

    Create font-baron.rb

---
+++ Casks/font-baron.rb
@@ -0,0 +1,15 @@
+cask 'font-baron' do
+  version '1.00'
+  sha256 '70a7ade63a79e3c90227fff890664976a50f40bd84ef4d7f97871eb8532b880c'
+
+  url 'http://www.fontfabric.com/downfont/baron.zip'
+  homepage 'http://www.fontfabric.com/baron-free-font/'
+  license :unknown
+
+  font 'Baron Neue Black Italic.otf'
+  font 'Baron Neue Black.otf'
+  font 'Baron Neue Bold Italic.otf'
+  font 'Baron Neue Bold.otf'
+  font 'Baron Neue Italic.otf'
+  font 'Baron Neue.otf'
+end
