e0c07738cb2aa61d7a4d6af5418f1829b32dffd9
Parker Moore <parkrmoore@gmail.com>
2016-01-11 13:32:59 -0800

    Add bsd license to font-consolas-for-powerline
    Per https://github.com/eugeii/consolas-powerline-vim/issues/4#issuecomment-170414668

--- Casks/font-consolas-for-powerline.rb
+++ Casks/font-consolas-for-powerline.rb
@@ -4,6 +4,7 @@ cask 'font-consolas-for-powerline' do
 
   url 'https://github.com/eugeii/consolas-powerline-vim/archive/master.zip'
   homepage 'https://github.com/eugeii/consolas-powerline-vim'
+  license :bsd
 
   font 'consolas-powerline-vim-master/CONSOLA-Powerline.ttf'
   font 'consolas-powerline-vim-master/CONSOLAB-Powerline.ttf'
