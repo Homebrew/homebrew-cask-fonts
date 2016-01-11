243805558bcfb2309c15d5ba72e1e2d10c3606b8
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-01-12 07:24:48 +0800

    font-jaapokki: Interpolate URL based on version

--- Casks/font-jaapokki.rb
+++ Casks/font-jaapokki.rb
@@ -1,12 +1,12 @@
 cask 'font-jaapokki' do
-  version '1.00'
+  version '1.0'
   sha256 'aa5f38cbb682df8732d406996e37349b83e47d3c14bae4af2125f2a7784430b7'
 
-  url 'http://mikkonuuttila.com/jaapokki/Jaapokki-font-package-1.0.zip'
+  url "http://mikkonuuttila.com/jaapokki/Jaapokki-font-package-#{version}.zip"
   homepage 'http://mikkonuuttila.com/jaapokki/'
   license :unknown
 
-  font 'Jaapokki-font-package-1.0/Regular/Jaapokki-Regular.otf'
-  font 'Jaapokki-font-package-1.0/enchance/Jaapokkienchance-Regular.otf'
-  font 'Jaapokki-font-package-1.0/subtract/Jaapokkisubtract-Regular.otf'
+  font "Jaapokki-font-package-#{version}/Regular/Jaapokki-Regular.otf"
+  font "Jaapokki-font-package-#{version}/enchance/Jaapokkienchance-Regular.otf"
+  font "Jaapokki-font-package-#{version}/subtract/Jaapokkisubtract-Regular.otf"
 end
