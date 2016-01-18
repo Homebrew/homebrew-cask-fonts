04c02daef4317821d2afe59da7437bda24f572ab
Kazuki Suda <kazuki.suda@gmail.com>
2016-01-18 11:35:05 +0900

    Update font-devicons to 1.8.0
    https://github.com/vorillaz/devicons/releases/tag/1.8.0

--- Casks/font-devicons.rb
+++ Casks/font-devicons.rb
@@ -1,6 +1,6 @@
 cask 'font-devicons' do
-  version '1.4.0'
-  sha256 '3826ed7973fc97508f6abd4bf7da1f164f7be6a883113b799ec2c04b0a388fa2'
+  version '1.8.0'
+  sha256 'fc0baa260f54832c059d1d9eab3798ae758d1a1cf0c1695e9883aab85d9a4308'
 
   url "https://github.com/vorillaz/devicons/archive/#{version}.zip"
   homepage 'http://vorillaz.github.io/devicons/'
