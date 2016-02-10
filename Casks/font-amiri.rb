76f2e4cf85078fc307c69ef6fa8c7cb4ded68a29
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 18:56:33 +0000

    updated font-amiri (0.108)

--- Casks/font-amiri.rb
+++ Casks/font-amiri.rb
@@ -1,16 +1,17 @@
 cask 'font-amiri' do
-  version '0.107'
-  sha256 '7e81fa2844ded0d7839dc8eedcc4f7ece3688b9cd9ac9b3fd806340cdcef5463'
+  version '0.108'
+  sha256 '1b33e4106b61ca8b9743e9c2b75230c637386b331c79cb6dd2bcddf50738f8f5'
 
-  # github.com is the official download host per the vendor homepage
-  url 'https://github.com/khaledhosny/amiri-font/releases/download/0.107/amiri-0.107.zip'
+  # github.com/khaledhosny/amiri-font was verified as official when first introduced to the cask
+  url "https://github.com/khaledhosny/amiri-font/releases/download/#{version}/amiri-#{version}.zip"
   appcast 'https://github.com/khaledhosny/amiri-font/releases.atom',
           checkpoint: 'b04d8b1e4ee31bdda5987d1c6f7f4bba332a27b971966d455da32d9cc1f20dfb'
+  name 'Amiri'
   homepage 'http://www.amirifont.org/'
   license :ofl
 
-  font 'amiri-0.107/amiri-bold.ttf'
-  font 'amiri-0.107/amiri-boldslanted.ttf'
-  font 'amiri-0.107/amiri-quran.ttf'
-  font 'amiri-0.107/amiri-regular.ttf'
+  font "amiri-#{version}/amiri-bold.ttf"
+  font "amiri-#{version}/amiri-boldslanted.ttf"
+  font "amiri-#{version}/amiri-quran.ttf"
+  font "amiri-#{version}/amiri-regular.ttf"
 end
