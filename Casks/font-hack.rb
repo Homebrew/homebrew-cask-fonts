081a806ae55acce2c48eece07e8140f401013ffc
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 19:01:42 +0000

    updated font-hack (2.019)

--- Casks/font-hack.rb
+++ Casks/font-hack.rb
@@ -2,7 +2,7 @@ cask 'font-hack' do
   version '2.019'
   sha256 '9e847ad7afb327ca089c4b09d1368091ca0c98e64a2fe284755162e6650bfdd2'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/chrissimpkins/Hack was verified as official when first introduced to the cask
   url "https://github.com/chrissimpkins/Hack/archive/v#{version}.zip"
   appcast 'https://github.com/chrissimpkins/Hack/releases.atom',
           checkpoint: 'a674ef1ff633112e83b25ac3d4f260f846c61c26d5c981c228e49ad1e9706369'
