8bc0ac5e1de772e33da966e04c73f09aa70dc0af
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 19:21:31 +0000

    updated font-material-icons (2.2.0)

--- Casks/font-material-icons.rb
+++ Casks/font-material-icons.rb
@@ -1,8 +1,12 @@
 cask 'font-material-icons' do
-  version '2.0.0'
-  sha256 '2e75ad776d1d9251215c14f887d55c1f7649561eed65928a42aeacdccc62322d'
+  version '2.2.0'
+  sha256 'a5b96fc7e41592d20bd14c2bad5f7228cf4e64a4de6837706bd6111829183132'
 
-  url "https://github.com/google/material-design-icons/releases/download/#{version}/material-design-icons-#{version}.zip"
+  # github.com/google/material-design-icons was verified as official when first introduced to the cask
+  url "https://github.com/google/material-design-icons/archive/#{version}.zip"
+  appcast 'https://github.com/google/material-design-icons/releases.atom',
+          checkpoint: 'd080da6f663b829a027fde7390eedc3d21d94720eaac172c130b09e6a28fcc10'
+  name 'Material Icons'
   homepage 'http://google.github.io/material-design-icons/'
   license :oss
 
