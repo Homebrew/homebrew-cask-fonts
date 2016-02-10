e29327fcfcadac13d01b222ae2d8d10c75a8d57e
Steve Dignam <steve@dignam.xyz>
2016-02-10 16:57:02 -0500

    updated font-nova-round to use github url

--- Casks/font-nova-round.rb
+++ Casks/font-nova-round.rb
@@ -1,8 +1,9 @@
 cask 'font-nova-round' do
-  version '2.000'
-  sha256 '9f427a08e6367f847d4b9452ab56252d99436b6a567206c1734d5cb4de895d95'
+  version :latest
+  sha256 :no_check
 
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/novaround/NovaRound.ttf'
+  url 'https://github.com/google/fonts/raw/master/ofl/novaround/NovaRound.ttf'
+  name 'Nova Round'
   homepage 'http://www.google.com/fonts/specimen/Nova%20Round'
   license :ofl
 
