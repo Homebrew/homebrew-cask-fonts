8bbc6c67a171053632df8d8a993ac31e52cfb133
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 19:06:09 +0000

    updated font-monoid (0.61)

--- Casks/font-monoid.rb
+++ Casks/font-monoid.rb
@@ -1,9 +1,12 @@
 cask 'font-monoid' do
-  version :latest
-  sha256 :no_check
+  version '0.61'
+  sha256 'f779e66609cb56bf13cd92500c72ed6a3e9dc05c6a9db81611421d1ff123600c'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/larsenwork/monoid was verified as official when first introduced to the cask
   url 'https://github.com/larsenwork/monoid/blob/release/Monoid.zip?raw=true'
+  appcast 'https://github.com/larsenwork/monoid/blob/releases.atom',
+          checkpoint: 'e94cd1ccc01488e99b07a372162ea9e555b6aef0e0911cbb198281f03b975467'
+  name 'Monoid'
   homepage 'http://larsenwork.com/monoid/'
   license :ofl
 
