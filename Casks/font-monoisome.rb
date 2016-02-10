d2e163785b7aab280d4fdeaec6b150feb3556446
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 19:08:25 +0000

    updated font-monoisome (0.61)

--- Casks/font-monoisome.rb
+++ Casks/font-monoisome.rb
@@ -1,9 +1,12 @@
 cask 'font-monoisome' do
-  version :latest
-  sha256 :no_check
+  version '0.61'
+  sha256 '0aedac5663ea33f5a7d5c802408ff0ee16cf62b680f9e05980ccd2ba40040930'
 
-  # github.com is the official download host per the vendor homepage
+  # github.com/larsenwork/monoid was verified as official when first introduced to the cask
   url 'https://github.com/larsenwork/monoid/blob/master/Monoisome/Monoisome-Regular.ttf?raw=true'
+  appcast 'https://github.com/larsenwork/monoid/blob/releases.atom',
+          checkpoint: 'e94cd1ccc01488e99b07a372162ea9e555b6aef0e0911cbb198281f03b975467'
+  name 'Monoisome'
   homepage 'http://larsenwork.com/monoid/'
   license :ofl
 
