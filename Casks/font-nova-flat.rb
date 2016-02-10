8d419541b9309fdb373ac9d8e533a58fa2cc1fbd
Steve Dignam <steve@dignam.xyz>
2016-02-10 16:40:53 -0500

    updated Nova Flat to use github

--- Casks/font-nova-flat.rb
+++ Casks/font-nova-flat.rb
@@ -1,8 +1,9 @@
 cask 'font-nova-flat' do
-  version '2.000'
-  sha256 'de302932cdca6d274f7f791b9805d6e0f84ec44ae6ed630f1b1e8c616fc04c42'
+  version :latest
+  sha256 :no_check
 
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/novaflat/NovaFlat.ttf'
+  url 'https://github.com/google/fonts/raw/master/ofl/novaflat/NovaFlat.ttf'
+  name 'Nova Flat'
   homepage 'http://www.google.com/fonts/specimen/Nova%20Flat'
   license :ofl
 
