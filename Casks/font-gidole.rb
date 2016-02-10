3de3be422c0a78654952d716a4b2ea268361d379
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 19:00:03 +0000

    updated font-gidole (latest)

--- Casks/font-gidole.rb
+++ Casks/font-gidole.rb
@@ -2,8 +2,11 @@ cask 'font-gidole' do
   version :latest
   sha256 :no_check
 
-  # github.com is the official download host per the vendor homepage
-  url 'https://github.com/andreaslarsen/Gidole/blob/master/gidole.zip?raw=true'
+  # github.com/gidole was verified as official when first introduced to the cask
+  url 'https://github.com/gidole/sans/blob/master/gidole.zip?raw=true'
+  appcast 'https://github.com/larsenwork/Gidole/releases.atom',
+          checkpoint: '1df38a5178b492b9843b8bc31393e1bd7cb6f4b71e682a6144dea19d244aa9f0'
+  name 'Gidole'
   homepage 'https://gidole.github.io'
   license :ofl
 
