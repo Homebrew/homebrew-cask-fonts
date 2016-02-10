3a84bb2705cbcf26d65ee4c43ebdf8e397e68abf
Steve Dignam <steve@dignam.xyz>
2016-02-10 16:49:57 -0500

    updated font-nova-oval to use github url

--- Casks/font-nova-oval.rb
+++ Casks/font-nova-oval.rb
@@ -1,8 +1,9 @@
 cask 'font-nova-oval' do
-  version '2.000'
-  sha256 '2ae90689c17afdcb69fd45325fc65cd98ca8d537a63ed2c33eed9827ac6a16f6'
+  version :latest
+  sha256 :no_check
 
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/novaoval/NovaOval.ttf'
+  url 'https://github.com/google/fonts/raw/master/ofl/novaoval/NovaOval.ttf'
+  name 'Nova Oval'
   homepage 'http://www.google.com/fonts/specimen/Nova%20Oval'
   license :ofl
 
