847c1ad8d4a02d57c96155f4ce8014de15f1b1e7
Steve Dignam <steve@dignam.xyz>
2016-02-10 17:38:13 -0500

    added font-red-october latest version

---
+++ Casks/font-red-october.rb
@@ -0,0 +1,12 @@
+cask 'font-red-october' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://dl.dafont.com/dl/?f=red_october'
+  name 'Red October'
+  homepage 'http://www.dafont.com/red-october.font'
+  license :unknown
+
+  font 'Red October-Fat.ttf'
+  font 'Red October-Regular.ttf'
+end
