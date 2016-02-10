9b5bbf15b5883724e5d5931129fab4a60c61f281
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 19:24:38 +0000

    updated font-source-code-pro (2.010R-ro-1.030R-it)

--- Casks/font-source-code-pro.rb
+++ Casks/font-source-code-pro.rb
@@ -2,7 +2,11 @@ cask 'font-source-code-pro' do
   version '2.010R-ro-1.030R-it'
   sha256 '3bed70c2eac576d85673204919ae4b16fadf4a70c65f80360e0dc0eb820ff4a4'
 
+  # github.com/adobe-fonts/source-code-pro was verified as official when first introduced to the cask
   url "https://github.com/adobe-fonts/source-code-pro/archive/#{version.sub('ro-', 'ro/')}.zip"
+  appcast 'https://github.com/adobe-fonts/source-code-pro/releases.atom',
+          checkpoint: 'c0ca7b9cf1badb064e72ced71af5bc476009e7db38c7b284a5fa6f5aa22668e8'
+  name 'Source Code Pro'
   homepage 'http://adobe-fonts.github.io/source-code-pro/'
   license :ofl
 
