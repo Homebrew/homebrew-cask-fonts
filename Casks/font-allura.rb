3f9eea9429e16e11a8239c10f6bed72587df2ae8
Steve Dignam <steve@dignam.xyz>
2016-02-13 23:55:47 -0500

    updated font-allura to use github url

--- Casks/font-allura.rb
+++ Casks/font-allura.rb
@@ -1,8 +1,9 @@
 cask 'font-allura' do
-  version '1.004'
-  sha256 '6cd27c460940193cdf957129b9ef1316b49656a9a6e92d95aa21781567da4397'
+  version :latest
+  sha256 :no_check
 
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/allura/Allura-Regular.ttf'
+  url 'https://github.com/google/fonts/raw/master/ofl/allura/Allura-Regular.ttf'
+  name 'Allura'
   homepage 'http://www.google.com/fonts/specimen/Allura'
   license :ofl
 
