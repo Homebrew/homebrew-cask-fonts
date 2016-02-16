5862a1ef7ab7611c8e63dd5eba8a8b1bda82cd46
wfdd <erdbanfed@gmail.com>
2016-02-16 03:44:35 +0200

    Add italic variants to Hasklig

--- Casks/font-hasklig.rb
+++ Casks/font-hasklig.rb
@@ -9,10 +9,17 @@ cask 'font-hasklig' do
   license :ofl
 
   font 'Hasklig-Black.otf'
+  font 'Hasklig-BlackIt.otf'
   font 'Hasklig-Bold.otf'
+  font 'Hasklig-BoldIt.otf'
   font 'Hasklig-ExtraLight.otf'
+  font 'Hasklig-ExtraLightIt.otf'
+  font 'Hasklig-It.otf'
   font 'Hasklig-Light.otf'
+  font 'Hasklig-LightIt.otf'
   font 'Hasklig-Medium.otf'
+  font 'Hasklig-MediumIt.otf'
   font 'Hasklig-Regular.otf'
   font 'Hasklig-Semibold.otf'
+  font 'Hasklig-SemiboldIt.otf'
 end
