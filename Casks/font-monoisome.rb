72342b149005b099db88af3169b03b62525fe101
Vítor Galvão <info@vitorgalvao.com>
2016-03-10 19:51:26 -0500

    font-monoisome.rb: fixed appcast

--- Casks/font-monoisome.rb
+++ Casks/font-monoisome.rb
@@ -4,7 +4,7 @@ cask 'font-monoisome' do
 
   # github.com/larsenwork/monoid was verified as official when first introduced to the cask
   url 'https://github.com/larsenwork/monoid/blob/master/Monoisome/Monoisome-Regular.ttf?raw=true'
-  appcast 'https://github.com/larsenwork/monoid/blob/releases.atom',
+  appcast 'https://github.com/larsenwork/monoid/releases.atom',
           checkpoint: 'e94cd1ccc01488e99b07a372162ea9e555b6aef0e0911cbb198281f03b975467'
   name 'Monoisome'
   homepage 'http://larsenwork.com/monoid/'
