172620f175bfeb97df26e99e080f4cb4c8c42c37
Gijs Key <gijs@keij.org>
2016-03-14 17:19:57 +0100

    Update font-aclonica.rb

--- Casks/font-aclonica.rb
+++ Casks/font-aclonica.rb
@@ -2,7 +2,7 @@ cask 'font-aclonica' do
   version '1.000'
   sha256 'aec4ac0902f0d9597761557c59e4e0d5c9ac117c70d106ca1ae806082f6b234d'
 
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/aclonica/Aclonica.ttf'
+  url 'https://github.com/google/fonts/blob/master/apache/aclonica/Aclonica.ttf'
   homepage 'http://www.google.com/fonts/specimen/Aclonica'
   license :apache
 
