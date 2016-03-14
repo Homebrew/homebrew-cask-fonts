2d918df95a289af30ac5ad88624a6a62fd0d0808
Gijs Key <gijs@keij.org>
2016-03-14 17:28:18 +0100

    Update font-calligraffitti.rb

--- Casks/font-calligraffitti.rb
+++ Casks/font-calligraffitti.rb
@@ -2,7 +2,8 @@ cask 'font-calligraffitti' do
   version '1.001'
   sha256 '97c508102499f08b8964d750ec1f455e19aa9c30adf51d7fb5d0d650d8e75399'
 
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/calligraffitti/Calligraffitti-Regular.ttf'
+  url 'https://github.com/google/fonts/blob/master/apache/calligraffitti/Calligraffitti-Regular.ttf'
+  name 'Calligraffitti-Regular'
   homepage 'http://www.google.com/fonts/specimen/Calligraffitti'
   license :apache
 
