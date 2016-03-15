df922da8a25710cedb87cf10a7d855562ad03fa9
Vítor Galvão <info@vitorgalvao.com>
2016-03-15 12:00:51 -0400

    font-aclonica.rb: added name

--- Casks/font-aclonica.rb
+++ Casks/font-aclonica.rb
@@ -3,6 +3,7 @@ cask 'font-aclonica' do
   sha256 'aec4ac0902f0d9597761557c59e4e0d5c9ac117c70d106ca1ae806082f6b234d'
 
   url 'https://github.com/google/fonts/blob/master/apache/aclonica/Aclonica.ttf'
+  name 'Aclonica'
   homepage 'http://www.google.com/fonts/specimen/Aclonica'
   license :apache
 
