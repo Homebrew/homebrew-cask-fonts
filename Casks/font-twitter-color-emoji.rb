90e99d625099cbbc3bb0a50bd0bf1611ba0aa1ae
0xADADA <0xadada.pub@0xadada.pub>
2016-03-21 13:49:24 -0400

    Twitter Color Emoji (New formula)
    Twitter released a set of the custom colored EMOJI they use across
    their web and mobile platforms. This project has taken the SVG and
    generated a font based on the SVG vectors.

---
+++ Casks/font-twitter-color-emoji.rb
@@ -0,0 +1,13 @@
+cask 'font-twitter-color-emoji' do
+  version '1.0-alpha'
+  sha256 '341e2d5ef6eed9c506f6010048e12f646f9d0ccce8e82676eaebf1ef06eb4ca3'
+
+  url "https://github.com/eosrei/twemoji-color-font/releases/download/v#{version}/TwitterColorEmoji-SVGinOT-#{version}.zip"
+  appcast 'https://github.com/eosrei/twemoji-color-font/releases',
+          checkpoint: 'f2c41bc4189380471797296bb9c796282eab68e455b6c5bb2875c764d0288727'
+  name 'Twitter Color Emoji'
+  homepage 'https://github.com/eosrei/twemoji-color-font/'
+  license :mit
+
+  font 'TwitterColorEmoji-SVGinOT.ttf'
+end
