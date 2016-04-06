2f4a7009accd5cacac6d5a5c5a638a845b01426a
Min Dong <mdong@odu.edu>
2016-04-06 14:32:32 -0400

    font-fantasque-sans-mono v1.7.1 (updated formula)
    Updated link and check sum, thats all

--- Casks/font-fantasque-sans-mono.rb
+++ Casks/font-fantasque-sans-mono.rb
@@ -1,6 +1,6 @@
 cask 'font-fantasque-sans-mono' do
-  version '1.6.4'
-  sha256 'da5a7f84ac0e1c02b49334690d7451d936691718fb1332f863eacc521816dccd'
+  version 'v1.7.1'
+  sha256 '6bb3b24413b78eed19ffa9bd233ae555982e3b185bd303e57dd1e05bebf17352'
 
   url "https://github.com/belluzj/fantasque-sans/releases/download/#{version}/FantasqueSansMono.zip"
   homepage 'https://github.com/belluzj/fantasque-sans'
