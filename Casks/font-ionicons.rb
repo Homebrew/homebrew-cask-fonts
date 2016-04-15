2c2e7698d75d612ada0d4f9c188c5addc287270e
Paul Howard <phoward8020+github@gmail.com>
2016-04-15 12:03:47 -0700

    Add font Ionicons

---
+++ Casks/font-ionicons.rb
@@ -0,0 +1,11 @@
+cask 'font-ionicons' do
+  version '2.0.1'
+  sha256 'b222fcaede908b71d5b206db9fb7b625a07d313be00ee908eabd267604868661'
+
+  url 'https://github.com/driftyco/ionicons/archive/v2.0.1.zip'
+  name 'Ionicons'
+  homepage 'https://github.com/driftyco/ionicons'
+  license :mit
+
+  font 'ionicons-2.0.1/fonts/ionicons.ttf'
+end
