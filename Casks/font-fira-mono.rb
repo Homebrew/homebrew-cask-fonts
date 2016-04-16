027799661417ded2f90ca4b0d07599083f350823
SuperBo <supernbo@gmail.com>
2016-04-16 09:36:52 +0700

    Upgrade Fira Sans to v4.106 and Fira Mono to v3.206 (#554)

--- Casks/font-fira-mono.rb
+++ Casks/font-fira-mono.rb
@@ -1,12 +1,13 @@
 cask 'font-fira-mono' do
-  version '3.204'
-  sha256 '4a986f97f3ea9baee0f419042327a71941e2e9ef94783b01e6739718d6e1da5e'
+  version '3.206'
+  sha256 'f14249f857d802d29844b9ebff764990665072cccef3748dce06b7c21be8c5fc'
 
-  url 'http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3204.zip'
+  url 'http://www.carrois.com/downloads/fira_mono_3_2/FiraMonoFonts3206.zip'
+  name 'Fira Mono'
   homepage 'http://www.carrois.com/fira-4-1/'
   license :ofl
 
-  font 'FiraMonoFonts3204/OTF/FiraMono-Bold.otf'
-  font 'FiraMonoFonts3204/OTF/FiraMono-Medium.otf'
-  font 'FiraMonoFonts3204/OTF/FiraMono-Regular.otf'
+  font 'FiraMonoFonts3206/OTF/FiraMono-Bold.otf'
+  font 'FiraMonoFonts3206/OTF/FiraMono-Medium.otf'
+  font 'FiraMonoFonts3206/OTF/FiraMono-Regular.otf'
 end
