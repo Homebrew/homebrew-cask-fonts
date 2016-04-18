66412038ec8bba9e721cf4b40979198a7fa0ec1c
Takayuki Maeda <mtakayuki1209@gmail.com>
2016-04-18 20:49:00 +0900

    Add font-nyashi

---
+++ Casks/font-nyashi.rb
@@ -0,0 +1,11 @@
+cask 'font-nyashi' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://marusexijaxs.web.fc2.com/nyashi_ote.zip'
+  name 'Font Nyashi'
+  homepage 'http://marusexijaxs.web.fc2.com/tegakifont.html'
+  license :cc # CC BY-ND
+
+  font 'nyashi_ote/Nyashi.otf'
+end
