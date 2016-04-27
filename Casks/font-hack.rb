9b955ad80dc44441bd0a87c6a07e58c84c3f3597
Chris Daish <chrisdaish@gmail.com>
2016-04-27 15:50:18 +0100

    Adding the checkpoint value

--- Casks/font-hack.rb
+++ Casks/font-hack.rb
@@ -5,7 +5,7 @@ cask 'font-hack' do
   # github.com/chrissimpkins/Hack was verified as official when first introduced to the cask
   url "https://github.com/chrissimpkins/Hack/archive/v#{version}.zip"
   appcast 'https://github.com/chrissimpkins/Hack/releases.atom',
-          checkpoint: 'a674ef1ff633112e83b25ac3d4f260f846c61c26d5c981c228e49ad1e9706369'
+          checkpoint: 'fccba3ddfc4b50a011498f4a8f5956758421ae96725ce54f7f9fac97fd5161d1'
   name 'Hack'
   homepage 'http://sourcefoundry.org/hack/'
   license :ofl
