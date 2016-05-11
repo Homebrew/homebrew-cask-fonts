f20b5dd56ddba6b86e428574ba3dcd4e522e6549
Vítor Galvão <info@vitorgalvao.com>
2016-05-11 12:11:23 -0400

    updated font-fontawesome (4.6.2) (#574)

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,10 +1,10 @@
 cask 'font-fontawesome' do
-  version '4.6.1'
-  sha256 '289269c8f118f330c51d8b896b3b9fe7d6cca4b982ed42b9e941c20e2d567e9d'
+  version '4.6.2'
+  sha256 '34e9884dd4bda231994c6b7d2bd486c2059d4e9f11d4c2040d10387fc807aa2c'
 
   url "https://fortawesome.github.io/Font-Awesome/assets/font-awesome-#{version}.zip"
   appcast 'https://github.com/FortAwesome/Font-Awesome/releases.atom',
-          checkpoint: '41264b6d8c1d633bcb4514dce62710269e70eeb880f8298e1c170fd0926b70b7'
+          checkpoint: '451b04895bc7679aa38647991546d409e5bd2c0cc73435ff9e22a10bab757177'
   name 'Font Awesome'
   homepage 'http://fortawesome.github.io/Font-Awesome/'
   license :oss
