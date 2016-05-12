4dccecc60e8c6431454466c8014af4bc1115c6ee
Oliver Joseph Ash <oliverjash@gmail.com>
2016-05-12 11:14:50 +0200

    Correct url for font-didact-gothic
    Fixes #577

--- Casks/font-didact-gothic.rb
+++ Casks/font-didact-gothic.rb
@@ -2,7 +2,7 @@ cask 'font-didact-gothic' do
   version '20110429'
   sha256 '01c9c7791a161a9022eee3b172ebfa33300891557471f94f488fb7b8442630d8'
 
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/didactgothic/DidactGothic.ttf'
+  url 'https://github.com/google/fonts/raw/master/ofl/didactgothic/DidactGothic.ttf'
   homepage 'http://www.google.com/fonts/specimen/Didact%20Gothic'
   license :ofl
 
