4d48200245fbf55d4596463d3a3423831638c08e
Josh Hagins <hagins.josh@gmail.com>
2016-05-18 11:16:48 -0400

    Add font-awesome-terminal-fonts

---
+++ Casks/font-awesome-terminal-fonts.rb
@@ -0,0 +1,13 @@
+cask 'font-awesome-terminal-fonts' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/gabrielelana/awesome-terminal-fonts/archive/patching-strategy.tar.gz'
+  name 'Awesome Terminal Fonts'
+  homepage 'https://github.com/gabrielelana/awesome-terminal-fonts'
+  license :mit
+
+  font 'awesome-terminal-fonts-patching-strategy/patched/Droid+Sans+Mono+Awesome.ttf'
+  font 'awesome-terminal-fonts-patching-strategy/patched/Inconsolata+Awesome.ttf'
+  font 'awesome-terminal-fonts-patching-strategy/patched/SourceCodePro+Powerline+Awesome+Regular.ttf'
+end
