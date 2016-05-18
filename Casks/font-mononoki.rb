2607f4786f81cdfc2f94a28d7654b31f2c69cda4
Ben Cohen <ben@bencohen.net>
2016-05-17 23:29:10 -0400

    no version check

--- Casks/font-mononoki.rb
+++ Casks/font-mononoki.rb
@@ -1,6 +1,6 @@
 cask 'font-mononoki' do
-  version '1.000'
-  sha256 '2f976671c4a44b37d66f04dd9bf104cc066d05d3ebf40e36e9f6a8f9f2589e25'
+  version :latest
+  sha256 :no_check
 
   url 'https://github.com/madmalik/mononoki/blob/master/export/mononoki.zip'
   name 'Mononoki'
