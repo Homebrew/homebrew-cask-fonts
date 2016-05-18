9be3f87707c6127567502c6d8ce8d5a61e36e7ac
Ben Cohen <ben@bencohen.net>
2016-05-17 23:27:12 -0400

    Following guidelines

--- Casks/font-mononoki.rb
+++ Casks/font-mononoki.rb
@@ -1,11 +1,14 @@
 cask 'font-mononoki' do
-  version :latest
-  sha256 :no_check
+  version '1.000'
+  sha256 '2f976671c4a44b37d66f04dd9bf104cc066d05d3ebf40e36e9f6a8f9f2589e25'
 
-  url 'https://github.com/madmalik/mononoki/blob/master/export/webfont/mononoki-Regular.ttf'
+  url 'https://github.com/madmalik/mononoki/blob/master/export/mononoki.zip'
   name 'Mononoki'
   homepage 'https://madmalik.github.io/mononoki/'
   license :ofl
 
+  font 'mononoki-Bold.ttf'
+  font 'mononoki-BoldItalic.ttf'
+  font 'mononoki-Italic.ttf'
   font 'mononoki-Regular.ttf'
 end
