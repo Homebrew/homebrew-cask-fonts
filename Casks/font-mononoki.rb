9694f0d223cb368f350d6da8e0cc13b34d7c3f59
Ben Cohen <ben@bencohen.net>
2016-05-17 23:10:35 -0400

    Added mononoki

---
+++ Casks/font-mononoki.rb
@@ -0,0 +1,11 @@
+cask 'font-mononoki' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/madmalik/mononoki/blob/master/export/webfont/mononoki-Regular.ttf'
+  name 'Mononoki'
+  homepage 'https://madmalik.github.io/mononoki/'
+  license :ofl
+
+  font 'mononoki-regular.ttf'
+end
