f276028d65b29997d9b2390ae0ceae3f66d8c968
Ben Cohen <heliostatic@gmail.com>
2016-05-21 10:21:45 -0400

    Add font-mononoki (#580)

---
+++ Casks/font-mononoki.rb
@@ -0,0 +1,14 @@
+cask 'font-mononoki' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/madmalik/mononoki/raw/master/export/mononoki.zip'
+  name 'Mononoki'
+  homepage 'https://madmalik.github.io/mononoki/'
+  license :ofl
+
+  font 'mononoki-Bold.ttf'
+  font 'mononoki-BoldItalic.ttf'
+  font 'mononoki-Italic.ttf'
+  font 'mononoki-Regular.ttf'
+end
