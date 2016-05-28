5db6494d680eecb9c5fcced8769fe9ab21941acf
Andrew Titmuss <andrewtitmuss2000@gmail.com>
2016-05-28 19:42:33 +1000

    updated font-fontawesome (4.6.3) (#585)

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,10 +1,10 @@
 cask 'font-fontawesome' do
-  version '4.6.2'
-  sha256 '34e9884dd4bda231994c6b7d2bd486c2059d4e9f11d4c2040d10387fc807aa2c'
+  version '4.6.3'
+  sha256 'd6fdb3f1130da80d7d9b45ca14625ca77bfaf0262abf28d08306f9e6f3b8a619'
 
   url "https://fortawesome.github.io/Font-Awesome/assets/font-awesome-#{version}.zip"
   appcast 'https://github.com/FortAwesome/Font-Awesome/releases.atom',
-          checkpoint: '451b04895bc7679aa38647991546d409e5bd2c0cc73435ff9e22a10bab757177'
+          checkpoint: 'b0af6dbc646732b2f9a36945ebb1606e010ff43efa7b2df0af884d5408d89276'
   name 'Font Awesome'
   homepage 'http://fortawesome.github.io/Font-Awesome/'
   license :oss
