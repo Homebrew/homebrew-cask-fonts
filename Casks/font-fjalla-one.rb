fe85cc38ec9563dbea8eb4672389c749bf35cc07
Mathias Fredriksson <mathias@walkbase.com>
2016-06-06 13:16:54 +0300

    Add name for font-fjalla-one

--- Casks/font-fjalla-one.rb
+++ Casks/font-fjalla-one.rb
@@ -3,6 +3,7 @@ cask 'font-fjalla-one' do
   sha256 '2c4a287ae4d7e6dbf6e45b6264612d02130147dcd5434cf168a1ba83055225bf'
 
   url 'https://github.com/google/fonts/raw/a30ff88e1a02d70aaf2589dfc3cef18a4128c495/ofl/fjallaone/FjallaOne-Regular.ttf'
+  name 'Fjalla One'
   homepage 'http://www.google.com/fonts/specimen/Fjalla%20One'
   license :ofl
 
