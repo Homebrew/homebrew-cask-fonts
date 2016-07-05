cfee0cd5ef4479575338e71e30aa50143d3b1927
tmaone <marios.daoutis@gmail.com>
2016-07-05 02:19:18 +0200

    name below URL fix

--- Casks/font-share-tech-mono.rb
+++ Casks/font-share-tech-mono.rb
@@ -2,8 +2,8 @@ cask 'font-share-tech-mono' do
   version '1.002'
   sha256 '9ceab1f87414829af259c0f537573ae03ef7dd3147c0b27a36a1a0beb6732677'
 
-  name 'Share Tech Mono'
   url 'https://github.com/google/fonts/raw/master/ofl/sharetechmono/ShareTechMono-Regular.ttf'
+  name 'Share Tech Mono'
   homepage 'http://www.google.com/fonts/specimen/Share%20Tech%20Mono'
   license :ofl
 
