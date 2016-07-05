b081fc26b01f3124c43e4f138b8808881fbcfc32
暖簾 <neronplex@users.noreply.github.com>
2016-07-06 01:47:02 +0900

    Update font-ricty-diminished.rb (#597)
    Changes associated with that font author has changed the public page.

--- Casks/font-ricty-diminished.rb
+++ Casks/font-ricty-diminished.rb
@@ -1,17 +1,18 @@
 cask 'font-ricty-diminished' do
   version '4.0.1'
-  sha256 '08ab33a200778ee66af5618f34091dfe12451c675508cd01c8ede753bf3d6159'
-
-  url "https://github.com/yascentur/RictyDiminished/archive/#{version}.tar.gz"
-  homepage 'https://github.com/yascentur/RictyDiminished'
+  sha256 'b32ba2877651df31914797cc4938b98d726d4777ecb2064c9dbbb620efcff24f'
+  
+  url "http://www.rs.tus.ac.jp/yyusa/ricty_diminished/ricty_diminished-#{version}.tar.gz"
+  name 'Ricty Diminished'
+  homepage 'http://www.rs.tus.ac.jp/yyusa/ricty_diminished.html'
   license :ofl
 
-  font "RictyDiminished-#{version}/RictyDiminished-Bold.ttf"
-  font "RictyDiminished-#{version}/RictyDiminished-BoldOblique.ttf"
-  font "RictyDiminished-#{version}/RictyDiminished-Oblique.ttf"
-  font "RictyDiminished-#{version}/RictyDiminished-Regular.ttf"
-  font "RictyDiminished-#{version}/RictyDiminishedDiscord-Bold.ttf"
-  font "RictyDiminished-#{version}/RictyDiminishedDiscord-BoldOblique.ttf"
-  font "RictyDiminished-#{version}/RictyDiminishedDiscord-Oblique.ttf"
-  font "RictyDiminished-#{version}/RictyDiminishedDiscord-Regular.ttf"
+  font 'RictyDiminished-Bold.ttf'
+  font 'RictyDiminished-BoldOblique.ttf'
+  font 'RictyDiminished-Oblique.ttf'
+  font 'RictyDiminished-Regular.ttf'
+  font 'RictyDiminishedDiscord-Bold.ttf'
+  font 'RictyDiminishedDiscord-BoldOblique.ttf'
+  font 'RictyDiminishedDiscord-Oblique.ttf'
+  font 'RictyDiminishedDiscord-Regular.ttf'
 end
