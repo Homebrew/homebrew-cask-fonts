f642729b64508a9355ec9163b2869e7f72009bfb
Tom Rothe <tom.rothe@bisdn.de>
2016-07-06 13:26:17 +0200

    style fixes to font-octicons.rb

--- Casks/font-octicons.rb
+++ Casks/font-octicons.rb
@@ -3,6 +3,7 @@ cask 'font-octicons' do
   sha256 :no_check
 
   url 'https://github.com/github/octicons/archive/master.zip'
+  name 'Octicons'
   homepage 'https://octicons.github.com'
   license :ofl
 
