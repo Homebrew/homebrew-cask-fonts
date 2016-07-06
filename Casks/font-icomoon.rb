6d25dc30b1d982ed46a8da9112cfb94a68270cab
Tom Rothe <tom.rothe@bisdn.de>
2016-07-06 10:17:11 +0200

    added icomoon formula

---
+++ Casks/font-icomoon.rb
@@ -0,0 +1,10 @@
+cask 'font-icomoon' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/Keyamoon/IcoMoon-Free/archive/master.zip'
+  homepage 'https://icomoon.io'
+  license :gpl
+
+  font 'IcoMoon-Free-master/Font/IcoMoon-Free.ttf'
+end
