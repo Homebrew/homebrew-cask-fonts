46d67ee381590e8dd713f8087e005548fef43d3e
Tom Rothe <tom.rothe@bisdn.de>
2016-07-06 10:18:26 +0200

    added foundation icon font

---
+++ Casks/font-foundation-icons.rb
@@ -0,0 +1,10 @@
+cask 'font-foundation-icons' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://zurb.com/playground/uploads/upload/upload/288/foundation-icons.zip'
+  homepage 'http://zurb.com/playground/foundation-icon-fonts-3'
+  license :mit
+
+  font 'foundation-icons/foundation-icons.ttf'
+end
