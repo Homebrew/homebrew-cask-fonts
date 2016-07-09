89f7f226ffe2826f2b0cf91df458f5227f867553
Jiajie Chen <jiegec@qq.com>
2016-07-09 09:42:31 +0800

    Add font-gnu-unifont (#601)

---
+++ Casks/font-gnu-unifont.rb
@@ -0,0 +1,11 @@
+cask 'font-gnu-unifont' do
+  version '9.0.01'
+  sha256 '0d36e005b962b7f15e55e93bbd25f931ca3a19ce3ed787bdeb721974f36f5b58'
+
+  url "http://unifoundry.com/pub/unifont-#{version}/font-builds/unifont-#{version}.ttf"
+  name 'GNU Unifont'
+  homepage 'http://unifoundry.com/unifont.html'
+  license :gpl
+
+  font "unifont-#{version}.ttf"
+end
