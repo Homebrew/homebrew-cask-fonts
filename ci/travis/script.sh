01554e1153f8b49ae21b25f3fb3b24d897f2f344
Josh Hagins <hagins.josh@gmail.com>
2016-07-15 13:14:13 -0400

    travis: audit modified casks

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -12,6 +12,7 @@ header 'Running script.sh...'
 
 if any_casks_modified; then
   modified_casks=($(modified_cask_files))
+  run brew cask _audit_modified_casks "${TRAVIS_COMMIT_RANGE}"
   run brew cask style "${modified_casks[@]}"
 fi
 
