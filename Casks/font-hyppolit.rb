b0ae46a503bd6c18aa49cdbae09299a882395df2
infinity5 <LippyCop@mailinator.com>
2016-07-18 11:22:46 -0400

    Create font-hyppolit.rb

---
+++ Casks/font-hyppolit.rb
@@ -0,0 +1,11 @@
+cask 'font-hyppolit' do
+  version '1.000'
+  sha256 '4841507652f30056c08d275ea68fe00fbdf1a47b46e3c8a0f5bd7218972966f1'
+
+  url 'http://dl.1001fonts.com/hyppolit.zip'
+  name 'Hyppolit'
+  homepage 'http://www.1001fonts.com/hyppolit-font.html'
+  license :unknown
+
+  font 'Hyppolit.ttf'
+end
