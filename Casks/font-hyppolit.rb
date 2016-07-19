3f97300d9bbc3e97dcfe9df1c0e3b451867a4f2b
Josh Hagins <hagins.josh@gmail.com>
2016-07-18 23:03:49 -0400

    font-hyppolit: version :latest

--- Casks/font-hyppolit.rb
+++ Casks/font-hyppolit.rb
@@ -1,6 +1,6 @@
 cask 'font-hyppolit' do
-  version '1.000'
-  sha256 '4841507652f30056c08d275ea68fe00fbdf1a47b46e3c8a0f5bd7218972966f1'
+  version :latest
+  sha256 :no_check
 
   url 'http://dl.1001fonts.com/hyppolit.zip'
   name 'Hyppolit'
