499606503a58029a156f06a24408a96df646264a
Wilhelm Schuster <ws@wilhelm.re>
2016-07-24 07:42:56 +0200

    Update font-terminus to 4.40.1

--- Casks/font-terminus.rb
+++ Casks/font-terminus.rb
@@ -1,12 +1,14 @@
 cask 'font-terminus' do
-  version '4.39'
-  sha256 '5d8a7075757fd1a66012fafc6bd761e5b758573ea66bc1197025f1d4eb343383'
+  version '4.40.1'
+  sha256 'c3cb690c2935123035a0b1f3bfdd9511c282dab489cd423e161a47c592edf188'
 
   url "http://files.ax86.net/terminus-ttf/files/#{version}/terminus-ttf-#{version}.zip"
+  name 'Terminus TTF'
   homepage 'http://files.ax86.net/terminus-ttf/'
-  license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :ofl
 
   font "terminus-ttf-#{version}/TerminusTTF-#{version}.ttf"
   font "terminus-ttf-#{version}/TerminusTTF-Bold-#{version}.ttf"
   font "terminus-ttf-#{version}/TerminusTTF-Italic-#{version}.ttf"
+  font "terminus-ttf-#{version}/TerminusTTF-Bold Italic-#{version}.ttf"
 end
