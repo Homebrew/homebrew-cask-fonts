48f2e80e4c0a4ce832c4a5a30120baa675152c7f
infinity5 <LippyCop@mailinator.com>
2016-07-29 19:02:25 -0400

    Create font-prince-valiant.rb

---
+++ Casks/font-prince-valiant.rb
@@ -0,0 +1,11 @@
+cask 'font-prince-valiant' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.1001freefonts.com/d/3801/prince_valiant.zip'
+  name 'Prince Valiant'
+  homepage 'http://www.1001freefonts.com/prince_valiant.font'
+  license :unknown
+
+  font 'Prince Valiant.ttf'
+end
