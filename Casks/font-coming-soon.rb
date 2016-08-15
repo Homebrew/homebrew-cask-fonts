556853dddd6badba46ca78ae1d69554281dca546
Leonid Komarovsky <shpoont@users.noreply.github.com>
2016-08-16 01:36:07 +0300

    Update font coming-soon

--- Casks/font-coming-soon.rb
+++ Casks/font-coming-soon.rb
@@ -2,7 +2,9 @@ cask 'font-coming-soon' do
   version '1.000'
   sha256 '1c7a44d75a7e130a89ca46bf6ece6d34b0085cd82291d745fae5a9844bd9a615'
 
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/apache/comingsoon/ComingSoon.ttf'
+  # Official google fonts repo
+  url 'https://github.com/google/fonts/raw/master/apache/comingsoon/ComingSoon.ttf'
+  name 'Coming Soon'
   homepage 'http://www.google.com/fonts/specimen/Coming%20Soon'
   license :apache
 
