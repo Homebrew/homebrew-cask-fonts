74ed7ee16bce602b261a580a454a2b8d0ec49160
Alex Guerrero <alexguerrero1092@gmail.com>
2016-09-02 16:37:05 +0200

    Update font-varela (#628)

--- Casks/font-varela.rb
+++ Casks/font-varela.rb
@@ -1,8 +1,13 @@
 cask 'font-varela' do
-  version '1.000'
-  sha256 '87cf0ddd50cd297cd6facfaac8bf59bf8d0b1a3b8b6619957ba08e72043d1896'
+  version :latest
+  sha256 :no_check
 
-  url 'https://googlefontdirectory.googlecode.com/hg-history/67342bc472599b4c32201ee4a002fe59a6447a42/ofl/varela/Varela-Regular.ttf'
+  # github.com/google/fonts/trunk/ofl/varela was verified as official when first introduced to the cask
+  url 'https://github.com/google/fonts/trunk/ofl/varela',
+      using:      :svn,
+      revision:   '796',
+      trust_cert: true
+  name 'Varela'
   homepage 'http://www.google.com/fonts/specimen/Varela'
   license :ofl
 
