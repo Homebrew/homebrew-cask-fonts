29be8301be3402b2d6f0a89c0c72a01661ca0866
Lutz F. Krebs <github.ws.4r2@lutzkrebs.net>
2016-09-11 14:42:06 +0200

    Adjusted stanza order and switched to single quotes
    I’ll get this eventually… Third time’s the charm!

--- Casks/font-andika.rb
+++ Casks/font-andika.rb
@@ -3,9 +3,9 @@ cask 'font-andika' do
   sha256 '604b7a1194be099bdf311ef76cbce086a054fa16d2b101cfaedcf527c63df507'
 
   url 'http://software.sil.org/downloads/andika/Andika-5.000.zip'
+  name 'Andika'
   homepage 'http://software.sil.org/andika/'
   license :ofl
-  name 'Andika'
 
   font 'Andika-R.ttf'
 end
--- Casks/font-gentium-plus.rb
+++ Casks/font-gentium-plus.rb
@@ -2,10 +2,10 @@ cask 'font-gentium-plus' do
   version '5.000'
   sha256 '335911f17bd2de4e43742e1d0367cfeff19a90abf7ed604f100a42705042e154'
 
-  url "http://software.sil.org/downloads/gentium/GentiumPlus-5.000.zip"
+  url 'http://software.sil.org/downloads/gentium/GentiumPlus-5.000.zip'
+  name 'Gentium'
   homepage 'http://software.sil.org/gentium/'
   license :ofl
-  name 'Gentium'
 
   font 'GentiumPlus-5.000/GentiumPlus-I.ttf'
   font 'GentiumPlus-5.000/GentiumPlus-R.ttf'
