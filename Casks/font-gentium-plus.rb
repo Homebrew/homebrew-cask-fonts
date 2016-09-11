5a6ff739c75bae86b03b90687e8b560a730680db
Lutz F. Krebs <github.ws.4r2@lutzkrebs.net>
2016-09-11 14:05:10 +0200

    Updated url of Gentium Plus (SIL)
    Previous download URL is broken. Updated the URL from the same site.
    SHA checksum remains the same.

--- Casks/font-gentium-plus.rb
+++ Casks/font-gentium-plus.rb
@@ -2,8 +2,8 @@ cask 'font-gentium-plus' do
   version '5.000'
   sha256 '335911f17bd2de4e43742e1d0367cfeff19a90abf7ed604f100a42705042e154'
 
-  url "http://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=GentiumPlus-#{version}.zip&filename=GentiumPlus-#{version}.zip"
-  homepage 'http://scripts.sil.org/cms/scripts/page.php?item_id=Gentium'
+  url "http://software.sil.org/downloads/gentium/GentiumPlus-5.000.zip"
+  homepage 'http://software.sil.org/gentium/'
   license :ofl
 
   font 'GentiumPlus-5.000/GentiumPlus-I.ttf'
