81aaad607d26a01b77ec766c6947903f0b2a8e4c
Vítor Galvão <info@vitorgalvao.com>
2016-09-11 15:29:17 +0100

    andika: interpolate version

--- Casks/font-andika.rb
+++ Casks/font-andika.rb
@@ -2,7 +2,7 @@ cask 'font-andika' do
   version '5.000'
   sha256 '604b7a1194be099bdf311ef76cbce086a054fa16d2b101cfaedcf527c63df507'
 
-  url 'http://software.sil.org/downloads/andika/Andika-5.000.zip'
+  url "http://software.sil.org/downloads/andika/Andika-#{version}.zip"
   name 'Andika'
   homepage 'http://software.sil.org/andika/'
   license :ofl
