c96b00ca442a29055f4660416e0765dddfe2976a
Vítor Galvão <info@vitorgalvao.com>
2016-09-11 15:29:21 +0100

    gentium-plus: interpolate version (#632)

--- Casks/font-gentium-plus.rb
+++ Casks/font-gentium-plus.rb
@@ -2,7 +2,7 @@ cask 'font-gentium-plus' do
   version '5.000'
   sha256 '335911f17bd2de4e43742e1d0367cfeff19a90abf7ed604f100a42705042e154'
 
-  url 'http://software.sil.org/downloads/gentium/GentiumPlus-5.000.zip'
+  url "http://software.sil.org/downloads/gentium/GentiumPlus-#{version}.zip"
   name 'Gentium'
   homepage 'http://software.sil.org/gentium/'
   license :ofl
