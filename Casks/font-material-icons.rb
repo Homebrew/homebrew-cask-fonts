e31257810ce720057252148794064f1c32931b4e
Maarten Paauw <maartenpaauw@gmail.com>
2016-09-14 14:42:33 +0200

    Update font-material-icons to 3.0.1 (#635)

--- Casks/font-material-icons.rb
+++ Casks/font-material-icons.rb
@@ -1,11 +1,11 @@
 cask 'font-material-icons' do
-  version '2.2.3'
-  sha256 'c68797889b285abe51898defa209d1a1bd1d959c9f20e5ef486c1d3f0e08eac2'
+  version '3.0.1'
+  sha256 '722e3b09121b82a3746f3da2ecd3a2db8d7d24153b8433324315695a45f06a90'
 
   # github.com/google/material-design-icons was verified as official when first introduced to the cask
   url "https://github.com/google/material-design-icons/archive/#{version}.zip"
   appcast 'https://github.com/google/material-design-icons/releases.atom',
-          checkpoint: '904d2021b0453db73abe506f4756b53eb78aa372f0628d57c8226a132c97323d'
+          checkpoint: '601c6c9de3ef09cd7127abcde40947cafc38607cd5a6e971194781fa582c9e1c'
   name 'Material Icons'
   homepage 'http://google.github.io/material-design-icons/'
   license :oss
