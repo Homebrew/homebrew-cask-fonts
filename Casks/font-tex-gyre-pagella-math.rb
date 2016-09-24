1e8d122322730c74b2b322a0920e0e5d1b6cdc72
ickc <ickc@users.noreply.github.com>
2016-09-24 00:51:21 -0700

    Update font-tex-gyre-pagella-math: bug fix (#638)

--- Casks/font-tex-gyre-pagella-math.rb
+++ Casks/font-tex-gyre-pagella-math.rb
@@ -1,10 +1,11 @@
 cask 'font-tex-gyre-pagella-math' do
   version '1.632'
-  sha256 '4c32b3094d14ccdb649c1a668871a43ee17b3fdc34192c9f0cb8792d68148f4c'
+  sha256 '68a9c0ce195915334673960b13a281f24d31a8ae380454a0e35652dba506d474'
 
-  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrepagella-math-1632.zip"
+  url 'http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrepagella-math-1632.zip'
+  name 'Pagella Math'
   homepage 'http://www.gust.org.pl/projects/e-foundry/tg-math'
   license :lppl
 
-  font 'texgyrepagella-math-1632/otf/texgyrepagella-math.otf'
+  font 'texgyrepagella-math-1632/opentype/texgyrepagella-math.otf'
 end
