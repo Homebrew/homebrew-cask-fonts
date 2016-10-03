3e6f8d1acf6f774ed7525395b821ae3eb0fc8ea8
Hylke Visser <htdvisser@gmail.com>
2016-10-03 19:58:09 +0200

    Fix League Spartan Font
    Directory name in zip file should be `league-spartan-master` instead of `league-spartan-#{version}` (at least, for latest version `c350408b07`)
    
    Also address #437
    
    Signed-off-by: Hylke Visser <htdvisser@gmail.com>

--- Casks/font-league-spartan.rb
+++ Casks/font-league-spartan.rb
@@ -3,8 +3,9 @@ cask 'font-league-spartan' do
   sha256 'a166294a7e156a0eb14df5714acfeeb3ad4db01eab2bc88f961695da917337fd'
 
   url "http://files.theleagueofmoveabletype.com/downloads/league-spartan-#{version}.zip"
+  name 'League Spartan'
   homepage 'https://www.theleagueofmoveabletype.com/league-spartan'
   license :ofl
 
-  font "league-spartan-#{version}/LeagueSpartan-Bold.otf"
+  font 'league-spartan-master/LeagueSpartan-Bold.otf'
 end
