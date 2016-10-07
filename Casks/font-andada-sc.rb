a283eaf10055610d70427de10fed1ee2d0b67114
Leandro Facchinetti <me@leafac.com>
2016-10-07 12:54:49 -0400

    added font-andada-sc

---
+++ Casks/font-andada-sc.rb
@@ -0,0 +1,18 @@
+cask 'font-andada-sc' do
+  # version '1.003'
+  version :latest
+  sha256 :no_check
+
+  # github.com/google/fonts was verified as official when first introduced to the cask
+  url 'https://github.com/google/fonts/trunk/ofl/andadasc',
+      using:      :svn,
+      revision:   '50',
+      trust_cert: true
+  name 'Andada SC'
+  homepage 'https://www.fontsquirrel.com/fonts/andada'
+
+  font 'AndadaSC-Bold.ttf'
+  font 'AndadaSC-BoldItalic.ttf'
+  font 'AndadaSC-Italic.ttf'
+  font 'AndadaSC-Regular.ttf'
+end
