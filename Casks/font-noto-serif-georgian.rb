88721a987f450934a4c37f138e70237e94c28f30
Max Nordlund gmail <max.nordlund@gmail.com>
2016-10-15 01:31:03 +0200

    Fix all google note fonts

--- Casks/font-noto-serif-georgian.rb
+++ Casks/font-noto-serif-georgian.rb
@@ -2,6 +2,7 @@ cask 'font-noto-serif-georgian' do
   version :latest
   sha256 :no_check
 
+  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifGeorgian-unhinted.zip'
   name 'Noto Serif Georgian'
   homepage 'https://www.google.com/get/noto/#serif-geor'
