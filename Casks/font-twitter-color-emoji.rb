aaf04754930ebaa3b6713744c2e679c30a2e4834
0xADADA <0xadada.pub@0xadada.pub>
2016-10-15 16:54:57 -0400

    Updated font-twitter-color-emoji v1.1
    * Upstream release includes Unicode 9.0 support
    * Upstream release included new emoji!

--- Casks/font-twitter-color-emoji.rb
+++ Casks/font-twitter-color-emoji.rb
@@ -1,12 +1,12 @@
 cask 'font-twitter-color-emoji' do
-  version '1.0-alpha'
-  sha256 '341e2d5ef6eed9c506f6010048e12f646f9d0ccce8e82676eaebf1ef06eb4ca3'
+  version '1.1'
+  sha256 '59ce24afb398820bd505430898316f18498a2d670bd82f05594b5cf21a790f6e'
 
   url "https://github.com/eosrei/twemoji-color-font/releases/download/v#{version}/TwitterColorEmoji-SVGinOT-#{version}.zip"
   appcast 'https://github.com/eosrei/twemoji-color-font/releases',
-          checkpoint: 'f2c41bc4189380471797296bb9c796282eab68e455b6c5bb2875c764d0288727'
+          checkpoint: 'e734efc99053850ffd2a5b8a4d924956540fe7f75c98e6faac13422cfd111009'
   name 'Twitter Color Emoji'
   homepage 'https://github.com/eosrei/twemoji-color-font/'
 
-  font 'TwitterColorEmoji-SVGinOT.ttf'
+  font 'TwitterColorEmoji-SVGinOT-#{version}/TwitterColorEmoji-SVGinOT.ttf'
 end
