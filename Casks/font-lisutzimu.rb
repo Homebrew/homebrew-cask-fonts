cbf703c9ce1171a5657f526a12b873be1d1371d3
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:50:59 +0100

    Update font-lisutzimu to latest

--- Casks/font-lisutzimu.rb
+++ Casks/font-lisutzimu.rb
@@ -1,15 +1,15 @@
 cask 'font-lisutzimu' do
-  version '1.0'
-  sha256 '15948eaee1c56b88e04fd17d8520cfe2cd4112a3b4dc2961e2adbc5a4e616ebd'
+  version :latest
+  sha256 :no_check
 
-  url "http://download739.mediafire.com/5s5j5857bnsg/nng3ontzxrt/Lisu+Tzimu-#{version}.exe"
+  # mediafire.com/?nng3ontzxrt was verified as official when first introduced to the cask
+  url 'http://www.mediafire.com/?nng3ontzxrt'
+  name 'LisuTzimu'
   homepage 'http://www.alanwood.net/unicode/fonts-east-asian.html#lisu'
 
   depends_on formula: 'unar'
   container type: :seven_zip
 
-  # The garbled characters in the directory name are intentional,
-  # including the NO-BREAK SPACE
   font 'ý €/lisutzimu.ttf'
   font 'ý €/lisutzimu-bold.ttf'
   font 'ý €/lisutzimu-bolditalic.ttf'
