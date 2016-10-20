c1acf7d5d462b79a0b2300ddf9b6b8afb6a1f0a9
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:20:19 +0100

    Update font-junicode to latest

--- Casks/font-junicode.rb
+++ Casks/font-junicode.rb
@@ -1,8 +1,9 @@
 cask 'font-junicode' do
-  version '0.7.8'
-  sha256 '7427c32a9383dd1ce485d202f4767b3baa79d4eae3a74b8ff2a10c2d8a84f3d1'
+  version :latest
+  sha256 :no_check
 
-  url "http://downloads.sourceforge.net/project/junicode/junicode/junicode-#{version.gsub('.', '-')}/junicode-#{version.gsub('.', '-')}.zip"
+  url 'https://sourceforge.net/projects/junicode/files/latest/download'
+  name 'Junicode'
   homepage 'http://junicode.sourceforge.net/'
 
   font 'junicode/fonts/Junicode-Bold.ttf'
