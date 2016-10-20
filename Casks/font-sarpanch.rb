48f63e8ea5bb197888c0ab553cd875d0dda65ea5
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:18:04 +0100

    Update font-sarpanch to 2.000

--- Casks/font-sarpanch.rb
+++ Casks/font-sarpanch.rb
@@ -2,9 +2,10 @@ cask 'font-sarpanch' do
   version '2.000'
   sha256 '5da058f7a7686ea5ad9d2811b2063343a580bf4da3d415a947d2c37d6c5bcf07'
 
-  url 'https://github.com/itfoundry/sarpanch/releases/download/v2.000/sarpanch-2_000.zip'
+  url "https://github.com/itfoundry/sarpanch/releases/download/v#{version}/sarpanch-#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/itfoundry/sarpanch/releases.atom',
           checkpoint: 'b7624a74f2a8275919bffed357df87385a5b32de8bc69e43ca072791e7993ba4'
+  name 'Sarpanch'
   homepage 'https://github.com/itfoundry/sarpanch'
 
   font 'Sarpanch-Black.otf'
