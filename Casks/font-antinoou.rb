6a3733fe8fe185e0316b3b9890764197dbf29701
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 17:14:17 +0100

    Update font-antinoou to 1.006

--- Casks/font-antinoou.rb
+++ Casks/font-antinoou.rb
@@ -3,8 +3,9 @@ cask 'font-antinoou' do
   sha256 'd7f961ff2ab5b6c707e4f0a24e8302c7a61c2e2ab2e9880c94a8deb6f5aeff69'
 
   url 'http://www.evertype.com/fonts/coptic/AntinoouFont.zip'
+  name 'Antinoou'
   homepage 'http://www.evertype.com/fonts/coptic/'
 
-  font 'AntinoouFont-1.0.6/Antinoou.ttf'
-  font 'AntinoouFont-1.0.6/AntinoouItalic.ttf'
+  font "AntinoouFont-#{version}/Antinoou.ttf"
+  font "AntinoouFont-#{version}/AntinoouItalic.ttf"
 end
