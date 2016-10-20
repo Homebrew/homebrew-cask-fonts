ae6867642bf43216a30e77fca27ebd9bdaf82741
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:25:53 +0100

    Update font-kalam to 2.000

--- Casks/font-kalam.rb
+++ Casks/font-kalam.rb
@@ -2,9 +2,10 @@ cask 'font-kalam' do
   version '2.000'
   sha256 '6b97c9719e7eaf3c069610059b548c2e130240d9996ec73ab61cfb17f1cbd1e6'
 
-  url 'https://github.com/itfoundry/kalam/releases/download/v2.000/kalam-2_000.zip'
+  url "https://github.com/itfoundry/kalam/releases/download/v#{version}/kalam-#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/itfoundry/kalam/releases.atom',
           checkpoint: '2ae5d72c3ec731407999c0dc22df8bb6e42cc544b708eabf148801d9b6fa339f'
+  name 'Kalam'
   homepage 'https://github.com/itfoundry/kalam'
 
   font 'Kalam-Bold.otf'
