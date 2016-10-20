61be54ed29e5e054f6263a257cd56e372ef5af65
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:50:32 +0100

    Update font-rotinonhsonni-serif to 4.2 (#839)

--- Casks/font-rotinonhsonni-serif.rb
+++ Casks/font-rotinonhsonni-serif.rb
@@ -1,12 +1,13 @@
 cask 'font-rotinonhsonni-serif' do
-  version '4.002'
+  version '4.2'
   sha256 'fd7f76b1043ec817b3fd109130eafd1db85a3249ca7d98014a16bd8c207488d2'
 
   url 'http://www.languagegeek.com/font/RotinonhSerif.zip'
+  name 'Rotinohnsonni Serif'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
-  font 'rotinonhSerif_4_2.ttf'
-  font 'rotinonhSerifb_4_2.ttf'
-  font 'rotinonhSerifbi_4_2.ttf'
-  font 'rotinonhSerifi_4_2.ttf'
+  font "rotinonhSerif_#{version.dots_to_underscores}.ttf"
+  font "rotinonhSerifb_#{version.dots_to_underscores}.ttf"
+  font "rotinonhSerifbi_#{version.dots_to_underscores}.ttf"
+  font "rotinonhSerifi_#{version.dots_to_underscores}.ttf"
 end
