ad6eba0ce7fb71a609cd21e969716fd3c4b336bd
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:03:15 +0100

    Update and rename font-wqy-microhei-lite.rb to font-wenquanyi-micro-hei-lite.rb

---
+++ Casks/font-wenquanyi-micro-hei-lite.rb
@@ -0,0 +1,11 @@
+cask 'font-wenquanyi-micro-hei-lite.rb' do
+  version '0.2.0-beta'
+  sha256 'bf67c2a38ef9919489afae2364496d2eeba2e6799823292d9290f857f3e78abc'
+
+  # downloads.sourceforge.net/wqy was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/wqy/wqy-microhei-lite-#{version}.tar.gz"
+  name 'WenQuanYi Micro Hei Lite'
+  homepage 'http://wenq.org/wqy2/index.cgi?MicroHei'
+
+  font 'wqy-microhei-lite/wqy-microhei-lite.ttc'
+end
--- Casks/font-wqy-microhei-lite.rb
+++
@@ -1,9 +0,0 @@
-cask 'font-wqy-microhei-lite' do
-  version '0.2.0-beta'
-  sha256 'bf67c2a38ef9919489afae2364496d2eeba2e6799823292d9290f857f3e78abc'
-
-  url "http://downloads.sourceforge.net/wqy/wqy-microhei-lite-#{version}.tar.gz"
-  homepage 'http://wenq.org/wqy2/index.cgi?MicroHei'
-
-  font 'wqy-microhei-lite/wqy-microhei-lite.ttc'
-end
