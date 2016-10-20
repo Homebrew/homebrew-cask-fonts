19051fdf2775161975d51fa88147d3a8f7b20ec8
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:33:20 +0100

    Update font-kisiska to latest (#763)

--- Casks/font-kisiska.rb
+++ Casks/font-kisiska.rb
@@ -3,6 +3,7 @@ cask 'font-kisiska' do
   sha256 :no_check
 
   url 'http://www.languagegeek.com/font/kisiska.zip'
+  name 'Kisiska'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
   font 'kisiska.otf'
