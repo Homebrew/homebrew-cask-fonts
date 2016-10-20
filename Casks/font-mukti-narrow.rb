ee4b66f18072031712eb0a22a5c4aa339d5e6e57
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 20:22:04 +0100

    Update and rename font-muktinarrow.rb to font-mukti-narrow.rb

--- Casks/font-mukti-narrow.rb
+++ Casks/font-mukti-narrow.rb
@@ -1,8 +1,9 @@
-cask 'font-muktinarrow' do
+cask 'font-mukti-narrow' do
   version :latest
   sha256 :no_check
 
   url 'http://www.omicronlab.com/download/fonts/muktinarrow.ttf'
+  name 'Mukti Narrow'
   homepage 'http://www.omicronlab.com/bangla-fonts.html'
 
   font 'muktinarrow.ttf'
