ecd4ee3ea85bc9ac9038ccebbf0f122084581afc
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:51:41 +0100

    Update font-lohit-assamese to 2.5.3

--- Casks/font-lohit-assamese.rb
+++ Casks/font-lohit-assamese.rb
@@ -3,6 +3,7 @@ cask 'font-lohit-assamese' do
   sha256 'ce8e510f4e78527eb4c514bf49ed69389b983cd8c4990f0a7b9b9f43b6bef9ce'
 
   url "https://fedorahosted.org/releases/l/o/lohit/lohit-assamese-ttf-#{version}.tar.gz"
+  name 'Lohit Assamese'
   homepage 'https://fedorahosted.org/lohit/'
 
   font "lohit-assamese-ttf-#{version}/Lohit-Assamese.ttf"
