1c178b6540535d9670551ef239b1d96b938af502
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:20:02 +0100

    Update font-everson-mono to 7.0.0

--- Casks/font-everson-mono.rb
+++ Casks/font-everson-mono.rb
@@ -1,15 +1,13 @@
 cask 'font-everson-mono' do
-  version :latest
-  sha256 :no_check
-
-  # version '7.0'
-  # sha256 'd74a8d99342da41716a8ddf51536f79bf5394a6577450a5068be25b1c326b149'
+  version '7.0.0'
+  sha256 'a7196feb79faa1933ecbfc34579586d87addc208b7f6ac9dd93b92d64b0d81f5'
 
   url 'http://www.evertype.com/emono/evermono.zip'
+  name 'Everson Mono'
   homepage 'http://www.evertype.com/emono/'
 
-  font 'evermono-7.0.0/Everson Mono.ttf'
-  font 'evermono-7.0.0/Everson Mono Bold.ttf'
-  font 'evermono-7.0.0/Everson Mono Bold Oblique.ttf'
-  font 'evermono-7.0.0/Everson Mono Oblique.ttf'
+  font "evermono-#{version}/Everson Mono.ttf"
+  font "evermono-#{version}/Everson Mono Bold.ttf"
+  font "evermono-#{version}/Everson Mono Bold Oblique.ttf"
+  font "evermono-#{version}/Everson Mono Oblique.ttf"
 end
