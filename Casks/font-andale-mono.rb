d89759cbd22c1b4ce5da06cc47550104b7361161
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:32:31 +0100

    Update font-andale-mono to 2.00 (#677)

--- Casks/font-andale-mono.rb
+++ Casks/font-andale-mono.rb
@@ -2,7 +2,8 @@ cask 'font-andale-mono' do
   version '2.00'
   sha256 '0524fe42951adc3a7eb870e32f0920313c71f170c859b5f770d82b4ee111e970'
 
-  url 'http://downloads.sourceforge.net/sourceforge/corefonts/andale32.exe'
+  url 'https://downloads.sourceforge.net/corefonts/andale32.exe'
+  name 'Andale Mono'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
 
   depends_on formula: 'cabextract'
