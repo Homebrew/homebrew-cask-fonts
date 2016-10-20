d1723a7708c58a1a026b7b8ddf2e9568229d6fd0
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:44:43 +0100

    Update font-oskidakelh to latest (#813)

--- Casks/font-oskidakelh.rb
+++ Casks/font-oskidakelh.rb
@@ -2,10 +2,8 @@ cask 'font-oskidakelh' do
   version :latest
   sha256 :no_check
 
-  # version '1.000'
-  # sha256 '0e3d7de2055273b845660a938a31edfe518b69caa257bc48bb40fa5162078587'
-
   url 'http://www.languagegeek.com/font/oskidakelh.zip'
+  name 'OskiDakelh'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
   font 'oskidakelh1.ttf'
