e0a4656c84e48d2fea6abfe0e824b14df73df8fe
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 17:12:23 +0100

    Update font-anonymous-pro to 1.002

--- Casks/font-anonymous-pro.rb
+++ Casks/font-anonymous-pro.rb
@@ -2,7 +2,8 @@ cask 'font-anonymous-pro' do
   version '1.002'
   sha256 '86665847a51cdfb58a1e1dfd8b1ba33f183485affe50b53e3304f63d3d3552ab'
 
-  url "http://www.marksimonson.com/assets/content/fonts/AnonymousPro-#{version}.zip"
+  url 'http://www.marksimonson.com/assets/content/fonts/AnonymousPro-1.002.zip'
+  name 'Anonymous Pro'
   homepage 'http://www.marksimonson.com/fonts/view/anonymous-pro'
 
   font "AnonymousPro-#{version}.001/Anonymous Pro B.ttf"
