2a20123f8e3d66b297cdd7e1bf94674add7e3411
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:53:59 +0100

    Update font-tillana to 2.000 (#868)

--- Casks/font-tillana.rb
+++ Casks/font-tillana.rb
@@ -2,9 +2,10 @@ cask 'font-tillana' do
   version '2.000'
   sha256 '26e3926523f8bc63ee344a4d1957b8bbbed55c721103a88fa9b1a678f4761e08'
 
-  url 'https://github.com/itfoundry/tillana/releases/download/v2.000/tillana-2_000.zip'
+  url "https://github.com/itfoundry/tillana/releases/download/v#{version}/tillana-#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/itfoundry/tillana/releases.atom',
           checkpoint: '8b22ba5ebe8499a7756b4454b89ee75019285c791347b535b763122d6c5d8979'
+  name 'Tillana'
   homepage 'https://github.com/itfoundry/tillana'
 
   font 'Tillana-Bold.otf'
