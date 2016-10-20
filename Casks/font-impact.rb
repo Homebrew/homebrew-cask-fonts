43ba364003900330bdd9a233a23782ba367a29b1
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:08:08 +0100

    Update font-impact to 2.35 (#744)

--- Casks/font-impact.rb
+++ Casks/font-impact.rb
@@ -2,7 +2,8 @@ cask 'font-impact' do
   version '2.35'
   sha256 '6061ef3b7401d9642f5dfdb5f2b376aa14663f6275e60a51207ad4facf2fccfb'
 
-  url 'http://downloads.sourceforge.net/sourceforge/corefonts/impact32.exe'
+  url 'https://downloads.sourceforge.net/corefonts/impact32.exe'
+  name 'Impact'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
 
   depends_on formula: 'cabextract'
