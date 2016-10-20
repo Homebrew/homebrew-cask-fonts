d144875f40e0cdaf8ffecc83a0760b854640a817
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:34:57 +0100

    Update font-symbola to latest

--- Casks/font-symbola.rb
+++ Casks/font-symbola.rb
@@ -1,9 +1,9 @@
 cask 'font-symbola' do
-  # version '8.00'
   version :latest
   sha256 :no_check
 
   url 'http://users.teilar.gr/~g1951d/Symbola.zip'
+  name 'Symbola'
   homepage 'http://users.teilar.gr/~g1951d/'
 
   font 'Symbola_hint.ttf'
