3e7cb9e12789c3a4d2c3c92dc24190c5c223b4d6
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:31:28 +0100

    Update font-khand to 2.000

--- Casks/font-khand.rb
+++ Casks/font-khand.rb
@@ -2,9 +2,10 @@ cask 'font-khand' do
   version '2.000'
   sha256 '668910b4cd3487a5bbf93f6dd7259914c1596eec7fef28b08877abdbb7775226'
 
-  url 'https://github.com/itfoundry/khand/releases/download/v2.000/khand-2_000.zip'
+  url "https://github.com/itfoundry/khand/releases/download/v#{version}/khand-#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/itfoundry/khand/releases.atom',
           checkpoint: '7ffb8f16e434382ed985ceea0992822e2ea95ce4ce3c291f00f11e1ed0081e66'
+  name 'Khand'
   homepage 'https://github.com/itfoundry/khand'
 
   font 'Khand-Black.otf'
