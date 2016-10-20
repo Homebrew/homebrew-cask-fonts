6a4fb6d4b6da01fda2eaf57f4836ba5b6270d015
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:39:40 +0100

    Update font-georgia to 2.05 (#734)

--- Casks/font-georgia.rb
+++ Casks/font-georgia.rb
@@ -2,7 +2,8 @@ cask 'font-georgia' do
   version '2.05'
   sha256 '2c2c7dcda6606ea5cf08918fb7cd3f3359e9e84338dc690013f20cd42e930301'
 
-  url 'http://downloads.sourceforge.net/sourceforge/corefonts/georgi32.exe'
+  url 'https://downloads.sourceforge.net/corefonts/georgi32.exe'
+  name 'Georgia'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
 
   depends_on formula: 'cabextract'
