73aff3f1ad8309ba1bfd8883e77bbdbedc34746d
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:45:01 +0100

    Update font-oskidenec to latest (#816)

--- Casks/font-oskidenec.rb
+++ Casks/font-oskidenec.rb
@@ -2,10 +2,8 @@ cask 'font-oskidenec' do
   version :latest
   sha256 :no_check
 
-  # version '1.000'
-  # sha256 '6bae2b488e9f29b50c950aa49298fe6ddf50a35a5d40a740e59f49e0096dec26'
-
   url 'http://www.languagegeek.com/font/OskiDeneC.zip'
+  name 'OskiDeneC'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
   font 'oskidene_c.ttf'
