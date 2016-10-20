37f1d951b2ef701be1c2c17919c6f41d8500996c
David Graham <dgraham@github.com>
2016-10-20 16:01:18 -0600

    Add sha256 for version 4.4.0

--- Casks/font-octicons.rb
+++ Casks/font-octicons.rb
@@ -1,6 +1,6 @@
 cask 'font-octicons' do
   version '4.4.0'
-  sha256 :no_check
+  sha256 '1d3b27b1096c57edb4d391a6b1ec1e672e64b250bec4c86662f11baba988db3a'
 
   url "https://github.com/primer/octicons/archive/v#{version}.zip"
   appcast 'https://github.com/primer/octicons/releases.atom',
