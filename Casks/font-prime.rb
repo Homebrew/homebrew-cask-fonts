25ee716ef0169e2a6aa1e57f354c0b90830b49d9
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:49:23 +0100

    Update font-prime to latest (#829)

--- Casks/font-prime.rb
+++ Casks/font-prime.rb
@@ -3,6 +3,7 @@ cask 'font-prime' do
   sha256 :no_check
 
   url 'http://fontfabric.com/downfont/prime.zip'
+  name 'Prime'
   homepage 'http://fontfabric.com/prime-free-font/'
 
   font 'Prime Light.otf'
