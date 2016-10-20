91d6b89647c5858fac635b4f1fbdcdd0d7749bd2
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 17:54:55 +0100

    Update font-code2002 to latest

--- Casks/font-code2002.rb
+++ Casks/font-code2002.rb
@@ -1,8 +1,9 @@
 cask 'font-code2002' do
-  version '0.917'
-  sha256 '081a5605e1620d1012781829cbee7eeda0c2d81c1fad42520305d6de2b3652f5'
+  version :latest
+  sha256 :no_check
 
   url 'http://www.ffonts.net/Code2002.font.zip'
+  name 'Code2002'
   homepage 'http://www.ffonts.net/Code2002.font.download'
 
   font 'CODE2002.TTF'
