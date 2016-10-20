a281a229657602a58cefbc1d446da9e0c50d702a
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:57:38 +0100

    Update font-han-nom-a to 2005 (#738)

--- Casks/font-han-nom-a.rb
+++ Casks/font-han-nom-a.rb
@@ -1,11 +1,10 @@
 cask 'font-han-nom-a' do
-  version '2.0'
+  version '2005'
   sha256 '428a31ff62e26dc33080789f8d49c39ac529dbfdbcfd50825f78cbd0903a3936'
 
-  # note, there is also a lo-res version of the font, but it carries the same
-  # family name, and there is not yet a working 'conflicts_with' stanza
-  url 'https://downloads.sourceforge.net/project/vietunicode/hannom/hannom%20v2005/hannomH.zip'
-  homepage 'https://vietunicode.sourceforge.net/fonts/fonts_hannom.html'
+  url 'https://downloads.sourceforge.net/vietunicode/hannomH.zip'
+  name 'HAN NOM A'
+  homepage 'https://sourceforge.net/projects/vietunicode/files/hannom/hannom%20v2005/'
 
   font 'HAN NOM A.ttf'
   font 'HAN NOM B.ttf'
