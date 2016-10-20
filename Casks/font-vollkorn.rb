e61a019a01fab8a0e784eedf003320f8cc2c6dad
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:54:11 +0100

    Update font-vollkorn to 3.005 (#873)

--- Casks/font-vollkorn.rb
+++ Casks/font-vollkorn.rb
@@ -2,7 +2,8 @@ cask 'font-vollkorn' do
   version '3.005'
   sha256 '2ba29d185bcf943f844a88546fbc4ec668d53f2564d8aa1c5837ffb9a45f4fab'
 
-  url 'http://vollkorn-typeface.com/download/vollkorn-3-005.zip'
+  url "http://vollkorn-typeface.com/download/vollkorn-#{version.dots_to_hyphens}.zip"
+  name 'Vollkorn'
   homepage 'http://vollkorn-typeface.com/'
 
   font 'PS-OTF/Vollkorn-Bold.otf'
