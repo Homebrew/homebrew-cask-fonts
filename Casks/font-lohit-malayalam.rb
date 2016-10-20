c175bb0c56afd745f2f30a86a6b23e70566ec59d
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:59:11 +0100

    Update font-lohit-malayalam to 2.5.4

--- Casks/font-lohit-malayalam.rb
+++ Casks/font-lohit-malayalam.rb
@@ -3,6 +3,7 @@ cask 'font-lohit-malayalam' do
   sha256 '8c230bbe4cee42d55caa3e548f52dc5f54bae42a35b8eefe575297bd23b2fde4'
 
   url "https://fedorahosted.org/releases/l/o/lohit/lohit-malayalam-ttf-#{version}.tar.gz"
+  name 'Lohit Malayalam'
   homepage 'https://fedorahosted.org/lohit/'
 
   font "lohit-malayalam-ttf-#{version}/Lohit-Malayalam.ttf"
