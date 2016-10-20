053c7ba28ff020559d0d84e709b83e78afea1809
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:52:28 +0100

    Update font-tex-gyre-schola to 2.005 (#864)

--- Casks/font-tex-gyre-schola.rb
+++ Casks/font-tex-gyre-schola.rb
@@ -3,6 +3,7 @@ cask 'font-tex-gyre-schola' do
   sha256 '24063368bfdc1046439e290299157621a437294ecbb39a938d2ddb2afa3daaf8'
 
   url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/schola/qcs#{version}otf.zip"
+  name 'TeX Gyre Schola'
   homepage 'http://www.gust.org.pl/projects/e-foundry/tex-gyre/schola'
 
   font 'texgyreschola-bold.otf'
