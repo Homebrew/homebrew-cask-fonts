9ad7a2f6d954029798ccd3e31a41a6e3794b88e0
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 20:01:31 +0100

    Update font-lohit-oriya to 2.5.4.1

--- Casks/font-lohit-oriya.rb
+++ Casks/font-lohit-oriya.rb
@@ -3,6 +3,7 @@ cask 'font-lohit-oriya' do
   sha256 '96cd1c0c2f13d286907235763b253a65589899ebd124defd6003e9ec9d9d8841'
 
   url "https://fedorahosted.org/releases/l/o/lohit/lohit-oriya-ttf-#{version}.tar.gz"
+  name 'Lohit Oriya'
   homepage 'https://fedorahosted.org/lohit/'
 
   font "lohit-oriya-ttf-#{version}/Lohit-Oriya.ttf"
