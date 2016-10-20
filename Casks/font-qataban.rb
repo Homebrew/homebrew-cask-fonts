2c94e7a12a23af48a8ef54b5cf06b402bfe8c72c
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:50:47 +0100

    Update font-qataban to 1.0 (#834)

--- Casks/font-qataban.rb
+++ Casks/font-qataban.rb
@@ -1,12 +1,10 @@
 cask 'font-qataban' do
-  version :latest
-  sha256 :no_check
-
-  # version '1.0'
-  # sha256 '9f3243f1caa97cd8740c87ea892e8dc15d4efac0546df08ce1df2bc6f5fbdf32'
+  version '1.0'
+  sha256 '9f3243f1caa97cd8740c87ea892e8dc15d4efac0546df08ce1df2bc6f5fbdf32'
 
   url 'http://www.alanwood.net/downloads/qataban.zip'
+  name 'Qataban'
   homepage 'http://www.alanwood.net/downloads/index.html'
 
-  font 'Qataban 1_0.ttf'
+  font "Qataban #{version.dots_to_underscores}.ttf"
 end
