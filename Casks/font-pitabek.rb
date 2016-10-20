50fba19e5a1afb1edf9fb82ef4b20cfe76a97bcc
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 20:59:43 +0100

    Update font-pitabek to latest

--- Casks/font-pitabek.rb
+++ Casks/font-pitabek.rb
@@ -2,10 +2,8 @@ cask 'font-pitabek' do
   version :latest
   sha256 :no_check
 
-  # version '1.000'
-  # sha256 '0ee433091364fc10c9bbcc77fd4007e2d8aa4901e926741256be6b37b848cd6a'
-
   url 'http://www.languagegeek.com/font/pitabek.zip'
+  name 'Pitabek'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
   font 'pitabek.ttf'
