ec829ed306b713639d6207b1a523c2e1d86e2098
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 20:37:27 +0100

    Update font-norwester to 1.2 (#804)

--- Casks/font-norwester.rb
+++ Casks/font-norwester.rb
@@ -1,9 +1,10 @@
 cask 'font-norwester' do
-  version '1.002'
+  version '1.2'
   sha256 '4da457006bd6f058cca990f1b15d130b2e9af16974c0dd172391de7ab446cf6b'
 
   url 'http://jamiewilson.io/norwester/assets/norwester.zip'
+  name 'Norwester'
   homepage 'http://jamiewilson.io/norwester/'
 
-  font 'norwester-v1.2/norwester.otf'
+  font "norwester-v#{version}/norwester.otf"
 end
