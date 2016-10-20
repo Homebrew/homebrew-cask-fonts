161daf4596485e0c80b71e56806a0f7d34587327
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 17:42:59 +0100

    Update font-camingocode to 1.000

--- Casks/font-camingocode.rb
+++ Casks/font-camingocode.rb
@@ -3,6 +3,7 @@ cask 'font-camingocode' do
   sha256 '13b188ca8d1b5fe2c3ba880771f20a6faac550405ae98d12edd46240f8896714'
 
   url 'http://janfromm.de/_data/downloads/CamingoCode-v1.0.zip'
+  name 'CamingoCode'
   homepage 'http://www.janfromm.de/typefaces/camingomono/camingocode/'
 
   font 'CamingoCode-Bold.ttf'
