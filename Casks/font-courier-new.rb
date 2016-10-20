d759998d8ac871d33472d124f228bffa71dcec97
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:34:29 +0100

    Update font-courier-new to 2.82 (#708)

--- Casks/font-courier-new.rb
+++ Casks/font-courier-new.rb
@@ -2,7 +2,8 @@ cask 'font-courier-new' do
   version '2.82'
   sha256 'bb511d861655dde879ae552eb86b134d6fae67cb58502e6ff73ec5d9151f3384'
 
-  url 'http://downloads.sourceforge.net/sourceforge/corefonts/courie32.exe'
+  url 'https://downloads.sourceforge.net/corefonts/courie32.exe'
+  name 'Courier New'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
 
   depends_on formula: 'cabextract'
