ead100dbdc5928caccf79d6ff068d65a785d38d0
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:31:16 +0100

    Update font-aboriginal-serif to latest (#667)

--- Casks/font-aboriginal-serif.rb
+++ Casks/font-aboriginal-serif.rb
@@ -2,10 +2,8 @@ cask 'font-aboriginal-serif' do
   version :latest
   sha256 :no_check
 
-  # version '9.606'
-  # sha256 '04e8548af9a7633f26439be61ba7cf04577361537ca4eaa7471279325680c7dd'
-
   url 'http://www.languagegeek.com/font/AboriginalSerif.zip'
+  name 'Aboriginal Serif'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
   font 'AboriginalSerifREGULAR.ttf'
