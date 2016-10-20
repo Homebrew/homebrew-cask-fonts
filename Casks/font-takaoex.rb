f1b775491f4c159de40ab0859a7fbd2bb00fa639
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:36:32 +0100

    Update font-takaoex to 00201.01

--- Casks/font-takaoex.rb
+++ Casks/font-takaoex.rb
@@ -3,6 +3,7 @@ cask 'font-takaoex' do
   sha256 '6af43928cb4d9943cbd5565079d1dfed60aef20badf4e99c3a1edc0e6a07d42b'
 
   url "https://launchpad.net/takao-fonts/trunk/15.03/+download/TakaoExFonts_#{version}.zip"
+  name 'TakaoEx'
   homepage 'https://launchpad.net/takao-fonts'
 
   font 'TakaoExGothic.ttf'
