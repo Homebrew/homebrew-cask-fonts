07ee9f2093233e6ce8c9684101fa04ee7b6a4b61
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:56:39 +0100

    Update font-lohit-devanagari to 2.93.0

--- Casks/font-lohit-devanagari.rb
+++ Casks/font-lohit-devanagari.rb
@@ -3,6 +3,7 @@ cask 'font-lohit-devanagari' do
   sha256 '0ee1cc070c0f378d4d35046514712a72a2ec0da6b7ed6cdc00d5ef815a164708'
 
   url "https://fedorahosted.org/releases/l/o/lohit/lohit-devanagari-ttf-#{version}.tar.gz"
+  name 'Lohit Devanagari'
   homepage 'https://fedorahosted.org/lohit/'
 
   font "lohit-devanagari-ttf-#{version}/Lohit-Devanagari.ttf"
