4eff4af74738ecabdc311bc5efc10603c193a0b1
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:42:36 +0100

    Update font-tex-gyre-cursor to 2.004

--- Casks/font-tex-gyre-cursor.rb
+++ Casks/font-tex-gyre-cursor.rb
@@ -3,6 +3,7 @@ cask 'font-tex-gyre-cursor' do
   sha256 'ae8db1c134ec5c1b8c3999116b0610a5ad1c6f47520c3b4712b9bc914458dce9'
 
   url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/cursor/qcr#{version}otf.zip"
+  name 'TeX Gyre Cursor'
   homepage 'http://www.gust.org.pl/projects/e-foundry/tex-gyre/cursor'
 
   font 'texgyrecursor-bold.otf'
