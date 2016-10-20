f56567076ae8e13a5fce700881e408d7708f9712
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:34:24 +0100

    Update font-laila to 2.000

--- Casks/font-laila.rb
+++ Casks/font-laila.rb
@@ -2,9 +2,10 @@ cask 'font-laila' do
   version '2.000'
   sha256 'caa87cae373702288a7510c74881077aa15e3b5c94a4746b61f3f977d8499f73'
 
-  url 'https://github.com/itfoundry/laila/releases/download/v2.000/laila-2_000.zip'
+  url "https://github.com/itfoundry/laila/releases/download/v#{version}/laila-#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/itfoundry/laila/releases.atom',
           checkpoint: '1a268b0686377b36db6e935f5fde881e6003194c09c30308fc4e9a4029a5c3eb'
+  name 'Laila'
   homepage 'https://github.com/itfoundry/laila'
 
   font 'Laila-Bold.otf'
