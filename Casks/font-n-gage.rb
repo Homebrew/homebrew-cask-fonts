0340001f02441b0fa9b351af5322ead45d515a7e
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 20:31:37 +0100

    Update font-n-gage to latest (#797)

--- Casks/font-n-gage.rb
+++ Casks/font-n-gage.rb
@@ -3,7 +3,8 @@ cask 'font-n-gage' do
   sha256 :no_check
 
   url 'http://dl.dafont.com/dl/?f=n_gage'
-  homepage 'http://www.alejandroconde.com/'
+  name 'N-Gage'
+  homepage 'http://www.dafont.com/n-gage.font'
 
   font 'N-Gage.ttf'
 end
