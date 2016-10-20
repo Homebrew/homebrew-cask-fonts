a666364c8eb43e0b04245e16de710340607ad611
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 20:02:42 +0100

    Update font-lohit-tamil-classical to 2.5.3 (#784)

--- Casks/font-lohit-tamil-classical.rb
+++ Casks/font-lohit-tamil-classical.rb
@@ -3,6 +3,7 @@ cask 'font-lohit-tamil-classical' do
   sha256 '325ea1496bb2ae4f77552c268190251a5155717dccda64d497da4388d17c2432'
 
   url "https://fedorahosted.org/releases/l/o/lohit/lohit-tamil-classical-ttf-#{version}.tar.gz"
+  name 'Lohit Tamil Classical'
   homepage 'https://fedorahosted.org/lohit/'
 
   font "lohit-tamil-classical-ttf-#{version}/Lohit-Tamil-Classical.ttf"
