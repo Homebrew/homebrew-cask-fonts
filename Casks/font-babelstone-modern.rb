442594f25d39f04d1f616b257670a86ad0316fa0
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 17:16:59 +0100

    Update font-babelstone-modern to latest

--- Casks/font-babelstone-modern.rb
+++ Casks/font-babelstone-modern.rb
@@ -2,10 +2,8 @@ cask 'font-babelstone-modern' do
   version :latest
   sha256 :no_check
 
-  # version '6.002'
-  # sha256 '65cd40476a970f91a4813aded21beb8c4b6aa708c627e1cc3a7753d898fdb60a'
-
   url 'http://www.babelstone.co.uk/Fonts/BabelStoneModern.ttf'
+  name 'BabelStone Modern'
   homepage 'http://www.babelstone.co.uk/Fonts/'
 
   font 'BabelStoneModern.ttf'
