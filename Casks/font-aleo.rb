e8165b4f01a91803d2f5b1800b13c18962344e5a
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 17:01:16 +0100

    Update font-aleo to 1.2.2

--- Casks/font-aleo.rb
+++ Casks/font-aleo.rb
@@ -3,6 +3,7 @@ cask 'font-aleo' do
   sha256 '64e98e1063da945086b4685535f0b3d463efbc1409e18bddf925624bf27b7e57'
 
   url 'http://fontfabric.com/downfont/aleo.zip'
+  name 'Aleo'
   homepage 'http://fontfabric.com/aleo-free-font/'
 
   font 'Aleo_font_v1.2.2/Desktop OTF/Aleo-Bold.otf'
