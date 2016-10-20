1dcb549c8af89ac13a7a7777bd302717b4076b6c
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:40:21 +0100

    Update font-tex-gyre-adventor to 2.003

--- Casks/font-tex-gyre-adventor.rb
+++ Casks/font-tex-gyre-adventor.rb
@@ -3,6 +3,7 @@ cask 'font-tex-gyre-adventor' do
   sha256 '7c115d00ff2b498fdad6ab787b4401b1e6ffe9e2f05e03614c47bb0cf0c47266'
 
   url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor/qag#{version}otf.zip"
+  name 'TeX Gyre Adventor'
   homepage 'http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor'
 
   font 'texgyreadventor-bold.otf'
