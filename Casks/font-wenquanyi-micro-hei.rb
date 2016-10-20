3a1cac78b5a5c0d32fce2177a138acf4aa2c770c
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:04:14 +0100

    Rename font-wenquanyi-micro-hei-lite.rb to font-wenquanyi-micro-hei.rb

--- Casks/font-wenquanyi-micro-hei.rb
+++ Casks/font-wenquanyi-micro-hei.rb
@@ -1,8 +1,10 @@
-cask 'font-wqy-microhei' do
+cask 'font-wenquanyi-micro-hei' do
   version '0.2.0-beta'
   sha256 '2802ac8023aa36a66ea6e7445854e3a078d377ffff42169341bd237871f7213e'
 
+  # downloads.sourceforge.net/wqy was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/wqy/wqy-microhei-#{version}.tar.gz"
+  name 'WenQuanYi Micro Hei'
   homepage 'http://wenq.org/wqy2/index.cgi?MicroHei'
 
   font 'wqy-microhei/wqy-microhei.ttc'
