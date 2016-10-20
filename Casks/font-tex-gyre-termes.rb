66c2242e81b34421e2b743588841226ce2508eba
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:44:27 +0100

    Update font-tex-gyre-termes to 2.004

--- Casks/font-tex-gyre-termes.rb
+++ Casks/font-tex-gyre-termes.rb
@@ -3,6 +3,7 @@ cask 'font-tex-gyre-termes' do
   sha256 '5d467d8db17c96037b04409d682f071d7cc33cf94eda35a0a0465776a2c862b2'
 
   url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/termes/qtm#{version}otf.zip"
+  name 'TeX Gyre Termes'
   homepage 'http://www.gust.org.pl/projects/e-foundry/tex-gyre/termes'
 
   font 'texgyretermes-bold.otf'
