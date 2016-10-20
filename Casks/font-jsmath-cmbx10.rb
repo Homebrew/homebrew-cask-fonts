2a087886606b136dba2e0d6e96ea2b626fcda7ef
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:15:30 +0100

    Update and rename font-jsmath.rb to font-jsmath-cmbx10.rb

--- Casks/font-jsmath-cmbx10.rb
+++ Casks/font-jsmath-cmbx10.rb
@@ -1,8 +1,9 @@
-cask 'font-jsmath' do
+cask 'font-jsmath-cmbx10' do
   version :latest
   sha256 :no_check
 
   url 'http://www.math.union.edu/~dpvc/jsmath/download/TeX-fonts-10.zip'
+  name 'jsMath-cmbx10'
   homepage 'http://www.math.union.edu/~dpvc/jsmath/download/jsmath-fonts.html'
 
   font 'TeX-fonts-10/jsMath-cmbx10.ttf'
