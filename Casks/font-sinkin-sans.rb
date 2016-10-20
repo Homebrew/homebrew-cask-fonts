d6e61949523409e414b14e249e8e140c0d210459
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:48:10 +0100

    Update font-sinkin-sans to latest (#852)

--- Casks/font-sinkin-sans.rb
+++ Casks/font-sinkin-sans.rb
@@ -1,8 +1,9 @@
 cask 'font-sinkin-sans' do
-  version '1.000'
-  sha256 '65a681a573b6af4d173466a14a9b39c93eb45f6878e1c1132b861b12f6166135'
+  version :latest
+  sha256 :no_check
 
   url 'http://www.fontsquirrel.com/fonts/download/sinkin-sans'
+  name 'Sinkin Sans'
   homepage 'http://www.fontsquirrel.com/fonts/sinkin-sans'
 
   font 'SinkinSans-100Thin.otf'
