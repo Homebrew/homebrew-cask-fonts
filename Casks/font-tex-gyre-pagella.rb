0d85751fc3188571f156872ecdc75357a55a3bc3
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:43:22 +0100

    Update font-tex-gyre-pagella to 2.004

--- Casks/font-tex-gyre-pagella.rb
+++ Casks/font-tex-gyre-pagella.rb
@@ -3,6 +3,7 @@ cask 'font-tex-gyre-pagella' do
   sha256 'e241b65216b0226ea787b5d7f9f06e9fd1e1013f6243ebc69aeb57c3ca006d78'
 
   url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/qpl#{version}otf.zip"
+  name 'TeX Gyre Pagella'
   homepage 'http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella'
 
   font 'texgyrepagella-bold.otf'
