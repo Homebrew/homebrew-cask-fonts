1190755e3473d6447386aabba4804d21e361dfe2
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:58:44 +0100

    Update font-lohit-kannada to 2.5.3

--- Casks/font-lohit-kannada.rb
+++ Casks/font-lohit-kannada.rb
@@ -3,6 +3,7 @@ cask 'font-lohit-kannada' do
   sha256 'f1a6b58a70720e03cce8f6772e8eb87dc12eff8fdf36173c88aef2157e592ef5'
 
   url "https://fedorahosted.org/releases/l/o/lohit/lohit-kannada-ttf-#{version}.tar.gz"
+  name 'Lohit Kannada'
   homepage 'https://fedorahosted.org/lohit/'
 
   font "lohit-kannada-ttf-#{version}/Lohit-Kannada.ttf"
