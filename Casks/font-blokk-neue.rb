9a240ebc432fc9c28b5dc8bff5f1f3ead3e196f6
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 17:37:30 +0100

    Update and rename font-blokk.rb to font-blokk-neue.rb

---
+++ Casks/font-blokk-neue.rb
@@ -0,0 +1,15 @@
+cask 'font-blokk-neue' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://www.blokkfont.com/cdn/v2/BLOKKNeue-Regular.zip'
+  name 'BLOKK Neue'
+  homepage 'http://blokkfont.com/'
+
+  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.eot'
+  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.otf'
+  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.svg'
+  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.ttf'
+  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.woff'
+  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.woff2'
+end
