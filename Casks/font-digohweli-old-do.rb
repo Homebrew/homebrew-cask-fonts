81dd3df0bfc4e7c14f9fd0521e580d76a52c0b59
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:05:44 +0100

    Update font-digohweli-old-do to latest

--- Casks/font-digohweli-old-do.rb
+++ Casks/font-digohweli-old-do.rb
@@ -2,10 +2,8 @@ cask 'font-digohweli-old-do' do
   version :latest
   sha256 :no_check
 
-  # version '1.615'
-  # sha256 '3946991311f4fedac9f3cd44fe27c8beaa5c24c5ec8b2a3644ab3b7d1ff7ba6e'
-
   url 'http://www.languagegeek.com/font/OldDODigohweli.zip'
+  name 'Digohweli Old DO'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
   font 'OldDODigohweli.ttf'
