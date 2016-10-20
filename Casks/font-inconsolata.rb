f297c211d2bd52659519ea9fda5166a506020ebd
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:12:48 +0100

    Update font-inconsolata to latest (#750)

--- Casks/font-inconsolata.rb
+++ Casks/font-inconsolata.rb
@@ -1,9 +1,9 @@
 cask 'font-inconsolata' do
-  # version '001.010'
   version :latest
   sha256 :no_check
 
   url 'http://levien.com/type/myfonts/Inconsolata.otf'
+  name 'Inconsolata'
   homepage 'http://levien.com/type/myfonts/inconsolata.html'
 
   font 'Inconsolata.otf'
