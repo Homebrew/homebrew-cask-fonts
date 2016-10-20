41c5e98c122dfc17c5112c8418db83e6d6516ba1
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 20:02:00 +0100

    Update font-lohit-punjabi to 2.5.3 (#783)

--- Casks/font-lohit-punjabi.rb
+++ Casks/font-lohit-punjabi.rb
@@ -3,6 +3,7 @@ cask 'font-lohit-punjabi' do
   sha256 'd14143d1cbb9d6a72385251875b5b903f95dfbfed9f69e93568f6ab6dfd557bd'
 
   url "https://fedorahosted.org/releases/l/o/lohit/lohit-punjabi-ttf-#{version}.tar.gz"
+  name 'Lohit Punjabi'
   homepage 'https://fedorahosted.org/lohit/'
 
   font "lohit-punjabi-ttf-#{version}/Lohit-Punjabi.ttf"
