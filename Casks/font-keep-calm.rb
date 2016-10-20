d3645f6b28c7b16ae2a822e1c66f1acf93537c71
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:30:23 +0100

    Update font-keep-calm to latest (#760)

--- Casks/font-keep-calm.rb
+++ Casks/font-keep-calm.rb
@@ -3,6 +3,7 @@ cask 'font-keep-calm' do
   sha256 :no_check
 
   url 'http://dl.1001fonts.com/keep-calm.zip'
+  name 'Keep Calm'
   homepage 'http://www.1001fonts.com/keep-calm-font.html'
 
   font 'Keep Calm Medium – Personal Use/KeepCalm-Medium.ttf'
