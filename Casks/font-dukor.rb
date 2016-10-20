482a2e88f20206630f2b99f887beff1e35708607
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:39:04 +0100

    Update font-dukor to 1.0.7 (#723)

--- Casks/font-dukor.rb
+++ Casks/font-dukor.rb
@@ -1,10 +1,11 @@
 cask 'font-dukor' do
-  version '1.007'
+  version '1.0.7'
   sha256 '15933e2f71c4db80f55189ee05957e87af26e517333b33df7b2c4b528a2848f4'
 
   url 'http://www.evertype.com/fonts/vai/dukorfont.zip'
+  name 'Dukor'
   homepage 'http://www.evertype.com/fonts/vai/'
 
-  font 'Dukor-1.0.7/Dukor.ttf'
-  font 'Dukor-1.0.7/DukorItalic.ttf'
+  font "Dukor-#{version}/Dukor.ttf"
+  font "Dukor-#{version}/DukorItalic.ttf"
 end
