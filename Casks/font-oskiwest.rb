231246d8926c56f3cf93e965dcb505ff9ac22dba
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:45:08 +0100

    Update font-oskiwest to latest (#819)

--- Casks/font-oskiwest.rb
+++ Casks/font-oskiwest.rb
@@ -2,10 +2,8 @@ cask 'font-oskiwest' do
   version :latest
   sha256 :no_check
 
-  # version '2.200'
-  # sha256 'f8d46508e28668fb3d107998fa79a0eedfab7b002b9ea6b2349035c6a4325232'
-
   url 'http://www.languagegeek.com/font/oskiwest.zip'
+  name 'OskiWest'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
   font 'oskiw.ttf'
