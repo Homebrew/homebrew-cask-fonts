eee96b9055fdce3a897a727ef4ba5dac8acc4178
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:45:17 +0100

    Update font-linux-biolinum to 5.3.0,2012_07_02

--- Casks/font-linux-biolinum.rb
+++ Casks/font-linux-biolinum.rb
@@ -1,8 +1,10 @@
 cask 'font-linux-biolinum' do
-  version '5.3.0'
+  version '5.3.0,2012_07_02'
   sha256 '24a593a949808d976850131a953c0c0d7a72299531dfbb348191964cc038d75d'
 
-  url 'http://downloads.sourceforge.net/project/linuxlibertine/linuxlibertine/5.3.0/LinLibertineTTF_5.3.0_2012_07_02.tgz'
+  # downloads.sourceforge.net/linuxlibertine was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/linuxlibertine/LinLibertineTTF_#{version.before_comma}_#{version.after_comma}.tgz"
+  name 'Linux Biolinum'
   homepage 'http://linuxlibertine.org'
 
   font 'LinBiolinum_Kah.ttf'
