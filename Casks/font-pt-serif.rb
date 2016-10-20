55a91e814c054a206d8e321ecae4b68a4415ca45
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:50:53 +0100

    Update font-pt-serif to latest (#833)

--- Casks/font-pt-serif.rb
+++ Casks/font-pt-serif.rb
@@ -1,8 +1,9 @@
 cask 'font-pt-serif' do
-  version '1.002'
-  sha256 '7a6f5fd596307a3f1b145c2383f7ae61abc417e7f8d5ba068295ef1c182a7474'
+  version :latest
+  sha256 :no_check
 
   url 'http://www.paratype.com/uni/public/PTSerif.zip'
+  name 'PT Serif'
   homepage 'http://www.paratype.com/public/'
 
   font 'PTF55F.ttf'
