abe9aaed7d00e3a2229fae0cd5cedbf37b04afdf
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:44:51 +0100

    Update font-oskidenea to latest (#814)

--- Casks/font-oskidenea.rb
+++ Casks/font-oskidenea.rb
@@ -2,10 +2,8 @@ cask 'font-oskidenea' do
   version :latest
   sha256 :no_check
 
-  # version '1.000'
-  # sha256 '7f19f39b6d7276b0e2425d670188772c55adb1b963c01fa103f5dba8b506d4e5'
-
   url 'http://www.languagegeek.com/font/OskiDeneA.zip'
+  name 'OskiDeneA'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
   font 'OskiDeneA.ttf'
