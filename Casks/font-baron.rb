fe7f0fc62700b4307ec52f77c0fc54b4fb67ad95
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:36:16 +0100

    Update font-baron to latest (#685)

--- Casks/font-baron.rb
+++ Casks/font-baron.rb
@@ -1,8 +1,9 @@
 cask 'font-baron' do
-  version '1.00'
-  sha256 '70a7ade63a79e3c90227fff890664976a50f40bd84ef4d7f97871eb8532b880c'
+  version :latest
+  sha256 :no_check
 
   url 'http://www.fontfabric.com/downfont/baron.zip'
+  name 'Baron'
   homepage 'http://www.fontfabric.com/baron-free-font/'
 
   font 'Baron Neue Black Italic.otf'
