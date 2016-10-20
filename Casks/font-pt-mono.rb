d1ee664fab663a9ba7c1ed798b30ea9d3b7315ca
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 21:07:08 +0100

    Update font-pt-mono to latest

--- Casks/font-pt-mono.rb
+++ Casks/font-pt-mono.rb
@@ -1,8 +1,9 @@
 cask 'font-pt-mono' do
-  version '1.003'
-  sha256 '741b1457066d69f045ac5196a512c2c1ca7900645b97bcb7280576d34e540af3'
+  version :latest
+  sha256 :no_check
 
   url 'http://www.paratype.com/uni/public/PTMono.zip'
+  name 'PT Mono'
   homepage 'http://www.paratype.com/public/'
 
   font 'PTM55F.ttf'
