0f3d5f2e852958ffafe2a82d5a186806e6fbad89
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:37:29 +0100

    Update font-geo-sans-light to latest (#733)

--- Casks/font-geo-sans-light.rb
+++ Casks/font-geo-sans-light.rb
@@ -3,6 +3,7 @@ cask 'font-geo-sans-light' do
   sha256 :no_check
 
   url 'http://moorstation.org/typoasis/designers/klein03/text02/pc/GeosansLight.zip'
+  name 'Geo Sans Light'
   homepage 'http://moorstation.org/typoasis/designers/klein03/text02/geosanslite.htm'
 
   font 'GeosansLight.ttf'
