3bf363709e05e45ade7317908caf905430c62e9f
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:28:13 +0100

    Update font-freesans to 20120503

--- Casks/font-freesans.rb
+++ Casks/font-freesans.rb
@@ -1,20 +1,21 @@
 cask 'font-freesans' do
-  version '0412.2268'
+  version '20120503'
   sha256 '3a6c51868c71b006c33c4bcde63d90927e6fcca8f51c965b8ad62d021614a860'
 
-  url 'http://ftp.gnu.org/gnu/freefont/freefont-otf-20120503.tar.gz'
+  url "http://ftp.gnu.org/gnu/freefont/freefont-otf-#{version}.tar.gz"
+  name 'FreeSans'
   homepage 'http://ftp.gnu.org/gnu/freefont/'
 
-  font 'freefont-20120503/FreeMono.otf'
-  font 'freefont-20120503/FreeMonoBold.otf'
-  font 'freefont-20120503/FreeMonoBoldOblique.otf'
-  font 'freefont-20120503/FreeMonoOblique.otf'
-  font 'freefont-20120503/FreeSans.otf'
-  font 'freefont-20120503/FreeSansBold.otf'
-  font 'freefont-20120503/FreeSansBoldOblique.otf'
-  font 'freefont-20120503/FreeSansOblique.otf'
-  font 'freefont-20120503/FreeSerif.otf'
-  font 'freefont-20120503/FreeSerifBold.otf'
-  font 'freefont-20120503/FreeSerifBoldItalic.otf'
-  font 'freefont-20120503/FreeSerifItalic.otf'
+  font "freefont-#{version}/FreeMono.otf"
+  font "freefont-#{version}/FreeMonoBold.otf"
+  font "freefont-#{version}/FreeMonoBoldOblique.otf"
+  font "freefont-#{version}/FreeMonoOblique.otf"
+  font "freefont-#{version}/FreeSans.otf"
+  font "freefont-#{version}/FreeSansBold.otf"
+  font "freefont-#{version}/FreeSansBoldOblique.otf"
+  font "freefont-#{version}/FreeSansOblique.otf"
+  font "freefont-#{version}/FreeSerif.otf"
+  font "freefont-#{version}/FreeSerifBold.otf"
+  font "freefont-#{version}/FreeSerifBoldItalic.otf"
+  font "freefont-#{version}/FreeSerifItalic.otf"
 end
