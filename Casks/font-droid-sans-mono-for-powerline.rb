9de3b8060d66515b030874ea0c18a0f953505cbb
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:11:11 +0100

    Update font-droid-sans-mono-for-powerline to latest

--- Casks/font-droid-sans-mono-for-powerline.rb
+++ Casks/font-droid-sans-mono-for-powerline.rb
@@ -5,6 +5,7 @@ cask 'font-droid-sans-mono-for-powerline' do
   url 'https://github.com/powerline/fonts/trunk/DroidSansMono',
       using:      :svn,
       trust_cert: true
+  name 'Droid Sans Mono for Powerline'
   homepage 'https://github.com/powerline/fonts/tree/master/DroidSansMono'
 
   font 'Droid Sans Mono for Powerline.otf'
