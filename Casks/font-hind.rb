e7cea875a8b3ec4471562eec08fc6ef62e0b26a6
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:07:11 +0100

    Update font-hind to 2.000 (#742)

--- Casks/font-hind.rb
+++ Casks/font-hind.rb
@@ -2,9 +2,10 @@ cask 'font-hind' do
   version '2.000'
   sha256 '8748ce1fa0db67d0c782d7824a9fdcf0b8544b9d063db477dff9733774571193'
 
-  url 'https://github.com/itfoundry/hind/releases/download/v2.000/hind-2_000.zip'
+  url "https://github.com/itfoundry/hind/releases/download/v2.000/hind-#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/itfoundry/hind/releases.atom',
           checkpoint: '017f74f380a4da8c683f65c195a11e17b6454bd53fe5ab757aa6d3e03b6a0bf4'
+  name 'Hind'
   homepage 'https://github.com/itfoundry/hind'
 
   font 'Hind-Bold.otf'
