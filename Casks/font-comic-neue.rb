c1c94846d93cbb7f2e57470ea5225d9e15d8da6a
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 17:55:32 +0100

    Update font-comic-neue to 2.2

--- Casks/font-comic-neue.rb
+++ Casks/font-comic-neue.rb
@@ -3,6 +3,7 @@ cask 'font-comic-neue' do
   sha256 'd67c7de093b3555a525a29db1999b7759723208f514f5fca117201d63b94b5b6'
 
   url "http://comicneue.com/comic-neue-#{version}.zip"
+  name 'Comic Neue'
   homepage 'http://comicneue.com/'
 
   font "comic-neue-#{version}/OTF/ComicNeue-Angular-Regular.otf"
