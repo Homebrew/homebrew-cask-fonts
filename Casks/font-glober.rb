00e4784c7da17da3b97e767121c8a888db00e111
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:37:24 +0100

    Update font-glober to latest (#735)

--- Casks/font-glober.rb
+++ Casks/font-glober.rb
@@ -3,6 +3,7 @@ cask 'font-glober' do
   sha256 :no_check
 
   url 'http://fontfabric.com/downfont/glober.zip'
+  name 'Gobler'
   homepage 'http://fontfabric.com/glober-free-font/'
 
   font 'Glober SemiBold Free.otf'
