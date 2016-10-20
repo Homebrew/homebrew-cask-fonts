7c15ba4489c377f5d5907f4eac1c5ccf6918a6d3
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 17:00:52 +0100

    Update font-alef to latest

--- Casks/font-alef.rb
+++ Casks/font-alef.rb
@@ -1,9 +1,9 @@
 cask 'font-alef' do
-  # version '1.001'
   version :latest
   sha256 :no_check
 
   url 'http://alef.hagilda.com/Alef.zip'
+  name 'Alef'
   homepage 'http://alef.hagilda.com/'
 
   font 'TTF/Alef-Bold.ttf'
