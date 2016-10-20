167b2f5e80ac4d5ab33a25595146303f0fc3aae4
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:34:25 +0100

    Update font-constructium to latest (#707)

--- Casks/font-constructium.rb
+++ Casks/font-constructium.rb
@@ -3,6 +3,7 @@ cask 'font-constructium' do
   sha256 :no_check
 
   url 'http://www.kreativekorp.com/swdownload/fonts/xlang/constructium.zip'
+  name 'Constructium'
   homepage 'http://www.kreativekorp.com/software/fonts/constructium.shtml'
 
   font 'Constructium.ttf'
