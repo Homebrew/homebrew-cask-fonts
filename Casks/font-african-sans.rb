afef902e060f8dd380745639d335ecb2a74db92b
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:31:32 +0100

    Update font-african-sans to 9.380 (#669)

--- Casks/font-african-sans.rb
+++ Casks/font-african-sans.rb
@@ -3,6 +3,7 @@ cask 'font-african-sans' do
   sha256 '66176e5517ace047affc23ca7b7137018c907eee00b113666a671f31254ac7a4'
 
   url 'http://www.languagegeek.com/font/AfricanSans.zip'
+  name 'African Sans'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
   font 'African Sans REGULAR 938.ttf'
