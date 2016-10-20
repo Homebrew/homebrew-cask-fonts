134e7128ddf5c2291f6d8a085a34964f55046133
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:35:28 +0100

    Update font-tai-le-valentinium to latest

--- Casks/font-tai-le-valentinium.rb
+++ Casks/font-tai-le-valentinium.rb
@@ -2,10 +2,8 @@ cask 'font-tai-le-valentinium' do
   version :latest
   sha256 :no_check
 
-  # version '1.11'
-  # sha256 '6ef203f75f692ded9aef9b80df16c31cfe9ba5dd167b4156d81f655e2dc29ab0'
-
   url 'http://www.alanwood.net/downloads/tai-le-valentinium.zip'
+  name 'Tai Le Valentinium'
   homepage 'http://www.alanwood.net/downloads/index.html'
 
   font 'taileval.ttf'
