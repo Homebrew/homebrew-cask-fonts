1d63e161354e3ba999b8355de652a98aaa92de6f
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:59:47 +0100

    Update font-lohit-marathi to 2.93.0

--- Casks/font-lohit-marathi.rb
+++ Casks/font-lohit-marathi.rb
@@ -3,6 +3,7 @@ cask 'font-lohit-marathi' do
   sha256 '4b45a36995f1df69a5d80ab8acca31e0e36ae8ed5a33f73690d5be959a6aa543'
 
   url "https://fedorahosted.org/releases/l/o/lohit/lohit-marathi-ttf-#{version}.tar.gz"
+  name 'Lohit Marathi'
   homepage 'https://fedorahosted.org/lohit/'
 
   font "lohit-marathi-ttf-#{version}/Lohit-Marathi.ttf"
