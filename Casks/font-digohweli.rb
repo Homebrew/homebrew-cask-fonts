33cc38a998cdbed21915e7b059ce5d52d7795ef5
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:38:46 +0100

    Update font-digohweli to 1.7 (#714)

--- Casks/font-digohweli.rb
+++ Casks/font-digohweli.rb
@@ -1,9 +1,10 @@
 cask 'font-digohweli' do
-  version '1.700'
+  version '1.7'
   sha256 'a17b6af34e0d96daf933b42f07fc382ae49f8abf66ccd2f397088c2d6672b77f'
 
   url 'http://www.languagegeek.com/font/Digohweli.zip'
+  name 'Digohweli'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
-  font 'Digohweli_1_7.ttf'
+  font "Digohweli_#{version.dots_to_underscores}.ttf"
 end
