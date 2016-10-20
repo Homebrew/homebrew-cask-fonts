edc43362f25b39aea7cd7b012bc9b4919eaeddd4
David Graham <dgraham@github.com>
2016-10-20 15:51:09 -0600

    Add appcast URL

--- Casks/font-octicons.rb
+++ Casks/font-octicons.rb
@@ -1,8 +1,10 @@
 cask 'font-octicons' do
-  version "4.4.0"
+  version '4.4.0'
   sha256 :no_check
 
   url "https://github.com/primer/octicons/archive/v#{version}.zip"
+  appcast 'https://github.com/primer/octicons/releases.atom',
+          checkpoint: 'af35fc21e4adb5ed1798748d12db7a1ba1d68f486bbbe30841af3d1319b7fbfb'
   name 'Octicons'
   homepage 'https://octicons.github.com'
 
