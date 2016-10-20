6a1c41c271b5586700b50faf19482692aeff2267
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:36:27 +0100

    Update font-batak-unicode to latest (#686)

--- Casks/font-batak-unicode.rb
+++ Casks/font-batak-unicode.rb
@@ -3,6 +3,7 @@ cask 'font-batak-unicode' do
   sha256 :no_check
 
   url 'http://ulikozok.com/fontfiles/Batak-Unicode-Regular.otf'
+  name 'Batak'
   homepage 'http://ulikozok.com/aksara-batak/batak-font/'
 
   font 'Batak-Unicode-Regular.otf'
