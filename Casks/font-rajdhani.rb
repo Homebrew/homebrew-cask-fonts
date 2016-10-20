a9c5822448fe9f5587f1f9868dfb8439658f0e68
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:48:57 +0100

    Update font-rajdhani to 2.000 (#836)

--- Casks/font-rajdhani.rb
+++ Casks/font-rajdhani.rb
@@ -2,9 +2,10 @@ cask 'font-rajdhani' do
   version '2.000'
   sha256 '2dd0b14ed9989319fde8518e4b596467ae635571ffe7733d7968201d8cdeb9cf'
 
-  url 'https://github.com/itfoundry/rajdhani/releases/download/v2.000/rajdhani-2_000.zip'
+  url "https://github.com/itfoundry/rajdhani/releases/download/v#{version}/rajdhani-#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/itfoundry/rajdhani/releases.atom',
           checkpoint: '83cb5df4b0a6b60138dabbe7c4a1ac8a262972c00cd9f6be757af73d147d8bb4'
+  name 'Rajdhani'
   homepage 'https://github.com/itfoundry/rajdhani'
 
   font 'Rajdhani-Bold.otf'
