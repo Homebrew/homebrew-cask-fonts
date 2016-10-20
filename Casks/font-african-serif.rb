5b95767580daa134f4dd0e1bab02a46dc255d95f
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 16:54:53 +0100

    Update font-african-serif to 9.380

--- Casks/font-african-serif.rb
+++ Casks/font-african-serif.rb
@@ -3,6 +3,7 @@ cask 'font-african-serif' do
   sha256 'fe352b95cf8a6e949d40a6b6f018b4fd6321c16dd8c5c40b35be3a69feb582f5'
 
   url 'http://www.languagegeek.com/font/AfricanSerif.zip'
+  name 'African Serif'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
   font 'African Serif REGULAR 938.ttf'
