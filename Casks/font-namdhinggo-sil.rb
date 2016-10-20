475cde7bfa460acf4e8d9505b52797106790c5b4
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 20:32:25 +0100

    Update font-namdhinggo-sil to 1.004

--- Casks/font-namdhinggo-sil.rb
+++ Casks/font-namdhinggo-sil.rb
@@ -3,6 +3,7 @@ cask 'font-namdhinggo-sil' do
   sha256 '8bd69ee93687f2b3fcb705b0c8867c8ff573edcaf9a5c51a08a8ca1c1ddc966b'
 
   url "https://scripts.sil.org/cms/scripts/render_download.php?format=file&media_id=NamdhinggoSIL#{version}&filename=NamdhinggoSIL#{version}.zip"
+  name 'Namdhinggo SIL '
   homepage 'https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=NamdhinggoSIL'
 
   font 'NamdhinggoSIL/NamdhinggoSIL-R.ttf'
