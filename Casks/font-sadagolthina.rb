de12d12899d5d87b2c955cd37e5bcaa167bb940f
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:48:37 +0100

    Update font-sadagolthina to 1.0.2 (#844)

--- Casks/font-sadagolthina.rb
+++ Casks/font-sadagolthina.rb
@@ -1,12 +1,10 @@
 cask 'font-sadagolthina' do
-  version :latest
-  sha256 :no_check
-
-  # version '1.002'
-  # sha256 'aa8b42ccf0cc74a72bb50ab95a45904e6e032e000e09d0c07d93fbee3870c112'
+  version '1.0.2'
+  sha256 'aa8b42ccf0cc74a72bb50ab95a45904e6e032e000e09d0c07d93fbee3870c112'
 
   url 'http://www.evertype.com/fonts/gothic/sadagolthina.zip'
+  name 'Sadagolthina'
   homepage 'http://www.evertype.com/fonts/gothic/'
 
-  font 'sadagolthina-1.0.2/Sadagolthina.ttf'
+  font "sadagolthina-#{version}/Sadagolthina.ttf"
 end
