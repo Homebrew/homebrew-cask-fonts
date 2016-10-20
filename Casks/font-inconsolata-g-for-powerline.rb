0e0bb7f0e1269f1d3489ca4072c37464ccc6e3ad
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:10:48 +0100

    Update font-inconsolata-g-for-powerline to latest

--- Casks/font-inconsolata-g-for-powerline.rb
+++ Casks/font-inconsolata-g-for-powerline.rb
@@ -5,6 +5,7 @@ cask 'font-inconsolata-g-for-powerline' do
   url 'https://github.com/powerline/fonts/trunk/Inconsolata-g',
       using:      :svn,
       trust_cert: true
+  name 'Inconsolata-g for Powerline'
   homepage 'https://github.com/powerline/fonts/tree/master/Inconsolata-g'
 
   font 'Inconsolata-g for Powerline.otf'
