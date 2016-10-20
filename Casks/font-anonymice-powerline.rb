c95f096ace8ad79d2303753cf41d9c1772602d52
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 17:12:04 +0100

    Update and rename font-anonymous-pro-for-powerline.rb to font-anonymice-powerline.rb

--- Casks/font-anonymice-powerline.rb
+++ Casks/font-anonymice-powerline.rb
@@ -1,10 +1,11 @@
-cask 'font-anonymous-pro-for-powerline' do
+cask 'font-anonymice-powerline' do
   version :latest
   sha256 :no_check
 
   url 'https://github.com/powerline/fonts/trunk/AnonymousPro',
       using:      :svn,
       trust_cert: true
+  name 'Anonymice Powerline'
   homepage 'https://github.com/powerline/fonts/tree/master/AnonymousPro'
 
   font 'Anonymice Powerline Bold Italic.ttf'
