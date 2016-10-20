d243a0e3de950718fc27db9cc6705ce72e7d5eb0
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:00:31 +0100

    Update font-hanazono-mincho.rb

--- Casks/font-hanazono-mincho.rb
+++ Casks/font-hanazono-mincho.rb
@@ -1,8 +1,9 @@
-cask 'font-hanazono-mincho' do
-  version '2016-02-01'
+cask 'font-hanamina' do
+  version '2016.02.01'
   sha256 '2b89af892d969edbb47b5adf75663a76c2bec5961467e65557b623f6b2add139'
 
-  url 'http://dl.osdn.jp/hanazono-font/64385/hanazono-20160201.zip'
+  url 'http://dl.osdn.jp/hanazono-font/64385/hanazono-#{version.no_dots}.zip'
+  name 'HanaMinA'
   homepage 'http://fonts.jp/hanazono/'
 
   font 'HanaMinA.ttf'
