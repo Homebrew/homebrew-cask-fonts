216bc460722767ec7538b9140741fc73454d55bb
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:39:22 +0100

    Update font-fantasque-sans-mono to 1.7.1 (#730)

--- Casks/font-fantasque-sans-mono.rb
+++ Casks/font-fantasque-sans-mono.rb
@@ -1,8 +1,11 @@
 cask 'font-fantasque-sans-mono' do
-  version 'v1.7.1'
+  version '1.7.1'
   sha256 '6bb3b24413b78eed19ffa9bd233ae555982e3b185bd303e57dd1e05bebf17352'
 
-  url "https://github.com/belluzj/fantasque-sans/releases/download/#{version}/FantasqueSansMono.zip"
+  url "https://github.com/belluzj/fantasque-sans/releases/download/v#{version}/FantasqueSansMono.zip"
+  appcast 'https://github.com/belluzj/fantasque-sans/releases.atom',
+          checkpoint: '8085c3dff43a9dbf3201ca790c57800a089d1b69fec91226a600c04d9c681e36'
+  name 'Fantasque Sans Mono'
   homepage 'https://github.com/belluzj/fantasque-sans'
 
   font 'OTF/FantasqueSansMono-Bold.otf'
