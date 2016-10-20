906563dff59cd89717f709d477cf4af4e1164efa
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 23:04:58 +0100

    Update and rename font-wqy-zenhei.rb to font-wenquanyi-zen-hei.rb

--- Casks/font-wenquanyi-zen-hei.rb
+++ Casks/font-wenquanyi-zen-hei.rb
@@ -1,8 +1,10 @@
-cask 'font-wqy-zenhei' do
+cask 'font-wenquanyi-zen-hei' do
   version '0.9.45'
   sha256 'e4b7e306475bf9427d1757578f0e4528930c84c44eaa3f167d4c42f110ee75d6'
 
+  # downloads.sourceforge.net/wqy was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/wqy/wqy-zenhei-#{version}.tar.gz"
+  name 'WenQuanYi Zen Hei'
   homepage 'http://wenq.org/wqy2/index.cgi?ZenHei'
 
   font 'wqy-zenhei/wqy-zenhei.ttc'
