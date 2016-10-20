69d91f2f3897556fa97c9239377a0a5978e7e3f0
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:29:54 +0100

    Update font-kayases to latest

--- Casks/font-kayases.rb
+++ Casks/font-kayases.rb
@@ -2,10 +2,8 @@ cask 'font-kayases' do
   version :latest
   sha256 :no_check
 
-  # version '001.000'
-  # sha256 'b62b8e2a7ec95d476a1f1fb91c3329f376b221716308547be9bcc946f4de5923'
-
   url 'http://www.languagegeek.com/font/Kayases.zip'
+  name 'Kayases'
   homepage 'http://www.languagegeek.com/font/fontdownload.html'
 
   font 'Kayases.ttf'
