f2ffb53aea5c2721266f1bf75384825f50f89024
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:53:27 +0100

    Update font-tuffy to 20120614

--- Casks/font-tuffy.rb
+++ Casks/font-tuffy.rb
@@ -1,12 +1,13 @@
 cask 'font-tuffy' do
-  version '1.28'
+  version '20120614'
   sha256 'c6f9eb7a878e270b96fbb5dae7783be29710b4405c76e856de60dcfc96386e0b'
 
-  url 'http://tulrich.com/fonts/tuffy-20120614.tar.gz'
+  url "http://tulrich.com/fonts/tuffy-#{version}.tar.gz"
+  name 'Tuffy'
   homepage 'http://tulrich.com/fonts/'
 
-  font 'tuffy-20120614/Tuffy.otf'
-  font 'tuffy-20120614/Tuffy_Bold.otf'
-  font 'tuffy-20120614/Tuffy_Bold_Italic.otf'
-  font 'tuffy-20120614/Tuffy_Italic.otf'
+  font "tuffy-#{version}/Tuffy.otf"
+  font "tuffy-#{version}/Tuffy_Bold.otf"
+  font "tuffy-#{version}/Tuffy_Bold_Italic.otf"
+  font "tuffy-#{version}/Tuffy_Italic.otf"
 end
