41b9965ea2dcebdf07ae07b8e7e78e84850af6b3
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:09:57 +0100

    Update font-inconsolata-dz to latest (#746)

--- Casks/font-inconsolata-dz.rb
+++ Casks/font-inconsolata-dz.rb
@@ -3,6 +3,7 @@ cask 'font-inconsolata-dz' do
   sha256 :no_check
 
   url 'http://media.nodnod.net/Inconsolata-dz.otf.zip'
+  name 'Incosolata-dz'
   homepage 'http://nodnod.net/2009/feb/12/adding-straight-single-and-double-quotes-inconsola/'
 
   font 'Inconsolata-dz.otf'
