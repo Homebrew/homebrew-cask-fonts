5ebad250f27e159adf2764868763a71a6ee28869
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:12:27 +0100

    Update font-inconsolata-lgc to latest (#749)

--- Casks/font-inconsolata-lgc.rb
+++ Casks/font-inconsolata-lgc.rb
@@ -1,11 +1,11 @@
 cask 'font-inconsolata-lgc' do
-  # version '1.1.0'
   version :latest
   sha256 :no_check
 
   url 'https://github.com/DeLaGuardo/Inconsolata-LGC/trunk',
       using:      :svn,
       trust_cert: true
+  name 'Inconsolata LGC'
   homepage 'https://github.com/DeLaGuardo/Inconsolata-LGC'
 
   font 'inconsolatalgc.ttf'
