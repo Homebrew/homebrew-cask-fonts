de35073eb365290c900a9103c2b6fa8aa1d13760
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:13:58 +0100

    Update font-rozha-one to 2.000

--- Casks/font-rozha-one.rb
+++ Casks/font-rozha-one.rb
@@ -2,9 +2,10 @@ cask 'font-rozha-one' do
   version '2.000'
   sha256 '9048def2a602b51466f6f7528a0e83fbc73abd72aadb87d70fba3887e6101c7c'
 
-  url 'https://github.com/itfoundry/rozhaone/releases/download/v2.000/rozhaone-2_000.zip'
+  url "https://github.com/itfoundry/rozhaone/releases/download/v#{version}/rozhaone-#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/itfoundry/rozhaone/releases.atom',
           checkpoint: '0aa351b7077db379253573a21001e38630f4321bd7ee00b8e9bdbcb5ea14a8f6'
+  name 'Rozha One'
   homepage 'https://github.com/itfoundry/rozhaone'
 
   font 'RozhaOne-Regular.otf'
