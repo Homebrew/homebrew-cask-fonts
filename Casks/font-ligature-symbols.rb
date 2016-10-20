70ffdfcddd6de04a4ed6e04080d3323a5b9a126f
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:41:07 +0100

    Update font-ligature-symbols to latest

--- Casks/font-ligature-symbols.rb
+++ Casks/font-ligature-symbols.rb
@@ -3,6 +3,7 @@ cask 'font-ligature-symbols' do
   sha256 :no_check
 
   url 'http://kudakurage.com/ligature_symbols/LigatureSymbols.zip'
+  name 'Ligature Symbols'
   homepage 'http://kudakurage.com/ligature_symbols/'
 
   font 'LigatureSymbols/LigatureSymbols-2.11.otf'
