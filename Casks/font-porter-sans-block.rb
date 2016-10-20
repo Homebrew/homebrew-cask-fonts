53d53c9ff83d32d0c923e3dc2ed3322c166add0d
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 21:02:09 +0100

    Update font-porter-sans-block to latest

--- Casks/font-porter-sans-block.rb
+++ Casks/font-porter-sans-block.rb
@@ -1,8 +1,9 @@
 cask 'font-porter-sans-block' do
-  version '1.000'
-  sha256 '4ee2175bfeca80bb4a6c20d07b598bfae0972f052bfc6616a18497c60b24abb8'
+  version :latest
+  sha256 :no_check
 
   url 'http://tylerfinck.com/portersansblock.zip'
+  name 'Porter Sans Block'
   homepage 'http://www.tylerfinck.com/2013/10/porter-sans-block-a-free-font/'
 
   font 'portersansblock/porter-sans-inline-block.otf'
