ded53cca4df168ba210b9e5071a00e888310227a
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:12:30 +0100

    Update font-droid-sans-thai to latest

--- Casks/font-droid-sans-thai.rb
+++ Casks/font-droid-sans-thai.rb
@@ -1,5 +1,4 @@
 cask 'font-droid-sans-thai' do
-  # version '1.00'
   version :latest
   sha256 :no_check
 
@@ -8,6 +7,7 @@ cask 'font-droid-sans-thai' do
       using:      :svn,
       revision:   '50',
       trust_cert: true
+  name 'Droid Sans Thai'
   homepage 'https://www.google.com/fonts/earlyaccess'
 
   font 'DroidSansThai-Bold.ttf'
