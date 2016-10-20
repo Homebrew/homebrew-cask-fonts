e3ba499db3e57f9d7f2bfe85eaf3a757f838a74f
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 18:39:09 +0100

    Update font-eeyek-unicode to latest (#726)

--- Casks/font-eeyek-unicode.rb
+++ Casks/font-eeyek-unicode.rb
@@ -3,6 +3,7 @@ cask 'font-eeyek-unicode' do
   sha256 :no_check
 
   url 'http://tabish.freeshell.org/eeyek/eeyek.zip'
+  name 'Eeyek Unicode'
   homepage 'http://tabish.freeshell.org/eeyek/download.html'
 
   font 'Eeyek_Unicode/Eeyek.ttf'
