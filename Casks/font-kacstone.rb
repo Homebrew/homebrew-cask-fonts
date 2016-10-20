76c696f1cfb1feb75a5f36edce38c8f96e522cfe
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 19:24:48 +0100

    Update font-kacstone to 5.0 (#754)

--- Casks/font-kacstone.rb
+++ Casks/font-kacstone.rb
@@ -2,7 +2,9 @@ cask 'font-kacstone' do
   version '5.0'
   sha256 '1b016f49f99de16a65dcd990f229e729e6c4c6df02b23409771f6e27b69186a7'
 
-  url "http://downloads.sourceforge.net/project/arabeyes/kacst_fonts/kacst_one_#{version}.tar.bz2"
+  # downloads.sourceforge.net/arabeyes was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/arabeyes/kacst_one_#{version}.tar.bz2"
+  name 'KacstOne'
   homepage 'http://projects.arabeyes.org/project.php?proj=Khotot'
 
   font 'KacstOne.ttf'
