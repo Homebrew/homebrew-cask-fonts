3cdfbeb3545eda466d9a7e883677621da9768b23
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:30:34 +0100

    Delete font-skiplegday.rb

--- Casks/font-skiplegday.rb
+++
@@ -1,9 +0,0 @@
-cask 'font-skiplegday' do
-  version :latest
-  sha256 :no_check
-
-  url 'http://dl.dafont.com/dl/?f=skiplegday'
-  homepage 'https://www.behance.net/gallery/18515345/Skip-Leg-Day-Typeface'
-
-  font 'SkipLegDay.ttf'
-end
