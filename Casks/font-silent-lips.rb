f3d0fc2a07ff9a798568607a4673b715b3feda15
Vítor Galvão <info@vitorgalvao.com>
2016-10-20 22:28:28 +0100

    Update font-silent-lips to 2014.07

--- Casks/font-silent-lips.rb
+++ Casks/font-silent-lips.rb
@@ -1,8 +1,10 @@
 cask 'font-silent-lips' do
-  version :latest
-  sha256 :no_check
+  version '2014.07'
+  sha256 'b19e19937111b5e2642e8f1b3da9e0f211f454dc897e3657b839067e59be653e'
 
-  url 'http://www.ffonts.net/Silent_Lips.font.zip'
+  # fontm.com was verified as official when first introduced to the cask
+  url "http://fontm.com/wp-content/uploads/#{version.dots_to_slashes}/Silent-Lips.zip"
+  name 'Silent Lips'
   homepage 'https://www.behance.net/gallery/18281129/Silent-Lips-Experimental-Free-Font'
 
   font 'Silent Lips/Silent Lips.ttf'
