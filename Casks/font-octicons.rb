ecc3160008b416c6f6003091ad44baeceebe8e00
David Graham <dgraham@github.com>
2016-10-21 06:37:16 -0600

    Fix Octicons archive location (#840)
    * Fix Octicons archive location
    
    The github/octicons repository moved to primer/octicons. The TTF file is no longer contained in the source repository, so download the latest release zip archive instead.
    
    * Add appcast URL
    
    * Add sha256 for version 4.4.0

--- Casks/font-octicons.rb
+++ Casks/font-octicons.rb
@@ -1,10 +1,12 @@
 cask 'font-octicons' do
-  version :latest
-  sha256 :no_check
+  version '4.4.0'
+  sha256 '1d3b27b1096c57edb4d391a6b1ec1e672e64b250bec4c86662f11baba988db3a'
 
-  url 'https://github.com/github/octicons/archive/master.zip'
+  url "https://github.com/primer/octicons/archive/v#{version}.zip"
+  appcast 'https://github.com/primer/octicons/releases.atom',
+          checkpoint: 'af35fc21e4adb5ed1798748d12db7a1ba1d68f486bbbe30841af3d1319b7fbfb'
   name 'Octicons'
   homepage 'https://octicons.github.com'
 
-  font 'octicons-master/build/font/octicons.ttf'
+  font "octicons-#{version}/build/font/octicons.ttf"
 end
