d7f80edc9c48d6aca51033e03e2f0996f01f0dc7
Vítor Galvão <info@vitorgalvao.com>
2016-10-24 13:08:12 +0100

    Update font-lalezar to 1.003

--- Casks/font-lalezar.rb
+++ Casks/font-lalezar.rb
@@ -1,12 +1,12 @@
 cask 'font-lalezar' do
-  version '1.0.0'
-  sha256 'fa838b82edc6cd63071c2c4e0bae2f3a76248bd804057154483215c3df95232e'
+  version '1.003'
+  sha256 'e2c758bd395b08d5cb440167d35b264d1393c26bab6854eab484672c09f10f42'
 
-  url "https://github.com/BornaIz/Lalezar/releases/download/v#{version}/Lalezar-v1.000-fonts.zip"
+  url "https://codeload.github.com/BornaIz/Lalezar/zip/#{version}"
   appcast 'https://github.com/BornaIz/Lalezar/releases.atom',
-          checkpoint: '1b4c79049953bdba2e977e4975031a3f96459565107b688dd3654c40e1c55dce'
+          checkpoint: '66aba340c111d02166d12a98bbaf7a8d0dee693afbae54e56be4799804cd2144'
   name 'Lalezar'
   homepage 'https://github.com/BornaIz/Lalezar'
 
-  font 'Lalezar-v1.000-fonts/Lalezar-Regular.otf'
+  font "Lalezar-v#{version}-fonts/Font files/Lalezar-Regular.otf"
 end
