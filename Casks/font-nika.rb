f4d7543b77067b637b34a363b9e5ef999b968dfb
Vítor Galvão <info@vitorgalvao.com>
2016-10-24 13:13:50 +0100

    Update font-nika to 1.0.0

--- Casks/font-nika.rb
+++ Casks/font-nika.rb
@@ -1,10 +1,10 @@
 cask 'font-nika' do
-  version '0.1.3'
-  sha256 'd07760993dca0ec7407a3ec8b01dcfb43767f144ae05be4cc0fc12fd4b35b9f1'
+  version '1.0.0'
+  sha256 '0ef9ec73a2406bc81405ea0bbdd81095f0543c6d79c46bc2246f72c7ebf551ba'
 
   url "https://github.com/font-store/font-nika/archive/v#{version}.zip"
   appcast 'https://github.com/font-store/font-nika/releases.atom',
-          checkpoint: '8403f392225a67a1d7236060bf7d9424f23ada8a4a61b0b1f55b84ac00a31a00'
+          checkpoint: 'a8141f7742de3164736c5e9b5d6da7d9cc2902e649653f1334d3bcedc70f1dd3'
   name 'Nika'
   homepage 'https://github.com/font-store/font-nika'
 
