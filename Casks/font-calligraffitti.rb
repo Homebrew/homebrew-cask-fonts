36865e21db2a660706b446471dff24f02a40a999
Vítor Galvão <info@vitorgalvao.com>
2016-10-24 12:27:52 +0100

    Update font-calligraffitti to latest

--- Casks/font-calligraffitti.rb
+++ Casks/font-calligraffitti.rb
@@ -1,6 +1,6 @@
 cask 'font-calligraffitti' do
-  version '1.001'
-  sha256 '97c508102499f08b8964d750ec1f455e19aa9c30adf51d7fb5d0d650d8e75399'
+  version :latest
+  sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/blob/master/apache/calligraffitti/Calligraffitti-Regular.ttf'
