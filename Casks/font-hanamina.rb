d99ed38ca30a2ad2a4d9aadf1b485ec16441cb4f
Vítor Galvão <info@vitorgalvao.com>
2016-10-24 11:52:45 +0100

    font-hanamina.rb: fix quotes in url

--- Casks/font-hanamina.rb
+++ Casks/font-hanamina.rb
@@ -3,7 +3,7 @@ cask 'font-hanamina' do
   sha256 '2b89af892d969edbb47b5adf75663a76c2bec5961467e65557b623f6b2add139'
 
   # dl.osdn.jp/hanazono-font was verified as official when first introduced to the cask
-  url 'http://dl.osdn.jp/hanazono-font/64385/hanazono-#{version.no_dots}.zip'
+  url "http://dl.osdn.jp/hanazono-font/64385/hanazono-#{version.no_dots}.zip"
   name 'HanaMinA'
   homepage 'http://fonts.jp/hanazono/'
 
