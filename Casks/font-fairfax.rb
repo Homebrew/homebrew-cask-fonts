32a8061aadd5afaa3c6960cb1d5b283c59bfc830
Vítor Galvão <info@vitorgalvao.com>
2016-10-24 12:30:19 +0100

    Update font-fairfax to latest

--- Casks/font-fairfax.rb
+++ Casks/font-fairfax.rb
@@ -2,7 +2,7 @@ cask 'font-fairfax' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.kreativekorp.com/swdownload/fonts/xlang/fairfax.zip'
+  url 'http://www.kreativekorp.com/swdownload/fonts/relaybm/fairfax.zip'
   name 'Fairfax'
   homepage 'http://www.kreativekorp.com/software/fonts/constructium.shtml'
 
