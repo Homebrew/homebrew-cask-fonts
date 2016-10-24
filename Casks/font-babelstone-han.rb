997b33e0dcedb9c41e55ad8484f3ad9e5dfd2666
Vítor Galvão <info@vitorgalvao.com>
2016-10-24 12:25:07 +0100

    Update font-babelstone-han to latest

--- Casks/font-babelstone-han.rb
+++ Casks/font-babelstone-han.rb
@@ -2,7 +2,7 @@ cask 'font-babelstone-han' do
   version :latest
   sha256 :no_check
 
-  url 'http://babelstone.co.uk/Fonts/BabelStoneHan.zip'
+  url 'http://babelstone.co.uk/Fonts/5521/BabelStoneHan.zip'
   name 'BabelStone Han'
   homepage 'http://babelstone.co.uk/Fonts/Han.html'
 
