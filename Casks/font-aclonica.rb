4ba3a61eae77b144c6d152edf7b9d05b27710c29
Vítor Galvão <info@vitorgalvao.com>
2016-10-24 12:22:54 +0100

    Update font-aclonica to latest

--- Casks/font-aclonica.rb
+++ Casks/font-aclonica.rb
@@ -1,6 +1,6 @@
 cask 'font-aclonica' do
-  version '1.000'
-  sha256 'aec4ac0902f0d9597761557c59e4e0d5c9ac117c70d106ca1ae806082f6b234d'
+  version :latest
+  sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/blob/master/apache/aclonica/Aclonica.ttf'
