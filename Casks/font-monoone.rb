831c357c2abbfb4313a9dcabfd5f270dc700b186
Vítor Galvão <info@vitorgalvao.com>
2016-10-24 11:30:26 +0100

    Update font-monoone.rb

--- Casks/font-monoone.rb
+++ Casks/font-monoone.rb
@@ -1,9 +1,9 @@
 cask 'font-monoone' do
-  # version '1.000'
   version :latest
   sha256 :no_check
 
   url 'https://github.com/madmalik/mononoki/raw/master/monoOne.otf'
+  name 'monoOne'
   homepage 'https://github.com/madmalik/mononoki'
 
   font 'monoOne.otf'
