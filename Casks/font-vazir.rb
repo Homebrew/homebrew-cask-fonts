345f2dce465ef3ebf112fa5a32a8854cb9422654
Vítor Galvão <info@vitorgalvao.com>
2016-10-24 13:25:16 +0100

    Update font-vazir to 4.4.0 (#899)

--- Casks/font-vazir.rb
+++ Casks/font-vazir.rb
@@ -1,11 +1,11 @@
 cask 'font-vazir' do
-  version '2-RC2'
-  sha256 'b56d3f4030868fe4851f21bce16252c381d49a9bd31f4996a4e7d86ab93d526c'
+  version '4.4.0'
+  sha256 'd4adaf432e96f85b9c63ad656cec817d2cb027df57e422256c8f509306035f41'
 
   # github.com/rastikerdar was verified as official when first introduced to the cask
   url "https://github.com/rastikerdar/vazir-font/releases/download/v#{version}/vazir-font-v#{version}.zip"
   appcast 'https://github.com/rastikerdar/vazir-font/releases.atom',
-          checkpoint: 'b42184ad4fddda5b4ae16825887a1872bb630fdc87381418a4cef879fdde2d36'
+          checkpoint: 'f7bb4514ae68155337d559c0a87f0412cf512875eb15143c9a1d7717d636d480'
   name 'Vazir'
   homepage 'http://rastikerdar.github.io/vazir-font'
 
