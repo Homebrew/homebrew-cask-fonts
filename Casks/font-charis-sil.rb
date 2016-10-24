b3fc168257eaa79ebece7be8cdb7ae8ae736a130
Vítor Galvão <info@vitorgalvao.com>
2016-10-24 12:29:26 +0100

    Update font-charis-sil to 5.000

--- Casks/font-charis-sil.rb
+++ Casks/font-charis-sil.rb
@@ -2,7 +2,7 @@ cask 'font-charis-sil' do
   version '5.000'
   sha256 '5e3e5473b30363008c289cc87e2aa584a0916087a63a3f689defa8e0cee09bfd'
 
-  url "http://software.sil.org/downloads/charis/CharisSIL-#{version}.zip"
+  url "http://software.sil.org/downloads/d/charis/CharisSIL-#{version}.zip"
   name 'Charis SIL'
   homepage 'http://software.sil.org/charis/download/'
 
