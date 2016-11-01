60c4847205e6e7dfb1555b9e5c647e1b63f65620
Alex Guerrero <alexguerrero1092@gmail.com>
2016-11-01 17:38:41 +0100

    Add font-bukyvede-bold

---
+++ Casks/font-bukyvede-bold.rb
@@ -0,0 +1,10 @@
+cask 'font-bukyvede-bold' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://kodeks.uni-bamberg.de/aksl/media/BukyVede-Bold.ttf'
+  name 'BukyVede Bold'
+  homepage 'http://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm'
+
+  font 'BukyVede-Bold.ttf'
+end
