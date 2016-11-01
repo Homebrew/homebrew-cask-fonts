b67d132c399d3eb2f717b4408685c85447b1b59c
Alex Guerrero <guerrero@users.noreply.github.com>
2016-11-01 19:09:36 +0100

    Add font-bukyvede-regular (#902)

---
+++ Casks/font-bukyvede-regular.rb
@@ -0,0 +1,10 @@
+cask 'font-bukyvede-regular' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://kodeks.uni-bamberg.de/aksl/media/BukyVede-Regular.ttf'
+  name 'BukyVede Regular'
+  homepage 'http://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm'
+
+  font 'BukyVede-Regular.ttf'
+end
