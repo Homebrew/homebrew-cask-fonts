d2930ad72fb8137b5c9acf6c5098959776cad142
Alex Guerrero <alexguerrero1092@gmail.com>
2016-11-02 18:28:59 +0100

    Add conflicting fonts from Google fonts

---
+++ font-preahvihear.rb
@@ -0,0 +1,11 @@
+cask 'font-preahvihear' do
+  version :latest
+  sha256 :no_check
+
+  # github.com/google/fonts was verified as official when first introduced to the cask
+  url 'https://github.com/google/fonts/raw/master/ofl/preahvihear/Preahvihear.ttf'
+  name 'Preahvihear'
+  homepage 'http://www.google.com/fonts/specimen/Preahvihear'
+
+  font 'Preahvihear.ttf'
+end
