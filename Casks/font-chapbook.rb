380174bbae9fb7bb18eefc41c78d32394a7b3c4b
Ben Cohen <heliostatic@gmail.com>
2016-11-15 15:44:28 -0500

    Adding font-chapbook

---
+++ Casks/font-chapbook.rb
@@ -0,0 +1,12 @@
+cask 'font-chapbook' do
+  version '4.000'
+  sha256 '3b623ae085b0bc69b59ffe99750da7f5b31140513a34d09359c0ea7ea2c54b33'
+
+  url 'http://www.feorag.com/freestuff/archives/Chapbook-OTF.zip'
+  name 'Chapbook'
+  homepage 'http://www.feorag.com/freestuff/chapbook.html'
+
+  font 'Chapbook OTF/Chapbook-Dingbats.otf'
+  font 'Chapbook OTF/Chapbook-Italic.otf'
+  font 'Chapbook OTF/Chapbook-Regular.otf'
+end
