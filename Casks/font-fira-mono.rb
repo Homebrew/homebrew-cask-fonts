91251079d89e7ec672d10a6af5476a6965e5a547
Vítor Galvão <info@vitorgalvao.com>
2016-11-16 16:09:44 +0000

    Update font-fira-mono.rb

--- Casks/font-fira-mono.rb
+++ Casks/font-fira-mono.rb
@@ -1,15 +1,15 @@
 cask 'font-fira-mono' do
-  version '3.206'
+  version '3.206,4.202'
   sha256 'd86269657387f144d77ba12011124f30f423f70672e1576dc16f918bb16ddfe4'
 
   # github.com/mozilla/Fira was verified as official when first introduced to the cask
-  url 'https://github.com/mozilla/Fira/archive/4.202.tar.gz'
+  url "https://github.com/mozilla/Fira/archive/#{version.after_comma}.tar.gz"
   appcast 'https://github.com/mozilla/Fira/releases.atom',
           checkpoint: '9e2d525c6d942160d9389752c3943eea9dbf886a2c7b651ae84d6524ede21d94'
   name 'Fira Mono'
   homepage 'https://mozilla.github.io/Fira/'
 
-  font 'Fira-4.202/otf/FiraMono-Bold.otf'
-  font 'Fira-4.202/otf/FiraMono-Medium.otf'
-  font 'Fira-4.202/otf/FiraMono-Regular.otf'
+  font "Fira-#{version.after_comma}/otf/FiraMono-Bold.otf"
+  font "Fira-#{version.after_comma}/otf/FiraMono-Medium.otf"
+  font "Fira-#{version.after_comma}/otf/FiraMono-Regular.otf"
 end
