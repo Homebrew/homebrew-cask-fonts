1cd6eda127f8cc751106ff8fe12fcd44cacd7e15
Vítor Galvão <info@vitorgalvao.com>
2016-11-16 22:41:31 +0000

    Update font-hack-nerd-font.rb

--- Casks/font-hack-nerd-font.rb
+++ Casks/font-hack-nerd-font.rb
@@ -3,7 +3,7 @@ cask 'font-hack-nerd-font' do
   sha256 '89aff63d5acbe582e11895e745b6da78a75fa2007552c7a1be1c9cc35296eb59'
 
   url 'https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hack.zip'
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
+  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
           checkpoint: 'ef06052e06ea6b843bc733c57d72041f750f7d021a7ae9723a790ba740e3a3cb'
   name 'Knack Nerd Font (Hack)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
