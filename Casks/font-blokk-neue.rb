65450bd75b6946e569352a1409736e36bd7822dc
Cory Gibbons <hello@corygibbons.com>
2016-11-15 22:30:17 -0800

    Revise font-blokk-neue

--- Casks/font-blokk-neue.rb
+++ Casks/font-blokk-neue.rb
@@ -6,10 +6,5 @@ cask 'font-blokk-neue' do
   name 'BLOKK Neue'
   homepage 'http://blokkfont.com/'
 
-  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.eot'
-  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.otf'
-  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.svg'
-  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.ttf'
-  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.woff'
-  font 'BLOKKNeue-Regular/webfonts/BLOKKNeue-Regular.woff2'
+  font 'BLOKKNeue-Regular/BLOKKNeue-Regular.otf'
 end
