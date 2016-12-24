9d76b05ad3a568136806681a18ec516c7505c615
Tim Sheridan <tghs@tghs.net>
2016-12-24 13:36:38 +0200

    Update font-gnu-unifont to 9.0.06 (#934)

--- Casks/font-gnu-unifont.rb
+++ Casks/font-gnu-unifont.rb
@@ -1,6 +1,6 @@
 cask 'font-gnu-unifont' do
-  version '9.0.01'
-  sha256 '0d36e005b962b7f15e55e93bbd25f931ca3a19ce3ed787bdeb721974f36f5b58'
+  version '9.0.06'
+  sha256 'e217fbf24ac1ba3f028ed937b30b6c27f31fd1288857ca0fb0b11100637c2665'
 
   url "http://unifoundry.com/pub/unifont-#{version}/font-builds/unifont-#{version}.ttf"
   name 'GNU Unifont'
