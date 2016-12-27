6f1dc81cfb94efa5dd282d28fce71612fc32a8df
Luavis <luaviskang@gmail.com>
2016-12-27 17:06:04 +0900

    Update font-d2conding to new url (#936)

--- Casks/font-d2coding.rb
+++ Casks/font-d2coding.rb
@@ -1,10 +1,10 @@
 cask 'font-d2coding' do
-  version '1.1,2015.11.03'
-  sha256 '62c8f3766769bfc435dee6f8adadb1e9820515d330bd495f74d0f876909f8daf'
+  version '1.21,1.2'
+  sha256 '788dfd603665ad48b69b8b81ef00ab804edb1b249d58788c6741b25b3625f46a'
 
-  url "http://dev.naver.com/frs/download.php/11568/D2Coding-Ver#{version.before_comma}-TTC-#{version.after_comma.no_dots}.zip"
+  url "https://github.com/naver/d2codingfont/releases/download/VER#{version.before_comma}/D2Coding-#{version.after_comma}.zip"
   name 'D2 Coding'
-  homepage 'http://dev.naver.com/projects/d2coding'
+  homepage 'https://github.com/naver/d2codingfont'
 
   font 'D2Coding.ttc'
 end
