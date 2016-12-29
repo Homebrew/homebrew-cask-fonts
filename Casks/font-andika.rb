d0897f8f2f940802a7661dbcd11667c85c3d5d04
Song Li <songl@outlook.com>
2016-12-29 14:32:40 +0800

    Fix A- font cask installation issues

--- Casks/font-andika.rb
+++ Casks/font-andika.rb
@@ -2,9 +2,9 @@ cask 'font-andika' do
   version '5.000'
   sha256 '604b7a1194be099bdf311ef76cbce086a054fa16d2b101cfaedcf527c63df507'
 
-  url "http://software.sil.org/downloads/andika/Andika-#{version}.zip"
+  url "http://software.sil.org/downloads/d/andika/Andika-#{version}.zip"
   name 'Andika'
   homepage 'http://software.sil.org/andika/'
 
-  font 'Andika-#{version}/Andika-R.ttf'
+  font "Andika-#{version}/Andika-R.ttf"
 end
