ed8828aa5ee4af341b8091216958e5a3539d0891
Song Li <songl@outlook.com>
2016-12-29 17:18:56 +0800

    Fix one V font installation issue

--- Casks/font-vt323.rb
+++ Casks/font-vt323.rb
@@ -1,6 +1,6 @@
 cask 'font-vt323' do
-  version '001.002'
-  sha256 '214c2bbd6f7d1a9787413a01dccdf6134fd1ed7934e425d07c2ad32b1568979a'
+  version :latest
+  sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/vt323/VT323-Regular.ttf'
