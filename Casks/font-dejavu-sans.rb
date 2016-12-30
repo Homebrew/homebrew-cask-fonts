622eb1156f3767890016691f5fe42b8e69cc6bb1
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-30 18:26:23 +0800

    Update font-dejavu-sans.rb

--- Casks/font-dejavu-sans.rb
+++ Casks/font-dejavu-sans.rb
@@ -2,7 +2,6 @@ cask 'font-dejavu-sans' do
   version '2.37'
   sha256 '7576310b219e04159d35ff61dd4a4ec4cdba4f35c00e002a136f00e96a908b0a'
 
-  # sourceforge.net/dejavu was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/dejavu/dejavu-fonts-ttf-#{version}.zip"
   name 'DejaVu'
   homepage 'https://sourceforge.net/projects/dejavu/'
