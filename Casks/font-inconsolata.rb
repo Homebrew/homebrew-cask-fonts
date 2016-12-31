83f018d40fcc625e17f84f682f062998f1b4e112
Takashi Nozawa <nozazon@gmail.com>
2016-12-31 10:07:56 +0900

    Update font-inconsolata to new url
    Use Google Web Font Directory as noted on the author's website.

--- Casks/font-inconsolata.rb
+++ Casks/font-inconsolata.rb
@@ -2,9 +2,13 @@ cask 'font-inconsolata' do
   version :latest
   sha256 :no_check
 
-  url 'http://levien.com/type/myfonts/Inconsolata.otf'
+  # github.com/google/fonts was verified as official when first introduced to the cask
+  url 'https://github.com/google/fonts/trunk/ofl/inconsolata',
+      using:      :svn,
+      trust_cert: true
   name 'Inconsolata'
   homepage 'http://levien.com/type/myfonts/inconsolata.html'
 
-  font 'Inconsolata.otf'
+  font 'Inconsolata-Regular.ttf'
+  font 'Inconsolata-Bold.ttf'
 end
