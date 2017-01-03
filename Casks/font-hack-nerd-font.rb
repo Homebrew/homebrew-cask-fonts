946dfad0dccf6d3bceccb62c2f10982762903801
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-01-04 00:10:41 +0900

    Update font-hack-nerd-font to 1.0.0

--- Casks/font-hack-nerd-font.rb
+++ Casks/font-hack-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-hack-nerd-font' do
-  version '0.8.0'
-  sha256 '89aff63d5acbe582e11895e745b6da78a75fa2007552c7a1be1c9cc35296eb59'
+  version '1.0.0'
+  sha256 '987eadbdaa11c4091a518a7d78c37f5f9bf2279b7c2aa6504907faf23e9673e5'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hack.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: 'ef06052e06ea6b843bc733c57d72041f750f7d021a7ae9723a790ba740e3a3cb'
+          checkpoint: '5677b051ebf92449af603ba6f1837c1bba529a881fd41fe0ec5dea830e1f7867'
   name 'Knack Nerd Font (Hack)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
