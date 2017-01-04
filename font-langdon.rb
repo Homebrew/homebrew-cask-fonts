a37e761d1aaa4d4094fea10015b0b60221aa5db0
Shyam Sunder <sgsunder1@gmail.com>
2017-01-04 11:52:07 -0500

    Added font-langdon

---
+++ font-langdon.rb
@@ -0,0 +1,10 @@
+cask 'font-langdon' do
+  version :latest
+  sha256 '4c90115dbc4f82f037bae60a73d204a4d876de6d93b9d7f166cbd38d078d7f2d'
+
+  url 'http://www.ffonts.net/Langdon.font.zip'
+  name 'Langdon'
+  homepage 'https://www.behance.net/gallery/7814505/Langdon'
+
+  font 'Langdon.otf'
+end
