c8b15fc0972d9b559ec689c4011e22c33e012d48
Vítor Galvão <info@vitorgalvao.com>
2017-02-28 01:00:42 +0000

    Update font-xkcd.rb

--- Casks/font-xkcd.rb
+++ Casks/font-xkcd.rb
@@ -3,8 +3,6 @@ cask 'font-xkcd' do
   sha256 :no_check
 
   url 'https://github.com/ipython/xkcd-font/raw/master/build/xkcd.otf'
-  appcast 'https://github.com/ipython/xkcd-font/releases.atom',
-          checkpoint: '08af66554aed4331a7af95aed32c5bfb909c18bd22a5231cb2695e1d5a5fbd4d'
   name 'xkcd'
   homepage 'https://github.com/ipython/xkcd-font'
 
