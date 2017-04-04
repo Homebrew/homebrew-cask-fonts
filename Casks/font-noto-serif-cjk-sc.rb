e2988f1fc3456e3ec008c0e0bf60332b6e0e41c3
Robert Lu <robberphex@gmail.com>
2017-04-04 18:41:53 +0800

    add Noto Serif CJK

---
+++ Casks/font-noto-serif-cjk-sc.rb
@@ -0,0 +1,17 @@
+cask 'font-noto-serif-cjk-sc' do
+  version :latest
+  sha256 :no_check
+
+  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
+  url 'https://noto-website.storage.googleapis.com/pkgs/NotoSerifCJKsc-hinted.zip'
+  name 'Noto Serif CJK SC'
+  homepage 'https://www.google.com/get/noto/#serif-hans'
+
+  font 'NotoSerifCJKsc-Bold.otf'
+  font 'NotoSerifCJKsc-Light.otf'
+  font 'NotoSerifCJKsc-Regular.otf'
+  font 'NotoSerifCJKsc-Black.otf'
+  font 'NotoSerifCJKsc-ExtraLight.otf'
+  font 'NotoSerifCJKsc-Medium.otf'
+  font 'NotoSerifCJKsc-SemiBold.otf'
+end
---
+++ Casks/font-noto-serif-cjk-tc.rb
@@ -0,0 +1,17 @@
+cask 'font-noto-serif-cjk-tc' do
+  version :latest
+  sha256 :no_check
+
+  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
+  url 'https://noto-website.storage.googleapis.com/pkgs/NotoSerifCJKtc-hinted.zip'
+  name 'Noto Serif CJK TC'
+  homepage 'https://www.google.com/get/noto/#serif-hant'
+
+  font 'NotoSerifCJKtc-Bold.otf'
+  font 'NotoSerifCJKtc-Light.otf'
+  font 'NotoSerifCJKtc-Regular.otf'
+  font 'NotoSerifCJKtc-Black.otf'
+  font 'NotoSerifCJKtc-ExtraLight.otf'
+  font 'NotoSerifCJKtc-Medium.otf'
+  font 'NotoSerifCJKtc-SemiBold.otf'
+end
