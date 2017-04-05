c9bfdde2fedb1396546c918ab69a63d63ecfc300
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-06 07:44:48 +0800

    Update font-source-han-serif-el-m.rb

--- Casks/font-source-han-serif-el-m.rb
+++ Casks/font-source-han-serif-el-m.rb
@@ -3,6 +3,8 @@ cask 'font-source-han-serif-el-m' do
   sha256 '175b3ffe8a936917c59139dcbea0b45632203f0bec5d5651c17ec0c545b540bf'
 
   url 'https://github.com/adobe-fonts/source-han-serif/raw/release/OTC/SourceHanSerifOTC_EL-M.zip'
+  appcast 'https://github.com/adobe-fonts/source-han-serif/releases.atom',
+          checkpoint: 'dafebca50a7068ffd66e74811b7de9214aaa369ff30898e5257a06f3340a2244'
   name 'Source Han Serif EL-M'
   homepage 'https://github.com/adobe-fonts/source-han-serif'
 
