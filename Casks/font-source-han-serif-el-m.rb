f1447e40ded2f5315547292d6a6c099ed09d8509
tbsmcd <tbsmcd@gmail.com>
2017-04-06 11:57:31 +0900

    added font-sourec-han-serif version 1.000 (#971)
    * added font-sourec-han-serif
    
    * Update font-source-han-serif-el-m.rb
    
    * Update font-source-han-serif-sb-h.rb
    
    * Update font-source-han-serif-sb-h.rb
    
    * Update font-source-han-serif-sb-h.rb
    
    * Update font-source-han-serif-sb-h.rb
    
    * Update font-source-han-serif-el-m.rb
    
    * Update font-source-han-serif-sb-h.rb

---
+++ Casks/font-source-han-serif-el-m.rb
@@ -0,0 +1,22 @@
+cask 'font-source-han-serif-el-m' do
+  version '1.000'
+  sha256 '175b3ffe8a936917c59139dcbea0b45632203f0bec5d5651c17ec0c545b540bf'
+
+  url 'https://github.com/adobe-fonts/source-han-serif/raw/release/OTC/SourceHanSerifOTC_EL-M.zip'
+  appcast 'https://github.com/adobe-fonts/source-han-serif/releases.atom',
+          checkpoint: 'dafebca50a7068ffd66e74811b7de9214aaa369ff30898e5257a06f3340a2244'
+  name 'Source Han Serif EL-M'
+  homepage 'https://github.com/adobe-fonts/source-han-serif'
+
+  font 'SourceHanSerifOTC_EL-M/SourceHanSerif-ExtraLight.ttc'
+  font 'SourceHanSerifOTC_EL-M/SourceHanSerif-Light.ttc'
+  font 'SourceHanSerifOTC_EL-M/SourceHanSerif-Medium.ttc'
+  font 'SourceHanSerifOTC_EL-M/SourceHanSerif-Regular.ttc'
+
+  caveats <<-EOS.undent
+    #{token} only installs the ExtraLight, Light, Regular and Medium weights.
+    To install SemiBold, Bold, and Heavy:
+
+      brew cask install font-source-han-serif-sb-h
+  EOS
+end
