f19734a5b947b8120b5b52ba229b76ab30a84986
giannitm <gianni-tammcmillan@giannis-mbp.home>
2017-04-11 22:32:17 +0100

    Update font-icomoon to latest

--- Casks/font-icomoon.rb
+++ Casks/font-icomoon.rb
@@ -5,7 +5,7 @@ cask 'font-icomoon' do
   # github.com/Keyamoon/IcoMoon-Free was verified as official when first introduced to the cask
   url 'https://github.com/Keyamoon/IcoMoon-Free/archive/master.zip'
   name 'IcoMoon'
-  homepage 'https://icomoon.io'
+  homepage 'https://icomoon.io/'
 
   font 'IcoMoon-Free-master/Font/IcoMoon-Free.ttf'
 end
