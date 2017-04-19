1c1377a64445961951880a3f1d2f6ce22ca70a0a
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-19 18:23:40 -0400

    Update font-babelstone-han to latest (#984)

--- Casks/font-babelstone-han.rb
+++ Casks/font-babelstone-han.rb
@@ -2,7 +2,7 @@ cask 'font-babelstone-han' do
   version :latest
   sha256 :no_check
 
-  url 'http://babelstone.co.uk/Fonts/5521/BabelStoneHan.zip'
+  url 'http://www.babelstone.co.uk/Fonts/8672/BabelStoneHan.zip'
   name 'BabelStone Han'
   homepage 'http://babelstone.co.uk/Fonts/Han.html'
 
