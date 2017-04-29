076db5a0bfc0118105740c8834d4b4a048258623
giannitm <gianni-tammcmillan@giannis-mbp.home>
2017-04-29 22:57:00 +0100

    Update font-georgia to 2.05 (#1000)

--- Casks/font-georgia.rb
+++ Casks/font-georgia.rb
@@ -3,6 +3,8 @@ cask 'font-georgia' do
   sha256 '2c2c7dcda6606ea5cf08918fb7cd3f3359e9e84338dc690013f20cd42e930301'
 
   url 'https://downloads.sourceforge.net/corefonts/georgi32.exe'
+  appcast 'https://sourceforge.net/projects/corefonts/rss',
+          checkpoint: '8d659740c2893218b3e1d16918a9372b2838f5e0e7ef0405d3103f2d563e7bd1'
   name 'Georgia'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
 
