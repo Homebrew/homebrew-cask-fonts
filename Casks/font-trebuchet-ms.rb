2ab96921b80950edbe5fa6485aa1d40005b0fa06
giannitm <giannitammcmillan@gmail.com>
2017-04-29 23:31:40 +0100

    Update font-trebuchet-ms to latest (#1008)

--- Casks/font-trebuchet-ms.rb
+++ Casks/font-trebuchet-ms.rb
@@ -3,6 +3,8 @@ cask 'font-trebuchet-ms' do
   sha256 :no_check
 
   url 'https://downloads.sourceforge.net/corefonts/trebuc32.exe'
+  appcast 'https://sourceforge.net/projects/corefonts/rss',
+          checkpoint: '8d659740c2893218b3e1d16918a9372b2838f5e0e7ef0405d3103f2d563e7bd1'
   name 'Trebuchet MS'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
 
