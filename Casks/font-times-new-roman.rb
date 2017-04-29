f86e11e611670972d2548b4d3cc252224ce193c7
giannitm <giannitammcmillan@gmail.com>
2017-04-29 23:30:54 +0100

    Update font-times-new-roman to latest (#1007)

--- Casks/font-times-new-roman.rb
+++ Casks/font-times-new-roman.rb
@@ -3,6 +3,8 @@ cask 'font-times-new-roman' do
   sha256 :no_check
 
   url 'https://downloads.sourceforge.net/corefonts/times32.exe'
+  appcast 'https://sourceforge.net/projects/corefonts/rss',
+          checkpoint: '8d659740c2893218b3e1d16918a9372b2838f5e0e7ef0405d3103f2d563e7bd1'
   name 'Times New Roman'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
 
