ef400a52a8098b801ba86d33df346e1e10d4178f
giannitm <gianni-tammcmillan@giannis-mbp.home>
2017-04-29 23:08:37 +0100

    Update font-junicode to latest (#1003)

--- Casks/font-junicode.rb
+++ Casks/font-junicode.rb
@@ -3,6 +3,8 @@ cask 'font-junicode' do
   sha256 :no_check
 
   url 'https://sourceforge.net/projects/junicode/files/latest/download'
+  appcast 'https://sourceforge.net/projects/junicode/rss',
+          checkpoint: 'ed16de5cb7bc4b9a99ef63598eaafcb0d8bf0b440fd9115b35a7866b8605d6bd'
   name 'Junicode'
   homepage 'http://junicode.sourceforge.net/'
 
