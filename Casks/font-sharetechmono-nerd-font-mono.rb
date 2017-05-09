24f4cc8d1445bc657ca30fbc8baba71ce51f9d54
giannitm <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-05-09 21:13:35 +0100

    Update font-sharetechmono-nerd-font-mono to 1.0.0

--- Casks/font-sharetechmono-nerd-font-mono.rb
+++ Casks/font-sharetechmono-nerd-font-mono.rb
@@ -4,7 +4,7 @@ cask 'font-sharetechmono-nerd-font-mono' do
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ShareTechMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '5677b051ebf92449af603ba6f1837c1bba529a881fd41fe0ec5dea830e1f7867'
+          checkpoint: '034cdfeb4f6a152086231f8388940cd9de7d8190aecbb26ec94e7a381bf7a3b7'
   name 'ShureTechMono Nerd Font (ShareTechMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
