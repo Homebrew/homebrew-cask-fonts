eabd7967b3e6b40092d4a19c3e8f72d58e8233b3
giannitm <giannitammcmillan@gmail.com>
2017-05-10 03:10:19 +0100

    Update font-kalam to 2.001 (#1091)

--- Casks/font-kalam.rb
+++ Casks/font-kalam.rb
@@ -1,10 +1,10 @@
 cask 'font-kalam' do
-  version '2.000'
-  sha256 '6b97c9719e7eaf3c069610059b548c2e130240d9996ec73ab61cfb17f1cbd1e6'
+  version '2.001'
+  sha256 '221df2b265d12789af5ef52afd25479ed84eabf7802e59de3de01291b237c605'
 
   url "https://github.com/itfoundry/kalam/releases/download/v#{version}/kalam-#{version.dots_to_underscores}.zip"
   appcast 'https://github.com/itfoundry/kalam/releases.atom',
-          checkpoint: '2ae5d72c3ec731407999c0dc22df8bb6e42cc544b708eabf148801d9b6fa339f'
+          checkpoint: '6e149085a2653d740ba1bc3e6ceaa881e31b4e16e729db8f7b9b551cf0d665d7'
   name 'Kalam'
   homepage 'https://github.com/itfoundry/kalam'
 
