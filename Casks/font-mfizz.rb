ddc7df6cf5f13b2b782572b484a35014564b4a7c
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-05-10 20:37:01 +0100

    Update font-mfizz to 2.4.1 (#1100)

--- Casks/font-mfizz.rb
+++ Casks/font-mfizz.rb
@@ -1,11 +1,11 @@
 cask 'font-mfizz' do
-  version '2.3.0'
-  sha256 '40284c1f095902e7d2ac043713b6051c62b729e115e32806dade2ea10aad817a'
+  version '2.4.1'
+  sha256 'c447c9d00a7a37b78af4e86ff8f787473fd6e9812b6b5033eb23dc62ffc5e044'
 
   # github.com/fizzed/font-mfizz was verified as official when first introduced to the cask
   url "https://github.com/fizzed/font-mfizz/releases/download/v#{version}/font-mfizz-#{version}.zip"
   appcast 'https://github.com/fizzed/font-mfizz/releases.atom',
-          checkpoint: '6ca7f4d16161f126408ced1c78385fc07f5cdebbc7507cd1082ae0415f719317'
+          checkpoint: '44080e750169f2958d4b54e6828a46dc9b72dcabdfe71f136b06ed2fcfe627d6'
   name 'Font Mfizz'
   homepage 'http://fizzed.com/oss/font-mfizz'
 
