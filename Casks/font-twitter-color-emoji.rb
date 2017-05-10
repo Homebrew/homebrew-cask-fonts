311b7c2be8624bd8ce1035b682422e2ba77b4ab3
giannitm <giannitm@users.noreply.github.com>
2017-05-10 21:12:33 +0100

    Update font-twitter-color-emoji to 1.2

--- Casks/font-twitter-color-emoji.rb
+++ Casks/font-twitter-color-emoji.rb
@@ -1,10 +1,10 @@
 cask 'font-twitter-color-emoji' do
-  version '1.1'
-  sha256 '59ce24afb398820bd505430898316f18498a2d670bd82f05594b5cf21a790f6e'
+  version '1.2'
+  sha256 '70f7e5e2838dc799fc9a0732f0837a9f94ed14d67cbedb1dc6fef774bb902650'
 
   url "https://github.com/eosrei/twemoji-color-font/releases/download/v#{version}/TwitterColorEmoji-SVGinOT-#{version}.zip"
   appcast 'https://github.com/eosrei/twemoji-color-font/releases',
-          checkpoint: 'd18e5fae1348653a1cb6c3be5bc1d5086ff32ec7c82fbf7a27e3bf1aba33ce04'
+          checkpoint: '12c1811f678df4bf301ad1482a5890cde5e73363337fcdef8db70917cba1a26d'
   name 'Twitter Color Emoji'
   homepage 'https://github.com/eosrei/twemoji-color-font/'
 
