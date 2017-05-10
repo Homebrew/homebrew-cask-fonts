aaea71a5b702ac5fbb4cbb8edec06361789debe8
giannitm <gianni-tammcmillan@Giannis-MacBook-Pro.local>
2017-05-10 18:41:54 +0100

    Update font-nika to 1.0.0

--- Casks/font-nika.rb
+++ Casks/font-nika.rb
@@ -3,8 +3,8 @@ cask 'font-nika' do
   sha256 'a0e435a23fe971eddb3f39883c606c92b43f9a7b1fc0d762509a291996960d2b'
 
   url "https://github.com/font-store/NikaFont/releases/download/v#{version}/nika.v#{version}.zip"
-  appcast 'https://github.com/font-store/font-nika/releases.atom',
-          checkpoint: '9c2bba39e9b30c65f708a26a4aa3bec03ef276f0c878115cc6252dd86756e4f1'
+  appcast 'https://github.com/font-store/NikaFont/releases.atom',
+          checkpoint: '92d8e58df9110eb6eb6ac60c9eff3c327e06209ede20de8b2937a65a1c179725'
   name 'Nika'
   homepage 'https://github.com/font-store/font-nika'
 
