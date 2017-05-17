730ea9aaa256f20e2bfd68cdd8adfad2e5e5877a
Gobinath Mallaiyan <i@gobinath.com>
2017-05-17 00:55:32 -0400

    Update font-didact-gothic to 20110429

--- Casks/font-didact-gothic.rb
+++ Casks/font-didact-gothic.rb
@@ -1,9 +1,9 @@
 cask 'font-didact-gothic' do
   version '20110429'
-  sha256 '01c9c7791a161a9022eee3b172ebfa33300891557471f94f488fb7b8442630d8'
+  sha256 'b98f9e091b6337a86f4e0f69c94c31905e1e287782853f472e176b4e28d3f81f'
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/didactgothic/DidactGothic.ttf'
+  url 'https://github.com/google/fonts/raw/master/ofl/didactgothic/DidactGothic-Regular.ttf'
   name 'Didact Gothic'
   homepage 'http://www.google.com/fonts/specimen/Didact%20Gothic'
 
