3641f357e958c60afba4122c7f6c7ea859faf2e9
Miccal Matthews <miccal.matthews@gmail.com>
2017-06-01 16:36:38 +0800

    Update font-gentium-basic.rb

--- Casks/font-gentium-basic.rb
+++ Casks/font-gentium-basic.rb
@@ -2,12 +2,12 @@ cask 'font-gentium-basic' do
   version '1.102'
   sha256 '2f1a2c5491d7305dffd3520c6375d2f3e14931ee35c6d8ae1e8f098bf1a7b3cc'
 
-  url "http://software.sil.org/downloads/r/gentium/GentiumBasic_1102.zip"
+  url "http://software.sil.org/downloads/r/gentium/GentiumBasic_#{version.no_dots}.zip"
   name 'Gentium Basic'
   homepage 'http://software.sil.org/gentium/'
 
-  font 'GentiumBasic_1102/GenBasB.ttf'
-  font 'GentiumBasic_1102/GenBasBI.ttf'
-  font 'GentiumBasic_1102/GenBasI.ttf'
-  font 'GentiumBasic_1102/GenBasR.ttf'
+  font "GentiumBasic_#{version.no_dots}/GenBasB.ttf"
+  font "GentiumBasic_#{version.no_dots}/GenBasBI.ttf"
+  font "GentiumBasic_#{version.no_dots}/GenBasI.ttf"
+  font "GentiumBasic_#{version.no_dots}/GenBasR.ttf"
 end
