0345712520cb0c5790a4bdc51b7ac6f1ac5efdf0
Caleb Maclennan <caleb@alerque.com>
2017-06-01 10:17:16 +0300

    Update gentium-basic and gentium-book-basic to 1.102 and update family URLs

--- Casks/font-gentium-book-basic.rb
+++ Casks/font-gentium-book-basic.rb
@@ -1,17 +1,13 @@
 cask 'font-gentium-book-basic' do
-  version :latest
-  sha256 :no_check
+  version '1.102'
+  sha256 '2f1a2c5491d7305dffd3520c6375d2f3e14931ee35c6d8ae1e8f098bf1a7b3cc'
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/gentiumbookbasic',
-      using:      :svn,
-      revision:   '50',
-      trust_cert: true
+  url "http://software.sil.org/downloads/r/gentium/GentiumBasic_1102.zip"
   name 'Gentium Book Basic'
-  homepage 'http://www.google.com/fonts/specimen/Gentium%20Book%20Basic'
+  homepage 'http://software.sil.org/gentium/'
 
-  font 'GenBkBasB.ttf'
-  font 'GenBkBasBI.ttf'
-  font 'GenBkBasI.ttf'
-  font 'GenBkBasR.ttf'
+  font 'GentiumBasic_1102/GenBkBasB.ttf'
+  font 'GentiumBasic_1102/GenBkBasBI.ttf'
+  font 'GentiumBasic_1102/GenBkBasI.ttf'
+  font 'GentiumBasic_1102/GenBkBasR.ttf'
 end
