652d70174b9486bb13f2765a1f4e2d2ab4e237d2
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-06-20 01:29:03 +0100

    Update font-vazir to 11.0.1 (#1209)

--- Casks/font-vazir.rb
+++ Casks/font-vazir.rb
@@ -1,11 +1,11 @@
 cask 'font-vazir' do
-  version '10.0.0-beta'
-  sha256 '117351354399015132ff2993ebb769600d889e0a9bbf16e7ea456996a85c8d47'
+  version '11.0.1'
+  sha256 '4181de7939e24d2fadedd92faadd25971e0af9076fa569125c2079cf1b28dd93'
 
   # github.com/rastikerdar was verified as official when first introduced to the cask
   url "https://github.com/rastikerdar/vazir-font/releases/download/v#{version}/vazir-font-v#{version}.zip"
   appcast 'https://github.com/rastikerdar/vazir-font/releases.atom',
-          checkpoint: '356523aaaf06bab31561115ba0189258b7e8c1f5721f92f424d620cd49adf14f'
+          checkpoint: '0e5df32d86a95a8e9222be11ef5d01289f15403258bbf068a43d14724c9ccbb5'
   name 'Vazir'
   homepage 'https://rastikerdar.github.io/vazir-font/'
 
