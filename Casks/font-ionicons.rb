a03ac1fcf64f8d97171c9753b56287cf8329fe39
Gianni Tam-McMillan <giannitm@users.noreply.github.com>
2017-06-20 01:28:49 +0100

    Update font-ionicons to 2.0.1 (#1208)
    * Update font-ionicons to 2.0.1
    
    * Update homepage and url for font-ionicons

--- Casks/font-ionicons.rb
+++ Casks/font-ionicons.rb
@@ -2,11 +2,11 @@ cask 'font-ionicons' do
   version '2.0.1'
   sha256 'b222fcaede908b71d5b206db9fb7b625a07d313be00ee908eabd267604868661'
 
-  url "https://github.com/driftyco/ionicons/archive/v#{version}.zip"
-  appcast 'https://github.com/driftyco/ionicons/releases.atom',
-          checkpoint: '3b4af4ca1eb9b498b480c6f6a0fb39f25d7f70538d720110e758c18dd10a1b0a'
+  url "https://github.com/ionic-team/ionicons/archive/v#{version}.zip"
+  appcast 'https://github.com/ionic-team/ionicons/releases.atom',
+          checkpoint: 'fc4c879e72002442cb2e7ba738b886070c4c8409148b5b3c74dc0a043343730d'
   name 'Ionicons'
-  homepage 'https://github.com/driftyco/ionicons'
+  homepage 'https://github.com/ionic-team/ionicons'
 
   font "ionicons-#{version}/fonts/ionicons.ttf"
 end
