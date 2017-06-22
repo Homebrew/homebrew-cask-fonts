d21d7c667eef884157ee31778ac0cba32dfb3cde
Tim Sheridan <tghs@tghs.net>
2017-06-22 18:41:42 +0100

    font-gnu-unifont: Use automatic mirror selection

--- Casks/font-gnu-unifont.rb
+++ Casks/font-gnu-unifont.rb
@@ -2,7 +2,8 @@ cask 'font-gnu-unifont' do
   version '10.0.01'
   sha256 '7ec094a19f0e7c6f972088f44d6cd31701d42b80689aad998167b5b8892fdf79'
 
-  url "http://unifoundry.com/pub/unifont-#{version}/font-builds/unifont-#{version}.ttf"
+  # ftpmirror.gnu.org/unifont was verified as official when first introduced to the cask
+  url "http://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.ttf"
   name 'GNU Unifont'
   homepage 'http://unifoundry.com/unifont.html'
 
