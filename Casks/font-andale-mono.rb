a41fdb704d4320c3cd88b3f878514a38f4b69a6a
commitay <commitay@users.noreply.github.com>
2017-06-24 23:48:05 +1000

    Update 7 casks - remove appcasts

--- Casks/font-andale-mono.rb
+++ Casks/font-andale-mono.rb
@@ -3,8 +3,6 @@ cask 'font-andale-mono' do
   sha256 :no_check
 
   url 'https://downloads.sourceforge.net/corefonts/andale32.exe'
-  appcast 'https://sourceforge.net/projects/corefonts/rss',
-          checkpoint: '8d659740c2893218b3e1d16918a9372b2838f5e0e7ef0405d3103f2d563e7bd1'
   name 'Andale Mono'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
 
