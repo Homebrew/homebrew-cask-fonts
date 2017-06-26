b9f81982b538c9f7f0ef58c4a3ac586a15009990
commitay <commitay1@gmail.com>
2017-06-26 18:35:47 +1000

    Update 7 casks - remove appcasts (#1212)

--- Casks/font-webdings.rb
+++ Casks/font-webdings.rb
@@ -3,8 +3,6 @@ cask 'font-webdings' do
   sha256 :no_check
 
   url 'https://downloads.sourceforge.net/corefonts/webdin32.exe'
-  appcast 'https://sourceforge.net/projects/corefonts/rss',
-          checkpoint: '8d659740c2893218b3e1d16918a9372b2838f5e0e7ef0405d3103f2d563e7bd1'
   name 'Webdings'
   homepage 'http://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
 
