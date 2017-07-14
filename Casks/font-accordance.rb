d2335aca8381237d93e4a607a6ee5dc64b9c7b48
commitay <commitay@users.noreply.github.com>
2017-07-14 10:32:45 +1000

    add comment

--- Casks/font-accordance.rb
+++ Casks/font-accordance.rb
@@ -2,6 +2,7 @@ cask 'font-accordance' do
   version :latest
   sha256 :no_check
 
+  # accordancefiles1.com/products was verified as official when first introduced to the cask
   url 'http://www.accordancefiles1.com/products/Accordance_Font.zip'
   name 'Accordance'
   homepage 'https://www.accordancebible.com/Accordance-Font'
