998d0e6d5c839816e9f1f68c9ae1eb830b2cdf18
giannitm <giannitm@users.noreply.github.com>
2017-07-15 17:25:54 +0100

    Update font-fantasque-sans-mono to 1.7.1

--- Casks/font-fantasque-sans-mono.rb
+++ Casks/font-fantasque-sans-mono.rb
@@ -4,7 +4,7 @@ cask 'font-fantasque-sans-mono' do
 
   url "https://github.com/belluzj/fantasque-sans/releases/download/v#{version}/FantasqueSansMono.zip"
   appcast 'https://github.com/belluzj/fantasque-sans/releases.atom',
-          checkpoint: 'c2194a85e1a4985ebb6b40f0ed2564fd47f8c933348ae3d02cb7e06071eda6bc'
+          checkpoint: 'ad0c508b0cf280b5e16618b64299a19c628eabf7c6f470262ba9b8c244463864'
   name 'Fantasque Sans Mono'
   homepage 'https://github.com/belluzj/fantasque-sans'
 
