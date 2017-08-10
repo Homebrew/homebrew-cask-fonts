4300313e546b48e8e54df9038e86a71c4aca2c8e
Vítor Galvão <info@vitorgalvao.com>
2017-08-10 10:44:58 +0100

    Update font-codenewroman-nerd-font to 1.1.0

--- Casks/font-codenewroman-nerd-font.rb
+++ Casks/font-codenewroman-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-codenewroman-nerd-font' do
-  version '1.0.0'
-  sha256 '70d26f71f6d336733218989db2170ededdbb5c5248628b1303c26d6a3dc7fba6'
+  version '1.1.0'
+  sha256 '04495de7301c44dad34490704f6eae51f7e9f775b3b84f6537dc7728462929ba'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/CodeNewRoman.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: 'dbe84e88af08eb844f7f21de92a1fc57e8df10d3028055aff03e0441598806df'
+          checkpoint: '109f18cfd453156e38ffac165683bcfc2745e0c8dc07bd379a7f9ea19d0cbe41'
   name 'CodeNewRoman Nerd Font (CodeNewRoman)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
