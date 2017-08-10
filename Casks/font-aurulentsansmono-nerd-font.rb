51ddd87f11c46be093110425ea8b64ca59198cfd
Vítor Galvão <info@vitorgalvao.com>
2017-08-10 10:42:47 +0100

    Update font-aurulentsansmono-nerd-font to 1.1.0

--- Casks/font-aurulentsansmono-nerd-font.rb
+++ Casks/font-aurulentsansmono-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-aurulentsansmono-nerd-font' do
-  version '1.0.0'
-  sha256 '322e758c3783c307e107ca295c3c51abd04cc8f4aaa7c28445038ccab7bb2503'
+  version '1.1.0'
+  sha256 '302c417aa3caaae553ddcd7f4482ea568a4996189623f740a6a87883bd5c5457'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AurulentSansMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: 'dbe84e88af08eb844f7f21de92a1fc57e8df10d3028055aff03e0441598806df'
+          checkpoint: '109f18cfd453156e38ffac165683bcfc2745e0c8dc07bd379a7f9ea19d0cbe41'
   name 'AurulentSansMono Nerd Font (AurulentSansMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
