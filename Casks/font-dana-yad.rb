69d5294ddeaf314a534a3ba359ac7bf3c615c427
Lawrence <lawrencews@jhu.edu>
2017-08-12 15:02:28 -0400

    Add Hebrew script font: font-dana-yad

---
+++ Casks/font-dana-yad.rb
@@ -0,0 +1,10 @@
+cask 'font-dana-yad' do
+  version '1.000'
+  sha256 'd0115c1227f97c1f6c8b6f507adc32ef390dea245ed1b53042f3df99dee08d2e'
+
+  url 'https://alefalefalef.co.il/wp-content/themes/aaa/fonts/free/dana-yad-alefalefalef.zip'
+  name 'Dana Yad'
+  homepage 'https://alefalefalef.co.il/resources/פונטים-בחינם/'
+
+  font 'DanaYadAlefAlefAlef-Normal.otf'
+end
