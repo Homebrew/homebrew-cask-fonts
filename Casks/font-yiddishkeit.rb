b12b9f21fa17eba82fea1d7eea2b9ca95388cebe
Lawrence <lawrencews@jhu.edu>
2017-08-12 15:09:20 -0400

    Add Hebrew script font: font-yiddishkeit

---
+++ Casks/font-yiddishkeit.rb
@@ -0,0 +1,10 @@
+cask 'font-yiddishkeit' do
+  version '1.000'
+  sha256 '8ac82be50e173b6869d41406d8b4d32bbbd00ca028e26dd5dfb3555be06877c4'
+
+  url 'https://alefalefalef.co.il/wp-content/themes/aaa/fonts/free/YiddishkeitAlefAlefAlef-Bold.zip'
+  name 'Yiddishkeit'
+  homepage 'https://alefalefalef.co.il/resources/פונטים-בחינם/'
+
+  font 'YiddishkeitAlefAlefAlef-Bold.otf'
+end
