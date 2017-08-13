72d90d4310913383ecf784852359da4ca068e7d6
commitay <commitay@users.noreply.github.com>
2017-08-14 09:04:44 +1000

    Update font-dana-yad.rb

--- Casks/font-dana-yad.rb
+++ Casks/font-dana-yad.rb
@@ -1,10 +1,10 @@
-cask 'font-yiddishkeit' do
+cask 'font-dana-yad' do
   version :latest
   sha256 :no_check
 
-  url 'https://alefalefalef.co.il/wp-content/themes/aaa/fonts/free/YiddishkeitAlefAlefAlef-Bold.zip'
-  name 'Yiddishkeit'
+  url 'https://alefalefalef.co.il/wp-content/themes/aaa/fonts/free/dana-yad-alefalefalef.zip'
+  name 'Dana Yad'
   homepage 'https://alefalefalef.co.il/resources/פונטים-בחינם/'
 
-  font 'YiddishkeitAlefAlefAlef-Bold.otf'
+  font 'DanaYadAlefAlefAlef-Normal.otf'
 end
