7271611cc90cafef5383cbb634baed2e2a33f841
commitay <commitay@users.noreply.github.com>
2017-08-14 09:03:32 +1000

    Update font-yiddishkeit.rb

--- Casks/font-yiddishkeit.rb
+++ Casks/font-yiddishkeit.rb
@@ -1,6 +1,6 @@
 cask 'font-yiddishkeit' do
-  version '1.000'
-  sha256 '8ac82be50e173b6869d41406d8b4d32bbbd00ca028e26dd5dfb3555be06877c4'
+  version :latest
+  sha256 :no_check
 
   url 'https://alefalefalef.co.il/wp-content/themes/aaa/fonts/free/YiddishkeitAlefAlefAlef-Bold.zip'
   name 'Yiddishkeit'
