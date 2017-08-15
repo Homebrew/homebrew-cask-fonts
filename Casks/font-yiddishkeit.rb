faace57154eee517c5284b54e4c387ea57743d8f
lwolfsonkin <lawrencews@jhu.edu>
2017-08-15 04:18:40 -0400

    Add Hebrew script font: font-yiddishkeit (#1256)
    * Add Hebrew script font: font-yiddishkeit
    
    * Update font-yiddishkeit.rb

---
+++ Casks/font-yiddishkeit.rb
@@ -0,0 +1,10 @@
+cask 'font-yiddishkeit' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://alefalefalef.co.il/wp-content/themes/aaa/fonts/free/YiddishkeitAlefAlefAlef-Bold.zip'
+  name 'Yiddishkeit'
+  homepage 'https://alefalefalef.co.il/resources/פונטים-בחינם/'
+
+  font 'YiddishkeitAlefAlefAlef-Bold.otf'
+end
