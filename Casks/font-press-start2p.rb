0acc3d3dcc117e48030d81d884a1fc733ac533f6
Miccal Matthews <miccal.matthews@gmail.com>
2017-08-15 16:19:31 +0800

    Update font-press-start2p.rb

--- Casks/font-press-start2p.rb
+++ Casks/font-press-start2p.rb
@@ -1,6 +1,6 @@
 cask 'font-press-start2p' do
-  version '3.000'
-  sha256 '034c77f1f05ec89421e4a63f0e3a4ca1ecf852cc6d2bf611f126f275728e017d'
+  version :latest
+  sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/pressstart2p/PressStart2P-Regular.ttf'
