1ec6f79a1b4cc63175a736de49f400edf6b1d6cc
lwolfsonkin <lawrencews@jhu.edu>
2017-08-15 04:18:24 -0400

    Add Hebrew script font: font-gveret-levin (#1255)
    * Add Hebrew script font: font-gveret-levin
    
    * Update font-gveret-levin.rb

---
+++ Casks/font-gveret-levin.rb
@@ -0,0 +1,10 @@
+cask 'font-gveret-levin' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://alefalefalef.co.il/wp-content/themes/aaa/fonts/free/GveretLevinAlefAlefAlef.zip'
+  name 'Gveret Levin'
+  homepage 'https://alefalefalef.co.il/resources/פונטים-בחינם/'
+
+  font 'otf/GveretLevinAlefAlefAlef-Regular.otf'
+end
