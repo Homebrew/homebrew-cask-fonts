8fa3b9c3ad37fee558987d2e18b2723358efc35f
Leif Andersen <leif@leifandersen.net>
2017-08-23 13:10:36 -0400

    The font filename has changed.
    From PermanentMarker to from PermanentMarker-Regular

--- Casks/font-permanent-marker.rb
+++ Casks/font-permanent-marker.rb
@@ -3,9 +3,9 @@ cask 'font-permanent-marker' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/permanentmarker/PermanentMarker.ttf'
+  url 'https://github.com/google/fonts/raw/master/apache/permanentmarker/PermanentMarker-Regular.ttf'
   name 'Permanent Marker'
   homepage 'http://www.google.com/fonts/specimen/Permanent+Marker'
 
-  font 'PermanentMarker.ttf'
+  font 'PermanentMarker-Regular.ttf'
 end
