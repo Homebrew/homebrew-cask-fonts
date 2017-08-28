f3aff4435bc6209976b910f2d007a61bb3c2a298
Sebastian Wiesner <swiesner@lunaryorn.com>
2017-08-28 10:03:45 +0200

    Update font-vollkorn to 4.015

--- Casks/font-vollkorn.rb
+++ Casks/font-vollkorn.rb
@@ -1,16 +1,16 @@
 cask 'font-vollkorn' do
-  version '3.005'
-  sha256 '2ba29d185bcf943f844a88546fbc4ec668d53f2564d8aa1c5837ffb9a45f4fab'
+  version '4.015'
+  sha256 'b5cb2bb11e9a9f19f6af09bba84f905f22c5c19619ee4424742dc73caa09dce5'
 
   url "http://vollkorn-typeface.com/download/vollkorn-#{version.dots_to_hyphens}.zip"
   name 'Vollkorn'
   homepage 'http://vollkorn-typeface.com/'
 
+  font 'PS-OTF/Vollkorn-Black.otf'
+  font 'PS-OTF/Vollkorn-BlackItalic.otf'
   font 'PS-OTF/Vollkorn-Bold.otf'
   font 'PS-OTF/Vollkorn-BoldItalic.otf'
   font 'PS-OTF/Vollkorn-Italic.otf'
-  font 'PS-OTF/Vollkorn-Medium.otf'
-  font 'PS-OTF/Vollkorn-MediumItalic.otf'
   font 'PS-OTF/Vollkorn-Regular.otf'
   font 'PS-OTF/Vollkorn-Semibold.otf'
   font 'PS-OTF/Vollkorn-SemiboldItalic.otf'
