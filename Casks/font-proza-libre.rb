a88fdea77277731dd4cd237693514f8683c8cc9c
David Thompson <david.thompson@kitware.com>
2017-08-31 21:47:44 -0400

    Add font-proza-libre v1.0.

---
+++ Casks/font-proza-libre.rb
@@ -0,0 +1,19 @@
+cask 'font-proza-libre' do
+  version '1.0'
+  sha256 'cbd3bb929d905330ad9e2d4b2efc3edf5c351eb5b4a238bd87367e74836fa9c9'
+
+  url 'https://github.com/jasperdewaard/Proza-Libre/archive/1.0.zip'
+  name 'Proza Libre'
+  homepage 'https://github.com/jasperdewaard/Proza-Libre'
+
+  font 'Proza-Libre-1.0/Fonts/ProzaLibre-Bold.ttf'
+  font 'Proza-Libre-1.0/Fonts/ProzaLibre-BoldItalic.ttf'
+  font 'Proza-Libre-1.0/Fonts/ProzaLibre-ExtraBold.ttf'
+  font 'Proza-Libre-1.0/Fonts/ProzaLibre-ExtraBoldItalic.ttf'
+  font 'Proza-Libre-1.0/Fonts/ProzaLibre-Italic.ttf'
+  font 'Proza-Libre-1.0/Fonts/ProzaLibre-Medium.ttf'
+  font 'Proza-Libre-1.0/Fonts/ProzaLibre-MediumItalic.ttf'
+  font 'Proza-Libre-1.0/Fonts/ProzaLibre-Regular.ttf'
+  font 'Proza-Libre-1.0/Fonts/ProzaLibre-SemiBold.ttf'
+  font 'Proza-Libre-1.0/Fonts/ProzaLibre-SemiBoldItalic.ttf'
+end
