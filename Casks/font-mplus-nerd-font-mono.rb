3d36f3a8a0de7e0c459f1322016ca473ef025b8e
giannitm <giannitm@users.noreply.github.com>
2017-09-10 17:48:02 +0100

    Update font-mplus-nerd-font-mono to 1.1.0

--- Casks/font-mplus-nerd-font-mono.rb
+++ Casks/font-mplus-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-mplus-nerd-font-mono' do
-  version '1.0.0'
-  sha256 '102e1bf17dd1962b19d508e76bdb29aef4571e7e18040e9b2561879f3955a82a'
+  version '1.1.0'
+  sha256 '3d5f7cb08eaeb22c256d5c62af807e4ddc66f5d9834590b9abdb0728e08f0125'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/MPlus.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: 'dbe84e88af08eb844f7f21de92a1fc57e8df10d3028055aff03e0441598806df'
+          checkpoint: '109f18cfd453156e38ffac165683bcfc2745e0c8dc07bd379a7f9ea19d0cbe41'
   name 'mplus Nerd Font (MPlus)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
