2efe7d02ed556bb28b132a15e1bebfbbf1b20db9
giannitm <giannitm@users.noreply.github.com>
2017-09-10 17:48:24 +0100

    Update font-profont-nerd-font-mono to 1.1.0 (#1292)

--- Casks/font-profont-nerd-font-mono.rb
+++ Casks/font-profont-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-profont-nerd-font-mono' do
-  version '1.0.0'
-  sha256 '8a6fdd5ffa5473ecb337cb9606cf89b4d26f1454c5722ca19ff7741055fd1ce5'
+  version '1.1.0'
+  sha256 '4463c9f5a4dd1dbd27d198c893819e7552d77e607f33e806e0b916fc506f7694'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ProFont.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: 'dbe84e88af08eb844f7f21de92a1fc57e8df10d3028055aff03e0441598806df'
+          checkpoint: '109f18cfd453156e38ffac165683bcfc2745e0c8dc07bd379a7f9ea19d0cbe41'
   name 'ProFontIIx Nerd Font (ProFont)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
