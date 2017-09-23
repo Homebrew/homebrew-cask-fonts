93572b39445877627b8fad98e74eecf9a10664cb
Nick Wind-McJetters <nick@elearningexperts.net>
2017-09-23 02:18:58 -0400

    Update font-luckiest-guy to latest

--- Casks/font-luckiest-guy.rb
+++ Casks/font-luckiest-guy.rb
@@ -3,7 +3,7 @@ cask 'font-luckiest-guy' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/luckiestguy/LuckiestGuy.ttf'
+  url 'https://github.com/google/fonts/raw/master/apache/luckiestguy/LuckiestGuy-Regular.ttf'
   name 'Luckiest Guy'
   homepage 'http://www.google.com/fonts/specimen/Luckiest+Guy'
 
