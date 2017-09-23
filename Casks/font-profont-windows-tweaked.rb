017a18c3628e2fae494494fa9084222986aa780b
nrlquaker <ustynov.vy@gmail.com>
2017-09-23 14:58:11 +0300

    Add font-profont-windows-tweaked 2.3 (#1310)
    * Add font-profont-windows-tweaked 2.3
    
    * latest

---
+++ Casks/font-profont-windows-tweaked.rb
@@ -0,0 +1,10 @@
+cask 'font-profont-windows-tweaked' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://tobiasjung.name/downloadfile.php?file=ProFontWinTweaked.zip'
+  name 'ProFontWindows'
+  homepage 'http://tobiasjung.name/profont/'
+
+  font 'ProFontWinTweaked/ProFontWindows.ttf'
+end
