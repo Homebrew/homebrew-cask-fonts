c424060fc897c91eebdd4b8690f543381ffec555
David <david.bialik@gmail.com>
2017-09-26 03:45:05 +0200

    Update font-xkcd to latest (#1317)
    The location of the file moved into a subfolder.

--- Casks/font-xkcd.rb
+++ Casks/font-xkcd.rb
@@ -2,7 +2,7 @@ cask 'font-xkcd' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/ipython/xkcd-font/raw/master/build/xkcd.otf'
+  url 'https://github.com/ipython/xkcd-font/raw/master/xkcd/build/xkcd.otf'
   name 'xkcd'
   homepage 'https://github.com/ipython/xkcd-font'
 
