e916acfa6db949e9b1b24bdbaf1404ff85b4edc2
Ali Karbassi <karbassi@users.noreply.github.com>
2017-10-12 16:05:38 -0500

    Updated Merriweather's HeavyItalic to BlackItalic

--- Casks/font-merriweather.rb
+++ Casks/font-merriweather.rb
@@ -5,15 +5,15 @@ cask 'font-merriweather' do
   # github.com/google/fonts was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/merriweather',
       using:      :svn,
-      revision:   '50',
+      revision:   '1208',
       trust_cert: true
   name 'Merriweather'
   homepage 'http://www.google.com/fonts/specimen/Merriweather'
 
   font 'Merriweather-Black.ttf'
+  font 'Merriweather-BlackItalic.ttf'
   font 'Merriweather-Bold.ttf'
   font 'Merriweather-BoldItalic.ttf'
-  font 'Merriweather-HeavyItalic.ttf'
   font 'Merriweather-Italic.ttf'
   font 'Merriweather-Light.ttf'
   font 'Merriweather-LightItalic.ttf'
