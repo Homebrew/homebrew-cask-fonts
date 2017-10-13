611e3f4030011b2c99ce9afc734161a697a84635
Ali Karbassi <karbassi@users.noreply.github.com>
2017-10-12 19:19:47 -0500

    Update font-oswald.rb (#1329)

--- Casks/font-oswald.rb
+++ Casks/font-oswald.rb
@@ -5,12 +5,15 @@ cask 'font-oswald' do
   # github.com/google/fonts was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/oswald',
       using:      :svn,
-      revision:   '50',
+      revision:   '1208',
       trust_cert: true
   name 'Oswald'
   homepage 'http://www.google.com/fonts/specimen/Oswald'
 
   font 'Oswald-Bold.ttf'
+  font 'Oswald-ExtraLight.ttf'
   font 'Oswald-Light.ttf'
+  font 'Oswald-Medium.ttf'
   font 'Oswald-Regular.ttf'
+  font 'Oswald-SemiBold.ttf'
 end
