f370691ec08fefe23be353aa2c618f49d97b0b74
Alex Guerrero <hello@alexguerrero.es>
2017-10-15 15:52:15 +0200

    Update font-walter-turncoat filename and download URL

--- Casks/font-walter-turncoat.rb
+++ Casks/font-walter-turncoat.rb
@@ -3,9 +3,9 @@ cask 'font-walter-turncoat' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/walterturncoat/WalterTurncoat.ttf'
+  url 'https://github.com/google/fonts/raw/master/apache/walterturncoat/WalterTurncoat-Regular.ttf'
   name 'Walter Turncoat'
   homepage 'http://www.google.com/fonts/specimen/Walter+Turncoat'
 
-  font 'WalterTurncoat.ttf'
+  font 'WalterTurncoat-Regular.ttf'
 end
