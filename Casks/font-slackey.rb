b572817e36540dc6e1d307c2c47434d670987ada
Alex Guerrero <hello@alexguerrero.es>
2017-10-15 16:15:41 +0200

    Update font-slackey filename and download URL

--- Casks/font-slackey.rb
+++ Casks/font-slackey.rb
@@ -3,9 +3,9 @@ cask 'font-slackey' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/slackey/Slackey.ttf'
+  url 'https://github.com/google/fonts/raw/master/apache/slackey/Slackey-Regular.ttf'
   name 'Slackey'
   homepage 'http://www.google.com/fonts/specimen/Slackey'
 
-  font 'Slackey.ttf'
+  font 'Slackey-Regular.ttf'
 end
