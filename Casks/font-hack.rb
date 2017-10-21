2e2ba6dd41ce7bd7157fba569a6e9280a6309750
commitay <commitay@users.noreply.github.com>
2017-10-21 11:13:19 +1000

    font-hack

--- Casks/font-hack.rb
+++ Casks/font-hack.rb
@@ -1,16 +1,16 @@
 cask 'font-hack' do
   version '3.000'
-  sha256 '0861ab5d6ce6756cfd329d9339dc29408888792f2d5877b43dc39da0ab79059a'
+  sha256 '238765b81e88dd207d241a6ae46abeab37346d850566334fc1cca8221ea6da40'
 
   # github.com/source-foundry/Hack was verified as official when first introduced to the cask
-  url "https://github.com/source-foundry/Hack/archive/v#{version}.zip"
+  url "https://github.com/source-foundry/Hack/releases/download/v#{version}/Hack-v#{version}-ttf.zip"
   appcast 'https://github.com/source-foundry/Hack/releases.atom',
           checkpoint: 'faf7232c5570d40cd2b95d73ddafe6d405f3badb19f72a914821ec748d63fd22'
   name 'Hack'
   homepage 'http://sourcefoundry.org/hack/'
 
-  font "Hack-#{version}/build/ttf/Hack-Regular.ttf"
-  font "Hack-#{version}/build/ttf/Hack-Italic.ttf"
-  font "Hack-#{version}/build/ttf/Hack-Bold.ttf"
-  font "Hack-#{version}/build/ttf/Hack-BoldItalic.ttf"
+  font 'ttf/Hack-Regular.ttf'
+  font 'ttf/Hack-Italic.ttf'
+  font 'ttf/Hack-Bold.ttf'
+  font 'ttf/Hack-BoldItalic.ttf'
 end
