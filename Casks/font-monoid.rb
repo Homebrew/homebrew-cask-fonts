207a07cc92d7e22115bba49cb23d789aa6701d61
commitay <commitay@users.noreply.github.com>
2017-10-22 04:12:53 +1000

    squiggly heredocs (#1342)

--- Casks/font-monoid.rb
+++ Casks/font-monoid.rb
@@ -14,7 +14,7 @@ cask 'font-monoid' do
   font 'Monoid-Regular.ttf'
   font 'Monoid-Retina.ttf'
 
-  caveats <<-EOS.undent
+  caveats <<~EOS
     #{token} only installs the Normal Weight, Medium LineHeight, with Ligatures variant.
     To get other styles, please tap the sscotth/homebrew-monoid repo
       brew tap sscotth/monoid
