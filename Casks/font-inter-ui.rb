5d29a15aa32c4ecb2d8d7d2df6b896aaa886854a
commitay <commitay@users.noreply.github.com>
2017-10-26 14:11:00 +1000

    Update font-inter-ui to 2.2 (#1344)
    * Update font-inter-ui to 2.2
    
    * italic

--- Casks/font-inter-ui.rb
+++ Casks/font-inter-ui.rb
@@ -1,11 +1,11 @@
 cask 'font-inter-ui' do
-  version '2.1'
-  sha256 '81d9ca2c73eea3df320f42239e66272b5b33a728af7f568e51afc35c15437eed'
+  version '2.2'
+  sha256 '6c09f26d252be17db4243cf7b9136fef5f5e2011c22865de5bfd6ada7430895c'
 
   # github.com/rsms/inter was verified as official when first introduced to the cask
   url "https://github.com/rsms/inter/releases/download/v#{version}/Inter-UI-#{version}.zip"
   appcast 'https://github.com/rsms/inter/releases.atom',
-          checkpoint: 'a1f418970b7a614152c367beea034db113dd27c9195262d072688e91f74589d0'
+          checkpoint: '74b54e95c064c6ef90dc21adaf909c3deb8335bc7645bd015a010d8d9a0e5299'
   name 'Inter UI'
   homepage 'https://rsms.me/inter/'
 
@@ -13,8 +13,8 @@ cask 'font-inter-ui' do
   font 'Inter UI (OTF)/Inter-UI-BlackItalic.otf'
   font 'Inter UI (OTF)/Inter-UI-Bold.otf'
   font 'Inter UI (OTF)/Inter-UI-BoldItalic.otf'
+  font 'Inter UI (OTF)/Inter-UI-Italic.otf'
   font 'Inter UI (OTF)/Inter-UI-Medium.otf'
   font 'Inter UI (OTF)/Inter-UI-MediumItalic.otf'
   font 'Inter UI (OTF)/Inter-UI-Regular.otf'
-  font 'Inter UI (OTF)/Inter-UI-RegularItalic.otf'
 end
