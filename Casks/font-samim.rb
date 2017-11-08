7332e3804fca6c3155e6e10446ffdf6bb702f872
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2017-11-08 02:26:48 +0000

    Update font-samim to 2.0.1 (#1349)

--- Casks/font-samim.rb
+++ Casks/font-samim.rb
@@ -1,11 +1,11 @@
 cask 'font-samim' do
-  version '0.10.2'
-  sha256 '802d687286d1f47c3797d1c8bd4c7b70fc35735a67e41a99b1a371ecbb0ea5fb'
+  version '2.0.1'
+  sha256 '2d03590a09edc23fc5336edb2a9b67586bbccda14b2320d26a06baf107f51684'
 
   # github.com/rastikerdar was verified as official when first introduced to the cask
   url "https://github.com/rastikerdar/samim-font/releases/download/v#{version}/samim-font-v#{version}.zip"
   appcast 'https://github.com/rastikerdar/samim-font/releases.atom',
-          checkpoint: '5f4845ee6fe7d40dbae27aa00ba7735e3120a1d079002bc1299890870518b01c'
+          checkpoint: '80d6573698525173e1b0cedf925ab8d66692179669fcfc007877b22a3ab712da'
   name 'Samim'
   homepage 'http://rastikerdar.github.io/samim-font'
 
