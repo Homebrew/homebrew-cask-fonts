608fb36fbdb1818b884373695a88a98570805d14
Tomonori Kimura <tomonori.kimura.cx@gmail.com>
2017-11-10 18:52:42 +0900

    Update font-architects-daughter to latest

--- Casks/font-architects-daughter.rb
+++ Casks/font-architects-daughter.rb
@@ -3,9 +3,9 @@ cask 'font-architects-daughter' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/architectsdaughter/ArchitectsDaughter.ttf'
+  url 'https://github.com/google/fonts/raw/master/ofl/architectsdaughter/ArchitectsDaughter-Regular.ttf'
   name 'Architects Daughter'
   homepage 'http://www.google.com/fonts/specimen/Architects+Daughter'
 
-  font 'ArchitectsDaughter.ttf'
+  font 'ArchitectsDaughter-Regular.ttf'
 end
