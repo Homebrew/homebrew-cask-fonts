014c7a4d88428ad1d50ab9e27bf3368b615194b5
commitay <commitay@users.noreply.github.com>
2017-11-11 15:10:30 +1000

    latest / no_check

--- Casks/font-coming-soon.rb
+++ Casks/font-coming-soon.rb
@@ -1,6 +1,6 @@
 cask 'font-coming-soon' do
-  version '1.001'
-  sha256 '0fa985cc8a627b9c25b5d31bb6cbb98aa5b0a630f33b40bd0f6cf93fa3aaf8cc'
+  version :latest
+  sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/apache/comingsoon/ComingSoon-Regular.ttf'
