92ae86d03f732a037130e3b1d01eb62a748827c6
Tomonori Kimura <tomonori.kimura.cx@gmail.com>
2017-11-11 11:45:30 +0900

    Update font-angkor to latest (#1360)

--- Casks/font-angkor.rb
+++ Casks/font-angkor.rb
@@ -3,9 +3,9 @@ cask 'font-angkor' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/angkor/Angkor.ttf'
+  url 'https://github.com/google/fonts/raw/master/ofl/angkor/Angkor-Regular.ttf'
   name 'Angkor'
   homepage 'http://www.google.com/fonts/specimen/Angkor'
 
-  font 'Angkor.ttf'
+  font 'Angkor-Regular.ttf'
 end
