dfa42fa9622e256cbe2c1064884756187c66b85a
Tomonori Kimura <tomonori.kimura.cx@gmail.com>
2017-11-11 13:53:59 +0900

    Update font-coda-caption to 1.002

--- Casks/font-coda-caption.rb
+++ Casks/font-coda-caption.rb
@@ -1,6 +1,6 @@
 cask 'font-coda-caption' do
-  version :latest
-  sha256 :no_check
+  version '1.002'
+  sha256 'c08c6f885864896047ce2be2e2ce744067e3a0d87f0f76929efaa9f70eeae576'
 
   # github.com/google/fonts was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/codacaption/CodaCaption-Heavy.ttf'
