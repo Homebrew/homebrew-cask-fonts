62537c1977ff4d669504e83cf5683b934018b319
Tomonori Kimura <tomonori.kimura.cx@gmail.com>
2017-11-11 13:57:27 +0900

    Update font-chewy to 1.001

--- Casks/font-chewy.rb
+++ Casks/font-chewy.rb
@@ -1,6 +1,6 @@
 cask 'font-chewy' do
-  version :latest
-  sha256 :no_check
+  version '1.001'
+  sha256 '7cf75ea288f82fd20badea8ab4da7a656a96a7277c170811e813b3d3d6294147'
 
   # github.com/google/fonts was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/apache/chewy/Chewy.ttf'
