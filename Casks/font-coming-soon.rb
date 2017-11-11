c30bebbf321afcc336107b2226de604c1a3ea95e
Tomonori Kimura <tomonori.kimura.cx@gmail.com>
2017-11-11 13:50:13 +0900

    Update font-coming-soon to 1.001

--- Casks/font-coming-soon.rb
+++ Casks/font-coming-soon.rb
@@ -1,11 +1,11 @@
 cask 'font-coming-soon' do
-  version '1.000'
-  sha256 '1c7a44d75a7e130a89ca46bf6ece6d34b0085cd82291d745fae5a9844bd9a615'
+  version '1.001'
+  sha256 '0fa985cc8a627b9c25b5d31bb6cbb98aa5b0a630f33b40bd0f6cf93fa3aaf8cc'
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/comingsoon/ComingSoon.ttf'
+  url 'https://github.com/google/fonts/raw/master/apache/comingsoon/ComingSoon-Regular.ttf'
   name 'Coming Soon'
   homepage 'http://www.google.com/fonts/specimen/Coming%20Soon'
 
-  font 'ComingSoon.ttf'
+  font 'ComingSoon-Regular.ttf'
 end
