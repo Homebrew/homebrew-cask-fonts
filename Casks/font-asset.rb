0c1814b33614bd616414039da29575209e0b57c5
Tomonori Kimura <tomonori.kimura.cx@gmail.com>
2017-11-11 11:44:50 +0900

    Update font-asset to latest (#1356)

--- Casks/font-asset.rb
+++ Casks/font-asset.rb
@@ -3,9 +3,9 @@ cask 'font-asset' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/asset/Asset.ttf'
+  url 'https://github.com/google/fonts/raw/master/ofl/asset/Asset-Regular.ttf'
   name 'Asset'
   homepage 'http://www.google.com/fonts/specimen/Asset'
 
-  font 'Asset.ttf'
+  font 'Asset-Regular.ttf'
 end
