fa0f1f62da9e66600e5652f9d199175da781902d
commitay <commitay@users.noreply.github.com>
2017-11-13 08:04:57 +1000

    Update font-volkhov to latest (#1368)

--- Casks/font-volkhov.rb
+++ Casks/font-volkhov.rb
@@ -3,7 +3,7 @@ cask 'font-volkhov' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/fonts/volkhov',
+  url 'https://github.com/google/fonts/trunk/ofl/volkhov',
       using:      :svn,
       revision:   '50',
       trust_cert: true
