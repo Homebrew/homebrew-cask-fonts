9338e1425035732f6d835d37eab0321cf56b56e8
Tomonori Kimura <tomonori.kimura.cx@gmail.com>
2017-11-14 13:24:38 +0900

    Update font-cherry-cream-soda to latest (#1371)

--- Casks/font-cherry-cream-soda.rb
+++ Casks/font-cherry-cream-soda.rb
@@ -3,9 +3,9 @@ cask 'font-cherry-cream-soda' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/cherrycreamsoda/CherryCreamSoda.ttf'
+  url 'https://github.com/google/fonts/raw/master/apache/cherrycreamsoda/CherryCreamSoda-Regular.ttf'
   name 'Cherry Cream Soda'
   homepage 'http://www.google.com/fonts/specimen/Cherry+Cream+Soda'
 
-  font 'CherryCreamSoda.ttf'
+  font 'CherryCreamSoda-Regular.ttf'
 end
