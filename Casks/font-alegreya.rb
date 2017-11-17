47efb6bf64154cd0a027d35e47509e68673adf23
Damien Pollet <damien.pollet@gmail.com>
2017-11-17 13:41:10 +0100

    Update font-alegreya & font-alegreya-sans to 2.004 from github (#1373)

--- Casks/font-alegreya.rb
+++ Casks/font-alegreya.rb
@@ -1,19 +1,31 @@
 cask 'font-alegreya' do
-  version :latest
-  sha256 :no_check
+  version '2.004'
+  sha256 'a3fd223741fcdf50328f2d37a378cffba2fe2dddc55d29f41e754cb2447f0b2c'
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/alegreya',
-      using:      :svn,
-      revision:   '50',
-      trust_cert: true
+  url "https://github.com/huertatipografica/Alegreya/archive/v#{version}.zip"
+  appcast 'https://github.com/huertatipografica/Alegreya/releases.atom',
+          checkpoint: '8a8502eac21fdf9cb5d3776254ece76483456eda4ede4817045a01b59c1ca0e0'
   name 'Alegreya'
-  homepage 'http://www.google.com/fonts/specimen/Alegreya'
+  homepage 'https://github.com/huertatipografica/Alegreya'
 
-  font 'Alegreya-Black.ttf'
-  font 'Alegreya-BlackItalic.ttf'
-  font 'Alegreya-Bold.ttf'
-  font 'Alegreya-BoldItalic.ttf'
-  font 'Alegreya-Italic.ttf'
-  font 'Alegreya-Regular.ttf'
+  font "Alegreya-#{version}/fonts/otf/Alegreya-Black.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-BlackItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-Bold.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-BoldItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-ExtraBold.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-ExtraBoldItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-Italic.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-Medium.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-MediumItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/Alegreya-Regular.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-Black.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-BlackItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-Bold.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-BoldItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-ExtraBold.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-ExtraBoldItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-Italic.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-Medium.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-MediumItalic.otf"
+  font "Alegreya-#{version}/fonts/otf/AlegreyaSC-Regular.otf"
 end
