f4de732b7ba9ffa0aebc979f3296d1d1a2389649
Tomonori Kimura <tomonori.kimura.cx@gmail.com>
2017-11-24 08:46:05 +0900

    Update font-crafty-girls to latest

--- Casks/font-crafty-girls.rb
+++ Casks/font-crafty-girls.rb
@@ -3,9 +3,9 @@ cask 'font-crafty-girls' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/craftygirls/CraftyGirls.ttf'
+  url 'https://github.com/google/fonts/raw/master/apache/craftygirls/CraftyGirls-Regular.ttf'
   name 'Crafty Girls'
   homepage 'http://www.google.com/fonts/specimen/Crafty+Girls'
 
-  font 'CraftyGirls.ttf'
+  font 'CraftyGirls-Regular.ttf'
 end
