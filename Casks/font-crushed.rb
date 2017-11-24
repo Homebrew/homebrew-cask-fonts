15dcb46cca84403d794f55800c83eabbbd5636b8
Tomonori Kimura <tomonori.kimura.cx@gmail.com>
2017-11-24 09:48:04 +0900

    Update font-crushed to latest (#1376)

--- Casks/font-crushed.rb
+++ Casks/font-crushed.rb
@@ -3,9 +3,9 @@ cask 'font-crushed' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/crushed/Crushed.ttf'
+  url 'https://github.com/google/fonts/raw/master/apache/crushed/Crushed-Regular.ttf'
   name 'Crushed'
   homepage 'http://www.google.com/fonts/specimen/Crushed'
 
-  font 'Crushed.ttf'
+  font 'Crushed-Regular.ttf'
 end
