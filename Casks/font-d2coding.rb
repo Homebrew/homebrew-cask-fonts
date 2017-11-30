47d54889cbd3ec445bcfb8e95a71e2690b1fd26c
commitay <commitay@users.noreply.github.com>
2017-11-30 16:17:39 +1000

    add appcast

--- Casks/font-d2coding.rb
+++ Casks/font-d2coding.rb
@@ -3,6 +3,8 @@ cask 'font-d2coding' do
   sha256 '3be175e6969852c6e7501c0a1afa387a6ac845e583c5c2510fa069551e567cdd'
 
   url "https://github.com/naver/d2codingfont/releases/download/VER#{version.before_comma}/D2Coding-#{version.after_comma}.zip"
+  appcast 'https://github.com/naver/d2codingfont/releases.atom',
+          checkpoint: '63d857249403dd983c2a79e390f9f4c0b7706eded063d834d5daad7c5d801c50'
   name 'D2 Coding'
   homepage 'https://github.com/naver/d2codingfont'
 
