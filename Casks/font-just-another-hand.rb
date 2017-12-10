8111609852d305b5d18b49764cd609d405a4b7df
commitay <commitay@users.noreply.github.com>
2017-12-10 18:36:15 +1000

    Update font-just-another-hand to latest (#1392)

--- Casks/font-just-another-hand.rb
+++ Casks/font-just-another-hand.rb
@@ -3,9 +3,9 @@ cask 'font-just-another-hand' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/justanotherhand/JustAnotherHand.ttf'
+  url 'https://github.com/google/fonts/raw/master/apache/justanotherhand/JustAnotherHand-Regular.ttf'
   name 'Just Another Hand'
-  homepage 'http://www.google.com/fonts/specimen/Just+Another+Hand'
+  homepage 'https://www.google.com/fonts/specimen/Just+Another+Hand'
 
-  font 'JustAnotherHand.ttf'
+  font 'JustAnotherHand-Regular.ttf'
 end
