771bd48a220238bd2419b7ef0409988f92375a36
resech <resech@chesssorrel.us>
2017-12-28 12:23:00 +1100

    Update font-ubuntumono-nerd-font to 1.2.0

--- Casks/font-ubuntumono-nerd-font.rb
+++ Casks/font-ubuntumono-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-ubuntumono-nerd-font' do
-  version '1.1.0'
-  sha256 'fb423fa6f5d1b3143df0200d9c66147d9677bf825d4f49a5c6ea843a278b8f1e'
+  version '1.2.0'
+  sha256 '9b8bacbabf4b7486fd68068507b208f6d2a017a6909521d19aeeb6f3026f3f6d'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/UbuntuMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '109f18cfd453156e38ffac165683bcfc2745e0c8dc07bd379a7f9ea19d0cbe41'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name 'UbuntuMono Nerd Font (UbuntuMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
