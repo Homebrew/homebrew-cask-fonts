d37155ea0059999697011badb0d84fdcd65b9a6e
resech <resech@chesssorrel.us>
2017-12-28 12:10:23 +1100

    Update font-lekton-nerd-font to 1.2.0

--- Casks/font-lekton-nerd-font.rb
+++ Casks/font-lekton-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-lekton-nerd-font' do
-  version '1.1.0'
-  sha256 '06fdd68a3f68f34004b56291f947480b1da1e0640651d02c40cb65d95497398e'
+  version '1.2.0'
+  sha256 '7a31875cdb2bbbe9f5ad1da342adec745cbaed22a677510e313d9a6bbab4c46d'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Lekton.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '109f18cfd453156e38ffac165683bcfc2745e0c8dc07bd379a7f9ea19d0cbe41'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name 'Lekton Nerd Font (Lekton)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
