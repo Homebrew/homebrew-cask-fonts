a4225a012415d1de0f405a57d5291e6b18832ad4
Ross Sechler <resech@chesssorrel.us>
2017-12-28 12:02:38 +1100

    Update font-anonymouspro-nerd-font to 1.2.0 (#1406)

--- Casks/font-anonymouspro-nerd-font.rb
+++ Casks/font-anonymouspro-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-anonymouspro-nerd-font' do
-  version '1.1.0'
-  sha256 'b0ae604f8841c7bf8ab3adb9eba10e702108964240fc817010a2e7c2e9ab2d7a'
+  version '1.2.0'
+  sha256 '38402e7acd35acbbb2c4d1bd2626237ef4a9fb3a4a30c77b6ca3fa49b7e95ff6'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AnonymousPro.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '109f18cfd453156e38ffac165683bcfc2745e0c8dc07bd379a7f9ea19d0cbe41'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name 'AnonymicePowerline Nerd Font (AnonymousPro)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
