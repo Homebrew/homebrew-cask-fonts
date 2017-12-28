872aca32b6e489759269612976d3a65d110c236d
commitay <commitay@users.noreply.github.com>
2017-12-28 15:23:54 +1000

    Update font-dejavusansmono-nerd-font-mono to 1.2.0

--- Casks/font-dejavusansmono-nerd-font-mono.rb
+++ Casks/font-dejavusansmono-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-dejavusansmono-nerd-font-mono' do
-  version '1.1.0'
-  sha256 '72c9f1acf4a92812965dbdfa80d236d8f251329bd6aa4cbb3277c67756a20a4b'
+  version '1.2.0'
+  sha256 'e0e8becdf376561063a804bc82ae3234595cc1a7c7ca035c3555cb04b619b03f'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DejaVuSansMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '109f18cfd453156e38ffac165683bcfc2745e0c8dc07bd379a7f9ea19d0cbe41'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name 'DejaVuSansMono Nerd Font (DejaVuSansMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
