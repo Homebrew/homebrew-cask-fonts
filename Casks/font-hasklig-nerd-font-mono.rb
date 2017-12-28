07da67ea59865e24fb69614a745c082dcdde3d6e
commitay <commitay@users.noreply.github.com>
2017-12-28 15:47:25 +1000

    Update font-hasklig-nerd-font-mono to 1.2.0

--- Casks/font-hasklig-nerd-font-mono.rb
+++ Casks/font-hasklig-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-hasklig-nerd-font-mono' do
-  version '1.1.0'
-  sha256 '6b07b8b61791fbfe88735a44e119ad54a2535d816d1054895b04ca03e5dd9bc5'
+  version '1.2.0'
+  sha256 '53109dac1a5924c84e19d36767e650ced72ff58137547ecaa0a8abd3d8ac7060'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hasklig.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '109f18cfd453156e38ffac165683bcfc2745e0c8dc07bd379a7f9ea19d0cbe41'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name 'Hasklug Nerd Font (Hasklig)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
