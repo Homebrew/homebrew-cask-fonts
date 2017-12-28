808841a1cad612092929ecfa15bc2d14ee811b7c
commitay <commitay@users.noreply.github.com>
2017-12-28 15:30:55 +1000

    Update font-firacode-nerd-font-mono to 1.2.0

--- Casks/font-firacode-nerd-font-mono.rb
+++ Casks/font-firacode-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-firacode-nerd-font-mono' do
-  version '1.1.0'
-  sha256 '551309fad856238876d051bfb2d81d9c85707f797de562529cb5aa2505e2ac7d'
+  version '1.2.0'
+  sha256 '54f1ade117e2efcc7386d3578da27b13c58b0e7e6afc2212b858ce0716d9d915'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraCode.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '109f18cfd453156e38ffac165683bcfc2745e0c8dc07bd379a7f9ea19d0cbe41'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name 'FuraCode Nerd Font (FiraCode)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
