1774fbfed58abfee3aba2faa4462c45bf4f44a40
commitay <commitay@users.noreply.github.com>
2017-12-28 16:00:18 +1000

    Update font-sharetechmono-nerd-font-mono to 1.2.0

--- Casks/font-sharetechmono-nerd-font-mono.rb
+++ Casks/font-sharetechmono-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-sharetechmono-nerd-font-mono' do
-  version '1.1.0'
-  sha256 '47e6b2a728457e47f449bc114c663c67273e2bba323d0163c38e53fb815835d6'
+  version '1.2.0'
+  sha256 '0840c2ff6bbc887c716f57991a101ad9a2a7cb304572e7f964fbe9e36a08350d'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/ShareTechMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '109f18cfd453156e38ffac165683bcfc2745e0c8dc07bd379a7f9ea19d0cbe41'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name 'ShureTechMono Nerd Font (ShareTechMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
