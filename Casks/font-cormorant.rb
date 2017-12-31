9400aeac2684c612dc425dbe9c84fd30dbdfab4d
Rens Baardman <rensbaardman@users.noreply.github.com>
2017-12-31 22:10:29 +0100

    Add font-cormorant v3.504 (#1470)
    * added cask for Cormorant
    
    * fix {version}-syntax
    
    * fix #{version}-replacement again; follow style guide
    
    * add appcast

---
+++ Casks/font-cormorant.rb
@@ -0,0 +1,56 @@
+cask 'font-cormorant' do
+  version '3.504'
+  sha256 '0915ccd7d396a50c2f4e83846717c0fe3afda1a0da8e6800d5382b24647c5f45'
+
+  url "https://github.com/CatharsisFonts/Cormorant/releases/download/v#{version}/Cormorant_Install_v#{version}.zip"
+  appcast 'https://github.com/CatharsisFonts/Cormorant/releases.atom',
+          checkpoint: 'd007ef931e050b40c5af03746fe8d7bdb4bc7ec4239eefc8ec457275720922c4'
+  name 'Cormorant'
+  homepage 'https://github.com/CatharsisFonts/Cormorant/'
+
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/Cormorant-Bold.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/Cormorant-BoldItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/Cormorant-Italic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/Cormorant-Light.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/Cormorant-LightItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/Cormorant-Medium.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/Cormorant-MediumItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/Cormorant-Regular.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/Cormorant-SemiBold.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/Cormorant-SemiBoldItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantGaramond-Bold.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantGaramond-BoldItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantGaramond-Italic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantGaramond-Light.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantGaramond-LightItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantGaramond-Medium.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantGaramond-MediumItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantGaramond-Regular.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantGaramond-SemiBold.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantGaramond-SemiBoldItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantInfant-Bold.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantInfant-BoldItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantInfant-Italic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantInfant-Light.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantInfant-LightItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantInfant-Medium.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantInfant-MediumItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantInfant-Regular.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantInfant-SemiBold.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantInfant-SemiBoldItalic.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantSC-Bold.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantSC-Light.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantSC-Medium.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantSC-Regular.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantSC-SemiBold.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantUnicase-Bold.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantUnicase-Light.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantUnicase-Medium.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantUnicase-Regular.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantUnicase-SemiBold.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantUpright-Bold.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantUpright-Light.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantUpright-Medium.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantUpright-Regular.ttf"
+  font "Cormorant_Install_v#{version}/1. TrueType Font Files/CormorantUpright-SemiBold.ttf"
+end
