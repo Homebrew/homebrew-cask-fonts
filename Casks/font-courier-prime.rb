928e374214369b3400d7d91fc5b681c78fe59a6c
commitay <commitay@users.noreply.github.com>
2018-01-02 16:28:06 +1000

    Update font-courier-prime to latest (#1474)

--- Casks/font-courier-prime.rb
+++ Casks/font-courier-prime.rb
@@ -2,9 +2,9 @@ cask 'font-courier-prime' do
   version :latest
   sha256 :no_check
 
-  url 'http://quoteunquoteapps.com/downloads/courier-prime.zip'
+  url 'https://quoteunquoteapps.com/downloads/courier-prime.zip'
   name 'Courier Prime'
-  homepage 'http://quoteunquoteapps.com/courierprime/'
+  homepage 'https://quoteunquoteapps.com/courierprime/'
 
   font 'Courier Prime/Courier Prime.ttf'
   font 'Courier Prime/Courier Prime Bold.ttf'
