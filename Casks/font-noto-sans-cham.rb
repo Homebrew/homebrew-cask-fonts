76bbbd619a2dbd60cdc67ac9115abf03195ebf30
Anthony Maki <4cm4k1@gmail.com>
2018-01-14 20:21:01 -0600

    Update font-noto-sans-cham (latest)

--- Casks/font-noto-sans-cham.rb
+++ Casks/font-noto-sans-cham.rb
@@ -2,11 +2,18 @@ cask 'font-noto-sans-cham' do
   version :latest
   sha256 :no_check
 
-  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
-  url 'https://noto-website.storage.googleapis.com/pkgs/NotoSansCham-unhinted.zip'
+  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCham-unhinted.zip'
   name 'Noto Sans Cham'
   homepage 'https://www.google.com/get/noto/#sans-cham'
 
+  font 'NotoSansCham-Black.ttf'
   font 'NotoSansCham-Bold.ttf'
+  font 'NotoSansCham-ExtraBold.ttf'
+  font 'NotoSansCham-ExtraLight.ttf'
+  font 'NotoSansCham-Light.ttf'
+  font 'NotoSansCham-Medium.ttf'
   font 'NotoSansCham-Regular.ttf'
+  font 'NotoSansCham-SemiBold.ttf'
+  font 'NotoSansCham-Thin.ttf'
 end
