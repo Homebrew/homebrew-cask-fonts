804ec505a72baece319a95da62cc066bda5fdad6
Anthony Maki <4cm4k1@gmail.com>
2018-01-14 20:59:57 -0600

    Update font-noto-serif-cjk-sc (latest)

--- Casks/font-noto-serif-cjk-sc.rb
+++ Casks/font-noto-serif-cjk-sc.rb
@@ -2,8 +2,8 @@ cask 'font-noto-serif-cjk-sc' do
   version :latest
   sha256 :no_check
 
-  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
-  url 'https://noto-website.storage.googleapis.com/pkgs/NotoSerifCJKsc-hinted.zip'
+  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKsc-hinted.zip'
   name 'Noto Serif CJK SC'
   homepage 'https://www.google.com/get/noto/#serif-hans'
 
