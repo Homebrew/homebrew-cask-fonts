d6060b97aadb8ac5a4e0193df4c580cf1ab0ad80
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-15 20:10:07 +0000

    Update font-babelstone-han to 7932

--- Casks/font-babelstone-han.rb
+++ Casks/font-babelstone-han.rb
@@ -1,8 +1,8 @@
 cask 'font-babelstone-han' do
-  version :latest
-  sha256 :no_check
+  version '7932'
+  sha256 '089c864e46f3b6046ac737b3c7792ce04cd73c94eb40f659bc0460f0e06c76ee'
 
-  url 'http://www.babelstone.co.uk/Fonts/9974/BabelStoneHan.zip'
+  url "http://www.babelstone.co.uk/Fonts/#{version}/BabelStoneHan.zip"
   name 'BabelStone Han'
   homepage 'http://babelstone.co.uk/Fonts/Han.html'
 
