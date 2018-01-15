9d273ecdca67c6e8bfc07e4df095e8bf280a0fda
Anthony Maki <4cm4k1@gmail.com>
2018-01-14 20:10:10 -0600

    Update font-noto-color-emoji (latest)

--- Casks/font-noto-color-emoji.rb
+++ Casks/font-noto-color-emoji.rb
@@ -2,10 +2,10 @@ cask 'font-noto-color-emoji' do
   version :latest
   sha256 :no_check
 
-  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
-  url 'https://noto-website.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip'
+  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip'
   name 'Noto Color Emoji'
-  homepage 'https://www.google.com/get/noto/#emoji-qaae-color'
+  homepage 'https://www.google.com/get/noto/#emoji-zsye-color'
 
   font 'NotoColorEmoji.ttf'
 end
