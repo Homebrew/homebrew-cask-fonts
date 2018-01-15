a39af00d7ae27e294f14353e485138e6a9fa6e9f
Anthony Maki <4cm4k1@gmail.com>
2018-01-14 20:21:07 -0600

    Update font-noto-sans-cjk (latest)

--- Casks/font-noto-sans-cjk.rb
+++ Casks/font-noto-sans-cjk.rb
@@ -2,8 +2,8 @@ cask 'font-noto-sans-cjk' do
   version :latest
   sha256 :no_check
 
-  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
-  url 'https://noto-website.storage.googleapis.com/pkgs/NotoSansCJK.ttc.zip'
+  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJK.ttc.zip'
   name 'Noto Sans CJK'
   homepage 'https://www.google.com/get/noto/help/cjk/'
 
