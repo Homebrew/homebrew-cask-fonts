9ce56f541b8fac4d504871e4db3f2b70355a4805
Anthony Maki <4cm4k1@gmail.com>
2018-01-14 20:21:13 -0600

    Update font-noto-sans-egyptian-hieroglyphs (latest)

--- Casks/font-noto-sans-egyptian-hieroglyphs.rb
+++ Casks/font-noto-sans-egyptian-hieroglyphs.rb
@@ -2,8 +2,8 @@ cask 'font-noto-sans-egyptian-hieroglyphs' do
   version :latest
   sha256 :no_check
 
-  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
-  url 'https://noto-website.storage.googleapis.com/pkgs/NotoSansEgyptianHieroglyphs-unhinted.zip'
+  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansEgyptianHieroglyphs-unhinted.zip'
   name 'Noto Sans Egyptian Hieroglyphs'
   homepage 'https://www.google.com/get/noto/#sans-egyp'
 
