8f822e557bc1f181a7dc4d9962fb8f3745e5ccd8
Anthony Maki <4cm4k1@gmail.com>
2018-01-14 20:53:08 -0600

    Update font-noto-sans-syloti-nagri (latest)

--- Casks/font-noto-sans-syloti-nagri.rb
+++ Casks/font-noto-sans-syloti-nagri.rb
@@ -2,8 +2,8 @@ cask 'font-noto-sans-syloti-nagri' do
   version :latest
   sha256 :no_check
 
-  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
-  url 'https://noto-website.storage.googleapis.com/pkgs/NotoSansSylotiNagri-unhinted.zip'
+  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSylotiNagri-unhinted.zip'
   name 'Noto Sans Syloti Nagri'
   homepage 'https://www.google.com/get/noto/#sans-sylo'
 
