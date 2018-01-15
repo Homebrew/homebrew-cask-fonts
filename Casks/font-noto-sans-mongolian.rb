a58c9866d14b32c0a1434e2a7ea589366771f2cc
Anthony Maki <4cm4k1@gmail.com>
2018-01-14 20:36:03 -0600

    Update font-noto-sans-mongolian (latest)

--- Casks/font-noto-sans-mongolian.rb
+++ Casks/font-noto-sans-mongolian.rb
@@ -2,8 +2,8 @@ cask 'font-noto-sans-mongolian' do
   version :latest
   sha256 :no_check
 
-  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
-  url 'https://noto-website.storage.googleapis.com/pkgs/NotoSansMongolian-unhinted.zip'
+  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMongolian-unhinted.zip'
   name 'Noto Sans Mongolian'
   homepage 'https://www.google.com/get/noto/#sans-mong'
 
