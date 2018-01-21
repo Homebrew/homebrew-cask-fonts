429fe80b45ad0fcc4027e2cc38e6908c61e6d571
Anthony Maki <4cm4k1@gmail.com>
2018-01-21 05:49:21 -0600

    Update 15 existing Noto font families (part 6) (#1493)
    * Update font-noto-sans-shavian (latest)
    
    * Update font-noto-sans-sinhala (latest)
    
    * Update font-noto-sans-sundanese (latest)
    
    * Update font-noto-sans-syloti-nagri (latest)
    
    * Update font-noto-sans-symbols (latest)
    
    * Update font-noto-sans-syriac-eastern (latest)
    
    * Update font-noto-sans-syriac-estrangela (latest)
    
    * Update font-noto-sans-syriac-western (latest)
    
    * Update font-noto-sans-tagalog (latest)
    
    * Update font-noto-sans-tagbanwa (latest)
    
    * Update font-noto-sans-tai-le (latest)
    
    * Update font-noto-sans-tai-tham (latest)
    
    * Update font-noto-sans-tai-viet (latest)
    
    * Update font-noto-sans-tamil (latest)
    
    * Update font-noto-sans-telugu (latest)

--- Casks/font-noto-sans-syriac-western.rb
+++ Casks/font-noto-sans-syriac-western.rb
@@ -2,8 +2,8 @@ cask 'font-noto-sans-syriac-western' do
   version :latest
   sha256 :no_check
 
-  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
-  url 'https://noto-website.storage.googleapis.com/pkgs/NotoSansSyriacWestern-unhinted.zip'
+  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacWestern-unhinted.zip'
   name 'Noto Sans Syriac Western'
   homepage 'https://www.google.com/get/noto/#sans-syrc-western'
 
