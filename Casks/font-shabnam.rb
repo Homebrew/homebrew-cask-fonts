54fae1de7f635077d142eed417dcccb6eb486bb7
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-23 18:52:07 +0000

    Update font-shabnam to 2.3.0

--- Casks/font-shabnam.rb
+++ Casks/font-shabnam.rb
@@ -1,11 +1,11 @@
 cask 'font-shabnam' do
-  version '2.1.1'
-  sha256 '1a116e9648cb6df10bb387c821df5149fcc7deb1ef8883ace3ca3805b892ab85'
+  version '2.3.0'
+  sha256 '47317a0562e3ab032ebd46a8134b482e2f0c6b21881906ec2865604eda371a9a'
 
   # github.com/rastikerdar was verified as official when first introduced to the cask
   url "https://github.com/rastikerdar/shabnam-font/releases/download/v#{version}/shabnam-font-v#{version}.zip"
   appcast 'https://github.com/rastikerdar/shabnam-font/releases.atom',
-          checkpoint: '7e0a1553d9064805047967a2228c4386a372958a972c7608394bf698cd01969b'
+          checkpoint: '1a25ddae25693615114c03542e10acf997fddf5d56395b815ef7d1477b75cb64'
   name 'Shabnam'
   homepage 'http://rastikerdar.github.io/shabnam-font'
 
