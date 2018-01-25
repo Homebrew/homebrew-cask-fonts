c2e8b99d0903ce75f04f19853fe66090a759d705
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-25 10:36:11 +0000

    Update font-d2coding to 1.3.1,20180115 (#1504)
    * Update font-d2coding to 1.3.1,20180115
    
    * Update font-d2coding.rb

--- Casks/font-d2coding.rb
+++ Casks/font-d2coding.rb
@@ -1,12 +1,12 @@
 cask 'font-d2coding' do
-  version '1.3,Ver1.3-20171129'
-  sha256 '3be175e6969852c6e7501c0a1afa387a6ac845e583c5c2510fa069551e567cdd'
+  version '1.3.1,20180115'
+  sha256 '06f3fdb7a5a02e695af47ae95bd246713745e191628fd9a1bea670a67528b82c'
 
-  url "https://github.com/naver/d2codingfont/releases/download/VER#{version.before_comma}/D2Coding-#{version.after_comma}.zip"
+  url "https://github.com/naver/d2codingfont/releases/download/VER#{version.before_comma}/D2Coding-Ver#{version.before_comma}-#{version.after_comma}.zip"
   appcast 'https://github.com/naver/d2codingfont/releases.atom',
-          checkpoint: '63d857249403dd983c2a79e390f9f4c0b7706eded063d834d5daad7c5d801c50'
+          checkpoint: '7624423ea790852c136928f39779d169315f5df1fb880eec077f7d4c7251ac3a'
   name 'D2 Coding'
   homepage 'https://github.com/naver/d2codingfont'
 
-  font "D2Coding-#{version.after_comma}/D2Coding-#{version.after_comma}.ttc"
+  font "D2Coding-Ver#{version.before_comma}-#{version.after_comma}/D2Coding/D2Coding-Ver#{version.before_comma}-#{version.after_comma}.ttc"
 end
