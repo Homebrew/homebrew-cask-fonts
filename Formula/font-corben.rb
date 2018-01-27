160410dc7e39a88ef6f31d599251fb690a4f52d6
asciian <asciian@users.noreply.github.com>
2018-01-28 07:36:21 +0900

    Import from cask:     * Update 58 google fonts: https homepage

---
+++ Formula/font-corben.rb
@@ -0,0 +1,15 @@
+class FontCorben < Formula
+  desc "Corben"
+  homepage "https://www.google.com/fonts/specimen/Corben"
+  head "https://github.com/google/fonts/trunk/ofl/corben", using: :svn, revision: "50", trust_cert: true
+
+  bottle :unneeded
+
+  def install
+    (share/"fonts").install "Corben-Bold.ttf"
+    (share/"fonts").install "Corben-Regular.ttf"
+  end
+
+  test do
+  end
+end
