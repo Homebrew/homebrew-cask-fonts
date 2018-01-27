8fa57af07f4cde809ff92268f80aa260109c7dee
asciian <asciian@users.noreply.github.com>
2018-01-28 07:36:39 +0900

    Import from cask:     Update font-firacode-nerd-font to 1.2.0 (#1413)

---
+++ Formula/font-firacode-nerd-font.rb
@@ -0,0 +1,20 @@
+class FontFiracodeNerdFont < Formula
+  desc "FuraCode Nerd Font (FiraCode)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraCode.zip"
+  version "1.2.0"
+  sha256 "54f1ade117e2efcc7386d3578da27b13c58b0e7e6afc2212b858ce0716d9d915"
+
+  bottle :unneeded
+
+  def install
+    (share/"fonts").install "Fura Code Bold Nerd Font Complete.otf"
+    (share/"fonts").install "Fura Code Medium Nerd Font Complete.otf"
+    (share/"fonts").install "Fura Code Retina Nerd Font Complete.otf"
+    (share/"fonts").install "Fura Code Regular Nerd Font Complete.otf"
+    (share/"fonts").install "Fura Code Light Nerd Font Complete.otf"
+  end
+
+  test do
+  end
+end
