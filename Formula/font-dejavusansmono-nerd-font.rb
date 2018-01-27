956d8973cbea11e4cbd811d47ece5226aa78cf39
asciian <asciian@users.noreply.github.com>
2018-01-28 07:36:26 +0900

    Import from cask:     Update font-dejavusansmono-nerd-font to 1.2.0 (#1410)

---
+++ Formula/font-dejavusansmono-nerd-font.rb
@@ -0,0 +1,23 @@
+class FontDejavusansmonoNerdFont < Formula
+  desc "DejaVuSansMonoForPowerline Nerd Font (DejaVuSansMono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DejaVuSansMono.zip"
+  version "1.2.0"
+  sha256 "e0e8becdf376561063a804bc82ae3234595cc1a7c7ca035c3555cb04b619b03f"
+
+  bottle :unneeded
+
+  def install
+    (share/"fonts").install "DejaVu Sans Mono Bold for Powerline Nerd Font Complete.ttf"
+    (share/"fonts").install "DejaVu Sans Mono Bold Nerd Font Complete.ttf"
+    (share/"fonts").install "DejaVu Sans Mono Bold Oblique for Powerline Nerd Font Complete.ttf"
+    (share/"fonts").install "DejaVu Sans Mono Bold Oblique Nerd Font Complete.ttf"
+    (share/"fonts").install "DejaVu Sans Mono Nerd Font Complete.ttf"
+    (share/"fonts").install "DejaVu Sans Mono for Powerline Nerd Font Complete.ttf"
+    (share/"fonts").install "DejaVu Sans Mono Oblique Nerd Font Complete.ttf"
+    (share/"fonts").install "DejaVu Sans Mono Oblique for Powerline Nerd Font Complete.ttf"
+  end
+
+  test do
+  end
+end
