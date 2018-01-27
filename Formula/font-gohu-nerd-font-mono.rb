7ba15e740cbc23781a7c381bb0ed41c33294bd82
asciian <asciian@users.noreply.github.com>
2018-01-28 07:36:50 +0900

    Import from cask:     Update font-gohu-nerd-font-mono to 1.2.0 (#1448)

---
+++ Formula/font-gohu-nerd-font-mono.rb
@@ -0,0 +1,17 @@
+class FontGohuNerdFontMono < Formula
+  desc "GohuFontBold Nerd Font (Gohu)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Gohu.zip"
+  version "1.2.0"
+  sha256 "3c2cc94365093054b7b32638dfa1cba0ea09e02a8abd28395612de4c03fd1f4b"
+
+  bottle :unneeded
+
+  def install
+    (share/"fonts").install "GohuFont-Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "GohuFont-Medium Nerd Font Complete Mono.ttf"
+  end
+
+  test do
+  end
+end
