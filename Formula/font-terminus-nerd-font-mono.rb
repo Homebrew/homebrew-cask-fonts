513a2162ea7242774c293e9ac52158aeae744218
asciian <asciian@users.noreply.github.com>
2018-01-28 07:40:13 +0900

    Import from cask:     Update font-terminus-nerd-font-mono to 1.2.0 (#1468)

---
+++ Formula/font-terminus-nerd-font-mono.rb
@@ -0,0 +1,19 @@
+class FontTerminusNerdFontMono < Formula
+  desc "TerminessTTF Nerd Font (Terminus)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Terminus.zip"
+  version "1.2.0"
+  sha256 "3beef1d5ae482a3273cd5331cc643a57202bfab4c4fde793a024c0233a42901d"
+
+  bottle :unneeded
+
+  def install
+    (share/"fonts").install "Terminess (TTF) Bold Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Terminess (TTF) Bold Italic Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Terminess (TTF) Nerd Font Complete Mono.ttf"
+    (share/"fonts").install "Terminess (TTF) Italic Nerd Font Complete Mono.ttf"
+  end
+
+  test do
+  end
+end
