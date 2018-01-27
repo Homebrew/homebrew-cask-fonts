657298306ca5c83fa7f690a411347cdd3e801767
asciian <asciian@users.noreply.github.com>
2018-01-28 07:40:11 +0900

    Import from cask:     * Update 58 google fonts: https homepage

---
+++ Formula/font-syncopate.rb
@@ -0,0 +1,15 @@
+class FontSyncopate < Formula
+  desc "Syncopate"
+  homepage "https://www.google.com/fonts/specimen/Syncopate"
+  head "https://github.com/google/fonts/trunk/apache/syncopate", using: :svn, revision: "50", trust_cert: true
+
+  bottle :unneeded
+
+  def install
+    (share/"fonts").install "Syncopate-Bold.ttf"
+    (share/"fonts").install "Syncopate-Regular.ttf"
+  end
+
+  test do
+  end
+end
