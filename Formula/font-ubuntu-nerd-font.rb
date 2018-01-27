a34e6ba908db9cea87493fd862882d65bf50d40d
asciian <asciian@users.noreply.github.com>
2018-01-28 07:40:29 +0900

    Import from cask:     Adds 14 new Nerd Fonts from 1.2.0 release (#1478)

---
+++ Formula/font-ubuntu-nerd-font.rb
@@ -0,0 +1,24 @@
+class FontUbuntuNerdFont < Formula
+  desc "Ubuntu Nerd Font (Ubuntu)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Ubuntu.zip"
+  version "1.2.0"
+  sha256 "7d386304371ef6b3f265a317307d5562cf4c536b45d8bfbf795b64678a2b00bf"
+
+  bottle :unneeded
+
+  def install
+    (share/"fonts").install "Ubuntu Bold Nerd Font Complete.ttf"
+    (share/"fonts").install "Ubuntu Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "Ubuntu Bold Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "Ubuntu Medium Nerd Font Complete.ttf"
+    (share/"fonts").install "Ubuntu Nerd Font Complete.ttf"
+    (share/"fonts").install "Ubuntu Medium Italic Nerd Font Complete.ttf"
+    (share/"fonts").install "Ubuntu Condensed Nerd Font Complete.ttf"
+    (share/"fonts").install "Ubuntu Light Nerd Font Complete.ttf"
+    (share/"fonts").install "Ubuntu Light Italic Nerd Font Complete.ttf"
+  end
+
+  test do
+  end
+end
