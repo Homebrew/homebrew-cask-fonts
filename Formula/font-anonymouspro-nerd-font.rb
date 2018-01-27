5ef75bf800e91ae03d96a59861cb40c627b335e8
asciian <asciian@users.noreply.github.com>
2018-01-28 07:35:59 +0900

    Import from cask:     Update font-anonymouspro-nerd-font to 1.2.0 (#1406)

---
+++ Formula/font-anonymouspro-nerd-font.rb
@@ -0,0 +1,16 @@
+class FontAnonymousproNerdFont < Formula
+  desc "AnonymicePowerline Nerd Font (AnonymousPro)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AnonymousPro.zip"
+  version "1.2.0"
+  sha256 "38402e7acd35acbbb2c4d1bd2626237ef4a9fb3a4a30c77b6ca3fa49b7e95ff6"
+
+  bottle :unneeded
+
+  def install
+    (share/"fonts").install "Anonymice Powerline Nerd Font Complete.ttf"
+  end
+
+  test do
+  end
+end
