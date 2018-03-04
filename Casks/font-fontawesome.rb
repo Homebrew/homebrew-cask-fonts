bec51e2c7c4ea0f5a6bf96198a7155abbb5946d5
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-03-04 15:43:28 +0000

    Update font-fontawesome to 5.0.8 (#1536)
    * Update font-fontawesome to 5.0.8
    
    * Update font-fontawesome.rb

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,13 +1,15 @@
 cask 'font-fontawesome' do
-  version '4.7.0'
-  sha256 '35d79ae86c8d4fd0f931843ec969191700940875bb941044a4c860eb200f166c'
+  version '5.0.8'
+  sha256 'e2d95dae41cb06951a270d82ff72eb38892963d0b3a8f44e28335794d4b08c28'
 
   # github.com/FortAwesome/Font-Awesome was verified as official when first introduced to the cask
-  url "https://github.com/FortAwesome/Font-Awesome/archive/v#{version}.zip"
+  url "https://github.com/FortAwesome/Font-Awesome/archive/#{version}.zip"
   appcast 'https://github.com/FortAwesome/Font-Awesome/releases.atom',
-          checkpoint: 'fa1e9fdd7a074dc2d844c8a23a2145362983b63e2340d5e066902e49f8071327'
+          checkpoint: '72a41a22b695f7d2b5ac8910b4bde1ed1d1f03c5d9da3e7530bdc2bd8d1febc7'
   name 'Font Awesome'
   homepage 'http://fontawesome.io/'
 
-  font "Font-Awesome-#{version}/fonts/FontAwesome.otf"
+  font "Font-Awesome-#{version}/use-on-desktop/Font Awesome #{version.major} Brands-Regular-400.otf"
+  font "Font-Awesome-#{version}/use-on-desktop/Font Awesome #{version.major} Free-Regular-400.otf"
+  font "Font-Awesome-#{version}/use-on-desktop/Font Awesome #{version.major} Free-Solid-900.otf"
 end
