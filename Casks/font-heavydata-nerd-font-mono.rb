4bd6196682c1edfb4cac3ebff8c7561fc16e3357
commitay <commitay@users.noreply.github.com>
2018-03-20 08:40:45 +1000

    Revert "Update font-heavydata-nerd-font-mono to 2.0.0 (#1575)"
    This reverts commit 6b5810d191156c0e1bb1bca72934de8ac9d3c159.

--- Casks/font-heavydata-nerd-font-mono.rb
+++ Casks/font-heavydata-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-heavydata-nerd-font-mono' do
-  version '2.0.0'
-  sha256 '1b7218999ed0b20882e9498618f88c56c548e22feeb89d098f70784d36e15c47'
+  version '1.2.0'
+  sha256 '101310c911cc41e159e100a9afc2df3bc5d2e212475c9a53eec785904d9135cf'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/HeavyData.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '722a75922628bdd6138fdd43bbf5f21d2ceeb711768fa1839942636dc1dd6e83'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name 'HeavyData Nerd Font (HeavyData)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
