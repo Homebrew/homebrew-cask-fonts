d5275e3c78cafb41b0ee23193d122f80989d5245
Thammachart <bustheheroall@gmail.com>
2018-03-19 15:49:27 +0700

    Update font-inconsolatalgc-nerd-font to 2.0.0

--- Casks/font-inconsolatalgc-nerd-font.rb
+++ Casks/font-inconsolatalgc-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-inconsolatalgc-nerd-font' do
-  version '1.2.0'
-  sha256 '0fd2492b0c5c4140718120ae97517efb0236694b1a3e10252492addfc38fffda'
+  version '2.0.0'
+  sha256 'dadb77ba0f1a139933456b7ccbc353960e87a714e928203fdff300832d9fae5d'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/InconsolataLGC.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+          checkpoint: '722a75922628bdd6138fdd43bbf5f21d2ceeb711768fa1839942636dc1dd6e83'
   name 'InconsolataLGC Nerd Font (InconsolataLGC)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
