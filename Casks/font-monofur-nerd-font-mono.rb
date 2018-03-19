e70af104c5ab0ef624e26ae0dcaa0fd33f1ca1e7
commitay <commitay@users.noreply.github.com>
2018-03-20 08:40:24 +1000

    Revert "Update font-monofur-nerd-font-mono to 2.0.0 (#1592)"
    This reverts commit 08387af3eded4e3e6e46d700d57c082983fc7ce6.

--- Casks/font-monofur-nerd-font-mono.rb
+++ Casks/font-monofur-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-monofur-nerd-font-mono' do
-  version '2.0.0'
-  sha256 '22d1eb9c8c6bdc781f5d875886145b014f4f79e430809a6baf6eca0a20981ef6'
+  version '1.2.0'
+  sha256 '002ae1e8f7a1f6027202f0dd71e72674c9afdee157442ca8207313f363499d4c'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Monofur.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '722a75922628bdd6138fdd43bbf5f21d2ceeb711768fa1839942636dc1dd6e83'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name 'MonofurboldForPowerline Nerd Font (Monofur)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
