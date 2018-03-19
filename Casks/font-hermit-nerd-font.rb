006f99aa13dc41f7790f9b9257c15e7d375d2b8b
Thammachart Chinvarapon <Thammachart@users.noreply.github.com>
2018-03-19 15:54:04 +0700

    Update font-hermit-nerd-font to 2.0.0 (#1576)

--- Casks/font-hermit-nerd-font.rb
+++ Casks/font-hermit-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-hermit-nerd-font' do
-  version '1.2.0'
-  sha256 '98da4c26a22cf7d8c3161e71d7ae5ebfa592b8312dc39426db646a1f32623b25'
+  version '2.0.0'
+  sha256 '5414b1fb0ea62463749c5b9107b338c89e45267b362124af21dbdc53be84b7ba'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hermit.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+          checkpoint: '722a75922628bdd6138fdd43bbf5f21d2ceeb711768fa1839942636dc1dd6e83'
   name 'Hurmit Nerd Font (Hermit)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
