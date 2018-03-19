acdbbc8ca0d369338e8023d7e9da44fd0480ba50
commitay <commitay@users.noreply.github.com>
2018-03-20 08:41:10 +1000

    Revert "Update font-3270-nerd-font to 2.0.0 (#1546)"
    This reverts commit 1e885552c62d33ff8ba265773859d345b5500796.

--- Casks/font-3270-nerd-font.rb
+++ Casks/font-3270-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-3270-nerd-font' do
-  version '2.0.0'
-  sha256 '4fb411ff4a6aaaeb6482047825416b21131d9f301c56222f22f86a1ffb502f81'
+  version '1.2.0'
+  sha256 '37cd9e4c31b7f585a202ce85f358e318e6042de489e668c948ecee0d0daf4472'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/3270.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '722a75922628bdd6138fdd43bbf5f21d2ceeb711768fa1839942636dc1dd6e83'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name '3270Medium Nerd Font (3270)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
