95dfe20abe5f5211d8a8017753544e3b2b13b1a9
Thammachart <bustheheroall@gmail.com>
2018-03-19 15:10:05 +0700

    Update font-gohu-nerd-font to 2.0.0

--- Casks/font-gohu-nerd-font.rb
+++ Casks/font-gohu-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-gohu-nerd-font' do
-  version '1.2.0'
+  version '2.0.0'
   sha256 '3c2cc94365093054b7b32638dfa1cba0ea09e02a8abd28395612de4c03fd1f4b'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Gohu.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+          checkpoint: '722a75922628bdd6138fdd43bbf5f21d2ceeb711768fa1839942636dc1dd6e83'
   name 'GohuFontBold Nerd Font (Gohu)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
