1473474df2c9489d005fdbf88bbde773429e9fe0
Thammachart <bustheheroall@gmail.com>
2018-03-19 15:09:31 +0700

    Update font-go-mono-nerd-font-mono to 2.0.0

--- Casks/font-go-mono-nerd-font-mono.rb
+++ Casks/font-go-mono-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-go-mono-nerd-font-mono' do
-  version '1.2.0'
-  sha256 'c6873e9563766a6ff9b36b35d25366bad406a0423b181a33cda6a8e58dd4bfc9'
+  version '2.0.0'
+  sha256 '6b4742103210e403813cdfa5c8846f22431c8bcf5c82fe9af3a9d9bfb6900470'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Go-Mono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+          checkpoint: '722a75922628bdd6138fdd43bbf5f21d2ceeb711768fa1839942636dc1dd6e83'
   name 'GoMono Nerd Font Mono (Go-Mono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
