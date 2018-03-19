37e41c8544de0dda9a6915a2eb2d9fb92d45b2f2
commitay <commitay@users.noreply.github.com>
2018-03-20 08:40:03 +1000

    Revert "Update font-spacemono-nerd-font-mono to 2.0.0 (#1610)"
    This reverts commit d10c1e9cd45ce87901e70d2dfa429638aa13ec29.

--- Casks/font-spacemono-nerd-font-mono.rb
+++ Casks/font-spacemono-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-spacemono-nerd-font-mono' do
-  version '2.0.0'
-  sha256 '9080db305426d89feaf96ef3f5899580729cd1b7c9abf2b0a54cf90134a541ec'
+  version '1.2.0'
+  sha256 '1e39f8af44d0ad2e3aa8b874b56c2f31c96ec5f224b170119909eeedba883ac4'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/SpaceMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '722a75922628bdd6138fdd43bbf5f21d2ceeb711768fa1839942636dc1dd6e83'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name 'SpaceMono Nerd Font (SpaceMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
