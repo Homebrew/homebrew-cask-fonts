e0b9b78250e0ae5c9da9e0b89a0f8d4cb1b6f3a6
Thammachart <bustheheroall@gmail.com>
2018-03-19 14:15:30 +0700

    Update font-fantasquesansmono-nerd-font to 2.0.0

--- Casks/font-fantasquesansmono-nerd-font.rb
+++ Casks/font-fantasquesansmono-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-fantasquesansmono-nerd-font' do
-  version '1.2.0'
-  sha256 'd40a2dbebe57868181cc1dfd3e2c0321c15c641392e556e94809d3dd0d6f70be'
+  version '2.0.0'
+  sha256 '96a4159ab2d52f54525156c25dddfc0b1c4c20cd432a8374d8cc84da918a876b'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FantasqueSansMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+          checkpoint: '722a75922628bdd6138fdd43bbf5f21d2ceeb711768fa1839942636dc1dd6e83'
   name 'FantasqueSansMono Nerd Font (FantasqueSansMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
