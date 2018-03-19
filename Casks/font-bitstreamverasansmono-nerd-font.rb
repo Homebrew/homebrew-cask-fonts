2719976ed2bce453b6f8a16ead8f0f04cf13321e
commitay <commitay@users.noreply.github.com>
2018-03-20 08:41:02 +1000

    Revert "Update font-bitstreamverasansmono-nerd-font to 2.0.0 (#1554)"
    This reverts commit 8ff2b66017fd314e0a7e666a6ff5c8e7048b19ee.

--- Casks/font-bitstreamverasansmono-nerd-font.rb
+++ Casks/font-bitstreamverasansmono-nerd-font.rb
@@ -1,10 +1,10 @@
 cask 'font-bitstreamverasansmono-nerd-font' do
-  version '2.0.0'
-  sha256 '7a5537c1b6e51fdebba73cc4925ebe937d215068f43e916caa049e90c28ef7ad'
+  version '1.2.0'
+  sha256 'caa575cf0df9ab974f531c681b9ec9c7fb9a1b313e336bb820d5a6627857e95d'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BitstreamVeraSansMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '722a75922628bdd6138fdd43bbf5f21d2ceeb711768fa1839942636dc1dd6e83'
+          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
   name 'BitstreamVeraSansMono Nerd Font (BitstreamVeraSansMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
