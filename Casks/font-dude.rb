9b7045d1df32b2ece9ea27324308f012efb58140
Daniel Bayley <daniel.bayley@me.com>
2018-03-30 18:57:07 +0100

    Add Dude

---
+++ Casks/font-dude.rb
@@ -0,0 +1,21 @@
+cask 'font-dude' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://losttype.com/delivery/Dude_Personal_License.zip'
+  name 'Dude'
+  homepage 'https://losttype.com/font/?name=dude'
+
+  font 'Dude_Personal_License/Dude_Dolly.otf'
+  font 'Dude_Personal_License/Dude_Hank.otf'
+  font 'Dude_Personal_License/Dude_Johnny.otf'
+  font 'Dude_Personal_License/Dude_June.otf'
+  font 'Dude_Personal_License/Dude_Kitty.otf'
+  font 'Dude_Personal_License/Dude_Loretta.otf'
+  font 'Dude_Personal_License/Dude_Merle.otf'
+  font 'Dude_Personal_License/Dude_Patsy.otf'
+  font 'Dude_Personal_License/Dude_Tammy.otf'
+  font 'Dude_Personal_License/Dude_Townes.otf'
+  font 'Dude_Personal_License/Dude_Waylon.otf'
+  font 'Dude_Personal_License/Dude_Willie.otf'
+end
