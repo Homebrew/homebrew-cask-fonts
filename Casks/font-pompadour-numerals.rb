b43ab68400408716540b114a02f3044f67df4443
Daniel Bayley <daniel.bayley@me.com>
2018-03-30 20:29:42 +0100

    Add Pompadour Numerals
    Free for personal use.

---
+++ Casks/font-pompadour-numerals.rb
@@ -0,0 +1,10 @@
+cask 'font-pompadour-numerals' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://losttype.com/delivery/PompadourNumerals_Personal_License.zip'
+  name 'Pompadour Numerals'
+  homepage 'https://losttype.com/font/?name=pompadour'
+
+  font 'PompadourNumerals_Personal_License/Pompadour.ttf'
+end
