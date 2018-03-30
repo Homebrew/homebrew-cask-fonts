82a157ecedfacdc040e627dfe7553c5cc79ca145
Daniel Bayley <daniel.bayley@me.com>
2018-03-30 19:14:58 +0100

    Add Ribbon

---
+++ Casks/font-ribbon.rb
@@ -0,0 +1,10 @@
+cask 'font-ribbon' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://losttype.com/delivery/Ribbon_Personal_License.zip'
+  name 'Ribbon'
+  homepage 'https://losttype.com/font/?name=ribbon'
+
+  font 'Ribbon_Personal_License/Ribbon_V2_2011.otf'
+end
