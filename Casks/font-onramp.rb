b75e69fdd3673d9dd3777b50d9bb339c3faf37a2
Daniel Bayley <daniel.bayley@me.com>
2018-03-30 20:36:23 +0100

    Add Onramp
    Free for personal use.

---
+++ Casks/font-onramp.rb
@@ -0,0 +1,10 @@
+cask 'font-onramp' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://losttype.com/delivery/Onramp_Personal_License.zip'
+  name 'Onramp'
+  homepage 'https://losttype.com/font/?name=onramp'
+
+  font 'Onramp_Personal_License/ONRAMP.ttf'
+end
