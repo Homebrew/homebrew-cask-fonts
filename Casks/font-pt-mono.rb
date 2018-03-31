b51155c4c8ca3e5631933b21d3019d7937aa19b1
commitay <commitay@users.noreply.github.com>
2018-03-31 16:29:02 +1000

    Update 3 paratype fonts: URL

--- Casks/font-pt-mono.rb
+++ Casks/font-pt-mono.rb
@@ -2,7 +2,7 @@ cask 'font-pt-mono' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.paratype.com/uni/public/PTMono.zip'
+  url 'http://old.paratype.com/uni/public/PTMono.zip'
   name 'PT Mono'
   homepage 'http://www.paratype.com/public/'
 
