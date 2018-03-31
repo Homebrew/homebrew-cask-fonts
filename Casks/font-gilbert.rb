d49026455a21f95a072af0c0f44445ee786bed2b
commitay <commitay@users.noreply.github.com>
2018-03-31 18:39:23 +1000

    Update font-gilbert (#1653)

--- Casks/font-gilbert.rb
+++ Casks/font-gilbert.rb
@@ -1,14 +1,14 @@
 cask 'font-gilbert' do
-  version '1.004'
+  version '1.004,alpha'
   sha256 '0cea7456bdc704c9b68c023a6bb40590e327da3d84f8ff5511c6460a712987a9'
 
   # github.com/Fontself/TypeWithPride was verified as official when first introduced to the cask
-  url "https://github.com/Fontself/TypeWithPride/releases/download/#{version}/Gilbert_#{version}_alpha.zip"
+  url "https://github.com/Fontself/TypeWithPride/releases/download/#{version.before_comma}/Gilbert_#{version.before_comma}_#{version.after_comma}.zip"
   appcast 'https://github.com/Fontself/TypeWithPride/releases.atom',
           checkpoint: 'a66b7331a823c0d2dc1762b92115451e9dd9749204ab0d25a21d4c6fd6b9019c'
   name 'Gilbert'
   homepage 'https://typewithpride.com/'
 
-  font 'Gilbert_1.004_alpha/Gilbert Bold-Preview_1004.otf'
-  font 'Gilbert_1.004_alpha/Gilbert-Color Bold-Preview_1004.otf'
+  font "Gilbert_#{version.before_comma}_#{version.after_comma}/Gilbert Bold-Preview_#{version.before_comma.no_dots}.otf"
+  font "Gilbert_#{version.before_comma}_#{version.after_comma}/Gilbert-Color Bold-Preview_#{version.before_comma.no_dots}.otf"
 end
