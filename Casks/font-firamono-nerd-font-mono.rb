7c680a0787561096ceae4ba66d22c3e839ec06f2
Thammachart <bustheheroall@gmail.com>
2018-04-03 03:01:09 +0700

    Update font-firamono-nerd-font-mono to 2.0.0

--- Casks/font-firamono-nerd-font-mono.rb
+++ Casks/font-firamono-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-firamono-nerd-font-mono' do
-  version '1.2.0'
-  sha256 '77ffee4498e23e3215edc9ea6eefa9f10b03deb6815dc3ccb9ad3336cd478f5c'
+  version '2.0.0'
+  sha256 '1eb587b0985d61a9323e0cc979ce16cf820013f0dd235c3a5df979029f6e6e34'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+          checkpoint: 'fc41f3d5a5b0df76caa2586a08c53a4571f05adb0fe0a6e30897f43899ee6ea2'
   name 'FuraMonoForPowerline Nerd Font (FiraMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
