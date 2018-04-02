72e34fe970d511c107f147bb7e17ab431fd2325d
Thammachart <bustheheroall@gmail.com>
2018-04-03 03:43:08 +0700

    Update font-spacemono-nerd-font-mono to 2.0.0

--- Casks/font-spacemono-nerd-font-mono.rb
+++ Casks/font-spacemono-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-spacemono-nerd-font-mono' do
-  version '1.2.0'
-  sha256 '1e39f8af44d0ad2e3aa8b874b56c2f31c96ec5f224b170119909eeedba883ac4'
+  version '2.0.0'
+  sha256 'c41c8b4d9a634a8662aaa70d4541a534e82a13fbcc40c14149a01cda32925648'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/SpaceMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+          checkpoint: 'fc41f3d5a5b0df76caa2586a08c53a4571f05adb0fe0a6e30897f43899ee6ea2'
   name 'SpaceMono Nerd Font (SpaceMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
