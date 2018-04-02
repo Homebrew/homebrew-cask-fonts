431bd068ea5aa82770db0fd21686e459a9bf710c
commitay <commitay@users.noreply.github.com>
2018-04-03 08:31:14 +1000

    Update dejavusansmono-nerd-fonts fonts (#1659)

--- Casks/font-dejavusansmono-nerd-font.rb
+++ Casks/font-dejavusansmono-nerd-font.rb
@@ -5,15 +5,11 @@ cask 'font-dejavusansmono-nerd-font' do
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DejaVuSansMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
           checkpoint: 'fc41f3d5a5b0df76caa2586a08c53a4571f05adb0fe0a6e30897f43899ee6ea2'
-  name 'DejaVuSansMonoForPowerline Nerd Font (DejaVuSansMono)'
+  name 'DejaVuSansMono Nerd Font (DejaVuSansMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
-  font 'DejaVu Sans Mono Bold for Powerline Nerd Font Complete.ttf'
   font 'DejaVu Sans Mono Bold Nerd Font Complete.ttf'
-  font 'DejaVu Sans Mono Bold Oblique for Powerline Nerd Font Complete.ttf'
   font 'DejaVu Sans Mono Bold Oblique Nerd Font Complete.ttf'
   font 'DejaVu Sans Mono Nerd Font Complete.ttf'
-  font 'DejaVu Sans Mono for Powerline Nerd Font Complete.ttf'
   font 'DejaVu Sans Mono Oblique Nerd Font Complete.ttf'
-  font 'DejaVu Sans Mono Oblique for Powerline Nerd Font Complete.ttf'
 end
