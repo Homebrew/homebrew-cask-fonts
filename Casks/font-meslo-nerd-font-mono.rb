e08d7dec278ab04808709172daa98ef9351f43e3
Thammachart <bustheheroall@gmail.com>
2018-04-03 03:24:06 +0700

    Update font-meslo-nerd-font-mono to 2.0.0

--- Casks/font-meslo-nerd-font-mono.rb
+++ Casks/font-meslo-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-meslo-nerd-font-mono' do
-  version '1.2.0'
-  sha256 '1b2ea3e623c467a2142df7ee1bac120029add774b8f0229f8426452c01e70b87'
+  version '2.0.0'
+  sha256 '350ff0b1061ca0d1e933c59861d6421ebb2667d494875fcb1821d3df44f08476'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Meslo.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+          checkpoint: 'fc41f3d5a5b0df76caa2586a08c53a4571f05adb0fe0a6e30897f43899ee6ea2'
   name 'MesloLGM Nerd Font (Meslo)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
