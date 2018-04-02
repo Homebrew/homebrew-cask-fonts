217210ea7c9ac530ac2e3cd62b8a6ab5d6d1e5ab
commitay <commitay@users.noreply.github.com>
2018-04-03 08:18:05 +1000

    Update font-meslo-nerd-font-mono

--- Casks/font-meslo-nerd-font-mono.rb
+++ Casks/font-meslo-nerd-font-mono.rb
@@ -5,13 +5,13 @@ cask 'font-meslo-nerd-font-mono' do
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Meslo.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
           checkpoint: 'fc41f3d5a5b0df76caa2586a08c53a4571f05adb0fe0a6e30897f43899ee6ea2'
-  name 'MesloLGM Nerd Font (Meslo)'
+  name 'Meslo Nerd Font (Meslo)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
-  font 'Meslo LG M Regular for Powerline Nerd Font Complete Mono.otf'
-  font 'Meslo LG L Regular for Powerline Nerd Font Complete Mono.otf'
-  font 'Meslo LG S Regular for Powerline Nerd Font Complete Mono.otf'
-  font 'Meslo LG L DZ Regular for Powerline Nerd Font Complete Mono.otf'
-  font 'Meslo LG M DZ Regular for Powerline Nerd Font Complete Mono.otf'
-  font 'Meslo LG S DZ Regular for Powerline Nerd Font Complete Mono.otf'
+  font 'Meslo LG M Regular Nerd Font Complete Mono.otf'
+  font 'Meslo LG L Regular Nerd Font Complete Mono.otf'
+  font 'Meslo LG S Regular Nerd Font Complete Mono.otf'
+  font 'Meslo LG L DZ Regular Nerd Font Complete Mono.otf'
+  font 'Meslo LG M DZ Regular Nerd Font Complete Mono.otf'
+  font 'Meslo LG S DZ Regular Nerd Font Complete Mono.otf'
 end
