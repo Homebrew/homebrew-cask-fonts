db0900e29bfbdddf5342a01e231ebbe3542dc0c7
Thammachart <bustheheroall@gmail.com>
2018-04-03 04:06:17 +0700

    Knack changed to Hack (font-hack-nerd-font 2.0.0)

--- Casks/font-hack-nerd-font.rb
+++ Casks/font-hack-nerd-font.rb
@@ -5,11 +5,11 @@ cask 'font-hack-nerd-font' do
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hack.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
           checkpoint: 'fc41f3d5a5b0df76caa2586a08c53a4571f05adb0fe0a6e30897f43899ee6ea2'
-  name 'Knack Nerd Font (Hack)'
+  name 'Hack Nerd Font (Hack)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
-  font 'Knack Bold Nerd Font Complete.ttf'
-  font 'Knack Bold Italic Nerd Font Complete.ttf'
-  font 'Knack Regular Nerd Font Complete.ttf'
-  font 'Knack Italic Nerd Font Complete.ttf'
+  font 'Hack Bold Nerd Font Complete.ttf'
+  font 'Hack Bold Italic Nerd Font Complete.ttf'
+  font 'Hack Regular Nerd Font Complete.ttf'
+  font 'Hack Italic Nerd Font Complete.ttf'
 end
