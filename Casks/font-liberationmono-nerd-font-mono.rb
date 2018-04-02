a3fc7ebc86f72b585e832de90526d236c880c31c
Thammachart <bustheheroall@gmail.com>
2018-04-03 03:21:51 +0700

    Update font-liberationmono-nerd-font-mono to 2.0.0

--- Casks/font-liberationmono-nerd-font-mono.rb
+++ Casks/font-liberationmono-nerd-font-mono.rb
@@ -1,10 +1,10 @@
 cask 'font-liberationmono-nerd-font-mono' do
-  version '1.2.0'
-  sha256 '8eb8f93c4a4ea15044790ddc1dfd50c5acaf883a2f2e6cb148a5e0a2e0224547'
+  version '2.0.0'
+  sha256 'c4c705a351483ce096d4ebb2af2a45364d193b295cb870cfe0f38e8b3a7ac136'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/LiberationMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
-          checkpoint: '7dedec17cde17542418131f94e739265707a4abe9d0773287d14f175c02325f7'
+          checkpoint: 'fc41f3d5a5b0df76caa2586a08c53a4571f05adb0fe0a6e30897f43899ee6ea2'
   name 'LiterationMonoPowerline Nerd Font (LiberationMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
