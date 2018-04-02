b342b6ec6c29ee24ef63a7778a4e9b7ccc5cea65
commitay <commitay@users.noreply.github.com>
2018-04-03 08:27:17 +1000

    Update droidsansmono-nerd-font fonts

--- Casks/font-droidsansmono-nerd-font.rb
+++ Casks/font-droidsansmono-nerd-font.rb
@@ -5,8 +5,8 @@ cask 'font-droidsansmono-nerd-font' do
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DroidSansMono.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom',
           checkpoint: 'fc41f3d5a5b0df76caa2586a08c53a4571f05adb0fe0a6e30897f43899ee6ea2'
-  name 'DroidSansMonoForPowerline Nerd Font (DroidSansMono)'
+  name 'DroidSansMono Nerd Font (DroidSansMono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts'
 
-  font 'Droid Sans Mono for Powerline Nerd Font Complete.otf'
+  font 'Droid Sans Mono Nerd Font Complete.otf'
 end
