3b381933fee997cc25f77f6c198b713b24b82e2d
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-04-15 15:20:47 +0100

    Update font-babelstone-han to latest

--- Casks/font-babelstone-han.rb
+++ Casks/font-babelstone-han.rb
@@ -1,10 +1,10 @@
 cask 'font-babelstone-han' do
-  version '11.0.0,3902'
-  sha256 '0e3e59db73b7ee03271b52cfd49cb7988460353db4064557ddbb8d7aac5c7cbd'
+  version :latest
+  sha256 :no_check
 
-  url "http://www.babelstone.co.uk/Fonts/#{version.after_comma}/BabelStoneHan.zip"
+  url 'http://www.babelstone.co.uk/Fonts/Download/BabelStoneHan.zip'
   name 'BabelStone Han'
   homepage 'http://babelstone.co.uk/Fonts/Han.html'
 
-  font "BabelStoneHan_#{version.before_comma.dots_to_underscores}.ttf"
+  font 'BabelStoneHan.ttf'
 end
