23774dce6c45f063e46453685642b80169fda4dd
Afonso <afonso.graca@me.com>
2018-04-18 06:43:13 +1000

    fix cask submission
    fix cask according to `brew cask audit --download` and `brew cask style --fix`

--- Casks/font-simple-line-icons.rb
+++ Casks/font-simple-line-icons.rb
@@ -2,11 +2,12 @@ cask 'font-simple-line-icons' do
   version '2.4.1'
   sha256 'fd0e9edd1550f8f8ded705160f0e433aae4170fe9f21a595eb583ac54c7e2d12'
 
-  url 'https://github.com/thesabbir/simple-line-icons/archive/#{version}.zip'
+  # github.com/thesabbir/simple-line-icons was verified as official when first introduced to the cask
+  url "https://github.com/thesabbir/simple-line-icons/archive/#{version}.zip"
   appcast 'https://github.com/thesabbir/simple-line-icons/releases.atom',
-            checkpoint: '7e51c187ad2d125bf82c310b02bc47d775b802dc6485dfe0d089368653d71314'
+          checkpoint: 'a54dd5378eaa2bbe2ab51aec5101ca50d160df9f7a010bbefc085868eeb56447'
   name 'Simple Line Icons'
-  homepage 'http://simplelineicons.com'
+  homepage 'http://simplelineicons.com/'
 
-  font 'simple-line-icons-#{version}/fonts/Simple-Line-Icons.ttf'
+  font "simple-line-icons-#{version}/fonts/Simple-Line-Icons.ttf"
 end
