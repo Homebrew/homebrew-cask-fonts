ff4047e768a2328694aab115746fbc8964747c54
Thammachart Chinvarapon <Thammachart@users.noreply.github.com>
2018-05-03 01:24:43 +0700

    Update font-iosevka to 1.14.2 (#1683)

--- Casks/font-iosevka.rb
+++ Casks/font-iosevka.rb
@@ -1,10 +1,10 @@
 cask 'font-iosevka' do
-  version '1.14.1'
-  sha256 '41d35e3ac2fce0cfc9b549e26d5d7a8246d264388cf5ae3038651a306d2d2280'
+  version '1.14.2'
+  sha256 '50521c7fa9707f6e03493b7362c65eba94e29f1c738d2605d3d58758e32942f4'
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/iosevka-pack-#{version}.zip"
   appcast 'https://github.com/be5invis/Iosevka/releases.atom',
-          checkpoint: '9a79d126b511a1760e6c166ed13263645b816ad045a60fda46a8f2bd73f55a73'
+          checkpoint: '1756f837e8add4d1cdd972bc2950e17d6c9adf92fae91d5988ccd2a540de70c0'
   name 'Iosevka'
   homepage 'https://github.com/be5invis/Iosevka/'
 
