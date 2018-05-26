4b99cf92553f8a553681b9e5adb7e179b1636709
Wonsup Yoon <pusnow@me.com>
2018-05-26 22:05:53 +0900

    Add font-kopub 1.0.1

---
+++ Casks/font-kopub.rb
@@ -0,0 +1,15 @@
+cask 'font-kopub' do
+  version '1.0.1'
+  sha256 'ec82c77a037c159a8835a46698b270157fea216465afa64e4398f420f1f1f95e'
+
+  url 'http://www.kopus.org/Download_font.aspx?font_id=4&name=KOPUB_OTF_FONTS.zip'
+  name 'KoPub'
+  homepage 'http://www.kopus.org/Biz/electronic/Font.aspx'
+
+  font 'KoPub Batang_Pro Bold.otf'
+  font 'KoPub Batang_Pro Light.otf'
+  font 'KoPub Batang_Pro Medium.otf'
+  font 'KoPub Dotum_Pro Bold.otf'
+  font 'KoPub Dotum_Pro Light.otf'
+  font 'KoPub Dotum_Pro Medium.otf'
+end
