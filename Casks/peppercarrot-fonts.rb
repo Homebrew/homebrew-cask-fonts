06a797ca068eed307047db89247ed5cdcb74cf7d
L. E. Segovia <amyspark@users.noreply.github.com>
2018-06-13 15:48:43 +0000

    Replace already available fonts with Cask deps

--- Casks/peppercarrot-fonts.rb
+++ Casks/peppercarrot-fonts.rb
@@ -7,6 +7,18 @@ cask 'peppercarrot-fonts' do
   name 'Pepper&Carrot fonts'
   homepage 'https://framagit.org/peppercarrot/webcomics/tree/master/fonts'
 
+  # P&C ZIP duplicates some fonts Homebrew already provides.
+  # These have been commented out below.
+  depends_on cask: 'font-lobster'
+  depends_on cask: 'font-neucha'
+  depends_on cask: 'font-ruslan-display'
+  depends_on cask: 'font-gveret-levin'
+  depends_on cask: 'font-rounded-m-plus'
+  depends_on cask: 'font-dynalight'
+  depends_on cask: 'font-fondamento'
+  depends_on cask: 'font-risque'
+  depends_on cask: 'font-yanone-kaffeesatz'
+
   font 'fonts/Arabic/DroidKufi-Regular.ttf'
   # Renamed for disambiguation between the Arabic and Farsi versions.
   font 'fonts/Arabic/Midan-Black.ttf', target: 'Midan-Black-Arabic.ttf'
@@ -14,9 +26,10 @@ cask 'peppercarrot-fonts' do
   font 'fonts/Arabic/ae_AlHor.ttf'
   font 'fonts/Arabic/ae_Tholoth.ttf'
   font 'fonts/Bengali/lohit_bn.ttf'
-  font 'fonts/Cyrillic/Lobster-Regular.ttf'
-  font 'fonts/Cyrillic/Neucha.ttf'
-  font 'fonts/Cyrillic/RuslanDisplay.ttf'
+  # Duplicated fonts.
+  # font 'fonts/Cyrillic/Lobster-Regular.ttf'
+  # font 'fonts/Cyrillic/Neucha.ttf'
+  # font 'fonts/Cyrillic/RuslanDisplay.ttf'
   font 'fonts/Farsi/Midan-Black.ttf', target: 'Midan-Black-Farsi.ttf'
   font 'fonts/Farsi/Midan.ttf', target: 'Midan-Farsi.ttf'
   font 'fonts/Farsi/Mj_Talat.TTF'
@@ -27,27 +40,31 @@ cask 'peppercarrot-fonts' do
   font 'fonts/Farsi/XP ZibaBd.ttf'
   font 'fonts/Greek/Pecita.otf'
   font 'fonts/Greek/VAG-HandWritten.otf'
-  font 'fonts/Hebrew/GveretLevinAlefAlefAlef-Regular.ttf'
+  # Duplicated fonts.
+  # font 'fonts/Hebrew/GveretLevinAlefAlefAlef-Regular.ttf'
   # Renamed for disambiguation between the Japanese and Vietnamese versions.
-  font 'fonts/Japanese/rounded-mplus-1c-medium.ttf', target: 'rounded-mplus-1c-medium-japanese.ttf'
+  # font 'fonts/Japanese/rounded-mplus-1c-medium.ttf', target: 'rounded-mplus-1c-medium-japanese.ttf'
   font 'fonts/Korean/NanumPen.otf'
   font 'fonts/Latin/Combat.otf'
   font 'fonts/Latin/Cursivah.ttf'
   font 'fonts/Latin/DrukaatieBurti.ttf'
-  font 'fonts/Latin/Dynalight.otf'
-  font 'fonts/Latin/Fondamento.ttf'
+  # Duplicated fonts.
+  # font 'fonts/Latin/Dynalight.otf'
+  # font 'fonts/Latin/Fondamento.ttf'
   font 'fonts/Latin/Lavi-Bold.ttf'
   font 'fonts/Latin/Lavi-Italic.ttf'
   font 'fonts/Latin/Lavi.ttf'
-  font 'fonts/Latin/Risque.otf'
-  font 'fonts/Latin/YanoneKaffeesatz-Bold.otf'
-  font 'fonts/Latin/YanoneKaffeesatz-Light.otf'
-  font 'fonts/Latin/YanoneKaffeesatz-Regular.otf'
-  font 'fonts/Latin/YanoneKaffeesatz-Thin.otf'
+  # Duplicated fonts.
+  # font 'fonts/Latin/Risque.otf'
+  # font 'fonts/Latin/YanoneKaffeesatz-Bold.otf'
+  # font 'fonts/Latin/YanoneKaffeesatz-Light.otf'
+  # font 'fonts/Latin/YanoneKaffeesatz-Regular.otf'
+  # font 'fonts/Latin/YanoneKaffeesatz-Thin.otf'
   font 'fonts/Malayalam/Chilanka-Regular.ttf'
   font 'fonts/Malayalam/Suruma.ttf'
   font 'fonts/Sinhalese/BhashitaComplexBold.ttf'
+  # Duplicated fonts.
   # Renamed for disambiguation between the Japanese and Vietnamese versions.
-  font 'fonts/Vietnamese/rounded-mplus-1c-medium.ttf', target: 'rounded-mplus-1c-medium-vietnamese.ttf'
+  # font 'fonts/Vietnamese/rounded-mplus-1c-medium.ttf', target: 'rounded-mplus-1c-medium-vietnamese.ttf'
   font 'fonts/Web-ComicSpice/ComicSpice.ttf'
 end
