79b13982d69d7b8590ac3e616ca85569ed61b86b
Ali Karbassi <karbassi@users.noreply.github.com>
2018-06-19 12:37:30 -0500

    Create font-colus (#1700)

---
+++ Casks/font-colus.rb
@@ -0,0 +1,10 @@
+cask 'font-colus' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.fontfabric.com/downfont/colus.zip'
+  name 'Colus'
+  homepage 'https://www.fontfabric.com/colus/'
+
+  font 'Colus-Regular.otf'
+end
