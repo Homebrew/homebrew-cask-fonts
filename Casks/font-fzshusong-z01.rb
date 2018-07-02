9965f4170e4062776c314896acbf5637727ea481
Joseph J.C. Tang <jinntrance@gmail.com>
2018-07-02 23:51:25 +0800

    add latest font fzshusong-z01(Chinese GBK charset).

---
+++ Casks/font-fzshusong-z01.rb
@@ -0,0 +1,10 @@
+cask 'font-fzshusong-z01' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://cdn1.foundertype.com/Public/Uploads/ttf/FZSSK.TTF'
+  name 'Fangzheng Shusong GBK'
+  homepage 'http://www.foundertype.com/index.php/FontInfo/index.html?id=151'
+
+  font 'FZSSK.ttf'
+end
