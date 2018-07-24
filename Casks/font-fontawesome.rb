a165b24083161b2cfa06e6d7907b514407dc4c6f
David Hall <dchall@mgh.harvard.edu>
2018-07-24 14:33:18 -0700

    Update font-fontawesome to v5.2.0 Update URL to smaller file for desktop usage (reduces from 15 MB to 5 MB)

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,14 +1,14 @@
 cask 'font-fontawesome' do
-  version '5.0.13'
-  sha256 '79f59af3a08a1356a700e23d802758fcb40dd2446bf7fe0424600afb98e07906'
+  version '5.2.0'
+  sha256 '070fc7fedbff9cf41f822bf888ba06c797faa20a3fd7805d0059003eba591216'
 
   # github.com/FortAwesome/Font-Awesome was verified as official when first introduced to the cask
-  url "https://github.com/FortAwesome/Font-Awesome/archive/#{version}.zip"
+  url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip"
   appcast 'https://github.com/FortAwesome/Font-Awesome/releases.atom'
   name 'Font Awesome'
   homepage 'http://fontawesome.io/'
 
-  font "Font-Awesome-#{version}/use-on-desktop/Font Awesome #{version.major} Brands-Regular-400.otf"
-  font "Font-Awesome-#{version}/use-on-desktop/Font Awesome #{version.major} Free-Regular-400.otf"
-  font "Font-Awesome-#{version}/use-on-desktop/Font Awesome #{version.major} Free-Solid-900.otf"
+  font "fontawesome-free-#{version}-desktop/otfs/Font Awesome #{version.major} Brands-Regular-400.otf"
+  font "fontawesome-free-#{version}-desktop/otfs/Font Awesome #{version.major} Free-Regular-400.otf"
+  font "fontawesome-free-#{version}-desktop/otfs/Font Awesome #{version.major} Free-Solid-900.otf"
 end
