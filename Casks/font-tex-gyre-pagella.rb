d9530ff27bc7b97ec3f1010dec0685513c92c106
Gallian <24864749+galliangg@users.noreply.github.com>
2018-07-31 02:00:05 +0100

    Update font-tex-gyre-pagella.rb

--- Casks/font-tex-gyre-pagella.rb
+++ Casks/font-tex-gyre-pagella.rb
@@ -1,8 +1,8 @@
 cask 'font-tex-gyre-pagella' do
-  version '2_501'
+  version '2.501'
   sha256 'e16078d19860d68a54bcaedc00e35c2a81b396cdc36842700f1d556f89cf8ef2'
 
-  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/qpl#{version}otf.zip"
+  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/qpl2_501otf.zip"
   name 'TeX Gyre Pagella'
   homepage 'http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella'
 
