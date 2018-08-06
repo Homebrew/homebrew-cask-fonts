a85923586500949b9e2d3853ed516a2e5caa5f00
Colin Dean <colin.dean@arcadia.io>
2018-08-06 12:38:44 -0400

    Adds Encode Sans v2.000
    Pulls in the whole suite from Google Fonts.

---
+++ Casks/font-encodesans.rb
@@ -0,0 +1,23 @@
+cask 'font-encodesans' do
+  version :latest
+  sha256 :no_check
+
+  # github.com/google/fonts was verified as official when first introduced to the cask
+  url 'https://github.com/google/fonts/trunk/ofl/encodesans',
+      using:      :svn,
+      trust_cert: true
+  name 'Encode Sans'
+  homepage 'https://fonts.google.com/specimen/Encode+Sans'
+
+  depends_on macos: '>= :sierra'
+
+  font 'EncodeSans-Black.ttf'
+  font 'EncodeSans-Bold.ttf'
+  font 'EncodeSans-ExtraBold.ttf'
+  font 'EncodeSans-ExtraLight.ttf'
+  font 'EncodeSans-Light.ttf'
+  font 'EncodeSans-Medium.ttf'
+  font 'EncodeSans-Regular.ttf'
+  font 'EncodeSans-SemiBold.ttf'
+  font 'EncodeSans-Thin.ttf'
+end
