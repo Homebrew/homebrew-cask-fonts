9571a7374af76c2fd522d17f964f503e3ae925f5
Vítor Galvão <info@vitorgalvao.com>
2018-08-10 19:23:08 +0100

    Update font-poppins-latin.rb

--- Casks/font-poppins-latin.rb
+++ Casks/font-poppins-latin.rb
@@ -2,7 +2,7 @@ cask 'font-poppins-latin' do
   version '5.001'
   sha256 'cd4bdded93013fc9f123485b8fbc6113f62a7ff77492cc97cba6901cf6493738'
 
-  url 'https://github.com/itfoundry/Poppins/raw/master/products/PoppinsLatin-5.001-Latin-OTF.zip'
+  url "https://github.com/itfoundry/Poppins/raw/master/products/PoppinsLatin-#{version}-Latin-OTF.zip"
   name 'PoppinsLatin'
   homepage 'https://github.com/itfoundry/poppins'
 
