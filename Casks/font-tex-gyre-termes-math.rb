98abdd99b0df4e88437afe1041ba5e9fab1a3be7
Taekyung Kim <Taekyung.Kim.Maths@gmail.com>
2018-08-23 22:51:22 +0900

    Add TeX Gyre Termes Math v1.543
    One of the missing tex gyre math fonts.

---
+++ Casks/font-tex-gyre-termes-math.rb
@@ -0,0 +1,10 @@
+cask 'font-tex-gyre-termes-math' do
+  version '1.543'
+  sha256 '5875b39d3987cbe4258e5933a2292fbece870177053c5a8f495492769e2d4bb2'
+
+  url 'http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyretermes-math-1543.zip'
+  name 'Termes Math'
+  homepage 'http://www.gust.org.pl/projects/e-foundry/tg-math'
+
+  font 'texgyretermes-math-1543/opentype/texgyretermes-math.otf'
+end
