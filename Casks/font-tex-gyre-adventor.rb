88600e3ac0a5a9e27896e0c82af4f76fd633ef37
Taekyung <taekyung.kim.maths@gmail.com>
2018-08-26 02:24:47 +0900

    Upgrade font-tex-gyre-adventor to 2.501 (#1715)
    Also there are some upstream changes in filename conventions. I also fix
    the formula according to the new convention.

--- Casks/font-tex-gyre-adventor.rb
+++ Casks/font-tex-gyre-adventor.rb
@@ -1,13 +1,13 @@
 cask 'font-tex-gyre-adventor' do
-  version '2.003'
-  sha256 '7c115d00ff2b498fdad6ab787b4401b1e6ffe9e2f05e03614c47bb0cf0c47266'
+  version '2.501'
+  sha256 '9e619eb1c8af5cb55240f8bb865453562a2efd9059dee39d085fb71f7a00f7a2'
 
-  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor/qag#{version}otf.zip"
+  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor/qag#{version.dots_to_underscores}otf.zip"
   name 'TeX Gyre Adventor'
   homepage 'http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor'
 
-  font 'texgyreadventor-bold.otf'
-  font 'texgyreadventor-bolditalic.otf'
-  font 'texgyreadventor-italic.otf'
-  font 'texgyreadventor-regular.otf'
+  font "qag#{version.dots_to_underscores}otf/texgyreadventor-bold.otf"
+  font "qag#{version.dots_to_underscores}otf/texgyreadventor-bolditalic.otf"
+  font "qag#{version.dots_to_underscores}otf/texgyreadventor-italic.otf"
+  font "qag#{version.dots_to_underscores}otf/texgyreadventor-regular.otf"
 end
