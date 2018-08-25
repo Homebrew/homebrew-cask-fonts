ae069a537e694d476b567118c3c6c390bf25b74c
Taekyung <taekyung.kim.maths@gmail.com>
2018-08-26 07:35:28 +0900

    Add Tex gyre math fonts (#1714)
    * Add TeX-Gyre-Bonum-Math v1.005
    
    One of the missing tex gyre math fonts.
    
    * Add TeX Gyre Schola Math v1.533
    
    One of the missing tex gyre math fonts.
    
    * Add TeX Gyre Termes Math v1.543
    
    One of the missing tex gyre math fonts.

---
+++ Casks/font-tex-gyre-schola-math.rb
@@ -0,0 +1,10 @@
+cask 'font-tex-gyre-schola-math' do
+  version '1.533'
+  sha256 '53560861144138e25f89f1f487126d21c81c5086364ffcf2c8e5e46e37ebbe00'
+
+  url 'http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyreschola-math-1533.zip'
+  name 'Schola Math'
+  homepage 'http://www.gust.org.pl/projects/e-foundry/tg-math'
+
+  font 'texgyreschola-math-1533/opentype/texgyreschola-math.otf'
+end
