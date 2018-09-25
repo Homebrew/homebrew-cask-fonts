73efeaa335917da2489e3775d0a61fa5027eaf78
Daniel Rothmaler <drothmaler@users.noreply.github.com>
2018-09-25 15:01:08 +0200

    Add Inconsolata-g

---
+++ Casks/font-inconsolata-g.rb
@@ -0,0 +1,13 @@
+cask 'font-inconsolata-g' do
+  version :latest
+  sha256 :no_check
+
+  # fantascienza.net/leonardo/ar/inconsolatag/ was verified as official when first introduced to the cask
+  url 'http://www.fantascienza.net/leonardo/ar/inconsolatag/inconsolata-g_font.zip'
+  name 'Inconsolata-g'
+  homepage 'https://leonardo-m.livejournal.com/77079.html'
+
+  depends_on macos: '>= :sierra'
+
+  font 'Inconsolata-g.otf'
+end
