91765e1ba5a741013922791c536f1e5a3c9e8904
Vítor Galvão <info@vitorgalvao.com>
2018-10-04 12:49:14 +0100

    Update font-inconsolata-g.rb

--- Casks/font-inconsolata-g.rb
+++ Casks/font-inconsolata-g.rb
@@ -2,7 +2,7 @@ cask 'font-inconsolata-g' do
   version :latest
   sha256 :no_check
 
-  # fantascienza.net/leonardo/ar/inconsolatag/ was verified as official when first introduced to the cask
+  # fantascienza.net/leonardo/ar/inconsolatag was verified as official when first introduced to the cask
   url 'http://www.fantascienza.net/leonardo/ar/inconsolatag/inconsolata-g_font.zip'
   name 'Inconsolata-g'
   homepage 'https://leonardo-m.livejournal.com/77079.html'
