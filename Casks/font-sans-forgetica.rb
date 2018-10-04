c896347245fc332e58121021c8669aa467b6aad6
commitay <commitay@users.noreply.github.com>
2018-10-04 11:22:55 +1000

    Update font-sans-forgetica.rb

--- Casks/font-sans-forgetica.rb
+++ Casks/font-sans-forgetica.rb
@@ -2,9 +2,9 @@ cask 'font-sans-forgetica' do
   version :latest
   sha256 :no_check
 
-  url "http://sansforgetica.rmit/Common/Zips/Sans%20Forgetica.zip"
+  url 'http://sansforgetica.rmit/Common/Zips/Sans%20Forgetica.zip'
   name 'Sans Forgetica'
   homepage 'http://sansforgetica.rmit/'
 
-  font "Sans Forgetica/SansForgetica-Regular.otf"
+  font 'Sans Forgetica/SansForgetica-Regular.otf'
 end
