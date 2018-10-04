7175956f5e6223d8ba7ecc521dbc9944c6864c04
Cole Hudson <cole@colejhudson.com>
2018-10-04 14:00:49 -0700

    Add Cask for Museo 300 font

---
+++ Casks/font-museo-300.rb
@@ -0,0 +1,11 @@
+cask 'font-museo-300' do
+  version '2.002'
+  sha256 'c4c40653bc288aa123f38d6832c77498ba30e483caa16991f570cca625362344'
+
+  url 'http://www.abstractfonts.com/download/14890?option=zip'
+  name 'Museo 300'
+  homepage 'http://www.abstractfonts.com/font/14890'
+
+  font 'otf/Museo300-Regular.otf'
+  font 'otf/Museo700-Regular.otf'
+end
