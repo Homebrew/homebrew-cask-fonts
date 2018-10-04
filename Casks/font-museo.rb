325b620c6356eb4552d5790d503517fd66843482
Cole Hudson <cole@colejhudson.com>
2018-10-04 14:33:32 -0700

    Renamed Cask

--- Casks/font-museo.rb
+++ Casks/font-museo.rb
@@ -1,9 +1,9 @@
-cask 'font-museo-300' do
+cask 'font-museo' do
   version '2.002'
   sha256 'c4c40653bc288aa123f38d6832c77498ba30e483caa16991f570cca625362344'
 
   url 'http://www.abstractfonts.com/download/14890?option=zip'
-  name 'Museo 300'
+  name 'Museo'
   homepage 'http://www.abstractfonts.com/font/14890'
 
   font 'Museo300-Regular.otf'
