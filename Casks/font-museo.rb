152917c2c252e8486b20252f73c04cd4e52999a8
Cole Hudson <11510882+colejhudson@users.noreply.github.com>
2018-10-07 10:24:42 -0700

    Add Cask for Museo v2.002 (#1732)
    * Add Cask for Museo 300 font
    
    * Updated cask
    
    * Renamed Cask

---
+++ Casks/font-museo.rb
@@ -0,0 +1,11 @@
+cask 'font-museo' do
+  version '2.002'
+  sha256 'c4c40653bc288aa123f38d6832c77498ba30e483caa16991f570cca625362344'
+
+  url 'http://www.abstractfonts.com/download/14890?option=zip'
+  name 'Museo'
+  homepage 'http://www.abstractfonts.com/font/14890'
+
+  font 'Museo300-Regular.otf'
+  font 'Museo700-Regular.otf'
+end
