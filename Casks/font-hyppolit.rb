39df46afee88e73a56a60146b2cd3049973b1444
Viktor Szakats <vszakats@users.noreply.github.com>
2018-10-07 17:24:58 +0000

    Secure URLs (#1734)

--- Casks/font-hyppolit.rb
+++ Casks/font-hyppolit.rb
@@ -2,7 +2,7 @@ cask 'font-hyppolit' do
   version :latest
   sha256 :no_check
 
-  url 'http://dl.1001fonts.com/hyppolit.zip'
+  url 'https://dl.1001fonts.com/hyppolit.zip'
   name 'Hyppolit'
   homepage 'https://www.1001fonts.com/hyppolit-font.html'
 
