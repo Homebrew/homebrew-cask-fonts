f7d756b685b7fc17e10b3a3894e32092e29cc9ae
Masayuki Nagamachi <masayuki.nagamachi@gmail.com>
2018-10-08 02:26:20 +0900

    Fix unzip errors occurred in font-myrica and font-myricam installation (#1725)
    * Fix unzip errors occurred in font-myrica and font-myricam installation
    
    * Update font-myricam.rb
    
    * Update font-myrica.rb

--- Casks/font-myrica.rb
+++ Casks/font-myrica.rb
@@ -1,9 +1,9 @@
 cask 'font-myrica' do
   version '2.006.20150301'
-  sha256 'ac85d476a7a8cc809be015b9593afff2d998e7cea3b9fd9aee7d9d9a05ba449b'
+  sha256 'a90eb9b79885f02ad9e0e752a0b979b699847be7de13dc3b6113658f006d12bd'
 
   # codeload.github.com/tomokuni/Myrica was verified as official when first introduced to the cask
-  url "https://codeload.github.com/tomokuni/Myrica/zip/#{version}"
+  url "https://codeload.github.com/tomokuni/Myrica/tar.gz/#{version}"
   appcast 'https://github.com/tomokuni/Myrica/releases.atom'
   name 'Myrica'
   homepage 'https://myrica.estable.jp/'
