65d042e93be90d0f4aa4c30b750eac9ddbdbf12e
jerryskye <jaroslaw.siedlarz@netguru.pl>
2018-10-12 11:53:09 +0200

    Update FontAwesome to 5.4.1

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,6 +1,6 @@
 cask 'font-fontawesome' do
-  version '5.2.0'
-  sha256 '070fc7fedbff9cf41f822bf888ba06c797faa20a3fd7805d0059003eba591216'
+  version '5.4.1'
+  sha256 '9defb90a848d9538212b4bf078301f3b53e0520032c565e07b00b595c7d9f9ac'
 
   # github.com/FortAwesome/Font-Awesome was verified as official when first introduced to the cask
   url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip"
