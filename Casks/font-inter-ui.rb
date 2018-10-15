0f30108601512103c6a09e7bcafeb5c60b7a9692
Denny Trebbin <fibric@users.noreply.github.com>
2018-10-15 18:18:39 +0200

    Font update Inter-UI to v3.0

--- Casks/font-inter-ui.rb
+++ Casks/font-inter-ui.rb
@@ -1,6 +1,6 @@
 cask 'font-inter-ui' do
-  version '2.5'
-  sha256 '0a8a74623d254fa6dbf92ef8c564e310727013b2a2cf701f76f448568f7d05e7'
+  version '3.0'
+  sha256 'c9adea61ca421234b21aa85e6d3c0b9e28a6040e835f8a5b48b6de72072bdbfa'
 
   # github.com/rsms/inter was verified as official when first introduced to the cask
   url "https://github.com/rsms/inter/releases/download/v#{version}/Inter-UI-#{version}.zip"
