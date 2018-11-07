2e5aa1970fbe0fcd3b50e2eddc93868f281b6e6e
Andrew Dunning <adunning@users.noreply.github.com>
2018-11-07 16:11:29 -0500

    Use version variable

--- Casks/font-brill.rb
+++ Casks/font-brill.rb
@@ -6,8 +6,8 @@ cask 'font-brill' do
   name 'Brill'
   homepage 'https://brill.com/page/BrillFont/'
 
-  font 'Brill_Typeface_2.06/Brill Bold 2.06 051.ttf'
-  font 'Brill_Typeface_2.06/Brill Bold Italic 2.06 051.ttf'
-  font 'Brill_Typeface_2.06/Brill Italic 2.06 051.ttf'
-  font 'Brill_Typeface_2.06/Brill Roman 2.06 051.ttf'
+  font "Brill_Typeface_#{version}/Brill Bold #{version} 051.ttf"
+  font "Brill_Typeface_#{version}/Brill Bold Italic #{version} 051.ttf"
+  font "Brill_Typeface_#{version}/Brill Italic #{version} 051.ttf"
+  font "Brill_Typeface_#{version}/Brill Roman #{version} 051.ttf"
 end
