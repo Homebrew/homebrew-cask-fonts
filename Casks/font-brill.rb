41f81936c23f40109512334d429a32d5a4f973dd
Andrew Dunning <adunning@users.noreply.github.com>
2018-11-07 14:35:12 -0500

    Add Brill font

---
+++ Casks/font-brill.rb
@@ -0,0 +1,13 @@
+cask 'font-brill' do
+  version '2.06'
+  sha256 'b58eeb26ea43d9a020ac9891a395ff7364bac7215d0714910561b675e4947166'
+
+  url 'https://brill.com/fileasset/downloads_static/static_typefacedownload_typefacepackage.zip'
+  name 'Brill'
+  homepage 'https://brill.com/page/BrillFont/'
+
+  font 'Brill_Typeface_2.06/Brill Bold 2.06 051.ttf'
+  font 'Brill_Typeface_2.06/Brill Bold Italic 2.06 051.ttf'
+  font 'Brill_Typeface_2.06/Brill Italic 2.06 051.ttf'
+  font 'Brill_Typeface_2.06/Brill Roman 2.06 051.ttf'
+end
