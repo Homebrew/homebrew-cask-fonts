fa450c7c135583e5887cf8d3dc0a94a49b8f96cb
makigumo <makigumo@users.noreply.github.com>
2018-11-19 21:51:36 +0100

    Update font-source-han-sans to 2.0000

--- Casks/font-source-han-sans.rb
+++ Casks/font-source-han-sans.rb
@@ -1,8 +1,8 @@
 cask 'font-source-han-sans' do
-  version '1.004'
-  sha256 'b684c9b659ec4998e72d22ff5ed3f531d31d506ae00a48ec72b9846be23e7c10'
+  version '2.000'
+  sha256 '500a516d67ec25328f9bd9e357f7ee14bfa560372b6002b76b18749207469da9'
 
-  url 'https://github.com/adobe-fonts/source-han-sans/raw/5f5311e71cb628321cc0cffb51fb38d862b726aa/SuperOTC/SourceHanSans.ttc.zip'
+  url 'https://github.com/adobe-fonts/source-han-sans/raw/2.000R/SuperOTC/SourceHanSans.ttc.zip'
   name 'Source Han Sans'
   homepage 'https://github.com/adobe-fonts/source-han-sans'
 
