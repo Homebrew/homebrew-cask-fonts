d13ab5dafe049b8601217db86b8ca50057d96c91
Maecenas Li <lx70716@gmail.com>
2018-11-27 16:06:36 +0800

    Fix stanzas within the same group to have no lines between

--- Casks/font-source-han-serif.rb
+++ Casks/font-source-han-serif.rb
@@ -3,7 +3,6 @@ cask 'font-source-han-serif' do
   sha256 '85cc634fa228286ca307803bbb4ca61f61fa821b3ed573f4f07c6f0c064426b5'
 
   url 'https://github.com/adobe-fonts/source-han-serif/releases/download/1.001R/SourceHanSerif.ttc'
-
   appcast 'https://github.com/adobe-fonts/source-han-serif/releases.atom'
   name 'Source Han Serif'
   homepage 'https://github.com/adobe-fonts/source-han-serif'
