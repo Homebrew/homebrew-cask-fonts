5dbd44fa52481319a4ede707a0b5bbe889331827
Maecenas Li <lx70716@gmail.com>
2018-11-28 20:55:46 +0800

    Add Font Source Hans Serif v1.001R SuperOTC (#1756)
    * Add Font Source Hans Serif v1.001R SuperOTC
    
    * Fix stanzas within the same group to have no lines between

---
+++ Casks/font-source-han-serif.rb
@@ -0,0 +1,13 @@
+cask 'font-source-han-serif' do
+  version '1.001R'
+  sha256 '85cc634fa228286ca307803bbb4ca61f61fa821b3ed573f4f07c6f0c064426b5'
+
+  url 'https://github.com/adobe-fonts/source-han-serif/releases/download/1.001R/SourceHanSerif.ttc'
+  appcast 'https://github.com/adobe-fonts/source-han-serif/releases.atom'
+  name 'Source Han Serif'
+  homepage 'https://github.com/adobe-fonts/source-han-serif'
+
+  depends_on macos: '>= :mountain_lion'
+
+  font 'SourceHanSerif.ttc'
+end
