9bf127453d7b82cc2c9f84e7dc02af820a5e4a63
Dustin Knopoff <knopoff.d@husky.neu.edu>
2018-12-13 09:49:34 -0500

    Add Optician Sans v2 (#1760)
    * Add Optician Sans v2
    
    * Fix styling for Add Optician Sans v2
    
    * Removed trailing whitespace
    
    * Fix stanza grouping
    
    * Update font-optician-sans.rb

---
+++ Casks/font-optician-sans.rb
@@ -0,0 +1,10 @@
+cask 'font-optician-sans' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://optician-sans.com/font-files/v2/OpticianSans.zip'
+  name 'Optician Sans'
+  homepage 'https://optician-sans.com/'
+
+  font 'Optician-Sans.otf'
+end
