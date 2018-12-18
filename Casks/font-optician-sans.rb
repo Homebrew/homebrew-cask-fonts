6fa8287be5a9e2fba90296e400981b1f42aa23c2
Dustin Knopoff <dustinknopoff@gmail.com>
2018-12-18 08:48:46 -0500

    Update Optician Sans to use GitHub as source
    As per [author's request](https://github.com/anewtypeofinterference/Optician-Sans/issues/1)

--- Casks/font-optician-sans.rb
+++ Casks/font-optician-sans.rb
@@ -2,7 +2,7 @@ cask 'font-optician-sans' do
   version :latest
   sha256 :no_check
 
-  url 'https://optician-sans.com/font-files/v2/OpticianSans.zip'
+  url 'https://github.com/anewtypeofinterference/Optician-Sans/raw/master/OpenType-PS/Optician-Sans.otf'
   name 'Optician Sans'
   homepage 'https://optician-sans.com/'
 
