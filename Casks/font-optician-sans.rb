7f2b12ddc269bbfc35f4e62931607025a4efcf6e
Vítor Galvão <info@vitorgalvao.com>
2018-12-19 22:08:58 +0000

    Update font-optician-sans.rb

--- Casks/font-optician-sans.rb
+++ Casks/font-optician-sans.rb
@@ -2,6 +2,7 @@ cask 'font-optician-sans' do
   version :latest
   sha256 :no_check
 
+  # github.com/anewtypeofinterference/Optician-Sans was verified as official when first introduced to the cask
   url 'https://github.com/anewtypeofinterference/Optician-Sans/raw/master/OpenType-PS/Optician-Sans.otf'
   name 'Optician Sans'
   homepage 'https://optician-sans.com/'
