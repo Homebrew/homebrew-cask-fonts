34c44c7bbc878460c13a8cfcb48c44f2327376ff
Birger J. Nordølum <birgerjn@gmail.com>
2018-12-23 14:02:49 +0100

    Update font-iosevka to 2.0.2

--- Casks/font-iosevka.rb
+++ Casks/font-iosevka.rb
@@ -1,6 +1,6 @@
 cask 'font-iosevka' do
-  version '2.0.1'
-  sha256 '1779df346353305878aae87b24ab49cb55d259949e8e906bf7667746422a3aae'
+  version '2.0.2'
+  sha256 '03010cc668725e3f40e72fc8c48e4d76f4617714cf2c0e69a7dfdc0a59ef48a8'
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
   appcast 'https://github.com/be5invis/Iosevka/releases.atom'
