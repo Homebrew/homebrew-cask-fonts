52eb64934bb15ce1448b89017965ef6bc714f94e
Birger J. Nordølum <birgerjn@gmail.com>
2018-12-23 14:51:12 +0100

    Update font-iosevka-slab to 2.0.2 (#1763)

--- Casks/font-iosevka-slab.rb
+++ Casks/font-iosevka-slab.rb
@@ -1,6 +1,6 @@
 cask 'font-iosevka-slab' do
-  version '2.0.1'
-  sha256 '9011da07e46b98ef16b224e22ffbcefe489975b84597f4ad72adcc030187d41e'
+  version '2.0.2'
+  sha256 'f6f16f1d9615e95cce1b04f4ce9833b252badaca605d97fde22884bfcec088dd'
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-slab-#{version}.zip"
   appcast 'https://github.com/be5invis/Iosevka/releases.atom'
