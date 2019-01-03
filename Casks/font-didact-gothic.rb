dc9eccd7555c62e9b2e7115398cb22717cf7dd24
404NetworkError <404NetworkError@users.noreply.github.com>
2019-01-03 14:18:07 -0500

    Update font-didact-gothic to 2.101

--- Casks/font-didact-gothic.rb
+++ Casks/font-didact-gothic.rb
@@ -1,5 +1,5 @@
 cask 'font-didact-gothic' do
-  version '20110429'
+  version '2.101'
   sha256 'b98f9e091b6337a86f4e0f69c94c31905e1e287782853f472e176b4e28d3f81f'
 
   # github.com/google/fonts was verified as official when first introduced to the cask
@@ -7,5 +7,5 @@ cask 'font-didact-gothic' do
   name 'Didact Gothic'
   homepage 'https://www.google.com/fonts/specimen/Didact%20Gothic'
 
-  font 'DidactGothic.ttf'
+  font 'DidactGothic-Regular.ttf'
 end
