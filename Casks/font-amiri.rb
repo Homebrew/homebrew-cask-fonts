4f11690ece4930519924d32966e64e9c1fb5c25b
404NetworkError <404NetworkError@users.noreply.github.com>
2019-01-03 13:44:47 -0500

    Update font-amiri to 0.111

--- Casks/font-amiri.rb
+++ Casks/font-amiri.rb
@@ -3,13 +3,15 @@ cask 'font-amiri' do
   sha256 '1fbfccced6348b5db2c1c21d5b319cd488e14d055702fa817a0f6cb83d882166'
 
   # github.com/alif-type/amiri-font was verified as official when first introduced to the cask
-  url "https://github.com/alif-type/amiri-font/releases/download/#{version}/amiri-#{version}.zip"
+  url "https://github.com/alif-type/amiri-font/releases/download/#{version}/Amiri-#{version}.zip"
   appcast 'https://github.com/alif-type/amiri/releases.atom'
   name 'Amiri'
   homepage 'https://www.amirifont.org/'
 
-  font "amiri-#{version}/amiri-bold.ttf"
-  font "amiri-#{version}/amiri-boldslanted.ttf"
-  font "amiri-#{version}/amiri-quran.ttf"
-  font "amiri-#{version}/amiri-regular.ttf"
+  font "Amiri-#{version}/Amiri-Bold.ttf"
+  font "Amiri-#{version}/Amiri-BoldSlanted.ttf"
+  font "Amiri-#{version}/Amiri-Regular.ttf"
+  font "Amiri-#{version}/Amiri-Slanted.ttf"
+  font "Amiri-#{version}/AmiriQuran.ttf"
+  font "Amiri-#{version}/AmiriQuranColored.ttf"
 end
