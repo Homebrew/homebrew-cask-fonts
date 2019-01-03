ee0f91cb7022ae16e99f9b85e559aa0f6291c8b5
404NetworkError <404NetworkError@users.noreply.github.com>
2019-01-03 13:50:54 -0500

    Update font-average-sans to 1.002

--- Casks/font-average-sans.rb
+++ Casks/font-average-sans.rb
@@ -1,6 +1,6 @@
 cask 'font-average-sans' do
-  version '1.001'
-  sha256 '80e216cd92e30f9f2fe73d6bd3a80dfd2105626929b7d037d473831c25782e38'
+  version '1.002'
+  sha256 'f978bb0eb5ad93b02f3a8e4ef0ed525a1192b4076e8a8d0bb37b2546a297762e'
 
   # github.com/google/fonts was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/averagesans/AverageSans-Regular.ttf'
