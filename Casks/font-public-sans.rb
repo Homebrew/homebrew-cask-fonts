e6f0aebd76e29317924125ca6be21c89064e45ad
Vítor Galvão <info@vitorgalvao.com>
2019-01-15 22:24:19 +0000

    Update font-public-sans.rb

--- Casks/font-public-sans.rb
+++ Casks/font-public-sans.rb
@@ -1,9 +1,9 @@
 cask 'font-public-sans' do
-  version 'v1.0.0'
+  version '1.0.0'
   sha256 'f2ae81c5e3949e867957abac962588fece769bba04e34eb2328a7a1962034fba'
 
   # github.com/uswds/public-sans was verified as official when first introduced to the cask
-  url "https://github.com/uswds/public-sans/releases/download/#{version}/public-sans-#{version}.zip"
+  url "https://github.com/uswds/public-sans/releases/download/v#{version}/public-sans-v#{version}.zip"
   appcast 'https://github.com/uswds/public-sans/releases.atom'
   name 'Public Sans'
   homepage 'https://18franklin.18f.gov/'
