c5ce74503e067aca32d30a30704900f9f79f8a8a
Andrew Dunning <adunning@users.noreply.github.com>
2019-01-21 19:38:27 -0500

    Add IM Fell Types v3

---
+++ Casks/font-im-fell-types.rb
@@ -0,0 +1,22 @@
+cask 'font-im-fell-types' do
+  version '3.00'
+  sha256 'b65189a8c165dafa5ba979092ebe967e0ad86a4d584b203eee40e16d010f5308'
+
+  url 'http://iginomarini.com/fell/wp-content/uploads/IMFellTypesClass.zip'
+  name 'IM Fell Types'
+  homepage 'http://iginomarini.com/fell/'
+
+  font 'IMFeDPit29C.otf'
+  font 'IMFeDPrm29C.otf'
+  font 'IMFeENit29C.otf'
+  font 'IMFeENrm29C.otf'
+  font 'IMFeFCit29C.otf'
+  font 'IMFeFCrm29C.otf'
+  font 'IMFeFlow1.otf'
+  font 'IMFeFlow2.otf'
+  font 'IMFeGPit29C.otf'
+  font 'IMFeGPrm29C.otf'
+  font 'IMFePIit29C.otf'
+  font 'IMFePIrm29C.otf'
+  font 'IMFeTLrm29C.otf'
+end
