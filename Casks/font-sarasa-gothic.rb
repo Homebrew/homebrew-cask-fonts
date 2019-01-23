7acf6e0ff81679ae84777efb99dfa593aac5e5ad
SteamedFish <SteamedFish@users.noreply.github.com>
2019-01-24 02:50:55 +0800

    Update font-sarasa-gothic to 0.7.0 (#1791)

--- Casks/font-sarasa-gothic.rb
+++ Casks/font-sarasa-gothic.rb
@@ -1,6 +1,6 @@
 cask 'font-sarasa-gothic' do
-  version '0.6.0'
-  sha256 '5deb785be27abed5d2b3b671f9c4eb800a168d5e10505044b06903e6393f20ae'
+  version '0.7.0'
+  sha256 '9cd9fbdc741c99590a8b76feb2da4cda49695f148107df338a0eb57f6edc3dd3'
 
   url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
   appcast 'https://github.com/be5invis/Sarasa-Gothic/releases.atom'
