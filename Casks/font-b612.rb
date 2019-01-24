3f4fb06be88214f8933430de9c81469694a98045
Cameron Daniel <cam.daniel@gmail.com>
2019-01-24 01:49:42 +0100

    New Formula font-b612 (#1789)
    * New Formula font-b612
    
    * Update font-b612.rb
    
    * Update font-b612.rb

---
+++ Casks/font-b612.rb
@@ -0,0 +1,18 @@
+cask 'font-b612' do
+  version :latest
+  sha256 :no_check
+
+  # git.polarsys.org/c/b612/b612.git was verified as official when first introduced to the cask
+  url 'https://git.polarsys.org/c/b612/b612.git/snapshot/b612-master.zip'
+  name 'B612'
+  homepage 'http://b612-font.com/'
+
+  font 'b612-master/TTF/B612-Bold.ttf'
+  font 'b612-master/TTF/B612-BoldItalic.ttf'
+  font 'b612-master/TTF/B612-Italic.ttf'
+  font 'b612-master/TTF/B612-Regular.ttf'
+  font 'b612-master/TTF/B612 Mono-Bold.ttf'
+  font 'b612-master/TTF/B612 Mono-BoldItalic.ttf'
+  font 'b612-master/TTF/B612 Mono-Italic.ttf'
+  font 'b612-master/TTF/B612 Mono-Regular.ttf'
+end
