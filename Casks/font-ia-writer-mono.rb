15311c3e9565fa74553b79dd934e8baaa1bde269
DerZyklop <account-github@der-zyklop.de>
2019-01-30 16:53:05 +0100

    Add font-ia-writer-mono (#1793)
    * Create font-ia-writer-mono.rb
    
    * Update font-ia-writer-mono.rb

---
+++ Casks/font-ia-writer-mono.rb
@@ -0,0 +1,13 @@
+cask 'font-ia-writer-mono' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://codeload.github.com/iaolo/iA-Fonts/zip/master'
+  name 'iA Writer Mono'
+  homepage 'https://github.com/iaolo/iA-Fonts'
+
+  font 'iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-Bold.ttf'
+  font 'iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-BoldItalic.ttf'
+  font 'iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-Italic.ttf'
+  font 'iA-Fonts-master/iA Writer Mono/Static/iAWriterMonoS-Regular.ttf'
+end
