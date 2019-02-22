5b2b3d1577011c0ccc76e49c8e3f2de3d4f1d255
Vítor Galvão <info@vitorgalvao.com>
2019-02-22 20:55:36 +0000

    Update font-meltho.rb

--- Casks/font-meltho.rb
+++ Casks/font-meltho.rb
@@ -1,31 +1,31 @@
 cask 'font-meltho' do
-  version '1.21'
+  version '1.21,2018.06'
   sha256 'a22e61b0a48ac6c3e8bbc79d8f4dafcbd7ddb9572cf63b50478b9889cfbfdac4'
 
-  url 'https://bethmardutho.org/wp-content/uploads/2018/06/melthofonts-1.zip'
+  url 'https://bethmardutho.org/wp-content/uploads/#{version.after_comma.dots_to_slashes}/melthofonts-1.zip'
   name 'Meltho Fonts'
   homepage 'http://bethmardutho.org/syriacmac/'
 
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMAdiabene.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMAntioch.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMBatnan.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMBatnanBold.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMCtesiphon.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMEdessa.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMJerusalem.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMJerusalemBold.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMJerusalemItalic.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMJerusalemOutline.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMKharput.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMMalankara.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMMardin.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMMardinBold.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMMidyat.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMNisibin.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMNisibinOutline.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMQenNeshrin.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMTalada.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMTurAbdin.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMUrhoy.otf"
-  font "melthofonts-1/melthofonts-#{version}/SyrCOMUrhoyBold.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMAdiabene.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMAntioch.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMBatnan.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMBatnanBold.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMCtesiphon.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMEdessa.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMJerusalem.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMJerusalemBold.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMJerusalemItalic.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMJerusalemOutline.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMKharput.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMMalankara.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMMardin.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMMardinBold.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMMidyat.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMNisibin.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMNisibinOutline.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMQenNeshrin.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMTalada.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMTurAbdin.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMUrhoy.otf"
+  font "melthofonts-1/melthofonts-#{version.before_comma}/SyrCOMUrhoyBold.otf"
 end
