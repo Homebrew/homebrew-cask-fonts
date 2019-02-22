39dd36d89c9f294e96bb77b2001f429a5b014da3
Andrew Dunning <adunning@users.noreply.github.com>
2019-02-21 21:15:50 -0500

    Add Meltho Fonts 1.21

---
+++ Casks/font-meltho.rb
@@ -0,0 +1,31 @@
+cask 'font-meltho' do
+  version '1.21'
+  sha256 'a22e61b0a48ac6c3e8bbc79d8f4dafcbd7ddb9572cf63b50478b9889cfbfdac4'
+
+  url 'https://bethmardutho.org/wp-content/uploads/2018/06/melthofonts-1.zip'
+  name 'Meltho Fonts'
+  homepage 'http://bethmardutho.org/syriacmac/'
+
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMAdiabene.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMAntioch.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMBatnan.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMBatnanBold.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMCtesiphon.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMEdessa.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMJerusalem.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMJerusalemBold.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMJerusalemItalic.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMJerusalemOutline.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMKharput.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMMalankara.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMMardin.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMMardinBold.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMMidyat.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMNisibin.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMNisibinOutline.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMQenNeshrin.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMTalada.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMTurAbdin.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMUrhoy.otf"
+  font "melthofonts-1/melthofonts-#{version}/SyrCOMUrhoyBold.otf"
+end
