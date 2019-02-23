52fd5c7f0850aa4b8a6559e1260b50d84c4d1f08
Dustin Blackman <dev@dustinblackman.com>
2019-02-23 11:43:28 -0500

    add font-anka-coder-condensed

---
+++ Casks/font-anka-coder-condensed.rb
@@ -0,0 +1,13 @@
+cask 'font-anka-coder-condensed' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://fontlibrary.org/assets/downloads/anka-coder-condensed/5b5d74bc1795f57c315a532383ee0c0f/anka-coder-condensed.zip'
+  name 'Anka/Coder Condensed'
+  homepage 'https://fontlibrary.org/en/font/anka-coder-condensed'
+
+  font 'AnkaCoder-C87-b.ttf'
+  font 'AnkaCoder-C87-bi.ttf'
+  font 'AnkaCoder-C87-i.ttf'
+  font 'AnkaCoder-C87-r.ttf'
+end
