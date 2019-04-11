5b0ae9c83c82f55e37ebeea5351faafc4f45f7fb
Noah <2554594+nhmchls@users.noreply.github.com>
2019-04-11 13:01:55 -0400

    Update font-inter.rb

--- Casks/font-inter.rb
+++ Casks/font-inter.rb
@@ -1,6 +1,6 @@
 cask 'font-inter' do
-  version '3.3'
-  sha256 'b8733799800cedbc5459944ce48fbb8bb0b8fa674a865cd7359bfbaa0038fb0c'
+  version '3.5'
+  sha256 '81f6ac333068803eb1524ea7e0d8b33c9cdad140f97666e63fc7083ecb9e325f'
 
   # github.com/rsms/inter was verified as official when first introduced to the cask
   url "https://github.com/rsms/inter/releases/download/v#{version}/Inter-#{version}.zip"
@@ -14,10 +14,16 @@ cask 'font-inter' do
   font 'Inter (OTF)/Inter-BoldItalic.otf'
   font 'Inter (OTF)/Inter-ExtraBold.otf'
   font 'Inter (OTF)/Inter-ExtraBoldItalic.otf'
+  font 'Inter (OTF)/Inter-ExtraLight-BETA.otf'
+  font 'Inter (OTF)/Inter-ExtraLightItalic-BETA.otf'
   font 'Inter (OTF)/Inter-Italic.otf'
+  font 'Inter (OTF)/Inter-Light-BETA.otf'
+  font 'Inter (OTF)/Inter-LightItalic-BETA.otf'
   font 'Inter (OTF)/Inter-Medium.otf'
   font 'Inter (OTF)/Inter-MediumItalic.otf'
   font 'Inter (OTF)/Inter-Regular.otf'
   font 'Inter (OTF)/Inter-SemiBold.otf'
   font 'Inter (OTF)/Inter-SemiBoldItalic.otf'
+  font 'Inter (OTF)/Inter-Thin-BETA.otf'
+  font 'Inter (OTF)/Inter-ThinItalic-BETA.otf'
 end
