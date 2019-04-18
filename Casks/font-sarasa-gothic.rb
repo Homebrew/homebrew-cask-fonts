f4ce87409a499a9eeea1d951e615ffb4e0ba9542
Toshiki Murayama <murayama.toshiki@gmail.com>
2019-04-18 20:09:24 +0900

    update sarasa-gothic to 0.8.0 (#1826)

--- Casks/font-sarasa-gothic.rb
+++ Casks/font-sarasa-gothic.rb
@@ -1,6 +1,6 @@
 cask 'font-sarasa-gothic' do
-  version '0.7.0'
-  sha256 '9cd9fbdc741c99590a8b76feb2da4cda49695f148107df338a0eb57f6edc3dd3'
+  version '0.8.0'
+  sha256 '6a78ce58fc5c572e2f6e17d6af84589b26dc97cccaf253851496c6773ae40f65'
 
   url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
   appcast 'https://github.com/be5invis/Sarasa-Gothic/releases.atom'
@@ -9,6 +9,12 @@ cask 'font-sarasa-gothic' do
 
   font 'sarasa-bold.ttc'
   font 'sarasa-bolditalic.ttc'
+  font 'sarasa-extralight.ttc'
+  font 'sarasa-extralightitalic.ttc'
   font 'sarasa-italic.ttc'
+  font 'sarasa-light.ttc'
+  font 'sarasa-lightitalic.ttc'
+  font 'sarasa-medium.ttc'
+  font 'sarasa-mediumitalic.ttc'
   font 'sarasa-regular.ttc'
 end
