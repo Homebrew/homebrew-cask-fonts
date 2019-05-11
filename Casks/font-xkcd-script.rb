0afa3c140600d57f0eb64526fc1c64e6f44dd914
Song Li <boltomli@users.noreply.github.com>
2019-05-11 08:53:09 +0800

    Add font-xkcd-script from ipython/xkcd-font (#1833)
    xkcd is here but xkcd-script is not yet.

---
+++ Casks/font-xkcd-script.rb
@@ -0,0 +1,10 @@
+cask 'font-xkcd-script' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/ipython/xkcd-font/raw/master/xkcd-script/font/xkcd-script.ttf'
+  name 'xkcd-script'
+  homepage 'https://github.com/ipython/xkcd-font'
+
+  font 'xkcd-script.ttf'
+end
