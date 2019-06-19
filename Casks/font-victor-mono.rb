5ead1aad4d7398da5839c3ecb3f77acb9cbb8f0d
Rune Bjørnerås <rune@proweb.no>
2019-06-19 08:32:00 +0200

    Added Victor Mono

---
+++ Casks/font-victor-mono.rb
@@ -0,0 +1,12 @@
+cask 'font-victor-mono' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://rubjo.github.io/victor-mono/Victor%20Mono.zip'
+  name 'Victor Mono'
+  homepage 'https://rubjo.github.io/victor-mono/'
+
+  font 'VictorMono-Regular.otf'
+  font 'VictorMono-Oblique.otf'
+  font 'VictorMono-Italic.otf'
+end
