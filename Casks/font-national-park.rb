5c9ae36f037d4b1ca534fbbadf7ba3a37149f533
David Gasperoni <mcdado@gmail.com>
2019-06-23 13:45:07 +0200

    Add font National Park v1.000 (#1845)
    * Add font National Park v1.000
    
    * Fixed issues reported by brew cask style

---
+++ Casks/font-national-park.rb
@@ -0,0 +1,14 @@
+cask 'font-national-park' do
+  version '1.000'
+  sha256 'b03567a235e0a81dca30f961704f8f0a352e2dd40a5f91bbce111b31bee17322'
+
+  # cargocollective.com was verified as official when first introduced to the cask
+  url 'https://files.cargocollective.com/c206464/NationalPark.zip'
+  name 'National Park'
+  homepage 'https://nationalparktypeface.com/'
+
+  font 'NationalPark-Thin.otf'
+  font 'NationalPark-Regular.otf'
+  font 'NationalPark-Outline.otf'
+  font 'NationalPark-Heavy.otf'
+end
