64201d780eefb344b0e7f419bb840f41d5beca7e
David Gasperoni <mcdado@gmail.com>
2019-06-23 03:48:58 +0200

    Fixed issues reported by brew cask style

--- Casks/font-national-park.rb
+++ Casks/font-national-park.rb
@@ -2,9 +2,10 @@ cask 'font-national-park' do
   version '1.000'
   sha256 'b03567a235e0a81dca30f961704f8f0a352e2dd40a5f91bbce111b31bee17322'
 
-  url "https://files.cargocollective.com/c206464/NationalPark.zip"
+  # cargocollective.com was verified as official when first introduced to the cask
+  url 'https://files.cargocollective.com/c206464/NationalPark.zip'
   name 'National Park'
-  homepage 'https://nationalparktypeface.com'
+  homepage 'https://nationalparktypeface.com/'
 
   font 'NationalPark-Thin.otf'
   font 'NationalPark-Regular.otf'
