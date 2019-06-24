57b58571cc0b2c9910727bd575ee2ed578c8da23
rubjo <42270947+rubjo@users.noreply.github.com>
2019-06-24 15:53:13 +0200

    Victor Mono updated to latest version with multiple weights (#1846)

--- Casks/font-victor-mono.rb
+++ Casks/font-victor-mono.rb
@@ -2,11 +2,29 @@ cask 'font-victor-mono' do
   version :latest
   sha256 :no_check
 
-  url 'https://rubjo.github.io/victor-mono/Victor%20Mono.zip'
+  url 'https://rubjo.github.io/victor-mono/VictorMonoAll.zip'
   name 'Victor Mono'
   homepage 'https://rubjo.github.io/victor-mono/'
 
-  font 'VictorMono-Regular.otf'
-  font 'VictorMono-Oblique.otf'
-  font 'VictorMono-Italic.otf'
+  font 'OTF/VictorMono-Thin.otf'
+  font 'OTF/VictorMono-ExtraLight.otf'
+  font 'OTF/VictorMono-Light.otf'
+  font 'OTF/VictorMono-Regular.otf'
+  font 'OTF/VictorMono-Medium.otf'
+  font 'OTF/VictorMono-SemiBold.otf'
+  font 'OTF/VictorMono-Bold.otf'
+  font 'OTF/VictorMono-ObliqueThin.otf'
+  font 'OTF/VictorMono-ObliqueExtraLight.otf'
+  font 'OTF/VictorMono-ObliqueLight.otf'
+  font 'OTF/VictorMono-ObliqueRegular.otf'
+  font 'OTF/VictorMono-ObliqueMedium.otf'
+  font 'OTF/VictorMono-ObliqueSemiBold.otf'
+  font 'OTF/VictorMono-ObliqueBold.otf'
+  font 'OTF/VictorMono-ItalicThin.otf'
+  font 'OTF/VictorMono-ItalicExtraLight.otf'
+  font 'OTF/VictorMono-ItalicLight.otf'
+  font 'OTF/VictorMono-ItalicRegular.otf'
+  font 'OTF/VictorMono-ItalicMedium.otf'
+  font 'OTF/VictorMono-ItalicSemiBold.otf'
+  font 'OTF/VictorMono-ItalicBold.otf'
 end
