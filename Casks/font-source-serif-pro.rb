fbb4f24dbd083bb0ca4e113aa52c1052db0d6a38
Ian Max Andolina <iandol@users.noreply.github.com>
2019-06-27 20:49:38 +0800

    Update font-source-serif-pro.rb (#1847)
    Update to V3.000, I had to change the download URL slightly.

--- Casks/font-source-serif-pro.rb
+++ Casks/font-source-serif-pro.rb
@@ -1,22 +1,23 @@
 cask 'font-source-serif-pro' do
-  version '2.007R-ro,1.007R-it'
-  sha256 '46d9b5114e3e86b24769729e2fe09288b6a94c2d4f28a7e39ef572fbe2bec2da'
+  version '3.000R'
+  sha256 '6f3135849ece74461994bd57e5e7acf3da0f8d8c96ef94b5f3bb3e91f7d155cc'
 
   # github.com/adobe-fonts/source-serif-pro was verified as official when first introduced to the cask
-  url "https://github.com/adobe-fonts/source-serif-pro/archive/#{version.before_comma}/#{version.after_comma}.zip"
+  url "https://github.com/adobe-fonts/source-serif-pro/releases/download/#{version}/source-serif-pro-#{version}.zip"
   appcast 'https://github.com/adobe-fonts/source-serif-pro/releases.atom'
   name 'Source Serif Pro'
   homepage 'https://adobe-fonts.github.io/source-serif-pro/'
 
-  font 'source-serif-pro-2.007R-ro-1.007R-it/OTF/SourceSerifPro-Black.otf'
-  font 'source-serif-pro-2.007R-ro-1.007R-it/OTF/SourceSerifPro-BlackIt.otf'
-  font 'source-serif-pro-2.007R-ro-1.007R-it/OTF/SourceSerifPro-Bold.otf'
-  font 'source-serif-pro-2.007R-ro-1.007R-it/OTF/SourceSerifPro-BoldIt.otf'
-  font 'source-serif-pro-2.007R-ro-1.007R-it/OTF/SourceSerifPro-ExtraLight.otf'
-  font 'source-serif-pro-2.007R-ro-1.007R-it/OTF/SourceSerifPro-ExtraLightIt.otf'
-  font 'source-serif-pro-2.007R-ro-1.007R-it/OTF/SourceSerifPro-It.otf'
-  font 'source-serif-pro-2.007R-ro-1.007R-it/OTF/SourceSerifPro-Light.otf'
-  font 'source-serif-pro-2.007R-ro-1.007R-it/OTF/SourceSerifPro-LightIt.otf'
-  font 'source-serif-pro-2.007R-ro-1.007R-it/OTF/SourceSerifPro-Regular.otf'
-  font 'source-serif-pro-2.007R-ro-1.007R-it/OTF/SourceSerifPro-Semibold.otf'
+  font 'OTF/SourceSerifPro-Black.otf'
+  font 'OTF/SourceSerifPro-BlackIt.otf'
+  font 'OTF/SourceSerifPro-Bold.otf'
+  font 'OTF/SourceSerifPro-BoldIt.otf'
+  font 'OTF/SourceSerifPro-ExtraLight.otf'
+  font 'OTF/SourceSerifPro-ExtraLightIt.otf'
+  font 'OTF/SourceSerifPro-It.otf'
+  font 'OTF/SourceSerifPro-Light.otf'
+  font 'OTF/SourceSerifPro-LightIt.otf'
+  font 'OTF/SourceSerifPro-Regular.otf'
+  font 'OTF/SourceSerifPro-Semibold.otf'
+  font 'OTF/SourceSerifPro-SemiboldIt.otf'
 end
