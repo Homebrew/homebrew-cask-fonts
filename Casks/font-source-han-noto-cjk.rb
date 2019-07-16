30bf025dc813c085d7d4c23c7f62a83cb1ce0a23
Toshiki Murayama <murayama.toshiki@gmail.com>
2019-07-16 14:33:25 +0900

    Update font-source-han-noto-cjk to 20190603

--- Casks/font-source-han-noto-cjk.rb
+++ Casks/font-source-han-noto-cjk.rb
@@ -1,6 +1,6 @@
 cask 'font-source-han-noto-cjk' do
-  version '20180418'
-  sha256 '55b2eebafb76193208c979b43ef22b78d50b96ce7a795c31f18dd3cf3137e3cb'
+  version '20190603'
+  sha256 '128edf85c23a4fabd6f169977b535ce4b10b7a66e80147676e33d2f3d66fbb98'
 
   url "https://github.com/adobe-fonts/source-han-super-otc/releases/download/#{version}/SourceHanNotoCJK.ttc"
   appcast 'https://github.com/adobe-fonts/source-han-super-otc/releases.atom'
