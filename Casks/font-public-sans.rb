e31e30786ed802fa88d3ce569da848b2cdf7676a
Viktor Szakats <commit@vszakats.net>
2019-08-23 08:32:54 +0000

    font-public-sans 1.006

--- Casks/font-public-sans.rb
+++ Casks/font-public-sans.rb
@@ -1,29 +1,29 @@
 cask 'font-public-sans' do
-  version '1.0.0'
-  sha256 'f2ae81c5e3949e867957abac962588fece769bba04e34eb2328a7a1962034fba'
+  version '1.006'
+  sha256 'd849e9b590d7f60480d47488c939e097580449dae8fe0fbd3411d3144a11b2e1'
 
   # github.com/uswds/public-sans was verified as official when first introduced to the cask
   url "https://github.com/uswds/public-sans/releases/download/v#{version}/public-sans-v#{version}.zip"
   appcast 'https://github.com/uswds/public-sans/releases.atom'
   name 'Public Sans'
-  homepage 'https://18franklin.18f.gov/'
+  homepage 'https://public-sans.digital.gov/'
 
-  font "public-sans-v#{version}/fonts/otf/PublicSans-Black.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-BlackItalic.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-Bold.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-BoldItalic.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-ExtraBold.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-ExtraBoldItalic.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-ExtraLight.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-ExtraLightItalic.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-Italic.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-Light.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-LightItalic.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-Medium.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-MediumItalic.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-Regular.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-SemiBold.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-SemiBoldItalic.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-Thin.otf"
-  font "public-sans-v#{version}/fonts/otf/PublicSans-ThinItalic.otf"
+  font 'fonts/otf/PublicSans-Black.otf'
+  font 'fonts/otf/PublicSans-BlackItalic.otf'
+  font 'fonts/otf/PublicSans-Bold.otf'
+  font 'fonts/otf/PublicSans-BoldItalic.otf'
+  font 'fonts/otf/PublicSans-ExtraBold.otf'
+  font 'fonts/otf/PublicSans-ExtraBoldItalic.otf'
+  font 'fonts/otf/PublicSans-ExtraLight.otf'
+  font 'fonts/otf/PublicSans-ExtraLightItalic.otf'
+  font 'fonts/otf/PublicSans-Italic.otf'
+  font 'fonts/otf/PublicSans-Light.otf'
+  font 'fonts/otf/PublicSans-LightItalic.otf'
+  font 'fonts/otf/PublicSans-Medium.otf'
+  font 'fonts/otf/PublicSans-MediumItalic.otf'
+  font 'fonts/otf/PublicSans-Regular.otf'
+  font 'fonts/otf/PublicSans-SemiBold.otf'
+  font 'fonts/otf/PublicSans-SemiBoldItalic.otf'
+  font 'fonts/otf/PublicSans-Thin.otf'
+  font 'fonts/otf/PublicSans-ThinItalic.otf'
 end
