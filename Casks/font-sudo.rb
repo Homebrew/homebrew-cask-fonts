914f8ff4abd2688b16254e008dcdcfc7201cabe5
Gadzhi Kharkharov <me@kkga.me>
2019-08-27 14:14:37 +0300

    Add Sudo font v0.39

---
+++ Casks/font-sudo.rb
@@ -0,0 +1,24 @@
+cask 'font-sudo' do
+  version '0.39'
+  sha256 '8bb90bce533190b9d09c297ab959a4fba6764742bf2055364405b1e0ed16be67'
+
+  url 'https://github.com/jenskutilek/sudo-font/releases/download/v0.39/sudo.zip'
+  appcast 'https://github.com/jenskutilek/sudo-font/releases.atom'
+  name 'Sudo'
+  homepage 'https://www.kutilek.de/sudo-font/'
+
+  font 'sudo/Sudo-Bold.ttf'
+  font 'sudo/Sudo-BoldItalic.ttf'
+  font 'sudo/Sudo-Italic.ttf'
+  font 'sudo/Sudo-Light.ttf'
+  font 'sudo/Sudo-LightItalic.ttf'
+  font 'sudo/Sudo-Medium.ttf'
+  font 'sudo/Sudo-MediumItalic.ttf'
+  font 'sudo/Sudo-Regular.ttf'
+  font 'sudo/Sudo-Thin.ttf'
+  font 'sudo/Sudo-ThinItalic.ttf'
+  font 'sudo/SudoGX-1131.ttf'
+  font 'sudo/SudoGX-1230.ttf'
+  font 'sudo/SudoGX-1238.ttf'
+  font 'sudo/SudoVariable.ttf'
+end
