66273811202a0406a5aaab1a724c814474b9bbb1
Joshua Hoeflich <joshuahoeflich97@gmail.com>
2019-09-01 09:20:37 -0500

    Add font-fira-code-regular-symbol

---
+++ font-fira-code-regular-symbol.rb
@@ -0,0 +1,10 @@
+cask 'font-fira-code-regular-symbol' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/tonsky/FiraCode/files/412440/FiraCode-Regular-Symbol.zip'
+  name 'font-fira-code-regular-symbol'
+  homepage 'https://github.com/tonsky/FiraCode'
+
+  font 'FiraCode-Regular-Symbol.otf'
+end
