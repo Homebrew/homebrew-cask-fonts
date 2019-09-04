58a2e14d8baa216771ca0dd7c5b03e463803a6ef
Jens Kutilek <webmail@kutilek.de>
2019-09-04 10:10:16 +0200

    Update font to v0.40, install variable font only

--- Casks/font-sudo.rb
+++ Casks/font-sudo.rb
@@ -1,24 +1,11 @@
 cask 'font-sudo' do
-  version '0.39'
-  sha256 '8bb90bce533190b9d09c297ab959a4fba6764742bf2055364405b1e0ed16be67'
+  version '0.40'
+  sha256 'bd85aab2e8e9f0a903bd891fd4294e836cda6180ad926506f4907d1517d714b9'
 
   url "https://github.com/jenskutilek/sudo-font/releases/download/v#{version}/sudo.zip"
   appcast 'https://github.com/jenskutilek/sudo-font/releases.atom'
   name 'Sudo'
   homepage 'https://github.com/jenskutilek/sudo-font/'
 
-  font 'sudo/Sudo-Bold.ttf'
-  font 'sudo/Sudo-BoldItalic.ttf'
-  font 'sudo/Sudo-Italic.ttf'
-  font 'sudo/Sudo-Light.ttf'
-  font 'sudo/Sudo-LightItalic.ttf'
-  font 'sudo/Sudo-Medium.ttf'
-  font 'sudo/Sudo-MediumItalic.ttf'
-  font 'sudo/Sudo-Regular.ttf'
-  font 'sudo/Sudo-Thin.ttf'
-  font 'sudo/Sudo-ThinItalic.ttf'
-  font 'sudo/SudoGX-1131.ttf'
-  font 'sudo/SudoGX-1230.ttf'
-  font 'sudo/SudoGX-1238.ttf'
   font 'sudo/SudoVariable.ttf'
 end
