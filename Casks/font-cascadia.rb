f38687e861861e676f13bb87db8bee9827e02230
KarlZeo <nieyujiang@gmail.com>
2019-09-19 13:39:12 +0800

    remove appcast for cascadia-code

--- Casks/font-cascadia.rb
+++ Casks/font-cascadia.rb
@@ -3,7 +3,6 @@ cask 'font-cascadia' do
   sha256 :no_check
 
   url "https://github.com/microsoft/cascadia-code/releases/latest/download/Cascadia.ttf"
-  appcast 'https://github.com/microsoft/cascadia-code/releases.atom'
   name 'Cascadia'
   homepage 'https://github.com/microsoft/cascadia-code'
 
