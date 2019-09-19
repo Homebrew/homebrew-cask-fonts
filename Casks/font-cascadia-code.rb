79b8e9a3cd0d7b9c0557a844fc13bc15e1e187ba
Christina Warren <blog@planetchristina.com>
2019-09-19 02:42:00 -0700

    fml

--- Casks/font-cascadia-code.rb
+++ Casks/font-cascadia-code.rb
@@ -2,8 +2,8 @@ cask 'font-cascadia-code' do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/microsoft/cascadia-code/releases/latest/download/Cascadia.ttf"
-  appcast 'https://github.com/microsoft/cascadia-code/releases.atom'
+  url 'https://github.com/microsoft/cascadia-code/releases/latest/download/Cascadia.ttf'
+  
   name 'Cascadia Code'
   homepage 'https://github.com/microsoft/cascadia-code'
 
