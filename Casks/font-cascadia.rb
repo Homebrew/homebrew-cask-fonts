f45aaefda0c2fc9f4bc9100fc1eaca89a19eed48
KarlZeo <nieyujiang@gmail.com>
2019-09-19 20:42:50 +0800

    change Cascadia to versioned

--- Casks/font-cascadia.rb
+++ Casks/font-cascadia.rb
@@ -1,8 +1,9 @@
 cask 'font-cascadia' do
-  version :latest
-  sha256 :no_check
+  version '1909.16'
+  sha256 'c0485d313fbc40f9076e707ed0aa98c9e3f2649d8b30adff705cebf0a0dc1d93'
 
-  url "https://github.com/microsoft/cascadia-code/releases/latest/download/Cascadia.ttf"
+  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/Cascadia.ttf"
+  appcast 'https://github.com/microsoft/cascadia-code/releases.atom'
   name 'Cascadia'
   homepage 'https://github.com/microsoft/cascadia-code'
 
