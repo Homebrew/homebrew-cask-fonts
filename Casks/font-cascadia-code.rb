97063151b77b07929d829ec0fcf61c55b195b7bf
Christina Warren <blog@planetchristina.com>
2019-09-19 02:08:04 -0700

    initial commit for Cascadia Code

---
+++ Casks/font-cascadia-code.rb
@@ -0,0 +1,11 @@
+cask 'font-cascadia-code' do
+  version '1909.16'
+  sha256 '16a7f8c997a7732bd6e33b18db7d960c87dfb54afb839273968678ac0b40caa3'
+
+  url 'https://github.com/microsoft/cascadia-code/releases/latest/download/v#{version}/Cascadia.ttf'
+  appcast 'https://github.com/microsoft/cascadia-code/releases.atom'
+  name 'Cascadia Code'
+  homepage 'https://github.com/microsoft/cascadia-code'
+
+  font 'Cascadia.ttf'
+end
