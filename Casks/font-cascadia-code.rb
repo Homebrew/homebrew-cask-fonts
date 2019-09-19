75b5ad8a191212ccfac52cae74d6f4d257e513cd
Christina Warren <blog@planetchristina.com>
2019-09-19 02:32:28 -0700

    Fix checksum

--- Casks/font-cascadia-code.rb
+++ Casks/font-cascadia-code.rb
@@ -1,6 +1,6 @@
 cask 'font-cascadia-code' do
   version '1909.16'
-  sha256 '16a7f8c997a7732bd6e33b18db7d960c87dfb54afb839273968678ac0b40caa3'
+  sha256 :no_check
 
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/Cascadia.ttf"
   appcast 'https://github.com/microsoft/cascadia-code/releases.atom'
