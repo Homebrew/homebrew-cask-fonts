897a6edc8247523785972022e200f207ad0477b3
Licardo <1014660822@qq.com>
2019-10-06 16:20:08 +0800

    Update font-cascadia.rb (#1891)

--- Casks/font-cascadia.rb
+++ Casks/font-cascadia.rb
@@ -1,6 +1,6 @@
 cask 'font-cascadia' do
-  version '1909.16'
-  sha256 'c0485d313fbc40f9076e707ed0aa98c9e3f2649d8b30adff705cebf0a0dc1d93'
+  version '1910.04'
+  sha256 '044f2d1e854f976d0f9a639304d827718aa2b943707873bfff17575dda8f0551'
 
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/Cascadia.ttf"
   appcast 'https://github.com/microsoft/cascadia-code/releases.atom'
