d6a6351601ec6f2042b3b9b0e8bed3654c2f16aa
Asuna <chuxubank@qq.com>
2019-10-25 09:51:23 +0800

    Update font-sarasa-gothic

--- Casks/font-sarasa-gothic.rb
+++ Casks/font-sarasa-gothic.rb
@@ -1,6 +1,6 @@
 cask 'font-sarasa-gothic' do
   version '0.10.0'
-  sha256 '2a8cad733d706b4f1677f91a1d9e97434c00583b662823692358edc90762eabc'
+  sha256 '5f101908073e5f19ef95d5216f4dfd71e7adbb8e206260dbc4b5a4a041adc12c'
 
   url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
   appcast 'https://github.com/be5invis/Sarasa-Gothic/releases.atom'
