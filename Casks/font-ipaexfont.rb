54bcf650d47c71e2f17c8adb9633b9a2b6bfa1e6
Hi-king <hikingko1@gmail.com>
2019-11-09 19:52:54 +0900

    update download url of ipaexfont v004.01

--- Casks/font-ipaexfont.rb
+++ Casks/font-ipaexfont.rb
@@ -2,7 +2,7 @@ cask 'font-ipaexfont' do
   version '004.01'
   sha256 'bcf8374ab3f9672c421120430dd19a51c99f5265cf06fc340d9a661ddfd7974b'
 
-  url "https://oscdl.ipa.go.jp/IPAexfont/IPAexfont#{version.no_dots}.zip"
+  url "https://ipafont.ipa.go.jp/IPAexfont/IPAexfont#{version.no_dots}.zip"
   name 'IPAex Fonts'
   homepage 'https://ipafont.ipa.go.jp/'
 
