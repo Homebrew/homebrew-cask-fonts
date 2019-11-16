e927bee88cde81c9fb04b2ab3d4872482387ee51
Natthan <11040069+ansimita@users.noreply.github.com>
2019-11-16 23:49:16 +0000

    Update font-fantasque-sans-mono to 1.8.0 (#1910)

--- Casks/font-fantasque-sans-mono.rb
+++ Casks/font-fantasque-sans-mono.rb
@@ -1,6 +1,6 @@
 cask 'font-fantasque-sans-mono' do
-  version '1.7.2'
-  sha256 'f3c712d02b3f1f78a2ba1e5be95f1366e75f910b22b7b9242449b2bd43d1f194'
+  version '1.8.0'
+  sha256 '84be689e231ff773ed9d352e83dccd8151d9e445f1cb0b88cb0e9330fc4d9cfc'
 
   url "https://github.com/belluzj/fantasque-sans/releases/download/v#{version}/FantasqueSansMono-Normal.zip"
   appcast 'https://github.com/belluzj/fantasque-sans/releases.atom'
