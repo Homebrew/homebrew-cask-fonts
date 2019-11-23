fa725d59e23f22e371e6c0ba0eab67b43cb59d72
sal0max <msal.coding@gmail.com>
2019-11-23 11:19:20 +0100

    Update font-cascadia from 1910.20 to 1911.21 (#1914)

--- Casks/font-cascadia.rb
+++ Casks/font-cascadia.rb
@@ -1,6 +1,6 @@
 cask 'font-cascadia' do
-  version '1911.20'
-  sha256 '2def0097704c114c07e346a91876e5447e8f930c3ad1606e102a8299541b8842'
+  version '1911.21'
+  sha256 'cf5b69933c568eac4231303a952ce57c1581dac10c6e73c70b763cf9ecaabed4'
 
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/Cascadia.ttf"
   appcast 'https://github.com/microsoft/cascadia-code/releases.atom'
