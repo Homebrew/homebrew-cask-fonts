6977651a9f7b61c36f71d202df838b17fdfd5edb
Yevhenii Kurtov <yevhenii.kurtov@derivco.se>
2019-11-27 11:58:36 +0200

    Update font-hack-nerd-font

--- Casks/font-hack-nerd-font.rb
+++ Casks/font-hack-nerd-font.rb
@@ -1,6 +1,6 @@
 cask 'font-hack-nerd-font' do
   version '2.0.0'
-  sha256 'd1147483fd0310fa6bfce6799f47654e3f435010fdc709877b15a23f090ed41c'
+  sha256 'a2e7d9a02b3196955f28bdaea559f239d98f18766628448097e838d7bdfdacae'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hack.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
