ab456b490035c4a5724b6f174f3b38554eef164c
Vítor Galvão <info@vitorgalvao.com>
2019-12-10 14:19:29 +0000

    font-libre-caslon-display.rb: fix homepage

--- Casks/font-libre-caslon-display.rb
+++ Casks/font-libre-caslon-display.rb
@@ -2,10 +2,9 @@ cask 'font-libre-caslon-display' do
   version :latest
   sha256 :no_check
 
-  # github.com/impallari/Libre-Caslon-Display was verified as official when first introduced to the cask
   url 'https://github.com/impallari/Libre-Caslon-Display/archive/master.zip'
   name 'Libre Caslon Display'
-  homepage 'http://www.impallari.com/projects/overview/libre-caslon-display-and-text'
+  homepage 'https://github.com/impallari/Libre-Caslon-Display'
 
   font 'Libre-Caslon-Display-master/fonts/OTF/LibreCaslonDisplay-Regular.otf'
 end
