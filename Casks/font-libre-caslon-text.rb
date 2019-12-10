0491707c12746824c2943b8034fea02ef2bc9933
Vítor Galvão <info@vitorgalvao.com>
2019-12-10 14:33:41 +0000

    font-libre-caslon-text: fix home

--- Casks/font-libre-caslon-text.rb
+++ Casks/font-libre-caslon-text.rb
@@ -2,10 +2,9 @@ cask 'font-libre-caslon-text' do
   version :latest
   sha256 :no_check
 
-  # github.com/impallari/Libre-Caslon-Text was verified as official when first introduced to the cask
   url 'https://github.com/impallari/Libre-Caslon-Text/archive/master.zip'
   name 'Libre Caslon Text'
-  homepage 'http://www.impallari.com/projects/overview/libre-caslon-display-and-text'
+  homepage 'https://github.com/impallari/Libre-Caslon-Text/'
 
   font 'Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Bold.otf'
   font 'Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Italic.otf'
