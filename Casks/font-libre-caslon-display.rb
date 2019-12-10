251f2f39842b8aa39244c23eb9b8a6bea291e110
Vítor Galvão <info@vitorgalvao.com>
2019-12-10 14:33:15 +0000

    Update font-libre-caslon-display.rb

--- Casks/font-libre-caslon-display.rb
+++ Casks/font-libre-caslon-display.rb
@@ -4,7 +4,7 @@ cask 'font-libre-caslon-display' do
 
   url 'https://github.com/impallari/Libre-Caslon-Display/archive/master.zip'
   name 'Libre Caslon Display'
-  homepage 'https://github.com/impallari/Libre-Caslon-Display'
+  homepage 'https://github.com/impallari/Libre-Caslon-Display/'
 
   font 'Libre-Caslon-Display-master/fonts/OTF/LibreCaslonDisplay-Regular.otf'
 end
