1edad0706b21b673c1f94dd5be2bfab691c8af23
Vítor Galvão <info@vitorgalvao.com>
2019-12-12 13:47:38 +0000

    font-ricty-diminished.rb: fix links

--- Casks/font-ricty-diminished.rb
+++ Casks/font-ricty-diminished.rb
@@ -2,9 +2,10 @@ cask 'font-ricty-diminished' do
   version '4.1.1'
   sha256 '09d6cf8eaf3402205dd8a0fa35469d41833b4e85bedddfac235255d78747766b'
 
-  url "https://www.rs.tus.ac.jp/yyusa/ricty_diminished/ricty_diminished-#{version}.tar.gz"
+  url "http://www.yusa.lab.uec.ac.jp/~yusa/ricty_diminished/ricty_diminished-#{version}.tar.gz"
+  appcast 'http://www.yusa.lab.uec.ac.jp/~yusa/ricty_diminished.html'
   name 'Ricty Diminished'
-  homepage 'https://www.rs.tus.ac.jp/yyusa/ricty_diminished.html'
+  homepage 'http://www.yusa.lab.uec.ac.jp/~yusa/ricty_diminished.html'
 
   font 'RictyDiminished-Bold.ttf'
   font 'RictyDiminished-BoldOblique.ttf'
