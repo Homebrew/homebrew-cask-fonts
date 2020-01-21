fe55223b919df9f425fb9713d525688230b4a2c4
Vítor Galvão <info@vitorgalvao.com>
2020-01-21 14:30:10 +0000

    font-jetbrains-mono.rb: download from Github

--- Casks/font-jetbrains-mono.rb
+++ Casks/font-jetbrains-mono.rb
@@ -1,9 +1,10 @@
 cask 'font-jetbrains-mono' do
-  version '1.0.0'
-  sha256 '82bf0dec956e4ca9afa4da4978fa5de80a75a576b3353d570ccbaabe4e858389'
+  version '1.0.1'
+  sha256 '5c280da4dd1d8adcaddf14cd0d3a60b461d65cface30a0a0d7cb72a7ea1c0cd9'
 
-  url "https://download.jetbrains.com/fonts/JetBrainsMono-#{version}.zip"
-  appcast 'https://www.jetbrains.com/lp/mono'
+  # github.com/JetBrains/JetBrainsMono was verified as official when first introduced to the cask
+  url "https://github.com/JetBrains/JetBrainsMono/releases/download/#{version}/JetBrainsMono-#{version}.zip"
+  appcast 'https://github.com/JetBrains/JetBrainsMono/releases.atom'
   name 'JetBrains Mono'
   homepage 'https://www.jetbrains.com/lp/mono'
 
