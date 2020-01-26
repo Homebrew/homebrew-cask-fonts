969a61f2cd27278cd6c744b4d60a1e8bcaf33c2e
Sebastiano Poggi <rock3r@users.noreply.github.com>
2020-01-26 17:37:30 +0100

    Update JetBrains Mono to 1.0.2 (#1937)
    * Update JetBrains Mono to 1.0.2
    
    * Fix download URL for v1.0.2
    
    * Update font-jetbrains-mono.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/font-jetbrains-mono.rb
+++ Casks/font-jetbrains-mono.rb
@@ -1,19 +1,19 @@
 cask 'font-jetbrains-mono' do
-  version '1.0.1'
-  sha256 '5c280da4dd1d8adcaddf14cd0d3a60b461d65cface30a0a0d7cb72a7ea1c0cd9'
+  version '1.0.2'
+  sha256 '59f9b9762d5625eb438eedf034dbbcdcf09ed18ded994540b466872840229762'
 
   # github.com/JetBrains/JetBrainsMono was verified as official when first introduced to the cask
-  url "https://github.com/JetBrains/JetBrainsMono/releases/download/#{version}/JetBrainsMono-#{version}.zip"
+  url "https://github.com/JetBrains/JetBrainsMono/releases/download/v#{version}/JetBrainsMono-#{version}.zip"
   appcast 'https://github.com/JetBrains/JetBrainsMono/releases.atom'
   name 'JetBrains Mono'
   homepage 'https://www.jetbrains.com/lp/mono'
 
-  font 'ttf/JetBrainsMono-Bold-Italic.ttf'
-  font 'ttf/JetBrainsMono-Bold.ttf'
-  font 'ttf/JetBrainsMono-ExtraBold-Italic.ttf'
-  font 'ttf/JetBrainsMono-ExtraBold.ttf'
-  font 'ttf/JetBrainsMono-Italic.ttf'
-  font 'ttf/JetBrainsMono-Medium-Italic.ttf'
-  font 'ttf/JetBrainsMono-Medium.ttf'
-  font 'ttf/JetBrainsMono-Regular.ttf'
+  font "JetBrainsMono-#{version}/ttf/JetBrainsMono-Bold-Italic.ttf"
+  font "JetBrainsMono-#{version}/ttf/JetBrainsMono-Bold.ttf"
+  font "JetBrainsMono-#{version}/ttf/JetBrainsMono-ExtraBold-Italic.ttf"
+  font "JetBrainsMono-#{version}/ttf/JetBrainsMono-ExtraBold.ttf"
+  font "JetBrainsMono-#{version}/ttf/JetBrainsMono-Italic.ttf"
+  font "JetBrainsMono-#{version}/ttf/JetBrainsMono-Medium-Italic.ttf"
+  font "JetBrainsMono-#{version}/ttf/JetBrainsMono-Medium.ttf"
+  font "JetBrainsMono-#{version}/ttf/JetBrainsMono-Regular.ttf"
 end
