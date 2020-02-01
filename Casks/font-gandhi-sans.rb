65d6483e1560ecb017d16451a991394040f9256f
Caius Durling <dev@caius.name>
2020-02-01 11:42:09 +0000

    Add Gandhi Sans Font (#1941)

---
+++ Casks/font-gandhi-sans.rb
@@ -0,0 +1,13 @@
+cask 'font-gandhi-sans' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.fontsquirrel.com/fonts/download/gandhi-sans'
+  name 'Gandhi Sans'
+  homepage 'https://www.fontsquirrel.com/fonts/gandhi-sans'
+
+  font 'GandhiSans-Regular.otf'
+  font 'GandhiSans-Italic.otf'
+  font 'GandhiSans-Bold.otf'
+  font 'GandhiSans-BoldItalic.otf'
+end
