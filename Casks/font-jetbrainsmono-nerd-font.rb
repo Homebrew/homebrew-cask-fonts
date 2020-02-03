8d498398f83ace761231158adc9dbf8cf7d2d42e
Julien Ma <julienma@users.noreply.github.com>
2020-02-03 09:44:26 +0100

    Update font-jetbrainsmono-nerd-font

--- Casks/font-jetbrainsmono-nerd-font.rb
+++ Casks/font-jetbrainsmono-nerd-font.rb
@@ -1,19 +1,18 @@
 cask 'font-jetbrainsmono-nerd-font' do
-  version :latest
-  sha256 :no_check
+  version '2.1.0'
+  sha256 '842013fa44b6896d4eb91635a81ef75244d78d7f61ff866c9dfd3315a67788cd'
 
-  url 'https://github.com/ryanoasis/nerd-fonts/trunk/patched-fonts/JetBrainsMono',
-      using:      :svn,
-      trust_cert: true
+  url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/JetBrainsMono.zip"
+  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
   name 'JetBrains Mono Nerd Font (JetBrains Mono)'
   homepage 'https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/JetBrainsMono'
 
-  font 'Bold-Italic/complete/JetBrains Mono Bold Italic Nerd Font Complete.ttf'
-  font 'Bold/complete/JetBrains Mono Bold Nerd Font Complete.ttf'
-  font 'ExtraBold-Italic/complete/JetBrains Mono ExtraBold ExBd I Nerd Font Complete.ttf'
-  font 'ExtraBold/complete/JetBrains Mono ExtraBold ExtBd Nerd Font Complete.ttf'
-  font 'Italic/complete/JetBrains Mono Italic Nerd Font Complete.ttf'
-  font 'Medium-Italic/complete/JetBrains Mono Medium Med Ita Nerd Font Complete.ttf'
-  font 'Medium/complete/JetBrains Mono Medium Nerd Font Complete.ttf'
-  font 'Regular/complete/JetBrains Mono Regular Nerd Font Complete.ttf'
+  font 'JetBrains Mono Bold Italic Nerd Font Complete.ttf'
+  font 'JetBrains Mono Bold Nerd Font Complete.ttf'
+  font 'JetBrains Mono ExtraBold ExBd I Nerd Font Complete.ttf'
+  font 'JetBrains Mono ExtraBold ExtBd Nerd Font Complete.ttf'
+  font 'JetBrains Mono Italic Nerd Font Complete.ttf'
+  font 'JetBrains Mono Medium Med Ita Nerd Font Complete.ttf'
+  font 'JetBrains Mono Medium Medium Nerd Font Complete.ttf'
+  font 'JetBrains Mono Regular Nerd Font Complete.ttf'
 end
