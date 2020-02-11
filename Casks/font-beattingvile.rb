5870d014b70950bc17de2578275129ff607bab37
David Gasperoni <mcdado@gmail.com>
2020-02-11 11:55:47 +0100

    Add font-beattingvile v1.0

---
+++ Casks/font-beattingvile.rb
@@ -0,0 +1,11 @@
+cask 'font-beattingvile' do
+  version '1.0'
+  sha256 '0aac26846be14641f46948acd17e3714354699f0f48656de6fca3df1da2bc313'
+
+  # freetypography.com was verified as official when first introduced to the cask
+  url 'https://freetypography.com/freetypo/Beattingvile.zip'
+  name 'Beattingvile'
+  homepage 'https://www.behance.net/rismanginarwan'
+
+  font 'Beattingvile.otf'
+end
