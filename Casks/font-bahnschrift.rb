0fdd565d301fcea98f68b977081283955c7ac05e
tuxpeople <thomas@tuxpeople.org>
2020-02-11 01:36:17 +0100

    Bahnschrift version 1.0 (#1953)
    * Create font-bahnschrift.rb
    
    * fixes

---
+++ Casks/font-bahnschrift.rb
@@ -0,0 +1,24 @@
+cask 'font-bahnschrift' do
+  version '1.00'
+  sha256 'feca741ad5c3a8e27aacc25a083c3e0746d8f00473f329a8c83f3600bdc2bf7e'
+
+  url 'https://www.cufonfonts.com/download/font/bahnschrift'
+  name 'Bahnschrift'
+  homepage 'https://www.cufonfonts.com/font/bahnschrift'
+
+  font 'BAHNSCHRIFT 1.TTF'
+  font 'BAHNSCHRIFT 10.TTF'
+  font 'BAHNSCHRIFT 11.TTF'
+  font 'BAHNSCHRIFT 12.TTF'
+  font 'BAHNSCHRIFT 13.TTF'
+  font 'BAHNSCHRIFT 14.TTF'
+  font 'BAHNSCHRIFT 2.TTF'
+  font 'BAHNSCHRIFT 3.TTF'
+  font 'BAHNSCHRIFT 4.TTF'
+  font 'BAHNSCHRIFT 5.TTF'
+  font 'BAHNSCHRIFT 6.TTF'
+  font 'BAHNSCHRIFT 7.TTF'
+  font 'BAHNSCHRIFT 8.TTF'
+  font 'BAHNSCHRIFT 9.TTF'
+  font 'BAHNSCHRIFT.TTF'
+end
