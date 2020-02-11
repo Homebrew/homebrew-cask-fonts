6561335052292d2e9314f77ca6add9b4b74fdc71
Quirico Tursi <quirico.tursi@gmail.com>
2020-02-11 18:56:41 +0100

    Update font-metropolis to v11 (#1956)
    * Update font-metropolis to v11
    
    Corrected extract path
    
    * Update font-metropolis to v11
    
    Corrected extract path.

--- Casks/font-metropolis.rb
+++ Casks/font-metropolis.rb
@@ -1,28 +1,28 @@
 cask 'font-metropolis' do
-  version '9'
-  sha256 'ddc8cd6b0f24b7f1cefb7d2819a1ddf7340c4868eba7df17b5fe4123081169ad'
+  version '11'
+  sha256 'a9efb018df3fae5d39fcd3d05418bce35c4f88ecbf9c3eb54ffff7dbfdf79466'
 
   url "https://github.com/chrismsimpson/Metropolis/archive/r#{version}.zip"
   appcast 'https://github.com/chrismsimpson/Metropolis/releases.atom'
   name 'Metropolis'
   homepage 'https://github.com/chrismsimpson/Metropolis'
 
-  font "Metropolis-r#{version}/Metropolis-Black.otf"
-  font "Metropolis-r#{version}/Metropolis-BlackItalic.otf"
-  font "Metropolis-r#{version}/Metropolis-Bold.otf"
-  font "Metropolis-r#{version}/Metropolis-BoldItalic.otf"
-  font "Metropolis-r#{version}/Metropolis-ExtraBold.otf"
-  font "Metropolis-r#{version}/Metropolis-ExtraBoldItalic.otf"
-  font "Metropolis-r#{version}/Metropolis-ExtraLight.otf"
-  font "Metropolis-r#{version}/Metropolis-ExtraLightItalic.otf"
-  font "Metropolis-r#{version}/Metropolis-Light.otf"
-  font "Metropolis-r#{version}/Metropolis-LightItalic.otf"
-  font "Metropolis-r#{version}/Metropolis-Medium.otf"
-  font "Metropolis-r#{version}/Metropolis-MediumItalic.otf"
-  font "Metropolis-r#{version}/Metropolis-Regular.otf"
-  font "Metropolis-r#{version}/Metropolis-RegularItalic.otf"
-  font "Metropolis-r#{version}/Metropolis-SemiBold.otf"
-  font "Metropolis-r#{version}/Metropolis-SemiBoldItalic.otf"
-  font "Metropolis-r#{version}/Metropolis-Thin.otf"
-  font "Metropolis-r#{version}/Metropolis-ThinItalic.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-Black.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-BlackItalic.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-Bold.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-BoldItalic.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-ExtraBold.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-ExtraBoldItalic.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-ExtraLight.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-ExtraLightItalic.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-Light.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-LightItalic.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-Medium.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-MediumItalic.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-Regular.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-RegularItalic.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-SemiBold.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-SemiBoldItalic.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-Thin.otf"
+  font "Metropolis-r#{version}/Fonts/OpenType/Metropolis-ThinItalic.otf"
 end
