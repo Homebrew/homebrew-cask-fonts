754a846bdf88e6fd1e676935fc76a3cb0f154ce0
Vítor Galvão <info@vitorgalvao.com>
2020-02-24 03:35:06 +0000

    Update font-routed-gothic.rb

--- Casks/font-routed-gothic.rb
+++ Casks/font-routed-gothic.rb
@@ -3,6 +3,7 @@ cask 'font-routed-gothic' do
   sha256 '3ecffba109bf2619057815411253e5c6f47f82fedba38ade6c951e13a8b2ad5e'
 
   url "https://webonastick.com/fonts/routed-gothic/download/routed-gothic-ttf-v#{version}.zip"
+  appcast 'https://github.com/dse/routed-gothic/releases.atom'
   name 'Routed Gothic'
   homepage 'https://webonastick.com/fonts/routed-gothic/'
 
