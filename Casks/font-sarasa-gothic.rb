34ecdf9be7d6c3d1744ac4aecd391a8529cb6a2b
Rui Ying <yingrui205@gmail.com>
2020-03-10 14:47:16 +0800

    Update font-sarasa-gothic from 0.10.2 to 0.11.0 (#1975)

--- Casks/font-sarasa-gothic.rb
+++ Casks/font-sarasa-gothic.rb
@@ -1,6 +1,6 @@
 cask 'font-sarasa-gothic' do
-  version '0.10.2'
-  sha256 '888f998869840e982c5478d9aa40494cf83e9aaa85de3c0a8834537f64092d16'
+  version '0.11.0'
+  sha256 'f6c1a43ee6b9a4305a8d5c2cb24fc1aa7ab54abf3ffc6c4eca6f3d9f673fc9c9'
 
   url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
   appcast 'https://github.com/be5invis/Sarasa-Gothic/releases.atom'
