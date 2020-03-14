1fe5be56a362355775077336528eb0123aee2548
Kang-min Liu <gugod@gugod.org>
2020-03-14 14:19:00 +0800

    Add font-open-huninn 1.0

---
+++ Casks/font-open-huninn.rb
@@ -0,0 +1,11 @@
+cask 'font-open-huninn' do
+  version '1.0'
+  sha256 '20b18c056608ec078d22f1677b52fd1b616def1512f17be5d37831f09f517d5d'
+
+  # justfont.com/huninn was verified as the official when first introduced to the cask
+  url 'https://raw.githubusercontent.com/justfont/open-huninn-font/master/font/jf-openhuninn-1.0.ttf'
+  name 'jf-openhuninn-1.0'
+  homepage 'https://justfont.com/huninn/'
+
+  font 'jf-openhuninn-1.0.ttf'
+end
