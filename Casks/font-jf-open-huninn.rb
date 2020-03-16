2af9da93ddbd47db94ea21402431ebe9a80135c1
Kang-min Liu <gugod@gugod.org>
2020-03-16 11:57:03 +0800

    add the updated sha256 sum.
    deprectaes ecdaffdbf47f0844582c0d3f762260ab8c1a3207

--- Casks/font-jf-open-huninn.rb
+++ Casks/font-jf-open-huninn.rb
@@ -1,6 +1,6 @@
 cask 'font-jf-open-huninn' do
   version '1.0'
-  sha256 :no_check
+  sha256 'a74427f19405b25d95135b673aff81565e833b7f3165c43f283a010ff736ba07'
 
   # codeload.github.com/justfont/open-huninn-font was verified as official when first introduced to the cask
   url 'https://codeload.github.com/justfont/open-huninn-font/zip/master'
