a9875fa4bf106edf2870bbbe5db561359f711ee5
Kang-min Liu <gugod@gugod.org>
2020-03-16 10:53:41 +0800

    Various fixes to make the definition fit the convention.

--- Casks/font-jf-open-huninn.rb
+++ Casks/font-jf-open-huninn.rb
@@ -1,11 +1,11 @@
-cask 'font-open-huninn' do
+cask 'font-jf-open-huninn' do
   version '1.0'
   sha256 '8b0d0e8fd396a763371ae21bcf7140926e7837d5851d108108b858de42c9b5e2'
 
   # codeload.github.com/justfont/open-huninn-font was verified as official when first introduced to the cask
   url 'https://codeload.github.com/justfont/open-huninn-font/zip/master'
-  name 'jf-openhuninn-1.0'
+  name 'jf open huninn'
   homepage 'https://justfont.com/huninn/'
 
-  font 'open-huninn-font-master/font/jf-openhuninn-1.0.ttf'
+  font "open-huninn-font-master/font/jf-openhuninn-#{version}.ttf"
 end
