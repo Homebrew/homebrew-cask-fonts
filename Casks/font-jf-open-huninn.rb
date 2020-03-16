74eb5d82a707e61a6f8f428e39187f6476d9e6e2
Kang-min Liu <gugod@gugod.org>
2020-03-16 11:52:23 +0800

    add the name of the font like the output from fc-query.
    also suggested by @vitorgalvao

--- Casks/font-jf-open-huninn.rb
+++ Casks/font-jf-open-huninn.rb
@@ -5,6 +5,7 @@ cask 'font-jf-open-huninn' do
   # codeload.github.com/justfont/open-huninn-font was verified as official when first introduced to the cask
   url 'https://codeload.github.com/justfont/open-huninn-font/zip/master'
   name 'jf open huninn'
+  name 'jf open 粉圓'
   homepage 'https://justfont.com/huninn/'
 
   font "open-huninn-font-master/font/jf-openhuninn-#{version}.ttf"
