a1d86c31ff7f3d9db05ba258a91813f58f4e9c47
Kévin Huguenin <kevin.huguenin@unil.ch>
2020-03-26 00:15:59 +0100

    Updated font awesome to 5.13.0

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,6 +1,6 @@
 cask 'font-fontawesome' do
-  version '5.12.1'
-  sha256 '160a3e8f4c910cfe653b1d7804342233c2f5c97cbe8aff1d8fd35c1d5717a852'
+  version '5.13.0'
+  sha256 '5d54c86956e7b118f14c03d080882fd76c17cbb577d53b74237af22114658da8'
 
   # github.com/FortAwesome/Font-Awesome was verified as official when first introduced to the cask
   url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip"
