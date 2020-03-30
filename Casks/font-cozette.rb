54a3f585aafa8d308827a510a10214efc45258fe
Gadzhi Kharkharov <1460122+kkga@users.noreply.github.com>
2020-03-30 18:43:06 +0300

    Add Cozette v1.7.0 (#1990)
    * update Sudo to 0.40
    
    * add font-cozette
    
    * add appcast
    
    * Update font-cozette.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/font-cozette.rb
@@ -0,0 +1,11 @@
+cask 'font-cozette' do
+  version '1.7.0'
+  sha256 '325208db6555726a07a0217ef409fb20efd2d765b83bb0e4273a6d06494a69be'
+
+  url "https://github.com/slavfox/Cozette/releases/download/v.#{version}/CozetteVector.dfont"
+  appcast 'https://github.com/slavfox/Cozette/releases.atom'
+  name 'Cozette'
+  homepage 'https://github.com/slavfox/Cozette'
+
+  font 'CozetteVector.dfont'
+end
