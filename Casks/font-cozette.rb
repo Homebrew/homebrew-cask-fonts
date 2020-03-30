9aa0c00d68b0433df249e7ac0f89d37d8ec50fe8
Vítor Galvão <info@vitorgalvao.com>
2020-03-30 16:37:17 +0100

    Update font-cozette.rb

--- Casks/font-cozette.rb
+++ Casks/font-cozette.rb
@@ -2,7 +2,7 @@ cask 'font-cozette' do
   version '1.7.0'
   sha256 '325208db6555726a07a0217ef409fb20efd2d765b83bb0e4273a6d06494a69be'
 
-  url 'https://github.com/slavfox/Cozette/releases/download/v.1.7.0/CozetteVector.dfont'
+  url "https://github.com/slavfox/Cozette/releases/download/v.#{version}/CozetteVector.dfont"
   appcast 'https://github.com/slavfox/Cozette/releases.atom'
   name 'Cozette'
   homepage 'https://github.com/slavfox/Cozette'
