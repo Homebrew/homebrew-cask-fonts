54deaf63e0cebef1887220803efa241f98b923ce
Gadzhi Kharkharov <me@kkga.me>
2020-03-30 11:20:30 +0300

    add appcast

--- Casks/font-cozette.rb
+++ Casks/font-cozette.rb
@@ -3,6 +3,7 @@ cask 'font-cozette' do
   sha256 '325208db6555726a07a0217ef409fb20efd2d765b83bb0e4273a6d06494a69be'
 
   url 'https://github.com/slavfox/Cozette/releases/download/v.1.7.0/CozetteVector.dfont'
+  appcast 'https://github.com/slavfox/Cozette/releases.atom'
   name 'Cozette'
   homepage 'https://github.com/slavfox/Cozette'
 
