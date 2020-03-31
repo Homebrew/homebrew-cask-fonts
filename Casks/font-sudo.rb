deb93f319ffd73921f5c23eecaf7a1c2ef68eb89
Gadzhi Kharkharov <me@kkga.me>
2020-03-31 19:23:24 +0300

    Update font-sudo from 0.41 to 0.42

--- Casks/font-sudo.rb
+++ Casks/font-sudo.rb
@@ -1,6 +1,6 @@
 cask 'font-sudo' do
-  version '0.41'
-  sha256 'ca1c89691a5ee96cbd0c4b0df911109b3230177efb9f1c6d6763014ed0ffdcaf'
+  version '0.42'
+  sha256 'ae7979cce3f69160293416089b91fe9f5f20bd25a288099ba61f067930df3d64'
 
   url "https://github.com/jenskutilek/sudo-font/releases/download/v#{version}/sudo.zip"
   appcast 'https://github.com/jenskutilek/sudo-font/releases.atom'
