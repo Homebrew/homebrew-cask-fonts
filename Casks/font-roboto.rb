c2273f186d03575e4cb1266b687361a53451a23b
Jose Salcido <jose@otfusion.org>
2020-04-01 16:27:15 -0700

    remove using :git and keep it simple for font-roboto

--- Casks/font-roboto.rb
+++ Casks/font-roboto.rb
@@ -3,9 +3,7 @@ cask 'font-roboto' do
   sha256 :no_check
 
   # github.com/google/fonts was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts.git',
-      using:      :git,
-      trust_cert: true
+  url 'https://github.com/google/fonts.git'
   name 'Roboto'
   homepage 'https://www.google.com/fonts/specimen/Roboto'
 
