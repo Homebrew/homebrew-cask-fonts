c4fa6984fb400052ae7bc078887e148cad1111f7
Ricardo Bánffy <ricardo.banffy@workday.com>
2020-04-13 23:18:19 +0100

    Update font-3270 to version 2.0.4

--- Casks/font-3270.rb
+++ Casks/font-3270.rb
@@ -1,13 +1,16 @@
 cask 'font-3270' do
-  version '1.2.23,d250fd9'
-  sha256 '9cf7235088b3afe7827b24fec534a569b5cab15c26e0b5b6956cc2af0db837b6'
+  version '2.0.4,ece94f6'
+  sha256 'd5755c4774eb5ab81b8284d53021930ec55b191ab977f14d27b25c6f33358963'
 
   url "https://github.com/rbanffy/3270font/releases/download/v#{version.before_comma}/3270_fonts_#{version.after_comma}.zip"
   appcast 'https://github.com/rbanffy/3270font/releases.atom'
   name 'IBM 3270'
   homepage 'https://github.com/rbanffy/3270font'
 
-  font '3270Medium.otf'
-  font '3270Narrow.otf'
-  font '3270SemiNarrow.otf'
+  font '3270-Regular.otf'
+  font '3270-Regular.ttf'
+  font '3270Condensed-Regular.otf'
+  font '3270Condensed-Regular.ttf'
+  font '3270SemiCondensed-Regular.otf'
+  font '3270SemiCondensed-Regular.ttf'
 end
