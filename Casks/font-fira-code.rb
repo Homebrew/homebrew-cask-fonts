95a5dda88e2607dc50eb127df15f69e081f713fa
Hanlin He <hanlhe@tetrationanalytics.com>
2020-04-15 10:44:27 -0700

    Update font-fira-code from 3 to 3.1

--- Casks/font-fira-code.rb
+++ Casks/font-fira-code.rb
@@ -1,6 +1,6 @@
 cask 'font-fira-code' do
-  version '3'
-  sha256 '043fa00e002704aec6f8b5e1908faf099f9477160d00ad0260a8103329c3981b'
+  version '3.1'
+  sha256 'cbcabd2c412ee4c3fec4688be8387de18a33bb77bc5c5988e9fd9293a0b9dbb7'
 
   url "https://github.com/tonsky/FiraCode/releases/download/#{version}/FiraCode_#{version}.zip"
   appcast 'https://github.com/tonsky/FiraCode/releases.atom'
