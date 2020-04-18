c4e82f2f76c2ff6bd1e8f0b141401f364af15ca9
Matt Fine <17680862+chewrocca@users.noreply.github.com>
2020-04-18 09:05:10 -0500

    Update font-hack-nerd-font

--- Casks/font-hack-nerd-font.rb
+++ Casks/font-hack-nerd-font.rb
@@ -1,6 +1,6 @@
 cask 'font-hack-nerd-font' do
   version '2.1.0'
-  sha256 '70852e59fcffbe31d401f615625bcb9ebb6af72732c2f1fe9b9d5370c2565514'
+  sha256 'fe374c963491592924667a90641740ecc235d3b0dd435307223148bcc3bb7da6'
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hack.zip"
   appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
