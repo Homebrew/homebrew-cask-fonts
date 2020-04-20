7c05862e947680b00c2f28e3d528be45f87fa3ee
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:25 +0100

    font-kranky.rb: Add trailing slash to verified comment

--- Casks/font-kranky.rb
+++ Casks/font-kranky.rb
@@ -2,7 +2,7 @@ cask 'font-kranky' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/apache/kranky/Kranky-Regular.ttf'
   name 'Kranky'
   homepage 'https://www.google.com/fonts/specimen/Kranky'
