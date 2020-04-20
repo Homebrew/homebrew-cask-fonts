201ecb32ec5a245c840b3a33181eecd742c97e6d
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:31:46 +0100

    font-noto-serif-myanmar.rb: Add trailing slash to verified comment

--- Casks/font-noto-serif-myanmar.rb
+++ Casks/font-noto-serif-myanmar.rb
@@ -2,7 +2,7 @@ cask 'font-noto-serif-myanmar' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifMyanmar-unhinted.zip'
   name 'Noto Serif Myanmar'
   homepage 'https://www.google.com/get/noto/#serif-mymr'
