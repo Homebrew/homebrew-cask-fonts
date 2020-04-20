d277448155096bc158d47fc11548a53606a0701c
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:14 +0100

    font-open-iconic.rb: Add trailing slash to verified comment

--- Casks/font-open-iconic.rb
+++ Casks/font-open-iconic.rb
@@ -2,7 +2,7 @@ cask 'font-open-iconic' do
   version '1.1.1'
   sha256 '8acf49f08ae5a069935b48e6be20349c4e9f43fcfc773ea0aba5b972b5b3743c'
 
-  # codeload.github.com/iconic/open-iconic was verified as official when first introduced to the cask
+  # codeload.github.com/iconic/open-iconic/ was verified as official when first introduced to the cask
   url "https://codeload.github.com/iconic/open-iconic/zip/#{version}"
   appcast 'https://github.com/iconic/open-iconic/releases.atom'
   name 'Open Iconic'
