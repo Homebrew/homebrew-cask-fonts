6790d6b12f85dba03875aa816c20aeb12dead931
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:24:49 +0100

    font-dawning-of-a-new-day.rb: Add trailing slash to verified comment

--- Casks/font-dawning-of-a-new-day.rb
+++ Casks/font-dawning-of-a-new-day.rb
@@ -2,7 +2,7 @@ cask 'font-dawning-of-a-new-day' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/dawningofanewday/DawningofaNewDay.ttf'
   name 'Dawning of a New Day'
   homepage 'https://www.google.com/fonts/specimen/Dawning+of+a+New+Day'
