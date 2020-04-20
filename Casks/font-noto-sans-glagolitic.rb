c4c68775ccf803e3234eb61dec3cd2936f3aff37
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:22:11 +0100

    font-noto-sans-glagolitic.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-glagolitic.rb
+++ Casks/font-noto-sans-glagolitic.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-glagolitic' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGlagolitic-unhinted.zip'
   name 'Noto Sans Glagolitic'
   homepage 'https://www.google.com/get/noto/#sans-glag'
