9f53f14b5f2a6c5fc48607251d41a4801532e966
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:32:53 +0100

    font-kristi.rb: Add trailing slash to verified comment

--- Casks/font-kristi.rb
+++ Casks/font-kristi.rb
@@ -2,7 +2,7 @@ cask 'font-kristi' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/kristi/Kristi-Regular.ttf'
   name 'Kristi'
   homepage 'https://www.google.com/fonts/specimen/Kristi'
