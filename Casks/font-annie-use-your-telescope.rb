33369d389f75111883f8bbb2b6acd822f325eba2
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:41:41 +0100

    font-annie-use-your-telescope.rb: Add trailing slash to verified comment

--- Casks/font-annie-use-your-telescope.rb
+++ Casks/font-annie-use-your-telescope.rb
@@ -2,7 +2,7 @@ cask 'font-annie-use-your-telescope' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/annieuseyourtelescope/AnnieUseYourTelescope-Regular.ttf'
   name 'Annie Use Your Telescope'
   homepage 'https://www.google.com/fonts/specimen/Annie+Use+Your+Telescope'
