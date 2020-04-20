b08cb41097959ff4507c753698f2342b48517376
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:36 +0100

    font-miao-unicode.rb: Add trailing slash to verified comment

--- Casks/font-miao-unicode.rb
+++ Casks/font-miao-unicode.rb
@@ -2,7 +2,7 @@ cask 'font-miao-unicode' do
   version :latest
   sha256 :no_check
 
-  # github.com/phjamr/MiaoUnicode was verified as official when first introduced to the cask
+  # github.com/phjamr/MiaoUnicode/ was verified as official when first introduced to the cask
   url 'https://github.com/phjamr/MiaoUnicode/blob/master/MiaoUnicode-Regular.ttf?raw=true'
   name 'MiaoUnicode'
   homepage 'https://phjamr.github.io/miao.html'
