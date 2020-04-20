0f7279b9169e1c1cbcf3d753f4146d565e8308a8
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:19:19 +0100

    font-delius-swash-caps.rb: Add trailing slash to verified comment

--- Casks/font-delius-swash-caps.rb
+++ Casks/font-delius-swash-caps.rb
@@ -2,7 +2,7 @@ cask 'font-delius-swash-caps' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/deliusswashcaps/DeliusSwashCaps-Regular.ttf'
   name 'Delius Swash Caps'
   homepage 'https://www.google.com/fonts/specimen/Delius+Swash+Caps'
