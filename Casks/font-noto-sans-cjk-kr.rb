6d5a0cb79a61278914e5c3b8ca55a8faa3893df1
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:35:33 +0100

    font-noto-sans-cjk-kr.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-cjk-kr.rb
+++ Casks/font-noto-sans-cjk-kr.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-cjk-kr' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKkr-hinted.zip'
   name 'Noto Sans CJK KR'
   homepage 'https://www.google.com/get/noto/#sans-kore'
