a60634e8b8da045a6521e5218d2ab3dc1c742860
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:42:30 +0100

    font-graduate.rb: Add trailing slash to verified comment

--- Casks/font-graduate.rb
+++ Casks/font-graduate.rb
@@ -2,7 +2,7 @@ cask 'font-graduate' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/graduate/Graduate-Regular.ttf'
   name 'Graduate'
   homepage 'https://www.google.com/fonts/specimen/Graduate'
