081ad37862eaee5fac03fa1d04d3d36961d82f66
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:36:01 +0100

    font-aclonica.rb: Add trailing slash to verified comment

--- Casks/font-aclonica.rb
+++ Casks/font-aclonica.rb
@@ -2,7 +2,7 @@ cask 'font-aclonica' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/apache/aclonica/Aclonica-Regular.ttf'
   name 'Aclonica'
   homepage 'https://www.google.com/fonts/specimen/Aclonica'
