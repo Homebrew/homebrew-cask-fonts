c1619900418a4b1091c327fe129efaf7dab57737
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:11 +0100

    font-balsamiq-sans.rb: Add trailing slash to verified comment

--- Casks/font-balsamiq-sans.rb
+++ Casks/font-balsamiq-sans.rb
@@ -2,7 +2,7 @@ cask 'font-balsamiq-sans' do
   version :latest
   sha256 :no_check
 
-  # github.com/balsamiq/balsamiqsans was verified as official when first introduced to the cask
+  # github.com/balsamiq/balsamiqsans/ was verified as official when first introduced to the cask
   url 'https://github.com/balsamiq/balsamiqsans/archive/master.zip'
   name 'Balsamiq Sans'
   homepage 'https://balsamiq.com/givingback/opensource/font/'
