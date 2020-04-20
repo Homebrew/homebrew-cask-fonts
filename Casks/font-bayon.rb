89f2ca86140f67ab7790ad2ebf97f3149f648a57
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:35:41 +0100

    font-bayon.rb: Add trailing slash to verified comment

--- Casks/font-bayon.rb
+++ Casks/font-bayon.rb
@@ -2,7 +2,7 @@ cask 'font-bayon' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/bayon/Bayon-Regular.ttf'
   name 'Bayon'
   homepage 'https://www.google.com/fonts/specimen/Bayon'
