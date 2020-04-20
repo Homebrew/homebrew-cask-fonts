ba43f5d031b06f13d2638f6a3ee03993dbebbf2c
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:28:46 +0100

    font-monda.rb: Add trailing slash to verified comment

--- Casks/font-monda.rb
+++ Casks/font-monda.rb
@@ -2,7 +2,7 @@ cask 'font-monda' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/monda',
       using:      :svn,
       revision:   '50',
