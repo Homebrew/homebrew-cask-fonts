69ef441ab2aff8ed033efb619dc5b4c5d507a755
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:20:36 +0100

    font-michroma.rb: Add trailing slash to verified comment

--- Casks/font-michroma.rb
+++ Casks/font-michroma.rb
@@ -2,7 +2,7 @@ cask 'font-michroma' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/michroma/Michroma.ttf'
   name 'Michroma'
   homepage 'https://www.google.com/fonts/specimen/Michroma'
