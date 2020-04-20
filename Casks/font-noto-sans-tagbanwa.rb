f307682cbc0e40c53cf972c1d24d5ef8244032df
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:37:42 +0100

    font-noto-sans-tagbanwa.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-tagbanwa.rb
+++ Casks/font-noto-sans-tagbanwa.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-tagbanwa' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagbanwa-unhinted.zip'
   name 'Noto Sans Tagbanwa'
   homepage 'https://www.google.com/get/noto/#sans-tagb'
