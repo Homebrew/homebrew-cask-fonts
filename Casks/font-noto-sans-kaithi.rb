ad5a65ae0d10cf0ef30635d2517317fc7c49a62f
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:26:24 +0100

    font-noto-sans-kaithi.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-kaithi.rb
+++ Casks/font-noto-sans-kaithi.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-kaithi' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKaithi-unhinted.zip'
   name 'Noto Sans Kaithi'
   homepage 'https://www.google.com/get/noto/#sans-kthi'
