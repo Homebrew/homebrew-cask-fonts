780e0af99dc28b6402f59fe01538493815c68451
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:15:48 +0100

    font-nova-oval.rb: Add trailing slash to verified comment

--- Casks/font-nova-oval.rb
+++ Casks/font-nova-oval.rb
@@ -2,7 +2,7 @@ cask 'font-nova-oval' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/novaoval/NovaOval.ttf'
   name 'Nova Oval'
   homepage 'https://www.google.com/fonts/specimen/Nova%20Oval'
