9c817984a6de889c58b04543b16904667047502b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:24:09 +0100

    font-noto-serif-armenian.rb: Add trailing slash to verified comment

--- Casks/font-noto-serif-armenian.rb
+++ Casks/font-noto-serif-armenian.rb
@@ -2,7 +2,7 @@ cask 'font-noto-serif-armenian' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifArmenian-unhinted.zip'
   name 'Noto Serif Armenian'
   homepage 'https://www.google.com/get/noto/#serif-armn'
