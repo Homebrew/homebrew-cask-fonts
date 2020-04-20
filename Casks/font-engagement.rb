9bf01e447df188fc4201613b0a46b1a678cbba69
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:25:36 +0100

    font-engagement.rb: Add trailing slash to verified comment

--- Casks/font-engagement.rb
+++ Casks/font-engagement.rb
@@ -2,7 +2,7 @@ cask 'font-engagement' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/engagement/Engagement-Regular.ttf'
   name 'Engagement'
   homepage 'https://www.google.com/fonts/specimen/Engagement'
