1c34f8100905eb5daa2e93d3f3f5773dcbe58a42
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:30:26 +0100

    font-bungee.rb: Add trailing slash to verified comment

--- Casks/font-bungee.rb
+++ Casks/font-bungee.rb
@@ -2,7 +2,7 @@ cask 'font-bungee' do
   version '1.1.0'
   sha256 'd012a9e6293201c3165feba64342d29c42bb4e67b1cc66e07509c12bab760a6f'
 
-  # github.com/djrrb/bungee was verified as official when first introduced to the cask
+  # github.com/djrrb/bungee/ was verified as official when first introduced to the cask
   url "https://github.com/djrrb/bungee/releases/download/#{version}/Bungee-fonts.zip"
   appcast 'https://github.com/djrrb/bungee/releases.atom'
   name 'Bungee'
