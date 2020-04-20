28e402bd491292c03d56a66f56d38343cab5ecba
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:38:41 +0100

    font-noto-serif-hebrew.rb: Add trailing slash to verified comment

--- Casks/font-noto-serif-hebrew.rb
+++ Casks/font-noto-serif-hebrew.rb
@@ -2,7 +2,7 @@ cask 'font-noto-serif-hebrew' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifHebrew-unhinted.zip'
   name 'Noto Serif Hebrew'
   homepage 'https://www.google.com/get/noto/#serif-hebr'
