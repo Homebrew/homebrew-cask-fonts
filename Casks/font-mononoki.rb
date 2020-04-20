2f9158ab5ac0ef36b2f237da1e7027525a7c40da
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:43:13 +0100

    font-mononoki.rb: Add trailing slash to verified comment

--- Casks/font-mononoki.rb
+++ Casks/font-mononoki.rb
@@ -2,7 +2,7 @@ cask 'font-mononoki' do
   version '1.2'
   sha256 'cf29ea7663dbfe94feaedaf5c0be4f4ae6a5b98c1293e0e772e69c28ac5dc658'
 
-  # github.com/madmalik/mononoki was verified as official when first introduced to the cask
+  # github.com/madmalik/mononoki/ was verified as official when first introduced to the cask
   url "https://github.com/madmalik/mononoki/releases/download/#{version}/mononoki.zip"
   appcast 'https://github.com/madmalik/mononoki/releases.atom'
   name 'Mononoki'
