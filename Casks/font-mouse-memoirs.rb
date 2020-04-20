3472d9d24b8d78d0a8dbf14071544062e73410fc
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:22:47 +0100

    font-mouse-memoirs.rb: Add trailing slash to verified comment

--- Casks/font-mouse-memoirs.rb
+++ Casks/font-mouse-memoirs.rb
@@ -2,7 +2,7 @@ cask 'font-mouse-memoirs' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/mousememoirs/MouseMemoirs-Regular.ttf'
   name 'Mouse Memoirs'
   homepage 'https://www.google.com/fonts/specimen/Mouse+Memoirs'
