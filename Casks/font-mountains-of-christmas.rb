e85bde5a0f98763728ab4f2204838760560fbfb0
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:41:20 +0100

    font-mountains-of-christmas.rb: Add trailing slash to verified comment

--- Casks/font-mountains-of-christmas.rb
+++ Casks/font-mountains-of-christmas.rb
@@ -2,7 +2,7 @@ cask 'font-mountains-of-christmas' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/apache/mountainsofchristmas',
       using:      :svn,
       revision:   '50',
