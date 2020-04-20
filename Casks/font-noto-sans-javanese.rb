40fcb6c4a15b4c9f0e4320c89f3acc8f444ae1cc
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:25:43 +0100

    font-noto-sans-javanese.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-javanese.rb
+++ Casks/font-noto-sans-javanese.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-javanese' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansJavanese-unhinted.zip'
   name 'Noto Sans Javanese'
   homepage 'https://www.google.com/get/noto/#sans-java'
