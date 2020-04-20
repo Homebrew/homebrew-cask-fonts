1d8be0b835e2caa15dbda7872eecfb7415f69c64
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:26:08 +0100

    font-federant.rb: Add trailing slash to verified comment

--- Casks/font-federant.rb
+++ Casks/font-federant.rb
@@ -2,7 +2,7 @@ cask 'font-federant' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/federant/Federant-Regular.ttf'
   name 'Federant'
   homepage 'https://www.google.com/fonts/specimen/Federant'
