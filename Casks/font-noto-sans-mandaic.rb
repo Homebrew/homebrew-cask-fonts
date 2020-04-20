4889d9f0370e607375599afd1a884afc943207e3
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:30:23 +0100

    font-noto-sans-mandaic.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-mandaic.rb
+++ Casks/font-noto-sans-mandaic.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-mandaic' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMandaic-unhinted.zip'
   name 'Noto Sans Mandaic'
   homepage 'https://www.google.com/get/noto/#sans-mand'
