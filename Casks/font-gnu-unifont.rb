60573b355d5d8025e96651e9eab14730f3001e19
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:23:11 +0100

    font-gnu-unifont.rb: Add trailing slash to verified comment

--- Casks/font-gnu-unifont.rb
+++ Casks/font-gnu-unifont.rb
@@ -2,7 +2,7 @@ cask 'font-gnu-unifont' do
   version '13.0.01'
   sha256 '8b486450b2bd1dc9357ceca096faf493d13f7e582f29984e46552ca8faa87feb'
 
-  # ftpmirror.gnu.org/unifont was verified as official when first introduced to the cask
+  # ftpmirror.gnu.org/unifont/ was verified as official when first introduced to the cask
   url "https://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.tar.gz"
   name 'GNU Unifont'
   homepage 'http://unifoundry.com/unifont.html'
