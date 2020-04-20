ef0b42fad501d45345512ed721d49dde165d82b6
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:37:33 +0100

    font-noto-sans-imperial-aramaic.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-imperial-aramaic.rb
+++ Casks/font-noto-sans-imperial-aramaic.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-imperial-aramaic' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansImperialAramaic-unhinted.zip'
   name 'Noto Sans Imperial Aramaic'
   homepage 'https://www.google.com/get/noto/#sans-armi'
