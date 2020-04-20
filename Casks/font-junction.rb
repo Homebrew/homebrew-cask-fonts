a26e7f71051b64a93ac447fffff6939b20f1cc37
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:35:44 +0100

    font-junction.rb: Add trailing slash to verified comment

--- Casks/font-junction.rb
+++ Casks/font-junction.rb
@@ -2,7 +2,7 @@ cask 'font-junction' do
   version :latest
   sha256 :no_check
 
-  # github.com/theleagueof/junction was verified as official when first introduced to the cask
+  # github.com/theleagueof/junction/ was verified as official when first introduced to the cask
   url 'https://github.com/theleagueof/junction/archive/master.zip'
   name 'Junction'
   homepage 'https://www.theleagueofmoveabletype.com/junction'
