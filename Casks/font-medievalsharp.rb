a1e7cc10f4d3ad5355a3a2085b426e9d255ae212
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:44:16 +0100

    font-medievalsharp.rb: Add trailing slash to verified comment

--- Casks/font-medievalsharp.rb
+++ Casks/font-medievalsharp.rb
@@ -2,7 +2,7 @@ cask 'font-medievalsharp' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/medievalsharp/MedievalSharp.ttf'
   name 'MedievalSharp'
   homepage 'https://www.google.com/fonts/specimen/MedievalSharp'
