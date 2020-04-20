85f9f5be84fc1c8cb7e1481a54e4e475f8e9851c
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:16:20 +0100

    font-balthazar.rb: Add trailing slash to verified comment

--- Casks/font-balthazar.rb
+++ Casks/font-balthazar.rb
@@ -2,7 +2,7 @@ cask 'font-balthazar' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/balthazar/Balthazar-Regular.ttf'
   name 'Balthazar'
   homepage 'https://www.google.com/fonts/specimen/Balthazar'
