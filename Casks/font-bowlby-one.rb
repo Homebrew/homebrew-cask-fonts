3263a89e11ba34db09018de66193b75e9d1e9708
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:43:28 +0100

    font-bowlby-one.rb: Add trailing slash to verified comment

--- Casks/font-bowlby-one.rb
+++ Casks/font-bowlby-one.rb
@@ -2,7 +2,7 @@ cask 'font-bowlby-one' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/bowlbyone/BowlbyOne-Regular.ttf'
   name 'Bowlby One'
   homepage 'https://www.google.com/fonts/specimen/Bowlby+One'
