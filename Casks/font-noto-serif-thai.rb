f39f92ffd6f124bc3a0350d1b9396ad44a758d5d
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:30:09 +0100

    font-noto-serif-thai.rb: Add trailing slash to verified comment

--- Casks/font-noto-serif-thai.rb
+++ Casks/font-noto-serif-thai.rb
@@ -2,7 +2,7 @@ cask 'font-noto-serif-thai' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifThai-unhinted.zip'
   name 'Noto Serif Thai'
   homepage 'https://www.google.com/get/noto/#serif-thai'
