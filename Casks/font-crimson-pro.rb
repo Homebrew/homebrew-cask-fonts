8caa0fcec5da369311b0c2441525f34a85442f48
1v1_32 <fanninpm@miamioh.edu>
2020-04-19 21:55:24 -0400

    WIP add Crimson Pro (v. 1.000) (#2019)
    * add Crimson Pro (v. 1.000)
    
    * made `brew cask audit` and `brew cask style` pass
    
    * Update font-crimson-pro.rb
    
    * Update font-crimson-pro.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/font-crimson-pro.rb
@@ -0,0 +1,16 @@
+cask 'font-crimson-pro' do
+  version :latest
+  sha256 :no_check
+
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
+  url 'https://github.com/google/fonts/trunk/ofl/crimsonpro',
+      using:      :svn,
+      trust_cert: true
+  name 'Crimson Pro'
+  homepage 'https://www.google.com/fonts/specimen/Crimson%20Pro'
+
+  depends_on macos: '>= :sierra'
+
+  font 'CrimsonPro[wght].ttf'
+  font 'CrimsonPro-Italic[wght].ttf'
+end
