55bbabb817ac1c17e822f71fc08bc941e1e935d7
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:16:07 +0100

    font-stix.rb: Add trailing slash to verified comment

--- Casks/font-stix.rb
+++ Casks/font-stix.rb
@@ -2,7 +2,7 @@ cask 'font-stix' do
   version '2.0.0'
   sha256 '667e3187a22cc63ba0de7083fc69c7920737cd44a44ff5eb2921cbd188531aa9'
 
-  # github.com/stipub/stixfonts was verified as official when first introduced to the cask
+  # github.com/stipub/stixfonts/ was verified as official when first introduced to the cask
   url "https://github.com/stipub/stixfonts/archive/#{version}.tar.gz"
   appcast 'https://github.com/stipub/stixfonts/releases.atom'
   name 'STIX'
