84827834a32cd5f20ef45a66d60e5da7509e2f79
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:18:52 +0100

    font-actor.rb: Add trailing slash to verified comment

--- Casks/font-actor.rb
+++ Casks/font-actor.rb
@@ -2,7 +2,7 @@ cask 'font-actor' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/actor/Actor-Regular.ttf'
   name 'Actor'
   homepage 'https://www.google.com/fonts/specimen/Actor'
