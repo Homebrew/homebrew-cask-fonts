b2d8ed45c091ebe4c753bea3f439ff99f4a4daa1
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:05 +0100

    font-noto-serif-cjk.rb: Add trailing slash to verified comment

--- Casks/font-noto-serif-cjk.rb
+++ Casks/font-noto-serif-cjk.rb
@@ -2,7 +2,7 @@ cask 'font-noto-serif-cjk' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJK.ttc.zip'
   name 'Noto Serif CJK'
   homepage 'https://www.google.com/get/noto/help/cjk/'
