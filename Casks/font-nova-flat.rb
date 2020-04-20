5d769aa212af04cd59aeff234ec9f06c640a38ad
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:25:08 +0100

    font-nova-flat.rb: Add trailing slash to verified comment

--- Casks/font-nova-flat.rb
+++ Casks/font-nova-flat.rb
@@ -2,7 +2,7 @@ cask 'font-nova-flat' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/novaflat/NovaFlat.ttf'
   name 'Nova Flat'
   homepage 'https://www.google.com/fonts/specimen/Nova%20Flat'
