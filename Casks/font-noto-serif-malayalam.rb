75606b44ee6b15a3ba6086f2f8a08b95d9736dd1
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:22:08 +0100

    font-noto-serif-malayalam.rb: Add trailing slash to verified comment

--- Casks/font-noto-serif-malayalam.rb
+++ Casks/font-noto-serif-malayalam.rb
@@ -2,7 +2,7 @@ cask 'font-noto-serif-malayalam' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifMalayalam-unhinted.zip'
   name 'Noto Serif Malayalam'
   homepage 'https://www.google.com/get/noto/#serif-mlym'
