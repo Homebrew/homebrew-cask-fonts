463846eb93561c1615142da2cd439cb80fe67567
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:13:11 +0100

    font-noto-sans-syriac-western.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-syriac-western.rb
+++ Casks/font-noto-sans-syriac-western.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-syriac-western' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacWestern-unhinted.zip'
   name 'Noto Sans Syriac Western'
   homepage 'https://www.google.com/get/noto/#sans-syrc-western'
