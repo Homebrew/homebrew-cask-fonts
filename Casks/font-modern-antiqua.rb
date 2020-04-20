b5659323a15a7a39bf0c8a4b7811d4e714749080
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:22 +0100

    font-modern-antiqua.rb: Add trailing slash to verified comment

--- Casks/font-modern-antiqua.rb
+++ Casks/font-modern-antiqua.rb
@@ -2,7 +2,7 @@ cask 'font-modern-antiqua' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/modernantiqua/ModernAntiqua-Regular.ttf'
   name 'Modern Antiqua'
   homepage 'https://www.google.com/fonts/specimen/Modern+Antiqua'
