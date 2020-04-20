30c65318ba85c17caa3581a1cede6bf8e63bfd34
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:40:46 +0100

    font-ribeye.rb: Add trailing slash to verified comment

--- Casks/font-ribeye.rb
+++ Casks/font-ribeye.rb
@@ -2,7 +2,7 @@ cask 'font-ribeye' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/ribeye/Ribeye-Regular.ttf'
   name 'Ribeye'
   homepage 'https://www.google.com/fonts/specimen/Ribeye'
