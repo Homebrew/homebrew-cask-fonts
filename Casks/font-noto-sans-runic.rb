0a6eb9f1df80e63e272ed6fbaba2802c22461672
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:40:04 +0100

    font-noto-sans-runic.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-runic.rb
+++ Casks/font-noto-sans-runic.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-runic' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansRunic-unhinted.zip'
   name 'Noto Sans Runic'
   homepage 'https://www.google.com/get/noto/#sans-runr'
