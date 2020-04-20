b95b80aa6f1a5796d6b59bf4890a7f6dcce08544
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:39:21 +0100

    font-abril-fatface.rb: Add trailing slash to verified comment

--- Casks/font-abril-fatface.rb
+++ Casks/font-abril-fatface.rb
@@ -2,7 +2,7 @@ cask 'font-abril-fatface' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/abrilfatface/AbrilFatface-Regular.ttf'
   name 'Abril Fatface'
   homepage 'https://www.google.com/fonts/specimen/Abril%20Fatface'
