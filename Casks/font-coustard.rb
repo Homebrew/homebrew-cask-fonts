3331ec5241454bc6781ab595a38b9d90c74b3b44
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:14:14 +0100

    font-coustard.rb: Add trailing slash to verified comment

--- Casks/font-coustard.rb
+++ Casks/font-coustard.rb
@@ -2,7 +2,7 @@ cask 'font-coustard' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/coustard',
       using:      :svn,
       revision:   '50',
