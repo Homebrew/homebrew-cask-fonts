4ec2658c9a6346feb924f951b1ab74237a6575aa
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:30:37 +0100

    font-open-sans.rb: Add trailing slash to verified comment

--- Casks/font-open-sans.rb
+++ Casks/font-open-sans.rb
@@ -2,7 +2,7 @@ cask 'font-open-sans' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/apache/opensans',
       using:      :svn,
       revision:   '48',
