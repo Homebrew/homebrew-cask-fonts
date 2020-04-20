fbc28e0cf5b7b31bf1ad6ff72778eb8226d2164a
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:19:31 +0100

    font-roboto.rb: Add trailing slash to verified comment

--- Casks/font-roboto.rb
+++ Casks/font-roboto.rb
@@ -2,7 +2,7 @@ cask 'font-roboto' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/roboto/static',
       using:      :svn,
       trust_cert: true
