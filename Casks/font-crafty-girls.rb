88c4f91c4724ab43f620f4a139384b52dc8783db
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:22:56 +0100

    font-crafty-girls.rb: Add trailing slash to verified comment

--- Casks/font-crafty-girls.rb
+++ Casks/font-crafty-girls.rb
@@ -2,7 +2,7 @@ cask 'font-crafty-girls' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/apache/craftygirls/CraftyGirls-Regular.ttf'
   name 'Crafty Girls'
   homepage 'https://www.google.com/fonts/specimen/Crafty+Girls'
