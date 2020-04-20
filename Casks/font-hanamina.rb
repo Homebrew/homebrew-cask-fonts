9db3a29d1e05dd83ada22df49d698a0fd9178c91
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:23:29 +0100

    font-hanamina.rb: Add trailing slash to verified comment

--- Casks/font-hanamina.rb
+++ Casks/font-hanamina.rb
@@ -2,7 +2,7 @@ cask 'font-hanamina' do
   version '2017.09.04,68253'
   sha256 '571cd4a09ae7da0c642d640fc2442c050aa450ebb0587a95cdd097d41a9c9572'
 
-  # osdn.dl.osdn.jp/hanazono-font was verified as official when first introduced to the cask
+  # osdn.dl.osdn.jp/hanazono-font/ was verified as official when first introduced to the cask
   url "https://osdn.dl.osdn.jp/hanazono-font/#{version.after_comma}/hanazono-#{version.before_comma.no_dots}.zip"
   name 'HanaMinA'
   homepage 'https://fonts.jp/hanazono/'
