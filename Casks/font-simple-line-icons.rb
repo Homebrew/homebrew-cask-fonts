5a36554228fce19d52c7f121e23b14014c3ac501
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:39:39 +0100

    font-simple-line-icons.rb: Add trailing slash to verified comment

--- Casks/font-simple-line-icons.rb
+++ Casks/font-simple-line-icons.rb
@@ -2,7 +2,7 @@ cask 'font-simple-line-icons' do
   version '2.4.1'
   sha256 'fd0e9edd1550f8f8ded705160f0e433aae4170fe9f21a595eb583ac54c7e2d12'
 
-  # github.com/thesabbir/simple-line-icons was verified as official when first introduced to the cask
+  # github.com/thesabbir/simple-line-icons/ was verified as official when first introduced to the cask
   url "https://github.com/thesabbir/simple-line-icons/archive/#{version}.zip"
   appcast 'https://github.com/thesabbir/simple-line-icons/releases.atom'
   name 'Simple Line Icons'
