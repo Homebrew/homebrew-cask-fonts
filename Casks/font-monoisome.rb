d75d60f8a03b4db140963ae79c12463393556f9b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:19:25 +0100

    font-monoisome.rb: Add trailing slash to verified comment

--- Casks/font-monoisome.rb
+++ Casks/font-monoisome.rb
@@ -2,7 +2,7 @@ cask 'font-monoisome' do
   version '0.61'
   sha256 '0aedac5663ea33f5a7d5c802408ff0ee16cf62b680f9e05980ccd2ba40040930'
 
-  # github.com/larsenwork/monoid was verified as official when first introduced to the cask
+  # github.com/larsenwork/monoid/ was verified as official when first introduced to the cask
   url 'https://github.com/larsenwork/monoid/blob/master/Monoisome/Monoisome-Regular.ttf?raw=true'
   appcast 'https://github.com/larsenwork/monoid/releases.atom'
   name 'Monoisome'
