6ea1f05db20980e5c26e9221575d33f44357c44b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:33:16 +0100

    font-mr-dafoe.rb: Add trailing slash to verified comment

--- Casks/font-mr-dafoe.rb
+++ Casks/font-mr-dafoe.rb
@@ -2,7 +2,7 @@ cask 'font-mr-dafoe' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/mrdafoe/MrDafoe-Regular.ttf'
   name 'Mr Dafoe'
   homepage 'https://www.google.com/fonts/specimen/Mr+Dafoe'
