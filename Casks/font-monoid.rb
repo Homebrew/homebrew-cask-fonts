e5793d5bebd718133f00fe7babe661056f5686d7
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:23:09 +0100

    font-monoid.rb: Add trailing slash to verified comment

--- Casks/font-monoid.rb
+++ Casks/font-monoid.rb
@@ -2,7 +2,7 @@ cask 'font-monoid' do
   version '0.61'
   sha256 'c28acd76ae1f0244f7c869bf1ba3272da3eb900dd50fbbd368265468a1a34794'
 
-  # github.com/larsenwork/monoid was verified as official when first introduced to the cask
+  # github.com/larsenwork/monoid/ was verified as official when first introduced to the cask
   url 'https://github.com/larsenwork/monoid/blob/release/Monoid.zip?raw=true'
   appcast 'https://github.com/larsenwork/monoid/releases.atom'
   name 'Monoid'
