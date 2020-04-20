b7959fc66dde9246f4f2a860b5bc5ee2a98e03ef
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:41:02 +0100

    font-noto-sans-mongolian.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-mongolian.rb
+++ Casks/font-noto-sans-mongolian.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-mongolian' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMongolian-unhinted.zip'
   name 'Noto Sans Mongolian'
   homepage 'https://www.google.com/get/noto/#sans-mong'
