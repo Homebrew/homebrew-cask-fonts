4b5614008e662e13b1ab559a7ff2de60fa14e0e5
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:14:47 +0100

    font-fresca.rb: Add trailing slash to verified comment

--- Casks/font-fresca.rb
+++ Casks/font-fresca.rb
@@ -2,7 +2,7 @@ cask 'font-fresca' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/fresca/Fresca-Regular.ttf'
   name 'Fresca'
   homepage 'https://www.google.com/fonts/specimen/Fresca'
