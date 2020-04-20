c0f2684206e0f855c4a7b57a9b85b6c5342a1711
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:45:11 +0100

    font-cantata-one.rb: Add trailing slash to verified comment

--- Casks/font-cantata-one.rb
+++ Casks/font-cantata-one.rb
@@ -2,7 +2,7 @@ cask 'font-cantata-one' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/cantataone/CantataOne-Regular.ttf'
   name 'Cantata One'
   homepage 'https://www.google.com/fonts/specimen/Cantata+One'
