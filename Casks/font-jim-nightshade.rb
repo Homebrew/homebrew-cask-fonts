12817f9cd4bbbb88ae6ce9ff5450c9fad9ca737d
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:21:45 +0100

    font-jim-nightshade.rb: Add trailing slash to verified comment

--- Casks/font-jim-nightshade.rb
+++ Casks/font-jim-nightshade.rb
@@ -2,7 +2,7 @@ cask 'font-jim-nightshade' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/jimnightshade/JimNightshade-Regular.ttf'
   name 'Jim Nightshade'
   homepage 'https://www.google.com/fonts/specimen/Jim+Nightshade'
