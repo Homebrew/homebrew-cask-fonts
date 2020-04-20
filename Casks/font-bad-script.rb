082e1cec94d73ad2f8091279e366f6d0f21d9a73
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:37:24 +0100

    font-bad-script.rb: Add trailing slash to verified comment

--- Casks/font-bad-script.rb
+++ Casks/font-bad-script.rb
@@ -2,7 +2,7 @@ cask 'font-bad-script' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/badscript/BadScript-Regular.ttf'
   name 'Bad Script'
   homepage 'https://www.google.com/fonts/specimen/Bad+Script'
