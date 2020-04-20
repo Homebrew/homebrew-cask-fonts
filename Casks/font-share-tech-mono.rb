317a7a771877463872a2873159b21737f5636839
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:24:15 +0100

    font-share-tech-mono.rb: Add trailing slash to verified comment

--- Casks/font-share-tech-mono.rb
+++ Casks/font-share-tech-mono.rb
@@ -2,7 +2,7 @@ cask 'font-share-tech-mono' do
   version '1.002'
   sha256 '9ceab1f87414829af259c0f537573ae03ef7dd3147c0b27a36a1a0beb6732677'
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/sharetechmono/ShareTechMono-Regular.ttf'
   name 'Share Tech Mono'
   homepage 'https://www.google.com/fonts/specimen/Share%20Tech%20Mono'
