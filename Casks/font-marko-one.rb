0adbc910c4bebeccb8da617fe87fc25d6ceb141d
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:35:47 +0100

    font-marko-one.rb: Add trailing slash to verified comment

--- Casks/font-marko-one.rb
+++ Casks/font-marko-one.rb
@@ -2,7 +2,7 @@ cask 'font-marko-one' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/markoone/MarkoOne-Regular.ttf'
   name 'Marko One'
   homepage 'https://www.google.com/fonts/specimen/Marko+One'
