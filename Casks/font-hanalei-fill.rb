d9730425b7d185e189ca85a5ed25d32f085f1ada
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:28:49 +0100

    font-hanalei-fill.rb: Add trailing slash to verified comment

--- Casks/font-hanalei-fill.rb
+++ Casks/font-hanalei-fill.rb
@@ -2,7 +2,7 @@ cask 'font-hanalei-fill' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/hanaleifill/HanaleiFill-Regular.ttf'
   name 'Hanalei Fill'
   homepage 'https://www.google.com/fonts/specimen/Hanalei+Fill'
