a913c7b6ef37786d2981475304b514968fc3abc1
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:26:33 +0100

    font-life-savers.rb: Add trailing slash to verified comment

--- Casks/font-life-savers.rb
+++ Casks/font-life-savers.rb
@@ -2,7 +2,7 @@ cask 'font-life-savers' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/lifesavers',
       using:      :svn,
       revision:   '50',
