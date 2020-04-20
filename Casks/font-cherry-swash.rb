12f5fd29be40d024873210594b800477e273aa7c
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:25:46 +0100

    font-cherry-swash.rb: Add trailing slash to verified comment

--- Casks/font-cherry-swash.rb
+++ Casks/font-cherry-swash.rb
@@ -2,7 +2,7 @@ cask 'font-cherry-swash' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/cherryswash',
       using:      :svn,
       revision:   '50',
