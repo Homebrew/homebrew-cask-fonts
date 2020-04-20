ed8a8e62dc1c35fd7903213376fe1a5a87e2c07a
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:13:33 +0100

    font-noto-serif.rb: Add trailing slash to verified comment

--- Casks/font-noto-serif.rb
+++ Casks/font-noto-serif.rb
@@ -2,7 +2,7 @@ cask 'font-noto-serif' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerif-unhinted.zip'
   name 'Noto Serif'
   homepage 'https://www.google.com/get/noto/#serif-lgc'
