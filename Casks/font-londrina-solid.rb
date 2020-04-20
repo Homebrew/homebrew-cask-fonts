447d5a5557e5cbaaaf77c366aaafee279dbe68a5
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:36:33 +0100

    font-londrina-solid.rb: Add trailing slash to verified comment

--- Casks/font-londrina-solid.rb
+++ Casks/font-londrina-solid.rb
@@ -2,7 +2,7 @@ cask 'font-londrina-solid' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/londrinasolid/LondrinaSolid-Regular.ttf'
   name 'Londrina Solid'
   homepage 'https://www.google.com/fonts/specimen/Londrina+Solid'
