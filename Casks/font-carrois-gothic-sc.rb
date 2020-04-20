8d9a5164649006ebf5812d140599d2b2c832959a
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:18:43 +0100

    font-carrois-gothic-sc.rb: Add trailing slash to verified comment

--- Casks/font-carrois-gothic-sc.rb
+++ Casks/font-carrois-gothic-sc.rb
@@ -2,7 +2,7 @@ cask 'font-carrois-gothic-sc' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/carroisgothicsc/CarroisGothicSC-Regular.ttf'
   name 'Carrois Gothic SC'
   homepage 'https://www.google.com/fonts/specimen/Carrois+Gothic+SC'
