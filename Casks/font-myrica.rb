9b2e03bd3712935eaaaf02b2f47b9fad7c9db27b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:28 +0100

    font-myrica.rb: Add trailing slash to verified comment

--- Casks/font-myrica.rb
+++ Casks/font-myrica.rb
@@ -2,7 +2,7 @@ cask 'font-myrica' do
   version '2.006.20150301'
   sha256 'a90eb9b79885f02ad9e0e752a0b979b699847be7de13dc3b6113658f006d12bd'
 
-  # codeload.github.com/tomokuni/Myrica was verified as official when first introduced to the cask
+  # codeload.github.com/tomokuni/Myrica/ was verified as official when first introduced to the cask
   url "https://codeload.github.com/tomokuni/Myrica/tar.gz/#{version}"
   appcast 'https://github.com/tomokuni/Myrica/releases.atom'
   name 'Myrica'
