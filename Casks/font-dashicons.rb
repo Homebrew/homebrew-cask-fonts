4639fe8f4bfe95434419577c5521dbab908cb755
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:57 +0100

    font-dashicons.rb: Add trailing slash to verified comment

--- Casks/font-dashicons.rb
+++ Casks/font-dashicons.rb
@@ -2,7 +2,7 @@ cask 'font-dashicons' do
   version :latest
   sha256 :no_check
 
-  # github.com/WordPress/dashicons was verified as official when first introduced to the cask
+  # github.com/WordPress/dashicons/ was verified as official when first introduced to the cask
   url 'https://github.com/WordPress/dashicons/raw/master/icon-font/fonts/dashicons.ttf'
   name 'Dashicons'
   homepage 'https://developer.wordpress.org/resource/dashicons'
