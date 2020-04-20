278e86724e3d09cb2b1dbe9a23bb22da9372b32c
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:29:52 +0100

    font-mate-sc.rb: Add trailing slash to verified comment

--- Casks/font-mate-sc.rb
+++ Casks/font-mate-sc.rb
@@ -2,7 +2,7 @@ cask 'font-mate-sc' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/matesc/MateSC-Regular.ttf'
   name 'Mate SC'
   homepage 'https://www.google.com/fonts/specimen/Mate+SC'
