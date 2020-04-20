a020d410e5445e27f46f8052b180524b6c5df930
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:23:23 +0100

    font-fascinate.rb: Add trailing slash to verified comment

--- Casks/font-fascinate.rb
+++ Casks/font-fascinate.rb
@@ -2,7 +2,7 @@ cask 'font-fascinate' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/fascinate/Fascinate-Regular.ttf'
   name 'Fascinate'
   homepage 'https://www.google.com/fonts/specimen/Fascinate'
