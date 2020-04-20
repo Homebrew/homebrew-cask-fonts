9d359b54632720b59a406c001e48923dfd78525d
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:29:12 +0100

    font-architects-daughter.rb: Add trailing slash to verified comment

--- Casks/font-architects-daughter.rb
+++ Casks/font-architects-daughter.rb
@@ -2,7 +2,7 @@ cask 'font-architects-daughter' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/architectsdaughter/ArchitectsDaughter-Regular.ttf'
   name 'Architects Daughter'
   homepage 'https://www.google.com/fonts/specimen/Architects+Daughter'
