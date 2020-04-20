ec95fe8e30050f5341ada4b8f703c6a6c694a8fe
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:12:32 +0100

    font-noto-emoji.rb: Add trailing slash to verified comment

--- Casks/font-noto-emoji.rb
+++ Casks/font-noto-emoji.rb
@@ -2,7 +2,7 @@ cask 'font-noto-emoji' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoEmoji-unhinted.zip'
   name 'Noto Emoji'
   homepage 'https://www.google.com/get/noto/#emoji-zsye'
