c15c946c83214801481de68ffffc4a573fb7ee66
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:37 +0100

    font-belgrano.rb: Add trailing slash to verified comment

--- Casks/font-belgrano.rb
+++ Casks/font-belgrano.rb
@@ -2,7 +2,7 @@ cask 'font-belgrano' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/belgrano/Belgrano-Regular.ttf'
   name 'Belgrano'
   homepage 'https://www.google.com/fonts/specimen/Belgrano'
