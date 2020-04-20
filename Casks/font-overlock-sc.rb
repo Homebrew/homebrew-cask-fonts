d73ebf06b075de85e349229f8628ced6eb4887fc
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:53 +0100

    font-overlock-sc.rb: Add trailing slash to verified comment

--- Casks/font-overlock-sc.rb
+++ Casks/font-overlock-sc.rb
@@ -2,7 +2,7 @@ cask 'font-overlock-sc' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/overlocksc/OverlockSC-Regular.ttf'
   name 'Overlock SC'
   homepage 'https://www.google.com/fonts/specimen/Overlock+SC'
