857723c5e0ae706471696f162c67a2d4fb6d9e58
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:27:27 +0100

    font-nova-script.rb: Add trailing slash to verified comment

--- Casks/font-nova-script.rb
+++ Casks/font-nova-script.rb
@@ -2,7 +2,7 @@ cask 'font-nova-script' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/novascript/NovaScript-Regular.ttf'
   name 'Nova Script'
   homepage 'https://www.google.com/fonts/specimen/Nova%20Script'
