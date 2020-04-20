606a6980c39c57e5ea37c11cc32c7a49d13ddc60
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:35:03 +0100

    font-alike-angular.rb: Add trailing slash to verified comment

--- Casks/font-alike-angular.rb
+++ Casks/font-alike-angular.rb
@@ -2,7 +2,7 @@ cask 'font-alike-angular' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/alikeangular/AlikeAngular-Regular.ttf'
   name 'Alike Angular'
   homepage 'https://www.google.com/fonts/specimen/Alike+Angular'
