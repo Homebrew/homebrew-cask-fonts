8b206284dc4174929d0d630ae51d33bfb9413534
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:37:36 +0100

    font-fira-mono.rb: Add trailing slash to verified comment

--- Casks/font-fira-mono.rb
+++ Casks/font-fira-mono.rb
@@ -2,7 +2,7 @@ cask 'font-fira-mono' do
   version '3.206,4.202'
   sha256 'd86269657387f144d77ba12011124f30f423f70672e1576dc16f918bb16ddfe4'
 
-  # github.com/mozilla/Fira was verified as official when first introduced to the cask
+  # github.com/mozilla/Fira/ was verified as official when first introduced to the cask
   url "https://github.com/mozilla/Fira/archive/#{version.after_comma}.tar.gz"
   appcast 'https://github.com/mozilla/Fira/releases.atom'
   name 'Fira Mono'
