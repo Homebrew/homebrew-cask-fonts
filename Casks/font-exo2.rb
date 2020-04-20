789e4456443f0ab5eaad7a72ae6028daa6a2d691
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:32:38 +0100

    font-exo2.rb: Add trailing slash to verified comment

--- Casks/font-exo2.rb
+++ Casks/font-exo2.rb
@@ -2,7 +2,7 @@ cask 'font-exo2' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/exo2',
       using:      :svn,
       revision:   '50',
