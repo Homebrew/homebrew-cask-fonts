32a8deca3c4bd7237f842e6a5e21a6125243f4b4
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:32:32 +0100

    font-materialdesignicons-webfont.rb: Add trailing slash to verified comment

--- Casks/font-materialdesignicons-webfont.rb
+++ Casks/font-materialdesignicons-webfont.rb
@@ -2,7 +2,7 @@ cask 'font-materialdesignicons-webfont' do
   version :latest
   sha256 :no_check
 
-  # github.com/templarian/materialdesign-webfont was verified as official when first introduced to the cask
+  # github.com/templarian/materialdesign-webfont/ was verified as official when first introduced to the cask
   url 'https://github.com/templarian/materialdesign-webfont/trunk/fonts',
       using:      :svn,
       revision:   '50',
