ca322092a865ab90a56df4893d29e5fbd34224ba
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:27:40 +0100

    font-coda.rb: Add trailing slash to verified comment

--- Casks/font-coda.rb
+++ Casks/font-coda.rb
@@ -2,7 +2,7 @@ cask 'font-coda' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/coda',
       using:      :svn,
       revision:   '50',
