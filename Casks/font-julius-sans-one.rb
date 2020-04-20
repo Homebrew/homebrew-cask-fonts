f3f245f0ca37f869634326ea7b504dffdd0d9b41
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:43:43 +0100

    font-julius-sans-one.rb: Add trailing slash to verified comment

--- Casks/font-julius-sans-one.rb
+++ Casks/font-julius-sans-one.rb
@@ -2,7 +2,7 @@ cask 'font-julius-sans-one' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/juliussansone/JuliusSansOne-Regular.ttf'
   name 'Julius Sans One'
   homepage 'https://www.google.com/fonts/specimen/Julius+Sans+One'
