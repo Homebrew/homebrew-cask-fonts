57b00e0ee23c3f0ad04cc577cf19f15bf8ad4a84
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:30:02 +0100

    font-didact-gothic.rb: Add trailing slash to verified comment

--- Casks/font-didact-gothic.rb
+++ Casks/font-didact-gothic.rb
@@ -2,7 +2,7 @@ cask 'font-didact-gothic' do
   version '2.101'
   sha256 'b98f9e091b6337a86f4e0f69c94c31905e1e287782853f472e176b4e28d3f81f'
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/didactgothic/DidactGothic-Regular.ttf'
   name 'Didact Gothic'
   homepage 'https://www.google.com/fonts/specimen/Didact%20Gothic'
