6b3c1ca7db878c0f077541b4c096b0686a4a90b6
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:50 +0100

    font-prociono.rb: Add trailing slash to verified comment

--- Casks/font-prociono.rb
+++ Casks/font-prociono.rb
@@ -2,7 +2,7 @@ cask 'font-prociono' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/prociono/Prociono-Regular.ttf'
   name 'Prociono'
   homepage 'https://www.google.com/fonts/specimen/Prociono'
