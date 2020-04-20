51221bb0766e762008d601c1ba2fb98c8b66e9c2
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:17:16 +0100

    font-noto-sans-gurmukhi.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-gurmukhi.rb
+++ Casks/font-noto-sans-gurmukhi.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-gurmukhi' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGurmukhi-unhinted.zip'
   name 'Noto Sans Gurmukhi'
   homepage 'https://www.google.com/get/noto/#sans-guru'
