be8094332ebc8dd4b84a031ef6577965524b0e51
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:12:21 +0100

    font-kawkab-mono.rb: Add trailing slash to verified comment

--- Casks/font-kawkab-mono.rb
+++ Casks/font-kawkab-mono.rb
@@ -2,7 +2,7 @@ cask 'font-kawkab-mono' do
   version '0.501'
   sha256 '11c06f57dddefaf0166d74caaa072865ab6ff8d34076e7ec5d2c20edda145666'
 
-  # github.com/aiaf/kawkab-mono was verified as official when first introduced to the cask
+  # github.com/aiaf/kawkab-mono/ was verified as official when first introduced to the cask
   url "https://github.com/aiaf/kawkab-mono/releases/download/v#{version}/kawkab-mono-#{version}.zip"
   appcast 'https://github.com/aiaf/kawkab-mono/releases.atom'
   name 'Kawkab Mono'
