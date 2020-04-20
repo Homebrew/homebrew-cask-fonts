cfb3664412f45dfa48589afcff31f1d51d75bddc
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:29:15 +0100

    font-creepster.rb: Add trailing slash to verified comment

--- Casks/font-creepster.rb
+++ Casks/font-creepster.rb
@@ -2,7 +2,7 @@ cask 'font-creepster' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/creepster/Creepster-Regular.ttf'
   name 'Creepster'
   homepage 'https://www.google.com/fonts/specimen/Creepster'
