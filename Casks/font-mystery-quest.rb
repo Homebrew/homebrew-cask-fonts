c18dc649f90b9d8b4e7a30e6e099c3703bce34af
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:20:04 +0100

    font-mystery-quest.rb: Add trailing slash to verified comment

--- Casks/font-mystery-quest.rb
+++ Casks/font-mystery-quest.rb
@@ -2,7 +2,7 @@ cask 'font-mystery-quest' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/mysteryquest/MysteryQuest-Regular.ttf'
   name 'Mystery Quest'
   homepage 'https://www.google.com/fonts/specimen/Mystery+Quest'
