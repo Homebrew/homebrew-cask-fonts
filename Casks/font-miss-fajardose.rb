c4adf0d4fa9455e3639b386835d4d7f5de763eef
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:40:07 +0100

    font-miss-fajardose.rb: Add trailing slash to verified comment

--- Casks/font-miss-fajardose.rb
+++ Casks/font-miss-fajardose.rb
@@ -2,7 +2,7 @@ cask 'font-miss-fajardose' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/missfajardose/MissFajardose-Regular.ttf'
   name 'Miss Fajardose'
   homepage 'https://www.google.com/fonts/specimen/Miss+Fajardose'
