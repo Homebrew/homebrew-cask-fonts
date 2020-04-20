9b33ce7f6678288f307041cf541566bfa12422db
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:20:51 +0100

    font-noto-sans-egyptian-hieroglyphs.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-egyptian-hieroglyphs.rb
+++ Casks/font-noto-sans-egyptian-hieroglyphs.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-egyptian-hieroglyphs' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansEgyptianHieroglyphs-unhinted.zip'
   name 'Noto Sans Egyptian Hieroglyphs'
   homepage 'https://www.google.com/get/noto/#sans-egyp'
