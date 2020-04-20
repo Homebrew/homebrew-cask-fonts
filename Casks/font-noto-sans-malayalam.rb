6244a16b3310bc140e34b7d489db53a46e5bde56
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:44:01 +0100

    font-noto-sans-malayalam.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-malayalam.rb
+++ Casks/font-noto-sans-malayalam.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-malayalam' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMalayalam-unhinted.zip'
   name 'Noto Sans Malayalam'
   homepage 'https://www.google.com/get/noto/#sans-mlym'
