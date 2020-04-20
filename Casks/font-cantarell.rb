e980df9202137ac4db58497144baa33e5dab3ca7
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:19:52 +0100

    font-cantarell.rb: Add trailing slash to verified comment

--- Casks/font-cantarell.rb
+++ Casks/font-cantarell.rb
@@ -2,7 +2,7 @@ cask 'font-cantarell' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/cantarell',
       using:      :svn,
       revision:   '50',
