89fcf127aa08943597357536c69a540781e73043
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:24:31 +0100

    font-nova-square.rb: Add trailing slash to verified comment

--- Casks/font-nova-square.rb
+++ Casks/font-nova-square.rb
@@ -2,7 +2,7 @@ cask 'font-nova-square' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/novasquare/NovaSquare.ttf'
   name 'Nova Square'
   homepage 'https://www.google.com/fonts/specimen/Nova%20Square'
