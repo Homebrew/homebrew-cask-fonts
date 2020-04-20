38974c21155544e066f07639ff8eea080d843d1e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:50:44 +0100

    Update font-crimson-pro.rb

--- Casks/font-crimson-pro.rb
+++ Casks/font-crimson-pro.rb
@@ -2,7 +2,7 @@ cask 'font-crimson-pro' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/crimsonpro',
       using:      :svn,
       revision:   '6584',
