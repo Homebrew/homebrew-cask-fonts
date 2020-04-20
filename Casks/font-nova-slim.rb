e777e990162e7b07bad2cf48e05d0064395f9fbe
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:27:21 +0100

    font-nova-slim.rb: Add trailing slash to verified comment

--- Casks/font-nova-slim.rb
+++ Casks/font-nova-slim.rb
@@ -2,7 +2,7 @@ cask 'font-nova-slim' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/novaslim/NovaSlim.ttf'
   name 'Nova Slim'
   homepage 'https://www.google.com/fonts/specimen/Nova%20Slim'
