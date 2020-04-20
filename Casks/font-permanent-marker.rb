a656f7a598299348faaf8361a0afa1c2621ed076
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:12:12 +0100

    font-permanent-marker.rb: Add trailing slash to verified comment

--- Casks/font-permanent-marker.rb
+++ Casks/font-permanent-marker.rb
@@ -2,7 +2,7 @@ cask 'font-permanent-marker' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/apache/permanentmarker/PermanentMarker-Regular.ttf'
   name 'Permanent Marker'
   homepage 'https://www.google.com/fonts/specimen/Permanent+Marker'
