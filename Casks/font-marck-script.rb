737515eeecb97226daa09bec57255127d688e1c7
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:16:24 +0100

    font-marck-script.rb: Add trailing slash to verified comment

--- Casks/font-marck-script.rb
+++ Casks/font-marck-script.rb
@@ -2,7 +2,7 @@ cask 'font-marck-script' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/marckscript/MarckScript-Regular.ttf'
   name 'Marck Script'
   homepage 'https://www.google.com/fonts/specimen/Marck+Script'
