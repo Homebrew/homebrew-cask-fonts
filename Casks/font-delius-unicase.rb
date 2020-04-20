8db254c2ec346fe6fa88e026a89ee1a5c8491bd4
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:45:02 +0100

    font-delius-unicase.rb: Add trailing slash to verified comment

--- Casks/font-delius-unicase.rb
+++ Casks/font-delius-unicase.rb
@@ -2,7 +2,7 @@ cask 'font-delius-unicase' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/trunk/ofl/deliusunicase',
       using:      :svn,
       revision:   '50',
