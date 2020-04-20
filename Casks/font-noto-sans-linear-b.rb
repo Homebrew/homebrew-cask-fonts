d817e068eb9a756ff01c89007dd31b34bcf1f29e
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:12:15 +0100

    font-noto-sans-linear-b.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-linear-b.rb
+++ Casks/font-noto-sans-linear-b.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-linear-b' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLinearB-unhinted.zip'
   name 'Noto Sans Linear B'
   homepage 'https://www.google.com/get/noto/#sans-linb'
