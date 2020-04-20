d3b24e7f0307ab7ac03d10605401c19a9cff86c0
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:41:17 +0100

    font-noto-sans-meetei-mayek.rb: Add trailing slash to verified comment

--- Casks/font-noto-sans-meetei-mayek.rb
+++ Casks/font-noto-sans-meetei-mayek.rb
@@ -2,7 +2,7 @@ cask 'font-noto-sans-meetei-mayek' do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com was verified as official when first introduced to the cask
+  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
   url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMeeteiMayek-unhinted.zip'
   name 'Noto Sans Meetei Mayek'
   homepage 'https://www.google.com/get/noto/#sans-mtei'
