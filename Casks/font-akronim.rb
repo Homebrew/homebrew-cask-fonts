e876550cacb75fb1ac27fa7ffeba75d787920962
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:34:39 +0100

    font-akronim.rb: Add trailing slash to verified comment

--- Casks/font-akronim.rb
+++ Casks/font-akronim.rb
@@ -2,7 +2,7 @@ cask 'font-akronim' do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts was verified as official when first introduced to the cask
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/akronim/Akronim-Regular.ttf'
   name 'Akronim'
   homepage 'https://www.google.com/fonts/specimen/Akronim'
