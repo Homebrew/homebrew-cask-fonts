6eec51da02d6f72268732d20c1a12f358ded7664
Piotr Kowalski <piecioshka@gmail.com>
2020-04-23 22:54:53 +0200

    Add Ruda

---
+++ Casks/font-ruda.rb
@@ -0,0 +1,18 @@
+cask 'font-ruda' do
+  version :latest
+  sha256 :no_check
+
+  # github.com/google/fonts/ was verified as official when first introduced to the cask
+  url 'https://github.com/google/fonts/trunk/ofl/ruda/static',
+      using:      :svn,
+      revision:   '50',
+      trust_cert: true
+  name 'Ruda'
+  homepage 'https://www.google.com/fonts/specimen/Ruda'
+
+  depends_on macos: '>= :sierra'
+
+  font 'Ruda-Black.ttf'
+  font 'Ruda-Bold.ttf'
+  font 'Ruda-Regular.ttf'
+end
