35b81ac0c689dbc142327d71331145244c7a7a53
Piotr Kowalski <piecioshka@gmail.com>
2020-04-24 11:31:31 +0200

    Split font to three files

--- Casks/font-ruda-black.rb
+++ Casks/font-ruda-black.rb
@@ -1,13 +1,13 @@
-cask 'font-ruda' do
+cask 'font-ruda-black' do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/blob/master/ofl/ruda/static/Ruda-Regular.ttf'
-  name 'Ruda'
+  url 'https://github.com/google/fonts/blob/master/ofl/ruda/static/Ruda-Black.ttf'
+  name 'Ruda Black'
   homepage 'https://www.google.com/fonts/specimen/Ruda'
 
   depends_on macos: '>= :sierra'
 
-  font 'Ruda-Regular.ttf'
+  font 'Ruda-Black.ttf'
 end
