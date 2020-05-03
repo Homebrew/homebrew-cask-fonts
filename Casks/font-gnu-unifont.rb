5c1d98b8e2d2212ef561ef3d4232a2291a055539
Alexander Ronald Altman <alexanderaltman@me.com>
2020-05-02 18:17:22 -0700

    font-gnu-unifont 13.0.02 (#2032)

--- Casks/font-gnu-unifont.rb
+++ Casks/font-gnu-unifont.rb
@@ -1,6 +1,6 @@
 cask 'font-gnu-unifont' do
-  version '13.0.01'
-  sha256 '8b486450b2bd1dc9357ceca096faf493d13f7e582f29984e46552ca8faa87feb'
+  version '13.0.02'
+  sha256 'b1aa56482e38abf3f3151472da3432fd75fadf1ed2e17f979b24c0a63002e9b9'
 
   # ftpmirror.gnu.org/unifont/ was verified as official when first introduced to the cask
   url "https://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.tar.gz"
