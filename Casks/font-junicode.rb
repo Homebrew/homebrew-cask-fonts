7bbf0dd885f48efc649e01229a16401affb87078
Viktor Szakats <vszakats@users.noreply.github.com>
2020-05-05 16:31:57 +0200

    junicode: secure homepage (#2033)

--- Casks/font-junicode.rb
+++ Casks/font-junicode.rb
@@ -2,10 +2,11 @@ cask 'font-junicode' do
   version '1.002'
   sha256 'c199d96c8424be60fcab8d00d2eee39ea8ae632cfd5e710cbbd70626d6a729e7'
 
+  # downloads.sourceforge.net/junicode/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/junicode/junicode-#{version}.zip"
   appcast 'https://sourceforge.net/projects/junicode/rss'
   name 'Junicode'
-  homepage 'http://junicode.sourceforge.net/'
+  homepage 'https://junicode.sourceforge.io/'
 
   font 'FoulisGreek.ttf'
   font 'Junicode-Bold.ttf'
