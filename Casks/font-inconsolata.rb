d75670a15b1bb009173c1637dbd9f8137476a171
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-05-15 07:18:30 +0200

    Update font-inconsolata.rb
    The old link is broken: https://github.com/google/fonts/trunk/ofl/inconsolata is not valid anymore.
    Has to become: https://github.com/google/fonts/tree/master/ofl/inconsolata

--- Casks/font-inconsolata.rb
+++ Casks/font-inconsolata.rb
@@ -3,7 +3,7 @@ cask 'font-inconsolata' do
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/inconsolata',
+  url 'https://github.com/google/fonts/tree/master/ofl/inconsolata',
       using:      :svn,
       trust_cert: true
   name 'Inconsolata'
