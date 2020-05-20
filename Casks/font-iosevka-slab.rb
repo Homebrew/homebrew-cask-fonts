4c076b5cb47cd2fd10e0b9a4c3abfd887f432424
Andy Lindeman <andy@lindeman.io>
2020-05-20 10:30:05 -0400

    iosevka 3.0.1

--- Casks/font-iosevka-slab.rb
+++ Casks/font-iosevka-slab.rb
@@ -1,19 +1,19 @@
 cask 'font-iosevka-slab' do
-  version '3.0.0'
-  sha256 '0f1cc938a3d63d52ca7f72472fce1f9a37be27ae5cd58cf4c5167febd864e783'
+  version '3.0.1'
+  sha256 '25fdff0897aec4b855ee84dfcf7bcbd38cae89aec1779c3231f45e5782565981'
 
-  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/pkg-iosevka-slab-#{version}.zip"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-slab-#{version}.zip"
   appcast 'https://github.com/be5invis/Iosevka/releases.atom'
   name 'Iosevka Slab'
   homepage 'https://github.com/be5invis/Iosevka/'
 
-  font 'ttc/iosevka-slab-bold.ttc'
-  font 'ttc/iosevka-slab-extrabold.ttc'
-  font 'ttc/iosevka-slab-extralight.ttc'
-  font 'ttc/iosevka-slab-heavy.ttc'
-  font 'ttc/iosevka-slab-light.ttc'
-  font 'ttc/iosevka-slab-medium.ttc'
-  font 'ttc/iosevka-slab-regular.ttc'
-  font 'ttc/iosevka-slab-semibold.ttc'
-  font 'ttc/iosevka-slab-thin.ttc'
+  font 'iosevka-slab-bold.ttc'
+  font 'iosevka-slab-extrabold.ttc'
+  font 'iosevka-slab-extralight.ttc'
+  font 'iosevka-slab-heavy.ttc'
+  font 'iosevka-slab-light.ttc'
+  font 'iosevka-slab-medium.ttc'
+  font 'iosevka-slab-regular.ttc'
+  font 'iosevka-slab-semibold.ttc'
+  font 'iosevka-slab-thin.ttc'
 end
