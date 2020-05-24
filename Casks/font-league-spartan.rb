22533ec03d067eeab77b20c5770b110f8ca66d03
Vítor Galvão <info@vitorgalvao.com>
2020-05-24 14:09:24 +0100

    Update font-league-spartan.rb

--- Casks/font-league-spartan.rb
+++ Casks/font-league-spartan.rb
@@ -2,10 +2,11 @@ cask 'font-league-spartan' do
   version '2.201'
   sha256 '6c027fb675c3479cc50da28bdd43bdae0debcb40eabdd42a659d4df8bb315d0a'
 
+  # github.com/theleagueof/league-spartan/ was verified as official when first introduced to the cask
   url "https://github.com/theleagueof/league-spartan/archive/#{version}.zip"
   appcast 'https://github.com/theleagueof/league-spartan/releases.atom'
   name 'League Spartan'
-  homepage 'https://github.com/theleagueof/league-spartan'
+  homepage 'https://www.theleagueofmoveabletype.com/league-spartan'
 
   font "league-spartan-#{version}/fonts/static/otf/LeagueSpartan-Black.otf"
   font "league-spartan-#{version}/fonts/static/otf/LeagueSpartan-Bold.otf"
