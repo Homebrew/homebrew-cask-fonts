440eb2630427a49c1a216b5f9847f337ee12913d
An Nyeong <me@annyeong.me>
2020-05-24 12:17:55 +0900

    Update font-neodunggeunmo from 1.40 to 1.50 (#2045)

--- Casks/font-neodunggeunmo.rb
+++ Casks/font-neodunggeunmo.rb
@@ -1,6 +1,6 @@
 cask 'font-neodunggeunmo' do
-  version '1.40'
-  sha256 'e6a50ae85e9cfccce9a03d2db86e245df8586b2baa5fa11b5112690e665952b8'
+  version '1.50'
+  sha256 '67765532d17afe0c5b81bd0f6109e0d2619ba2f15c860f3e8cc9b561287a5b28'
 
   # github.com/Dalgona/neodgm/ was verified as official when first introduced to the cask
   url "https://github.com/Dalgona/neodgm/releases/download/v#{version}/neodgm.ttf"
