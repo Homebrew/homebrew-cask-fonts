1c3ca2790271cd3748804f9ecb10b74c41e2ebd2
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-05-27 20:14:12 +0100

    Update font-charter

--- Casks/font-charter.rb
+++ Casks/font-charter.rb
@@ -1,13 +1,13 @@
 cask 'font-charter' do
-  version :latest
-  sha256 :no_check
+  version '200512'
+  sha256 '39d1615e8b9c71f6e339c4d66f9cd1c6c5e85a63e201cb880f7691fa2d26d3a3'
 
-  url 'https://practicaltypography.com/fonts/charter.zip'
+  url "https://practicaltypography.com/fonts/Charter%20#{version}.zip"
   name 'Charter'
   homepage 'https://practicaltypography.com/charter.html'
 
-  font 'charter/otf/Charter Bold Italic.otf'
-  font 'charter/otf/Charter Bold.otf'
-  font 'charter/otf/Charter Italic.otf'
-  font 'charter/otf/Charter Regular.otf'
+  font 'Charter/Charter/OpenType/Charter Bold Italic.otf'
+  font 'Charter/Charter/OpenType/Charter Bold.otf'
+  font 'Charter/Charter/OpenType/Charter Italic.otf'
+  font 'Charter/Charter/OpenType/Charter Regular.otf'
 end
