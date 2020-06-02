16e08b64b6ebd56284aac7e9f7d890d84cabc873
Reno <25192197+singularitti@users.noreply.github.com>
2020-06-02 13:36:05 -0400

    Add Glow Sans SC fonts (#2075)

---
+++ Casks/font-glow-sans-sc-condensed.rb
@@ -0,0 +1,19 @@
+cask 'font-glow-sans-sc-condensed' do
+  version '0.9'
+  sha256 'c7dc655e73861b92971a405dea919fc6a9c99726ce8080c3118433dca455acc7'
+
+  url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansSC-Condensed-v#{version}.zip"
+  appcast 'https://github.com/welai/glow-sans/releases.atom'
+  name 'Glow Sans SC Condensed'
+  homepage 'https://github.com/welai/glow-sans'
+
+  font 'GlowSansSC-Condensed-Bold.otf'
+  font 'GlowSansSC-Condensed-Book.otf'
+  font 'GlowSansSC-Condensed-ExtraBold.otf'
+  font 'GlowSansSC-Condensed-ExtraLight.otf'
+  font 'GlowSansSC-Condensed-Heavy.otf'
+  font 'GlowSansSC-Condensed-Light.otf'
+  font 'GlowSansSC-Condensed-Medium.otf'
+  font 'GlowSansSC-Condensed-Regular.otf'
+  font 'GlowSansSC-Condensed-Thin.otf'
+end
---
+++ Casks/font-glow-sans-sc-normal.rb
@@ -0,0 +1,19 @@
+cask 'font-glow-sans-sc-normal' do
+  version '0.9'
+  sha256 '0652f5e9bfaf7e4fe19b9c4b605c546e79828cf4779f63467411e907f482213b'
+
+  url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansSC-Normal-v#{version}.zip"
+  appcast 'https://github.com/welai/glow-sans/releases.atom'
+  name 'Glow Sans SC Normal'
+  homepage 'https://github.com/welai/glow-sans'
+
+  font 'GlowSansSC-Normal-Bold.otf'
+  font 'GlowSansSC-Normal-Book.otf'
+  font 'GlowSansSC-Normal-ExtraBold.otf'
+  font 'GlowSansSC-Normal-ExtraLight.otf'
+  font 'GlowSansSC-Normal-Heavy.otf'
+  font 'GlowSansSC-Normal-Light.otf'
+  font 'GlowSansSC-Normal-Medium.otf'
+  font 'GlowSansSC-Normal-Regular.otf'
+  font 'GlowSansSC-Normal-Thin.otf'
+end
