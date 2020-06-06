58b565dbb283916e75077f0845dcbd2258822049
Guill Lo <guill.bout@gmail.com>
2020-06-07 04:04:44 +0800

    update URL and font paths

--- Casks/font-roboto-mono.rb
+++ Casks/font-roboto-mono.rb
@@ -3,20 +3,20 @@ cask 'font-roboto-mono' do
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/tree/master/apache/robotomono/static'
+  url 'https://github.com/google/fonts/tree/master/apache/robotomono'
   name 'Roboto Mono'
   homepage 'https://www.google.com/fonts/specimen/Roboto%20Mono'
 
   depends_on macos: '>= :sierra'
 
-  font 'RobotoMono-Bold.ttf'
-  font 'RobotoMono-BoldItalic.ttf'
-  font 'RobotoMono-Italic.ttf'
-  font 'RobotoMono-Light.ttf'
-  font 'RobotoMono-LightItalic.ttf'
-  font 'RobotoMono-Medium.ttf'
-  font 'RobotoMono-MediumItalic.ttf'
-  font 'RobotoMono-Regular.ttf'
-  font 'RobotoMono-Thin.ttf'
-  font 'RobotoMono-ThinItalic.ttf'
+  font 'static/RobotoMono-Bold.ttf'
+  font 'static/RobotoMono-BoldItalic.ttf'
+  font 'static/RobotoMono-Italic.ttf'
+  font 'static/RobotoMono-Light.ttf'
+  font 'static/RobotoMono-LightItalic.ttf'
+  font 'static/RobotoMono-Medium.ttf'
+  font 'static/RobotoMono-MediumItalic.ttf'
+  font 'static/RobotoMono-Regular.ttf'
+  font 'static/RobotoMono-Thin.ttf'
+  font 'static/RobotoMono-ThinItalic.ttf'
 end
