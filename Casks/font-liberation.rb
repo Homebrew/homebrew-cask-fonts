a30602055cf168d692f504bbba6e505bbeca961d
Vítor Galvão <info@vitorgalvao.com>
2020-06-08 15:59:44 +0100

    Update font-liberation.rb

--- Casks/font-liberation.rb
+++ Casks/font-liberation.rb
@@ -1,8 +1,8 @@
 cask 'font-liberation' do
-  version '2.1.1'
+  version '2.1.1,4743886'
   sha256 '8ee2c16fe0f055e60dd8375449aff72d25dd623b9cc6f24790ce9d2e91446fca'
 
-  url "https://github.com/liberationfonts/liberation-fonts/files/4743886/liberation-fonts-ttf-#{version}.tar.gz"
+  url "https://github.com/liberationfonts/liberation-fonts/files/#{version.after_comma}/liberation-fonts-ttf-#{version.before_comma}.tar.gz"
   appcast 'https://github.com/liberationfonts/liberation-fonts/releases.atom'
   name 'Liberation Sans'
   homepage 'https://github.com/liberationfonts/liberation-fonts'
