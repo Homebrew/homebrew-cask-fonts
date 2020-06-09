74cd808f4a5d695ca8ac14f88429226d0686a179
Nikita Prokopov <niki@tonsky.me>
2020-06-09 17:48:05 +0200

    Update Fira Code to v5 (#2083)

--- Casks/font-fira-code.rb
+++ Casks/font-fira-code.rb
@@ -1,16 +1,16 @@
 cask 'font-fira-code' do
-  version '4'
-  sha256 '46ed45d1a793a56e13d31ed10fb7e09f5277731953a0d9522915644fc59086d8'
+  version '5'
+  sha256 'a095333b5e24d57f6536efb62d5425d3325243808dca410c6755d0cf7c5bd8da'
 
   url "https://github.com/tonsky/FiraCode/releases/download/#{version}/Fira_Code_v#{version}.zip"
   appcast 'https://github.com/tonsky/FiraCode/releases.atom'
   name 'Fira Code'
   homepage 'https://github.com/tonsky/FiraCode'
 
-  font 'otf/FiraCode-Bold.otf'
-  font 'otf/FiraCode-Light.otf'
-  font 'otf/FiraCode-Medium.otf'
-  font 'otf/FiraCode-Regular.otf'
-  font 'otf/FiraCode-Retina.otf'
-  font 'otf/FiraCode-SemiBold.otf'
+  font 'ttf/FiraCode-Bold.ttf'
+  font 'ttf/FiraCode-Light.ttf'
+  font 'ttf/FiraCode-Medium.ttf'
+  font 'ttf/FiraCode-Regular.ttf'
+  font 'ttf/FiraCode-Retina.ttf'
+  font 'ttf/FiraCode-SemiBold.ttf'
 end
