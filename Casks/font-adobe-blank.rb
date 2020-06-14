de282e42cea4a2d486f0ba406fed10f2b2018910
Andrew Dunning <adunning@users.noreply.github.com>
2020-06-14 19:42:05 +0100

    Adobe Blank: Use source repository

--- Casks/font-adobe-blank.rb
+++ Casks/font-adobe-blank.rb
@@ -1,11 +1,10 @@
 cask 'font-adobe-blank' do
-  version :latest
-  sha256 :no_check
+  version '1.045'
+  sha256 'c5ebe5fc6f8258ecf8f987eed3069728e49c34e438714d85a606bf7c9b2b7a1f'
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/adobeblank/AdobeBlank-Regular.ttf'
+  url "https://github.com/adobe-fonts/adobe-blank/archive/#{version}.zip"
   name 'Adobe Blank'
-  homepage 'https://fonts.google.com/specimen/Adobe+Blank'
+  homepage 'https://github.com/adobe-fonts/adobe-blank'
 
-  font 'AdobeBlank-Regular.ttf'
+  font 'adobe-blank-1.045/AdobeBlank.otf'
 end
