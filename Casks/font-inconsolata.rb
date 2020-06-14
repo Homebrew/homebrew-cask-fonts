daeb7b91be564d7465b57c6bf7879f4f7dc79e6f
Andrew Dunning <adunning@users.noreply.github.com>
2020-06-14 23:21:39 +0100

    Inconsolata: Use upstream repository

--- Casks/font-inconsolata.rb
+++ Casks/font-inconsolata.rb
@@ -1,11 +1,11 @@
 cask 'font-inconsolata' do
-  version :latest
-  sha256 :no_check
+  version '3.000'
+  sha256 '33dd95333e9e51ed75c18ccd340f4f7166773ff5303ddedb9d084a623f829d08'
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/inconsolata/Inconsolata%5Bwdth%2Cwght%5D.ttf'
+  url "https://github.com/googlefonts/Inconsolata/releases/download/v#{version}/Inconsolata-VF.ttf"
+  appcast 'https://github.com/googlefonts/Inconsolata/releases.atom'
   name 'Inconsolata'
-  homepage 'https://fonts.google.com/specimen/Inconsolata'
+  homepage 'https://github.com/googlefonts/Inconsolata'
 
-  font 'Inconsolata[wdth,wght].ttf'
+  font 'Inconsolata-VF.ttf'
 end
