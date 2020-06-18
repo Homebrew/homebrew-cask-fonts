4002bfa454011d0bb83f8897637055bbe5f9550a
Andrew Dunning <adunning@users.noreply.github.com>
2020-06-18 18:00:34 +0100

    Update Source Sans Pro to 3.006R (#2105)

--- Casks/font-source-sans-pro.rb
+++ Casks/font-source-sans-pro.rb
@@ -1,12 +1,11 @@
 cask 'font-source-sans-pro' do
-  version '2.045R-ro-1.095R-it'
-  sha256 '5f090b821e117f452ef399c3c1b8b244c1647728a1e901c4d167cbf7fc298ab6'
+  version '3.006R'
+  sha256 'b22c7a4c45050ed8ae1b5293a9fd3b886d6f5150eb1e2ded429b9a11ac1602c5'
 
-  # github.com/adobe-fonts/source-sans-pro/ was verified as official when first introduced to the cask
-  url "https://github.com/adobe-fonts/source-sans-pro/archive/#{version.sub('ro-', 'ro/')}.zip"
+  url "https://github.com/adobe-fonts/source-sans-pro/releases/download/#{version}/source-sans-pro-#{version}.zip"
   appcast 'https://github.com/adobe-fonts/source-sans-pro/releases.atom'
   name 'Source Sans Pro'
-  homepage 'https://adobe-fonts.github.io/source-sans-pro/'
+  homepage 'https://github.com/adobe-fonts/source-sans-pro'
 
   font "source-sans-pro-#{version}/OTF/SourceSansPro-Black.otf"
   font "source-sans-pro-#{version}/OTF/SourceSansPro-BlackIt.otf"
@@ -20,4 +19,6 @@ cask 'font-source-sans-pro' do
   font "source-sans-pro-#{version}/OTF/SourceSansPro-Regular.otf"
   font "source-sans-pro-#{version}/OTF/SourceSansPro-Semibold.otf"
   font "source-sans-pro-#{version}/OTF/SourceSansPro-SemiboldIt.otf"
+  font "source-sans-pro-#{version}/VAR/SourceSansVariable-Italic.otf"
+  font "source-sans-pro-#{version}/VAR/SourceSansVariable-Roman.otf"
 end
