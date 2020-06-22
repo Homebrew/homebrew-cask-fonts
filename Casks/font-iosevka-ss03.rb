4f41f455342f9a439dc6943ea6d46f604fc4a90a
Reno <25192197+singularitti@users.noreply.github.com>
2020-06-22 09:17:31 -0400

    Update font-iosevka-ss03 from 3.2.1 to 3.2.2 (#2166)

--- Casks/font-iosevka-ss03.rb
+++ Casks/font-iosevka-ss03.rb
@@ -1,6 +1,6 @@
 cask 'font-iosevka-ss03' do
-  version '3.2.1'
-  sha256 '843cca4cba9aec3e7e99d1474b9c19c22d435c9b17f55e99d08008c8a258e500'
+  version '3.2.2'
+  sha256 '02ccc193426b9ad4ef69165cd24cfb9754f3c405fcdd9714f7263f8de35f2dee'
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss03-#{version}.zip"
   appcast 'https://github.com/be5invis/Iosevka/releases.atom'
