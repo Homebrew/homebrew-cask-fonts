585e32dad8046d110b27c00398661b71826322a3
Reno <25192197+singularitti@users.noreply.github.com>
2020-06-22 01:47:30 -0400

    Update font-iosevka-etoile from 3.2.0 to 3.2.1 (#2154)

--- Casks/font-iosevka-etoile.rb
+++ Casks/font-iosevka-etoile.rb
@@ -1,6 +1,6 @@
 cask 'font-iosevka-etoile' do
-  version '3.2.0'
-  sha256 'c8c5940f199644b2e9b8a466c96e8ab2ba386f8e1423f14048bd2675d5185ecc'
+  version '3.2.1'
+  sha256 '2bc0890b1ebeddbad082f424827f54fb71895ae8d5bcfc728c791b0954cea877'
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-etoile-#{version}.zip"
   appcast 'https://github.com/be5invis/Iosevka/releases.atom'
