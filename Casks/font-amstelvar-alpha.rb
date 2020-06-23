48f6fe2ff7de18be0b6d89961c30b2130738a914
Aidan Feldman <aidan.feldman@gmail.com>
2020-06-23 02:00:50 -0400

    leave -alpha on google fonts

--- Casks/font-amstelvar-alpha.rb
+++ Casks/font-amstelvar-alpha.rb
@@ -1,10 +1,10 @@
-cask 'font-amstelvar' do
+cask 'font-amstelvar-alpha' do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
   url 'https://github.com/google/fonts/raw/master/ofl/amstelvaralpha/AmstelvarAlpha-VF.ttf'
-  name 'Amstelvar'
+  name 'Amstelvar Alpha'
   homepage 'https://fonts.google.com/earlyaccess'
 
   font 'AmstelvarAlpha-VF.ttf'
