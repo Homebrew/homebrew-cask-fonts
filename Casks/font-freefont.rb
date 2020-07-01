26c72a60bd9a1f16f30bf99d67a93f80272de028
Waldir Pimenta <waldyrious@gmail.com>
2020-07-01 03:15:23 +0100

    Rename font-freesans.rb to font-freefont.rb (#2180)
    This cask provides not just FreeSans, but also FreeMono and FreeSerif.
    The actual project name is FreeFont.
    
    I also took the opportunity to update the homepage stanza
    to point to the actual project website (rather than the FTP directory).

--- Casks/font-freefont.rb
+++ Casks/font-freefont.rb
@@ -1,10 +1,10 @@
-cask 'font-freesans' do
+cask 'font-freefont' do
   version '20120503'
   sha256 '3a6c51868c71b006c33c4bcde63d90927e6fcca8f51c965b8ad62d021614a860'
 
   url "https://ftp.gnu.org/gnu/freefont/freefont-otf-#{version}.tar.gz"
-  name 'FreeSans'
-  homepage 'https://ftp.gnu.org/gnu/freefont/'
+  name 'FreeFont'
+  homepage 'https://www.gnu.org/software/freefont/'
 
   font "freefont-#{version}/FreeMono.otf"
   font "freefont-#{version}/FreeMonoBold.otf"
