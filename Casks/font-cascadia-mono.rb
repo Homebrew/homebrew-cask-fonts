f3aa6b72bcc58bc57f23c6b8c0c1bc2af3f9e098
Max <msal.coding@gmail.com>
2020-07-01 15:21:49 +0200

    fix broken download link

--- Casks/font-cascadia-mono.rb
+++ Casks/font-cascadia-mono.rb
@@ -2,7 +2,7 @@ cask 'font-cascadia-mono' do
   version '2007.01'
   sha256 '9f066d0d9cb2669bea2e130d7add43d496bf24ef995f42dc603fc2014574a3a4'
 
-  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode_#{version}.zip"
+  url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
   appcast 'https://github.com/microsoft/cascadia-code/releases.atom'
   name 'Cascadia Mono'
   homepage 'https://github.com/microsoft/cascadia-code'
