8f39c99dd7ab6405e371e610614a1016f2d4b58a
singularitti <singularitti@outlook.com>
2020-07-14 23:22:16 -0400

    Update font-iosevka-ss01 from 3.2.2 to 3.3.0

--- Casks/font-iosevka-ss01.rb
+++ Casks/font-iosevka-ss01.rb
@@ -1,6 +1,6 @@
 cask 'font-iosevka-ss01' do
-  version '3.2.2'
-  sha256 '3b93c728a375fddc7c50205e84d81ef551615d1395d66cbf9bca507d9822d5e3'
+  version '3.3.0'
+  sha256 'a6bc69d54d104bf750efec318cf372074d115b5c48982e7e2c1ff14c732caf05'
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss01-#{version}.zip"
   appcast 'https://github.com/be5invis/Iosevka/releases.atom'
