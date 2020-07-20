cc734e3d0cdff76e8529bc5637648f554fee2546
Birger J. Nordølum <contact@mindtooth.no>
2020-07-20 13:34:54 +0200

    Update font-spleen from 1.8.0 to 1.8.1

--- Casks/font-spleen.rb
+++ Casks/font-spleen.rb
@@ -1,11 +1,15 @@
 cask 'font-spleen' do
-  version '1.8.0'
-  sha256 'e761249e859ec3cebacb79b6ea2f0e51701316866305940b2431e61157207747'
+  version '1.8.1'
+  sha256 '59119044399b2262fdf4c504c6f1a8b1230d349dfd07c68a2fb07a394a880e1e'
 
   url "https://github.com/fcambus/spleen/releases/download/#{version}/spleen-#{version}.tar.gz"
   appcast 'https://github.com/fcambus/spleen/releases.atom'
   name 'Spleen'
   homepage 'https://github.com/fcambus/spleen'
 
+  font "spleen-#{version}/spleen-6x12.otf"
+  font "spleen-#{version}/spleen-8x16.otf"
+  font "spleen-#{version}/spleen-12x24.otf"
+  font "spleen-#{version}/spleen-16x32.otf"
   font "spleen-#{version}/spleen-32x64.otf"
 end
