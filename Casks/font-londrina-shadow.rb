c1a08e1c1122fbdc3ffd66ba0471faa875e2f798
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:36 +0100

    font-londrina-shadow.rb: fix for new style

--- Casks/font-londrina-shadow.rb
+++ Casks/font-londrina-shadow.rb
@@ -1,11 +1,11 @@
-cask 'font-londrina-shadow' do
+cask "font-londrina-shadow" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/londrinashadow/LondrinaShadow-Regular.ttf'
-  name 'Londrina Shadow'
-  homepage 'https://fonts.google.com/specimen/Londrina+Shadow'
+  url "https://github.com/google/fonts/raw/master/ofl/londrinashadow/LondrinaShadow-Regular.ttf"
+  name "Londrina Shadow"
+  homepage "https://fonts.google.com/specimen/Londrina+Shadow"
 
-  font 'LondrinaShadow-Regular.ttf'
+  font "LondrinaShadow-Regular.ttf"
 end
