02d445dde6b7a23d6ebf254d098e481965eee890
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:15 +0100

    font-schoolbell.rb: fix for new style

--- Casks/font-schoolbell.rb
+++ Casks/font-schoolbell.rb
@@ -1,11 +1,11 @@
-cask 'font-schoolbell' do
+cask "font-schoolbell" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/schoolbell/Schoolbell-Regular.ttf'
-  name 'Schoolbell'
-  homepage 'https://fonts.google.com/specimen/Schoolbell'
+  url "https://github.com/google/fonts/raw/master/apache/schoolbell/Schoolbell-Regular.ttf"
+  name "Schoolbell"
+  homepage "https://fonts.google.com/specimen/Schoolbell"
 
-  font 'Schoolbell-Regular.ttf'
+  font "Schoolbell-Regular.ttf"
 end
