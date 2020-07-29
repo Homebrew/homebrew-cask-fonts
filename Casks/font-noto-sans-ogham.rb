ba8868eabaa6ea72df6a087f499e108d48680760
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:55 +0100

    font-noto-sans-ogham.rb: fix for new style

--- Casks/font-noto-sans-ogham.rb
+++ Casks/font-noto-sans-ogham.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-ogham' do
+cask "font-noto-sans-ogham" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOgham-unhinted.zip'
-  name 'Noto Sans Ogham'
-  homepage 'https://www.google.com/get/noto/#sans-ogam'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOgham-unhinted.zip"
+  name "Noto Sans Ogham"
+  homepage "https://www.google.com/get/noto/#sans-ogam"
 
-  font 'NotoSansOgham-Regular.ttf'
+  font "NotoSansOgham-Regular.ttf"
 end
