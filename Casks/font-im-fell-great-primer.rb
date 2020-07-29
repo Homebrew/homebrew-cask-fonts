7e819954c9e80ad64ed3f14afab277cca596a12c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:15 +0100

    font-im-fell-great-primer.rb: fix for new style

--- Casks/font-im-fell-great-primer.rb
+++ Casks/font-im-fell-great-primer.rb
@@ -1,16 +1,16 @@
-cask 'font-im-fell-great-primer' do
+cask "font-im-fell-great-primer" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/imfellgreatprimer',
+  url "https://github.com/google/fonts/trunk/ofl/imfellgreatprimer",
       using:      :svn,
       trust_cert: true
-  name 'IM Fell Great Primer'
-  homepage 'https://fonts.google.com/specimen/IM+Fell+Great+Primer'
+  name "IM Fell Great Primer"
+  homepage "https://fonts.google.com/specimen/IM+Fell+Great+Primer"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'IMFeGPit28P.ttf'
-  font 'IMFeGPrm28P.ttf'
+  font "IMFeGPit28P.ttf"
+  font "IMFeGPrm28P.ttf"
 end
