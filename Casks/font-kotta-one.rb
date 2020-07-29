01983489dbcae397bbc45585c80d5386ba345469
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:29 +0100

    font-kotta-one.rb: fix for new style

--- Casks/font-kotta-one.rb
+++ Casks/font-kotta-one.rb
@@ -1,11 +1,11 @@
-cask 'font-kotta-one' do
+cask "font-kotta-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/kottaone/KottaOne-Regular.ttf'
-  name 'Kotta One'
-  homepage 'https://fonts.google.com/specimen/Kotta+One'
+  url "https://github.com/google/fonts/raw/master/ofl/kottaone/KottaOne-Regular.ttf"
+  name "Kotta One"
+  homepage "https://fonts.google.com/specimen/Kotta+One"
 
-  font 'KottaOne-Regular.ttf'
+  font "KottaOne-Regular.ttf"
 end
