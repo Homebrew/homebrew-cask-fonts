f3ed4626e4c71395056c3f815c01ae5b1ba0263c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:13 +0100

    font-rufina.rb: fix for new style

--- Casks/font-rufina.rb
+++ Casks/font-rufina.rb
@@ -1,16 +1,16 @@
-cask 'font-rufina' do
+cask "font-rufina" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/rufina',
+  url "https://github.com/google/fonts/trunk/ofl/rufina",
       using:      :svn,
       trust_cert: true
-  name 'Rufina'
-  homepage 'https://fonts.google.com/specimen/Rufina'
+  name "Rufina"
+  homepage "https://fonts.google.com/specimen/Rufina"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Rufina-Bold.ttf'
-  font 'Rufina-Regular.ttf'
+  font "Rufina-Bold.ttf"
+  font "Rufina-Regular.ttf"
 end
