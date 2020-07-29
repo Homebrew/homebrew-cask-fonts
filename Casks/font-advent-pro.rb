657654a9b72e21c65adfd4d3835bc7b888559882
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:42 +0100

    font-advent-pro.rb: fix for new style

--- Casks/font-advent-pro.rb
+++ Casks/font-advent-pro.rb
@@ -1,21 +1,21 @@
-cask 'font-advent-pro' do
+cask "font-advent-pro" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/adventpro',
+  url "https://github.com/google/fonts/trunk/ofl/adventpro",
       using:      :svn,
       trust_cert: true
-  name 'Advent Pro'
-  homepage 'https://fonts.google.com/specimen/Advent+Pro'
+  name "Advent Pro"
+  homepage "https://fonts.google.com/specimen/Advent+Pro"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'AdventPro-Bold.ttf'
-  font 'AdventPro-ExtraLight.ttf'
-  font 'AdventPro-Light.ttf'
-  font 'AdventPro-Medium.ttf'
-  font 'AdventPro-Regular.ttf'
-  font 'AdventPro-SemiBold.ttf'
-  font 'AdventPro-Thin.ttf'
+  font "AdventPro-Bold.ttf"
+  font "AdventPro-ExtraLight.ttf"
+  font "AdventPro-Light.ttf"
+  font "AdventPro-Medium.ttf"
+  font "AdventPro-Regular.ttf"
+  font "AdventPro-SemiBold.ttf"
+  font "AdventPro-Thin.ttf"
 end
