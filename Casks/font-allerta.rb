0a5bf97d2d55c2713174ce20848c99182a9d9ddb
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:02 +0100

    font-allerta.rb: fix for new style

--- Casks/font-allerta.rb
+++ Casks/font-allerta.rb
@@ -1,11 +1,11 @@
-cask 'font-allerta' do
+cask "font-allerta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/allerta/Allerta-Regular.ttf'
-  name 'Allerta'
-  homepage 'https://fonts.google.com/specimen/Allerta'
+  url "https://github.com/google/fonts/raw/master/ofl/allerta/Allerta-Regular.ttf"
+  name "Allerta"
+  homepage "https://fonts.google.com/specimen/Allerta"
 
-  font 'Allerta-Regular.ttf'
+  font "Allerta-Regular.ttf"
 end
