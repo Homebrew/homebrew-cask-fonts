f62cf3acf10d36fb9e37eda42ef1f622802996da
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:06 +0100

    font-prata.rb: fix for new style

--- Casks/font-prata.rb
+++ Casks/font-prata.rb
@@ -1,11 +1,11 @@
-cask 'font-prata' do
+cask "font-prata" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/prata/Prata-Regular.ttf'
-  name 'Prata'
-  homepage 'https://fonts.google.com/specimen/Prata'
+  url "https://github.com/google/fonts/raw/master/ofl/prata/Prata-Regular.ttf"
+  name "Prata"
+  homepage "https://fonts.google.com/specimen/Prata"
 
-  font 'Prata-Regular.ttf'
+  font "Prata-Regular.ttf"
 end
