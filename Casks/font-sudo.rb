6b1c58c6cf8be6b4defce7cea7245d5ee3db6049
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:21 +0100

    font-sudo.rb: fix for new style

--- Casks/font-sudo.rb
+++ Casks/font-sudo.rb
@@ -1,11 +1,11 @@
-cask 'font-sudo' do
-  version '0.42'
-  sha256 'ae7979cce3f69160293416089b91fe9f5f20bd25a288099ba61f067930df3d64'
+cask "font-sudo" do
+  version "0.42"
+  sha256 "ae7979cce3f69160293416089b91fe9f5f20bd25a288099ba61f067930df3d64"
 
   url "https://github.com/jenskutilek/sudo-font/releases/download/v#{version}/sudo.zip"
-  appcast 'https://github.com/jenskutilek/sudo-font/releases.atom'
-  name 'Sudo'
-  homepage 'https://github.com/jenskutilek/sudo-font/'
+  appcast "https://github.com/jenskutilek/sudo-font/releases.atom"
+  name "Sudo"
+  homepage "https://github.com/jenskutilek/sudo-font/"
 
-  font 'sudo/SudoVariable.ttf'
+  font "sudo/SudoVariable.ttf"
 end
