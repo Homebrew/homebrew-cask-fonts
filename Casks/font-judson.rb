39ea0b5e916caed5726472481d3cb93da608f46b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:25 +0100

    font-judson.rb: fix for new style

--- Casks/font-judson.rb
+++ Casks/font-judson.rb
@@ -1,17 +1,17 @@
-cask 'font-judson' do
+cask "font-judson" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/judson',
+  url "https://github.com/google/fonts/trunk/ofl/judson",
       using:      :svn,
       trust_cert: true
-  name 'Judson'
-  homepage 'https://fonts.google.com/specimen/Judson'
+  name "Judson"
+  homepage "https://fonts.google.com/specimen/Judson"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Judson-Bold.ttf'
-  font 'Judson-Italic.ttf'
-  font 'Judson-Regular.ttf'
+  font "Judson-Bold.ttf"
+  font "Judson-Italic.ttf"
+  font "Judson-Regular.ttf"
 end
