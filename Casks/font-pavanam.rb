d9be3cb2a93b4a22b689c40bc75ab2410c56c5ec
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:04 +0100

    font-pavanam.rb: fix for new style

--- Casks/font-pavanam.rb
+++ Casks/font-pavanam.rb
@@ -1,11 +1,11 @@
-cask 'font-pavanam' do
+cask "font-pavanam" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/pavanam/Pavanam-Regular.ttf'
-  name 'Pavanam'
-  homepage 'https://fonts.google.com/specimen/Pavanam'
+  url "https://github.com/google/fonts/raw/master/ofl/pavanam/Pavanam-Regular.ttf"
+  name "Pavanam"
+  homepage "https://fonts.google.com/specimen/Pavanam"
 
-  font 'Pavanam-Regular.ttf'
+  font "Pavanam-Regular.ttf"
 end
