950bd4f253abb868e70a660a6a9e6f44cd437307
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:22 +0100

    font-bowlby-one-sc.rb: fix for new style

--- Casks/font-bowlby-one-sc.rb
+++ Casks/font-bowlby-one-sc.rb
@@ -1,11 +1,11 @@
-cask 'font-bowlby-one-sc' do
+cask "font-bowlby-one-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bowlbyonesc/BowlbyOneSC-Regular.ttf'
-  name 'Bowlby One SC'
-  homepage 'https://fonts.google.com/specimen/Bowlby+One+SC'
+  url "https://github.com/google/fonts/raw/master/ofl/bowlbyonesc/BowlbyOneSC-Regular.ttf"
+  name "Bowlby One SC"
+  homepage "https://fonts.google.com/specimen/Bowlby+One+SC"
 
-  font 'BowlbyOneSC-Regular.ttf'
+  font "BowlbyOneSC-Regular.ttf"
 end
