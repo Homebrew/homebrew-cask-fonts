0c2494d2a28ae991a197895fb6c91cd48f161b29
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:50 +0100

    font-fahkwang.rb: fix for new style

--- Casks/font-fahkwang.rb
+++ Casks/font-fahkwang.rb
@@ -1,26 +1,26 @@
-cask 'font-fahkwang' do
+cask "font-fahkwang" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/fahkwang',
+  url "https://github.com/google/fonts/trunk/ofl/fahkwang",
       using:      :svn,
       trust_cert: true
-  name 'Fahkwang'
-  homepage 'https://fonts.google.com/specimen/Fahkwang'
+  name "Fahkwang"
+  homepage "https://fonts.google.com/specimen/Fahkwang"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Fahkwang-Bold.ttf'
-  font 'Fahkwang-BoldItalic.ttf'
-  font 'Fahkwang-ExtraLight.ttf'
-  font 'Fahkwang-ExtraLightItalic.ttf'
-  font 'Fahkwang-Italic.ttf'
-  font 'Fahkwang-Light.ttf'
-  font 'Fahkwang-LightItalic.ttf'
-  font 'Fahkwang-Medium.ttf'
-  font 'Fahkwang-MediumItalic.ttf'
-  font 'Fahkwang-Regular.ttf'
-  font 'Fahkwang-SemiBold.ttf'
-  font 'Fahkwang-SemiBoldItalic.ttf'
+  font "Fahkwang-Bold.ttf"
+  font "Fahkwang-BoldItalic.ttf"
+  font "Fahkwang-ExtraLight.ttf"
+  font "Fahkwang-ExtraLightItalic.ttf"
+  font "Fahkwang-Italic.ttf"
+  font "Fahkwang-Light.ttf"
+  font "Fahkwang-LightItalic.ttf"
+  font "Fahkwang-Medium.ttf"
+  font "Fahkwang-MediumItalic.ttf"
+  font "Fahkwang-Regular.ttf"
+  font "Fahkwang-SemiBold.ttf"
+  font "Fahkwang-SemiBoldItalic.ttf"
 end
