9d5c4397a30188282b8585f29528533c52a6a429
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:17 +0100

    font-inika.rb: fix for new style

--- Casks/font-inika.rb
+++ Casks/font-inika.rb
@@ -1,16 +1,16 @@
-cask 'font-inika' do
+cask "font-inika" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/inika',
+  url "https://github.com/google/fonts/trunk/ofl/inika",
       using:      :svn,
       trust_cert: true
-  name 'Inika'
-  homepage 'https://fonts.google.com/specimen/Inika'
+  name "Inika"
+  homepage "https://fonts.google.com/specimen/Inika"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Inika-Bold.ttf'
-  font 'Inika-Regular.ttf'
+  font "Inika-Bold.ttf"
+  font "Inika-Regular.ttf"
 end
