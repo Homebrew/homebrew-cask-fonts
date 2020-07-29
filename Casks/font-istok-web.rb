3aeff99397639e2859a879678c0162accd1e6199
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:21 +0100

    font-istok-web.rb: fix for new style

--- Casks/font-istok-web.rb
+++ Casks/font-istok-web.rb
@@ -1,18 +1,18 @@
-cask 'font-istok-web' do
+cask "font-istok-web" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/istokweb',
+  url "https://github.com/google/fonts/trunk/ofl/istokweb",
       using:      :svn,
       trust_cert: true
-  name 'Istok Web'
-  homepage 'https://fonts.google.com/specimen/Istok+Web'
+  name "Istok Web"
+  homepage "https://fonts.google.com/specimen/Istok+Web"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'IstokWeb-Bold.ttf'
-  font 'IstokWeb-BoldItalic.ttf'
-  font 'IstokWeb-Italic.ttf'
-  font 'IstokWeb-Regular.ttf'
+  font "IstokWeb-Bold.ttf"
+  font "IstokWeb-BoldItalic.ttf"
+  font "IstokWeb-Italic.ttf"
+  font "IstokWeb-Regular.ttf"
 end
