5e167ea09bfbea49f5619d90040bc06fdfd47800
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:05 +0100

    font-podkova-vf-beta.rb: fix for new style

--- Casks/font-podkova-vf-beta.rb
+++ Casks/font-podkova-vf-beta.rb
@@ -1,11 +1,11 @@
-cask 'font-podkova-vf-beta' do
+cask "font-podkova-vf-beta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/podkovavfbeta/PodkovaVFBeta.ttf'
-  name 'Podkova VF Beta'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/podkovavfbeta/PodkovaVFBeta.ttf"
+  name "Podkova VF Beta"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'PodkovaVFBeta.ttf'
+  font "PodkovaVFBeta.ttf"
 end
