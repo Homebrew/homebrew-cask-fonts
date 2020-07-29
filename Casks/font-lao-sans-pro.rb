f6c3410b8396b6dcddf0ce764b0e9f22f5a7d689
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:31 +0100

    font-lao-sans-pro.rb: fix for new style

--- Casks/font-lao-sans-pro.rb
+++ Casks/font-lao-sans-pro.rb
@@ -1,11 +1,11 @@
-cask 'font-lao-sans-pro' do
+cask "font-lao-sans-pro" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/laosanspro/LaoSansPro-Regular.ttf'
-  name 'Lao Sans Pro'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/laosanspro/LaoSansPro-Regular.ttf"
+  name "Lao Sans Pro"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'LaoSansPro-Regular.ttf'
+  font "LaoSansPro-Regular.ttf"
 end
