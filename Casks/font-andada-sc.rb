df50ba34abd9d9e2095c4936fdd5e529c311a6a2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:51 +0100

    font-andada-sc.rb: fix for new style

--- Casks/font-andada-sc.rb
+++ Casks/font-andada-sc.rb
@@ -1,18 +1,18 @@
-cask 'font-andada-sc' do
+cask "font-andada-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/andadasc',
+  url "https://github.com/google/fonts/trunk/ofl/andadasc",
       using:      :svn,
       trust_cert: true
-  name 'Andada SC'
-  homepage 'https://fonts.google.com/specimen/Andada+SC'
+  name "Andada SC"
+  homepage "https://fonts.google.com/specimen/Andada+SC"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'AndadaSC-Bold.ttf'
-  font 'AndadaSC-BoldItalic.ttf'
-  font 'AndadaSC-Italic.ttf'
-  font 'AndadaSC-Regular.ttf'
+  font "AndadaSC-Bold.ttf"
+  font "AndadaSC-BoldItalic.ttf"
+  font "AndadaSC-Italic.ttf"
+  font "AndadaSC-Regular.ttf"
 end
