38e18d12d84019a4984a8190494a95dd74fe7bd1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:29 +0100

    font-changa.rb: fix for new style

--- Casks/font-changa.rb
+++ Casks/font-changa.rb
@@ -1,11 +1,11 @@
-cask 'font-changa' do
+cask "font-changa" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/changa/Changa%5Bwght%5D.ttf'
-  name 'Changa'
-  homepage 'https://fonts.google.com/specimen/Changa'
+  url "https://github.com/google/fonts/raw/master/ofl/changa/Changa%5Bwght%5D.ttf"
+  name "Changa"
+  homepage "https://fonts.google.com/specimen/Changa"
 
-  font 'Changa[wght].ttf'
+  font "Changa[wght].ttf"
 end
