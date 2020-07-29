dfac66a1527536cc1a57656e6218782c0f654cdd
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:03 +0100

    font-ovo.rb: fix for new style

--- Casks/font-ovo.rb
+++ Casks/font-ovo.rb
@@ -1,11 +1,11 @@
-cask 'font-ovo' do
+cask "font-ovo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/ovo/Ovo-Regular.ttf'
-  name 'Ovo'
-  homepage 'https://fonts.google.com/specimen/Ovo'
+  url "https://github.com/google/fonts/raw/master/ofl/ovo/Ovo-Regular.ttf"
+  name "Ovo"
+  homepage "https://fonts.google.com/specimen/Ovo"
 
-  font 'Ovo-Regular.ttf'
+  font "Ovo-Regular.ttf"
 end
