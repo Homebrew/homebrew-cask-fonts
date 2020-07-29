022edbe6a3d57204faaa402f8510d4e62db16db2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:00 +0100

    font-ntr.rb: fix for new style

--- Casks/font-ntr.rb
+++ Casks/font-ntr.rb
@@ -1,11 +1,11 @@
-cask 'font-ntr' do
+cask "font-ntr" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/ntr/NTR-Regular.ttf'
-  name 'NTR'
-  homepage 'https://fonts.google.com/specimen/NTR'
+  url "https://github.com/google/fonts/raw/master/ofl/ntr/NTR-Regular.ttf"
+  name "NTR"
+  homepage "https://fonts.google.com/specimen/NTR"
 
-  font 'NTR-Regular.ttf'
+  font "NTR-Regular.ttf"
 end
