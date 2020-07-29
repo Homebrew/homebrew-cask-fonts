8be774797cade34c0cabd1313fb786d7e3599d6c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:57 +0100

    font-noto-sans-telugu.rb: fix for new style

--- Casks/font-noto-sans-telugu.rb
+++ Casks/font-noto-sans-telugu.rb
@@ -1,12 +1,12 @@
-cask 'font-noto-sans-telugu' do
+cask "font-noto-sans-telugu" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTelugu-unhinted.zip'
-  name 'Noto Sans Telugu'
-  homepage 'https://www.google.com/get/noto/#sans-telu'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTelugu-unhinted.zip"
+  name "Noto Sans Telugu"
+  homepage "https://www.google.com/get/noto/#sans-telu"
 
-  font 'NotoSansTelugu-Regular.ttf'
-  font 'NotoSansTelugu-Bold.ttf'
+  font "NotoSansTelugu-Regular.ttf"
+  font "NotoSansTelugu-Bold.ttf"
 end
