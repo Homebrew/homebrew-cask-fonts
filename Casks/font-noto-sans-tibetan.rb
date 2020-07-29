d6455962da25ac21130daf30cb283f50508acf99
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:57 +0100

    font-noto-sans-tibetan.rb: fix for new style

--- Casks/font-noto-sans-tibetan.rb
+++ Casks/font-noto-sans-tibetan.rb
@@ -1,12 +1,12 @@
-cask 'font-noto-sans-tibetan' do
+cask "font-noto-sans-tibetan" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTibetan-unhinted.zip'
-  name 'Noto Sans Tibetan'
-  homepage 'https://www.google.com/get/noto/#sans-tibt'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTibetan-unhinted.zip"
+  name "Noto Sans Tibetan"
+  homepage "https://www.google.com/get/noto/#sans-tibt"
 
-  font 'NotoSansTibetan-Bold.ttf'
-  font 'NotoSansTibetan-Regular.ttf'
+  font "NotoSansTibetan-Bold.ttf"
+  font "NotoSansTibetan-Regular.ttf"
 end
