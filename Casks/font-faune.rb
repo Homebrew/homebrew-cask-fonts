70c35fb389ac7620c586eafaefb2cbd4d116908a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:52 +0100

    font-faune.rb: fix for new style

--- Casks/font-faune.rb
+++ Casks/font-faune.rb
@@ -1,15 +1,15 @@
-cask 'font-faune' do
-  version '1.000'
-  sha256 '416ef022cc944628c0907d16d26fab849bc783332d660e57016be19b85fea3b9'
+cask "font-faune" do
+  version "1.000"
+  sha256 "416ef022cc944628c0907d16d26fab849bc783332d660e57016be19b85fea3b9"
 
-  url 'http://www.cnap.graphismeenfrance.fr/faune/Faune_Fonts.zip'
-  name 'Faune'
-  homepage 'http://www.cnap.graphismeenfrance.fr/faune/en.html'
+  url "http://www.cnap.graphismeenfrance.fr/faune/Faune_Fonts.zip"
+  name "Faune"
+  homepage "http://www.cnap.graphismeenfrance.fr/faune/en.html"
 
-  font 'Faune_Fonts/Otf/Faune-DisplayBlack.otf'
-  font 'Faune_Fonts/Otf/Faune-DisplayBoldItalic.otf'
-  font 'Faune_Fonts/Otf/Faune-DisplayThin.otf'
-  font 'Faune_Fonts/Otf/Faune-TextBold.otf'
-  font 'Faune_Fonts/Otf/Faune-TextItalic.otf'
-  font 'Faune_Fonts/Otf/Faune-TextRegular.otf'
+  font "Faune_Fonts/Otf/Faune-DisplayBlack.otf"
+  font "Faune_Fonts/Otf/Faune-DisplayBoldItalic.otf"
+  font "Faune_Fonts/Otf/Faune-DisplayThin.otf"
+  font "Faune_Fonts/Otf/Faune-TextBold.otf"
+  font "Faune_Fonts/Otf/Faune-TextItalic.otf"
+  font "Faune_Fonts/Otf/Faune-TextRegular.otf"
 end
