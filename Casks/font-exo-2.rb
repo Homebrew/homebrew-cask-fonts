14bc1c839b19fe4f9296223beaca541bc73d4664
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:50 +0100

    font-exo-2.rb: fix for new style

--- Casks/font-exo-2.rb
+++ Casks/font-exo-2.rb
@@ -1,16 +1,16 @@
-cask 'font-exo-2' do
+cask "font-exo-2" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/exo2',
+  url "https://github.com/google/fonts/trunk/ofl/exo2",
       using:      :svn,
       trust_cert: true
-  name 'Exo 2'
-  homepage 'https://fonts.google.com/specimen/Exo+2'
+  name "Exo 2"
+  homepage "https://fonts.google.com/specimen/Exo+2"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Exo2-Italic[wght].ttf'
-  font 'Exo2[wght].ttf'
+  font "Exo2-Italic[wght].ttf"
+  font "Exo2[wght].ttf"
 end
