e58a2fa4e819663824fdceed2e2919cf43b23fbb
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:43 +0100

    font-amita.rb: fix for new style

--- Casks/font-amita.rb
+++ Casks/font-amita.rb
@@ -1,16 +1,16 @@
-cask 'font-amita' do
+cask "font-amita" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/amita',
+  url "https://github.com/google/fonts/trunk/ofl/amita",
       using:      :svn,
       trust_cert: true
-  name 'Amita'
-  homepage 'https://fonts.google.com/specimen/Amita'
+  name "Amita"
+  homepage "https://fonts.google.com/specimen/Amita"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Amita-Bold.ttf'
-  font 'Amita-Regular.ttf'
+  font "Amita-Bold.ttf"
+  font "Amita-Regular.ttf"
 end
