71ac1244bf319263b51f2f9577a5f9e7b1c0c1ba
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:47 +0100

    font-mukta.rb: fix for new style

--- Casks/font-mukta.rb
+++ Casks/font-mukta.rb
@@ -1,21 +1,21 @@
-cask 'font-mukta' do
+cask "font-mukta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/mukta',
+  url "https://github.com/google/fonts/trunk/ofl/mukta",
       using:      :svn,
       trust_cert: true
-  name 'Mukta'
-  homepage 'https://fonts.google.com/specimen/Mukta'
+  name "Mukta"
+  homepage "https://fonts.google.com/specimen/Mukta"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Mukta-Bold.ttf'
-  font 'Mukta-ExtraBold.ttf'
-  font 'Mukta-ExtraLight.ttf'
-  font 'Mukta-Light.ttf'
-  font 'Mukta-Medium.ttf'
-  font 'Mukta-Regular.ttf'
-  font 'Mukta-SemiBold.ttf'
+  font "Mukta-Bold.ttf"
+  font "Mukta-ExtraBold.ttf"
+  font "Mukta-ExtraLight.ttf"
+  font "Mukta-Light.ttf"
+  font "Mukta-Medium.ttf"
+  font "Mukta-Regular.ttf"
+  font "Mukta-SemiBold.ttf"
 end
