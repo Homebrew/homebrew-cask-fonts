c0e2bb0b29791dba21c23b52f1ae4cda32094918
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:56 +0100

    font-frank-ruhl-libre.rb: fix for new style

--- Casks/font-frank-ruhl-libre.rb
+++ Casks/font-frank-ruhl-libre.rb
@@ -1,19 +1,19 @@
-cask 'font-frank-ruhl-libre' do
+cask "font-frank-ruhl-libre" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/frankruhllibre',
+  url "https://github.com/google/fonts/trunk/ofl/frankruhllibre",
       using:      :svn,
       trust_cert: true
-  name 'Frank Ruhl Libre'
-  homepage 'https://fonts.google.com/specimen/Frank+Ruhl+Libre'
+  name "Frank Ruhl Libre"
+  homepage "https://fonts.google.com/specimen/Frank+Ruhl+Libre"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'FrankRuhlLibre-Black.ttf'
-  font 'FrankRuhlLibre-Bold.ttf'
-  font 'FrankRuhlLibre-Light.ttf'
-  font 'FrankRuhlLibre-Medium.ttf'
-  font 'FrankRuhlLibre-Regular.ttf'
+  font "FrankRuhlLibre-Black.ttf"
+  font "FrankRuhlLibre-Bold.ttf"
+  font "FrankRuhlLibre-Light.ttf"
+  font "FrankRuhlLibre-Medium.ttf"
+  font "FrankRuhlLibre-Regular.ttf"
 end
