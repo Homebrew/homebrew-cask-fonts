2d01a85beacf351184ea2eb75d5e0c04fd45016e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:43 +0100

    font-miriam-libre.rb: fix for new style

--- Casks/font-miriam-libre.rb
+++ Casks/font-miriam-libre.rb
@@ -1,16 +1,16 @@
-cask 'font-miriam-libre' do
+cask "font-miriam-libre" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/miriamlibre',
+  url "https://github.com/google/fonts/trunk/ofl/miriamlibre",
       using:      :svn,
       trust_cert: true
-  name 'Miriam Libre'
-  homepage 'https://fonts.google.com/specimen/Miriam+Libre'
+  name "Miriam Libre"
+  homepage "https://fonts.google.com/specimen/Miriam+Libre"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'MiriamLibre-Bold.ttf'
-  font 'MiriamLibre-Regular.ttf'
+  font "MiriamLibre-Bold.ttf"
+  font "MiriamLibre-Regular.ttf"
 end
