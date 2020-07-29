1bfadcee2a1024d2eb91053f99e96260d9c30c26
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:19 +0100

    font-source-han-sans.rb: fix for new style

--- Casks/font-source-han-sans.rb
+++ Casks/font-source-han-sans.rb
@@ -1,10 +1,10 @@
-cask 'font-source-han-sans' do
-  version '2.001'
-  sha256 '5eb728fc73f86b0e5f4129b6028eaf3cccc5ac4782e93e7c90323f6a550218a0'
+cask "font-source-han-sans" do
+  version "2.001"
+  sha256 "5eb728fc73f86b0e5f4129b6028eaf3cccc5ac4782e93e7c90323f6a550218a0"
 
   url "https://github.com/adobe-fonts/source-han-sans/raw/#{version}R/SuperOTC/SourceHanSans.ttc.zip"
-  name 'Source Han Sans'
-  homepage 'https://github.com/adobe-fonts/source-han-sans'
+  name "Source Han Sans"
+  homepage "https://github.com/adobe-fonts/source-han-sans"
 
-  font 'SourceHanSans.ttc'
+  font "SourceHanSans.ttc"
 end
