22e0999ebe829eedfafcc9a07fb86b557520d30f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:07 +0100

    font-prociono.rb: fix for new style

--- Casks/font-prociono.rb
+++ Casks/font-prociono.rb
@@ -1,11 +1,11 @@
-cask 'font-prociono' do
+cask "font-prociono" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/prociono/Prociono-Regular.ttf'
-  name 'Prociono'
-  homepage 'https://fonts.google.com/specimen/Prociono'
+  url "https://github.com/google/fonts/raw/master/ofl/prociono/Prociono-Regular.ttf"
+  name "Prociono"
+  homepage "https://fonts.google.com/specimen/Prociono"
 
-  font 'Prociono-Regular.ttf'
+  font "Prociono-Regular.ttf"
 end
