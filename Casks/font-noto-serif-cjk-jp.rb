1cf5e9c3ad607478b58cc50f0e8ccc531d76e90f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:58 +0100

    font-noto-serif-cjk-jp.rb: fix for new style

--- Casks/font-noto-serif-cjk-jp.rb
+++ Casks/font-noto-serif-cjk-jp.rb
@@ -1,17 +1,17 @@
-cask 'font-noto-serif-cjk-jp' do
+cask "font-noto-serif-cjk-jp" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKjp-hinted.zip'
-  name 'Noto Serif CJK JP'
-  homepage 'https://www.google.com/get/noto/#serif-jpan'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKjp-hinted.zip"
+  name "Noto Serif CJK JP"
+  homepage "https://www.google.com/get/noto/#serif-jpan"
 
-  font 'NotoSerifCJKjp-Black.otf'
-  font 'NotoSerifCJKjp-Bold.otf'
-  font 'NotoSerifCJKjp-ExtraLight.otf'
-  font 'NotoSerifCJKjp-Light.otf'
-  font 'NotoSerifCJKjp-Medium.otf'
-  font 'NotoSerifCJKjp-Regular.otf'
-  font 'NotoSerifCJKjp-SemiBold.otf'
+  font "NotoSerifCJKjp-Black.otf"
+  font "NotoSerifCJKjp-Bold.otf"
+  font "NotoSerifCJKjp-ExtraLight.otf"
+  font "NotoSerifCJKjp-Light.otf"
+  font "NotoSerifCJKjp-Medium.otf"
+  font "NotoSerifCJKjp-Regular.otf"
+  font "NotoSerifCJKjp-SemiBold.otf"
 end
