f6696c3de0afbfd5fe478cc6e2e62f636f182735
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:01 +0100

    font-open-sans-hebrew.rb: fix for new style

--- Casks/font-open-sans-hebrew.rb
+++ Casks/font-open-sans-hebrew.rb
@@ -1,22 +1,22 @@
-cask 'font-open-sans-hebrew' do
+cask "font-open-sans-hebrew" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/apache/opensanshebrew',
+  url "https://github.com/google/fonts/trunk/apache/opensanshebrew",
       using:      :svn,
       trust_cert: true
-  name 'Open Sans Hebrew'
-  homepage 'https://fonts.google.com/earlyaccess'
+  name "Open Sans Hebrew"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'OpenSansHebrew-Bold.ttf'
-  font 'OpenSansHebrew-BoldItalic.ttf'
-  font 'OpenSansHebrew-ExtraBold.ttf'
-  font 'OpenSansHebrew-ExtraBoldItalic.ttf'
-  font 'OpenSansHebrew-Italic.ttf'
-  font 'OpenSansHebrew-Light.ttf'
-  font 'OpenSansHebrew-LightItalic.ttf'
-  font 'OpenSansHebrew-Regular.ttf'
+  font "OpenSansHebrew-Bold.ttf"
+  font "OpenSansHebrew-BoldItalic.ttf"
+  font "OpenSansHebrew-ExtraBold.ttf"
+  font "OpenSansHebrew-ExtraBoldItalic.ttf"
+  font "OpenSansHebrew-Italic.ttf"
+  font "OpenSansHebrew-Light.ttf"
+  font "OpenSansHebrew-LightItalic.ttf"
+  font "OpenSansHebrew-Regular.ttf"
 end
