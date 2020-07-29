c6e12e96cd140ab30e91ab82c04422f324c1d035
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:38 +0100

    font-cozette.rb: fix for new style

--- Casks/font-cozette.rb
+++ Casks/font-cozette.rb
@@ -1,11 +1,11 @@
-cask 'font-cozette' do
-  version '1.8.0'
-  sha256 'c6d153c8d7989ff8f7d0d3206cdc5e6863e376b774b181744c477ad7aa5710b2'
+cask "font-cozette" do
+  version "1.8.0"
+  sha256 "c6d153c8d7989ff8f7d0d3206cdc5e6863e376b774b181744c477ad7aa5710b2"
 
   url "https://github.com/slavfox/Cozette/releases/download/v.#{version}/CozetteVector.dfont"
-  appcast 'https://github.com/slavfox/Cozette/releases.atom'
-  name 'Cozette'
-  homepage 'https://github.com/slavfox/Cozette'
+  appcast "https://github.com/slavfox/Cozette/releases.atom"
+  name "Cozette"
+  homepage "https://github.com/slavfox/Cozette"
 
-  font 'CozetteVector.dfont'
+  font "CozetteVector.dfont"
 end
