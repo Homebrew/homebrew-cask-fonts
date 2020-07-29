b642fbcf5a74927c4e6f81405fe5e2d21903d37b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:02 +0100

    font-gilbert.rb: fix for new style

--- Casks/font-gilbert.rb
+++ Casks/font-gilbert.rb
@@ -1,12 +1,12 @@
-cask 'font-gilbert' do
-  version '1.004,alpha'
-  sha256 '0cea7456bdc704c9b68c023a6bb40590e327da3d84f8ff5511c6460a712987a9'
+cask "font-gilbert" do
+  version "1.004,alpha"
+  sha256 "0cea7456bdc704c9b68c023a6bb40590e327da3d84f8ff5511c6460a712987a9"
 
   # github.com/Fontself/TypeWithPride/ was verified as official when first introduced to the cask
   url "https://github.com/Fontself/TypeWithPride/releases/download/#{version.before_comma}/Gilbert_#{version.before_comma}_#{version.after_comma}.zip"
-  appcast 'https://github.com/Fontself/TypeWithPride/releases.atom'
-  name 'Gilbert'
-  homepage 'https://typewithpride.com/'
+  appcast "https://github.com/Fontself/TypeWithPride/releases.atom"
+  name "Gilbert"
+  homepage "https://typewithpride.com/"
 
   font "Gilbert_#{version.before_comma}_#{version.after_comma}/Gilbert Bold-Preview_#{version.before_comma.no_dots}.otf"
   font "Gilbert_#{version.before_comma}_#{version.after_comma}/Gilbert-Color Bold-Preview_#{version.before_comma.no_dots}.otf"
