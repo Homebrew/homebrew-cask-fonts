5b0f3a783f99566f86bc457ba3f6801dfebbb611
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:52 +0100

    font-faustina.rb: fix for new style

--- Casks/font-faustina.rb
+++ Casks/font-faustina.rb
@@ -1,16 +1,16 @@
-cask 'font-faustina' do
+cask "font-faustina" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/faustina',
+  url "https://github.com/google/fonts/trunk/ofl/faustina",
       using:      :svn,
       trust_cert: true
-  name 'Faustina'
-  homepage 'https://fonts.google.com/specimen/Faustina'
+  name "Faustina"
+  homepage "https://fonts.google.com/specimen/Faustina"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Faustina-Italic[wght].ttf'
-  font 'Faustina[wght].ttf'
+  font "Faustina-Italic[wght].ttf"
+  font "Faustina[wght].ttf"
 end
