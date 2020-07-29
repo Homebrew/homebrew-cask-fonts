5f008ba06d93d956b9672b722cc78085e17ab800
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:04 +0100

    font-gnu-unifont.rb: fix for new style

--- Casks/font-gnu-unifont.rb
+++ Casks/font-gnu-unifont.rb
@@ -1,11 +1,11 @@
-cask 'font-gnu-unifont' do
-  version '13.0.02'
-  sha256 'b1aa56482e38abf3f3151472da3432fd75fadf1ed2e17f979b24c0a63002e9b9'
+cask "font-gnu-unifont" do
+  version "13.0.02"
+  sha256 "b1aa56482e38abf3f3151472da3432fd75fadf1ed2e17f979b24c0a63002e9b9"
 
   # ftpmirror.gnu.org/unifont/ was verified as official when first introduced to the cask
   url "https://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.tar.gz"
-  name 'GNU Unifont'
-  homepage 'http://unifoundry.com/unifont.html'
+  name "GNU Unifont"
+  homepage "http://unifoundry.com/unifont.html"
 
   font "unifont-#{version}/font/precompiled/unifont-#{version}.ttf"
   font "unifont-#{version}/font/precompiled/unifont_upper-#{version}.ttf"
