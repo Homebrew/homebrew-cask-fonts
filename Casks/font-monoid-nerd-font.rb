8c3c6c40aac81f9c9fdf9abb3f7a79a2c092a709
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:44 +0100

    font-monoid-nerd-font.rb: fix for new style

--- Casks/font-monoid-nerd-font.rb
+++ Casks/font-monoid-nerd-font.rb
@@ -1,18 +1,18 @@
-cask 'font-monoid-nerd-font' do
-  version '2.1.0'
-  sha256 'ad111056f16d6174482aa5c4ae23b0300e71acc095a20d698a15f3e8a440f154'
+cask "font-monoid-nerd-font" do
+  version "2.1.0"
+  sha256 "ad111056f16d6174482aa5c4ae23b0300e71acc095a20d698a15f3e8a440f154"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Monoid.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'Monoid Nerd Font (Monoid)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "Monoid Nerd Font (Monoid)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Monoid Italic Nerd Font Complete.ttf'
-  font 'Monoid Bold Nerd Font Complete.ttf'
-  font 'Monoid Retina Nerd Font Complete.ttf'
-  font 'Monoid Regular Nerd Font Complete.ttf'
-  font 'Monoid Italic Nerd Font Complete Mono.ttf'
-  font 'Monoid Bold Nerd Font Complete Mono.ttf'
-  font 'Monoid Retina Nerd Font Complete Mono.ttf'
-  font 'Monoid Regular Nerd Font Complete Mono.ttf'
+  font "Monoid Italic Nerd Font Complete.ttf"
+  font "Monoid Bold Nerd Font Complete.ttf"
+  font "Monoid Retina Nerd Font Complete.ttf"
+  font "Monoid Regular Nerd Font Complete.ttf"
+  font "Monoid Italic Nerd Font Complete Mono.ttf"
+  font "Monoid Bold Nerd Font Complete Mono.ttf"
+  font "Monoid Retina Nerd Font Complete Mono.ttf"
+  font "Monoid Regular Nerd Font Complete Mono.ttf"
 end
