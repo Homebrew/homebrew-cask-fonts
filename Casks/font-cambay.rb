4b65fc407feb5a95f6a4212ba9d79a33cfc71773
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:26 +0100

    font-cambay.rb: fix for new style

--- Casks/font-cambay.rb
+++ Casks/font-cambay.rb
@@ -1,18 +1,18 @@
-cask 'font-cambay' do
+cask "font-cambay" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/cambay',
+  url "https://github.com/google/fonts/trunk/ofl/cambay",
       using:      :svn,
       trust_cert: true
-  name 'Cambay'
-  homepage 'https://fonts.google.com/specimen/Cambay'
+  name "Cambay"
+  homepage "https://fonts.google.com/specimen/Cambay"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Cambay-Bold.ttf'
-  font 'Cambay-BoldItalic.ttf'
-  font 'Cambay-Italic.ttf'
-  font 'Cambay-Regular.ttf'
+  font "Cambay-Bold.ttf"
+  font "Cambay-BoldItalic.ttf"
+  font "Cambay-Italic.ttf"
+  font "Cambay-Regular.ttf"
 end
