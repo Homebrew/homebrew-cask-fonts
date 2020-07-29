34ff49ab135ca571632956068fd0dd61afb8af8e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:20 +0100

    font-bio-rhyme.rb: fix for new style

--- Casks/font-bio-rhyme.rb
+++ Casks/font-bio-rhyme.rb
@@ -1,19 +1,19 @@
-cask 'font-bio-rhyme' do
+cask "font-bio-rhyme" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/biorhyme',
+  url "https://github.com/google/fonts/trunk/ofl/biorhyme",
       using:      :svn,
       trust_cert: true
-  name 'Bio Rhyme'
-  homepage 'https://fonts.google.com/specimen/Bio+Rhyme'
+  name "Bio Rhyme"
+  homepage "https://fonts.google.com/specimen/Bio+Rhyme"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'BioRhyme-Bold.ttf'
-  font 'BioRhyme-ExtraBold.ttf'
-  font 'BioRhyme-ExtraLight.ttf'
-  font 'BioRhyme-Light.ttf'
-  font 'BioRhyme-Regular.ttf'
+  font "BioRhyme-Bold.ttf"
+  font "BioRhyme-ExtraBold.ttf"
+  font "BioRhyme-ExtraLight.ttf"
+  font "BioRhyme-Light.ttf"
+  font "BioRhyme-Regular.ttf"
 end
