85af6ac118c40286e769f3822bb40ea7cecc0367
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:46 +0100

    font-economica.rb: fix for new style

--- Casks/font-economica.rb
+++ Casks/font-economica.rb
@@ -1,18 +1,18 @@
-cask 'font-economica' do
+cask "font-economica" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/economica',
+  url "https://github.com/google/fonts/trunk/ofl/economica",
       using:      :svn,
       trust_cert: true
-  name 'Economica'
-  homepage 'https://fonts.google.com/specimen/Economica'
+  name "Economica"
+  homepage "https://fonts.google.com/specimen/Economica"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Economica-Bold.ttf'
-  font 'Economica-BoldItalic.ttf'
-  font 'Economica-Italic.ttf'
-  font 'Economica-Regular.ttf'
+  font "Economica-Bold.ttf"
+  font "Economica-BoldItalic.ttf"
+  font "Economica-Italic.ttf"
+  font "Economica-Regular.ttf"
 end
