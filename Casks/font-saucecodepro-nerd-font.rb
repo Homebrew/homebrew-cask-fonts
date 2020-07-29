05d6c59b6577e6826f27dbd10e5869d97533ad8a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:15 +0100

    font-saucecodepro-nerd-font.rb: fix for new style

--- Casks/font-saucecodepro-nerd-font.rb
+++ Casks/font-saucecodepro-nerd-font.rb
@@ -1,38 +1,38 @@
-cask 'font-saucecodepro-nerd-font' do
-  version '2.1.0'
-  sha256 'a771689e0bc1d020e2082c705e2fb6113b7f8fbc1c56c639957f12546bd39619'
+cask "font-saucecodepro-nerd-font" do
+  version "2.1.0"
+  sha256 "a771689e0bc1d020e2082c705e2fb6113b7f8fbc1c56c639957f12546bd39619"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/SourceCodePro.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'SauceCodePro Nerd Font (Source Code Pro)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "SauceCodePro Nerd Font (Source Code Pro)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Sauce Code Pro Bold Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Bold Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro Bold Italic Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro Bold Italic Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Medium Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Medium Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro ExtraLight Nerd Font Complete.ttf'
-  font 'Sauce Code Pro ExtraLight Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro Black Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro Black Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro Black Italic Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Black Italic Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro Semibold Italic Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro Semibold Italic Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Medium Italic Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Medium Italic Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro ExtraLight Italic Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro ExtraLight Italic Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Italic Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Italic Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro Semibold Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro Semibold Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Light Nerd Font Complete.ttf'
-  font 'Sauce Code Pro Light Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro Light Italic Nerd Font Complete Mono.ttf'
-  font 'Sauce Code Pro Light Italic Nerd Font Complete.ttf'
+  font "Sauce Code Pro Bold Nerd Font Complete.ttf"
+  font "Sauce Code Pro Bold Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro Bold Italic Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro Bold Italic Nerd Font Complete.ttf"
+  font "Sauce Code Pro Medium Nerd Font Complete.ttf"
+  font "Sauce Code Pro Medium Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro ExtraLight Nerd Font Complete.ttf"
+  font "Sauce Code Pro ExtraLight Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro Black Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro Black Nerd Font Complete.ttf"
+  font "Sauce Code Pro Nerd Font Complete.ttf"
+  font "Sauce Code Pro Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro Black Italic Nerd Font Complete.ttf"
+  font "Sauce Code Pro Black Italic Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro Semibold Italic Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro Semibold Italic Nerd Font Complete.ttf"
+  font "Sauce Code Pro Medium Italic Nerd Font Complete.ttf"
+  font "Sauce Code Pro Medium Italic Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro ExtraLight Italic Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro ExtraLight Italic Nerd Font Complete.ttf"
+  font "Sauce Code Pro Italic Nerd Font Complete.ttf"
+  font "Sauce Code Pro Italic Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro Semibold Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro Semibold Nerd Font Complete.ttf"
+  font "Sauce Code Pro Light Nerd Font Complete.ttf"
+  font "Sauce Code Pro Light Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro Light Italic Nerd Font Complete Mono.ttf"
+  font "Sauce Code Pro Light Italic Nerd Font Complete.ttf"
 end
