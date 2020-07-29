6b96a6ff61cde235211a669b65a69e000ca87d50
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:22 +0100

    font-supermercado-one.rb: fix for new style

--- Casks/font-supermercado-one.rb
+++ Casks/font-supermercado-one.rb
@@ -1,11 +1,11 @@
-cask 'font-supermercado-one' do
+cask "font-supermercado-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/supermercadoone/SupermercadoOne-Regular.ttf'
-  name 'Supermercado One'
-  homepage 'https://fonts.google.com/specimen/Supermercado+One'
+  url "https://github.com/google/fonts/raw/master/ofl/supermercadoone/SupermercadoOne-Regular.ttf"
+  name "Supermercado One"
+  homepage "https://fonts.google.com/specimen/Supermercado+One"
 
-  font 'SupermercadoOne-Regular.ttf'
+  font "SupermercadoOne-Regular.ttf"
 end
