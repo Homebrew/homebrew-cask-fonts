d0444977d194df28e35ff344e3b0b7d58483af7e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:17 +0100

    font-be-vietnam.rb: fix for new style

--- Casks/font-be-vietnam.rb
+++ Casks/font-be-vietnam.rb
@@ -1,28 +1,28 @@
-cask 'font-be-vietnam' do
+cask "font-be-vietnam" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/bevietnam',
+  url "https://github.com/google/fonts/trunk/ofl/bevietnam",
       using:      :svn,
       trust_cert: true
-  name 'Be Vietnam'
-  homepage 'https://fonts.google.com/specimen/Be+Vietnam'
+  name "Be Vietnam"
+  homepage "https://fonts.google.com/specimen/Be+Vietnam"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'BeVietnam-Bold.ttf'
-  font 'BeVietnam-BoldItalic.ttf'
-  font 'BeVietnam-ExtraBold.ttf'
-  font 'BeVietnam-ExtraBoldItalic.ttf'
-  font 'BeVietnam-Italic.ttf'
-  font 'BeVietnam-Light.ttf'
-  font 'BeVietnam-LightItalic.ttf'
-  font 'BeVietnam-Medium.ttf'
-  font 'BeVietnam-MediumItalic.ttf'
-  font 'BeVietnam-Regular.ttf'
-  font 'BeVietnam-SemiBold.ttf'
-  font 'BeVietnam-SemiBoldItalic.ttf'
-  font 'BeVietnam-Thin.ttf'
-  font 'BeVietnam-ThinItalic.ttf'
+  font "BeVietnam-Bold.ttf"
+  font "BeVietnam-BoldItalic.ttf"
+  font "BeVietnam-ExtraBold.ttf"
+  font "BeVietnam-ExtraBoldItalic.ttf"
+  font "BeVietnam-Italic.ttf"
+  font "BeVietnam-Light.ttf"
+  font "BeVietnam-LightItalic.ttf"
+  font "BeVietnam-Medium.ttf"
+  font "BeVietnam-MediumItalic.ttf"
+  font "BeVietnam-Regular.ttf"
+  font "BeVietnam-SemiBold.ttf"
+  font "BeVietnam-SemiBoldItalic.ttf"
+  font "BeVietnam-Thin.ttf"
+  font "BeVietnam-ThinItalic.ttf"
 end
