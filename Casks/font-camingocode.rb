d15b54924aa920864dda8c4b156085a6024cf9f1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:26 +0100

    font-camingocode.rb: fix for new style

--- Casks/font-camingocode.rb
+++ Casks/font-camingocode.rb
@@ -1,13 +1,13 @@
-cask 'font-camingocode' do
-  version '1.000'
-  sha256 '13b188ca8d1b5fe2c3ba880771f20a6faac550405ae98d12edd46240f8896714'
+cask "font-camingocode" do
+  version "1.000"
+  sha256 "13b188ca8d1b5fe2c3ba880771f20a6faac550405ae98d12edd46240f8896714"
 
-  url 'https://janfromm.de/_data/downloads/CamingoCode-v1.0.zip'
-  name 'CamingoCode'
-  homepage 'https://www.janfromm.de/typefaces/camingomono/camingocode/'
+  url "https://janfromm.de/_data/downloads/CamingoCode-v1.0.zip"
+  name "CamingoCode"
+  homepage "https://www.janfromm.de/typefaces/camingomono/camingocode/"
 
-  font 'CamingoCode-Bold.ttf'
-  font 'CamingoCode-BoldItalic.ttf'
-  font 'CamingoCode-Regular.ttf'
-  font 'CamingoCode-Italic.ttf'
+  font "CamingoCode-Bold.ttf"
+  font "CamingoCode-BoldItalic.ttf"
+  font "CamingoCode-Regular.ttf"
+  font "CamingoCode-Italic.ttf"
 end
