c92538c7c8346b3c81c9980a6bc7c25898c5f634
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:01 +0100

    font-open-sans-condensed.rb: fix for new style

--- Casks/font-open-sans-condensed.rb
+++ Casks/font-open-sans-condensed.rb
@@ -1,17 +1,17 @@
-cask 'font-open-sans-condensed' do
+cask "font-open-sans-condensed" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/apache/opensanscondensed',
+  url "https://github.com/google/fonts/trunk/apache/opensanscondensed",
       using:      :svn,
       trust_cert: true
-  name 'Open Sans Condensed'
-  homepage 'https://fonts.google.com/specimen/Open+Sans+Condensed'
+  name "Open Sans Condensed"
+  homepage "https://fonts.google.com/specimen/Open+Sans+Condensed"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'OpenSansCondensed-Bold.ttf'
-  font 'OpenSansCondensed-Light.ttf'
-  font 'OpenSansCondensed-LightItalic.ttf'
+  font "OpenSansCondensed-Bold.ttf"
+  font "OpenSansCondensed-Light.ttf"
+  font "OpenSansCondensed-LightItalic.ttf"
 end
