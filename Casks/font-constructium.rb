f0638f02122230bf5bbb0ff5f45fec4a2eeb3158
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:35 +0100

    font-constructium.rb: fix for new style

--- Casks/font-constructium.rb
+++ Casks/font-constructium.rb
@@ -1,11 +1,11 @@
-cask 'font-constructium' do
+cask "font-constructium" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.kreativekorp.com/swdownload/fonts/xlang/constructium.zip',
+  url "https://www.kreativekorp.com/swdownload/fonts/xlang/constructium.zip",
       user_agent: :fake
-  name 'Constructium'
-  homepage 'https://www.kreativekorp.com/software/fonts/constructium.shtml'
+  name "Constructium"
+  homepage "https://www.kreativekorp.com/software/fonts/constructium.shtml"
 
-  font 'Constructium.ttf'
+  font "Constructium.ttf"
 end
