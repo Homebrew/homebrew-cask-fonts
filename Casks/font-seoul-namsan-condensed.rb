a5c2c3a9bcb85793c88a69f9865843e872353db7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:16 +0100

    font-seoul-namsan-condensed.rb: fix for new style

--- Casks/font-seoul-namsan-condensed.rb
+++ Casks/font-seoul-namsan-condensed.rb
@@ -1,19 +1,19 @@
-cask 'font-seoul-namsan-condensed' do
+cask "font-seoul-namsan-condensed" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/seoulnamsancondensed',
+  url "https://github.com/google/fonts/trunk/ofl/seoulnamsancondensed",
       using:      :svn,
       trust_cert: true
-  name 'Seoul Namsan Condensed'
-  homepage 'https://fonts.google.com/specimen/Seoul+Namsan+Condensed'
+  name "Seoul Namsan Condensed"
+  homepage "https://fonts.google.com/specimen/Seoul+Namsan+Condensed"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'SeoulNamsanCondensed-Black.ttf'
-  font 'SeoulNamsanCondensed-Bold.ttf'
-  font 'SeoulNamsanCondensed-ExtraBold.ttf'
-  font 'SeoulNamsanCondensed-Light.ttf'
-  font 'SeoulNamsanCondensed-Medium.ttf'
+  font "SeoulNamsanCondensed-Black.ttf"
+  font "SeoulNamsanCondensed-Bold.ttf"
+  font "SeoulNamsanCondensed-ExtraBold.ttf"
+  font "SeoulNamsanCondensed-Light.ttf"
+  font "SeoulNamsanCondensed-Medium.ttf"
 end
