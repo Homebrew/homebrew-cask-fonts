df13ae7c2f12f30cba8b4954fb827cfb63a01b8c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:01 +0100

    font-archivo-black.rb: fix for new style

--- Casks/font-archivo-black.rb
+++ Casks/font-archivo-black.rb
@@ -1,11 +1,11 @@
-cask 'font-archivo-black' do
+cask "font-archivo-black" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/archivoblack/ArchivoBlack-Regular.ttf'
-  name 'Archivo Black'
-  homepage 'https://fonts.google.com/specimen/Archivo+Black'
+  url "https://github.com/google/fonts/raw/master/ofl/archivoblack/ArchivoBlack-Regular.ttf"
+  name "Archivo Black"
+  homepage "https://fonts.google.com/specimen/Archivo+Black"
 
-  font 'ArchivoBlack-Regular.ttf'
+  font "ArchivoBlack-Regular.ttf"
 end
