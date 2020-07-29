18494a40172eb003a227ecc7f1e2038d6d6e4527
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:00 +0100

    font-nova-slim.rb: fix for new style

--- Casks/font-nova-slim.rb
+++ Casks/font-nova-slim.rb
@@ -1,11 +1,11 @@
-cask 'font-nova-slim' do
+cask "font-nova-slim" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/novaslim/NovaSlim.ttf'
-  name 'Nova Slim'
-  homepage 'https://fonts.google.com/specimen/Nova+Slim'
+  url "https://github.com/google/fonts/raw/master/ofl/novaslim/NovaSlim.ttf"
+  name "Nova Slim"
+  homepage "https://fonts.google.com/specimen/Nova+Slim"
 
-  font 'NovaSlim.ttf'
+  font "NovaSlim.ttf"
 end
