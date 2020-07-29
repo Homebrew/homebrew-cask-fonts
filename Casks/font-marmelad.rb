e66b88c6e2e7dfb909414a20b462ea2f7bca4fe6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:39 +0100

    font-marmelad.rb: fix for new style

--- Casks/font-marmelad.rb
+++ Casks/font-marmelad.rb
@@ -1,11 +1,11 @@
-cask 'font-marmelad' do
+cask "font-marmelad" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/marmelad/Marmelad-Regular.ttf'
-  name 'Marmelad'
-  homepage 'https://fonts.google.com/specimen/Marmelad'
+  url "https://github.com/google/fonts/raw/master/ofl/marmelad/Marmelad-Regular.ttf"
+  name "Marmelad"
+  homepage "https://fonts.google.com/specimen/Marmelad"
 
-  font 'Marmelad-Regular.ttf'
+  font "Marmelad-Regular.ttf"
 end
