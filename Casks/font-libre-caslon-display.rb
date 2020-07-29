320dbd9b49e97f85e4990a06d100e0797401fd3c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:35 +0100

    font-libre-caslon-display.rb: fix for new style

--- Casks/font-libre-caslon-display.rb
+++ Casks/font-libre-caslon-display.rb
@@ -1,10 +1,10 @@
-cask 'font-libre-caslon-display' do
+cask "font-libre-caslon-display" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/impallari/Libre-Caslon-Display/archive/master.zip'
-  name 'Libre Caslon Display'
-  homepage 'https://github.com/impallari/Libre-Caslon-Display/'
+  url "https://github.com/impallari/Libre-Caslon-Display/archive/master.zip"
+  name "Libre Caslon Display"
+  homepage "https://github.com/impallari/Libre-Caslon-Display/"
 
-  font 'Libre-Caslon-Display-master/fonts/OTF/LibreCaslonDisplay-Regular.otf'
+  font "Libre-Caslon-Display-master/fonts/OTF/LibreCaslonDisplay-Regular.otf"
 end
