b42d6be68a6f699e0695b134809f9c2a33a9e916
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:09 +0100

    font-rammetto-one.rb: fix for new style

--- Casks/font-rammetto-one.rb
+++ Casks/font-rammetto-one.rb
@@ -1,11 +1,11 @@
-cask 'font-rammetto-one' do
+cask "font-rammetto-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/rammettoone/RammettoOne-Regular.ttf'
-  name 'Rammetto One'
-  homepage 'https://fonts.google.com/specimen/Rammetto+One'
+  url "https://github.com/google/fonts/raw/master/ofl/rammettoone/RammettoOne-Regular.ttf"
+  name "Rammetto One"
+  homepage "https://fonts.google.com/specimen/Rammetto+One"
 
-  font 'RammettoOne-Regular.ttf'
+  font "RammettoOne-Regular.ttf"
 end
