71ddfd6ef0aa31bdaf7b7b705303cbf9b727234a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:39 +0100

    font-crimson-text.rb: fix for new style

--- Casks/font-crimson-text.rb
+++ Casks/font-crimson-text.rb
@@ -1,20 +1,20 @@
-cask 'font-crimson-text' do
+cask "font-crimson-text" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/crimsontext',
+  url "https://github.com/google/fonts/trunk/ofl/crimsontext",
       using:      :svn,
       trust_cert: true
-  name 'Crimson Text'
-  homepage 'https://fonts.google.com/specimen/Crimson+Text'
+  name "Crimson Text"
+  homepage "https://fonts.google.com/specimen/Crimson+Text"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'CrimsonText-Bold.ttf'
-  font 'CrimsonText-BoldItalic.ttf'
-  font 'CrimsonText-Italic.ttf'
-  font 'CrimsonText-Regular.ttf'
-  font 'CrimsonText-SemiBold.ttf'
-  font 'CrimsonText-SemiBoldItalic.ttf'
+  font "CrimsonText-Bold.ttf"
+  font "CrimsonText-BoldItalic.ttf"
+  font "CrimsonText-Italic.ttf"
+  font "CrimsonText-Regular.ttf"
+  font "CrimsonText-SemiBold.ttf"
+  font "CrimsonText-SemiBoldItalic.ttf"
 end
