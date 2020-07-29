7cd8ba229a738898713d9198151c15dce54019f2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:18 +0100

    font-slabo-13px.rb: fix for new style

--- Casks/font-slabo-13px.rb
+++ Casks/font-slabo-13px.rb
@@ -1,11 +1,11 @@
-cask 'font-slabo-13px' do
+cask "font-slabo-13px" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/slabo13px/Slabo13px-Regular.ttf'
-  name 'Slabo 13px'
-  homepage 'https://fonts.google.com/specimen/Slabo+13px'
+  url "https://github.com/google/fonts/raw/master/ofl/slabo13px/Slabo13px-Regular.ttf"
+  name "Slabo 13px"
+  homepage "https://fonts.google.com/specimen/Slabo+13px"
 
-  font 'Slabo13px-Regular.ttf'
+  font "Slabo13px-Regular.ttf"
 end
