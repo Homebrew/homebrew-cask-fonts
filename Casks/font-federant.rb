af13c59decea162eefa96c60802a510255e7a8ff
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:53 +0100

    font-federant.rb: fix for new style

--- Casks/font-federant.rb
+++ Casks/font-federant.rb
@@ -1,11 +1,11 @@
-cask 'font-federant' do
+cask "font-federant" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/federant/Federant-Regular.ttf'
-  name 'Federant'
-  homepage 'https://fonts.google.com/specimen/Federant'
+  url "https://github.com/google/fonts/raw/master/ofl/federant/Federant-Regular.ttf"
+  name "Federant"
+  homepage "https://fonts.google.com/specimen/Federant"
 
-  font 'Federant-Regular.ttf'
+  font "Federant-Regular.ttf"
 end
