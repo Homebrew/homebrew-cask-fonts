562e8dc40b864637b5b8aedfd6ca5147dd804c54
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:43 +0100

    font-digital-numbers.rb: fix for new style

--- Casks/font-digital-numbers.rb
+++ Casks/font-digital-numbers.rb
@@ -1,11 +1,11 @@
-cask 'font-digital-numbers' do
+cask "font-digital-numbers" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/digitalnumbers/DigitalNumbers-Regular.ttf'
-  name 'Digital Numbers'
-  homepage 'https://fonts.google.com/specimen/Digital+Numbers'
+  url "https://github.com/google/fonts/raw/master/ofl/digitalnumbers/DigitalNumbers-Regular.ttf"
+  name "Digital Numbers"
+  homepage "https://fonts.google.com/specimen/Digital+Numbers"
 
-  font 'DigitalNumbers-Regular.ttf'
+  font "DigitalNumbers-Regular.ttf"
 end
