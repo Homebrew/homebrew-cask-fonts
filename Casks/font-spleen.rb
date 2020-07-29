fcaef9a1835d3d7c4e19c9b4ca6eb9b50b2fc6a5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:20 +0100

    font-spleen.rb: fix for new style

--- Casks/font-spleen.rb
+++ Casks/font-spleen.rb
@@ -1,11 +1,11 @@
-cask 'font-spleen' do
-  version '1.8.1'
-  sha256 '59119044399b2262fdf4c504c6f1a8b1230d349dfd07c68a2fb07a394a880e1e'
+cask "font-spleen" do
+  version "1.8.1"
+  sha256 "59119044399b2262fdf4c504c6f1a8b1230d349dfd07c68a2fb07a394a880e1e"
 
   url "https://github.com/fcambus/spleen/releases/download/#{version}/spleen-#{version}.tar.gz"
-  appcast 'https://github.com/fcambus/spleen/releases.atom'
-  name 'Spleen'
-  homepage 'https://github.com/fcambus/spleen'
+  appcast "https://github.com/fcambus/spleen/releases.atom"
+  name "Spleen"
+  homepage "https://github.com/fcambus/spleen"
 
   font "spleen-#{version}/spleen-6x12.otf"
   font "spleen-#{version}/spleen-8x16.otf"
