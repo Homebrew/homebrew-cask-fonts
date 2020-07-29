dec743b2be5597faad5492318ca25cac554fb904
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:25 +0100

    font-cabin-condensed.rb: fix for new style

--- Casks/font-cabin-condensed.rb
+++ Casks/font-cabin-condensed.rb
@@ -1,18 +1,18 @@
-cask 'font-cabin-condensed' do
+cask "font-cabin-condensed" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/cabincondensed',
+  url "https://github.com/google/fonts/trunk/ofl/cabincondensed",
       using:      :svn,
       trust_cert: true
-  name 'Cabin Condensed'
-  homepage 'https://fonts.google.com/specimen/Cabin+Condensed'
+  name "Cabin Condensed"
+  homepage "https://fonts.google.com/specimen/Cabin+Condensed"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'CabinCondensed-Bold.ttf'
-  font 'CabinCondensed-Medium.ttf'
-  font 'CabinCondensed-Regular.ttf'
-  font 'CabinCondensed-SemiBold.ttf'
+  font "CabinCondensed-Bold.ttf"
+  font "CabinCondensed-Medium.ttf"
+  font "CabinCondensed-Regular.ttf"
+  font "CabinCondensed-SemiBold.ttf"
 end
