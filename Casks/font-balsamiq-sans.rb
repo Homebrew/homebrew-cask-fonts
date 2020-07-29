4cece3a14343e9ff9e086f5fddb8054bf3c72fa4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:15 +0100

    font-balsamiq-sans.rb: fix for new style

--- Casks/font-balsamiq-sans.rb
+++ Casks/font-balsamiq-sans.rb
@@ -1,14 +1,14 @@
-cask 'font-balsamiq-sans' do
+cask "font-balsamiq-sans" do
   version :latest
   sha256 :no_check
 
   # github.com/balsamiq/balsamiqsans/ was verified as official when first introduced to the cask
-  url 'https://github.com/balsamiq/balsamiqsans/archive/master.zip'
-  name 'Balsamiq Sans'
-  homepage 'https://balsamiq.com/givingback/opensource/font/'
+  url "https://github.com/balsamiq/balsamiqsans/archive/master.zip"
+  name "Balsamiq Sans"
+  homepage "https://balsamiq.com/givingback/opensource/font/"
 
-  font 'balsamiqsans-master/fonts/ttf/BalsamiqSans-Bold.ttf'
-  font 'balsamiqsans-master/fonts/ttf/BalsamiqSans-BoldItalic.ttf'
-  font 'balsamiqsans-master/fonts/ttf/BalsamiqSans-Italic.ttf'
-  font 'balsamiqsans-master/fonts/ttf/BalsamiqSans-Regular.ttf'
+  font "balsamiqsans-master/fonts/ttf/BalsamiqSans-Bold.ttf"
+  font "balsamiqsans-master/fonts/ttf/BalsamiqSans-BoldItalic.ttf"
+  font "balsamiqsans-master/fonts/ttf/BalsamiqSans-Italic.ttf"
+  font "balsamiqsans-master/fonts/ttf/BalsamiqSans-Regular.ttf"
 end
