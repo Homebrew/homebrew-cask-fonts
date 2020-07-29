ca69aad02c10e281430adc36e49660b5bbbe6abd
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:33 +0100

    font-lemonada-vf-beta.rb: fix for new style

--- Casks/font-lemonada-vf-beta.rb
+++ Casks/font-lemonada-vf-beta.rb
@@ -1,11 +1,11 @@
-cask 'font-lemonada-vf-beta' do
+cask "font-lemonada-vf-beta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/lemonadavfbeta/LemonadaVFBeta.ttf'
-  name 'Lemonada VF Beta'
-  homepage 'https://fonts.google.com/specimen/Lemonada+VF+Beta'
+  url "https://github.com/google/fonts/raw/master/ofl/lemonadavfbeta/LemonadaVFBeta.ttf"
+  name "Lemonada VF Beta"
+  homepage "https://fonts.google.com/specimen/Lemonada+VF+Beta"
 
-  font 'LemonadaVFBeta.ttf'
+  font "LemonadaVFBeta.ttf"
 end
