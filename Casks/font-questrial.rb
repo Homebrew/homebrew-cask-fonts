924b5ace4fb79e6f160e1a21825ecf93bddcf908
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:08 +0100

    font-questrial.rb: fix for new style

--- Casks/font-questrial.rb
+++ Casks/font-questrial.rb
@@ -1,11 +1,11 @@
-cask 'font-questrial' do
+cask "font-questrial" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/questrial/Questrial-Regular.ttf'
-  name 'Questrial'
-  homepage 'https://fonts.google.com/specimen/Questrial'
+  url "https://github.com/google/fonts/raw/master/ofl/questrial/Questrial-Regular.ttf"
+  name "Questrial"
+  homepage "https://fonts.google.com/specimen/Questrial"
 
-  font 'Questrial-Regular.ttf'
+  font "Questrial-Regular.ttf"
 end
