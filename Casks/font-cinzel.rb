c65a4d088f90a8e951297ddd9ae2f90c834e2325
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:32 +0100

    font-cinzel.rb: fix for new style

--- Casks/font-cinzel.rb
+++ Casks/font-cinzel.rb
@@ -1,17 +1,17 @@
-cask 'font-cinzel' do
+cask "font-cinzel" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/cinzel',
+  url "https://github.com/google/fonts/trunk/ofl/cinzel",
       using:      :svn,
       trust_cert: true
-  name 'Cinzel'
-  homepage 'https://fonts.google.com/specimen/Cinzel'
+  name "Cinzel"
+  homepage "https://fonts.google.com/specimen/Cinzel"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Cinzel-Black.ttf'
-  font 'Cinzel-Bold.ttf'
-  font 'Cinzel-Regular.ttf'
+  font "Cinzel-Black.ttf"
+  font "Cinzel-Bold.ttf"
+  font "Cinzel-Regular.ttf"
 end
