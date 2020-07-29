c8960d782853a9384b9597099a5ce10dab145452
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:49 +0100

    font-nothing-you-could-do.rb: fix for new style

--- Casks/font-nothing-you-could-do.rb
+++ Casks/font-nothing-you-could-do.rb
@@ -1,11 +1,11 @@
-cask 'font-nothing-you-could-do' do
+cask "font-nothing-you-could-do" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/nothingyoucoulddo/NothingYouCouldDo.ttf'
-  name 'Nothing You Could Do'
-  homepage 'https://fonts.google.com/specimen/Nothing+You+Could+Do'
+  url "https://github.com/google/fonts/raw/master/ofl/nothingyoucoulddo/NothingYouCouldDo.ttf"
+  name "Nothing You Could Do"
+  homepage "https://fonts.google.com/specimen/Nothing+You+Could+Do"
 
-  font 'NothingYouCouldDo.ttf'
+  font "NothingYouCouldDo.ttf"
 end
