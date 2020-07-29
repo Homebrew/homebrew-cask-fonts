7fa01d92068356dcf9793a80eb3ed6a2914d33a5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:07 +0100

    font-habibi.rb: fix for new style

--- Casks/font-habibi.rb
+++ Casks/font-habibi.rb
@@ -1,11 +1,11 @@
-cask 'font-habibi' do
+cask "font-habibi" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/habibi/Habibi-Regular.ttf'
-  name 'Habibi'
-  homepage 'https://fonts.google.com/specimen/Habibi'
+  url "https://github.com/google/fonts/raw/master/ofl/habibi/Habibi-Regular.ttf"
+  name "Habibi"
+  homepage "https://fonts.google.com/specimen/Habibi"
 
-  font 'Habibi-Regular.ttf'
+  font "Habibi-Regular.ttf"
 end
