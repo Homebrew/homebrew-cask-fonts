d3eb66f9b12f055aad9d655d74faac09e9acf862
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:13 +0100

    font-sacramento.rb: fix for new style

--- Casks/font-sacramento.rb
+++ Casks/font-sacramento.rb
@@ -1,11 +1,11 @@
-cask 'font-sacramento' do
+cask "font-sacramento" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/sacramento/Sacramento-Regular.ttf'
-  name 'Sacramento'
-  homepage 'https://fonts.google.com/specimen/Sacramento'
+  url "https://github.com/google/fonts/raw/master/ofl/sacramento/Sacramento-Regular.ttf"
+  name "Sacramento"
+  homepage "https://fonts.google.com/specimen/Sacramento"
 
-  font 'Sacramento-Regular.ttf'
+  font "Sacramento-Regular.ttf"
 end
