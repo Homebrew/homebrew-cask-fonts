49a9079bf98648555d2d28090ed12746190cf65b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:32:07 +0100

    font-aladin.rb: fix for new style

--- Casks/font-aladin.rb
+++ Casks/font-aladin.rb
@@ -1,11 +1,11 @@
-cask 'font-aladin' do
+cask "font-aladin" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/aladin/Aladin-Regular.ttf'
-  name 'Aladin'
-  homepage 'https://fonts.google.com/specimen/Aladin'
+  url "https://github.com/google/fonts/raw/master/ofl/aladin/Aladin-Regular.ttf"
+  name "Aladin"
+  homepage "https://fonts.google.com/specimen/Aladin"
 
-  font 'Aladin-Regular.ttf'
+  font "Aladin-Regular.ttf"
 end
