9fc1a434c8bf13f91ae3b0d16682c964c2101f41
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:38 +0100

    font-creepster-caps.rb: fix for new style

--- Casks/font-creepster-caps.rb
+++ Casks/font-creepster-caps.rb
@@ -1,11 +1,11 @@
-cask 'font-creepster-caps' do
+cask "font-creepster-caps" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/creepstercaps/CreepsterCaps-Regular.ttf'
-  name 'Creepster Caps'
-  homepage 'https://fonts.google.com/specimen/Creepster+Caps'
+  url "https://github.com/google/fonts/raw/master/apache/creepstercaps/CreepsterCaps-Regular.ttf"
+  name "Creepster Caps"
+  homepage "https://fonts.google.com/specimen/Creepster+Caps"
 
-  font 'CreepsterCaps-Regular.ttf'
+  font "CreepsterCaps-Regular.ttf"
 end
