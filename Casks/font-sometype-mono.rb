58f978f0375a7110415128bf774cc964a1102432
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:18 +0100

    font-sometype-mono.rb: fix for new style

--- Casks/font-sometype-mono.rb
+++ Casks/font-sometype-mono.rb
@@ -1,16 +1,16 @@
-cask 'font-sometype-mono' do
+cask "font-sometype-mono" do
   version :latest
   sha256 :no_check
 
   # github.com/dharmatype/Sometype-Mono/ was verified as official when first introduced to the cask
-  url 'https://github.com/dharmatype/Sometype-Mono/archive/master.zip'
-  name 'Sometype Mono'
-  homepage 'https://monospacedfont.com/'
+  url "https://github.com/dharmatype/Sometype-Mono/archive/master.zip"
+  name "Sometype Mono"
+  homepage "https://monospacedfont.com/"
 
-  font 'Sometype-Mono-master/fonts/otf/SometypeMono-Bold.otf'
-  font 'Sometype-Mono-master/fonts/otf/SometypeMono-BoldItalic.otf'
-  font 'Sometype-Mono-master/fonts/otf/SometypeMono-Medium.otf'
-  font 'Sometype-Mono-master/fonts/otf/SometypeMono-MediumItalic.otf'
-  font 'Sometype-Mono-master/fonts/otf/SometypeMono-Regular.otf'
-  font 'Sometype-Mono-master/fonts/otf/SometypeMono-RegularItalic.otf'
+  font "Sometype-Mono-master/fonts/otf/SometypeMono-Bold.otf"
+  font "Sometype-Mono-master/fonts/otf/SometypeMono-BoldItalic.otf"
+  font "Sometype-Mono-master/fonts/otf/SometypeMono-Medium.otf"
+  font "Sometype-Mono-master/fonts/otf/SometypeMono-MediumItalic.otf"
+  font "Sometype-Mono-master/fonts/otf/SometypeMono-Regular.otf"
+  font "Sometype-Mono-master/fonts/otf/SometypeMono-RegularItalic.otf"
 end
