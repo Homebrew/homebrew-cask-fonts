5e13e6c00ccc4a319fdc8899f9f065227e76e5e1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:12 +0100

    font-aboriginal-serif.rb: fix for new style

--- Casks/font-aboriginal-serif.rb
+++ Casks/font-aboriginal-serif.rb
@@ -1,13 +1,13 @@
-cask 'font-aboriginal-serif' do
+cask "font-aboriginal-serif" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.languagegeek.com/font/AboriginalSerif.zip'
-  name 'Aboriginal Serif'
-  homepage 'https://www.languagegeek.com/font/fontdownload.html'
+  url "https://www.languagegeek.com/font/AboriginalSerif.zip"
+  name "Aboriginal Serif"
+  homepage "https://www.languagegeek.com/font/fontdownload.html"
 
-  font 'AboriginalSerifREGULAR.ttf'
-  font 'AboriginalSerifBOLD.ttf'
-  font 'AboriginalSerifBOLDITALIC.ttf'
-  font 'AboriginalSerifITALIC.ttf'
+  font "AboriginalSerifREGULAR.ttf"
+  font "AboriginalSerifBOLD.ttf"
+  font "AboriginalSerifBOLDITALIC.ttf"
+  font "AboriginalSerifITALIC.ttf"
 end
