2ba9a97015e67cbe0fea4643f423ee15b1d6dec0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:54 +0100

    font-noto-sans-meetei-mayek.rb: fix for new style

--- Casks/font-noto-sans-meetei-mayek.rb
+++ Casks/font-noto-sans-meetei-mayek.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-meetei-mayek' do
+cask "font-noto-sans-meetei-mayek" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMeeteiMayek-unhinted.zip'
-  name 'Noto Sans Meetei Mayek'
-  homepage 'https://www.google.com/get/noto/#sans-mtei'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMeeteiMayek-unhinted.zip"
+  name "Noto Sans Meetei Mayek"
+  homepage "https://www.google.com/get/noto/#sans-mtei"
 
-  font 'NotoSansMeeteiMayek-Regular.ttf'
+  font "NotoSansMeeteiMayek-Regular.ttf"
 end
