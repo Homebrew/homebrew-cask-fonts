29b53e81b2a1e6284b0937071ea5161c7b45b4cf
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:50 +0100

    font-noto-kufi-arabic.rb: fix for new style

--- Casks/font-noto-kufi-arabic.rb
+++ Casks/font-noto-kufi-arabic.rb
@@ -1,12 +1,12 @@
-cask 'font-noto-kufi-arabic' do
+cask "font-noto-kufi-arabic" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoKufiArabic-unhinted.zip'
-  name 'Noto Kufi Arabic'
-  homepage 'https://www.google.com/get/noto/#kufi-arab'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoKufiArabic-unhinted.zip"
+  name "Noto Kufi Arabic"
+  homepage "https://www.google.com/get/noto/#kufi-arab"
 
-  font 'NotoKufiArabic-Bold.ttf'
-  font 'NotoKufiArabic-Regular.ttf'
+  font "NotoKufiArabic-Bold.ttf"
+  font "NotoKufiArabic-Regular.ttf"
 end
