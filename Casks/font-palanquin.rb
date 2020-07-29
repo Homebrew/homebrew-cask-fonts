a7b4ad2a34f2052f1fc3843ba3fa89049fc049c3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:03 +0100

    font-palanquin.rb: fix for new style

--- Casks/font-palanquin.rb
+++ Casks/font-palanquin.rb
@@ -1,21 +1,21 @@
-cask 'font-palanquin' do
+cask "font-palanquin" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/palanquin',
+  url "https://github.com/google/fonts/trunk/ofl/palanquin",
       using:      :svn,
       trust_cert: true
-  name 'Palanquin'
-  homepage 'https://fonts.google.com/specimen/Palanquin'
+  name "Palanquin"
+  homepage "https://fonts.google.com/specimen/Palanquin"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Palanquin-Bold.ttf'
-  font 'Palanquin-ExtraLight.ttf'
-  font 'Palanquin-Light.ttf'
-  font 'Palanquin-Medium.ttf'
-  font 'Palanquin-Regular.ttf'
-  font 'Palanquin-SemiBold.ttf'
-  font 'Palanquin-Thin.ttf'
+  font "Palanquin-Bold.ttf"
+  font "Palanquin-ExtraLight.ttf"
+  font "Palanquin-Light.ttf"
+  font "Palanquin-Medium.ttf"
+  font "Palanquin-Regular.ttf"
+  font "Palanquin-SemiBold.ttf"
+  font "Palanquin-Thin.ttf"
 end
