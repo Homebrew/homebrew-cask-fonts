3991f5bcfbf3b4e81a252ae086d43d165a6535f7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:22 +0100

    font-suez-one.rb: fix for new style

--- Casks/font-suez-one.rb
+++ Casks/font-suez-one.rb
@@ -1,11 +1,11 @@
-cask 'font-suez-one' do
+cask "font-suez-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/suezone/SuezOne-Regular.ttf'
-  name 'Suez One'
-  homepage 'https://fonts.google.com/specimen/Suez+One'
+  url "https://github.com/google/fonts/raw/master/ofl/suezone/SuezOne-Regular.ttf"
+  name "Suez One"
+  homepage "https://fonts.google.com/specimen/Suez+One"
 
-  font 'SuezOne-Regular.ttf'
+  font "SuezOne-Regular.ttf"
 end
