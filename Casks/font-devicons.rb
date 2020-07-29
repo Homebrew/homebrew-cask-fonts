80b877b45c974c99cfc21af7f8c6518981fa7a52
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:42 +0100

    font-devicons.rb: fix for new style

--- Casks/font-devicons.rb
+++ Casks/font-devicons.rb
@@ -1,12 +1,12 @@
-cask 'font-devicons' do
-  version '1.8.0'
-  sha256 'fc0baa260f54832c059d1d9eab3798ae758d1a1cf0c1695e9883aab85d9a4308'
+cask "font-devicons" do
+  version "1.8.0"
+  sha256 "fc0baa260f54832c059d1d9eab3798ae758d1a1cf0c1695e9883aab85d9a4308"
 
   # github.com/vorillaz/devicons/ was verified as official when first introduced to the cask
   url "https://github.com/vorillaz/devicons/archive/#{version}.zip"
-  appcast 'https://github.com/vorillaz/devicons/releases.atom'
-  name 'Devicons'
-  homepage 'https://vorillaz.github.io/devicons/'
+  appcast "https://github.com/vorillaz/devicons/releases.atom"
+  name "Devicons"
+  homepage "https://vorillaz.github.io/devicons/"
 
   font "devicons-#{version}/fonts/devicons.ttf"
 end
