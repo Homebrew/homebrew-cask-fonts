6beaaa23de6f03432327ab1140045d0d1d1d5e93
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:33 +0100

    font-codystar.rb: fix for new style

--- Casks/font-codystar.rb
+++ Casks/font-codystar.rb
@@ -1,16 +1,16 @@
-cask 'font-codystar' do
+cask "font-codystar" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/codystar',
+  url "https://github.com/google/fonts/trunk/ofl/codystar",
       using:      :svn,
       trust_cert: true
-  name 'Codystar'
-  homepage 'https://fonts.google.com/specimen/Codystar'
+  name "Codystar"
+  homepage "https://fonts.google.com/specimen/Codystar"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Codystar-Light.ttf'
-  font 'Codystar-Regular.ttf'
+  font "Codystar-Light.ttf"
+  font "Codystar-Regular.ttf"
 end
