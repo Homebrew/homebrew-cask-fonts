7ba0599c48659f87c02dd74f91db72e9461f9534
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:32 +0100

    font-clicker-script.rb: fix for new style

--- Casks/font-clicker-script.rb
+++ Casks/font-clicker-script.rb
@@ -1,11 +1,11 @@
-cask 'font-clicker-script' do
+cask "font-clicker-script" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/clickerscript/ClickerScript-Regular.ttf'
-  name 'Clicker Script'
-  homepage 'https://fonts.google.com/specimen/Clicker+Script'
+  url "https://github.com/google/fonts/raw/master/ofl/clickerscript/ClickerScript-Regular.ttf"
+  name "Clicker Script"
+  homepage "https://fonts.google.com/specimen/Clicker+Script"
 
-  font 'ClickerScript-Regular.ttf'
+  font "ClickerScript-Regular.ttf"
 end
