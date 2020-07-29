91ad2bff18f9a4ef8557b42d094d47dec7ce492b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:53 +0100

    font-fira-code-nerd-font.rb: fix for new style

--- Casks/font-fira-code-nerd-font.rb
+++ Casks/font-fira-code-nerd-font.rb
@@ -1,20 +1,20 @@
-cask 'font-fira-code-nerd-font' do
-  version '2.1.0'
-  sha256 '21de9aa0edaa3fd2dc1d87fb9ecec0b67c9b3b18bd1998a19904158067fea7e7'
+cask "font-fira-code-nerd-font" do
+  version "2.1.0"
+  sha256 "21de9aa0edaa3fd2dc1d87fb9ecec0b67c9b3b18bd1998a19904158067fea7e7"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraCode.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'FiraCode Nerd Font (Fira Code)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/FiraCode'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "FiraCode Nerd Font (Fira Code)"
+  homepage "https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/FiraCode"
 
-  font 'Fira Code Bold Nerd Font Complete.otf'
-  font 'Fira Code Medium Nerd Font Complete.otf'
-  font 'Fira Code Retina Nerd Font Complete.otf'
-  font 'Fira Code Regular Nerd Font Complete.otf'
-  font 'Fira Code Light Nerd Font Complete.otf'
-  font 'Fira Code Bold Nerd Font Complete Mono.otf'
-  font 'Fira Code Medium Nerd Font Complete Mono.otf'
-  font 'Fira Code Retina Nerd Font Complete Mono.otf'
-  font 'Fira Code Regular Nerd Font Complete Mono.otf'
-  font 'Fira Code Light Nerd Font Complete Mono.otf'
+  font "Fira Code Bold Nerd Font Complete.otf"
+  font "Fira Code Medium Nerd Font Complete.otf"
+  font "Fira Code Retina Nerd Font Complete.otf"
+  font "Fira Code Regular Nerd Font Complete.otf"
+  font "Fira Code Light Nerd Font Complete.otf"
+  font "Fira Code Bold Nerd Font Complete Mono.otf"
+  font "Fira Code Medium Nerd Font Complete Mono.otf"
+  font "Fira Code Retina Nerd Font Complete Mono.otf"
+  font "Fira Code Regular Nerd Font Complete Mono.otf"
+  font "Fira Code Light Nerd Font Complete Mono.otf"
 end
