2084b5b203ad1ff4de2ad26be581f49fc8cf892c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:15 +0100

    font-scada.rb: fix for new style

--- Casks/font-scada.rb
+++ Casks/font-scada.rb
@@ -1,18 +1,18 @@
-cask 'font-scada' do
+cask "font-scada" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/scada',
+  url "https://github.com/google/fonts/trunk/ofl/scada",
       using:      :svn,
       trust_cert: true
-  name 'Scada'
-  homepage 'https://fonts.google.com/specimen/Scada'
+  name "Scada"
+  homepage "https://fonts.google.com/specimen/Scada"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Scada-Bold.ttf'
-  font 'Scada-BoldItalic.ttf'
-  font 'Scada-Italic.ttf'
-  font 'Scada-Regular.ttf'
+  font "Scada-Bold.ttf"
+  font "Scada-BoldItalic.ttf"
+  font "Scada-Italic.ttf"
+  font "Scada-Regular.ttf"
 end
