7d07ba651e1d00ffa405db98051d315dd8e29232
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:13 +0100

    font-saira-semi-condensed.rb: fix for new style

--- Casks/font-saira-semi-condensed.rb
+++ Casks/font-saira-semi-condensed.rb
@@ -1,23 +1,23 @@
-cask 'font-saira-semi-condensed' do
+cask "font-saira-semi-condensed" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/sairasemicondensed',
+  url "https://github.com/google/fonts/trunk/ofl/sairasemicondensed",
       using:      :svn,
       trust_cert: true
-  name 'Saira Semi Condensed'
-  homepage 'https://fonts.google.com/specimen/Saira+Semi+Condensed'
+  name "Saira Semi Condensed"
+  homepage "https://fonts.google.com/specimen/Saira+Semi+Condensed"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'SairaSemiCondensed-Black.ttf'
-  font 'SairaSemiCondensed-Bold.ttf'
-  font 'SairaSemiCondensed-ExtraBold.ttf'
-  font 'SairaSemiCondensed-ExtraLight.ttf'
-  font 'SairaSemiCondensed-Light.ttf'
-  font 'SairaSemiCondensed-Medium.ttf'
-  font 'SairaSemiCondensed-Regular.ttf'
-  font 'SairaSemiCondensed-SemiBold.ttf'
-  font 'SairaSemiCondensed-Thin.ttf'
+  font "SairaSemiCondensed-Black.ttf"
+  font "SairaSemiCondensed-Bold.ttf"
+  font "SairaSemiCondensed-ExtraBold.ttf"
+  font "SairaSemiCondensed-ExtraLight.ttf"
+  font "SairaSemiCondensed-Light.ttf"
+  font "SairaSemiCondensed-Medium.ttf"
+  font "SairaSemiCondensed-Regular.ttf"
+  font "SairaSemiCondensed-SemiBold.ttf"
+  font "SairaSemiCondensed-Thin.ttf"
 end
