39d6c37b5bd07e32a7f3e850ec84b09ca388d498
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:05 +0100

    font-play.rb: fix for new style

--- Casks/font-play.rb
+++ Casks/font-play.rb
@@ -1,16 +1,16 @@
-cask 'font-play' do
+cask "font-play" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/play',
+  url "https://github.com/google/fonts/trunk/ofl/play",
       using:      :svn,
       trust_cert: true
-  name 'Play'
-  homepage 'https://fonts.google.com/specimen/Play'
+  name "Play"
+  homepage "https://fonts.google.com/specimen/Play"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Play-Bold.ttf'
-  font 'Play-Regular.ttf'
+  font "Play-Bold.ttf"
+  font "Play-Regular.ttf"
 end
