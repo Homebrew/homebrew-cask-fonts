a5a95553f7d9d800c51aaec3ca14e6ea26ba6d92
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:30 +0100

    font-zcool-kuaile.rb: fix for new style

--- Casks/font-zcool-kuaile.rb
+++ Casks/font-zcool-kuaile.rb
@@ -1,11 +1,11 @@
-cask 'font-zcool-kuaile' do
+cask "font-zcool-kuaile" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/zcoolkuaile/ZCOOLKuaiLe-Regular.ttf'
-  name 'ZCOOL KuaiLe'
-  homepage 'https://fonts.google.com/specimen/ZCOOL+KuaiLe'
+  url "https://github.com/google/fonts/raw/master/ofl/zcoolkuaile/ZCOOLKuaiLe-Regular.ttf"
+  name "ZCOOL KuaiLe"
+  homepage "https://fonts.google.com/specimen/ZCOOL+KuaiLe"
 
-  font 'ZCOOLKuaiLe-Regular.ttf'
+  font "ZCOOLKuaiLe-Regular.ttf"
 end
