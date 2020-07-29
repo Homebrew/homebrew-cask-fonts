9af584b5cee62373f98bee2cb38866a2f1e0c3b1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:28 +0100

    font-vibes.rb: fix for new style

--- Casks/font-vibes.rb
+++ Casks/font-vibes.rb
@@ -1,11 +1,11 @@
-cask 'font-vibes' do
+cask "font-vibes" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/vibes/Vibes-Regular.ttf'
-  name 'Vibes'
-  homepage 'https://fonts.google.com/specimen/Vibes'
+  url "https://github.com/google/fonts/raw/master/ofl/vibes/Vibes-Regular.ttf"
+  name "Vibes"
+  homepage "https://fonts.google.com/specimen/Vibes"
 
-  font 'Vibes-Regular.ttf'
+  font "Vibes-Regular.ttf"
 end
