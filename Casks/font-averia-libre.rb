705dc5ab55f892749ae1b3f7ea2b75099fc3cb9a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:51 +0100

    font-averia-libre.rb: fix for new style

--- Casks/font-averia-libre.rb
+++ Casks/font-averia-libre.rb
@@ -1,20 +1,20 @@
-cask 'font-averia-libre' do
+cask "font-averia-libre" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/averialibre',
+  url "https://github.com/google/fonts/trunk/ofl/averialibre",
       using:      :svn,
       trust_cert: true
-  name 'Averia Libre'
-  homepage 'https://fonts.google.com/specimen/Averia+Libre'
+  name "Averia Libre"
+  homepage "https://fonts.google.com/specimen/Averia+Libre"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'AveriaLibre-Bold.ttf'
-  font 'AveriaLibre-BoldItalic.ttf'
-  font 'AveriaLibre-Italic.ttf'
-  font 'AveriaLibre-Light.ttf'
-  font 'AveriaLibre-LightItalic.ttf'
-  font 'AveriaLibre-Regular.ttf'
+  font "AveriaLibre-Bold.ttf"
+  font "AveriaLibre-BoldItalic.ttf"
+  font "AveriaLibre-Italic.ttf"
+  font "AveriaLibre-Light.ttf"
+  font "AveriaLibre-LightItalic.ttf"
+  font "AveriaLibre-Regular.ttf"
 end
