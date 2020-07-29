68e6cd1dc344a3a68f4f31c6d3fabcbf45cbeea0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:32 +0100

    font-aurulentsansmono-nerd-font.rb: fix for new style

--- Casks/font-aurulentsansmono-nerd-font.rb
+++ Casks/font-aurulentsansmono-nerd-font.rb
@@ -1,12 +1,12 @@
-cask 'font-aurulentsansmono-nerd-font' do
-  version '2.1.0'
-  sha256 '5f318ab83d3b53ddcdd73739e2c522661d5dacd348abedde611845d510496560'
+cask "font-aurulentsansmono-nerd-font" do
+  version "2.1.0"
+  sha256 "5f318ab83d3b53ddcdd73739e2c522661d5dacd348abedde611845d510496560"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AurulentSansMono.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'AurulentSansMono Nerd Font (Aurulent Sans Mono)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "AurulentSansMono Nerd Font (Aurulent Sans Mono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'AurulentSansMono-Regular Nerd Font Complete.otf'
-  font 'AurulentSansMono-Regular Nerd Font Complete Mono.otf'
+  font "AurulentSansMono-Regular Nerd Font Complete.otf"
+  font "AurulentSansMono-Regular Nerd Font Complete Mono.otf"
 end
