9b30cf68b733566fd3ac66b4634ff3a899bc590a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:07 +0100

    font-archivo-vf-beta.rb: fix for new style

--- Casks/font-archivo-vf-beta.rb
+++ Casks/font-archivo-vf-beta.rb
@@ -1,16 +1,16 @@
-cask 'font-archivo-vf-beta' do
+cask "font-archivo-vf-beta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/archivovfbeta',
+  url "https://github.com/google/fonts/trunk/ofl/archivovfbeta",
       using:      :svn,
       trust_cert: true
-  name 'Archivo VF Beta'
-  homepage 'https://fonts.google.com/earlyaccess'
+  name "Archivo VF Beta"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'ArchivoVFBeta-Italic.ttf'
-  font 'ArchivoVFBeta.ttf'
+  font "ArchivoVFBeta-Italic.ttf"
+  font "ArchivoVFBeta.ttf"
 end
