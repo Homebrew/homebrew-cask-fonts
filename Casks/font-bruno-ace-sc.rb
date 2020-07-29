f0ca391cbf746e8c1ccb4f3505e74a047e56a760
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:23 +0100

    font-bruno-ace-sc.rb: fix for new style

--- Casks/font-bruno-ace-sc.rb
+++ Casks/font-bruno-ace-sc.rb
@@ -1,11 +1,11 @@
-cask 'font-bruno-ace-sc' do
+cask "font-bruno-ace-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/brunoacesc/BrunoAceSC-Regular.ttf'
-  name 'Bruno Ace SC'
-  homepage 'https://fonts.google.com/specimen/Bruno+Ace+SC'
+  url "https://github.com/google/fonts/raw/master/ofl/brunoacesc/BrunoAceSC-Regular.ttf"
+  name "Bruno Ace SC"
+  homepage "https://fonts.google.com/specimen/Bruno+Ace+SC"
 
-  font 'BrunoAceSC-Regular.ttf'
+  font "BrunoAceSC-Regular.ttf"
 end
