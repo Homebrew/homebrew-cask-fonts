f8a8bb522babcfa236954a15db64bfa1f434a510
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:14 +0100

    font-sarala.rb: fix for new style

--- Casks/font-sarala.rb
+++ Casks/font-sarala.rb
@@ -1,16 +1,16 @@
-cask 'font-sarala' do
+cask "font-sarala" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/sarala',
+  url "https://github.com/google/fonts/trunk/ofl/sarala",
       using:      :svn,
       trust_cert: true
-  name 'Sarala'
-  homepage 'https://fonts.google.com/specimen/Sarala'
+  name "Sarala"
+  homepage "https://fonts.google.com/specimen/Sarala"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Sarala-Bold.ttf'
-  font 'Sarala-Regular.ttf'
+  font "Sarala-Bold.ttf"
+  font "Sarala-Regular.ttf"
 end
