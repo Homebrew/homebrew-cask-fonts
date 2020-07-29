a6130f5c23c2a8738c8c540b7c15aaef05297ec1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:21 +0100

    font-stalin-one.rb: fix for new style

--- Casks/font-stalin-one.rb
+++ Casks/font-stalin-one.rb
@@ -1,11 +1,11 @@
-cask 'font-stalin-one' do
+cask "font-stalin-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/stalinone/StalinOne-Regular.ttf'
-  name 'Stalin One'
-  homepage 'https://fonts.google.com/specimen/Stalin+One'
+  url "https://github.com/google/fonts/raw/master/ofl/stalinone/StalinOne-Regular.ttf"
+  name "Stalin One"
+  homepage "https://fonts.google.com/specimen/Stalin+One"
 
-  font 'StalinOne-Regular.ttf'
+  font "StalinOne-Regular.ttf"
 end
