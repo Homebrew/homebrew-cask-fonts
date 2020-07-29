01050831f6b2998258360c4f86bec5ab8880d065
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:29 +0100

    font-yellowtail.rb: fix for new style

--- Casks/font-yellowtail.rb
+++ Casks/font-yellowtail.rb
@@ -1,11 +1,11 @@
-cask 'font-yellowtail' do
+cask "font-yellowtail" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/yellowtail/Yellowtail-Regular.ttf'
-  name 'Yellowtail'
-  homepage 'https://fonts.google.com/specimen/Yellowtail'
+  url "https://github.com/google/fonts/raw/master/apache/yellowtail/Yellowtail-Regular.ttf"
+  name "Yellowtail"
+  homepage "https://fonts.google.com/specimen/Yellowtail"
 
-  font 'Yellowtail-Regular.ttf'
+  font "Yellowtail-Regular.ttf"
 end
