8f9c2731a8488bc39153e243eb3b78b69ae57cf8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:23 +0100

    font-terminal-dosis-light.rb: fix for new style

--- Casks/font-terminal-dosis-light.rb
+++ Casks/font-terminal-dosis-light.rb
@@ -1,11 +1,11 @@
-cask 'font-terminal-dosis-light' do
+cask "font-terminal-dosis-light" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/terminaldosislight/TerminalDosis-Light.ttf'
-  name 'Terminal Dosis Light'
-  homepage 'https://fonts.google.com/specimen/Terminal+Dosis+Light'
+  url "https://github.com/google/fonts/raw/master/ofl/terminaldosislight/TerminalDosis-Light.ttf"
+  name "Terminal Dosis Light"
+  homepage "https://fonts.google.com/specimen/Terminal+Dosis+Light"
 
-  font 'TerminalDosis-Light.ttf'
+  font "TerminalDosis-Light.ttf"
 end
