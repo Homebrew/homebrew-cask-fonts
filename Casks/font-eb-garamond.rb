7b0449ca541a9a9169f50ab8c44f5b2190e57227
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:46 +0100

    font-eb-garamond.rb: fix for new style

--- Casks/font-eb-garamond.rb
+++ Casks/font-eb-garamond.rb
@@ -1,12 +1,12 @@
-cask 'font-eb-garamond' do
-  version '0.016'
-  sha256 'a0b77b405f55c10cff078787ef9d2389a9638e7604d53aa80207fe62e104c378'
+cask "font-eb-garamond" do
+  version "0.016"
+  sha256 "a0b77b405f55c10cff078787ef9d2389a9638e7604d53aa80207fe62e104c378"
 
   # bitbucket.org/georgd/eb-garamond/ was verified as official when first introduced to the cask
   url "https://bitbucket.org/georgd/eb-garamond/downloads/EBGaramond-#{version}.zip"
-  appcast 'https://github.com/georgd/EB-Garamond/releases.atom'
-  name 'EB Garamond'
-  homepage 'https://github.com/georgd/EB-Garamond'
+  appcast "https://github.com/georgd/EB-Garamond/releases.atom"
+  name "EB Garamond"
+  homepage "https://github.com/georgd/EB-Garamond"
 
   font "EBGaramond-#{version}/otf/EBGaramond-Initials.otf"
   font "EBGaramond-#{version}/otf/EBGaramond-InitialsF1.otf"
