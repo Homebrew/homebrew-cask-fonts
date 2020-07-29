cdde2d1a7c1909fc765e8ed160e072a0097f15c9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:30 +0100

    font-kulim-park.rb: fix for new style

--- Casks/font-kulim-park.rb
+++ Casks/font-kulim-park.rb
@@ -1,24 +1,24 @@
-cask 'font-kulim-park' do
+cask "font-kulim-park" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/kulimpark',
+  url "https://github.com/google/fonts/trunk/ofl/kulimpark",
       using:      :svn,
       trust_cert: true
-  name 'Kulim Park'
-  homepage 'https://fonts.google.com/specimen/Kulim+Park'
+  name "Kulim Park"
+  homepage "https://fonts.google.com/specimen/Kulim+Park"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'KulimPark-Bold.ttf'
-  font 'KulimPark-BoldItalic.ttf'
-  font 'KulimPark-ExtraLight.ttf'
-  font 'KulimPark-ExtraLightItalic.ttf'
-  font 'KulimPark-Italic.ttf'
-  font 'KulimPark-Light.ttf'
-  font 'KulimPark-LightItalic.ttf'
-  font 'KulimPark-Regular.ttf'
-  font 'KulimPark-SemiBold.ttf'
-  font 'KulimPark-SemiBoldItalic.ttf'
+  font "KulimPark-Bold.ttf"
+  font "KulimPark-BoldItalic.ttf"
+  font "KulimPark-ExtraLight.ttf"
+  font "KulimPark-ExtraLightItalic.ttf"
+  font "KulimPark-Italic.ttf"
+  font "KulimPark-Light.ttf"
+  font "KulimPark-LightItalic.ttf"
+  font "KulimPark-Regular.ttf"
+  font "KulimPark-SemiBold.ttf"
+  font "KulimPark-SemiBoldItalic.ttf"
 end
