b0ffcad42b9aa297874a95a5d322dbe8c9459743
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:42 +0100

    font-metal-mania.rb: fix for new style

--- Casks/font-metal-mania.rb
+++ Casks/font-metal-mania.rb
@@ -1,11 +1,11 @@
-cask 'font-metal-mania' do
+cask "font-metal-mania" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/metalmania/MetalMania-Regular.ttf'
-  name 'Metal Mania'
-  homepage 'https://fonts.google.com/specimen/Metal+Mania'
+  url "https://github.com/google/fonts/raw/master/ofl/metalmania/MetalMania-Regular.ttf"
+  name "Metal Mania"
+  homepage "https://fonts.google.com/specimen/Metal+Mania"
 
-  font 'MetalMania-Regular.ttf'
+  font "MetalMania-Regular.ttf"
 end
