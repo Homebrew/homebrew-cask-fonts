908cf75ed9a04a228199dbaeaabea04e43bdc358
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:29 +0100

    font-chango.rb: fix for new style

--- Casks/font-chango.rb
+++ Casks/font-chango.rb
@@ -1,11 +1,11 @@
-cask 'font-chango' do
+cask "font-chango" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/chango/Chango-Regular.ttf'
-  name 'Chango'
-  homepage 'https://fonts.google.com/specimen/Chango'
+  url "https://github.com/google/fonts/raw/master/ofl/chango/Chango-Regular.ttf"
+  name "Chango"
+  homepage "https://fonts.google.com/specimen/Chango"
 
-  font 'Chango-Regular.ttf'
+  font "Chango-Regular.ttf"
 end
