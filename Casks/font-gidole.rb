9bc05e0f48ecc5e023456f5d7c67d7c791a15376
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:01 +0100

    font-gidole.rb: fix for new style

--- Casks/font-gidole.rb
+++ Casks/font-gidole.rb
@@ -1,12 +1,12 @@
-cask 'font-gidole' do
+cask "font-gidole" do
   version :latest
   sha256 :no_check
 
   # github.com/gidole/ was verified as official when first introduced to the cask
-  url 'https://github.com/gidole/Gidole-Typefaces/raw/master/gidole.zip'
-  name 'Gidole'
-  homepage 'https://gidole.github.io/'
+  url "https://github.com/gidole/Gidole-Typefaces/raw/master/gidole.zip"
+  name "Gidole"
+  homepage "https://gidole.github.io/"
 
-  font 'GidoleFont/Gidole-Regular.ttf'
-  font 'GidoleFont/Gidolinya-Regular.otf'
+  font "GidoleFont/Gidole-Regular.ttf"
+  font "GidoleFont/Gidolinya-Regular.otf"
 end
