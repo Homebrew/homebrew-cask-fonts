7d9a442573f49fdea0a035df4d170e838cd764a0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:39 +0100

    font-markazi-text.rb: fix for new style

--- Casks/font-markazi-text.rb
+++ Casks/font-markazi-text.rb
@@ -1,11 +1,11 @@
-cask 'font-markazi-text' do
+cask "font-markazi-text" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/markazitextvfbeta/MarkaziText-VF.ttf'
-  name 'Markazi Text'
-  homepage 'https://fonts.google.com/specimen/Markazi+Text'
+  url "https://github.com/google/fonts/raw/master/ofl/markazitextvfbeta/MarkaziText-VF.ttf"
+  name "Markazi Text"
+  homepage "https://fonts.google.com/specimen/Markazi+Text"
 
-  font 'MarkaziText-VF.ttf'
+  font "MarkaziText-VF.ttf"
 end
