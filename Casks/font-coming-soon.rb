d11d16ed8c79c38874b8f8b2820218df3e9d31af
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:34 +0100

    font-coming-soon.rb: fix for new style

--- Casks/font-coming-soon.rb
+++ Casks/font-coming-soon.rb
@@ -1,11 +1,11 @@
-cask 'font-coming-soon' do
+cask "font-coming-soon" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/comingsoon/ComingSoon-Regular.ttf'
-  name 'Coming Soon'
-  homepage 'https://fonts.google.com/specimen/Coming+Soon'
+  url "https://github.com/google/fonts/raw/master/apache/comingsoon/ComingSoon-Regular.ttf"
+  name "Coming Soon"
+  homepage "https://fonts.google.com/specimen/Coming+Soon"
 
-  font 'ComingSoon-Regular.ttf'
+  font "ComingSoon-Regular.ttf"
 end
