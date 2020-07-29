8e7fcac880f216be94e054cec38363fbaef43bd5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:01 +0100

    font-gfs-neohellenic.rb: fix for new style

--- Casks/font-gfs-neohellenic.rb
+++ Casks/font-gfs-neohellenic.rb
@@ -1,18 +1,18 @@
-cask 'font-gfs-neohellenic' do
+cask "font-gfs-neohellenic" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/gfsneohellenic',
+  url "https://github.com/google/fonts/trunk/ofl/gfsneohellenic",
       using:      :svn,
       trust_cert: true
-  name 'GFS Neohellenic'
-  homepage 'https://fonts.google.com/specimen/GFS+Neohellenic'
+  name "GFS Neohellenic"
+  homepage "https://fonts.google.com/specimen/GFS+Neohellenic"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'GFSNeohellenic.ttf'
-  font 'GFSNeohellenicBold.ttf'
-  font 'GFSNeohellenicBoldItalic.ttf'
-  font 'GFSNeohellenicItalic.ttf'
+  font "GFSNeohellenic.ttf"
+  font "GFSNeohellenicBold.ttf"
+  font "GFSNeohellenicBoldItalic.ttf"
+  font "GFSNeohellenicItalic.ttf"
 end
