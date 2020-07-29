7c5629fdd8d96bc93fed5b6b57fc9f10f81094ba
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:50 +0100

    font-noto-mono-for-powerline.rb: fix for new style

--- Casks/font-noto-mono-for-powerline.rb
+++ Casks/font-noto-mono-for-powerline.rb
@@ -1,14 +1,14 @@
-cask 'font-noto-mono-for-powerline' do
+cask "font-noto-mono-for-powerline" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/powerline/fonts/trunk/NotoMono',
+  url "https://github.com/powerline/fonts/trunk/NotoMono",
       using:      :svn,
       trust_cert: true
-  name 'Noto Mono for Powerline'
-  homepage 'https://github.com/powerline/fonts/tree/master/NotoMono'
+  name "Noto Mono for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/NotoMono"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Noto Mono for Powerline.ttf'
+  font "Noto Mono for Powerline.ttf"
 end
