ccf2e4d3ac5f02aeddc0a08e1988464b9bb7b3f9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:25 +0100

    font-junicode.rb: fix for new style

--- Casks/font-junicode.rb
+++ Casks/font-junicode.rb
@@ -1,16 +1,16 @@
-cask 'font-junicode' do
-  version '1.002'
-  sha256 'c199d96c8424be60fcab8d00d2eee39ea8ae632cfd5e710cbbd70626d6a729e7'
+cask "font-junicode" do
+  version "1.002"
+  sha256 "c199d96c8424be60fcab8d00d2eee39ea8ae632cfd5e710cbbd70626d6a729e7"
 
   # downloads.sourceforge.net/junicode/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/junicode/junicode-#{version}.zip"
-  appcast 'https://sourceforge.net/projects/junicode/rss'
-  name 'Junicode'
-  homepage 'https://junicode.sourceforge.io/'
+  appcast "https://sourceforge.net/projects/junicode/rss"
+  name "Junicode"
+  homepage "https://junicode.sourceforge.io/"
 
-  font 'FoulisGreek.ttf'
-  font 'Junicode-Bold.ttf'
-  font 'Junicode-BoldItalic.ttf'
-  font 'Junicode-Italic.ttf'
-  font 'Junicode.ttf'
+  font "FoulisGreek.ttf"
+  font "Junicode-Bold.ttf"
+  font "Junicode-BoldItalic.ttf"
+  font "Junicode-Italic.ttf"
+  font "Junicode.ttf"
 end
