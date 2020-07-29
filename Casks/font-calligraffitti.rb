93081703fbb9e711d4de56792ed86a8adac2c1e6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:26 +0100

    font-calligraffitti.rb: fix for new style

--- Casks/font-calligraffitti.rb
+++ Casks/font-calligraffitti.rb
@@ -1,11 +1,11 @@
-cask 'font-calligraffitti' do
+cask "font-calligraffitti" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/calligraffitti/Calligraffitti-Regular.ttf'
-  name 'Calligraffitti'
-  homepage 'https://fonts.google.com/specimen/Calligraffitti'
+  url "https://github.com/google/fonts/raw/master/apache/calligraffitti/Calligraffitti-Regular.ttf"
+  name "Calligraffitti"
+  homepage "https://fonts.google.com/specimen/Calligraffitti"
 
-  font 'Calligraffitti-Regular.ttf'
+  font "Calligraffitti-Regular.ttf"
 end
