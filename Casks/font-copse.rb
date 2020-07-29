1e05f536cd590b2ed683531265d85d15e59ff6f9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:35 +0100

    font-copse.rb: fix for new style

--- Casks/font-copse.rb
+++ Casks/font-copse.rb
@@ -1,11 +1,11 @@
-cask 'font-copse' do
+cask "font-copse" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/copse/Copse-Regular.ttf'
-  name 'Copse'
-  homepage 'https://fonts.google.com/specimen/Copse'
+  url "https://github.com/google/fonts/raw/master/ofl/copse/Copse-Regular.ttf"
+  name "Copse"
+  homepage "https://fonts.google.com/specimen/Copse"
 
-  font 'Copse-Regular.ttf'
+  font "Copse-Regular.ttf"
 end
