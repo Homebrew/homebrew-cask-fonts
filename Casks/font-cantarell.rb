3d9d7d73c5d9b2a89bc3418ec15bff7cff4d3bd7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:26 +0100

    font-cantarell.rb: fix for new style

--- Casks/font-cantarell.rb
+++ Casks/font-cantarell.rb
@@ -1,18 +1,18 @@
-cask 'font-cantarell' do
+cask "font-cantarell" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/cantarell',
+  url "https://github.com/google/fonts/trunk/ofl/cantarell",
       using:      :svn,
       trust_cert: true
-  name 'Cantarell'
-  homepage 'https://fonts.google.com/specimen/Cantarell'
+  name "Cantarell"
+  homepage "https://fonts.google.com/specimen/Cantarell"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Cantarell-Bold.ttf'
-  font 'Cantarell-BoldOblique.ttf'
-  font 'Cantarell-Oblique.ttf'
-  font 'Cantarell-Regular.ttf'
+  font "Cantarell-Bold.ttf"
+  font "Cantarell-BoldOblique.ttf"
+  font "Cantarell-Oblique.ttf"
+  font "Cantarell-Regular.ttf"
 end
