ee3a4a7f69e00e4525ccda48b3231212b5173e8e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:02 +0100

    font-oregano.rb: fix for new style

--- Casks/font-oregano.rb
+++ Casks/font-oregano.rb
@@ -1,16 +1,16 @@
-cask 'font-oregano' do
+cask "font-oregano" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/oregano',
+  url "https://github.com/google/fonts/trunk/ofl/oregano",
       using:      :svn,
       trust_cert: true
-  name 'Oregano'
-  homepage 'https://fonts.google.com/specimen/Oregano'
+  name "Oregano"
+  homepage "https://fonts.google.com/specimen/Oregano"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Oregano-Italic.ttf'
-  font 'Oregano-Regular.ttf'
+  font "Oregano-Italic.ttf"
+  font "Oregano-Regular.ttf"
 end
