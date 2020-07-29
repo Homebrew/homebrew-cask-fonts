ba3fb447ac91d3830cf0a1f7349cdfd17a819f66
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:17 +0100

    font-baskervville.rb: fix for new style

--- Casks/font-baskervville.rb
+++ Casks/font-baskervville.rb
@@ -1,16 +1,16 @@
-cask 'font-baskervville' do
+cask "font-baskervville" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/baskervville',
+  url "https://github.com/google/fonts/trunk/ofl/baskervville",
       using:      :svn,
       trust_cert: true
-  name 'Baskervville'
-  homepage 'https://fonts.google.com/specimen/Baskervville'
+  name "Baskervville"
+  homepage "https://fonts.google.com/specimen/Baskervville"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Baskervville-Italic.ttf'
-  font 'Baskervville-Regular.ttf'
+  font "Baskervville-Italic.ttf"
+  font "Baskervville-Regular.ttf"
 end
