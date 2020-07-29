6ec1a249d5981a0ce11dd24648f4b8e51dd38300
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:59 +0100

    font-gemunu-libre.rb: fix for new style

--- Casks/font-gemunu-libre.rb
+++ Casks/font-gemunu-libre.rb
@@ -1,21 +1,21 @@
-cask 'font-gemunu-libre' do
+cask "font-gemunu-libre" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/gemunulibre',
+  url "https://github.com/google/fonts/trunk/ofl/gemunulibre",
       using:      :svn,
       trust_cert: true
-  name 'Gemunu Libre'
-  homepage 'https://fonts.google.com/specimen/Gemunu+Libre'
+  name "Gemunu Libre"
+  homepage "https://fonts.google.com/specimen/Gemunu+Libre"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'GemunuLibre-Bold.ttf'
-  font 'GemunuLibre-ExtraBold.ttf'
-  font 'GemunuLibre-ExtraLight.ttf'
-  font 'GemunuLibre-Light.ttf'
-  font 'GemunuLibre-Medium.ttf'
-  font 'GemunuLibre-Regular.ttf'
-  font 'GemunuLibre-SemiBold.ttf'
+  font "GemunuLibre-Bold.ttf"
+  font "GemunuLibre-ExtraBold.ttf"
+  font "GemunuLibre-ExtraLight.ttf"
+  font "GemunuLibre-Light.ttf"
+  font "GemunuLibre-Medium.ttf"
+  font "GemunuLibre-Regular.ttf"
+  font "GemunuLibre-SemiBold.ttf"
 end
