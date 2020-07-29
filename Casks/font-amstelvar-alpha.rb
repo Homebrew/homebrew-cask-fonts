c547bbf2b33027dd03f2db7fe6833eed6ebaf3da
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:46 +0100

    font-amstelvar-alpha.rb: fix for new style

--- Casks/font-amstelvar-alpha.rb
+++ Casks/font-amstelvar-alpha.rb
@@ -1,11 +1,11 @@
-cask 'font-amstelvar-alpha' do
+cask "font-amstelvar-alpha" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/amstelvaralpha/AmstelvarAlpha-VF.ttf'
-  name 'Amstelvar Alpha'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/amstelvaralpha/AmstelvarAlpha-VF.ttf"
+  name "Amstelvar Alpha"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'AmstelvarAlpha-VF.ttf'
+  font "AmstelvarAlpha-VF.ttf"
 end
