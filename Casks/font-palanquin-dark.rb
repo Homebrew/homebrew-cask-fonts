7d255bd0f0f7c29421ad5f991dba847f41680735
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:03 +0100

    font-palanquin-dark.rb: fix for new style

--- Casks/font-palanquin-dark.rb
+++ Casks/font-palanquin-dark.rb
@@ -1,18 +1,18 @@
-cask 'font-palanquin-dark' do
+cask "font-palanquin-dark" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/palanquindark',
+  url "https://github.com/google/fonts/trunk/ofl/palanquindark",
       using:      :svn,
       trust_cert: true
-  name 'Palanquin Dark'
-  homepage 'https://fonts.google.com/specimen/Palanquin+Dark'
+  name "Palanquin Dark"
+  homepage "https://fonts.google.com/specimen/Palanquin+Dark"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'PalanquinDark-Bold.ttf'
-  font 'PalanquinDark-Medium.ttf'
-  font 'PalanquinDark-Regular.ttf'
-  font 'PalanquinDark-SemiBold.ttf'
+  font "PalanquinDark-Bold.ttf"
+  font "PalanquinDark-Medium.ttf"
+  font "PalanquinDark-Regular.ttf"
+  font "PalanquinDark-SemiBold.ttf"
 end
