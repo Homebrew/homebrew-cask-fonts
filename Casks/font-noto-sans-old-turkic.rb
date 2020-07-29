d95e4ffa20ba9c7b5ec6bfdf31ead3ea97b8c890
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:55 +0100

    font-noto-sans-old-turkic.rb: fix for new style

--- Casks/font-noto-sans-old-turkic.rb
+++ Casks/font-noto-sans-old-turkic.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-old-turkic' do
+cask "font-noto-sans-old-turkic" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldTurkic-unhinted.zip'
-  name 'Noto Sans Old Turkic'
-  homepage 'https://www.google.com/get/noto/#sans-orkh'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldTurkic-unhinted.zip"
+  name "Noto Sans Old Turkic"
+  homepage "https://www.google.com/get/noto/#sans-orkh"
 
-  font 'NotoSansOldTurkic-Regular.ttf'
+  font "NotoSansOldTurkic-Regular.ttf"
 end
