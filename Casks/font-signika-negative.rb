b9b4adcf1418c3dd433358e4ca7ee035fc914efc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:17 +0100

    font-signika-negative.rb: fix for new style

--- Casks/font-signika-negative.rb
+++ Casks/font-signika-negative.rb
@@ -1,18 +1,18 @@
-cask 'font-signika-negative' do
+cask "font-signika-negative" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/signikanegative',
+  url "https://github.com/google/fonts/trunk/ofl/signikanegative",
       using:      :svn,
       trust_cert: true
-  name 'Signika Negative'
-  homepage 'https://fonts.google.com/specimen/Signika+Negative'
+  name "Signika Negative"
+  homepage "https://fonts.google.com/specimen/Signika+Negative"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'SignikaNegative-Bold.ttf'
-  font 'SignikaNegative-Light.ttf'
-  font 'SignikaNegative-Regular.ttf'
-  font 'SignikaNegative-SemiBold.ttf'
+  font "SignikaNegative-Bold.ttf"
+  font "SignikaNegative-Light.ttf"
+  font "SignikaNegative-Regular.ttf"
+  font "SignikaNegative-SemiBold.ttf"
 end
