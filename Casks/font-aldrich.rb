e30ed12881681933b8fa81e17fe0230745c49b86
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:32:16 +0100

    font-aldrich.rb: fix for new style

--- Casks/font-aldrich.rb
+++ Casks/font-aldrich.rb
@@ -1,11 +1,11 @@
-cask 'font-aldrich' do
+cask "font-aldrich" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/aldrich/Aldrich-Regular.ttf'
-  name 'Aldrich'
-  homepage 'https://fonts.google.com/specimen/Aldrich'
+  url "https://github.com/google/fonts/raw/master/ofl/aldrich/Aldrich-Regular.ttf"
+  name "Aldrich"
+  homepage "https://fonts.google.com/specimen/Aldrich"
 
-  font 'Aldrich-Regular.ttf'
+  font "Aldrich-Regular.ttf"
 end
