bd7d22e2310a4b26361edc2972f5931bade8d445
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:41 +0100

    font-merriweather-sans.rb: fix for new style

--- Casks/font-merriweather-sans.rb
+++ Casks/font-merriweather-sans.rb
@@ -1,22 +1,22 @@
-cask 'font-merriweather-sans' do
+cask "font-merriweather-sans" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/merriweathersans',
+  url "https://github.com/google/fonts/trunk/ofl/merriweathersans",
       using:      :svn,
       trust_cert: true
-  name 'Merriweather Sans'
-  homepage 'https://fonts.google.com/specimen/Merriweather+Sans'
+  name "Merriweather Sans"
+  homepage "https://fonts.google.com/specimen/Merriweather+Sans"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'MerriweatherSans-Bold.ttf'
-  font 'MerriweatherSans-BoldItalic.ttf'
-  font 'MerriweatherSans-ExtraBold.ttf'
-  font 'MerriweatherSans-ExtraBoldItalic.ttf'
-  font 'MerriweatherSans-Italic.ttf'
-  font 'MerriweatherSans-Light.ttf'
-  font 'MerriweatherSans-LightItalic.ttf'
-  font 'MerriweatherSans-Regular.ttf'
+  font "MerriweatherSans-Bold.ttf"
+  font "MerriweatherSans-BoldItalic.ttf"
+  font "MerriweatherSans-ExtraBold.ttf"
+  font "MerriweatherSans-ExtraBoldItalic.ttf"
+  font "MerriweatherSans-Italic.ttf"
+  font "MerriweatherSans-Light.ttf"
+  font "MerriweatherSans-LightItalic.ttf"
+  font "MerriweatherSans-Regular.ttf"
 end
