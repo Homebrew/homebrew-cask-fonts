06328620faa8ec221627aba923ce921a5b99f094
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:42 +0100

    font-miama.rb: fix for new style

--- Casks/font-miama.rb
+++ Casks/font-miama.rb
@@ -1,11 +1,11 @@
-cask 'font-miama' do
+cask "font-miama" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/miama/Miama-Regular.ttf'
-  name 'Miama'
-  homepage 'https://fonts.google.com/specimen/Miama'
+  url "https://github.com/google/fonts/raw/master/ofl/miama/Miama-Regular.ttf"
+  name "Miama"
+  homepage "https://fonts.google.com/specimen/Miama"
 
-  font 'Miama-Regular.ttf'
+  font "Miama-Regular.ttf"
 end
