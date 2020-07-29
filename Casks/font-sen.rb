7fa6b1b89c0c0dcf9116389536e55c9f631b6403
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:16 +0100

    font-sen.rb: fix for new style

--- Casks/font-sen.rb
+++ Casks/font-sen.rb
@@ -1,17 +1,17 @@
-cask 'font-sen' do
+cask "font-sen" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/sen',
+  url "https://github.com/google/fonts/trunk/ofl/sen",
       using:      :svn,
       trust_cert: true
-  name 'Sen'
-  homepage 'https://fonts.google.com/specimen/Sen'
+  name "Sen"
+  homepage "https://fonts.google.com/specimen/Sen"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Sen-Bold.ttf'
-  font 'Sen-ExtraBold.ttf'
-  font 'Sen-Regular.ttf'
+  font "Sen-Bold.ttf"
+  font "Sen-ExtraBold.ttf"
+  font "Sen-Regular.ttf"
 end
