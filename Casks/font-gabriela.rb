57c2c101d8e45aa49db35a11064fc0eb36a1a423
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:57 +0100

    font-gabriela.rb: fix for new style

--- Casks/font-gabriela.rb
+++ Casks/font-gabriela.rb
@@ -1,11 +1,11 @@
-cask 'font-gabriela' do
+cask "font-gabriela" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/gabriela/Gabriela-Regular.ttf'
-  name 'Gabriela'
-  homepage 'https://fonts.google.com/specimen/Gabriela'
+  url "https://github.com/google/fonts/raw/master/ofl/gabriela/Gabriela-Regular.ttf"
+  name "Gabriela"
+  homepage "https://fonts.google.com/specimen/Gabriela"
 
-  font 'Gabriela-Regular.ttf'
+  font "Gabriela-Regular.ttf"
 end
