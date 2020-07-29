cc0e73c9b63ea86a8de256d4f62a6118d550df2c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:59 +0100

    font-genryumin.rb: fix for new style

--- Casks/font-genryumin.rb
+++ Casks/font-genryumin.rb
@@ -1,17 +1,17 @@
-cask 'font-genryumin' do
-  version '1.501'
-  sha256 'e0ddfef92cd319e829fb968a44ce17301e5b35a727824b3c03e3aa9d946e25cd'
+cask "font-genryumin" do
+  version "1.501"
+  sha256 "e0ddfef92cd319e829fb968a44ce17301e5b35a727824b3c03e3aa9d946e25cd"
 
   url "https://github.com/ButTaiwan/genryu-font/releases/download/v#{version}/GenRyuMin.zip"
-  appcast 'https://github.com/ButTaiwan/genryu-font/releases.atom'
-  name 'GenRyuMin'
-  homepage 'https://github.com/ButTaiwan/genryu-font'
+  appcast "https://github.com/ButTaiwan/genryu-font/releases.atom"
+  name "GenRyuMin"
+  homepage "https://github.com/ButTaiwan/genryu-font"
 
-  font 'GenRyuMin-B.ttc'
-  font 'GenRyuMin-EL.ttc'
-  font 'GenRyuMin-H.ttc'
-  font 'GenRyuMin-L.ttc'
-  font 'GenRyuMin-M.ttc'
-  font 'GenRyuMin-R.ttc'
-  font 'GenRyuMin-SB.ttc'
+  font "GenRyuMin-B.ttc"
+  font "GenRyuMin-EL.ttc"
+  font "GenRyuMin-H.ttc"
+  font "GenRyuMin-L.ttc"
+  font "GenRyuMin-M.ttc"
+  font "GenRyuMin-R.ttc"
+  font "GenRyuMin-SB.ttc"
 end
