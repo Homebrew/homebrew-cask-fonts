de13914d2b0a8f70c06f4a0f3aa4d8a75419031f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:57 +0100

    font-noto-sans-tagalog.rb: fix for new style

--- Casks/font-noto-sans-tagalog.rb
+++ Casks/font-noto-sans-tagalog.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-tagalog' do
+cask "font-noto-sans-tagalog" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagalog-unhinted.zip'
-  name 'Noto Sans Tagalog'
-  homepage 'https://www.google.com/get/noto/#sans-tglg'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagalog-unhinted.zip"
+  name "Noto Sans Tagalog"
+  homepage "https://www.google.com/get/noto/#sans-tglg"
 
-  font 'NotoSansTagalog-Regular.ttf'
+  font "NotoSansTagalog-Regular.ttf"
 end
