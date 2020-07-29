f6a0acd9848cce0f6bc5cc3f5bcee20877fde336
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:25 +0100

    font-tillana.rb: fix for new style

--- Casks/font-tillana.rb
+++ Casks/font-tillana.rb
@@ -1,19 +1,19 @@
-cask 'font-tillana' do
+cask "font-tillana" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/tillana',
+  url "https://github.com/google/fonts/trunk/ofl/tillana",
       using:      :svn,
       trust_cert: true
-  name 'Tillana'
-  homepage 'https://fonts.google.com/specimen/Tillana'
+  name "Tillana"
+  homepage "https://fonts.google.com/specimen/Tillana"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Tillana-Bold.ttf'
-  font 'Tillana-ExtraBold.ttf'
-  font 'Tillana-Medium.ttf'
-  font 'Tillana-Regular.ttf'
-  font 'Tillana-SemiBold.ttf'
+  font "Tillana-Bold.ttf"
+  font "Tillana-ExtraBold.ttf"
+  font "Tillana-Medium.ttf"
+  font "Tillana-Regular.ttf"
+  font "Tillana-SemiBold.ttf"
 end
