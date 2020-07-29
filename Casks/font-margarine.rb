425320cd9835a45246fa06d4b35f866ede5390e3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:39 +0100

    font-margarine.rb: fix for new style

--- Casks/font-margarine.rb
+++ Casks/font-margarine.rb
@@ -1,11 +1,11 @@
-cask 'font-margarine' do
+cask "font-margarine" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/margarine/Margarine-Regular.ttf'
-  name 'Margarine'
-  homepage 'https://fonts.google.com/specimen/Margarine'
+  url "https://github.com/google/fonts/raw/master/ofl/margarine/Margarine-Regular.ttf"
+  name "Margarine"
+  homepage "https://fonts.google.com/specimen/Margarine"
 
-  font 'Margarine-Regular.ttf'
+  font "Margarine-Regular.ttf"
 end
