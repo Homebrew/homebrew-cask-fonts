c767169e7cd8a9cb02de93ed5d47ad1067452ec0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:10 +0100

    font-reem-kufi.rb: fix for new style

--- Casks/font-reem-kufi.rb
+++ Casks/font-reem-kufi.rb
@@ -1,11 +1,11 @@
-cask 'font-reem-kufi' do
+cask "font-reem-kufi" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/reemkufi/ReemKufi-Regular.ttf'
-  name 'Reem Kufi'
-  homepage 'https://fonts.google.com/specimen/Reem+Kufi'
+  url "https://github.com/google/fonts/raw/master/ofl/reemkufi/ReemKufi-Regular.ttf"
+  name "Reem Kufi"
+  homepage "https://fonts.google.com/specimen/Reem+Kufi"
 
-  font 'ReemKufi-Regular.ttf'
+  font "ReemKufi-Regular.ttf"
 end
