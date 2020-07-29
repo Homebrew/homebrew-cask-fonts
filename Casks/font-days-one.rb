cf8512d1d0bab90758a2e641a129eb747a692b13
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:41 +0100

    font-days-one.rb: fix for new style

--- Casks/font-days-one.rb
+++ Casks/font-days-one.rb
@@ -1,11 +1,11 @@
-cask 'font-days-one' do
+cask "font-days-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/daysone/DaysOne-Regular.ttf'
-  name 'Days One'
-  homepage 'https://fonts.google.com/specimen/Days+One'
+  url "https://github.com/google/fonts/raw/master/ofl/daysone/DaysOne-Regular.ttf"
+  name "Days One"
+  homepage "https://fonts.google.com/specimen/Days+One"
 
-  font 'DaysOne-Regular.ttf'
+  font "DaysOne-Regular.ttf"
 end
