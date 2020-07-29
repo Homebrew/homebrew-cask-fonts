f329b20e8710ad383df3943a02d00c6b1a1db608
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:39 +0100

    font-croissant-one.rb: fix for new style

--- Casks/font-croissant-one.rb
+++ Casks/font-croissant-one.rb
@@ -1,11 +1,11 @@
-cask 'font-croissant-one' do
+cask "font-croissant-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/croissantone/CroissantOne-Regular.ttf'
-  name 'Croissant One'
-  homepage 'https://fonts.google.com/specimen/Croissant+One'
+  url "https://github.com/google/fonts/raw/master/ofl/croissantone/CroissantOne-Regular.ttf"
+  name "Croissant One"
+  homepage "https://fonts.google.com/specimen/Croissant+One"
 
-  font 'CroissantOne-Regular.ttf'
+  font "CroissantOne-Regular.ttf"
 end
