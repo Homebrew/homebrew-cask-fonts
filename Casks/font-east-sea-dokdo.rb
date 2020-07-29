bf9b87129ac93c8c90b49c00c7eaebeb67683dfd
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:46 +0100

    font-east-sea-dokdo.rb: fix for new style

--- Casks/font-east-sea-dokdo.rb
+++ Casks/font-east-sea-dokdo.rb
@@ -1,11 +1,11 @@
-cask 'font-east-sea-dokdo' do
+cask "font-east-sea-dokdo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/eastseadokdo/EastSeaDokdo-Regular.ttf'
-  name 'East Sea Dokdo'
-  homepage 'https://fonts.google.com/specimen/East+Sea+Dokdo'
+  url "https://github.com/google/fonts/raw/master/ofl/eastseadokdo/EastSeaDokdo-Regular.ttf"
+  name "East Sea Dokdo"
+  homepage "https://fonts.google.com/specimen/East+Sea+Dokdo"
 
-  font 'EastSeaDokdo-Regular.ttf'
+  font "EastSeaDokdo-Regular.ttf"
 end
