966f019e58d86504519f1e776651ae3f26e87151
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:27 +0100

    font-anonymous-pro.rb: fix for new style

--- Casks/font-anonymous-pro.rb
+++ Casks/font-anonymous-pro.rb
@@ -1,10 +1,10 @@
-cask 'font-anonymous-pro' do
-  version '1.002'
-  sha256 '86665847a51cdfb58a1e1dfd8b1ba33f183485affe50b53e3304f63d3d3552ab'
+cask "font-anonymous-pro" do
+  version "1.002"
+  sha256 "86665847a51cdfb58a1e1dfd8b1ba33f183485affe50b53e3304f63d3d3552ab"
 
   url "https://www.marksimonson.com/assets/content/fonts/AnonymousPro-#{version}.zip"
-  name 'Anonymous Pro'
-  homepage 'https://www.marksimonson.com/fonts/view/anonymous-pro'
+  name "Anonymous Pro"
+  homepage "https://www.marksimonson.com/fonts/view/anonymous-pro"
 
   font "AnonymousPro-#{version}.001/Anonymous Pro B.ttf"
   font "AnonymousPro-#{version}.001/Anonymous Pro BI.ttf"
