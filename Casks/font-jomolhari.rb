7620ece47987c42cdf4a1a6e6a66ef5c483c3575
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:23 +0100

    font-jomolhari.rb: fix for new style

--- Casks/font-jomolhari.rb
+++ Casks/font-jomolhari.rb
@@ -1,11 +1,11 @@
-cask 'font-jomolhari' do
+cask "font-jomolhari" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/jomolhari/Jomolhari-Regular.ttf'
-  name 'Jomolhari'
-  homepage 'https://fonts.google.com/specimen/Jomolhari'
+  url "https://github.com/google/fonts/raw/master/ofl/jomolhari/Jomolhari-Regular.ttf"
+  name "Jomolhari"
+  homepage "https://fonts.google.com/specimen/Jomolhari"
 
-  font 'Jomolhari-Regular.ttf'
+  font "Jomolhari-Regular.ttf"
 end
