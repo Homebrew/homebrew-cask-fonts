bf5eea2ce78a4b29e903d31f42eb52d6a2497149
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:48 +0100

    font-nico-moji.rb: fix for new style

--- Casks/font-nico-moji.rb
+++ Casks/font-nico-moji.rb
@@ -1,11 +1,11 @@
-cask 'font-nico-moji' do
+cask "font-nico-moji" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/nicomoji/NicoMoji-Regular.ttf'
-  name 'Nico Moji'
-  homepage 'https://fonts.google.com/specimen/Nico+Moji'
+  url "https://github.com/google/fonts/raw/master/ofl/nicomoji/NicoMoji-Regular.ttf"
+  name "Nico Moji"
+  homepage "https://fonts.google.com/specimen/Nico+Moji"
 
-  font 'NicoMoji-Regular.ttf'
+  font "NicoMoji-Regular.ttf"
 end
