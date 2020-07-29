b9056b0125ccb2a4a2b830fb5e85d8b2044e6987
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:17 +0100

    font-siemreap.rb: fix for new style

--- Casks/font-siemreap.rb
+++ Casks/font-siemreap.rb
@@ -1,11 +1,11 @@
-cask 'font-siemreap' do
+cask "font-siemreap" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/siemreap/Siemreap.ttf'
-  name 'Siemreap'
-  homepage 'https://fonts.google.com/specimen/Siemreap'
+  url "https://github.com/google/fonts/raw/master/ofl/siemreap/Siemreap.ttf"
+  name "Siemreap"
+  homepage "https://fonts.google.com/specimen/Siemreap"
 
-  font 'Siemreap.ttf'
+  font "Siemreap.ttf"
 end
