2a286e366910cfb8e7368885220a5612eb32dc24
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:28 +0100

    font-vt323.rb: fix for new style

--- Casks/font-vt323.rb
+++ Casks/font-vt323.rb
@@ -1,11 +1,11 @@
-cask 'font-vt323' do
+cask "font-vt323" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/vt323/VT323-Regular.ttf'
-  name 'VT323'
-  homepage 'https://fonts.google.com/specimen/VT323'
+  url "https://github.com/google/fonts/raw/master/ofl/vt323/VT323-Regular.ttf"
+  name "VT323"
+  homepage "https://fonts.google.com/specimen/VT323"
 
-  font 'VT323-Regular.ttf'
+  font "VT323-Regular.ttf"
 end
