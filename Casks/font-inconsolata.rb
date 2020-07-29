5441ad3b211135f0e9c63b52f5a0240192a04d07
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:16 +0100

    font-inconsolata.rb: fix for new style

--- Casks/font-inconsolata.rb
+++ Casks/font-inconsolata.rb
@@ -1,11 +1,11 @@
-cask 'font-inconsolata' do
+cask "font-inconsolata" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/inconsolata/Inconsolata%5Bwdth%2Cwght%5D.ttf'
-  name 'Inconsolata'
-  homepage 'https://fonts.google.com/specimen/Inconsolata'
+  url "https://github.com/google/fonts/raw/master/ofl/inconsolata/Inconsolata%5Bwdth%2Cwght%5D.ttf"
+  name "Inconsolata"
+  homepage "https://fonts.google.com/specimen/Inconsolata"
 
-  font 'Inconsolata[wdth,wght].ttf'
+  font "Inconsolata[wdth,wght].ttf"
 end
