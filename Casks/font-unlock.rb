ae26d22d8a5fd68dabe08dde6d6d47391e74d13d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:27 +0100

    font-unlock.rb: fix for new style

--- Casks/font-unlock.rb
+++ Casks/font-unlock.rb
@@ -1,11 +1,11 @@
-cask 'font-unlock' do
+cask "font-unlock" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/unlock/Unlock-Regular.ttf'
-  name 'Unlock'
-  homepage 'https://fonts.google.com/specimen/Unlock'
+  url "https://github.com/google/fonts/raw/master/ofl/unlock/Unlock-Regular.ttf"
+  name "Unlock"
+  homepage "https://fonts.google.com/specimen/Unlock"
 
-  font 'Unlock-Regular.ttf'
+  font "Unlock-Regular.ttf"
 end
