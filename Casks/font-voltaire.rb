36059429c0872af4ad28b99d2bc74e48a78af34e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:28 +0100

    font-voltaire.rb: fix for new style

--- Casks/font-voltaire.rb
+++ Casks/font-voltaire.rb
@@ -1,11 +1,11 @@
-cask 'font-voltaire' do
+cask "font-voltaire" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/voltaire/Voltaire-Regular.ttf'
-  name 'Voltaire'
-  homepage 'https://fonts.google.com/specimen/Voltaire'
+  url "https://github.com/google/fonts/raw/master/ofl/voltaire/Voltaire-Regular.ttf"
+  name "Voltaire"
+  homepage "https://fonts.google.com/specimen/Voltaire"
 
-  font 'Voltaire-Regular.ttf'
+  font "Voltaire-Regular.ttf"
 end
