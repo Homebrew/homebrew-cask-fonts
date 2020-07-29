ea39b346f1ea06f255be8a01a7965c5d50160eda
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:19 +0100

    font-sorts-mill-goudy.rb: fix for new style

--- Casks/font-sorts-mill-goudy.rb
+++ Casks/font-sorts-mill-goudy.rb
@@ -1,16 +1,16 @@
-cask 'font-sorts-mill-goudy' do
+cask "font-sorts-mill-goudy" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/sortsmillgoudy',
+  url "https://github.com/google/fonts/trunk/ofl/sortsmillgoudy",
       using:      :svn,
       trust_cert: true
-  name 'Sorts Mill Goudy'
-  homepage 'https://fonts.google.com/specimen/Sorts+Mill+Goudy'
+  name "Sorts Mill Goudy"
+  homepage "https://fonts.google.com/specimen/Sorts+Mill+Goudy"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'SortsMillGoudy-Italic.ttf'
-  font 'SortsMillGoudy-Regular.ttf'
+  font "SortsMillGoudy-Italic.ttf"
+  font "SortsMillGoudy-Regular.ttf"
 end
