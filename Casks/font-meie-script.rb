5c538629e9242535100378481a903bf3cec042e9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:41 +0100

    font-meie-script.rb: fix for new style

--- Casks/font-meie-script.rb
+++ Casks/font-meie-script.rb
@@ -1,11 +1,11 @@
-cask 'font-meie-script' do
+cask "font-meie-script" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/meiescript/MeieScript-Regular.ttf'
-  name 'Meie Script'
-  homepage 'https://fonts.google.com/specimen/Meie+Script'
+  url "https://github.com/google/fonts/raw/master/ofl/meiescript/MeieScript-Regular.ttf"
+  name "Meie Script"
+  homepage "https://fonts.google.com/specimen/Meie+Script"
 
-  font 'MeieScript-Regular.ttf'
+  font "MeieScript-Regular.ttf"
 end
