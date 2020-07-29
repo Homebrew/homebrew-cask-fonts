a47eb3fd998fced76efff4075c0989b7f80b16f2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:38 +0100

    font-macondo.rb: fix for new style

--- Casks/font-macondo.rb
+++ Casks/font-macondo.rb
@@ -1,11 +1,11 @@
-cask 'font-macondo' do
+cask "font-macondo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/macondo/Macondo-Regular.ttf'
-  name 'Macondo'
-  homepage 'https://fonts.google.com/specimen/Macondo'
+  url "https://github.com/google/fonts/raw/master/ofl/macondo/Macondo-Regular.ttf"
+  name "Macondo"
+  homepage "https://fonts.google.com/specimen/Macondo"
 
-  font 'Macondo-Regular.ttf'
+  font "Macondo-Regular.ttf"
 end
