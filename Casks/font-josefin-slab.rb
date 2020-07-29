b0b02b9f530d6afd5bbdb581a98d1c4f5ba77d1b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:23 +0100

    font-josefin-slab.rb: fix for new style

--- Casks/font-josefin-slab.rb
+++ Casks/font-josefin-slab.rb
@@ -1,24 +1,24 @@
-cask 'font-josefin-slab' do
+cask "font-josefin-slab" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/josefinslab',
+  url "https://github.com/google/fonts/trunk/ofl/josefinslab",
       using:      :svn,
       trust_cert: true
-  name 'Josefin Slab'
-  homepage 'https://fonts.google.com/specimen/Josefin+Slab'
+  name "Josefin Slab"
+  homepage "https://fonts.google.com/specimen/Josefin+Slab"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'JosefinSlab-Bold.ttf'
-  font 'JosefinSlab-BoldItalic.ttf'
-  font 'JosefinSlab-Italic.ttf'
-  font 'JosefinSlab-Light.ttf'
-  font 'JosefinSlab-LightItalic.ttf'
-  font 'JosefinSlab-Regular.ttf'
-  font 'JosefinSlab-SemiBold.ttf'
-  font 'JosefinSlab-SemiBoldItalic.ttf'
-  font 'JosefinSlab-Thin.ttf'
-  font 'JosefinSlab-ThinItalic.ttf'
+  font "JosefinSlab-Bold.ttf"
+  font "JosefinSlab-BoldItalic.ttf"
+  font "JosefinSlab-Italic.ttf"
+  font "JosefinSlab-Light.ttf"
+  font "JosefinSlab-LightItalic.ttf"
+  font "JosefinSlab-Regular.ttf"
+  font "JosefinSlab-SemiBold.ttf"
+  font "JosefinSlab-SemiBoldItalic.ttf"
+  font "JosefinSlab-Thin.ttf"
+  font "JosefinSlab-ThinItalic.ttf"
 end
