994d2ecbf882813fcba5b5630c313ee51aa5f38a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:23 +0100

    font-buenard.rb: fix for new style

--- Casks/font-buenard.rb
+++ Casks/font-buenard.rb
@@ -1,16 +1,16 @@
-cask 'font-buenard' do
+cask "font-buenard" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/buenard',
+  url "https://github.com/google/fonts/trunk/ofl/buenard",
       using:      :svn,
       trust_cert: true
-  name 'Buenard'
-  homepage 'https://fonts.google.com/specimen/Buenard'
+  name "Buenard"
+  homepage "https://fonts.google.com/specimen/Buenard"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Buenard-Bold.ttf'
-  font 'Buenard-Regular.ttf'
+  font "Buenard-Bold.ttf"
+  font "Buenard-Regular.ttf"
 end
