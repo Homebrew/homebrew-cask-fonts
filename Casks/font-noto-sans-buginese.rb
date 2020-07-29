b1fca49004e5fb068fb107d9539fb8fdff7e5035
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:51 +0100

    font-noto-sans-buginese.rb: fix for new style

--- Casks/font-noto-sans-buginese.rb
+++ Casks/font-noto-sans-buginese.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-buginese' do
+cask "font-noto-sans-buginese" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBuginese-unhinted.zip'
-  name 'Noto Sans Buginese'
-  homepage 'https://www.google.com/get/noto/#sans-bugi'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBuginese-unhinted.zip"
+  name "Noto Sans Buginese"
+  homepage "https://www.google.com/get/noto/#sans-bugi"
 
-  font 'NotoSansBuginese-Regular.ttf'
+  font "NotoSansBuginese-Regular.ttf"
 end
