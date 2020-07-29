13430601141b982c3dd8d9f4b12bbd679833fc2f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:28 +0100

    font-catamaran.rb: fix for new style

--- Casks/font-catamaran.rb
+++ Casks/font-catamaran.rb
@@ -1,23 +1,23 @@
-cask 'font-catamaran' do
+cask "font-catamaran" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/catamaran',
+  url "https://github.com/google/fonts/trunk/ofl/catamaran",
       using:      :svn,
       trust_cert: true
-  name 'Catamaran'
-  homepage 'https://fonts.google.com/specimen/Catamaran'
+  name "Catamaran"
+  homepage "https://fonts.google.com/specimen/Catamaran"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Catamaran-Black.ttf'
-  font 'Catamaran-Bold.ttf'
-  font 'Catamaran-ExtraBold.ttf'
-  font 'Catamaran-ExtraLight.ttf'
-  font 'Catamaran-Light.ttf'
-  font 'Catamaran-Medium.ttf'
-  font 'Catamaran-Regular.ttf'
-  font 'Catamaran-SemiBold.ttf'
-  font 'Catamaran-Thin.ttf'
+  font "Catamaran-Black.ttf"
+  font "Catamaran-Bold.ttf"
+  font "Catamaran-ExtraBold.ttf"
+  font "Catamaran-ExtraLight.ttf"
+  font "Catamaran-Light.ttf"
+  font "Catamaran-Medium.ttf"
+  font "Catamaran-Regular.ttf"
+  font "Catamaran-SemiBold.ttf"
+  font "Catamaran-Thin.ttf"
 end
