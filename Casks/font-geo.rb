1026bff6360ade12f43438dca5844baf9ee40c6d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:00 +0100

    font-geo.rb: fix for new style

--- Casks/font-geo.rb
+++ Casks/font-geo.rb
@@ -1,16 +1,16 @@
-cask 'font-geo' do
+cask "font-geo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/geo',
+  url "https://github.com/google/fonts/trunk/ofl/geo",
       using:      :svn,
       trust_cert: true
-  name 'Geo'
-  homepage 'https://fonts.google.com/specimen/Geo'
+  name "Geo"
+  homepage "https://fonts.google.com/specimen/Geo"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Geo-Oblique.ttf'
-  font 'Geo-Regular.ttf'
+  font "Geo-Oblique.ttf"
+  font "Geo-Regular.ttf"
 end
