bbaab591961d6a23a5d880f1e26b3f31c23bd143
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:36 +0100

    font-courgette.rb: fix for new style

--- Casks/font-courgette.rb
+++ Casks/font-courgette.rb
@@ -1,11 +1,11 @@
-cask 'font-courgette' do
+cask "font-courgette" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/courgette/Courgette-Regular.ttf'
-  name 'Courgette'
-  homepage 'https://fonts.google.com/specimen/Courgette'
+  url "https://github.com/google/fonts/raw/master/ofl/courgette/Courgette-Regular.ttf"
+  name "Courgette"
+  homepage "https://fonts.google.com/specimen/Courgette"
 
-  font 'Courgette-Regular.ttf'
+  font "Courgette-Regular.ttf"
 end
