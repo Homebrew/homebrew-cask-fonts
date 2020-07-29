0f16223894f95d15d800d2b2364dd47600076730
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:20 +0100

    font-space-grotesk.rb: fix for new style

--- Casks/font-space-grotesk.rb
+++ Casks/font-space-grotesk.rb
@@ -1,11 +1,11 @@
-cask 'font-space-grotesk' do
-  version '1.1.0'
-  sha256 'a4aaf23fe43de4096ce85c3b8dec7076c15ec5d0b8dbfbc65e91da5b173ea8e6'
+cask "font-space-grotesk" do
+  version "1.1.0"
+  sha256 "a4aaf23fe43de4096ce85c3b8dec7076c15ec5d0b8dbfbc65e91da5b173ea8e6"
 
   url "https://github.com/floriankarsten/space-grotesk/releases/download/#{version}/SpaceGrotesk-v#{version}.zip"
-  appcast 'https://github.com/floriankarsten/space-grotesk/releases.atom'
-  name 'Space Grotesk'
-  homepage 'https://github.com/floriankarsten/space-grotesk'
+  appcast "https://github.com/floriankarsten/space-grotesk/releases.atom"
+  name "Space Grotesk"
+  homepage "https://github.com/floriankarsten/space-grotesk"
 
   font "SpaceGrotesk-v#{version}/otf/SpaceGrotesk-Bold.otf"
   font "SpaceGrotesk-v#{version}/otf/SpaceGrotesk-Light.otf"
