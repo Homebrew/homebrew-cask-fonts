949d3afc8a596f132f2712ca7f11e504e1937c70
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:03 +0100

    font-passion-one.rb: fix for new style

--- Casks/font-passion-one.rb
+++ Casks/font-passion-one.rb
@@ -1,17 +1,17 @@
-cask 'font-passion-one' do
+cask "font-passion-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/passionone',
+  url "https://github.com/google/fonts/trunk/ofl/passionone",
       using:      :svn,
       trust_cert: true
-  name 'Passion One'
-  homepage 'https://fonts.google.com/specimen/Passion+One'
+  name "Passion One"
+  homepage "https://fonts.google.com/specimen/Passion+One"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'PassionOne-Black.ttf'
-  font 'PassionOne-Bold.ttf'
-  font 'PassionOne-Regular.ttf'
+  font "PassionOne-Black.ttf"
+  font "PassionOne-Bold.ttf"
+  font "PassionOne-Regular.ttf"
 end
