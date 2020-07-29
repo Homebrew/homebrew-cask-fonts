5d65b6acca34a99c8cf59a2013ce94662e23649d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:16 +0100

    font-barriecito.rb: fix for new style

--- Casks/font-barriecito.rb
+++ Casks/font-barriecito.rb
@@ -1,11 +1,11 @@
-cask 'font-barriecito' do
+cask "font-barriecito" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/barriecito/Barriecito-Regular.ttf'
-  name 'Barriecito'
-  homepage 'https://fonts.google.com/specimen/Barriecito'
+  url "https://github.com/google/fonts/raw/master/ofl/barriecito/Barriecito-Regular.ttf"
+  name "Barriecito"
+  homepage "https://fonts.google.com/specimen/Barriecito"
 
-  font 'Barriecito-Regular.ttf'
+  font "Barriecito-Regular.ttf"
 end
