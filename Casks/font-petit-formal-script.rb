4f643185ffc661e7d04241adf42656e13cf20cc7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:04 +0100

    font-petit-formal-script.rb: fix for new style

--- Casks/font-petit-formal-script.rb
+++ Casks/font-petit-formal-script.rb
@@ -1,11 +1,11 @@
-cask 'font-petit-formal-script' do
+cask "font-petit-formal-script" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/petitformalscript/PetitFormalScript-Regular.ttf'
-  name 'Petit Formal Script'
-  homepage 'https://fonts.google.com/specimen/Petit+Formal+Script'
+  url "https://github.com/google/fonts/raw/master/ofl/petitformalscript/PetitFormalScript-Regular.ttf"
+  name "Petit Formal Script"
+  homepage "https://fonts.google.com/specimen/Petit+Formal+Script"
 
-  font 'PetitFormalScript-Regular.ttf'
+  font "PetitFormalScript-Regular.ttf"
 end
