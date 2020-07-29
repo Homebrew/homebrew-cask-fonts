a1eda209e4bb2d70c64d3e6f9cb93828fd094f80
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:57 +0100

    font-noto-sans-ugaritic.rb: fix for new style

--- Casks/font-noto-sans-ugaritic.rb
+++ Casks/font-noto-sans-ugaritic.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-ugaritic' do
+cask "font-noto-sans-ugaritic" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansUgaritic-unhinted.zip'
-  name 'Noto Sans Ugaritic'
-  homepage 'https://www.google.com/get/noto/#sans-ugar'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansUgaritic-unhinted.zip"
+  name "Noto Sans Ugaritic"
+  homepage "https://www.google.com/get/noto/#sans-ugar"
 
-  font 'NotoSansUgaritic-Regular.ttf'
+  font "NotoSansUgaritic-Regular.ttf"
 end
