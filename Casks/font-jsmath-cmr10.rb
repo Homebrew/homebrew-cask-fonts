46a8b191b242c76ea2c0b10230c2eb98ae94340c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:24 +0100

    font-jsmath-cmr10.rb: fix for new style

--- Casks/font-jsmath-cmr10.rb
+++ Casks/font-jsmath-cmr10.rb
@@ -1,11 +1,11 @@
-cask 'font-jsmath-cmr10' do
+cask "font-jsmath-cmr10" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/jsmathcmr10/jsMath-cmr10.ttf'
-  name 'jsMath cmr10'
-  homepage 'https://fonts.google.com/specimen/jsMath+cmr10'
+  url "https://github.com/google/fonts/raw/master/apache/jsmathcmr10/jsMath-cmr10.ttf"
+  name "jsMath cmr10"
+  homepage "https://fonts.google.com/specimen/jsMath+cmr10"
 
-  font 'jsMath-cmr10.ttf'
+  font "jsMath-cmr10.ttf"
 end
