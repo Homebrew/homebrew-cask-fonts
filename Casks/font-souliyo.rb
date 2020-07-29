c6c7314ba7b3c250802deadde03d55d927571d9b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:19 +0100

    font-souliyo.rb: fix for new style

--- Casks/font-souliyo.rb
+++ Casks/font-souliyo.rb
@@ -1,11 +1,11 @@
-cask 'font-souliyo' do
+cask "font-souliyo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/souliyo/Souliyo-Regular.ttf'
-  name 'Souliyo'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/souliyo/Souliyo-Regular.ttf"
+  name "Souliyo"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'Souliyo-Regular.ttf'
+  font "Souliyo-Regular.ttf"
 end
