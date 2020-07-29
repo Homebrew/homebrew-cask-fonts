a78f45f938a9a5f687cd2f1c978dc66aa6c64cdc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:15 +0100

    font-imprima.rb: fix for new style

--- Casks/font-imprima.rb
+++ Casks/font-imprima.rb
@@ -1,11 +1,11 @@
-cask 'font-imprima' do
+cask "font-imprima" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/imprima/Imprima-Regular.ttf'
-  name 'Imprima'
-  homepage 'https://fonts.google.com/specimen/Imprima'
+  url "https://github.com/google/fonts/raw/master/ofl/imprima/Imprima-Regular.ttf"
+  name "Imprima"
+  homepage "https://fonts.google.com/specimen/Imprima"
 
-  font 'Imprima-Regular.ttf'
+  font "Imprima-Regular.ttf"
 end
