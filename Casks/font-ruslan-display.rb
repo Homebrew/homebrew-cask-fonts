12c57a80618f0a57a7e9346f08ba3fdf91d3fa2c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:13 +0100

    font-ruslan-display.rb: fix for new style

--- Casks/font-ruslan-display.rb
+++ Casks/font-ruslan-display.rb
@@ -1,11 +1,11 @@
-cask 'font-ruslan-display' do
+cask "font-ruslan-display" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/ruslandisplay/RuslanDisplay.ttf'
-  name 'Ruslan Display'
-  homepage 'https://fonts.google.com/specimen/Ruslan+Display'
+  url "https://github.com/google/fonts/raw/master/ofl/ruslandisplay/RuslanDisplay.ttf"
+  name "Ruslan Display"
+  homepage "https://fonts.google.com/specimen/Ruslan+Display"
 
-  font 'RuslanDisplay.ttf'
+  font "RuslanDisplay.ttf"
 end
