44c03c233878590e50d5ec9c3f560a2a8a5f39ae
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:29 +0100

    font-cedarville-cursive.rb: fix for new style

--- Casks/font-cedarville-cursive.rb
+++ Casks/font-cedarville-cursive.rb
@@ -1,11 +1,11 @@
-cask 'font-cedarville-cursive' do
+cask "font-cedarville-cursive" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/cedarvillecursive/Cedarville-Cursive.ttf'
-  name 'Cedarville Cursive'
-  homepage 'https://fonts.google.com/specimen/Cedarville+Cursive'
+  url "https://github.com/google/fonts/raw/master/ofl/cedarvillecursive/Cedarville-Cursive.ttf"
+  name "Cedarville Cursive"
+  homepage "https://fonts.google.com/specimen/Cedarville+Cursive"
 
-  font 'Cedarville-Cursive.ttf'
+  font "Cedarville-Cursive.ttf"
 end
