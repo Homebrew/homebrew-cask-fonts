56f91f51d882fcad87e7fdaa212c44e6a33e0bb3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:20 +0100

    font-squada-one.rb: fix for new style

--- Casks/font-squada-one.rb
+++ Casks/font-squada-one.rb
@@ -1,11 +1,11 @@
-cask 'font-squada-one' do
+cask "font-squada-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/squadaone/SquadaOne-Regular.ttf'
-  name 'Squada One'
-  homepage 'https://fonts.google.com/specimen/Squada+One'
+  url "https://github.com/google/fonts/raw/master/ofl/squadaone/SquadaOne-Regular.ttf"
+  name "Squada One"
+  homepage "https://fonts.google.com/specimen/Squada+One"
 
-  font 'SquadaOne-Regular.ttf'
+  font "SquadaOne-Regular.ttf"
 end
