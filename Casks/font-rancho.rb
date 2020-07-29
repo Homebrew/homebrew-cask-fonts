d91b03858a21df48a9707ee1ae0adac4647642df
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:09 +0100

    font-rancho.rb: fix for new style

--- Casks/font-rancho.rb
+++ Casks/font-rancho.rb
@@ -1,11 +1,11 @@
-cask 'font-rancho' do
+cask "font-rancho" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/rancho/Rancho-Regular.ttf'
-  name 'Rancho'
-  homepage 'https://fonts.google.com/specimen/Rancho'
+  url "https://github.com/google/fonts/raw/master/apache/rancho/Rancho-Regular.ttf"
+  name "Rancho"
+  homepage "https://fonts.google.com/specimen/Rancho"
 
-  font 'Rancho-Regular.ttf'
+  font "Rancho-Regular.ttf"
 end
