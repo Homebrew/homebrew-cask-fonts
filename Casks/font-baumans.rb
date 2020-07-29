ab6ed0d583f2464285b5ec30bdd4612b77f7f373
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:17 +0100

    font-baumans.rb: fix for new style

--- Casks/font-baumans.rb
+++ Casks/font-baumans.rb
@@ -1,11 +1,11 @@
-cask 'font-baumans' do
+cask "font-baumans" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/baumans/Baumans-Regular.ttf'
-  name 'Baumans'
-  homepage 'https://fonts.google.com/specimen/Baumans'
+  url "https://github.com/google/fonts/raw/master/ofl/baumans/Baumans-Regular.ttf"
+  name "Baumans"
+  homepage "https://fonts.google.com/specimen/Baumans"
 
-  font 'Baumans-Regular.ttf'
+  font "Baumans-Regular.ttf"
 end
