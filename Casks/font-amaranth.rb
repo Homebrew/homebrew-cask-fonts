dbf52521bee7374aad1d2e89909b4764ff7ba0d1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:24 +0100

    font-amaranth.rb: fix for new style

--- Casks/font-amaranth.rb
+++ Casks/font-amaranth.rb
@@ -1,18 +1,18 @@
-cask 'font-amaranth' do
+cask "font-amaranth" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/amaranth',
+  url "https://github.com/google/fonts/trunk/ofl/amaranth",
       using:      :svn,
       trust_cert: true
-  name 'Amaranth'
-  homepage 'https://fonts.google.com/specimen/Amaranth'
+  name "Amaranth"
+  homepage "https://fonts.google.com/specimen/Amaranth"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Amaranth-Bold.ttf'
-  font 'Amaranth-BoldItalic.ttf'
-  font 'Amaranth-Italic.ttf'
-  font 'Amaranth-Regular.ttf'
+  font "Amaranth-Bold.ttf"
+  font "Amaranth-BoldItalic.ttf"
+  font "Amaranth-Italic.ttf"
+  font "Amaranth-Regular.ttf"
 end
