c3988f3517710dda2820934fa007ee2c172e9c3a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:23 +0100

    font-tauri.rb: fix for new style

--- Casks/font-tauri.rb
+++ Casks/font-tauri.rb
@@ -1,11 +1,11 @@
-cask 'font-tauri' do
+cask "font-tauri" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/tauri/Tauri-Regular.ttf'
-  name 'Tauri'
-  homepage 'https://fonts.google.com/specimen/Tauri'
+  url "https://github.com/google/fonts/raw/master/ofl/tauri/Tauri-Regular.ttf"
+  name "Tauri"
+  homepage "https://fonts.google.com/specimen/Tauri"
 
-  font 'Tauri-Regular.ttf'
+  font "Tauri-Regular.ttf"
 end
