dd9b16c00dc3b471b7bfe0867dab35cf36318190
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:33 +0100

    font-coda.rb: fix for new style

--- Casks/font-coda.rb
+++ Casks/font-coda.rb
@@ -1,16 +1,16 @@
-cask 'font-coda' do
+cask "font-coda" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/coda',
+  url "https://github.com/google/fonts/trunk/ofl/coda",
       using:      :svn,
       trust_cert: true
-  name 'Coda'
-  homepage 'https://fonts.google.com/specimen/Coda'
+  name "Coda"
+  homepage "https://fonts.google.com/specimen/Coda"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Coda-ExtraBold.ttf'
-  font 'Coda-Regular.ttf'
+  font "Coda-ExtraBold.ttf"
+  font "Coda-Regular.ttf"
 end
