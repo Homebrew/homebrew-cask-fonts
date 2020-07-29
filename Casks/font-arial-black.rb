e982e8330065befeb50ac8a7c03d08121cb99442
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:15 +0100

    font-arial-black.rb: fix for new style

--- Casks/font-arial-black.rb
+++ Casks/font-arial-black.rb
@@ -1,11 +1,11 @@
-cask 'font-arial-black' do
-  version '2.35'
-  sha256 'a425f0ffb6a1a5ede5b979ed6177f4f4f4fdef6ae7c302a7b7720ef332fec0a8'
+cask "font-arial-black" do
+  version "2.35"
+  sha256 "a425f0ffb6a1a5ede5b979ed6177f4f4f4fdef6ae7c302a7b7720ef332fec0a8"
 
-  url 'https://downloads.sourceforge.net/corefonts/arialb32.exe'
-  appcast 'https://sourceforge.net/projects/corefonts/rss'
-  name 'Arial Black'
-  homepage 'https://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  url "https://downloads.sourceforge.net/corefonts/arialb32.exe"
+  appcast "https://sourceforge.net/projects/corefonts/rss"
+  name "Arial Black"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
 
-  font 'AriBlk.TTF'
+  font "AriBlk.TTF"
 end
