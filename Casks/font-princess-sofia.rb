de5e1c902015771cc6bbb0ca4cb81bb6478e7384
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:07 +0100

    font-princess-sofia.rb: fix for new style

--- Casks/font-princess-sofia.rb
+++ Casks/font-princess-sofia.rb
@@ -1,11 +1,11 @@
-cask 'font-princess-sofia' do
+cask "font-princess-sofia" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/princesssofia/PrincessSofia-Regular.ttf'
-  name 'Princess Sofia'
-  homepage 'https://fonts.google.com/specimen/Princess+Sofia'
+  url "https://github.com/google/fonts/raw/master/ofl/princesssofia/PrincessSofia-Regular.ttf"
+  name "Princess Sofia"
+  homepage "https://fonts.google.com/specimen/Princess+Sofia"
 
-  font 'PrincessSofia-Regular.ttf'
+  font "PrincessSofia-Regular.ttf"
 end
