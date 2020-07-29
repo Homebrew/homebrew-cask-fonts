1365f3112a04be9be42545859d8d9fe8a73f8952
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:22 +0100

    font-sunflower.rb: fix for new style

--- Casks/font-sunflower.rb
+++ Casks/font-sunflower.rb
@@ -1,17 +1,17 @@
-cask 'font-sunflower' do
+cask "font-sunflower" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/sunflower',
+  url "https://github.com/google/fonts/trunk/ofl/sunflower",
       using:      :svn,
       trust_cert: true
-  name 'Sunflower'
-  homepage 'https://fonts.google.com/specimen/Sunflower'
+  name "Sunflower"
+  homepage "https://fonts.google.com/specimen/Sunflower"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Sunflower-Bold.ttf'
-  font 'Sunflower-Light.ttf'
-  font 'Sunflower-Medium.ttf'
+  font "Sunflower-Bold.ttf"
+  font "Sunflower-Light.ttf"
+  font "Sunflower-Medium.ttf"
 end
