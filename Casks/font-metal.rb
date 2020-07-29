5550803b7f3eac8f5788b7d226a6cf48726dd4a4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:42 +0100

    font-metal.rb: fix for new style

--- Casks/font-metal.rb
+++ Casks/font-metal.rb
@@ -1,11 +1,11 @@
-cask 'font-metal' do
+cask "font-metal" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/metal/Metal.ttf'
-  name 'Metal'
-  homepage 'https://fonts.google.com/specimen/Metal'
+  url "https://github.com/google/fonts/raw/master/ofl/metal/Metal.ttf"
+  name "Metal"
+  homepage "https://fonts.google.com/specimen/Metal"
 
-  font 'Metal.ttf'
+  font "Metal.ttf"
 end
