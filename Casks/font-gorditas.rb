f168ce75e70c7aa69cec27a921de6fab2fe08c2f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:05 +0100

    font-gorditas.rb: fix for new style

--- Casks/font-gorditas.rb
+++ Casks/font-gorditas.rb
@@ -1,16 +1,16 @@
-cask 'font-gorditas' do
+cask "font-gorditas" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/gorditas',
+  url "https://github.com/google/fonts/trunk/ofl/gorditas",
       using:      :svn,
       trust_cert: true
-  name 'Gorditas'
-  homepage 'https://fonts.google.com/specimen/Gorditas'
+  name "Gorditas"
+  homepage "https://fonts.google.com/specimen/Gorditas"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Gorditas-Bold.ttf'
-  font 'Gorditas-Regular.ttf'
+  font "Gorditas-Bold.ttf"
+  font "Gorditas-Regular.ttf"
 end
