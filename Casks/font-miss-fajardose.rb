77778e15ebe1215bad43c723347c7642b22b6126
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:43 +0100

    font-miss-fajardose.rb: fix for new style

--- Casks/font-miss-fajardose.rb
+++ Casks/font-miss-fajardose.rb
@@ -1,11 +1,11 @@
-cask 'font-miss-fajardose' do
+cask "font-miss-fajardose" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/missfajardose/MissFajardose-Regular.ttf'
-  name 'Miss Fajardose'
-  homepage 'https://fonts.google.com/specimen/Miss+Fajardose'
+  url "https://github.com/google/fonts/raw/master/ofl/missfajardose/MissFajardose-Regular.ttf"
+  name "Miss Fajardose"
+  homepage "https://fonts.google.com/specimen/Miss+Fajardose"
 
-  font 'MissFajardose-Regular.ttf'
+  font "MissFajardose-Regular.ttf"
 end
