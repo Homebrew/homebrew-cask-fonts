415302a05f15e2a4fcc5cf7e2a095b352d95f4e1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:19 +0100

    font-source-han-mono.rb: fix for new style

--- Casks/font-source-han-mono.rb
+++ Casks/font-source-han-mono.rb
@@ -1,11 +1,11 @@
-cask 'font-source-han-mono' do
-  version '1.002'
-  sha256 '0c192448de90848f11eb8336876883a9a36dc65b8965e489600cfcc7a67358c1'
+cask "font-source-han-mono" do
+  version "1.002"
+  sha256 "0c192448de90848f11eb8336876883a9a36dc65b8965e489600cfcc7a67358c1"
 
   url "https://github.com/adobe-fonts/source-han-mono/releases/download/#{version}/SourceHanMono.ttc"
-  appcast 'https://github.com/adobe-fonts/source-han-mono/releases.atom'
-  name 'Source Han Mono'
-  homepage 'https://github.com/adobe-fonts/source-han-mono'
+  appcast "https://github.com/adobe-fonts/source-han-mono/releases.atom"
+  name "Source Han Mono"
+  homepage "https://github.com/adobe-fonts/source-han-mono"
 
-  font 'SourceHanMono.ttc'
+  font "SourceHanMono.ttc"
 end
