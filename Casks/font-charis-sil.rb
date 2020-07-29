7d9e65d4dbf7cd20289452f829bc473375f28d1c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:30 +0100

    font-charis-sil.rb: fix for new style

--- Casks/font-charis-sil.rb
+++ Casks/font-charis-sil.rb
@@ -1,10 +1,10 @@
-cask 'font-charis-sil' do
-  version '5.000'
-  sha256 '5e3e5473b30363008c289cc87e2aa584a0916087a63a3f689defa8e0cee09bfd'
+cask "font-charis-sil" do
+  version "5.000"
+  sha256 "5e3e5473b30363008c289cc87e2aa584a0916087a63a3f689defa8e0cee09bfd"
 
   url "https://software.sil.org/downloads/r/charis/CharisSIL-#{version}.zip"
-  name 'Charis SIL'
-  homepage 'https://software.sil.org/charis/download/'
+  name "Charis SIL"
+  homepage "https://software.sil.org/charis/download/"
 
   font "CharisSIL-#{version}/CharisSIL-R.ttf"
   font "CharisSIL-#{version}/CharisSIL-B.ttf"
