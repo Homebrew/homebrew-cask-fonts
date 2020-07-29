81e19ea95faf3f491dbc268899f0859705e10511
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:13 +0100

    font-iceberg.rb: fix for new style

--- Casks/font-iceberg.rb
+++ Casks/font-iceberg.rb
@@ -1,11 +1,11 @@
-cask 'font-iceberg' do
+cask "font-iceberg" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/iceberg/Iceberg-Regular.ttf'
-  name 'Iceberg'
-  homepage 'https://fonts.google.com/specimen/Iceberg'
+  url "https://github.com/google/fonts/raw/master/ofl/iceberg/Iceberg-Regular.ttf"
+  name "Iceberg"
+  homepage "https://fonts.google.com/specimen/Iceberg"
 
-  font 'Iceberg-Regular.ttf'
+  font "Iceberg-Regular.ttf"
 end
