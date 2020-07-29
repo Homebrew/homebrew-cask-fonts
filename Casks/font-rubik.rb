b01d6b816c00285576ebb300fe738b391524e93c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:12 +0100

    font-rubik.rb: fix for new style

--- Casks/font-rubik.rb
+++ Casks/font-rubik.rb
@@ -1,24 +1,24 @@
-cask 'font-rubik' do
+cask "font-rubik" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/rubik',
+  url "https://github.com/google/fonts/trunk/ofl/rubik",
       using:      :svn,
       trust_cert: true
-  name 'Rubik'
-  homepage 'https://fonts.google.com/specimen/Rubik'
+  name "Rubik"
+  homepage "https://fonts.google.com/specimen/Rubik"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Rubik-Black.ttf'
-  font 'Rubik-BlackItalic.ttf'
-  font 'Rubik-Bold.ttf'
-  font 'Rubik-BoldItalic.ttf'
-  font 'Rubik-Italic.ttf'
-  font 'Rubik-Light.ttf'
-  font 'Rubik-LightItalic.ttf'
-  font 'Rubik-Medium.ttf'
-  font 'Rubik-MediumItalic.ttf'
-  font 'Rubik-Regular.ttf'
+  font "Rubik-Black.ttf"
+  font "Rubik-BlackItalic.ttf"
+  font "Rubik-Bold.ttf"
+  font "Rubik-BoldItalic.ttf"
+  font "Rubik-Italic.ttf"
+  font "Rubik-Light.ttf"
+  font "Rubik-LightItalic.ttf"
+  font "Rubik-Medium.ttf"
+  font "Rubik-MediumItalic.ttf"
+  font "Rubik-Regular.ttf"
 end
