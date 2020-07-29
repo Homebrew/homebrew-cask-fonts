d769e0faeadd53c1c87f02e5a4b0d7970a0dc530
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:39 +0100

    font-amiri.rb: fix for new style

--- Casks/font-amiri.rb
+++ Casks/font-amiri.rb
@@ -1,12 +1,12 @@
-cask 'font-amiri' do
-  version '0.111'
-  sha256 '1fbfccced6348b5db2c1c21d5b319cd488e14d055702fa817a0f6cb83d882166'
+cask "font-amiri" do
+  version "0.111"
+  sha256 "1fbfccced6348b5db2c1c21d5b319cd488e14d055702fa817a0f6cb83d882166"
 
   # github.com/alif-type/amiri-font/ was verified as official when first introduced to the cask
   url "https://github.com/alif-type/amiri-font/releases/download/#{version}/Amiri-#{version}.zip"
-  appcast 'https://github.com/alif-type/amiri/releases.atom'
-  name 'Amiri'
-  homepage 'https://www.amirifont.org/'
+  appcast "https://github.com/alif-type/amiri/releases.atom"
+  name "Amiri"
+  homepage "https://www.amirifont.org/"
 
   font "Amiri-#{version}/Amiri-Bold.ttf"
   font "Amiri-#{version}/Amiri-BoldSlanted.ttf"
