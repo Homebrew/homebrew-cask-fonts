b19a1d96553dad9320435e10307bf43014af8283
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:21 +0100

    font-stylish.rb: fix for new style

--- Casks/font-stylish.rb
+++ Casks/font-stylish.rb
@@ -1,11 +1,11 @@
-cask 'font-stylish' do
+cask "font-stylish" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/stylish/Stylish-Regular.ttf'
-  name 'Stylish'
-  homepage 'https://fonts.google.com/specimen/Stylish'
+  url "https://github.com/google/fonts/raw/master/ofl/stylish/Stylish-Regular.ttf"
+  name "Stylish"
+  homepage "https://fonts.google.com/specimen/Stylish"
 
-  font 'Stylish-Regular.ttf'
+  font "Stylish-Regular.ttf"
 end
