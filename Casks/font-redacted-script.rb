e369f2649ff7179b58e4aea5dfee7f8c0ec4fcb6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:10 +0100

    font-redacted-script.rb: fix for new style

--- Casks/font-redacted-script.rb
+++ Casks/font-redacted-script.rb
@@ -1,16 +1,16 @@
-cask 'font-redacted-script' do
+cask "font-redacted-script" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/redactedscript',
+  url "https://github.com/google/fonts/trunk/ofl/redactedscript",
       using:      :svn,
       trust_cert: true
-  name 'Redacted Script'
-  homepage 'https://fonts.google.com/specimen/Redacted+Script'
+  name "Redacted Script"
+  homepage "https://fonts.google.com/specimen/Redacted+Script"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'RedactedScript-Bold.ttf'
-  font 'RedactedScript-Regular.ttf'
+  font "RedactedScript-Bold.ttf"
+  font "RedactedScript-Regular.ttf"
 end
