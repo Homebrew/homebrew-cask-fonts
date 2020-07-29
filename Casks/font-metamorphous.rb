4e37fc4826b6556d2bc46c77432c33a6b8141b9a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:42 +0100

    font-metamorphous.rb: fix for new style

--- Casks/font-metamorphous.rb
+++ Casks/font-metamorphous.rb
@@ -1,11 +1,11 @@
-cask 'font-metamorphous' do
+cask "font-metamorphous" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/metamorphous/Metamorphous-Regular.ttf'
-  name 'Metamorphous'
-  homepage 'https://fonts.google.com/specimen/Metamorphous'
+  url "https://github.com/google/fonts/raw/master/ofl/metamorphous/Metamorphous-Regular.ttf"
+  name "Metamorphous"
+  homepage "https://fonts.google.com/specimen/Metamorphous"
 
-  font 'Metamorphous-Regular.ttf'
+  font "Metamorphous-Regular.ttf"
 end
