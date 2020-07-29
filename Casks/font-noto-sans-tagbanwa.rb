f7908e2722ea3ee8a840ca663e65b1bc45c0355b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:57 +0100

    font-noto-sans-tagbanwa.rb: fix for new style

--- Casks/font-noto-sans-tagbanwa.rb
+++ Casks/font-noto-sans-tagbanwa.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-tagbanwa' do
+cask "font-noto-sans-tagbanwa" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagbanwa-unhinted.zip'
-  name 'Noto Sans Tagbanwa'
-  homepage 'https://www.google.com/get/noto/#sans-tagb'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTagbanwa-unhinted.zip"
+  name "Noto Sans Tagbanwa"
+  homepage "https://www.google.com/get/noto/#sans-tagb"
 
-  font 'NotoSansTagbanwa-Regular.ttf'
+  font "NotoSansTagbanwa-Regular.ttf"
 end
