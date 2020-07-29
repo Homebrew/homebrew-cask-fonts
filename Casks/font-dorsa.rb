2d10c0eb7be59b6ca401fff64c39cd0255d57241
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:45 +0100

    font-dorsa.rb: fix for new style

--- Casks/font-dorsa.rb
+++ Casks/font-dorsa.rb
@@ -1,11 +1,11 @@
-cask 'font-dorsa' do
+cask "font-dorsa" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/dorsa/Dorsa-Regular.ttf'
-  name 'Dorsa'
-  homepage 'https://fonts.google.com/specimen/Dorsa'
+  url "https://github.com/google/fonts/raw/master/ofl/dorsa/Dorsa-Regular.ttf"
+  name "Dorsa"
+  homepage "https://fonts.google.com/specimen/Dorsa"
 
-  font 'Dorsa-Regular.ttf'
+  font "Dorsa-Regular.ttf"
 end
