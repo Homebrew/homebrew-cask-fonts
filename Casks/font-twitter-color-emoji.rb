5589b7005f99233ad3fdb0714a8505ae67886a0a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:26 +0100

    font-twitter-color-emoji.rb: fix for new style

--- Casks/font-twitter-color-emoji.rb
+++ Casks/font-twitter-color-emoji.rb
@@ -1,11 +1,11 @@
-cask 'font-twitter-color-emoji' do
-  version '12.0.1'
-  sha256 'b57aed282ae875fd0415b7a0caebb478164bf1aa1e25301cbd652a99e48aad66'
+cask "font-twitter-color-emoji" do
+  version "12.0.1"
+  sha256 "b57aed282ae875fd0415b7a0caebb478164bf1aa1e25301cbd652a99e48aad66"
 
   url "https://github.com/eosrei/twemoji-color-font/releases/download/v#{version}/TwitterColorEmoji-SVGinOT-#{version}.zip"
-  appcast 'https://github.com/eosrei/twemoji-color-font/releases.atom'
-  name 'Twitter Color Emoji'
-  homepage 'https://github.com/eosrei/twemoji-color-font/'
+  appcast "https://github.com/eosrei/twemoji-color-font/releases.atom"
+  name "Twitter Color Emoji"
+  homepage "https://github.com/eosrei/twemoji-color-font/"
 
   font "TwitterColorEmoji-SVGinOT-#{version}/TwitterColorEmoji-SVGinOT.ttf"
 end
