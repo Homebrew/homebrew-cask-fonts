1b4b3ce4b74333b6ade287169ec0714ae8bd0409
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:32:49 +0100

    font-alike-angular.rb: fix for new style

--- Casks/font-alike-angular.rb
+++ Casks/font-alike-angular.rb
@@ -1,11 +1,11 @@
-cask 'font-alike-angular' do
+cask "font-alike-angular" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/alikeangular/AlikeAngular-Regular.ttf'
-  name 'Alike Angular'
-  homepage 'https://fonts.google.com/specimen/Alike+Angular'
+  url "https://github.com/google/fonts/raw/master/ofl/alikeangular/AlikeAngular-Regular.ttf"
+  name "Alike Angular"
+  homepage "https://fonts.google.com/specimen/Alike+Angular"
 
-  font 'AlikeAngular-Regular.ttf'
+  font "AlikeAngular-Regular.ttf"
 end
