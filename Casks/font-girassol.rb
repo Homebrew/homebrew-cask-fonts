179ec6aef5fb9686cb3ff5f4aec7d7597de46d25
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:02 +0100

    font-girassol.rb: fix for new style

--- Casks/font-girassol.rb
+++ Casks/font-girassol.rb
@@ -1,11 +1,11 @@
-cask 'font-girassol' do
+cask "font-girassol" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/girassol/Girassol-Regular.ttf'
-  name 'Girassol'
-  homepage 'https://fonts.google.com/specimen/Girassol'
+  url "https://github.com/google/fonts/raw/master/ofl/girassol/Girassol-Regular.ttf"
+  name "Girassol"
+  homepage "https://fonts.google.com/specimen/Girassol"
 
-  font 'Girassol-Regular.ttf'
+  font "Girassol-Regular.ttf"
 end
