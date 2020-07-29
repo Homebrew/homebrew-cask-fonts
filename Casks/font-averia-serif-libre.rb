332014c1468d57e7803cc80e9d4f3f84cb087080
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:57 +0100

    font-averia-serif-libre.rb: fix for new style

--- Casks/font-averia-serif-libre.rb
+++ Casks/font-averia-serif-libre.rb
@@ -1,20 +1,20 @@
-cask 'font-averia-serif-libre' do
+cask "font-averia-serif-libre" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/averiaseriflibre',
+  url "https://github.com/google/fonts/trunk/ofl/averiaseriflibre",
       using:      :svn,
       trust_cert: true
-  name 'Averia Serif Libre'
-  homepage 'https://fonts.google.com/specimen/Averia+Serif+Libre'
+  name "Averia Serif Libre"
+  homepage "https://fonts.google.com/specimen/Averia+Serif+Libre"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'AveriaSerifLibre-Bold.ttf'
-  font 'AveriaSerifLibre-BoldItalic.ttf'
-  font 'AveriaSerifLibre-Italic.ttf'
-  font 'AveriaSerifLibre-Light.ttf'
-  font 'AveriaSerifLibre-LightItalic.ttf'
-  font 'AveriaSerifLibre-Regular.ttf'
+  font "AveriaSerifLibre-Bold.ttf"
+  font "AveriaSerifLibre-BoldItalic.ttf"
+  font "AveriaSerifLibre-Italic.ttf"
+  font "AveriaSerifLibre-Light.ttf"
+  font "AveriaSerifLibre-LightItalic.ttf"
+  font "AveriaSerifLibre-Regular.ttf"
 end
