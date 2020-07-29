87e8bdc0265ba283cddc7012ef3a7fec0464dabb
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:39 +0100

    font-cuprum.rb: fix for new style

--- Casks/font-cuprum.rb
+++ Casks/font-cuprum.rb
@@ -1,18 +1,18 @@
-cask 'font-cuprum' do
+cask "font-cuprum" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/cuprum',
+  url "https://github.com/google/fonts/trunk/ofl/cuprum",
       using:      :svn,
       trust_cert: true
-  name 'Cuprum'
-  homepage 'https://fonts.google.com/specimen/Cuprum'
+  name "Cuprum"
+  homepage "https://fonts.google.com/specimen/Cuprum"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Cuprum-Bold.ttf'
-  font 'Cuprum-BoldItalic.ttf'
-  font 'Cuprum-Italic.ttf'
-  font 'Cuprum-Regular.ttf'
+  font "Cuprum-Bold.ttf"
+  font "Cuprum-BoldItalic.ttf"
+  font "Cuprum-Italic.ttf"
+  font "Cuprum-Regular.ttf"
 end
