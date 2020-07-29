bc0c0d39b471e729ad2554e17d8e10e8e35b7193
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:49 +0100

    font-nokora.rb: fix for new style

--- Casks/font-nokora.rb
+++ Casks/font-nokora.rb
@@ -1,16 +1,16 @@
-cask 'font-nokora' do
+cask "font-nokora" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/apache/nokora',
+  url "https://github.com/google/fonts/trunk/apache/nokora",
       using:      :svn,
       trust_cert: true
-  name 'Nokora'
-  homepage 'https://fonts.google.com/specimen/Nokora'
+  name "Nokora"
+  homepage "https://fonts.google.com/specimen/Nokora"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Nokora-Bold.ttf'
-  font 'Nokora-Regular.ttf'
+  font "Nokora-Bold.ttf"
+  font "Nokora-Regular.ttf"
 end
