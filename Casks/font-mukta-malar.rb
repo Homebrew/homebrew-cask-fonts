31a7af51b265945b19897a83622c3627d0eefaf6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:46 +0100

    font-mukta-malar.rb: fix for new style

--- Casks/font-mukta-malar.rb
+++ Casks/font-mukta-malar.rb
@@ -1,21 +1,21 @@
-cask 'font-mukta-malar' do
+cask "font-mukta-malar" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/muktamalar',
+  url "https://github.com/google/fonts/trunk/ofl/muktamalar",
       using:      :svn,
       trust_cert: true
-  name 'Mukta Malar'
-  homepage 'https://fonts.google.com/specimen/Mukta+Malar'
+  name "Mukta Malar"
+  homepage "https://fonts.google.com/specimen/Mukta+Malar"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'MuktaMalar-Bold.ttf'
-  font 'MuktaMalar-ExtraBold.ttf'
-  font 'MuktaMalar-ExtraLight.ttf'
-  font 'MuktaMalar-Light.ttf'
-  font 'MuktaMalar-Medium.ttf'
-  font 'MuktaMalar-Regular.ttf'
-  font 'MuktaMalar-SemiBold.ttf'
+  font "MuktaMalar-Bold.ttf"
+  font "MuktaMalar-ExtraBold.ttf"
+  font "MuktaMalar-ExtraLight.ttf"
+  font "MuktaMalar-Light.ttf"
+  font "MuktaMalar-Medium.ttf"
+  font "MuktaMalar-Regular.ttf"
+  font "MuktaMalar-SemiBold.ttf"
 end
