164b2c645d3e41c4c5ba9c8e9c25e93697a678b7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:03 +0100

    font-oxygen-mono.rb: fix for new style

--- Casks/font-oxygen-mono.rb
+++ Casks/font-oxygen-mono.rb
@@ -1,11 +1,11 @@
-cask 'font-oxygen-mono' do
+cask "font-oxygen-mono" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/oxygenmono/OxygenMono-Regular.ttf'
-  name 'Oxygen Mono'
-  homepage 'https://fonts.google.com/specimen/Oxygen+Mono'
+  url "https://github.com/google/fonts/raw/master/ofl/oxygenmono/OxygenMono-Regular.ttf"
+  name "Oxygen Mono"
+  homepage "https://fonts.google.com/specimen/Oxygen+Mono"
 
-  font 'OxygenMono-Regular.ttf'
+  font "OxygenMono-Regular.ttf"
 end
