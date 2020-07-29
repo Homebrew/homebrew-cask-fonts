8a666a1e8aed9144f67227cfa200f6ead5629f50
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:40 +0100

    font-darker-grotesque.rb: fix for new style

--- Casks/font-darker-grotesque.rb
+++ Casks/font-darker-grotesque.rb
@@ -1,21 +1,21 @@
-cask 'font-darker-grotesque' do
+cask "font-darker-grotesque" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/darkergrotesque',
+  url "https://github.com/google/fonts/trunk/ofl/darkergrotesque",
       using:      :svn,
       trust_cert: true
-  name 'Darker Grotesque'
-  homepage 'https://fonts.google.com/specimen/Darker+Grotesque'
+  name "Darker Grotesque"
+  homepage "https://fonts.google.com/specimen/Darker+Grotesque"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'DarkerGrotesque-Black.ttf'
-  font 'DarkerGrotesque-Bold.ttf'
-  font 'DarkerGrotesque-ExtraBold.ttf'
-  font 'DarkerGrotesque-Light.ttf'
-  font 'DarkerGrotesque-Medium.ttf'
-  font 'DarkerGrotesque-Regular.ttf'
-  font 'DarkerGrotesque-SemiBold.ttf'
+  font "DarkerGrotesque-Black.ttf"
+  font "DarkerGrotesque-Bold.ttf"
+  font "DarkerGrotesque-ExtraBold.ttf"
+  font "DarkerGrotesque-Light.ttf"
+  font "DarkerGrotesque-Medium.ttf"
+  font "DarkerGrotesque-Regular.ttf"
+  font "DarkerGrotesque-SemiBold.ttf"
 end
