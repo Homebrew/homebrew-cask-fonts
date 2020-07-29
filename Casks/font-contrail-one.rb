79e0caaff0642972f4267aa3f7cbcd1614f6153b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:35 +0100

    font-contrail-one.rb: fix for new style

--- Casks/font-contrail-one.rb
+++ Casks/font-contrail-one.rb
@@ -1,11 +1,11 @@
-cask 'font-contrail-one' do
+cask "font-contrail-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/contrailone/ContrailOne-Regular.ttf'
-  name 'Contrail One'
-  homepage 'https://fonts.google.com/specimen/Contrail+One'
+  url "https://github.com/google/fonts/raw/master/ofl/contrailone/ContrailOne-Regular.ttf"
+  name "Contrail One"
+  homepage "https://fonts.google.com/specimen/Contrail+One"
 
-  font 'ContrailOne-Regular.ttf'
+  font "ContrailOne-Regular.ttf"
 end
