c74078ea970a8147a9297b1445f43ff844d67408
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:41 +0100

    font-meslo-for-powerline.rb: fix for new style

--- Casks/font-meslo-for-powerline.rb
+++ Casks/font-meslo-for-powerline.rb
@@ -1,11 +1,11 @@
-cask 'font-meslo-for-powerline' do
-  version '2015-12-04'
-  sha256 'b6673e0e28441cc04065f8e5c79960aeaa220dae52351852b37e6cf3a03bb8af'
+cask "font-meslo-for-powerline" do
+  version "2015-12-04"
+  sha256 "b6673e0e28441cc04065f8e5c79960aeaa220dae52351852b37e6cf3a03bb8af"
 
   url "https://github.com/powerline/fonts/archive/#{version}.zip"
-  appcast 'https://github.com/powerline/fonts/releases.atom'
-  name 'Meslo for Powerline'
-  homepage 'https://github.com/powerline/fonts'
+  appcast "https://github.com/powerline/fonts/releases.atom"
+  name "Meslo for Powerline"
+  homepage "https://github.com/powerline/fonts"
 
   font "fonts-#{version}/Meslo/Meslo LG L DZ Regular for Powerline.otf"
   font "fonts-#{version}/Meslo/Meslo LG L Regular for Powerline.otf"
