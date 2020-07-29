b1f02ba1f3409814b5fa30b709399a5c60ba7488
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:26 +0100

    font-caladea.rb: fix for new style

--- Casks/font-caladea.rb
+++ Casks/font-caladea.rb
@@ -1,18 +1,18 @@
-cask 'font-caladea' do
+cask "font-caladea" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/caladea',
+  url "https://github.com/google/fonts/trunk/ofl/caladea",
       using:      :svn,
       trust_cert: true
-  name 'Caladea'
-  homepage 'https://fonts.google.com/specimen/Caladea'
+  name "Caladea"
+  homepage "https://fonts.google.com/specimen/Caladea"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Caladea-Bold.ttf'
-  font 'Caladea-BoldItalic.ttf'
-  font 'Caladea-Italic.ttf'
-  font 'Caladea-Regular.ttf'
+  font "Caladea-Bold.ttf"
+  font "Caladea-BoldItalic.ttf"
+  font "Caladea-Italic.ttf"
+  font "Caladea-Regular.ttf"
 end
