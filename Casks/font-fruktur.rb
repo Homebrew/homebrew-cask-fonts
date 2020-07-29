b1b65d787439128535766435104d898ffffdc3c4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:57 +0100

    font-fruktur.rb: fix for new style

--- Casks/font-fruktur.rb
+++ Casks/font-fruktur.rb
@@ -1,11 +1,11 @@
-cask 'font-fruktur' do
+cask "font-fruktur" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/fruktur/Fruktur-Regular.ttf'
-  name 'Fruktur'
-  homepage 'https://fonts.google.com/specimen/Fruktur'
+  url "https://github.com/google/fonts/raw/master/ofl/fruktur/Fruktur-Regular.ttf"
+  name "Fruktur"
+  homepage "https://fonts.google.com/specimen/Fruktur"
 
-  font 'Fruktur-Regular.ttf'
+  font "Fruktur-Regular.ttf"
 end
