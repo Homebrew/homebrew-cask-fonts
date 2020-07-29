71eb0481651756d377f5f359cd72b93477c84140
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:43 +0100

    font-dhyana.rb: fix for new style

--- Casks/font-dhyana.rb
+++ Casks/font-dhyana.rb
@@ -1,16 +1,16 @@
-cask 'font-dhyana' do
+cask "font-dhyana" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/dhyana',
+  url "https://github.com/google/fonts/trunk/ofl/dhyana",
       using:      :svn,
       trust_cert: true
-  name 'Dhyana'
-  homepage 'https://fonts.google.com/specimen/Dhyana'
+  name "Dhyana"
+  homepage "https://fonts.google.com/specimen/Dhyana"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Dhyana-Bold.ttf'
-  font 'Dhyana-Regular.ttf'
+  font "Dhyana-Bold.ttf"
+  font "Dhyana-Regular.ttf"
 end
