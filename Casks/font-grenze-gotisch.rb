f34e6bffc8c6eb226af50afd5bfac802fab8306f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:06 +0100

    font-grenze-gotisch.rb: fix for new style

--- Casks/font-grenze-gotisch.rb
+++ Casks/font-grenze-gotisch.rb
@@ -1,11 +1,11 @@
-cask 'font-grenze-gotisch' do
+cask "font-grenze-gotisch" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/grenzegotisch/GrenzeGotisch%5Bwght%5D.ttf'
-  name 'Grenze Gotisch'
-  homepage 'https://fonts.google.com/specimen/Grenze+Gotisch'
+  url "https://github.com/google/fonts/raw/master/ofl/grenzegotisch/GrenzeGotisch%5Bwght%5D.ttf"
+  name "Grenze Gotisch"
+  homepage "https://fonts.google.com/specimen/Grenze+Gotisch"
 
-  font 'GrenzeGotisch[wght].ttf'
+  font "GrenzeGotisch[wght].ttf"
 end
