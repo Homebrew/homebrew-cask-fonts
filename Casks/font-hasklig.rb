978fdd83193b8d136177daffbeca0149dae0c09e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:09 +0100

    font-hasklig.rb: fix for new style

--- Casks/font-hasklig.rb
+++ Casks/font-hasklig.rb
@@ -1,24 +1,24 @@
-cask 'font-hasklig' do
-  version '1.1'
-  sha256 '98eccec22bc0ae3838a099a911230f62197e681b6a6c30e5149c52f5fc6a95c3'
+cask "font-hasklig" do
+  version "1.1"
+  sha256 "98eccec22bc0ae3838a099a911230f62197e681b6a6c30e5149c52f5fc6a95c3"
 
   url "https://github.com/i-tu/Hasklig/releases/download/#{version}/Hasklig-#{version}.zip"
-  appcast 'https://github.com/i-tu/Hasklig/releases.atom'
-  name 'Hasklig'
-  homepage 'https://github.com/i-tu/Hasklig'
+  appcast "https://github.com/i-tu/Hasklig/releases.atom"
+  name "Hasklig"
+  homepage "https://github.com/i-tu/Hasklig"
 
-  font 'Hasklig-Black.otf'
-  font 'Hasklig-BlackIt.otf'
-  font 'Hasklig-Bold.otf'
-  font 'Hasklig-BoldIt.otf'
-  font 'Hasklig-ExtraLight.otf'
-  font 'Hasklig-ExtraLightIt.otf'
-  font 'Hasklig-It.otf'
-  font 'Hasklig-Light.otf'
-  font 'Hasklig-LightIt.otf'
-  font 'Hasklig-Medium.otf'
-  font 'Hasklig-MediumIt.otf'
-  font 'Hasklig-Regular.otf'
-  font 'Hasklig-Semibold.otf'
-  font 'Hasklig-SemiboldIt.otf'
+  font "Hasklig-Black.otf"
+  font "Hasklig-BlackIt.otf"
+  font "Hasklig-Bold.otf"
+  font "Hasklig-BoldIt.otf"
+  font "Hasklig-ExtraLight.otf"
+  font "Hasklig-ExtraLightIt.otf"
+  font "Hasklig-It.otf"
+  font "Hasklig-Light.otf"
+  font "Hasklig-LightIt.otf"
+  font "Hasklig-Medium.otf"
+  font "Hasklig-MediumIt.otf"
+  font "Hasklig-Regular.otf"
+  font "Hasklig-Semibold.otf"
+  font "Hasklig-SemiboldIt.otf"
 end
