58b81ff6e51a3a8b9cf69d64fa2aac3445b6f48d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:46 +0100

    font-mouse-memoirs.rb: fix for new style

--- Casks/font-mouse-memoirs.rb
+++ Casks/font-mouse-memoirs.rb
@@ -1,11 +1,11 @@
-cask 'font-mouse-memoirs' do
+cask "font-mouse-memoirs" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/mousememoirs/MouseMemoirs-Regular.ttf'
-  name 'Mouse Memoirs'
-  homepage 'https://fonts.google.com/specimen/Mouse+Memoirs'
+  url "https://github.com/google/fonts/raw/master/ofl/mousememoirs/MouseMemoirs-Regular.ttf"
+  name "Mouse Memoirs"
+  homepage "https://fonts.google.com/specimen/Mouse+Memoirs"
 
-  font 'MouseMemoirs-Regular.ttf'
+  font "MouseMemoirs-Regular.ttf"
 end
