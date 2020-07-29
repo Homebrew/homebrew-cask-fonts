9b36e4f7271da866895052e6cb849fb4ebe3d687
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:21 +0100

    font-black-ops-one.rb: fix for new style

--- Casks/font-black-ops-one.rb
+++ Casks/font-black-ops-one.rb
@@ -1,11 +1,11 @@
-cask 'font-black-ops-one' do
+cask "font-black-ops-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/blackopsone/BlackOpsOne-Regular.ttf'
-  name 'Black Ops One'
-  homepage 'https://fonts.google.com/specimen/Black+Ops+One'
+  url "https://github.com/google/fonts/raw/master/ofl/blackopsone/BlackOpsOne-Regular.ttf"
+  name "Black Ops One"
+  homepage "https://fonts.google.com/specimen/Black+Ops+One"
 
-  font 'BlackOpsOne-Regular.ttf'
+  font "BlackOpsOne-Regular.ttf"
 end
