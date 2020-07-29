1e149785fcc074ec95a574b10ba73e3ebacef496
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:21 +0100

    font-italiana.rb: fix for new style

--- Casks/font-italiana.rb
+++ Casks/font-italiana.rb
@@ -1,11 +1,11 @@
-cask 'font-italiana' do
+cask "font-italiana" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/italiana/Italiana-Regular.ttf'
-  name 'Italiana'
-  homepage 'https://fonts.google.com/specimen/Italiana'
+  url "https://github.com/google/fonts/raw/master/ofl/italiana/Italiana-Regular.ttf"
+  name "Italiana"
+  homepage "https://fonts.google.com/specimen/Italiana"
 
-  font 'Italiana-Regular.ttf'
+  font "Italiana-Regular.ttf"
 end
