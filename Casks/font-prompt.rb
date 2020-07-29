027ff29231c42e2a04aaf16ce069088151668089
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:07 +0100

    font-prompt.rb: fix for new style

--- Casks/font-prompt.rb
+++ Casks/font-prompt.rb
@@ -1,32 +1,32 @@
-cask 'font-prompt' do
+cask "font-prompt" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/prompt',
+  url "https://github.com/google/fonts/trunk/ofl/prompt",
       using:      :svn,
       trust_cert: true
-  name 'Prompt'
-  homepage 'https://fonts.google.com/specimen/Prompt'
+  name "Prompt"
+  homepage "https://fonts.google.com/specimen/Prompt"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Prompt-Black.ttf'
-  font 'Prompt-BlackItalic.ttf'
-  font 'Prompt-Bold.ttf'
-  font 'Prompt-BoldItalic.ttf'
-  font 'Prompt-ExtraBold.ttf'
-  font 'Prompt-ExtraBoldItalic.ttf'
-  font 'Prompt-ExtraLight.ttf'
-  font 'Prompt-ExtraLightItalic.ttf'
-  font 'Prompt-Italic.ttf'
-  font 'Prompt-Light.ttf'
-  font 'Prompt-LightItalic.ttf'
-  font 'Prompt-Medium.ttf'
-  font 'Prompt-MediumItalic.ttf'
-  font 'Prompt-Regular.ttf'
-  font 'Prompt-SemiBold.ttf'
-  font 'Prompt-SemiBoldItalic.ttf'
-  font 'Prompt-Thin.ttf'
-  font 'Prompt-ThinItalic.ttf'
+  font "Prompt-Black.ttf"
+  font "Prompt-BlackItalic.ttf"
+  font "Prompt-Bold.ttf"
+  font "Prompt-BoldItalic.ttf"
+  font "Prompt-ExtraBold.ttf"
+  font "Prompt-ExtraBoldItalic.ttf"
+  font "Prompt-ExtraLight.ttf"
+  font "Prompt-ExtraLightItalic.ttf"
+  font "Prompt-Italic.ttf"
+  font "Prompt-Light.ttf"
+  font "Prompt-LightItalic.ttf"
+  font "Prompt-Medium.ttf"
+  font "Prompt-MediumItalic.ttf"
+  font "Prompt-Regular.ttf"
+  font "Prompt-SemiBold.ttf"
+  font "Prompt-SemiBoldItalic.ttf"
+  font "Prompt-Thin.ttf"
+  font "Prompt-ThinItalic.ttf"
 end
