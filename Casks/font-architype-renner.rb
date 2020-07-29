f58cde89b41b405ad049b7f45eeeebc0d10fb429
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:59 +0100

    font-architype-renner.rb: fix for new style

--- Casks/font-architype-renner.rb
+++ Casks/font-architype-renner.rb
@@ -1,13 +1,13 @@
-cask 'font-architype-renner' do
-  version '001.000'
-  sha256 '8a8de50838960d7e597b5eda4214c22418b32121505a61f3d0bbb120ff7fd24a'
+cask "font-architype-renner" do
+  version "001.000"
+  sha256 "8a8de50838960d7e597b5eda4214c22418b32121505a61f3d0bbb120ff7fd24a"
 
-  url 'https://fontlot.com/wp-content/uploads/2017/08/archityperenner.zip'
-  name 'Architype Renner'
-  homepage 'https://fontlot.com/4598/architype-renner/'
+  url "https://fontlot.com/wp-content/uploads/2017/08/archityperenner.zip"
+  name "Architype Renner"
+  homepage "https://fontlot.com/4598/architype-renner/"
 
-  font 'ArchitypeRenner-Bold.otf'
-  font 'ArchitypeRenner-Demi.otf'
-  font 'ArchitypeRenner-Medium.otf'
-  font 'ArchitypeRenner-Regular.otf'
+  font "ArchitypeRenner-Bold.otf"
+  font "ArchitypeRenner-Demi.otf"
+  font "ArchitypeRenner-Medium.otf"
+  font "ArchitypeRenner-Regular.otf"
 end
