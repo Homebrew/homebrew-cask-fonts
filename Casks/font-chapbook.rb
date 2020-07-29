8461117f7cf148304202c0679b0885ef4efbf1ef
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:29 +0100

    font-chapbook.rb: fix for new style

--- Casks/font-chapbook.rb
+++ Casks/font-chapbook.rb
@@ -1,12 +1,12 @@
-cask 'font-chapbook' do
+cask "font-chapbook" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.feorag.com/freestuff/archives/Chapbook-OTF.zip'
-  name 'Chapbook'
-  homepage 'https://www.feorag.com/freestuff/chapbook.html'
+  url "https://www.feorag.com/freestuff/archives/Chapbook-OTF.zip"
+  name "Chapbook"
+  homepage "https://www.feorag.com/freestuff/chapbook.html"
 
-  font 'Chapbook OTF/Chapbook-Dingbats.otf'
-  font 'Chapbook OTF/Chapbook-Italic.otf'
-  font 'Chapbook OTF/Chapbook-Regular.otf'
+  font "Chapbook OTF/Chapbook-Dingbats.otf"
+  font "Chapbook OTF/Chapbook-Italic.otf"
+  font "Chapbook OTF/Chapbook-Regular.otf"
 end
