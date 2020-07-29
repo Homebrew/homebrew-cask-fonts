f9244d704ed830036053866f1867f2e03079faa0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:21 +0100

    font-irish-grover.rb: fix for new style

--- Casks/font-irish-grover.rb
+++ Casks/font-irish-grover.rb
@@ -1,11 +1,11 @@
-cask 'font-irish-grover' do
+cask "font-irish-grover" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/irishgrover/IrishGrover-Regular.ttf'
-  name 'Irish Grover'
-  homepage 'https://fonts.google.com/specimen/Irish+Grover'
+  url "https://github.com/google/fonts/raw/master/apache/irishgrover/IrishGrover-Regular.ttf"
+  name "Irish Grover"
+  homepage "https://fonts.google.com/specimen/Irish+Grover"
 
-  font 'IrishGrover-Regular.ttf'
+  font "IrishGrover-Regular.ttf"
 end
