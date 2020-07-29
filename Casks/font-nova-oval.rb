555af33464f777386ad4a2b0783a524de373df16
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:59 +0100

    font-nova-oval.rb: fix for new style

--- Casks/font-nova-oval.rb
+++ Casks/font-nova-oval.rb
@@ -1,11 +1,11 @@
-cask 'font-nova-oval' do
+cask "font-nova-oval" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/novaoval/NovaOval.ttf'
-  name 'Nova Oval'
-  homepage 'https://fonts.google.com/specimen/Nova+Oval'
+  url "https://github.com/google/fonts/raw/master/ofl/novaoval/NovaOval.ttf"
+  name "Nova Oval"
+  homepage "https://fonts.google.com/specimen/Nova+Oval"
 
-  font 'NovaOval.ttf'
+  font "NovaOval.ttf"
 end
