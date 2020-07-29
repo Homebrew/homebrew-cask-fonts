916740a8856fc1f93f2e04c54615cd1c19b0401f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:43 +0100

    font-miniver.rb: fix for new style

--- Casks/font-miniver.rb
+++ Casks/font-miniver.rb
@@ -1,11 +1,11 @@
-cask 'font-miniver' do
+cask "font-miniver" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/miniver/Miniver-Regular.ttf'
-  name 'Miniver'
-  homepage 'https://fonts.google.com/specimen/Miniver'
+  url "https://github.com/google/fonts/raw/master/ofl/miniver/Miniver-Regular.ttf"
+  name "Miniver"
+  homepage "https://fonts.google.com/specimen/Miniver"
 
-  font 'Miniver-Regular.ttf'
+  font "Miniver-Regular.ttf"
 end
