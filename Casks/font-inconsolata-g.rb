c6e062c738e57a0c66afb598b480bd1ea82f8301
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:16 +0100

    font-inconsolata-g.rb: fix for new style

--- Casks/font-inconsolata-g.rb
+++ Casks/font-inconsolata-g.rb
@@ -1,11 +1,11 @@
-cask 'font-inconsolata-g' do
+cask "font-inconsolata-g" do
   version :latest
   sha256 :no_check
 
   # fantascienza.net/leonardo/ar/inconsolatag/ was verified as official when first introduced to the cask
-  url 'http://www.fantascienza.net/leonardo/ar/inconsolatag/inconsolata-g_font.zip'
-  name 'Inconsolata-g'
-  homepage 'https://leonardo-m.livejournal.com/77079.html'
+  url "http://www.fantascienza.net/leonardo/ar/inconsolatag/inconsolata-g_font.zip"
+  name "Inconsolata-g"
+  homepage "https://leonardo-m.livejournal.com/77079.html"
 
-  font 'Inconsolata-g.otf'
+  font "Inconsolata-g.otf"
 end
