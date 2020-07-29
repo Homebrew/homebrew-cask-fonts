a681a5eda461d984853b7300d43a56290be085f9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:55 +0100

    font-foundation-icons.rb: fix for new style

--- Casks/font-foundation-icons.rb
+++ Casks/font-foundation-icons.rb
@@ -1,10 +1,10 @@
-cask 'font-foundation-icons' do
+cask "font-foundation-icons" do
   version :latest
   sha256 :no_check
 
-  url 'https://zurb.com/playground/uploads/upload/upload/288/foundation-icons.zip'
-  name 'Foundation Icons'
-  homepage 'https://zurb.com/playground/foundation-icon-fonts-3'
+  url "https://zurb.com/playground/uploads/upload/upload/288/foundation-icons.zip"
+  name "Foundation Icons"
+  homepage "https://zurb.com/playground/foundation-icon-fonts-3"
 
-  font 'foundation-icons/foundation-icons.ttf'
+  font "foundation-icons/foundation-icons.ttf"
 end
