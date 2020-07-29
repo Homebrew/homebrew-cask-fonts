a82a93a3e673a7ddf80cb85780eb53acfe65ced4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:07 +0100

    font-gugi.rb: fix for new style

--- Casks/font-gugi.rb
+++ Casks/font-gugi.rb
@@ -1,11 +1,11 @@
-cask 'font-gugi' do
+cask "font-gugi" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/gugi/Gugi-Regular.ttf'
-  name 'Gugi'
-  homepage 'https://fonts.google.com/specimen/Gugi'
+  url "https://github.com/google/fonts/raw/master/ofl/gugi/Gugi-Regular.ttf"
+  name "Gugi"
+  homepage "https://fonts.google.com/specimen/Gugi"
 
-  font 'Gugi-Regular.ttf'
+  font "Gugi-Regular.ttf"
 end
