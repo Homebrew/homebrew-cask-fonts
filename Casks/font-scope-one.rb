6199a4b45fb7b1e4fd61c99ba3c6298f1bd70caa
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:15 +0100

    font-scope-one.rb: fix for new style

--- Casks/font-scope-one.rb
+++ Casks/font-scope-one.rb
@@ -1,11 +1,11 @@
-cask 'font-scope-one' do
+cask "font-scope-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/scopeone/ScopeOne-Regular.ttf'
-  name 'Scope One'
-  homepage 'https://fonts.google.com/specimen/Scope+One'
+  url "https://github.com/google/fonts/raw/master/ofl/scopeone/ScopeOne-Regular.ttf"
+  name "Scope One"
+  homepage "https://fonts.google.com/specimen/Scope+One"
 
-  font 'ScopeOne-Regular.ttf'
+  font "ScopeOne-Regular.ttf"
 end
