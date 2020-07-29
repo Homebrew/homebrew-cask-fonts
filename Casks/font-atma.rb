27408c35e760cf6ac01d39ca894237968b56f05c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:21 +0100

    font-atma.rb: fix for new style

--- Casks/font-atma.rb
+++ Casks/font-atma.rb
@@ -1,19 +1,19 @@
-cask 'font-atma' do
+cask "font-atma" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/atma',
+  url "https://github.com/google/fonts/trunk/ofl/atma",
       using:      :svn,
       trust_cert: true
-  name 'Atma'
-  homepage 'https://fonts.google.com/specimen/Atma'
+  name "Atma"
+  homepage "https://fonts.google.com/specimen/Atma"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Atma-Bold.ttf'
-  font 'Atma-Light.ttf'
-  font 'Atma-Medium.ttf'
-  font 'Atma-Regular.ttf'
-  font 'Atma-SemiBold.ttf'
+  font "Atma-Bold.ttf"
+  font "Atma-Light.ttf"
+  font "Atma-Medium.ttf"
+  font "Atma-Regular.ttf"
+  font "Atma-SemiBold.ttf"
 end
