73276d161519659201e94bad753be239cd57e40e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:31 +0100

    font-actor.rb: fix for new style

--- Casks/font-actor.rb
+++ Casks/font-actor.rb
@@ -1,11 +1,11 @@
-cask 'font-actor' do
+cask "font-actor" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/actor/Actor-Regular.ttf'
-  name 'Actor'
-  homepage 'https://fonts.google.com/specimen/Actor'
+  url "https://github.com/google/fonts/raw/master/ofl/actor/Actor-Regular.ttf"
+  name "Actor"
+  homepage "https://fonts.google.com/specimen/Actor"
 
-  font 'Actor-Regular.ttf'
+  font "Actor-Regular.ttf"
 end
