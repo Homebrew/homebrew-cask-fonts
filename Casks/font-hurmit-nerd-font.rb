4f74006953a3147456aadb5aa2679600447aa13f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:12 +0100

    font-hurmit-nerd-font.rb: fix for new style

--- Casks/font-hurmit-nerd-font.rb
+++ Casks/font-hurmit-nerd-font.rb
@@ -1,16 +1,16 @@
-cask 'font-hurmit-nerd-font' do
-  version '2.1.0'
-  sha256 '551fcb801963cd5ced87a85d135589053bbd23b6f5674ddb985d9e50b2bc49c6'
+cask "font-hurmit-nerd-font" do
+  version "2.1.0"
+  sha256 "551fcb801963cd5ced87a85d135589053bbd23b6f5674ddb985d9e50b2bc49c6"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hermit.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'Hurmit Nerd Font (Hermit)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "Hurmit Nerd Font (Hermit)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Hurmit Bold Nerd Font Complete.otf'
-  font 'Hurmit Medium Nerd Font Complete.otf'
-  font 'Hurmit Light Nerd Font Complete.otf'
-  font 'Hurmit Bold Nerd Font Complete Mono.otf'
-  font 'Hurmit Medium Nerd Font Complete Mono.otf'
-  font 'Hurmit Light Nerd Font Complete Mono.otf'
+  font "Hurmit Bold Nerd Font Complete.otf"
+  font "Hurmit Medium Nerd Font Complete.otf"
+  font "Hurmit Light Nerd Font Complete.otf"
+  font "Hurmit Bold Nerd Font Complete Mono.otf"
+  font "Hurmit Medium Nerd Font Complete Mono.otf"
+  font "Hurmit Light Nerd Font Complete Mono.otf"
 end
