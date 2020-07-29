a1fcdbbc13bae28d00d4c40d2a03053c51cebbf6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:38 +0100

    font-covered-by-your-grace.rb: fix for new style

--- Casks/font-covered-by-your-grace.rb
+++ Casks/font-covered-by-your-grace.rb
@@ -1,11 +1,11 @@
-cask 'font-covered-by-your-grace' do
+cask "font-covered-by-your-grace" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/coveredbyyourgrace/CoveredByYourGrace.ttf'
-  name 'Covered By Your Grace'
-  homepage 'https://fonts.google.com/specimen/Covered+By+Your+Grace'
+  url "https://github.com/google/fonts/raw/master/ofl/coveredbyyourgrace/CoveredByYourGrace.ttf"
+  name "Covered By Your Grace"
+  homepage "https://fonts.google.com/specimen/Covered+By+Your+Grace"
 
-  font 'CoveredByYourGrace.ttf'
+  font "CoveredByYourGrace.ttf"
 end
