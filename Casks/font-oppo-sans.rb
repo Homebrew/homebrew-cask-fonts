8385eabefed82ef6ef7daf509fd87a71fcfd99f7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:01 +0100

    font-oppo-sans.rb: fix for new style

--- Casks/font-oppo-sans.rb
+++ Casks/font-oppo-sans.rb
@@ -1,14 +1,14 @@
-cask 'font-oppo-sans' do
+cask "font-oppo-sans" do
   version :latest
   sha256 :no_check
 
-  url 'https://static01.coloros.com/www/public/img/topic7/font-opposans.zip'
-  name 'OPPO Sans'
-  homepage 'https://www.coloros.com/topic/coloros7.html'
+  url "https://static01.coloros.com/www/public/img/topic7/font-opposans.zip"
+  name "OPPO Sans"
+  homepage "https://www.coloros.com/topic/coloros7.html"
 
-  font 'Font-OPPOSans/OPPOSans-B.ttf'
-  font 'Font-OPPOSans/OPPOSans-H.ttf'
-  font 'Font-OPPOSans/OPPOSans-L.ttf'
-  font 'Font-OPPOSans/OPPOSans-M.ttf'
-  font 'Font-OPPOSans/OPPOSans-R.ttf'
+  font "Font-OPPOSans/OPPOSans-B.ttf"
+  font "Font-OPPOSans/OPPOSans-H.ttf"
+  font "Font-OPPOSans/OPPOSans-L.ttf"
+  font "Font-OPPOSans/OPPOSans-M.ttf"
+  font "Font-OPPOSans/OPPOSans-R.ttf"
 end
