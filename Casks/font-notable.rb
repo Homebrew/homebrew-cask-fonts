7650ddd64038c5c169843ab0a5583c7418f10b3c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:49 +0100

    font-notable.rb: fix for new style

--- Casks/font-notable.rb
+++ Casks/font-notable.rb
@@ -1,11 +1,11 @@
-cask 'font-notable' do
+cask "font-notable" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/notable/Notable-Regular.ttf'
-  name 'Notable'
-  homepage 'https://fonts.google.com/specimen/Notable'
+  url "https://github.com/google/fonts/raw/master/ofl/notable/Notable-Regular.ttf"
+  name "Notable"
+  homepage "https://fonts.google.com/specimen/Notable"
 
-  font 'Notable-Regular.ttf'
+  font "Notable-Regular.ttf"
 end
