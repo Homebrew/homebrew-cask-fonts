8b6c8a022efb832bfcb7558bfd307fdb8d89b90b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:20 +0100

    font-ipamjmincho.rb: fix for new style

--- Casks/font-ipamjmincho.rb
+++ Casks/font-ipamjmincho.rb
@@ -1,11 +1,11 @@
-cask 'font-ipamjmincho' do
-  version '006.01'
-  sha256 '35494e0f2896f38b3f7369a8421a895cea6440a42c0a66ac95eab47d6ed25b68'
+cask "font-ipamjmincho" do
+  version "006.01"
+  sha256 "35494e0f2896f38b3f7369a8421a895cea6440a42c0a66ac95eab47d6ed25b68"
 
   url "https://mojikiban.ipa.go.jp/OSCDL/IPAmjMincho/ipamjm#{version.no_dots}.zip"
-  name 'IPAmjMincho'
-  name 'IPAmj明朝'
-  homepage 'https://mojikiban.ipa.go.jp/1300.html'
+  name "IPAmjMincho"
+  name "IPAmj明朝"
+  homepage "https://mojikiban.ipa.go.jp/1300.html"
 
-  font 'ipamjm.ttf'
+  font "ipamjm.ttf"
 end
