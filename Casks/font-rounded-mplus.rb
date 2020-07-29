4baec551c71bf44f660b39abe5e7e2669b8b8eca
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:12 +0100

    font-rounded-mplus.rb: fix for new style

--- Casks/font-rounded-mplus.rb
+++ Casks/font-rounded-mplus.rb
@@ -1,21 +1,21 @@
-cask 'font-rounded-mplus' do
+cask "font-rounded-mplus" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/roundedmplus1c',
+  url "https://github.com/google/fonts/trunk/ofl/roundedmplus1c",
       using:      :svn,
       trust_cert: true
-  name 'Rounded Mplus'
-  homepage 'https://fonts.google.com/specimen/Rounded+Mplus'
+  name "Rounded Mplus"
+  homepage "https://fonts.google.com/specimen/Rounded+Mplus"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'RoundedMplus1c-Black.ttf'
-  font 'RoundedMplus1c-Bold.ttf'
-  font 'RoundedMplus1c-ExtraBold.ttf'
-  font 'RoundedMplus1c-Light.ttf'
-  font 'RoundedMplus1c-Medium.ttf'
-  font 'RoundedMplus1c-Regular.ttf'
-  font 'RoundedMplus1c-Thin.ttf'
+  font "RoundedMplus1c-Black.ttf"
+  font "RoundedMplus1c-Bold.ttf"
+  font "RoundedMplus1c-ExtraBold.ttf"
+  font "RoundedMplus1c-Light.ttf"
+  font "RoundedMplus1c-Medium.ttf"
+  font "RoundedMplus1c-Regular.ttf"
+  font "RoundedMplus1c-Thin.ttf"
 end
