221d44860304c283c65253a441ab4983a7e3a789
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:22 +0100

    font-bokor.rb: fix for new style

--- Casks/font-bokor.rb
+++ Casks/font-bokor.rb
@@ -1,11 +1,11 @@
-cask 'font-bokor' do
+cask "font-bokor" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bokor/Bokor-Regular.ttf'
-  name 'Bokor'
-  homepage 'https://fonts.google.com/specimen/Bokor'
+  url "https://github.com/google/fonts/raw/master/ofl/bokor/Bokor-Regular.ttf"
+  name "Bokor"
+  homepage "https://fonts.google.com/specimen/Bokor"
 
-  font 'Bokor-Regular.ttf'
+  font "Bokor-Regular.ttf"
 end
