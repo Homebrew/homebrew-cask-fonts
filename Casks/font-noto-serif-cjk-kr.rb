d57a0e668078691eb0706a1672d17e2d9361f171
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:58 +0100

    font-noto-serif-cjk-kr.rb: fix for new style

--- Casks/font-noto-serif-cjk-kr.rb
+++ Casks/font-noto-serif-cjk-kr.rb
@@ -1,17 +1,17 @@
-cask 'font-noto-serif-cjk-kr' do
+cask "font-noto-serif-cjk-kr" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKkr-hinted.zip'
-  name 'Noto Serif CJK KR'
-  homepage 'https://www.google.com/get/noto/#serif-kore'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKkr-hinted.zip"
+  name "Noto Serif CJK KR"
+  homepage "https://www.google.com/get/noto/#serif-kore"
 
-  font 'NotoSerifCJKkr-Black.otf'
-  font 'NotoSerifCJKkr-Bold.otf'
-  font 'NotoSerifCJKkr-ExtraLight.otf'
-  font 'NotoSerifCJKkr-Light.otf'
-  font 'NotoSerifCJKkr-Medium.otf'
-  font 'NotoSerifCJKkr-Regular.otf'
-  font 'NotoSerifCJKkr-SemiBold.otf'
+  font "NotoSerifCJKkr-Black.otf"
+  font "NotoSerifCJKkr-Bold.otf"
+  font "NotoSerifCJKkr-ExtraLight.otf"
+  font "NotoSerifCJKkr-Light.otf"
+  font "NotoSerifCJKkr-Medium.otf"
+  font "NotoSerifCJKkr-Regular.otf"
+  font "NotoSerifCJKkr-SemiBold.otf"
 end
