f884df78ae9925708f9e63e3cb4abc1ba093e1db
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:47 +0100

    font-eczar.rb: fix for new style

--- Casks/font-eczar.rb
+++ Casks/font-eczar.rb
@@ -1,19 +1,19 @@
-cask 'font-eczar' do
+cask "font-eczar" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/eczar',
+  url "https://github.com/google/fonts/trunk/ofl/eczar",
       using:      :svn,
       trust_cert: true
-  name 'Eczar'
-  homepage 'https://fonts.google.com/specimen/Eczar'
+  name "Eczar"
+  homepage "https://fonts.google.com/specimen/Eczar"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Eczar-Bold.ttf'
-  font 'Eczar-ExtraBold.ttf'
-  font 'Eczar-Medium.ttf'
-  font 'Eczar-Regular.ttf'
-  font 'Eczar-SemiBold.ttf'
+  font "Eczar-Bold.ttf"
+  font "Eczar-ExtraBold.ttf"
+  font "Eczar-Medium.ttf"
+  font "Eczar-Regular.ttf"
+  font "Eczar-SemiBold.ttf"
 end
