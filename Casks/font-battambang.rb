ad3e8bd3d448461eba753cff7cc2108f078ece99
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:17 +0100

    font-battambang.rb: fix for new style

--- Casks/font-battambang.rb
+++ Casks/font-battambang.rb
@@ -1,16 +1,16 @@
-cask 'font-battambang' do
+cask "font-battambang" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/battambang',
+  url "https://github.com/google/fonts/trunk/ofl/battambang",
       using:      :svn,
       trust_cert: true
-  name 'Battambang'
-  homepage 'https://fonts.google.com/specimen/Battambang'
+  name "Battambang"
+  homepage "https://fonts.google.com/specimen/Battambang"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Battambang-Bold.ttf'
-  font 'Battambang-Regular.ttf'
+  font "Battambang-Bold.ttf"
+  font "Battambang-Regular.ttf"
 end
