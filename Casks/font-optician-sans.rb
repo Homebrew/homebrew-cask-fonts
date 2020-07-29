e6e35438c8b9db0d6ae434989c979fb9db68688f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:02 +0100

    font-optician-sans.rb: fix for new style

--- Casks/font-optician-sans.rb
+++ Casks/font-optician-sans.rb
@@ -1,11 +1,11 @@
-cask 'font-optician-sans' do
+cask "font-optician-sans" do
   version :latest
   sha256 :no_check
 
   # github.com/anewtypeofinterference/Optician-Sans/ was verified as official when first introduced to the cask
-  url 'https://github.com/anewtypeofinterference/Optician-Sans/raw/master/OpenType-PS/Optician-Sans.otf'
-  name 'Optician Sans'
-  homepage 'https://optician-sans.com/'
+  url "https://github.com/anewtypeofinterference/Optician-Sans/raw/master/OpenType-PS/Optician-Sans.otf"
+  name "Optician Sans"
+  homepage "https://optician-sans.com/"
 
-  font 'Optician-Sans.otf'
+  font "Optician-Sans.otf"
 end
