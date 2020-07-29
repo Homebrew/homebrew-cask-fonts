46d59581e35feb16a2c69b8a7b796877e6b140c6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:26 +0100

    font-ultra.rb: fix for new style

--- Casks/font-ultra.rb
+++ Casks/font-ultra.rb
@@ -1,11 +1,11 @@
-cask 'font-ultra' do
+cask "font-ultra" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/ultra/Ultra-Regular.ttf'
-  name 'Ultra'
-  homepage 'https://fonts.google.com/specimen/Ultra'
+  url "https://github.com/google/fonts/raw/master/apache/ultra/Ultra-Regular.ttf"
+  name "Ultra"
+  homepage "https://fonts.google.com/specimen/Ultra"
 
-  font 'Ultra-Regular.ttf'
+  font "Ultra-Regular.ttf"
 end
