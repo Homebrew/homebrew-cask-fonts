b5969962b6d869f36f001469e838c2a65dd01579
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:39 +0100

    font-manuale.rb: fix for new style

--- Casks/font-manuale.rb
+++ Casks/font-manuale.rb
@@ -1,16 +1,16 @@
-cask 'font-manuale' do
+cask "font-manuale" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/manuale',
+  url "https://github.com/google/fonts/trunk/ofl/manuale",
       using:      :svn,
       trust_cert: true
-  name 'Manuale'
-  homepage 'https://fonts.google.com/specimen/Manuale'
+  name "Manuale"
+  homepage "https://fonts.google.com/specimen/Manuale"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Manuale-Italic[wght].ttf'
-  font 'Manuale[wght].ttf'
+  font "Manuale-Italic[wght].ttf"
+  font "Manuale[wght].ttf"
 end
