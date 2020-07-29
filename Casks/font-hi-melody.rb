89b61e2adb31f99aecc85b56629b555520f96cff
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:10 +0100

    font-hi-melody.rb: fix for new style

--- Casks/font-hi-melody.rb
+++ Casks/font-hi-melody.rb
@@ -1,11 +1,11 @@
-cask 'font-hi-melody' do
+cask "font-hi-melody" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/himelody/HiMelody-Regular.ttf'
-  name 'Hi Melody'
-  homepage 'https://fonts.google.com/specimen/Hi+Melody'
+  url "https://github.com/google/fonts/raw/master/ofl/himelody/HiMelody-Regular.ttf"
+  name "Hi Melody"
+  homepage "https://fonts.google.com/specimen/Hi+Melody"
 
-  font 'HiMelody-Regular.ttf'
+  font "HiMelody-Regular.ttf"
 end
