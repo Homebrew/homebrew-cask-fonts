84d783c97c130e48224c6f0e4f8a774fa755fbc7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:29 +0100

    font-xkcd-script.rb: fix for new style

--- Casks/font-xkcd-script.rb
+++ Casks/font-xkcd-script.rb
@@ -1,10 +1,10 @@
-cask 'font-xkcd-script' do
+cask "font-xkcd-script" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/ipython/xkcd-font/raw/master/xkcd-script/font/xkcd-script.ttf'
-  name 'xkcd-script'
-  homepage 'https://github.com/ipython/xkcd-font'
+  url "https://github.com/ipython/xkcd-font/raw/master/xkcd-script/font/xkcd-script.ttf"
+  name "xkcd-script"
+  homepage "https://github.com/ipython/xkcd-font"
 
-  font 'xkcd-script.ttf'
+  font "xkcd-script.ttf"
 end
