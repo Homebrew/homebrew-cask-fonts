3ea583d74fe8256bd4462770b3bc3acbf2e4032e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:07 +0100

    font-pt-mono.rb: fix for new style

--- Casks/font-pt-mono.rb
+++ Casks/font-pt-mono.rb
@@ -1,11 +1,11 @@
-cask 'font-pt-mono' do
+cask "font-pt-mono" do
   version :latest
   sha256 :no_check
 
-  url 'https://company.paratype.com/system/attachments/631/original/ptmono.zip'
-  name 'PT Mono'
-  homepage 'https://www.paratype.com/public/'
+  url "https://company.paratype.com/system/attachments/631/original/ptmono.zip"
+  name "PT Mono"
+  homepage "https://www.paratype.com/public/"
 
-  font 'PTM55F.ttf'
-  font 'PTM75F.ttf'
+  font "PTM55F.ttf"
+  font "PTM75F.ttf"
 end
