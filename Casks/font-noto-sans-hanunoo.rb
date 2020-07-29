5a770cf7905da481b2a49a62251ee09f2254fe5f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:53 +0100

    font-noto-sans-hanunoo.rb: fix for new style

--- Casks/font-noto-sans-hanunoo.rb
+++ Casks/font-noto-sans-hanunoo.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-hanunoo' do
+cask "font-noto-sans-hanunoo" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansHanunoo-unhinted.zip'
-  name 'Noto Sans Hanunoo'
-  homepage 'https://www.google.com/get/noto/#sans-hano'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansHanunoo-unhinted.zip"
+  name "Noto Sans Hanunoo"
+  homepage "https://www.google.com/get/noto/#sans-hano"
 
-  font 'NotoSansHanunoo-Regular.ttf'
+  font "NotoSansHanunoo-Regular.ttf"
 end
