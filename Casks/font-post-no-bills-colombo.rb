e6bf3506721c010e0666475c345e4bee359d6362
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:06 +0100

    font-post-no-bills-colombo.rb: fix for new style

--- Casks/font-post-no-bills-colombo.rb
+++ Casks/font-post-no-bills-colombo.rb
@@ -1,20 +1,20 @@
-cask 'font-post-no-bills-colombo' do
+cask "font-post-no-bills-colombo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/postnobillscolombo',
+  url "https://github.com/google/fonts/trunk/ofl/postnobillscolombo",
       using:      :svn,
       trust_cert: true
-  name 'Post No Bills Colombo'
-  homepage 'https://fonts.google.com/specimen/Post+No+Bills+Colombo'
+  name "Post No Bills Colombo"
+  homepage "https://fonts.google.com/specimen/Post+No+Bills+Colombo"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'PostNoBillsColombo-Bold.ttf'
-  font 'PostNoBillsColombo-ExtraBold.ttf'
-  font 'PostNoBillsColombo-Light.ttf'
-  font 'PostNoBillsColombo-Medium.ttf'
-  font 'PostNoBillsColombo-Regular.ttf'
-  font 'PostNoBillsColombo-SemiBold.ttf'
+  font "PostNoBillsColombo-Bold.ttf"
+  font "PostNoBillsColombo-ExtraBold.ttf"
+  font "PostNoBillsColombo-Light.ttf"
+  font "PostNoBillsColombo-Medium.ttf"
+  font "PostNoBillsColombo-Regular.ttf"
+  font "PostNoBillsColombo-SemiBold.ttf"
 end
