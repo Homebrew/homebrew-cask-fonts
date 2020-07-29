4c92cb93b9e5eba2dd26a26f35c41511752abecb
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:09 +0100

    font-aboriginal-sans.rb: fix for new style

--- Casks/font-aboriginal-sans.rb
+++ Casks/font-aboriginal-sans.rb
@@ -1,13 +1,13 @@
-cask 'font-aboriginal-sans' do
+cask "font-aboriginal-sans" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.languagegeek.com/font/AboriginalSans.zip'
-  name 'LanguageGeek Aboriginal Sans Serif'
-  homepage 'https://www.languagegeek.com/font/fontdownload.html'
+  url "https://www.languagegeek.com/font/AboriginalSans.zip"
+  name "LanguageGeek Aboriginal Sans Serif"
+  homepage "https://www.languagegeek.com/font/fontdownload.html"
 
-  font 'AboriginalSansREGULAR.ttf'
-  font 'AboriginalSansBOLD.ttf'
-  font 'AboriginalSansBOLDITALIC.ttf'
-  font 'AboriginalSansITALIC.ttf'
+  font "AboriginalSansREGULAR.ttf"
+  font "AboriginalSansBOLD.ttf"
+  font "AboriginalSansBOLDITALIC.ttf"
+  font "AboriginalSansITALIC.ttf"
 end
