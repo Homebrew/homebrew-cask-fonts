b01ba995d5498f4235f2d8116ea20a0c64ea3d7d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:11 +0100

    font-homemade-apple.rb: fix for new style

--- Casks/font-homemade-apple.rb
+++ Casks/font-homemade-apple.rb
@@ -1,11 +1,11 @@
-cask 'font-homemade-apple' do
+cask "font-homemade-apple" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/homemadeapple/HomemadeApple-Regular.ttf'
-  name 'Homemade Apple'
-  homepage 'https://fonts.google.com/specimen/Homemade+Apple'
+  url "https://github.com/google/fonts/raw/master/apache/homemadeapple/HomemadeApple-Regular.ttf"
+  name "Homemade Apple"
+  homepage "https://fonts.google.com/specimen/Homemade+Apple"
 
-  font 'HomemadeApple-Regular.ttf'
+  font "HomemadeApple-Regular.ttf"
 end
