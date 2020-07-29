22ace2403c5f249d6abc07048a4788b243f16340
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:14 +0100

    font-im-fell-double-pica.rb: fix for new style

--- Casks/font-im-fell-double-pica.rb
+++ Casks/font-im-fell-double-pica.rb
@@ -1,16 +1,16 @@
-cask 'font-im-fell-double-pica' do
+cask "font-im-fell-double-pica" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/imfelldoublepica',
+  url "https://github.com/google/fonts/trunk/ofl/imfelldoublepica",
       using:      :svn,
       trust_cert: true
-  name 'IM Fell Double Pica'
-  homepage 'https://fonts.google.com/specimen/IM+Fell+Double+Pica'
+  name "IM Fell Double Pica"
+  homepage "https://fonts.google.com/specimen/IM+Fell+Double+Pica"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'IMFELLDoublePica-Italic.ttf'
-  font 'IMFELLDoublePica-Regular.ttf'
+  font "IMFELLDoublePica-Italic.ttf"
+  font "IMFELLDoublePica-Regular.ttf"
 end
