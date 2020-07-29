b7c66b2f6978325f5897c127513b851e9e457ab5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:22 +0100

    font-jf-open-huninn.rb: fix for new style

--- Casks/font-jf-open-huninn.rb
+++ Casks/font-jf-open-huninn.rb
@@ -1,13 +1,13 @@
-cask 'font-jf-open-huninn' do
-  version '1.1'
-  sha256 '1b5a8d0204f5763408609d5e8182455d23a2ebc7450730aed19cc13c619bca37'
+cask "font-jf-open-huninn" do
+  version "1.1"
+  sha256 "1b5a8d0204f5763408609d5e8182455d23a2ebc7450730aed19cc13c619bca37"
 
   # github.com/justfont/open-huninn-font/ was verified as official when first introduced to the cask
   url "https://github.com/justfont/open-huninn-font/releases/download/v#{version}/jf-openhuninn-#{version}.zip"
-  appcast 'https://github.com/justfont/open-huninn-font/releases.atom'
-  name 'jf open huninn'
-  name 'jf open 粉圓'
-  homepage 'https://justfont.com/huninn/'
+  appcast "https://github.com/justfont/open-huninn-font/releases.atom"
+  name "jf open huninn"
+  name "jf open 粉圓"
+  homepage "https://justfont.com/huninn/"
 
   font "jf-openhuninn-#{version}/jf-openhuninn-#{version}.ttf"
 end
