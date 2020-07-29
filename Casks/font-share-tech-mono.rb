46b49cd73cdead6c38bde5025ffe5314f31abac6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:16 +0100

    font-share-tech-mono.rb: fix for new style

--- Casks/font-share-tech-mono.rb
+++ Casks/font-share-tech-mono.rb
@@ -1,11 +1,11 @@
-cask 'font-share-tech-mono' do
+cask "font-share-tech-mono" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/sharetechmono/ShareTechMono-Regular.ttf'
-  name 'Share Tech Mono'
-  homepage 'https://fonts.google.com/specimen/Share+Tech+Mono'
+  url "https://github.com/google/fonts/raw/master/ofl/sharetechmono/ShareTechMono-Regular.ttf"
+  name "Share Tech Mono"
+  homepage "https://fonts.google.com/specimen/Share+Tech+Mono"
 
-  font 'ShareTechMono-Regular.ttf'
+  font "ShareTechMono-Regular.ttf"
 end
