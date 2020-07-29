da4cc06e1892abdebd6aaf6d23832a389e913d40
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:09 +0100

    font-raleway.rb: fix for new style

--- Casks/font-raleway.rb
+++ Casks/font-raleway.rb
@@ -1,16 +1,16 @@
-cask 'font-raleway' do
+cask "font-raleway" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/raleway',
+  url "https://github.com/google/fonts/trunk/ofl/raleway",
       using:      :svn,
       trust_cert: true
-  name 'Raleway'
-  homepage 'https://fonts.google.com/specimen/Raleway'
+  name "Raleway"
+  homepage "https://fonts.google.com/specimen/Raleway"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Raleway-Italic[wght].ttf'
-  font 'Raleway[wght].ttf'
+  font "Raleway-Italic[wght].ttf"
+  font "Raleway[wght].ttf"
 end
