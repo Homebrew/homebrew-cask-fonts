ee8fbcc86fcdd705d994cbf3baad203e1414adfc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:33 +0100

    font-lexend-peta.rb: fix for new style

--- Casks/font-lexend-peta.rb
+++ Casks/font-lexend-peta.rb
@@ -1,11 +1,11 @@
-cask 'font-lexend-peta' do
+cask "font-lexend-peta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/lexendpeta/LexendPeta-Regular.ttf'
-  name 'Lexend Peta'
-  homepage 'https://fonts.google.com/specimen/Lexend+Peta'
+  url "https://github.com/google/fonts/raw/master/ofl/lexendpeta/LexendPeta-Regular.ttf"
+  name "Lexend Peta"
+  homepage "https://fonts.google.com/specimen/Lexend+Peta"
 
-  font 'LexendPeta-Regular.ttf'
+  font "LexendPeta-Regular.ttf"
 end
