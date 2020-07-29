6bc56decb7b00b2eaa5f624d3ec957294b2f9310
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:40 +0100

    font-materialdesignicons-webfont.rb: fix for new style

--- Casks/font-materialdesignicons-webfont.rb
+++ Casks/font-materialdesignicons-webfont.rb
@@ -1,16 +1,16 @@
-cask 'font-materialdesignicons-webfont' do
+cask "font-materialdesignicons-webfont" do
   version :latest
   sha256 :no_check
 
   # github.com/templarian/materialdesign-webfont/ was verified as official when first introduced to the cask
-  url 'https://github.com/templarian/materialdesign-webfont/trunk/fonts',
+  url "https://github.com/templarian/materialdesign-webfont/trunk/fonts",
       using:      :svn,
-      revision:   '50',
+      revision:   "50",
       trust_cert: true
-  name 'Material Design Icons Webfont'
-  homepage 'https://materialdesignicons.com/'
+  name "Material Design Icons Webfont"
+  homepage "https://materialdesignicons.com/"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'materialdesignicons-webfont.ttf'
+  font "materialdesignicons-webfont.ttf"
 end
