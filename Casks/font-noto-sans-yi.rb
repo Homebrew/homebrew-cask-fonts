e9f0e4c71812f7e77bfd916698f857fc22315e77
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:57 +0100

    font-noto-sans-yi.rb: fix for new style

--- Casks/font-noto-sans-yi.rb
+++ Casks/font-noto-sans-yi.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-yi' do
+cask "font-noto-sans-yi" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansYi-unhinted.zip'
-  name 'Noto Sans Yi'
-  homepage 'https://www.google.com/get/noto/#sans-yiii'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansYi-unhinted.zip"
+  name "Noto Sans Yi"
+  homepage "https://www.google.com/get/noto/#sans-yiii"
 
-  font 'NotoSansYi-Regular.ttf'
+  font "NotoSansYi-Regular.ttf"
 end
