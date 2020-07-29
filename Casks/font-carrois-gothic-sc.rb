025a847c91b689a74f38bec4def2fdf867eb74d3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:27 +0100

    font-carrois-gothic-sc.rb: fix for new style

--- Casks/font-carrois-gothic-sc.rb
+++ Casks/font-carrois-gothic-sc.rb
@@ -1,11 +1,11 @@
-cask 'font-carrois-gothic-sc' do
+cask "font-carrois-gothic-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/carroisgothicsc/CarroisGothicSC-Regular.ttf'
-  name 'Carrois Gothic SC'
-  homepage 'https://fonts.google.com/specimen/Carrois+Gothic+SC'
+  url "https://github.com/google/fonts/raw/master/ofl/carroisgothicsc/CarroisGothicSC-Regular.ttf"
+  name "Carrois Gothic SC"
+  homepage "https://fonts.google.com/specimen/Carrois+Gothic+SC"
 
-  font 'CarroisGothicSC-Regular.ttf'
+  font "CarroisGothicSC-Regular.ttf"
 end
