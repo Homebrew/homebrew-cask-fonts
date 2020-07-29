72a6017364b9b4c9454c558056237fbff5a49d8b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:21 +0100

    font-italianno.rb: fix for new style

--- Casks/font-italianno.rb
+++ Casks/font-italianno.rb
@@ -1,11 +1,11 @@
-cask 'font-italianno' do
+cask "font-italianno" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/italianno/Italianno-Regular.ttf'
-  name 'Italianno'
-  homepage 'https://fonts.google.com/specimen/Italianno'
+  url "https://github.com/google/fonts/raw/master/ofl/italianno/Italianno-Regular.ttf"
+  name "Italianno"
+  homepage "https://fonts.google.com/specimen/Italianno"
 
-  font 'Italianno-Regular.ttf'
+  font "Italianno-Regular.ttf"
 end
