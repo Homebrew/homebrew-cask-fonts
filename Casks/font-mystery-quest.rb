6b14c52fd0ab3b1cd3320710ac57a77c33417ba8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:47 +0100

    font-mystery-quest.rb: fix for new style

--- Casks/font-mystery-quest.rb
+++ Casks/font-mystery-quest.rb
@@ -1,11 +1,11 @@
-cask 'font-mystery-quest' do
+cask "font-mystery-quest" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/mysteryquest/MysteryQuest-Regular.ttf'
-  name 'Mystery Quest'
-  homepage 'https://fonts.google.com/specimen/Mystery+Quest'
+  url "https://github.com/google/fonts/raw/master/ofl/mysteryquest/MysteryQuest-Regular.ttf"
+  name "Mystery Quest"
+  homepage "https://fonts.google.com/specimen/Mystery+Quest"
 
-  font 'MysteryQuest-Regular.ttf'
+  font "MysteryQuest-Regular.ttf"
 end
