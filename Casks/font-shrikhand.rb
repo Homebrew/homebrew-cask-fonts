0b03e77b31a3412457d9556eca1ff39516b21232
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:17 +0100

    font-shrikhand.rb: fix for new style

--- Casks/font-shrikhand.rb
+++ Casks/font-shrikhand.rb
@@ -1,11 +1,11 @@
-cask 'font-shrikhand' do
+cask "font-shrikhand" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/shrikhand/Shrikhand-Regular.ttf'
-  name 'Shrikhand'
-  homepage 'https://fonts.google.com/specimen/Shrikhand'
+  url "https://github.com/google/fonts/raw/master/ofl/shrikhand/Shrikhand-Regular.ttf"
+  name "Shrikhand"
+  homepage "https://fonts.google.com/specimen/Shrikhand"
 
-  font 'Shrikhand-Regular.ttf'
+  font "Shrikhand-Regular.ttf"
 end
