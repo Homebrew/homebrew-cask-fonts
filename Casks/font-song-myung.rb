cb74394805b00104d38011987d3bc54cbbff50b8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:18 +0100

    font-song-myung.rb: fix for new style

--- Casks/font-song-myung.rb
+++ Casks/font-song-myung.rb
@@ -1,11 +1,11 @@
-cask 'font-song-myung' do
+cask "font-song-myung" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/songmyung/SongMyung-Regular.ttf'
-  name 'Song Myung'
-  homepage 'https://fonts.google.com/specimen/Song+Myung'
+  url "https://github.com/google/fonts/raw/master/ofl/songmyung/SongMyung-Regular.ttf"
+  name "Song Myung"
+  homepage "https://fonts.google.com/specimen/Song+Myung"
 
-  font 'SongMyung-Regular.ttf'
+  font "SongMyung-Regular.ttf"
 end
