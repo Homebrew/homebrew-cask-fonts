331fe1946db9d1efe9f9d93804a30ba7d9f3f8d7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:49 +0100

    font-niramit.rb: fix for new style

--- Casks/font-niramit.rb
+++ Casks/font-niramit.rb
@@ -1,26 +1,26 @@
-cask 'font-niramit' do
+cask "font-niramit" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/niramit',
+  url "https://github.com/google/fonts/trunk/ofl/niramit",
       using:      :svn,
       trust_cert: true
-  name 'Niramit'
-  homepage 'https://fonts.google.com/specimen/Niramit'
+  name "Niramit"
+  homepage "https://fonts.google.com/specimen/Niramit"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Niramit-Bold.ttf'
-  font 'Niramit-BoldItalic.ttf'
-  font 'Niramit-ExtraLight.ttf'
-  font 'Niramit-ExtraLightItalic.ttf'
-  font 'Niramit-Italic.ttf'
-  font 'Niramit-Light.ttf'
-  font 'Niramit-LightItalic.ttf'
-  font 'Niramit-Medium.ttf'
-  font 'Niramit-MediumItalic.ttf'
-  font 'Niramit-Regular.ttf'
-  font 'Niramit-SemiBold.ttf'
-  font 'Niramit-SemiBoldItalic.ttf'
+  font "Niramit-Bold.ttf"
+  font "Niramit-BoldItalic.ttf"
+  font "Niramit-ExtraLight.ttf"
+  font "Niramit-ExtraLightItalic.ttf"
+  font "Niramit-Italic.ttf"
+  font "Niramit-Light.ttf"
+  font "Niramit-LightItalic.ttf"
+  font "Niramit-Medium.ttf"
+  font "Niramit-MediumItalic.ttf"
+  font "Niramit-Regular.ttf"
+  font "Niramit-SemiBold.ttf"
+  font "Niramit-SemiBoldItalic.ttf"
 end
