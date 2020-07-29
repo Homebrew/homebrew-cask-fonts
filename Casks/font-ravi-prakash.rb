ed6dff8aaf125a5b440da10d2026e7cc93b47d4d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:10 +0100

    font-ravi-prakash.rb: fix for new style

--- Casks/font-ravi-prakash.rb
+++ Casks/font-ravi-prakash.rb
@@ -1,11 +1,11 @@
-cask 'font-ravi-prakash' do
+cask "font-ravi-prakash" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/raviprakash/RaviPrakash-Regular.ttf'
-  name 'Ravi Prakash'
-  homepage 'https://fonts.google.com/specimen/Ravi+Prakash'
+  url "https://github.com/google/fonts/raw/master/ofl/raviprakash/RaviPrakash-Regular.ttf"
+  name "Ravi Prakash"
+  homepage "https://fonts.google.com/specimen/Ravi+Prakash"
 
-  font 'RaviPrakash-Regular.ttf'
+  font "RaviPrakash-Regular.ttf"
 end
