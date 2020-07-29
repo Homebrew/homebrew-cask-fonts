bb42b348ed3983472b3bb5e84febcc6c3cb7398e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:53 +0100

    font-noto-sans-imperial-aramaic.rb: fix for new style

--- Casks/font-noto-sans-imperial-aramaic.rb
+++ Casks/font-noto-sans-imperial-aramaic.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-imperial-aramaic' do
+cask "font-noto-sans-imperial-aramaic" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansImperialAramaic-unhinted.zip'
-  name 'Noto Sans Imperial Aramaic'
-  homepage 'https://www.google.com/get/noto/#sans-armi'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansImperialAramaic-unhinted.zip"
+  name "Noto Sans Imperial Aramaic"
+  homepage "https://www.google.com/get/noto/#sans-armi"
 
-  font 'NotoSansImperialAramaic-Regular.ttf'
+  font "NotoSansImperialAramaic-Regular.ttf"
 end
