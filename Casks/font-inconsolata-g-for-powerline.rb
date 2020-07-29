7a6140161757cb38d9ca5a724872ce9845485a5f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:16 +0100

    font-inconsolata-g-for-powerline.rb: fix for new style

--- Casks/font-inconsolata-g-for-powerline.rb
+++ Casks/font-inconsolata-g-for-powerline.rb
@@ -1,14 +1,14 @@
-cask 'font-inconsolata-g-for-powerline' do
+cask "font-inconsolata-g-for-powerline" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/powerline/fonts/trunk/Inconsolata-g',
+  url "https://github.com/powerline/fonts/trunk/Inconsolata-g",
       using:      :svn,
       trust_cert: true
-  name 'Inconsolata-g for Powerline'
-  homepage 'https://github.com/powerline/fonts/tree/master/Inconsolata-g'
+  name "Inconsolata-g for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/Inconsolata-g"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Inconsolata-g for Powerline.otf'
+  font "Inconsolata-g for Powerline.otf"
 end
