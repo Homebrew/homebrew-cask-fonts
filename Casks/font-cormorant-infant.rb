68b91e459be2b7119f7f088abcab9cbef56631b7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:36 +0100

    font-cormorant-infant.rb: fix for new style

--- Casks/font-cormorant-infant.rb
+++ Casks/font-cormorant-infant.rb
@@ -1,24 +1,24 @@
-cask 'font-cormorant-infant' do
+cask "font-cormorant-infant" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/cormorantinfant',
+  url "https://github.com/google/fonts/trunk/ofl/cormorantinfant",
       using:      :svn,
       trust_cert: true
-  name 'Cormorant Infant'
-  homepage 'https://fonts.google.com/specimen/Cormorant+Infant'
+  name "Cormorant Infant"
+  homepage "https://fonts.google.com/specimen/Cormorant+Infant"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'CormorantInfant-Bold.ttf'
-  font 'CormorantInfant-BoldItalic.ttf'
-  font 'CormorantInfant-Italic.ttf'
-  font 'CormorantInfant-Light.ttf'
-  font 'CormorantInfant-LightItalic.ttf'
-  font 'CormorantInfant-Medium.ttf'
-  font 'CormorantInfant-MediumItalic.ttf'
-  font 'CormorantInfant-Regular.ttf'
-  font 'CormorantInfant-SemiBold.ttf'
-  font 'CormorantInfant-SemiBoldItalic.ttf'
+  font "CormorantInfant-Bold.ttf"
+  font "CormorantInfant-BoldItalic.ttf"
+  font "CormorantInfant-Italic.ttf"
+  font "CormorantInfant-Light.ttf"
+  font "CormorantInfant-LightItalic.ttf"
+  font "CormorantInfant-Medium.ttf"
+  font "CormorantInfant-MediumItalic.ttf"
+  font "CormorantInfant-Regular.ttf"
+  font "CormorantInfant-SemiBold.ttf"
+  font "CormorantInfant-SemiBoldItalic.ttf"
 end
