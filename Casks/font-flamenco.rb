78624037b98fb8e76e2f688df779483a54da5f4f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:55 +0100

    font-flamenco.rb: fix for new style

--- Casks/font-flamenco.rb
+++ Casks/font-flamenco.rb
@@ -1,16 +1,16 @@
-cask 'font-flamenco' do
+cask "font-flamenco" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/flamenco',
+  url "https://github.com/google/fonts/trunk/ofl/flamenco",
       using:      :svn,
       trust_cert: true
-  name 'Flamenco'
-  homepage 'https://fonts.google.com/specimen/Flamenco'
+  name "Flamenco"
+  homepage "https://fonts.google.com/specimen/Flamenco"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Flamenco-Light.ttf'
-  font 'Flamenco-Regular.ttf'
+  font "Flamenco-Light.ttf"
+  font "Flamenco-Regular.ttf"
 end
