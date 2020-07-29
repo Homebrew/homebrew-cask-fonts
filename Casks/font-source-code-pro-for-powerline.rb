e919e5a2056330ab3e7df6a640750e8bb13aef9a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:19 +0100

    font-source-code-pro-for-powerline.rb: fix for new style

--- Casks/font-source-code-pro-for-powerline.rb
+++ Casks/font-source-code-pro-for-powerline.rb
@@ -1,27 +1,27 @@
-cask 'font-source-code-pro-for-powerline' do
+cask "font-source-code-pro-for-powerline" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/powerline/fonts/trunk/SourceCodePro',
+  url "https://github.com/powerline/fonts/trunk/SourceCodePro",
       using:      :svn,
       trust_cert: true
-  name 'Source Code Pro for Powerline'
-  homepage 'https://github.com/powerline/fonts/tree/master/SourceCodePro'
+  name "Source Code Pro for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/SourceCodePro"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Source Code Pro Black for Powerline.otf'
-  font 'Source Code Pro Bold for Powerline.otf'
-  font 'Source Code Pro ExtraLight for Powerline.otf'
-  font 'Source Code Pro Light for Powerline.otf'
-  font 'Source Code Pro Medium for Powerline.otf'
-  font 'Source Code Pro Powerline BlackItalic.otf'
-  font 'Source Code Pro Powerline BoldItalic.otf'
-  font 'Source Code Pro Powerline ExtraLightItalic.otf'
-  font 'Source Code Pro Powerline Italic.otf'
-  font 'Source Code Pro Powerline LightItalic.otf'
-  font 'Source Code Pro Powerline MediumItalic.otf'
-  font 'Source Code Pro Powerline SemiboldItalic.otf'
-  font 'Source Code Pro Semibold for Powerline.otf'
-  font 'Source Code Pro for Powerline.otf'
+  font "Source Code Pro Black for Powerline.otf"
+  font "Source Code Pro Bold for Powerline.otf"
+  font "Source Code Pro ExtraLight for Powerline.otf"
+  font "Source Code Pro Light for Powerline.otf"
+  font "Source Code Pro Medium for Powerline.otf"
+  font "Source Code Pro Powerline BlackItalic.otf"
+  font "Source Code Pro Powerline BoldItalic.otf"
+  font "Source Code Pro Powerline ExtraLightItalic.otf"
+  font "Source Code Pro Powerline Italic.otf"
+  font "Source Code Pro Powerline LightItalic.otf"
+  font "Source Code Pro Powerline MediumItalic.otf"
+  font "Source Code Pro Powerline SemiboldItalic.otf"
+  font "Source Code Pro Semibold for Powerline.otf"
+  font "Source Code Pro for Powerline.otf"
 end
