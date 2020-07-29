da1f9d2baa0c1bc54d1bb8d6f5e2cb59c1c1a594
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:28 +0100

    font-victormono-nerd-font.rb: fix for new style

--- Casks/font-victormono-nerd-font.rb
+++ Casks/font-victormono-nerd-font.rb
@@ -1,52 +1,52 @@
-cask 'font-victormono-nerd-font' do
-  version '2.1.0'
-  sha256 '386d1327671165989cd197a22352098b2c064b51cd27504bcb1cbbf7f814e9a3'
+cask "font-victormono-nerd-font" do
+  version "2.1.0"
+  sha256 "386d1327671165989cd197a22352098b2c064b51cd27504bcb1cbbf7f814e9a3"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/VictorMono.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'VictorMono Nerd Font (Victor Mono)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "VictorMono Nerd Font (Victor Mono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Victor Mono Bold Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Bold Nerd Font Complete.ttf'
-  font 'Victor Mono SemiBold Nerd Font Complete Mono.ttf'
-  font 'Victor Mono SemiBold Nerd Font Complete.ttf'
-  font 'Victor Mono Thin Oblique Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Thin Oblique Nerd Font Complete.ttf'
-  font 'Victor Mono Bold Italic Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Bold Italic Nerd Font Complete.ttf'
-  font 'Victor Mono Light Oblique Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Light Oblique Nerd Font Complete.ttf'
-  font 'Victor Mono Bold Oblique Nerd Font Complete.ttf'
-  font 'Victor Mono Bold Oblique Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Medium Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Medium Nerd Font Complete.ttf'
-  font 'Victor Mono ExtraLight Nerd Font Complete.ttf'
-  font 'Victor Mono ExtraLight Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Medium Oblique Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Medium Oblique Nerd Font Complete.ttf'
-  font 'Victor Mono SemiBold Italic Nerd Font Complete Mono.ttf'
-  font 'Victor Mono SemiBold Italic Nerd Font Complete.ttf'
-  font 'Victor Mono ExtraLight Italic Nerd Font Complete Mono.ttf'
-  font 'Victor Mono ExtraLight Italic Nerd Font Complete.ttf'
-  font 'Victor Mono Regular Nerd Font Complete.ttf'
-  font 'Victor Mono Regular Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Thin Italic Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Thin Italic Nerd Font Complete.ttf'
-  font 'Victor Mono SemiBold Oblique Nerd Font Complete.ttf'
-  font 'Victor Mono SemiBold Oblique Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Thin Nerd Font Complete.ttf'
-  font 'Victor Mono Thin Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Medium Italic Nerd Font Complete.ttf'
-  font 'Victor Mono Medium Italic Nerd Font Complete Mono.ttf'
-  font 'Victor Mono ExtraLight Oblique Nerd Font Complete Mono.ttf'
-  font 'Victor Mono ExtraLight Oblique Nerd Font Complete.ttf'
-  font 'Victor Mono Oblique Nerd Font Complete.ttf'
-  font 'Victor Mono Oblique Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Italic Nerd Font Complete.ttf'
-  font 'Victor Mono Italic Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Light Nerd Font Complete.ttf'
-  font 'Victor Mono Light Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Light Italic Nerd Font Complete Mono.ttf'
-  font 'Victor Mono Light Italic Nerd Font Complete.ttf'
+  font "Victor Mono Bold Nerd Font Complete Mono.ttf"
+  font "Victor Mono Bold Nerd Font Complete.ttf"
+  font "Victor Mono SemiBold Nerd Font Complete Mono.ttf"
+  font "Victor Mono SemiBold Nerd Font Complete.ttf"
+  font "Victor Mono Thin Oblique Nerd Font Complete Mono.ttf"
+  font "Victor Mono Thin Oblique Nerd Font Complete.ttf"
+  font "Victor Mono Bold Italic Nerd Font Complete Mono.ttf"
+  font "Victor Mono Bold Italic Nerd Font Complete.ttf"
+  font "Victor Mono Light Oblique Nerd Font Complete Mono.ttf"
+  font "Victor Mono Light Oblique Nerd Font Complete.ttf"
+  font "Victor Mono Bold Oblique Nerd Font Complete.ttf"
+  font "Victor Mono Bold Oblique Nerd Font Complete Mono.ttf"
+  font "Victor Mono Medium Nerd Font Complete Mono.ttf"
+  font "Victor Mono Medium Nerd Font Complete.ttf"
+  font "Victor Mono ExtraLight Nerd Font Complete.ttf"
+  font "Victor Mono ExtraLight Nerd Font Complete Mono.ttf"
+  font "Victor Mono Medium Oblique Nerd Font Complete Mono.ttf"
+  font "Victor Mono Medium Oblique Nerd Font Complete.ttf"
+  font "Victor Mono SemiBold Italic Nerd Font Complete Mono.ttf"
+  font "Victor Mono SemiBold Italic Nerd Font Complete.ttf"
+  font "Victor Mono ExtraLight Italic Nerd Font Complete Mono.ttf"
+  font "Victor Mono ExtraLight Italic Nerd Font Complete.ttf"
+  font "Victor Mono Regular Nerd Font Complete.ttf"
+  font "Victor Mono Regular Nerd Font Complete Mono.ttf"
+  font "Victor Mono Thin Italic Nerd Font Complete Mono.ttf"
+  font "Victor Mono Thin Italic Nerd Font Complete.ttf"
+  font "Victor Mono SemiBold Oblique Nerd Font Complete.ttf"
+  font "Victor Mono SemiBold Oblique Nerd Font Complete Mono.ttf"
+  font "Victor Mono Thin Nerd Font Complete.ttf"
+  font "Victor Mono Thin Nerd Font Complete Mono.ttf"
+  font "Victor Mono Medium Italic Nerd Font Complete.ttf"
+  font "Victor Mono Medium Italic Nerd Font Complete Mono.ttf"
+  font "Victor Mono ExtraLight Oblique Nerd Font Complete Mono.ttf"
+  font "Victor Mono ExtraLight Oblique Nerd Font Complete.ttf"
+  font "Victor Mono Oblique Nerd Font Complete.ttf"
+  font "Victor Mono Oblique Nerd Font Complete Mono.ttf"
+  font "Victor Mono Italic Nerd Font Complete.ttf"
+  font "Victor Mono Italic Nerd Font Complete Mono.ttf"
+  font "Victor Mono Light Nerd Font Complete.ttf"
+  font "Victor Mono Light Nerd Font Complete Mono.ttf"
+  font "Victor Mono Light Italic Nerd Font Complete Mono.ttf"
+  font "Victor Mono Light Italic Nerd Font Complete.ttf"
 end
