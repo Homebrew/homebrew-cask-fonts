4ad0bb96fe16569d7a11a03c8349903db7e436c6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:27 +0100

    font-unifrakturcook.rb: fix for new style

--- Casks/font-unifrakturcook.rb
+++ Casks/font-unifrakturcook.rb
@@ -1,11 +1,11 @@
-cask 'font-unifrakturcook' do
+cask "font-unifrakturcook" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/unifrakturcook/UnifrakturCook-Bold.ttf'
-  name 'UnifrakturCook'
-  homepage 'https://fonts.google.com/specimen/UnifrakturCook'
+  url "https://github.com/google/fonts/raw/master/ofl/unifrakturcook/UnifrakturCook-Bold.ttf"
+  name "UnifrakturCook"
+  homepage "https://fonts.google.com/specimen/UnifrakturCook"
 
-  font 'UnifrakturCook-Bold.ttf'
+  font "UnifrakturCook-Bold.ttf"
 end
