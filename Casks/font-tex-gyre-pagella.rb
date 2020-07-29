4c91284c063b73d86e2505cf7877ac7a03949324
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:24 +0100

    font-tex-gyre-pagella.rb: fix for new style

--- Casks/font-tex-gyre-pagella.rb
+++ Casks/font-tex-gyre-pagella.rb
@@ -1,10 +1,10 @@
-cask 'font-tex-gyre-pagella' do
-  version '2.501'
-  sha256 'e16078d19860d68a54bcaedc00e35c2a81b396cdc36842700f1d556f89cf8ef2'
+cask "font-tex-gyre-pagella" do
+  version "2.501"
+  sha256 "e16078d19860d68a54bcaedc00e35c2a81b396cdc36842700f1d556f89cf8ef2"
 
   url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella/qpl#{version.dots_to_underscores}otf.zip"
-  name 'TeX Gyre Pagella'
-  homepage 'http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella'
+  name "TeX Gyre Pagella"
+  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/pagella"
 
   font "qpl#{version.dots_to_underscores}otf/texgyrepagella-bold.otf"
   font "qpl#{version.dots_to_underscores}otf/texgyrepagella-bolditalic.otf"
