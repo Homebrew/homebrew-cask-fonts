bc54e1b235865ad158aed5d69a875f9833e984e9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:55 +0100

    font-noto-sans-phags-pa.rb: fix for new style

--- Casks/font-noto-sans-phags-pa.rb
+++ Casks/font-noto-sans-phags-pa.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-phags-pa' do
+cask "font-noto-sans-phags-pa" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansPhagsPa-unhinted.zip'
-  name 'Noto Sans Phags Pa'
-  homepage 'https://www.google.com/get/noto/#sans-phag'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansPhagsPa-unhinted.zip"
+  name "Noto Sans Phags Pa"
+  homepage "https://www.google.com/get/noto/#sans-phag"
 
-  font 'NotoSansPhagsPa-Regular.ttf'
+  font "NotoSansPhagsPa-Regular.ttf"
 end
