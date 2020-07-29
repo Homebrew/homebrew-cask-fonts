c452caa9fea59a88feac0b29ed4295acbf7c7259
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:51 +0100

    font-noto-sans-carian.rb: fix for new style

--- Casks/font-noto-sans-carian.rb
+++ Casks/font-noto-sans-carian.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-carian' do
+cask "font-noto-sans-carian" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCarian-unhinted.zip'
-  name 'Noto Sans Carian'
-  homepage 'https://www.google.com/get/noto/#sans-cari'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCarian-unhinted.zip"
+  name "Noto Sans Carian"
+  homepage "https://www.google.com/get/noto/#sans-cari"
 
-  font 'NotoSansCarian-Regular.ttf'
+  font "NotoSansCarian-Regular.ttf"
 end
