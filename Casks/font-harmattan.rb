5ad32b6a95419db8b90a02f05fe8f6bf6c8de9f1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:09 +0100

    font-harmattan.rb: fix for new style

--- Casks/font-harmattan.rb
+++ Casks/font-harmattan.rb
@@ -1,11 +1,11 @@
-cask 'font-harmattan' do
+cask "font-harmattan" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/harmattan/Harmattan-Regular.ttf'
-  name 'Harmattan'
-  homepage 'https://fonts.google.com/specimen/Harmattan'
+  url "https://github.com/google/fonts/raw/master/ofl/harmattan/Harmattan-Regular.ttf"
+  name "Harmattan"
+  homepage "https://fonts.google.com/specimen/Harmattan"
 
-  font 'Harmattan-Regular.ttf'
+  font "Harmattan-Regular.ttf"
 end
