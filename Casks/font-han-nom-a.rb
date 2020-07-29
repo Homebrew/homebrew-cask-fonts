5d99665b478c120b5c3190884aad32ee60ce6f93
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:08 +0100

    font-han-nom-a.rb: fix for new style

--- Casks/font-han-nom-a.rb
+++ Casks/font-han-nom-a.rb
@@ -1,12 +1,12 @@
-cask 'font-han-nom-a' do
-  version '2005'
-  sha256 '428a31ff62e26dc33080789f8d49c39ac529dbfdbcfd50825f78cbd0903a3936'
+cask "font-han-nom-a" do
+  version "2005"
+  sha256 "428a31ff62e26dc33080789f8d49c39ac529dbfdbcfd50825f78cbd0903a3936"
 
-  url 'https://downloads.sourceforge.net/vietunicode/hannomH.zip'
-  appcast 'https://sourceforge.net/projects/vietunicode/rss'
-  name 'HAN NOM A'
-  homepage 'https://sourceforge.net/projects/vietunicode/files/hannom/hannom%20v2005/'
+  url "https://downloads.sourceforge.net/vietunicode/hannomH.zip"
+  appcast "https://sourceforge.net/projects/vietunicode/rss"
+  name "HAN NOM A"
+  homepage "https://sourceforge.net/projects/vietunicode/files/hannom/hannom%20v2005/"
 
-  font 'HAN NOM A.ttf'
-  font 'HAN NOM B.ttf'
+  font "HAN NOM A.ttf"
+  font "HAN NOM B.ttf"
 end
