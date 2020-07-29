dae52df3a515be0e672ae615aa2ec893b58733e6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:43 +0100

    font-mirza.rb: fix for new style

--- Casks/font-mirza.rb
+++ Casks/font-mirza.rb
@@ -1,18 +1,18 @@
-cask 'font-mirza' do
+cask "font-mirza" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/mirza',
+  url "https://github.com/google/fonts/trunk/ofl/mirza",
       using:      :svn,
       trust_cert: true
-  name 'Mirza'
-  homepage 'https://fonts.google.com/specimen/Mirza'
+  name "Mirza"
+  homepage "https://fonts.google.com/specimen/Mirza"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Mirza-Bold.ttf'
-  font 'Mirza-Medium.ttf'
-  font 'Mirza-Regular.ttf'
-  font 'Mirza-SemiBold.ttf'
+  font "Mirza-Bold.ttf"
+  font "Mirza-Medium.ttf"
+  font "Mirza-Regular.ttf"
+  font "Mirza-SemiBold.ttf"
 end
