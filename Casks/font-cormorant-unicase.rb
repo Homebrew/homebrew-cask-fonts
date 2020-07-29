edec628b8688c49eb428d2a8451aa55492828944
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:36 +0100

    font-cormorant-unicase.rb: fix for new style

--- Casks/font-cormorant-unicase.rb
+++ Casks/font-cormorant-unicase.rb
@@ -1,19 +1,19 @@
-cask 'font-cormorant-unicase' do
+cask "font-cormorant-unicase" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/cormorantunicase',
+  url "https://github.com/google/fonts/trunk/ofl/cormorantunicase",
       using:      :svn,
       trust_cert: true
-  name 'Cormorant Unicase'
-  homepage 'https://fonts.google.com/specimen/Cormorant+Unicase'
+  name "Cormorant Unicase"
+  homepage "https://fonts.google.com/specimen/Cormorant+Unicase"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'CormorantUnicase-Bold.ttf'
-  font 'CormorantUnicase-Light.ttf'
-  font 'CormorantUnicase-Medium.ttf'
-  font 'CormorantUnicase-Regular.ttf'
-  font 'CormorantUnicase-SemiBold.ttf'
+  font "CormorantUnicase-Bold.ttf"
+  font "CormorantUnicase-Light.ttf"
+  font "CormorantUnicase-Medium.ttf"
+  font "CormorantUnicase-Regular.ttf"
+  font "CormorantUnicase-SemiBold.ttf"
 end
