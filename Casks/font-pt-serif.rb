146cd877bf3b660ed5c7e24d947a7cb532998378
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:08 +0100

    font-pt-serif.rb: fix for new style

--- Casks/font-pt-serif.rb
+++ Casks/font-pt-serif.rb
@@ -1,15 +1,15 @@
-cask 'font-pt-serif' do
+cask "font-pt-serif" do
   version :latest
   sha256 :no_check
 
-  url 'https://company.paratype.com/system/attachments/634/original/ptserif.zip'
-  name 'PT Serif'
-  homepage 'https://www.paratype.com/public/'
+  url "https://company.paratype.com/system/attachments/634/original/ptserif.zip"
+  name "PT Serif"
+  homepage "https://www.paratype.com/public/"
 
-  font 'PTF55F.ttf'
-  font 'PTF56F.ttf'
-  font 'PTF75F.ttf'
-  font 'PTF76F.ttf'
-  font 'PTZ55F.ttf'
-  font 'PTZ56F.ttf'
+  font "PTF55F.ttf"
+  font "PTF56F.ttf"
+  font "PTF75F.ttf"
+  font "PTF76F.ttf"
+  font "PTZ55F.ttf"
+  font "PTZ56F.ttf"
 end
