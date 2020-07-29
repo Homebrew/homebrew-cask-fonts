99184d104fc73ba8b004e22c19e9234c734ab3e7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:22 +0100

    font-boogaloo.rb: fix for new style

--- Casks/font-boogaloo.rb
+++ Casks/font-boogaloo.rb
@@ -1,11 +1,11 @@
-cask 'font-boogaloo' do
+cask "font-boogaloo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/boogaloo/Boogaloo-Regular.ttf'
-  name 'Boogaloo'
-  homepage 'https://fonts.google.com/specimen/Boogaloo'
+  url "https://github.com/google/fonts/raw/master/ofl/boogaloo/Boogaloo-Regular.ttf"
+  name "Boogaloo"
+  homepage "https://fonts.google.com/specimen/Boogaloo"
 
-  font 'Boogaloo-Regular.ttf'
+  font "Boogaloo-Regular.ttf"
 end
