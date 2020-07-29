4e73f4ee768e8860776f9e1b52f728043b2bc94f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:18 +0100

    font-bentham.rb: fix for new style

--- Casks/font-bentham.rb
+++ Casks/font-bentham.rb
@@ -1,11 +1,11 @@
-cask 'font-bentham' do
+cask "font-bentham" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bentham/Bentham-Regular.ttf'
-  name 'Bentham'
-  homepage 'https://fonts.google.com/specimen/Bentham'
+  url "https://github.com/google/fonts/raw/master/ofl/bentham/Bentham-Regular.ttf"
+  name "Bentham"
+  homepage "https://fonts.google.com/specimen/Bentham"
 
-  font 'Bentham-Regular.ttf'
+  font "Bentham-Regular.ttf"
 end
