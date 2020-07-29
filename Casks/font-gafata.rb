60ef65c16c35dbf652265fcbd51a9827fa0a7f62
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:58 +0100

    font-gafata.rb: fix for new style

--- Casks/font-gafata.rb
+++ Casks/font-gafata.rb
@@ -1,11 +1,11 @@
-cask 'font-gafata' do
+cask "font-gafata" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/gafata/Gafata-Regular.ttf'
-  name 'Gafata'
-  homepage 'https://fonts.google.com/specimen/Gafata'
+  url "https://github.com/google/fonts/raw/master/ofl/gafata/Gafata-Regular.ttf"
+  name "Gafata"
+  homepage "https://fonts.google.com/specimen/Gafata"
 
-  font 'Gafata-Regular.ttf'
+  font "Gafata-Regular.ttf"
 end
