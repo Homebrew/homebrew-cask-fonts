5e6870bafd9703da67c4baa6895502e504530be8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:16 +0100

    font-seoul-namsan.rb: fix for new style

--- Casks/font-seoul-namsan.rb
+++ Casks/font-seoul-namsan.rb
@@ -1,18 +1,18 @@
-cask 'font-seoul-namsan' do
+cask "font-seoul-namsan" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/seoulnamsan',
+  url "https://github.com/google/fonts/trunk/ofl/seoulnamsan",
       using:      :svn,
       trust_cert: true
-  name 'Seoul Namsan'
-  homepage 'https://fonts.google.com/specimen/Seoul+Namsan'
+  name "Seoul Namsan"
+  homepage "https://fonts.google.com/specimen/Seoul+Namsan"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'SeoulNamsan-Bold.ttf'
-  font 'SeoulNamsan-ExtraBold.ttf'
-  font 'SeoulNamsan-Light.ttf'
-  font 'SeoulNamsan-Medium.ttf'
+  font "SeoulNamsan-Bold.ttf"
+  font "SeoulNamsan-ExtraBold.ttf"
+  font "SeoulNamsan-Light.ttf"
+  font "SeoulNamsan-Medium.ttf"
 end
