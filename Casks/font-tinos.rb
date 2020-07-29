2a076b6ddc42c74e5925cf086c543b85f316b6e6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:25 +0100

    font-tinos.rb: fix for new style

--- Casks/font-tinos.rb
+++ Casks/font-tinos.rb
@@ -1,18 +1,18 @@
-cask 'font-tinos' do
+cask "font-tinos" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/apache/tinos',
+  url "https://github.com/google/fonts/trunk/apache/tinos",
       using:      :svn,
       trust_cert: true
-  name 'Tinos'
-  homepage 'https://fonts.google.com/specimen/Tinos'
+  name "Tinos"
+  homepage "https://fonts.google.com/specimen/Tinos"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Tinos-Bold.ttf'
-  font 'Tinos-BoldItalic.ttf'
-  font 'Tinos-Italic.ttf'
-  font 'Tinos-Regular.ttf'
+  font "Tinos-Bold.ttf"
+  font "Tinos-BoldItalic.ttf"
+  font "Tinos-Italic.ttf"
+  font "Tinos-Regular.ttf"
 end
