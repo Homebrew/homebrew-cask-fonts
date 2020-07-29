0c324d6beb409d8932eace981a32d26a3224b418
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:15 +0100

    font-sedgwick-ave-display.rb: fix for new style

--- Casks/font-sedgwick-ave-display.rb
+++ Casks/font-sedgwick-ave-display.rb
@@ -1,11 +1,11 @@
-cask 'font-sedgwick-ave-display' do
+cask "font-sedgwick-ave-display" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/sedgwickavedisplay/SedgwickAveDisplay-Regular.ttf'
-  name 'Sedgwick Ave Display'
-  homepage 'https://fonts.google.com/specimen/Sedgwick+Ave+Display'
+  url "https://github.com/google/fonts/raw/master/ofl/sedgwickavedisplay/SedgwickAveDisplay-Regular.ttf"
+  name "Sedgwick Ave Display"
+  homepage "https://fonts.google.com/specimen/Sedgwick+Ave+Display"
 
-  font 'SedgwickAveDisplay-Regular.ttf'
+  font "SedgwickAveDisplay-Regular.ttf"
 end
