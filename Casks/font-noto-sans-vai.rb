92c722a0b5055b22df05718940f1e1ea4eb84bc3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:57 +0100

    font-noto-sans-vai.rb: fix for new style

--- Casks/font-noto-sans-vai.rb
+++ Casks/font-noto-sans-vai.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-vai' do
+cask "font-noto-sans-vai" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansVai-unhinted.zip'
-  name 'Noto Sans Vai'
-  homepage 'https://www.google.com/get/noto/#sans-vaii'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansVai-unhinted.zip"
+  name "Noto Sans Vai"
+  homepage "https://www.google.com/get/noto/#sans-vaii"
 
-  font 'NotoSansVai-Regular.ttf'
+  font "NotoSansVai-Regular.ttf"
 end
