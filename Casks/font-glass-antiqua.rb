ca750daa4689b8b6542a3e5eaed33b9b25bbe66d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:02 +0100

    font-glass-antiqua.rb: fix for new style

--- Casks/font-glass-antiqua.rb
+++ Casks/font-glass-antiqua.rb
@@ -1,11 +1,11 @@
-cask 'font-glass-antiqua' do
+cask "font-glass-antiqua" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/glassantiqua/GlassAntiqua-Regular.ttf'
-  name 'Glass Antiqua'
-  homepage 'https://fonts.google.com/specimen/Glass+Antiqua'
+  url "https://github.com/google/fonts/raw/master/ofl/glassantiqua/GlassAntiqua-Regular.ttf"
+  name "Glass Antiqua"
+  homepage "https://fonts.google.com/specimen/Glass+Antiqua"
 
-  font 'GlassAntiqua-Regular.ttf'
+  font "GlassAntiqua-Regular.ttf"
 end
