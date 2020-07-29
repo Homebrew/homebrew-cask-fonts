d7d0b278b14fccf7731b247dc64161165681c54a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:24 +0100

    font-butcherman.rb: fix for new style

--- Casks/font-butcherman.rb
+++ Casks/font-butcherman.rb
@@ -1,11 +1,11 @@
-cask 'font-butcherman' do
+cask "font-butcherman" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/butcherman/Butcherman-Regular.ttf'
-  name 'Butcherman'
-  homepage 'https://fonts.google.com/specimen/Butcherman'
+  url "https://github.com/google/fonts/raw/master/ofl/butcherman/Butcherman-Regular.ttf"
+  name "Butcherman"
+  homepage "https://fonts.google.com/specimen/Butcherman"
 
-  font 'Butcherman-Regular.ttf'
+  font "Butcherman-Regular.ttf"
 end
