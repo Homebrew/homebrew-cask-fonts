9727f5a83e6a1e343359a2959bf0e8e843fc2442
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:25 +0100

    font-junge.rb: fix for new style

--- Casks/font-junge.rb
+++ Casks/font-junge.rb
@@ -1,11 +1,11 @@
-cask 'font-junge' do
+cask "font-junge" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/junge/Junge-Regular.ttf'
-  name 'Junge'
-  homepage 'https://fonts.google.com/specimen/Junge'
+  url "https://github.com/google/fonts/raw/master/ofl/junge/Junge-Regular.ttf"
+  name "Junge"
+  homepage "https://fonts.google.com/specimen/Junge"
 
-  font 'Junge-Regular.ttf'
+  font "Junge-Regular.ttf"
 end
