b354a437e3bf04f121c0a71cfd5047e6596acd5b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:24 +0100

    font-bungee.rb: fix for new style

--- Casks/font-bungee.rb
+++ Casks/font-bungee.rb
@@ -1,26 +1,26 @@
-cask 'font-bungee' do
-  version '1.1.0'
-  sha256 'd012a9e6293201c3165feba64342d29c42bb4e67b1cc66e07509c12bab760a6f'
+cask "font-bungee" do
+  version "1.1.0"
+  sha256 "d012a9e6293201c3165feba64342d29c42bb4e67b1cc66e07509c12bab760a6f"
 
   # github.com/djrrb/bungee/ was verified as official when first introduced to the cask
   url "https://github.com/djrrb/bungee/releases/download/#{version}/Bungee-fonts.zip"
-  appcast 'https://github.com/djrrb/bungee/releases.atom'
-  name 'Bungee'
-  homepage 'https://djr.com/bungee/'
+  appcast "https://github.com/djrrb/bungee/releases.atom"
+  name "Bungee"
+  homepage "https://djr.com/bungee/"
 
-  font 'fonts/Bungee_Color_Fonts/BungeeColor-Regular_sbix_MacOS.ttf'
-  font 'fonts/Bungee_Color_Fonts/BungeeColor-Regular_svg.ttf'
-  font 'fonts/Bungee_Desktop/Bungee/Bungee-Hairline.otf'
-  font 'fonts/Bungee_Desktop/Bungee/Bungee-Inline.otf'
-  font 'fonts/Bungee_Desktop/Bungee/Bungee-Outline.otf'
-  font 'fonts/Bungee_Desktop/Bungee/Bungee-Regular.otf'
-  font 'fonts/Bungee_Desktop/Bungee/Bungee-Shade.otf'
-  font 'fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Inline.otf'
-  font 'fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Outline.otf'
-  font 'fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Regular.otf'
-  font 'fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Shade.otf'
-  font 'fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Inline.otf'
-  font 'fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Outline.otf'
-  font 'fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Regular.otf'
-  font 'fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Shade.otf'
+  font "fonts/Bungee_Color_Fonts/BungeeColor-Regular_sbix_MacOS.ttf"
+  font "fonts/Bungee_Color_Fonts/BungeeColor-Regular_svg.ttf"
+  font "fonts/Bungee_Desktop/Bungee/Bungee-Hairline.otf"
+  font "fonts/Bungee_Desktop/Bungee/Bungee-Inline.otf"
+  font "fonts/Bungee_Desktop/Bungee/Bungee-Outline.otf"
+  font "fonts/Bungee_Desktop/Bungee/Bungee-Regular.otf"
+  font "fonts/Bungee_Desktop/Bungee/Bungee-Shade.otf"
+  font "fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Inline.otf"
+  font "fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Outline.otf"
+  font "fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Regular.otf"
+  font "fonts/Bungee_Desktop/BungeeLayers/BungeeLayers-Shade.otf"
+  font "fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Inline.otf"
+  font "fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Outline.otf"
+  font "fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Regular.otf"
+  font "fonts/Bungee_Desktop/BungeeLayersRotated/BungeeLayersRotated-Shade.otf"
 end
