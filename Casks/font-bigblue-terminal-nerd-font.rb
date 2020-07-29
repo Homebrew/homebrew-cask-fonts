bb182937fe752657300e5038cce91f5c1fb6a2e2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:19 +0100

    font-bigblue-terminal-nerd-font.rb: fix for new style

--- Casks/font-bigblue-terminal-nerd-font.rb
+++ Casks/font-bigblue-terminal-nerd-font.rb
@@ -1,14 +1,14 @@
-cask 'font-bigblue-terminal-nerd-font' do
-  version '2.1.0'
-  sha256 '5b34861f23af88a66f1c77e0d382128e71168dd05ca553f33aa76bd94515bc7a'
+cask "font-bigblue-terminal-nerd-font" do
+  version "2.1.0"
+  sha256 "5b34861f23af88a66f1c77e0d382128e71168dd05ca553f33aa76bd94515bc7a"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BigBlueTerminal.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'BigBlue_Terminal Nerd Font families (BigBlue Terminal)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "BigBlue_Terminal Nerd Font families (BigBlue Terminal)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'BigBlue TerminalPlus Nerd Font Complete.TTF'
-  font 'BigBlue Terminal 437TT Nerd Font Complete.TTF'
-  font 'BigBlue TerminalPlus Nerd Font Complete Mono.TTF'
-  font 'BigBlue Terminal 437TT Nerd Font Complete Mono.TTF'
+  font "BigBlue TerminalPlus Nerd Font Complete.TTF"
+  font "BigBlue Terminal 437TT Nerd Font Complete.TTF"
+  font "BigBlue TerminalPlus Nerd Font Complete Mono.TTF"
+  font "BigBlue Terminal 437TT Nerd Font Complete Mono.TTF"
 end
