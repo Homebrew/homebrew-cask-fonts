43a864ed55edd256855f4567c36542c42aa58de9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:18 +0100

    font-benchnine.rb: fix for new style

--- Casks/font-benchnine.rb
+++ Casks/font-benchnine.rb
@@ -1,17 +1,17 @@
-cask 'font-benchnine' do
+cask "font-benchnine" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/benchnine',
+  url "https://github.com/google/fonts/trunk/ofl/benchnine",
       using:      :svn,
       trust_cert: true
-  name 'BenchNine'
-  homepage 'https://fonts.google.com/specimen/BenchNine'
+  name "BenchNine"
+  homepage "https://fonts.google.com/specimen/BenchNine"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'BenchNine-Bold.ttf'
-  font 'BenchNine-Light.ttf'
-  font 'BenchNine-Regular.ttf'
+  font "BenchNine-Bold.ttf"
+  font "BenchNine-Light.ttf"
+  font "BenchNine-Regular.ttf"
 end
