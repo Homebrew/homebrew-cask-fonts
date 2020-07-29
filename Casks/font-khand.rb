f6915cf5b24fff9104fe0a8220841f4758290f0a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:28 +0100

    font-khand.rb: fix for new style

--- Casks/font-khand.rb
+++ Casks/font-khand.rb
@@ -1,19 +1,19 @@
-cask 'font-khand' do
+cask "font-khand" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/khand',
+  url "https://github.com/google/fonts/trunk/ofl/khand",
       using:      :svn,
       trust_cert: true
-  name 'Khand'
-  homepage 'https://fonts.google.com/specimen/Khand'
+  name "Khand"
+  homepage "https://fonts.google.com/specimen/Khand"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Khand-Bold.ttf'
-  font 'Khand-Light.ttf'
-  font 'Khand-Medium.ttf'
-  font 'Khand-Regular.ttf'
-  font 'Khand-SemiBold.ttf'
+  font "Khand-Bold.ttf"
+  font "Khand-Light.ttf"
+  font "Khand-Medium.ttf"
+  font "Khand-Regular.ttf"
+  font "Khand-SemiBold.ttf"
 end
