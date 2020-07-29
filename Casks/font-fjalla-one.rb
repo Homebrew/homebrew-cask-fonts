c9bfd6295e612a24f0a5f1c524cfee4815f94e52
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:54 +0100

    font-fjalla-one.rb: fix for new style

--- Casks/font-fjalla-one.rb
+++ Casks/font-fjalla-one.rb
@@ -1,11 +1,11 @@
-cask 'font-fjalla-one' do
+cask "font-fjalla-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/fjallaone/FjallaOne-Regular.ttf'
-  name 'Fjalla One'
-  homepage 'https://fonts.google.com/specimen/Fjalla+One'
+  url "https://github.com/google/fonts/raw/master/ofl/fjallaone/FjallaOne-Regular.ttf"
+  name "Fjalla One"
+  homepage "https://fonts.google.com/specimen/Fjalla+One"
 
-  font 'FjallaOne-Regular.ttf'
+  font "FjallaOne-Regular.ttf"
 end
