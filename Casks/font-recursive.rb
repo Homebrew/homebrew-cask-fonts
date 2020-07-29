3f60d0e11f72d3c04d1dcce7d8bd55fab8bb3a00
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:10 +0100

    font-recursive.rb: fix for new style

--- Casks/font-recursive.rb
+++ Casks/font-recursive.rb
@@ -1,11 +1,11 @@
-cask 'font-recursive' do
+cask "font-recursive" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/recursive/Recursive%5BCASL%2CCRSV%2CMONO%2Cslnt%2Cwght%5D.ttf'
-  name 'Recursive'
-  homepage 'https://fonts.google.com/specimen/Recursive'
+  url "https://github.com/google/fonts/raw/master/ofl/recursive/Recursive%5BCASL%2CCRSV%2CMONO%2Cslnt%2Cwght%5D.ttf"
+  name "Recursive"
+  homepage "https://fonts.google.com/specimen/Recursive"
 
-  font 'Recursive[CASL,CRSV,MONO,slnt,wght].ttf'
+  font "Recursive[CASL,CRSV,MONO,slnt,wght].ttf"
 end
