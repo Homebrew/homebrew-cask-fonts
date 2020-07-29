3f3e667f2790f543384fdf6c47b6738fc4148da9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:42 +0100

    font-average-sans.rb: fix for new style

--- Casks/font-average-sans.rb
+++ Casks/font-average-sans.rb
@@ -1,11 +1,11 @@
-cask 'font-average-sans' do
+cask "font-average-sans" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/averagesans/AverageSans-Regular.ttf'
-  name 'Average Sans'
-  homepage 'https://fonts.google.com/specimen/Average+Sans'
+  url "https://github.com/google/fonts/raw/master/ofl/averagesans/AverageSans-Regular.ttf"
+  name "Average Sans"
+  homepage "https://fonts.google.com/specimen/Average+Sans"
 
-  font 'AverageSans-Regular.ttf'
+  font "AverageSans-Regular.ttf"
 end
