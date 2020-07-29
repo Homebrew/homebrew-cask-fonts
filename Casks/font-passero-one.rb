33eddaff62006169f0da154490712527cfdfd962
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:03 +0100

    font-passero-one.rb: fix for new style

--- Casks/font-passero-one.rb
+++ Casks/font-passero-one.rb
@@ -1,11 +1,11 @@
-cask 'font-passero-one' do
+cask "font-passero-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/passeroone/PasseroOne-Regular.ttf'
-  name 'Passero One'
-  homepage 'https://fonts.google.com/specimen/Passero+One'
+  url "https://github.com/google/fonts/raw/master/ofl/passeroone/PasseroOne-Regular.ttf"
+  name "Passero One"
+  homepage "https://fonts.google.com/specimen/Passero+One"
 
-  font 'PasseroOne-Regular.ttf'
+  font "PasseroOne-Regular.ttf"
 end
