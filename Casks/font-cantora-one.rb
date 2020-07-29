845d43370f4a7e8bd3c4931bc76d398238a7df91
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:27 +0100

    font-cantora-one.rb: fix for new style

--- Casks/font-cantora-one.rb
+++ Casks/font-cantora-one.rb
@@ -1,11 +1,11 @@
-cask 'font-cantora-one' do
+cask "font-cantora-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/cantoraone/CantoraOne-Regular.ttf'
-  name 'Cantora One'
-  homepage 'https://fonts.google.com/specimen/Cantora+One'
+  url "https://github.com/google/fonts/raw/master/ofl/cantoraone/CantoraOne-Regular.ttf"
+  name "Cantora One"
+  homepage "https://fonts.google.com/specimen/Cantora+One"
 
-  font 'CantoraOne-Regular.ttf'
+  font "CantoraOne-Regular.ttf"
 end
