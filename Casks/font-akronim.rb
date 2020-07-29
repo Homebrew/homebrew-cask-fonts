780f8f0ba6877e240d657be7143268c51914195d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:32:02 +0100

    font-akronim.rb: fix for new style

--- Casks/font-akronim.rb
+++ Casks/font-akronim.rb
@@ -1,11 +1,11 @@
-cask 'font-akronim' do
+cask "font-akronim" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/akronim/Akronim-Regular.ttf'
-  name 'Akronim'
-  homepage 'https://fonts.google.com/specimen/Akronim'
+  url "https://github.com/google/fonts/raw/master/ofl/akronim/Akronim-Regular.ttf"
+  name "Akronim"
+  homepage "https://fonts.google.com/specimen/Akronim"
 
-  font 'Akronim-Regular.ttf'
+  font "Akronim-Regular.ttf"
 end
