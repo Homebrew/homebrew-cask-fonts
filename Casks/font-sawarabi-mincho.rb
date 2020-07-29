ff0e92f4b8593a76b0a7e1faaea1b7276401cfc2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:15 +0100

    font-sawarabi-mincho.rb: fix for new style

--- Casks/font-sawarabi-mincho.rb
+++ Casks/font-sawarabi-mincho.rb
@@ -1,11 +1,11 @@
-cask 'font-sawarabi-mincho' do
+cask "font-sawarabi-mincho" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/sawarabimincho/SawarabiMincho-Regular.ttf'
-  name 'Sawarabi Mincho'
-  homepage 'https://fonts.google.com/specimen/Sawarabi+Mincho'
+  url "https://github.com/google/fonts/raw/master/ofl/sawarabimincho/SawarabiMincho-Regular.ttf"
+  name "Sawarabi Mincho"
+  homepage "https://fonts.google.com/specimen/Sawarabi+Mincho"
 
-  font 'SawarabiMincho-Regular.ttf'
+  font "SawarabiMincho-Regular.ttf"
 end
