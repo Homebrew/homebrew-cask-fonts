069a663b6c1973e7e44f049d3a492f1b2aff4a83
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:30 +0100

    font-antic-didone.rb: fix for new style

--- Casks/font-antic-didone.rb
+++ Casks/font-antic-didone.rb
@@ -1,11 +1,11 @@
-cask 'font-antic-didone' do
+cask "font-antic-didone" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/anticdidone/AnticDidone-Regular.ttf'
-  name 'Antic Didone'
-  homepage 'https://fonts.google.com/specimen/Antic+Didone'
+  url "https://github.com/google/fonts/raw/master/ofl/anticdidone/AnticDidone-Regular.ttf"
+  name "Antic Didone"
+  homepage "https://fonts.google.com/specimen/Antic+Didone"
 
-  font 'AnticDidone-Regular.ttf'
+  font "AnticDidone-Regular.ttf"
 end
