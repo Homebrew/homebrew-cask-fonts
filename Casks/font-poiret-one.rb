d620b44afe4446290d67d7335d3627a7c4c30985
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:05 +0100

    font-poiret-one.rb: fix for new style

--- Casks/font-poiret-one.rb
+++ Casks/font-poiret-one.rb
@@ -1,11 +1,11 @@
-cask 'font-poiret-one' do
+cask "font-poiret-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/poiretone/PoiretOne-Regular.ttf'
-  name 'Poiret One'
-  homepage 'https://fonts.google.com/specimen/Poiret+One'
+  url "https://github.com/google/fonts/raw/master/ofl/poiretone/PoiretOne-Regular.ttf"
+  name "Poiret One"
+  homepage "https://fonts.google.com/specimen/Poiret+One"
 
-  font 'PoiretOne-Regular.ttf'
+  font "PoiretOne-Regular.ttf"
 end
