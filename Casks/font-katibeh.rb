d49a19b5b13b00cbbf256a33692a72d5d1195a31
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:27 +0100

    font-katibeh.rb: fix for new style

--- Casks/font-katibeh.rb
+++ Casks/font-katibeh.rb
@@ -1,11 +1,11 @@
-cask 'font-katibeh' do
+cask "font-katibeh" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/katibeh/Katibeh-Regular.ttf'
-  name 'Katibeh'
-  homepage 'https://fonts.google.com/specimen/Katibeh'
+  url "https://github.com/google/fonts/raw/master/ofl/katibeh/Katibeh-Regular.ttf"
+  name "Katibeh"
+  homepage "https://fonts.google.com/specimen/Katibeh"
 
-  font 'Katibeh-Regular.ttf'
+  font "Katibeh-Regular.ttf"
 end
