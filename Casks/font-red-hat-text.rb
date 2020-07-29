c8636c4b0d782e5783b773a47131e48395136fa2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:10 +0100

    font-red-hat-text.rb: fix for new style

--- Casks/font-red-hat-text.rb
+++ Casks/font-red-hat-text.rb
@@ -1,20 +1,20 @@
-cask 'font-red-hat-text' do
+cask "font-red-hat-text" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/redhattext',
+  url "https://github.com/google/fonts/trunk/ofl/redhattext",
       using:      :svn,
       trust_cert: true
-  name 'Red Hat Text'
-  homepage 'https://fonts.google.com/specimen/Red+Hat+Text'
+  name "Red Hat Text"
+  homepage "https://fonts.google.com/specimen/Red+Hat+Text"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'RedHatText-Bold.ttf'
-  font 'RedHatText-BoldItalic.ttf'
-  font 'RedHatText-Italic.ttf'
-  font 'RedHatText-Medium.ttf'
-  font 'RedHatText-MediumItalic.ttf'
-  font 'RedHatText-Regular.ttf'
+  font "RedHatText-Bold.ttf"
+  font "RedHatText-BoldItalic.ttf"
+  font "RedHatText-Italic.ttf"
+  font "RedHatText-Medium.ttf"
+  font "RedHatText-MediumItalic.ttf"
+  font "RedHatText-Regular.ttf"
 end
