5b0de800eb145a21e6a80f802f8cf37f4678bf59
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:22 +0100

    font-anonymice-nerd-font.rb: fix for new style

--- Casks/font-anonymice-nerd-font.rb
+++ Casks/font-anonymice-nerd-font.rb
@@ -1,12 +1,12 @@
-cask 'font-anonymice-nerd-font' do
-  version '2.1.0'
-  sha256 'b51b3dd9aa5bcf061240d8dfcc203e78b085eeb97a76d91f6ad8cd9473467668'
+cask "font-anonymice-nerd-font" do
+  version "2.1.0"
+  sha256 "b51b3dd9aa5bcf061240d8dfcc203e78b085eeb97a76d91f6ad8cd9473467668"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AnonymousPro.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'Anonymice Nerd Font (Anonymous Pro)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "Anonymice Nerd Font (Anonymous Pro)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Anonymice Nerd Font Complete.ttf'
-  font 'Anonymice Nerd Font Complete Mono.ttf'
+  font "Anonymice Nerd Font Complete.ttf"
+  font "Anonymice Nerd Font Complete Mono.ttf"
 end
