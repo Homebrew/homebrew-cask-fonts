684efb9ee63c112b7b533078f84738bc051a4b30
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:01 +0100

    font-oleo-script.rb: fix for new style

--- Casks/font-oleo-script.rb
+++ Casks/font-oleo-script.rb
@@ -1,16 +1,16 @@
-cask 'font-oleo-script' do
+cask "font-oleo-script" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/oleoscript',
+  url "https://github.com/google/fonts/trunk/ofl/oleoscript",
       using:      :svn,
       trust_cert: true
-  name 'Oleo Script'
-  homepage 'https://fonts.google.com/specimen/Oleo+Script'
+  name "Oleo Script"
+  homepage "https://fonts.google.com/specimen/Oleo+Script"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'OleoScript-Bold.ttf'
-  font 'OleoScript-Regular.ttf'
+  font "OleoScript-Bold.ttf"
+  font "OleoScript-Regular.ttf"
 end
