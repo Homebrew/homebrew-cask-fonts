0e3135298a9fcd71c4ae70cbc9e5cafeeb58d823
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:11 +0100

    font-rhodium-libre.rb: fix for new style

--- Casks/font-rhodium-libre.rb
+++ Casks/font-rhodium-libre.rb
@@ -1,11 +1,11 @@
-cask 'font-rhodium-libre' do
+cask "font-rhodium-libre" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/rhodiumlibre/RhodiumLibre-Regular.ttf'
-  name 'Rhodium Libre'
-  homepage 'https://fonts.google.com/specimen/Rhodium+Libre'
+  url "https://github.com/google/fonts/raw/master/ofl/rhodiumlibre/RhodiumLibre-Regular.ttf"
+  name "Rhodium Libre"
+  homepage "https://fonts.google.com/specimen/Rhodium+Libre"
 
-  font 'RhodiumLibre-Regular.ttf'
+  font "RhodiumLibre-Regular.ttf"
 end
