7d8d4b770a0d1569bbf4d0945fc193745b9984a1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:29 +0100

    font-wire-one.rb: fix for new style

--- Casks/font-wire-one.rb
+++ Casks/font-wire-one.rb
@@ -1,11 +1,11 @@
-cask 'font-wire-one' do
+cask "font-wire-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/wireone/WireOne.ttf'
-  name 'Wire One'
-  homepage 'https://fonts.google.com/specimen/Wire+One'
+  url "https://github.com/google/fonts/raw/master/ofl/wireone/WireOne.ttf"
+  name "Wire One"
+  homepage "https://fonts.google.com/specimen/Wire+One"
 
-  font 'WireOne.ttf'
+  font "WireOne.ttf"
 end
