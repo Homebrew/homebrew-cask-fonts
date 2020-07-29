9eda30210190bf8df157f9f253450f8df9f30423
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:11 +0100

    font-ribeye-marrow.rb: fix for new style

--- Casks/font-ribeye-marrow.rb
+++ Casks/font-ribeye-marrow.rb
@@ -1,11 +1,11 @@
-cask 'font-ribeye-marrow' do
+cask "font-ribeye-marrow" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/ribeyemarrow/RibeyeMarrow-Regular.ttf'
-  name 'Ribeye Marrow'
-  homepage 'https://fonts.google.com/specimen/Ribeye+Marrow'
+  url "https://github.com/google/fonts/raw/master/ofl/ribeyemarrow/RibeyeMarrow-Regular.ttf"
+  name "Ribeye Marrow"
+  homepage "https://fonts.google.com/specimen/Ribeye+Marrow"
 
-  font 'RibeyeMarrow-Regular.ttf'
+  font "RibeyeMarrow-Regular.ttf"
 end
