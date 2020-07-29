4acccd612a33945f1be3e0684c62187c42894731
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:47 +0100

    font-ek-mukta.rb: fix for new style

--- Casks/font-ek-mukta.rb
+++ Casks/font-ek-mukta.rb
@@ -1,21 +1,21 @@
-cask 'font-ek-mukta' do
+cask "font-ek-mukta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/ekmukta',
+  url "https://github.com/google/fonts/trunk/ofl/ekmukta",
       using:      :svn,
       trust_cert: true
-  name 'Ek Mukta'
-  homepage 'https://fonts.google.com/specimen/Ek+Mukta'
+  name "Ek Mukta"
+  homepage "https://fonts.google.com/specimen/Ek+Mukta"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'EkMukta-Bold.ttf'
-  font 'EkMukta-ExtraBold.ttf'
-  font 'EkMukta-ExtraLight.ttf'
-  font 'EkMukta-Light.ttf'
-  font 'EkMukta-Medium.ttf'
-  font 'EkMukta-Regular.ttf'
-  font 'EkMukta-SemiBold.ttf'
+  font "EkMukta-Bold.ttf"
+  font "EkMukta-ExtraBold.ttf"
+  font "EkMukta-ExtraLight.ttf"
+  font "EkMukta-Light.ttf"
+  font "EkMukta-Medium.ttf"
+  font "EkMukta-Regular.ttf"
+  font "EkMukta-SemiBold.ttf"
 end
