0ce1500ef425dd485429fea709b3335b4dc1ff15
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:54 +0100

    font-noto-sans-lisu.rb: fix for new style

--- Casks/font-noto-sans-lisu.rb
+++ Casks/font-noto-sans-lisu.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-lisu' do
+cask "font-noto-sans-lisu" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLisu-unhinted.zip'
-  name 'Noto Sans Lisu'
-  homepage 'https://www.google.com/get/noto/#sans-lisu'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLisu-unhinted.zip"
+  name "Noto Sans Lisu"
+  homepage "https://www.google.com/get/noto/#sans-lisu"
 
-  font 'NotoSansLisu-Regular.ttf'
+  font "NotoSansLisu-Regular.ttf"
 end
