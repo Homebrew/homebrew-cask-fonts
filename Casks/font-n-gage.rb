f5643587d08fa702e36dcd559a95667b8f7a26bb
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:47 +0100

    font-n-gage.rb: fix for new style

--- Casks/font-n-gage.rb
+++ Casks/font-n-gage.rb
@@ -1,10 +1,10 @@
-cask 'font-n-gage' do
+cask "font-n-gage" do
   version :latest
   sha256 :no_check
 
-  url 'https://dl.dafont.com/dl/?f=n_gage'
-  name 'N-Gage'
-  homepage 'https://www.dafont.com/n-gage.font'
+  url "https://dl.dafont.com/dl/?f=n_gage"
+  name "N-Gage"
+  homepage "https://www.dafont.com/n-gage.font"
 
-  font 'N-Gage.ttf'
+  font "N-Gage.ttf"
 end
