e90541d1fb1e47fb69912d642742e427803de44e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:31 +0100

    font-lao-muang-don.rb: fix for new style

--- Casks/font-lao-muang-don.rb
+++ Casks/font-lao-muang-don.rb
@@ -1,11 +1,11 @@
-cask 'font-lao-muang-don' do
+cask "font-lao-muang-don" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/laomuangdon/LaoMuangDon-Regular.ttf'
-  name 'Lao Muang Don'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/laomuangdon/LaoMuangDon-Regular.ttf"
+  name "Lao Muang Don"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'LaoMuangDon-Regular.ttf'
+  font "LaoMuangDon-Regular.ttf"
 end
