0c4c771b16b715efa738d7df7ffd4826138d4b12
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:16 +0100

    font-share-tech.rb: fix for new style

--- Casks/font-share-tech.rb
+++ Casks/font-share-tech.rb
@@ -1,11 +1,11 @@
-cask 'font-share-tech' do
+cask "font-share-tech" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/sharetech/ShareTech-Regular.ttf'
-  name 'Share Tech'
-  homepage 'https://fonts.google.com/specimen/Share+Tech'
+  url "https://github.com/google/fonts/raw/master/ofl/sharetech/ShareTech-Regular.ttf"
+  name "Share Tech"
+  homepage "https://fonts.google.com/specimen/Share+Tech"
 
-  font 'ShareTech-Regular.ttf'
+  font "ShareTech-Regular.ttf"
 end
