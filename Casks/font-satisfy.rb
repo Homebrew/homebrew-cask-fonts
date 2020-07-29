62e6184d3d40965b6b95cf08882f69c90402f06d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:15 +0100

    font-satisfy.rb: fix for new style

--- Casks/font-satisfy.rb
+++ Casks/font-satisfy.rb
@@ -1,11 +1,11 @@
-cask 'font-satisfy' do
+cask "font-satisfy" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/satisfy/Satisfy-Regular.ttf'
-  name 'Satisfy'
-  homepage 'https://fonts.google.com/specimen/Satisfy'
+  url "https://github.com/google/fonts/raw/master/apache/satisfy/Satisfy-Regular.ttf"
+  name "Satisfy"
+  homepage "https://fonts.google.com/specimen/Satisfy"
 
-  font 'Satisfy-Regular.ttf'
+  font "Satisfy-Regular.ttf"
 end
