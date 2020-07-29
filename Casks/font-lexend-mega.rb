1d2e172181e438eee05c0c7a83f9c4f4ea39425c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:33 +0100

    font-lexend-mega.rb: fix for new style

--- Casks/font-lexend-mega.rb
+++ Casks/font-lexend-mega.rb
@@ -1,11 +1,11 @@
-cask 'font-lexend-mega' do
+cask "font-lexend-mega" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/lexendmega/LexendMega-Regular.ttf'
-  name 'Lexend Mega'
-  homepage 'https://fonts.google.com/specimen/Lexend+Mega'
+  url "https://github.com/google/fonts/raw/master/ofl/lexendmega/LexendMega-Regular.ttf"
+  name "Lexend Mega"
+  homepage "https://fonts.google.com/specimen/Lexend+Mega"
 
-  font 'LexendMega-Regular.ttf'
+  font "LexendMega-Regular.ttf"
 end
