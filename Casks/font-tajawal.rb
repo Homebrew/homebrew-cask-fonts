875068ee992585e55f3f388513a04565cf6179a3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:22 +0100

    font-tajawal.rb: fix for new style

--- Casks/font-tajawal.rb
+++ Casks/font-tajawal.rb
@@ -1,21 +1,21 @@
-cask 'font-tajawal' do
+cask "font-tajawal" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/tajawal',
+  url "https://github.com/google/fonts/trunk/ofl/tajawal",
       using:      :svn,
       trust_cert: true
-  name 'Tajawal'
-  homepage 'https://fonts.google.com/specimen/Tajawal'
+  name "Tajawal"
+  homepage "https://fonts.google.com/specimen/Tajawal"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Tajawal-Black.ttf'
-  font 'Tajawal-Bold.ttf'
-  font 'Tajawal-ExtraBold.ttf'
-  font 'Tajawal-ExtraLight.ttf'
-  font 'Tajawal-Light.ttf'
-  font 'Tajawal-Medium.ttf'
-  font 'Tajawal-Regular.ttf'
+  font "Tajawal-Black.ttf"
+  font "Tajawal-Bold.ttf"
+  font "Tajawal-ExtraBold.ttf"
+  font "Tajawal-ExtraLight.ttf"
+  font "Tajawal-Light.ttf"
+  font "Tajawal-Medium.ttf"
+  font "Tajawal-Regular.ttf"
 end
