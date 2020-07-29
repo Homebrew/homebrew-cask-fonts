e4c9cf2b336f2670b30f98230db20b3183131e41
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:41 +0100

    font-merienda.rb: fix for new style

--- Casks/font-merienda.rb
+++ Casks/font-merienda.rb
@@ -1,16 +1,16 @@
-cask 'font-merienda' do
+cask "font-merienda" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/merienda',
+  url "https://github.com/google/fonts/trunk/ofl/merienda",
       using:      :svn,
       trust_cert: true
-  name 'Merienda'
-  homepage 'https://fonts.google.com/specimen/Merienda'
+  name "Merienda"
+  homepage "https://fonts.google.com/specimen/Merienda"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Merienda-Bold.ttf'
-  font 'Merienda-Regular.ttf'
+  font "Merienda-Bold.ttf"
+  font "Merienda-Regular.ttf"
 end
