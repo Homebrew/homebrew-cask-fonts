36d9174a5ffafcd3449f9a58bb8bde810f624d35
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:21 +0100

    font-arima-madurai.rb: fix for new style

--- Casks/font-arima-madurai.rb
+++ Casks/font-arima-madurai.rb
@@ -1,22 +1,22 @@
-cask 'font-arima-madurai' do
+cask "font-arima-madurai" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/arimamadurai',
+  url "https://github.com/google/fonts/trunk/ofl/arimamadurai",
       using:      :svn,
       trust_cert: true
-  name 'Arima Madurai'
-  homepage 'https://fonts.google.com/specimen/Arima+Madurai'
+  name "Arima Madurai"
+  homepage "https://fonts.google.com/specimen/Arima+Madurai"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'ArimaMadurai-Black.ttf'
-  font 'ArimaMadurai-Bold.ttf'
-  font 'ArimaMadurai-ExtraBold.ttf'
-  font 'ArimaMadurai-ExtraLight.ttf'
-  font 'ArimaMadurai-Light.ttf'
-  font 'ArimaMadurai-Medium.ttf'
-  font 'ArimaMadurai-Regular.ttf'
-  font 'ArimaMadurai-Thin.ttf'
+  font "ArimaMadurai-Black.ttf"
+  font "ArimaMadurai-Bold.ttf"
+  font "ArimaMadurai-ExtraBold.ttf"
+  font "ArimaMadurai-ExtraLight.ttf"
+  font "ArimaMadurai-Light.ttf"
+  font "ArimaMadurai-Medium.ttf"
+  font "ArimaMadurai-Regular.ttf"
+  font "ArimaMadurai-Thin.ttf"
 end
