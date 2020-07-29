7991dbe4a9c33c8240cde249e7e36d82c03b14d8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:48 +0100

    font-new-rocker.rb: fix for new style

--- Casks/font-new-rocker.rb
+++ Casks/font-new-rocker.rb
@@ -1,11 +1,11 @@
-cask 'font-new-rocker' do
+cask "font-new-rocker" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/newrocker/NewRocker-Regular.ttf'
-  name 'New Rocker'
-  homepage 'https://fonts.google.com/specimen/New+Rocker'
+  url "https://github.com/google/fonts/raw/master/ofl/newrocker/NewRocker-Regular.ttf"
+  name "New Rocker"
+  homepage "https://fonts.google.com/specimen/New+Rocker"
 
-  font 'NewRocker-Regular.ttf'
+  font "NewRocker-Regular.ttf"
 end
