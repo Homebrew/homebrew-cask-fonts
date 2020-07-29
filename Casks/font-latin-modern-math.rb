e256354c37b365a17295f87d9ebb0949ad0d882c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:32 +0100

    font-latin-modern-math.rb: fix for new style

--- Casks/font-latin-modern-math.rb
+++ Casks/font-latin-modern-math.rb
@@ -1,10 +1,10 @@
-cask 'font-latin-modern-math' do
+cask "font-latin-modern-math" do
   version :latest
   sha256 :no_check
 
-  url 'http://www.gust.org.pl/projects/e-foundry/lm-math/download/latinmodern-math-1959.zip'
-  name 'Latin Modern Math'
-  homepage 'http://www.gust.org.pl/projects/e-foundry/lm-math'
+  url "http://www.gust.org.pl/projects/e-foundry/lm-math/download/latinmodern-math-1959.zip"
+  name "Latin Modern Math"
+  homepage "http://www.gust.org.pl/projects/e-foundry/lm-math"
 
-  font 'latinmodern-math-1959/otf/latinmodern-math.otf'
+  font "latinmodern-math-1959/otf/latinmodern-math.otf"
 end
