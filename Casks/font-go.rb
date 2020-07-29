2f2a5d73603a8b5772ddbc151888d179d6ecbfa4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:04 +0100

    font-go.rb: fix for new style

--- Casks/font-go.rb
+++ Casks/font-go.rb
@@ -1,21 +1,21 @@
-cask 'font-go' do
+cask "font-go" do
   version :latest
   sha256 :no_check
 
-  url 'https://go.googlesource.com/image/+archive/master/font/gofont/ttfs.tar.gz'
-  name 'Go'
-  homepage 'https://go.googlesource.com/image/+/master/font/gofont/ttfs/README'
+  url "https://go.googlesource.com/image/+archive/master/font/gofont/ttfs.tar.gz"
+  name "Go"
+  homepage "https://go.googlesource.com/image/+/master/font/gofont/ttfs/README"
 
-  font 'Go-Bold-Italic.ttf'
-  font 'Go-Bold.ttf'
-  font 'Go-Italic.ttf'
-  font 'Go-Medium-Italic.ttf'
-  font 'Go-Medium.ttf'
-  font 'Go-Mono-Bold-Italic.ttf'
-  font 'Go-Mono-Bold.ttf'
-  font 'Go-Mono-Italic.ttf'
-  font 'Go-Mono.ttf'
-  font 'Go-Regular.ttf'
-  font 'Go-Smallcaps-Italic.ttf'
-  font 'Go-Smallcaps.ttf'
+  font "Go-Bold-Italic.ttf"
+  font "Go-Bold.ttf"
+  font "Go-Italic.ttf"
+  font "Go-Medium-Italic.ttf"
+  font "Go-Medium.ttf"
+  font "Go-Mono-Bold-Italic.ttf"
+  font "Go-Mono-Bold.ttf"
+  font "Go-Mono-Italic.ttf"
+  font "Go-Mono.ttf"
+  font "Go-Regular.ttf"
+  font "Go-Smallcaps-Italic.ttf"
+  font "Go-Smallcaps.ttf"
 end
