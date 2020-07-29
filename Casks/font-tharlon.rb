e4d8877fc79512a50e415f57257af51287b43753
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:24 +0100

    font-tharlon.rb: fix for new style

--- Casks/font-tharlon.rb
+++ Casks/font-tharlon.rb
@@ -1,11 +1,11 @@
-cask 'font-tharlon' do
+cask "font-tharlon" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/tharlon/Tharlon-Regular.ttf'
-  name 'Tharlon'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/tharlon/Tharlon-Regular.ttf"
+  name "Tharlon"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'Tharlon-Regular.ttf'
+  font "Tharlon-Regular.ttf"
 end
