ffc17ea583b1c40cba2715db78397e86e93a2326
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:52 +0100

    font-fascinate-inline.rb: fix for new style

--- Casks/font-fascinate-inline.rb
+++ Casks/font-fascinate-inline.rb
@@ -1,11 +1,11 @@
-cask 'font-fascinate-inline' do
+cask "font-fascinate-inline" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/fascinateinline/FascinateInline-Regular.ttf'
-  name 'Fascinate Inline'
-  homepage 'https://fonts.google.com/specimen/Fascinate+Inline'
+  url "https://github.com/google/fonts/raw/master/ofl/fascinateinline/FascinateInline-Regular.ttf"
+  name "Fascinate Inline"
+  homepage "https://fonts.google.com/specimen/Fascinate+Inline"
 
-  font 'FascinateInline-Regular.ttf'
+  font "FascinateInline-Regular.ttf"
 end
