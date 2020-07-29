d23aae311bf7fcfd2a3b8d94ad0fbc77581b7c72
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:54 +0100

    font-fira-sans.rb: fix for new style

--- Casks/font-fira-sans.rb
+++ Casks/font-fira-sans.rb
@@ -1,12 +1,12 @@
-cask 'font-fira-sans' do
-  version '4.202'
-  sha256 'd86269657387f144d77ba12011124f30f423f70672e1576dc16f918bb16ddfe4'
+cask "font-fira-sans" do
+  version "4.202"
+  sha256 "d86269657387f144d77ba12011124f30f423f70672e1576dc16f918bb16ddfe4"
 
   # github.com/mozilla/Fira/ was verified as official when first introduced to the cask
   url "https://github.com/mozilla/Fira/archive/#{version}.tar.gz"
-  appcast 'https://github.com/mozilla/Fira/releases.atom'
-  name 'Fira Sans'
-  homepage 'https://mozilla.github.io/Fira/'
+  appcast "https://github.com/mozilla/Fira/releases.atom"
+  name "Fira Sans"
+  homepage "https://mozilla.github.io/Fira/"
 
   font "Fira-#{version}/otf/FiraSans-BoldItalic.otf"
   font "Fira-#{version}/otf/FiraSans-Bold.otf"
