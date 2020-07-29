e96655920eb5eaa7be55e330e4a3ff8d0b341f8b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:25 +0100

    font-aclonica.rb: fix for new style

--- Casks/font-aclonica.rb
+++ Casks/font-aclonica.rb
@@ -1,11 +1,11 @@
-cask 'font-aclonica' do
+cask "font-aclonica" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/aclonica/Aclonica-Regular.ttf'
-  name 'Aclonica'
-  homepage 'https://fonts.google.com/specimen/Aclonica'
+  url "https://github.com/google/fonts/raw/master/apache/aclonica/Aclonica-Regular.ttf"
+  name "Aclonica"
+  homepage "https://fonts.google.com/specimen/Aclonica"
 
-  font 'Aclonica-Regular.ttf'
+  font "Aclonica-Regular.ttf"
 end
