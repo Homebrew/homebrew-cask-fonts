72d1b3f75c172ab81ed8eecd39dac779145fe075
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:34 +0100

    font-liberation-nerd-font.rb: fix for new style

--- Casks/font-liberation-nerd-font.rb
+++ Casks/font-liberation-nerd-font.rb
@@ -1,34 +1,34 @@
-cask 'font-liberation-nerd-font' do
-  version '2.1.0'
-  sha256 '30b0e02385c43c92a9556689eda6c83731525c431f6eba9dad556b3512aa77a5'
+cask "font-liberation-nerd-font" do
+  version "2.1.0"
+  sha256 "30b0e02385c43c92a9556689eda6c83731525c431f6eba9dad556b3512aa77a5"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/LiberationMono.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'Literation Nerd Font families (Liberation)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "Literation Nerd Font families (Liberation)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Literation Mono Nerd Font Complete Mono.ttf'
-  font 'Literation Serif Bold Italic Nerd Font Complete.ttf'
-  font 'Literation Sans Bold Italic Nerd Font Complete Mono.ttf'
-  font 'Literation Mono Italic Nerd Font Complete Mono.ttf'
-  font 'Literation Sans Nerd Font Complete Mono.ttf'
-  font 'Literation Mono Bold Nerd Font Complete.ttf'
-  font 'Literation Serif Nerd Font Complete.ttf'
-  font 'Literation Serif Nerd Font Complete Mono.ttf'
-  font 'Literation Sans Bold Nerd Font Complete.ttf'
-  font 'Literation Mono Italic Nerd Font Complete.ttf'
-  font 'Literation Serif Bold Italic Nerd Font Complete Mono.ttf'
-  font 'Literation Mono Bold Nerd Font Complete Mono.ttf'
-  font 'Literation Serif Italic Nerd Font Complete.ttf'
-  font 'Literation Serif Bold Nerd Font Complete Mono.ttf'
-  font 'Literation Sans Bold Nerd Font Complete Mono.ttf'
-  font 'Literation Serif Italic Nerd Font Complete Mono.ttf'
-  font 'Literation Mono Bold Italic Nerd Font Complete.ttf'
-  font 'Literation Sans Italic Nerd Font Complete.ttf'
-  font 'Literation Sans Bold Italic Nerd Font Complete.ttf'
-  font 'Literation Sans Nerd Font Complete.ttf'
-  font 'Literation Sans Italic Nerd Font Complete Mono.ttf'
-  font 'Literation Mono Nerd Font Complete.ttf'
-  font 'Literation Serif Bold Nerd Font Complete.ttf'
-  font 'Literation Mono Bold Italic Nerd Font Complete Mono.ttf'
+  font "Literation Mono Nerd Font Complete Mono.ttf"
+  font "Literation Serif Bold Italic Nerd Font Complete.ttf"
+  font "Literation Sans Bold Italic Nerd Font Complete Mono.ttf"
+  font "Literation Mono Italic Nerd Font Complete Mono.ttf"
+  font "Literation Sans Nerd Font Complete Mono.ttf"
+  font "Literation Mono Bold Nerd Font Complete.ttf"
+  font "Literation Serif Nerd Font Complete.ttf"
+  font "Literation Serif Nerd Font Complete Mono.ttf"
+  font "Literation Sans Bold Nerd Font Complete.ttf"
+  font "Literation Mono Italic Nerd Font Complete.ttf"
+  font "Literation Serif Bold Italic Nerd Font Complete Mono.ttf"
+  font "Literation Mono Bold Nerd Font Complete Mono.ttf"
+  font "Literation Serif Italic Nerd Font Complete.ttf"
+  font "Literation Serif Bold Nerd Font Complete Mono.ttf"
+  font "Literation Sans Bold Nerd Font Complete Mono.ttf"
+  font "Literation Serif Italic Nerd Font Complete Mono.ttf"
+  font "Literation Mono Bold Italic Nerd Font Complete.ttf"
+  font "Literation Sans Italic Nerd Font Complete.ttf"
+  font "Literation Sans Bold Italic Nerd Font Complete.ttf"
+  font "Literation Sans Nerd Font Complete.ttf"
+  font "Literation Sans Italic Nerd Font Complete Mono.ttf"
+  font "Literation Mono Nerd Font Complete.ttf"
+  font "Literation Serif Bold Nerd Font Complete.ttf"
+  font "Literation Mono Bold Italic Nerd Font Complete Mono.ttf"
 end
