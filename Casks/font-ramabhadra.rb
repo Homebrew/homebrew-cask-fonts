2e58a645dd033ab4d48860bb556ffd7fedb4df4b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:09 +0100

    font-ramabhadra.rb: fix for new style

--- Casks/font-ramabhadra.rb
+++ Casks/font-ramabhadra.rb
@@ -1,11 +1,11 @@
-cask 'font-ramabhadra' do
+cask "font-ramabhadra" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/ramabhadra/Ramabhadra-Regular.ttf'
-  name 'Ramabhadra'
-  homepage 'https://fonts.google.com/specimen/Ramabhadra'
+  url "https://github.com/google/fonts/raw/master/ofl/ramabhadra/Ramabhadra-Regular.ttf"
+  name "Ramabhadra"
+  homepage "https://fonts.google.com/specimen/Ramabhadra"
 
-  font 'Ramabhadra-Regular.ttf'
+  font "Ramabhadra-Regular.ttf"
 end
