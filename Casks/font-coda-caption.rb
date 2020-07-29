4b6b5737953cdbe4b2e84f91ea7dc8bff7c754d4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:33 +0100

    font-coda-caption.rb: fix for new style

--- Casks/font-coda-caption.rb
+++ Casks/font-coda-caption.rb
@@ -1,11 +1,11 @@
-cask 'font-coda-caption' do
+cask "font-coda-caption" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/codacaption/CodaCaption-ExtraBold.ttf'
-  name 'Coda Caption'
-  homepage 'https://fonts.google.com/specimen/Coda+Caption'
+  url "https://github.com/google/fonts/raw/master/ofl/codacaption/CodaCaption-ExtraBold.ttf"
+  name "Coda Caption"
+  homepage "https://fonts.google.com/specimen/Coda+Caption"
 
-  font 'CodaCaption-ExtraBold.ttf'
+  font "CodaCaption-ExtraBold.ttf"
 end
