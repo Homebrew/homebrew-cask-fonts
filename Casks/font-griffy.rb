5742512d897f6389b758ef4f54f1cb8a571482ce
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:06 +0100

    font-griffy.rb: fix for new style

--- Casks/font-griffy.rb
+++ Casks/font-griffy.rb
@@ -1,11 +1,11 @@
-cask 'font-griffy' do
+cask "font-griffy" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/griffy/Griffy-Regular.ttf'
-  name 'Griffy'
-  homepage 'https://fonts.google.com/specimen/Griffy'
+  url "https://github.com/google/fonts/raw/master/ofl/griffy/Griffy-Regular.ttf"
+  name "Griffy"
+  homepage "https://fonts.google.com/specimen/Griffy"
 
-  font 'Griffy-Regular.ttf'
+  font "Griffy-Regular.ttf"
 end
