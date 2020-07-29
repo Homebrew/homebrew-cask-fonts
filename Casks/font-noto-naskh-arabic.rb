561c7917bdb0aaa92ab83e4d980f526bf0cbf3f2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:50 +0100

    font-noto-naskh-arabic.rb: fix for new style

--- Casks/font-noto-naskh-arabic.rb
+++ Casks/font-noto-naskh-arabic.rb
@@ -1,12 +1,12 @@
-cask 'font-noto-naskh-arabic' do
+cask "font-noto-naskh-arabic" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoNaskhArabic-unhinted.zip'
-  name 'Noto Naskh Arabic'
-  homepage 'https://www.google.com/get/noto/#naskh-arab'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoNaskhArabic-unhinted.zip"
+  name "Noto Naskh Arabic"
+  homepage "https://www.google.com/get/noto/#naskh-arab"
 
-  font 'NotoNaskhArabic-Bold.ttf'
-  font 'NotoNaskhArabic-Regular.ttf'
+  font "NotoNaskhArabic-Bold.ttf"
+  font "NotoNaskhArabic-Regular.ttf"
 end
