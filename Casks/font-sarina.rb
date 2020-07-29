00bca8b91e580804b04742ad0193589ff13896ac
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:14 +0100

    font-sarina.rb: fix for new style

--- Casks/font-sarina.rb
+++ Casks/font-sarina.rb
@@ -1,11 +1,11 @@
-cask 'font-sarina' do
+cask "font-sarina" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/sarina/Sarina-Regular.ttf'
-  name 'Sarina'
-  homepage 'https://fonts.google.com/specimen/Sarina'
+  url "https://github.com/google/fonts/raw/master/ofl/sarina/Sarina-Regular.ttf"
+  name "Sarina"
+  homepage "https://fonts.google.com/specimen/Sarina"
 
-  font 'Sarina-Regular.ttf'
+  font "Sarina-Regular.ttf"
 end
