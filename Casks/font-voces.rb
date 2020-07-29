2a8c72cae40924e735935b69185fd08d2c36e59b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:28 +0100

    font-voces.rb: fix for new style

--- Casks/font-voces.rb
+++ Casks/font-voces.rb
@@ -1,11 +1,11 @@
-cask 'font-voces' do
+cask "font-voces" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/voces/Voces-Regular.ttf'
-  name 'Voces'
-  homepage 'https://fonts.google.com/specimen/Voces'
+  url "https://github.com/google/fonts/raw/master/ofl/voces/Voces-Regular.ttf"
+  name "Voces"
+  homepage "https://fonts.google.com/specimen/Voces"
 
-  font 'Voces-Regular.ttf'
+  font "Voces-Regular.ttf"
 end
