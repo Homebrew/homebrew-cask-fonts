fb895ce5684e0128787a793e73bc005810e1a8e4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:31 +0100

    font-cherry-cream-soda.rb: fix for new style

--- Casks/font-cherry-cream-soda.rb
+++ Casks/font-cherry-cream-soda.rb
@@ -1,11 +1,11 @@
-cask 'font-cherry-cream-soda' do
+cask "font-cherry-cream-soda" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/cherrycreamsoda/CherryCreamSoda-Regular.ttf'
-  name 'Cherry Cream Soda'
-  homepage 'https://fonts.google.com/specimen/Cherry+Cream+Soda'
+  url "https://github.com/google/fonts/raw/master/apache/cherrycreamsoda/CherryCreamSoda-Regular.ttf"
+  name "Cherry Cream Soda"
+  homepage "https://fonts.google.com/specimen/Cherry+Cream+Soda"
 
-  font 'CherryCreamSoda-Regular.ttf'
+  font "CherryCreamSoda-Regular.ttf"
 end
