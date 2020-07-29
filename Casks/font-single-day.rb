569cf3f3a42974507105467c4ac6abe1a729e981
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:17 +0100

    font-single-day.rb: fix for new style

--- Casks/font-single-day.rb
+++ Casks/font-single-day.rb
@@ -1,11 +1,11 @@
-cask 'font-single-day' do
+cask "font-single-day" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/singleday/SingleDay-Regular.ttf'
-  name 'Single Day'
-  homepage 'https://fonts.google.com/specimen/Single+Day'
+  url "https://github.com/google/fonts/raw/master/ofl/singleday/SingleDay-Regular.ttf"
+  name "Single Day"
+  homepage "https://fonts.google.com/specimen/Single+Day"
 
-  font 'SingleDay-Regular.ttf'
+  font "SingleDay-Regular.ttf"
 end
