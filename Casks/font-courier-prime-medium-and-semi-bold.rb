709e07fe0b8bfafc5f88cce58269a63d39b37fdc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:37 +0100

    font-courier-prime-medium-and-semi-bold.rb: fix for new style

--- Casks/font-courier-prime-medium-and-semi-bold.rb
+++ Casks/font-courier-prime-medium-and-semi-bold.rb
@@ -1,11 +1,11 @@
-cask 'font-courier-prime-medium-and-semi-bold' do
+cask "font-courier-prime-medium-and-semi-bold" do
   version :latest
   sha256 :no_check
 
-  url 'https://quoteunquoteapps.com/courierprime/downloads/courier-prime-medium-semi-bold.zip'
-  name 'Courier Prime Medium & Semi-Bold'
-  homepage 'https://quoteunquoteapps.com/courierprime/#cyrillic-semi-bold'
+  url "https://quoteunquoteapps.com/courierprime/downloads/courier-prime-medium-semi-bold.zip"
+  name "Courier Prime Medium & Semi-Bold"
+  homepage "https://quoteunquoteapps.com/courierprime/#cyrillic-semi-bold"
 
-  font 'CourierPrime-Medium.ttf'
-  font 'CourierPrime-SemiBold.ttf'
+  font "CourierPrime-Medium.ttf"
+  font "CourierPrime-SemiBold.ttf"
 end
