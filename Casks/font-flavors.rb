231f7843f9e9bc71f868c63f5c8c5055cf593ec7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:55 +0100

    font-flavors.rb: fix for new style

--- Casks/font-flavors.rb
+++ Casks/font-flavors.rb
@@ -1,11 +1,11 @@
-cask 'font-flavors' do
+cask "font-flavors" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/flavors/Flavors-Regular.ttf'
-  name 'Flavors'
-  homepage 'https://fonts.google.com/specimen/Flavors'
+  url "https://github.com/google/fonts/raw/master/ofl/flavors/Flavors-Regular.ttf"
+  name "Flavors"
+  homepage "https://fonts.google.com/specimen/Flavors"
 
-  font 'Flavors-Regular.ttf'
+  font "Flavors-Regular.ttf"
 end
