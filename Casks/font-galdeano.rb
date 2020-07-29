e39cf40b89c21ea24073fe903aaf66553959dd93
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:58 +0100

    font-galdeano.rb: fix for new style

--- Casks/font-galdeano.rb
+++ Casks/font-galdeano.rb
@@ -1,11 +1,11 @@
-cask 'font-galdeano' do
+cask "font-galdeano" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/galdeano/Galdeano-Regular.ttf'
-  name 'Galdeano'
-  homepage 'https://fonts.google.com/specimen/Galdeano'
+  url "https://github.com/google/fonts/raw/master/ofl/galdeano/Galdeano-Regular.ttf"
+  name "Galdeano"
+  homepage "https://fonts.google.com/specimen/Galdeano"
 
-  font 'Galdeano-Regular.ttf'
+  font "Galdeano-Regular.ttf"
 end
