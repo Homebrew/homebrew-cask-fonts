7701e6c128b823762009081f1462c80b46d06743
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:05 +0100

    font-playball.rb: fix for new style

--- Casks/font-playball.rb
+++ Casks/font-playball.rb
@@ -1,11 +1,11 @@
-cask 'font-playball' do
+cask "font-playball" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/playball/Playball-Regular.ttf'
-  name 'Playball'
-  homepage 'https://fonts.google.com/specimen/Playball'
+  url "https://github.com/google/fonts/raw/master/ofl/playball/Playball-Regular.ttf"
+  name "Playball"
+  homepage "https://fonts.google.com/specimen/Playball"
 
-  font 'Playball-Regular.ttf'
+  font "Playball-Regular.ttf"
 end
