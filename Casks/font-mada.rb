5d3ce26b67fb86dd06b93176a5a882f2c3063556
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:38 +0100

    font-mada.rb: fix for new style

--- Casks/font-mada.rb
+++ Casks/font-mada.rb
@@ -1,21 +1,21 @@
-cask 'font-mada' do
+cask "font-mada" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/mada',
+  url "https://github.com/google/fonts/trunk/ofl/mada",
       using:      :svn,
       trust_cert: true
-  name 'Mada'
-  homepage 'https://fonts.google.com/specimen/Mada'
+  name "Mada"
+  homepage "https://fonts.google.com/specimen/Mada"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Mada-Black.ttf'
-  font 'Mada-Bold.ttf'
-  font 'Mada-ExtraLight.ttf'
-  font 'Mada-Light.ttf'
-  font 'Mada-Medium.ttf'
-  font 'Mada-Regular.ttf'
-  font 'Mada-SemiBold.ttf'
+  font "Mada-Black.ttf"
+  font "Mada-Bold.ttf"
+  font "Mada-ExtraLight.ttf"
+  font "Mada-Light.ttf"
+  font "Mada-Medium.ttf"
+  font "Mada-Regular.ttf"
+  font "Mada-SemiBold.ttf"
 end
