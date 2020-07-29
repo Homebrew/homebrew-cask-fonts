a632ca485a344480906d1cbd228505a2d52dda1f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:58 +0100

    font-gelasio.rb: fix for new style

--- Casks/font-gelasio.rb
+++ Casks/font-gelasio.rb
@@ -1,22 +1,22 @@
-cask 'font-gelasio' do
+cask "font-gelasio" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/gelasio',
+  url "https://github.com/google/fonts/trunk/ofl/gelasio",
       using:      :svn,
       trust_cert: true
-  name 'Gelasio'
-  homepage 'https://fonts.google.com/specimen/Gelasio'
+  name "Gelasio"
+  homepage "https://fonts.google.com/specimen/Gelasio"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Gelasio-Bold.ttf'
-  font 'Gelasio-BoldItalic.ttf'
-  font 'Gelasio-Italic.ttf'
-  font 'Gelasio-Medium.ttf'
-  font 'Gelasio-MediumItalic.ttf'
-  font 'Gelasio-Regular.ttf'
-  font 'Gelasio-SemiBold.ttf'
-  font 'Gelasio-SemiBoldItalic.ttf'
+  font "Gelasio-Bold.ttf"
+  font "Gelasio-BoldItalic.ttf"
+  font "Gelasio-Italic.ttf"
+  font "Gelasio-Medium.ttf"
+  font "Gelasio-MediumItalic.ttf"
+  font "Gelasio-Regular.ttf"
+  font "Gelasio-SemiBold.ttf"
+  font "Gelasio-SemiBoldItalic.ttf"
 end
