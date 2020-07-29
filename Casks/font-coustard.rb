bc2708174d8823636bbc7c5368366bb7c706d39b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:38 +0100

    font-coustard.rb: fix for new style

--- Casks/font-coustard.rb
+++ Casks/font-coustard.rb
@@ -1,16 +1,16 @@
-cask 'font-coustard' do
+cask "font-coustard" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/coustard',
+  url "https://github.com/google/fonts/trunk/ofl/coustard",
       using:      :svn,
       trust_cert: true
-  name 'Coustard'
-  homepage 'https://fonts.google.com/specimen/Coustard'
+  name "Coustard"
+  homepage "https://fonts.google.com/specimen/Coustard"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Coustard-Black.ttf'
-  font 'Coustard-Regular.ttf'
+  font "Coustard-Black.ttf"
+  font "Coustard-Regular.ttf"
 end
