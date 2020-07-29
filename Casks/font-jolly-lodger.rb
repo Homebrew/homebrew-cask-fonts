1c7a610bb1066c9f845005ab249baa990369ed55
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:23 +0100

    font-jolly-lodger.rb: fix for new style

--- Casks/font-jolly-lodger.rb
+++ Casks/font-jolly-lodger.rb
@@ -1,11 +1,11 @@
-cask 'font-jolly-lodger' do
+cask "font-jolly-lodger" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/jollylodger/JollyLodger-Regular.ttf'
-  name 'Jolly Lodger'
-  homepage 'https://fonts.google.com/specimen/Jolly+Lodger'
+  url "https://github.com/google/fonts/raw/master/ofl/jollylodger/JollyLodger-Regular.ttf"
+  name "Jolly Lodger"
+  homepage "https://fonts.google.com/specimen/Jolly+Lodger"
 
-  font 'JollyLodger-Regular.ttf'
+  font "JollyLodger-Regular.ttf"
 end
