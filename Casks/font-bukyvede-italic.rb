c9655436c47ee74fa64e9b2a422b633160678fd5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:23 +0100

    font-bukyvede-italic.rb: fix for new style

--- Casks/font-bukyvede-italic.rb
+++ Casks/font-bukyvede-italic.rb
@@ -1,10 +1,10 @@
-cask 'font-bukyvede-italic' do
+cask "font-bukyvede-italic" do
   version :latest
   sha256 :no_check
 
-  url 'https://kodeks.uni-bamberg.de/aksl/media/BukyVede-Italic.ttf'
-  name 'BukyVede Italic'
-  homepage 'https://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm'
+  url "https://kodeks.uni-bamberg.de/aksl/media/BukyVede-Italic.ttf"
+  name "BukyVede Italic"
+  homepage "https://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm"
 
-  font 'BukyVede-Italic.ttf'
+  font "BukyVede-Italic.ttf"
 end
