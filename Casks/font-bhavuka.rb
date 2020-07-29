b2ad00dd0a5760763e123cca4de5aead729cbcae
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:19 +0100

    font-bhavuka.rb: fix for new style

--- Casks/font-bhavuka.rb
+++ Casks/font-bhavuka.rb
@@ -1,11 +1,11 @@
-cask 'font-bhavuka' do
+cask "font-bhavuka" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bhavuka/Bhavuka-Regular.ttf'
-  name 'Bhavuka'
-  homepage 'https://fonts.google.com/specimen/Bhavuka'
+  url "https://github.com/google/fonts/raw/master/ofl/bhavuka/Bhavuka-Regular.ttf"
+  name "Bhavuka"
+  homepage "https://fonts.google.com/specimen/Bhavuka"
 
-  font 'Bhavuka-Regular.ttf'
+  font "Bhavuka-Regular.ttf"
 end
