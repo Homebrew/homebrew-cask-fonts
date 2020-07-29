0ef0c64fcbeb6687d04071965ac28b111d4e82f0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:29 +0100

    font-changa-one.rb: fix for new style

--- Casks/font-changa-one.rb
+++ Casks/font-changa-one.rb
@@ -1,16 +1,16 @@
-cask 'font-changa-one' do
+cask "font-changa-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/changaone',
+  url "https://github.com/google/fonts/trunk/ofl/changaone",
       using:      :svn,
       trust_cert: true
-  name 'Changa One'
-  homepage 'https://fonts.google.com/specimen/Changa+One'
+  name "Changa One"
+  homepage "https://fonts.google.com/specimen/Changa+One"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'ChangaOne-Italic.ttf'
-  font 'ChangaOne-Regular.ttf'
+  font "ChangaOne-Italic.ttf"
+  font "ChangaOne-Regular.ttf"
 end
