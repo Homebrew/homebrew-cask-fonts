ed10f473aa2a410dd186059294f688c4ef175409
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:53 +0100

    font-noto-sans-kannada.rb: fix for new style

--- Casks/font-noto-sans-kannada.rb
+++ Casks/font-noto-sans-kannada.rb
@@ -1,12 +1,12 @@
-cask 'font-noto-sans-kannada' do
+cask "font-noto-sans-kannada" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKannada-unhinted.zip'
-  name 'Noto Sans Kannada'
-  homepage 'https://www.google.com/get/noto/#sans-knda'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansKannada-unhinted.zip"
+  name "Noto Sans Kannada"
+  homepage "https://www.google.com/get/noto/#sans-knda"
 
-  font 'NotoSansKannada-Bold.ttf'
-  font 'NotoSansKannada-Regular.ttf'
+  font "NotoSansKannada-Bold.ttf"
+  font "NotoSansKannada-Regular.ttf"
 end
