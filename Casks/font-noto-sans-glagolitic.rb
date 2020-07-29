45dbe3cd823a2512eeecc5ee37c56bcf6ce78e3c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:52 +0100

    font-noto-sans-glagolitic.rb: fix for new style

--- Casks/font-noto-sans-glagolitic.rb
+++ Casks/font-noto-sans-glagolitic.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-glagolitic' do
+cask "font-noto-sans-glagolitic" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGlagolitic-unhinted.zip'
-  name 'Noto Sans Glagolitic'
-  homepage 'https://www.google.com/get/noto/#sans-glag'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGlagolitic-unhinted.zip"
+  name "Noto Sans Glagolitic"
+  homepage "https://www.google.com/get/noto/#sans-glag"
 
-  font 'NotoSansGlagolitic-Regular.ttf'
+  font "NotoSansGlagolitic-Regular.ttf"
 end
