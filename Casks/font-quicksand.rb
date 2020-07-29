90802638c217698242a94008d3add280f32073ae
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:08 +0100

    font-quicksand.rb: fix for new style

--- Casks/font-quicksand.rb
+++ Casks/font-quicksand.rb
@@ -1,11 +1,11 @@
-cask 'font-quicksand' do
+cask "font-quicksand" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/quicksand/Quicksand%5Bwght%5D.ttf'
-  name 'Quicksand'
-  homepage 'https://fonts.google.com/specimen/Quicksand'
+  url "https://github.com/google/fonts/raw/master/ofl/quicksand/Quicksand%5Bwght%5D.ttf"
+  name "Quicksand"
+  homepage "https://fonts.google.com/specimen/Quicksand"
 
-  font 'Quicksand[wght].ttf'
+  font "Quicksand[wght].ttf"
 end
