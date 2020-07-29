80b241668d41bd1ae63d93657c2de21276f0a25a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:11 +0100

    font-robotomono-nerd-font.rb: fix for new style

--- Casks/font-robotomono-nerd-font.rb
+++ Casks/font-robotomono-nerd-font.rb
@@ -1,30 +1,30 @@
-cask 'font-robotomono-nerd-font' do
-  version '2.1.0'
-  sha256 '1dcd5d319bb70c098cb3499059f1aa7536be4c59399724db0de833c07eca0bda'
+cask "font-robotomono-nerd-font" do
+  version "2.1.0"
+  sha256 "1dcd5d319bb70c098cb3499059f1aa7536be4c59399724db0de833c07eca0bda"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/RobotoMono.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'RobotoMono Nerd Font (RobotoMono)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "RobotoMono Nerd Font (RobotoMono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Roboto Mono Medium Nerd Font Complete.ttf'
-  font 'Roboto Mono Medium Italic Nerd Font Complete.ttf'
-  font 'Roboto Mono Light Italic Nerd Font Complete.ttf'
-  font 'Roboto Mono Italic Nerd Font Complete.ttf'
-  font 'Roboto Mono Bold Italic Nerd Font Complete.ttf'
-  font 'Roboto Mono Bold Nerd Font Complete.ttf'
-  font 'Roboto Mono Nerd Font Complete.ttf'
-  font 'Roboto Mono Thin Nerd Font Complete.ttf'
-  font 'Roboto Mono Thin Italic Nerd Font Complete.ttf'
-  font 'Roboto Mono Light Nerd Font Complete.ttf'
-  font 'Roboto Mono Medium Nerd Font Complete Mono.ttf'
-  font 'Roboto Mono Medium Italic Nerd Font Complete Mono.ttf'
-  font 'Roboto Mono Light Italic Nerd Font Complete Mono.ttf'
-  font 'Roboto Mono Italic Nerd Font Complete Mono.ttf'
-  font 'Roboto Mono Bold Italic Nerd Font Complete Mono.ttf'
-  font 'Roboto Mono Bold Nerd Font Complete Mono.ttf'
-  font 'Roboto Mono Nerd Font Complete Mono.ttf'
-  font 'Roboto Mono Thin Nerd Font Complete Mono.ttf'
-  font 'Roboto Mono Thin Italic Nerd Font Complete Mono.ttf'
-  font 'Roboto Mono Light Nerd Font Complete Mono.ttf'
+  font "Roboto Mono Medium Nerd Font Complete.ttf"
+  font "Roboto Mono Medium Italic Nerd Font Complete.ttf"
+  font "Roboto Mono Light Italic Nerd Font Complete.ttf"
+  font "Roboto Mono Italic Nerd Font Complete.ttf"
+  font "Roboto Mono Bold Italic Nerd Font Complete.ttf"
+  font "Roboto Mono Bold Nerd Font Complete.ttf"
+  font "Roboto Mono Nerd Font Complete.ttf"
+  font "Roboto Mono Thin Nerd Font Complete.ttf"
+  font "Roboto Mono Thin Italic Nerd Font Complete.ttf"
+  font "Roboto Mono Light Nerd Font Complete.ttf"
+  font "Roboto Mono Medium Nerd Font Complete Mono.ttf"
+  font "Roboto Mono Medium Italic Nerd Font Complete Mono.ttf"
+  font "Roboto Mono Light Italic Nerd Font Complete Mono.ttf"
+  font "Roboto Mono Italic Nerd Font Complete Mono.ttf"
+  font "Roboto Mono Bold Italic Nerd Font Complete Mono.ttf"
+  font "Roboto Mono Bold Nerd Font Complete Mono.ttf"
+  font "Roboto Mono Nerd Font Complete Mono.ttf"
+  font "Roboto Mono Thin Nerd Font Complete Mono.ttf"
+  font "Roboto Mono Thin Italic Nerd Font Complete Mono.ttf"
+  font "Roboto Mono Light Nerd Font Complete Mono.ttf"
 end
