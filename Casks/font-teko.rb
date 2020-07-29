5e4891f87c5a4a3d5169504d9019ac5d197d4f11
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:23 +0100

    font-teko.rb: fix for new style

--- Casks/font-teko.rb
+++ Casks/font-teko.rb
@@ -1,19 +1,19 @@
-cask 'font-teko' do
+cask "font-teko" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/teko',
+  url "https://github.com/google/fonts/trunk/ofl/teko",
       using:      :svn,
       trust_cert: true
-  name 'Teko'
-  homepage 'https://fonts.google.com/specimen/Teko'
+  name "Teko"
+  homepage "https://fonts.google.com/specimen/Teko"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Teko-Bold.ttf'
-  font 'Teko-Light.ttf'
-  font 'Teko-Medium.ttf'
-  font 'Teko-Regular.ttf'
-  font 'Teko-SemiBold.ttf'
+  font "Teko-Bold.ttf"
+  font "Teko-Light.ttf"
+  font "Teko-Medium.ttf"
+  font "Teko-Regular.ttf"
+  font "Teko-SemiBold.ttf"
 end
