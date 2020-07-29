b5708abf3601407f03959c13d8d1b9256287dbdc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:30 +0100

    font-kurale.rb: fix for new style

--- Casks/font-kurale.rb
+++ Casks/font-kurale.rb
@@ -1,11 +1,11 @@
-cask 'font-kurale' do
+cask "font-kurale" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/kurale/Kurale-Regular.ttf'
-  name 'Kurale'
-  homepage 'https://fonts.google.com/specimen/Kurale'
+  url "https://github.com/google/fonts/raw/master/ofl/kurale/Kurale-Regular.ttf"
+  name "Kurale"
+  homepage "https://fonts.google.com/specimen/Kurale"
 
-  font 'Kurale-Regular.ttf'
+  font "Kurale-Regular.ttf"
 end
