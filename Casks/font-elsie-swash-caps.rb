e9ebae7aa2b23bdbc855dac9378f656a4ec554f1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:47 +0100

    font-elsie-swash-caps.rb: fix for new style

--- Casks/font-elsie-swash-caps.rb
+++ Casks/font-elsie-swash-caps.rb
@@ -1,16 +1,16 @@
-cask 'font-elsie-swash-caps' do
+cask "font-elsie-swash-caps" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/elsieswashcaps',
+  url "https://github.com/google/fonts/trunk/ofl/elsieswashcaps",
       using:      :svn,
       trust_cert: true
-  name 'Elsie Swash Caps'
-  homepage 'https://fonts.google.com/specimen/Elsie+Swash+Caps'
+  name "Elsie Swash Caps"
+  homepage "https://fonts.google.com/specimen/Elsie+Swash+Caps"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'ElsieSwashCaps-Black.ttf'
-  font 'ElsieSwashCaps-Regular.ttf'
+  font "ElsieSwashCaps-Black.ttf"
+  font "ElsieSwashCaps-Regular.ttf"
 end
