a37ebab6b0c82677ea04b519833389034d758aeb
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:52 +0100

    font-fauna-one.rb: fix for new style

--- Casks/font-fauna-one.rb
+++ Casks/font-fauna-one.rb
@@ -1,11 +1,11 @@
-cask 'font-fauna-one' do
+cask "font-fauna-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/faunaone/FaunaOne-Regular.ttf'
-  name 'Fauna One'
-  homepage 'https://fonts.google.com/specimen/Fauna+One'
+  url "https://github.com/google/fonts/raw/master/ofl/faunaone/FaunaOne-Regular.ttf"
+  name "Fauna One"
+  homepage "https://fonts.google.com/specimen/Fauna+One"
 
-  font 'FaunaOne-Regular.ttf'
+  font "FaunaOne-Regular.ttf"
 end
