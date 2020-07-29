aa18d3c3910b9737cc2fed4d184b342d6ff73f9b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:53 +0100

    font-noto-sans-gujarati.rb: fix for new style

--- Casks/font-noto-sans-gujarati.rb
+++ Casks/font-noto-sans-gujarati.rb
@@ -1,12 +1,12 @@
-cask 'font-noto-sans-gujarati' do
+cask "font-noto-sans-gujarati" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGujarati-unhinted.zip'
-  name 'Noto Sans Gujarati'
-  homepage 'https://www.google.com/get/noto/#sans-gujr'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGujarati-unhinted.zip"
+  name "Noto Sans Gujarati"
+  homepage "https://www.google.com/get/noto/#sans-gujr"
 
-  font 'NotoSansGujarati-Bold.ttf'
-  font 'NotoSansGujarati-Regular.ttf'
+  font "NotoSansGujarati-Bold.ttf"
+  font "NotoSansGujarati-Regular.ttf"
 end
