cfe5b003d39fadb4a9e20a32926de60f7f89bb3f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:01 +0100

    font-germania-one.rb: fix for new style

--- Casks/font-germania-one.rb
+++ Casks/font-germania-one.rb
@@ -1,11 +1,11 @@
-cask 'font-germania-one' do
+cask "font-germania-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/germaniaone/GermaniaOne-Regular.ttf'
-  name 'Germania One'
-  homepage 'https://fonts.google.com/specimen/Germania+One'
+  url "https://github.com/google/fonts/raw/master/ofl/germaniaone/GermaniaOne-Regular.ttf"
+  name "Germania One"
+  homepage "https://fonts.google.com/specimen/Germania+One"
 
-  font 'GermaniaOne-Regular.ttf'
+  font "GermaniaOne-Regular.ttf"
 end
