05a7c26a27710ac1e7fc08783685e8089c537d39
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:51 +0100

    font-noto-sans-balinese.rb: fix for new style

--- Casks/font-noto-sans-balinese.rb
+++ Casks/font-noto-sans-balinese.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-balinese' do
+cask "font-noto-sans-balinese" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBalinese-unhinted.zip'
-  name 'Noto Sans Balinese'
-  homepage 'https://www.google.com/get/noto/#sans-bali'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBalinese-unhinted.zip"
+  name "Noto Sans Balinese"
+  homepage "https://www.google.com/get/noto/#sans-bali"
 
-  font 'NotoSansBalinese-Regular.ttf'
+  font "NotoSansBalinese-Regular.ttf"
 end
