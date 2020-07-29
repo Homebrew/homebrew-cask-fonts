2504d0eb04f4c014b16ee68c6b05698df1740dd0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:12 +0100

    font-rosario.rb: fix for new style

--- Casks/font-rosario.rb
+++ Casks/font-rosario.rb
@@ -1,16 +1,16 @@
-cask 'font-rosario' do
+cask "font-rosario" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/rosario',
+  url "https://github.com/google/fonts/trunk/ofl/rosario",
       using:      :svn,
       trust_cert: true
-  name 'Rosario'
-  homepage 'https://fonts.google.com/specimen/Rosario'
+  name "Rosario"
+  homepage "https://fonts.google.com/specimen/Rosario"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Rosario-Italic[wght].ttf'
-  font 'Rosario[wght].ttf'
+  font "Rosario-Italic[wght].ttf"
+  font "Rosario[wght].ttf"
 end
