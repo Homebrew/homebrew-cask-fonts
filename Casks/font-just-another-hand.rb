ce2960a3d81b390baeb5ae71b1ac2eb68c0df9ff
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:25 +0100

    font-just-another-hand.rb: fix for new style

--- Casks/font-just-another-hand.rb
+++ Casks/font-just-another-hand.rb
@@ -1,11 +1,11 @@
-cask 'font-just-another-hand' do
+cask "font-just-another-hand" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/justanotherhand/JustAnotherHand-Regular.ttf'
-  name 'Just Another Hand'
-  homepage 'https://fonts.google.com/specimen/Just+Another+Hand'
+  url "https://github.com/google/fonts/raw/master/apache/justanotherhand/JustAnotherHand-Regular.ttf"
+  name "Just Another Hand"
+  homepage "https://fonts.google.com/specimen/Just+Another+Hand"
 
-  font 'JustAnotherHand-Regular.ttf'
+  font "JustAnotherHand-Regular.ttf"
 end
