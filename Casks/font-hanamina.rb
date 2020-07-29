5e63914701ad117c30068df56bd58a7c8686b6d5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:08 +0100

    font-hanamina.rb: fix for new style

--- Casks/font-hanamina.rb
+++ Casks/font-hanamina.rb
@@ -1,12 +1,12 @@
-cask 'font-hanamina' do
-  version '2017.09.04,68253'
-  sha256 '571cd4a09ae7da0c642d640fc2442c050aa450ebb0587a95cdd097d41a9c9572'
+cask "font-hanamina" do
+  version "2017.09.04,68253"
+  sha256 "571cd4a09ae7da0c642d640fc2442c050aa450ebb0587a95cdd097d41a9c9572"
 
   # osdn.dl.osdn.jp/hanazono-font/ was verified as official when first introduced to the cask
   url "https://osdn.dl.osdn.jp/hanazono-font/#{version.after_comma}/hanazono-#{version.before_comma.no_dots}.zip"
-  name 'HanaMinA'
-  homepage 'https://fonts.jp/hanazono/'
+  name "HanaMinA"
+  homepage "https://fonts.jp/hanazono/"
 
-  font 'HanaMinA.ttf'
-  font 'HanaMinB.ttf'
+  font "HanaMinA.ttf"
+  font "HanaMinB.ttf"
 end
