a4993a8352466c97712f1dca9e6fc7715942c8b7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:19 +0100

    font-bigelow-rules.rb: fix for new style

--- Casks/font-bigelow-rules.rb
+++ Casks/font-bigelow-rules.rb
@@ -1,11 +1,11 @@
-cask 'font-bigelow-rules' do
+cask "font-bigelow-rules" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bigelowrules/BigelowRules-Regular.ttf'
-  name 'Bigelow Rules'
-  homepage 'https://fonts.google.com/specimen/Bigelow+Rules'
+  url "https://github.com/google/fonts/raw/master/ofl/bigelowrules/BigelowRules-Regular.ttf"
+  name "Bigelow Rules"
+  homepage "https://fonts.google.com/specimen/Bigelow+Rules"
 
-  font 'BigelowRules-Regular.ttf'
+  font "BigelowRules-Regular.ttf"
 end
