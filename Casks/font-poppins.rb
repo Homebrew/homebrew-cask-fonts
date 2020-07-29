65415671dc24efa7a92f34a7ce6c636867f71805
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:06 +0100

    font-poppins.rb: fix for new style

--- Casks/font-poppins.rb
+++ Casks/font-poppins.rb
@@ -1,15 +1,15 @@
-cask 'font-poppins' do
-  version '3.200'
-  sha256 'a05ecec9599a698fd7a52fa492c868af1fa3462be33bb038d14dbbac7a5456dc'
+cask "font-poppins" do
+  version "3.200"
+  sha256 "a05ecec9599a698fd7a52fa492c868af1fa3462be33bb038d14dbbac7a5456dc"
 
   url "https://github.com/itfoundry/Poppins/releases/download/v#{version}/Poppins-#{version.no_dots}-OTF.zip"
-  appcast 'https://github.com/itfoundry/poppins/releases.atom'
-  name 'Poppins'
-  homepage 'https://github.com/itfoundry/poppins'
+  appcast "https://github.com/itfoundry/poppins/releases.atom"
+  name "Poppins"
+  homepage "https://github.com/itfoundry/poppins"
 
-  font 'Poppins-Bold.otf'
-  font 'Poppins-Light.otf'
-  font 'Poppins-Medium.otf'
-  font 'Poppins-Regular.otf'
-  font 'Poppins-SemiBold.otf'
+  font "Poppins-Bold.otf"
+  font "Poppins-Light.otf"
+  font "Poppins-Medium.otf"
+  font "Poppins-Regular.otf"
+  font "Poppins-SemiBold.otf"
 end
