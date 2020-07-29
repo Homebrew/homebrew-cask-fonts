0076b12cce7374cd9d557edd645d25ec14be2478
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:27 +0100

    font-cantata-one.rb: fix for new style

--- Casks/font-cantata-one.rb
+++ Casks/font-cantata-one.rb
@@ -1,11 +1,11 @@
-cask 'font-cantata-one' do
+cask "font-cantata-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/cantataone/CantataOne-Regular.ttf'
-  name 'Cantata One'
-  homepage 'https://fonts.google.com/specimen/Cantata+One'
+  url "https://github.com/google/fonts/raw/master/ofl/cantataone/CantataOne-Regular.ttf"
+  name "Cantata One"
+  homepage "https://fonts.google.com/specimen/Cantata+One"
 
-  font 'CantataOne-Regular.ttf'
+  font "CantataOne-Regular.ttf"
 end
