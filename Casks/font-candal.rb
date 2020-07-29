a3be8458e59b0291a2f59f88b75f958b3724fbf3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:26 +0100

    font-candal.rb: fix for new style

--- Casks/font-candal.rb
+++ Casks/font-candal.rb
@@ -1,11 +1,11 @@
-cask 'font-candal' do
+cask "font-candal" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/candal/Candal.ttf'
-  name 'Candal'
-  homepage 'https://fonts.google.com/specimen/Candal'
+  url "https://github.com/google/fonts/raw/master/ofl/candal/Candal.ttf"
+  name "Candal"
+  homepage "https://fonts.google.com/specimen/Candal"
 
-  font 'Candal.ttf'
+  font "Candal.ttf"
 end
