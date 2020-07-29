51a17d1b826a319336e85eff083ba10a4a9621fa
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:31 +0100

    font-lao-muang-khong.rb: fix for new style

--- Casks/font-lao-muang-khong.rb
+++ Casks/font-lao-muang-khong.rb
@@ -1,11 +1,11 @@
-cask 'font-lao-muang-khong' do
+cask "font-lao-muang-khong" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/laomuangkhong/LaoMuangKhong-Regular.ttf'
-  name 'Lao Muang Khong'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/laomuangkhong/LaoMuangKhong-Regular.ttf"
+  name "Lao Muang Khong"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'LaoMuangKhong-Regular.ttf'
+  font "LaoMuangKhong-Regular.ttf"
 end
