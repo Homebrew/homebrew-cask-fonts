ce9b1c0f677faad34d2b6264beabbecc6ec51029
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:00 +0100

    font-andale-mono.rb: fix for new style

--- Casks/font-andale-mono.rb
+++ Casks/font-andale-mono.rb
@@ -1,10 +1,10 @@
-cask 'font-andale-mono' do
+cask "font-andale-mono" do
   version :latest
   sha256 :no_check
 
-  url 'https://downloads.sourceforge.net/corefonts/andale32.exe'
-  name 'Andale Mono'
-  homepage 'https://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  url "https://downloads.sourceforge.net/corefonts/andale32.exe"
+  name "Andale Mono"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
 
-  font 'AndaleMo.TTF'
+  font "AndaleMo.TTF"
 end
