c2b51884fe0ab1c01770fbad82fa790fcb48b44c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:30 +0100

    font-yinmar.rb: fix for new style

--- Casks/font-yinmar.rb
+++ Casks/font-yinmar.rb
@@ -1,11 +1,11 @@
-cask 'font-yinmar' do
+cask "font-yinmar" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/yinmar/Yinmar-Regular.ttf'
-  name 'Yinmar'
-  homepage 'https://fonts.google.com/specimen/Yinmar'
+  url "https://github.com/google/fonts/raw/master/ofl/yinmar/Yinmar-Regular.ttf"
+  name "Yinmar"
+  homepage "https://fonts.google.com/specimen/Yinmar"
 
-  font 'Yinmar-Regular.ttf'
+  font "Yinmar-Regular.ttf"
 end
