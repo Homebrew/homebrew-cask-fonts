93d2ca7582f2e37b9cd106cded7794be9a4ffd00
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:30 +0100

    font-zcool-xiaowei.rb: fix for new style

--- Casks/font-zcool-xiaowei.rb
+++ Casks/font-zcool-xiaowei.rb
@@ -1,11 +1,11 @@
-cask 'font-zcool-xiaowei' do
+cask "font-zcool-xiaowei" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/zcoolxiaowei/ZCOOLXiaoWei-Regular.ttf'
-  name 'ZCOOL XiaoWei'
-  homepage 'https://fonts.google.com/specimen/ZCOOL+XiaoWei'
+  url "https://github.com/google/fonts/raw/master/ofl/zcoolxiaowei/ZCOOLXiaoWei-Regular.ttf"
+  name "ZCOOL XiaoWei"
+  homepage "https://fonts.google.com/specimen/ZCOOL+XiaoWei"
 
-  font 'ZCOOLXiaoWei-Regular.ttf'
+  font "ZCOOLXiaoWei-Regular.ttf"
 end
