19f688987961ef19c89915e8672d9137ad08bd8c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:24 +0100

    font-butcherman-caps.rb: fix for new style

--- Casks/font-butcherman-caps.rb
+++ Casks/font-butcherman-caps.rb
@@ -1,11 +1,11 @@
-cask 'font-butcherman-caps' do
+cask "font-butcherman-caps" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/butchermancaps/ButchermanCaps-Regular.ttf'
-  name 'Butcherman Caps'
-  homepage 'https://fonts.google.com/specimen/Butcherman+Caps'
+  url "https://github.com/google/fonts/raw/master/ofl/butchermancaps/ButchermanCaps-Regular.ttf"
+  name "Butcherman Caps"
+  homepage "https://fonts.google.com/specimen/Butcherman+Caps"
 
-  font 'ButchermanCaps-Regular.ttf'
+  font "ButchermanCaps-Regular.ttf"
 end
