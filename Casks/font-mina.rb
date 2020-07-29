d4b359b66fbbf0347d593d04b0d4331faaa3fc5f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:43 +0100

    font-mina.rb: fix for new style

--- Casks/font-mina.rb
+++ Casks/font-mina.rb
@@ -1,16 +1,16 @@
-cask 'font-mina' do
+cask "font-mina" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/mina',
+  url "https://github.com/google/fonts/trunk/ofl/mina",
       using:      :svn,
       trust_cert: true
-  name 'Mina'
-  homepage 'https://fonts.google.com/specimen/Mina'
+  name "Mina"
+  homepage "https://fonts.google.com/specimen/Mina"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Mina-Bold.ttf'
-  font 'Mina-Regular.ttf'
+  font "Mina-Bold.ttf"
+  font "Mina-Regular.ttf"
 end
