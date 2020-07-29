223afe02bf15ad72384c3a2f1fe34a2496d8c94c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:53 +0100

    font-noto-sans-gothic.rb: fix for new style

--- Casks/font-noto-sans-gothic.rb
+++ Casks/font-noto-sans-gothic.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-gothic' do
+cask "font-noto-sans-gothic" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGothic-unhinted.zip'
-  name 'Noto Sans Gothic'
-  homepage 'https://www.google.com/get/noto/#sans-goth'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansGothic-unhinted.zip"
+  name "Noto Sans Gothic"
+  homepage "https://www.google.com/get/noto/#sans-goth"
 
-  font 'NotoSansGothic-Regular.ttf'
+  font "NotoSansGothic-Regular.ttf"
 end
