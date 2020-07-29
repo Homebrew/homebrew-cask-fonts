46e1fd44c4a72b901ce962039de030b9576e7643
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:04 +0100

    font-peddana.rb: fix for new style

--- Casks/font-peddana.rb
+++ Casks/font-peddana.rb
@@ -1,11 +1,11 @@
-cask 'font-peddana' do
+cask "font-peddana" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/peddana/Peddana-Regular.ttf'
-  name 'Peddana'
-  homepage 'https://fonts.google.com/specimen/Peddana'
+  url "https://github.com/google/fonts/raw/master/ofl/peddana/Peddana-Regular.ttf"
+  name "Peddana"
+  homepage "https://fonts.google.com/specimen/Peddana"
 
-  font 'Peddana-Regular.ttf'
+  font "Peddana-Regular.ttf"
 end
