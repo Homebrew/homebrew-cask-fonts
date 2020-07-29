c97375071c030101da44b8331c2fa83f869a7cd7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:56 +0100

    font-noto-sans-syriac-eastern.rb: fix for new style

--- Casks/font-noto-sans-syriac-eastern.rb
+++ Casks/font-noto-sans-syriac-eastern.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-syriac-eastern' do
+cask "font-noto-sans-syriac-eastern" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEastern-unhinted.zip'
-  name 'Noto Sans Syriac Eastern'
-  homepage 'https://www.google.com/get/noto/#sans-syrc-eastern'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEastern-unhinted.zip"
+  name "Noto Sans Syriac Eastern"
+  homepage "https://www.google.com/get/noto/#sans-syrc-eastern"
 
-  font 'NotoSansSyriacEastern-Regular.ttf'
+  font "NotoSansSyriacEastern-Regular.ttf"
 end
