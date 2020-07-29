1828cc25d155cefacfb31931cda83363e0240355
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:03 +0100

    font-asar.rb: fix for new style

--- Casks/font-asar.rb
+++ Casks/font-asar.rb
@@ -1,11 +1,11 @@
-cask 'font-asar' do
+cask "font-asar" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/asar/Asar-Regular.ttf'
-  name 'Asar'
-  homepage 'https://fonts.google.com/specimen/Asar'
+  url "https://github.com/google/fonts/raw/master/ofl/asar/Asar-Regular.ttf"
+  name "Asar"
+  homepage "https://fonts.google.com/specimen/Asar"
 
-  font 'Asar-Regular.ttf'
+  font "Asar-Regular.ttf"
 end
