49ec069f57fa635f023bbd8940c87c8152f3334d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:47 +0100

    font-muli.rb: fix for new style

--- Casks/font-muli.rb
+++ Casks/font-muli.rb
@@ -1,16 +1,16 @@
-cask 'font-muli' do
+cask "font-muli" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/muli',
+  url "https://github.com/google/fonts/trunk/ofl/muli",
       using:      :svn,
       trust_cert: true
-  name 'Muli'
-  homepage 'https://fonts.google.com/specimen/Muli'
+  name "Muli"
+  homepage "https://fonts.google.com/specimen/Muli"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Muli-Italic[wght].ttf'
-  font 'Muli[wght].ttf'
+  font "Muli-Italic[wght].ttf"
+  font "Muli[wght].ttf"
 end
