0b952e5e5b4a719c33e85fb5e10e0d3c02592103
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:30 +0100

    font-charmonman.rb: fix for new style

--- Casks/font-charmonman.rb
+++ Casks/font-charmonman.rb
@@ -1,16 +1,16 @@
-cask 'font-charmonman' do
+cask "font-charmonman" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/charmonman',
+  url "https://github.com/google/fonts/trunk/ofl/charmonman",
       using:      :svn,
       trust_cert: true
-  name 'Charmonman'
-  homepage 'https://fonts.google.com/specimen/Charmonman'
+  name "Charmonman"
+  homepage "https://fonts.google.com/specimen/Charmonman"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Charmonman-Bold.ttf'
-  font 'Charmonman-Regular.ttf'
+  font "Charmonman-Bold.ttf"
+  font "Charmonman-Regular.ttf"
 end
