2401496e3654c5a5162188144274ec3efc7ce789
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:00 +0100

    font-genyomin.rb: fix for new style

--- Casks/font-genyomin.rb
+++ Casks/font-genyomin.rb
@@ -1,17 +1,17 @@
-cask 'font-genyomin' do
-  version '1.501'
-  sha256 '6feafd61b261bdeccc4e69eba8661f5f1dd7ec79d8ef3a78b19fb20759cf366f'
+cask "font-genyomin" do
+  version "1.501"
+  sha256 "6feafd61b261bdeccc4e69eba8661f5f1dd7ec79d8ef3a78b19fb20759cf366f"
 
   url "https://github.com/ButTaiwan/genyo-font/releases/download/v#{version}/GenYoMin.zip"
-  appcast 'https://github.com/ButTaiwan/genyo-font/releases.atom'
-  name 'GenYoMin'
-  homepage 'https://github.com/ButTaiwan/genyo-font'
+  appcast "https://github.com/ButTaiwan/genyo-font/releases.atom"
+  name "GenYoMin"
+  homepage "https://github.com/ButTaiwan/genyo-font"
 
-  font 'GenYoMin-B.ttc'
-  font 'GenYoMin-EL.ttc'
-  font 'GenYoMin-H.ttc'
-  font 'GenYoMin-L.ttc'
-  font 'GenYoMin-M.ttc'
-  font 'GenYoMin-R.ttc'
-  font 'GenYoMin-SB.ttc'
+  font "GenYoMin-B.ttc"
+  font "GenYoMin-EL.ttc"
+  font "GenYoMin-H.ttc"
+  font "GenYoMin-L.ttc"
+  font "GenYoMin-M.ttc"
+  font "GenYoMin-R.ttc"
+  font "GenYoMin-SB.ttc"
 end
