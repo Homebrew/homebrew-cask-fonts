1387d7bb6b0c7f26b792248a5ff42b47ed4b8e18
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:08 +0100

    font-hanuman.rb: fix for new style

--- Casks/font-hanuman.rb
+++ Casks/font-hanuman.rb
@@ -1,16 +1,16 @@
-cask 'font-hanuman' do
+cask "font-hanuman" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/hanuman',
+  url "https://github.com/google/fonts/trunk/ofl/hanuman",
       using:      :svn,
       trust_cert: true
-  name 'Hanuman'
-  homepage 'https://fonts.google.com/specimen/Hanuman'
+  name "Hanuman"
+  homepage "https://fonts.google.com/specimen/Hanuman"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Hanuman-Bold.ttf'
-  font 'Hanuman-Regular.ttf'
+  font "Hanuman-Bold.ttf"
+  font "Hanuman-Regular.ttf"
 end
