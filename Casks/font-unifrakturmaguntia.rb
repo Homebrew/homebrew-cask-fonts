468c9620b6e24a14216f1e45860337a50ba1b9d0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:27 +0100

    font-unifrakturmaguntia.rb: fix for new style

--- Casks/font-unifrakturmaguntia.rb
+++ Casks/font-unifrakturmaguntia.rb
@@ -1,11 +1,11 @@
-cask 'font-unifrakturmaguntia' do
+cask "font-unifrakturmaguntia" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/unifrakturmaguntia/UnifrakturMaguntia-Book.ttf'
-  name 'UnifrakturMaguntia'
-  homepage 'https://fonts.google.com/specimen/UnifrakturMaguntia'
+  url "https://github.com/google/fonts/raw/master/ofl/unifrakturmaguntia/UnifrakturMaguntia-Book.ttf"
+  name "UnifrakturMaguntia"
+  homepage "https://fonts.google.com/specimen/UnifrakturMaguntia"
 
-  font 'UnifrakturMaguntia-Book.ttf'
+  font "UnifrakturMaguntia-Book.ttf"
 end
