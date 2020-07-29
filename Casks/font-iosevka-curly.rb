0df4c7a174edae8445fa66ace718181301237d4d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:18 +0100

    font-iosevka-curly.rb: fix for new style

--- Casks/font-iosevka-curly.rb
+++ Casks/font-iosevka-curly.rb
@@ -1,19 +1,19 @@
-cask 'font-iosevka-curly' do
-  version '3.3.1'
-  sha256 'c546580fe938c6bb7809350ef40d1005fb617964d92092fef6787a1b56c892a0'
+cask "font-iosevka-curly" do
+  version "3.3.1"
+  sha256 "c546580fe938c6bb7809350ef40d1005fb617964d92092fef6787a1b56c892a0"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-#{version}.zip"
-  appcast 'https://github.com/be5invis/Iosevka/releases.atom'
-  name 'Iosevka Curly'
-  homepage 'https://github.com/be5invis/Iosevka/'
+  appcast "https://github.com/be5invis/Iosevka/releases.atom"
+  name "Iosevka Curly"
+  homepage "https://github.com/be5invis/Iosevka/"
 
-  font 'iosevka-curly-bold.ttc'
-  font 'iosevka-curly-extrabold.ttc'
-  font 'iosevka-curly-extralight.ttc'
-  font 'iosevka-curly-heavy.ttc'
-  font 'iosevka-curly-light.ttc'
-  font 'iosevka-curly-medium.ttc'
-  font 'iosevka-curly-regular.ttc'
-  font 'iosevka-curly-semibold.ttc'
-  font 'iosevka-curly-thin.ttc'
+  font "iosevka-curly-bold.ttc"
+  font "iosevka-curly-extrabold.ttc"
+  font "iosevka-curly-extralight.ttc"
+  font "iosevka-curly-heavy.ttc"
+  font "iosevka-curly-light.ttc"
+  font "iosevka-curly-medium.ttc"
+  font "iosevka-curly-regular.ttc"
+  font "iosevka-curly-semibold.ttc"
+  font "iosevka-curly-thin.ttc"
 end
