c3401eeb509aec24a9bc3463ca58c164b54bb83c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:53 +0100

    font-noto-sans-inscriptional-pahlavi.rb: fix for new style

--- Casks/font-noto-sans-inscriptional-pahlavi.rb
+++ Casks/font-noto-sans-inscriptional-pahlavi.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-inscriptional-pahlavi' do
+cask "font-noto-sans-inscriptional-pahlavi" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansInscriptionalPahlavi-unhinted.zip'
-  name 'Noto Sans Inscriptional Pahlavi'
-  homepage 'https://www.google.com/get/noto/#sans-phli'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansInscriptionalPahlavi-unhinted.zip"
+  name "Noto Sans Inscriptional Pahlavi"
+  homepage "https://www.google.com/get/noto/#sans-phli"
 
-  font 'NotoSansInscriptionalPahlavi-Regular.ttf'
+  font "NotoSansInscriptionalPahlavi-Regular.ttf"
 end
