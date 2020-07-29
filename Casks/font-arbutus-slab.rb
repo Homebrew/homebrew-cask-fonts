570e580791fba18bef5779128982ff4604dda8be
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:50 +0100

    font-arbutus-slab.rb: fix for new style

--- Casks/font-arbutus-slab.rb
+++ Casks/font-arbutus-slab.rb
@@ -1,11 +1,11 @@
-cask 'font-arbutus-slab' do
+cask "font-arbutus-slab" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/arbutusslab/ArbutusSlab-Regular.ttf'
-  name 'Arbutus Slab'
-  homepage 'https://fonts.google.com/specimen/Arbutus+Slab'
+  url "https://github.com/google/fonts/raw/master/ofl/arbutusslab/ArbutusSlab-Regular.ttf"
+  name "Arbutus Slab"
+  homepage "https://fonts.google.com/specimen/Arbutus+Slab"
 
-  font 'ArbutusSlab-Regular.ttf'
+  font "ArbutusSlab-Regular.ttf"
 end
