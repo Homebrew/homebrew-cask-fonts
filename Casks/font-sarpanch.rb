d61f15939198c8b3d215227d1f4cf390907bd40d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:14 +0100

    font-sarpanch.rb: fix for new style

--- Casks/font-sarpanch.rb
+++ Casks/font-sarpanch.rb
@@ -1,20 +1,20 @@
-cask 'font-sarpanch' do
+cask "font-sarpanch" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/sarpanch',
+  url "https://github.com/google/fonts/trunk/ofl/sarpanch",
       using:      :svn,
       trust_cert: true
-  name 'Sarpanch'
-  homepage 'https://fonts.google.com/specimen/Sarpanch'
+  name "Sarpanch"
+  homepage "https://fonts.google.com/specimen/Sarpanch"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Sarpanch-Black.ttf'
-  font 'Sarpanch-Bold.ttf'
-  font 'Sarpanch-ExtraBold.ttf'
-  font 'Sarpanch-Medium.ttf'
-  font 'Sarpanch-Regular.ttf'
-  font 'Sarpanch-SemiBold.ttf'
+  font "Sarpanch-Black.ttf"
+  font "Sarpanch-Bold.ttf"
+  font "Sarpanch-ExtraBold.ttf"
+  font "Sarpanch-Medium.ttf"
+  font "Sarpanch-Regular.ttf"
+  font "Sarpanch-SemiBold.ttf"
 end
