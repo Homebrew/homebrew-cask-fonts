70664e958f64d215cac1f9dde472e163cb969712
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:40 +0100

    font-mclaren.rb: fix for new style

--- Casks/font-mclaren.rb
+++ Casks/font-mclaren.rb
@@ -1,11 +1,11 @@
-cask 'font-mclaren' do
+cask "font-mclaren" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/mclaren/McLaren-Regular.ttf'
-  name 'McLaren'
-  homepage 'https://fonts.google.com/specimen/McLaren'
+  url "https://github.com/google/fonts/raw/master/ofl/mclaren/McLaren-Regular.ttf"
+  name "McLaren"
+  homepage "https://fonts.google.com/specimen/McLaren"
 
-  font 'McLaren-Regular.ttf'
+  font "McLaren-Regular.ttf"
 end
