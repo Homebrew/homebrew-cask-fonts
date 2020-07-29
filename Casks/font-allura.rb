a5284e85343065b9b5ad9565e6656aba84a3975c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:05 +0100

    font-allura.rb: fix for new style

--- Casks/font-allura.rb
+++ Casks/font-allura.rb
@@ -1,11 +1,11 @@
-cask 'font-allura' do
+cask "font-allura" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/allura/Allura-Regular.ttf'
-  name 'Allura'
-  homepage 'https://fonts.google.com/specimen/Allura'
+  url "https://github.com/google/fonts/raw/master/ofl/allura/Allura-Regular.ttf"
+  name "Allura"
+  homepage "https://fonts.google.com/specimen/Allura"
 
-  font 'Allura-Regular.ttf'
+  font "Allura-Regular.ttf"
 end
