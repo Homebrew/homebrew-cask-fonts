ec6cbe9f013475102629f2701777aa29d8bbf17c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:57 +0100

    font-fzshusong-z01.rb: fix for new style

--- Casks/font-fzshusong-z01.rb
+++ Casks/font-fzshusong-z01.rb
@@ -1,10 +1,10 @@
-cask 'font-fzshusong-z01' do
+cask "font-fzshusong-z01" do
   version :latest
   sha256 :no_check
 
-  url 'https://cdn1.foundertype.com/Public/Uploads/ttf/FZSSK.TTF'
-  name 'Fangzheng Shusong GBK'
-  homepage 'https://www.foundertype.com/index.php/FontInfo/index.html?id=151'
+  url "https://cdn1.foundertype.com/Public/Uploads/ttf/FZSSK.TTF"
+  name "Fangzheng Shusong GBK"
+  homepage "https://www.foundertype.com/index.php/FontInfo/index.html?id=151"
 
-  font 'FZSSK.ttf'
+  font "FZSSK.ttf"
 end
