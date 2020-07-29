a96b0f1154327ac2546b2468a82a8d73ec929b2d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:06 +0100

    font-port-lligat-sans.rb: fix for new style

--- Casks/font-port-lligat-sans.rb
+++ Casks/font-port-lligat-sans.rb
@@ -1,11 +1,11 @@
-cask 'font-port-lligat-sans' do
+cask "font-port-lligat-sans" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/portlligatsans/PortLligatSans-Regular.ttf'
-  name 'Port Lligat Sans'
-  homepage 'https://fonts.google.com/specimen/Port+Lligat+Sans'
+  url "https://github.com/google/fonts/raw/master/ofl/portlligatsans/PortLligatSans-Regular.ttf"
+  name "Port Lligat Sans"
+  homepage "https://fonts.google.com/specimen/Port+Lligat+Sans"
 
-  font 'PortLligatSans-Regular.ttf'
+  font "PortLligatSans-Regular.ttf"
 end
