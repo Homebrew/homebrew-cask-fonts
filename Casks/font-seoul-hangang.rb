c8e893be6c05c433dac1e2b60b483d839c05675b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:16 +0100

    font-seoul-hangang.rb: fix for new style

--- Casks/font-seoul-hangang.rb
+++ Casks/font-seoul-hangang.rb
@@ -1,18 +1,18 @@
-cask 'font-seoul-hangang' do
+cask "font-seoul-hangang" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/seoulhangang',
+  url "https://github.com/google/fonts/trunk/ofl/seoulhangang",
       using:      :svn,
       trust_cert: true
-  name 'Seoul Hangang'
-  homepage 'https://fonts.google.com/specimen/Seoul+Hangang'
+  name "Seoul Hangang"
+  homepage "https://fonts.google.com/specimen/Seoul+Hangang"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'SeoulHangang-Bold.ttf'
-  font 'SeoulHangang-ExtraBold.ttf'
-  font 'SeoulHangang-Light.ttf'
-  font 'SeoulHangang-Medium.ttf'
+  font "SeoulHangang-Bold.ttf"
+  font "SeoulHangang-ExtraBold.ttf"
+  font "SeoulHangang-Light.ttf"
+  font "SeoulHangang-Medium.ttf"
 end
