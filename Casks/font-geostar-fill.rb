738026e72f5480e765efb2cade3aafb0567ecd69
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:01 +0100

    font-geostar-fill.rb: fix for new style

--- Casks/font-geostar-fill.rb
+++ Casks/font-geostar-fill.rb
@@ -1,11 +1,11 @@
-cask 'font-geostar-fill' do
+cask "font-geostar-fill" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/geostarfill/GeostarFill-Regular.ttf'
-  name 'Geostar Fill'
-  homepage 'https://fonts.google.com/specimen/Geostar+Fill'
+  url "https://github.com/google/fonts/raw/master/ofl/geostarfill/GeostarFill-Regular.ttf"
+  name "Geostar Fill"
+  homepage "https://fonts.google.com/specimen/Geostar+Fill"
 
-  font 'GeostarFill-Regular.ttf'
+  font "GeostarFill-Regular.ttf"
 end
