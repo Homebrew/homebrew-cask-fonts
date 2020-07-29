0026bcdc777eec0f7b4cfd7e769838edbc8fa1d5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:17 +0100

    font-simonetta.rb: fix for new style

--- Casks/font-simonetta.rb
+++ Casks/font-simonetta.rb
@@ -1,18 +1,18 @@
-cask 'font-simonetta' do
+cask "font-simonetta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/simonetta',
+  url "https://github.com/google/fonts/trunk/ofl/simonetta",
       using:      :svn,
       trust_cert: true
-  name 'Simonetta'
-  homepage 'https://fonts.google.com/specimen/Simonetta'
+  name "Simonetta"
+  homepage "https://fonts.google.com/specimen/Simonetta"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Simonetta-Black.ttf'
-  font 'Simonetta-BlackItalic.ttf'
-  font 'Simonetta-Italic.ttf'
-  font 'Simonetta-Regular.ttf'
+  font "Simonetta-Black.ttf"
+  font "Simonetta-BlackItalic.ttf"
+  font "Simonetta-Italic.ttf"
+  font "Simonetta-Regular.ttf"
 end
