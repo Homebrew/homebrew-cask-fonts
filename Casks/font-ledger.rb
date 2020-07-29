b17c404de4b6eb3bee6151c7341a73f86779de59
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:32 +0100

    font-ledger.rb: fix for new style

--- Casks/font-ledger.rb
+++ Casks/font-ledger.rb
@@ -1,11 +1,11 @@
-cask 'font-ledger' do
+cask "font-ledger" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/ledger/Ledger-Regular.ttf'
-  name 'Ledger'
-  homepage 'https://fonts.google.com/specimen/Ledger'
+  url "https://github.com/google/fonts/raw/master/ofl/ledger/Ledger-Regular.ttf"
+  name "Ledger"
+  homepage "https://fonts.google.com/specimen/Ledger"
 
-  font 'Ledger-Regular.ttf'
+  font "Ledger-Regular.ttf"
 end
