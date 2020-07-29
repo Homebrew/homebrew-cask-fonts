f44f358acd09aec3dd3b2e4e6585e5d4cada23d6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:08 +0100

    font-pt-sans.rb: fix for new style

--- Casks/font-pt-sans.rb
+++ Casks/font-pt-sans.rb
@@ -1,17 +1,17 @@
-cask 'font-pt-sans' do
+cask "font-pt-sans" do
   version :latest
   sha256 :no_check
 
-  url 'https://company.paratype.com/system/attachments/629/original/ptsans.zip'
-  name 'PT Sans'
-  homepage 'https://www.paratype.com/public/'
+  url "https://company.paratype.com/system/attachments/629/original/ptsans.zip"
+  name "PT Sans"
+  homepage "https://www.paratype.com/public/"
 
-  font 'PTC55F.ttf'
-  font 'PTC75F.ttf'
-  font 'PTN57F.ttf'
-  font 'PTN77F.ttf'
-  font 'PTS55F.ttf'
-  font 'PTS56F.ttf'
-  font 'PTS75F.ttf'
-  font 'PTS76F.ttf'
+  font "PTC55F.ttf"
+  font "PTC75F.ttf"
+  font "PTN57F.ttf"
+  font "PTN77F.ttf"
+  font "PTS55F.ttf"
+  font "PTS56F.ttf"
+  font "PTS75F.ttf"
+  font "PTS76F.ttf"
 end
