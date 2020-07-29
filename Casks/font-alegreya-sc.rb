5e76601db11cb4ea464dd0082a12024ba95231b2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:32:32 +0100

    font-alegreya-sc.rb: fix for new style

--- Casks/font-alegreya-sc.rb
+++ Casks/font-alegreya-sc.rb
@@ -1,24 +1,24 @@
-cask 'font-alegreya-sc' do
+cask "font-alegreya-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/alegreyasc',
+  url "https://github.com/google/fonts/trunk/ofl/alegreyasc",
       using:      :svn,
       trust_cert: true
-  name 'Alegreya SC'
-  homepage 'https://fonts.google.com/specimen/Alegreya+SC'
+  name "Alegreya SC"
+  homepage "https://fonts.google.com/specimen/Alegreya+SC"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'AlegreyaSC-Black.ttf'
-  font 'AlegreyaSC-BlackItalic.ttf'
-  font 'AlegreyaSC-Bold.ttf'
-  font 'AlegreyaSC-BoldItalic.ttf'
-  font 'AlegreyaSC-ExtraBold.ttf'
-  font 'AlegreyaSC-ExtraBoldItalic.ttf'
-  font 'AlegreyaSC-Italic.ttf'
-  font 'AlegreyaSC-Medium.ttf'
-  font 'AlegreyaSC-MediumItalic.ttf'
-  font 'AlegreyaSC-Regular.ttf'
+  font "AlegreyaSC-Black.ttf"
+  font "AlegreyaSC-BlackItalic.ttf"
+  font "AlegreyaSC-Bold.ttf"
+  font "AlegreyaSC-BoldItalic.ttf"
+  font "AlegreyaSC-ExtraBold.ttf"
+  font "AlegreyaSC-ExtraBoldItalic.ttf"
+  font "AlegreyaSC-Italic.ttf"
+  font "AlegreyaSC-Medium.ttf"
+  font "AlegreyaSC-MediumItalic.ttf"
+  font "AlegreyaSC-Regular.ttf"
 end
