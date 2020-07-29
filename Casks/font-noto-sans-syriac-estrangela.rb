457f110cb817a5d4bc64b9fc9b9caf31e78ca0ce
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:56 +0100

    font-noto-sans-syriac-estrangela.rb: fix for new style

--- Casks/font-noto-sans-syriac-estrangela.rb
+++ Casks/font-noto-sans-syriac-estrangela.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-syriac-estrangela' do
+cask "font-noto-sans-syriac-estrangela" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEstrangela-unhinted.zip'
-  name 'Noto Sans Syriac Estrangela'
-  homepage 'https://www.google.com/get/noto/#sans-syrc-estrangela'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEstrangela-unhinted.zip"
+  name "Noto Sans Syriac Estrangela"
+  homepage "https://www.google.com/get/noto/#sans-syrc-estrangela"
 
-  font 'NotoSansSyriacEstrangela-Regular.ttf'
+  font "NotoSansSyriacEstrangela-Regular.ttf"
 end
