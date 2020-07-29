8520517ebd1b9c358ec66aa25437a06df8abcbcd
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:56 +0100

    font-architects-daughter.rb: fix for new style

--- Casks/font-architects-daughter.rb
+++ Casks/font-architects-daughter.rb
@@ -1,11 +1,11 @@
-cask 'font-architects-daughter' do
+cask "font-architects-daughter" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/architectsdaughter/ArchitectsDaughter-Regular.ttf'
-  name 'Architects Daughter'
-  homepage 'https://fonts.google.com/specimen/Architects+Daughter'
+  url "https://github.com/google/fonts/raw/master/ofl/architectsdaughter/ArchitectsDaughter-Regular.ttf"
+  name "Architects Daughter"
+  homepage "https://fonts.google.com/specimen/Architects+Daughter"
 
-  font 'ArchitectsDaughter-Regular.ttf'
+  font "ArchitectsDaughter-Regular.ttf"
 end
