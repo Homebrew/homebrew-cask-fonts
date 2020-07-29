0fbc8ba9bd56ad62ef7cea426cc7bc51909f3434
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:18 +0100

    font-iosevka-etoile.rb: fix for new style

--- Casks/font-iosevka-etoile.rb
+++ Casks/font-iosevka-etoile.rb
@@ -1,19 +1,19 @@
-cask 'font-iosevka-etoile' do
-  version '3.3.1'
-  sha256 '407f9c012a4f50bf797dc3fa0d1ea9c549752e6818c9f37ca7ac47e55c0c811e'
+cask "font-iosevka-etoile" do
+  version "3.3.1"
+  sha256 "407f9c012a4f50bf797dc3fa0d1ea9c549752e6818c9f37ca7ac47e55c0c811e"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-etoile-#{version}.zip"
-  appcast 'https://github.com/be5invis/Iosevka/releases.atom'
-  name 'Iosevka Etoile'
-  homepage 'https://github.com/be5invis/Iosevka/'
+  appcast "https://github.com/be5invis/Iosevka/releases.atom"
+  name "Iosevka Etoile"
+  homepage "https://github.com/be5invis/Iosevka/"
 
-  font 'iosevka-etoile-bold.ttc'
-  font 'iosevka-etoile-extrabold.ttc'
-  font 'iosevka-etoile-extralight.ttc'
-  font 'iosevka-etoile-heavy.ttc'
-  font 'iosevka-etoile-light.ttc'
-  font 'iosevka-etoile-medium.ttc'
-  font 'iosevka-etoile-regular.ttc'
-  font 'iosevka-etoile-semibold.ttc'
-  font 'iosevka-etoile-thin.ttc'
+  font "iosevka-etoile-bold.ttc"
+  font "iosevka-etoile-extrabold.ttc"
+  font "iosevka-etoile-extralight.ttc"
+  font "iosevka-etoile-heavy.ttc"
+  font "iosevka-etoile-light.ttc"
+  font "iosevka-etoile-medium.ttc"
+  font "iosevka-etoile-regular.ttc"
+  font "iosevka-etoile-semibold.ttc"
+  font "iosevka-etoile-thin.ttc"
 end
