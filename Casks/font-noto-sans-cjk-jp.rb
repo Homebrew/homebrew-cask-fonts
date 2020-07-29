62ef21dd2158d4f3bdbe6d7d8115a191df9019b5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:51 +0100

    font-noto-sans-cjk-jp.rb: fix for new style

--- Casks/font-noto-sans-cjk-jp.rb
+++ Casks/font-noto-sans-cjk-jp.rb
@@ -1,19 +1,19 @@
-cask 'font-noto-sans-cjk-jp' do
+cask "font-noto-sans-cjk-jp" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip'
-  name 'Noto Sans CJK JP'
-  homepage 'https://www.google.com/get/noto/#sans-jpan'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKjp-hinted.zip"
+  name "Noto Sans CJK JP"
+  homepage "https://www.google.com/get/noto/#sans-jpan"
 
-  font 'NotoSansCJKjp-Black.otf'
-  font 'NotoSansCJKjp-Bold.otf'
-  font 'NotoSansCJKjp-DemiLight.otf'
-  font 'NotoSansCJKjp-Light.otf'
-  font 'NotoSansCJKjp-Medium.otf'
-  font 'NotoSansCJKjp-Regular.otf'
-  font 'NotoSansCJKjp-Thin.otf'
-  font 'NotoSansMonoCJKjp-Bold.otf'
-  font 'NotoSansMonoCJKjp-Regular.otf'
+  font "NotoSansCJKjp-Black.otf"
+  font "NotoSansCJKjp-Bold.otf"
+  font "NotoSansCJKjp-DemiLight.otf"
+  font "NotoSansCJKjp-Light.otf"
+  font "NotoSansCJKjp-Medium.otf"
+  font "NotoSansCJKjp-Regular.otf"
+  font "NotoSansCJKjp-Thin.otf"
+  font "NotoSansMonoCJKjp-Bold.otf"
+  font "NotoSansMonoCJKjp-Regular.otf"
 end
