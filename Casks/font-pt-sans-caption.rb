6502fe454b570c2d47a65b47af88ddfbb2136ae5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:07 +0100

    font-pt-sans-caption.rb: fix for new style

--- Casks/font-pt-sans-caption.rb
+++ Casks/font-pt-sans-caption.rb
@@ -1,16 +1,16 @@
-cask 'font-pt-sans-caption' do
+cask "font-pt-sans-caption" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/ptsanscaption',
+  url "https://github.com/google/fonts/trunk/ofl/ptsanscaption",
       using:      :svn,
       trust_cert: true
-  name 'PT Sans Caption'
-  homepage 'https://fonts.google.com/specimen/PT+Sans+Caption'
+  name "PT Sans Caption"
+  homepage "https://fonts.google.com/specimen/PT+Sans+Caption"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'PT_Sans-Caption-Web-Bold.ttf'
-  font 'PT_Sans-Caption-Web-Regular.ttf'
+  font "PT_Sans-Caption-Web-Bold.ttf"
+  font "PT_Sans-Caption-Web-Regular.ttf"
 end
