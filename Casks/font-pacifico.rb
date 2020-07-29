d147849e977cada87befb8b4a7110ae03e498fca
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:03 +0100

    font-pacifico.rb: fix for new style

--- Casks/font-pacifico.rb
+++ Casks/font-pacifico.rb
@@ -1,11 +1,11 @@
-cask 'font-pacifico' do
+cask "font-pacifico" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/pacifico/Pacifico-Regular.ttf'
-  name 'Pacifico'
-  homepage 'https://fonts.google.com/specimen/Pacifico'
+  url "https://github.com/google/fonts/raw/master/ofl/pacifico/Pacifico-Regular.ttf"
+  name "Pacifico"
+  homepage "https://fonts.google.com/specimen/Pacifico"
 
-  font 'Pacifico-Regular.ttf'
+  font "Pacifico-Regular.ttf"
 end
