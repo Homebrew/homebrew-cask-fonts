24c32a73627b5e0cf005d5228a6bf92073a50a7b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:37 +0100

    font-lovers-quarrel.rb: fix for new style

--- Casks/font-lovers-quarrel.rb
+++ Casks/font-lovers-quarrel.rb
@@ -1,11 +1,11 @@
-cask 'font-lovers-quarrel' do
+cask "font-lovers-quarrel" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/loversquarrel/LoversQuarrel-Regular.ttf'
-  name 'Lovers Quarrel'
-  homepage 'https://fonts.google.com/specimen/Lovers+Quarrel'
+  url "https://github.com/google/fonts/raw/master/ofl/loversquarrel/LoversQuarrel-Regular.ttf"
+  name "Lovers Quarrel"
+  homepage "https://fonts.google.com/specimen/Lovers+Quarrel"
 
-  font 'LoversQuarrel-Regular.ttf'
+  font "LoversQuarrel-Regular.ttf"
 end
