77eed3d9a99afaa95f2228f7386eee46a74ea22b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:11 +0100

    font-hind-vadodara.rb: fix for new style

--- Casks/font-hind-vadodara.rb
+++ Casks/font-hind-vadodara.rb
@@ -1,19 +1,19 @@
-cask 'font-hind-vadodara' do
+cask "font-hind-vadodara" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/hindvadodara',
+  url "https://github.com/google/fonts/trunk/ofl/hindvadodara",
       using:      :svn,
       trust_cert: true
-  name 'Hind Vadodara'
-  homepage 'https://fonts.google.com/specimen/Hind+Vadodara'
+  name "Hind Vadodara"
+  homepage "https://fonts.google.com/specimen/Hind+Vadodara"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'HindVadodara-Bold.ttf'
-  font 'HindVadodara-Light.ttf'
-  font 'HindVadodara-Medium.ttf'
-  font 'HindVadodara-Regular.ttf'
-  font 'HindVadodara-SemiBold.ttf'
+  font "HindVadodara-Bold.ttf"
+  font "HindVadodara-Light.ttf"
+  font "HindVadodara-Medium.ttf"
+  font "HindVadodara-Regular.ttf"
+  font "HindVadodara-SemiBold.ttf"
 end
