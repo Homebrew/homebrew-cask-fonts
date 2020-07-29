c4b2ec23ae8352d19c1197ba8cc34ac1b4e8f98c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:24 +0100

    font-bungee-hairline.rb: fix for new style

--- Casks/font-bungee-hairline.rb
+++ Casks/font-bungee-hairline.rb
@@ -1,11 +1,11 @@
-cask 'font-bungee-hairline' do
+cask "font-bungee-hairline" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bungeehairline/BungeeHairline-Regular.ttf'
-  name 'Bungee Hairline'
-  homepage 'https://fonts.google.com/specimen/Bungee+Hairline'
+  url "https://github.com/google/fonts/raw/master/ofl/bungeehairline/BungeeHairline-Regular.ttf"
+  name "Bungee Hairline"
+  homepage "https://fonts.google.com/specimen/Bungee+Hairline"
 
-  font 'BungeeHairline-Regular.ttf'
+  font "BungeeHairline-Regular.ttf"
 end
