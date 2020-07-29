580f7d71ec4e4626ab0863e9e527d2232731caf6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:54 +0100

    font-noto-sans-lepcha.rb: fix for new style

--- Casks/font-noto-sans-lepcha.rb
+++ Casks/font-noto-sans-lepcha.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-lepcha' do
+cask "font-noto-sans-lepcha" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLepcha-unhinted.zip'
-  name 'Noto Sans Lepcha'
-  homepage 'https://www.google.com/get/noto/#sans-lepc'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLepcha-unhinted.zip"
+  name "Noto Sans Lepcha"
+  homepage "https://www.google.com/get/noto/#sans-lepc"
 
-  font 'NotoSansLepcha-Regular.ttf'
+  font "NotoSansLepcha-Regular.ttf"
 end
