4e4e9afda623e18fbf3ff1c9f72bfb2ddd673397
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:44 +0100

    font-modak.rb: fix for new style

--- Casks/font-modak.rb
+++ Casks/font-modak.rb
@@ -1,11 +1,11 @@
-cask 'font-modak' do
+cask "font-modak" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/modak/Modak-Regular.ttf'
-  name 'Modak'
-  homepage 'https://fonts.google.com/specimen/Modak'
+  url "https://github.com/google/fonts/raw/master/ofl/modak/Modak-Regular.ttf"
+  name "Modak"
+  homepage "https://fonts.google.com/specimen/Modak"
 
-  font 'Modak-Regular.ttf'
+  font "Modak-Regular.ttf"
 end
