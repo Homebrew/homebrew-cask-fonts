2f2092f934e2eaa860b5a08e441fe0b6590aa54a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:36 +0100

    font-lohit-bengali.rb: fix for new style

--- Casks/font-lohit-bengali.rb
+++ Casks/font-lohit-bengali.rb
@@ -1,11 +1,11 @@
-cask 'font-lohit-bengali' do
+cask "font-lohit-bengali" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/lohitbengali/Lohit-Bengali.ttf'
-  name 'Lohit Bengali'
-  homepage 'https://fonts.google.com/specimen/Lohit+Bengali'
+  url "https://github.com/google/fonts/raw/master/ofl/lohitbengali/Lohit-Bengali.ttf"
+  name "Lohit Bengali"
+  homepage "https://fonts.google.com/specimen/Lohit+Bengali"
 
-  font 'Lohit-Bengali.ttf'
+  font "Lohit-Bengali.ttf"
 end
