010f893088b0549e4c7305b4399c6c2946ab063a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:44 +0100

    font-dokdo.rb: fix for new style

--- Casks/font-dokdo.rb
+++ Casks/font-dokdo.rb
@@ -1,11 +1,11 @@
-cask 'font-dokdo' do
+cask "font-dokdo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/dokdo/Dokdo-Regular.ttf'
-  name 'Dokdo'
-  homepage 'https://fonts.google.com/specimen/Dokdo'
+  url "https://github.com/google/fonts/raw/master/ofl/dokdo/Dokdo-Regular.ttf"
+  name "Dokdo"
+  homepage "https://fonts.google.com/specimen/Dokdo"
 
-  font 'Dokdo-Regular.ttf'
+  font "Dokdo-Regular.ttf"
 end
