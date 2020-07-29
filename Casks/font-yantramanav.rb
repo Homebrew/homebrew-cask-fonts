29cbc4a9c1217fdc5646ab8a6c05d0133ec2fcf8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:29 +0100

    font-yantramanav.rb: fix for new style

--- Casks/font-yantramanav.rb
+++ Casks/font-yantramanav.rb
@@ -1,20 +1,20 @@
-cask 'font-yantramanav' do
+cask "font-yantramanav" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/yantramanav',
+  url "https://github.com/google/fonts/trunk/ofl/yantramanav",
       using:      :svn,
       trust_cert: true
-  name 'Yantramanav'
-  homepage 'https://fonts.google.com/specimen/Yantramanav'
+  name "Yantramanav"
+  homepage "https://fonts.google.com/specimen/Yantramanav"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Yantramanav-Black.ttf'
-  font 'Yantramanav-Bold.ttf'
-  font 'Yantramanav-Light.ttf'
-  font 'Yantramanav-Medium.ttf'
-  font 'Yantramanav-Regular.ttf'
-  font 'Yantramanav-Thin.ttf'
+  font "Yantramanav-Black.ttf"
+  font "Yantramanav-Bold.ttf"
+  font "Yantramanav-Light.ttf"
+  font "Yantramanav-Medium.ttf"
+  font "Yantramanav-Regular.ttf"
+  font "Yantramanav-Thin.ttf"
 end
