71d72dac07dbbf9f4390ec3a0e597ee9ea324ec4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:15 +0100

    font-almendra-sc.rb: fix for new style

--- Casks/font-almendra-sc.rb
+++ Casks/font-almendra-sc.rb
@@ -1,11 +1,11 @@
-cask 'font-almendra-sc' do
+cask "font-almendra-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/almendrasc/AlmendraSC-Regular.ttf'
-  name 'Almendra SC'
-  homepage 'https://fonts.google.com/specimen/Almendra+SC'
+  url "https://github.com/google/fonts/raw/master/ofl/almendrasc/AlmendraSC-Regular.ttf"
+  name "Almendra SC"
+  homepage "https://fonts.google.com/specimen/Almendra+SC"
 
-  font 'AlmendraSC-Regular.ttf'
+  font "AlmendraSC-Regular.ttf"
 end
