330b89ce0013c88767ea57db1dbe9814fdf4bd3b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:48 +0100

    font-nanum-gothic.rb: fix for new style

--- Casks/font-nanum-gothic.rb
+++ Casks/font-nanum-gothic.rb
@@ -1,17 +1,17 @@
-cask 'font-nanum-gothic' do
+cask "font-nanum-gothic" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/nanumgothic',
+  url "https://github.com/google/fonts/trunk/ofl/nanumgothic",
       using:      :svn,
       trust_cert: true
-  name 'Nanum Gothic'
-  homepage 'https://fonts.google.com/specimen/Nanum+Gothic'
+  name "Nanum Gothic"
+  homepage "https://fonts.google.com/specimen/Nanum+Gothic"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'NanumGothic-Bold.ttf'
-  font 'NanumGothic-ExtraBold.ttf'
-  font 'NanumGothic-Regular.ttf'
+  font "NanumGothic-Bold.ttf"
+  font "NanumGothic-ExtraBold.ttf"
+  font "NanumGothic-Regular.ttf"
 end
