4e55ebb26026bba99b8f66666638348b10c19bb1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:18 +0100

    font-iosevka-sparkle.rb: fix for new style

--- Casks/font-iosevka-sparkle.rb
+++ Casks/font-iosevka-sparkle.rb
@@ -1,19 +1,19 @@
-cask 'font-iosevka-sparkle' do
-  version '3.3.1'
-  sha256 '35224c3a95f9a96d2afabe548a09ecfaa50cafb63308cd90fe227d51beaab554'
+cask "font-iosevka-sparkle" do
+  version "3.3.1"
+  sha256 "35224c3a95f9a96d2afabe548a09ecfaa50cafb63308cd90fe227d51beaab554"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-sparkle-#{version}.zip"
-  appcast 'https://github.com/be5invis/Iosevka/releases.atom'
-  name 'Iosevka Sparkle'
-  homepage 'https://github.com/be5invis/Iosevka/'
+  appcast "https://github.com/be5invis/Iosevka/releases.atom"
+  name "Iosevka Sparkle"
+  homepage "https://github.com/be5invis/Iosevka/"
 
-  font 'iosevka-sparkle-bold.ttc'
-  font 'iosevka-sparkle-extrabold.ttc'
-  font 'iosevka-sparkle-extralight.ttc'
-  font 'iosevka-sparkle-heavy.ttc'
-  font 'iosevka-sparkle-light.ttc'
-  font 'iosevka-sparkle-medium.ttc'
-  font 'iosevka-sparkle-regular.ttc'
-  font 'iosevka-sparkle-semibold.ttc'
-  font 'iosevka-sparkle-thin.ttc'
+  font "iosevka-sparkle-bold.ttc"
+  font "iosevka-sparkle-extrabold.ttc"
+  font "iosevka-sparkle-extralight.ttc"
+  font "iosevka-sparkle-heavy.ttc"
+  font "iosevka-sparkle-light.ttc"
+  font "iosevka-sparkle-medium.ttc"
+  font "iosevka-sparkle-regular.ttc"
+  font "iosevka-sparkle-semibold.ttc"
+  font "iosevka-sparkle-thin.ttc"
 end
