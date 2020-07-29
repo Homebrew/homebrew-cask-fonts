9c8a49571a5c286918071e2e923b269993e822c5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:05 +0100

    font-goudy-bookletter-1911.rb: fix for new style

--- Casks/font-goudy-bookletter-1911.rb
+++ Casks/font-goudy-bookletter-1911.rb
@@ -1,11 +1,11 @@
-cask 'font-goudy-bookletter-1911' do
+cask "font-goudy-bookletter-1911" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/goudybookletter1911/GoudyBookletter1911.ttf'
-  name 'Goudy Bookletter 1911'
-  homepage 'https://fonts.google.com/specimen/Goudy+Bookletter+1911'
+  url "https://github.com/google/fonts/raw/master/ofl/goudybookletter1911/GoudyBookletter1911.ttf"
+  name "Goudy Bookletter 1911"
+  homepage "https://fonts.google.com/specimen/Goudy+Bookletter+1911"
 
-  font 'GoudyBookletter1911.ttf'
+  font "GoudyBookletter1911.ttf"
 end
