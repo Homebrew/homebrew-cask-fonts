e77ac0473623339b0044dde8dd1b52b008c826ea
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:12 +0100

    font-rowdies.rb: fix for new style

--- Casks/font-rowdies.rb
+++ Casks/font-rowdies.rb
@@ -1,17 +1,17 @@
-cask 'font-rowdies' do
+cask "font-rowdies" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/rowdies',
+  url "https://github.com/google/fonts/trunk/ofl/rowdies",
       using:      :svn,
       trust_cert: true
-  name 'Rowdies'
-  homepage 'https://fonts.google.com/specimen/Rowdies'
+  name "Rowdies"
+  homepage "https://fonts.google.com/specimen/Rowdies"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Rowdies-Bold.ttf'
-  font 'Rowdies-Light.ttf'
-  font 'Rowdies-Regular.ttf'
+  font "Rowdies-Bold.ttf"
+  font "Rowdies-Light.ttf"
+  font "Rowdies-Regular.ttf"
 end
