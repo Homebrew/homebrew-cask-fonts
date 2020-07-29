f8da4cb7a6dd784aa012460de719c7c3bdc0a953
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:34 +0100

    font-comfortaa.rb: fix for new style

--- Casks/font-comfortaa.rb
+++ Casks/font-comfortaa.rb
@@ -1,11 +1,11 @@
-cask 'font-comfortaa' do
+cask "font-comfortaa" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/comfortaa/Comfortaa%5Bwght%5D.ttf'
-  name 'Comfortaa'
-  homepage 'https://fonts.google.com/specimen/Comfortaa'
+  url "https://github.com/google/fonts/raw/master/ofl/comfortaa/Comfortaa%5Bwght%5D.ttf"
+  name "Comfortaa"
+  homepage "https://fonts.google.com/specimen/Comfortaa"
 
-  font 'Comfortaa[wght].ttf'
+  font "Comfortaa[wght].ttf"
 end
