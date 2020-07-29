933262b85b4c6ab27caec2eb5d623a9fcdeb7269
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:27 +0100

    font-varta.rb: fix for new style

--- Casks/font-varta.rb
+++ Casks/font-varta.rb
@@ -1,18 +1,18 @@
-cask 'font-varta' do
+cask "font-varta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/varta',
+  url "https://github.com/google/fonts/trunk/ofl/varta",
       using:      :svn,
       trust_cert: true
-  name 'Varta'
-  homepage 'https://fonts.google.com/specimen/Varta'
+  name "Varta"
+  homepage "https://fonts.google.com/specimen/Varta"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Varta-Bold.ttf'
-  font 'Varta-Light.ttf'
-  font 'Varta-Regular.ttf'
-  font 'Varta-SemiBold.ttf'
+  font "Varta-Bold.ttf"
+  font "Varta-Light.ttf"
+  font "Varta-Regular.ttf"
+  font "Varta-SemiBold.ttf"
 end
