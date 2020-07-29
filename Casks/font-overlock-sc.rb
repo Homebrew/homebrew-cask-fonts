891a4cf7620db9debeed1efd8935c90587921569
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:02 +0100

    font-overlock-sc.rb: fix for new style

--- Casks/font-overlock-sc.rb
+++ Casks/font-overlock-sc.rb
@@ -1,11 +1,11 @@
-cask 'font-overlock-sc' do
+cask "font-overlock-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/overlocksc/OverlockSC-Regular.ttf'
-  name 'Overlock SC'
-  homepage 'https://fonts.google.com/specimen/Overlock+SC'
+  url "https://github.com/google/fonts/raw/master/ofl/overlocksc/OverlockSC-Regular.ttf"
+  name "Overlock SC"
+  homepage "https://fonts.google.com/specimen/Overlock+SC"
 
-  font 'OverlockSC-Regular.ttf'
+  font "OverlockSC-Regular.ttf"
 end
