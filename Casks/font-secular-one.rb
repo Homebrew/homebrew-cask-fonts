b42aa46cb36e26e931c3c166a747f7da8fc16178
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:15 +0100

    font-secular-one.rb: fix for new style

--- Casks/font-secular-one.rb
+++ Casks/font-secular-one.rb
@@ -1,11 +1,11 @@
-cask 'font-secular-one' do
+cask "font-secular-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/secularone/SecularOne-Regular.ttf'
-  name 'Secular One'
-  homepage 'https://fonts.google.com/specimen/Secular+One'
+  url "https://github.com/google/fonts/raw/master/ofl/secularone/SecularOne-Regular.ttf"
+  name "Secular One"
+  homepage "https://fonts.google.com/specimen/Secular+One"
 
-  font 'SecularOne-Regular.ttf'
+  font "SecularOne-Regular.ttf"
 end
