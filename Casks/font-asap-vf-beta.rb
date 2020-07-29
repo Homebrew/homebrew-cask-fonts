97a3645f1612320a0de73a2534ad4a35a1d3c2db
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:57 +0100

    font-asap-vf-beta.rb: fix for new style

--- Casks/font-asap-vf-beta.rb
+++ Casks/font-asap-vf-beta.rb
@@ -1,17 +1,17 @@
-cask 'font-asap-vf-beta' do
+cask "font-asap-vf-beta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/asapvfbeta',
+  url "https://github.com/google/fonts/trunk/ofl/asapvfbeta",
       using:      :svn,
       trust_cert: true
-  name 'Asap VF Beta'
-  homepage 'https://fonts.google.com/earlyaccess'
+  name "Asap VF Beta"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'AsapVFBeta-Condensed.ttf'
-  font 'AsapVFBeta-Italic.ttf'
-  font 'AsapVFBeta.ttf'
+  font "AsapVFBeta-Condensed.ttf"
+  font "AsapVFBeta-Italic.ttf"
+  font "AsapVFBeta.ttf"
 end
