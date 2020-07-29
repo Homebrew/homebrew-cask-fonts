512f828d1eb6e8db76a8f5e5c3a6011413b09378
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:56 +0100

    font-noto-sans-runic.rb: fix for new style

--- Casks/font-noto-sans-runic.rb
+++ Casks/font-noto-sans-runic.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-runic' do
+cask "font-noto-sans-runic" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansRunic-unhinted.zip'
-  name 'Noto Sans Runic'
-  homepage 'https://www.google.com/get/noto/#sans-runr'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansRunic-unhinted.zip"
+  name "Noto Sans Runic"
+  homepage "https://www.google.com/get/noto/#sans-runr"
 
-  font 'NotoSansRunic-Regular.ttf'
+  font "NotoSansRunic-Regular.ttf"
 end
