95c964ba3cdaec52d5b0dd9fb22b2efe7d480906
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:37 +0100

    font-luckiest-guy.rb: fix for new style

--- Casks/font-luckiest-guy.rb
+++ Casks/font-luckiest-guy.rb
@@ -1,11 +1,11 @@
-cask 'font-luckiest-guy' do
+cask "font-luckiest-guy" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/luckiestguy/LuckiestGuy-Regular.ttf'
-  name 'Luckiest Guy'
-  homepage 'https://fonts.google.com/specimen/Luckiest+Guy'
+  url "https://github.com/google/fonts/raw/master/apache/luckiestguy/LuckiestGuy-Regular.ttf"
+  name "Luckiest Guy"
+  homepage "https://fonts.google.com/specimen/Luckiest+Guy"
 
-  font 'LuckiestGuy-Regular.ttf'
+  font "LuckiestGuy-Regular.ttf"
 end
