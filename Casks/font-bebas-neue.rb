dca68896a33251cce3e98fde21a06aa67f1cd8a2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:17 +0100

    font-bebas-neue.rb: fix for new style

--- Casks/font-bebas-neue.rb
+++ Casks/font-bebas-neue.rb
@@ -1,11 +1,11 @@
-cask 'font-bebas-neue' do
+cask "font-bebas-neue" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bebasneue/BebasNeue-Regular.ttf'
-  name 'Bebas Neue'
-  homepage 'https://fonts.google.com/specimen/Bebas+Neue'
+  url "https://github.com/google/fonts/raw/master/ofl/bebasneue/BebasNeue-Regular.ttf"
+  name "Bebas Neue"
+  homepage "https://fonts.google.com/specimen/Bebas+Neue"
 
-  font 'BebasNeue-Regular.ttf'
+  font "BebasNeue-Regular.ttf"
 end
