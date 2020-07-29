f0ba37166ae0439ff17ddafbee7d3031e62e630a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:10 +0100

    font-herr-von-muellerhoff.rb: fix for new style

--- Casks/font-herr-von-muellerhoff.rb
+++ Casks/font-herr-von-muellerhoff.rb
@@ -1,11 +1,11 @@
-cask 'font-herr-von-muellerhoff' do
+cask "font-herr-von-muellerhoff" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/herrvonmuellerhoff/HerrVonMuellerhoff-Regular.ttf'
-  name 'Herr Von Muellerhoff'
-  homepage 'https://fonts.google.com/specimen/Herr+Von+Muellerhoff'
+  url "https://github.com/google/fonts/raw/master/ofl/herrvonmuellerhoff/HerrVonMuellerhoff-Regular.ttf"
+  name "Herr Von Muellerhoff"
+  homepage "https://fonts.google.com/specimen/Herr+Von+Muellerhoff"
 
-  font 'HerrVonMuellerhoff-Regular.ttf'
+  font "HerrVonMuellerhoff-Regular.ttf"
 end
