13c52951ebc0192b1a9d188a7fdc05d3c4de6711
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:29 +0100

    font-kokoro.rb: fix for new style

--- Casks/font-kokoro.rb
+++ Casks/font-kokoro.rb
@@ -1,11 +1,11 @@
-cask 'font-kokoro' do
+cask "font-kokoro" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/kokoro/Kokoro-Regular.ttf'
-  name 'Kokoro'
-  homepage 'https://fonts.google.com/specimen/Kokoro'
+  url "https://github.com/google/fonts/raw/master/ofl/kokoro/Kokoro-Regular.ttf"
+  name "Kokoro"
+  homepage "https://fonts.google.com/specimen/Kokoro"
 
-  font 'Kokoro-Regular.ttf'
+  font "Kokoro-Regular.ttf"
 end
