bcafc9f3b6ff9bf934648f4bbc3f10ff4b2a804f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:04 +0100

    font-pecita.rb: fix for new style

--- Casks/font-pecita.rb
+++ Casks/font-pecita.rb
@@ -1,11 +1,11 @@
-cask 'font-pecita' do
+cask "font-pecita" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/pecita/Pecita.ttf'
-  name 'Pecita'
-  homepage 'https://fonts.google.com/specimen/Pecita'
+  url "https://github.com/google/fonts/raw/master/ofl/pecita/Pecita.ttf"
+  name "Pecita"
+  homepage "https://fonts.google.com/specimen/Pecita"
 
-  font 'Pecita.ttf'
+  font "Pecita.ttf"
 end
