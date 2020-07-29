d931efb8b3893410fd968375b953026a19a7cfea
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:44 +0100

    font-monoid.rb: fix for new style

--- Casks/font-monoid.rb
+++ Casks/font-monoid.rb
@@ -1,17 +1,17 @@
-cask 'font-monoid' do
-  version '0.61'
-  sha256 'c28acd76ae1f0244f7c869bf1ba3272da3eb900dd50fbbd368265468a1a34794'
+cask "font-monoid" do
+  version "0.61"
+  sha256 "c28acd76ae1f0244f7c869bf1ba3272da3eb900dd50fbbd368265468a1a34794"
 
   # github.com/larsenwork/monoid/ was verified as official when first introduced to the cask
-  url 'https://github.com/larsenwork/monoid/blob/release/Monoid.zip?raw=true'
-  appcast 'https://github.com/larsenwork/monoid/releases.atom'
-  name 'Monoid'
-  homepage 'https://larsenwork.com/monoid/'
+  url "https://github.com/larsenwork/monoid/blob/release/Monoid.zip?raw=true"
+  appcast "https://github.com/larsenwork/monoid/releases.atom"
+  name "Monoid"
+  homepage "https://larsenwork.com/monoid/"
 
-  font 'Monoid-Bold.ttf'
-  font 'Monoid-Italic.ttf'
-  font 'Monoid-Regular.ttf'
-  font 'Monoid-Retina.ttf'
+  font "Monoid-Bold.ttf"
+  font "Monoid-Italic.ttf"
+  font "Monoid-Regular.ttf"
+  font "Monoid-Retina.ttf"
 
   caveats <<~EOS
     #{token} only installs the Normal Weight, Medium LineHeight, with Ligatures variant.
