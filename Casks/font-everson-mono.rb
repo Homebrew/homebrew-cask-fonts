80f3d82ddc48b29a7134808036734ebd838d713a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:49 +0100

    font-everson-mono.rb: fix for new style

--- Casks/font-everson-mono.rb
+++ Casks/font-everson-mono.rb
@@ -1,10 +1,10 @@
-cask 'font-everson-mono' do
-  version '7.0.0'
-  sha256 'a7196feb79faa1933ecbfc34579586d87addc208b7f6ac9dd93b92d64b0d81f5'
+cask "font-everson-mono" do
+  version "7.0.0"
+  sha256 "a7196feb79faa1933ecbfc34579586d87addc208b7f6ac9dd93b92d64b0d81f5"
 
-  url 'https://www.evertype.com/emono/evermono.zip'
-  name 'Everson Mono'
-  homepage 'https://www.evertype.com/emono/'
+  url "https://www.evertype.com/emono/evermono.zip"
+  name "Everson Mono"
+  homepage "https://www.evertype.com/emono/"
 
   font "evermono-#{version}/Everson Mono.ttf"
   font "evermono-#{version}/Everson Mono Bold.ttf"
