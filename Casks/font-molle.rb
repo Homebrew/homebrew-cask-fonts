9154bc9e024a6583499e153eb2e6e48723022638
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:44 +0100

    font-molle.rb: fix for new style

--- Casks/font-molle.rb
+++ Casks/font-molle.rb
@@ -1,11 +1,11 @@
-cask 'font-molle' do
+cask "font-molle" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/molle/Molle-Regular.ttf'
-  name 'Molle'
-  homepage 'https://fonts.google.com/specimen/Molle'
+  url "https://github.com/google/fonts/raw/master/ofl/molle/Molle-Regular.ttf"
+  name "Molle"
+  homepage "https://fonts.google.com/specimen/Molle"
 
-  font 'Molle-Regular.ttf'
+  font "Molle-Regular.ttf"
 end
