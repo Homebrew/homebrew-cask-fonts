91cc08cd7821a099f4008dcb5f929e8e95aafd19
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:57 +0100

    font-noto-sans-tai-tham.rb: fix for new style

--- Casks/font-noto-sans-tai-tham.rb
+++ Casks/font-noto-sans-tai-tham.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-tai-tham' do
+cask "font-noto-sans-tai-tham" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiTham-unhinted.zip'
-  name 'Noto Sans Tai Tham'
-  homepage 'https://www.google.com/get/noto/#sans-lana'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiTham-unhinted.zip"
+  name "Noto Sans Tai Tham"
+  homepage "https://www.google.com/get/noto/#sans-lana"
 
-  font 'NotoSansTaiTham-Regular.ttf'
+  font "NotoSansTaiTham-Regular.ttf"
 end
