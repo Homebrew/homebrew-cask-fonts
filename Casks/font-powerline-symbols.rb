b92754edb086d8b734f74ab98508bcf278dfed6c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:06 +0100

    font-powerline-symbols.rb: fix for new style

--- Casks/font-powerline-symbols.rb
+++ Casks/font-powerline-symbols.rb
@@ -1,10 +1,10 @@
-cask 'font-powerline-symbols' do
+cask "font-powerline-symbols" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/powerline/powerline/raw/master/font/PowerlineSymbols.otf'
-  name 'Powerline Symbols'
-  homepage 'https://github.com/powerline/powerline'
+  url "https://github.com/powerline/powerline/raw/master/font/PowerlineSymbols.otf"
+  name "Powerline Symbols"
+  homepage "https://github.com/powerline/powerline"
 
-  font 'PowerlineSymbols.otf'
+  font "PowerlineSymbols.otf"
 end
