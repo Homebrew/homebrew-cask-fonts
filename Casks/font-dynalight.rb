c0bfbb93addb5a00905905cbb508d7181dc569da
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:46 +0100

    font-dynalight.rb: fix for new style

--- Casks/font-dynalight.rb
+++ Casks/font-dynalight.rb
@@ -1,11 +1,11 @@
-cask 'font-dynalight' do
+cask "font-dynalight" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/dynalight/Dynalight-Regular.ttf'
-  name 'Dynalight'
-  homepage 'https://fonts.google.com/specimen/Dynalight'
+  url "https://github.com/google/fonts/raw/master/ofl/dynalight/Dynalight-Regular.ttf"
+  name "Dynalight"
+  homepage "https://fonts.google.com/specimen/Dynalight"
 
-  font 'Dynalight-Regular.ttf'
+  font "Dynalight-Regular.ttf"
 end
