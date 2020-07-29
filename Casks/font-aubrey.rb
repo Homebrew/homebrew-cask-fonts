979893c8ee466bb1d2a5786cb990b88f2620cad5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:27 +0100

    font-aubrey.rb: fix for new style

--- Casks/font-aubrey.rb
+++ Casks/font-aubrey.rb
@@ -1,11 +1,11 @@
-cask 'font-aubrey' do
+cask "font-aubrey" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/aubrey/Aubrey-Regular.ttf'
-  name 'Aubrey'
-  homepage 'https://fonts.google.com/specimen/Aubrey'
+  url "https://github.com/google/fonts/raw/master/ofl/aubrey/Aubrey-Regular.ttf"
+  name "Aubrey"
+  homepage "https://fonts.google.com/specimen/Aubrey"
 
-  font 'Aubrey-Regular.ttf'
+  font "Aubrey-Regular.ttf"
 end
