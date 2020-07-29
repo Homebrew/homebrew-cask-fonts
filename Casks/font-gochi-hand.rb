2202815324d178e61eb624bcbacb9d39d7234a81
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:05 +0100

    font-gochi-hand.rb: fix for new style

--- Casks/font-gochi-hand.rb
+++ Casks/font-gochi-hand.rb
@@ -1,11 +1,11 @@
-cask 'font-gochi-hand' do
+cask "font-gochi-hand" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/gochihand/GochiHand-Regular.ttf'
-  name 'Gochi Hand'
-  homepage 'https://fonts.google.com/specimen/Gochi+Hand'
+  url "https://github.com/google/fonts/raw/master/ofl/gochihand/GochiHand-Regular.ttf"
+  name "Gochi Hand"
+  homepage "https://fonts.google.com/specimen/Gochi+Hand"
 
-  font 'GochiHand-Regular.ttf'
+  font "GochiHand-Regular.ttf"
 end
