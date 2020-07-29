a2bce35cd5aacf4514c001550fd7302ad30ca609
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:19 +0100

    font-bevan.rb: fix for new style

--- Casks/font-bevan.rb
+++ Casks/font-bevan.rb
@@ -1,11 +1,11 @@
-cask 'font-bevan' do
+cask "font-bevan" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bevan/Bevan-Regular.ttf'
-  name 'Bevan'
-  homepage 'https://fonts.google.com/specimen/Bevan'
+  url "https://github.com/google/fonts/raw/master/ofl/bevan/Bevan-Regular.ttf"
+  name "Bevan"
+  homepage "https://fonts.google.com/specimen/Bevan"
 
-  font 'Bevan-Regular.ttf'
+  font "Bevan-Regular.ttf"
 end
