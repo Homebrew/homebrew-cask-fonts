ca2eceac5c8e7b03165de90b0466fafda7a488d9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:48 +0100

    font-niconne.rb: fix for new style

--- Casks/font-niconne.rb
+++ Casks/font-niconne.rb
@@ -1,11 +1,11 @@
-cask 'font-niconne' do
+cask "font-niconne" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/niconne/Niconne-Regular.ttf'
-  name 'Niconne'
-  homepage 'https://fonts.google.com/specimen/Niconne'
+  url "https://github.com/google/fonts/raw/master/ofl/niconne/Niconne-Regular.ttf"
+  name "Niconne"
+  homepage "https://fonts.google.com/specimen/Niconne"
 
-  font 'Niconne-Regular.ttf'
+  font "Niconne-Regular.ttf"
 end
