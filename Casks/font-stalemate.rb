fe873a85a0e8c3a31c84b1a69cd7b483d4182eb5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:21 +0100

    font-stalemate.rb: fix for new style

--- Casks/font-stalemate.rb
+++ Casks/font-stalemate.rb
@@ -1,11 +1,11 @@
-cask 'font-stalemate' do
+cask "font-stalemate" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/stalemate/Stalemate-Regular.ttf'
-  name 'Stalemate'
-  homepage 'https://fonts.google.com/specimen/Stalemate'
+  url "https://github.com/google/fonts/raw/master/ofl/stalemate/Stalemate-Regular.ttf"
+  name "Stalemate"
+  homepage "https://fonts.google.com/specimen/Stalemate"
 
-  font 'Stalemate-Regular.ttf'
+  font "Stalemate-Regular.ttf"
 end
