9cf4f35ed940312126080516189157bb4c4b9f3f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:29 +0100

    font-xits.rb: fix for new style

--- Casks/font-xits.rb
+++ Casks/font-xits.rb
@@ -1,11 +1,11 @@
-cask 'font-xits' do
-  version '1.200'
-  sha256 '039493b95b86d15e451e818e065e6f6719977a31e43504eb6946513ba09c8a72'
+cask "font-xits" do
+  version "1.200"
+  sha256 "039493b95b86d15e451e818e065e6f6719977a31e43504eb6946513ba09c8a72"
 
   url "https://github.com/khaledhosny/xits/archive/v#{version}.zip"
-  appcast 'https://github.com/khaledhosny/xits/releases.atom'
-  name 'XITS'
-  homepage 'https://github.com/khaledhosny/xits'
+  appcast "https://github.com/khaledhosny/xits/releases.atom"
+  name "XITS"
+  homepage "https://github.com/khaledhosny/xits"
 
   font "xits-#{version}/xits-bold.otf"
   font "xits-#{version}/xits-bolditalic.otf"
