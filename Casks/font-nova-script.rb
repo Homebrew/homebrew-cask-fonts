2e3404b5b5a4d635ef44a0fcbedfe1ba032427ec
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:00 +0100

    font-nova-script.rb: fix for new style

--- Casks/font-nova-script.rb
+++ Casks/font-nova-script.rb
@@ -1,11 +1,11 @@
-cask 'font-nova-script' do
+cask "font-nova-script" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/novascript/NovaScript-Regular.ttf'
-  name 'Nova Script'
-  homepage 'https://fonts.google.com/specimen/Nova+Script'
+  url "https://github.com/google/fonts/raw/master/ofl/novascript/NovaScript-Regular.ttf"
+  name "Nova Script"
+  homepage "https://fonts.google.com/specimen/Nova+Script"
 
-  font 'NovaScript-Regular.ttf'
+  font "NovaScript-Regular.ttf"
 end
