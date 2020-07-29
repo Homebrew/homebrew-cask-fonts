87d10a727e61b55ffb4f3d8ae87aebb8d0ee8d52
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:18 +0100

    font-sniglet.rb: fix for new style

--- Casks/font-sniglet.rb
+++ Casks/font-sniglet.rb
@@ -1,16 +1,16 @@
-cask 'font-sniglet' do
+cask "font-sniglet" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/sniglet',
+  url "https://github.com/google/fonts/trunk/ofl/sniglet",
       using:      :svn,
       trust_cert: true
-  name 'Sniglet'
-  homepage 'https://fonts.google.com/specimen/Sniglet'
+  name "Sniglet"
+  homepage "https://fonts.google.com/specimen/Sniglet"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Sniglet-ExtraBold.ttf'
-  font 'Sniglet-Regular.ttf'
+  font "Sniglet-ExtraBold.ttf"
+  font "Sniglet-Regular.ttf"
 end
