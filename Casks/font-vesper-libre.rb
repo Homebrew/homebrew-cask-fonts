812e552dbc9692a7dec2423249f3f78c3d353d99
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:27 +0100

    font-vesper-libre.rb: fix for new style

--- Casks/font-vesper-libre.rb
+++ Casks/font-vesper-libre.rb
@@ -1,18 +1,18 @@
-cask 'font-vesper-libre' do
+cask "font-vesper-libre" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/vesperlibre',
+  url "https://github.com/google/fonts/trunk/ofl/vesperlibre",
       using:      :svn,
       trust_cert: true
-  name 'Vesper Libre'
-  homepage 'https://fonts.google.com/specimen/Vesper+Libre'
+  name "Vesper Libre"
+  homepage "https://fonts.google.com/specimen/Vesper+Libre"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'VesperLibre-Bold.ttf'
-  font 'VesperLibre-Heavy.ttf'
-  font 'VesperLibre-Medium.ttf'
-  font 'VesperLibre-Regular.ttf'
+  font "VesperLibre-Bold.ttf"
+  font "VesperLibre-Heavy.ttf"
+  font "VesperLibre-Medium.ttf"
+  font "VesperLibre-Regular.ttf"
 end
