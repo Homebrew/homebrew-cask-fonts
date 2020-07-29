2089e9872739703a9a51b47bde4edb6235e2243a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:42 +0100

    font-miao-unicode.rb: fix for new style

--- Casks/font-miao-unicode.rb
+++ Casks/font-miao-unicode.rb
@@ -1,11 +1,11 @@
-cask 'font-miao-unicode' do
+cask "font-miao-unicode" do
   version :latest
   sha256 :no_check
 
   # github.com/phjamr/MiaoUnicode/ was verified as official when first introduced to the cask
-  url 'https://github.com/phjamr/MiaoUnicode/blob/master/MiaoUnicode-Regular.ttf?raw=true'
-  name 'MiaoUnicode'
-  homepage 'https://phjamr.github.io/miao.html'
+  url "https://github.com/phjamr/MiaoUnicode/blob/master/MiaoUnicode-Regular.ttf?raw=true"
+  name "MiaoUnicode"
+  homepage "https://phjamr.github.io/miao.html"
 
-  font 'MiaoUnicode-Regular.ttf'
+  font "MiaoUnicode-Regular.ttf"
 end
