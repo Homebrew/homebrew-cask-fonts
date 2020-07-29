617cc00569e01282a956aa7bfae9d2c8b9c9903e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:10 +0100

    font-rasa.rb: fix for new style

--- Casks/font-rasa.rb
+++ Casks/font-rasa.rb
@@ -1,19 +1,19 @@
-cask 'font-rasa' do
+cask "font-rasa" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/rasa',
+  url "https://github.com/google/fonts/trunk/ofl/rasa",
       using:      :svn,
       trust_cert: true
-  name 'Rasa'
-  homepage 'https://fonts.google.com/specimen/Rasa'
+  name "Rasa"
+  homepage "https://fonts.google.com/specimen/Rasa"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Rasa-Bold.ttf'
-  font 'Rasa-Light.ttf'
-  font 'Rasa-Medium.ttf'
-  font 'Rasa-Regular.ttf'
-  font 'Rasa-SemiBold.ttf'
+  font "Rasa-Bold.ttf"
+  font "Rasa-Light.ttf"
+  font "Rasa-Medium.ttf"
+  font "Rasa-Regular.ttf"
+  font "Rasa-SemiBold.ttf"
 end
