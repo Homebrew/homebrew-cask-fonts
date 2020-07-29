3fa92e9ee7308b01da9bb1aa6da655a6b655a07d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:00 +0100

    font-nova-square.rb: fix for new style

--- Casks/font-nova-square.rb
+++ Casks/font-nova-square.rb
@@ -1,11 +1,11 @@
-cask 'font-nova-square' do
+cask "font-nova-square" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/novasquare/NovaSquare.ttf'
-  name 'Nova Square'
-  homepage 'https://fonts.google.com/specimen/Nova+Square'
+  url "https://github.com/google/fonts/raw/master/ofl/novasquare/NovaSquare.ttf"
+  name "Nova Square"
+  homepage "https://fonts.google.com/specimen/Nova+Square"
 
-  font 'NovaSquare.ttf'
+  font "NovaSquare.ttf"
 end
