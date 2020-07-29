bc7a83b3aa5454289b1312edcf4b689ff8d816fc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:36 +0100

    font-autour-one.rb: fix for new style

--- Casks/font-autour-one.rb
+++ Casks/font-autour-one.rb
@@ -1,11 +1,11 @@
-cask 'font-autour-one' do
+cask "font-autour-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/autourone/AutourOne-Regular.ttf'
-  name 'Autour One'
-  homepage 'https://fonts.google.com/specimen/Autour+One'
+  url "https://github.com/google/fonts/raw/master/ofl/autourone/AutourOne-Regular.ttf"
+  name "Autour One"
+  homepage "https://fonts.google.com/specimen/Autour+One"
 
-  font 'AutourOne-Regular.ttf'
+  font "AutourOne-Regular.ttf"
 end
