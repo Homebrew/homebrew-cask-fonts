254414e8e3fe5abc2e62400928db48e4ac2d872c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:48 +0100

    font-averia-gruesa-libre.rb: fix for new style

--- Casks/font-averia-gruesa-libre.rb
+++ Casks/font-averia-gruesa-libre.rb
@@ -1,11 +1,11 @@
-cask 'font-averia-gruesa-libre' do
+cask "font-averia-gruesa-libre" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/averiagruesalibre/AveriaGruesaLibre-Regular.ttf'
-  name 'Averia Gruesa Libre'
-  homepage 'https://fonts.google.com/specimen/Averia+Gruesa+Libre'
+  url "https://github.com/google/fonts/raw/master/ofl/averiagruesalibre/AveriaGruesaLibre-Regular.ttf"
+  name "Averia Gruesa Libre"
+  homepage "https://fonts.google.com/specimen/Averia+Gruesa+Libre"
 
-  font 'AveriaGruesaLibre-Regular.ttf'
+  font "AveriaGruesaLibre-Regular.ttf"
 end
