3d402bffb8e278b845f9bf76c564e99451c364da
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:38 +0100

    font-magra.rb: fix for new style

--- Casks/font-magra.rb
+++ Casks/font-magra.rb
@@ -1,16 +1,16 @@
-cask 'font-magra' do
+cask "font-magra" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/magra',
+  url "https://github.com/google/fonts/trunk/ofl/magra",
       using:      :svn,
       trust_cert: true
-  name 'Magra'
-  homepage 'https://fonts.google.com/specimen/Magra'
+  name "Magra"
+  homepage "https://fonts.google.com/specimen/Magra"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Magra-Bold.ttf'
-  font 'Magra-Regular.ttf'
+  font "Magra-Bold.ttf"
+  font "Magra-Regular.ttf"
 end
