9e7cef3991d17770f3105ad9112d615bda10285c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:22 +0100

    font-sura.rb: fix for new style

--- Casks/font-sura.rb
+++ Casks/font-sura.rb
@@ -1,16 +1,16 @@
-cask 'font-sura' do
+cask "font-sura" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/sura',
+  url "https://github.com/google/fonts/trunk/ofl/sura",
       using:      :svn,
       trust_cert: true
-  name 'Sura'
-  homepage 'https://fonts.google.com/specimen/Sura'
+  name "Sura"
+  homepage "https://fonts.google.com/specimen/Sura"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Sura-Bold.ttf'
-  font 'Sura-Regular.ttf'
+  font "Sura-Bold.ttf"
+  font "Sura-Regular.ttf"
 end
