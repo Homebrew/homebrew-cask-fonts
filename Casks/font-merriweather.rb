65fa2c11a1abcd4609f17e52c4abddceb3be6aca
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:41 +0100

    font-merriweather.rb: fix for new style

--- Casks/font-merriweather.rb
+++ Casks/font-merriweather.rb
@@ -1,22 +1,22 @@
-cask 'font-merriweather' do
+cask "font-merriweather" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/merriweather',
+  url "https://github.com/google/fonts/trunk/ofl/merriweather",
       using:      :svn,
       trust_cert: true
-  name 'Merriweather'
-  homepage 'https://fonts.google.com/specimen/Merriweather'
+  name "Merriweather"
+  homepage "https://fonts.google.com/specimen/Merriweather"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Merriweather-Black.ttf'
-  font 'Merriweather-BlackItalic.ttf'
-  font 'Merriweather-Bold.ttf'
-  font 'Merriweather-BoldItalic.ttf'
-  font 'Merriweather-Italic.ttf'
-  font 'Merriweather-Light.ttf'
-  font 'Merriweather-LightItalic.ttf'
-  font 'Merriweather-Regular.ttf'
+  font "Merriweather-Black.ttf"
+  font "Merriweather-BlackItalic.ttf"
+  font "Merriweather-Bold.ttf"
+  font "Merriweather-BoldItalic.ttf"
+  font "Merriweather-Italic.ttf"
+  font "Merriweather-Light.ttf"
+  font "Merriweather-LightItalic.ttf"
+  font "Merriweather-Regular.ttf"
 end
