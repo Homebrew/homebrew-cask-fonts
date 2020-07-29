c25fccbbfbb58e7029b2d2c7a8d1e5ba77a49f65
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:15 +0100

    font-sedan.rb: fix for new style

--- Casks/font-sedan.rb
+++ Casks/font-sedan.rb
@@ -1,16 +1,16 @@
-cask 'font-sedan' do
+cask "font-sedan" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/sedan',
+  url "https://github.com/google/fonts/trunk/ofl/sedan",
       using:      :svn,
       trust_cert: true
-  name 'Sedan'
-  homepage 'https://fonts.google.com/specimen/Sedan'
+  name "Sedan"
+  homepage "https://fonts.google.com/specimen/Sedan"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Sedan-Italic.ttf'
-  font 'Sedan-Regular.ttf'
+  font "Sedan-Italic.ttf"
+  font "Sedan-Regular.ttf"
 end
