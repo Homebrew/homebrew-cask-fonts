24ecdf50ef5c14f70ef0a3d26a346aa69f1773f5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:02 +0100

    font-oswald.rb: fix for new style

--- Casks/font-oswald.rb
+++ Casks/font-oswald.rb
@@ -1,11 +1,11 @@
-cask 'font-oswald' do
+cask "font-oswald" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/oswald/Oswald%5Bwght%5D.ttf'
-  name 'Oswald'
-  homepage 'https://fonts.google.com/specimen/Oswald'
+  url "https://github.com/google/fonts/raw/master/ofl/oswald/Oswald%5Bwght%5D.ttf"
+  name "Oswald"
+  homepage "https://fonts.google.com/specimen/Oswald"
 
-  font 'Oswald[wght].ttf'
+  font "Oswald[wght].ttf"
 end
