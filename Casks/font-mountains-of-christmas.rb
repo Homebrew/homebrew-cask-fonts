c9a7327a4d2ab53f29e46a3dbed9020f1067080c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:46 +0100

    font-mountains-of-christmas.rb: fix for new style

--- Casks/font-mountains-of-christmas.rb
+++ Casks/font-mountains-of-christmas.rb
@@ -1,16 +1,16 @@
-cask 'font-mountains-of-christmas' do
+cask "font-mountains-of-christmas" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/apache/mountainsofchristmas',
+  url "https://github.com/google/fonts/trunk/apache/mountainsofchristmas",
       using:      :svn,
       trust_cert: true
-  name 'Mountains of Christmas'
-  homepage 'https://fonts.google.com/specimen/Mountains+of+Christmas'
+  name "Mountains of Christmas"
+  homepage "https://fonts.google.com/specimen/Mountains+of+Christmas"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'MountainsofChristmas-Bold.ttf'
-  font 'MountainsofChristmas-Regular.ttf'
+  font "MountainsofChristmas-Bold.ttf"
+  font "MountainsofChristmas-Regular.ttf"
 end
