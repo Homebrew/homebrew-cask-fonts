65d4ea48d90f2af820d1c8e85187c631e6769df8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:42 +0100

    font-michroma.rb: fix for new style

--- Casks/font-michroma.rb
+++ Casks/font-michroma.rb
@@ -1,11 +1,11 @@
-cask 'font-michroma' do
+cask "font-michroma" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/michroma/Michroma.ttf'
-  name 'Michroma'
-  homepage 'https://fonts.google.com/specimen/Michroma'
+  url "https://github.com/google/fonts/raw/master/ofl/michroma/Michroma.ttf"
+  name "Michroma"
+  homepage "https://fonts.google.com/specimen/Michroma"
 
-  font 'Michroma.ttf'
+  font "Michroma.ttf"
 end
