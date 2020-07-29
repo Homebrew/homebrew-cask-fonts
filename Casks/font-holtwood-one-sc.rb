2bb263abfa932f766fc518b0f359213bea46f798
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:11 +0100

    font-holtwood-one-sc.rb: fix for new style

--- Casks/font-holtwood-one-sc.rb
+++ Casks/font-holtwood-one-sc.rb
@@ -1,11 +1,11 @@
-cask 'font-holtwood-one-sc' do
+cask "font-holtwood-one-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/holtwoodonesc/HoltwoodOneSC.ttf'
-  name 'Holtwood One SC'
-  homepage 'https://fonts.google.com/specimen/Holtwood+One+SC'
+  url "https://github.com/google/fonts/raw/master/ofl/holtwoodonesc/HoltwoodOneSC.ttf"
+  name "Holtwood One SC"
+  homepage "https://fonts.google.com/specimen/Holtwood+One+SC"
 
-  font 'HoltwoodOneSC.ttf'
+  font "HoltwoodOneSC.ttf"
 end
