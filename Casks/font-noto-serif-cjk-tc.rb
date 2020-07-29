7baa28c91b9100faf47e008c829809c9d29af373
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:58 +0100

    font-noto-serif-cjk-tc.rb: fix for new style

--- Casks/font-noto-serif-cjk-tc.rb
+++ Casks/font-noto-serif-cjk-tc.rb
@@ -1,17 +1,17 @@
-cask 'font-noto-serif-cjk-tc' do
+cask "font-noto-serif-cjk-tc" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKtc-hinted.zip'
-  name 'Noto Serif CJK TC'
-  homepage 'https://www.google.com/get/noto/#serif-hant'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKtc-hinted.zip"
+  name "Noto Serif CJK TC"
+  homepage "https://www.google.com/get/noto/#serif-hant"
 
-  font 'NotoSerifCJKtc-Bold.otf'
-  font 'NotoSerifCJKtc-Light.otf'
-  font 'NotoSerifCJKtc-Regular.otf'
-  font 'NotoSerifCJKtc-Black.otf'
-  font 'NotoSerifCJKtc-ExtraLight.otf'
-  font 'NotoSerifCJKtc-Medium.otf'
-  font 'NotoSerifCJKtc-SemiBold.otf'
+  font "NotoSerifCJKtc-Bold.otf"
+  font "NotoSerifCJKtc-Light.otf"
+  font "NotoSerifCJKtc-Regular.otf"
+  font "NotoSerifCJKtc-Black.otf"
+  font "NotoSerifCJKtc-ExtraLight.otf"
+  font "NotoSerifCJKtc-Medium.otf"
+  font "NotoSerifCJKtc-SemiBold.otf"
 end
