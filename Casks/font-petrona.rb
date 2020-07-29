8c129b02c91d555fb089fd50dad62dce6209b6be
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:04 +0100

    font-petrona.rb: fix for new style

--- Casks/font-petrona.rb
+++ Casks/font-petrona.rb
@@ -1,11 +1,11 @@
-cask 'font-petrona' do
+cask "font-petrona" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/petrona/Petrona-Regular.ttf'
-  name 'Petrona'
-  homepage 'https://fonts.google.com/specimen/Petrona'
+  url "https://github.com/google/fonts/raw/master/ofl/petrona/Petrona-Regular.ttf"
+  name "Petrona"
+  homepage "https://fonts.google.com/specimen/Petrona"
 
-  font 'Petrona-Regular.ttf'
+  font "Petrona-Regular.ttf"
 end
