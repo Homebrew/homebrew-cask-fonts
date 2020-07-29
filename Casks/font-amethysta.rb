e9bffdb3a58aa2d4e1acac9e5c9fadba3a8de16f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:33 +0100

    font-amethysta.rb: fix for new style

--- Casks/font-amethysta.rb
+++ Casks/font-amethysta.rb
@@ -1,11 +1,11 @@
-cask 'font-amethysta' do
+cask "font-amethysta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/amethysta/Amethysta-Regular.ttf'
-  name 'Amethysta'
-  homepage 'https://fonts.google.com/specimen/Amethysta'
+  url "https://github.com/google/fonts/raw/master/ofl/amethysta/Amethysta-Regular.ttf"
+  name "Amethysta"
+  homepage "https://fonts.google.com/specimen/Amethysta"
 
-  font 'Amethysta-Regular.ttf'
+  font "Amethysta-Regular.ttf"
 end
