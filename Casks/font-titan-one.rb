978833df47faa548a08d038c583d931aac339a32
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:25 +0100

    font-titan-one.rb: fix for new style

--- Casks/font-titan-one.rb
+++ Casks/font-titan-one.rb
@@ -1,11 +1,11 @@
-cask 'font-titan-one' do
+cask "font-titan-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/titanone/TitanOne-Regular.ttf'
-  name 'Titan One'
-  homepage 'https://fonts.google.com/specimen/Titan+One'
+  url "https://github.com/google/fonts/raw/master/ofl/titanone/TitanOne-Regular.ttf"
+  name "Titan One"
+  homepage "https://fonts.google.com/specimen/Titan+One"
 
-  font 'TitanOne-Regular.ttf'
+  font "TitanOne-Regular.ttf"
 end
