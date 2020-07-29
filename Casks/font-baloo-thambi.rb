7f797c2df2b6b03c8106f2e9dd90b8b7bb189305
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:15 +0100

    font-baloo-thambi.rb: fix for new style

--- Casks/font-baloo-thambi.rb
+++ Casks/font-baloo-thambi.rb
@@ -1,11 +1,11 @@
-cask 'font-baloo-thambi' do
+cask "font-baloo-thambi" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/baloothambi/BalooThambi-Regular.ttf'
-  name 'Baloo Thambi'
-  homepage 'https://fonts.google.com/specimen/Baloo+Thambi'
+  url "https://github.com/google/fonts/raw/master/ofl/baloothambi/BalooThambi-Regular.ttf"
+  name "Baloo Thambi"
+  homepage "https://fonts.google.com/specimen/Baloo+Thambi"
 
-  font 'BalooThambi-Regular.ttf'
+  font "BalooThambi-Regular.ttf"
 end
