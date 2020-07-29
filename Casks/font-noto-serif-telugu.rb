65bd53eba2cf0781e1776046afd39977979e3641
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:59 +0100

    font-noto-serif-telugu.rb: fix for new style

--- Casks/font-noto-serif-telugu.rb
+++ Casks/font-noto-serif-telugu.rb
@@ -1,12 +1,12 @@
-cask 'font-noto-serif-telugu' do
+cask "font-noto-serif-telugu" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifTelugu-unhinted.zip'
-  name 'Noto Serif Telugu'
-  homepage 'https://www.google.com/get/noto/#serif-telu'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifTelugu-unhinted.zip"
+  name "Noto Serif Telugu"
+  homepage "https://www.google.com/get/noto/#serif-telu"
 
-  font 'NotoSerifTelugu-Bold.ttf'
-  font 'NotoSerifTelugu-Regular.ttf'
+  font "NotoSerifTelugu-Bold.ttf"
+  font "NotoSerifTelugu-Regular.ttf"
 end
