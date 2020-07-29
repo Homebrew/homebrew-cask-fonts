1462aaca665db5d0acc9467787075de91b71a99e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:27 +0100

    font-verdana.rb: fix for new style

--- Casks/font-verdana.rb
+++ Casks/font-verdana.rb
@@ -1,13 +1,13 @@
-cask 'font-verdana' do
+cask "font-verdana" do
   version :latest
   sha256 :no_check
 
-  url 'https://downloads.sourceforge.net/corefonts/verdan32.exe'
-  name 'Verdana'
-  homepage 'https://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  url "https://downloads.sourceforge.net/corefonts/verdan32.exe"
+  name "Verdana"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
 
-  font 'Verdana.TTF'
-  font 'Verdanab.TTF'
-  font 'Verdanai.TTF'
-  font 'Verdanaz.TTF'
+  font "Verdana.TTF"
+  font "Verdanab.TTF"
+  font "Verdanai.TTF"
+  font "Verdanaz.TTF"
 end
