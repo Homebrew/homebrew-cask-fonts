36359e33d69e93a184f2003cac5b4a6a277e12d2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:15 +0100

    font-abril-fatface.rb: fix for new style

--- Casks/font-abril-fatface.rb
+++ Casks/font-abril-fatface.rb
@@ -1,11 +1,11 @@
-cask 'font-abril-fatface' do
+cask "font-abril-fatface" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/abrilfatface/AbrilFatface-Regular.ttf'
-  name 'Abril Fatface'
-  homepage 'https://fonts.google.com/specimen/Abril+Fatface'
+  url "https://github.com/google/fonts/raw/master/ofl/abrilfatface/AbrilFatface-Regular.ttf"
+  name "Abril Fatface"
+  homepage "https://fonts.google.com/specimen/Abril+Fatface"
 
-  font 'AbrilFatface-Regular.ttf'
+  font "AbrilFatface-Regular.ttf"
 end
