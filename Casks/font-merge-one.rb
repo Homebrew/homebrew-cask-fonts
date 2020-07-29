72369366962833507ea26a882956c11e2ad59344
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:41 +0100

    font-merge-one.rb: fix for new style

--- Casks/font-merge-one.rb
+++ Casks/font-merge-one.rb
@@ -1,11 +1,11 @@
-cask 'font-merge-one' do
+cask "font-merge-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/mergeone/MergeOne-Regular.ttf'
-  name 'Merge One'
-  homepage 'https://fonts.google.com/specimen/Merge+One'
+  url "https://github.com/google/fonts/raw/master/ofl/mergeone/MergeOne-Regular.ttf"
+  name "Merge One"
+  homepage "https://fonts.google.com/specimen/Merge+One"
 
-  font 'MergeOne-Regular.ttf'
+  font "MergeOne-Regular.ttf"
 end
