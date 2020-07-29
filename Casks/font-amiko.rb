219a546a76b2085d237d423fd5f8468e9bc56173
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:36 +0100

    font-amiko.rb: fix for new style

--- Casks/font-amiko.rb
+++ Casks/font-amiko.rb
@@ -1,17 +1,17 @@
-cask 'font-amiko' do
+cask "font-amiko" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/amiko',
+  url "https://github.com/google/fonts/trunk/ofl/amiko",
       using:      :svn,
       trust_cert: true
-  name 'Amiko'
-  homepage 'https://fonts.google.com/specimen/Amiko'
+  name "Amiko"
+  homepage "https://fonts.google.com/specimen/Amiko"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Amiko-Bold.ttf'
-  font 'Amiko-Regular.ttf'
-  font 'Amiko-SemiBold.ttf'
+  font "Amiko-Bold.ttf"
+  font "Amiko-Regular.ttf"
+  font "Amiko-SemiBold.ttf"
 end
