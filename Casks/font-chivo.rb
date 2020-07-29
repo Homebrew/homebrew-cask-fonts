fb2ab8bc7b0cca9903a4c7392bf1aa676d6dadd8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:32 +0100

    font-chivo.rb: fix for new style

--- Casks/font-chivo.rb
+++ Casks/font-chivo.rb
@@ -1,22 +1,22 @@
-cask 'font-chivo' do
+cask "font-chivo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/chivo',
+  url "https://github.com/google/fonts/trunk/ofl/chivo",
       using:      :svn,
       trust_cert: true
-  name 'Chivo'
-  homepage 'https://fonts.google.com/specimen/Chivo'
+  name "Chivo"
+  homepage "https://fonts.google.com/specimen/Chivo"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Chivo-Black.ttf'
-  font 'Chivo-BlackItalic.ttf'
-  font 'Chivo-Bold.ttf'
-  font 'Chivo-BoldItalic.ttf'
-  font 'Chivo-Italic.ttf'
-  font 'Chivo-Light.ttf'
-  font 'Chivo-LightItalic.ttf'
-  font 'Chivo-Regular.ttf'
+  font "Chivo-Black.ttf"
+  font "Chivo-BlackItalic.ttf"
+  font "Chivo-Bold.ttf"
+  font "Chivo-BoldItalic.ttf"
+  font "Chivo-Italic.ttf"
+  font "Chivo-Light.ttf"
+  font "Chivo-LightItalic.ttf"
+  font "Chivo-Regular.ttf"
 end
