010d5245e5258fa91dd7f25df01c5574730de5cc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:55 +0100

    font-noto-sans-new-tai-lue.rb: fix for new style

--- Casks/font-noto-sans-new-tai-lue.rb
+++ Casks/font-noto-sans-new-tai-lue.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-new-tai-lue' do
+cask "font-noto-sans-new-tai-lue" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansNewTaiLue-unhinted.zip'
-  name 'Noto Sans New Tai Lue'
-  homepage 'https://www.google.com/get/noto/#sans-talu'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansNewTaiLue-unhinted.zip"
+  name "Noto Sans New Tai Lue"
+  homepage "https://www.google.com/get/noto/#sans-talu"
 
-  font 'NotoSansNewTaiLue-Regular.ttf'
+  font "NotoSansNewTaiLue-Regular.ttf"
 end
