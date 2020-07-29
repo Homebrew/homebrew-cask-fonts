5fa735cc7c8a263641a9a8e74fcb01847dfd388e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:35 +0100

    font-content.rb: fix for new style

--- Casks/font-content.rb
+++ Casks/font-content.rb
@@ -1,16 +1,16 @@
-cask 'font-content' do
+cask "font-content" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/content',
+  url "https://github.com/google/fonts/trunk/ofl/content",
       using:      :svn,
       trust_cert: true
-  name 'Content'
-  homepage 'https://fonts.google.com/specimen/Content'
+  name "Content"
+  homepage "https://fonts.google.com/specimen/Content"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Content-Bold.ttf'
-  font 'Content-Regular.ttf'
+  font "Content-Bold.ttf"
+  font "Content-Regular.ttf"
 end
