702dca2a6ca44ee79d5249e39719371f41a9617f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:28 +0100

    font-ko-pub-batang.rb: fix for new style

--- Casks/font-ko-pub-batang.rb
+++ Casks/font-ko-pub-batang.rb
@@ -1,17 +1,17 @@
-cask 'font-ko-pub-batang' do
+cask "font-ko-pub-batang" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/kopubbatang',
+  url "https://github.com/google/fonts/trunk/ofl/kopubbatang",
       using:      :svn,
       trust_cert: true
-  name 'Ko Pub Batang'
-  homepage 'https://fonts.google.com/earlyaccess'
+  name "Ko Pub Batang"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'KoPubBatang-Bold.ttf'
-  font 'KoPubBatang-Light.ttf'
-  font 'KoPubBatang-Regular.ttf'
+  font "KoPubBatang-Bold.ttf"
+  font "KoPubBatang-Light.ttf"
+  font "KoPubBatang-Regular.ttf"
 end
