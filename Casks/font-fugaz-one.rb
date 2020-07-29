81635e7220330a316e12899be56748bf673dacca
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:57 +0100

    font-fugaz-one.rb: fix for new style

--- Casks/font-fugaz-one.rb
+++ Casks/font-fugaz-one.rb
@@ -1,11 +1,11 @@
-cask 'font-fugaz-one' do
+cask "font-fugaz-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/fugazone/FugazOne-Regular.ttf'
-  name 'Fugaz One'
-  homepage 'https://fonts.google.com/specimen/Fugaz+One'
+  url "https://github.com/google/fonts/raw/master/ofl/fugazone/FugazOne-Regular.ttf"
+  name "Fugaz One"
+  homepage "https://fonts.google.com/specimen/Fugaz+One"
 
-  font 'FugazOne-Regular.ttf'
+  font "FugazOne-Regular.ttf"
 end
