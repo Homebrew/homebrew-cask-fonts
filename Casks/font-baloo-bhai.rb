e30d97aeee10e9b10702be6d4acdd5b1a73ba836
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:37:34 +0100

    font-baloo-bhai.rb: fix for new style

--- Casks/font-baloo-bhai.rb
+++ Casks/font-baloo-bhai.rb
@@ -1,11 +1,11 @@
-cask 'font-baloo-bhai' do
+cask "font-baloo-bhai" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/baloobhai/BalooBhai-Regular.ttf'
-  name 'Baloo Bhai'
-  homepage 'https://fonts.google.com/specimen/Baloo+Bhai'
+  url "https://github.com/google/fonts/raw/master/ofl/baloobhai/BalooBhai-Regular.ttf"
+  name "Baloo Bhai"
+  homepage "https://fonts.google.com/specimen/Baloo+Bhai"
 
-  font 'BalooBhai-Regular.ttf'
+  font "BalooBhai-Regular.ttf"
 end
