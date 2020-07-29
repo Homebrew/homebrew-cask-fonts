dbb18dc933d946f136b75126d25ae9d95696cca2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:56 +0100

    font-ahuramzda.rb: fix for new style

--- Casks/font-ahuramzda.rb
+++ Casks/font-ahuramzda.rb
@@ -1,10 +1,10 @@
-cask 'font-ahuramzda' do
-  version '1.000'
-  sha256 '5a8afb0b24ceeb98f1ef121406ceecb124f2a300c5ee7877a7ff6abdd697b160'
+cask "font-ahuramzda" do
+  version "1.000"
+  sha256 "5a8afb0b24ceeb98f1ef121406ceecb124f2a300c5ee7877a7ff6abdd697b160"
 
-  url 'https://fontlibrary.org/assets/downloads/ahuramazda/b2c0eeb9186f389749746f075b5a1abf/ahuramazda.zip'
-  name 'Ahuramzda'
-  homepage 'https://fontlibrary.org/font/ahuramazda/'
+  url "https://fontlibrary.org/assets/downloads/ahuramazda/b2c0eeb9186f389749746f075b5a1abf/ahuramazda.zip"
+  name "Ahuramzda"
+  homepage "https://fontlibrary.org/font/ahuramazda/"
 
-  font 'Ahuramazda-Avestan-Font-1.0/ahuramazda.ttf'
+  font "Ahuramazda-Avestan-Font-1.0/ahuramazda.ttf"
 end
