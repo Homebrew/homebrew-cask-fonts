bb4e1faeeb6106f3f8acac3d1df38276a0157a52
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:17 +0100

    font-infini.rb: fix for new style

--- Casks/font-infini.rb
+++ Casks/font-infini.rb
@@ -1,13 +1,13 @@
-cask 'font-infini' do
-  version '1.003'
-  sha256 '410d742658b3da3f5e7446a8aa18cedb3be12b26b0e7eebe5b7c830058be56b5'
+cask "font-infini" do
+  version "1.003"
+  sha256 "410d742658b3da3f5e7446a8aa18cedb3be12b26b0e7eebe5b7c830058be56b5"
 
-  url 'http://www.cnap.graphismeenfrance.fr/infini/en/download/Infini-cnap-engl_fonts-specimen-v2.zip'
-  name 'Infini'
-  homepage 'http://www.cnap.graphismeenfrance.fr/infini/en/'
+  url "http://www.cnap.graphismeenfrance.fr/infini/en/download/Infini-cnap-engl_fonts-specimen-v2.zip"
+  name "Infini"
+  homepage "http://www.cnap.graphismeenfrance.fr/infini/en/"
 
-  font 'Infini-cnap-engl_fonts-specimen-v2/01_Fonts/01_Regular/infini-regular.otf'
-  font 'Infini-cnap-engl_fonts-specimen-v2/01_Fonts/02_Italic/infini-italic.otf'
-  font 'Infini-cnap-engl_fonts-specimen-v2/01_Fonts/03_Bold/infini-bold.otf'
-  font 'Infini-cnap-engl_fonts-specimen-v2/01_Fonts/04_Pictos/infini-picto.otf'
+  font "Infini-cnap-engl_fonts-specimen-v2/01_Fonts/01_Regular/infini-regular.otf"
+  font "Infini-cnap-engl_fonts-specimen-v2/01_Fonts/02_Italic/infini-italic.otf"
+  font "Infini-cnap-engl_fonts-specimen-v2/01_Fonts/03_Bold/infini-bold.otf"
+  font "Infini-cnap-engl_fonts-specimen-v2/01_Fonts/04_Pictos/infini-picto.otf"
 end
