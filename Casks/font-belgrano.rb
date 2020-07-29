e34937992c8a9cc4e28658d529e43b26ff217f9e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:17 +0100

    font-belgrano.rb: fix for new style

--- Casks/font-belgrano.rb
+++ Casks/font-belgrano.rb
@@ -1,11 +1,11 @@
-cask 'font-belgrano' do
+cask "font-belgrano" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/belgrano/Belgrano-Regular.ttf'
-  name 'Belgrano'
-  homepage 'https://fonts.google.com/specimen/Belgrano'
+  url "https://github.com/google/fonts/raw/master/ofl/belgrano/Belgrano-Regular.ttf"
+  name "Belgrano"
+  homepage "https://fonts.google.com/specimen/Belgrano"
 
-  font 'Belgrano-Regular.ttf'
+  font "Belgrano-Regular.ttf"
 end
