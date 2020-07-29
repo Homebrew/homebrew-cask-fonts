1e8329647cdc100d7c5f18bce0c6417b6dc806f4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:37 +0100

    font-lustria.rb: fix for new style

--- Casks/font-lustria.rb
+++ Casks/font-lustria.rb
@@ -1,11 +1,11 @@
-cask 'font-lustria' do
+cask "font-lustria" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/lustria/Lustria-Regular.ttf'
-  name 'Lustria'
-  homepage 'https://fonts.google.com/specimen/Lustria'
+  url "https://github.com/google/fonts/raw/master/ofl/lustria/Lustria-Regular.ttf"
+  name "Lustria"
+  homepage "https://fonts.google.com/specimen/Lustria"
 
-  font 'Lustria-Regular.ttf'
+  font "Lustria-Regular.ttf"
 end
