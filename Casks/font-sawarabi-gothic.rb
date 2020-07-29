564cec80e63ce202f6792afb8a7f346a8bded9bc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:15 +0100

    font-sawarabi-gothic.rb: fix for new style

--- Casks/font-sawarabi-gothic.rb
+++ Casks/font-sawarabi-gothic.rb
@@ -1,11 +1,11 @@
-cask 'font-sawarabi-gothic' do
+cask "font-sawarabi-gothic" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/sawarabigothic/SawarabiGothic-Regular.ttf'
-  name 'Sawarabi Gothic'
-  homepage 'https://fonts.google.com/specimen/Sawarabi+Gothic'
+  url "https://github.com/google/fonts/raw/master/ofl/sawarabigothic/SawarabiGothic-Regular.ttf"
+  name "Sawarabi Gothic"
+  homepage "https://fonts.google.com/specimen/Sawarabi+Gothic"
 
-  font 'SawarabiGothic-Regular.ttf'
+  font "SawarabiGothic-Regular.ttf"
 end
