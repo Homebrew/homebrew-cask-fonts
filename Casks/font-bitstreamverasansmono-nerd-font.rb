fe69d152d0b1ef8bb8a6d9a231d7611c64167273
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:20 +0100

    font-bitstreamverasansmono-nerd-font.rb: fix for new style

--- Casks/font-bitstreamverasansmono-nerd-font.rb
+++ Casks/font-bitstreamverasansmono-nerd-font.rb
@@ -1,18 +1,18 @@
-cask 'font-bitstreamverasansmono-nerd-font' do
-  version '2.1.0'
-  sha256 'b31595d1e717156a1f42b0ff635b93d5da631312e4f0bd5c581259171e4a42d8'
+cask "font-bitstreamverasansmono-nerd-font" do
+  version "2.1.0"
+  sha256 "b31595d1e717156a1f42b0ff635b93d5da631312e4f0bd5c581259171e4a42d8"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BitstreamVeraSansMono.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'BitstreamVeraSansMono Nerd Font (Bitstream Vera Sans Mono)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "BitstreamVeraSansMono Nerd Font (Bitstream Vera Sans Mono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Bitstream Vera Sans Mono Bold Nerd Font Complete.ttf'
-  font 'Bitstream Vera Sans Mono Bold Nerd Font Complete Mono.ttf'
-  font 'Bitstream Vera Sans Mono Bold Oblique Nerd Font Complete.ttf'
-  font 'Bitstream Vera Sans Mono Bold Oblique Nerd Font Complete Mono.ttf'
-  font 'Bitstream Vera Sans Mono Nerd Font Complete Mono.ttf'
-  font 'Bitstream Vera Sans Mono Nerd Font Complete.ttf'
-  font 'Bitstream Vera Sans Mono Oblique Nerd Font Complete.ttf'
-  font 'Bitstream Vera Sans Mono Oblique Nerd Font Complete Mono.ttf'
+  font "Bitstream Vera Sans Mono Bold Nerd Font Complete.ttf"
+  font "Bitstream Vera Sans Mono Bold Nerd Font Complete Mono.ttf"
+  font "Bitstream Vera Sans Mono Bold Oblique Nerd Font Complete.ttf"
+  font "Bitstream Vera Sans Mono Bold Oblique Nerd Font Complete Mono.ttf"
+  font "Bitstream Vera Sans Mono Nerd Font Complete Mono.ttf"
+  font "Bitstream Vera Sans Mono Nerd Font Complete.ttf"
+  font "Bitstream Vera Sans Mono Oblique Nerd Font Complete.ttf"
+  font "Bitstream Vera Sans Mono Oblique Nerd Font Complete Mono.ttf"
 end
