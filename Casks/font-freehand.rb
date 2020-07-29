a62c6ce6d424d77455297c16e434678051186379
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:57 +0100

    font-freehand.rb: fix for new style

--- Casks/font-freehand.rb
+++ Casks/font-freehand.rb
@@ -1,11 +1,11 @@
-cask 'font-freehand' do
+cask "font-freehand" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/freehand/Freehand.ttf'
-  name 'Freehand'
-  homepage 'https://fonts.google.com/specimen/Freehand'
+  url "https://github.com/google/fonts/raw/master/ofl/freehand/Freehand.ttf"
+  name "Freehand"
+  homepage "https://fonts.google.com/specimen/Freehand"
 
-  font 'Freehand.ttf'
+  font "Freehand.ttf"
 end
