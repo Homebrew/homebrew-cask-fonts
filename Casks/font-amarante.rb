ed9ed1faf35f53fc26930a74c50af7a1f49116b8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:21 +0100

    font-amarante.rb: fix for new style

--- Casks/font-amarante.rb
+++ Casks/font-amarante.rb
@@ -1,11 +1,11 @@
-cask 'font-amarante' do
+cask "font-amarante" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/amarante/Amarante-Regular.ttf'
-  name 'Amarante'
-  homepage 'https://fonts.google.com/specimen/Amarante'
+  url "https://github.com/google/fonts/raw/master/ofl/amarante/Amarante-Regular.ttf"
+  name "Amarante"
+  homepage "https://fonts.google.com/specimen/Amarante"
 
-  font 'Amarante-Regular.ttf'
+  font "Amarante-Regular.ttf"
 end
