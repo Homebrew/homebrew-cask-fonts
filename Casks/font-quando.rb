45f4861118029fe1baca719fbff444cd96c32fb7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:08 +0100

    font-quando.rb: fix for new style

--- Casks/font-quando.rb
+++ Casks/font-quando.rb
@@ -1,11 +1,11 @@
-cask 'font-quando' do
+cask "font-quando" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/quando/Quando-Regular.ttf'
-  name 'Quando'
-  homepage 'https://fonts.google.com/specimen/Quando'
+  url "https://github.com/google/fonts/raw/master/ofl/quando/Quando-Regular.ttf"
+  name "Quando"
+  homepage "https://fonts.google.com/specimen/Quando"
 
-  font 'Quando-Regular.ttf'
+  font "Quando-Regular.ttf"
 end
