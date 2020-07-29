c5fee99dab24fc82a4284bf1aa9e2ccce6171aa0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:56 +0100

    font-noto-sans-syriac-western.rb: fix for new style

--- Casks/font-noto-sans-syriac-western.rb
+++ Casks/font-noto-sans-syriac-western.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-syriac-western' do
+cask "font-noto-sans-syriac-western" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacWestern-unhinted.zip'
-  name 'Noto Sans Syriac Western'
-  homepage 'https://www.google.com/get/noto/#sans-syrc-western'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacWestern-unhinted.zip"
+  name "Noto Sans Syriac Western"
+  homepage "https://www.google.com/get/noto/#sans-syrc-western"
 
-  font 'NotoSansSyriacWestern-Regular.ttf'
+  font "NotoSansSyriacWestern-Regular.ttf"
 end
