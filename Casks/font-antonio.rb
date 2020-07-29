92acb911cbc8caa2b37b0c8f8476afcbbe0a2fc5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:44 +0100

    font-antonio.rb: fix for new style

--- Casks/font-antonio.rb
+++ Casks/font-antonio.rb
@@ -1,17 +1,17 @@
-cask 'font-antonio' do
+cask "font-antonio" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/antonio',
+  url "https://github.com/google/fonts/trunk/ofl/antonio",
       using:      :svn,
       trust_cert: true
-  name 'Antonio'
-  homepage 'https://fonts.google.com/specimen/Antonio'
+  name "Antonio"
+  homepage "https://fonts.google.com/specimen/Antonio"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Antonio-Bold.ttf'
-  font 'Antonio-Light.ttf'
-  font 'Antonio-Regular.ttf'
+  font "Antonio-Bold.ttf"
+  font "Antonio-Light.ttf"
+  font "Antonio-Regular.ttf"
 end
