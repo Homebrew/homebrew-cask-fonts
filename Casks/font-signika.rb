2ff8dcab83fa18f616523592cc84bdfd772be963
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:17 +0100

    font-signika.rb: fix for new style

--- Casks/font-signika.rb
+++ Casks/font-signika.rb
@@ -1,18 +1,18 @@
-cask 'font-signika' do
+cask "font-signika" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/signika',
+  url "https://github.com/google/fonts/trunk/ofl/signika",
       using:      :svn,
       trust_cert: true
-  name 'Signika'
-  homepage 'https://fonts.google.com/specimen/Signika'
+  name "Signika"
+  homepage "https://fonts.google.com/specimen/Signika"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Signika-Bold.ttf'
-  font 'Signika-Light.ttf'
-  font 'Signika-Regular.ttf'
-  font 'Signika-SemiBold.ttf'
+  font "Signika-Bold.ttf"
+  font "Signika-Light.ttf"
+  font "Signika-Regular.ttf"
+  font "Signika-SemiBold.ttf"
 end
