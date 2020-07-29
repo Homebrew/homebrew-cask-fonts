551fce707d7e572123f5529cb1df2d0ed3e339ab
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:45 +0100

    font-monoisome.rb: fix for new style

--- Casks/font-monoisome.rb
+++ Casks/font-monoisome.rb
@@ -1,12 +1,12 @@
-cask 'font-monoisome' do
-  version '0.61'
-  sha256 '0aedac5663ea33f5a7d5c802408ff0ee16cf62b680f9e05980ccd2ba40040930'
+cask "font-monoisome" do
+  version "0.61"
+  sha256 "0aedac5663ea33f5a7d5c802408ff0ee16cf62b680f9e05980ccd2ba40040930"
 
   # github.com/larsenwork/monoid/ was verified as official when first introduced to the cask
-  url 'https://github.com/larsenwork/monoid/blob/master/Monoisome/Monoisome-Regular.ttf?raw=true'
-  appcast 'https://github.com/larsenwork/monoid/releases.atom'
-  name 'Monoisome'
-  homepage 'https://larsenwork.com/monoid/'
+  url "https://github.com/larsenwork/monoid/blob/master/Monoisome/Monoisome-Regular.ttf?raw=true"
+  appcast "https://github.com/larsenwork/monoid/releases.atom"
+  name "Monoisome"
+  homepage "https://larsenwork.com/monoid/"
 
-  font 'Monoisome-Regular.ttf'
+  font "Monoisome-Regular.ttf"
 end
