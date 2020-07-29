abb1d35bb084b41c2546483e6f1bc39d6a68ae98
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:22 +0100

    font-suranna.rb: fix for new style

--- Casks/font-suranna.rb
+++ Casks/font-suranna.rb
@@ -1,11 +1,11 @@
-cask 'font-suranna' do
+cask "font-suranna" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/suranna/Suranna-Regular.ttf'
-  name 'Suranna'
-  homepage 'https://fonts.google.com/specimen/Suranna'
+  url "https://github.com/google/fonts/raw/master/ofl/suranna/Suranna-Regular.ttf"
+  name "Suranna"
+  homepage "https://fonts.google.com/specimen/Suranna"
 
-  font 'Suranna-Regular.ttf'
+  font "Suranna-Regular.ttf"
 end
