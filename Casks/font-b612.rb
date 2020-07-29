4cfdec926b2008ce5e8e188e39c781c2c917b584
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:37:06 +0100

    font-b612.rb: fix for new style

--- Casks/font-b612.rb
+++ Casks/font-b612.rb
@@ -1,18 +1,18 @@
-cask 'font-b612' do
+cask "font-b612" do
   version :latest
   sha256 :no_check
 
   # github.com/polarsys/b612/ was verified as official when first introduced to the cask
-  url 'https://github.com/polarsys/b612/archive/master.zip'
-  name 'B612'
-  homepage 'https://b612-font.com/'
+  url "https://github.com/polarsys/b612/archive/master.zip"
+  name "B612"
+  homepage "https://b612-font.com/"
 
-  font 'b612-master/fonts/ttf/B612-Bold.ttf'
-  font 'b612-master/fonts/ttf/B612-BoldItalic.ttf'
-  font 'b612-master/fonts/ttf/B612-Italic.ttf'
-  font 'b612-master/fonts/ttf/B612-Regular.ttf'
-  font 'b612-master/fonts/ttf/B612Mono-Bold.ttf'
-  font 'b612-master/fonts/ttf/B612Mono-BoldItalic.ttf'
-  font 'b612-master/fonts/ttf/B612Mono-Italic.ttf'
-  font 'b612-master/fonts/ttf/B612Mono-Regular.ttf'
+  font "b612-master/fonts/ttf/B612-Bold.ttf"
+  font "b612-master/fonts/ttf/B612-BoldItalic.ttf"
+  font "b612-master/fonts/ttf/B612-Italic.ttf"
+  font "b612-master/fonts/ttf/B612-Regular.ttf"
+  font "b612-master/fonts/ttf/B612Mono-Bold.ttf"
+  font "b612-master/fonts/ttf/B612Mono-BoldItalic.ttf"
+  font "b612-master/fonts/ttf/B612Mono-Italic.ttf"
+  font "b612-master/fonts/ttf/B612Mono-Regular.ttf"
 end
