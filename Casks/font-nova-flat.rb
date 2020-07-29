7fde465895327cc6fd7e6e3da665c64c2b245bd3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:59 +0100

    font-nova-flat.rb: fix for new style

--- Casks/font-nova-flat.rb
+++ Casks/font-nova-flat.rb
@@ -1,11 +1,11 @@
-cask 'font-nova-flat' do
+cask "font-nova-flat" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/novaflat/NovaFlat.ttf'
-  name 'Nova Flat'
-  homepage 'https://fonts.google.com/specimen/Nova+Flat'
+  url "https://github.com/google/fonts/raw/master/ofl/novaflat/NovaFlat.ttf"
+  name "Nova Flat"
+  homepage "https://fonts.google.com/specimen/Nova+Flat"
 
-  font 'NovaFlat.ttf'
+  font "NovaFlat.ttf"
 end
