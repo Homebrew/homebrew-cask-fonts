de11b2e8c4deeb1c165feb0a821e79ada719d92e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:52 +0100

    font-noto-sans-cjk.rb: fix for new style

--- Casks/font-noto-sans-cjk.rb
+++ Casks/font-noto-sans-cjk.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-cjk' do
+cask "font-noto-sans-cjk" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJK.ttc.zip'
-  name 'Noto Sans CJK'
-  homepage 'https://www.google.com/get/noto/help/cjk/'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJK.ttc.zip"
+  name "Noto Sans CJK"
+  homepage "https://www.google.com/get/noto/help/cjk/"
 
-  font 'NotoSansCJK.ttc'
+  font "NotoSansCJK.ttc"
 end
