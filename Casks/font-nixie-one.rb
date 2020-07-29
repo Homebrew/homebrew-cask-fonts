035bc0181f0cfcb7d495eef4ce726446df20c61c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:49 +0100

    font-nixie-one.rb: fix for new style

--- Casks/font-nixie-one.rb
+++ Casks/font-nixie-one.rb
@@ -1,11 +1,11 @@
-cask 'font-nixie-one' do
+cask "font-nixie-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/nixieone/NixieOne-Regular.ttf'
-  name 'Nixie One'
-  homepage 'https://fonts.google.com/specimen/Nixie+One'
+  url "https://github.com/google/fonts/raw/master/ofl/nixieone/NixieOne-Regular.ttf"
+  name "Nixie One"
+  homepage "https://fonts.google.com/specimen/Nixie+One"
 
-  font 'NixieOne-Regular.ttf'
+  font "NixieOne-Regular.ttf"
 end
