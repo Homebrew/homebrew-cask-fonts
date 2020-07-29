43396d035eabaf8cfe318c739d96643861338c38
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:32 +0100

    font-armata.rb: fix for new style

--- Casks/font-armata.rb
+++ Casks/font-armata.rb
@@ -1,11 +1,11 @@
-cask 'font-armata' do
+cask "font-armata" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/armata/Armata-Regular.ttf'
-  name 'Armata'
-  homepage 'https://fonts.google.com/specimen/Armata'
+  url "https://github.com/google/fonts/raw/master/ofl/armata/Armata-Regular.ttf"
+  name "Armata"
+  homepage "https://fonts.google.com/specimen/Armata"
 
-  font 'Armata-Regular.ttf'
+  font "Armata-Regular.ttf"
 end
