f9ed82f9babed2aefd0f037e92aec53ea13c5431
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:06 +0100

    font-pragati-narrow.rb: fix for new style

--- Casks/font-pragati-narrow.rb
+++ Casks/font-pragati-narrow.rb
@@ -1,16 +1,16 @@
-cask 'font-pragati-narrow' do
+cask "font-pragati-narrow" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/pragatinarrow',
+  url "https://github.com/google/fonts/trunk/ofl/pragatinarrow",
       using:      :svn,
       trust_cert: true
-  name 'Pragati Narrow'
-  homepage 'https://fonts.google.com/specimen/Pragati+Narrow'
+  name "Pragati Narrow"
+  homepage "https://fonts.google.com/specimen/Pragati+Narrow"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'PragatiNarrow-Bold.ttf'
-  font 'PragatiNarrow-Regular.ttf'
+  font "PragatiNarrow-Bold.ttf"
+  font "PragatiNarrow-Regular.ttf"
 end
