e94c40d192a9d0118f0540b33a97709cd630c407
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:39 +0100

    font-crimson-pro.rb: fix for new style

--- Casks/font-crimson-pro.rb
+++ Casks/font-crimson-pro.rb
@@ -1,16 +1,16 @@
-cask 'font-crimson-pro' do
+cask "font-crimson-pro" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/crimsonpro',
+  url "https://github.com/google/fonts/trunk/ofl/crimsonpro",
       using:      :svn,
       trust_cert: true
-  name 'Crimson Pro'
-  homepage 'https://fonts.google.com/specimen/Crimson+Pro'
+  name "Crimson Pro"
+  homepage "https://fonts.google.com/specimen/Crimson+Pro"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'CrimsonPro-Italic[wght].ttf'
-  font 'CrimsonPro[wght].ttf'
+  font "CrimsonPro-Italic[wght].ttf"
+  font "CrimsonPro[wght].ttf"
 end
