5e0d7b879fd2fb5b9d9bb6456e5bbd04f42733a3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:08 +0100

    font-hannari.rb: fix for new style

--- Casks/font-hannari.rb
+++ Casks/font-hannari.rb
@@ -1,11 +1,11 @@
-cask 'font-hannari' do
+cask "font-hannari" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/hannari/Hannari-Regular.ttf'
-  name 'Hannari'
-  homepage 'https://fonts.google.com/specimen/Hannari'
+  url "https://github.com/google/fonts/raw/master/ofl/hannari/Hannari-Regular.ttf"
+  name "Hannari"
+  homepage "https://fonts.google.com/specimen/Hannari"
 
-  font 'Hannari-Regular.ttf'
+  font "Hannari-Regular.ttf"
 end
