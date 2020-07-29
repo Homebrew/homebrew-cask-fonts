4f234ece160d342b802832042e34d10d429ce4b3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:55 +0100

    font-noto-sans-ol-chiki.rb: fix for new style

--- Casks/font-noto-sans-ol-chiki.rb
+++ Casks/font-noto-sans-ol-chiki.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-ol-chiki' do
+cask "font-noto-sans-ol-chiki" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOlChiki-unhinted.zip'
-  name 'Noto Sans Ol Chiki'
-  homepage 'https://www.google.com/get/noto/#sans-olck'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOlChiki-unhinted.zip"
+  name "Noto Sans Ol Chiki"
+  homepage "https://www.google.com/get/noto/#sans-olck"
 
-  font 'NotoSansOlChiki-Regular.ttf'
+  font "NotoSansOlChiki-Regular.ttf"
 end
