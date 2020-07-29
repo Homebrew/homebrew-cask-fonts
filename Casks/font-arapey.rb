976b4a05901cbb6dc94b0cf31b7d1967c5e562fc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:47 +0100

    font-arapey.rb: fix for new style

--- Casks/font-arapey.rb
+++ Casks/font-arapey.rb
@@ -1,16 +1,16 @@
-cask 'font-arapey' do
+cask "font-arapey" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/arapey',
+  url "https://github.com/google/fonts/trunk/ofl/arapey",
       using:      :svn,
       trust_cert: true
-  name 'Arapey'
-  homepage 'https://fonts.google.com/specimen/Arapey'
+  name "Arapey"
+  homepage "https://fonts.google.com/specimen/Arapey"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Arapey-Italic.ttf'
-  font 'Arapey-Regular.ttf'
+  font "Arapey-Italic.ttf"
+  font "Arapey-Regular.ttf"
 end
