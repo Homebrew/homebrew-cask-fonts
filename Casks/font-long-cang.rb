8b9b474a93a44c2e85c42cfc21f09733c73e3490
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:37 +0100

    font-long-cang.rb: fix for new style

--- Casks/font-long-cang.rb
+++ Casks/font-long-cang.rb
@@ -1,11 +1,11 @@
-cask 'font-long-cang' do
+cask "font-long-cang" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/longcang/LongCang-Regular.ttf'
-  name 'Long Cang'
-  homepage 'https://fonts.google.com/specimen/Long+Cang'
+  url "https://github.com/google/fonts/raw/master/ofl/longcang/LongCang-Regular.ttf"
+  name "Long Cang"
+  homepage "https://fonts.google.com/specimen/Long+Cang"
 
-  font 'LongCang-Regular.ttf'
+  font "LongCang-Regular.ttf"
 end
