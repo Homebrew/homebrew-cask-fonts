31bfa44d834a48a8048c15894953cdd5a8a3c585
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:01 +0100

    font-old-standard-tt.rb: fix for new style

--- Casks/font-old-standard-tt.rb
+++ Casks/font-old-standard-tt.rb
@@ -1,17 +1,17 @@
-cask 'font-old-standard-tt' do
+cask "font-old-standard-tt" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/oldstandardtt',
+  url "https://github.com/google/fonts/trunk/ofl/oldstandardtt",
       using:      :svn,
       trust_cert: true
-  name 'Old Standard TT'
-  homepage 'https://fonts.google.com/specimen/Old+Standard+TT'
+  name "Old Standard TT"
+  homepage "https://fonts.google.com/specimen/Old+Standard+TT"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'OldStandard-Bold.ttf'
-  font 'OldStandard-Italic.ttf'
-  font 'OldStandard-Regular.ttf'
+  font "OldStandard-Bold.ttf"
+  font "OldStandard-Italic.ttf"
+  font "OldStandard-Regular.ttf"
 end
