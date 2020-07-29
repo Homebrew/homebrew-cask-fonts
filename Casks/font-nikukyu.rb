dc9047830f838e01462bcbd87b407fcc4cedbbbe
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:49 +0100

    font-nikukyu.rb: fix for new style

--- Casks/font-nikukyu.rb
+++ Casks/font-nikukyu.rb
@@ -1,11 +1,11 @@
-cask 'font-nikukyu' do
+cask "font-nikukyu" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/nikukyu/Nikukyu-Regular.ttf'
-  name 'Nikukyu'
-  homepage 'https://fonts.google.com/specimen/Nikukyu'
+  url "https://github.com/google/fonts/raw/master/ofl/nikukyu/Nikukyu-Regular.ttf"
+  name "Nikukyu"
+  homepage "https://fonts.google.com/specimen/Nikukyu"
 
-  font 'Nikukyu-Regular.ttf'
+  font "Nikukyu-Regular.ttf"
 end
