b2116d9ab4c24cef87bdcd0fb9fe79f309bd76d2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:10 +0100

    font-redressed.rb: fix for new style

--- Casks/font-redressed.rb
+++ Casks/font-redressed.rb
@@ -1,11 +1,11 @@
-cask 'font-redressed' do
+cask "font-redressed" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/redressed/Redressed-Regular.ttf'
-  name 'Redressed'
-  homepage 'https://fonts.google.com/specimen/Redressed'
+  url "https://github.com/google/fonts/raw/master/apache/redressed/Redressed-Regular.ttf"
+  name "Redressed"
+  homepage "https://fonts.google.com/specimen/Redressed"
 
-  font 'Redressed-Regular.ttf'
+  font "Redressed-Regular.ttf"
 end
