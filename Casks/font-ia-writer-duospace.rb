f8d45ba757040f476011cad654b375b170bbe254
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:12 +0100

    font-ia-writer-duospace.rb: fix for new style

--- Casks/font-ia-writer-duospace.rb
+++ Casks/font-ia-writer-duospace.rb
@@ -1,13 +1,13 @@
-cask 'font-ia-writer-duospace' do
+cask "font-ia-writer-duospace" do
   version :latest
   sha256 :no_check
 
-  url 'https://codeload.github.com/iaolo/iA-Fonts/zip/master'
-  name 'iA Writer Duospace'
-  homepage 'https://github.com/iaolo/iA-Fonts'
+  url "https://codeload.github.com/iaolo/iA-Fonts/zip/master"
+  name "iA Writer Duospace"
+  homepage "https://github.com/iaolo/iA-Fonts"
 
-  font 'iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Bold.otf'
-  font 'iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-BoldItalic.otf'
-  font 'iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Regular.otf'
-  font 'iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Italic.otf'
+  font "iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Bold.otf"
+  font "iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-BoldItalic.otf"
+  font "iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Regular.otf"
+  font "iA-Fonts-master/iA Writer Duospace/OTF (Mac)/iAWriterDuospace-Italic.otf"
 end
