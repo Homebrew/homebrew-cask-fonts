628bcda3fea880da388461a10e26d6a3708c3b6a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:46 +0100

    font-eater-caps.rb: fix for new style

--- Casks/font-eater-caps.rb
+++ Casks/font-eater-caps.rb
@@ -1,11 +1,11 @@
-cask 'font-eater-caps' do
+cask "font-eater-caps" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/eatercaps/EaterCaps-Regular.ttf'
-  name 'Eater Caps'
-  homepage 'https://fonts.google.com/specimen/Eater+Caps'
+  url "https://github.com/google/fonts/raw/master/ofl/eatercaps/EaterCaps-Regular.ttf"
+  name "Eater Caps"
+  homepage "https://fonts.google.com/specimen/Eater+Caps"
 
-  font 'EaterCaps-Regular.ttf'
+  font "EaterCaps-Regular.ttf"
 end
