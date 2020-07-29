c7f52eedf284c802f8999bf87b2cc8ed3eb891af
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:14 +0100

    font-sarasa-gothic.rb: fix for new style

--- Casks/font-sarasa-gothic.rb
+++ Casks/font-sarasa-gothic.rb
@@ -1,20 +1,20 @@
-cask 'font-sarasa-gothic' do
-  version '0.12.10'
-  sha256 '78a4aa58d573b10f5f61f7a3d704bcee1e3e39b82aa4c824be7570889cb07755'
+cask "font-sarasa-gothic" do
+  version "0.12.10"
+  sha256 "78a4aa58d573b10f5f61f7a3d704bcee1e3e39b82aa4c824be7570889cb07755"
 
   url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
-  appcast 'https://github.com/be5invis/Sarasa-Gothic/releases.atom'
-  name 'Sarasa Gothic'
-  homepage 'https://github.com/be5invis/Sarasa-Gothic'
+  appcast "https://github.com/be5invis/Sarasa-Gothic/releases.atom"
+  name "Sarasa Gothic"
+  homepage "https://github.com/be5invis/Sarasa-Gothic"
 
-  font 'sarasa-bold.ttc'
-  font 'sarasa-bolditalic.ttc'
-  font 'sarasa-extralight.ttc'
-  font 'sarasa-extralightitalic.ttc'
-  font 'sarasa-italic.ttc'
-  font 'sarasa-light.ttc'
-  font 'sarasa-lightitalic.ttc'
-  font 'sarasa-regular.ttc'
-  font 'sarasa-semibold.ttc'
-  font 'sarasa-semibolditalic.ttc'
+  font "sarasa-bold.ttc"
+  font "sarasa-bolditalic.ttc"
+  font "sarasa-extralight.ttc"
+  font "sarasa-extralightitalic.ttc"
+  font "sarasa-italic.ttc"
+  font "sarasa-light.ttc"
+  font "sarasa-lightitalic.ttc"
+  font "sarasa-regular.ttc"
+  font "sarasa-semibold.ttc"
+  font "sarasa-semibolditalic.ttc"
 end
