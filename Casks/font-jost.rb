4398db3590687f152d4246fb60aeddac5be48afa
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:24 +0100

    font-jost.rb: fix for new style

--- Casks/font-jost.rb
+++ Casks/font-jost.rb
@@ -1,16 +1,16 @@
-cask 'font-jost' do
+cask "font-jost" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/jost',
+  url "https://github.com/google/fonts/trunk/ofl/jost",
       using:      :svn,
       trust_cert: true
-  name 'Jost'
-  homepage 'https://fonts.google.com/specimen/Jost'
+  name "Jost"
+  homepage "https://fonts.google.com/specimen/Jost"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Jost-Italic[wght].ttf'
-  font 'Jost[wght].ttf'
+  font "Jost-Italic[wght].ttf"
+  font "Jost[wght].ttf"
 end
