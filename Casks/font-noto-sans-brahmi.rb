32789eae5728d897b1777570874908701f80dd55
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:51 +0100

    font-noto-sans-brahmi.rb: fix for new style

--- Casks/font-noto-sans-brahmi.rb
+++ Casks/font-noto-sans-brahmi.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-brahmi' do
+cask "font-noto-sans-brahmi" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBrahmi-unhinted.zip'
-  name 'Noto Sans Brahmi'
-  homepage 'https://www.google.com/get/noto/#sans-brah'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBrahmi-unhinted.zip"
+  name "Noto Sans Brahmi"
+  homepage "https://www.google.com/get/noto/#sans-brah"
 
-  font 'NotoSansBrahmi-Regular.ttf'
+  font "NotoSansBrahmi-Regular.ttf"
 end
