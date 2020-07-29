b553b51f757ef008cacd9aebb4a08b187f0af177
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:24 +0100

    font-bungee-outline.rb: fix for new style

--- Casks/font-bungee-outline.rb
+++ Casks/font-bungee-outline.rb
@@ -1,11 +1,11 @@
-cask 'font-bungee-outline' do
+cask "font-bungee-outline" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bungeeoutline/BungeeOutline-Regular.ttf'
-  name 'Bungee Outline'
-  homepage 'https://fonts.google.com/specimen/Bungee+Outline'
+  url "https://github.com/google/fonts/raw/master/ofl/bungeeoutline/BungeeOutline-Regular.ttf"
+  name "Bungee Outline"
+  homepage "https://fonts.google.com/specimen/Bungee+Outline"
 
-  font 'BungeeOutline-Regular.ttf'
+  font "BungeeOutline-Regular.ttf"
 end
