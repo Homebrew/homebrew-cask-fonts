302203126c3a3001282f181f89b05c8f5a8708db
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:17 +0100

    font-bayon.rb: fix for new style

--- Casks/font-bayon.rb
+++ Casks/font-bayon.rb
@@ -1,11 +1,11 @@
-cask 'font-bayon' do
+cask "font-bayon" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bayon/Bayon-Regular.ttf'
-  name 'Bayon'
-  homepage 'https://fonts.google.com/specimen/Bayon'
+  url "https://github.com/google/fonts/raw/master/ofl/bayon/Bayon-Regular.ttf"
+  name "Bayon"
+  homepage "https://fonts.google.com/specimen/Bayon"
 
-  font 'Bayon-Regular.ttf'
+  font "Bayon-Regular.ttf"
 end
