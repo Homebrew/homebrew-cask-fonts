c2dde4caf74bcdcba17441f0feb660abf9b4b2b2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:10 +0100

    font-archivo.rb: fix for new style

--- Casks/font-archivo.rb
+++ Casks/font-archivo.rb
@@ -1,22 +1,22 @@
-cask 'font-archivo' do
+cask "font-archivo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/archivo',
+  url "https://github.com/google/fonts/trunk/ofl/archivo",
       using:      :svn,
       trust_cert: true
-  name 'Archivo'
-  homepage 'https://fonts.google.com/specimen/Archivo'
+  name "Archivo"
+  homepage "https://fonts.google.com/specimen/Archivo"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Archivo-Bold.ttf'
-  font 'Archivo-BoldItalic.ttf'
-  font 'Archivo-Italic.ttf'
-  font 'Archivo-Medium.ttf'
-  font 'Archivo-MediumItalic.ttf'
-  font 'Archivo-Regular.ttf'
-  font 'Archivo-SemiBold.ttf'
-  font 'Archivo-SemiBoldItalic.ttf'
+  font "Archivo-Bold.ttf"
+  font "Archivo-BoldItalic.ttf"
+  font "Archivo-Italic.ttf"
+  font "Archivo-Medium.ttf"
+  font "Archivo-MediumItalic.ttf"
+  font "Archivo-Regular.ttf"
+  font "Archivo-SemiBold.ttf"
+  font "Archivo-SemiBoldItalic.ttf"
 end
