1624c050e1cef4007eb17c290bfaef7354b1457e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:23 +0100

    font-tangerine.rb: fix for new style

--- Casks/font-tangerine.rb
+++ Casks/font-tangerine.rb
@@ -1,16 +1,16 @@
-cask 'font-tangerine' do
+cask "font-tangerine" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/tangerine',
+  url "https://github.com/google/fonts/trunk/ofl/tangerine",
       using:      :svn,
       trust_cert: true
-  name 'Tangerine'
-  homepage 'https://fonts.google.com/specimen/Tangerine'
+  name "Tangerine"
+  homepage "https://fonts.google.com/specimen/Tangerine"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Tangerine-Bold.ttf'
-  font 'Tangerine-Regular.ttf'
+  font "Tangerine-Bold.ttf"
+  font "Tangerine-Regular.ttf"
 end
