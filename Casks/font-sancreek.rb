a318568c98a756be8919fe24066e7f714f1c1697
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:14 +0100

    font-sancreek.rb: fix for new style

--- Casks/font-sancreek.rb
+++ Casks/font-sancreek.rb
@@ -1,11 +1,11 @@
-cask 'font-sancreek' do
+cask "font-sancreek" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/sancreek/Sancreek-Regular.ttf'
-  name 'Sancreek'
-  homepage 'https://fonts.google.com/specimen/Sancreek'
+  url "https://github.com/google/fonts/raw/master/ofl/sancreek/Sancreek-Regular.ttf"
+  name "Sancreek"
+  homepage "https://fonts.google.com/specimen/Sancreek"
 
-  font 'Sancreek-Regular.ttf'
+  font "Sancreek-Regular.ttf"
 end
