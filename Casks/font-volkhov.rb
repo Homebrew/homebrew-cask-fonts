5752be069e2c7abcaa2b7a56c6c0d24684d16b40
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:28 +0100

    font-volkhov.rb: fix for new style

--- Casks/font-volkhov.rb
+++ Casks/font-volkhov.rb
@@ -1,18 +1,18 @@
-cask 'font-volkhov' do
+cask "font-volkhov" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/volkhov',
+  url "https://github.com/google/fonts/trunk/ofl/volkhov",
       using:      :svn,
       trust_cert: true
-  name 'Volkhov'
-  homepage 'https://fonts.google.com/specimen/Volkhov'
+  name "Volkhov"
+  homepage "https://fonts.google.com/specimen/Volkhov"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Volkhov-Bold.ttf'
-  font 'Volkhov-BoldItalic.ttf'
-  font 'Volkhov-Italic.ttf'
-  font 'Volkhov-Regular.ttf'
+  font "Volkhov-Bold.ttf"
+  font "Volkhov-BoldItalic.ttf"
+  font "Volkhov-Italic.ttf"
+  font "Volkhov-Regular.ttf"
 end
