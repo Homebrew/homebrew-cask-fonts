e51a276e1c5f80fa58ad3a38ef143fead4071f88
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:18 +0100

    font-almendra.rb: fix for new style

--- Casks/font-almendra.rb
+++ Casks/font-almendra.rb
@@ -1,18 +1,18 @@
-cask 'font-almendra' do
+cask "font-almendra" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/almendra',
+  url "https://github.com/google/fonts/trunk/ofl/almendra",
       using:      :svn,
       trust_cert: true
-  name 'Almendra'
-  homepage 'https://fonts.google.com/specimen/Almendra'
+  name "Almendra"
+  homepage "https://fonts.google.com/specimen/Almendra"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Almendra-Bold.ttf'
-  font 'Almendra-BoldItalic.ttf'
-  font 'Almendra-Italic.ttf'
-  font 'Almendra-Regular.ttf'
+  font "Almendra-Bold.ttf"
+  font "Almendra-BoldItalic.ttf"
+  font "Almendra-Italic.ttf"
+  font "Almendra-Regular.ttf"
 end
