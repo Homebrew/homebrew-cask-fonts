f832447018dd44e85b6168d906bc83d83c1075e2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:54 +0100

    font-noto-sans-lydian.rb: fix for new style

--- Casks/font-noto-sans-lydian.rb
+++ Casks/font-noto-sans-lydian.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-lydian' do
+cask "font-noto-sans-lydian" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLydian-unhinted.zip'
-  name 'Noto Sans Lydian'
-  homepage 'https://www.google.com/get/noto/#sans-lydi'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLydian-unhinted.zip"
+  name "Noto Sans Lydian"
+  homepage "https://www.google.com/get/noto/#sans-lydi"
 
-  font 'NotoSansLydian-Regular.ttf'
+  font "NotoSansLydian-Regular.ttf"
 end
