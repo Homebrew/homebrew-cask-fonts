7a06e9b65155e7fc0518b8ae5d2bd1dea25b93b8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:45 +0100

    font-moulpali.rb: fix for new style

--- Casks/font-moulpali.rb
+++ Casks/font-moulpali.rb
@@ -1,11 +1,11 @@
-cask 'font-moulpali' do
+cask "font-moulpali" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/moulpali/Moulpali.ttf'
-  name 'Moulpali'
-  homepage 'https://fonts.google.com/specimen/Moulpali'
+  url "https://github.com/google/fonts/raw/master/ofl/moulpali/Moulpali.ttf"
+  name "Moulpali"
+  homepage "https://fonts.google.com/specimen/Moulpali"
 
-  font 'Moulpali.ttf'
+  font "Moulpali.ttf"
 end
