b5602e7f9067cc823fbdf6d56ca9524c757cc68d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:11 +0100

    font-ribeye.rb: fix for new style

--- Casks/font-ribeye.rb
+++ Casks/font-ribeye.rb
@@ -1,11 +1,11 @@
-cask 'font-ribeye' do
+cask "font-ribeye" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/ribeye/Ribeye-Regular.ttf'
-  name 'Ribeye'
-  homepage 'https://fonts.google.com/specimen/Ribeye'
+  url "https://github.com/google/fonts/raw/master/ofl/ribeye/Ribeye-Regular.ttf"
+  name "Ribeye"
+  homepage "https://fonts.google.com/specimen/Ribeye"
 
-  font 'Ribeye-Regular.ttf'
+  font "Ribeye-Regular.ttf"
 end
