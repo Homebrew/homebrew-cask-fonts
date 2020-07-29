dbdf2ef839af1c5bd6943e5213751b2e9ce48e93
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:37 +0100

    font-ma-shan-zheng.rb: fix for new style

--- Casks/font-ma-shan-zheng.rb
+++ Casks/font-ma-shan-zheng.rb
@@ -1,11 +1,11 @@
-cask 'font-ma-shan-zheng' do
+cask "font-ma-shan-zheng" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/mashanzheng/MaShanZheng-Regular.ttf'
-  name 'Ma Shan Zheng'
-  homepage 'https://fonts.google.com/specimen/Ma+Shan+Zheng'
+  url "https://github.com/google/fonts/raw/master/ofl/mashanzheng/MaShanZheng-Regular.ttf"
+  name "Ma Shan Zheng"
+  homepage "https://fonts.google.com/specimen/Ma+Shan+Zheng"
 
-  font 'MaShanZheng-Regular.ttf'
+  font "MaShanZheng-Regular.ttf"
 end
