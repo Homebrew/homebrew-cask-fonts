3d947741e0418052ceff2538ebaa6f6dcafa0989
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:02 +0100

    font-overlock.rb: fix for new style

--- Casks/font-overlock.rb
+++ Casks/font-overlock.rb
@@ -1,20 +1,20 @@
-cask 'font-overlock' do
+cask "font-overlock" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/overlock',
+  url "https://github.com/google/fonts/trunk/ofl/overlock",
       using:      :svn,
       trust_cert: true
-  name 'Overlock'
-  homepage 'https://fonts.google.com/specimen/Overlock'
+  name "Overlock"
+  homepage "https://fonts.google.com/specimen/Overlock"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Overlock-Black.ttf'
-  font 'Overlock-BlackItalic.ttf'
-  font 'Overlock-Bold.ttf'
-  font 'Overlock-BoldItalic.ttf'
-  font 'Overlock-Italic.ttf'
-  font 'Overlock-Regular.ttf'
+  font "Overlock-Black.ttf"
+  font "Overlock-BlackItalic.ttf"
+  font "Overlock-Bold.ttf"
+  font "Overlock-BoldItalic.ttf"
+  font "Overlock-Italic.ttf"
+  font "Overlock-Regular.ttf"
 end
