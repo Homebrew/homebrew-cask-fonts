a4587a6379981afa8178aa396d0cbee88b19371d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:48 +0100

    font-encode-sans-expanded.rb: fix for new style

--- Casks/font-encode-sans-expanded.rb
+++ Casks/font-encode-sans-expanded.rb
@@ -1,23 +1,23 @@
-cask 'font-encode-sans-expanded' do
+cask "font-encode-sans-expanded" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/encodesansexpanded',
+  url "https://github.com/google/fonts/trunk/ofl/encodesansexpanded",
       using:      :svn,
       trust_cert: true
-  name 'Encode Sans Expanded'
-  homepage 'https://fonts.google.com/specimen/Encode+Sans+Expanded'
+  name "Encode Sans Expanded"
+  homepage "https://fonts.google.com/specimen/Encode+Sans+Expanded"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'EncodeSansExpanded-Black.ttf'
-  font 'EncodeSansExpanded-Bold.ttf'
-  font 'EncodeSansExpanded-ExtraBold.ttf'
-  font 'EncodeSansExpanded-ExtraLight.ttf'
-  font 'EncodeSansExpanded-Light.ttf'
-  font 'EncodeSansExpanded-Medium.ttf'
-  font 'EncodeSansExpanded-Regular.ttf'
-  font 'EncodeSansExpanded-SemiBold.ttf'
-  font 'EncodeSansExpanded-Thin.ttf'
+  font "EncodeSansExpanded-Black.ttf"
+  font "EncodeSansExpanded-Bold.ttf"
+  font "EncodeSansExpanded-ExtraBold.ttf"
+  font "EncodeSansExpanded-ExtraLight.ttf"
+  font "EncodeSansExpanded-Light.ttf"
+  font "EncodeSansExpanded-Medium.ttf"
+  font "EncodeSansExpanded-Regular.ttf"
+  font "EncodeSansExpanded-SemiBold.ttf"
+  font "EncodeSansExpanded-Thin.ttf"
 end
