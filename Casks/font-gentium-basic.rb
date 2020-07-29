e10ab28070ebf3c4bad9ffcc5e1fa621095aa9b1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:59 +0100

    font-gentium-basic.rb: fix for new style

--- Casks/font-gentium-basic.rb
+++ Casks/font-gentium-basic.rb
@@ -1,10 +1,10 @@
-cask 'font-gentium-basic' do
-  version '1.102'
-  sha256 '2f1a2c5491d7305dffd3520c6375d2f3e14931ee35c6d8ae1e8f098bf1a7b3cc'
+cask "font-gentium-basic" do
+  version "1.102"
+  sha256 "2f1a2c5491d7305dffd3520c6375d2f3e14931ee35c6d8ae1e8f098bf1a7b3cc"
 
   url "https://software.sil.org/downloads/r/gentium/GentiumBasic_#{version.no_dots}.zip"
-  name 'Gentium Basic'
-  homepage 'https://software.sil.org/gentium/'
+  name "Gentium Basic"
+  homepage "https://software.sil.org/gentium/"
 
   font "GentiumBasic_#{version.no_dots}/GenBasB.ttf"
   font "GentiumBasic_#{version.no_dots}/GenBasBI.ttf"
