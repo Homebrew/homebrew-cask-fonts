605fccc5cc08d7d0b1dece9485989723f697ce00
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:11 +0100

    font-ricty-diminished.rb: fix for new style

--- Casks/font-ricty-diminished.rb
+++ Casks/font-ricty-diminished.rb
@@ -1,18 +1,18 @@
-cask 'font-ricty-diminished' do
-  version '4.1.1'
-  sha256 '09d6cf8eaf3402205dd8a0fa35469d41833b4e85bedddfac235255d78747766b'
+cask "font-ricty-diminished" do
+  version "4.1.1"
+  sha256 "09d6cf8eaf3402205dd8a0fa35469d41833b4e85bedddfac235255d78747766b"
 
   url "https://rictyfonts.github.io/files/ricty_diminished-#{version}.tar.gz"
-  appcast 'https://rictyfonts.github.io/diminished'
-  name 'Ricty Diminished'
-  homepage 'https://rictyfonts.github.io/diminished'
+  appcast "https://rictyfonts.github.io/diminished"
+  name "Ricty Diminished"
+  homepage "https://rictyfonts.github.io/diminished"
 
-  font 'RictyDiminished-Bold.ttf'
-  font 'RictyDiminished-BoldOblique.ttf'
-  font 'RictyDiminished-Oblique.ttf'
-  font 'RictyDiminished-Regular.ttf'
-  font 'RictyDiminishedDiscord-Bold.ttf'
-  font 'RictyDiminishedDiscord-BoldOblique.ttf'
-  font 'RictyDiminishedDiscord-Oblique.ttf'
-  font 'RictyDiminishedDiscord-Regular.ttf'
+  font "RictyDiminished-Bold.ttf"
+  font "RictyDiminished-BoldOblique.ttf"
+  font "RictyDiminished-Oblique.ttf"
+  font "RictyDiminished-Regular.ttf"
+  font "RictyDiminishedDiscord-Bold.ttf"
+  font "RictyDiminishedDiscord-BoldOblique.ttf"
+  font "RictyDiminishedDiscord-Oblique.ttf"
+  font "RictyDiminishedDiscord-Regular.ttf"
 end
