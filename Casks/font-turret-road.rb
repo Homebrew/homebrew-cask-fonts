c4891184b1bafa81d4ea7074f3a0bd70258f1f4f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:26 +0100

    font-turret-road.rb: fix for new style

--- Casks/font-turret-road.rb
+++ Casks/font-turret-road.rb
@@ -1,20 +1,20 @@
-cask 'font-turret-road' do
+cask "font-turret-road" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/turretroad',
+  url "https://github.com/google/fonts/trunk/ofl/turretroad",
       using:      :svn,
       trust_cert: true
-  name 'Turret Road'
-  homepage 'https://fonts.google.com/specimen/Turret+Road'
+  name "Turret Road"
+  homepage "https://fonts.google.com/specimen/Turret+Road"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'TurretRoad-Bold.ttf'
-  font 'TurretRoad-ExtraBold.ttf'
-  font 'TurretRoad-ExtraLight.ttf'
-  font 'TurretRoad-Light.ttf'
-  font 'TurretRoad-Medium.ttf'
-  font 'TurretRoad-Regular.ttf'
+  font "TurretRoad-Bold.ttf"
+  font "TurretRoad-ExtraBold.ttf"
+  font "TurretRoad-ExtraLight.ttf"
+  font "TurretRoad-Light.ttf"
+  font "TurretRoad-Medium.ttf"
+  font "TurretRoad-Regular.ttf"
 end
