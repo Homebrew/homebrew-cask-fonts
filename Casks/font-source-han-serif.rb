745ab5cf9d096592b97540d1c96757fb9a6bf571
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:19 +0100

    font-source-han-serif.rb: fix for new style

--- Casks/font-source-han-serif.rb
+++ Casks/font-source-han-serif.rb
@@ -1,11 +1,11 @@
-cask 'font-source-han-serif' do
-  version '1.001R'
-  sha256 '85cc634fa228286ca307803bbb4ca61f61fa821b3ed573f4f07c6f0c064426b5'
+cask "font-source-han-serif" do
+  version "1.001R"
+  sha256 "85cc634fa228286ca307803bbb4ca61f61fa821b3ed573f4f07c6f0c064426b5"
 
-  url 'https://github.com/adobe-fonts/source-han-serif/releases/download/1.001R/SourceHanSerif.ttc'
-  appcast 'https://github.com/adobe-fonts/source-han-serif/releases.atom'
-  name 'Source Han Serif'
-  homepage 'https://github.com/adobe-fonts/source-han-serif'
+  url "https://github.com/adobe-fonts/source-han-serif/releases/download/1.001R/SourceHanSerif.ttc"
+  appcast "https://github.com/adobe-fonts/source-han-serif/releases.atom"
+  name "Source Han Serif"
+  homepage "https://github.com/adobe-fonts/source-han-serif"
 
-  font 'SourceHanSerif.ttc'
+  font "SourceHanSerif.ttc"
 end
