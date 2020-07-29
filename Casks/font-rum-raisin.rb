bfb2e20cdbf6e19e16d72ad875ed96baf1f06e1d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:13 +0100

    font-rum-raisin.rb: fix for new style

--- Casks/font-rum-raisin.rb
+++ Casks/font-rum-raisin.rb
@@ -1,11 +1,11 @@
-cask 'font-rum-raisin' do
+cask "font-rum-raisin" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/rumraisin/RumRaisin-Regular.ttf'
-  name 'Rum Raisin'
-  homepage 'https://fonts.google.com/specimen/Rum+Raisin'
+  url "https://github.com/google/fonts/raw/master/ofl/rumraisin/RumRaisin-Regular.ttf"
+  name "Rum Raisin"
+  homepage "https://fonts.google.com/specimen/Rum+Raisin"
 
-  font 'RumRaisin-Regular.ttf'
+  font "RumRaisin-Regular.ttf"
 end
