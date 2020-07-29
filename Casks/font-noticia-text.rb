3f5f3fd3fc17622ad61e05a765f7c9c0249d9c47
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:49 +0100

    font-noticia-text.rb: fix for new style

--- Casks/font-noticia-text.rb
+++ Casks/font-noticia-text.rb
@@ -1,18 +1,18 @@
-cask 'font-noticia-text' do
+cask "font-noticia-text" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/noticiatext',
+  url "https://github.com/google/fonts/trunk/ofl/noticiatext",
       using:      :svn,
       trust_cert: true
-  name 'Noticia Text'
-  homepage 'https://fonts.google.com/specimen/Noticia+Text'
+  name "Noticia Text"
+  homepage "https://fonts.google.com/specimen/Noticia+Text"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'NoticiaText-Bold.ttf'
-  font 'NoticiaText-BoldItalic.ttf'
-  font 'NoticiaText-Italic.ttf'
-  font 'NoticiaText-Regular.ttf'
+  font "NoticiaText-Bold.ttf"
+  font "NoticiaText-BoldItalic.ttf"
+  font "NoticiaText-Italic.ttf"
+  font "NoticiaText-Regular.ttf"
 end
