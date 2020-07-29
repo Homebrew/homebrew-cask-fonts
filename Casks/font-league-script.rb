f26f96100021ed86aa862f3b5a9c45d681bd5f3d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:32 +0100

    font-league-script.rb: fix for new style

--- Casks/font-league-script.rb
+++ Casks/font-league-script.rb
@@ -1,11 +1,11 @@
-cask 'font-league-script' do
+cask "font-league-script" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/leaguescript/LeagueScript-Regular.ttf'
-  name 'League Script'
-  homepage 'https://fonts.google.com/specimen/League+Script'
+  url "https://github.com/google/fonts/raw/master/ofl/leaguescript/LeagueScript-Regular.ttf"
+  name "League Script"
+  homepage "https://fonts.google.com/specimen/League+Script"
 
-  font 'LeagueScript-Regular.ttf'
+  font "LeagueScript-Regular.ttf"
 end
