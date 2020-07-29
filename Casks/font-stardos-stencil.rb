0b82caef3c7d3e4936464670782b107af62d7174
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:21 +0100

    font-stardos-stencil.rb: fix for new style

--- Casks/font-stardos-stencil.rb
+++ Casks/font-stardos-stencil.rb
@@ -1,16 +1,16 @@
-cask 'font-stardos-stencil' do
+cask "font-stardos-stencil" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/stardosstencil',
+  url "https://github.com/google/fonts/trunk/ofl/stardosstencil",
       using:      :svn,
       trust_cert: true
-  name 'Stardos Stencil'
-  homepage 'https://fonts.google.com/specimen/Stardos+Stencil'
+  name "Stardos Stencil"
+  homepage "https://fonts.google.com/specimen/Stardos+Stencil"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'StardosStencil-Bold.ttf'
-  font 'StardosStencil-Regular.ttf'
+  font "StardosStencil-Bold.ttf"
+  font "StardosStencil-Regular.ttf"
 end
