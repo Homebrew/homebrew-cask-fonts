ad3c3d66e6bed5bc5c2ad53678d85e8b66d9d37d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:48 +0100

    font-nanum-myeongjo.rb: fix for new style

--- Casks/font-nanum-myeongjo.rb
+++ Casks/font-nanum-myeongjo.rb
@@ -1,17 +1,17 @@
-cask 'font-nanum-myeongjo' do
+cask "font-nanum-myeongjo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/nanummyeongjo',
+  url "https://github.com/google/fonts/trunk/ofl/nanummyeongjo",
       using:      :svn,
       trust_cert: true
-  name 'Nanum Myeongjo'
-  homepage 'https://fonts.google.com/specimen/Nanum+Myeongjo'
+  name "Nanum Myeongjo"
+  homepage "https://fonts.google.com/specimen/Nanum+Myeongjo"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'NanumMyeongjo-Bold.ttf'
-  font 'NanumMyeongjo-ExtraBold.ttf'
-  font 'NanumMyeongjo-Regular.ttf'
+  font "NanumMyeongjo-Bold.ttf"
+  font "NanumMyeongjo-ExtraBold.ttf"
+  font "NanumMyeongjo-Regular.ttf"
 end
