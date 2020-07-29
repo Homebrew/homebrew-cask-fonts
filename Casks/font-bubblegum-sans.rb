ad8a9009e6838491d2ba55854abae4871997fc6c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:23 +0100

    font-bubblegum-sans.rb: fix for new style

--- Casks/font-bubblegum-sans.rb
+++ Casks/font-bubblegum-sans.rb
@@ -1,11 +1,11 @@
-cask 'font-bubblegum-sans' do
+cask "font-bubblegum-sans" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bubblegumsans/BubblegumSans-Regular.ttf'
-  name 'Bubblegum Sans'
-  homepage 'https://fonts.google.com/specimen/Bubblegum+Sans'
+  url "https://github.com/google/fonts/raw/master/ofl/bubblegumsans/BubblegumSans-Regular.ttf"
+  name "Bubblegum Sans"
+  homepage "https://fonts.google.com/specimen/Bubblegum+Sans"
 
-  font 'BubblegumSans-Regular.ttf'
+  font "BubblegumSans-Regular.ttf"
 end
