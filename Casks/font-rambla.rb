097d1620d85b91584c664fbb45f5c9b6224eb9ab
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:09 +0100

    font-rambla.rb: fix for new style

--- Casks/font-rambla.rb
+++ Casks/font-rambla.rb
@@ -1,18 +1,18 @@
-cask 'font-rambla' do
+cask "font-rambla" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/rambla',
+  url "https://github.com/google/fonts/trunk/ofl/rambla",
       using:      :svn,
       trust_cert: true
-  name 'Rambla'
-  homepage 'https://fonts.google.com/specimen/Rambla'
+  name "Rambla"
+  homepage "https://fonts.google.com/specimen/Rambla"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Rambla-Bold.ttf'
-  font 'Rambla-BoldItalic.ttf'
-  font 'Rambla-Italic.ttf'
-  font 'Rambla-Regular.ttf'
+  font "Rambla-Bold.ttf"
+  font "Rambla-BoldItalic.ttf"
+  font "Rambla-Italic.ttf"
+  font "Rambla-Regular.ttf"
 end
