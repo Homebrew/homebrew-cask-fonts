99d2dbe77d5e4daaaf79b4cb7b1bb9e2348a2c11
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:54 +0100

    font-noto-sans-linear-b.rb: fix for new style

--- Casks/font-noto-sans-linear-b.rb
+++ Casks/font-noto-sans-linear-b.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-linear-b' do
+cask "font-noto-sans-linear-b" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLinearB-unhinted.zip'
-  name 'Noto Sans Linear B'
-  homepage 'https://www.google.com/get/noto/#sans-linb'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLinearB-unhinted.zip"
+  name "Noto Sans Linear B"
+  homepage "https://www.google.com/get/noto/#sans-linb"
 
-  font 'NotoSansLinearB-Regular.ttf'
+  font "NotoSansLinearB-Regular.ttf"
 end
