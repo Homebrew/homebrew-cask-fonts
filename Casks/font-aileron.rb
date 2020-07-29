b11dfdd8dfd48c0ac7fbecfb73bb6757f637e5c5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:59 +0100

    font-aileron.rb: fix for new style

--- Casks/font-aileron.rb
+++ Casks/font-aileron.rb
@@ -1,27 +1,27 @@
-cask 'font-aileron' do
-  version '0.102'
-  sha256 'a93a1327f44912a7b1410ad0056fec3e904074413b0bd9da550f6175587cf958'
+cask "font-aileron" do
+  version "0.102"
+  sha256 "a93a1327f44912a7b1410ad0056fec3e904074413b0bd9da550f6175587cf958"
 
   url "https://dotcolon.net/download/fonts/aileron_#{version.no_dots}.zip"
-  appcast 'https://dotcolon.net/font/aileron/',
+  appcast "https://dotcolon.net/font/aileron/",
           must_contain: version.no_dots
-  name 'Aileron'
-  homepage 'https://dotcolon.net/font/aileron/'
+  name "Aileron"
+  homepage "https://dotcolon.net/font/aileron/"
 
-  font 'Aileron-Black.otf'
-  font 'Aileron-BlackItalic.otf'
-  font 'Aileron-Bold.otf'
-  font 'Aileron-BoldItalic.otf'
-  font 'Aileron-Heavy.otf'
-  font 'Aileron-HeavyItalic.otf'
-  font 'Aileron-Italic.otf'
-  font 'Aileron-Light.otf'
-  font 'Aileron-LightItalic.otf'
-  font 'Aileron-Regular.otf'
-  font 'Aileron-SemiBold.otf'
-  font 'Aileron-SemiBoldItalic.otf'
-  font 'Aileron-Thin.otf'
-  font 'Aileron-ThinItalic.otf'
-  font 'Aileron-UltraLight.otf'
-  font 'Aileron-UltraLightItalic.otf'
+  font "Aileron-Black.otf"
+  font "Aileron-BlackItalic.otf"
+  font "Aileron-Bold.otf"
+  font "Aileron-BoldItalic.otf"
+  font "Aileron-Heavy.otf"
+  font "Aileron-HeavyItalic.otf"
+  font "Aileron-Italic.otf"
+  font "Aileron-Light.otf"
+  font "Aileron-LightItalic.otf"
+  font "Aileron-Regular.otf"
+  font "Aileron-SemiBold.otf"
+  font "Aileron-SemiBoldItalic.otf"
+  font "Aileron-Thin.otf"
+  font "Aileron-ThinItalic.otf"
+  font "Aileron-UltraLight.otf"
+  font "Aileron-UltraLightItalic.otf"
 end
