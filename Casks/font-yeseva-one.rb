1769d7c3ba254d92dc91ad6b7996b1fdd32d2ebc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:29 +0100

    font-yeseva-one.rb: fix for new style

--- Casks/font-yeseva-one.rb
+++ Casks/font-yeseva-one.rb
@@ -1,11 +1,11 @@
-cask 'font-yeseva-one' do
+cask "font-yeseva-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/yesevaone/YesevaOne-Regular.ttf'
-  name 'Yeseva One'
-  homepage 'https://fonts.google.com/specimen/Yeseva+One'
+  url "https://github.com/google/fonts/raw/master/ofl/yesevaone/YesevaOne-Regular.ttf"
+  name "Yeseva One"
+  homepage "https://fonts.google.com/specimen/Yeseva+One"
 
-  font 'YesevaOne-Regular.ttf'
+  font "YesevaOne-Regular.ttf"
 end
