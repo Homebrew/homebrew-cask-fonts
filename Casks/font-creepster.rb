884d077342c17d8106cf08ef7673a9fe872a333d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:38 +0100

    font-creepster.rb: fix for new style

--- Casks/font-creepster.rb
+++ Casks/font-creepster.rb
@@ -1,11 +1,11 @@
-cask 'font-creepster' do
+cask "font-creepster" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/creepster/Creepster-Regular.ttf'
-  name 'Creepster'
-  homepage 'https://fonts.google.com/specimen/Creepster'
+  url "https://github.com/google/fonts/raw/master/ofl/creepster/Creepster-Regular.ttf"
+  name "Creepster"
+  homepage "https://fonts.google.com/specimen/Creepster"
 
-  font 'Creepster-Regular.ttf'
+  font "Creepster-Regular.ttf"
 end
