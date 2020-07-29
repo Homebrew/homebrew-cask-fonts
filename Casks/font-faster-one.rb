38a7db6ece56d740be8f730ebe62a255ad8afc48
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:52 +0100

    font-faster-one.rb: fix for new style

--- Casks/font-faster-one.rb
+++ Casks/font-faster-one.rb
@@ -1,11 +1,11 @@
-cask 'font-faster-one' do
+cask "font-faster-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/fasterone/FasterOne-Regular.ttf'
-  name 'Faster One'
-  homepage 'https://fonts.google.com/specimen/Faster+One'
+  url "https://github.com/google/fonts/raw/master/ofl/fasterone/FasterOne-Regular.ttf"
+  name "Faster One"
+  homepage "https://fonts.google.com/specimen/Faster+One"
 
-  font 'FasterOne-Regular.ttf'
+  font "FasterOne-Regular.ttf"
 end
