cbe686a666ccbd3c6d43aa1f00499aeea9ab66a1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:31 +0100

    font-cherry-swash.rb: fix for new style

--- Casks/font-cherry-swash.rb
+++ Casks/font-cherry-swash.rb
@@ -1,16 +1,16 @@
-cask 'font-cherry-swash' do
+cask "font-cherry-swash" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/cherryswash',
+  url "https://github.com/google/fonts/trunk/ofl/cherryswash",
       using:      :svn,
       trust_cert: true
-  name 'Cherry Swash'
-  homepage 'https://fonts.google.com/specimen/Cherry+Swash'
+  name "Cherry Swash"
+  homepage "https://fonts.google.com/specimen/Cherry+Swash"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'CherrySwash-Bold.ttf'
-  font 'CherrySwash-Regular.ttf'
+  font "CherrySwash-Bold.ttf"
+  font "CherrySwash-Regular.ttf"
 end
