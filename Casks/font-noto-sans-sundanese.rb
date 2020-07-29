0f6ee5755ea936042106fc409a55e738dcb2c903
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:56 +0100

    font-noto-sans-sundanese.rb: fix for new style

--- Casks/font-noto-sans-sundanese.rb
+++ Casks/font-noto-sans-sundanese.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-sundanese' do
+cask "font-noto-sans-sundanese" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSundanese-unhinted.zip'
-  name 'Noto Sans Sundanese'
-  homepage 'https://www.google.com/get/noto/#sans-sund'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSundanese-unhinted.zip"
+  name "Noto Sans Sundanese"
+  homepage "https://www.google.com/get/noto/#sans-sund"
 
-  font 'NotoSansSundanese-Regular.ttf'
+  font "NotoSansSundanese-Regular.ttf"
 end
