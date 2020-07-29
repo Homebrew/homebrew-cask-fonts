807595046d05b1ac5f030d3100b1ee94e3501dbf
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:24 +0100

    font-joti-one.rb: fix for new style

--- Casks/font-joti-one.rb
+++ Casks/font-joti-one.rb
@@ -1,11 +1,11 @@
-cask 'font-joti-one' do
+cask "font-joti-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/jotione/JotiOne-Regular.ttf'
-  name 'Joti One'
-  homepage 'https://fonts.google.com/specimen/Joti+One'
+  url "https://github.com/google/fonts/raw/master/ofl/jotione/JotiOne-Regular.ttf"
+  name "Joti One"
+  homepage "https://fonts.google.com/specimen/Joti+One"
 
-  font 'JotiOne-Regular.ttf'
+  font "JotiOne-Regular.ttf"
 end
