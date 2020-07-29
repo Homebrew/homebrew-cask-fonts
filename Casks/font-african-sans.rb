42ecade8e1b85c4b42e31d3fe0bce59fc0e4d708
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:45 +0100

    font-african-sans.rb: fix for new style

--- Casks/font-african-sans.rb
+++ Casks/font-african-sans.rb
@@ -1,13 +1,13 @@
-cask 'font-african-sans' do
-  version '9.380'
-  sha256 '66176e5517ace047affc23ca7b7137018c907eee00b113666a671f31254ac7a4'
+cask "font-african-sans" do
+  version "9.380"
+  sha256 "66176e5517ace047affc23ca7b7137018c907eee00b113666a671f31254ac7a4"
 
-  url 'https://www.languagegeek.com/font/AfricanSans.zip'
-  name 'African Sans'
-  homepage 'https://www.languagegeek.com/font/fontdownload.html'
+  url "https://www.languagegeek.com/font/AfricanSans.zip"
+  name "African Sans"
+  homepage "https://www.languagegeek.com/font/fontdownload.html"
 
-  font 'African Sans REGULAR 938.ttf'
-  font 'African Sans BOLD 938.ttf'
-  font 'African Sans BOLD ITALIC 938.ttf'
-  font 'African Sans ITALIC 938.ttf'
+  font "African Sans REGULAR 938.ttf"
+  font "African Sans BOLD 938.ttf"
+  font "African Sans BOLD ITALIC 938.ttf"
+  font "African Sans ITALIC 938.ttf"
 end
