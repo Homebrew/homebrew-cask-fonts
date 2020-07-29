d30b12b0ac1b466477f3aa78e8501a2d6a43d7ae
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:03 +0100

    font-oxygen.rb: fix for new style

--- Casks/font-oxygen.rb
+++ Casks/font-oxygen.rb
@@ -1,17 +1,17 @@
-cask 'font-oxygen' do
+cask "font-oxygen" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/oxygen',
+  url "https://github.com/google/fonts/trunk/ofl/oxygen",
       using:      :svn,
       trust_cert: true
-  name 'Oxygen'
-  homepage 'https://fonts.google.com/specimen/Oxygen'
+  name "Oxygen"
+  homepage "https://fonts.google.com/specimen/Oxygen"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Oxygen-Bold.ttf'
-  font 'Oxygen-Light.ttf'
-  font 'Oxygen-Regular.ttf'
+  font "Oxygen-Bold.ttf"
+  font "Oxygen-Light.ttf"
+  font "Oxygen-Regular.ttf"
 end
