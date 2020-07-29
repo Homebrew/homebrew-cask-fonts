76a35d2723cd47a803620432af7888d2b5158d0d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:48 +0100

    font-neuton.rb: fix for new style

--- Casks/font-neuton.rb
+++ Casks/font-neuton.rb
@@ -1,20 +1,20 @@
-cask 'font-neuton' do
+cask "font-neuton" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/neuton',
+  url "https://github.com/google/fonts/trunk/ofl/neuton",
       using:      :svn,
       trust_cert: true
-  name 'Neuton'
-  homepage 'https://fonts.google.com/specimen/Neuton'
+  name "Neuton"
+  homepage "https://fonts.google.com/specimen/Neuton"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Neuton-Bold.ttf'
-  font 'Neuton-ExtraBold.ttf'
-  font 'Neuton-ExtraLight.ttf'
-  font 'Neuton-Italic.ttf'
-  font 'Neuton-Light.ttf'
-  font 'Neuton-Regular.ttf'
+  font "Neuton-Bold.ttf"
+  font "Neuton-ExtraBold.ttf"
+  font "Neuton-ExtraLight.ttf"
+  font "Neuton-Italic.ttf"
+  font "Neuton-Light.ttf"
+  font "Neuton-Regular.ttf"
 end
