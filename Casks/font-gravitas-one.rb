acacc1a8e08e8ec362b11bc5736c07967d68428a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:06 +0100

    font-gravitas-one.rb: fix for new style

--- Casks/font-gravitas-one.rb
+++ Casks/font-gravitas-one.rb
@@ -1,11 +1,11 @@
-cask 'font-gravitas-one' do
+cask "font-gravitas-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/gravitasone/GravitasOne.ttf'
-  name 'Gravitas One'
-  homepage 'https://fonts.google.com/specimen/Gravitas+One'
+  url "https://github.com/google/fonts/raw/master/ofl/gravitasone/GravitasOne.ttf"
+  name "Gravitas One"
+  homepage "https://fonts.google.com/specimen/Gravitas+One"
 
-  font 'GravitasOne.ttf'
+  font "GravitasOne.ttf"
 end
