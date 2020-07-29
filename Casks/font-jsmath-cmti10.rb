fc3241226184874cacd0ed768a7c29171473c1c2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:24 +0100

    font-jsmath-cmti10.rb: fix for new style

--- Casks/font-jsmath-cmti10.rb
+++ Casks/font-jsmath-cmti10.rb
@@ -1,11 +1,11 @@
-cask 'font-jsmath-cmti10' do
+cask "font-jsmath-cmti10" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/jsmathcmti10/jsMath-cmti10.ttf'
-  name 'jsMath cmti10'
-  homepage 'https://fonts.google.com/specimen/jsMath+cmti10'
+  url "https://github.com/google/fonts/raw/master/apache/jsmathcmti10/jsMath-cmti10.ttf"
+  name "jsMath cmti10"
+  homepage "https://fonts.google.com/specimen/jsMath+cmti10"
 
-  font 'jsMath-cmti10.ttf'
+  font "jsMath-cmti10.ttf"
 end
