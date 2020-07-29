a67478e60d9faa21cacd4ab2e703eee625332ee0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:37:09 +0100

    font-bad-script.rb: fix for new style

--- Casks/font-bad-script.rb
+++ Casks/font-bad-script.rb
@@ -1,11 +1,11 @@
-cask 'font-bad-script' do
+cask "font-bad-script" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/badscript/BadScript-Regular.ttf'
-  name 'Bad Script'
-  homepage 'https://fonts.google.com/specimen/Bad+Script'
+  url "https://github.com/google/fonts/raw/master/ofl/badscript/BadScript-Regular.ttf"
+  name "Bad Script"
+  homepage "https://fonts.google.com/specimen/Bad+Script"
 
-  font 'BadScript-Regular.ttf'
+  font "BadScript-Regular.ttf"
 end
