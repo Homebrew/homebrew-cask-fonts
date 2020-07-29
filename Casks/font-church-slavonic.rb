154a6c08b2ad29f62ce061d42a5b270b0de1297f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:32 +0100

    font-church-slavonic.rb: fix for new style

--- Casks/font-church-slavonic.rb
+++ Casks/font-church-slavonic.rb
@@ -1,19 +1,19 @@
-cask 'font-church-slavonic' do
-  version '2.1'
-  sha256 '76ac825d4c7c853cad374b6cba9ca18be7040a892e2ebf3954ed978623f43cab'
+cask "font-church-slavonic" do
+  version "2.1"
+  sha256 "76ac825d4c7c853cad374b6cba9ca18be7040a892e2ebf3954ed978623f43cab"
 
   url "https://github.com/typiconman/fonts-cu/releases/download/v#{version}/fonts-churchslavonic.zip"
-  appcast 'https://github.com/typiconman/fonts-cu/releases.atom'
-  name 'Church Slavonic Fonts'
-  homepage 'https://github.com/typiconman/fonts-cu'
+  appcast "https://github.com/typiconman/fonts-cu/releases.atom"
+  name "Church Slavonic Fonts"
+  homepage "https://github.com/typiconman/fonts-cu"
 
-  font 'CathismaUnicode.otf'
-  font 'FedorovskUnicode.otf'
-  font 'IndictionUnicode.otf'
-  font 'MenaionUnicode.otf'
-  font 'MonomakhUnicode.otf'
-  font 'OglavieUnicode.otf'
-  font 'PomorskyUnicode.otf'
-  font 'PonomarUnicode.otf'
-  font 'VertogradUnicode.otf'
+  font "CathismaUnicode.otf"
+  font "FedorovskUnicode.otf"
+  font "IndictionUnicode.otf"
+  font "MenaionUnicode.otf"
+  font "MonomakhUnicode.otf"
+  font "OglavieUnicode.otf"
+  font "PomorskyUnicode.otf"
+  font "PonomarUnicode.otf"
+  font "VertogradUnicode.otf"
 end
