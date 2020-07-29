2aa7315e4f2b084544d821412b7bd30b358d66e3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:23 +0100

    font-terminus.rb: fix for new style

--- Casks/font-terminus.rb
+++ Casks/font-terminus.rb
@@ -1,10 +1,10 @@
-cask 'font-terminus' do
-  version '4.40.1'
-  sha256 'c3cb690c2935123035a0b1f3bfdd9511c282dab489cd423e161a47c592edf188'
+cask "font-terminus" do
+  version "4.40.1"
+  sha256 "c3cb690c2935123035a0b1f3bfdd9511c282dab489cd423e161a47c592edf188"
 
   url "https://files.ax86.net/terminus-ttf/files/#{version}/terminus-ttf-#{version}.zip"
-  name 'Terminus TTF'
-  homepage 'https://files.ax86.net/terminus-ttf/'
+  name "Terminus TTF"
+  homepage "https://files.ax86.net/terminus-ttf/"
 
   font "terminus-ttf-#{version}/TerminusTTF-#{version}.ttf"
   font "terminus-ttf-#{version}/TerminusTTF-Bold-#{version}.ttf"
