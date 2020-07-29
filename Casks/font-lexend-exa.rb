695c54adf26712b89de92bdf4be92290683c06ab
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:33 +0100

    font-lexend-exa.rb: fix for new style

--- Casks/font-lexend-exa.rb
+++ Casks/font-lexend-exa.rb
@@ -1,11 +1,11 @@
-cask 'font-lexend-exa' do
+cask "font-lexend-exa" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/lexendexa/LexendExa-Regular.ttf'
-  name 'Lexend Exa'
-  homepage 'https://fonts.google.com/specimen/Lexend+Exa'
+  url "https://github.com/google/fonts/raw/master/ofl/lexendexa/LexendExa-Regular.ttf"
+  name "Lexend Exa"
+  homepage "https://fonts.google.com/specimen/Lexend+Exa"
 
-  font 'LexendExa-Regular.ttf'
+  font "LexendExa-Regular.ttf"
 end
