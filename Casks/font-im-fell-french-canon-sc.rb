6a72ff749d9e96dc5509d21e9a300f6ac6e2c99c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:14 +0100

    font-im-fell-french-canon-sc.rb: fix for new style

--- Casks/font-im-fell-french-canon-sc.rb
+++ Casks/font-im-fell-french-canon-sc.rb
@@ -1,11 +1,11 @@
-cask 'font-im-fell-french-canon-sc' do
+cask "font-im-fell-french-canon-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/imfellfrenchcanonsc/IMFeFCsc28P.ttf'
-  name 'IM Fell French Canon SC'
-  homepage 'https://fonts.google.com/specimen/IM+Fell+French+Canon+SC'
+  url "https://github.com/google/fonts/raw/master/ofl/imfellfrenchcanonsc/IMFeFCsc28P.ttf"
+  name "IM Fell French Canon SC"
+  homepage "https://fonts.google.com/specimen/IM+Fell+French+Canon+SC"
 
-  font 'IMFeFCsc28P.ttf'
+  font "IMFeFCsc28P.ttf"
 end
