502a4275d7539b318f1bb45df56fff85d0a1e668
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:19 +0100

    font-source-han-serif-el-m.rb: fix for new style

--- Casks/font-source-han-serif-el-m.rb
+++ Casks/font-source-han-serif-el-m.rb
@@ -1,16 +1,16 @@
-cask 'font-source-han-serif-el-m' do
-  version '1.001R'
-  sha256 '3d23f70a6d134fd4206a920c4d54376f2b68393beabf60af1d56a451d03eae26'
+cask "font-source-han-serif-el-m" do
+  version "1.001R"
+  sha256 "3d23f70a6d134fd4206a920c4d54376f2b68393beabf60af1d56a451d03eae26"
 
-  url 'https://github.com/adobe-fonts/source-han-serif/raw/release/OTC/SourceHanSerifOTC_EL-M.zip'
-  appcast 'https://github.com/adobe-fonts/source-han-serif/releases.atom'
-  name 'Source Han Serif EL-M'
-  homepage 'https://github.com/adobe-fonts/source-han-serif'
+  url "https://github.com/adobe-fonts/source-han-serif/raw/release/OTC/SourceHanSerifOTC_EL-M.zip"
+  appcast "https://github.com/adobe-fonts/source-han-serif/releases.atom"
+  name "Source Han Serif EL-M"
+  homepage "https://github.com/adobe-fonts/source-han-serif"
 
-  font 'SourceHanSerifOTC_EL-M/SourceHanSerif-ExtraLight.ttc'
-  font 'SourceHanSerifOTC_EL-M/SourceHanSerif-Light.ttc'
-  font 'SourceHanSerifOTC_EL-M/SourceHanSerif-Medium.ttc'
-  font 'SourceHanSerifOTC_EL-M/SourceHanSerif-Regular.ttc'
+  font "SourceHanSerifOTC_EL-M/SourceHanSerif-ExtraLight.ttc"
+  font "SourceHanSerifOTC_EL-M/SourceHanSerif-Light.ttc"
+  font "SourceHanSerifOTC_EL-M/SourceHanSerif-Medium.ttc"
+  font "SourceHanSerifOTC_EL-M/SourceHanSerif-Regular.ttc"
 
   caveats <<~EOS
     #{token} only installs the ExtraLight, Light, Regular and Medium weights.
