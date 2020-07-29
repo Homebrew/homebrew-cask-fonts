5286346c6e1bd175b23316c8cd764df232f2f5a2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:27 +0100

    font-kawkab-mono.rb: fix for new style

--- Casks/font-kawkab-mono.rb
+++ Casks/font-kawkab-mono.rb
@@ -1,12 +1,12 @@
-cask 'font-kawkab-mono' do
-  version '0.501'
-  sha256 '11c06f57dddefaf0166d74caaa072865ab6ff8d34076e7ec5d2c20edda145666'
+cask "font-kawkab-mono" do
+  version "0.501"
+  sha256 "11c06f57dddefaf0166d74caaa072865ab6ff8d34076e7ec5d2c20edda145666"
 
   # github.com/aiaf/kawkab-mono/ was verified as official when first introduced to the cask
   url "https://github.com/aiaf/kawkab-mono/releases/download/v#{version}/kawkab-mono-#{version}.zip"
-  appcast 'https://github.com/aiaf/kawkab-mono/releases.atom'
-  name 'Kawkab Mono'
-  homepage 'https://makkuk.com/kawkab-mono'
+  appcast "https://github.com/aiaf/kawkab-mono/releases.atom"
+  name "Kawkab Mono"
+  homepage "https://makkuk.com/kawkab-mono"
 
   font "kawkab-mono-#{version}/KawkabMono-Bold.otf"
   font "kawkab-mono-#{version}/KawkabMono-Light.otf"
