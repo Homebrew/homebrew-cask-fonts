0cc03fea40a07421e67b9d986d51d4fe68819c88
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:25 +0100

    font-cabin.rb: fix for new style

--- Casks/font-cabin.rb
+++ Casks/font-cabin.rb
@@ -1,22 +1,22 @@
-cask 'font-cabin' do
+cask "font-cabin" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/cabin',
+  url "https://github.com/google/fonts/trunk/ofl/cabin",
       using:      :svn,
       trust_cert: true
-  name 'Cabin'
-  homepage 'https://fonts.google.com/specimen/Cabin'
+  name "Cabin"
+  homepage "https://fonts.google.com/specimen/Cabin"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Cabin-Bold.ttf'
-  font 'Cabin-BoldItalic.ttf'
-  font 'Cabin-Italic.ttf'
-  font 'Cabin-Medium.ttf'
-  font 'Cabin-MediumItalic.ttf'
-  font 'Cabin-Regular.ttf'
-  font 'Cabin-SemiBold.ttf'
-  font 'Cabin-SemiBoldItalic.ttf'
+  font "Cabin-Bold.ttf"
+  font "Cabin-BoldItalic.ttf"
+  font "Cabin-Italic.ttf"
+  font "Cabin-Medium.ttf"
+  font "Cabin-MediumItalic.ttf"
+  font "Cabin-Regular.ttf"
+  font "Cabin-SemiBold.ttf"
+  font "Cabin-SemiBoldItalic.ttf"
 end
