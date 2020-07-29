c5b8fd931495995d15cd121d9ae9f8538378f660
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:22 +0100

    font-bonbon.rb: fix for new style

--- Casks/font-bonbon.rb
+++ Casks/font-bonbon.rb
@@ -1,11 +1,11 @@
-cask 'font-bonbon' do
+cask "font-bonbon" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bonbon/Bonbon-Regular.ttf'
-  name 'Bonbon'
-  homepage 'https://fonts.google.com/specimen/Bonbon'
+  url "https://github.com/google/fonts/raw/master/ofl/bonbon/Bonbon-Regular.ttf"
+  name "Bonbon"
+  homepage "https://fonts.google.com/specimen/Bonbon"
 
-  font 'Bonbon-Regular.ttf'
+  font "Bonbon-Regular.ttf"
 end
