c0e2bdda703c3f26ac1b9b0fda99b3f24d381c78
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:09 +0100

    font-rakkas.rb: fix for new style

--- Casks/font-rakkas.rb
+++ Casks/font-rakkas.rb
@@ -1,11 +1,11 @@
-cask 'font-rakkas' do
+cask "font-rakkas" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/rakkas/Rakkas-Regular.ttf'
-  name 'Rakkas'
-  homepage 'https://fonts.google.com/specimen/Rakkas'
+  url "https://github.com/google/fonts/raw/master/ofl/rakkas/Rakkas-Regular.ttf"
+  name "Rakkas"
+  homepage "https://fonts.google.com/specimen/Rakkas"
 
-  font 'Rakkas-Regular.ttf'
+  font "Rakkas-Regular.ttf"
 end
