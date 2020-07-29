ee66918b0f36adec5c69737891bd370508087f4a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:10 +0100

    font-red-hat-display.rb: fix for new style

--- Casks/font-red-hat-display.rb
+++ Casks/font-red-hat-display.rb
@@ -1,22 +1,22 @@
-cask 'font-red-hat-display' do
+cask "font-red-hat-display" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/redhatdisplay',
+  url "https://github.com/google/fonts/trunk/ofl/redhatdisplay",
       using:      :svn,
       trust_cert: true
-  name 'Red Hat Display'
-  homepage 'https://fonts.google.com/specimen/Red+Hat+Display'
+  name "Red Hat Display"
+  homepage "https://fonts.google.com/specimen/Red+Hat+Display"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'RedHatDisplay-Black.ttf'
-  font 'RedHatDisplay-BlackItalic.ttf'
-  font 'RedHatDisplay-Bold.ttf'
-  font 'RedHatDisplay-BoldItalic.ttf'
-  font 'RedHatDisplay-Italic.ttf'
-  font 'RedHatDisplay-Medium.ttf'
-  font 'RedHatDisplay-MediumItalic.ttf'
-  font 'RedHatDisplay-Regular.ttf'
+  font "RedHatDisplay-Black.ttf"
+  font "RedHatDisplay-BlackItalic.ttf"
+  font "RedHatDisplay-Bold.ttf"
+  font "RedHatDisplay-BoldItalic.ttf"
+  font "RedHatDisplay-Italic.ttf"
+  font "RedHatDisplay-Medium.ttf"
+  font "RedHatDisplay-MediumItalic.ttf"
+  font "RedHatDisplay-Regular.ttf"
 end
