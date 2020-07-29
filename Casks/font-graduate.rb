70bcf1ff2aa6e020e4b0ed6cb15d8cff6d7b8566
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:06 +0100

    font-graduate.rb: fix for new style

--- Casks/font-graduate.rb
+++ Casks/font-graduate.rb
@@ -1,11 +1,11 @@
-cask 'font-graduate' do
+cask "font-graduate" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/graduate/Graduate-Regular.ttf'
-  name 'Graduate'
-  homepage 'https://fonts.google.com/specimen/Graduate'
+  url "https://github.com/google/fonts/raw/master/ofl/graduate/Graduate-Regular.ttf"
+  name "Graduate"
+  homepage "https://fonts.google.com/specimen/Graduate"
 
-  font 'Graduate-Regular.ttf'
+  font "Graduate-Regular.ttf"
 end
