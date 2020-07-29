4999898667a8b3c103ef40060ce541272eeae148
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:29 +0100

    font-kranky.rb: fix for new style

--- Casks/font-kranky.rb
+++ Casks/font-kranky.rb
@@ -1,11 +1,11 @@
-cask 'font-kranky' do
+cask "font-kranky" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/kranky/Kranky-Regular.ttf'
-  name 'Kranky'
-  homepage 'https://fonts.google.com/specimen/Kranky'
+  url "https://github.com/google/fonts/raw/master/apache/kranky/Kranky-Regular.ttf"
+  name "Kranky"
+  homepage "https://fonts.google.com/specimen/Kranky"
 
-  font 'Kranky-Regular.ttf'
+  font "Kranky-Regular.ttf"
 end
