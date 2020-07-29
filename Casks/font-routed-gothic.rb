980f71edc65aeb5dfaee1a9c6968cfbe01a3e73d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:12 +0100

    font-routed-gothic.rb: fix for new style

--- Casks/font-routed-gothic.rb
+++ Casks/font-routed-gothic.rb
@@ -1,11 +1,11 @@
-cask 'font-routed-gothic' do
-  version '1.0.0'
-  sha256 '3ecffba109bf2619057815411253e5c6f47f82fedba38ade6c951e13a8b2ad5e'
+cask "font-routed-gothic" do
+  version "1.0.0"
+  sha256 "3ecffba109bf2619057815411253e5c6f47f82fedba38ade6c951e13a8b2ad5e"
 
   url "https://webonastick.com/fonts/routed-gothic/download/routed-gothic-ttf-v#{version}.zip"
-  appcast 'https://github.com/dse/routed-gothic/releases.atom'
-  name 'Routed Gothic'
-  homepage 'https://webonastick.com/fonts/routed-gothic/'
+  appcast "https://github.com/dse/routed-gothic/releases.atom"
+  name "Routed Gothic"
+  homepage "https://webonastick.com/fonts/routed-gothic/"
 
   font "routed-gothic-ttf-v#{version}/routed-gothic-half-italic.ttf"
   font "routed-gothic-ttf-v#{version}/routed-gothic-italic.ttf"
