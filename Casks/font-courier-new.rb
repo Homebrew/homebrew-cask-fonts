98f72838a12589f276922013b7b8ae7d21bde30a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:37 +0100

    font-courier-new.rb: fix for new style

--- Casks/font-courier-new.rb
+++ Casks/font-courier-new.rb
@@ -1,14 +1,14 @@
-cask 'font-courier-new' do
-  version '2.82'
-  sha256 'bb511d861655dde879ae552eb86b134d6fae67cb58502e6ff73ec5d9151f3384'
+cask "font-courier-new" do
+  version "2.82"
+  sha256 "bb511d861655dde879ae552eb86b134d6fae67cb58502e6ff73ec5d9151f3384"
 
-  url 'https://downloads.sourceforge.net/corefonts/courie32.exe'
-  appcast 'https://sourceforge.net/projects/corefonts/rss'
-  name 'Courier New'
-  homepage 'https://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  url "https://downloads.sourceforge.net/corefonts/courie32.exe"
+  appcast "https://sourceforge.net/projects/corefonts/rss"
+  name "Courier New"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
 
-  font 'cour.ttf'
-  font 'courbd.ttf'
-  font 'courbi.ttf'
-  font 'couri.ttf'
+  font "cour.ttf"
+  font "courbd.ttf"
+  font "courbi.ttf"
+  font "couri.ttf"
 end
