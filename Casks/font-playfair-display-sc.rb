4d75c181c8c23888afbe689705548abc62ff2129
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:05 +0100

    font-playfair-display-sc.rb: fix for new style

--- Casks/font-playfair-display-sc.rb
+++ Casks/font-playfair-display-sc.rb
@@ -1,20 +1,20 @@
-cask 'font-playfair-display-sc' do
+cask "font-playfair-display-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/playfairdisplaysc',
+  url "https://github.com/google/fonts/trunk/ofl/playfairdisplaysc",
       using:      :svn,
       trust_cert: true
-  name 'Playfair Display SC'
-  homepage 'https://fonts.google.com/specimen/Playfair+Display+SC'
+  name "Playfair Display SC"
+  homepage "https://fonts.google.com/specimen/Playfair+Display+SC"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'PlayfairDisplaySC-Black.ttf'
-  font 'PlayfairDisplaySC-BlackItalic.ttf'
-  font 'PlayfairDisplaySC-Bold.ttf'
-  font 'PlayfairDisplaySC-BoldItalic.ttf'
-  font 'PlayfairDisplaySC-Italic.ttf'
-  font 'PlayfairDisplaySC-Regular.ttf'
+  font "PlayfairDisplaySC-Black.ttf"
+  font "PlayfairDisplaySC-BlackItalic.ttf"
+  font "PlayfairDisplaySC-Bold.ttf"
+  font "PlayfairDisplaySC-BoldItalic.ttf"
+  font "PlayfairDisplaySC-Italic.ttf"
+  font "PlayfairDisplaySC-Regular.ttf"
 end
