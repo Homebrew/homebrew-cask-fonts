fc67bab9071af39858b60def22ba47b7bc3ed9e0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:21 +0100

    font-srisakdi.rb: fix for new style

--- Casks/font-srisakdi.rb
+++ Casks/font-srisakdi.rb
@@ -1,16 +1,16 @@
-cask 'font-srisakdi' do
+cask "font-srisakdi" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/srisakdi',
+  url "https://github.com/google/fonts/trunk/ofl/srisakdi",
       using:      :svn,
       trust_cert: true
-  name 'Srisakdi'
-  homepage 'https://fonts.google.com/specimen/Srisakdi'
+  name "Srisakdi"
+  homepage "https://fonts.google.com/specimen/Srisakdi"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Srisakdi-Bold.ttf'
-  font 'Srisakdi-Regular.ttf'
+  font "Srisakdi-Bold.ttf"
+  font "Srisakdi-Regular.ttf"
 end
