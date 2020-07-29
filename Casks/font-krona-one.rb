a6909791df8f1f4da69e63f61a0f6c113b0eea69
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:29 +0100

    font-krona-one.rb: fix for new style

--- Casks/font-krona-one.rb
+++ Casks/font-krona-one.rb
@@ -1,11 +1,11 @@
-cask 'font-krona-one' do
+cask "font-krona-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/kronaone/KronaOne-Regular.ttf'
-  name 'Krona One'
-  homepage 'https://fonts.google.com/specimen/Krona+One'
+  url "https://github.com/google/fonts/raw/master/ofl/kronaone/KronaOne-Regular.ttf"
+  name "Krona One"
+  homepage "https://fonts.google.com/specimen/Krona+One"
 
-  font 'KronaOne-Regular.ttf'
+  font "KronaOne-Regular.ttf"
 end
