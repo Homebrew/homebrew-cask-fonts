3abab2756af9571c8cc7654483f816fb908b9558
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:32 +0100

    font-leckerli-one.rb: fix for new style

--- Casks/font-leckerli-one.rb
+++ Casks/font-leckerli-one.rb
@@ -1,11 +1,11 @@
-cask 'font-leckerli-one' do
+cask "font-leckerli-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/leckerlione/LeckerliOne-Regular.ttf'
-  name 'Leckerli One'
-  homepage 'https://fonts.google.com/specimen/Leckerli+One'
+  url "https://github.com/google/fonts/raw/master/ofl/leckerlione/LeckerliOne-Regular.ttf"
+  name "Leckerli One"
+  homepage "https://fonts.google.com/specimen/Leckerli+One"
 
-  font 'LeckerliOne-Regular.ttf'
+  font "LeckerliOne-Regular.ttf"
 end
