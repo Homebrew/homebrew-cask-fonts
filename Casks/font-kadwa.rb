1c70922636d58cbe70a2e033db3c6463f294244c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:26 +0100

    font-kadwa.rb: fix for new style

--- Casks/font-kadwa.rb
+++ Casks/font-kadwa.rb
@@ -1,16 +1,16 @@
-cask 'font-kadwa' do
+cask "font-kadwa" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/kadwa',
+  url "https://github.com/google/fonts/trunk/ofl/kadwa",
       using:      :svn,
       trust_cert: true
-  name 'Kadwa'
-  homepage 'https://fonts.google.com/specimen/Kadwa'
+  name "Kadwa"
+  homepage "https://fonts.google.com/specimen/Kadwa"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Kadwa-Bold.ttf'
-  font 'Kadwa-Regular.ttf'
+  font "Kadwa-Bold.ttf"
+  font "Kadwa-Regular.ttf"
 end
