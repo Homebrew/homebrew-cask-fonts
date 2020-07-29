fa42c0bd21cfb5b4c2ba6034d94dd4dbacf9c902
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:47 +0100

    font-arvo.rb: fix for new style

--- Casks/font-arvo.rb
+++ Casks/font-arvo.rb
@@ -1,18 +1,18 @@
-cask 'font-arvo' do
+cask "font-arvo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/arvo',
+  url "https://github.com/google/fonts/trunk/ofl/arvo",
       using:      :svn,
       trust_cert: true
-  name 'Arvo'
-  homepage 'https://fonts.google.com/specimen/Arvo'
+  name "Arvo"
+  homepage "https://fonts.google.com/specimen/Arvo"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Arvo-Bold.ttf'
-  font 'Arvo-BoldItalic.ttf'
-  font 'Arvo-Italic.ttf'
-  font 'Arvo-Regular.ttf'
+  font "Arvo-Bold.ttf"
+  font "Arvo-BoldItalic.ttf"
+  font "Arvo-Italic.ttf"
+  font "Arvo-Regular.ttf"
 end
