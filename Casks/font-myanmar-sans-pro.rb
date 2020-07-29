12f8607ab33cda1fe7345000bcbdc7d991b106af
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:47 +0100

    font-myanmar-sans-pro.rb: fix for new style

--- Casks/font-myanmar-sans-pro.rb
+++ Casks/font-myanmar-sans-pro.rb
@@ -1,11 +1,11 @@
-cask 'font-myanmar-sans-pro' do
+cask "font-myanmar-sans-pro" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/myanmarsanspro/MyanmarSansPro-Regular.ttf'
-  name 'Myanmar Sans Pro'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/myanmarsanspro/MyanmarSansPro-Regular.ttf"
+  name "Myanmar Sans Pro"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'MyanmarSansPro-Regular.ttf'
+  font "MyanmarSansPro-Regular.ttf"
 end
