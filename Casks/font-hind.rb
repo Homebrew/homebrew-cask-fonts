b48f74565170c977af2a781e2aa8a148215308e0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:11 +0100

    font-hind.rb: fix for new style

--- Casks/font-hind.rb
+++ Casks/font-hind.rb
@@ -1,15 +1,15 @@
-cask 'font-hind' do
-  version '2.000'
-  sha256 '8748ce1fa0db67d0c782d7824a9fdcf0b8544b9d063db477dff9733774571193'
+cask "font-hind" do
+  version "2.000"
+  sha256 "8748ce1fa0db67d0c782d7824a9fdcf0b8544b9d063db477dff9733774571193"
 
   url "https://github.com/itfoundry/hind/releases/download/v2.000/hind-#{version.dots_to_underscores}.zip"
-  appcast 'https://github.com/itfoundry/hind/releases.atom'
-  name 'Hind'
-  homepage 'https://github.com/itfoundry/hind'
+  appcast "https://github.com/itfoundry/hind/releases.atom"
+  name "Hind"
+  homepage "https://github.com/itfoundry/hind"
 
-  font 'Hind-Bold.otf'
-  font 'Hind-Light.otf'
-  font 'Hind-Medium.otf'
-  font 'Hind-Regular.otf'
-  font 'Hind-SemiBold.otf'
+  font "Hind-Bold.otf"
+  font "Hind-Light.otf"
+  font "Hind-Medium.otf"
+  font "Hind-Regular.otf"
+  font "Hind-SemiBold.otf"
 end
