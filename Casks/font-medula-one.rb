7649b63480eb2f00f30d7102b740149a2eb9ee9e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:40 +0100

    font-medula-one.rb: fix for new style

--- Casks/font-medula-one.rb
+++ Casks/font-medula-one.rb
@@ -1,11 +1,11 @@
-cask 'font-medula-one' do
+cask "font-medula-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/medulaone/MedulaOne-Regular.ttf'
-  name 'Medula One'
-  homepage 'https://fonts.google.com/specimen/Medula+One'
+  url "https://github.com/google/fonts/raw/master/ofl/medulaone/MedulaOne-Regular.ttf"
+  name "Medula One"
+  homepage "https://fonts.google.com/specimen/Medula+One"
 
-  font 'MedulaOne-Regular.ttf'
+  font "MedulaOne-Regular.ttf"
 end
