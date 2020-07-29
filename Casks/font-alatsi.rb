631e39daf312c58ff519f19c1efddb97f0c15e20
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:32:13 +0100

    font-alatsi.rb: fix for new style

--- Casks/font-alatsi.rb
+++ Casks/font-alatsi.rb
@@ -1,11 +1,11 @@
-cask 'font-alatsi' do
+cask "font-alatsi" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/alatsi/Alatsi-Regular.ttf'
-  name 'Alatsi'
-  homepage 'https://fonts.google.com/specimen/Alatsi'
+  url "https://github.com/google/fonts/raw/master/ofl/alatsi/Alatsi-Regular.ttf"
+  name "Alatsi"
+  homepage "https://fonts.google.com/specimen/Alatsi"
 
-  font 'Alatsi-Regular.ttf'
+  font "Alatsi-Regular.ttf"
 end
