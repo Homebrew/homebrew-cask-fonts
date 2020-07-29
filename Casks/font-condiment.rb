52257c8187956384d638df5ea92eded07fb1bf0e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:34 +0100

    font-condiment.rb: fix for new style

--- Casks/font-condiment.rb
+++ Casks/font-condiment.rb
@@ -1,11 +1,11 @@
-cask 'font-condiment' do
+cask "font-condiment" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/condiment/Condiment-Regular.ttf'
-  name 'Condiment'
-  homepage 'https://fonts.google.com/specimen/Condiment'
+  url "https://github.com/google/fonts/raw/master/ofl/condiment/Condiment-Regular.ttf"
+  name "Condiment"
+  homepage "https://fonts.google.com/specimen/Condiment"
 
-  font 'Condiment-Regular.ttf'
+  font "Condiment-Regular.ttf"
 end
