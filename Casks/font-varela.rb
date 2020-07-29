8050dd287deff4a516d2095f8c125b3538b7acad
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:27 +0100

    font-varela.rb: fix for new style

--- Casks/font-varela.rb
+++ Casks/font-varela.rb
@@ -1,11 +1,11 @@
-cask 'font-varela' do
+cask "font-varela" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/varela/Varela-Regular.ttf'
-  name 'Varela'
-  homepage 'https://fonts.google.com/specimen/Varela'
+  url "https://github.com/google/fonts/raw/master/ofl/varela/Varela-Regular.ttf"
+  name "Varela"
+  homepage "https://fonts.google.com/specimen/Varela"
 
-  font 'Varela-Regular.ttf'
+  font "Varela-Regular.ttf"
 end
