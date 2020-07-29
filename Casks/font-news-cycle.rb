e4ae782454dbfbbfe1df4308249717cb5cb2b044
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:48 +0100

    font-news-cycle.rb: fix for new style

--- Casks/font-news-cycle.rb
+++ Casks/font-news-cycle.rb
@@ -1,16 +1,16 @@
-cask 'font-news-cycle' do
+cask "font-news-cycle" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/newscycle',
+  url "https://github.com/google/fonts/trunk/ofl/newscycle",
       using:      :svn,
       trust_cert: true
-  name 'News Cycle'
-  homepage 'https://fonts.google.com/specimen/News+Cycle'
+  name "News Cycle"
+  homepage "https://fonts.google.com/specimen/News+Cycle"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'NewsCycle-Bold.ttf'
-  font 'NewsCycle-Regular.ttf'
+  font "NewsCycle-Bold.ttf"
+  font "NewsCycle-Regular.ttf"
 end
