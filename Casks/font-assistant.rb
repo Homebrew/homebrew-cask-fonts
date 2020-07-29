0c947e744c18dcf5eb185820e453e812ace20215
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:09 +0100

    font-assistant.rb: fix for new style

--- Casks/font-assistant.rb
+++ Casks/font-assistant.rb
@@ -1,20 +1,20 @@
-cask 'font-assistant' do
+cask "font-assistant" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/assistant',
+  url "https://github.com/google/fonts/trunk/ofl/assistant",
       using:      :svn,
       trust_cert: true
-  name 'Assistant'
-  homepage 'https://fonts.google.com/specimen/Assistant'
+  name "Assistant"
+  homepage "https://fonts.google.com/specimen/Assistant"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Assistant-Bold.ttf'
-  font 'Assistant-ExtraBold.ttf'
-  font 'Assistant-ExtraLight.ttf'
-  font 'Assistant-Light.ttf'
-  font 'Assistant-Regular.ttf'
-  font 'Assistant-SemiBold.ttf'
+  font "Assistant-Bold.ttf"
+  font "Assistant-ExtraBold.ttf"
+  font "Assistant-ExtraLight.ttf"
+  font "Assistant-Light.ttf"
+  font "Assistant-Regular.ttf"
+  font "Assistant-SemiBold.ttf"
 end
