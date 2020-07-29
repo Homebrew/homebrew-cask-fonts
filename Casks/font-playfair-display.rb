2ac94754286c014fedffdea063f9f5474972c6c9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:05 +0100

    font-playfair-display.rb: fix for new style

--- Casks/font-playfair-display.rb
+++ Casks/font-playfair-display.rb
@@ -1,16 +1,16 @@
-cask 'font-playfair-display' do
+cask "font-playfair-display" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/playfairdisplay',
+  url "https://github.com/google/fonts/trunk/ofl/playfairdisplay",
       using:      :svn,
       trust_cert: true
-  name 'Playfair Display'
-  homepage 'https://fonts.google.com/specimen/Playfair+Display'
+  name "Playfair Display"
+  homepage "https://fonts.google.com/specimen/Playfair+Display"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'PlayfairDisplay-Italic[wght].ttf'
-  font 'PlayfairDisplay[wght].ttf'
+  font "PlayfairDisplay-Italic[wght].ttf"
+  font "PlayfairDisplay[wght].ttf"
 end
