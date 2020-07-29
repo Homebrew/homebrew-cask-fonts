fa21408ce812c6c11147e7bc73b50b557cc528a6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:19 +0100

    font-source-han-serif-sb-h.rb: fix for new style

--- Casks/font-source-han-serif-sb-h.rb
+++ Casks/font-source-han-serif-sb-h.rb
@@ -1,15 +1,15 @@
-cask 'font-source-han-serif-sb-h' do
-  version '1.001R'
-  sha256 'e7c7c0c02e43f7cc6b1dd54e5e3a313b434123a796c8b64459f95174a6021645'
+cask "font-source-han-serif-sb-h" do
+  version "1.001R"
+  sha256 "e7c7c0c02e43f7cc6b1dd54e5e3a313b434123a796c8b64459f95174a6021645"
 
-  url 'https://github.com/adobe-fonts/source-han-serif/raw/release/OTC/SourceHanSerifOTC_SB-H.zip'
-  appcast 'https://github.com/adobe-fonts/source-han-serif/releases.atom'
-  name 'Source Han Serif SB-H'
-  homepage 'https://github.com/adobe-fonts/source-han-serif'
+  url "https://github.com/adobe-fonts/source-han-serif/raw/release/OTC/SourceHanSerifOTC_SB-H.zip"
+  appcast "https://github.com/adobe-fonts/source-han-serif/releases.atom"
+  name "Source Han Serif SB-H"
+  homepage "https://github.com/adobe-fonts/source-han-serif"
 
-  font 'SourceHanSerifOTC_SB-H/SourceHanSerif-SemiBold.ttc'
-  font 'SourceHanSerifOTC_SB-H/SourceHanSerif-Bold.ttc'
-  font 'SourceHanSerifOTC_SB-H/SourceHanSerif-Heavy.ttc'
+  font "SourceHanSerifOTC_SB-H/SourceHanSerif-SemiBold.ttc"
+  font "SourceHanSerifOTC_SB-H/SourceHanSerif-Bold.ttc"
+  font "SourceHanSerifOTC_SB-H/SourceHanSerif-Heavy.ttc"
 
   caveats <<~EOS
     #{token} only installs the SemiBold, Bold, and Heavy weights.
