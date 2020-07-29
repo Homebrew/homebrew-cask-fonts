34d7cdd72edde2062ec20292a83bb4c5fbba8621
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:28 +0100

    font-vidaloka.rb: fix for new style

--- Casks/font-vidaloka.rb
+++ Casks/font-vidaloka.rb
@@ -1,11 +1,11 @@
-cask 'font-vidaloka' do
+cask "font-vidaloka" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/vidaloka/Vidaloka-Regular.ttf'
-  name 'Vidaloka'
-  homepage 'https://fonts.google.com/specimen/Vidaloka'
+  url "https://github.com/google/fonts/raw/master/ofl/vidaloka/Vidaloka-Regular.ttf"
+  name "Vidaloka"
+  homepage "https://fonts.google.com/specimen/Vidaloka"
 
-  font 'Vidaloka-Regular.ttf'
+  font "Vidaloka-Regular.ttf"
 end
