f26ba06ed88a738dc5a67e50575534c172bf5e54
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:40 +0100

    font-mate.rb: fix for new style

--- Casks/font-mate.rb
+++ Casks/font-mate.rb
@@ -1,16 +1,16 @@
-cask 'font-mate' do
+cask "font-mate" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/mate',
+  url "https://github.com/google/fonts/trunk/ofl/mate",
       using:      :svn,
       trust_cert: true
-  name 'Mate'
-  homepage 'https://fonts.google.com/specimen/Mate'
+  name "Mate"
+  homepage "https://fonts.google.com/specimen/Mate"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Mate-Italic.ttf'
-  font 'Mate-Regular.ttf'
+  font "Mate-Italic.ttf"
+  font "Mate-Regular.ttf"
 end
