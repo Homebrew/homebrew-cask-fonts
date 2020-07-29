c370452dc6bbb223201854d6e0ab997fe56cadce
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:14 +0100

    font-im-fell-english-sc.rb: fix for new style

--- Casks/font-im-fell-english-sc.rb
+++ Casks/font-im-fell-english-sc.rb
@@ -1,11 +1,11 @@
-cask 'font-im-fell-english-sc' do
+cask "font-im-fell-english-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/imfellenglishsc/IMFeENsc28P.ttf'
-  name 'IM Fell English SC'
-  homepage 'https://fonts.google.com/specimen/IM+Fell+English+SC'
+  url "https://github.com/google/fonts/raw/master/ofl/imfellenglishsc/IMFeENsc28P.ttf"
+  name "IM Fell English SC"
+  homepage "https://fonts.google.com/specimen/IM+Fell+English+SC"
 
-  font 'IMFeENsc28P.ttf'
+  font "IMFeENsc28P.ttf"
 end
