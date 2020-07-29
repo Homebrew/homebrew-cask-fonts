1803aeba441c0f32c705362e8feffab1d87cea0d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:59 +0100

    font-nova-cut.rb: fix for new style

--- Casks/font-nova-cut.rb
+++ Casks/font-nova-cut.rb
@@ -1,11 +1,11 @@
-cask 'font-nova-cut' do
+cask "font-nova-cut" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/novacut/NovaCut.ttf'
-  name 'Nova Cut'
-  homepage 'https://fonts.google.com/specimen/Nova+Cut'
+  url "https://github.com/google/fonts/raw/master/ofl/novacut/NovaCut.ttf"
+  name "Nova Cut"
+  homepage "https://fonts.google.com/specimen/Nova+Cut"
 
-  font 'NovaCut.ttf'
+  font "NovaCut.ttf"
 end
