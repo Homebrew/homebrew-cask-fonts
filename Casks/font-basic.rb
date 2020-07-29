113f55b132b9cb571291af83bc7431c17a7ffc8a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:17 +0100

    font-basic.rb: fix for new style

--- Casks/font-basic.rb
+++ Casks/font-basic.rb
@@ -1,11 +1,11 @@
-cask 'font-basic' do
+cask "font-basic" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/basic/Basic-Regular.ttf'
-  name 'Basic'
-  homepage 'https://fonts.google.com/specimen/Basic'
+  url "https://github.com/google/fonts/raw/master/ofl/basic/Basic-Regular.ttf"
+  name "Basic"
+  homepage "https://fonts.google.com/specimen/Basic"
 
-  font 'Basic-Regular.ttf'
+  font "Basic-Regular.ttf"
 end
