a0fa1ca296982c3488c2c887d1be8602847a6f70
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:22 +0100

    font-jeju-gothic.rb: fix for new style

--- Casks/font-jeju-gothic.rb
+++ Casks/font-jeju-gothic.rb
@@ -1,11 +1,11 @@
-cask 'font-jeju-gothic' do
+cask "font-jeju-gothic" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/jejugothic/JejuGothic-Regular.ttf'
-  name 'Jeju Gothic'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/jejugothic/JejuGothic-Regular.ttf"
+  name "Jeju Gothic"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'JejuGothic-Regular.ttf'
+  font "JejuGothic-Regular.ttf"
 end
