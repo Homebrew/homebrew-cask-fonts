e4a506f10eee34f031d646e40699f96c88005752
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:34 +0100

    font-comic-neue.rb: fix for new style

--- Casks/font-comic-neue.rb
+++ Casks/font-comic-neue.rb
@@ -1,10 +1,10 @@
-cask 'font-comic-neue' do
-  version '2.2'
-  sha256 'd67c7de093b3555a525a29db1999b7759723208f514f5fca117201d63b94b5b6'
+cask "font-comic-neue" do
+  version "2.2"
+  sha256 "d67c7de093b3555a525a29db1999b7759723208f514f5fca117201d63b94b5b6"
 
   url "http://comicneue.com/comic-neue-#{version}.zip"
-  name 'Comic Neue'
-  homepage 'http://comicneue.com/'
+  name "Comic Neue"
+  homepage "http://comicneue.com/"
 
   font "comic-neue-#{version}/OTF/ComicNeue-Angular-Regular.otf"
   font "comic-neue-#{version}/OTF/ComicNeue-Angular_Bold.otf"
