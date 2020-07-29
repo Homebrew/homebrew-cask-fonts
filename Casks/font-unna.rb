838539eb80a39557c024ac6fbd0f14e326aae2e4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:27 +0100

    font-unna.rb: fix for new style

--- Casks/font-unna.rb
+++ Casks/font-unna.rb
@@ -1,18 +1,18 @@
-cask 'font-unna' do
+cask "font-unna" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/unna',
+  url "https://github.com/google/fonts/trunk/ofl/unna",
       using:      :svn,
       trust_cert: true
-  name 'Unna'
-  homepage 'https://fonts.google.com/specimen/Unna'
+  name "Unna"
+  homepage "https://fonts.google.com/specimen/Unna"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Unna-Bold.ttf'
-  font 'Unna-BoldItalic.ttf'
-  font 'Unna-Italic.ttf'
-  font 'Unna-Regular.ttf'
+  font "Unna-Bold.ttf"
+  font "Unna-BoldItalic.ttf"
+  font "Unna-Italic.ttf"
+  font "Unna-Regular.ttf"
 end
