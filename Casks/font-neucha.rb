7324ec7990d4bde148b355f951f1489e336f454a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:48 +0100

    font-neucha.rb: fix for new style

--- Casks/font-neucha.rb
+++ Casks/font-neucha.rb
@@ -1,11 +1,11 @@
-cask 'font-neucha' do
+cask "font-neucha" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/neucha/Neucha.ttf'
-  name 'Neucha'
-  homepage 'https://fonts.google.com/specimen/Neucha'
+  url "https://github.com/google/fonts/raw/master/ofl/neucha/Neucha.ttf"
+  name "Neucha"
+  homepage "https://fonts.google.com/specimen/Neucha"
 
-  font 'Neucha.ttf'
+  font "Neucha.ttf"
 end
