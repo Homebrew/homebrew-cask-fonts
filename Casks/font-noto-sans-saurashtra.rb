b9e222a6312d2d7e7456351229617218faf238a3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:56 +0100

    font-noto-sans-saurashtra.rb: fix for new style

--- Casks/font-noto-sans-saurashtra.rb
+++ Casks/font-noto-sans-saurashtra.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-saurashtra' do
+cask "font-noto-sans-saurashtra" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSaurashtra-unhinted.zip'
-  name 'Noto Sans Saurashtra'
-  homepage 'https://www.google.com/get/noto/#sans-saur'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSaurashtra-unhinted.zip"
+  name "Noto Sans Saurashtra"
+  homepage "https://www.google.com/get/noto/#sans-saur"
 
-  font 'NotoSansSaurashtra-Regular.ttf'
+  font "NotoSansSaurashtra-Regular.ttf"
 end
