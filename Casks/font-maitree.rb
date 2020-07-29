c87bcefd4a5ac58be19a2dafbb4dcbf41b111c40
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:38 +0100

    font-maitree.rb: fix for new style

--- Casks/font-maitree.rb
+++ Casks/font-maitree.rb
@@ -1,20 +1,20 @@
-cask 'font-maitree' do
+cask "font-maitree" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/maitree',
+  url "https://github.com/google/fonts/trunk/ofl/maitree",
       using:      :svn,
       trust_cert: true
-  name 'Maitree'
-  homepage 'https://fonts.google.com/specimen/Maitree'
+  name "Maitree"
+  homepage "https://fonts.google.com/specimen/Maitree"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Maitree-Bold.ttf'
-  font 'Maitree-ExtraLight.ttf'
-  font 'Maitree-Light.ttf'
-  font 'Maitree-Medium.ttf'
-  font 'Maitree-Regular.ttf'
-  font 'Maitree-SemiBold.ttf'
+  font "Maitree-Bold.ttf"
+  font "Maitree-ExtraLight.ttf"
+  font "Maitree-Light.ttf"
+  font "Maitree-Medium.ttf"
+  font "Maitree-Regular.ttf"
+  font "Maitree-SemiBold.ttf"
 end
