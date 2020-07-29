c210528fe08d2637366615a86bf3a561ffae9e5c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:36 +0100

    font-lobster-two.rb: fix for new style

--- Casks/font-lobster-two.rb
+++ Casks/font-lobster-two.rb
@@ -1,18 +1,18 @@
-cask 'font-lobster-two' do
+cask "font-lobster-two" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/lobstertwo',
+  url "https://github.com/google/fonts/trunk/ofl/lobstertwo",
       using:      :svn,
       trust_cert: true
-  name 'Lobster Two'
-  homepage 'https://fonts.google.com/specimen/Lobster+Two'
+  name "Lobster Two"
+  homepage "https://fonts.google.com/specimen/Lobster+Two"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'LobsterTwo-Bold.ttf'
-  font 'LobsterTwo-BoldItalic.ttf'
-  font 'LobsterTwo-Italic.ttf'
-  font 'LobsterTwo-Regular.ttf'
+  font "LobsterTwo-Bold.ttf"
+  font "LobsterTwo-BoldItalic.ttf"
+  font "LobsterTwo-Italic.ttf"
+  font "LobsterTwo-Regular.ttf"
 end
