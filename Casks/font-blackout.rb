de657b566bdeeb1c3cabe9eaab61b7ca200d6a1c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:21 +0100

    font-blackout.rb: fix for new style

--- Casks/font-blackout.rb
+++ Casks/font-blackout.rb
@@ -1,13 +1,13 @@
-cask 'font-blackout' do
+cask "font-blackout" do
   version :latest
   sha256 :no_check
 
   # github.com/theleagueof/blackout/ was verified as official when first introduced to the cask
-  url 'https://github.com/theleagueof/blackout/archive/master.zip'
-  name 'Blackout'
-  homepage 'https://www.theleagueofmoveabletype.com/blackout'
+  url "https://github.com/theleagueof/blackout/archive/master.zip"
+  name "Blackout"
+  homepage "https://www.theleagueofmoveabletype.com/blackout"
 
-  font 'blackout-master/Blackout 2 AM.ttf'
-  font 'blackout-master/Blackout Midnight.ttf'
-  font 'blackout-master/Blackout Sunrise.ttf'
+  font "blackout-master/Blackout 2 AM.ttf"
+  font "blackout-master/Blackout Midnight.ttf"
+  font "blackout-master/Blackout Sunrise.ttf"
 end
