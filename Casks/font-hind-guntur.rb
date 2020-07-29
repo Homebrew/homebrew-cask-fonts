4e7f851e5c3c32f5c66dd479c7208b40a51b8734
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:10 +0100

    font-hind-guntur.rb: fix for new style

--- Casks/font-hind-guntur.rb
+++ Casks/font-hind-guntur.rb
@@ -1,19 +1,19 @@
-cask 'font-hind-guntur' do
+cask "font-hind-guntur" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/hindguntur',
+  url "https://github.com/google/fonts/trunk/ofl/hindguntur",
       using:      :svn,
       trust_cert: true
-  name 'Hind Guntur'
-  homepage 'https://fonts.google.com/specimen/Hind+Guntur'
+  name "Hind Guntur"
+  homepage "https://fonts.google.com/specimen/Hind+Guntur"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'HindGuntur-Bold.ttf'
-  font 'HindGuntur-Light.ttf'
-  font 'HindGuntur-Medium.ttf'
-  font 'HindGuntur-Regular.ttf'
-  font 'HindGuntur-SemiBold.ttf'
+  font "HindGuntur-Bold.ttf"
+  font "HindGuntur-Light.ttf"
+  font "HindGuntur-Medium.ttf"
+  font "HindGuntur-Regular.ttf"
+  font "HindGuntur-SemiBold.ttf"
 end
