29b4d5a78758b628329277c7a9af4d6dbecedeff
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:19 +0100

    font-source-han-code-jp.rb: fix for new style

--- Casks/font-source-han-code-jp.rb
+++ Casks/font-source-han-code-jp.rb
@@ -1,10 +1,10 @@
-cask 'font-source-han-code-jp' do
-  version '2.011'
-  sha256 '0f98beb0a381d9d19067f9b58f65d2dd65c808a3012ac9cb9866e575ad6d2e7f'
+cask "font-source-han-code-jp" do
+  version "2.011"
+  sha256 "0f98beb0a381d9d19067f9b58f65d2dd65c808a3012ac9cb9866e575ad6d2e7f"
 
   url "https://github.com/adobe-fonts/source-han-code-jp/archive/#{version}R.zip"
-  name 'Source Han Code JP'
-  homepage 'https://github.com/adobe-fonts/source-han-code-jp'
+  name "Source Han Code JP"
+  homepage "https://github.com/adobe-fonts/source-han-code-jp"
 
   font "source-han-code-jp-#{version}R/OTF/SourceHanCodeJP-Bold.otf"
   font "source-han-code-jp-#{version}R/OTF/SourceHanCodeJP-BoldIt.otf"
