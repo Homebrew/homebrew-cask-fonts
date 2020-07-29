49175b457e041751f0025bbbff949818889ecab6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:29 +0100

    font-yaldevi-colombo.rb: fix for new style

--- Casks/font-yaldevi-colombo.rb
+++ Casks/font-yaldevi-colombo.rb
@@ -1,20 +1,20 @@
-cask 'font-yaldevi-colombo' do
+cask "font-yaldevi-colombo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/yaldevicolombo',
+  url "https://github.com/google/fonts/trunk/ofl/yaldevicolombo",
       using:      :svn,
       trust_cert: true
-  name 'Yaldevi Colombo'
-  homepage 'https://fonts.google.com/specimen/Yaldevi+Colombo'
+  name "Yaldevi Colombo"
+  homepage "https://fonts.google.com/specimen/Yaldevi+Colombo"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'YaldeviColombo-Bold.ttf'
-  font 'YaldeviColombo-ExtraLight.ttf'
-  font 'YaldeviColombo-Light.ttf'
-  font 'YaldeviColombo-Medium.ttf'
-  font 'YaldeviColombo-Regular.ttf'
-  font 'YaldeviColombo-SemiBold.ttf'
+  font "YaldeviColombo-Bold.ttf"
+  font "YaldeviColombo-ExtraLight.ttf"
+  font "YaldeviColombo-Light.ttf"
+  font "YaldeviColombo-Medium.ttf"
+  font "YaldeviColombo-Regular.ttf"
+  font "YaldeviColombo-SemiBold.ttf"
 end
