f125a87fd44037917bc3e4d44c21f0757fba92fa
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:51 +0100

    font-noto-sans-cherokee.rb: fix for new style

--- Casks/font-noto-sans-cherokee.rb
+++ Casks/font-noto-sans-cherokee.rb
@@ -1,19 +1,19 @@
-cask 'font-noto-sans-cherokee' do
+cask "font-noto-sans-cherokee" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCherokee-unhinted.zip'
-  name 'Noto Sans Cherokee'
-  homepage 'https://www.google.com/get/noto/#sans-cher'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCherokee-unhinted.zip"
+  name "Noto Sans Cherokee"
+  homepage "https://www.google.com/get/noto/#sans-cher"
 
-  font 'NotoSansCherokee-Black.ttf'
-  font 'NotoSansCherokee-Bold.ttf'
-  font 'NotoSansCherokee-ExtraBold.ttf'
-  font 'NotoSansCherokee-ExtraLight.ttf'
-  font 'NotoSansCherokee-Light.ttf'
-  font 'NotoSansCherokee-Medium.ttf'
-  font 'NotoSansCherokee-Regular.ttf'
-  font 'NotoSansCherokee-SemiBold.ttf'
-  font 'NotoSansCherokee-Thin.ttf'
+  font "NotoSansCherokee-Black.ttf"
+  font "NotoSansCherokee-Bold.ttf"
+  font "NotoSansCherokee-ExtraBold.ttf"
+  font "NotoSansCherokee-ExtraLight.ttf"
+  font "NotoSansCherokee-Light.ttf"
+  font "NotoSansCherokee-Medium.ttf"
+  font "NotoSansCherokee-Regular.ttf"
+  font "NotoSansCherokee-SemiBold.ttf"
+  font "NotoSansCherokee-Thin.ttf"
 end
