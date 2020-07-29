8210e1fb743cc62c7ded3e2748a13b12e2e82728
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:30:55 +0100

    font-3270-nerd-font.rb: fix for new style

--- Casks/font-3270-nerd-font.rb
+++ Casks/font-3270-nerd-font.rb
@@ -1,16 +1,16 @@
-cask 'font-3270-nerd-font' do
-  version '2.1.0'
-  sha256 'b39bb03a46bf51cacf094a53d9f07c090ede4571efa2c24600c66a25a3fee1cc'
+cask "font-3270-nerd-font" do
+  version "2.1.0"
+  sha256 "b39bb03a46bf51cacf094a53d9f07c090ede4571efa2c24600c66a25a3fee1cc"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/3270.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name '3270 Nerd Font (3270)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "3270 Nerd Font (3270)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font '3270-Medium Nerd Font Complete.otf'
-  font '3270 Narrow Nerd Font Complete.otf'
-  font '3270 Semi-Narrow Nerd Font Complete.otf'
-  font '3270-Medium Nerd Font Complete Mono.otf'
-  font '3270 Narrow Nerd Font Complete Mono.otf'
-  font '3270 Semi-Narrow Nerd Font Complete Mono.otf'
+  font "3270-Medium Nerd Font Complete.otf"
+  font "3270 Narrow Nerd Font Complete.otf"
+  font "3270 Semi-Narrow Nerd Font Complete.otf"
+  font "3270-Medium Nerd Font Complete Mono.otf"
+  font "3270 Narrow Nerd Font Complete Mono.otf"
+  font "3270 Semi-Narrow Nerd Font Complete Mono.otf"
 end
