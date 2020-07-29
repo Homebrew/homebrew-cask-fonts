84fbb5f1cfb2d16b0f3493dd9781f77b4e453a8c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:34 +0100

    font-libre-barcode-128.rb: fix for new style

--- Casks/font-libre-barcode-128.rb
+++ Casks/font-libre-barcode-128.rb
@@ -1,11 +1,11 @@
-cask 'font-libre-barcode-128' do
+cask "font-libre-barcode-128" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/librebarcode128/LibreBarcode128-Regular.ttf'
-  name 'Libre Barcode 128'
-  homepage 'https://fonts.google.com/specimen/Libre+Barcode+128'
+  url "https://github.com/google/fonts/raw/master/ofl/librebarcode128/LibreBarcode128-Regular.ttf"
+  name "Libre Barcode 128"
+  homepage "https://fonts.google.com/specimen/Libre+Barcode+128"
 
-  font 'LibreBarcode128-Regular.ttf'
+  font "LibreBarcode128-Regular.ttf"
 end
