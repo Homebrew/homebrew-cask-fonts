069536ab3cd88eb86701cd96854bac516798e571
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:46 +0100

    font-mr-bedfort.rb: fix for new style

--- Casks/font-mr-bedfort.rb
+++ Casks/font-mr-bedfort.rb
@@ -1,11 +1,11 @@
-cask 'font-mr-bedfort' do
+cask "font-mr-bedfort" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/mrbedfort/MrBedfort-Regular.ttf'
-  name 'Mr Bedfort'
-  homepage 'https://fonts.google.com/specimen/Mr+Bedfort'
+  url "https://github.com/google/fonts/raw/master/ofl/mrbedfort/MrBedfort-Regular.ttf"
+  name "Mr Bedfort"
+  homepage "https://fonts.google.com/specimen/Mr+Bedfort"
 
-  font 'MrBedfort-Regular.ttf'
+  font "MrBedfort-Regular.ttf"
 end
