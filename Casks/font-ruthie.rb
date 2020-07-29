41932dc1a1289b32fcfa8d61a8e6e3294c8bcad5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:13 +0100

    font-ruthie.rb: fix for new style

--- Casks/font-ruthie.rb
+++ Casks/font-ruthie.rb
@@ -1,11 +1,11 @@
-cask 'font-ruthie' do
+cask "font-ruthie" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/ruthie/Ruthie-Regular.ttf'
-  name 'Ruthie'
-  homepage 'https://fonts.google.com/specimen/Ruthie'
+  url "https://github.com/google/fonts/raw/master/ofl/ruthie/Ruthie-Regular.ttf"
+  name "Ruthie"
+  homepage "https://fonts.google.com/specimen/Ruthie"
 
-  font 'Ruthie-Regular.ttf'
+  font "Ruthie-Regular.ttf"
 end
