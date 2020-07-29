4672ede0ef546c630020bea99dbd7b0cecd4e32a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:28 +0100

    font-acme.rb: fix for new style

--- Casks/font-acme.rb
+++ Casks/font-acme.rb
@@ -1,11 +1,11 @@
-cask 'font-acme' do
+cask "font-acme" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/acme/Acme-Regular.ttf'
-  name 'Acme'
-  homepage 'https://fonts.google.com/specimen/Acme'
+  url "https://github.com/google/fonts/raw/master/ofl/acme/Acme-Regular.ttf"
+  name "Acme"
+  homepage "https://fonts.google.com/specimen/Acme"
 
-  font 'Acme-Regular.ttf'
+  font "Acme-Regular.ttf"
 end
