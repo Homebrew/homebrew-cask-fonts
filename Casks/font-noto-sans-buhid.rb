ee0a692fdbbd61b860950ddcf4c12c169eb850fc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:51 +0100

    font-noto-sans-buhid.rb: fix for new style

--- Casks/font-noto-sans-buhid.rb
+++ Casks/font-noto-sans-buhid.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-buhid' do
+cask "font-noto-sans-buhid" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBuhid-unhinted.zip'
-  name 'Noto Sans Buhid'
-  homepage 'https://www.google.com/get/noto/#sans-buhd'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBuhid-unhinted.zip"
+  name "Noto Sans Buhid"
+  homepage "https://www.google.com/get/noto/#sans-buhd"
 
-  font 'NotoSansBuhid-Regular.ttf'
+  font "NotoSansBuhid-Regular.ttf"
 end
