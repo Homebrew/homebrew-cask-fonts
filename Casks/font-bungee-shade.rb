cfbf342f41640a7943cc2dc19bb101083ddf4af6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:24 +0100

    font-bungee-shade.rb: fix for new style

--- Casks/font-bungee-shade.rb
+++ Casks/font-bungee-shade.rb
@@ -1,11 +1,11 @@
-cask 'font-bungee-shade' do
+cask "font-bungee-shade" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bungeeshade/BungeeShade-Regular.ttf'
-  name 'Bungee Shade'
-  homepage 'https://fonts.google.com/specimen/Bungee+Shade'
+  url "https://github.com/google/fonts/raw/master/ofl/bungeeshade/BungeeShade-Regular.ttf"
+  name "Bungee Shade"
+  homepage "https://fonts.google.com/specimen/Bungee+Shade"
 
-  font 'BungeeShade-Regular.ttf'
+  font "BungeeShade-Regular.ttf"
 end
