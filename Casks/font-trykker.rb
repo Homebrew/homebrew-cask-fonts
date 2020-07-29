5f2be1bfebea1cc502e5a80fdd1bd04d756b44d5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:25 +0100

    font-trykker.rb: fix for new style

--- Casks/font-trykker.rb
+++ Casks/font-trykker.rb
@@ -1,11 +1,11 @@
-cask 'font-trykker' do
+cask "font-trykker" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/trykker/Trykker-Regular.ttf'
-  name 'Trykker'
-  homepage 'https://fonts.google.com/specimen/Trykker'
+  url "https://github.com/google/fonts/raw/master/ofl/trykker/Trykker-Regular.ttf"
+  name "Trykker"
+  homepage "https://fonts.google.com/specimen/Trykker"
 
-  font 'Trykker-Regular.ttf'
+  font "Trykker-Regular.ttf"
 end
