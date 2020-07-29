5117a1edd093b8fb9e8c68985e46fbb5ab01c0be
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:00 +0100

    font-gentium-plus.rb: fix for new style

--- Casks/font-gentium-plus.rb
+++ Casks/font-gentium-plus.rb
@@ -1,11 +1,11 @@
-cask 'font-gentium-plus' do
-  version '5.000'
-  sha256 '335911f17bd2de4e43742e1d0367cfeff19a90abf7ed604f100a42705042e154'
+cask "font-gentium-plus" do
+  version "5.000"
+  sha256 "335911f17bd2de4e43742e1d0367cfeff19a90abf7ed604f100a42705042e154"
 
   url "https://software.sil.org/downloads/r/gentium/GentiumPlus-#{version}.zip"
-  name 'Gentium'
-  homepage 'https://software.sil.org/gentium/'
+  name "Gentium"
+  homepage "https://software.sil.org/gentium/"
 
-  font 'GentiumPlus-5.000/GentiumPlus-I.ttf'
-  font 'GentiumPlus-5.000/GentiumPlus-R.ttf'
+  font "GentiumPlus-5.000/GentiumPlus-I.ttf"
+  font "GentiumPlus-5.000/GentiumPlus-R.ttf"
 end
