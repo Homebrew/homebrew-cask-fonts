bad3ace39f501d21742dfc05cd776a3f0fe8c58f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:38 +0100

    font-maiden-orange.rb: fix for new style

--- Casks/font-maiden-orange.rb
+++ Casks/font-maiden-orange.rb
@@ -1,11 +1,11 @@
-cask 'font-maiden-orange' do
+cask "font-maiden-orange" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/maidenorange/MaidenOrange-Regular.ttf'
-  name 'Maiden Orange'
-  homepage 'https://fonts.google.com/specimen/Maiden+Orange'
+  url "https://github.com/google/fonts/raw/master/apache/maidenorange/MaidenOrange-Regular.ttf"
+  name "Maiden Orange"
+  homepage "https://fonts.google.com/specimen/Maiden+Orange"
 
-  font 'MaidenOrange-Regular.ttf'
+  font "MaidenOrange-Regular.ttf"
 end
