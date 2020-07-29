d01b66906f91705b7450d5b2c7bcce0df9fd53c8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:32:52 +0100

    font-alike.rb: fix for new style

--- Casks/font-alike.rb
+++ Casks/font-alike.rb
@@ -1,11 +1,11 @@
-cask 'font-alike' do
+cask "font-alike" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/alike/Alike-Regular.ttf'
-  name 'Alike'
-  homepage 'https://fonts.google.com/specimen/Alike'
+  url "https://github.com/google/fonts/raw/master/ofl/alike/Alike-Regular.ttf"
+  name "Alike"
+  homepage "https://fonts.google.com/specimen/Alike"
 
-  font 'Alike-Regular.ttf'
+  font "Alike-Regular.ttf"
 end
