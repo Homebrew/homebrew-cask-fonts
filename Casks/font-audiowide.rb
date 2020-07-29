c1395c6096d6a56868d2a615557ea1ead1c35954
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:29 +0100

    font-audiowide.rb: fix for new style

--- Casks/font-audiowide.rb
+++ Casks/font-audiowide.rb
@@ -1,11 +1,11 @@
-cask 'font-audiowide' do
+cask "font-audiowide" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/audiowide/Audiowide-Regular.ttf'
-  name 'Audiowide'
-  homepage 'https://fonts.google.com/specimen/Audiowide'
+  url "https://github.com/google/fonts/raw/master/ofl/audiowide/Audiowide-Regular.ttf"
+  name "Audiowide"
+  homepage "https://fonts.google.com/specimen/Audiowide"
 
-  font 'Audiowide-Regular.ttf'
+  font "Audiowide-Regular.ttf"
 end
