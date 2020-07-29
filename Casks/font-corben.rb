c5b6958a9973254c6691f83c858b19bd25fd2250
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:36 +0100

    font-corben.rb: fix for new style

--- Casks/font-corben.rb
+++ Casks/font-corben.rb
@@ -1,16 +1,16 @@
-cask 'font-corben' do
+cask "font-corben" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/corben',
+  url "https://github.com/google/fonts/trunk/ofl/corben",
       using:      :svn,
       trust_cert: true
-  name 'Corben'
-  homepage 'https://fonts.google.com/specimen/Corben'
+  name "Corben"
+  homepage "https://fonts.google.com/specimen/Corben"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Corben-Bold.ttf'
-  font 'Corben-Regular.ttf'
+  font "Corben-Bold.ttf"
+  font "Corben-Regular.ttf"
 end
