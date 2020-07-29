162feb2bd6b2585de3cdbae4d848f5a30477c6ed
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:27 +0100

    font-karma.rb: fix for new style

--- Casks/font-karma.rb
+++ Casks/font-karma.rb
@@ -1,15 +1,15 @@
-cask 'font-karma' do
-  version '2.000'
-  sha256 'ebbe01be41c18aed6e538ea8d88eec65bb1bca046afc36b2fc6a84e808bda7e4'
+cask "font-karma" do
+  version "2.000"
+  sha256 "ebbe01be41c18aed6e538ea8d88eec65bb1bca046afc36b2fc6a84e808bda7e4"
 
-  url 'https://github.com/itfoundry/karma/releases/download/v2.000/karma-2_000.zip'
-  appcast 'https://github.com/itfoundry/karma/releases.atom'
-  name 'Karma'
-  homepage 'https://github.com/itfoundry/karma'
+  url "https://github.com/itfoundry/karma/releases/download/v2.000/karma-2_000.zip"
+  appcast "https://github.com/itfoundry/karma/releases.atom"
+  name "Karma"
+  homepage "https://github.com/itfoundry/karma"
 
-  font 'Karma-Bold.otf'
-  font 'Karma-Light.otf'
-  font 'Karma-Medium.otf'
-  font 'Karma-Regular.otf'
-  font 'Karma-SemiBold.otf'
+  font "Karma-Bold.otf"
+  font "Karma-Light.otf"
+  font "Karma-Medium.otf"
+  font "Karma-Regular.otf"
+  font "Karma-SemiBold.otf"
 end
