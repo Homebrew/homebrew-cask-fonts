5a5b2d33745dee4a35856c5b2bb03dced474b0dc
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:23 +0100

    font-telex.rb: fix for new style

--- Casks/font-telex.rb
+++ Casks/font-telex.rb
@@ -1,11 +1,11 @@
-cask 'font-telex' do
+cask "font-telex" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/telex/Telex-Regular.ttf'
-  name 'Telex'
-  homepage 'https://fonts.google.com/specimen/Telex'
+  url "https://github.com/google/fonts/raw/master/ofl/telex/Telex-Regular.ttf"
+  name "Telex"
+  homepage "https://fonts.google.com/specimen/Telex"
 
-  font 'Telex-Regular.ttf'
+  font "Telex-Regular.ttf"
 end
