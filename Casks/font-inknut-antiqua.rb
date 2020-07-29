ac77e05217225cee89636aef019aa62bd0772be6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:17 +0100

    font-inknut-antiqua.rb: fix for new style

--- Casks/font-inknut-antiqua.rb
+++ Casks/font-inknut-antiqua.rb
@@ -1,21 +1,21 @@
-cask 'font-inknut-antiqua' do
+cask "font-inknut-antiqua" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/inknutantiqua',
+  url "https://github.com/google/fonts/trunk/ofl/inknutantiqua",
       using:      :svn,
       trust_cert: true
-  name 'Inknut Antiqua'
-  homepage 'https://fonts.google.com/specimen/Inknut+Antiqua'
+  name "Inknut Antiqua"
+  homepage "https://fonts.google.com/specimen/Inknut+Antiqua"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'InknutAntiqua-Black.ttf'
-  font 'InknutAntiqua-Bold.ttf'
-  font 'InknutAntiqua-ExtraBold.ttf'
-  font 'InknutAntiqua-Light.ttf'
-  font 'InknutAntiqua-Medium.ttf'
-  font 'InknutAntiqua-Regular.ttf'
-  font 'InknutAntiqua-SemiBold.ttf'
+  font "InknutAntiqua-Black.ttf"
+  font "InknutAntiqua-Bold.ttf"
+  font "InknutAntiqua-ExtraBold.ttf"
+  font "InknutAntiqua-Light.ttf"
+  font "InknutAntiqua-Medium.ttf"
+  font "InknutAntiqua-Regular.ttf"
+  font "InknutAntiqua-SemiBold.ttf"
 end
