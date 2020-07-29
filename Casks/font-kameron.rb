f1ec90d041737016bc0e0e4149dd0b3217b598f9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:26 +0100

    font-kameron.rb: fix for new style

--- Casks/font-kameron.rb
+++ Casks/font-kameron.rb
@@ -1,16 +1,16 @@
-cask 'font-kameron' do
+cask "font-kameron" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/kameron',
+  url "https://github.com/google/fonts/trunk/ofl/kameron",
       using:      :svn,
       trust_cert: true
-  name 'Kameron'
-  homepage 'https://fonts.google.com/specimen/Kameron'
+  name "Kameron"
+  homepage "https://fonts.google.com/specimen/Kameron"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Kameron-Bold.ttf'
-  font 'Kameron-Regular.ttf'
+  font "Kameron-Bold.ttf"
+  font "Kameron-Regular.ttf"
 end
