bd0ed80140da6092ee0e6345b2226af249036ca8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:45 +0100

    font-droid-sans-mono-for-powerline.rb: fix for new style

--- Casks/font-droid-sans-mono-for-powerline.rb
+++ Casks/font-droid-sans-mono-for-powerline.rb
@@ -1,14 +1,14 @@
-cask 'font-droid-sans-mono-for-powerline' do
+cask "font-droid-sans-mono-for-powerline" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/powerline/fonts/trunk/DroidSansMono',
+  url "https://github.com/powerline/fonts/trunk/DroidSansMono",
       using:      :svn,
       trust_cert: true
-  name 'Droid Sans Mono for Powerline'
-  homepage 'https://github.com/powerline/fonts/tree/master/DroidSansMono'
+  name "Droid Sans Mono for Powerline"
+  homepage "https://github.com/powerline/fonts/tree/master/DroidSansMono"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Droid Sans Mono for Powerline.otf'
+  font "Droid Sans Mono for Powerline.otf"
 end
