d854a25b2c0c067f7627aaa99d74aaa27f95fa22
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:39 +0100

    font-cute-font.rb: fix for new style

--- Casks/font-cute-font.rb
+++ Casks/font-cute-font.rb
@@ -1,11 +1,11 @@
-cask 'font-cute-font' do
+cask "font-cute-font" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/cutefont/CuteFont-Regular.ttf'
-  name 'Cute Font'
-  homepage 'https://fonts.google.com/specimen/Cute+Font'
+  url "https://github.com/google/fonts/raw/master/ofl/cutefont/CuteFont-Regular.ttf"
+  name "Cute Font"
+  homepage "https://fonts.google.com/specimen/Cute+Font"
 
-  font 'CuteFont-Regular.ttf'
+  font "CuteFont-Regular.ttf"
 end
