3b1c3dee7d0c65f78a010570b87b4539f87810fd
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:00 +0100

    font-ocr.rb: fix for new style

--- Casks/font-ocr.rb
+++ Casks/font-ocr.rb
@@ -1,11 +1,11 @@
-cask 'font-ocr' do
-  version '0.2'
-  sha256 '39289c641520265ecedbade99f01600f316f8196ec57f71c8402d3ba09438666'
+cask "font-ocr" do
+  version "0.2"
+  sha256 "39289c641520265ecedbade99f01600f316f8196ec57f71c8402d3ba09438666"
 
   # osdn.dl.osdn.jp/tsukurimashou/ was verified as official when first introduced to the cask
   url "https://osdn.dl.osdn.jp/tsukurimashou/56948/ocr-#{version}.zip"
-  name 'OCR'
-  homepage 'https://ansuz.sooke.bc.ca/page/fonts#ocra'
+  name "OCR"
+  homepage "https://ansuz.sooke.bc.ca/page/fonts#ocra"
 
   font "ocr-#{version}/OCRA.otf"
   font "ocr-#{version}/OCRB.otf"
