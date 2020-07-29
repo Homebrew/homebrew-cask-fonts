57d82feff3e0151e88a7d3963dc64cc64c38956f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:53 +0100

    font-fenix.rb: fix for new style

--- Casks/font-fenix.rb
+++ Casks/font-fenix.rb
@@ -1,11 +1,11 @@
-cask 'font-fenix' do
+cask "font-fenix" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/fenix/Fenix-Regular.ttf'
-  name 'Fenix'
-  homepage 'https://fonts.google.com/specimen/Fenix'
+  url "https://github.com/google/fonts/raw/master/ofl/fenix/Fenix-Regular.ttf"
+  name "Fenix"
+  homepage "https://fonts.google.com/specimen/Fenix"
 
-  font 'Fenix-Regular.ttf'
+  font "Fenix-Regular.ttf"
 end
