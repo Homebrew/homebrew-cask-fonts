0a036a9b20e39b0253530be295cc81196aaa450a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:59 +0100

    font-nova-mono.rb: fix for new style

--- Casks/font-nova-mono.rb
+++ Casks/font-nova-mono.rb
@@ -1,11 +1,11 @@
-cask 'font-nova-mono' do
+cask "font-nova-mono" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/novamono/NovaMono.ttf'
-  name 'Nova Mono'
-  homepage 'https://fonts.google.com/specimen/Nova+Mono'
+  url "https://github.com/google/fonts/raw/master/ofl/novamono/NovaMono.ttf"
+  name "Nova Mono"
+  homepage "https://fonts.google.com/specimen/Nova+Mono"
 
-  font 'NovaMono.ttf'
+  font "NovaMono.ttf"
 end
