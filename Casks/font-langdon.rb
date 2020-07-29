ab30040db8a6d151c98c802a686763d0116a7de2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:31 +0100

    font-langdon.rb: fix for new style

--- Casks/font-langdon.rb
+++ Casks/font-langdon.rb
@@ -1,10 +1,10 @@
-cask 'font-langdon' do
+cask "font-langdon" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.ffonts.net/Langdon.font.zip'
-  name 'Langdon'
-  homepage 'https://www.ffonts.net/Langdon.font'
+  url "https://www.ffonts.net/Langdon.font.zip"
+  name "Langdon"
+  homepage "https://www.ffonts.net/Langdon.font"
 
-  font 'Langdon.otf'
+  font "Langdon.otf"
 end
