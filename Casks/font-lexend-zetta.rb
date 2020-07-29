553ce442f98d82890311a3b34f30ee70b429377f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:34 +0100

    font-lexend-zetta.rb: fix for new style

--- Casks/font-lexend-zetta.rb
+++ Casks/font-lexend-zetta.rb
@@ -1,11 +1,11 @@
-cask 'font-lexend-zetta' do
+cask "font-lexend-zetta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/lexendzetta/LexendZetta-Regular.ttf'
-  name 'Lexend Zetta'
-  homepage 'https://fonts.google.com/specimen/Lexend+Zetta'
+  url "https://github.com/google/fonts/raw/master/ofl/lexendzetta/LexendZetta-Regular.ttf"
+  name "Lexend Zetta"
+  homepage "https://fonts.google.com/specimen/Lexend+Zetta"
 
-  font 'LexendZetta-Regular.ttf'
+  font "LexendZetta-Regular.ttf"
 end
