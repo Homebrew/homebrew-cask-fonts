437c887aa83c20fedebc3c2fe62d031eab763f48
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:57 +0100

    font-noto-sans-tifinagh.rb: fix for new style

--- Casks/font-noto-sans-tifinagh.rb
+++ Casks/font-noto-sans-tifinagh.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-tifinagh' do
+cask "font-noto-sans-tifinagh" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTifinagh-unhinted.zip'
-  name 'Noto Sans Tifinagh'
-  homepage 'https://www.google.com/get/noto/#sans-tfng'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTifinagh-unhinted.zip"
+  name "Noto Sans Tifinagh"
+  homepage "https://www.google.com/get/noto/#sans-tfng"
 
-  font 'NotoSansTifinagh-Regular.ttf'
+  font "NotoSansTifinagh-Regular.ttf"
 end
