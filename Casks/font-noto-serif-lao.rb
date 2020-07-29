b6f164df0d7a124a0bef18fbc1271dd23982aa70
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:59 +0100

    font-noto-serif-lao.rb: fix for new style

--- Casks/font-noto-serif-lao.rb
+++ Casks/font-noto-serif-lao.rb
@@ -1,46 +1,46 @@
-cask 'font-noto-serif-lao' do
+cask "font-noto-serif-lao" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifLao-unhinted.zip'
-  name 'Noto Serif Lao'
-  homepage 'https://www.google.com/get/noto/#serif-laoo'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifLao-unhinted.zip"
+  name "Noto Serif Lao"
+  homepage "https://www.google.com/get/noto/#serif-laoo"
 
-  font 'NotoSerifLao-Black.ttf'
-  font 'NotoSerifLao-Bold.ttf'
-  font 'NotoSerifLao-Condensed.ttf'
-  font 'NotoSerifLao-CondensedBlack.ttf'
-  font 'NotoSerifLao-CondensedBold.ttf'
-  font 'NotoSerifLao-CondensedExtraBold.ttf'
-  font 'NotoSerifLao-CondensedExtraLight.ttf'
-  font 'NotoSerifLao-CondensedLight.ttf'
-  font 'NotoSerifLao-CondensedMedium.ttf'
-  font 'NotoSerifLao-CondensedSemiBold.ttf'
-  font 'NotoSerifLao-CondensedThin.ttf'
-  font 'NotoSerifLao-ExtraBold.ttf'
-  font 'NotoSerifLao-ExtraCondensed.ttf'
-  font 'NotoSerifLao-ExtraCondensedBlack.ttf'
-  font 'NotoSerifLao-ExtraCondensedBold.ttf'
-  font 'NotoSerifLao-ExtraCondensedExtraBold.ttf'
-  font 'NotoSerifLao-ExtraCondensedExtraLight.ttf'
-  font 'NotoSerifLao-ExtraCondensedLight.ttf'
-  font 'NotoSerifLao-ExtraCondensedMedium.ttf'
-  font 'NotoSerifLao-ExtraCondensedSemiBold.ttf'
-  font 'NotoSerifLao-ExtraCondensedThin.ttf'
-  font 'NotoSerifLao-ExtraLight.ttf'
-  font 'NotoSerifLao-Light.ttf'
-  font 'NotoSerifLao-Medium.ttf'
-  font 'NotoSerifLao-Regular.ttf'
-  font 'NotoSerifLao-SemiBold.ttf'
-  font 'NotoSerifLao-SemiCondensed.ttf'
-  font 'NotoSerifLao-SemiCondensedBlack.ttf'
-  font 'NotoSerifLao-SemiCondensedBold.ttf'
-  font 'NotoSerifLao-SemiCondensedExtraBold.ttf'
-  font 'NotoSerifLao-SemiCondensedExtraLight.ttf'
-  font 'NotoSerifLao-SemiCondensedLight.ttf'
-  font 'NotoSerifLao-SemiCondensedMedium.ttf'
-  font 'NotoSerifLao-SemiCondensedSemiBold.ttf'
-  font 'NotoSerifLao-SemiCondensedThin.ttf'
-  font 'NotoSerifLao-Thin.ttf'
+  font "NotoSerifLao-Black.ttf"
+  font "NotoSerifLao-Bold.ttf"
+  font "NotoSerifLao-Condensed.ttf"
+  font "NotoSerifLao-CondensedBlack.ttf"
+  font "NotoSerifLao-CondensedBold.ttf"
+  font "NotoSerifLao-CondensedExtraBold.ttf"
+  font "NotoSerifLao-CondensedExtraLight.ttf"
+  font "NotoSerifLao-CondensedLight.ttf"
+  font "NotoSerifLao-CondensedMedium.ttf"
+  font "NotoSerifLao-CondensedSemiBold.ttf"
+  font "NotoSerifLao-CondensedThin.ttf"
+  font "NotoSerifLao-ExtraBold.ttf"
+  font "NotoSerifLao-ExtraCondensed.ttf"
+  font "NotoSerifLao-ExtraCondensedBlack.ttf"
+  font "NotoSerifLao-ExtraCondensedBold.ttf"
+  font "NotoSerifLao-ExtraCondensedExtraBold.ttf"
+  font "NotoSerifLao-ExtraCondensedExtraLight.ttf"
+  font "NotoSerifLao-ExtraCondensedLight.ttf"
+  font "NotoSerifLao-ExtraCondensedMedium.ttf"
+  font "NotoSerifLao-ExtraCondensedSemiBold.ttf"
+  font "NotoSerifLao-ExtraCondensedThin.ttf"
+  font "NotoSerifLao-ExtraLight.ttf"
+  font "NotoSerifLao-Light.ttf"
+  font "NotoSerifLao-Medium.ttf"
+  font "NotoSerifLao-Regular.ttf"
+  font "NotoSerifLao-SemiBold.ttf"
+  font "NotoSerifLao-SemiCondensed.ttf"
+  font "NotoSerifLao-SemiCondensedBlack.ttf"
+  font "NotoSerifLao-SemiCondensedBold.ttf"
+  font "NotoSerifLao-SemiCondensedExtraBold.ttf"
+  font "NotoSerifLao-SemiCondensedExtraLight.ttf"
+  font "NotoSerifLao-SemiCondensedLight.ttf"
+  font "NotoSerifLao-SemiCondensedMedium.ttf"
+  font "NotoSerifLao-SemiCondensedSemiBold.ttf"
+  font "NotoSerifLao-SemiCondensedThin.ttf"
+  font "NotoSerifLao-Thin.ttf"
 end
