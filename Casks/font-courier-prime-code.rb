79a3f9de3a60efc2c874f67bf70be3e165b8f617
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:37 +0100

    font-courier-prime-code.rb: fix for new style

--- Casks/font-courier-prime-code.rb
+++ Casks/font-courier-prime-code.rb
@@ -1,11 +1,11 @@
-cask 'font-courier-prime-code' do
+cask "font-courier-prime-code" do
   version :latest
   sha256 :no_check
 
-  url 'https://quoteunquoteapps.com/courierprime/downloads/courier-prime-code.zip'
-  name 'Courier Prime Code'
-  homepage 'https://quoteunquoteapps.com/courierprime/#code-sans'
+  url "https://quoteunquoteapps.com/courierprime/downloads/courier-prime-code.zip"
+  name "Courier Prime Code"
+  homepage "https://quoteunquoteapps.com/courierprime/#code-sans"
 
-  font 'ttf/Courier Prime Code Italic.ttf'
-  font 'ttf/Courier Prime Code.ttf'
+  font "ttf/Courier Prime Code Italic.ttf"
+  font "ttf/Courier Prime Code.ttf"
 end
