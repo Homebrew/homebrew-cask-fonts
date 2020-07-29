1714047eae4ced7528bcd274f8a8c57e688aefed
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:38 +0100

    font-crafty-girls.rb: fix for new style

--- Casks/font-crafty-girls.rb
+++ Casks/font-crafty-girls.rb
@@ -1,11 +1,11 @@
-cask 'font-crafty-girls' do
+cask "font-crafty-girls" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/craftygirls/CraftyGirls-Regular.ttf'
-  name 'Crafty Girls'
-  homepage 'https://fonts.google.com/specimen/Crafty+Girls'
+  url "https://github.com/google/fonts/raw/master/apache/craftygirls/CraftyGirls-Regular.ttf"
+  name "Crafty Girls"
+  homepage "https://fonts.google.com/specimen/Crafty+Girls"
 
-  font 'CraftyGirls-Regular.ttf'
+  font "CraftyGirls-Regular.ttf"
 end
