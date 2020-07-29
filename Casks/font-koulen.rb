f9cb7eb7baaf2e465b70e75c815a8169a75c6dfe
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:29 +0100

    font-koulen.rb: fix for new style

--- Casks/font-koulen.rb
+++ Casks/font-koulen.rb
@@ -1,11 +1,11 @@
-cask 'font-koulen' do
+cask "font-koulen" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/koulen/Koulen.ttf'
-  name 'Koulen'
-  homepage 'https://fonts.google.com/specimen/Koulen'
+  url "https://github.com/google/fonts/raw/master/ofl/koulen/Koulen.ttf"
+  name "Koulen"
+  homepage "https://fonts.google.com/specimen/Koulen"
 
-  font 'Koulen.ttf'
+  font "Koulen.ttf"
 end
