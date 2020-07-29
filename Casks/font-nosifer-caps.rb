fb68e69041dd70a0993d322a3808d28af54eb1fa
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:49 +0100

    font-nosifer-caps.rb: fix for new style

--- Casks/font-nosifer-caps.rb
+++ Casks/font-nosifer-caps.rb
@@ -1,11 +1,11 @@
-cask 'font-nosifer-caps' do
+cask "font-nosifer-caps" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/nosifercaps/NosiferCaps-Regular.ttf'
-  name 'Nosifer Caps'
-  homepage 'https://fonts.google.com/specimen/Nosifer+Caps'
+  url "https://github.com/google/fonts/raw/master/ofl/nosifercaps/NosiferCaps-Regular.ttf"
+  name "Nosifer Caps"
+  homepage "https://fonts.google.com/specimen/Nosifer+Caps"
 
-  font 'NosiferCaps-Regular.ttf'
+  font "NosiferCaps-Regular.ttf"
 end
