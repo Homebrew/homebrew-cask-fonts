9b1bfac1c393c9d9d46d93448f0b5750261cf98b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:39 +0100

    font-marvel.rb: fix for new style

--- Casks/font-marvel.rb
+++ Casks/font-marvel.rb
@@ -1,18 +1,18 @@
-cask 'font-marvel' do
+cask "font-marvel" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/marvel',
+  url "https://github.com/google/fonts/trunk/ofl/marvel",
       using:      :svn,
       trust_cert: true
-  name 'Marvel'
-  homepage 'https://fonts.google.com/specimen/Marvel'
+  name "Marvel"
+  homepage "https://fonts.google.com/specimen/Marvel"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Marvel-Bold.ttf'
-  font 'Marvel-BoldItalic.ttf'
-  font 'Marvel-Italic.ttf'
-  font 'Marvel-Regular.ttf'
+  font "Marvel-Bold.ttf"
+  font "Marvel-BoldItalic.ttf"
+  font "Marvel-Italic.ttf"
+  font "Marvel-Regular.ttf"
 end
