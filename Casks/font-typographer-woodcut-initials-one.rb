9b8d4eba7ce61a40397b70bd3fd8defb560b6a26
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:26 +0100

    font-typographer-woodcut-initials-one.rb: fix for new style

--- Casks/font-typographer-woodcut-initials-one.rb
+++ Casks/font-typographer-woodcut-initials-one.rb
@@ -1,10 +1,10 @@
-cask 'font-typographer-woodcut-initials-one' do
+cask "font-typographer-woodcut-initials-one" do
   version :latest
   sha256 :no_check
 
-  url 'https://dl.1001fonts.com/typographerwoodcutinitialsone.zip'
-  name 'Typographer Woodcut Initials One'
-  homepage 'https://www.1001fonts.com/typographerwoodcutinitialsone-font.html'
+  url "https://dl.1001fonts.com/typographerwoodcutinitialsone.zip"
+  name "Typographer Woodcut Initials One"
+  homepage "https://www.1001fonts.com/typographerwoodcutinitialsone-font.html"
 
-  font 'TypographerWoodcut01.ttf'
+  font "TypographerWoodcut01.ttf"
 end
