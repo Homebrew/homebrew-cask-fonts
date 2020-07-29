91b0f09e367a6f13838ab802c159793f52f9f25c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:32:46 +0100

    font-alice.rb: fix for new style

--- Casks/font-alice.rb
+++ Casks/font-alice.rb
@@ -1,11 +1,11 @@
-cask 'font-alice' do
+cask "font-alice" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/alice/Alice-Regular.ttf'
-  name 'Alice'
-  homepage 'https://fonts.google.com/specimen/Alice'
+  url "https://github.com/google/fonts/raw/master/ofl/alice/Alice-Regular.ttf"
+  name "Alice"
+  homepage "https://fonts.google.com/specimen/Alice"
 
-  font 'Alice-Regular.ttf'
+  font "Alice-Regular.ttf"
 end
