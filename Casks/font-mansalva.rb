395eae7d5c8eb626dbf64da19f75953bb4515cb7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:39 +0100

    font-mansalva.rb: fix for new style

--- Casks/font-mansalva.rb
+++ Casks/font-mansalva.rb
@@ -1,11 +1,11 @@
-cask 'font-mansalva' do
+cask "font-mansalva" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/mansalva/Mansalva-Regular.ttf'
-  name 'Mansalva'
-  homepage 'https://fonts.google.com/specimen/Mansalva'
+  url "https://github.com/google/fonts/raw/master/ofl/mansalva/Mansalva-Regular.ttf"
+  name "Mansalva"
+  homepage "https://fonts.google.com/specimen/Mansalva"
 
-  font 'Mansalva-Regular.ttf'
+  font "Mansalva-Regular.ttf"
 end
