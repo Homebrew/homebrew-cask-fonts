952a160dc4af0f7ff8c5b914cfe9326782378bc5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:08 +0100

    font-andika.rb: fix for new style

--- Casks/font-andika.rb
+++ Casks/font-andika.rb
@@ -1,10 +1,10 @@
-cask 'font-andika' do
-  version '5.000'
-  sha256 '604b7a1194be099bdf311ef76cbce086a054fa16d2b101cfaedcf527c63df507'
+cask "font-andika" do
+  version "5.000"
+  sha256 "604b7a1194be099bdf311ef76cbce086a054fa16d2b101cfaedcf527c63df507"
 
   url "https://software.sil.org/downloads/r/andika/Andika-#{version}.zip"
-  name 'Andika'
-  homepage 'https://software.sil.org/andika/'
+  name "Andika"
+  homepage "https://software.sil.org/andika/"
 
   font "Andika-#{version}/Andika-R.ttf"
 end
