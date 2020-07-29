3f3b16056b40872067ee80548914db4209ff11e9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:14 +0100

    font-im-fell-dw-pica.rb: fix for new style

--- Casks/font-im-fell-dw-pica.rb
+++ Casks/font-im-fell-dw-pica.rb
@@ -1,16 +1,16 @@
-cask 'font-im-fell-dw-pica' do
+cask "font-im-fell-dw-pica" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/imfelldwpica',
+  url "https://github.com/google/fonts/trunk/ofl/imfelldwpica",
       using:      :svn,
       trust_cert: true
-  name 'IM Fell DW Pica'
-  homepage 'https://fonts.google.com/specimen/IM+Fell+DW+Pica'
+  name "IM Fell DW Pica"
+  homepage "https://fonts.google.com/specimen/IM+Fell+DW+Pica"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'IMFePIit28P.ttf'
-  font 'IMFePIrm28P.ttf'
+  font "IMFePIit28P.ttf"
+  font "IMFePIrm28P.ttf"
 end
