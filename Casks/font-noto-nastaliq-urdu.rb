436cd02c34cb6208984234768e665323dabf3e68
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:50 +0100

    font-noto-nastaliq-urdu.rb: fix for new style

--- Casks/font-noto-nastaliq-urdu.rb
+++ Casks/font-noto-nastaliq-urdu.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-nastaliq-urdu' do
+cask "font-noto-nastaliq-urdu" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoNastaliqUrdu-unhinted.zip'
-  name 'Noto Nastaliq Urdu'
-  homepage 'https://www.google.com/get/noto/#nastaliq-aran'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoNastaliqUrdu-unhinted.zip"
+  name "Noto Nastaliq Urdu"
+  homepage "https://www.google.com/get/noto/#nastaliq-aran"
 
-  font 'NotoNastaliqUrdu-Regular.ttf'
+  font "NotoNastaliqUrdu-Regular.ttf"
 end
