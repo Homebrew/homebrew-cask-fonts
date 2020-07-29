2bec0fac6be9fc756e139db80e19af988cf24488
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:34 +0100

    font-liberation.rb: fix for new style

--- Casks/font-liberation.rb
+++ Casks/font-liberation.rb
@@ -1,11 +1,11 @@
-cask 'font-liberation' do
-  version '2.1.1,4743886'
-  sha256 '8ee2c16fe0f055e60dd8375449aff72d25dd623b9cc6f24790ce9d2e91446fca'
+cask "font-liberation" do
+  version "2.1.1,4743886"
+  sha256 "8ee2c16fe0f055e60dd8375449aff72d25dd623b9cc6f24790ce9d2e91446fca"
 
   url "https://github.com/liberationfonts/liberation-fonts/files/#{version.after_comma}/liberation-fonts-ttf-#{version.before_comma}.tar.gz"
-  appcast 'https://github.com/liberationfonts/liberation-fonts/releases.atom'
-  name 'Liberation'
-  homepage 'https://github.com/liberationfonts/liberation-fonts'
+  appcast "https://github.com/liberationfonts/liberation-fonts/releases.atom"
+  name "Liberation"
+  homepage "https://github.com/liberationfonts/liberation-fonts"
 
   font "liberation-fonts-ttf-#{version.before_comma}/LiberationMono-Bold.ttf"
   font "liberation-fonts-ttf-#{version.before_comma}/LiberationMono-BoldItalic.ttf"
