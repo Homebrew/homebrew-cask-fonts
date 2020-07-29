a6c4f6cf0a7b85d365bff9e8e19d6218672eeb7a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:34 +0100

    font-comic-sans-ms.rb: fix for new style

--- Casks/font-comic-sans-ms.rb
+++ Casks/font-comic-sans-ms.rb
@@ -1,12 +1,12 @@
-cask 'font-comic-sans-ms' do
-  version '2.10'
-  sha256 '9c6df3feefde26d4e41d4a4fe5db2a89f9123a772594d7f59afd062625cd204e'
+cask "font-comic-sans-ms" do
+  version "2.10"
+  sha256 "9c6df3feefde26d4e41d4a4fe5db2a89f9123a772594d7f59afd062625cd204e"
 
-  url 'https://downloads.sourceforge.net/corefonts/comic32.exe'
-  appcast 'https://sourceforge.net/projects/corefonts/rss'
-  name 'Comic Sans'
-  homepage 'https://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  url "https://downloads.sourceforge.net/corefonts/comic32.exe"
+  appcast "https://sourceforge.net/projects/corefonts/rss"
+  name "Comic Sans"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
 
-  font 'Comicbd.TTF'
-  font 'Comic.TTF'
+  font "Comicbd.TTF"
+  font "Comic.TTF"
 end
