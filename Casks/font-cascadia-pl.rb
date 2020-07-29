7edd7ce171c0c486ed1350e55db5fb25c4891909
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:28 +0100

    font-cascadia-pl.rb: fix for new style

--- Casks/font-cascadia-pl.rb
+++ Casks/font-cascadia-pl.rb
@@ -1,11 +1,11 @@
-cask 'font-cascadia-pl' do
-  version '2007.01'
-  sha256 '9f066d0d9cb2669bea2e130d7add43d496bf24ef995f42dc603fc2014574a3a4'
+cask "font-cascadia-pl" do
+  version "2007.01"
+  sha256 "9f066d0d9cb2669bea2e130d7add43d496bf24ef995f42dc603fc2014574a3a4"
 
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
-  appcast 'https://github.com/microsoft/cascadia-code/releases.atom'
-  name 'Cascadia PL'
-  homepage 'https://github.com/microsoft/cascadia-code'
+  appcast "https://github.com/microsoft/cascadia-code/releases.atom"
+  name "Cascadia PL"
+  homepage "https://github.com/microsoft/cascadia-code"
 
-  font 'ttf/CascadiaCodePL.ttf'
+  font "ttf/CascadiaCodePL.ttf"
 end
