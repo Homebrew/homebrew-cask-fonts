989661cc63832e639cc52076cff964faae274b4f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:21 +0100

    font-strong.rb: fix for new style

--- Casks/font-strong.rb
+++ Casks/font-strong.rb
@@ -1,11 +1,11 @@
-cask 'font-strong' do
+cask "font-strong" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/strong/Strong-Regular.ttf'
-  name 'Strong'
-  homepage 'https://fonts.google.com/specimen/Strong'
+  url "https://github.com/google/fonts/raw/master/ofl/strong/Strong-Regular.ttf"
+  name "Strong"
+  homepage "https://fonts.google.com/specimen/Strong"
 
-  font 'Strong-Regular.ttf'
+  font "Strong-Regular.ttf"
 end
