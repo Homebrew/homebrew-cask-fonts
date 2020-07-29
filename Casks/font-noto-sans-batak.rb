329eee5e5cd7c668277abb227efbe02edd45e506
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:51 +0100

    font-noto-sans-batak.rb: fix for new style

--- Casks/font-noto-sans-batak.rb
+++ Casks/font-noto-sans-batak.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-batak' do
+cask "font-noto-sans-batak" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBatak-unhinted.zip'
-  name 'Noto Sans Batak'
-  homepage 'https://www.google.com/get/noto/#sans-batk'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBatak-unhinted.zip"
+  name "Noto Sans Batak"
+  homepage "https://www.google.com/get/noto/#sans-batk"
 
-  font 'NotoSansBatak-Regular.ttf'
+  font "NotoSansBatak-Regular.ttf"
 end
