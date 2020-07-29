afd0fe53a29148f07d658967b3a50afbcea61445
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:35 +0100

    font-linux-biolinum.rb: fix for new style

--- Casks/font-linux-biolinum.rb
+++ Casks/font-linux-biolinum.rb
@@ -1,15 +1,15 @@
-cask 'font-linux-biolinum' do
-  version '5.3.0_2012_07_02'
-  sha256 '24a593a949808d976850131a953c0c0d7a72299531dfbb348191964cc038d75d'
+cask "font-linux-biolinum" do
+  version "5.3.0_2012_07_02"
+  sha256 "24a593a949808d976850131a953c0c0d7a72299531dfbb348191964cc038d75d"
 
   # downloads.sourceforge.net/linuxlibertine/ was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/linuxlibertine/LinLibertineTTF_#{version}.tgz"
-  appcast 'https://sourceforge.net/projects/linuxlibertine/rss'
-  name 'Linux Biolinum'
-  homepage 'http://linuxlibertine.org/'
+  appcast "https://sourceforge.net/projects/linuxlibertine/rss"
+  name "Linux Biolinum"
+  homepage "http://linuxlibertine.org/"
 
-  font 'LinBiolinum_Kah.ttf'
-  font 'LinBiolinum_RBah.ttf'
-  font 'LinBiolinum_RIah.ttf'
-  font 'LinBiolinum_Rah.ttf'
+  font "LinBiolinum_Kah.ttf"
+  font "LinBiolinum_RBah.ttf"
+  font "LinBiolinum_RIah.ttf"
+  font "LinBiolinum_Rah.ttf"
 end
