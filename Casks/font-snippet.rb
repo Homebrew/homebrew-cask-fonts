25ceb1c65bd2eec6d5a350be34c91cd8ab99dced
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:18 +0100

    font-snippet.rb: fix for new style

--- Casks/font-snippet.rb
+++ Casks/font-snippet.rb
@@ -1,11 +1,11 @@
-cask 'font-snippet' do
+cask "font-snippet" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/snippet/Snippet.ttf'
-  name 'Snippet'
-  homepage 'https://fonts.google.com/specimen/Snippet'
+  url "https://github.com/google/fonts/raw/master/ofl/snippet/Snippet.ttf"
+  name "Snippet"
+  homepage "https://fonts.google.com/specimen/Snippet"
 
-  font 'Snippet.ttf'
+  font "Snippet.ttf"
 end
