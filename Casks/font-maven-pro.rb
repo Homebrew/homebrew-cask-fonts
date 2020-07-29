9daf5c54c3ef2d004185a31b86a9b63f7d384cfb
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:40 +0100

    font-maven-pro.rb: fix for new style

--- Casks/font-maven-pro.rb
+++ Casks/font-maven-pro.rb
@@ -1,11 +1,11 @@
-cask 'font-maven-pro' do
+cask "font-maven-pro" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/mavenpro/MavenPro%5Bwght%5D.ttf'
-  name 'Maven Pro'
-  homepage 'https://fonts.google.com/specimen/Maven+Pro'
+  url "https://github.com/google/fonts/raw/master/ofl/mavenpro/MavenPro%5Bwght%5D.ttf"
+  name "Maven Pro"
+  homepage "https://fonts.google.com/specimen/Maven+Pro"
 
-  font 'MavenPro[wght].ttf'
+  font "MavenPro[wght].ttf"
 end
