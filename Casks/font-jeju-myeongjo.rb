2e1d1d0ed8ab1cbb7fac2ab339495846fa1cf2db
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:22 +0100

    font-jeju-myeongjo.rb: fix for new style

--- Casks/font-jeju-myeongjo.rb
+++ Casks/font-jeju-myeongjo.rb
@@ -1,11 +1,11 @@
-cask 'font-jeju-myeongjo' do
+cask "font-jeju-myeongjo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/jejumyeongjo/JejuMyeongjo-Regular.ttf'
-  name 'Jeju Myeongjo'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/jejumyeongjo/JejuMyeongjo-Regular.ttf"
+  name "Jeju Myeongjo"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'JejuMyeongjo-Regular.ttf'
+  font "JejuMyeongjo-Regular.ttf"
 end
