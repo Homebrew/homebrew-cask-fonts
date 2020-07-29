1a59bf522b4306a21d9e09a166af9df0c20e4d33
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:26 +0100

    font-ubuntu-condensed.rb: fix for new style

--- Casks/font-ubuntu-condensed.rb
+++ Casks/font-ubuntu-condensed.rb
@@ -1,11 +1,11 @@
-cask 'font-ubuntu-condensed' do
+cask "font-ubuntu-condensed" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ufl/ubuntucondensed/UbuntuCondensed-Regular.ttf'
-  name 'Ubuntu Condensed'
-  homepage 'https://fonts.google.com/specimen/Ubuntu+Condensed'
+  url "https://github.com/google/fonts/raw/master/ufl/ubuntucondensed/UbuntuCondensed-Regular.ttf"
+  name "Ubuntu Condensed"
+  homepage "https://fonts.google.com/specimen/Ubuntu+Condensed"
 
-  font 'UbuntuCondensed-Regular.ttf'
+  font "UbuntuCondensed-Regular.ttf"
 end
