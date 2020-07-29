29f79da2cb51e43b79b1fc6011e2d9a099e53cd1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:18 +0100

    font-iosevka-aile.rb: fix for new style

--- Casks/font-iosevka-aile.rb
+++ Casks/font-iosevka-aile.rb
@@ -1,19 +1,19 @@
-cask 'font-iosevka-aile' do
-  version '3.3.1'
-  sha256 '30e9c2e13c440a36cf9e4c6fdbac6bbe2cd4b0b177b64167821b21cae2ee2de7'
+cask "font-iosevka-aile" do
+  version "3.3.1"
+  sha256 "30e9c2e13c440a36cf9e4c6fdbac6bbe2cd4b0b177b64167821b21cae2ee2de7"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-aile-#{version}.zip"
-  appcast 'https://github.com/be5invis/Iosevka/releases.atom'
-  name 'Iosevka Aile'
-  homepage 'https://github.com/be5invis/Iosevka/'
+  appcast "https://github.com/be5invis/Iosevka/releases.atom"
+  name "Iosevka Aile"
+  homepage "https://github.com/be5invis/Iosevka/"
 
-  font 'iosevka-aile-bold.ttc'
-  font 'iosevka-aile-extrabold.ttc'
-  font 'iosevka-aile-extralight.ttc'
-  font 'iosevka-aile-heavy.ttc'
-  font 'iosevka-aile-light.ttc'
-  font 'iosevka-aile-medium.ttc'
-  font 'iosevka-aile-regular.ttc'
-  font 'iosevka-aile-semibold.ttc'
-  font 'iosevka-aile-thin.ttc'
+  font "iosevka-aile-bold.ttc"
+  font "iosevka-aile-extrabold.ttc"
+  font "iosevka-aile-extralight.ttc"
+  font "iosevka-aile-heavy.ttc"
+  font "iosevka-aile-light.ttc"
+  font "iosevka-aile-medium.ttc"
+  font "iosevka-aile-regular.ttc"
+  font "iosevka-aile-semibold.ttc"
+  font "iosevka-aile-thin.ttc"
 end
