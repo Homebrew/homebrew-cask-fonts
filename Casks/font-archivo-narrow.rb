4e8314158619941081860ff4dea9d8c055507770
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:04 +0100

    font-archivo-narrow.rb: fix for new style

--- Casks/font-archivo-narrow.rb
+++ Casks/font-archivo-narrow.rb
@@ -1,16 +1,16 @@
-cask 'font-archivo-narrow' do
+cask "font-archivo-narrow" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/archivonarrow',
+  url "https://github.com/google/fonts/trunk/ofl/archivonarrow",
       using:      :svn,
       trust_cert: true
-  name 'Archivo Narrow'
-  homepage 'https://fonts.google.com/specimen/Archivo+Narrow'
+  name "Archivo Narrow"
+  homepage "https://fonts.google.com/specimen/Archivo+Narrow"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'ArchivoNarrow-Italic[wght].ttf'
-  font 'ArchivoNarrow[wght].ttf'
+  font "ArchivoNarrow-Italic[wght].ttf"
+  font "ArchivoNarrow[wght].ttf"
 end
