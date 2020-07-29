2933be15034021f53892da06ee1627b8ed23e124
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:05 +0100

    font-gotu.rb: fix for new style

--- Casks/font-gotu.rb
+++ Casks/font-gotu.rb
@@ -1,11 +1,11 @@
-cask 'font-gotu' do
+cask "font-gotu" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/gotu/Gotu-Regular.ttf'
-  name 'Gotu'
-  homepage 'https://fonts.google.com/specimen/Gotu'
+  url "https://github.com/google/fonts/raw/master/ofl/gotu/Gotu-Regular.ttf"
+  name "Gotu"
+  homepage "https://fonts.google.com/specimen/Gotu"
 
-  font 'Gotu-Regular.ttf'
+  font "Gotu-Regular.ttf"
 end
