c7d21f2849201db77b8b6568398277edc67e7a7c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:54 +0100

    font-andada.rb: fix for new style

--- Casks/font-andada.rb
+++ Casks/font-andada.rb
@@ -1,11 +1,11 @@
-cask 'font-andada' do
+cask "font-andada" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/andada/Andada-Regular.ttf'
-  name 'Andada'
-  homepage 'https://fonts.google.com/specimen/Andada'
+  url "https://github.com/google/fonts/raw/master/ofl/andada/Andada-Regular.ttf"
+  name "Andada"
+  homepage "https://fonts.google.com/specimen/Andada"
 
-  font 'Andada-Regular.ttf'
+  font "Andada-Regular.ttf"
 end
