e8bf5f6ce7b28d1254f11d91324dd132fb1778ad
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:28 +0100

    font-khyay.rb: fix for new style

--- Casks/font-khyay.rb
+++ Casks/font-khyay.rb
@@ -1,11 +1,11 @@
-cask 'font-khyay' do
+cask "font-khyay" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/khyay/Khyay-Regular.ttf'
-  name 'Khyay'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/khyay/Khyay-Regular.ttf"
+  name "Khyay"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'Khyay-Regular.ttf'
+  font "Khyay-Regular.ttf"
 end
