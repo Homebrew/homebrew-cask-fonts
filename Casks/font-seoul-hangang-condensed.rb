c7d1ce4a00688ff1a105a8027cef709cdf03117f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:16 +0100

    font-seoul-hangang-condensed.rb: fix for new style

--- Casks/font-seoul-hangang-condensed.rb
+++ Casks/font-seoul-hangang-condensed.rb
@@ -1,19 +1,19 @@
-cask 'font-seoul-hangang-condensed' do
+cask "font-seoul-hangang-condensed" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/seoulhangangcondensed',
+  url "https://github.com/google/fonts/trunk/ofl/seoulhangangcondensed",
       using:      :svn,
       trust_cert: true
-  name 'Seoul Hangang Condensed'
-  homepage 'https://fonts.google.com/specimen/Seoul+Hangang+Condensed'
+  name "Seoul Hangang Condensed"
+  homepage "https://fonts.google.com/specimen/Seoul+Hangang+Condensed"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'SeoulHangangCondensed-Bold.ttf'
-  font 'SeoulHangangCondensed-BoldL.ttf'
-  font 'SeoulHangangCondensed-ExtraBold.ttf'
-  font 'SeoulHangangCondensed-Light.ttf'
-  font 'SeoulHangangCondensed-Medium.ttf'
+  font "SeoulHangangCondensed-Bold.ttf"
+  font "SeoulHangangCondensed-BoldL.ttf"
+  font "SeoulHangangCondensed-ExtraBold.ttf"
+  font "SeoulHangangCondensed-Light.ttf"
+  font "SeoulHangangCondensed-Medium.ttf"
 end
