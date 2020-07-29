93de659f8e66d6a2062f5649e8fa1bacfb8f7a02
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:21 +0100

    font-bm.rb: fix for new style

--- Casks/font-bm.rb
+++ Casks/font-bm.rb
@@ -1,11 +1,11 @@
-cask 'font-bm' do
+cask "font-bm" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/hanna/BM-HANNA.ttf'
-  name 'BM'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/hanna/BM-HANNA.ttf"
+  name "BM"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'BM-HANNA.ttf'
+  font "BM-HANNA.ttf"
 end
