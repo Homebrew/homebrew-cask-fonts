2b2349b69634628da73672c655cd83354f5d0156
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:54 +0100

    font-noto-sans-limbu.rb: fix for new style

--- Casks/font-noto-sans-limbu.rb
+++ Casks/font-noto-sans-limbu.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-limbu' do
+cask "font-noto-sans-limbu" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLimbu-unhinted.zip'
-  name 'Noto Sans Limbu'
-  homepage 'https://www.google.com/get/noto/#sans-limb'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansLimbu-unhinted.zip"
+  name "Noto Sans Limbu"
+  homepage "https://www.google.com/get/noto/#sans-limb"
 
-  font 'NotoSansLimbu-Regular.ttf'
+  font "NotoSansLimbu-Regular.ttf"
 end
