0fae21846cf1a1063297675a0f2bcaf340fe0218
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:35 +0100

    font-limelight.rb: fix for new style

--- Casks/font-limelight.rb
+++ Casks/font-limelight.rb
@@ -1,11 +1,11 @@
-cask 'font-limelight' do
+cask "font-limelight" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/limelight/Limelight-Regular.ttf'
-  name 'Limelight'
-  homepage 'https://fonts.google.com/specimen/Limelight'
+  url "https://github.com/google/fonts/raw/master/ofl/limelight/Limelight-Regular.ttf"
+  name "Limelight"
+  homepage "https://fonts.google.com/specimen/Limelight"
 
-  font 'Limelight-Regular.ttf'
+  font "Limelight-Regular.ttf"
 end
