b82d67efffb687d9e233a7683b1c426eb01115be
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:33 +0100

    font-code2002.rb: fix for new style

--- Casks/font-code2002.rb
+++ Casks/font-code2002.rb
@@ -1,10 +1,10 @@
-cask 'font-code2002' do
+cask "font-code2002" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.ffonts.net/Code2002.font.zip'
-  name 'Code2002'
-  homepage 'https://www.ffonts.net/Code2002.font.download'
+  url "https://www.ffonts.net/Code2002.font.zip"
+  name "Code2002"
+  homepage "https://www.ffonts.net/Code2002.font.download"
 
-  font 'CODE2002.TTF'
+  font "CODE2002.TTF"
 end
