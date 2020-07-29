73496d4e02fffed17b77c74eebecd35b4c14d15f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:51 +0100

    font-fantasquesansmono-nerd-font.rb: fix for new style

--- Casks/font-fantasquesansmono-nerd-font.rb
+++ Casks/font-fantasquesansmono-nerd-font.rb
@@ -1,18 +1,18 @@
-cask 'font-fantasquesansmono-nerd-font' do
-  version '2.1.0'
-  sha256 '66bf67eb5031c1614e5f76b4e011860dd249a61350e5d921331c443b6609f090'
+cask "font-fantasquesansmono-nerd-font" do
+  version "2.1.0"
+  sha256 "66bf67eb5031c1614e5f76b4e011860dd249a61350e5d921331c443b6609f090"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FantasqueSansMono.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'FantasqueSansMono Nerd Font (Fantasque Sans Mono)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "FantasqueSansMono Nerd Font (Fantasque Sans Mono)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Fantasque Sans Mono Bold Nerd Font Complete Mono.ttf'
-  font 'Fantasque Sans Mono Bold Nerd Font Complete.ttf'
-  font 'Fantasque Sans Mono Bold Italic Nerd Font Complete Mono.ttf'
-  font 'Fantasque Sans Mono Bold Italic Nerd Font Complete.ttf'
-  font 'Fantasque Sans Mono Regular Nerd Font Complete Mono.ttf'
-  font 'Fantasque Sans Mono Regular Nerd Font Complete.ttf'
-  font 'Fantasque Sans Mono Italic Nerd Font Complete Mono.ttf'
-  font 'Fantasque Sans Mono Italic Nerd Font Complete.ttf'
+  font "Fantasque Sans Mono Bold Nerd Font Complete Mono.ttf"
+  font "Fantasque Sans Mono Bold Nerd Font Complete.ttf"
+  font "Fantasque Sans Mono Bold Italic Nerd Font Complete Mono.ttf"
+  font "Fantasque Sans Mono Bold Italic Nerd Font Complete.ttf"
+  font "Fantasque Sans Mono Regular Nerd Font Complete Mono.ttf"
+  font "Fantasque Sans Mono Regular Nerd Font Complete.ttf"
+  font "Fantasque Sans Mono Italic Nerd Font Complete Mono.ttf"
+  font "Fantasque Sans Mono Italic Nerd Font Complete.ttf"
 end
