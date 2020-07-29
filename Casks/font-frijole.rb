a24c2fd306dc74db1dbc1c41ff59e45676fc183b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:57 +0100

    font-frijole.rb: fix for new style

--- Casks/font-frijole.rb
+++ Casks/font-frijole.rb
@@ -1,11 +1,11 @@
-cask 'font-frijole' do
+cask "font-frijole" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/frijole/Frijole-Regular.ttf'
-  name 'Frijole'
-  homepage 'https://fonts.google.com/specimen/Frijole'
+  url "https://github.com/google/fonts/raw/master/ofl/frijole/Frijole-Regular.ttf"
+  name "Frijole"
+  homepage "https://fonts.google.com/specimen/Frijole"
 
-  font 'Frijole-Regular.ttf'
+  font "Frijole-Regular.ttf"
 end
