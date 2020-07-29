894482f85e4a4a059fb2b4772350af0522183c5d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:40 +0100

    font-arsenal.rb: fix for new style

--- Casks/font-arsenal.rb
+++ Casks/font-arsenal.rb
@@ -1,18 +1,18 @@
-cask 'font-arsenal' do
+cask "font-arsenal" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/arsenal',
+  url "https://github.com/google/fonts/trunk/ofl/arsenal",
       using:      :svn,
       trust_cert: true
-  name 'Arsenal'
-  homepage 'https://fonts.google.com/specimen/Arsenal'
+  name "Arsenal"
+  homepage "https://fonts.google.com/specimen/Arsenal"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Arsenal-Bold.ttf'
-  font 'Arsenal-BoldItalic.ttf'
-  font 'Arsenal-Italic.ttf'
-  font 'Arsenal-Regular.ttf'
+  font "Arsenal-Bold.ttf"
+  font "Arsenal-BoldItalic.ttf"
+  font "Arsenal-Italic.ttf"
+  font "Arsenal-Regular.ttf"
 end
