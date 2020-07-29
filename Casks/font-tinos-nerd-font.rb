11c56bd3f510e6aa34ac795be13a9957a62158c7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:25 +0100

    font-tinos-nerd-font.rb: fix for new style

--- Casks/font-tinos-nerd-font.rb
+++ Casks/font-tinos-nerd-font.rb
@@ -1,18 +1,18 @@
-cask 'font-tinos-nerd-font' do
-  version '2.1.0'
-  sha256 '4d5e33948bbb51e2a65bec8390afae7e9e0286c55f003a5d8e0b13e8cd24f61c'
+cask "font-tinos-nerd-font" do
+  version "2.1.0"
+  sha256 "4d5e33948bbb51e2a65bec8390afae7e9e0286c55f003a5d8e0b13e8cd24f61c"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Tinos.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'Tinos Nerd Font (Tinos)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "Tinos Nerd Font (Tinos)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Tinos Nerd Font Complete.ttf'
-  font 'Tinos Italic Nerd Font Complete.ttf'
-  font 'Tinos Bold Nerd Font Complete.ttf'
-  font 'Tinos Bold Italic Nerd Font Complete.ttf'
-  font 'Tinos Nerd Font Complete Mono.ttf'
-  font 'Tinos Italic Nerd Font Complete Mono.ttf'
-  font 'Tinos Bold Nerd Font Complete Mono.ttf'
-  font 'Tinos Bold Italic Nerd Font Complete Mono.ttf'
+  font "Tinos Nerd Font Complete.ttf"
+  font "Tinos Italic Nerd Font Complete.ttf"
+  font "Tinos Bold Nerd Font Complete.ttf"
+  font "Tinos Bold Italic Nerd Font Complete.ttf"
+  font "Tinos Nerd Font Complete Mono.ttf"
+  font "Tinos Italic Nerd Font Complete Mono.ttf"
+  font "Tinos Bold Nerd Font Complete Mono.ttf"
+  font "Tinos Bold Italic Nerd Font Complete Mono.ttf"
 end
