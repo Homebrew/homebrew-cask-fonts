773efd36d24cb0c560273d38980e499ccce51352
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:27 +0100

    font-arimo.rb: fix for new style

--- Casks/font-arimo.rb
+++ Casks/font-arimo.rb
@@ -1,18 +1,18 @@
-cask 'font-arimo' do
+cask "font-arimo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/apache/arimo',
+  url "https://github.com/google/fonts/trunk/apache/arimo",
       using:      :svn,
       trust_cert: true
-  name 'Arimo'
-  homepage 'https://fonts.google.com/specimen/Arimo'
+  name "Arimo"
+  homepage "https://fonts.google.com/specimen/Arimo"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Arimo-Bold.ttf'
-  font 'Arimo-BoldItalic.ttf'
-  font 'Arimo-Italic.ttf'
-  font 'Arimo-Regular.ttf'
+  font "Arimo-Bold.ttf"
+  font "Arimo-BoldItalic.ttf"
+  font "Arimo-Italic.ttf"
+  font "Arimo-Regular.ttf"
 end
