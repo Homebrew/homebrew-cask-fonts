010495893f856a9741d2b0b2d27a50a5b78cf542
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:22 +0100

    font-syncopate.rb: fix for new style

--- Casks/font-syncopate.rb
+++ Casks/font-syncopate.rb
@@ -1,16 +1,16 @@
-cask 'font-syncopate' do
+cask "font-syncopate" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/apache/syncopate',
+  url "https://github.com/google/fonts/trunk/apache/syncopate",
       using:      :svn,
       trust_cert: true
-  name 'Syncopate'
-  homepage 'https://fonts.google.com/specimen/Syncopate'
+  name "Syncopate"
+  homepage "https://fonts.google.com/specimen/Syncopate"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Syncopate-Bold.ttf'
-  font 'Syncopate-Regular.ttf'
+  font "Syncopate-Bold.ttf"
+  font "Syncopate-Regular.ttf"
 end
