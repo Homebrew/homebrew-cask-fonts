745d420b9247398be942e5c249aba334a47180ed
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:09 +0100

    font-heavydata-nerd-font.rb: fix for new style

--- Casks/font-heavydata-nerd-font.rb
+++ Casks/font-heavydata-nerd-font.rb
@@ -1,12 +1,12 @@
-cask 'font-heavydata-nerd-font' do
-  version '2.1.0'
-  sha256 'cbe30eb404ac2f6460b6afcec0812e2dffc2f8d77b701b4310b789f20f86b918'
+cask "font-heavydata-nerd-font" do
+  version "2.1.0"
+  sha256 "cbe30eb404ac2f6460b6afcec0812e2dffc2f8d77b701b4310b789f20f86b918"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/HeavyData.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'HeavyData Nerd Font (Heavy Data)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "HeavyData Nerd Font (Heavy Data)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Heavy Data Nerd Font Complete.ttf'
-  font 'Heavy Data Nerd Font Complete Mono.ttf'
+  font "Heavy Data Nerd Font Complete.ttf"
+  font "Heavy Data Nerd Font Complete Mono.ttf"
 end
