cc9c6007b7aaaec1b1d19e69f0a221a7325948bf
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:04 +0100

    font-patrick-hand.rb: fix for new style

--- Casks/font-patrick-hand.rb
+++ Casks/font-patrick-hand.rb
@@ -1,11 +1,11 @@
-cask 'font-patrick-hand' do
+cask "font-patrick-hand" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/patrickhand/PatrickHand-Regular.ttf'
-  name 'Patrick Hand'
-  homepage 'https://fonts.google.com/specimen/Patrick+Hand'
+  url "https://github.com/google/fonts/raw/master/ofl/patrickhand/PatrickHand-Regular.ttf"
+  name "Patrick Hand"
+  homepage "https://fonts.google.com/specimen/Patrick+Hand"
 
-  font 'PatrickHand-Regular.ttf'
+  font "PatrickHand-Regular.ttf"
 end
