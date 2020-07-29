923c10baf30bafb68a4dea8bd64550f663a4cfd3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:36 +0100

    font-londrina-sketch.rb: fix for new style

--- Casks/font-londrina-sketch.rb
+++ Casks/font-londrina-sketch.rb
@@ -1,11 +1,11 @@
-cask 'font-londrina-sketch' do
+cask "font-londrina-sketch" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/londrinasketch/LondrinaSketch-Regular.ttf'
-  name 'Londrina Sketch'
-  homepage 'https://fonts.google.com/specimen/Londrina+Sketch'
+  url "https://github.com/google/fonts/raw/master/ofl/londrinasketch/LondrinaSketch-Regular.ttf"
+  name "Londrina Sketch"
+  homepage "https://fonts.google.com/specimen/Londrina+Sketch"
 
-  font 'LondrinaSketch-Regular.ttf'
+  font "LondrinaSketch-Regular.ttf"
 end
