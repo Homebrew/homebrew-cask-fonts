67d704f0fa0d81333702ed28be5fcc3d32ccc3c9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:25 +0100

    font-trochut.rb: fix for new style

--- Casks/font-trochut.rb
+++ Casks/font-trochut.rb
@@ -1,17 +1,17 @@
-cask 'font-trochut' do
+cask "font-trochut" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/trochut',
+  url "https://github.com/google/fonts/trunk/ofl/trochut",
       using:      :svn,
       trust_cert: true
-  name 'Trochut'
-  homepage 'https://fonts.google.com/specimen/Trochut'
+  name "Trochut"
+  homepage "https://fonts.google.com/specimen/Trochut"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Trochut-Bold.ttf'
-  font 'Trochut-Italic.ttf'
-  font 'Trochut-Regular.ttf'
+  font "Trochut-Bold.ttf"
+  font "Trochut-Italic.ttf"
+  font "Trochut-Regular.ttf"
 end
