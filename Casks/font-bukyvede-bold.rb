976534aa248989349e5f62b40a7d2000cd4c25ce
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:23 +0100

    font-bukyvede-bold.rb: fix for new style

--- Casks/font-bukyvede-bold.rb
+++ Casks/font-bukyvede-bold.rb
@@ -1,10 +1,10 @@
-cask 'font-bukyvede-bold' do
+cask "font-bukyvede-bold" do
   version :latest
   sha256 :no_check
 
-  url 'https://kodeks.uni-bamberg.de/aksl/media/BukyVede-Bold.ttf'
-  name 'BukyVede Bold'
-  homepage 'https://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm'
+  url "https://kodeks.uni-bamberg.de/aksl/media/BukyVede-Bold.ttf"
+  name "BukyVede Bold"
+  homepage "https://kodeks.uni-bamberg.de/aksl/Schrift/BukyVede.htm"
 
-  font 'BukyVede-Bold.ttf'
+  font "BukyVede-Bold.ttf"
 end
