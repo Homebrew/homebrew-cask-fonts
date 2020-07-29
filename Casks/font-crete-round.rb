b067b35eb807aac7fdaa649ea7aa57cf99bd5478
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:38 +0100

    font-crete-round.rb: fix for new style

--- Casks/font-crete-round.rb
+++ Casks/font-crete-round.rb
@@ -1,16 +1,16 @@
-cask 'font-crete-round' do
+cask "font-crete-round" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/creteround',
+  url "https://github.com/google/fonts/trunk/ofl/creteround",
       using:      :svn,
       trust_cert: true
-  name 'Crete Round'
-  homepage 'https://fonts.google.com/specimen/Crete+Round'
+  name "Crete Round"
+  homepage "https://fonts.google.com/specimen/Crete+Round"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'CreteRound-Italic.ttf'
-  font 'CreteRound-Regular.ttf'
+  font "CreteRound-Italic.ttf"
+  font "CreteRound-Regular.ttf"
 end
