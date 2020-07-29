982587ca2c6bf50817a4401b0008b844ed2c96d4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:49 +0100

    font-enriqueta.rb: fix for new style

--- Casks/font-enriqueta.rb
+++ Casks/font-enriqueta.rb
@@ -1,18 +1,18 @@
-cask 'font-enriqueta' do
+cask "font-enriqueta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/enriqueta',
+  url "https://github.com/google/fonts/trunk/ofl/enriqueta",
       using:      :svn,
       trust_cert: true
-  name 'Enriqueta'
-  homepage 'https://fonts.google.com/specimen/Enriqueta'
+  name "Enriqueta"
+  homepage "https://fonts.google.com/specimen/Enriqueta"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Enriqueta-Bold.ttf'
-  font 'Enriqueta-Medium.ttf'
-  font 'Enriqueta-Regular.ttf'
-  font 'Enriqueta-SemiBold.ttf'
+  font "Enriqueta-Bold.ttf"
+  font "Enriqueta-Medium.ttf"
+  font "Enriqueta-Regular.ttf"
+  font "Enriqueta-SemiBold.ttf"
 end
