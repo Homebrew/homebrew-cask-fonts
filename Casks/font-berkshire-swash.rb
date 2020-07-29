c5a4fa4014af48618e16b0f7aa513a7caae37818
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:18 +0100

    font-berkshire-swash.rb: fix for new style

--- Casks/font-berkshire-swash.rb
+++ Casks/font-berkshire-swash.rb
@@ -1,11 +1,11 @@
-cask 'font-berkshire-swash' do
+cask "font-berkshire-swash" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/berkshireswash/BerkshireSwash-Regular.ttf'
-  name 'Berkshire Swash'
-  homepage 'https://fonts.google.com/specimen/Berkshire+Swash'
+  url "https://github.com/google/fonts/raw/master/ofl/berkshireswash/BerkshireSwash-Regular.ttf"
+  name "Berkshire Swash"
+  homepage "https://fonts.google.com/specimen/Berkshire+Swash"
 
-  font 'BerkshireSwash-Regular.ttf'
+  font "BerkshireSwash-Regular.ttf"
 end
