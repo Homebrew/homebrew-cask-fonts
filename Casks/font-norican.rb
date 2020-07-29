01a5274b1d53b1119b2ef8e7d8251fb66e7e9c4c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:49 +0100

    font-norican.rb: fix for new style

--- Casks/font-norican.rb
+++ Casks/font-norican.rb
@@ -1,11 +1,11 @@
-cask 'font-norican' do
+cask "font-norican" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/norican/Norican-Regular.ttf'
-  name 'Norican'
-  homepage 'https://fonts.google.com/specimen/Norican'
+  url "https://github.com/google/fonts/raw/master/ofl/norican/Norican-Regular.ttf"
+  name "Norican"
+  homepage "https://fonts.google.com/specimen/Norican"
 
-  font 'Norican-Regular.ttf'
+  font "Norican-Regular.ttf"
 end
