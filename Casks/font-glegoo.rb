70ea65138cbb4f81d135825d332c2061c7bf5a1d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:02 +0100

    font-glegoo.rb: fix for new style

--- Casks/font-glegoo.rb
+++ Casks/font-glegoo.rb
@@ -1,16 +1,16 @@
-cask 'font-glegoo' do
+cask "font-glegoo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/glegoo',
+  url "https://github.com/google/fonts/trunk/ofl/glegoo",
       using:      :svn,
       trust_cert: true
-  name 'Glegoo'
-  homepage 'https://fonts.google.com/specimen/Glegoo'
+  name "Glegoo"
+  homepage "https://fonts.google.com/specimen/Glegoo"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Glegoo-Bold.ttf'
-  font 'Glegoo-Regular.ttf'
+  font "Glegoo-Bold.ttf"
+  font "Glegoo-Regular.ttf"
 end
