1b42feb0c22ae19a3b6a772896b9e52f33f26a0f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:30 +0100

    font-la-belle-aurore.rb: fix for new style

--- Casks/font-la-belle-aurore.rb
+++ Casks/font-la-belle-aurore.rb
@@ -1,11 +1,11 @@
-cask 'font-la-belle-aurore' do
+cask "font-la-belle-aurore" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/labelleaurore/LaBelleAurore.ttf'
-  name 'La Belle Aurore'
-  homepage 'https://fonts.google.com/specimen/La+Belle+Aurore'
+  url "https://github.com/google/fonts/raw/master/ofl/labelleaurore/LaBelleAurore.ttf"
+  name "La Belle Aurore"
+  homepage "https://fonts.google.com/specimen/La+Belle+Aurore"
 
-  font 'LaBelleAurore.ttf'
+  font "LaBelleAurore.ttf"
 end
