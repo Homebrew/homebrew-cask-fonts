19d3bc45b9d934c377473051be9aa0aace1a1e9c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:53 +0100

    font-asap-condensed.rb: fix for new style

--- Casks/font-asap-condensed.rb
+++ Casks/font-asap-condensed.rb
@@ -1,22 +1,22 @@
-cask 'font-asap-condensed' do
+cask "font-asap-condensed" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/asapcondensed',
+  url "https://github.com/google/fonts/trunk/ofl/asapcondensed",
       using:      :svn,
       trust_cert: true
-  name 'Asap Condensed'
-  homepage 'https://fonts.google.com/specimen/Asap+Condensed'
+  name "Asap Condensed"
+  homepage "https://fonts.google.com/specimen/Asap+Condensed"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'AsapCondensed-Bold.ttf'
-  font 'AsapCondensed-BoldItalic.ttf'
-  font 'AsapCondensed-Italic.ttf'
-  font 'AsapCondensed-Medium.ttf'
-  font 'AsapCondensed-MediumItalic.ttf'
-  font 'AsapCondensed-Regular.ttf'
-  font 'AsapCondensed-SemiBold.ttf'
-  font 'AsapCondensed-SemiBoldItalic.ttf'
+  font "AsapCondensed-Bold.ttf"
+  font "AsapCondensed-BoldItalic.ttf"
+  font "AsapCondensed-Italic.ttf"
+  font "AsapCondensed-Medium.ttf"
+  font "AsapCondensed-MediumItalic.ttf"
+  font "AsapCondensed-Regular.ttf"
+  font "AsapCondensed-SemiBold.ttf"
+  font "AsapCondensed-SemiBoldItalic.ttf"
 end
