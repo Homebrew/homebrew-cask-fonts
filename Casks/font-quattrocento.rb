63c4e0f8c6ebad47c56c19f3fa64cadb12de8459
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:08 +0100

    font-quattrocento.rb: fix for new style

--- Casks/font-quattrocento.rb
+++ Casks/font-quattrocento.rb
@@ -1,16 +1,16 @@
-cask 'font-quattrocento' do
+cask "font-quattrocento" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/quattrocento',
+  url "https://github.com/google/fonts/trunk/ofl/quattrocento",
       using:      :svn,
       trust_cert: true
-  name 'Quattrocento'
-  homepage 'https://fonts.google.com/specimen/Quattrocento'
+  name "Quattrocento"
+  homepage "https://fonts.google.com/specimen/Quattrocento"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Quattrocento-Bold.ttf'
-  font 'Quattrocento-Regular.ttf'
+  font "Quattrocento-Bold.ttf"
+  font "Quattrocento-Regular.ttf"
 end
