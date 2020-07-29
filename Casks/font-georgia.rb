f4ff49b0bd8162b2ed01538c134a5b8728f023df
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:00 +0100

    font-georgia.rb: fix for new style

--- Casks/font-georgia.rb
+++ Casks/font-georgia.rb
@@ -1,14 +1,14 @@
-cask 'font-georgia' do
-  version '2.05'
-  sha256 '2c2c7dcda6606ea5cf08918fb7cd3f3359e9e84338dc690013f20cd42e930301'
+cask "font-georgia" do
+  version "2.05"
+  sha256 "2c2c7dcda6606ea5cf08918fb7cd3f3359e9e84338dc690013f20cd42e930301"
 
-  url 'https://downloads.sourceforge.net/corefonts/georgi32.exe'
-  appcast 'https://sourceforge.net/projects/corefonts/rss'
-  name 'Georgia'
-  homepage 'https://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  url "https://downloads.sourceforge.net/corefonts/georgi32.exe"
+  appcast "https://sourceforge.net/projects/corefonts/rss"
+  name "Georgia"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
 
-  font 'Georgiaz.TTF'
-  font 'Georgiab.TTF'
-  font 'Georgiai.TTF'
-  font 'Georgia.TTF'
+  font "Georgiaz.TTF"
+  font "Georgiab.TTF"
+  font "Georgiai.TTF"
+  font "Georgia.TTF"
 end
