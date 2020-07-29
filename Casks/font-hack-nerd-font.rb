a535bbae8b3d847762021dddc44e2cd356510f1d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:07 +0100

    font-hack-nerd-font.rb: fix for new style

--- Casks/font-hack-nerd-font.rb
+++ Casks/font-hack-nerd-font.rb
@@ -1,18 +1,18 @@
-cask 'font-hack-nerd-font' do
-  version '2.1.0'
-  sha256 '70852e59fcffbe31d401f615625bcb9ebb6af72732c2f1fe9b9d5370c2565514'
+cask "font-hack-nerd-font" do
+  version "2.1.0"
+  sha256 "70852e59fcffbe31d401f615625bcb9ebb6af72732c2f1fe9b9d5370c2565514"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hack.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'Hack Nerd Font (Hack)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "Hack Nerd Font (Hack)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Hack Bold Nerd Font Complete Mono.ttf'
-  font 'Hack Bold Nerd Font Complete.ttf'
-  font 'Hack Bold Italic Nerd Font Complete Mono.ttf'
-  font 'Hack Bold Italic Nerd Font Complete.ttf'
-  font 'Hack Regular Nerd Font Complete.ttf'
-  font 'Hack Regular Nerd Font Complete Mono.ttf'
-  font 'Hack Italic Nerd Font Complete Mono.ttf'
-  font 'Hack Italic Nerd Font Complete.ttf'
+  font "Hack Bold Nerd Font Complete Mono.ttf"
+  font "Hack Bold Nerd Font Complete.ttf"
+  font "Hack Bold Italic Nerd Font Complete Mono.ttf"
+  font "Hack Bold Italic Nerd Font Complete.ttf"
+  font "Hack Regular Nerd Font Complete.ttf"
+  font "Hack Regular Nerd Font Complete Mono.ttf"
+  font "Hack Italic Nerd Font Complete Mono.ttf"
+  font "Hack Italic Nerd Font Complete.ttf"
 end
