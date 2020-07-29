b081efdc66d809826b967ae6d6026c9668efe9d2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:48 +0100

    font-emilys-candy.rb: fix for new style

--- Casks/font-emilys-candy.rb
+++ Casks/font-emilys-candy.rb
@@ -1,11 +1,11 @@
-cask 'font-emilys-candy' do
+cask "font-emilys-candy" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/emilyscandy/EmilysCandy-Regular.ttf'
-  name 'Emilys Candy'
-  homepage 'https://fonts.google.com/specimen/Emilys+Candy'
+  url "https://github.com/google/fonts/raw/master/ofl/emilyscandy/EmilysCandy-Regular.ttf"
+  name "Emilys Candy"
+  homepage "https://fonts.google.com/specimen/Emilys+Candy"
 
-  font 'EmilysCandy-Regular.ttf'
+  font "EmilysCandy-Regular.ttf"
 end
