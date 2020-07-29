89cd84cdf9fa013eab1ddc3c24095763270b0f07
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:49 +0100

    font-englebert.rb: fix for new style

--- Casks/font-englebert.rb
+++ Casks/font-englebert.rb
@@ -1,11 +1,11 @@
-cask 'font-englebert' do
+cask "font-englebert" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/englebert/Englebert-Regular.ttf'
-  name 'Englebert'
-  homepage 'https://fonts.google.com/specimen/Englebert'
+  url "https://github.com/google/fonts/raw/master/ofl/englebert/Englebert-Regular.ttf"
+  name "Englebert"
+  homepage "https://fonts.google.com/specimen/Englebert"
 
-  font 'Englebert-Regular.ttf'
+  font "Englebert-Regular.ttf"
 end
