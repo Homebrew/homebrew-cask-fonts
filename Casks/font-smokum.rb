2c2d0bbb14d629cca9a2b33f88663b9973903005
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:18 +0100

    font-smokum.rb: fix for new style

--- Casks/font-smokum.rb
+++ Casks/font-smokum.rb
@@ -1,11 +1,11 @@
-cask 'font-smokum' do
+cask "font-smokum" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/smokum/Smokum-Regular.ttf'
-  name 'Smokum'
-  homepage 'https://fonts.google.com/specimen/Smokum'
+  url "https://github.com/google/fonts/raw/master/apache/smokum/Smokum-Regular.ttf"
+  name "Smokum"
+  homepage "https://fonts.google.com/specimen/Smokum"
 
-  font 'Smokum-Regular.ttf'
+  font "Smokum-Regular.ttf"
 end
