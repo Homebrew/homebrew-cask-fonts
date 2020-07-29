9d33da9786ed414d0fca280e08eee2fc1c0da2d5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:18 +0100

    font-iosevka-nerd-font.rb: fix for new style

--- Casks/font-iosevka-nerd-font.rb
+++ Casks/font-iosevka-nerd-font.rb
@@ -1,94 +1,94 @@
-cask 'font-iosevka-nerd-font' do
-  version '2.1.0'
-  sha256 '6bd29ef886b808d1d76dd85f82b8823452265fda582801734aab6f9460270de3'
+cask "font-iosevka-nerd-font" do
+  version "2.1.0"
+  sha256 "6bd29ef886b808d1d76dd85f82b8823452265fda582801734aab6f9460270de3"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Iosevka.zip"
-  appcast 'https://github.com/ryanoasis/nerd-fonts/releases.atom'
-  name 'Iosevka Nerd Font (Iosevka)'
-  homepage 'https://github.com/ryanoasis/nerd-fonts'
+  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
+  name "Iosevka Nerd Font (Iosevka)"
+  homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font 'Iosevka Bold Nerd Font Complete.ttf'
-  font 'Iosevka Term Bold Nerd Font Complete.ttf'
-  font 'Iosevka Bold Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Bold Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Heavy Italic Nerd Font Complete.ttf'
-  font 'Iosevka Heavy Italic Nerd Font Complete.ttf'
-  font 'Iosevka Heavy Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Heavy Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Thin Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Term Thin Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Thin Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Thin Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Bold Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Bold Italic Nerd Font Complete.ttf'
-  font 'Iosevka Term Bold Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Bold Italic Nerd Font Complete.ttf'
-  font 'Iosevka Light Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Light Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Term Light Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Light Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Bold Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Term Bold Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Bold Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Bold Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Medium Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Medium Nerd Font Complete.ttf'
-  font 'Iosevka Medium Nerd Font Complete Mono.ttf'
-  font 'Iosevka Medium Nerd Font Complete.ttf'
-  font 'Iosevka Term Extralight Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Extralight Nerd Font Complete.ttf'
-  font 'Iosevka Extralight Nerd Font Complete Mono.ttf'
-  font 'Iosevka Extralight Nerd Font Complete.ttf'
-  font 'Iosevka Medium Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Medium Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Medium Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Term Medium Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Extralight Italic Nerd Font Complete.ttf'
-  font 'Iosevka Extralight Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Extralight Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Extralight Italic Nerd Font Complete.ttf'
-  font 'Iosevka Term Nerd Font Complete Mono.ttf'
-  font 'Iosevka Nerd Font Complete.ttf'
-  font 'Iosevka Term Nerd Font Complete.ttf'
-  font 'Iosevka Nerd Font Complete Mono.ttf'
-  font 'Iosevka Heavy Nerd Font Complete.ttf'
-  font 'Iosevka Term Heavy Nerd Font Complete Mono.ttf'
-  font 'Iosevka Heavy Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Heavy Nerd Font Complete.ttf'
-  font 'Iosevka Thin Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Thin Italic Nerd Font Complete.ttf'
-  font 'Iosevka Thin Italic Nerd Font Complete.ttf'
-  font 'Iosevka Term Thin Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Thin Nerd Font Complete.ttf'
-  font 'Iosevka Term Thin Nerd Font Complete.ttf'
-  font 'Iosevka Term Thin Nerd Font Complete Mono.ttf'
-  font 'Iosevka Thin Nerd Font Complete Mono.ttf'
-  font 'Iosevka Medium Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Medium Italic Nerd Font Complete.ttf'
-  font 'Iosevka Term Medium Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Medium Italic Nerd Font Complete.ttf'
-  font 'Iosevka Extralight Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Term Extralight Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Extralight Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Extralight Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Italic Nerd Font Complete.ttf'
-  font 'Iosevka Italic Nerd Font Complete.ttf'
-  font 'Iosevka Heavy Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Term Heavy Oblique Nerd Font Complete.ttf'
-  font 'Iosevka Heavy Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Heavy Oblique Nerd Font Complete Mono.ttf'
-  font 'Iosevka Light Nerd Font Complete.ttf'
-  font 'Iosevka Light Nerd Font Complete Mono.ttf'
-  font 'Iosevka Term Light Nerd Font Complete.ttf'
-  font 'Iosevka Term Light Nerd Font Complete Mono.ttf'
-  font 'Iosevka Light Italic Nerd Font Complete Mono.ttf'
-  font 'Iosevka Light Italic Nerd Font Complete.ttf'
-  font 'Iosevka Term Light Italic Nerd Font Complete.ttf'
-  font 'Iosevka Term Light Italic Nerd Font Complete Mono.ttf'
+  font "Iosevka Bold Nerd Font Complete.ttf"
+  font "Iosevka Term Bold Nerd Font Complete.ttf"
+  font "Iosevka Bold Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Bold Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Heavy Italic Nerd Font Complete.ttf"
+  font "Iosevka Heavy Italic Nerd Font Complete.ttf"
+  font "Iosevka Heavy Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Heavy Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Thin Oblique Nerd Font Complete.ttf"
+  font "Iosevka Term Thin Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Thin Oblique Nerd Font Complete.ttf"
+  font "Iosevka Thin Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Bold Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Bold Italic Nerd Font Complete.ttf"
+  font "Iosevka Term Bold Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Bold Italic Nerd Font Complete.ttf"
+  font "Iosevka Light Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Light Oblique Nerd Font Complete.ttf"
+  font "Iosevka Term Light Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Light Oblique Nerd Font Complete.ttf"
+  font "Iosevka Bold Oblique Nerd Font Complete.ttf"
+  font "Iosevka Term Bold Oblique Nerd Font Complete.ttf"
+  font "Iosevka Bold Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Bold Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Medium Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Medium Nerd Font Complete.ttf"
+  font "Iosevka Medium Nerd Font Complete Mono.ttf"
+  font "Iosevka Medium Nerd Font Complete.ttf"
+  font "Iosevka Term Extralight Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Extralight Nerd Font Complete.ttf"
+  font "Iosevka Extralight Nerd Font Complete Mono.ttf"
+  font "Iosevka Extralight Nerd Font Complete.ttf"
+  font "Iosevka Medium Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Medium Oblique Nerd Font Complete.ttf"
+  font "Iosevka Medium Oblique Nerd Font Complete.ttf"
+  font "Iosevka Term Medium Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Extralight Italic Nerd Font Complete.ttf"
+  font "Iosevka Extralight Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Extralight Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Extralight Italic Nerd Font Complete.ttf"
+  font "Iosevka Term Nerd Font Complete Mono.ttf"
+  font "Iosevka Nerd Font Complete.ttf"
+  font "Iosevka Term Nerd Font Complete.ttf"
+  font "Iosevka Nerd Font Complete Mono.ttf"
+  font "Iosevka Heavy Nerd Font Complete.ttf"
+  font "Iosevka Term Heavy Nerd Font Complete Mono.ttf"
+  font "Iosevka Heavy Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Heavy Nerd Font Complete.ttf"
+  font "Iosevka Thin Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Thin Italic Nerd Font Complete.ttf"
+  font "Iosevka Thin Italic Nerd Font Complete.ttf"
+  font "Iosevka Term Thin Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Thin Nerd Font Complete.ttf"
+  font "Iosevka Term Thin Nerd Font Complete.ttf"
+  font "Iosevka Term Thin Nerd Font Complete Mono.ttf"
+  font "Iosevka Thin Nerd Font Complete Mono.ttf"
+  font "Iosevka Medium Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Medium Italic Nerd Font Complete.ttf"
+  font "Iosevka Term Medium Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Medium Italic Nerd Font Complete.ttf"
+  font "Iosevka Extralight Oblique Nerd Font Complete.ttf"
+  font "Iosevka Term Extralight Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Extralight Oblique Nerd Font Complete.ttf"
+  font "Iosevka Extralight Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Oblique Nerd Font Complete.ttf"
+  font "Iosevka Oblique Nerd Font Complete.ttf"
+  font "Iosevka Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Italic Nerd Font Complete.ttf"
+  font "Iosevka Italic Nerd Font Complete.ttf"
+  font "Iosevka Heavy Oblique Nerd Font Complete.ttf"
+  font "Iosevka Term Heavy Oblique Nerd Font Complete.ttf"
+  font "Iosevka Heavy Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Heavy Oblique Nerd Font Complete Mono.ttf"
+  font "Iosevka Light Nerd Font Complete.ttf"
+  font "Iosevka Light Nerd Font Complete Mono.ttf"
+  font "Iosevka Term Light Nerd Font Complete.ttf"
+  font "Iosevka Term Light Nerd Font Complete Mono.ttf"
+  font "Iosevka Light Italic Nerd Font Complete Mono.ttf"
+  font "Iosevka Light Italic Nerd Font Complete.ttf"
+  font "Iosevka Term Light Italic Nerd Font Complete.ttf"
+  font "Iosevka Term Light Italic Nerd Font Complete Mono.ttf"
 end
