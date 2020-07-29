e0c679639f9f01cc908051413f23f4ad820fdf3f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:52 +0100

    font-noto-sans-cjk-kr.rb: fix for new style

--- Casks/font-noto-sans-cjk-kr.rb
+++ Casks/font-noto-sans-cjk-kr.rb
@@ -1,19 +1,19 @@
-cask 'font-noto-sans-cjk-kr' do
+cask "font-noto-sans-cjk-kr" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKkr-hinted.zip'
-  name 'Noto Sans CJK KR'
-  homepage 'https://www.google.com/get/noto/#sans-kore'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCJKkr-hinted.zip"
+  name "Noto Sans CJK KR"
+  homepage "https://www.google.com/get/noto/#sans-kore"
 
-  font 'NotoSansCJKkr-Black.otf'
-  font 'NotoSansCJKkr-Bold.otf'
-  font 'NotoSansCJKkr-DemiLight.otf'
-  font 'NotoSansCJKkr-Light.otf'
-  font 'NotoSansCJKkr-Medium.otf'
-  font 'NotoSansCJKkr-Regular.otf'
-  font 'NotoSansCJKkr-Thin.otf'
-  font 'NotoSansMonoCJKkr-Bold.otf'
-  font 'NotoSansMonoCJKkr-Regular.otf'
+  font "NotoSansCJKkr-Black.otf"
+  font "NotoSansCJKkr-Bold.otf"
+  font "NotoSansCJKkr-DemiLight.otf"
+  font "NotoSansCJKkr-Light.otf"
+  font "NotoSansCJKkr-Medium.otf"
+  font "NotoSansCJKkr-Regular.otf"
+  font "NotoSansCJKkr-Thin.otf"
+  font "NotoSansMonoCJKkr-Bold.otf"
+  font "NotoSansMonoCJKkr-Regular.otf"
 end
