f9fd0ff4ac59d6952eed4742645d7f1d71d7efbf
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:36 +0100

    font-antic.rb: fix for new style

--- Casks/font-antic.rb
+++ Casks/font-antic.rb
@@ -1,11 +1,11 @@
-cask 'font-antic' do
+cask "font-antic" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/antic/Antic-Regular.ttf'
-  name 'Antic'
-  homepage 'https://fonts.google.com/specimen/Antic'
+  url "https://github.com/google/fonts/raw/master/ofl/antic/Antic-Regular.ttf"
+  name "Antic"
+  homepage "https://fonts.google.com/specimen/Antic"
 
-  font 'Antic-Regular.ttf'
+  font "Antic-Regular.ttf"
 end
