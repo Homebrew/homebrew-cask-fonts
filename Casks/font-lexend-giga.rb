d37aee70cfb66b3fb100f59bec42f612cd3676eb
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:33 +0100

    font-lexend-giga.rb: fix for new style

--- Casks/font-lexend-giga.rb
+++ Casks/font-lexend-giga.rb
@@ -1,11 +1,11 @@
-cask 'font-lexend-giga' do
+cask "font-lexend-giga" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/lexendgiga/LexendGiga-Regular.ttf'
-  name 'Lexend Giga'
-  homepage 'https://fonts.google.com/specimen/Lexend+Giga'
+  url "https://github.com/google/fonts/raw/master/ofl/lexendgiga/LexendGiga-Regular.ttf"
+  name "Lexend Giga"
+  homepage "https://fonts.google.com/specimen/Lexend+Giga"
 
-  font 'LexendGiga-Regular.ttf'
+  font "LexendGiga-Regular.ttf"
 end
