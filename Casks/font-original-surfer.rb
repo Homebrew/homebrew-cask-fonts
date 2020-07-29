f3db257f5255afe7c4330e83dbce85384bbbe082
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:02 +0100

    font-original-surfer.rb: fix for new style

--- Casks/font-original-surfer.rb
+++ Casks/font-original-surfer.rb
@@ -1,11 +1,11 @@
-cask 'font-original-surfer' do
+cask "font-original-surfer" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/originalsurfer/OriginalSurfer-Regular.ttf'
-  name 'Original Surfer'
-  homepage 'https://fonts.google.com/specimen/Original+Surfer'
+  url "https://github.com/google/fonts/raw/master/ofl/originalsurfer/OriginalSurfer-Regular.ttf"
+  name "Original Surfer"
+  homepage "https://fonts.google.com/specimen/Original+Surfer"
 
-  font 'OriginalSurfer-Regular.ttf'
+  font "OriginalSurfer-Regular.ttf"
 end
