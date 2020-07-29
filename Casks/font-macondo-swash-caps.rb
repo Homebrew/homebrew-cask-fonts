8694905ba73770dc986552d140277dd0a39d429a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:37 +0100

    font-macondo-swash-caps.rb: fix for new style

--- Casks/font-macondo-swash-caps.rb
+++ Casks/font-macondo-swash-caps.rb
@@ -1,11 +1,11 @@
-cask 'font-macondo-swash-caps' do
+cask "font-macondo-swash-caps" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/macondoswashcaps/MacondoSwashCaps-Regular.ttf'
-  name 'Macondo Swash Caps'
-  homepage 'https://fonts.google.com/specimen/Macondo+Swash+Caps'
+  url "https://github.com/google/fonts/raw/master/ofl/macondoswashcaps/MacondoSwashCaps-Regular.ttf"
+  name "Macondo Swash Caps"
+  homepage "https://fonts.google.com/specimen/Macondo+Swash+Caps"
 
-  font 'MacondoSwashCaps-Regular.ttf'
+  font "MacondoSwashCaps-Regular.ttf"
 end
