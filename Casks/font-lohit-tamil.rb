b96ed744f84467afbc407a0b559500c0cf332e86
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:36 +0100

    font-lohit-tamil.rb: fix for new style

--- Casks/font-lohit-tamil.rb
+++ Casks/font-lohit-tamil.rb
@@ -1,11 +1,11 @@
-cask 'font-lohit-tamil' do
+cask "font-lohit-tamil" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/lohittamil/Lohit-Tamil.ttf'
-  name 'Lohit Tamil'
-  homepage 'https://fonts.google.com/specimen/Lohit+Tamil'
+  url "https://github.com/google/fonts/raw/master/ofl/lohittamil/Lohit-Tamil.ttf"
+  name "Lohit Tamil"
+  homepage "https://fonts.google.com/specimen/Lohit+Tamil"
 
-  font 'Lohit-Tamil.ttf'
+  font "Lohit-Tamil.ttf"
 end
