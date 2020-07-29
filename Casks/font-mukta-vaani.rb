031329a9fe61378fc4fe5b843bc171f5f71afe32
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:47 +0100

    font-mukta-vaani.rb: fix for new style

--- Casks/font-mukta-vaani.rb
+++ Casks/font-mukta-vaani.rb
@@ -1,21 +1,21 @@
-cask 'font-mukta-vaani' do
+cask "font-mukta-vaani" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/muktavaani',
+  url "https://github.com/google/fonts/trunk/ofl/muktavaani",
       using:      :svn,
       trust_cert: true
-  name 'Mukta Vaani'
-  homepage 'https://fonts.google.com/specimen/Mukta+Vaani'
+  name "Mukta Vaani"
+  homepage "https://fonts.google.com/specimen/Mukta+Vaani"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'MuktaVaani-Bold.ttf'
-  font 'MuktaVaani-ExtraBold.ttf'
-  font 'MuktaVaani-ExtraLight.ttf'
-  font 'MuktaVaani-Light.ttf'
-  font 'MuktaVaani-Medium.ttf'
-  font 'MuktaVaani-Regular.ttf'
-  font 'MuktaVaani-SemiBold.ttf'
+  font "MuktaVaani-Bold.ttf"
+  font "MuktaVaani-ExtraBold.ttf"
+  font "MuktaVaani-ExtraLight.ttf"
+  font "MuktaVaani-Light.ttf"
+  font "MuktaVaani-Medium.ttf"
+  font "MuktaVaani-Regular.ttf"
+  font "MuktaVaani-SemiBold.ttf"
 end
