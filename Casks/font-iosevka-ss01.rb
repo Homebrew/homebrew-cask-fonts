733ddd77540f3f2f18546e4db69a9e1fd8b18537
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:19 +0100

    font-iosevka-ss01.rb: fix for new style

--- Casks/font-iosevka-ss01.rb
+++ Casks/font-iosevka-ss01.rb
@@ -1,19 +1,19 @@
-cask 'font-iosevka-ss01' do
-  version '3.3.1'
-  sha256 '489c4ed9c37ee556ee1e3fef0c41cda25b6e6305a7b743bc573fae128db9cdc2'
+cask "font-iosevka-ss01" do
+  version "3.3.1"
+  sha256 "489c4ed9c37ee556ee1e3fef0c41cda25b6e6305a7b743bc573fae128db9cdc2"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss01-#{version}.zip"
-  appcast 'https://github.com/be5invis/Iosevka/releases.atom'
-  name 'Iosevka SS01'
-  homepage 'https://github.com/be5invis/Iosevka/'
+  appcast "https://github.com/be5invis/Iosevka/releases.atom"
+  name "Iosevka SS01"
+  homepage "https://github.com/be5invis/Iosevka/"
 
-  font 'iosevka-ss01-bold.ttc'
-  font 'iosevka-ss01-extrabold.ttc'
-  font 'iosevka-ss01-extralight.ttc'
-  font 'iosevka-ss01-heavy.ttc'
-  font 'iosevka-ss01-light.ttc'
-  font 'iosevka-ss01-medium.ttc'
-  font 'iosevka-ss01-regular.ttc'
-  font 'iosevka-ss01-semibold.ttc'
-  font 'iosevka-ss01-thin.ttc'
+  font "iosevka-ss01-bold.ttc"
+  font "iosevka-ss01-extrabold.ttc"
+  font "iosevka-ss01-extralight.ttc"
+  font "iosevka-ss01-heavy.ttc"
+  font "iosevka-ss01-light.ttc"
+  font "iosevka-ss01-medium.ttc"
+  font "iosevka-ss01-regular.ttc"
+  font "iosevka-ss01-semibold.ttc"
+  font "iosevka-ss01-thin.ttc"
 end
