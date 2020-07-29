dd91fe5f9612f284f0dc832c0eb353c06febe7f9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:30 +0100

    font-zhi-mang-xing.rb: fix for new style

--- Casks/font-zhi-mang-xing.rb
+++ Casks/font-zhi-mang-xing.rb
@@ -1,11 +1,11 @@
-cask 'font-zhi-mang-xing' do
+cask "font-zhi-mang-xing" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/zhimangxing/ZhiMangXing-Regular.ttf'
-  name 'Zhi Mang Xing'
-  homepage 'https://fonts.google.com/specimen/Zhi+Mang+Xing'
+  url "https://github.com/google/fonts/raw/master/ofl/zhimangxing/ZhiMangXing-Regular.ttf"
+  name "Zhi Mang Xing"
+  homepage "https://fonts.google.com/specimen/Zhi+Mang+Xing"
 
-  font 'ZhiMangXing-Regular.ttf'
+  font "ZhiMangXing-Regular.ttf"
 end
