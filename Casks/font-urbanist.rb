85b946ad59ffeeab945ac5e25fd2a3d3766afebd
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:27 +0100

    font-urbanist.rb: fix for new style

--- Casks/font-urbanist.rb
+++ Casks/font-urbanist.rb
@@ -1,11 +1,11 @@
-cask 'font-urbanist' do
-  version '1.220'
-  sha256 'ed4f5d9fb76ce6e147cce266a81268e9aa270ee2c2633f198499e51be3b1ae8e'
+cask "font-urbanist" do
+  version "1.220"
+  sha256 "ed4f5d9fb76ce6e147cce266a81268e9aa270ee2c2633f198499e51be3b1ae8e"
 
   url "https://github.com/coreywho/Urbanist/releases/download/#{version}/Urbanist.Font.Family.zip"
-  appcast 'https://github.com/coreywho/Urbanist/releases.atom'
-  name 'Urbanist'
-  homepage 'https://github.com/coreywho/Urbanist'
+  appcast "https://github.com/coreywho/Urbanist/releases.atom"
+  name "Urbanist"
+  homepage "https://github.com/coreywho/Urbanist"
 
-  font 'fonts/variable/UrbanistGX.ttf'
+  font "fonts/variable/UrbanistGX.ttf"
 end
