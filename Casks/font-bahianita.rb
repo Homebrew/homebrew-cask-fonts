06be382131c10377820d9c737c45a3cc44863428
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:37:20 +0100

    font-bahianita.rb: fix for new style

--- Casks/font-bahianita.rb
+++ Casks/font-bahianita.rb
@@ -1,11 +1,11 @@
-cask 'font-bahianita' do
+cask "font-bahianita" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bahianita/Bahianita-Regular.ttf'
-  name 'Bahianita'
-  homepage 'https://fonts.google.com/specimen/Bahianita'
+  url "https://github.com/google/fonts/raw/master/ofl/bahianita/Bahianita-Regular.ttf"
+  name "Bahianita"
+  homepage "https://fonts.google.com/specimen/Bahianita"
 
-  font 'Bahianita-Regular.ttf'
+  font "Bahianita-Regular.ttf"
 end
