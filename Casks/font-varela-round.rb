c734240a08e0389771b435fc2e188f655a96788c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:27 +0100

    font-varela-round.rb: fix for new style

--- Casks/font-varela-round.rb
+++ Casks/font-varela-round.rb
@@ -1,11 +1,11 @@
-cask 'font-varela-round' do
+cask "font-varela-round" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/varelaround/VarelaRound-Regular.ttf'
-  name 'Varela Round'
-  homepage 'https://fonts.google.com/specimen/Varela+Round'
+  url "https://github.com/google/fonts/raw/master/ofl/varelaround/VarelaRound-Regular.ttf"
+  name "Varela Round"
+  homepage "https://fonts.google.com/specimen/Varela+Round"
 
-  font 'VarelaRound-Regular.ttf'
+  font "VarelaRound-Regular.ttf"
 end
