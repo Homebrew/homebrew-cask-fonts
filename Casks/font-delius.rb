b94b73dd4bf13b98fe92a78bed026ea752223ee6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:42 +0100

    font-delius.rb: fix for new style

--- Casks/font-delius.rb
+++ Casks/font-delius.rb
@@ -1,11 +1,11 @@
-cask 'font-delius' do
+cask "font-delius" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/delius/Delius-Regular.ttf'
-  name 'Delius'
-  homepage 'https://fonts.google.com/specimen/Delius'
+  url "https://github.com/google/fonts/raw/master/ofl/delius/Delius-Regular.ttf"
+  name "Delius"
+  homepage "https://fonts.google.com/specimen/Delius"
 
-  font 'Delius-Regular.ttf'
+  font "Delius-Regular.ttf"
 end
