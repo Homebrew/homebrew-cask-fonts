cbd0235f22d555aa503004b183c6542f5e786760
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:29 +0100

    font-caveat.rb: fix for new style

--- Casks/font-caveat.rb
+++ Casks/font-caveat.rb
@@ -1,16 +1,16 @@
-cask 'font-caveat' do
+cask "font-caveat" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/caveat',
+  url "https://github.com/google/fonts/trunk/ofl/caveat",
       using:      :svn,
       trust_cert: true
-  name 'Caveat'
-  homepage 'https://fonts.google.com/specimen/Caveat'
+  name "Caveat"
+  homepage "https://fonts.google.com/specimen/Caveat"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Caveat-Bold.ttf'
-  font 'Caveat-Regular.ttf'
+  font "Caveat-Bold.ttf"
+  font "Caveat-Regular.ttf"
 end
