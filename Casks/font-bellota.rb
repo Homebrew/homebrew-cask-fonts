1086789b3b9bdeb2aeebc75953ffa847f9197f7c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:18 +0100

    font-bellota.rb: fix for new style

--- Casks/font-bellota.rb
+++ Casks/font-bellota.rb
@@ -1,20 +1,20 @@
-cask 'font-bellota' do
+cask "font-bellota" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/bellota',
+  url "https://github.com/google/fonts/trunk/ofl/bellota",
       using:      :svn,
       trust_cert: true
-  name 'Bellota'
-  homepage 'https://fonts.google.com/specimen/Bellota'
+  name "Bellota"
+  homepage "https://fonts.google.com/specimen/Bellota"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Bellota-Bold.ttf'
-  font 'Bellota-BoldItalic.ttf'
-  font 'Bellota-Italic.ttf'
-  font 'Bellota-Light.ttf'
-  font 'Bellota-LightItalic.ttf'
-  font 'Bellota-Regular.ttf'
+  font "Bellota-Bold.ttf"
+  font "Bellota-BoldItalic.ttf"
+  font "Bellota-Italic.ttf"
+  font "Bellota-Light.ttf"
+  font "Bellota-LightItalic.ttf"
+  font "Bellota-Regular.ttf"
 end
