dbc4bbecb5e8d89576b0b85a1aad7ce302d8444f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:42 +0100

    font-metrophobic.rb: fix for new style

--- Casks/font-metrophobic.rb
+++ Casks/font-metrophobic.rb
@@ -1,11 +1,11 @@
-cask 'font-metrophobic' do
+cask "font-metrophobic" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/metrophobic/Metrophobic-Regular.ttf'
-  name 'Metrophobic'
-  homepage 'https://fonts.google.com/specimen/Metrophobic'
+  url "https://github.com/google/fonts/raw/master/ofl/metrophobic/Metrophobic-Regular.ttf"
+  name "Metrophobic"
+  homepage "https://fonts.google.com/specimen/Metrophobic"
 
-  font 'Metrophobic-Regular.ttf'
+  font "Metrophobic-Regular.ttf"
 end
