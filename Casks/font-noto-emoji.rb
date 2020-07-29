36f6bf7e193a6619eb03540d2d6d84e7f0e5d4f8
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:50 +0100

    font-noto-emoji.rb: fix for new style

--- Casks/font-noto-emoji.rb
+++ Casks/font-noto-emoji.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-emoji' do
+cask "font-noto-emoji" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoEmoji-unhinted.zip'
-  name 'Noto Emoji'
-  homepage 'https://www.google.com/get/noto/#emoji-zsye'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoEmoji-unhinted.zip"
+  name "Noto Emoji"
+  homepage "https://www.google.com/get/noto/#emoji-zsye"
 
-  font 'NotoEmoji-Regular.ttf'
+  font "NotoEmoji-Regular.ttf"
 end
