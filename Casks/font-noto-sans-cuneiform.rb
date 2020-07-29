74ecde49ace4c312e45623da186a8fba365d5f54
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:52 +0100

    font-noto-sans-cuneiform.rb: fix for new style

--- Casks/font-noto-sans-cuneiform.rb
+++ Casks/font-noto-sans-cuneiform.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-cuneiform' do
+cask "font-noto-sans-cuneiform" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCuneiform-unhinted.zip'
-  name 'Noto Sans Cuneiform'
-  homepage 'https://www.google.com/get/noto/#sans-xsux'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCuneiform-unhinted.zip"
+  name "Noto Sans Cuneiform"
+  homepage "https://www.google.com/get/noto/#sans-xsux"
 
-  font 'NotoSansCuneiform-Regular.ttf'
+  font "NotoSansCuneiform-Regular.ttf"
 end
