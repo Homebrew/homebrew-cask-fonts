9f41672a33f362e8683a93ecf6e6e1d607c9c7af
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:19 +0100

    font-sonsie-one.rb: fix for new style

--- Casks/font-sonsie-one.rb
+++ Casks/font-sonsie-one.rb
@@ -1,11 +1,11 @@
-cask 'font-sonsie-one' do
+cask "font-sonsie-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/sonsieone/SonsieOne-Regular.ttf'
-  name 'Sonsie One'
-  homepage 'https://fonts.google.com/specimen/Sonsie+One'
+  url "https://github.com/google/fonts/raw/master/ofl/sonsieone/SonsieOne-Regular.ttf"
+  name "Sonsie One"
+  homepage "https://fonts.google.com/specimen/Sonsie+One"
 
-  font 'SonsieOne-Regular.ttf'
+  font "SonsieOne-Regular.ttf"
 end
