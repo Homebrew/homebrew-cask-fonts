666508de874b6d9c2eb572cfff4ddff684d50022
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:57 +0100

    font-gaegu.rb: fix for new style

--- Casks/font-gaegu.rb
+++ Casks/font-gaegu.rb
@@ -1,17 +1,17 @@
-cask 'font-gaegu' do
+cask "font-gaegu" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/gaegu',
+  url "https://github.com/google/fonts/trunk/ofl/gaegu",
       using:      :svn,
       trust_cert: true
-  name 'Gaegu'
-  homepage 'https://fonts.google.com/specimen/Gaegu'
+  name "Gaegu"
+  homepage "https://fonts.google.com/specimen/Gaegu"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Gaegu-Bold.ttf'
-  font 'Gaegu-Light.ttf'
-  font 'Gaegu-Regular.ttf'
+  font "Gaegu-Bold.ttf"
+  font "Gaegu-Light.ttf"
+  font "Gaegu-Regular.ttf"
 end
