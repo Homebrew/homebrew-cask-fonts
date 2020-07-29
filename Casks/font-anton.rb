dadb2c2c34b1a467c365a0178264bbcc215f6868
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:42 +0100

    font-anton.rb: fix for new style

--- Casks/font-anton.rb
+++ Casks/font-anton.rb
@@ -1,11 +1,11 @@
-cask 'font-anton' do
+cask "font-anton" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/anton/Anton-Regular.ttf'
-  name 'Anton'
-  homepage 'https://fonts.google.com/specimen/Anton'
+  url "https://github.com/google/fonts/raw/master/ofl/anton/Anton-Regular.ttf"
+  name "Anton"
+  homepage "https://fonts.google.com/specimen/Anton"
 
-  font 'Anton-Regular.ttf'
+  font "Anton-Regular.ttf"
 end
