cd505f59d1d0732153b6745b459a99b7dfd45e58
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:53 +0100

    font-finger-paint.rb: fix for new style

--- Casks/font-finger-paint.rb
+++ Casks/font-finger-paint.rb
@@ -1,11 +1,11 @@
-cask 'font-finger-paint' do
+cask "font-finger-paint" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/fingerpaint/FingerPaint-Regular.ttf'
-  name 'Finger Paint'
-  homepage 'https://fonts.google.com/specimen/Finger+Paint'
+  url "https://github.com/google/fonts/raw/master/ofl/fingerpaint/FingerPaint-Regular.ttf"
+  name "Finger Paint"
+  homepage "https://fonts.google.com/specimen/Finger+Paint"
 
-  font 'FingerPaint-Regular.ttf'
+  font "FingerPaint-Regular.ttf"
 end
