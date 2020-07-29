721fe74e0a09525a31c851209e7587ff1c700434
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:08 +0100

    font-almarai.rb: fix for new style

--- Casks/font-almarai.rb
+++ Casks/font-almarai.rb
@@ -1,18 +1,18 @@
-cask 'font-almarai' do
+cask "font-almarai" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/almarai',
+  url "https://github.com/google/fonts/trunk/ofl/almarai",
       using:      :svn,
       trust_cert: true
-  name 'Almarai'
-  homepage 'https://fonts.google.com/specimen/Almarai'
+  name "Almarai"
+  homepage "https://fonts.google.com/specimen/Almarai"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Almarai-Bold.ttf'
-  font 'Almarai-ExtraBold.ttf'
-  font 'Almarai-Light.ttf'
-  font 'Almarai-Regular.ttf'
+  font "Almarai-Bold.ttf"
+  font "Almarai-ExtraBold.ttf"
+  font "Almarai-Light.ttf"
+  font "Almarai-Regular.ttf"
 end
