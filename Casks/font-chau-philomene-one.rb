f5b311629323fbc6c8a93cd9b429883528da87ba
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:30 +0100

    font-chau-philomene-one.rb: fix for new style

--- Casks/font-chau-philomene-one.rb
+++ Casks/font-chau-philomene-one.rb
@@ -1,16 +1,16 @@
-cask 'font-chau-philomene-one' do
+cask "font-chau-philomene-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/chauphilomeneone',
+  url "https://github.com/google/fonts/trunk/ofl/chauphilomeneone",
       using:      :svn,
       trust_cert: true
-  name 'Chau Philomene One'
-  homepage 'https://fonts.google.com/specimen/Chau+Philomene+One'
+  name "Chau Philomene One"
+  homepage "https://fonts.google.com/specimen/Chau+Philomene+One"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'ChauPhilomeneOne-Italic.ttf'
-  font 'ChauPhilomeneOne-Regular.ttf'
+  font "ChauPhilomeneOne-Italic.ttf"
+  font "ChauPhilomeneOne-Regular.ttf"
 end
