c5320fff3c52e5ee878330baedcab1434c10ba34
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:11 +0100

    font-roboto.rb: fix for new style

--- Casks/font-roboto.rb
+++ Casks/font-roboto.rb
@@ -1,16 +1,16 @@
-cask 'font-roboto' do
+cask "font-roboto" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/roboto',
+  url "https://github.com/google/fonts/trunk/ofl/roboto",
       using:      :svn,
       trust_cert: true
-  name 'Roboto'
-  homepage 'https://fonts.google.com/specimen/Roboto'
+  name "Roboto"
+  homepage "https://fonts.google.com/specimen/Roboto"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Roboto-Italic[wdth,wght].ttf'
-  font 'Roboto[wdth,wght].ttf'
+  font "Roboto-Italic[wdth,wght].ttf"
+  font "Roboto[wdth,wght].ttf"
 end
