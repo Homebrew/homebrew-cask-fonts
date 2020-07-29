25d4c6d0a31ab37e59521dc851b1262aef36f5b4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:43 +0100

    font-miltonian.rb: fix for new style

--- Casks/font-miltonian.rb
+++ Casks/font-miltonian.rb
@@ -1,11 +1,11 @@
-cask 'font-miltonian' do
+cask "font-miltonian" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/miltonian/Miltonian-Regular.ttf'
-  name 'Miltonian'
-  homepage 'https://fonts.google.com/specimen/Miltonian'
+  url "https://github.com/google/fonts/raw/master/ofl/miltonian/Miltonian-Regular.ttf"
+  name "Miltonian"
+  homepage "https://fonts.google.com/specimen/Miltonian"
 
-  font 'Miltonian-Regular.ttf'
+  font "Miltonian-Regular.ttf"
 end
