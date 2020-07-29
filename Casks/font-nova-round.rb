3468b7575f3dd43ae34479beeda87a6d98f06a34
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:00 +0100

    font-nova-round.rb: fix for new style

--- Casks/font-nova-round.rb
+++ Casks/font-nova-round.rb
@@ -1,11 +1,11 @@
-cask 'font-nova-round' do
+cask "font-nova-round" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/novaround/NovaRound.ttf'
-  name 'Nova Round'
-  homepage 'https://fonts.google.com/specimen/Nova+Round'
+  url "https://github.com/google/fonts/raw/master/ofl/novaround/NovaRound.ttf"
+  name "Nova Round"
+  homepage "https://fonts.google.com/specimen/Nova+Round"
 
-  font 'NovaRound.ttf'
+  font "NovaRound.ttf"
 end
