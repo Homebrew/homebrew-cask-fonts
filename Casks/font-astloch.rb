d1b48e38f57bbece5c9737fd9bf8282c02d12b70
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:12 +0100

    font-astloch.rb: fix for new style

--- Casks/font-astloch.rb
+++ Casks/font-astloch.rb
@@ -1,16 +1,16 @@
-cask 'font-astloch' do
+cask "font-astloch" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/astloch',
+  url "https://github.com/google/fonts/trunk/ofl/astloch",
       using:      :svn,
       trust_cert: true
-  name 'Astloch'
-  homepage 'https://fonts.google.com/specimen/Astloch'
+  name "Astloch"
+  homepage "https://fonts.google.com/specimen/Astloch"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Astloch-Bold.ttf'
-  font 'Astloch-Regular.ttf'
+  font "Astloch-Bold.ttf"
+  font "Astloch-Regular.ttf"
 end
