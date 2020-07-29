5d5f9efadcc195974b414a0ba2a383e6e6537340
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:32:10 +0100

    font-alata.rb: fix for new style

--- Casks/font-alata.rb
+++ Casks/font-alata.rb
@@ -1,11 +1,11 @@
-cask 'font-alata' do
+cask "font-alata" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/alata/Alata-Regular.ttf'
-  name 'Alata'
-  homepage 'https://fonts.google.com/specimen/Alata'
+  url "https://github.com/google/fonts/raw/master/ofl/alata/Alata-Regular.ttf"
+  name "Alata"
+  homepage "https://fonts.google.com/specimen/Alata"
 
-  font 'Alata-Regular.ttf'
+  font "Alata-Regular.ttf"
 end
