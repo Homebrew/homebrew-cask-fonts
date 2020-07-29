309fbd39ba3e97c38d3220337993927477915190
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:25 +0100

    font-julius-sans-one.rb: fix for new style

--- Casks/font-julius-sans-one.rb
+++ Casks/font-julius-sans-one.rb
@@ -1,11 +1,11 @@
-cask 'font-julius-sans-one' do
+cask "font-julius-sans-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/juliussansone/JuliusSansOne-Regular.ttf'
-  name 'Julius Sans One'
-  homepage 'https://fonts.google.com/specimen/Julius+Sans+One'
+  url "https://github.com/google/fonts/raw/master/ofl/juliussansone/JuliusSansOne-Regular.ttf"
+  name "Julius Sans One"
+  homepage "https://fonts.google.com/specimen/Julius+Sans+One"
 
-  font 'JuliusSansOne-Regular.ttf'
+  font "JuliusSansOne-Regular.ttf"
 end
