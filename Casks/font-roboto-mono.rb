6b9735d87e4dbe992ad1eb663cbde6447df06957
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:11 +0100

    font-roboto-mono.rb: fix for new style

--- Casks/font-roboto-mono.rb
+++ Casks/font-roboto-mono.rb
@@ -1,16 +1,16 @@
-cask 'font-roboto-mono' do
+cask "font-roboto-mono" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/apache/robotomono',
+  url "https://github.com/google/fonts/trunk/apache/robotomono",
       using:      :svn,
       trust_cert: true
-  name 'Roboto Mono'
-  homepage 'https://fonts.google.com/specimen/Roboto+Mono'
+  name "Roboto Mono"
+  homepage "https://fonts.google.com/specimen/Roboto+Mono"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'RobotoMono-Italic[wght].ttf'
-  font 'RobotoMono[wght].ttf'
+  font "RobotoMono-Italic[wght].ttf"
+  font "RobotoMono[wght].ttf"
 end
