24c1ee510491fc127908dbbd80b3e84ca56f18d1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:51 +0100

    font-noto-sans-bamum.rb: fix for new style

--- Casks/font-noto-sans-bamum.rb
+++ Casks/font-noto-sans-bamum.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-bamum' do
+cask "font-noto-sans-bamum" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBamum-unhinted.zip'
-  name 'Noto Sans Bamum'
-  homepage 'https://www.google.com/get/noto/#sans-bamu'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansBamum-unhinted.zip"
+  name "Noto Sans Bamum"
+  homepage "https://www.google.com/get/noto/#sans-bamu"
 
-  font 'NotoSansBamum-Regular.ttf'
+  font "NotoSansBamum-Regular.ttf"
 end
