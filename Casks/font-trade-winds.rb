7ab79b4dbc5072e6abd427ce618fe6636cd56462
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:25 +0100

    font-trade-winds.rb: fix for new style

--- Casks/font-trade-winds.rb
+++ Casks/font-trade-winds.rb
@@ -1,11 +1,11 @@
-cask 'font-trade-winds' do
+cask "font-trade-winds" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/tradewinds/TradeWinds-Regular.ttf'
-  name 'Trade Winds'
-  homepage 'https://fonts.google.com/specimen/Trade+Winds'
+  url "https://github.com/google/fonts/raw/master/ofl/tradewinds/TradeWinds-Regular.ttf"
+  name "Trade Winds"
+  homepage "https://fonts.google.com/specimen/Trade+Winds"
 
-  font 'TradeWinds-Regular.ttf'
+  font "TradeWinds-Regular.ttf"
 end
