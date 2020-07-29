7568c57bdd9d8dd054b4b1b292848e146a2a371f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:24 +0100

    font-tienne.rb: fix for new style

--- Casks/font-tienne.rb
+++ Casks/font-tienne.rb
@@ -1,17 +1,17 @@
-cask 'font-tienne' do
+cask "font-tienne" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/tienne',
+  url "https://github.com/google/fonts/trunk/ofl/tienne",
       using:      :svn,
       trust_cert: true
-  name 'Tienne'
-  homepage 'https://fonts.google.com/specimen/Tienne'
+  name "Tienne"
+  homepage "https://fonts.google.com/specimen/Tienne"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Tienne-Black.ttf'
-  font 'Tienne-Bold.ttf'
-  font 'Tienne-Regular.ttf'
+  font "Tienne-Black.ttf"
+  font "Tienne-Bold.ttf"
+  font "Tienne-Regular.ttf"
 end
