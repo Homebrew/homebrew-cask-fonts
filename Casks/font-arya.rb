3bc2f3a365671a9593a89ca18ac32e5694bf871f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:35:50 +0100

    font-arya.rb: fix for new style

--- Casks/font-arya.rb
+++ Casks/font-arya.rb
@@ -1,16 +1,16 @@
-cask 'font-arya' do
+cask "font-arya" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/arya',
+  url "https://github.com/google/fonts/trunk/ofl/arya",
       using:      :svn,
       trust_cert: true
-  name 'Arya'
-  homepage 'https://fonts.google.com/specimen/Arya'
+  name "Arya"
+  homepage "https://fonts.google.com/specimen/Arya"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Arya-Bold.ttf'
-  font 'Arya-Regular.ttf'
+  font "Arya-Bold.ttf"
+  font "Arya-Regular.ttf"
 end
