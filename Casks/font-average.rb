2fd1fed4c471e5353955d2d32709659e752e7fbd
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:36:45 +0100

    font-average.rb: fix for new style

--- Casks/font-average.rb
+++ Casks/font-average.rb
@@ -1,11 +1,11 @@
-cask 'font-average' do
+cask "font-average" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/average/Average-Regular.ttf'
-  name 'Average'
-  homepage 'https://fonts.google.com/specimen/Average'
+  url "https://github.com/google/fonts/raw/master/ofl/average/Average-Regular.ttf"
+  name "Average"
+  homepage "https://fonts.google.com/specimen/Average"
 
-  font 'Average-Regular.ttf'
+  font "Average-Regular.ttf"
 end
