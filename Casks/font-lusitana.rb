3165df65b2871047dbcad24124de478562e7368c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:37 +0100

    font-lusitana.rb: fix for new style

--- Casks/font-lusitana.rb
+++ Casks/font-lusitana.rb
@@ -1,16 +1,16 @@
-cask 'font-lusitana' do
+cask "font-lusitana" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/lusitana',
+  url "https://github.com/google/fonts/trunk/ofl/lusitana",
       using:      :svn,
       trust_cert: true
-  name 'Lusitana'
-  homepage 'https://fonts.google.com/specimen/Lusitana'
+  name "Lusitana"
+  homepage "https://fonts.google.com/specimen/Lusitana"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Lusitana-Bold.ttf'
-  font 'Lusitana-Regular.ttf'
+  font "Lusitana-Bold.ttf"
+  font "Lusitana-Regular.ttf"
 end
