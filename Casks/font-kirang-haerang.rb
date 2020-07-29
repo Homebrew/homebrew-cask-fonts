c3702721f4703e750e0c9286459ed5a256ed55a9
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:28 +0100

    font-kirang-haerang.rb: fix for new style

--- Casks/font-kirang-haerang.rb
+++ Casks/font-kirang-haerang.rb
@@ -1,11 +1,11 @@
-cask 'font-kirang-haerang' do
+cask "font-kirang-haerang" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/kiranghaerang/KirangHaerang-Regular.ttf'
-  name 'Kirang Haerang'
-  homepage 'https://fonts.google.com/specimen/Kirang+Haerang'
+  url "https://github.com/google/fonts/raw/master/ofl/kiranghaerang/KirangHaerang-Regular.ttf"
+  name "Kirang Haerang"
+  homepage "https://fonts.google.com/specimen/Kirang+Haerang"
 
-  font 'KirangHaerang-Regular.ttf'
+  font "KirangHaerang-Regular.ttf"
 end
