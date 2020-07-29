8ac69a69ef73a8868135dd9fa5fe0d82cb6c8cc2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:25 +0100

    font-cabin-vf-beta.rb: fix for new style

--- Casks/font-cabin-vf-beta.rb
+++ Casks/font-cabin-vf-beta.rb
@@ -1,16 +1,16 @@
-cask 'font-cabin-vf-beta' do
+cask "font-cabin-vf-beta" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/cabinvfbeta',
+  url "https://github.com/google/fonts/trunk/ofl/cabinvfbeta",
       using:      :svn,
       trust_cert: true
-  name 'Cabin VF Beta'
-  homepage 'https://fonts.google.com/earlyaccess'
+  name "Cabin VF Beta"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'CabinVFBeta-Italic.ttf'
-  font 'CabinVFBeta.ttf'
+  font "CabinVFBeta-Italic.ttf"
+  font "CabinVFBeta.ttf"
 end
