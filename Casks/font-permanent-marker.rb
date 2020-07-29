cd25ea84f3eff80ce64b61d3d9b6a4dc3fab74ac
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:04 +0100

    font-permanent-marker.rb: fix for new style

--- Casks/font-permanent-marker.rb
+++ Casks/font-permanent-marker.rb
@@ -1,11 +1,11 @@
-cask 'font-permanent-marker' do
+cask "font-permanent-marker" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/apache/permanentmarker/PermanentMarker-Regular.ttf'
-  name 'Permanent Marker'
-  homepage 'https://fonts.google.com/specimen/Permanent+Marker'
+  url "https://github.com/google/fonts/raw/master/apache/permanentmarker/PermanentMarker-Regular.ttf"
+  name "Permanent Marker"
+  homepage "https://fonts.google.com/specimen/Permanent+Marker"
 
-  font 'PermanentMarker-Regular.ttf'
+  font "PermanentMarker-Regular.ttf"
 end
