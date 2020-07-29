362d9e0e38d5370ab3c733c7223935dfd31c3caf
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:08 +0100

    font-handlee.rb: fix for new style

--- Casks/font-handlee.rb
+++ Casks/font-handlee.rb
@@ -1,11 +1,11 @@
-cask 'font-handlee' do
+cask "font-handlee" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/handlee/Handlee-Regular.ttf'
-  name 'Handlee'
-  homepage 'https://fonts.google.com/specimen/Handlee'
+  url "https://github.com/google/fonts/raw/master/ofl/handlee/Handlee-Regular.ttf"
+  name "Handlee"
+  homepage "https://fonts.google.com/specimen/Handlee"
 
-  font 'Handlee-Regular.ttf'
+  font "Handlee-Regular.ttf"
 end
