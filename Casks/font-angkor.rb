5f49e852efc612709428b7486cf1a3fdca1327ad
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:34:13 +0100

    font-angkor.rb: fix for new style

--- Casks/font-angkor.rb
+++ Casks/font-angkor.rb
@@ -1,11 +1,11 @@
-cask 'font-angkor' do
+cask "font-angkor" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/angkor/Angkor-Regular.ttf'
-  name 'Angkor'
-  homepage 'https://fonts.google.com/specimen/Angkor'
+  url "https://github.com/google/fonts/raw/master/ofl/angkor/Angkor-Regular.ttf"
+  name "Angkor"
+  homepage "https://fonts.google.com/specimen/Angkor"
 
-  font 'Angkor-Regular.ttf'
+  font "Angkor-Regular.ttf"
 end
