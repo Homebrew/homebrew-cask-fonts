cbddf8f07fad2945f01b92d554448de4d8076092
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:55 +0100

    font-noto-sans-n-ko.rb: fix for new style

--- Casks/font-noto-sans-n-ko.rb
+++ Casks/font-noto-sans-n-ko.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-n-ko' do
+cask "font-noto-sans-n-ko" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansNKo-unhinted.zip'
-  name 'Noto Sans NKo'
-  homepage 'https://www.google.com/get/noto/#sans-nkoo'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansNKo-unhinted.zip"
+  name "Noto Sans NKo"
+  homepage "https://www.google.com/get/noto/#sans-nkoo"
 
-  font 'NotoSansNKo-Regular.ttf'
+  font "NotoSansNKo-Regular.ttf"
 end
