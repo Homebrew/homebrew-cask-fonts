3b842aa4d856b2f983714ac014d2609b793d9adf
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:26 +0100

    font-karla-tamil-upright.rb: fix for new style

--- Casks/font-karla-tamil-upright.rb
+++ Casks/font-karla-tamil-upright.rb
@@ -1,16 +1,16 @@
-cask 'font-karla-tamil-upright' do
+cask "font-karla-tamil-upright" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/karlatamilupright',
+  url "https://github.com/google/fonts/trunk/ofl/karlatamilupright",
       using:      :svn,
       trust_cert: true
-  name 'Karla Tamil Upright'
-  homepage 'https://fonts.google.com/specimen/Karla+Tamil+Upright'
+  name "Karla Tamil Upright"
+  homepage "https://fonts.google.com/specimen/Karla+Tamil+Upright"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'KarlaTamilUpright-Bold.ttf'
-  font 'KarlaTamilUpright-Regular.ttf'
+  font "KarlaTamilUpright-Bold.ttf"
+  font "KarlaTamilUpright-Regular.ttf"
 end
