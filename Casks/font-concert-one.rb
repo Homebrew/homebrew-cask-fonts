7c2771c46d626ac124145859555b5f71964c4e13
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:34 +0100

    font-concert-one.rb: fix for new style

--- Casks/font-concert-one.rb
+++ Casks/font-concert-one.rb
@@ -1,11 +1,11 @@
-cask 'font-concert-one' do
+cask "font-concert-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/concertone/ConcertOne-Regular.ttf'
-  name 'Concert One'
-  homepage 'https://fonts.google.com/specimen/Concert+One'
+  url "https://github.com/google/fonts/raw/master/ofl/concertone/ConcertOne-Regular.ttf"
+  name "Concert One"
+  homepage "https://fonts.google.com/specimen/Concert+One"
 
-  font 'ConcertOne-Regular.ttf'
+  font "ConcertOne-Regular.ttf"
 end
