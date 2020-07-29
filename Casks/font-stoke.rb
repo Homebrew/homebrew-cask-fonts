a37174d510d453cf97821eafabbc95f2a675220c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:21 +0100

    font-stoke.rb: fix for new style

--- Casks/font-stoke.rb
+++ Casks/font-stoke.rb
@@ -1,16 +1,16 @@
-cask 'font-stoke' do
+cask "font-stoke" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/stoke',
+  url "https://github.com/google/fonts/trunk/ofl/stoke",
       using:      :svn,
       trust_cert: true
-  name 'Stoke'
-  homepage 'https://fonts.google.com/specimen/Stoke'
+  name "Stoke"
+  homepage "https://fonts.google.com/specimen/Stoke"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Stoke-Light.ttf'
-  font 'Stoke-Regular.ttf'
+  font "Stoke-Light.ttf"
+  font "Stoke-Regular.ttf"
 end
