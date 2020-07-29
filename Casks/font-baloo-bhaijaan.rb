31d9891cca6a8604e1e97c50d98867a5473d7be4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:37:37 +0100

    font-baloo-bhaijaan.rb: fix for new style

--- Casks/font-baloo-bhaijaan.rb
+++ Casks/font-baloo-bhaijaan.rb
@@ -1,11 +1,11 @@
-cask 'font-baloo-bhaijaan' do
+cask "font-baloo-bhaijaan" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/baloobhaijaan/BalooBhaijaan-Regular.ttf'
-  name 'Baloo Bhaijaan'
-  homepage 'https://fonts.google.com/specimen/Baloo+Bhaijaan'
+  url "https://github.com/google/fonts/raw/master/ofl/baloobhaijaan/BalooBhaijaan-Regular.ttf"
+  name "Baloo Bhaijaan"
+  homepage "https://fonts.google.com/specimen/Baloo+Bhaijaan"
 
-  font 'BalooBhaijaan-Regular.ttf'
+  font "BalooBhaijaan-Regular.ttf"
 end
