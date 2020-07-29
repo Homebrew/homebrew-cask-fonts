e1b48208ecbb75dc9d3cb2da5d043f5c12a6d8a1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:55 +0100

    font-noto-sans-old-persian.rb: fix for new style

--- Casks/font-noto-sans-old-persian.rb
+++ Casks/font-noto-sans-old-persian.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-old-persian' do
+cask "font-noto-sans-old-persian" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldPersian-unhinted.zip'
-  name 'Noto Sans Old Persian'
-  homepage 'https://www.google.com/get/noto/#sans-xpeo'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldPersian-unhinted.zip"
+  name "Noto Sans Old Persian"
+  homepage "https://www.google.com/get/noto/#sans-xpeo"
 
-  font 'NotoSansOldPersian-Regular.ttf'
+  font "NotoSansOldPersian-Regular.ttf"
 end
