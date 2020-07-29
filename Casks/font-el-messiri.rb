60fb52f489e8231abb727208abff04510772d331
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:47 +0100

    font-el-messiri.rb: fix for new style

--- Casks/font-el-messiri.rb
+++ Casks/font-el-messiri.rb
@@ -1,11 +1,11 @@
-cask 'font-el-messiri' do
+cask "font-el-messiri" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/elmessiri/ElMessiri%5Bwght%5D.ttf'
-  name 'El Messiri'
-  homepage 'https://fonts.google.com/specimen/El+Messiri'
+  url "https://github.com/google/fonts/raw/master/ofl/elmessiri/ElMessiri%5Bwght%5D.ttf"
+  name "El Messiri"
+  homepage "https://fonts.google.com/specimen/El+Messiri"
 
-  font 'ElMessiri[wght].ttf'
+  font "ElMessiri[wght].ttf"
 end
