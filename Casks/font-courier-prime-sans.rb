4dcef41e41465a9b3a9aea01e1c2931892bffca1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:37 +0100

    font-courier-prime-sans.rb: fix for new style

--- Casks/font-courier-prime-sans.rb
+++ Casks/font-courier-prime-sans.rb
@@ -1,13 +1,13 @@
-cask 'font-courier-prime-sans' do
+cask "font-courier-prime-sans" do
   version :latest
   sha256 :no_check
 
-  url 'https://quoteunquoteapps.com/courierprime/downloads/courier-prime-sans.zip'
-  name 'Courier Prime Sans'
-  homepage 'https://quoteunquoteapps.com/courierprime/#code-sans'
+  url "https://quoteunquoteapps.com/courierprime/downloads/courier-prime-sans.zip"
+  name "Courier Prime Sans"
+  homepage "https://quoteunquoteapps.com/courierprime/#code-sans"
 
-  font 'CourierPrimeSans-master/ttf/Courier Prime Sans Bold Italic.ttf'
-  font 'CourierPrimeSans-master/ttf/Courier Prime Sans Bold.ttf'
-  font 'CourierPrimeSans-master/ttf/Courier Prime Sans Italic.ttf'
-  font 'CourierPrimeSans-master/ttf/Courier Prime Sans.ttf'
+  font "CourierPrimeSans-master/ttf/Courier Prime Sans Bold Italic.ttf"
+  font "CourierPrimeSans-master/ttf/Courier Prime Sans Bold.ttf"
+  font "CourierPrimeSans-master/ttf/Courier Prime Sans Italic.ttf"
+  font "CourierPrimeSans-master/ttf/Courier Prime Sans.ttf"
 end
