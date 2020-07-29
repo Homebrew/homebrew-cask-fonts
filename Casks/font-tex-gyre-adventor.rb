765a19c3362ef71d3fa5789e85c9bcf84ecaa6e6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:23 +0100

    font-tex-gyre-adventor.rb: fix for new style

--- Casks/font-tex-gyre-adventor.rb
+++ Casks/font-tex-gyre-adventor.rb
@@ -1,10 +1,10 @@
-cask 'font-tex-gyre-adventor' do
-  version '2.501'
-  sha256 '9e619eb1c8af5cb55240f8bb865453562a2efd9059dee39d085fb71f7a00f7a2'
+cask "font-tex-gyre-adventor" do
+  version "2.501"
+  sha256 "9e619eb1c8af5cb55240f8bb865453562a2efd9059dee39d085fb71f7a00f7a2"
 
   url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor/qag#{version.dots_to_underscores}otf.zip"
-  name 'TeX Gyre Adventor'
-  homepage 'http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor'
+  name "TeX Gyre Adventor"
+  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/adventor"
 
   font "qag#{version.dots_to_underscores}otf/texgyreadventor-bold.otf"
   font "qag#{version.dots_to_underscores}otf/texgyreadventor-bolditalic.otf"
