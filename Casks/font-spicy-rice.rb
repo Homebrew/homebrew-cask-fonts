21ccfa80afb7042e115df5c28994f638c1424b8a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:20 +0100

    font-spicy-rice.rb: fix for new style

--- Casks/font-spicy-rice.rb
+++ Casks/font-spicy-rice.rb
@@ -1,11 +1,11 @@
-cask 'font-spicy-rice' do
+cask "font-spicy-rice" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/spicyrice/SpicyRice-Regular.ttf'
-  name 'Spicy Rice'
-  homepage 'https://fonts.google.com/specimen/Spicy+Rice'
+  url "https://github.com/google/fonts/raw/master/ofl/spicyrice/SpicyRice-Regular.ttf"
+  name "Spicy Rice"
+  homepage "https://fonts.google.com/specimen/Spicy+Rice"
 
-  font 'SpicyRice-Regular.ttf'
+  font "SpicyRice-Regular.ttf"
 end
