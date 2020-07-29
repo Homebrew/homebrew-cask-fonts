dd07a5e9e894f2166198bc04fb0b39292afb0caa
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:07 +0100

    font-gurajada.rb: fix for new style

--- Casks/font-gurajada.rb
+++ Casks/font-gurajada.rb
@@ -1,11 +1,11 @@
-cask 'font-gurajada' do
+cask "font-gurajada" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/gurajada/Gurajada-Regular.ttf'
-  name 'Gurajada'
-  homepage 'https://fonts.google.com/specimen/Gurajada'
+  url "https://github.com/google/fonts/raw/master/ofl/gurajada/Gurajada-Regular.ttf"
+  name "Gurajada"
+  homepage "https://fonts.google.com/specimen/Gurajada"
 
-  font 'Gurajada-Regular.ttf'
+  font "Gurajada-Regular.ttf"
 end
