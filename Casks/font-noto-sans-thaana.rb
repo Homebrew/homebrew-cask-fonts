aa47116f506458944e77e714ef4709cb967278c5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:57 +0100

    font-noto-sans-thaana.rb: fix for new style

--- Casks/font-noto-sans-thaana.rb
+++ Casks/font-noto-sans-thaana.rb
@@ -1,12 +1,12 @@
-cask 'font-noto-sans-thaana' do
+cask "font-noto-sans-thaana" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansThaana-unhinted.zip'
-  name 'Noto Sans Thaana'
-  homepage 'https://www.google.com/get/noto/#sans-thaa'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansThaana-unhinted.zip"
+  name "Noto Sans Thaana"
+  homepage "https://www.google.com/get/noto/#sans-thaa"
 
-  font 'NotoSansThaana-Bold.ttf'
-  font 'NotoSansThaana-Regular.ttf'
+  font "NotoSansThaana-Bold.ttf"
+  font "NotoSansThaana-Regular.ttf"
 end
