2f1624295696d34eecf391dfe8c57dc8c7026ef2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:08 +0100

    font-pt-serif-caption.rb: fix for new style

--- Casks/font-pt-serif-caption.rb
+++ Casks/font-pt-serif-caption.rb
@@ -1,16 +1,16 @@
-cask 'font-pt-serif-caption' do
+cask "font-pt-serif-caption" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/ptserifcaption',
+  url "https://github.com/google/fonts/trunk/ofl/ptserifcaption",
       using:      :svn,
       trust_cert: true
-  name 'PT Serif Caption'
-  homepage 'https://fonts.google.com/specimen/PT+Serif+Caption'
+  name "PT Serif Caption"
+  homepage "https://fonts.google.com/specimen/PT+Serif+Caption"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'PT_Serif-Caption-Web-Italic.ttf'
-  font 'PT_Serif-Caption-Web-Regular.ttf'
+  font "PT_Serif-Caption-Web-Italic.ttf"
+  font "PT_Serif-Caption-Web-Regular.ttf"
 end
