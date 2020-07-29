708e875f7e903453e97b95db2a19795c8e99c1e6
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:45 +0100

    font-montserrat-subrayada.rb: fix for new style

--- Casks/font-montserrat-subrayada.rb
+++ Casks/font-montserrat-subrayada.rb
@@ -1,16 +1,16 @@
-cask 'font-montserrat-subrayada' do
+cask "font-montserrat-subrayada" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/montserratsubrayada',
+  url "https://github.com/google/fonts/trunk/ofl/montserratsubrayada",
       using:      :svn,
       trust_cert: true
-  name 'Montserrat Subrayada'
-  homepage 'https://fonts.google.com/specimen/Montserrat+Subrayada'
+  name "Montserrat Subrayada"
+  homepage "https://fonts.google.com/specimen/Montserrat+Subrayada"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'MontserratSubrayada-Bold.ttf'
-  font 'MontserratSubrayada-Regular.ttf'
+  font "MontserratSubrayada-Bold.ttf"
+  font "MontserratSubrayada-Regular.ttf"
 end
