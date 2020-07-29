36550fbad3c62ef085ea04db8ac34c7f28c40fd3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:25 +0100

    font-trocchi.rb: fix for new style

--- Casks/font-trocchi.rb
+++ Casks/font-trocchi.rb
@@ -1,11 +1,11 @@
-cask 'font-trocchi' do
+cask "font-trocchi" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/trocchi/Trocchi-Regular.ttf'
-  name 'Trocchi'
-  homepage 'https://fonts.google.com/specimen/Trocchi'
+  url "https://github.com/google/fonts/raw/master/ofl/trocchi/Trocchi-Regular.ttf"
+  name "Trocchi"
+  homepage "https://fonts.google.com/specimen/Trocchi"
 
-  font 'Trocchi-Regular.ttf'
+  font "Trocchi-Regular.ttf"
 end
