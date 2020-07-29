dbde368bb62c94926f814356b2840a1e7ee89575
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:12 +0100

    font-romanesco.rb: fix for new style

--- Casks/font-romanesco.rb
+++ Casks/font-romanesco.rb
@@ -1,11 +1,11 @@
-cask 'font-romanesco' do
+cask "font-romanesco" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/romanesco/Romanesco-Regular.ttf'
-  name 'Romanesco'
-  homepage 'https://fonts.google.com/specimen/Romanesco'
+  url "https://github.com/google/fonts/raw/master/ofl/romanesco/Romanesco-Regular.ttf"
+  name "Romanesco"
+  homepage "https://fonts.google.com/specimen/Romanesco"
 
-  font 'Romanesco-Regular.ttf'
+  font "Romanesco-Regular.ttf"
 end
