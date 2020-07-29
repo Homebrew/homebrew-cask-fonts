4bb7c2cafa90e110493c4e6f33abf2b69d243313
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:43 +0100

    font-milonga.rb: fix for new style

--- Casks/font-milonga.rb
+++ Casks/font-milonga.rb
@@ -1,11 +1,11 @@
-cask 'font-milonga' do
+cask "font-milonga" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/milonga/Milonga-Regular.ttf'
-  name 'Milonga'
-  homepage 'https://fonts.google.com/specimen/Milonga'
+  url "https://github.com/google/fonts/raw/master/ofl/milonga/Milonga-Regular.ttf"
+  name "Milonga"
+  homepage "https://fonts.google.com/specimen/Milonga"
 
-  font 'Milonga-Regular.ttf'
+  font "Milonga-Regular.ttf"
 end
