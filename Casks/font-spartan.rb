83a72d5fe98c7e0486559957658335521c63c6df
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:20 +0100

    font-spartan.rb: fix for new style

--- Casks/font-spartan.rb
+++ Casks/font-spartan.rb
@@ -1,11 +1,11 @@
-cask 'font-spartan' do
+cask "font-spartan" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/spartan/Spartan%5Bwght%5D.ttf'
-  name 'Spartan'
-  homepage 'https://fonts.google.com/specimen/Spartan'
+  url "https://github.com/google/fonts/raw/master/ofl/spartan/Spartan%5Bwght%5D.ttf"
+  name "Spartan"
+  homepage "https://fonts.google.com/specimen/Spartan"
 
-  font 'Spartan[wght].ttf'
+  font "Spartan[wght].ttf"
 end
