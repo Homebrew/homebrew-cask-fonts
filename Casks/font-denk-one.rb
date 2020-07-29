a9f0fae5e1e2ea869dfb091cd2ebeeab53de58d1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:42 +0100

    font-denk-one.rb: fix for new style

--- Casks/font-denk-one.rb
+++ Casks/font-denk-one.rb
@@ -1,11 +1,11 @@
-cask 'font-denk-one' do
+cask "font-denk-one" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/denkone/DenkOne-Regular.ttf'
-  name 'Denk One'
-  homepage 'https://fonts.google.com/specimen/Denk+One'
+  url "https://github.com/google/fonts/raw/master/ofl/denkone/DenkOne-Regular.ttf"
+  name "Denk One"
+  homepage "https://fonts.google.com/specimen/Denk+One"
 
-  font 'DenkOne-Regular.ttf'
+  font "DenkOne-Regular.ttf"
 end
