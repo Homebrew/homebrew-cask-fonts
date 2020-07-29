65d7b153f7fbf11596268e3369cac20766811679
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:57 +0100

    font-noto-sans-tai-viet.rb: fix for new style

--- Casks/font-noto-sans-tai-viet.rb
+++ Casks/font-noto-sans-tai-viet.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-tai-viet' do
+cask "font-noto-sans-tai-viet" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiViet-unhinted.zip'
-  name 'Noto Sans Tai Viet'
-  homepage 'https://www.google.com/get/noto/#sans-tavt'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansTaiViet-unhinted.zip"
+  name "Noto Sans Tai Viet"
+  homepage "https://www.google.com/get/noto/#sans-tavt"
 
-  font 'NotoSansTaiViet-Regular.ttf'
+  font "NotoSansTaiViet-Regular.ttf"
 end
