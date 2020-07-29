857a337f04f4c7015cd7c5ff2718458ef7f6f43f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:48 +0100

    font-neodunggeunmo.rb: fix for new style

--- Casks/font-neodunggeunmo.rb
+++ Casks/font-neodunggeunmo.rb
@@ -1,12 +1,12 @@
-cask 'font-neodunggeunmo' do
-  version '1.50'
-  sha256 '67765532d17afe0c5b81bd0f6109e0d2619ba2f15c860f3e8cc9b561287a5b28'
+cask "font-neodunggeunmo" do
+  version "1.50"
+  sha256 "67765532d17afe0c5b81bd0f6109e0d2619ba2f15c860f3e8cc9b561287a5b28"
 
   # github.com/Dalgona/neodgm/ was verified as official when first introduced to the cask
   url "https://github.com/Dalgona/neodgm/releases/download/v#{version}/neodgm.ttf"
-  appcast 'https://github.com/Dalgona/neodgm/releases.atom'
-  name 'NeoDunggeunmo'
-  homepage 'https://dalgona.dev/neodgm'
+  appcast "https://github.com/Dalgona/neodgm/releases.atom"
+  name "NeoDunggeunmo"
+  homepage "https://dalgona.dev/neodgm"
 
-  font 'neodgm.ttf'
+  font "neodgm.ttf"
 end
