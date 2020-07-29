c11e8d0425e287aef603fd3c216e5b3363ea1999
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:16 +0100

    font-shojumaru.rb: fix for new style

--- Casks/font-shojumaru.rb
+++ Casks/font-shojumaru.rb
@@ -1,11 +1,11 @@
-cask 'font-shojumaru' do
+cask "font-shojumaru" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/shojumaru/Shojumaru-Regular.ttf'
-  name 'Shojumaru'
-  homepage 'https://fonts.google.com/specimen/Shojumaru'
+  url "https://github.com/google/fonts/raw/master/ofl/shojumaru/Shojumaru-Regular.ttf"
+  name "Shojumaru"
+  homepage "https://fonts.google.com/specimen/Shojumaru"
 
-  font 'Shojumaru-Regular.ttf'
+  font "Shojumaru-Regular.ttf"
 end
