c85bc8fbc8122bb559c5801c6db77da072b5f025
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:43 +0100

    font-diplomata-sc.rb: fix for new style

--- Casks/font-diplomata-sc.rb
+++ Casks/font-diplomata-sc.rb
@@ -1,11 +1,11 @@
-cask 'font-diplomata-sc' do
+cask "font-diplomata-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/diplomatasc/DiplomataSC-Regular.ttf'
-  name 'Diplomata SC'
-  homepage 'https://fonts.google.com/specimen/Diplomata+SC'
+  url "https://github.com/google/fonts/raw/master/ofl/diplomatasc/DiplomataSC-Regular.ttf"
+  name "Diplomata SC"
+  homepage "https://fonts.google.com/specimen/Diplomata+SC"
 
-  font 'DiplomataSC-Regular.ttf'
+  font "DiplomataSC-Regular.ttf"
 end
