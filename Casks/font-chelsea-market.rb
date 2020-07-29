97b18722d2eb85fa4a59691ae97b20421f54d921
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:30 +0100

    font-chelsea-market.rb: fix for new style

--- Casks/font-chelsea-market.rb
+++ Casks/font-chelsea-market.rb
@@ -1,11 +1,11 @@
-cask 'font-chelsea-market' do
+cask "font-chelsea-market" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/chelseamarket/ChelseaMarket-Regular.ttf'
-  name 'Chelsea Market'
-  homepage 'https://fonts.google.com/specimen/Chelsea+Market'
+  url "https://github.com/google/fonts/raw/master/ofl/chelseamarket/ChelseaMarket-Regular.ttf"
+  name "Chelsea Market"
+  homepage "https://fonts.google.com/specimen/Chelsea+Market"
 
-  font 'ChelseaMarket-Regular.ttf'
+  font "ChelseaMarket-Regular.ttf"
 end
