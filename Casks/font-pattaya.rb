ea62bae2d5e14bb9891a180bd50d62b38d69954c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:04 +0100

    font-pattaya.rb: fix for new style

--- Casks/font-pattaya.rb
+++ Casks/font-pattaya.rb
@@ -1,11 +1,11 @@
-cask 'font-pattaya' do
+cask "font-pattaya" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/pattaya/Pattaya-Regular.ttf'
-  name 'Pattaya'
-  homepage 'https://fonts.google.com/specimen/Pattaya'
+  url "https://github.com/google/fonts/raw/master/ofl/pattaya/Pattaya-Regular.ttf"
+  name "Pattaya"
+  homepage "https://fonts.google.com/specimen/Pattaya"
 
-  font 'Pattaya-Regular.ttf'
+  font "Pattaya-Regular.ttf"
 end
