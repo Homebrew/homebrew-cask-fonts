5c5d37b93bfd535752b72737be921a38582d920e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:25 +0100

    font-trebuchet-ms.rb: fix for new style

--- Casks/font-trebuchet-ms.rb
+++ Casks/font-trebuchet-ms.rb
@@ -1,13 +1,13 @@
-cask 'font-trebuchet-ms' do
+cask "font-trebuchet-ms" do
   version :latest
   sha256 :no_check
 
-  url 'https://downloads.sourceforge.net/corefonts/trebuc32.exe'
-  name 'Trebuchet MS'
-  homepage 'https://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  url "https://downloads.sourceforge.net/corefonts/trebuc32.exe"
+  name "Trebuchet MS"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
 
-  font 'trebuc.ttf'
-  font 'Trebucbd.ttf'
-  font 'trebucbi.ttf'
-  font 'trebucit.ttf'
+  font "trebuc.ttf"
+  font "Trebucbd.ttf"
+  font "trebucbi.ttf"
+  font "trebucit.ttf"
 end
