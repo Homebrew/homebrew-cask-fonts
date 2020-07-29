dbb6891738465945eb7c3a4656c1ca9497625dae
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:20 +0100

    font-sriracha.rb: fix for new style

--- Casks/font-sriracha.rb
+++ Casks/font-sriracha.rb
@@ -1,11 +1,11 @@
-cask 'font-sriracha' do
+cask "font-sriracha" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/sriracha/Sriracha-Regular.ttf'
-  name 'Sriracha'
-  homepage 'https://fonts.google.com/specimen/Sriracha'
+  url "https://github.com/google/fonts/raw/master/ofl/sriracha/Sriracha-Regular.ttf"
+  name "Sriracha"
+  homepage "https://fonts.google.com/specimen/Sriracha"
 
-  font 'Sriracha-Regular.ttf'
+  font "Sriracha-Regular.ttf"
 end
