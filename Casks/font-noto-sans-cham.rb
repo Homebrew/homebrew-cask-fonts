3a6a7fdd1fe354fa03e9d791a24d1cb15edceddf
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:51 +0100

    font-noto-sans-cham.rb: fix for new style

--- Casks/font-noto-sans-cham.rb
+++ Casks/font-noto-sans-cham.rb
@@ -1,19 +1,19 @@
-cask 'font-noto-sans-cham' do
+cask "font-noto-sans-cham" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCham-unhinted.zip'
-  name 'Noto Sans Cham'
-  homepage 'https://www.google.com/get/noto/#sans-cham'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCham-unhinted.zip"
+  name "Noto Sans Cham"
+  homepage "https://www.google.com/get/noto/#sans-cham"
 
-  font 'NotoSansCham-Black.ttf'
-  font 'NotoSansCham-Bold.ttf'
-  font 'NotoSansCham-ExtraBold.ttf'
-  font 'NotoSansCham-ExtraLight.ttf'
-  font 'NotoSansCham-Light.ttf'
-  font 'NotoSansCham-Medium.ttf'
-  font 'NotoSansCham-Regular.ttf'
-  font 'NotoSansCham-SemiBold.ttf'
-  font 'NotoSansCham-Thin.ttf'
+  font "NotoSansCham-Black.ttf"
+  font "NotoSansCham-Bold.ttf"
+  font "NotoSansCham-ExtraBold.ttf"
+  font "NotoSansCham-ExtraLight.ttf"
+  font "NotoSansCham-Light.ttf"
+  font "NotoSansCham-Medium.ttf"
+  font "NotoSansCham-Regular.ttf"
+  font "NotoSansCham-SemiBold.ttf"
+  font "NotoSansCham-Thin.ttf"
 end
