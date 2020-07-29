fb0d0e69f0aea16a75d2e256a1c9a7eed1540695
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:44 +0100

    font-monda.rb: fix for new style

--- Casks/font-monda.rb
+++ Casks/font-monda.rb
@@ -1,16 +1,16 @@
-cask 'font-monda' do
+cask "font-monda" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/monda',
+  url "https://github.com/google/fonts/trunk/ofl/monda",
       using:      :svn,
       trust_cert: true
-  name 'Monda'
-  homepage 'https://fonts.google.com/specimen/Monda'
+  name "Monda"
+  homepage "https://fonts.google.com/specimen/Monda"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Monda-Bold.ttf'
-  font 'Monda-Regular.ttf'
+  font "Monda-Bold.ttf"
+  font "Monda-Regular.ttf"
 end
