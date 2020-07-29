f55193f8a578dc95b8bdcb7b7d2e350a7375c47d
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:34 +0100

    font-libertinus.rb: fix for new style

--- Casks/font-libertinus.rb
+++ Casks/font-libertinus.rb
@@ -1,11 +1,11 @@
-cask 'font-libertinus' do
-  version '6.12'
-  sha256 '64249bc2ed80f41ffa78a5fdd396e53b4fb5601e469f0f93c842fc6a775ac9e7'
+cask "font-libertinus" do
+  version "6.12"
+  sha256 "64249bc2ed80f41ffa78a5fdd396e53b4fb5601e469f0f93c842fc6a775ac9e7"
 
   url "https://github.com/alif-type/libertinus/archive/v#{version}.tar.gz"
-  appcast 'https://github.com/alif-type/libertinus/releases.atom'
-  name 'Libertinus'
-  homepage 'https://github.com/alif-type/libertinus'
+  appcast "https://github.com/alif-type/libertinus/releases.atom"
+  name "Libertinus"
+  homepage "https://github.com/alif-type/libertinus"
 
   font "libertinus-#{version}/libertinuskeyboard-regular.otf"
   font "libertinus-#{version}/libertinusmath-regular.otf"
