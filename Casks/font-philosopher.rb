2fac4a2f1f6d1431f655b99fdf6269bad024aed4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:04 +0100

    font-philosopher.rb: fix for new style

--- Casks/font-philosopher.rb
+++ Casks/font-philosopher.rb
@@ -1,18 +1,18 @@
-cask 'font-philosopher' do
+cask "font-philosopher" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/philosopher',
+  url "https://github.com/google/fonts/trunk/ofl/philosopher",
       using:      :svn,
       trust_cert: true
-  name 'Philosopher'
-  homepage 'https://fonts.google.com/specimen/Philosopher'
+  name "Philosopher"
+  homepage "https://fonts.google.com/specimen/Philosopher"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Philosopher-Bold.ttf'
-  font 'Philosopher-BoldItalic.ttf'
-  font 'Philosopher-Italic.ttf'
-  font 'Philosopher-Regular.ttf'
+  font "Philosopher-Bold.ttf"
+  font "Philosopher-BoldItalic.ttf"
+  font "Philosopher-Italic.ttf"
+  font "Philosopher-Regular.ttf"
 end
