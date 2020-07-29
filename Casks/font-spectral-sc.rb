9286080866baa4546ee87c450eda1c604e18585c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:20 +0100

    font-spectral-sc.rb: fix for new style

--- Casks/font-spectral-sc.rb
+++ Casks/font-spectral-sc.rb
@@ -1,28 +1,28 @@
-cask 'font-spectral-sc' do
+cask "font-spectral-sc" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/spectralsc',
+  url "https://github.com/google/fonts/trunk/ofl/spectralsc",
       using:      :svn,
       trust_cert: true
-  name 'Spectral SC'
-  homepage 'https://fonts.google.com/specimen/Spectral+SC'
+  name "Spectral SC"
+  homepage "https://fonts.google.com/specimen/Spectral+SC"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'SpectralSC-Bold.ttf'
-  font 'SpectralSC-BoldItalic.ttf'
-  font 'SpectralSC-ExtraBold.ttf'
-  font 'SpectralSC-ExtraBoldItalic.ttf'
-  font 'SpectralSC-ExtraLight.ttf'
-  font 'SpectralSC-ExtraLightItalic.ttf'
-  font 'SpectralSC-Italic.ttf'
-  font 'SpectralSC-Light.ttf'
-  font 'SpectralSC-LightItalic.ttf'
-  font 'SpectralSC-Medium.ttf'
-  font 'SpectralSC-MediumItalic.ttf'
-  font 'SpectralSC-Regular.ttf'
-  font 'SpectralSC-SemiBold.ttf'
-  font 'SpectralSC-SemiBoldItalic.ttf'
+  font "SpectralSC-Bold.ttf"
+  font "SpectralSC-BoldItalic.ttf"
+  font "SpectralSC-ExtraBold.ttf"
+  font "SpectralSC-ExtraBoldItalic.ttf"
+  font "SpectralSC-ExtraLight.ttf"
+  font "SpectralSC-ExtraLightItalic.ttf"
+  font "SpectralSC-Italic.ttf"
+  font "SpectralSC-Light.ttf"
+  font "SpectralSC-LightItalic.ttf"
+  font "SpectralSC-Medium.ttf"
+  font "SpectralSC-MediumItalic.ttf"
+  font "SpectralSC-Regular.ttf"
+  font "SpectralSC-SemiBold.ttf"
+  font "SpectralSC-SemiBoldItalic.ttf"
 end
