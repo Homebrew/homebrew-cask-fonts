8986a9a304e3ff1c2076ff99917cbb26a5e982e0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:40 +0100

    font-cutive.rb: fix for new style

--- Casks/font-cutive.rb
+++ Casks/font-cutive.rb
@@ -1,11 +1,11 @@
-cask 'font-cutive' do
+cask "font-cutive" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/cutive/Cutive-Regular.ttf'
-  name 'Cutive'
-  homepage 'https://fonts.google.com/specimen/Cutive'
+  url "https://github.com/google/fonts/raw/master/ofl/cutive/Cutive-Regular.ttf"
+  name "Cutive"
+  homepage "https://fonts.google.com/specimen/Cutive"
 
-  font 'Cutive-Regular.ttf'
+  font "Cutive-Regular.ttf"
 end
