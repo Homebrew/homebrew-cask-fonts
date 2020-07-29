82af79defd83216fbf253d0a2ebcb5f72978baca
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:15 +0100

    font-impact.rb: fix for new style

--- Casks/font-impact.rb
+++ Casks/font-impact.rb
@@ -1,11 +1,11 @@
-cask 'font-impact' do
-  version '2.35'
-  sha256 '6061ef3b7401d9642f5dfdb5f2b376aa14663f6275e60a51207ad4facf2fccfb'
+cask "font-impact" do
+  version "2.35"
+  sha256 "6061ef3b7401d9642f5dfdb5f2b376aa14663f6275e60a51207ad4facf2fccfb"
 
-  url 'https://downloads.sourceforge.net/corefonts/impact32.exe'
-  appcast 'https://sourceforge.net/projects/corefonts/rss'
-  name 'Impact'
-  homepage 'https://sourceforge.net/projects/corefonts/files/the%20fonts/final/'
+  url "https://downloads.sourceforge.net/corefonts/impact32.exe"
+  appcast "https://sourceforge.net/projects/corefonts/rss"
+  name "Impact"
+  homepage "https://sourceforge.net/projects/corefonts/files/the%20fonts/final/"
 
-  font 'Impact.TTF'
+  font "Impact.TTF"
 end
