0c86dce7e6ae7b9c8741849b350738bca145125a
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:44 +0100

    font-mohave.rb: fix for new style

--- Casks/font-mohave.rb
+++ Casks/font-mohave.rb
@@ -1,16 +1,16 @@
-cask 'font-mohave' do
+cask "font-mohave" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/mohave',
+  url "https://github.com/google/fonts/trunk/ofl/mohave",
       using:      :svn,
       trust_cert: true
-  name 'Mohave'
-  homepage 'https://fonts.google.com/specimen/Mohave'
+  name "Mohave"
+  homepage "https://fonts.google.com/specimen/Mohave"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Mohave-Italic[wght].ttf'
-  font 'Mohave[wght].ttf'
+  font "Mohave-Italic[wght].ttf"
+  font "Mohave[wght].ttf"
 end
