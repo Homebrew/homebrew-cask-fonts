542775b8a0863cc4323bc8fb55a191439c948817
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:01 +0100

    font-abeezee.rb: fix for new style

--- Casks/font-abeezee.rb
+++ Casks/font-abeezee.rb
@@ -1,16 +1,16 @@
-cask 'font-abeezee' do
+cask "font-abeezee" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/abeezee',
+  url "https://github.com/google/fonts/trunk/ofl/abeezee",
       using:      :svn,
       trust_cert: true
-  name 'ABeeZee'
-  homepage 'https://fonts.google.com/specimen/ABeeZee'
+  name "ABeeZee"
+  homepage "https://fonts.google.com/specimen/ABeeZee"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'ABeeZee-Italic.ttf'
-  font 'ABeeZee-Regular.ttf'
+  font "ABeeZee-Italic.ttf"
+  font "ABeeZee-Regular.ttf"
 end
