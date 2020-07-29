ede2e5c7cc962b1db00f25f11c8d664b22d9c893
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:28 +0100

    font-caudex.rb: fix for new style

--- Casks/font-caudex.rb
+++ Casks/font-caudex.rb
@@ -1,18 +1,18 @@
-cask 'font-caudex' do
+cask "font-caudex" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/caudex',
+  url "https://github.com/google/fonts/trunk/ofl/caudex",
       using:      :svn,
       trust_cert: true
-  name 'Caudex'
-  homepage 'https://fonts.google.com/specimen/Caudex'
+  name "Caudex"
+  homepage "https://fonts.google.com/specimen/Caudex"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Caudex-Bold.ttf'
-  font 'Caudex-BoldItalic.ttf'
-  font 'Caudex-Italic.ttf'
-  font 'Caudex-Regular.ttf'
+  font "Caudex-Bold.ttf"
+  font "Caudex-BoldItalic.ttf"
+  font "Caudex-Italic.ttf"
+  font "Caudex-Regular.ttf"
 end
