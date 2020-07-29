66eea5705b7e2b4c5e699addb99088fb8c76f728
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:46 +0100

    font-eater.rb: fix for new style

--- Casks/font-eater.rb
+++ Casks/font-eater.rb
@@ -1,11 +1,11 @@
-cask 'font-eater' do
+cask "font-eater" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/eater/Eater-Regular.ttf'
-  name 'Eater'
-  homepage 'https://fonts.google.com/specimen/Eater'
+  url "https://github.com/google/fonts/raw/master/ofl/eater/Eater-Regular.ttf"
+  name "Eater"
+  homepage "https://fonts.google.com/specimen/Eater"
 
-  font 'Eater-Regular.ttf'
+  font "Eater-Regular.ttf"
 end
