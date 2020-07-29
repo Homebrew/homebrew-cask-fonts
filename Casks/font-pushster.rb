43ade0494a97115ce4805434f18c14015c2f9781
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:08 +0100

    font-pushster.rb: fix for new style

--- Casks/font-pushster.rb
+++ Casks/font-pushster.rb
@@ -1,11 +1,11 @@
-cask 'font-pushster' do
+cask "font-pushster" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/pushster/Pushster-Regular.ttf'
-  name 'Pushster'
-  homepage 'https://fonts.google.com/specimen/Pushster'
+  url "https://github.com/google/fonts/raw/master/ofl/pushster/Pushster-Regular.ttf"
+  name "Pushster"
+  homepage "https://fonts.google.com/specimen/Pushster"
 
-  font 'Pushster-Regular.ttf'
+  font "Pushster-Regular.ttf"
 end
