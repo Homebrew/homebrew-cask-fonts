4ff3bfb09e8b21b48fc36d881b1a424e9ae7854b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:23 +0100

    font-josefin-sans.rb: fix for new style

--- Casks/font-josefin-sans.rb
+++ Casks/font-josefin-sans.rb
@@ -1,16 +1,16 @@
-cask 'font-josefin-sans' do
+cask "font-josefin-sans" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/josefinsans',
+  url "https://github.com/google/fonts/trunk/ofl/josefinsans",
       using:      :svn,
       trust_cert: true
-  name 'Josefin Sans'
-  homepage 'https://fonts.google.com/specimen/Josefin+Sans'
+  name "Josefin Sans"
+  homepage "https://fonts.google.com/specimen/Josefin+Sans"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'JosefinSans-Italic[wght].ttf'
-  font 'JosefinSans[wght].ttf'
+  font "JosefinSans-Italic[wght].ttf"
+  font "JosefinSans[wght].ttf"
 end
