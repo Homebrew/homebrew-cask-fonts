69523b70583cff63b1c526233db344ff5f44cdb4
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:35 +0100

    font-libre-caslon-text.rb: fix for new style

--- Casks/font-libre-caslon-text.rb
+++ Casks/font-libre-caslon-text.rb
@@ -1,12 +1,12 @@
-cask 'font-libre-caslon-text' do
+cask "font-libre-caslon-text" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/impallari/Libre-Caslon-Text/archive/master.zip'
-  name 'Libre Caslon Text'
-  homepage 'https://github.com/impallari/Libre-Caslon-Text/'
+  url "https://github.com/impallari/Libre-Caslon-Text/archive/master.zip"
+  name "Libre Caslon Text"
+  homepage "https://github.com/impallari/Libre-Caslon-Text/"
 
-  font 'Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Bold.otf'
-  font 'Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Italic.otf'
-  font 'Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Regular.otf'
+  font "Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Bold.otf"
+  font "Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Italic.otf"
+  font "Libre-Caslon-Text-master/fonts/OTF/LibreCaslonText-Regular.otf"
 end
