2107ac70c5529ac074fd4aca7be7f93286bc7816
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:21 +0100

    font-black-han-sans.rb: fix for new style

--- Casks/font-black-han-sans.rb
+++ Casks/font-black-han-sans.rb
@@ -1,11 +1,11 @@
-cask 'font-black-han-sans' do
+cask "font-black-han-sans" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/blackhansans/BlackHanSans-Regular.ttf'
-  name 'Black Han Sans'
-  homepage 'https://fonts.google.com/specimen/Black+Han+Sans'
+  url "https://github.com/google/fonts/raw/master/ofl/blackhansans/BlackHanSans-Regular.ttf"
+  name "Black Han Sans"
+  homepage "https://fonts.google.com/specimen/Black+Han+Sans"
 
-  font 'BlackHanSans-Regular.ttf'
+  font "BlackHanSans-Regular.ttf"
 end
