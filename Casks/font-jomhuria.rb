f2bd015500da0a71fac5f37975e5f82f5a4e4a12
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:23 +0100

    font-jomhuria.rb: fix for new style

--- Casks/font-jomhuria.rb
+++ Casks/font-jomhuria.rb
@@ -1,11 +1,11 @@
-cask 'font-jomhuria' do
+cask "font-jomhuria" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/jomhuria/Jomhuria-Regular.ttf'
-  name 'Jomhuria'
-  homepage 'https://fonts.google.com/specimen/Jomhuria'
+  url "https://github.com/google/fonts/raw/master/ofl/jomhuria/Jomhuria-Regular.ttf"
+  name "Jomhuria"
+  homepage "https://fonts.google.com/specimen/Jomhuria"
 
-  font 'Jomhuria-Regular.ttf'
+  font "Jomhuria-Regular.ttf"
 end
