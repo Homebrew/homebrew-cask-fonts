522bcfb1ad50488991eb104dbb43058bdcd86142
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:26 +0100

    font-karla.rb: fix for new style

--- Casks/font-karla.rb
+++ Casks/font-karla.rb
@@ -1,16 +1,16 @@
-cask 'font-karla' do
+cask "font-karla" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/karla',
+  url "https://github.com/google/fonts/trunk/ofl/karla",
       using:      :svn,
       trust_cert: true
-  name 'Karla'
-  homepage 'https://fonts.google.com/specimen/Karla'
+  name "Karla"
+  homepage "https://fonts.google.com/specimen/Karla"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Karla-Italic[wght].ttf'
-  font 'Karla[wght].ttf'
+  font "Karla-Italic[wght].ttf"
+  font "Karla[wght].ttf"
 end
