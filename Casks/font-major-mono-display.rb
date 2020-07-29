11dc3b288888d81304329ff08461f34dbff3a97e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:38 +0100

    font-major-mono-display.rb: fix for new style

--- Casks/font-major-mono-display.rb
+++ Casks/font-major-mono-display.rb
@@ -1,11 +1,11 @@
-cask 'font-major-mono-display' do
+cask "font-major-mono-display" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/majormonodisplay/MajorMonoDisplay-Regular.ttf'
-  name 'Major Mono Display'
-  homepage 'https://fonts.google.com/specimen/Major+Mono+Display'
+  url "https://github.com/google/fonts/raw/master/ofl/majormonodisplay/MajorMonoDisplay-Regular.ttf"
+  name "Major Mono Display"
+  homepage "https://fonts.google.com/specimen/Major+Mono+Display"
 
-  font 'MajorMonoDisplay-Regular.ttf'
+  font "MajorMonoDisplay-Regular.ttf"
 end
