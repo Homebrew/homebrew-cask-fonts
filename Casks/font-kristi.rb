dcd89fbfb3e387cc3b636207b3427953a4317336
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:29 +0100

    font-kristi.rb: fix for new style

--- Casks/font-kristi.rb
+++ Casks/font-kristi.rb
@@ -1,11 +1,11 @@
-cask 'font-kristi' do
+cask "font-kristi" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/kristi/Kristi-Regular.ttf'
-  name 'Kristi'
-  homepage 'https://fonts.google.com/specimen/Kristi'
+  url "https://github.com/google/fonts/raw/master/ofl/kristi/Kristi-Regular.ttf"
+  name "Kristi"
+  homepage "https://fonts.google.com/specimen/Kristi"
 
-  font 'Kristi-Regular.ttf'
+  font "Kristi-Regular.ttf"
 end
