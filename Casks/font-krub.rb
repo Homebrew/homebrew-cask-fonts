a60fc855f6211574a85596ef977ac8ba38550aad
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:30 +0100

    font-krub.rb: fix for new style

--- Casks/font-krub.rb
+++ Casks/font-krub.rb
@@ -1,26 +1,26 @@
-cask 'font-krub' do
+cask "font-krub" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/krub',
+  url "https://github.com/google/fonts/trunk/ofl/krub",
       using:      :svn,
       trust_cert: true
-  name 'Krub'
-  homepage 'https://fonts.google.com/specimen/Krub'
+  name "Krub"
+  homepage "https://fonts.google.com/specimen/Krub"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Krub-Bold.ttf'
-  font 'Krub-BoldItalic.ttf'
-  font 'Krub-ExtraLight.ttf'
-  font 'Krub-ExtraLightItalic.ttf'
-  font 'Krub-Italic.ttf'
-  font 'Krub-Light.ttf'
-  font 'Krub-LightItalic.ttf'
-  font 'Krub-Medium.ttf'
-  font 'Krub-MediumItalic.ttf'
-  font 'Krub-Regular.ttf'
-  font 'Krub-SemiBold.ttf'
-  font 'Krub-SemiBoldItalic.ttf'
+  font "Krub-Bold.ttf"
+  font "Krub-BoldItalic.ttf"
+  font "Krub-ExtraLight.ttf"
+  font "Krub-ExtraLightItalic.ttf"
+  font "Krub-Italic.ttf"
+  font "Krub-Light.ttf"
+  font "Krub-LightItalic.ttf"
+  font "Krub-Medium.ttf"
+  font "Krub-MediumItalic.ttf"
+  font "Krub-Regular.ttf"
+  font "Krub-SemiBold.ttf"
+  font "Krub-SemiBoldItalic.ttf"
 end
