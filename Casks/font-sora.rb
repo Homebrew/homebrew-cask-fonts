53d02f388c0c628329b11da7b32019c20ff9d583
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:19 +0100

    font-sora.rb: fix for new style

--- Casks/font-sora.rb
+++ Casks/font-sora.rb
@@ -1,11 +1,11 @@
-cask 'font-sora' do
+cask "font-sora" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/sora/Sora%5Bwght%5D.ttf'
-  name 'Sora'
-  homepage 'https://fonts.google.com/specimen/Sora'
+  url "https://github.com/google/fonts/raw/master/ofl/sora/Sora%5Bwght%5D.ttf"
+  name "Sora"
+  homepage "https://fonts.google.com/specimen/Sora"
 
-  font 'Sora[wght].ttf'
+  font "Sora[wght].ttf"
 end
