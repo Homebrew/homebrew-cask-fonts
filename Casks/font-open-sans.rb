75371a54889e5c4ddb5ed4731529b13ae14adc5c
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:01 +0100

    font-open-sans.rb: fix for new style

--- Casks/font-open-sans.rb
+++ Casks/font-open-sans.rb
@@ -1,24 +1,24 @@
-cask 'font-open-sans' do
+cask "font-open-sans" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/apache/opensans',
+  url "https://github.com/google/fonts/trunk/apache/opensans",
       using:      :svn,
       trust_cert: true
-  name 'Open Sans'
-  homepage 'https://fonts.google.com/specimen/Open+Sans'
+  name "Open Sans"
+  homepage "https://fonts.google.com/specimen/Open+Sans"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'OpenSans-Bold.ttf'
-  font 'OpenSans-BoldItalic.ttf'
-  font 'OpenSans-ExtraBold.ttf'
-  font 'OpenSans-ExtraBoldItalic.ttf'
-  font 'OpenSans-Italic.ttf'
-  font 'OpenSans-Light.ttf'
-  font 'OpenSans-LightItalic.ttf'
-  font 'OpenSans-Regular.ttf'
-  font 'OpenSans-SemiBold.ttf'
-  font 'OpenSans-SemiBoldItalic.ttf'
+  font "OpenSans-Bold.ttf"
+  font "OpenSans-BoldItalic.ttf"
+  font "OpenSans-ExtraBold.ttf"
+  font "OpenSans-ExtraBoldItalic.ttf"
+  font "OpenSans-Italic.ttf"
+  font "OpenSans-Light.ttf"
+  font "OpenSans-LightItalic.ttf"
+  font "OpenSans-Regular.ttf"
+  font "OpenSans-SemiBold.ttf"
+  font "OpenSans-SemiBoldItalic.ttf"
 end
