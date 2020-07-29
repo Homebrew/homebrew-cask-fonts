8cff420474672a5096eb19f89784619cc8459331
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:40 +0100

    font-dangrek.rb: fix for new style

--- Casks/font-dangrek.rb
+++ Casks/font-dangrek.rb
@@ -1,11 +1,11 @@
-cask 'font-dangrek' do
+cask "font-dangrek" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/dangrek/Dangrek.ttf'
-  name 'Dangrek'
-  homepage 'https://fonts.google.com/specimen/Dangrek'
+  url "https://github.com/google/fonts/raw/master/ofl/dangrek/Dangrek.ttf"
+  name "Dangrek"
+  homepage "https://fonts.google.com/specimen/Dangrek"
 
-  font 'Dangrek.ttf'
+  font "Dangrek.ttf"
 end
