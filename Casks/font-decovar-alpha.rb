1def69fe45e03c29b89e51696201fe87e3deaed0
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:41 +0100

    font-decovar-alpha.rb: fix for new style

--- Casks/font-decovar-alpha.rb
+++ Casks/font-decovar-alpha.rb
@@ -1,11 +1,11 @@
-cask 'font-decovar-alpha' do
+cask "font-decovar-alpha" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/decovaralpha/DecovarAlpha-VF.ttf'
-  name 'Decovar Alpha'
-  homepage 'https://fonts.google.com/earlyaccess'
+  url "https://github.com/google/fonts/raw/master/ofl/decovaralpha/DecovarAlpha-VF.ttf"
+  name "Decovar Alpha"
+  homepage "https://fonts.google.com/earlyaccess"
 
-  font 'DecovarAlpha-VF.ttf'
+  font "DecovarAlpha-VF.ttf"
 end
