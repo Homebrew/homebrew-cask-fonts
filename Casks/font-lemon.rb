30d965efffcd06f21b5b07f93054d16e208fceda
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:33 +0100

    font-lemon.rb: fix for new style

--- Casks/font-lemon.rb
+++ Casks/font-lemon.rb
@@ -1,11 +1,11 @@
-cask 'font-lemon' do
+cask "font-lemon" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/lemon/Lemon-Regular.ttf'
-  name 'Lemon'
-  homepage 'https://fonts.google.com/specimen/Lemon'
+  url "https://github.com/google/fonts/raw/master/ofl/lemon/Lemon-Regular.ttf"
+  name "Lemon"
+  homepage "https://fonts.google.com/specimen/Lemon"
 
-  font 'Lemon-Regular.ttf'
+  font "Lemon-Regular.ttf"
 end
