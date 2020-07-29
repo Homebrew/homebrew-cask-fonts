b074e2ed3687fa4cb74261a65503a260013a7907
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:55 +0100

    font-noto-sans-old-south-arabian.rb: fix for new style

--- Casks/font-noto-sans-old-south-arabian.rb
+++ Casks/font-noto-sans-old-south-arabian.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-old-south-arabian' do
+cask "font-noto-sans-old-south-arabian" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldSouthArabian-unhinted.zip'
-  name 'Noto Sans Old South Arabian'
-  homepage 'https://www.google.com/get/noto/#sans-sarb'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOldSouthArabian-unhinted.zip"
+  name "Noto Sans Old South Arabian"
+  homepage "https://www.google.com/get/noto/#sans-sarb"
 
-  font 'NotoSansOldSouthArabian-Regular.ttf'
+  font "NotoSansOldSouthArabian-Regular.ttf"
 end
