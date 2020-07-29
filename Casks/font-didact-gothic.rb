f98c561cb323f83ee44819c8df6f2fd02f68bb78
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:43 +0100

    font-didact-gothic.rb: fix for new style

--- Casks/font-didact-gothic.rb
+++ Casks/font-didact-gothic.rb
@@ -1,11 +1,11 @@
-cask 'font-didact-gothic' do
+cask "font-didact-gothic" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/didactgothic/DidactGothic-Regular.ttf'
-  name 'Didact Gothic'
-  homepage 'https://fonts.google.com/specimen/Didact+Gothic'
+  url "https://github.com/google/fonts/raw/master/ofl/didactgothic/DidactGothic-Regular.ttf"
+  name "Didact Gothic"
+  homepage "https://fonts.google.com/specimen/Didact+Gothic"
 
-  font 'DidactGothic-Regular.ttf'
+  font "DidactGothic-Regular.ttf"
 end
