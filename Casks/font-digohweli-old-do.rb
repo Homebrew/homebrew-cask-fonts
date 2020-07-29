f41e6dcd499d33b90a06076b089528e2fcf1fde2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:43 +0100

    font-digohweli-old-do.rb: fix for new style

--- Casks/font-digohweli-old-do.rb
+++ Casks/font-digohweli-old-do.rb
@@ -1,10 +1,10 @@
-cask 'font-digohweli-old-do' do
+cask "font-digohweli-old-do" do
   version :latest
   sha256 :no_check
 
-  url 'https://www.languagegeek.com/font/OldDODigohweli.zip'
-  name 'Digohweli Old DO'
-  homepage 'https://www.languagegeek.com/font/fontdownload.html'
+  url "https://www.languagegeek.com/font/OldDODigohweli.zip"
+  name "Digohweli Old DO"
+  homepage "https://www.languagegeek.com/font/fontdownload.html"
 
-  font 'OldDODigohweli.ttf'
+  font "OldDODigohweli.ttf"
 end
