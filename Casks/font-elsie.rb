7066f7415666b5571b1a3ae6cf71578f969ce6a1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:47 +0100

    font-elsie.rb: fix for new style

--- Casks/font-elsie.rb
+++ Casks/font-elsie.rb
@@ -1,16 +1,16 @@
-cask 'font-elsie' do
+cask "font-elsie" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/elsie',
+  url "https://github.com/google/fonts/trunk/ofl/elsie",
       using:      :svn,
       trust_cert: true
-  name 'Elsie'
-  homepage 'https://fonts.google.com/specimen/Elsie'
+  name "Elsie"
+  homepage "https://fonts.google.com/specimen/Elsie"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Elsie-Black.ttf'
-  font 'Elsie-Regular.ttf'
+  font "Elsie-Black.ttf"
+  font "Elsie-Regular.ttf"
 end
