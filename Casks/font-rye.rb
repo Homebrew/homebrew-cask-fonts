20517791e395f71e39bb21311cb14c2dc2aec5c2
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:13 +0100

    font-rye.rb: fix for new style

--- Casks/font-rye.rb
+++ Casks/font-rye.rb
@@ -1,11 +1,11 @@
-cask 'font-rye' do
+cask "font-rye" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/rye/Rye-Regular.ttf'
-  name 'Rye'
-  homepage 'https://fonts.google.com/specimen/Rye'
+  url "https://github.com/google/fonts/raw/master/ofl/rye/Rye-Regular.ttf"
+  name "Rye"
+  homepage "https://fonts.google.com/specimen/Rye"
 
-  font 'Rye-Regular.ttf'
+  font "Rye-Regular.ttf"
 end
