f65e3cc8b795781734016eb1b737056aca33c1e5
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:54 +0100

    font-noto-sans-mandaic.rb: fix for new style

--- Casks/font-noto-sans-mandaic.rb
+++ Casks/font-noto-sans-mandaic.rb
@@ -1,11 +1,11 @@
-cask 'font-noto-sans-mandaic' do
+cask "font-noto-sans-mandaic" do
   version :latest
   sha256 :no_check
 
   # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url 'https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMandaic-unhinted.zip'
-  name 'Noto Sans Mandaic'
-  homepage 'https://www.google.com/get/noto/#sans-mand'
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMandaic-unhinted.zip"
+  name "Noto Sans Mandaic"
+  homepage "https://www.google.com/get/noto/#sans-mand"
 
-  font 'NotoSansMandaic-Regular.ttf'
+  font "NotoSansMandaic-Regular.ttf"
 end
