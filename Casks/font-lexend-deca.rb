71df3993649866b5709f16461b35f9fc012bb6c1
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:33 +0100

    font-lexend-deca.rb: fix for new style

--- Casks/font-lexend-deca.rb
+++ Casks/font-lexend-deca.rb
@@ -1,10 +1,10 @@
-cask 'font-lexend-deca' do
+cask "font-lexend-deca" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/ThomasJockin/lexend/raw/master/fonts/ttf/LexendDeca-Regular.ttf'
-  name 'Lexend Deca'
-  homepage 'https://github.com/ThomasJockin/lexend'
+  url "https://github.com/ThomasJockin/lexend/raw/master/fonts/ttf/LexendDeca-Regular.ttf"
+  name "Lexend Deca"
+  homepage "https://github.com/ThomasJockin/lexend"
 
-  font 'LexendDeca-Regular.ttf'
+  font "LexendDeca-Regular.ttf"
 end
