15f15e8e528c7db64a9a13cb9efee750c02dd7aa
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:26 +0100

    font-cambo.rb: fix for new style

--- Casks/font-cambo.rb
+++ Casks/font-cambo.rb
@@ -1,11 +1,11 @@
-cask 'font-cambo' do
+cask "font-cambo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/cambo/Cambo-Regular.ttf'
-  name 'Cambo'
-  homepage 'https://fonts.google.com/specimen/Cambo'
+  url "https://github.com/google/fonts/raw/master/ofl/cambo/Cambo-Regular.ttf"
+  name "Cambo"
+  homepage "https://fonts.google.com/specimen/Cambo"
 
-  font 'Cambo-Regular.ttf'
+  font "Cambo-Regular.ttf"
 end
