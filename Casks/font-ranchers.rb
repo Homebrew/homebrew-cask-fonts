f95bbc2c80b7c8a2a2539478f622d65a97a266d3
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:09 +0100

    font-ranchers.rb: fix for new style

--- Casks/font-ranchers.rb
+++ Casks/font-ranchers.rb
@@ -1,11 +1,11 @@
-cask 'font-ranchers' do
+cask "font-ranchers" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/ranchers/Ranchers-Regular.ttf'
-  name 'Ranchers'
-  homepage 'https://fonts.google.com/specimen/Ranchers'
+  url "https://github.com/google/fonts/raw/master/ofl/ranchers/Ranchers-Regular.ttf"
+  name "Ranchers"
+  homepage "https://fonts.google.com/specimen/Ranchers"
 
-  font 'Ranchers-Regular.ttf'
+  font "Ranchers-Regular.ttf"
 end
