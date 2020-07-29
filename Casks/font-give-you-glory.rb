714d58f4bd5acec670576d0902634f758771195e
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:02 +0100

    font-give-you-glory.rb: fix for new style

--- Casks/font-give-you-glory.rb
+++ Casks/font-give-you-glory.rb
@@ -1,11 +1,11 @@
-cask 'font-give-you-glory' do
+cask "font-give-you-glory" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/giveyouglory/GiveYouGlory.ttf'
-  name 'Give You Glory'
-  homepage 'https://fonts.google.com/specimen/Give+You+Glory'
+  url "https://github.com/google/fonts/raw/master/ofl/giveyouglory/GiveYouGlory.ttf"
+  name "Give You Glory"
+  homepage "https://fonts.google.com/specimen/Give+You+Glory"
 
-  font 'GiveYouGlory.ttf'
+  font "GiveYouGlory.ttf"
 end
