0781fa322615097ad734768b66cbf38904490709
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:41:16 +0100

    font-inconsolata-lgc.rb: fix for new style

--- Casks/font-inconsolata-lgc.rb
+++ Casks/font-inconsolata-lgc.rb
@@ -1,17 +1,17 @@
-cask 'font-inconsolata-lgc' do
+cask "font-inconsolata-lgc" do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/DeLaGuardo/Inconsolata-LGC/trunk',
+  url "https://github.com/DeLaGuardo/Inconsolata-LGC/trunk",
       using:      :svn,
       trust_cert: true
-  name 'Inconsolata LGC'
-  homepage 'https://github.com/DeLaGuardo/Inconsolata-LGC'
+  name "Inconsolata LGC"
+  homepage "https://github.com/DeLaGuardo/Inconsolata-LGC"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'inconsolatalgc.ttf'
-  font 'inconsolatalgcbold.ttf'
-  font 'inconsolatalgcbolditalic.ttf'
-  font 'inconsolatalgcitalic.ttf'
+  font "inconsolatalgc.ttf"
+  font "inconsolatalgcbold.ttf"
+  font "inconsolatalgcbolditalic.ttf"
+  font "inconsolatalgcitalic.ttf"
 end
