5e3903e4026a3570034751be0dd30786670eb66b
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:42:12 +0100

    font-rosarivo.rb: fix for new style

--- Casks/font-rosarivo.rb
+++ Casks/font-rosarivo.rb
@@ -1,16 +1,16 @@
-cask 'font-rosarivo' do
+cask "font-rosarivo" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/trunk/ofl/rosarivo',
+  url "https://github.com/google/fonts/trunk/ofl/rosarivo",
       using:      :svn,
       trust_cert: true
-  name 'Rosarivo'
-  homepage 'https://fonts.google.com/specimen/Rosarivo'
+  name "Rosarivo"
+  homepage "https://fonts.google.com/specimen/Rosarivo"
 
-  depends_on macos: '>= :sierra'
+  depends_on macos: ">= :sierra"
 
-  font 'Rosarivo-Italic.ttf'
-  font 'Rosarivo-Regular.ttf'
+  font "Rosarivo-Italic.ttf"
+  font "Rosarivo-Regular.ttf"
 end
