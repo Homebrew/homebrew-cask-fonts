7b8e1fdfaeb859dac58014d8fe161a75b4bc5de7
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:40:19 +0100

    font-beth-ellen.rb: fix for new style

--- Casks/font-beth-ellen.rb
+++ Casks/font-beth-ellen.rb
@@ -1,11 +1,11 @@
-cask 'font-beth-ellen' do
+cask "font-beth-ellen" do
   version :latest
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url 'https://github.com/google/fonts/raw/master/ofl/bethellen/BethEllen-Regular.ttf'
-  name 'Beth Ellen'
-  homepage 'https://fonts.google.com/specimen/Beth+Ellen'
+  url "https://github.com/google/fonts/raw/master/ofl/bethellen/BethEllen-Regular.ttf"
+  name "Beth Ellen"
+  homepage "https://fonts.google.com/specimen/Beth+Ellen"
 
-  font 'BethEllen-Regular.ttf'
+  font "BethEllen-Regular.ttf"
 end
