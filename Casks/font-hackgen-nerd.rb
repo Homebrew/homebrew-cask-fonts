036efc7430dab0227d3531f47ef187cf9c5eeefb
Yushi Sako <oskamathis@gmail.com>
2020-07-31 23:16:04 +0900

    Add HackGenNerd Fonts (#2238)
    * Add HackGenNerd Fonts
    
    * Fix font name

---
+++ Casks/font-hackgen-nerd.rb
@@ -0,0 +1,18 @@
+cask "font-hackgen-nerd" do
+  version "2.1.0"
+  sha256 "678e1597448ccf75ba29a1f47600f5aded00ab13220ca3db99ad3b1266e5dfa4"
+
+  url "https://github.com/yuru7/HackGen/releases/download/v#{version}/HackGenNerd_v#{version}.zip"
+  appcast "https://github.com/yuru7/HackGen/releases.atom"
+  name "HackGenNerd"
+  homepage "https://github.com/yuru7/HackGen"
+
+  font "HackGenNerd_v#{version}/HackGen35Nerd-Bold.ttf"
+  font "HackGenNerd_v#{version}/HackGen35Nerd-Regular.ttf"
+  font "HackGenNerd_v#{version}/HackGen35NerdConsole-Bold.ttf"
+  font "HackGenNerd_v#{version}/HackGen35NerdConsole-Regular.ttf"
+  font "HackGenNerd_v#{version}/HackGenNerd-Bold.ttf"
+  font "HackGenNerd_v#{version}/HackGenNerd-Regular.ttf"
+  font "HackGenNerd_v#{version}/HackGenNerdConsole-Bold.ttf"
+  font "HackGenNerd_v#{version}/HackGenNerdConsole-Regular.ttf"
+end
