8f174fecfee6e43d5f98ff51d664478c4f01e02f
Kazuhiro NISHIYAMA <znz@users.noreply.github.com>
2020-07-31 23:05:18 +0900

    Update font-cica from 5.0.1 to 5.0.2 (#2243)

--- Casks/font-cica.rb
+++ Casks/font-cica.rb
@@ -1,6 +1,6 @@
 cask "font-cica" do
-  version "5.0.1"
-  sha256 "b61ae55fb1a4a98dd78e90fe68649bcfaabb20a5eaaffa57c246356890c6b0df"
+  version "5.0.2"
+  sha256 "b5d2eaa5841f629ed8cfd239aa30f5a4b45aa1aa2ef9f4c2b3f715f3d59bb8de"
 
   url "https://github.com/miiton/Cica/releases/download/v#{version}/Cica_v#{version}_with_emoji.zip"
   appcast "https://github.com/miiton/Cica/releases.atom"
