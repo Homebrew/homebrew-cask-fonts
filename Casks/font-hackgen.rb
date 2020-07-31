d0e82dc8400c4508008c84bc3eaabb3ae38c1171
Yushi Sako <oskamathis@gmail.com>
2020-07-31 23:16:40 +0900

    Update font-hackgen from 1.4.0 to 2.1.0 (#2237)
    * Update font-hackgen from 1.4.0 to 2.1.0
    
    * Insert final newline
    
    * Remove final newline

--- Casks/font-hackgen.rb
+++ Casks/font-hackgen.rb
@@ -1,6 +1,6 @@
 cask "font-hackgen" do
-  version "1.4.0"
-  sha256 "c21de5388fa94c7d2dbd16111524fa871dd0f2a3908941fb2200154cab48972e"
+  version "2.1.0"
+  sha256 "a1ccb44e72abb7bf0bc3dd6bc208cc9514d5b22e3096010cf82ebccecc1d0384"
 
   url "https://github.com/yuru7/HackGen/releases/download/v#{version}/HackGen_v#{version}.zip"
   appcast "https://github.com/yuru7/HackGen/releases.atom"
@@ -11,12 +11,8 @@ cask "font-hackgen" do
   font "HackGen_v#{version}/HackGen-Regular.ttf"
   font "HackGen_v#{version}/HackGen35-Bold.ttf"
   font "HackGen_v#{version}/HackGen35-Regular.ttf"
-  font "HackGen_v#{version}/HackGen35Console-Bold-forPowerline.ttf"
   font "HackGen_v#{version}/HackGen35Console-Bold.ttf"
-  font "HackGen_v#{version}/HackGen35Console-Regular-forPowerline.ttf"
   font "HackGen_v#{version}/HackGen35Console-Regular.ttf"
-  font "HackGen_v#{version}/HackGenConsole-Bold-forPowerline.ttf"
   font "HackGen_v#{version}/HackGenConsole-Bold.ttf"
-  font "HackGen_v#{version}/HackGenConsole-Regular-forPowerline.ttf"
   font "HackGen_v#{version}/HackGenConsole-Regular.ttf"
 end
