7b8e417102f22f2ec8adab20ad8c92d9c1e66ecf
Youfu Zhang <1315097+zhangyoufu@users.noreply.github.com>
2020-08-14 00:26:07 +0800

    Update font-ubuntu.rb (#2256)

--- Casks/font-ubuntu.rb
+++ Casks/font-ubuntu.rb
@@ -1,6 +1,6 @@
 cask "font-ubuntu" do
-  version "0.83,fad7939b"
-  sha256 "456d7d42797febd0d7d4cf1b782a2e03680bb4a5ee43cc9d06bda172bac05b42"
+  version "0.83,0cef8205"
+  sha256 "61a2b342526fd552f19fef438bb9211a8212de19ad96e32a1209c039f1d68ecf"
 
   url "https://assets.ubuntu.com/v1/#{version.after_comma}-ubuntu-font-family-#{version.before_comma}.zip"
   name "Ubuntu"
@@ -15,6 +15,7 @@ cask "font-ubuntu" do
   font "ubuntu-font-family-#{version.before_comma}/Ubuntu-MI.ttf"
   font "ubuntu-font-family-#{version.before_comma}/Ubuntu-R.ttf"
   font "ubuntu-font-family-#{version.before_comma}/Ubuntu-RI.ttf"
+  font "ubuntu-font-family-#{version.before_comma}/Ubuntu-Th.ttf"
   font "ubuntu-font-family-#{version.before_comma}/UbuntuMono-B.ttf"
   font "ubuntu-font-family-#{version.before_comma}/UbuntuMono-BI.ttf"
   font "ubuntu-font-family-#{version.before_comma}/UbuntuMono-R.ttf"
