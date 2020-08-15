011d0a60093331b0712b759404f40eb2130863e1
Markus Reiter <me@reitermark.us>
2020-08-15 06:53:25 +0200

    Update font-epilogue.rb

--- Casks/font-epilogue.rb
+++ Casks/font-epilogue.rb
@@ -7,7 +7,7 @@ cask "font-epilogue" do
       using:      :svn,
       trust_cert: true
   name "Epilogue"
-  desc "Led by tyler finck — type designer running etcetera type co in ithaca, new-york, usa"
+  desc "Sans serif variable font with a weight axis"
   homepage "https://fonts.google.com/specimen/Epilogue"
 
   font "Epilogue-Italic[wght].ttf"
