9eee24eb62ff9638ff13b98d5de569549cb59972
Markus Reiter <me@reitermark.us>
2020-08-17 01:02:49 +0200

    Update font-signika.rb

--- Casks/font-signika.rb
+++ Casks/font-signika.rb
@@ -5,6 +5,7 @@ cask "font-signika" do
   # github.com/google/fonts/ was verified as official when first introduced to the cask
   url "https://github.com/google/fonts/raw/master/ofl/signika/Signika%5Bwght%5D.ttf"
   name "Signika"
+  desc "Sans-serif signage typeface with a gentle character"
   homepage "https://fonts.google.com/specimen/Signika"
 
   font "Signika[wght].ttf"
