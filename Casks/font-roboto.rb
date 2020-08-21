6e5d658c3d66e04616b20e46a2a57d9f5634954d
Yurii Kolesnykov <root@yurikoles.com>
2020-08-21 17:24:07 +0300

    fix roboto path (#2265)
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/font-roboto.rb
+++ Casks/font-roboto.rb
@@ -3,7 +3,7 @@ cask "font-roboto" do
   sha256 :no_check
 
   # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/trunk/ofl/roboto",
+  url "https://github.com/google/fonts/trunk/apache/roboto",
       using:      :svn,
       trust_cert: true
   name "Roboto"
