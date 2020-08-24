a829889904fe8945c9d7c2f66042d8ae03f508b9
Reno <25192197+singularitti@users.noreply.github.com>
2020-08-24 13:47:34 -0400

    Update font-iosevka-sparkle from 3.3.1 to 3.4.6 (#2276)

--- Casks/font-iosevka-sparkle.rb
+++ Casks/font-iosevka-sparkle.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-sparkle" do
-  version "3.3.1"
-  sha256 "35224c3a95f9a96d2afabe548a09ecfaa50cafb63308cd90fe227d51beaab554"
+  version "3.4.6"
+  sha256 "7200ea7e8d1e4b4f5768a0d5941de37298e16a23d0479cd1189cf2aa886c3457"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-sparkle-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
