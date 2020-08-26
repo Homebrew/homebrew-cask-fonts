381e1b3d84701dbead78cf2abd5ec6ae05b372e5
Max <msal.coding@gmail.com>
2020-08-27 00:23:58 +0200

    Update font-cascadia variants from 2007.01 to 2008.25

--- Casks/font-cascadia-mono.rb
+++ Casks/font-cascadia-mono.rb
@@ -1,11 +1,16 @@
 cask "font-cascadia-mono" do
-  version "2007.01"
-  sha256 "9f066d0d9cb2669bea2e130d7add43d496bf24ef995f42dc603fc2014574a3a4"
+  version "2008.25"
+  sha256 "4602a0b8b986b18a1c3f460fd959ec1ce522aedcf11ab4c0d41a5fc5ed839be0"
 
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
   appcast "https://github.com/microsoft/cascadia-code/releases.atom"
   name "Cascadia Mono"
   homepage "https://github.com/microsoft/cascadia-code"
 
-  font "ttf/CascadiaMono.ttf"
+  font "ttf/static/CascadiaMono-Bold.ttf"
+  font "ttf/static/CascadiaMono-ExtraLight.ttf"
+  font "ttf/static/CascadiaMono-Light.ttf"
+  font "ttf/static/CascadiaMono-Regular.ttf"
+  font "ttf/static/CascadiaMono-SemiBold.ttf"
+  font "ttf/static/CascadiaMono-SemiLight.ttf"
 end
--- Casks/font-cascadia-pl.rb
+++ Casks/font-cascadia-pl.rb
@@ -1,11 +1,16 @@
 cask "font-cascadia-pl" do
-  version "2007.01"
-  sha256 "9f066d0d9cb2669bea2e130d7add43d496bf24ef995f42dc603fc2014574a3a4"
+  version "2008.25"
+  sha256 "4602a0b8b986b18a1c3f460fd959ec1ce522aedcf11ab4c0d41a5fc5ed839be0"
 
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
   appcast "https://github.com/microsoft/cascadia-code/releases.atom"
   name "Cascadia PL"
   homepage "https://github.com/microsoft/cascadia-code"
 
-  font "ttf/CascadiaCodePL.ttf"
+  font "ttf/static/CascadiaCodePL-Bold.ttf"
+  font "ttf/static/CascadiaCodePL-ExtraLight.ttf"
+  font "ttf/static/CascadiaCodePL-Light.ttf"
+  font "ttf/static/CascadiaCodePL-Regular.ttf"
+  font "ttf/static/CascadiaCodePL-SemiBold.ttf"
+  font "ttf/static/CascadiaCodePL-SemiLight.ttf"
 end
