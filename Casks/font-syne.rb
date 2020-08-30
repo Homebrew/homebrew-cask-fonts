439ba2804985ced3637ef647a8a33bac7d4b2a6f
Markus Reiter <me@reitermark.us>
2020-08-30 13:35:23 +0200

    Update font-syne.rb

--- Casks/font-syne.rb
+++ Casks/font-syne.rb
@@ -5,7 +5,7 @@ cask "font-syne" do
   # github.com/google/fonts/ was verified as official when first introduced to the cask
   url "https://github.com/google/fonts/raw/master/ofl/syne/Syne%5Bwght%5D.ttf"
   name "Syne"
-  desc "Assemblage of personalities — they can live all together, in small groups or on their own and will always create a surprising and distinctive visual situation"
+  desc "Typeface originally designed for the art center Synesthésie"
   homepage "https://fonts.google.com/specimen/Syne"
 
   font "Syne[wght].ttf"
