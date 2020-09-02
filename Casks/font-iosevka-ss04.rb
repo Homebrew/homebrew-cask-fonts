e21879c5a6252f1ac64e02ea7a821f05323ac8a8
Reno <25192197+singularitti@users.noreply.github.com>
2020-09-02 12:50:56 -0400

    Update font-iosevka-ss04 from 3.4.6 to 3.4.7 (#2316)

--- Casks/font-iosevka-ss04.rb
+++ Casks/font-iosevka-ss04.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss04" do
-  version "3.4.6"
-  sha256 "a62b7578b0ad5ed9cea1498dd2861da6165307eb7007cf8e8be1b403aa4b1535"
+  version "3.4.7"
+  sha256 "c39cac10d20c7928887ba5cf4e00cc68b4d810e7b1106fc9b531d4401ca0c084"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss04-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
