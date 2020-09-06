2cc6c4cd7f91146ae32a7aa5f002b19a80248e83
Markus Reiter <me@reitermark.us>
2020-09-06 17:12:41 +0200

    Update font-sansita-swashed.rb

--- Casks/font-sansita-swashed.rb
+++ Casks/font-sansita-swashed.rb
@@ -5,7 +5,7 @@ cask "font-sansita-swashed" do
   # github.com/google/fonts/ was verified as official when first introduced to the cask
   url "https://github.com/google/fonts/raw/master/ofl/sansitaswashed/SansitaSwashed%5Bwght%5D.ttf"
   name "Sansita Swashed"
-  desc "Designed by pablo cosgaya (omnibus-type) and developed by aldo de losa"
+  desc "Beautiful display font in vintage form"
   homepage "https://fonts.google.com/specimen/Sansita+Swashed"
 
   font "SansitaSwashed[wght].ttf"
