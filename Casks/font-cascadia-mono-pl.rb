4935c2420612394b888fc936b710ff57a73aa7e9
Max <msal.coding@gmail.com>
2020-09-17 15:00:03 +0200

    Update font-cascadia variants from 2008.25 to 2009.14

--- Casks/font-cascadia-mono-pl.rb
+++ Casks/font-cascadia-mono-pl.rb
@@ -1,6 +1,6 @@
 cask "font-cascadia-mono-pl" do
-  version "2008.25"
-  sha256 "4602a0b8b986b18a1c3f460fd959ec1ce522aedcf11ab4c0d41a5fc5ed839be0"
+  version "2009.14"
+  sha256 "7a08523d2d68ef3c6efdd5336dd49aa2698dd348fa4734463a17719632bbc3c8"
 
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
   appcast "https://github.com/microsoft/cascadia-code/releases.atom"
