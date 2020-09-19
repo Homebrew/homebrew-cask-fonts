111a644f7561ce6ef76241f916483503387fd23d
singularitti <singularitti@outlook.com>
2020-09-19 15:10:11 -0400

    Update font-iosevka-ss06 from 3.6.0 to 3.6.1

--- Casks/font-iosevka-ss06.rb
+++ Casks/font-iosevka-ss06.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss06" do
-  version "3.6.0"
-  sha256 "054acc280226a7f1d6d7e45e7f6afea1f4c90facd95642d43f79f4148c71ca80"
+  version "3.6.1"
+  sha256 "65bb87ebfd6b61bbbb5833eefc18a0a892b8e024e754db32f11765f095c783ae"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss06-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
