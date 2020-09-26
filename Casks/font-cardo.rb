e1f9f9ed7ca864f223f903bc8b4abacebe8f2923
Viktor Szakats <vszakats@users.noreply.github.com>
2020-09-26 04:13:32 +0200

    font-cardo: secure urls (#2436)

--- Casks/font-cardo.rb
+++ Casks/font-cardo.rb
@@ -2,9 +2,9 @@ cask "font-cardo" do
   version "1.04"
   sha256 "9401db6357cb71fa1f8791323679f81d6b0473d6280a7ec8abdf11b5e52f455f"
 
-  url "http://scholarsfonts.net/cardo#{version.no_dots}.zip"
+  url "https://scholarsfonts.net/cardo#{version.no_dots}.zip"
   name "Cardo"
-  homepage "http://scholarsfonts.net/cardofnt.html"
+  homepage "https://scholarsfonts.net/cardofnt.html"
 
   font "Cardo104s.ttf"
   font "Cardoi99.ttf"
