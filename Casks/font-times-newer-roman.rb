3e94c38f5bbb302eef37e06ff3d9bd2206f65488
FIGBERT <figbert@figbert.com>
2020-10-01 10:04:55 -0700

    Add Times Newer Roman version 1.0 (#2464)

---
+++ Casks/font-times-newer-roman.rb
@@ -0,0 +1,14 @@
+cask "font-times-newer-roman" do
+  version "1.000"
+  sha256 "2f4d66839d5b72105269d3a9d019ec19f0946280b33b2437460364c0d98dadba"
+
+  url "https://timesnewerroman.com/assets/TimesNewerRoman.zip"
+  name "Times Newer Roman"
+  desc "Looks just like Times New Roman, but characters are 5-10% wider"
+  homepage "https://timesnewerroman.com/"
+
+  font "TimesNewerRoman-Bold.otf"
+  font "TimesNewerRoman-BoldItalic.otf"
+  font "TimesNewerRoman-Italic.otf"
+  font "TimesNewerRoman-Regular.otf"
+end
