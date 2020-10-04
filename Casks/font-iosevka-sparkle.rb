b57668299b2d4593b68cf9e387dbef7318c4b5cb
singularitti <singularitti@outlook.com>
2020-10-04 14:35:15 -0400

    Update font-iosevka-sparkle from 3.6.2 to 3.6.3

--- Casks/font-iosevka-sparkle.rb
+++ Casks/font-iosevka-sparkle.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-sparkle" do
-  version "3.6.2"
-  sha256 "0e6e2d617111d5eca98b0bec358395f7bdae258eef787155b7166181553ab444"
+  version "3.6.3"
+  sha256 "2cfeb907176346ce4240478b432579599630f0ced0c9071d53db32441d70b474"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-sparkle-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
