c6339051c3414c20a0963862d15eb5c4b85e7ffe
singularitti <singularitti@outlook.com>
2020-10-04 14:29:06 -0400

    Update font-iosevka-ss12 from 3.6.2 to 3.6.3

--- Casks/font-iosevka-ss12.rb
+++ Casks/font-iosevka-ss12.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss12" do
-  version "3.6.2"
-  sha256 "2cb0724aae4b3c00f3d01610c406b7ef26145dad420015f8bbcd25fdfbe52a7b"
+  version "3.6.3"
+  sha256 "526a572b88b367ac1858b6a659f4fbdf40f55cc60f3ed3e861842d87957a2432"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss12-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
