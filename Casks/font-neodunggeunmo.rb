cf2cb626a322b3c383df7be304a2cc56113ae4b5
An Nyeong <me@annyeong.me>
2020-10-09 03:14:23 +0900

    Update font-neodunggeunmo[-code] to 1.510 (#2490)

--- Casks/font-neodunggeunmo.rb
+++ Casks/font-neodunggeunmo.rb
@@ -1,11 +1,12 @@
 cask "font-neodunggeunmo" do
-  version "1.50"
-  sha256 "67765532d17afe0c5b81bd0f6109e0d2619ba2f15c860f3e8cc9b561287a5b28"
+  version "1.510"
+  sha256 "16f5ccff456d2a4e1baec5315e263a177c2a2dbfae265a01084f6b7f9a532a44"
 
   # github.com/Dalgona/neodgm/ was verified as official when first introduced to the cask
   url "https://github.com/Dalgona/neodgm/releases/download/v#{version}/neodgm.ttf"
   appcast "https://github.com/Dalgona/neodgm/releases.atom"
   name "NeoDunggeunmo"
+  desc "Modern TrueType font based on an old-but-good Korean bitmap font"
   homepage "https://neodgm.dalgona.dev/"
 
   font "neodgm.ttf"
