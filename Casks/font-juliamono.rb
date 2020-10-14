a3c899fcce397c5097073c3e024ebee15c623063
Reno <25192197+singularitti@users.noreply.github.com>
2020-10-14 13:50:02 -0400

    Update font-juliamono from 0.020 to 0.021 (#2500)

--- Casks/font-juliamono.rb
+++ Casks/font-juliamono.rb
@@ -1,6 +1,6 @@
 cask "font-juliamono" do
-  version "0.020"
-  sha256 "ece2e5e51f56599d36699a877e75ef214b1a7a27bf4e0997fb70ae763478342b"
+  version "0.021"
+  sha256 "f81f7494638c5f0a2b0cd72c4322e95bad8a1c77786c6280d836afcf3f04a474"
 
   url "https://github.com/cormullion/juliamono/releases/download/v#{version}/JuliaMono.tar.gz"
   appcast "https://github.com/cormullion/juliamono/releases.atom"
