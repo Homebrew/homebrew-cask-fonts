a9c60bea48d0c6ce850a37d405389d14d0d4382e
yhu266 <56977205+yhu266@users.noreply.github.com>
2020-10-15 20:14:29 -0700

    Fix "font source not there"

--- Casks/font-atkinson-hyperlegible.rb
+++ Casks/font-atkinson-hyperlegible.rb
@@ -1,14 +1,14 @@
 cask "font-atkinson-hyperlegible" do
-  version :latest
+  version "2020-0514"
   sha256 "9e1a65d53ddadc8253791e958a07bf5aba210ef6155ea9c91b2b4c46bbce53e3"
 
-  url "https://www.brailleinstitute.org/wp-content/uploads/atkinson-hyperlegible-font/Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514.zip"
+  url "https://www.brailleinstitute.org/wp-content/uploads/atkinson-hyperlegible-font/Atkinson-Hyperlegible-Font-Print-and-Web-#{version}.zip"
   name "Atkinson Hyperlegible"
   desc "Atkinson Hyperlegible font focuses on letterform distinction to increase character recognition, ultimately improving readability."
   homepage "https://www.brailleinstitute.org/freefont"
 
-  font "Print\ Fonts/Atkinson-Hyperlegible-Bold-102.otf"
-  font "Print\ Fonts/Atkinson-Hyperlegible-BoldItalic-102.otf"
-  font "Print\ Fonts/Atkinson-Hyperlegible-Italic-102.otf"
-  font "Print\ Fonts/Atkinson-Hyperlegible-Regular-102.otf"
+  font "Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Print Fonts/Atkinson-Hyperlegible-Bold-102.otf"
+  font "Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Print Fonts/Atkinson-Hyperlegible-BoldItalic-102.otf"
+  font "Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Print Fonts/Atkinson-Hyperlegible-Italic-102.otf"
+  font "Atkinson-Hyperlegible-Font-Print-and-Web-2020-0514/Print Fonts/Atkinson-Hyperlegible-Regular-102.otf"
 end
