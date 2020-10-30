e79f16466519cddf2338425abb7640bff16b9577
Caleb Maclennan <caleb@alerque.com>
2020-10-30 05:36:38 +0300

    Update recent releases from The League of Moveable Type (#2556)
    * Update font-league-spartan from 2.201 to 2.220
    
    * Update font-raleway from unofficial to 4.101
    
    * Add font-league-mono
    
    * Add missing descriptions to LOMT font packages

--- Casks/font-league-spartan.rb
+++ Casks/font-league-spartan.rb
@@ -1,12 +1,21 @@
 cask "font-league-spartan" do
-  version "2.201"
-  sha256 "6c027fb675c3479cc50da28bdd43bdae0debcb40eabdd42a659d4df8bb315d0a"
+  version "2.220"
+  sha256 "141a96e215554871504dca16be159901bbb0d56f3a84224f39fe472c7ab4ef47"
 
   # github.com/theleagueof/league-spartan/ was verified as official when first introduced to the cask
-  url "https://github.com/theleagueof/league-spartan/archive/#{version}.zip"
+  url "https://github.com/theleagueof/league-spartan/releases/download/#{version}/LeagueSpartan-#{version}.tar.xz"
   appcast "https://github.com/theleagueof/league-spartan/releases.atom"
   name "League Spartan"
+  desc "Geometric sans-serif revival of ATF’s classic Spartan"
   homepage "https://www.theleagueofmoveabletype.com/league-spartan"
 
-  font "league-spartan-#{version}/fonts/variable/LeagueSpartanVariable.ttf"
+  font "LeagueSpartan-#{version}/static/OTF/LeagueSpartan-ExtraLight.otf"
+  font "LeagueSpartan-#{version}/static/OTF/LeagueSpartan-Light.otf"
+  font "LeagueSpartan-#{version}/static/OTF/LeagueSpartan-Medium.otf"
+  font "LeagueSpartan-#{version}/static/OTF/LeagueSpartan-Regular.otf"
+  font "LeagueSpartan-#{version}/static/OTF/LeagueSpartan-SemiBold.otf"
+  font "LeagueSpartan-#{version}/static/OTF/LeagueSpartan-Bold.otf"
+  font "LeagueSpartan-#{version}/static/OTF/LeagueSpartan-ExtraBold.otf"
+  font "LeagueSpartan-#{version}/static/OTF/LeagueSpartan-Black.otf"
+  font "LeagueSpartan-#{version}/variable/TTF/LeagueSpartan-VF.ttf"
 end
