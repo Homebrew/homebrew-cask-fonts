0c88396e4c39d3de6ef531a18904248f906f3773
Shalin Okada <shalinokada@gmail.com>
2020-11-03 18:37:58 +0900

    :art: descとhomepageの順番を入れ替え

--- Casks/font-monaco-for-powerline.rb
+++ Casks/font-monaco-for-powerline.rb
@@ -5,8 +5,8 @@ cask "font-monaco-for-powerline" do
   # github.com/google/fonts/ was verified as official when first introduced to the cask
   url "https://gist.github.com/baopham/1838072/raw/616d338cea8b9dcc3a5b17c12fe3070df1b738c0/Monaco%2520for%2520Powerline.otf"
   name "Monaco for Powerline"
-  homepage "https://gist.github.com/baopham/1838072"
   desc "Powerline font of Monaco"
+  homepage "https://gist.github.com/baopham/1838072"
 
   font "Monaco for Powerline.otf"
 end
