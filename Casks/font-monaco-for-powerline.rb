bf64f8418bc52e7ceb59d4ed5bcefffa9d873c29
Shalin Okada <shalinokada@gmail.com>
2020-11-03 18:28:04 +0900

    :bug: add a descreption

--- Casks/font-monaco-for-powerline.rb
+++ Casks/font-monaco-for-powerline.rb
@@ -6,6 +6,7 @@ cask "font-monaco-for-powerline" do
   url "https://gist.github.com/baopham/1838072/raw/616d338cea8b9dcc3a5b17c12fe3070df1b738c0/Monaco%2520for%2520Powerline.otf"
   name "Monaco for Powerline"
   homepage "https://gist.github.com/baopham/1838072"
+  desc "Powerline font of Monaco"
 
   font "Monaco for Powerline.otf"
 end
