43c9c8e5ad6f3f0bd379cfc0f463d77561d9884d
Tomy Hsieh <tomy0000000@gmail.com>
2020-11-05 14:57:41 +0800

    Add mamelon

---
+++ Casks/font-mamelon.rb
@@ -0,0 +1,14 @@
+cask "font-mamelon" do
+  version :latest
+  sha256 :no_check
+
+  url "https://moji-waku.com/download/mamelon_hireg.zip"
+  name "Mamelon"
+  desc "Dailylife-styled round gothic font for electronic device"
+  homepage "https://moji-waku.com/mamelon/index.html"
+
+  font "mamelon_hireg/Mamelon-3HiRegular.otf"
+  font "mamelon_hireg/Mamelon-3.5HiRegular.otf"
+  font "mamelon_hireg/Mamelon-4HiRegular.otf"
+  font "mamelon_hireg/Mamelon-5HiRegular.otf"
+end
