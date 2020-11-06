32f378901b63fc2b84cd69e0ca01499647bd19d6
Vítor Galvão <info@vitorgalvao.com>
2020-11-06 16:59:37 +0000

    Update font-source-sans-pro.rb

--- Casks/font-source-sans-pro.rb
+++ Casks/font-source-sans-pro.rb
@@ -8,18 +8,6 @@ cask "font-source-sans-pro" do
   desc "Fonts designed for user interfaces."
   homepage "https://github.com/adobe-fonts/source-sans-pro"
 
-  font "OTF/SourceSans3-Black.otf"
-  font "OTF/SourceSans3-BlackIt.otf"
-  font "OTF/SourceSans3-Bold.otf"
-  font "OTF/SourceSans3-BoldIt.otf"
-  font "OTF/SourceSans3-ExtraLight.otf"
-  font "OTF/SourceSans3-ExtraLightIt.otf"
-  font "OTF/SourceSans3-It.otf"
-  font "OTF/SourceSans3-Light.otf"
-  font "OTF/SourceSans3-LightIt.otf"
-  font "OTF/SourceSans3-Regular.otf"
-  font "OTF/SourceSans3-Semibold.otf"
-  font "OTF/SourceSans3-SemiboldIt.otf"
   font "VAR/SourceSans3VF-Italic.otf"
   font "VAR/SourceSans3VF-Roman.otf"
 end
