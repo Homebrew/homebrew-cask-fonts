12ac54fead7189c4477d7f4bd9548bf44725e068
singularitti <singularitti@outlook.com>
2020-11-08 01:37:00 -0500

    Update font-iosevka-ss12 from 3.7.1 to 4.0.0-alpha.1

--- Casks/font-iosevka-ss12.rb
+++ Casks/font-iosevka-ss12.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss12" do
-  version "3.7.1"
-  sha256 "12d28ab234a6efc775927f4f0b32b5b0b9ea32b6cc02b28c29d9682084b017b9"
+  version "4.0.0-alpha.1"
+  sha256 "a0b4ba9d05675bcafb56bc6988e41d0d2551061870bfe4987c78d101b7b9bb9d"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss12-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
