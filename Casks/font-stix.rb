56131e6329dc40daeab39dac1f6eca6a27333e08
Alexander Ronald Altman <alexanderaltman@me.com>
2020-11-09 19:02:30 -0800

    Update font-stix from 2.0.0 to 2.0.2

--- Casks/font-stix.rb
+++ Casks/font-stix.rb
@@ -1,9 +1,9 @@
 cask "font-stix" do
-  version "2.0.0"
-  sha256 "667e3187a22cc63ba0de7083fc69c7920737cd44a44ff5eb2921cbd188531aa9"
+  version "2.0.2"
+  sha256 "b8eb0e63739e839ad620c82db1f6f38d8927f7fd30abcc8f147700ba3bc71918"
 
   # github.com/stipub/stixfonts/ was verified as official when first introduced to the cask
-  url "https://github.com/stipub/stixfonts/archive/#{version}.tar.gz"
+  url "https://github.com/stipub/stixfonts/archive/v#{version}.tar.gz"
   appcast "https://github.com/stipub/stixfonts/releases.atom"
   name "STIX"
   homepage "https://stixfonts.org/"
