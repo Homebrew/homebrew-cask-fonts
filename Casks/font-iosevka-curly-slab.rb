807ff4534dda75eab34193c0bb8c733e310f3b0f
singularitti <singularitti@outlook.com>
2020-11-14 08:11:27 -0500

    Update font-iosevka-curly-slab from 4.0.0-alpha.1 to 4.0.0-beta.1

--- Casks/font-iosevka-curly-slab.rb
+++ Casks/font-iosevka-curly-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly-slab" do
-  version "4.0.0-alpha.1"
-  sha256 "3f6ed31264785f8bb1997bbc3f2129627c0da15bbef230353b13ce0ca1c0de2c"
+  version "4.0.0-beta.1"
+  sha256 "7b249f632ad7859e8c3e76ae2b416a448bfdaff478a37710e4957162d82b44a9"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-slab-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
