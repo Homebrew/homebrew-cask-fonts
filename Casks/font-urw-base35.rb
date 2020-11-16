ca1db1c71a1a12e72c92d2cb59de27cfc73ddc06
Markus Reiter <me@reitermark.us>
2020-11-16 02:37:15 +0100

    Fix RuboCop offenses.

--- Casks/font-urw-base35.rb
+++ Casks/font-urw-base35.rb
@@ -1,5 +1,5 @@
 cask "font-urw-base35" do
-  # note: "35" is not a version number, but an intrinsic part of the product name
+  # NOTE: "35" is not a version number, but an intrinsic part of the product name
   version "20170801"
   sha256 "4505042c8859166f5bff77e33907e244b66eb4e04b56646e14e0a97e5757cd21"
 
