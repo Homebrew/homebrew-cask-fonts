c633e2a9507ca4dc2c8095c297a1c3ed49bfe7ed
Reno <25192197+singularitti@users.noreply.github.com>
2020-11-21 06:16:32 -0500

    Update font-iosevka-ss12 from 4.0.0-beta.2 to 4.0.0-beta.3 (#2658)

--- Casks/font-iosevka-ss12.rb
+++ Casks/font-iosevka-ss12.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss12" do
-  version "4.0.0-beta.2"
-  sha256 "1bb2f9ce97f751580620e8ecb689944cd6d2c66f6acd87cce1b1b1939bbd5329"
+  version "4.0.0-beta.3"
+  sha256 "aa5424a02b40ecf08044d0c08662be78bc09bf508793c8fc147676578b4df100"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss12-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
