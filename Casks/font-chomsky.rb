602a8d2f28d7977f8155f82ebdce089c1aa42a51
yhu266 <56977205+yhu266@users.noreply.github.com>
2020-11-21 21:54:47 -0800

    font-chomsky: add appcast stanza

--- Casks/font-chomsky.rb
+++ Casks/font-chomsky.rb
@@ -3,6 +3,7 @@ cask "font-chomsky" do
   sha256 "8011db20146d3bf36ca39015e5d091158ed7f3a856170d389c76f4ab32ae9ab9"
 
   url "https://github.com/ctrlcctrlv/chomsky/releases/download/v#{version}/Chomsky.otf"
+  appcast "https://github.com/ctrlcctrlv/chomsky/releases.atom"
   name "Chomsky"
   desc "New York Times masthead, reimagined as a full font"
   homepage "https://github.com/ctrlcctrlv/chomsky"
