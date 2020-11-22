ed88a5a009c46b5a1beee6d7c66f51abcad3a4d8
Tomy Hsieh <tomy0000000@gmail.com>
2020-11-22 15:09:04 +0800

    Add font-pigmo

---
+++ Casks/font-pigmo.rb
@@ -0,0 +1,11 @@
+cask "font-pigmo" do
+  version :latest
+  sha256 :no_check
+
+  url "https://moji-waku.com/download/pigmo00.zip"
+  name "Pigmo"
+  desc "Cute drawing styled Japanese font"
+  homepage "https://moji-waku.com/pigmo/index.html"
+
+  font "pigmo00/Pigmo-00.otf"
+end
