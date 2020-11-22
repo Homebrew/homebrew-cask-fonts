e4abf8fd02033b6ef572fe7bdced7ae2a9f1fbd1
yhu266 <56977205+yhu266@users.noreply.github.com>
2020-11-21 21:19:52 -0800

    font-tt2020: fix style

--- Casks/font-tt2020.rb
+++ Casks/font-tt2020.rb
@@ -1,11 +1,11 @@
-cask 'font-tt2020' do
-  version '0.2'
-  sha256 '7f3d04477db50553e65b37800b5c72201c852f28c1aa2caccb5baf1f79854ac3'
+cask "font-tt2020" do
+  version "0.2"
+  sha256 "7f3d04477db50553e65b37800b5c72201c852f28c1aa2caccb5baf1f79854ac3"
 
   url "https://github.com/ctrlcctrlv/TT2020/archive/v#{version}.zip"
   appcast "https://github.com/ctrlcctrlv/TT2020/releases.atom"
   name "TT2020"
-  desc "Advanced, open source, hyperrealistic, multilingual typewriter font for a new decade"
+  desc "Advanced, open source, hyperrealistic, multilingual typewriter font"
   homepage "https://github.com/ctrlcctrlv/TT2020"
 
   font "TT2020-#{version}/dist/TT2020Base-Italic.ttf"
