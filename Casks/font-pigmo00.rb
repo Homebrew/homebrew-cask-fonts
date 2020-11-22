5aa4e587881c44100921f675e11f845297e65c3f
Markus Reiter <me@reitermark.us>
2020-11-22 20:58:38 +0100

    Update and rename font-pigmo.rb to font-pigmo00.rb

--- Casks/font-pigmo00.rb
+++ Casks/font-pigmo00.rb
@@ -1,9 +1,9 @@
-cask "font-pigmo" do
+cask "font-pigmo00" do
   version :latest
   sha256 :no_check
 
   url "https://moji-waku.com/download/pigmo00.zip"
-  name "Pigmo"
+  name "Pigmo00"
   desc "Cute drawing styled Japanese font"
   homepage "https://moji-waku.com/pigmo/index.html"
 
