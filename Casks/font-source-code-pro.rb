bb35769c022b85251a2f3448a7769de4dc386d5d
Andrew Dunning <adunning@users.noreply.github.com>
2020-11-23 14:20:10 +0000

    Source Code Pro: Include variable version
    As with Source Serif, the variable version installs as Source Code Variable and does not conflict with Source Code Pro.

--- Casks/font-source-code-pro.rb
+++ Casks/font-source-code-pro.rb
@@ -2,11 +2,10 @@ cask "font-source-code-pro" do
   version "2.032R-ro-1.052R-it-1.012R-VAR"
   sha256 "4d28868a2dc4cd6b767a91cd3d8462983aa8924ae18149f3b1d1a135090a0f8d"
 
-  # github.com/adobe-fonts/source-code-pro/ was verified as official when first introduced to the cask
   url "https://github.com/adobe-fonts/source-code-pro/archive/#{version.gsub(/(?<=ro)-|(?<=it)-/, "/")}.zip"
   appcast "https://github.com/adobe-fonts/source-code-pro/releases.atom"
   name "Source Code Pro"
-  homepage "https://adobe-fonts.github.io/source-code-pro/"
+  homepage "https://github.com/adobe-fonts/source-code-pro"
 
   font "source-code-pro-#{version}/OTF/SourceCodePro-Black.otf"
   font "source-code-pro-#{version}/OTF/SourceCodePro-BlackIt.otf"
@@ -22,4 +21,6 @@ cask "font-source-code-pro" do
   font "source-code-pro-#{version}/OTF/SourceCodePro-Regular.otf"
   font "source-code-pro-#{version}/OTF/SourceCodePro-Semibold.otf"
   font "source-code-pro-#{version}/OTF/SourceCodePro-SemiboldIt.otf"
+  font "source-code-pro-#{version}/VAR/SourceCodeVariable-Italic.otf"
+  font "source-code-pro-#{version}/VAR/SourceCodeVariable-Roman.otf"
 end
