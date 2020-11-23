cd2c00fd0337ecd1cbf801888b274b19ddbe154a
Padraic Fanning <fanninpm@miamioh.edu>
2020-11-22 20:49:12 -0500

    Remove trailing whitespace

--- Casks/font-commissioner.rb
+++ Casks/font-commissioner.rb
@@ -1,6 +1,6 @@
 cask "font-commissioner" do
   version "1.000"
-  sha256 "0e317c1183643062eb2f86f98d22e6da7234b66ad2ed57a59c831ad3abd233e1" 
+  sha256 "0e317c1183643062eb2f86f98d22e6da7234b66ad2ed57a59c831ad3abd233e1"
 
   url "https://github.com/kosbarts/Commissioner/releases/download/#{version}/Commissioner_#{version}.zip"
   appcast "https://github.com/kosbarts/Commissioner/releases.atom"
