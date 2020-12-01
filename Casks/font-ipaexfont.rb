7a7e34b3087993356f2bcfc6e8275e9bba6ecc4b
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-01 14:01:39 +0000

    Update font-ipaexfont

--- Casks/font-ipaexfont.rb
+++ Casks/font-ipaexfont.rb
@@ -2,9 +2,9 @@ cask "font-ipaexfont" do
   version "004.01"
   sha256 "bcf8374ab3f9672c421120430dd19a51c99f5265cf06fc340d9a661ddfd7974b"
 
-  url "https://ipafont.ipa.go.jp/IPAexfont/IPAexfont#{version.no_dots}.zip"
+  url "https://moji.or.jp/wp-content/ipafont/IPAexfont/IPAexfont#{version.no_dots}.zip"
   name "IPAex Fonts"
-  homepage "https://ipafont.ipa.go.jp/"
+  homepage "https://moji.or.jp/ipafont/"
 
   font "IPAexfont#{version.no_dots}/ipaexg.ttf"
   font "IPAexfont#{version.no_dots}/ipaexm.ttf"
