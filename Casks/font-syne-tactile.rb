bd3670af7fa2782e2c28824b0e16c7127c91c0f3
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-05 16:51:40 +0800

    Update font-syne-tactile.rb

--- Casks/font-syne-tactile.rb
+++ Casks/font-syne-tactile.rb
@@ -2,8 +2,8 @@ cask "font-syne-tactile" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/synetactile/SyneTactile-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/synetactile/SyneTactile-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Syne Tactile"
   desc "Originally designed for the Art Center 'Synesthésie'"
   homepage "https://fonts.google.com/specimen/Syne+Tactile"
