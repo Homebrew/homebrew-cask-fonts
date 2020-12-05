ceaf55d9f52fe5d4003ffaab2ff355de6ef669fd
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-05 16:49:53 +0800

    Update font-langar.rb

--- Casks/font-langar.rb
+++ Casks/font-langar.rb
@@ -2,8 +2,8 @@ cask "font-langar" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/langar/Langar-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/langar/Langar-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Langar"
   desc "One-weight latin/gurmukhi display font based on informal, playful letterforms"
   homepage "https://fonts.google.com/specimen/Langar"
