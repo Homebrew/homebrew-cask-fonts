e0f790ef600ef87fe5e3be28ab894034aa6fba2f
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-05 16:47:54 +0800

    Update font-ballet.rb

--- Casks/font-ballet.rb
+++ Casks/font-ballet.rb
@@ -3,7 +3,7 @@ cask "font-ballet" do
   sha256 :no_check
 
   url "https://github.com/google/fonts/raw/master/ofl/ballet/Ballet%5Bopsz%5D.ttf",
-      verified:   "github.com/google/fonts/"
+      verified: "github.com/google/fonts/"
   name "Ballet"
   desc "Designed by maximiliano sproviero and developed by eduardo tunni"
   homepage "https://fonts.google.com/specimen/Ballet"
