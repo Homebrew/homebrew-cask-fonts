22740a732c6127c33bd565c3ce9e3fe26fbb6684
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-05 16:28:53 +0800

    Update font-big-shoulders-stencil-display.rb

--- Casks/font-big-shoulders-stencil-display.rb
+++ Casks/font-big-shoulders-stencil-display.rb
@@ -7,6 +7,7 @@ cask "font-big-shoulders-stencil-display" do
       using:      :svn,
       trust_cert: true
   name "Big Shoulders Stencil Display"
+  desc "Superfamily of condensed American Gothic typefaces"
   homepage "https://fonts.google.com/specimen/Big+Shoulders+Stencil+Display"
 
   font "BigShouldersStencilDisplay-Black.ttf"
