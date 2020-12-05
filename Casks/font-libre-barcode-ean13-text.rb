8599b5f55217de9c20b93ea6ef915a5614e08806
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-05 16:50:22 +0800

    Update font-libre-barcode-ean13-text.rb

--- Casks/font-libre-barcode-ean13-text.rb
+++ Casks/font-libre-barcode-ean13-text.rb
@@ -2,8 +2,8 @@ cask "font-libre-barcode-ean13-text" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/librebarcodeean13text/LibreBarcodeEAN13Text-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/librebarcodeean13text/LibreBarcodeEAN13Text-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Libre Barcode EAN13 Text"
   desc "Led by lasse fister, a font and web developer based in nuremberg, germany"
   homepage "https://fonts.google.com/specimen/Libre+Barcode+EAN13+Text"
