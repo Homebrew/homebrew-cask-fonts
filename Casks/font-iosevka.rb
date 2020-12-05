501ff22fdab58fcbe357715af1844d861b00b9de
Reno <25192197+singularitti@users.noreply.github.com>
2020-12-05 05:12:29 -0500

    Update font-iosevka from 4.0.0 to 4.0.1 (#2730)

--- Casks/font-iosevka.rb
+++ Casks/font-iosevka.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka" do
-  version "4.0.0"
-  sha256 "689e97f69cf207203d232f14382fa746bfeb5988c73d24fd2fcfcd93d1559417"
+  version "4.0.1"
+  sha256 "8c78e447cfc2a5ba864c73114656929a11dc92c95c4b4858aa0935fb35f2b3f0"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
