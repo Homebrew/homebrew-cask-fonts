a03e5bc92377ff29d4063f1c7c2f5e4e913a605b
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-05 16:46:48 +0800

    Update font-andika-new-basic.rb

--- Casks/font-andika-new-basic.rb
+++ Casks/font-andika-new-basic.rb
@@ -2,10 +2,10 @@ cask "font-andika-new-basic" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url "https://github.com/google/fonts/trunk/ofl/andikanewbasic",
       using:      :svn,
-      trust_cert: true
+      trust_cert: true,
+      verified:   "github.com/google/fonts/"
   name "Andika New Basic"
   desc "Limited-character-set version of andika"
   homepage "https://fonts.google.com/specimen/Andika+New+Basic"
