d7cccf4c357c88b2b2c206404327dc69f0d468f2
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-05 16:49:31 +0800

    Update font-castoro.rb

--- Casks/font-castoro.rb
+++ Casks/font-castoro.rb
@@ -2,10 +2,10 @@ cask "font-castoro" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url "https://github.com/google/fonts/trunk/ofl/castoro",
       using:      :svn,
-      trust_cert: true
+      trust_cert: true,
+      verified:   "github.com/google/fonts/"
   name "Castoro"
   desc "Named for the north american beaver, castor canadensis"
   homepage "https://fonts.google.com/specimen/Castoro"
