2cfe3281aae355badf35eacac9784fccb6e20988
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-05 16:50:51 +0800

    Update font-nerko-one.rb

--- Casks/font-nerko-one.rb
+++ Casks/font-nerko-one.rb
@@ -2,8 +2,8 @@ cask "font-nerko-one" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/nerkoone/NerkoOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/nerkoone/NerkoOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Nerko One"
   desc "Chunky ‘marker’ effect font"
   homepage "https://fonts.google.com/specimen/Nerko+One"
