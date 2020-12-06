db8de4b0a38aa569f6feba51e1f04bb47090a2e8
singularitti <singularitti@outlook.com>
2020-12-06 11:18:15 -0500

    Update font-iosevka-ss02 from 4.0.1 to 4.0.2

--- Casks/font-iosevka-ss02.rb
+++ Casks/font-iosevka-ss02.rb
@@ -1,10 +1,11 @@
 cask "font-iosevka-ss02" do
-  version "4.0.1"
-  sha256 "3558d93b4131a8f6378f967013b97f719eca58f8842fd1954bed19714f1eba8e"
+  version "4.0.2"
+  sha256 "2dfb0608163f17353dd7566fffd9d1dad57e093ea70cb81b73e573ad1d1ab759"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss02-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
   name "Iosevka SS02"
+  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
 
   font "iosevka-ss02-bold.ttc"
