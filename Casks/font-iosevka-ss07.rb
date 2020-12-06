0b825cb4fb0dcf029a4fc0f194237424bef87937
Reno <25192197+singularitti@users.noreply.github.com>
2020-12-06 13:13:40 -0500

    Update font-iosevka-ss07 from 4.0.1 to 4.0.2 (#2778)

--- Casks/font-iosevka-ss07.rb
+++ Casks/font-iosevka-ss07.rb
@@ -1,10 +1,11 @@
 cask "font-iosevka-ss07" do
-  version "4.0.1"
-  sha256 "9b5a2ad3463bcccb55117573b0e6cac4197321335d6c74b199a5727c878c369b"
+  version "4.0.2"
+  sha256 "4e95fc87aa2b684d083720fff5234a348f800a0bf7c6b299405317ff5dbf0de8"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss07-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
   name "Iosevka SS07"
+  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
 
   font "iosevka-ss07-bold.ttc"
