b10f075b36f3f6146d1f32824f2dc7cba82c9694
Reno <25192197+singularitti@users.noreply.github.com>
2020-12-06 21:21:09 -0500

    Update font-source-han-sans from 2.001 to 2.002 (#2798)

--- Casks/font-source-han-sans.rb
+++ Casks/font-source-han-sans.rb
@@ -1,6 +1,6 @@
 cask "font-source-han-sans" do
-  version "2.001"
-  sha256 "5eb728fc73f86b0e5f4129b6028eaf3cccc5ac4782e93e7c90323f6a550218a0"
+  version "2.002"
+  sha256 "8f78220f16845de12e3bb0dc460c082ffcd3871e5f92cbb0934cdeccffb48dbb"
 
   url "https://github.com/adobe-fonts/source-han-sans/raw/#{version}R/SuperOTC/SourceHanSans.ttc.zip"
   name "Source Han Sans"
