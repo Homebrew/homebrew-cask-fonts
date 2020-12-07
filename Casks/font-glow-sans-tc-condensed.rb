b29fca0e2db2eb2bc38ac4e74d40958cdbefdf9e
singularitti <singularitti@outlook.com>
2020-12-06 20:19:39 -0500

    Update font-glow-sans-tc-condensed from 0.9 to 0.91

--- Casks/font-glow-sans-tc-condensed.rb
+++ Casks/font-glow-sans-tc-condensed.rb
@@ -1,6 +1,6 @@
 cask "font-glow-sans-tc-condensed" do
-  version "0.9"
-  sha256 "3abb8c20dc2e8a531b02b01dd02fb3e6654f0f677dc529974d78d73f4a160f43"
+  version "0.91"
+  sha256 "a3063ac9a861ccf3aa73aa530d6faa012f20d0f77455b268a41a5aa37740acbd"
 
   url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansTC-Condensed-v#{version}.zip"
   appcast "https://github.com/welai/glow-sans/releases.atom"
