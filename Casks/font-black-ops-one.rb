4c23cd5a5a85f180ea8ed6213c725cc84fd679aa
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 23:14:12 +0000

    font-black-ops-one.rb: Update verified parameter (#2947)

--- Casks/font-black-ops-one.rb
+++ Casks/font-black-ops-one.rb
@@ -2,8 +2,8 @@ cask "font-black-ops-one" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/blackopsone/BlackOpsOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/blackopsone/BlackOpsOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Black Ops One"
   homepage "https://fonts.google.com/specimen/Black+Ops+One"
 
