1f3059f1ab30015ebe21e328e88cc2b16dafa6e7
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 22:38:48 +0000

    font-buda.rb: Update verified parameter

--- Casks/font-buda.rb
+++ Casks/font-buda.rb
@@ -2,8 +2,8 @@ cask "font-buda" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/buda/Buda-Light.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/buda/Buda-Light.ttf",
+      verified: "github.com/google/fonts/"
   name "Buda"
   homepage "https://fonts.google.com/specimen/Buda"
 
