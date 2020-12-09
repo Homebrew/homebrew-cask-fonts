339503c93ed4e688a56c141bd9427724b522f292
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 12:05:20 +0000

    font-armata.rb: Update verified parameter

--- Casks/font-armata.rb
+++ Casks/font-armata.rb
@@ -2,8 +2,8 @@ cask "font-armata" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/armata/Armata-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/armata/Armata-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Armata"
   homepage "https://fonts.google.com/specimen/Armata"
 
