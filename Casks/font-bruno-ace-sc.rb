f44507401df5961d1d52887f6b7ff02640e6b266
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 23:14:39 +0000

    font-bruno-ace-sc.rb: Update verified parameter (#2959)

--- Casks/font-bruno-ace-sc.rb
+++ Casks/font-bruno-ace-sc.rb
@@ -2,8 +2,8 @@ cask "font-bruno-ace-sc" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/brunoacesc/BrunoAceSC-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/brunoacesc/BrunoAceSC-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Bruno Ace SC"
   homepage "https://fonts.google.com/specimen/Bruno+Ace+SC"
 
