92b23fe5427ceeabbf0dfeb9c23195406d07590f
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 23:08:39 +0000

    font-balsamiq-sans.rb: Update verified parameter (#2910)

--- Casks/font-balsamiq-sans.rb
+++ Casks/font-balsamiq-sans.rb
@@ -2,8 +2,8 @@ cask "font-balsamiq-sans" do
   version :latest
   sha256 :no_check
 
-  # github.com/balsamiq/balsamiqsans/ was verified as official when first introduced to the cask
-  url "https://github.com/balsamiq/balsamiqsans/archive/master.zip"
+  url "https://github.com/balsamiq/balsamiqsans/archive/master.zip",
+      verified: "github.com/balsamiq/balsamiqsans/"
   name "Balsamiq Sans"
   homepage "https://balsamiq.com/givingback/opensource/font/"
 
