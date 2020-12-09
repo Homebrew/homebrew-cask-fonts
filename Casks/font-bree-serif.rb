9f1d45244eeb58ecd10e9c97d0e0e7eb91916ca0
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 22:38:14 +0000

    font-bree-serif.rb: Update verified parameter

--- Casks/font-bree-serif.rb
+++ Casks/font-bree-serif.rb
@@ -2,8 +2,8 @@ cask "font-bree-serif" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/breeserif/BreeSerif-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/breeserif/BreeSerif-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Bree Serif"
   homepage "https://fonts.google.com/specimen/Bree+Serif"
 
