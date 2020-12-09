b101ead7860ffe8ca1f31c47cac72e7c2c038e8e
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 02:14:54 +0000

    font-abril-fatface.rb: Update verified parameter (#2806)

--- Casks/font-abril-fatface.rb
+++ Casks/font-abril-fatface.rb
@@ -2,8 +2,8 @@ cask "font-abril-fatface" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/abrilfatface/AbrilFatface-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/abrilfatface/AbrilFatface-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Abril Fatface"
   homepage "https://fonts.google.com/specimen/Abril+Fatface"
 
