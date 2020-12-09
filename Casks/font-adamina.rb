085b932dc5eed6edfc026839c6954e6f4a244d45
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 01:54:35 +0000

    font-adamina.rb: Update verified parameter

--- Casks/font-adamina.rb
+++ Casks/font-adamina.rb
@@ -2,8 +2,8 @@ cask "font-adamina" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/adamina/Adamina-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/adamina/Adamina-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Adamina"
   homepage "https://fonts.google.com/specimen/Adamina"
 
