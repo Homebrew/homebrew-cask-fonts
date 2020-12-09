07b4f12444ed15f6ec979df1fb9ae2a84e950620
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 01:55:33 +0000

    font-aldrich.rb: Update verified parameter

--- Casks/font-aldrich.rb
+++ Casks/font-aldrich.rb
@@ -2,8 +2,8 @@ cask "font-aldrich" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/aldrich/Aldrich-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/aldrich/Aldrich-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Aldrich"
   homepage "https://fonts.google.com/specimen/Aldrich"
 
