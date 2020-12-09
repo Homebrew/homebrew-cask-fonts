3a7391d4f9c074fc19339c5ea6f0543a8674b923
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 20:39:28 +0000

    font-bm.rb: Update verified parameter

--- Casks/font-bm.rb
+++ Casks/font-bm.rb
@@ -2,8 +2,8 @@ cask "font-bm" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/hanna/BM-HANNA.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/hanna/BM-HANNA.ttf",
+      verified: "github.com/google/fonts/"
   name "BM"
   homepage "https://fonts.google.com/earlyaccess"
 
