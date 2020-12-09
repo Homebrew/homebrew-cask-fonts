f3f0d6fba4541080269ce4ae00db086fa1d32e78
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 05:14:37 +0000

    font-amethysta.rb: Update verified parameter (#2841)

--- Casks/font-amethysta.rb
+++ Casks/font-amethysta.rb
@@ -2,8 +2,8 @@ cask "font-amethysta" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/amethysta/Amethysta-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/amethysta/Amethysta-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Amethysta"
   homepage "https://fonts.google.com/specimen/Amethysta"
 
