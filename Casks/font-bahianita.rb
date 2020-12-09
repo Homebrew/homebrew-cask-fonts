420aa6b785a6db7404f3146ce39ff8b3fb01dcfd
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 23:07:50 +0000

    font-bahianita.rb: Update verified parameter (#2899)

--- Casks/font-bahianita.rb
+++ Casks/font-bahianita.rb
@@ -2,8 +2,8 @@ cask "font-bahianita" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/bahianita/Bahianita-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/bahianita/Bahianita-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Bahianita"
   homepage "https://fonts.google.com/specimen/Bahianita"
 
