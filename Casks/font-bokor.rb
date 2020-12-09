1e26cd892b9c27983d32c1912683305ba7ef0b62
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 23:14:27 +0000

    font-bokor.rb: Update verified parameter (#2951)

--- Casks/font-bokor.rb
+++ Casks/font-bokor.rb
@@ -2,8 +2,8 @@ cask "font-bokor" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/bokor/Bokor-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/bokor/Bokor-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Bokor"
   homepage "https://fonts.google.com/specimen/Bokor"
 
