22c40c8a34104f486292d9adecba7c1dd1f630a2
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 20:35:18 +0000

    font-belgrano.rb: Update verified parameter

--- Casks/font-belgrano.rb
+++ Casks/font-belgrano.rb
@@ -2,8 +2,8 @@ cask "font-belgrano" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/belgrano/Belgrano-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/belgrano/Belgrano-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Belgrano"
   homepage "https://fonts.google.com/specimen/Belgrano"
 
