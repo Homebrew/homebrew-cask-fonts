c7b8e26d012c4ebc6d011508e26cbd936d395e2e
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 02:49:34 +0000

    font-antic-didone.rb: Update verified parameter

--- Casks/font-antic-didone.rb
+++ Casks/font-antic-didone.rb
@@ -2,8 +2,8 @@ cask "font-antic-didone" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/anticdidone/AnticDidone-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/anticdidone/AnticDidone-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Antic Didone"
   homepage "https://fonts.google.com/specimen/Antic+Didone"
 
