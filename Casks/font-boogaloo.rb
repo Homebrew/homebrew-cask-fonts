e44fffe457a3bd70996d9f9d4c6414bf941515ab
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 23:06:29 +0000

    font-boogaloo.rb: Update verified parameter (#2954)

--- Casks/font-boogaloo.rb
+++ Casks/font-boogaloo.rb
@@ -2,8 +2,8 @@ cask "font-boogaloo" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/boogaloo/Boogaloo-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/boogaloo/Boogaloo-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Boogaloo"
   homepage "https://fonts.google.com/specimen/Boogaloo"
 
