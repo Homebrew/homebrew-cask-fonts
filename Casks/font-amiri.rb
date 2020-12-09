f37715aa1f2b953090330044432614c33308976e
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-09 13:55:56 +0800

    Update font-amiri.rb

--- Casks/font-amiri.rb
+++ Casks/font-amiri.rb
@@ -2,8 +2,8 @@ cask "font-amiri" do
   version "0.113"
   sha256 "0e43960c075ca372cffe563d00b72d13aa775237868fea4f8c6a6fe03818a68d"
 
-  url "https://github.com/alif-type/amiri-font/releases/download/#{version}/Amiri-#{version}.zip",
-      verified: "github.com/alif-type/amiri-font/"
+  url "https://github.com/alif-type/amiri/releases/download/#{version}/Amiri-#{version}.zip",
+      verified: "github.com/alif-type/amiri/"
   appcast "https://github.com/alif-type/amiri/releases.atom"
   name "Amiri"
   homepage "https://www.amirifont.org/"
