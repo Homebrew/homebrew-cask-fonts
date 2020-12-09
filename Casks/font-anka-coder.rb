d6b10a39ad3044a0e1535f49a49d6f31d83e74a3
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 02:49:21 +0000

    font-anka-coder.rb: Update verified parameter

--- Casks/font-anka-coder.rb
+++ Casks/font-anka-coder.rb
@@ -2,8 +2,8 @@ cask "font-anka-coder" do
   version "1.100"
   sha256 "3c3cd48ed57796cd490a2c22614c73e32ad202790d10e9af3d632cd985c0e172"
 
-  # storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/anka-coder-fonts/ was verified as official when first introduced to the cask
-  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/anka-coder-fonts/AnkaCoder.#{version}.zip"
+  url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/anka-coder-fonts/AnkaCoder.#{version}.zip",
+      verified: "storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/anka-coder-fonts/"
   name "Anka/Coder"
   homepage "https://code.google.com/p/anka-coder-fonts/"
 
