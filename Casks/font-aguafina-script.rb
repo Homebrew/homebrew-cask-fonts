cc5f4e41f230a832c278aa93eb56389df26c2c09
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 01:54:54 +0000

    font-aguafina-script.rb: Update verified parameter

--- Casks/font-aguafina-script.rb
+++ Casks/font-aguafina-script.rb
@@ -2,8 +2,8 @@ cask "font-aguafina-script" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/aguafinascript/AguafinaScript-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/aguafinascript/AguafinaScript-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Aguafina Script"
   homepage "https://fonts.google.com/specimen/Aguafina+Script"
 
