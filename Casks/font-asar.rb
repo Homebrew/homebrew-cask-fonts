b734a64518da33c008f016651a8280979ca5e772
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 12:06:12 +0000

    font-asar.rb: Update verified parameter

--- Casks/font-asar.rb
+++ Casks/font-asar.rb
@@ -2,8 +2,8 @@ cask "font-asar" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/asar/Asar-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/asar/Asar-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Asar"
   homepage "https://fonts.google.com/specimen/Asar"
 
