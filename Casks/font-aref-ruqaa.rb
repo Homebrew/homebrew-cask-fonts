75de4d47ab75ac535ca4163b990f6d1f01ad69a0
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 23:05:16 +0000

    font-aref-ruqaa.rb: Update verified parameter (#2865)

--- Casks/font-aref-ruqaa.rb
+++ Casks/font-aref-ruqaa.rb
@@ -2,8 +2,8 @@ cask "font-aref-ruqaa" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
   url "https://github.com/google/fonts/trunk/ofl/arefruqaa",
+      verified:   "github.com/google/fonts/",
       using:      :svn,
       trust_cert: true
   name "Aref Ruqaa"
