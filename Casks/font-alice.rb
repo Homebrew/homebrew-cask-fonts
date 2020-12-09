b3f5d29378abc1c8aa22de150c0e654aef6d4a5e
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 01:56:16 +0000

    font-alice.rb: Update verified parameter

--- Casks/font-alice.rb
+++ Casks/font-alice.rb
@@ -2,8 +2,8 @@ cask "font-alice" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/alice/Alice-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/alice/Alice-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Alice"
   homepage "https://fonts.google.com/specimen/Alice"
 
