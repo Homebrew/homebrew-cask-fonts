8c08ef2d9f3440105e03b1e6ea62c40ca112e9d9
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 03:14:33 +0000

    font-alex-brush.rb: Update verified parameter (#2823)

--- Casks/font-alex-brush.rb
+++ Casks/font-alex-brush.rb
@@ -2,8 +2,8 @@ cask "font-alex-brush" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/alexbrush/AlexBrush-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/alexbrush/AlexBrush-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Alex Brush"
   homepage "https://fonts.google.com/specimen/Alex+Brush"
 
