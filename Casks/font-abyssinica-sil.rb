5140a82cbc9012d31e88c174a992af3a62aecd19
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 01:53:13 +0000

    font-abyssinica-sil.rb: Update verified parameter

--- Casks/font-abyssinica-sil.rb
+++ Casks/font-abyssinica-sil.rb
@@ -2,8 +2,8 @@ cask "font-abyssinica-sil" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/abyssinicasil/AbyssinicaSIL-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/abyssinicasil/AbyssinicaSIL-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Abyssinica SIL"
   homepage "https://fonts.google.com/specimen/Abyssinica+SIL"
 
