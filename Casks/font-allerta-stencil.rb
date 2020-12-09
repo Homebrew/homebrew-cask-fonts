82e343749fa691af6e78e531918ceb08e8ce2bda
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 01:56:41 +0000

    font-allerta-stencil.rb: Update verified parameter

--- Casks/font-allerta-stencil.rb
+++ Casks/font-allerta-stencil.rb
@@ -2,8 +2,8 @@ cask "font-allerta-stencil" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/allertastencil/AllertaStencil-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/allertastencil/AllertaStencil-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Allerta Stencil"
   homepage "https://fonts.google.com/specimen/Allerta+Stencil"
 
