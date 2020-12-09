c7911e16a3659ab227f65b31b7744513b84be6dc
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 23:14:15 +0000

    font-blackout.rb: Update verified parameter (#2948)

--- Casks/font-blackout.rb
+++ Casks/font-blackout.rb
@@ -2,8 +2,8 @@ cask "font-blackout" do
   version :latest
   sha256 :no_check
 
-  # github.com/theleagueof/blackout/ was verified as official when first introduced to the cask
-  url "https://github.com/theleagueof/blackout/archive/master.zip"
+  url "https://github.com/theleagueof/blackout/archive/master.zip",
+      verified: "github.com/theleagueof/blackout/"
   name "Blackout"
   homepage "https://www.theleagueofmoveabletype.com/blackout"
 
