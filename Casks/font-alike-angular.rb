5701e474db0a9a58a81c0216cf92e735f018be5c
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-09 12:14:53 +0800

    Update font-alike-angular.rb

--- Casks/font-alike-angular.rb
+++ Casks/font-alike-angular.rb
@@ -3,7 +3,7 @@ cask "font-alike-angular" do
   sha256 :no_check
 
   url "https://github.com/google/fonts/raw/master/ofl/alikeangular/AlikeAngular-Regular.ttf",
-    verified: "github.com/google/fonts/"
+      verified: "github.com/google/fonts/"
   name "Alike Angular"
   homepage "https://fonts.google.com/specimen/Alike+Angular"
 
