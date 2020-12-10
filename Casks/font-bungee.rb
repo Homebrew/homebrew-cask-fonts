ca2cb1191cd6b1b7ed77854449f895c6292dcfcc
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 08:06:37 +0000

    font-bungee.rb: Update verified parameter (#2969)

--- Casks/font-bungee.rb
+++ Casks/font-bungee.rb
@@ -2,8 +2,8 @@ cask "font-bungee" do
   version "1.1.0"
   sha256 "d012a9e6293201c3165feba64342d29c42bb4e67b1cc66e07509c12bab760a6f"
 
-  # github.com/djrrb/bungee/ was verified as official when first introduced to the cask
-  url "https://github.com/djrrb/bungee/releases/download/#{version}/Bungee-fonts.zip"
+  url "https://github.com/djrrb/bungee/releases/download/#{version}/Bungee-fonts.zip",
+      verified: "github.com/djrrb/bungee/"
   appcast "https://github.com/djrrb/bungee/releases.atom"
   name "Bungee"
   homepage "https://djr.com/bungee/"
