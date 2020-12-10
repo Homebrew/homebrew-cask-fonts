18f95234b25dd8193f61d58c3cdce54e63b82970
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 11:33:54 +0000

    font-finger-paint.rb: Update verified parameter

--- Casks/font-finger-paint.rb
+++ Casks/font-finger-paint.rb
@@ -2,8 +2,8 @@ cask "font-finger-paint" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/fingerpaint/FingerPaint-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/fingerpaint/FingerPaint-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Finger Paint"
   homepage "https://fonts.google.com/specimen/Finger+Paint"
 
