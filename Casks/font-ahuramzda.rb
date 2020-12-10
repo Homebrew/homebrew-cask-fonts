b0b4466f07090c0cea8eee00d2c78672d6b40553
Markus Reiter <me@reitermark.us>
2020-12-10 10:56:36 +0100

    font-ahuramzda: use `sha256 :no_check` for unversioned URL

--- Casks/font-ahuramzda.rb
+++ Casks/font-ahuramzda.rb
@@ -1,6 +1,6 @@
 cask "font-ahuramzda" do
   version "1.000"
-  sha256 "5a8afb0b24ceeb98f1ef121406ceecb124f2a300c5ee7877a7ff6abdd697b160"
+  sha256 :no_check
 
   url "https://fontlibrary.org/assets/downloads/ahuramazda/b2c0eeb9186f389749746f075b5a1abf/ahuramazda.zip"
   name "Ahuramzda"
