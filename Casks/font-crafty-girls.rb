95144411f0d7bdaae49cc102f82da8e30bf793f2
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 03:35:51 +0000

    font-crafty-girls.rb: Update verified parameter

--- Casks/font-crafty-girls.rb
+++ Casks/font-crafty-girls.rb
@@ -2,8 +2,8 @@ cask "font-crafty-girls" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/apache/craftygirls/CraftyGirls-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/apache/craftygirls/CraftyGirls-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Crafty Girls"
   homepage "https://fonts.google.com/specimen/Crafty+Girls"
 
