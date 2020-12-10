981b9089aab2dc514a2001cc83d5eff777f38d72
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 02:24:30 +0000

    font-clicker-script.rb: Update verified parameter

--- Casks/font-clicker-script.rb
+++ Casks/font-clicker-script.rb
@@ -2,8 +2,8 @@ cask "font-clicker-script" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/clickerscript/ClickerScript-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/clickerscript/ClickerScript-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Clicker Script"
   homepage "https://fonts.google.com/specimen/Clicker+Script"
 
