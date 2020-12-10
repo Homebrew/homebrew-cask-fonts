8557e57d0064126e0608cfc99194963dc6e88929
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 07:54:31 +0000

    font-dokdo.rb: Update verified parameter (#3088)

--- Casks/font-dokdo.rb
+++ Casks/font-dokdo.rb
@@ -2,8 +2,8 @@ cask "font-dokdo" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/dokdo/Dokdo-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/dokdo/Dokdo-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Dokdo"
   homepage "https://fonts.google.com/specimen/Dokdo"
 
