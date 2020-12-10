ca3c05236f9229c3daaecd7997a023f8c776642e
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 03:48:29 +0000

    font-concert-one.rb: Update verified parameter (#3031)

--- Casks/font-concert-one.rb
+++ Casks/font-concert-one.rb
@@ -2,8 +2,8 @@ cask "font-concert-one" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/concertone/ConcertOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/concertone/ConcertOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Concert One"
   homepage "https://fonts.google.com/specimen/Concert+One"
 
