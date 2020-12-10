da736293c2fde4a0db3ec23edb22e7a50f455eac
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 02:25:27 +0000

    font-commissioner.rb: Update verified parameter

--- Casks/font-commissioner.rb
+++ Casks/font-commissioner.rb
@@ -2,8 +2,8 @@ cask "font-commissioner" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/commissioner/Commissioner%5Bslnt%2Cwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/commissioner/Commissioner%5Bslnt%2Cwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Commissioner"
   desc "Low-contrast humanist sans-serif font with almost classical proportions"
   homepage "https://fonts.google.com/specimen/Commissioner"
