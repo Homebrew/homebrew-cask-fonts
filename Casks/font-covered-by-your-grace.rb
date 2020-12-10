6bea23742686644af4f1ecded485c4cc201ab875
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 03:35:45 +0000

    font-covered-by-your-grace.rb: Update verified parameter

--- Casks/font-covered-by-your-grace.rb
+++ Casks/font-covered-by-your-grace.rb
@@ -2,8 +2,8 @@ cask "font-covered-by-your-grace" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/coveredbyyourgrace/CoveredByYourGrace.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/coveredbyyourgrace/CoveredByYourGrace.ttf",
+      verified: "github.com/google/fonts/"
   name "Covered By Your Grace"
   homepage "https://fonts.google.com/specimen/Covered+By+Your+Grace"
 
