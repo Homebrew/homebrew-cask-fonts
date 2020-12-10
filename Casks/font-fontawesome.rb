9ecbb66b462fc02783a172438f060cfb32f99280
Kévin Huguenin <kevin.huguenin@unil.ch>
2020-12-10 09:02:35 +0100

    Updated font awesome to 5.15.1 (#2952)

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,11 +1,12 @@
 cask "font-fontawesome" do
-  version "5.14.0"
-  sha256 "3295a2578a9fec3bf12f485a3ddba23eea8eab5802580c3de81ebc292405eeeb"
+  version "5.15.1"
+  sha256 "d269f26f3bc10925f6b33632e9dd877e6cdf0326f78dae6a47a60479ff1eb743"
 
-  # github.com/FortAwesome/Font-Awesome/ was verified as official when first introduced to the cask
-  url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip"
+  url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip",
+      verified: "github.com/FortAwesome/Font-Awesome/"
   appcast "https://github.com/FortAwesome/Font-Awesome/releases.atom"
   name "Font Awesome"
+  desc "Icon set and toolkit"
   homepage "https://fontawesome.com/"
 
   font "fontawesome-free-#{version}-desktop/otfs/Font Awesome #{version.major} Brands-Regular-400.otf"
