bbbf650112b2087197782f5f1cf34a6c6b51429a
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 04:45:27 +0000

    font-didact-gothic.rb: Update verified parameter

--- Casks/font-didact-gothic.rb
+++ Casks/font-didact-gothic.rb
@@ -2,8 +2,8 @@ cask "font-didact-gothic" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/didactgothic/DidactGothic-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/didactgothic/DidactGothic-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Didact Gothic"
   homepage "https://fonts.google.com/specimen/Didact+Gothic"
 
