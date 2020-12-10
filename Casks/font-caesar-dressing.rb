933a1a795905f8414bf6cd0e0172e59be5aed4ad
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 02:24:27 +0000

    font-caesar-dressing.rb: Update verified parameter (#2977)

--- Casks/font-caesar-dressing.rb
+++ Casks/font-caesar-dressing.rb
@@ -2,8 +2,8 @@ cask "font-caesar-dressing" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/caesardressing/CaesarDressing-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/caesardressing/CaesarDressing-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Caesar Dressing"
   homepage "https://fonts.google.com/specimen/Caesar+Dressing"
 
