defb552514dd73d645e0a9b6cb56bf3c5c13ee31
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 04:45:33 +0000

    font-cutive-mono.rb: Update verified parameter (#3059)

--- Casks/font-cutive-mono.rb
+++ Casks/font-cutive-mono.rb
@@ -2,8 +2,8 @@ cask "font-cutive-mono" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/cutivemono/CutiveMono-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/cutivemono/CutiveMono-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Cutive Mono"
   homepage "https://fonts.google.com/specimen/Cutive+Mono"
 
