1ee1fc1f9321f39de81d2ff2170bdbbf6cc74d78
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 03:55:28 +0000

    font-cooper-hewitt.rb: Update verified parameter (#3037)

--- Casks/font-cooper-hewitt.rb
+++ Casks/font-cooper-hewitt.rb
@@ -2,8 +2,8 @@ cask "font-cooper-hewitt" do
   version :latest
   sha256 :no_check
 
-  # uh8yh30l48rpize52xh0q1o6i-wpengine.netdna-ssl.com/wp-content/uploads/fonts/ was verified as official when first introduced to the cask
-  url "https://uh8yh30l48rpize52xh0q1o6i-wpengine.netdna-ssl.com/wp-content/uploads/fonts/CooperHewitt-OTF-public.zip"
+  url "https://uh8yh30l48rpize52xh0q1o6i-wpengine.netdna-ssl.com/wp-content/uploads/fonts/CooperHewitt-OTF-public.zip",
+      verified: "uh8yh30l48rpize52xh0q1o6i-wpengine.netdna-ssl.com/wp-content/uploads/fonts/"
   name "Cooper Hewitt"
   homepage "https://www.cooperhewitt.org/open-source-at-cooper-hewitt/cooper-hewitt-the-typeface-by-chester-jenkins/"
 
