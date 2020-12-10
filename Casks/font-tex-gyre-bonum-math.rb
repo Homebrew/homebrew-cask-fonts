521976d638d1c875bea0f29926cefad94bcceea8
Markus Reiter <me@reitermark.us>
2020-12-10 10:48:45 +0100

    font-tex-gyre-bonum-math: interpolate version

--- Casks/font-tex-gyre-bonum-math.rb
+++ Casks/font-tex-gyre-bonum-math.rb
@@ -2,7 +2,7 @@ cask "font-tex-gyre-bonum-math" do
   version "1.005"
   sha256 "8f8dc6f52ff838201f581f20b4ab634508e6d4b1e2745fe5d6b7732e1df73290"
 
-  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrebonum-math-1005.zip"
+  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrebonum-math-#{version.no_dots}.zip"
   name "Bonum Math"
   homepage "http://www.gust.org.pl/projects/e-foundry/tg-math"
 
