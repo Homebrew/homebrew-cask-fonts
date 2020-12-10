affca41a674f8c30ad3ff83254e01eec9b9f0891
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 04:45:34 +0000

    font-digital-numbers.rb: Update verified parameter

--- Casks/font-digital-numbers.rb
+++ Casks/font-digital-numbers.rb
@@ -2,8 +2,8 @@ cask "font-digital-numbers" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/digitalnumbers/DigitalNumbers-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/digitalnumbers/DigitalNumbers-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Digital Numbers"
   homepage "https://fonts.google.com/specimen/Digital+Numbers"
 
