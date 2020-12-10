cdbf6737addb2a5a905bf2a9c16c1854aa26200e
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 14:17:50 +0000

    font-electrolize.rb: Update verified parameter (#3119)

--- Casks/font-electrolize.rb
+++ Casks/font-electrolize.rb
@@ -2,8 +2,8 @@ cask "font-electrolize" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/electrolize/Electrolize-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/electrolize/Electrolize-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Electrolize"
   homepage "https://fonts.google.com/specimen/Electrolize"
 
