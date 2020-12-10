7d9a3013481c48bfddba4cdb76191dcf126271e4
Markus Reiter <me@reitermark.us>
2020-12-10 11:07:56 +0100

    font-source-han-serif: interpolate version (#3108)

--- Casks/font-source-han-serif.rb
+++ Casks/font-source-han-serif.rb
@@ -2,7 +2,7 @@ cask "font-source-han-serif" do
   version "1.001R"
   sha256 "85cc634fa228286ca307803bbb4ca61f61fa821b3ed573f4f07c6f0c064426b5"
 
-  url "https://github.com/adobe-fonts/source-han-serif/releases/download/1.001R/SourceHanSerif.ttc"
+  url "https://github.com/adobe-fonts/source-han-serif/releases/download/#{version}/SourceHanSerif.ttc"
   appcast "https://github.com/adobe-fonts/source-han-serif/releases.atom"
   name "Source Han Serif"
   name "思源宋體"
