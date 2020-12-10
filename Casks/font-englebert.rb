ba67b1309bec15c70b49b6f1dd905eb914f8630b
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 14:17:14 +0000

    font-englebert.rb: Update verified parameter (#3130)

--- Casks/font-englebert.rb
+++ Casks/font-englebert.rb
@@ -2,8 +2,8 @@ cask "font-englebert" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/englebert/Englebert-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/englebert/Englebert-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Englebert"
   homepage "https://fonts.google.com/specimen/Englebert"
 
