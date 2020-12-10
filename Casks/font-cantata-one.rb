e579ce47463434b309a48df0b585d959c493381b
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 00:22:56 +0000

    font-cantata-one.rb: Update verified parameter

--- Casks/font-cantata-one.rb
+++ Casks/font-cantata-one.rb
@@ -2,8 +2,8 @@ cask "font-cantata-one" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/cantataone/CantataOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/cantataone/CantataOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Cantata One"
   homepage "https://fonts.google.com/specimen/Cantata+One"
 
