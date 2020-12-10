c1fc014442ce8ba6c84550a05753b9038603f2c8
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 04:58:04 +0000

    font-dawning-of-a-new-day.rb: Update verified parameter (#3066)

--- Casks/font-dawning-of-a-new-day.rb
+++ Casks/font-dawning-of-a-new-day.rb
@@ -2,8 +2,8 @@ cask "font-dawning-of-a-new-day" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/dawningofanewday/DawningofaNewDay.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/dawningofanewday/DawningofaNewDay.ttf",
+      verified: "github.com/google/fonts/"
   name "Dawning of a New Day"
   homepage "https://fonts.google.com/specimen/Dawning+of+a+New+Day"
 
