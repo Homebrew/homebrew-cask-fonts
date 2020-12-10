06b59a814aeabf8529c3075b2df11835b8b4ae61
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 11:36:19 +0000

    font-freehand.rb: Update verified parameter

--- Casks/font-freehand.rb
+++ Casks/font-freehand.rb
@@ -2,8 +2,8 @@ cask "font-freehand" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/freehand/Freehand.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/freehand/Freehand.ttf",
+      verified: "github.com/google/fonts/"
   name "Freehand"
   homepage "https://fonts.google.com/specimen/Freehand"
 
