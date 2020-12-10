3564eccaf8cb89ae72c66381ec90c4bafda5f8dd
Markus Reiter <me@reitermark.us>
2020-12-10 10:56:36 +0100

    font-architype-renner: use `sha256 :no_check` for unversioned URL

--- Casks/font-architype-renner.rb
+++ Casks/font-architype-renner.rb
@@ -1,6 +1,6 @@
 cask "font-architype-renner" do
   version "001.000"
-  sha256 "8a8de50838960d7e597b5eda4214c22418b32121505a61f3d0bbb120ff7fd24a"
+  sha256 :no_check
 
   url "https://fontlot.com/wp-content/uploads/2017/08/archityperenner.zip"
   name "Architype Renner"
