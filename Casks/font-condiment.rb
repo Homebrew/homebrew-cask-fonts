7090f4b098353d216211a067f15bc2f8f80f0465
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 03:41:39 +0000

    font-condiment.rb: Update verified parameter (#3032)

--- Casks/font-condiment.rb
+++ Casks/font-condiment.rb
@@ -2,8 +2,8 @@ cask "font-condiment" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/condiment/Condiment-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/condiment/Condiment-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Condiment"
   homepage "https://fonts.google.com/specimen/Condiment"
 
