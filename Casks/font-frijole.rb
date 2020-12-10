278acea2dbe634aa3d161da48b178321da2dbf9d
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 11:36:34 +0000

    font-frijole.rb: Update verified parameter

--- Casks/font-frijole.rb
+++ Casks/font-frijole.rb
@@ -2,8 +2,8 @@ cask "font-frijole" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/frijole/Frijole-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/frijole/Frijole-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Frijole"
   homepage "https://fonts.google.com/specimen/Frijole"
 
