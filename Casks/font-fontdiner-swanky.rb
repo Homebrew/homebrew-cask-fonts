e09030c2912b7512a1a69611ccf846a8730cbd90
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 11:35:17 +0000

    font-fontdiner-swanky.rb: Update verified parameter

--- Casks/font-fontdiner-swanky.rb
+++ Casks/font-fontdiner-swanky.rb
@@ -2,8 +2,8 @@ cask "font-fontdiner-swanky" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/apache/fontdinerswanky/FontdinerSwanky-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/apache/fontdinerswanky/FontdinerSwanky-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Fontdiner Swanky"
   homepage "https://fonts.google.com/specimen/Fontdiner+Swanky"
 
