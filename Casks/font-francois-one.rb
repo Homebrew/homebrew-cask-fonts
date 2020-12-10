4e2953ed1b700f3de04200c20d2edc56bbec652b
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 11:35:33 +0000

    font-francois-one.rb: Update verified parameter

--- Casks/font-francois-one.rb
+++ Casks/font-francois-one.rb
@@ -2,8 +2,8 @@ cask "font-francois-one" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/francoisone/FrancoisOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/francoisone/FrancoisOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Francois One"
   homepage "https://fonts.google.com/specimen/Francois+One"
 
