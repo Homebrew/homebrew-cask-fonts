45cedc6341c8f678537d824529f02f12d1404cfe
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 02:21:31 +0000

    font-ceviche-one.rb: Update verified parameter (#2999)

--- Casks/font-ceviche-one.rb
+++ Casks/font-ceviche-one.rb
@@ -2,8 +2,8 @@ cask "font-ceviche-one" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/cevicheone/CevicheOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/cevicheone/CevicheOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Ceviche One"
   homepage "https://fonts.google.com/specimen/Ceviche+One"
 
