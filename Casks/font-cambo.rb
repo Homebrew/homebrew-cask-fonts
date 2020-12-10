2905136476f860a353f5d6035db7574288cf04d8
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 02:26:33 +0000

    font-cambo.rb: Update verified parameter (#2984)

--- Casks/font-cambo.rb
+++ Casks/font-cambo.rb
@@ -2,8 +2,8 @@ cask "font-cambo" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/cambo/Cambo-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/cambo/Cambo-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Cambo"
   homepage "https://fonts.google.com/specimen/Cambo"
 
