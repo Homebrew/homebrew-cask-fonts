94ceb0d010dedfc6c232563120ce3019015dbebe
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 04:43:31 +0000

    font-croissant-one.rb: Update verified parameter (#3055)

--- Casks/font-croissant-one.rb
+++ Casks/font-croissant-one.rb
@@ -2,8 +2,8 @@ cask "font-croissant-one" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/croissantone/CroissantOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/croissantone/CroissantOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Croissant One"
   homepage "https://fonts.google.com/specimen/Croissant+One"
 
