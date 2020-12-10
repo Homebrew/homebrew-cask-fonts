54095c4fc93e73eeea3b933513d09445dc26ed1b
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 02:26:03 +0000

    font-contrail-one.rb: Update verified parameter

--- Casks/font-contrail-one.rb
+++ Casks/font-contrail-one.rb
@@ -2,8 +2,8 @@ cask "font-contrail-one" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/contrailone/ContrailOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/contrailone/ContrailOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Contrail One"
   homepage "https://fonts.google.com/specimen/Contrail+One"
 
