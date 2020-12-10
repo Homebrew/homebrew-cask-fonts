c3cfaa3f41ffb9695b8079e28e6e074238cbff99
Markus Reiter <me@reitermark.us>
2020-12-10 11:55:08 +0100

    font-tex-gyre-pagella-math: interpolate version (#3110)

--- Casks/font-tex-gyre-pagella-math.rb
+++ Casks/font-tex-gyre-pagella-math.rb
@@ -2,9 +2,9 @@ cask "font-tex-gyre-pagella-math" do
   version "1.632"
   sha256 "68a9c0ce195915334673960b13a281f24d31a8ae380454a0e35652dba506d474"
 
-  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrepagella-math-1632.zip"
+  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyrepagella-math-#{version.no_dots}.zip"
   name "Pagella Math"
   homepage "http://www.gust.org.pl/projects/e-foundry/tg-math"
 
-  font "texgyrepagella-math-1632/opentype/texgyrepagella-math.otf"
+  font "texgyrepagella-math-#{version.no_dots}/opentype/texgyrepagella-math.otf"
 end
