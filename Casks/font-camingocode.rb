9af094ee50f56d02559f26788d10758e85a6f606
Markus Reiter <me@reitermark.us>
2020-12-10 11:03:39 +0100

    font-camingocode: interpolate version (#3105)

--- Casks/font-camingocode.rb
+++ Casks/font-camingocode.rb
@@ -1,8 +1,8 @@
 cask "font-camingocode" do
-  version "1.000"
+  version "1.0"
   sha256 "13b188ca8d1b5fe2c3ba880771f20a6faac550405ae98d12edd46240f8896714"
 
-  url "https://janfromm.de/_data/downloads/CamingoCode-v1.0.zip"
+  url "https://janfromm.de/_data/downloads/CamingoCode-v#{version}.zip"
   name "CamingoCode"
   homepage "https://www.janfromm.de/typefaces/camingomono/camingocode/"
 
