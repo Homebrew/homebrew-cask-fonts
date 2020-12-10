7b58efc2f278da262b54f8592325c9c2a358c3ae
Markus Reiter <me@reitermark.us>
2020-12-10 10:52:08 +0100

    font-tex-gyre-termes-math: interpolate version

--- Casks/font-tex-gyre-termes-math.rb
+++ Casks/font-tex-gyre-termes-math.rb
@@ -2,7 +2,7 @@ cask "font-tex-gyre-termes-math" do
   version "1.543"
   sha256 "5875b39d3987cbe4258e5933a2292fbece870177053c5a8f495492769e2d4bb2"
 
-  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyretermes-math-1543.zip"
+  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyretermes-math-#{version.no_dots}.zip"
   name "Termes Math"
   homepage "http://www.gust.org.pl/projects/e-foundry/tg-math"
 
