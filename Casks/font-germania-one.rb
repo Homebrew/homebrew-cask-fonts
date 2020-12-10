f09b8f0f4bcc4c859b9035c3590801821e33d249
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 11:39:09 +0000

    font-germania-one.rb: Update verified parameter

--- Casks/font-germania-one.rb
+++ Casks/font-germania-one.rb
@@ -2,8 +2,8 @@ cask "font-germania-one" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/germaniaone/GermaniaOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/germaniaone/GermaniaOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Germania One"
   homepage "https://fonts.google.com/specimen/Germania+One"
 
