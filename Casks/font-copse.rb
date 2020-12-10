363c4271d9d3edfeae7e6ad8d632c86069d97b28
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 02:26:32 +0000

    font-copse.rb: Update verified parameter

--- Casks/font-copse.rb
+++ Casks/font-copse.rb
@@ -2,8 +2,8 @@ cask "font-copse" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/copse/Copse-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/copse/Copse-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Copse"
   homepage "https://fonts.google.com/specimen/Copse"
 
