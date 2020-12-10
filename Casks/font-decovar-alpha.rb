9cb43b5f6c10530f0a3d75a4480b6f377b08ed44
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 03:37:47 +0000

    font-decovar-alpha.rb: Update verified parameter

--- Casks/font-decovar-alpha.rb
+++ Casks/font-decovar-alpha.rb
@@ -2,8 +2,8 @@ cask "font-decovar-alpha" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/decovaralpha/DecovarAlpha-VF.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/decovaralpha/DecovarAlpha-VF.ttf",
+      verified: "github.com/google/fonts/"
   name "Decovar Alpha"
   homepage "https://fonts.google.com/earlyaccess"
 
