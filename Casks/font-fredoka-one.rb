37fd7eb5c3e9cef8373b0fee5d7c3222b00bb803
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 16:10:20 +0000

    font-fredoka-one.rb: Update verified parameter (#3174)

--- Casks/font-fredoka-one.rb
+++ Casks/font-fredoka-one.rb
@@ -2,8 +2,8 @@ cask "font-fredoka-one" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/fredokaone/FredokaOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/fredokaone/FredokaOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Fredoka One"
   homepage "https://fonts.google.com/specimen/Fredoka+One"
 
