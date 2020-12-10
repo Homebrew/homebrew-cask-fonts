dd10249dc3b0183445f500f3f83d1934ecc31c7c
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 10:48:19 +0000

    font-el-messiri.rb: Update verified parameter

--- Casks/font-el-messiri.rb
+++ Casks/font-el-messiri.rb
@@ -2,8 +2,8 @@ cask "font-el-messiri" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/elmessiri/ElMessiri%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/elmessiri/ElMessiri%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "El Messiri"
   homepage "https://fonts.google.com/specimen/El+Messiri"
 
