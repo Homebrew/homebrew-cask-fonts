7e88139808a651b8d1409c8cd99f80b17b1a9fe1
Markus Reiter <me@reitermark.us>
2020-12-10 10:51:26 +0100

    font-tex-gyre-schola-math: interpolate version

--- Casks/font-tex-gyre-schola-math.rb
+++ Casks/font-tex-gyre-schola-math.rb
@@ -2,7 +2,7 @@ cask "font-tex-gyre-schola-math" do
   version "1.533"
   sha256 "53560861144138e25f89f1f487126d21c81c5086364ffcf2c8e5e46e37ebbe00"
 
-  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyreschola-math-1533.zip"
+  url "http://www.gust.org.pl/projects/e-foundry/tg-math/download/texgyreschola-math-#{version.no_dots}.zip"
   name "Schola Math"
   homepage "http://www.gust.org.pl/projects/e-foundry/tg-math"
 
