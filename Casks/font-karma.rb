1746f1bcf14fe79d5ab2d89c7eebc8ccf02b6fbc
Markus Reiter <me@reitermark.us>
2020-12-10 10:46:56 +0100

    font-karma: interpolate version

--- Casks/font-karma.rb
+++ Casks/font-karma.rb
@@ -2,7 +2,7 @@ cask "font-karma" do
   version "2.000"
   sha256 "ebbe01be41c18aed6e538ea8d88eec65bb1bca046afc36b2fc6a84e808bda7e4"
 
-  url "https://github.com/itfoundry/karma/releases/download/v2.000/karma-2_000.zip"
+  url "https://github.com/itfoundry/karma/releases/download/v#{version}/karma-#{version.dots_to_underscores}.zip"
   appcast "https://github.com/itfoundry/karma/releases.atom"
   name "Karma"
   homepage "https://github.com/itfoundry/karma"
