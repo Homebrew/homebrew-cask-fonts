f2c0fec24947062cb85291b2edc03914addf912e
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 02:27:00 +0000

    font-carrois-gothic-sc.rb: Update verified parameter (#2991)

--- Casks/font-carrois-gothic-sc.rb
+++ Casks/font-carrois-gothic-sc.rb
@@ -2,8 +2,8 @@ cask "font-carrois-gothic-sc" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/carroisgothicsc/CarroisGothicSC-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/carroisgothicsc/CarroisGothicSC-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Carrois Gothic SC"
   homepage "https://fonts.google.com/specimen/Carrois+Gothic+SC"
 
