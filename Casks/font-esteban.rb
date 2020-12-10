b7c18ea2b4098f492bac39563e27c92b6643099c
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 14:17:01 +0000

    font-esteban.rb: Update verified parameter (#3134)

--- Casks/font-esteban.rb
+++ Casks/font-esteban.rb
@@ -2,8 +2,8 @@ cask "font-esteban" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/esteban/Esteban-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/esteban/Esteban-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Esteban"
   homepage "https://fonts.google.com/specimen/Esteban"
 
