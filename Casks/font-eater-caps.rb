4d5cb2a0a81e0699ad46441161b7df63202cf633
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 07:53:39 +0000

    font-eater-caps.rb: Update verified parameter (#3099)

--- Casks/font-eater-caps.rb
+++ Casks/font-eater-caps.rb
@@ -2,8 +2,8 @@ cask "font-eater-caps" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/eatercaps/EaterCaps-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/eatercaps/EaterCaps-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Eater Caps"
   homepage "https://fonts.google.com/specimen/Eater+Caps"
 
