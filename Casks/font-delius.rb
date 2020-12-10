115e7f2c7babcadb1d1d5914637629c8dcce1f4e
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-10 05:20:13 +0000

    font-delius.rb: Update verified parameter (#3072)

--- Casks/font-delius.rb
+++ Casks/font-delius.rb
@@ -2,8 +2,8 @@ cask "font-delius" do
   version :latest
   sha256 :no_check
 
-  # github.com/google/fonts/ was verified as official when first introduced to the cask
-  url "https://github.com/google/fonts/raw/master/ofl/delius/Delius-Regular.ttf"
+  url "https://github.com/google/fonts/raw/master/ofl/delius/Delius-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Delius"
   homepage "https://fonts.google.com/specimen/Delius"
 
