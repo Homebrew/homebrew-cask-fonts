8a28898d26787fef63fe23af23e58b485be2fbcf
Markus Reiter <me@reitermark.us>
2020-12-13 06:04:50 +0100

    Update font-slackside-one.rb

--- Casks/font-slackside-one.rb
+++ Casks/font-slackside-one.rb
@@ -5,6 +5,7 @@ cask "font-slackside-one" do
   url "https://github.com/google/fonts/raw/master/ofl/slacksideone/SlacksideOne-Regular.ttf",
       verified: "github.com/google/fonts/"
   name "Slackside One"
+  desc "Handwritten display font"
   homepage "https://fonts.google.com/specimen/Slackside+One"
 
   font "SlacksideOne-Regular.ttf"
