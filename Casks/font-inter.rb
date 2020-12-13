af98c6ca80897a9a09ba9a8e777749679d2ff707
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:35 +0100

    font-inter: switch to `verified` parameter

--- Casks/font-inter.rb
+++ Casks/font-inter.rb
@@ -2,8 +2,8 @@ cask "font-inter" do
   version "3.15"
   sha256 "1534288eb599f57acbf2c5ac0010da9495c2ee53116e08d799555c6f8ee22156"
 
-  # github.com/rsms/inter/ was verified as official when first introduced to the cask
-  url "https://github.com/rsms/inter/releases/download/v#{version}/Inter-#{version}.zip"
+  url "https://github.com/rsms/inter/releases/download/v#{version}/Inter-#{version}.zip",
+      verified: "github.com/rsms/inter/"
   appcast "https://github.com/rsms/inter/releases.atom"
   name "Inter"
   homepage "https://rsms.me/inter/"
