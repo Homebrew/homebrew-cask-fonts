2dc038125d19631943eabcf32b5403fcb37a793f
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:40 +0100

    font-noto-sans-coptic: switch to `verified` parameter

--- Casks/font-noto-sans-coptic.rb
+++ Casks/font-noto-sans-coptic.rb
@@ -2,8 +2,8 @@ cask "font-noto-sans-coptic" do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCoptic-unhinted.zip"
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansCoptic-unhinted.zip",
+      verified: "noto-website-2.storage.googleapis.com/"
   name "Noto Sans Coptic"
   homepage "https://www.google.com/get/noto/#sans-copt"
 
