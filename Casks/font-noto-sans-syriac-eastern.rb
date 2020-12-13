af24c476784c5c8b8818f05784a71eb1348889de
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:43 +0100

    font-noto-sans-syriac-eastern: switch to `verified` parameter

--- Casks/font-noto-sans-syriac-eastern.rb
+++ Casks/font-noto-sans-syriac-eastern.rb
@@ -2,8 +2,8 @@ cask "font-noto-sans-syriac-eastern" do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEastern-unhinted.zip"
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansSyriacEastern-unhinted.zip",
+      verified: "noto-website-2.storage.googleapis.com/"
   name "Noto Sans Syriac Eastern"
   homepage "https://www.google.com/get/noto/#sans-syrc-eastern"
 
