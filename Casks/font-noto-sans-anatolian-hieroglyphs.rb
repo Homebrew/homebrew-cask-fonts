bf786efca2d812adc4946e32198ed042df8e9f15
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:38 +0100

    font-noto-sans-anatolian-hieroglyphs: switch to `verified` parameter

--- Casks/font-noto-sans-anatolian-hieroglyphs.rb
+++ Casks/font-noto-sans-anatolian-hieroglyphs.rb
@@ -2,8 +2,8 @@ cask "font-noto-sans-anatolian-hieroglyphs" do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAnatolianHieroglyphs-unhinted.zip"
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansAnatolianHieroglyphs-unhinted.zip",
+      verified: "noto-website-2.storage.googleapis.com/"
   name "Noto Sans Anatolian Hieroglyphs"
   homepage "https://www.google.com/get/noto/#sans-hluw"
 
