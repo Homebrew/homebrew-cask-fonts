ec989afe83e49732f1aff9eae8b05270afff1c21
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:44 +0100

    font-noto-serif-cjk-kr: switch to `verified` parameter

--- Casks/font-noto-serif-cjk-kr.rb
+++ Casks/font-noto-serif-cjk-kr.rb
@@ -2,8 +2,8 @@ cask "font-noto-serif-cjk-kr" do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKkr-hinted.zip"
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJKkr-hinted.zip",
+      verified: "noto-website-2.storage.googleapis.com/"
   name "Noto Serif CJK KR"
   homepage "https://www.google.com/get/noto/#serif-kore"
 
