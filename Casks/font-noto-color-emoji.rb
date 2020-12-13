ccaad3307269a58c9092550babc6ce9aa4b7256b
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:37 +0100

    font-noto-color-emoji: switch to `verified` parameter

--- Casks/font-noto-color-emoji.rb
+++ Casks/font-noto-color-emoji.rb
@@ -2,8 +2,8 @@ cask "font-noto-color-emoji" do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip"
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip",
+      verified: "noto-website-2.storage.googleapis.com/"
   name "Noto Color Emoji"
   homepage "https://www.google.com/get/noto/#emoji-zsye-color"
 
