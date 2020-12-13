51952c9f09df3f30109bd84a9ae0d3939e8bc23d
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:42 +0100

    font-noto-sans-oriya: switch to `verified` parameter

--- Casks/font-noto-sans-oriya.rb
+++ Casks/font-noto-sans-oriya.rb
@@ -2,8 +2,8 @@ cask "font-noto-sans-oriya" do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOriya-unhinted.zip"
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOriya-unhinted.zip",
+      verified: "noto-website-2.storage.googleapis.com/"
   name "Noto Sans Oriya"
   homepage "https://www.google.com/get/noto/#sans-orya"
 
