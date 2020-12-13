9c97dfa0ea9144b23238b63d6687ef8b370cf79b
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:37 +0100

    font-mononoki: switch to `verified` parameter

--- Casks/font-mononoki.rb
+++ Casks/font-mononoki.rb
@@ -2,8 +2,8 @@ cask "font-mononoki" do
   version "1.2"
   sha256 "cf29ea7663dbfe94feaedaf5c0be4f4ae6a5b98c1293e0e772e69c28ac5dc658"
 
-  # github.com/madmalik/mononoki/ was verified as official when first introduced to the cask
-  url "https://github.com/madmalik/mononoki/releases/download/#{version}/mononoki.zip"
+  url "https://github.com/madmalik/mononoki/releases/download/#{version}/mononoki.zip",
+      verified: "github.com/madmalik/mononoki/"
   appcast "https://github.com/madmalik/mononoki/releases.atom"
   name "Mononoki"
   homepage "https://madmalik.github.io/mononoki/"
