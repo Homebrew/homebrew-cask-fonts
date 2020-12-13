1c0c040b671534afc5fa917c36c43c8d5b03b4a6
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:36 +0100

    font-league-gothic: switch to `verified` parameter

--- Casks/font-league-gothic.rb
+++ Casks/font-league-gothic.rb
@@ -2,8 +2,8 @@ cask "font-league-gothic" do
   version "1.601"
   sha256 "a1d9d3aaba2abda2791eaad03d51dbfd97aa6f6f2ea23c3b2b2b31f90d8cbeb5"
 
-  # github.com/theleagueof/league-gothic/ was verified as official when first introduced to the cask
-  url "https://github.com/theleagueof/league-gothic/releases/download/#{version}/LeagueGothic-#{version}.tar.xz"
+  url "https://github.com/theleagueof/league-gothic/releases/download/#{version}/LeagueGothic-#{version}.tar.xz",
+      verified: "github.com/theleagueof/league-gothic/"
   appcast "https://github.com/theleagueof/league-gothic/releases.atom"
   name "League Gothic"
   desc "Revival of an old classic, Alternate Gothic #1"
