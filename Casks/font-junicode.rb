7260536fb08a845c8fe615f6a74d6f925490428b
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:36 +0100

    font-junicode: switch to `verified` parameter

--- Casks/font-junicode.rb
+++ Casks/font-junicode.rb
@@ -2,8 +2,8 @@ cask "font-junicode" do
   version "1.002"
   sha256 "c199d96c8424be60fcab8d00d2eee39ea8ae632cfd5e710cbbd70626d6a729e7"
 
-  # downloads.sourceforge.net/junicode/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/junicode/junicode-#{version}.zip"
+  url "https://downloads.sourceforge.net/junicode/junicode-#{version}.zip",
+      verified: "downloads.sourceforge.net/junicode/"
   appcast "https://sourceforge.net/projects/junicode/rss"
   name "Junicode"
   homepage "https://junicode.sourceforge.io/"
