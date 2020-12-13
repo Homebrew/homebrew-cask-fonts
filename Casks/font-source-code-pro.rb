7c7ec161b90612146ee55fc667e2b95fb3b3489d
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:45 +0100

    font-source-code-pro: switch to `verified` parameter

--- Casks/font-source-code-pro.rb
+++ Casks/font-source-code-pro.rb
@@ -2,8 +2,8 @@ cask "font-source-code-pro" do
   version "2.032R-ro-1.052R-it-1.012R-VAR"
   sha256 "4d28868a2dc4cd6b767a91cd3d8462983aa8924ae18149f3b1d1a135090a0f8d"
 
-  # github.com/adobe-fonts/source-code-pro/ was verified as official when first introduced to the cask
-  url "https://github.com/adobe-fonts/source-code-pro/archive/#{version.gsub(/(?<=ro)-|(?<=it)-/, "/")}.zip"
+  url "https://github.com/adobe-fonts/source-code-pro/archive/#{version.gsub(/(?<=ro)-|(?<=it)-/, "/")}.zip",
+      verified: "github.com/adobe-fonts/source-code-pro/"
   appcast "https://github.com/adobe-fonts/source-code-pro/releases.atom"
   name "Source Code Pro"
   homepage "https://adobe-fonts.github.io/source-code-pro/"
