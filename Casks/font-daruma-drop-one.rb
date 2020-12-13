53185e857201759492195b32ef1779a3abd1521a
Markus Reiter <me@reitermark.us>
2020-12-13 06:01:47 +0100

    Update font-daruma-drop-one.rb

--- Casks/font-daruma-drop-one.rb
+++ Casks/font-daruma-drop-one.rb
@@ -5,6 +5,7 @@ cask "font-daruma-drop-one" do
   url "https://github.com/google/fonts/raw/master/ofl/darumadropone/DarumaDropOne-Regular.ttf",
       verified: "github.com/google/fonts/"
   name "Daruma Drop One"
+  desc "Drop cap font"
   homepage "https://fonts.google.com/specimen/Daruma+Drop+One"
 
   font "DarumaDropOne-Regular.ttf"
