c4262d8504a57b8d86ffc96ab55013f4434d8918
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:42 +0100

    font-noto-sans-osmanya: switch to `verified` parameter

--- Casks/font-noto-sans-osmanya.rb
+++ Casks/font-noto-sans-osmanya.rb
@@ -2,8 +2,8 @@ cask "font-noto-sans-osmanya" do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOsmanya-unhinted.zip"
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansOsmanya-unhinted.zip",
+      verified: "noto-website-2.storage.googleapis.com/"
   name "Noto Sans Osmanya"
   homepage "https://www.google.com/get/noto/#sans-osma"
 
