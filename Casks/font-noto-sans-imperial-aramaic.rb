880e3068d875cd24ced0323f402cccf5881eae52
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:41 +0100

    font-noto-sans-imperial-aramaic: switch to `verified` parameter

--- Casks/font-noto-sans-imperial-aramaic.rb
+++ Casks/font-noto-sans-imperial-aramaic.rb
@@ -2,8 +2,8 @@ cask "font-noto-sans-imperial-aramaic" do
   version :latest
   sha256 :no_check
 
-  # noto-website-2.storage.googleapis.com/ was verified as official when first introduced to the cask
-  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansImperialAramaic-unhinted.zip"
+  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansImperialAramaic-unhinted.zip",
+      verified: "noto-website-2.storage.googleapis.com/"
   name "Noto Sans Imperial Aramaic"
   homepage "https://www.google.com/get/noto/#sans-armi"
 
