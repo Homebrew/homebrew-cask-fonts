b8399e3965c4fb5fc0b0ae10867c196f780571e4
Markus Reiter <me@reitermark.us>
2020-12-13 04:43:45 +0100

    font-simple-line-icons: switch to `verified` parameter

--- Casks/font-simple-line-icons.rb
+++ Casks/font-simple-line-icons.rb
@@ -2,8 +2,8 @@ cask "font-simple-line-icons" do
   version "2.4.1"
   sha256 "fd0e9edd1550f8f8ded705160f0e433aae4170fe9f21a595eb583ac54c7e2d12"
 
-  # github.com/thesabbir/simple-line-icons/ was verified as official when first introduced to the cask
-  url "https://github.com/thesabbir/simple-line-icons/archive/#{version}.zip"
+  url "https://github.com/thesabbir/simple-line-icons/archive/#{version}.zip",
+      verified: "github.com/thesabbir/simple-line-icons/"
   appcast "https://github.com/thesabbir/simple-line-icons/releases.atom"
   name "Simple Line Icons"
   homepage "https://simplelineicons.github.io/"
