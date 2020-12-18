ebafdcbb949970c6e5497ecf14c4e07b91aa9950
Reno <25192197+singularitti@users.noreply.github.com>
2020-12-17 21:36:45 -0500

    Update font-glow-sans-tc-compressed from 0.91 to 0.92 (#3250)

--- Casks/font-glow-sans-tc-compressed.rb
+++ Casks/font-glow-sans-tc-compressed.rb
@@ -1,6 +1,6 @@
 cask "font-glow-sans-tc-compressed" do
-  version "0.91"
-  sha256 "7fcaaaf01bd20a8d76e475a488fbe180c748d7e3b424ab21545845131e111278"
+  version "0.92"
+  sha256 "b18ccef2f9b055f0e3d4a84ded7371c2fae78353f3f1fcfdafa5a3b52fe032b1"
 
   url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansTC-Compressed-v#{version}.zip"
   appcast "https://github.com/welai/glow-sans/releases.atom"
