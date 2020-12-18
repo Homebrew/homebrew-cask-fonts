28c7978761045cef0abf52e364c33fc853765f89
Reno <25192197+singularitti@users.noreply.github.com>
2020-12-17 21:25:41 -0500

    Update font-glow-sans-j-wide from 0.91 to 0.92 (#3241)

--- Casks/font-glow-sans-j-wide.rb
+++ Casks/font-glow-sans-j-wide.rb
@@ -1,6 +1,6 @@
 cask "font-glow-sans-j-wide" do
-  version "0.91"
-  sha256 "d8ad8d7de90a7da9db22721804f62ef2a4392b6cde11c04ebf7c55ab9d1baa0a"
+  version "0.92"
+  sha256 "5d0d6811fb9e6778f9389b490aa728fa5a6e15fcade55882710538a6b3102a29"
 
   url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansJ-Wide-v#{version}.zip"
   appcast "https://github.com/welai/glow-sans/releases.atom"
