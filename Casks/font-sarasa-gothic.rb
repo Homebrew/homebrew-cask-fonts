0cc49f2e63dd8738179750179438d7221126d824
singularitti <singularitti@outlook.com>
2020-12-19 21:40:49 -0500

    Update font-sarasa-gothic from 0.16.1 to 0.16.2

--- Casks/font-sarasa-gothic.rb
+++ Casks/font-sarasa-gothic.rb
@@ -1,6 +1,6 @@
 cask "font-sarasa-gothic" do
-  version "0.16.1"
-  sha256 "19999934c98cb8b68e6f0a703ec4672c3104376d619ae61602f476a53754c41d"
+  version "0.16.2"
+  sha256 "7ccafc916cae9a518758552ff6833fe44aff06cf8ffa85599740f6201a0bd24f"
 
   url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
   appcast "https://github.com/be5invis/Sarasa-Gothic/releases.atom"
