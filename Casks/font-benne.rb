c1e0a36f052582b168f384fca9711f892adf849b
BrewTestBot <BrewTestBot@users.noreply.github.com>
2020-12-27 00:18:16 +0000

    Update Google Fonts

---
+++ Casks/font-benne.rb
@@ -0,0 +1,12 @@
+cask "font-benne" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/master/ofl/benne/Benne-Regular.ttf",
+      verified: "github.com/google/fonts/"
+  name "Benne"
+  desc "Kannada text font developed by john harrington"
+  homepage "https://fonts.google.com/specimen/Benne"
+
+  font "Benne-Regular.ttf"
+end
