40747f9078c7c4dea87634857d3d7d045df7cf59
singularitti <singularitti@outlook.com>
2021-01-09 05:50:34 -0500

    Update font-iosevka-ss13 from 4.3.0 to 4.4.0

--- Casks/font-iosevka-ss13.rb
+++ Casks/font-iosevka-ss13.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss13" do
-  version "4.3.0"
-  sha256 "3ca246bc9b574321819b9bfe551d88b78ec84aae0ff3019d16ccee2d6ab23ff3"
+  version "4.4.0"
+  sha256 "8cdd6a484c2f036d02122228d8baf772f7ba0459e6212d61b85fa6681523a0e1"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss13-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
