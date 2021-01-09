b63d23b9ef7432e408caa41032bdafd47b5e817f
singularitti <singularitti@outlook.com>
2021-01-09 05:54:16 -0500

    Update font-iosevka-sparkle from 4.3.0 to 4.4.0

--- Casks/font-iosevka-sparkle.rb
+++ Casks/font-iosevka-sparkle.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-sparkle" do
-  version "4.3.0"
-  sha256 "7dda0ef8206d913e93b6647cc41c3d9f5f6992570b9d3a665eea85a05990a9b8"
+  version "4.4.0"
+  sha256 "e3ea8decf916282a01ab940662e38a89d3b65b775419d0c3654832165a1b86db"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-sparkle-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
