f9cabb8f2de5547959293ea019e6563e4dfc9afe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-10 06:48:39 +0100

    Automatic addition of Google Fonts (#3385)
    * Update Google Fonts
    
    * Update font-shippori-mincho.rb
    
    * Update font-shippori-mincho-b1.rb
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/font-shippori-mincho.rb
@@ -0,0 +1,18 @@
+cask "font-shippori-mincho" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/trunk/ofl/shipporimincho",
+      verified:   "github.com/google/fonts/",
+      using:      :svn,
+      trust_cert: true
+  name "Shippori Mincho"
+  desc "Based on the Tsukiji Typeface making facility of Tokyo"
+  homepage "https://fonts.google.com/specimen/Shippori+Mincho"
+
+  font "ShipporiMincho-Bold.ttf"
+  font "ShipporiMincho-ExtraBold.ttf"
+  font "ShipporiMincho-Medium.ttf"
+  font "ShipporiMincho-Regular.ttf"
+  font "ShipporiMincho-SemiBold.ttf"
+end
