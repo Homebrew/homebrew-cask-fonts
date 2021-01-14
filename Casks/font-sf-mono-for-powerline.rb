368aadf59ee1f193eb18f43b2e33ba022dac25fd
Xiaolin Zhang <leoncamel@gmail.com>
2021-01-15 02:38:46 +0800

    Add new cask sf-mono-for-powerline (#3391)
    * Add new cask sf-mono-for-powerline
    
    * Update font-sf-mono-for-powerline.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/font-sf-mono-for-powerline.rb
@@ -0,0 +1,22 @@
+cask "font-sf-mono-for-powerline" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/Twixes/SF-Mono-Powerline/archive/master.zip"
+  name "SF Mono for Powerline"
+  desc "Apple's SF Mono font patched with the Nerd Fonts patcher for Powerline support"
+  homepage "https://github.com/Twixes/SF-Mono-Powerline"
+
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Bold-Italic.otf"
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Medium-Italic.otf"
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Bold.otf"
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Medium.otf"
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Heavy-Italic.otf"
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Regular-Italic.otf"
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Heavy.otf"
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Regular.otf"
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Light-Italic.otf"
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Semibold-Italic.otf"
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Light.otf"
+  font "SF-Mono-Powerline-master/SF-Mono-Powerline-Semibold.otf"
+end
