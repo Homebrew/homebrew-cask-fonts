f8f1f63c21c4d086fa346d75c3fe03b50212b05f
Reno <singularitti@outlook.com>
2021-01-16 14:14:57 -0500

    Update font-iosevka-ss07 from 4.4.0 to 4.5.0 (#3412)

--- Casks/font-iosevka-ss07.rb
+++ Casks/font-iosevka-ss07.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss07" do
-  version "4.4.0"
-  sha256 "2d855bc296eac0d8a398b48bbe5e6a8a1c6f41510a3d6e7943db95667db1e298"
+  version "4.5.0"
+  sha256 "6fe73b14a0fef5c351a9d99d2da9374cc1f88df6e214a3ed7271afe208b1c11d"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss07-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
