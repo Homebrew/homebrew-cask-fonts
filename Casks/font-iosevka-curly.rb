87e054886ac4e26acf689d4e9e0eb73df3377dc7
singularitti <singularitti@outlook.com>
2021-01-24 18:07:13 -0500

    Update font-iosevka-curly from 4.5.0 to 5.0.0-beta.1

--- Casks/font-iosevka-curly.rb
+++ Casks/font-iosevka-curly.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly" do
-  version "4.5.0"
-  sha256 "e492326b99cdb28f6dfa3f4cff7cf258819499b13cfbe0fc8a102d15fd6b3775"
+  version "5.0.0-beta.1"
+  sha256 "4ae6a0ef8e66d40c0c65ee9a8f50a012f15796f313e48e91b0fc90bfa88738f1"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
