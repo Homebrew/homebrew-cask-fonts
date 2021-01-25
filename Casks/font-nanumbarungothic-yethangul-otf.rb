02e64295f55737bc56e4904c54b58df22d3c8328
Mina Her <minacle@live.com>
2021-01-25 15:55:00 +0900

    Add font-nanumbarungothic-yethangul-otf

---
+++ Casks/font-nanumbarungothic-yethangul-otf.rb
@@ -0,0 +1,12 @@
+cask "font-nanumbarungothic-yethangul-otf" do
+  version :latest
+  sha256 :no_check
+
+  url "http://appdown.naver.com/naver/font/NanumFont/setupmac/NanumBarunGothicYetHangul_OTF_hangeulcamp.dmg"
+  name "NanumBarunGothicOTF YetHangul"
+  name "나눔바른고딕OTF 옛한글"
+  desc "사라져 가는 고어의 아름다움, 나눔옛한글"
+  homepage "https://hangeul.naver.com/2014/archaicword"
+
+  font "NanumBarunGothicOTF-YetHangul.otf"
+end
