be4421167bdba8973ffd1bc06a3a0886472a007a
Reno <singularitti@outlook.com>
2021-01-25 11:17:11 -0500

    Update font-iosevka-ss11 from 5.0.0-beta.1 to 5.0.0-beta.2 (#3460)

--- Casks/font-iosevka-ss11.rb
+++ Casks/font-iosevka-ss11.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss11" do
-  version "5.0.0-beta.1"
-  sha256 "0afed4fc046543cae0ff398b27fb5ba50d68fe81f5a2d7019029054f15e84373"
+  version "5.0.0-beta.2"
+  sha256 "7070eee4efd5965bf85ac6b4537e557f82a0ce581cadb913c29b7cf71b54afbe"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss11-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
