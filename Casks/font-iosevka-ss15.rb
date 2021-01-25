590389eca919f622ee371f50b3eb08f3b88809bc
Reno <singularitti@outlook.com>
2021-01-24 20:41:16 -0500

    Add new font "Iosevka SS15" (#3440)

---
+++ Casks/font-iosevka-ss15.rb
@@ -0,0 +1,20 @@
+cask "font-iosevka-ss15" do
+  version "5.0.0-beta.1"
+  sha256 "1d5d51e3ebfe98740d784e19a2abad5d3dcaaa1e21d4543d7aa2c081fe25baa6"
+
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss15-#{version}.zip"
+  appcast "https://github.com/be5invis/Iosevka/releases.atom"
+  name "Iosevka SS15"
+  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
+  homepage "https://github.com/be5invis/Iosevka/"
+
+  font "iosevka-ss15-bold.ttc"
+  font "iosevka-ss15-extrabold.ttc"
+  font "iosevka-ss15-extralight.ttc"
+  font "iosevka-ss15-heavy.ttc"
+  font "iosevka-ss15-light.ttc"
+  font "iosevka-ss15-medium.ttc"
+  font "iosevka-ss15-regular.ttc"
+  font "iosevka-ss15-semibold.ttc"
+  font "iosevka-ss15-thin.ttc"
+end
