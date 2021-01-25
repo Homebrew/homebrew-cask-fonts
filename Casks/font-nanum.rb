e450b31d4ad71f26e6c28440c40b4fb2e7c308d6
Mina Her <minacle@live.com>
2021-01-25 15:18:00 +0900

    Add font-nanum

---
+++ Casks/font-nanum.rb
@@ -0,0 +1,34 @@
+cask "font-nanum" do
+  version :latest
+  sha256 :no_check
+
+  url "https://hangeul.naver.com/api/nanum/font/NanumFont_MacOS_TTF_ALL_hangeulcamp.dmg"
+  name "Nanum fonts"
+  name "나눔글꼴"
+  desc "한글의 아름다움, 네이버가 만든 글꼴로 만납니다"
+  homepage "https://hangeul.naver.com/2017/nanum"
+
+  font "NanumBarunGothic.ttf"
+  font "NanumBarunGothicBold.ttf"
+  font "NanumBarunGothicLight.ttf"
+  font "NanumBarunGothicUltraLight.ttf"
+  font "NanumBarunpenBold.ttf"
+  font "NanumBarunpenRegular.ttf"
+  font "NanumBrush.ttf"
+  font "NanumGothic.ttf"
+  font "NanumGothicBold.ttf"
+  font "NanumGothicExtraBold.ttf"
+  font "NanumGothicLight.ttf"
+  font "NanumMyeongjo.ttf"
+  font "NanumMyeongjoBold.ttf"
+  font "NanumMyeongjoExtraBold.ttf"
+  font "NanumPen.ttf"
+  font "NanumSquareBold.ttf"
+  font "NanumSquareExtraBold.ttf"
+  font "NanumSquareLight.ttf"
+  font "NanumSquareRegular.ttf"
+  font "NanumSquareRoundB.ttf"
+  font "NanumSquareRoundEB.ttf"
+  font "NanumSquareRoundL.ttf"
+  font "NanumSquareRoundR.ttf"
+end
