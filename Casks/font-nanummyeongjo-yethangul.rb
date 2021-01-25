91a5ef32d1ac16a18f29862909089e9782a1e9ad
Mina Her <minacle@live.com>
2021-01-25 15:54:32 +0900

    Add font-nanummyeongjo-yethangul

---
+++ Casks/font-nanummyeongjo-yethangul.rb
@@ -0,0 +1,12 @@
+cask "font-nanummyeongjo-yethangul" do
+  version :latest
+  sha256 :no_check
+
+  url "http://appdown.naver.com/naver/font/NanumFont/setupmac/NanumMyeongjoYetHangul_TTF_hangeulcamp.dmg"
+  name "NanumMyeongjo YetHangul"
+  name "나눔명조 옛한글"
+  desc "사라져 가는 고어의 아름다움, 나눔옛한글"
+  homepage "https://hangeul.naver.com/2014/archaicword"
+
+  font "NanumMyeongjo-YetHangul.ttf"
+end
