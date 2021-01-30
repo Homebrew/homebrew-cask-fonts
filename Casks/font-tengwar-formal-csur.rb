e6234f1d69289d53c37730975d9eccda2ced5ea5
stefanocoretta <stefano.coretta@gmail.com>
2021-01-30 21:49:29 +0100

    Add new cask font-tengwar-formal-csur

---
+++ Casks/font-tengwar-formal-csur.rb
@@ -0,0 +1,10 @@
+cask 'font-tengwar-formal-csur' do
+  version '1.1'
+  sha256 '924c282f7f967ecf6ca81003d95d4355dcccda9cf3fb7afdc746cb9d8f8250c7'
+
+  url 'http://sourceforge.net/projects/freetengwar/files/TengwarFont/TengwarFormalCSUR11.zip/download'
+  name 'Tengwar Formal CSUR'
+  homepage 'http://freetengwar.sourceforge.net/formal.html'
+
+  font 'TengwarFormalCSUR11/TengwarFormalCSUR.ttf'
+end
