3aedf606231cb50625c199f740badcdb755f2cc7
stefanocoretta <stefano.coretta@gmail.com>
2021-01-30 22:08:19 +0100

    Update cask font-tengwar-telcontar

--- Casks/font-tengwar-telcontar.rb
+++ Casks/font-tengwar-telcontar.rb
@@ -1,11 +1,12 @@
-cask 'font-tengwar-telcontar' do
-  version '0.08'
-  sha256 'f88780510e5c14f66e252fabce5d58febc7bec962d5c17f21ac82002ce85b925'
+cask "font-tengwar-telcontar" do
+  version "0.08"
+  sha256 "f88780510e5c14f66e252fabce5d58febc7bec962d5c17f21ac82002ce85b925"
 
-  url 'http://sourceforge.net/projects/freetengwar/files/TengwarFont/TengwarTelcontar.008.zip/download'
-  name 'Tengwar Telcontar'
-  homepage 'http://freetengwar.sourceforge.net/tengtelc.html'
+  url "https://downloads.sourceforge.net/freetengwar/TengwarTelcontar.008.zip"
+  appcast "https://sourceforge.net/projects/freetengwar/rss?path=/TengwarFont"
+  name "Tengwar Telcontar"
+  homepage "http://freetengwar.sourceforge.net/tengtelc.html"
 
-  font 'tengtelc.ttf'
-  font 'tengtelcb.ttf'
+  font "tengtelc.ttf"
+  font "tengtelcb.ttf"
 end
