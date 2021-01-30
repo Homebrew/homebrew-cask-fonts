f017d510f58bc93d47b95b82a4712a95d727a206
stefanocoretta <stefano.coretta@gmail.com>
2021-01-30 21:50:24 +0100

    Add new cask font-free-mono-tengwar

---
+++ Casks/font-free-mono-tengwar.rb
@@ -0,0 +1,11 @@
+cask 'font-free-mono-tengwar' do
+  version '2013-07-21'
+  sha256 '11a33522a6a7aa43120f0e9f98862d922a89228ed762738809132e3b26784907'
+
+  url 'http://sourceforge.net/projects/freetengwar/files/TengwarFont/FreeMonoTengwar.2013-07-21.zip/download'
+  name 'FreeMonoTengwar'
+  homepage 'http://freetengwar.sourceforge.net/freemonotengwar.html'
+
+  font 'FreeMonoTengwar.2013-07-21/FreeMonoTengwar-embedding.ttf'
+  font 'FreeMonoTengwar.2013-07-21/FreeMonoTengwar.ttf'
+end
