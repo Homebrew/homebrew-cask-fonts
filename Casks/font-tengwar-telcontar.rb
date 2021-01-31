36b2364fff6b0aa81e740bed7766c6127cc5919b
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-01 01:27:35 +0800

    Update font-tengwar-telcontar.rb

--- Casks/font-tengwar-telcontar.rb
+++ Casks/font-tengwar-telcontar.rb
@@ -1,8 +1,8 @@
 cask "font-tengwar-telcontar" do
   version "008"
-  sha256 :no_check
+  sha256 "f88780510e5c14f66e252fabce5d58febc7bec962d5c17f21ac82002ce85b925"
 
-  url "https://downloads.sourceforge.net/freetengwar/TengwarTelcontar.008.zip"
+  url "https://downloads.sourceforge.net/freetengwar/TengwarTelcontar.#{version}.zip"
   appcast "https://sourceforge.net/projects/freetengwar/rss?path=/TengwarFont"
   name "Tengwar Telcontar"
   desc "Tengwar Telcontar - a Unicode Tengwar font"
