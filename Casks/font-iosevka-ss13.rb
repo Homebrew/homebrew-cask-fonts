dfa0ba5cbff534b02bfecb237cbd43cc1d8cf0b3
singularitti <singularitti@outlook.com>
2021-01-31 12:40:52 -0500

    Update font-iosevka-ss13 from 5.0.0-beta.2 to 5.0.0-beta.3

--- Casks/font-iosevka-ss13.rb
+++ Casks/font-iosevka-ss13.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss13" do
-  version "5.0.0-beta.2"
-  sha256 "0fd03017fc4920821430e1d4bdfdc17b9cbc7f15da583ab05996b3df2bfa6ed4"
+  version "5.0.0-beta.3"
+  sha256 "7494c9382fbe7da3a0f0a4d76ccb7e2ce93fe401d0a2a399a684b6cff6a906e4"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss13-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
