fc6a33edc14dd2306d3f4758788dd927a148d79a
Alexander Ronald Altman <alexanderaltman@me.com>
2021-01-31 15:35:40 -0800

    Update font-gnu-unifont from 13.0.03 to 13.0.05 (#3496)

--- Casks/font-gnu-unifont.rb
+++ Casks/font-gnu-unifont.rb
@@ -1,6 +1,6 @@
 cask "font-gnu-unifont" do
-  version "13.0.03"
-  sha256 "071e2ca005d97d72e79b8cb68df886f1f730cb5f0e7739a4c6e8d068ded28212"
+  version "13.0.05"
+  sha256 "f98e4866b71b4d86c2d7458405679914511c58963bb86d1fbf2251e8ed536f4e"
 
   url "https://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.tar.gz",
       verified: "ftpmirror.gnu.org/unifont/"
