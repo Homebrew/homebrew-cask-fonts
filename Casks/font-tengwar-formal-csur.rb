455e032d75ff21e35a3620791404a629a1a315ca
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-01 01:28:42 +0800

    Update font-tengwar-formal-csur.rb

--- Casks/font-tengwar-formal-csur.rb
+++ Casks/font-tengwar-formal-csur.rb
@@ -1,11 +1,11 @@
 cask "font-tengwar-formal-csur" do
   version "11"
-  sha256 :no_check
+  sha256 "924c282f7f967ecf6ca81003d95d4355dcccda9cf3fb7afdc746cb9d8f8250c7"
 
-  url "https://downloads.sourceforge.net/freetengwar/TengwarFormalCSUR11.zip"
+  url "https://downloads.sourceforge.net/freetengwar/TengwarFormalCSUR#{version}.zip"
   appcast "https://sourceforge.net/projects/freetengwar/rss?path=/TengwarFont"
   name "Tengwar Formal CSUR"
-  desc "Tengwar Unicode font compliant with  CSUR"
+  desc "Tengwar Unicode font compliant with CSUR"
   homepage "http://freetengwar.sourceforge.net/formal.html"
 
   font "TengwarFormalCSUR11/TengwarFormalCSUR.ttf"
