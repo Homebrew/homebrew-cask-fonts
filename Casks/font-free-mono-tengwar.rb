acd1eff094bb63bf429efd90ea0714576bcb934d
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-01 01:31:16 +0800

    Update font-free-mono-tengwar.rb

--- Casks/font-free-mono-tengwar.rb
+++ Casks/font-free-mono-tengwar.rb
@@ -1,13 +1,13 @@
 cask "font-free-mono-tengwar" do
   version "2013-07-21"
-  sha256 :no_check
+  sha256 "11a33522a6a7aa43120f0e9f98862d922a89228ed762738809132e3b26784907"
 
-  url "https://downloads.sourceforge.net/freetengwar/FreeMonoTengwar.2013-07-21.zip"
+  url "https://downloads.sourceforge.net/freetengwar/FreeMonoTengwar.#{version}.zip"
   appcast "https://sourceforge.net/projects/freetengwar/rss?path=/TengwarFont"
   name "FreeMonoTengwar"
   desc "Unicode tengwar monospaced font"
   homepage "http://freetengwar.sourceforge.net/freemonotengwar.html"
 
-  font "FreeMonoTengwar.2013-07-21/FreeMonoTengwar-embedding.ttf"
-  font "FreeMonoTengwar.2013-07-21/FreeMonoTengwar.ttf"
+  font "FreeMonoTengwar.#{version}/FreeMonoTengwar-embedding.ttf"
+  font "FreeMonoTengwar.#{version}/FreeMonoTengwar.ttf"
 end
