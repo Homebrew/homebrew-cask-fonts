75ccaffd96644dd42132fadb39065613489da62e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-01-31 18:25:23 +0100

    Update Google Fonts (#3470)
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>

--- Casks/font-markazi-text.rb
+++ Casks/font-markazi-text.rb
@@ -2,10 +2,10 @@ cask "font-markazi-text" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/master/ofl/markazitext/MarkaziText%5Bwght%5D.ttf",
+  url "https://github.com/google/fonts/raw/master/ofl/markazitextvfbeta/MarkaziText-VF.ttf",
       verified: "github.com/google/fonts/"
   name "Markazi Text"
   homepage "https://fonts.google.com/specimen/Markazi+Text"
 
-  font "MarkaziText[wght].ttf"
+  font "MarkaziText-VF.ttf"
 end
