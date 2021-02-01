908da3b1cf94b4c6ba3b59765c0cbb4984452fcc
Alexander Ronald Altman <alexanderaltman@me.com>
2021-01-31 18:50:58 -0800

    Deduplicate Noto Sans Meetei Mayek and Noto Sans Symbols 2

--- Casks/font-noto-sans-meetei-mayek.rb
+++ Casks/font-noto-sans-meetei-mayek.rb
@@ -2,10 +2,10 @@ cask "font-noto-sans-meetei-mayek" do
   version :latest
   sha256 :no_check
 
-  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMeeteiMayek-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/google/fonts/raw/master/ofl/notosansmeeteimayek/NotoSansMeeteiMayek%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans Meetei Mayek"
-  homepage "https://www.google.com/get/noto/#sans-mtei"
+  homepage "https://fonts.google.com/specimen/Noto+Sans+MeeteiMayek"
 
-  font "NotoSansMeeteiMayek-Regular.ttf"
+  font "NotoSansMeeteiMayek[wght].ttf"
 end
