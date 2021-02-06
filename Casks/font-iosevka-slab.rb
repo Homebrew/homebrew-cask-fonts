7f6dda4356bc20465ec0b786181288ccabc2313a
singularitti <singularitti@outlook.com>
2021-02-06 17:55:31 -0500

    Update font-iosevka-slab

--- Casks/font-iosevka-slab.rb
+++ Casks/font-iosevka-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-slab" do
-  version "5.0.0-beta.3"
-  sha256 "1daac1f8c695b9c6406ccd592e4dc0d30d31c5950cff9680778adcd44ee2e735"
+  version "5.0.0-rc.1"
+  sha256 "50b5856ce536e7b873b833b7a343b3649b2bd298d40399ceddda7cc2f1ead64d"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-slab-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
