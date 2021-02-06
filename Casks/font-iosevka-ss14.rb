41c0db9a4b8237a4375f3824b4a7d7a793b3828e
Reno <singularitti@outlook.com>
2021-02-06 18:01:16 -0500

    Update font-iosevka-ss14 (#3503)

--- Casks/font-iosevka-ss14.rb
+++ Casks/font-iosevka-ss14.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss14" do
-  version "5.0.0-beta.3"
-  sha256 "5ed933b056f2026e98e5bb345138af76963032243ee3ac0d16b5eac6c20e9a86"
+  version "5.0.0-rc.1"
+  sha256 "bcfe855bf714c041fcfa3bb0f9edde40ee5028f9104f4da3b7bfe5aacbba7c8d"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss14-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
