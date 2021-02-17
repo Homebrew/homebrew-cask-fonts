475990d44344104614aba69188b0f09d0457d4ab
singularitti <singularitti@outlook.com>
2021-02-17 05:01:57 -0500

    Update font-iosevka-curly-slab from 5.0.1 to 5.0.2

--- Casks/font-iosevka-curly-slab.rb
+++ Casks/font-iosevka-curly-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly-slab" do
-  version "5.0.1"
-  sha256 "b90e0f293e143f38d49f81de0ac960f21bd4915727933edce1fc61294814cb25"
+  version "5.0.2"
+  sha256 "901ac80f4749cb0ecebff3b8aed9aa9b085e33241f6a8bded48eea748355bfab"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-slab-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
