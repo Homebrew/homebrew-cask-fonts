aa9d3796dc582d6cb5d76be306b812372e5c51db
singularitti <singularitti@outlook.com>
2021-02-18 16:04:08 -0500

    Update font-juliamono from 0.032 to 0.033

--- Casks/font-juliamono.rb
+++ Casks/font-juliamono.rb
@@ -1,6 +1,6 @@
 cask "font-juliamono" do
-  version "0.032"
-  sha256 "fddcc9528431f502f6db6257122bca79db5f8d1f492c0c5a23ecc3ba37568f1f"
+  version "0.033"
+  sha256 "e0874049c8816511367271405dadbacbb15033171441085e9ee72c2326955531"
 
   url "https://github.com/cormullion/juliamono/releases/download/v#{version}/JuliaMono.tar.gz"
   appcast "https://github.com/cormullion/juliamono/releases.atom"
