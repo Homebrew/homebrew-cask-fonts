13f260153cb2595c8a5a9e62668593cafacf1f19
Wen Kokke <wenkokke@users.noreply.github.com>
2021-02-19 23:00:04 +0000

    Update font-mononoki from 1.3 to 1.3 (#3604)

--- Casks/font-mononoki.rb
+++ Casks/font-mononoki.rb
@@ -1,6 +1,6 @@
 cask "font-mononoki" do
-  version "1.2"
-  sha256 "cf29ea7663dbfe94feaedaf5c0be4f4ae6a5b98c1293e0e772e69c28ac5dc658"
+  version "1.3"
+  sha256 "37291a9371b4127bb03e012ace7ecc79375f6eccfa688be81a36ccc5d29e3ee6"
 
   url "https://github.com/madmalik/mononoki/releases/download/#{version}/mononoki.zip",
       verified: "github.com/madmalik/mononoki/"
@@ -8,8 +8,8 @@ cask "font-mononoki" do
   name "Mononoki"
   homepage "https://madmalik.github.io/mononoki/"
 
-  font "mononoki-Bold.ttf"
-  font "mononoki-BoldItalic.ttf"
-  font "mononoki-Italic.ttf"
-  font "mononoki-Regular.ttf"
+  font "webfont/mononoki-Bold.ttf"
+  font "webfont/mononoki-BoldItalic.ttf"
+  font "webfont/mononoki-Italic.ttf"
+  font "webfont/mononoki-Regular.ttf"
 end
