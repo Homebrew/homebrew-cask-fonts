8d3fae3502bd882eca1f5d4b7cd6a160f41969fe
Fabian <31616408+EinJochel@users.noreply.github.com>
2021-02-20 13:50:38 +0100

    Add font KanjiStrokeOrders

---
+++ Casks/font-kanjistrokeorders.rb
@@ -0,0 +1,15 @@
+cask "font-kanjistrokeorders" do
+  version "4.004,1oyQoTB531tbhlYaOW7ugvutXZ7HSlJfW"
+  sha256 "3374338e6d72c703ac2d70f69d8822463e8e285f32accc497e8ec8fe2ddbec2d"
+
+  url "https://drive.google.com/uc?export=download&id=#{version.after_comma}"
+  name "KanjiStrokeOrders"
+  desc "Stroke order diagrams for 6500+ kanji, 180+ kana symbols and Latin characters"
+  homepage "https://sites.google.com/site/nihilistorguk/"
+
+  font "KanjiStrokeOrders_v#{version.before_comma}.ttf"
+
+  caveats do
+    license "https://drive.google.com/uc?export=download&id=1HOybxFTh2aD6zoLoh-ixHf2vTSgqFs6f"
+  end
+end
