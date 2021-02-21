176808531e3bdfa2faa6221526da86859cb0c331
Reno <singularitti@outlook.com>
2021-02-20 21:00:58 -0500

    Update font-iosevka-ss13 from 5.0.2 to 5.0.3 (#3614)

--- Casks/font-iosevka-ss13.rb
+++ Casks/font-iosevka-ss13.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss13" do
-  version "5.0.2"
-  sha256 "03739b140510750e66c92f5268250669fc22fea898e77f9b82d101eda708e784"
+  version "5.0.3"
+  sha256 "f0b3de585a4591882ee59b3937dca7f8d1594945d93fea88eca4d0c176a84343"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss13-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
