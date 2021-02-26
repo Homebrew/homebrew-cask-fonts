a30942134068f18f953b89ecb09f483b96eda63b
Miccal Matthews <miccal.matthews@gmail.com>
2021-02-27 04:47:01 +0800

    Update font-comfortaa.rb

--- Casks/font-comfortaa.rb
+++ Casks/font-comfortaa.rb
@@ -2,7 +2,7 @@ cask "font-comfortaa" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/master/ofl/comfortaa/Comfortaa%5Bwght%5D.ttf",
+  url "https://github.com/google/fonts/raw/main/ofl/comfortaa/Comfortaa%5Bwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Comfortaa"
   homepage "https://fonts.google.com/specimen/Comfortaa"
