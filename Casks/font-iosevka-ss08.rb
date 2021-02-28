e85c3a7c1e64c70268275f5c89059ed258294a76
Reno <25192197+singularitti@users.noreply.github.com>
2021-02-28 01:15:54 -0500

    Update font-iosevka-ss08 from 5.0.3 to 5.0.4 (#3644)

--- Casks/font-iosevka-ss08.rb
+++ Casks/font-iosevka-ss08.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss08" do
-  version "5.0.3"
-  sha256 "ffd8c0459e77df79edb32dd2b1b42436cc7cd412eaeaca2d9082174f1ff7106f"
+  version "5.0.4"
+  sha256 "42b77288e256b7871c83b0296de437f4276c1e225f87ac1521e586139fd1b892"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss08-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
