55d53ba2686d124b9820bd90e6a89e46d34e933b
Fabio Sangiovanni <4040184+sanjioh@users.noreply.github.com>
2021-03-08 11:02:53 +0100

    Update font-iosevka from 5.0.4 to 5.0.5 (#3692)

--- Casks/font-iosevka.rb
+++ Casks/font-iosevka.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka" do
-  version "5.0.4"
-  sha256 "339d2e5559e94ab89926b311f158afd71bf0da10fe73e681974e1c19fbbf623c"
+  version "5.0.5"
+  sha256 "18c598daca7e041754bbb88777868ba438375c6c76883a3ca2bab2d635406abb"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
