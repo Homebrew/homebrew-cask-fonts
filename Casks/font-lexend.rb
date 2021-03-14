f86d021aef06411f7300c614dfcca242b0cbffaf
BrewTestBot <BrewTestBot@users.noreply.github.com>
2021-03-14 00:12:25 +0000

    Update Google Fonts

---
+++ Casks/font-lexend.rb
@@ -0,0 +1,20 @@
+cask "font-lexend" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/trunk/ofl/lexend",
+      verified:   "github.com/google/fonts/",
+      using:      :svn,
+      trust_cert: true
+  name "Lexend"
+  desc "Expanded by font bureau with a range of 8 weights"
+  homepage "https://fonts.google.com/specimen/Lexend"
+
+  font "Lexend-Bold.ttf"
+  font "Lexend-ExtraBold.ttf"
+  font "Lexend-Light.ttf"
+  font "Lexend-Medium.ttf"
+  font "Lexend-Regular.ttf"
+  font "Lexend-SemiBold.ttf"
+  font "Lexend-Thin.ttf"
+end
