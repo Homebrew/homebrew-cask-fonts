8e1061efc30a0f17b71705da494367a4c0ee0879
Kévin Huguenin <kevin.huguenin@unil.ch>
2021-03-17 13:05:20 +0100

    Update font-academicons to 1.9.1

--- Casks/font-academicons.rb
+++ Casks/font-academicons.rb
@@ -1,10 +1,14 @@
+# typed: false
+# frozen_string_literal: true
+
 cask "font-academicons" do
-  version "1.8.6"
-  sha256 "777e4c42031f0c8ecb0f228285955bd7516b6f1b82497ffa77434e616529b7df"
+  version "1.9.1"
+  sha256 "f7c2c991c6d4f075bd5de688f3c4f961421e52043c480084039add989c82154f"
 
   url "https://github.com/jpswalsh/academicons/archive/v#{version}.zip"
   appcast "https://github.com/jpswalsh/academicons/releases.atom"
   name "Academicons"
+  desc "Specialist icon font for academics"
   homepage "https://github.com/jpswalsh/academicons"
 
   font "academicons-#{version}/fonts/academicons.ttf"
