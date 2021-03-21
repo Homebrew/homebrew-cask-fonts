4b20b2dff2d5f6a51c18a0a8ccb9f39ccdcb0557
BrewTestBot <BrewTestBot@users.noreply.github.com>
2021-03-21 00:12:16 +0000

    Update Google Fonts

--- Casks/font-antonio.rb
+++ Casks/font-antonio.rb
@@ -2,14 +2,10 @@ cask "font-antonio" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/ofl/antonio",
-      verified:   "github.com/google/fonts/",
-      using:      :svn,
-      trust_cert: true
+  url "https://github.com/google/fonts/raw/main/ofl/antonio/Antonio%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Antonio"
   homepage "https://fonts.google.com/specimen/Antonio"
 
-  font "Antonio-Bold.ttf"
-  font "Antonio-Light.ttf"
-  font "Antonio-Regular.ttf"
+  font "Antonio[wght].ttf"
 end
