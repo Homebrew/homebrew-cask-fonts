ea72af8010d916e3f95c750790668e4e9d565bab
singularitti <singularitti@outlook.com>
2021-03-22 11:19:36 -0400

    Update font-iosevka-ss17 from 5.0.8 to 5.0.9

--- Casks/font-iosevka-ss17.rb
+++ Casks/font-iosevka-ss17.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss17" do
-  version "5.0.8"
-  sha256 "c9ed53f675ed081cb0110e70786c312679c4abd53baf8680a8061cc0516f9e44"
+  version "5.0.9"
+  sha256 "05c00d6cd0843df2d7adbbfd5fe2093ed68a1143d0611cb59f17fecafd4cda36"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss17-#{version}.zip"
   appcast "https://github.com/be5invis/Iosevka/releases.atom"
