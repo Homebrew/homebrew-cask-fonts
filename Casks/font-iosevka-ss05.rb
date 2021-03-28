5db30ceedfb0a231ed91a4cf0de66b5a1b087740
Reno <25192197+singularitti@users.noreply.github.com>
2021-03-28 02:38:34 -0400

    Remove appcast

--- Casks/font-iosevka-ss05.rb
+++ Casks/font-iosevka-ss05.rb
@@ -3,7 +3,6 @@ cask "font-iosevka-ss05" do
   sha256 "f3c8a4457a2eee5d016c62beea67047a3622923e721fd8cbe44d526d9644fae1"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss05-#{version}.zip"
-  appcast "https://github.com/be5invis/Iosevka/releases.atom"
   name "Iosevka SS05"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
