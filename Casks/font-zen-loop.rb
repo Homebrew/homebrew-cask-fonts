2a24e639908b718ffaf9091ff4aa8a88a82e3f5f
BrewTestBot <BrewTestBot@users.noreply.github.com>
2021-03-28 00:07:05 +0000

    Update Google Fonts

---
+++ Casks/font-zen-loop.rb
@@ -0,0 +1,15 @@
+cask "font-zen-loop" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/trunk/ofl/zenloop",
+      verified:   "github.com/google/fonts/",
+      using:      :svn,
+      trust_cert: true
+  name "Zen Loop"
+  desc "One of three latin fonts designed by yoshimichi ohira, as part of zen fonts collection"
+  homepage "https://fonts.google.com/specimen/Zen+Loop"
+
+  font "ZenLoop-Italic.ttf"
+  font "ZenLoop-Regular.ttf"
+end
