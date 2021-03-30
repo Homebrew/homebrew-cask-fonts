0ab4f13c9122bbbc27a368229dda9b7eef3b5ded
Reno <25192197+singularitti@users.noreply.github.com>
2021-03-30 00:31:44 -0400

    Update font-iosevka-ss12 from 5.0.9 to 5.1.0 (#3788)
    * Update font-iosevka-ss12 from 5.0.9 to 5.1.0
    
    * Remove appcast

--- Casks/font-iosevka-ss12.rb
+++ Casks/font-iosevka-ss12.rb
@@ -1,9 +1,8 @@
 cask "font-iosevka-ss12" do
-  version "5.0.9"
-  sha256 "5b83cf75fcd2e51746bcf4e88efe58309f1c127c909dafb1e9c0a459681494cf"
+  version "5.1.0"
+  sha256 "ccd608fb2543f21432c650317e193a598dd2039d04932dedb344b5eb93598044"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss12-#{version}.zip"
-  appcast "https://github.com/be5invis/Iosevka/releases.atom"
   name "Iosevka SS12"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
