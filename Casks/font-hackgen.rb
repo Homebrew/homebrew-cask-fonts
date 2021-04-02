07d88542103cc2c8f1c48a3ac9f85ba2b49bf350
OskaMathis <oskamathis@gmail.com>
2021-04-02 18:58:27 +0900

    Update font-hackgen from 2.2.3 to 2.3.2

--- Casks/font-hackgen.rb
+++ Casks/font-hackgen.rb
@@ -1,9 +1,8 @@
 cask "font-hackgen" do
-  version "2.2.3"
-  sha256 "81dc942372856d7d7d0e3f8a1ecef11471e23161dce25165f0cddc01bcef62f1"
+  version "2.3.2"
+  sha256 "e3341b88e9e2f171fd7e07812c72097b5fdf30ccd6351751fe57b6c8d3926a1b"
 
   url "https://github.com/yuru7/HackGen/releases/download/v#{version}/HackGen_v#{version}.zip"
-  appcast "https://github.com/yuru7/HackGen/releases.atom"
   name "HackGen"
   desc "Japanese programming font which is a composed of Hack and GenJyuu-Gothic"
   homepage "https://github.com/yuru7/HackGen"
