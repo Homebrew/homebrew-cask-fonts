b65601481436c9a58a842c8a7166493cc1fb9c70
Reno <25192197+singularitti@users.noreply.github.com>
2021-04-04 14:20:03 -0400

    Update font-iosevka-ss09 from 5.2.0 to 5.2.1

--- Casks/font-iosevka-ss09.rb
+++ Casks/font-iosevka-ss09.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss09" do
-  version "5.2.0"
-  sha256 "b2a69c6f5cfd95cf2d2b16f52e73ba703411dc91b905dc29f8ca03a136864f6c"
+  version "5.2.1"
+  sha256 "5a438638f3c959619562f76a96c1457e5dfc259ee64780b5ad25261ae319384e"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss09-#{version}.zip"
   name "Iosevka SS09"
