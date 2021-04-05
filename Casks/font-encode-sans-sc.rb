1f50855c09796dd5277a592c911bd6e81f7648f8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-04-05 01:52:02 +0100

    Automatic update of Google Fonts (#3860)

--- Casks/font-encode-sans-sc.rb
+++ Casks/font-encode-sans-sc.rb
@@ -5,7 +5,7 @@ cask "font-encode-sans-sc" do
   url "https://github.com/google/fonts/raw/main/ofl/encodesanssc/EncodeSansSC%5Bwdth%2Cwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Encode Sans SC"
-  homepage "https://fonts.google.com/specimen/Encode+Sans+Semi+Condensed"
+  homepage "https://fonts.google.com/specimen/Encode+Sans+SC"
 
   font "EncodeSansSC[wdth,wght].ttf"
 end
