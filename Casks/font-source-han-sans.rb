a0ee021ec62a5bcd78d2ec3c16d90ecf2a0c213b
exgphe <exgphe@icloud.com>
2021-04-10 18:29:09 +0800

    Update font-source-han-sans from 2.002 to 2.003 (#3866)
    * Update font-source-han-sans from 2.002 to 2.003
    
    * Update font-source-han-sans.rb
    
    * Update font-source-han-sans.rb
    
    * Update font-source-han-sans.rb
    
    * Update font-source-han-sans.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-source-han-sans.rb
+++ Casks/font-source-han-sans.rb
@@ -1,6 +1,6 @@
 cask "font-source-han-sans" do
-  version "2.002"
-  sha256 "8f78220f16845de12e3bb0dc460c082ffcd3871e5f92cbb0934cdeccffb48dbb"
+  version "2.003"
+  sha256 "02947fcec1fbb97b3369dc30113c9f07363359aed82788bf38eef17b894bf045"
 
   url "https://github.com/adobe-fonts/source-han-sans/raw/#{version}R/SuperOTC/SourceHanSans.ttc.zip"
   name "Source Han Sans"
@@ -9,6 +9,12 @@ cask "font-source-han-sans" do
   name "본고딕"
   desc "OpenType/CFF Pan-CJK fonts"
   homepage "https://github.com/adobe-fonts/source-han-sans"
+
+  livecheck do
+    url "https://github.com/adobe-fonts/source-han-sans/releases/latest"
+    strategy :page_match
+    regex(%r{tag/(\d+(?:\.\d+)*)}i)
+  end
 
   font "SourceHanSans.ttc"
 end
