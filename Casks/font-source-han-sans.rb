2f397084a82856a9a2b7ad31d2183bc521126455
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-10 18:02:13 +0800

    Update font-source-han-sans.rb

--- Casks/font-source-han-sans.rb
+++ Casks/font-source-han-sans.rb
@@ -1,8 +1,8 @@
 cask "font-source-han-sans" do
-  version "2.003"
+  version "2.003R"
   sha256 "02947fcec1fbb97b3369dc30113c9f07363359aed82788bf38eef17b894bf045"
 
-  url "https://github.com/adobe-fonts/source-han-sans/raw/#{version}R/SuperOTC/SourceHanSans.ttc.zip"
+  url "https://github.com/adobe-fonts/source-han-sans/raw/#{version}/SuperOTC/SourceHanSans.ttc.zip"
   name "Source Han Sans"
   name "思源黑體"
   name "源ノ角ゴシック"
@@ -11,7 +11,7 @@ cask "font-source-han-sans" do
   homepage "https://github.com/adobe-fonts/source-han-sans"
 
   livecheck do
-    url :url
+    url "https://github.com/adobe-fonts/source-han-sans/releases"
     strategy :github_latest
   end
 
