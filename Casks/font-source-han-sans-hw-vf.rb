4a15bedf94e40ad90d23ff6c5af8e72344b29a9a
Jiachen Chen <chenjiachen@me.com>
2021-04-10 18:43:54 -0700

    Add Source Han Sans Variable

---
+++ Casks/font-source-han-sans-hw-vf.rb
@@ -0,0 +1,13 @@
+cask "font-source-han-sans-hw-vf" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/adobe-fonts/source-han-sans/raw/release/Variable/OTC/SourceHanSansHW-VF.ttc"
+  name "Source Han Sans HW VF"
+  name "思源黑體 HW VF"
+  name "源ノ角ゴシック HW VF"
+  name "본고딕 HW VF"
+  homepage "https://github.com/adobe-fonts/source-han-sans"
+
+  font "SourceHanSansHW-VF.ttc"
+end
---
+++ Casks/font-source-han-sans-vf.rb
@@ -0,0 +1,13 @@
+cask "font-source-han-sans-vf" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/adobe-fonts/source-han-sans/raw/release/Variable/OTC/SourceHanSans-VF.ttc"
+  name "Source Han Sans VF"
+  name "思源黑體 VF"
+  name "源ノ角ゴシック VF"
+  name "본고딕 VF"
+  homepage "https://github.com/adobe-fonts/source-han-sans"
+
+  font "SourceHanSans-VF.ttc"
+end
