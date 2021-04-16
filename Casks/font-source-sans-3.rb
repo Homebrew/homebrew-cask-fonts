275f252ef975b616d343b4413827fd24c7ed1fa6
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2021-04-16 18:02:44 +0900

    Follow the family name changes in Source Sans/Serif
    This PR addresses the issue #3867.
    The following changes have been made.
    
    - Source Sans 3: new cask
    - Source Sans Pro: downgrade to version 2
    - Source Serif 4: new cask
    - Source Serif Pro: update file source and point to version 3

---
+++ Casks/font-source-sans-3.rb
@@ -0,0 +1,25 @@
+cask "font-source-sans-3" do
+  version "3.028"
+  sha256 "1186d594ecb1336679a1d102bb6ddbe6041f0fd029482e53f02e46aa3ab011cf"
+
+  url "https://github.com/adobe-fonts/source-sans/releases/download/#{version}R/source-sans-#{version.sub(/\./, "v")}R.zip"
+  appcast "https://github.com/adobe-fonts/source-sans/releases.atom"
+  name "Source Sans 3"
+  desc "Fonts designed for user interfaces"
+  homepage "https://github.com/adobe-fonts/source-sans"
+
+  font "OTF/SourceSans3-Black.otf"
+  font "OTF/SourceSans3-BlackIt.otf"
+  font "OTF/SourceSans3-Bold.otf"
+  font "OTF/SourceSans3-BoldIt.otf"
+  font "OTF/SourceSans3-ExtraLight.otf"
+  font "OTF/SourceSans3-ExtraLightIt.otf"
+  font "OTF/SourceSans3-It.otf"
+  font "OTF/SourceSans3-Light.otf"
+  font "OTF/SourceSans3-LightIt.otf"
+  font "OTF/SourceSans3-Regular.otf"
+  font "OTF/SourceSans3-Semibold.otf"
+  font "OTF/SourceSans3-SemiboldIt.otf"
+  font "VAR/SourceSans3VF-Italic.otf"
+  font "VAR/SourceSans3VF-Roman.otf"
+end
--- Casks/font-source-serif-pro.rb
+++ Casks/font-source-serif-pro.rb
@@ -1,23 +1,24 @@
 cask "font-source-serif-pro" do
-  version :latest
-  sha256 :no_check
+  version "3.001"
+  sha256 "e061cf3184c60fb8811ffb429d82083f6dd16810250b71e676e5fa1abb1e5934"
 
-  url "https://github.com/google/fonts/trunk/ofl/sourceserifpro",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/adobe-fonts/source-serif/releases/download/#{version}R/source-serif-pro-#{version}R.zip"
   name "Source Serif Pro"
-  homepage "https://fonts.google.com/specimen/Source+Serif+Pro"
+  desc "Open-source typeface to complement the Source Sans family"
+  homepage "https://github.com/adobe-fonts/source-serif"
 
-  font "SourceSerifPro-Black.ttf"
-  font "SourceSerifPro-BlackItalic.ttf"
-  font "SourceSerifPro-Bold.ttf"
-  font "SourceSerifPro-BoldItalic.ttf"
-  font "SourceSerifPro-ExtraLight.ttf"
-  font "SourceSerifPro-ExtraLightItalic.ttf"
-  font "SourceSerifPro-Italic.ttf"
-  font "SourceSerifPro-Light.ttf"
-  font "SourceSerifPro-LightItalic.ttf"
-  font "SourceSerifPro-Regular.ttf"
-  font "SourceSerifPro-SemiBold.ttf"
-  font "SourceSerifPro-SemiBoldItalic.ttf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-Black.otf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-BlackIt.otf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-Bold.otf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-BoldIt.otf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-ExtraLight.otf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-ExtraLightIt.otf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-It.otf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-Light.otf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-LightIt.otf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-Regular.otf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-Semibold.otf"
+  font "source-serif-pro-#{version}R/OTF/SourceSerifPro-SemiboldIt.otf"
+  font "source-serif-pro-#{version}R/VAR/SourceSerifVariable-Italic.otf"
+  font "source-serif-pro-#{version}R/VAR/SourceSerifVariable-Roman.otf"
 end
