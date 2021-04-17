7a6835e195b97b6fd5f8d7a3bde379a77b9d72ba
loyalsoldier <10487845+Loyalsoldier@users.noreply.github.com>
2021-04-17 16:22:05 +0800

    Update source-han-sans fonts

--- Casks/font-source-han-sans-hw-vf.rb
+++ Casks/font-source-han-sans-hw-vf.rb
@@ -1,13 +1,20 @@
 cask "font-source-han-sans-hw-vf" do
-  version :latest
-  sha256 :no_check
+  version "2.003"
+  sha256 "559bb7f8114258b73cbfe8bbb3bfe5a09dae63d73e82ed287f42a95fc3547a1d"
 
-  url "https://github.com/adobe-fonts/source-han-sans/raw/release/Variable/OTC/SourceHanSansHW-VF.ttc"
+  url "https://github.com/adobe-fonts/source-han-sans/raw/#{version}R/Variable/OTC/SourceHanSansHW-VF.ttc"
   name "Source Han Sans HW VF"
   name "思源黑體 HW VF"
   name "源ノ角ゴシック HW VF"
   name "본고딕 HW VF"
+  desc "Variable font edition of source-han-sans"
   homepage "https://github.com/adobe-fonts/source-han-sans"
+
+  livecheck do
+    url "https://github.com/adobe-fonts/source-han-sans/releases/latest"
+    strategy :page_match
+    regex(%r{tag/(\d+(?:\.\d+)*)}i)
+  end
 
   font "SourceHanSansHW-VF.ttc"
 end
--- Casks/font-source-han-sans-vf.rb
+++ Casks/font-source-han-sans-vf.rb
@@ -1,13 +1,20 @@
 cask "font-source-han-sans-vf" do
-  version :latest
-  sha256 :no_check
+  version "2.003"
+  sha256 "f06c326fb2ca0d211b03bf752777e7006049867825188da9aecab42945550a07"
 
-  url "https://github.com/adobe-fonts/source-han-sans/raw/release/Variable/OTC/SourceHanSans-VF.ttc"
+  url "https://github.com/adobe-fonts/source-han-sans/raw/#{version}R/Variable/OTC/SourceHanSans-VF.ttc"
   name "Source Han Sans VF"
   name "思源黑體 VF"
   name "源ノ角ゴシック VF"
   name "본고딕 VF"
+  desc "Variable font edition of source-han-sans"
   homepage "https://github.com/adobe-fonts/source-han-sans"
+
+  livecheck do
+    url "https://github.com/adobe-fonts/source-han-sans/releases/latest"
+    strategy :page_match
+    regex(%r{tag/(\d+(?:\.\d+)*)}i)
+  end
 
   font "SourceHanSans-VF.ttc"
 end
