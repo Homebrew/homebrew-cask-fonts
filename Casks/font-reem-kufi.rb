c5d397cf0a70aed2185890b8c01ef355bc02aeca
BrewTestBot <BrewTestBot@users.noreply.github.com>
2021-04-18 00:05:39 +0000

    Update Google Fonts

--- Casks/font-reem-kufi.rb
+++ Casks/font-reem-kufi.rb
@@ -2,10 +2,10 @@ cask "font-reem-kufi" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/reemkufi/ReemKufi-Regular.ttf",
+  url "https://github.com/google/fonts/raw/main/ofl/reemkufi/ReemKufi%5Bwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Reem Kufi"
   homepage "https://fonts.google.com/specimen/Reem+Kufi"
 
-  font "ReemKufi-Regular.ttf"
+  font "ReemKufi[wght].ttf"
 end
