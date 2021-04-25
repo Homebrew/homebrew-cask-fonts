6bb82f0683669937d19622861af2c88e1271fdbe
Markus Reiter <me@reitermark.us>
2021-04-25 04:27:20 +0200

    Update font-shippori-antique-b1.rb

--- Casks/font-shippori-antique-b1.rb
+++ Casks/font-shippori-antique-b1.rb
@@ -5,7 +5,6 @@ cask "font-shippori-antique-b1" do
   url "https://github.com/google/fonts/raw/main/ofl/shipporiantiqueb1/ShipporiAntiqueB1-Regular.ttf",
       verified: "github.com/google/fonts/"
   name "Shippori Antique B1"
-  desc "The standard version of the font"
   homepage "https://fonts.google.com/specimen/Shippori+Antique+B1"
 
   font "ShipporiAntiqueB1-Regular.ttf"
