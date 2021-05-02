e171687c84cd00557aaf92612851d4fb1748831e
BrewTestBot <BrewTestBot@users.noreply.github.com>
2021-05-02 00:05:03 +0000

    Update Google Fonts

---
+++ Casks/font-festive.rb
@@ -0,0 +1,11 @@
+cask "font-festive" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/festive/Festive-Regular.ttf",
+      verified: "github.com/google/fonts/"
+  name "Festive"
+  homepage "https://fonts.google.com/specimen/Festive"
+
+  font "Festive-Regular.ttf"
+end
