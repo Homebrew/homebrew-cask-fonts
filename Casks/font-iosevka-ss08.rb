db81f308bbeb2a6ba6925b4215d71d9e3eea6adc
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-02 15:27:57 -0400

    Update font-iosevka-ss08 from 6.0.1 to 6.1.2

--- Casks/font-iosevka-ss08.rb
+++ Casks/font-iosevka-ss08.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss08" do
-  version "6.0.1"
-  sha256 "efd562898ea5ec8a2c2b62f947fe3f9f7be9f6e26879f41e19fb1694f7abe976"
+  version "6.1.2"
+  sha256 "01352575bdfe6ddfcba55288e053b48ebcf43e593a9a95ffa6eaddfe5cefe1d2"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss08-#{version}.zip"
   name "Iosevka SS08"
