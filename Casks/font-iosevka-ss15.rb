f5cb7ca6b9ebb35c09058e6a50788fbc297ed2a9
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-02 08:45:25 -0400

    Update font-iosevka-ss15 from 6.0.1 to 6.1.1

--- Casks/font-iosevka-ss15.rb
+++ Casks/font-iosevka-ss15.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss15" do
-  version "6.0.1"
-  sha256 "60e5dc22eb66199be438664287cedb0314df017f61361d269183799d09324db2"
+  version "6.1.1"
+  sha256 "c68e684694ffdf338d22b04a9636427d842102a23f1326b4bba7c150723951ba"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss15-#{version}.zip"
   name "Iosevka SS15"
