22e8aa1ddc3bb093d3a8a8f296cef2b639f041b3
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-02 15:52:22 -0400

    Update font-iosevka-curly from 6.0.1 to 6.1.2 (#4010)

--- Casks/font-iosevka-curly.rb
+++ Casks/font-iosevka-curly.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly" do
-  version "6.0.1"
-  sha256 "08c6f84304716e87920ce2ccbb1a4258eb127b338921ecb00b6571b42184d3b6"
+  version "6.1.2"
+  sha256 "093d5d4e8e80f0614e50d4f8570524727a39c5bd03428ff999ce49a6b692f088"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-#{version}.zip"
   name "Iosevka Curly"
