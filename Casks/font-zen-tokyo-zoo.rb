788a0762f1f2ed82fa6e8f0549591fb6d7886e7b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2021-05-09 03:15:20 +0200

    Update Google Fonts (#4048)
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>

---
+++ Casks/font-zen-tokyo-zoo.rb
@@ -0,0 +1,11 @@
+cask "font-zen-tokyo-zoo" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/zentokyozoo/ZenTokyoZoo-Regular.ttf",
+      verified: "github.com/google/fonts/"
+  name "Zen Tokyo Zoo"
+  homepage "https://fonts.google.com/specimen/Zen+Tokyo+Zoo"
+
+  font "ZenTokyoZoo-Regular.ttf"
+end
