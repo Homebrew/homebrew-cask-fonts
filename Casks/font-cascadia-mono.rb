08c676ab2b9112194919e5e556ea8b5940e8f43e
Derek Schinke <derekschinke@me.com>
2021-05-29 18:35:25 -0400

    Update font-cascadia-mono from 2102.25 to 2105.24 (#4068)
    * Update font-cascadia-mono from 2102.25 to 2105.24
    
    * Update font-cascadia-mono.rb

--- Casks/font-cascadia-mono.rb
+++ Casks/font-cascadia-mono.rb
@@ -1,9 +1,8 @@
 cask "font-cascadia-mono" do
-  version "2102.25"
-  sha256 "a64e8834f06ec9acca4a5307cb6cf35922021d4fd0ca19096f8bf6a3e2515ad3"
+  version "2105.24"
+  sha256 "4edd89afd1009e1da121792c44bec6192001f3ffe2186a9e6f554c3bb8f5fb0b"
 
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
-  appcast "https://github.com/microsoft/cascadia-code/releases.atom"
   name "Cascadia Mono"
   desc "Version of Cascadia Code without ligatures"
   homepage "https://github.com/microsoft/cascadia-code"
@@ -14,4 +13,10 @@ cask "font-cascadia-mono" do
   font "ttf/static/CascadiaMono-Regular.ttf"
   font "ttf/static/CascadiaMono-SemiBold.ttf"
   font "ttf/static/CascadiaMono-SemiLight.ttf"
+  font "ttf/static/CascadiaMonoItalic-BoldItalic.ttf"
+  font "ttf/static/CascadiaMonoItalic-ExtraLightItalic.ttf"
+  font "ttf/static/CascadiaMonoItalic-Italic.ttf"
+  font "ttf/static/CascadiaMonoItalic-LightItalic.ttf"
+  font "ttf/static/CascadiaMonoItalic-SemiBoldItalic.ttf"
+  font "ttf/static/CascadiaMonoItalic-SemiLightItalic.ttf"
 end
