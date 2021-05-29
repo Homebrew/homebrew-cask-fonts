6385e5ee879c2cdbbb7f007184f9002c33a3c489
Derek Schinke <derekschinke@me.com>
2021-05-29 18:34:16 -0400

    Update font-cascadia-code-pl from 2102.25 to 2105.24 (#4067)
    * Update font-cascadia-code-pl from 2102.25 to 2105.24
    
    * Update font-cascadia-code-pl.rb

--- Casks/font-cascadia-code-pl.rb
+++ Casks/font-cascadia-code-pl.rb
@@ -1,9 +1,8 @@
 cask "font-cascadia-code-pl" do
-  version "2102.25"
-  sha256 "a64e8834f06ec9acca4a5307cb6cf35922021d4fd0ca19096f8bf6a3e2515ad3"
+  version "2105.24"
+  sha256 "4edd89afd1009e1da121792c44bec6192001f3ffe2186a9e6f554c3bb8f5fb0b"
 
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
-  appcast "https://github.com/microsoft/cascadia-code/releases.atom"
   name "Cascadia Code PL"
   desc "Version of Cascadia Code with embedded Powerline symbols"
   homepage "https://github.com/microsoft/cascadia-code"
@@ -14,4 +13,10 @@ cask "font-cascadia-code-pl" do
   font "ttf/static/CascadiaCodePL-Regular.ttf"
   font "ttf/static/CascadiaCodePL-SemiBold.ttf"
   font "ttf/static/CascadiaCodePL-SemiLight.ttf"
+  font "ttf/static/CascadiaCodePLItalic-BoldItalic.ttf"
+  font "ttf/static/CascadiaCodePLItalic-ExtraLightItalic.ttf"
+  font "ttf/static/CascadiaCodePLItalic-Italic.ttf"
+  font "ttf/static/CascadiaCodePLItalic-LightItalic.ttf"
+  font "ttf/static/CascadiaCodePLItalic-SemiBoldItalic.ttf"
+  font "ttf/static/CascadiaCodePLItalic-SemiLightItalic.ttf"
 end
