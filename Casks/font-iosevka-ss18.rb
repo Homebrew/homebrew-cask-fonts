df2ccf5b0dd2741892980faf1a7831dae6c1f116
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-29 19:26:44 -0400

    Add font "font-iosevka-ss18"

---
+++ Casks/font-iosevka-ss18.rb
@@ -0,0 +1,19 @@
+cask "font-iosevka-ss18" do
+  version "7.0.0"
+  sha256 "f46588ba82964d451738334eceebfbc07a2cf356e6cb0c0530242e701d15adb7"
+
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss18-#{version}.zip"
+  name "Iosevka SS18"
+  desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
+  homepage "https://github.com/be5invis/Iosevka/"
+
+  font "iosevka-ss18-bold.ttc"
+  font "iosevka-ss18-extrabold.ttc"
+  font "iosevka-ss18-extralight.ttc"
+  font "iosevka-ss18-heavy.ttc"
+  font "iosevka-ss18-light.ttc"
+  font "iosevka-ss18-medium.ttc"
+  font "iosevka-ss18-regular.ttc"
+  font "iosevka-ss18-semibold.ttc"
+  font "iosevka-ss18-thin.ttc"
+end
