ad595d24a8940fd9b416e26d4026d018db2ec932
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-29 19:16:04 -0400

    Update font-iosevka-slab from 6.1.3 to 7.0.0

--- Casks/font-iosevka-slab.rb
+++ Casks/font-iosevka-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-slab" do
-  version "6.1.3"
-  sha256 "e1157db779e0c906b50222a1bba9c83c6fa3a46fc0ec03a5c80d07d471f27c8d"
+  version "7.0.0"
+  sha256 "98c177a4e4044147fd06c5d8b0094f1a3a67a422336b914df1a1dcfb411e1e9c"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-slab-#{version}.zip"
   name "Iosevka Slab"
