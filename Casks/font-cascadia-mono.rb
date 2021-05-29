d293ad369f6259ad7f4d295e8db68a59c8c524df
Derek Schinke <derekschinke@me.com>
2021-05-29 17:06:13 -0400

    Update font-cascadia-mono.rb

--- Casks/font-cascadia-mono.rb
+++ Casks/font-cascadia-mono.rb
@@ -3,7 +3,6 @@ cask "font-cascadia-mono" do
   sha256 "4edd89afd1009e1da121792c44bec6192001f3ffe2186a9e6f554c3bb8f5fb0b"
 
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
-  appcast "https://github.com/microsoft/cascadia-code/releases.atom"
   name "Cascadia Mono"
   desc "Version of Cascadia Code without ligatures"
   homepage "https://github.com/microsoft/cascadia-code"
@@ -14,4 +13,10 @@ cask "font-cascadia-mono" do
   font "ttf/static/CascadiaMono-Regular.ttf"
   font "ttf/static/CascadiaMono-SemiBold.ttf"
   font "ttf/static/CascadiaMono-SemiLight.ttf"
+  font "ttf/static/CascadiaMonoItalic-BoldItalic.ttf"
+  font "ttf/static/CascadiaMonoItalic-ExtraLightItalic.ttf"
+  font "ttf/static/CascadiaMonoItalic-Italic.ttf"
+  font "ttf/static/CascadiaMonoItalic-LightItalic.ttf"
+  font "ttf/static/CascadiaMonoItalic-SemiBoldItalic.ttf"
+  font "ttf/static/CascadiaMonoItalic-SemiLightItalic.ttf"
 end
