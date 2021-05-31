92e6048ea8ec970ff6ae46b1907f08e4b960c201
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-31 18:04:30 -0400

    Update font-iosevka-ss02 from 7.0.1 to 7.0.2 (#4138)

--- Casks/font-iosevka-ss02.rb
+++ Casks/font-iosevka-ss02.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss02" do
-  version "7.0.1"
-  sha256 "2b46f361ac902d28a52b06d369c91079f3378e934edb84af21ac0422218efccd"
+  version "7.0.2"
+  sha256 "0db9b84fa3b698a56759c217387fd5484d9d39d913d5b8feb200ad86a62052dc"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss02-#{version}.zip"
   name "Iosevka SS02"
