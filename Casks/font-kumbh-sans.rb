d8254cc5250fbc94fcbf106af40ca390c22b9b19
BrewTestBot <BrewTestBot@users.noreply.github.com>
2021-06-06 00:09:10 +0000

    Update Google Fonts

--- Casks/font-kumbh-sans.rb
+++ Casks/font-kumbh-sans.rb
@@ -2,14 +2,11 @@ cask "font-kumbh-sans" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/ofl/kumbhsans",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/google/fonts/raw/main/ofl/kumbhsans/KumbhSans%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Kumbh Sans"
   desc "Geometric sans-serif multi-purpose font"
   homepage "https://fonts.google.com/specimen/Kumbh+Sans"
 
-  font "KumbhSans-Bold.ttf"
-  font "KumbhSans-Light.ttf"
-  font "KumbhSans-Regular.ttf"
+  font "KumbhSans[wght].ttf"
 end
