0cb976d7aef06f25b48908832b5ab3bd412a17b8
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-20 07:43:23 -0400

    Update font-iosevka-ss14 from 7.1.0 to 7.1.1 (#4246)

--- Casks/font-iosevka-ss14.rb
+++ Casks/font-iosevka-ss14.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss14" do
-  version "7.1.0"
-  sha256 "4690395447ba5f77a48262c5e918894bf37405f687a25609bd5d2c5a7e354039"
+  version "7.1.1"
+  sha256 "cbf2bf14ec17438d829c8b9bd04f2a03ae26c5bb7a3ab74f7216f25c3b14fd11"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss14-#{version}.zip"
   name "Iosevka SS14"
