1d1dfc4628d92a83de4de3c01a2307bf9c0c2ba4
Bevan Kay <email@bevankay.me>
2021-06-26 22:31:23 +1000

    Update font-material-icons from 3.0.1 to 4.0.0

--- Casks/font-material-icons.rb
+++ Casks/font-material-icons.rb
@@ -1,6 +1,6 @@
 cask "font-material-icons" do
-  version "3.0.1"
-  sha256 "722e3b09121b82a3746f3da2ecd3a2db8d7d24153b8433324315695a45f06a90"
+  version "4.0.0"
+  sha256 "3decc1c1f9b2352dafc769df58d1044293818a758bac150cc817fa377933f9a9"
 
   url "https://github.com/google/material-design-icons/archive/#{version}.zip",
       verified: "github.com/google/material-design-icons/"
