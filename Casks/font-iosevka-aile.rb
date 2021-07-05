67a035a451e62747f142a351bbbe5a5d1579358d
Bevan Kay <email@bevankay.me>
2021-07-05 19:08:23 +1000

    Update font-iosevka-aile from 7.2.5 to 7.2.6

--- Casks/font-iosevka-aile.rb
+++ Casks/font-iosevka-aile.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-aile" do
-  version "7.2.5"
-  sha256 "f7326604b3a3e56994274d0c57f71d69f8c8c79520df72b5ddaf63ca0296c014"
+  version "7.2.6"
+  sha256 "1d600e6255d407f993197d1c67eaaf5f68f2ae13119d00fc34dc13f7fc3f7416"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-aile-#{version}.zip"
   name "Iosevka Aile"
