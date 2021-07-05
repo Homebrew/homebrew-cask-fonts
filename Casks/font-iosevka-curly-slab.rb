58d3acc52812b10236fc2848fffe69969b369e65
Bevan Kay <email@bevankay.me>
2021-07-05 21:16:44 +1000

    Update font-iosevka-curly-slab from 7.2.5 to 7.2.6 (#4387)

--- Casks/font-iosevka-curly-slab.rb
+++ Casks/font-iosevka-curly-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly-slab" do
-  version "7.2.5"
-  sha256 "19a84267718b209dcfdab69636374f028c472b3efb4c9e2d809f8265dcb245ba"
+  version "7.2.6"
+  sha256 "93089f5b00ed36fff95f21abe8976bf983218db07f62a8a1114cd63de5ff07df"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-slab-#{version}.zip"
   name "Iosevka Curly Slab"
