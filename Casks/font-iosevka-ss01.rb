6f9395fb8cfbf8b133f4995839e863825256f147
Bevan Kay <email@bevankay.me>
2021-07-05 19:10:44 +1000

    Update font-iosevka-ss01 from 7.2.5 to 7.2.6

--- Casks/font-iosevka-ss01.rb
+++ Casks/font-iosevka-ss01.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss01" do
-  version "7.2.5"
-  sha256 "7cd0a625e4a08cfe7be8782ed16156eef5d252d3ca3aad64d0834db3ee980870"
+  version "7.2.6"
+  sha256 "3e3aa90a37ebfd9b2dce7a21cdb0883ffce25f524fee1cd8dae4e606ffbd9807"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss01-#{version}.zip"
   name "Iosevka SS01"
