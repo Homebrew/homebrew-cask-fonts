b4f0cd1559d569352e308a934d3b201ea7a40d46
Bevan Kay <email@bevankay.me>
2021-07-06 16:10:18 +1000

    remove appcast

--- Casks/font-sudo.rb
+++ Casks/font-sudo.rb
@@ -3,7 +3,6 @@ cask "font-sudo" do
   sha256 "fd39608d0a531b68777c68c5ab31f873a6803191a210be102d3d4cc1d66cce59"
 
   url "https://github.com/jenskutilek/sudo-font/releases/download/v#{version}/sudo.zip"
-  appcast "https://github.com/jenskutilek/sudo-font/releases.atom"
   name "Sudo"
   homepage "https://github.com/jenskutilek/sudo-font/"
 
