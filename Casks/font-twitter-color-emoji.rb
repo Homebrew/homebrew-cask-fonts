83bc9739bfa9c4bee5fe284d66b09cbeca962a72
Bevan Kay <email@bevankay.me>
2021-07-06 16:11:44 +1000

    remove appcast

--- Casks/font-twitter-color-emoji.rb
+++ Casks/font-twitter-color-emoji.rb
@@ -3,7 +3,6 @@ cask "font-twitter-color-emoji" do
   sha256 "4ef8af7ebe5f04ed40bc76ff6bc02ed8ebeb252af1468462eba80ca3b8682e97"
 
   url "https://github.com/eosrei/twemoji-color-font/releases/download/v#{version}/TwitterColorEmoji-SVGinOT-#{version}.zip"
-  appcast "https://github.com/eosrei/twemoji-color-font/releases.atom"
   name "Twitter Color Emoji"
   homepage "https://github.com/eosrei/twemoji-color-font/"
 
