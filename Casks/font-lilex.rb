7563b09fd55135cb324ee81409b4489bfd23cfc4
Bevan Kay <email@bevankay.me>
2021-07-06 15:38:14 +1000

    Update font-lilex.rb

--- Casks/font-lilex.rb
+++ Casks/font-lilex.rb
@@ -3,12 +3,11 @@ cask "font-lilex" do
   sha256 "1570385aa6fd596d11971a3ab800a220f10a6e8fd1f3b164177561244495bb7d"
 
   url "https://github.com/mishamyrt/Lilex/releases/download/#{version}/Lilex-#{version}.zip"
-  appcast "https://github.com/mishamyrt/Lilex/releases.atom"
   name "Lilex"
   homepage "https://github.com/mishamyrt/Lilex"
 
   font "otf/Lilex-Bold.otf"
-  font "otf/Lilex-Light.otf"
   font "otf/Lilex-Medium.otf"
   font "otf/Lilex-Regular.otf"
+  font "variable_ttf/Lilex-VF.ttf"
 end
