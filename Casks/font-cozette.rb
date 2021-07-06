35a02e6c1bd26b026a9f2345940a0f2cacf1e3e6
Bevan Kay <email@bevankay.me>
2021-07-06 15:29:59 +1000

    Update font-cozette.rb

--- Casks/font-cozette.rb
+++ Casks/font-cozette.rb
@@ -3,7 +3,6 @@ cask "font-cozette" do
   sha256 "a8941c4485ff75c3aab55b2c21994d00b440d9d7415143b1cc71337ba58cc3e4"
 
   url "https://github.com/slavfox/Cozette/releases/download/v.#{version}/CozetteVector.dfont"
-  appcast "https://github.com/slavfox/Cozette/releases.atom"
   name "Cozette"
   homepage "https://github.com/slavfox/Cozette"
 
