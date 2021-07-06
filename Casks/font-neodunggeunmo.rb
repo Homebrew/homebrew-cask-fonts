6f39dc8d0516f08e1e9ad1a7d284bf7da7223293
Bevan Kay <email@bevankay.me>
2021-07-06 20:37:15 +1000

    Update font-neodunggeunmo from 1.510 to 1.521 (#4415)
    * Update font-neodunggeunmo from 1.510 to 1.521
    
    * Update font-neodunggeunmo.rb

--- Casks/font-neodunggeunmo.rb
+++ Casks/font-neodunggeunmo.rb
@@ -1,10 +1,9 @@
 cask "font-neodunggeunmo" do
-  version "1.510"
-  sha256 "16f5ccff456d2a4e1baec5315e263a177c2a2dbfae265a01084f6b7f9a532a44"
+  version "1.521"
+  sha256 "7462308eb606f7a9125cc8798088f3284fb8fb2b97626bf86b5f7b326d78d36a"
 
   url "https://github.com/Dalgona/neodgm/releases/download/v#{version}/neodgm.ttf",
       verified: "github.com/Dalgona/neodgm/"
-  appcast "https://github.com/Dalgona/neodgm/releases.atom"
   name "NeoDunggeunmo"
   desc "Modern TrueType font based on an old-but-good Korean bitmap font"
   homepage "https://neodgm.dalgona.dev/"
