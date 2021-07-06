cf8d131010bed6d1ed728334df68e4e5e74b9a32
Bevan Kay <email@bevankay.me>
2021-07-06 15:27:06 +1000

    Update font-neodunggeunmo-code from 1.510 to 1.521

--- Casks/font-neodunggeunmo-code.rb
+++ Casks/font-neodunggeunmo-code.rb
@@ -1,6 +1,6 @@
 cask "font-neodunggeunmo-code" do
-  version "1.510"
-  sha256 "9a7465b01f006e3b4720a594e114a2985e7aba8694a54ce752baa76dbb677fc6"
+  version "1.521"
+  sha256 "930848a122844b7b5ca38589b3c1d355492c284ada64e22c8924ae691bef9eac"
 
   url "https://github.com/Dalgona/neodgm/releases/download/v#{version}/neodgm_code.ttf",
       verified: "github.com/Dalgona/neodgm/"
