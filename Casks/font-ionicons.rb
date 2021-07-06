d8ffb2d5b8b514f3354330ad8955dc2bd11a6b23
Bevan Kay <email@bevankay.me>
2021-07-06 15:30:21 +1000

    Update font-ionicons.rb

--- Casks/font-ionicons.rb
+++ Casks/font-ionicons.rb
@@ -3,7 +3,6 @@ cask "font-ionicons" do
   sha256 "b3181a6132313b8190fd4c081e8438f595b762469c7073742c0cf837b7245b3a"
 
   url "https://github.com/ionic-team/ionicons/archive/v#{version}.zip"
-  appcast "https://github.com/ionic-team/ionicons/releases.atom"
   name "Ionicons"
   homepage "https://github.com/ionic-team/ionicons"
 
