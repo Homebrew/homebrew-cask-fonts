bf8b653bee36882ab2096b3ef01bf53e130ebfb7
Bevan Kay <email@bevankay.me>
2021-07-06 20:38:54 +1000

    Update font-cozette from 1.8.0 to 1.11.3 (#4409)
    * Update font-cozette from 1.8.0 to 1.11.3
    
    * Update font-cozette.rb

--- Casks/font-cozette.rb
+++ Casks/font-cozette.rb
@@ -1,9 +1,8 @@
 cask "font-cozette" do
-  version "1.8.0"
-  sha256 "c6d153c8d7989ff8f7d0d3206cdc5e6863e376b774b181744c477ad7aa5710b2"
+  version "1.11.3"
+  sha256 "a8941c4485ff75c3aab55b2c21994d00b440d9d7415143b1cc71337ba58cc3e4"
 
   url "https://github.com/slavfox/Cozette/releases/download/v.#{version}/CozetteVector.dfont"
-  appcast "https://github.com/slavfox/Cozette/releases.atom"
   name "Cozette"
   homepage "https://github.com/slavfox/Cozette"
 
