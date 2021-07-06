3396c54ac4b786c6f3cf0ae704cdb74561544acc
Bevan Kay <email@bevankay.me>
2021-07-06 15:32:01 +1000

    Update font-urw-base35 from 20170801 to 20200910

--- Casks/font-urw-base35.rb
+++ Casks/font-urw-base35.rb
@@ -1,7 +1,7 @@
 cask "font-urw-base35" do
   # NOTE: "35" is not a version number, but an intrinsic part of the product name
-  version "20170801"
-  sha256 "4505042c8859166f5bff77e33907e244b66eb4e04b56646e14e0a97e5757cd21"
+  version "20200910"
+  sha256 "66eed7ca2dfbf44665aa34cb80559f4a90807d46858ccf76c34f9ac1701cfa27"
 
   url "https://github.com/ArtifexSoftware/urw-base35-fonts/archive/#{version}.zip"
   appcast "https://github.com/ArtifexSoftware/urw-base35-fonts/releases.atom"
