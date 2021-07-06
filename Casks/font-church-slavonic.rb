ebd5f6b38f5acc083a1e52a94795208a59fd2a91
Bevan Kay <email@bevankay.me>
2021-07-06 15:34:07 +1000

    Update font-church-slavonic.rb

--- Casks/font-church-slavonic.rb
+++ Casks/font-church-slavonic.rb
@@ -3,17 +3,22 @@ cask "font-church-slavonic" do
   sha256 "6fd44c6fb4fecc01ecea8dda6efc18bf46646f2e5d997c7d60e0cbae3aa8ff2e"
 
   url "https://github.com/typiconman/fonts-cu/releases/download/v#{version}/fonts-churchslavonic.zip"
-  appcast "https://github.com/typiconman/fonts-cu/releases.atom"
   name "Church Slavonic Fonts"
   homepage "https://github.com/typiconman/fonts-cu"
 
+  font "Acathist-Regular.otf"
   font "CathismaUnicode.otf"
   font "FedorovskUnicode.otf"
   font "IndictionUnicode.otf"
   font "MenaionUnicode.otf"
+  font "MezenetsUnicode.otf"
   font "MonomakhUnicode.otf"
   font "OglavieUnicode.otf"
+  font "PochaevskUnicode.otf"
   font "PomorskyUnicode.otf"
   font "PonomarUnicode.otf"
+  font "Shafarik-Regular.otf"
+  font "TriodionUnicode.otf"
   font "VertogradUnicode.otf"
+  font "Voskresensky-Regular.otf"
 end
