85b50c10a1d86ee1d911de9d196087910525820c
Bevan Kay <email@bevankay.me>
2021-07-09 15:03:55 +1000

    Update font-lilex from 1.000 to 1.100 (#4413)
    * Update font-lilex from 1.000 to 1.100
    
    * Update font-lilex.rb

--- Casks/font-lilex.rb
+++ Casks/font-lilex.rb
@@ -1,14 +1,13 @@
 cask "font-lilex" do
-  version "1.000"
-  sha256 "c73b0241fedac898014cbfe9b8701593e8d6472f43adb6b8bc31a5c45d44c904"
+  version "1.100"
+  sha256 "1570385aa6fd596d11971a3ab800a220f10a6e8fd1f3b164177561244495bb7d"
 
   url "https://github.com/mishamyrt/Lilex/releases/download/#{version}/Lilex-#{version}.zip"
-  appcast "https://github.com/mishamyrt/Lilex/releases.atom"
   name "Lilex"
   homepage "https://github.com/mishamyrt/Lilex"
 
   font "otf/Lilex-Bold.otf"
-  font "otf/Lilex-Light.otf"
   font "otf/Lilex-Medium.otf"
   font "otf/Lilex-Regular.otf"
+  font "variable_ttf/Lilex-VF.ttf"
 end
