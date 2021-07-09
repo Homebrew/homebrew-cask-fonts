6b859e7ff673c804959f1efde4c4994c0bd4d5cc
Bevan Kay <email@bevankay.me>
2021-07-09 15:03:14 +1000

    font-d2coding: update livecheck (#4425)

--- Casks/font-d2coding.rb
+++ Casks/font-d2coding.rb
@@ -3,9 +3,16 @@ cask "font-d2coding" do
   sha256 "0f1c9192eac7d56329dddc620f9f1666b707e9c8ed38fe1f988d0ae3e30b24e6"
 
   url "https://github.com/naver/d2codingfont/releases/download/VER#{version.before_comma}/D2Coding-Ver#{version.before_comma}-#{version.after_comma}.zip"
-  appcast "https://github.com/naver/d2codingfont/releases.atom"
   name "D2 Coding"
   homepage "https://github.com/naver/d2codingfont"
+
+  livecheck do
+    url "https://github.com/naver/d2codingfont/releases/latest"
+    strategy :page_match do |page|
+      page.scan(/href=.*?D2Coding[._-](?:Ver)?(\d+(?:\.\d+)+)[_-](\d+)\.zip/i)
+          .map { |matches| "#{matches[0]},#{matches[1]}" }
+    end
+  end
 
   font "D2Coding/D2Coding-Ver#{version.before_comma}-#{version.after_comma}.ttc"
 end
