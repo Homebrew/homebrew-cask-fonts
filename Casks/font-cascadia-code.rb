4ee2f2f725d4d146549416aedf81a4a24b2895ec
Lonre Wang <lonrevip@gmail.com>
2021-07-14 14:40:57 +0800

    Update font-cascadia-code from 2105.24 to 2106.17 (#4485)
    * Update font-cascadia-code from 2105.24 to 2106.17
    
    * Update font-cascadia-code.rb

--- Casks/font-cascadia-code.rb
+++ Casks/font-cascadia-code.rb
@@ -1,6 +1,6 @@
 cask "font-cascadia-code" do
-  version "2105.24"
-  sha256 "4edd89afd1009e1da121792c44bec6192001f3ffe2186a9e6f554c3bb8f5fb0b"
+  version "2106.17"
+  sha256 "22c030fec5e5c516580fd6e24b385c935baae2f6c110024aeba20240d533a68a"
 
   url "https://github.com/microsoft/cascadia-code/releases/download/v#{version}/CascadiaCode-#{version}.zip"
   name "Cascadia Code"
@@ -8,15 +8,15 @@ cask "font-cascadia-code" do
   homepage "https://github.com/microsoft/cascadia-code"
 
   font "ttf/static/CascadiaCode-Bold.ttf"
+  font "ttf/static/CascadiaCode-BoldItalic.ttf"
   font "ttf/static/CascadiaCode-ExtraLight.ttf"
+  font "ttf/static/CascadiaCode-ExtraLightItalic.ttf"
+  font "ttf/static/CascadiaCode-Italic.ttf"
   font "ttf/static/CascadiaCode-Light.ttf"
+  font "ttf/static/CascadiaCode-LightItalic.ttf"
   font "ttf/static/CascadiaCode-Regular.ttf"
   font "ttf/static/CascadiaCode-SemiBold.ttf"
+  font "ttf/static/CascadiaCode-SemiBoldItalic.ttf"
   font "ttf/static/CascadiaCode-SemiLight.ttf"
-  font "ttf/static/CascadiaCodeItalic-BoldItalic.ttf"
-  font "ttf/static/CascadiaCodeItalic-ExtraLightItalic.ttf"
-  font "ttf/static/CascadiaCodeItalic-Italic.ttf"
-  font "ttf/static/CascadiaCodeItalic-LightItalic.ttf"
-  font "ttf/static/CascadiaCodeItalic-SemiBoldItalic.ttf"
-  font "ttf/static/CascadiaCodeItalic-SemiLightItalic.ttf"
+  font "ttf/static/CascadiaCode-SemiLightItalic.ttf"
 end
