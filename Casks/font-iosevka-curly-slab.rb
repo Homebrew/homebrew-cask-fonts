65bf3fca92cecdb8f946bf6f405722f66aeeda7a
Reno <25192197+singularitti@users.noreply.github.com>
2021-07-17 15:30:37 -0400

    Update font-iosevka-curly-slab from 7.2.8 to 7.3.0

--- Casks/font-iosevka-curly-slab.rb
+++ Casks/font-iosevka-curly-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly-slab" do
-  version "7.2.8"
-  sha256 "453436f9a4e5ca178fb0e74c5e3678df6cb5744ee9adb5471ca86819ae83d026"
+  version "7.3.0"
+  sha256 "dcc13ddf9b9cdc9fcda93fd88c8971d9e47a5b9074e70ddd15315fb98403f4ba"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-slab-#{version}.zip"
   name "Iosevka Curly Slab"
