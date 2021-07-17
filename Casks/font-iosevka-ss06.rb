8ddbccfe30113ca91d0e3212ef0d5dc7182cb6dd
Bevan Kay <bevanjkay@gmail.com>
2021-07-17 19:51:44 +1000

    Revert "Update font-iosevka-ss06 from 7.2.8 to 7.3.0 (#4500)"
    This reverts commit f136c54ce4c15cb56d58407cb160bdb7344fe5d5.

--- Casks/font-iosevka-ss06.rb
+++ Casks/font-iosevka-ss06.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss06" do
-  version "7.3.0"
-  sha256 "41b0ee19833b3e923e972bb19fd5d7155009e5ef6851953f3dbe93de8f4ba456"
+  version "7.2.8"
+  sha256 "fd2574b867e175cf534dc4bcf9887f4a1f45da8922674704b1e922b389437f40"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss06-#{version}.zip"
   name "Iosevka SS06"
