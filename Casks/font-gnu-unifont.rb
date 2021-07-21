e05ef11beafe902f51c7d2cd0d2c0dd8b12295e3
Alexander Ronald Altman <alexanderaltman@me.com>
2021-07-20 17:48:27 -0700

    Update font-gnu-unifont from 13.0.05 to 13.0.06 (#4590)
    * Update font-gnu-unifont from 13.0.05 to 13.0.06
    
    * add livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/font-gnu-unifont.rb
+++ Casks/font-gnu-unifont.rb
@@ -1,11 +1,16 @@
 cask "font-gnu-unifont" do
-  version "13.0.05"
-  sha256 "f98e4866b71b4d86c2d7458405679914511c58963bb86d1fbf2251e8ed536f4e"
+  version "13.0.06"
+  sha256 "68def7a46df44241c7bf62de7ce0444e8ee9782f159c4b7553da9cfdc00be925"
 
   url "https://ftpmirror.gnu.org/unifont/unifont-#{version}/unifont-#{version}.tar.gz",
       verified: "ftpmirror.gnu.org/unifont/"
   name "GNU Unifont"
   homepage "https://unifoundry.com/unifont.html"
+
+  livecheck do
+    url "https://unifoundry.com/pub/unifont/"
+    regex(%r{href=["']?unifont[._-]v?(\d+(?:\.\d+)+)/?["' >]}i)
+  end
 
   font "unifont-#{version}/font/precompiled/unifont-#{version}.ttf"
   font "unifont-#{version}/font/precompiled/unifont_upper-#{version}.ttf"
