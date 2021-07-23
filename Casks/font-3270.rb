ce3847279b586826aa8af2ddf6b5b6e52cbefa2b
Bevan Kay <email@bevankay.me>
2021-07-24 00:27:00 +1000

    Update font-3270 from 2.0.4,ece94f6 to 2.3.0,fd00815 (#4642)
    * Update font-3270 from 2.0.4,ece94f6 to 2.3.0,fd00815
    
    * update livecheck

--- Casks/font-3270.rb
+++ Casks/font-3270.rb
@@ -1,11 +1,19 @@
 cask "font-3270" do
-  version "2.0.4,ece94f6"
-  sha256 "d5755c4774eb5ab81b8284d53021930ec55b191ab977f14d27b25c6f33358963"
+  version "2.3.0,fd00815"
+  sha256 "34332e5c2e501511d13fa739a6415168d7d7172728c93a27aef96c84f77d8ab6"
 
   url "https://github.com/rbanffy/3270font/releases/download/v#{version.before_comma}/3270_fonts_#{version.after_comma}.zip"
-  appcast "https://github.com/rbanffy/3270font/releases.atom"
   name "IBM 3270"
   homepage "https://github.com/rbanffy/3270font"
+
+  livecheck do
+    url "https://github.com/rbanffy/3270font/releases/latest"
+    regex(%r{href=.*/v?(\d+(?:\.\d+)+)/3270[._-]fonts[._-](.*)\.zip}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex)
+          .map { |matches| "#{matches[0]},#{matches[1]}" }
+    end
+  end
 
   font "3270-Regular.otf"
   font "3270-Regular.ttf"
