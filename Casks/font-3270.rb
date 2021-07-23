d80a02ce98b7194a8cda39b9432630905e376a9b
Bevan Kay <email@bevankay.me>
2021-07-23 20:58:27 +1000

    update livecheck

--- Casks/font-3270.rb
+++ Casks/font-3270.rb
@@ -3,9 +3,17 @@ cask "font-3270" do
   sha256 "34332e5c2e501511d13fa739a6415168d7d7172728c93a27aef96c84f77d8ab6"
 
   url "https://github.com/rbanffy/3270font/releases/download/v#{version.before_comma}/3270_fonts_#{version.after_comma}.zip"
-  appcast "https://github.com/rbanffy/3270font/releases.atom"
   name "IBM 3270"
   homepage "https://github.com/rbanffy/3270font"
+
+  livecheck do
+    url "https://github.com/rbanffy/3270font/releases/latest"
+    regex(%r{href=.*/v?(\d+(?:\.\d+)+)/3270[._-]fonts[._-](.*)\.zip}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex)
+          .map { |matches| "#{matches[0]},#{matches[1]}" }
+    end
+  end
 
   font "3270-Regular.otf"
   font "3270-Regular.ttf"
