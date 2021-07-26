dd4cf1df4c6a1560c81590d5c1c7e400a99462c6
Bevan Kay <email@bevankay.me>
2021-07-26 13:15:12 +1000

    font-tengwar-telcontar: add livecheck

--- Casks/font-tengwar-telcontar.rb
+++ Casks/font-tengwar-telcontar.rb
@@ -3,10 +3,15 @@ cask "font-tengwar-telcontar" do
   sha256 "f88780510e5c14f66e252fabce5d58febc7bec962d5c17f21ac82002ce85b925"
 
   url "https://downloads.sourceforge.net/freetengwar/TengwarTelcontar.#{version}.zip"
-  appcast "https://sourceforge.net/projects/freetengwar/rss?path=/TengwarFont"
   name "Tengwar Telcontar"
   desc "Tengwar Telcontar - a Unicode Tengwar font"
   homepage "http://freetengwar.sourceforge.net/tengtelc.html"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/href=.*?TengwarTelcontar[._-]v?(\d+)\.zip/i)
+  end
 
   font "tengtelc.ttf"
   font "tengtelcb.ttf"
