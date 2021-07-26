ad9c73d02246a6cf234338f9f9b9a1d38c099d72
Bevan Kay <email@bevankay.me>
2021-07-26 18:21:45 +1000

    font-tengwar-formal-csur: add livecheck (#4676)
    * font-tengwar-formal-csur: add livecheck
    
    * Update font-tengwar-formal-csur.rb

--- Casks/font-tengwar-formal-csur.rb
+++ Casks/font-tengwar-formal-csur.rb
@@ -3,10 +3,15 @@ cask "font-tengwar-formal-csur" do
   sha256 "924c282f7f967ecf6ca81003d95d4355dcccda9cf3fb7afdc746cb9d8f8250c7"
 
   url "https://downloads.sourceforge.net/freetengwar/TengwarFormalCSUR#{version}.zip"
-  appcast "https://sourceforge.net/projects/freetengwar/rss?path=/TengwarFont"
   name "Tengwar Formal CSUR"
   desc "Tengwar Unicode font compliant with CSUR"
   homepage "http://freetengwar.sourceforge.net/formal.html"
+
+  livecheck do
+    url :homepage
+    strategy :page_match
+    regex(/href=.*?TengwarFormalCSUR(\d+)\.zip/i)
+  end
 
   font "TengwarFormalCSUR#{version}/TengwarFormalCSUR.ttf"
 end
