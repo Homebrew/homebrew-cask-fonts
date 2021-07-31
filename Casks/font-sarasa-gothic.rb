bfc560329113c05894cfaa2ea4f0c73a51794fb0
Reno <25192197+singularitti@users.noreply.github.com>
2021-07-31 09:30:23 -0400

    Update font-sarasa-gothic from 0.33.1 to 0.34.0

--- Casks/font-sarasa-gothic.rb
+++ Casks/font-sarasa-gothic.rb
@@ -1,6 +1,6 @@
 cask "font-sarasa-gothic" do
-  version "0.33.1"
-  sha256 "3311e1dfe0c813c37402143993310058b4a36bdc3efeedb95c255401d7cb66b2"
+  version "0.34.0"
+  sha256 "d8191e3a0867170e62fa8b238c5ae7ed7f0174c3bf08edff91b5b1ecef1677aa"
 
   url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
   name "Sarasa Gothic"
