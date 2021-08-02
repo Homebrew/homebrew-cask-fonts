3a87b334c20ebffcd92811345ee5946755e7f71a
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-02 10:44:24 -0400

    Update font-iosevka-ss01 from 9.0.0 to 9.0.1 (#4767)

--- Casks/font-iosevka-ss01.rb
+++ Casks/font-iosevka-ss01.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss01" do
-  version "9.0.0"
-  sha256 "57c0de636c0bdde19c9460fedc03fb53cbe4364b9de68ff24d3e662eb3e2a63c"
+  version "9.0.1"
+  sha256 "e9ddf8d5f8edcf73fef1d64c3fb1147a0d6ad3af861644bc3958af14e20214e0"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss01-#{version}.zip"
   name "Iosevka SS01"
