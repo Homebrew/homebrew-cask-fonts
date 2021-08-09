b95952f580a06c8b7f84d6e23c30e482a4204833
Hideo Yasumoto <hideo54@hideo54.com>
2021-08-09 19:30:09 +0900

    brew style --fix

--- Casks/font-firge.rb
+++ Casks/font-firge.rb
@@ -1,11 +1,11 @@
 cask "font-firge" do
   version "0.1.0"
-  desc "Fira Mono と源真ゴシックを合成したプログラミングフォント Firge (ファージ)"
   sha256 "d94ef9db6016d9dbf043b03ab45f36dd266ca930063294f464976fc09fa99489"
 
   url "https://github.com/yuru7/Firge/releases/download/v#{version}/Firge_v#{version}.zip"
   appcast "https://github.com/yuru7/Firge/releases.atom"
   name "Firge"
+  desc "Fira Mono と源真ゴシックを合成したプログラミングフォント Firge (ファージ)"
   homepage "https://github.com/yuru7/Firge"
 
   font "Firge_v0.1.0/Firge-Regular.ttf"
--- Casks/font-firgenerd.rb
+++ Casks/font-firgenerd.rb
@@ -1,11 +1,11 @@
 cask "font-firgenerd" do
   version "0.1.0"
-  desc "Fira Mono と源真ゴシックを合成したプログラミングフォント Firge (ファージ)"
   sha256 "239048c643207db4912e248d7fd3aaab948f9be78756b0f59e23cd93eab436f6"
 
   url "https://github.com/yuru7/Firge/releases/download/v#{version}/FirgeNerd_v#{version}.zip"
   appcast "https://github.com/yuru7/Firge/releases.atom"
   name "FirgeNerd"
+  desc "Fira Mono と源真ゴシックを合成したプログラミングフォント Firge (ファージ)"
   homepage "https://github.com/yuru7/Firge"
 
   font "FirgeNerd_v0.1.0/FirgeNerd-Regular.ttf"
