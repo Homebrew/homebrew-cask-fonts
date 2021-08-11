2103f5cd4011426dcca46b00c5aed2feaeb19815
Victor Song <vms2@rice.edu>
2021-08-11 17:38:56 -0500

    Update font-recursive-code from 1.078 to 1.079 (#4793)
    * Update font-recursive-code from 1.078 to 1.079
    
    * Update font-recursive-code.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-recursive-code.rb
+++ Casks/font-recursive-code.rb
@@ -1,6 +1,6 @@
 cask "font-recursive-code" do
-  version "1.078"
-  sha256 "2571694c0ebfef1b89604b9f66db80961941048b1ca5a0015c2be7401c67c556"
+  version "1.079"
+  sha256 "2ceb98649200c4f4cac06e43748a447de41540f9f8895a8d090af10e6beae282"
 
   url "https://github.com/arrowtype/recursive/releases/download/v#{version}/ArrowType-Recursive-#{version}.zip"
   name "Recursive Code"
