e6dfc62b7d3af6b5cd6ff86c7e766a95c34f1e23
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-14 20:15:00 -0400

    Update font-iosevka-aile from 9.0.1 to 10.0.0

--- Casks/font-iosevka-aile.rb
+++ Casks/font-iosevka-aile.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-aile" do
-  version "9.0.1"
-  sha256 "9a32890749f42e59cfb40040787afb791d45d9faf8070e56b231b8e796dffeb8"
+  version "10.0.0"
+  sha256 "d1b3934f78b509ea9ef998cbc653763bf7e7b5258dcc98030abbbc0a3fd2a0da"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-aile-#{version}.zip"
   name "Iosevka Aile"
