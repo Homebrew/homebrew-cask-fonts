bf37570ac75cf51d10882c92953e903fd5393954
Bevan Kay <email@bevankay.me>
2021-08-29 20:04:25 +1000

    Update font-iosevka-aile from 10.1.0 to 10.1.1 (#4851)

--- Casks/font-iosevka-aile.rb
+++ Casks/font-iosevka-aile.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-aile" do
-  version "10.1.0"
-  sha256 "d68b4f556c1834ce88c0d373524c27b586868676f8f8820aeb96a952b79cde1e"
+  version "10.1.1"
+  sha256 "4c95b598e12d0792a7a21cd2f8e6c6648012b94df89820e35c28d02616f08f16"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-aile-#{version}.zip"
   name "Iosevka Aile"
