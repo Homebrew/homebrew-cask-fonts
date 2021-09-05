aad463484986830fd1cd355b37bdc1f1a6749ed0
Reno <25192197+singularitti@users.noreply.github.com>
2021-09-04 21:46:57 -0400

    Update font-juliamono from 0.041 to 0.042 (#4877)

--- Casks/font-juliamono.rb
+++ Casks/font-juliamono.rb
@@ -1,6 +1,6 @@
 cask "font-juliamono" do
-  version "0.041"
-  sha256 "124181c585eb63dbc67677220c8d8a5d1680e6542380f015958d3d8e5abb1c18"
+  version "0.042"
+  sha256 "bf7894f2c682f00a5ec733cd1ac02883a8d3f7b03e093a5bcd8848d08e9c1ffc"
 
   url "https://github.com/cormullion/juliamono/releases/download/v#{version}/JuliaMono.tar.gz"
   name "JuliaMono"
