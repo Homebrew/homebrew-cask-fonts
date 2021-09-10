621d106b9d52434404718dfa93c15e424f538bbe
Bevan Kay <email@bevankay.me>
2021-09-10 23:05:16 +1000

    Update font-glow-sans-tc-condensed.rb

--- Casks/font-glow-sans-tc-condensed.rb
+++ Casks/font-glow-sans-tc-condensed.rb
@@ -3,7 +3,6 @@ cask "font-glow-sans-tc-condensed" do
   sha256 "3ebe0b46abecd7a0f8d6232dd310205c38ab3d30ab624bdba18fae591bfd5d50"
 
   url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansTC-Condensed-v#{version}.zip"
-  appcast "https://github.com/welai/glow-sans/releases.atom"
   name "Glow Sans TC Condensed"
   homepage "https://github.com/welai/glow-sans"
 
