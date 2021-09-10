28b9ccb6f392e1f772ca3b8791dfa627ffa49aff
Bevan Kay <email@bevankay.me>
2021-09-10 23:23:05 +1000

    Update font-glow-sans-sc-wide from 0.92 to 0.93 (#4889)
    * Update font-glow-sans-sc-wide from 0.92 to 0.93
    
    * Update font-glow-sans-sc-wide.rb

--- Casks/font-glow-sans-sc-wide.rb
+++ Casks/font-glow-sans-sc-wide.rb
@@ -1,9 +1,8 @@
 cask "font-glow-sans-sc-wide" do
-  version "0.92"
-  sha256 "0cd44c72522f4c45c888c8159a7d5436a21e5af7aa12d9c82cfac4cb80aa7d40"
+  version "0.93"
+  sha256 "036eca2916a3636b32d006937d498cc33873317c8bf01057580b21526f59e79b"
 
   url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansSC-Wide-v#{version}.zip"
-  appcast "https://github.com/welai/glow-sans/releases.atom"
   name "Glow Sans SC Wide"
   homepage "https://github.com/welai/glow-sans"
 
