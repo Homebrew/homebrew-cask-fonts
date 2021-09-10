3a0074366a83bf52515e133920dd1d5367e35318
Bevan Kay <email@bevankay.me>
2021-09-10 23:05:07 +1000

    Update font-glow-sans-tc-extended.rb

--- Casks/font-glow-sans-tc-extended.rb
+++ Casks/font-glow-sans-tc-extended.rb
@@ -3,7 +3,6 @@ cask "font-glow-sans-tc-extended" do
   sha256 "a1e4745dc320c948875aa38f435886b0c54ab24a4410d3fdba719a854e9b491f"
 
   url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansTC-Extended-v#{version}.zip"
-  appcast "https://github.com/welai/glow-sans/releases.atom"
   name "Glow Sans TC Extended"
   homepage "https://github.com/welai/glow-sans"
 
