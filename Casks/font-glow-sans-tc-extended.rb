bea3071c11d492977771b7b79f755493039bb362
Bevan Kay <email@bevankay.me>
2021-09-10 23:24:41 +1000

    Update font-glow-sans-tc-extended from 0.92 to 0.93 (#4891)
    * Update font-glow-sans-tc-extended from 0.92 to 0.93
    
    * Update font-glow-sans-tc-extended.rb

--- Casks/font-glow-sans-tc-extended.rb
+++ Casks/font-glow-sans-tc-extended.rb
@@ -1,9 +1,8 @@
 cask "font-glow-sans-tc-extended" do
-  version "0.92"
-  sha256 "9c83a7cdf6ea3359e88f1a581ee7450d87028c011d7f69527841e4a54e8ddc67"
+  version "0.93"
+  sha256 "a1e4745dc320c948875aa38f435886b0c54ab24a4410d3fdba719a854e9b491f"
 
   url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansTC-Extended-v#{version}.zip"
-  appcast "https://github.com/welai/glow-sans/releases.atom"
   name "Glow Sans TC Extended"
   homepage "https://github.com/welai/glow-sans"
 
