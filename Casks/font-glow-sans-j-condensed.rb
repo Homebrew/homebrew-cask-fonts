4a3b41f247142753655f7af120cb1df0a8f2ef34
Bevan Kay <email@bevankay.me>
2021-09-10 23:18:35 +1000

    Update font-glow-sans-j-condensed from 0.92 to 0.93 (#4881)
    * Update font-glow-sans-j-condensed from 0.92 to 0.93
    
    * Update font-glow-sans-j-condensed.rb

--- Casks/font-glow-sans-j-condensed.rb
+++ Casks/font-glow-sans-j-condensed.rb
@@ -1,9 +1,8 @@
 cask "font-glow-sans-j-condensed" do
-  version "0.92"
-  sha256 "08339895dacfc9e23c28d60418dc532571ba2d68eaf0d3a14aff611e24b631be"
+  version "0.93"
+  sha256 "3b6d38f707d204a2e8341b06ff0402dd9bb7daa095a6d5ab9da132c1966597b5"
 
   url "https://github.com/welai/glow-sans/releases/download/v#{version}/GlowSansJ-Condensed-v#{version}.zip"
-  appcast "https://github.com/welai/glow-sans/releases.atom"
   name "Glow Sans J Condensed"
   homepage "https://github.com/welai/glow-sans"
 
