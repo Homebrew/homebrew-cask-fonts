edbe5be23cce54576e96e07dff077b2f7a28e8f4
Bevan Kay <email@bevankay.me>
2021-09-19 23:04:18 +1000

    Update font-iosevka-slab from 10.2.0 to 10.3.0 (#4931)

--- Casks/font-iosevka-slab.rb
+++ Casks/font-iosevka-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-slab" do
-  version "10.2.0"
-  sha256 "b6e2defdaf65cf3fa13de9bb99e25a51ebb591cdca38be4d848daf45db5d1d9b"
+  version "10.3.0"
+  sha256 "e69b28872d6a5dc5e25ae0609abb6c596523b587808b9a4eaec38a80cf130f4b"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-slab-#{version}.zip"
   name "Iosevka Slab"
