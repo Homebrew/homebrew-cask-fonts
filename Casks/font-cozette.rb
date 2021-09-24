3b165d2c8c552c45521f8885bdc99526adfa06d8
Bevan Kay <email@bevankay.me>
2021-09-24 17:59:55 +1000

    Update font-cozette from 1.11.3 to 1.12.0 (#4954)

--- Casks/font-cozette.rb
+++ Casks/font-cozette.rb
@@ -1,6 +1,6 @@
 cask "font-cozette" do
-  version "1.11.3"
-  sha256 "a8941c4485ff75c3aab55b2c21994d00b440d9d7415143b1cc71337ba58cc3e4"
+  version "1.12.0"
+  sha256 "1929ee2fa0ae93170a11b97d48f97e14c0888e03b5831180be78b141fb921840"
 
   url "https://github.com/slavfox/Cozette/releases/download/v.#{version}/CozetteVector.dfont"
   name "Cozette"
