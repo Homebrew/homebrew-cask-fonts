32c5d819bad6c5c652cf11cb0a0631757f402844
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-02 19:22:10 -0400

    Update font-iosevka from 10.3.0 to 10.3.1 (#4968)

--- Casks/font-iosevka.rb
+++ Casks/font-iosevka.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka" do
-  version "10.3.0"
-  sha256 "3f5d1d06d4ec270ec76842e5a7afd7a3db444874a64e07fbc9d74b58c3eb7369"
+  version "10.3.1"
+  sha256 "cb3332244a4dc1c6bec85b7b6d52155df24285da14c407682e3fe4d2e981ff0b"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
   name "Iosevka"
