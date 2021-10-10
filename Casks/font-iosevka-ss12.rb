34e1497a7f1c3bbb318750db90837ebe9b21ae91
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-10 11:22:35 -0400

    Update font-iosevka-ss12 from 10.3.1 to 10.3.2 (#5011)

--- Casks/font-iosevka-ss12.rb
+++ Casks/font-iosevka-ss12.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss12" do
-  version "10.3.1"
-  sha256 "66e31a5557bb8ac24ea6c2b9745c3324f7b84f5ae1b888cc232afdc2b10d05b3"
+  version "10.3.2"
+  sha256 "7274f7017ef030af342fa68b3daf9cfbc889a0f4cd4b2d87e22941e1e4a89fa0"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss12-#{version}.zip"
   name "Iosevka SS12"
