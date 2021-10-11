b53855813e575f0c4f7646b2de7a2d8e74811735
shaunsingh <shaunsingh0207@gmail.com>
2021-10-11 16:25:46 -0400

    Add Ligaturized SFMono Nerd Font
    Correct Formatting

---
+++ Casks/font-sf-mono-nerd-font-ligaturized.rb
@@ -0,0 +1,22 @@
+cask "font-sf-mono-nerd-font-ligaturized" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/shaunsingh/SFMono-Nerd-Font-Ligaturized/archive/main.zip"
+  name "SFMono Nerd-Font-Ligaturized"
+  desc "SFMono with Nerd Font Glyphs and Fira Code Ligatures"
+  homepage "https://github.com/shaunsingh/SFMono-Nerd-Font-Ligaturized"
+
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-Bold.otf"
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-BoldItalic.otf"
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-Heavy.otf"
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-HeavyItalic.otf"
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-Light.otf"
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-LightItalic.otf"
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-Medium.otf"
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-MediumItalic.otf"
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-Regular.otf"
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-RegularItalic.otf"
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-Semibold.otf"
+  font "SFMono-Nerd-Font-Ligaturized-Main/LigaSFMonoNerdFont-SemiboldItalic.otf"
+end
