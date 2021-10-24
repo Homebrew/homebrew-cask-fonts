4eadc4f26f33c3d202ae6d1c35cf93097162b7d9
Bevan Kay <email@bevankay.me>
2021-10-25 09:06:33 +1100

    Update font-iosevka from 10.3.3 to 10.3.4

--- Casks/font-iosevka.rb
+++ Casks/font-iosevka.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka" do
-  version "10.3.3"
-  sha256 "3c259c401f175fa958a028d01ce2a6f8357df7d5211bcb5395f74da2ea563d6c"
+  version "10.3.4"
+  sha256 "f851ba5917a84a983e07e74a834af737e91577f650a2480953ef3691872866a9"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
   name "Iosevka"
