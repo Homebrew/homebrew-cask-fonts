7a12ab150b878a665a12fb680fd05bbadd6fb3fd
Bevan Kay <email@bevankay.me>
2021-11-10 10:50:11 +1100

    font-museo: update url

--- Casks/font-museo.rb
+++ Casks/font-museo.rb
@@ -2,7 +2,8 @@ cask "font-museo" do
   version "2.002"
   sha256 :no_check
 
-  url "http://www.abstractfonts.com/download/14890?option=zip"
+  url "https://abstractfonts-downloads.s3.amazonaws.com/zips/1/4/8/9/0/contents/museo-300.zip",
+      verified: "abstractfonts-downloads.s3.amazonaws.com/"
   name "Museo"
   homepage "http://www.abstractfonts.com/font/14890"
 
