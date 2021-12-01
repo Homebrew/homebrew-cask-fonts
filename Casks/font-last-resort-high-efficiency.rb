4f0ed8b41e5b11f375580924fbeb87e59d3cc653
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-01 10:59:59 -0800

    Update Last Resort to 14.000

--- Casks/font-last-resort-high-efficiency.rb
+++ Casks/font-last-resort-high-efficiency.rb
@@ -1,9 +1,8 @@
 cask "font-last-resort-high-efficiency" do
-  version "13.001"
-  sha256 "07b64116e1a39fa42dee23bbe81fd464809043cc4c0243d3e67622ed29efbe6c"
+  version "14.000"
+  sha256 "74507aa7ae9b02a469a6acbba53db83f978b76a89e8ccec95bb5bc14941588f9"
 
   url "https://github.com/unicode-org/last-resort-font/releases/download/#{version}/LastResortHE-Regular.ttf"
-  appcast "https://github.com/unicode-org/last-resort-font/releases.atom"
   name "Last Resort High-Efficiency"
   desc "Special-purpose font, providing glyphs to represent types of Unicode characters"
   homepage "https://github.com/unicode-org/last-resort-font"
