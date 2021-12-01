e83fb12574d8d15a3eb3b21fa816da821285e4e6
Alexander Ronald Altman <alexanderaltman@me.com>
2021-12-01 10:59:59 -0800

    Update Last Resort to 14.000

--- Casks/font-last-resort.rb
+++ Casks/font-last-resort.rb
@@ -1,9 +1,8 @@
 cask "font-last-resort" do
-  version "13.001"
-  sha256 "2cdfa3f7d70ee06c32e9bb37c94634cecd54ba018e5a8110e853b394e0f91f01"
+  version "14.000"
+  sha256 "da83a62294e74d963a10de4c3750ccf089273e3b7fc6744daef9844163ade078"
 
   url "https://github.com/unicode-org/last-resort-font/releases/download/#{version}/LastResort-Regular.ttf"
-  appcast "https://github.com/unicode-org/last-resort-font/releases.atom"
   name "Last Resort"
   desc "Special-purpose font, providing glyphs to represent types of Unicode characters"
   homepage "https://github.com/unicode-org/last-resort-font"
