d1f6048f4ebc23efdf4a9e42c840194ca0bfeecb
yhu266 <56977205+yhu266@users.noreply.github.com>
2021-12-06 12:48:14 -0600

    remove appcast

--- Casks/font-chomsky.rb
+++ Casks/font-chomsky.rb
@@ -3,7 +3,6 @@ cask "font-chomsky" do
   sha256 "7b1767776b6653a31dfa510ca2927d3f1cd38b17c026515acfe510bb8789cefa"
 
   url "https://github.com/ctrlcctrlv/chomsky/releases/download/v#{version}/Chomsky.otf"
-  appcast "https://github.com/ctrlcctrlv/chomsky/releases.atom"
   name "Chomsky"
   desc "New York Times masthead, reimagined as a full font"
   homepage "https://github.com/ctrlcctrlv/chomsky"
