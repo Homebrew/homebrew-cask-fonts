9a26765a3c706504a1147e9e5c75fb316c23b3bc
Nikita Prokopov <niki@tonsky.me>
2021-12-06 22:24:10 +0100

    Update font-fira-code.rb (#5209)

--- Casks/font-fira-code.rb
+++ Casks/font-fira-code.rb
@@ -1,6 +1,6 @@
 cask "font-fira-code" do
-  version "6.1"
-  sha256 "0fca4e7393a0dfb7b975cb2738d5cab5eac87222c900f127a135ad856afc95cc"
+  version "6.2"
+  sha256 "0949915ba8eb24d89fd93d10a7ff623f42830d7c5ffc3ecbf960e4ecad3e3e79"
 
   url "https://github.com/tonsky/FiraCode/releases/download/#{version}/Fira_Code_v#{version}.zip"
   name "Fira Code"
