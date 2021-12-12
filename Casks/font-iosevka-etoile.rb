82de9d39453e3ac1243a1b1c9bbe326e29daa87d
Reno <25192197+singularitti@users.noreply.github.com>
2021-12-11 19:56:57 -0500

    Update font-iosevka-etoile from 11.2.0 to 11.2.1

--- Casks/font-iosevka-etoile.rb
+++ Casks/font-iosevka-etoile.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-etoile" do
-  version "11.2.0"
-  sha256 "c7eb70a31133251ec90d74d29abeb14b9139ce6cd4f0e1f5c52e11159942eb24"
+  version "11.2.1"
+  sha256 "c062078db3ea57d1cecd87788f282e7fba7e8e335cc8b4337d9d8e8e9fde24b5"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-etoile-#{version}.zip"
   name "Iosevka Etoile"
