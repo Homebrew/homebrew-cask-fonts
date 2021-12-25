6af430f9b3d20978036584fe3e87a2d64f4dd644
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-25 11:00:36 +0800

    Update font-smooch.rb

--- Casks/font-smooch.rb
+++ Casks/font-smooch.rb
@@ -5,7 +5,7 @@ cask "font-smooch" do
   url "https://github.com/google/fonts/raw/main/ofl/smooch/Smooch-Regular.ttf",
       verified: "github.com/google/fonts/"
   name "Smooch"
-  desc "Slightly bolder than other hand-lettered scripts by its creator and has up to five stylistic sets as well as initial and final positional forms expanding its utility"
+  desc "Slightly bolder than other hand-lettered scripts"
   homepage "https://fonts.google.com/specimen/Smooch"
 
   font "Smooch-Regular.ttf"
