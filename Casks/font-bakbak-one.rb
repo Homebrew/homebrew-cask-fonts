4e459cbf0f8c0b28cf29e18117bb5db3cbf4964e
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-25 10:48:31 +0800

    Update font-bakbak-one.rb

--- Casks/font-bakbak-one.rb
+++ Casks/font-bakbak-one.rb
@@ -5,7 +5,7 @@ cask "font-bakbak-one" do
   url "https://github.com/google/fonts/raw/main/ofl/bakbakone/BakbakOne-Regular.ttf",
       verified: "github.com/google/fonts/"
   name "Bakbak One"
-  desc "Loud and present but also neutral enough to render anything from a warning to a mockery"
+  desc "Render anything from a warning to a mockery"
   homepage "https://fonts.google.com/specimen/Bakbak+One"
 
   font "BakbakOne-Regular.ttf"
