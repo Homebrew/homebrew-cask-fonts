0841658cdda341419df504cb96fb01b34446b63b
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-25 11:08:38 +0800

    Update font-waterfall.rb

--- Casks/font-waterfall.rb
+++ Casks/font-waterfall.rb
@@ -5,7 +5,7 @@ cask "font-waterfall" do
   url "https://github.com/google/fonts/raw/main/ofl/waterfall/Waterfall-Regular.ttf",
       verified: "github.com/google/fonts/"
   name "Waterfall"
-  desc "Calligraphic script that combines elements of traditional hand lettered italic form with more formal, elegant script connecting characters"
+  desc "Calligraphic script"
   homepage "https://fonts.google.com/specimen/Waterfall"
 
   font "Waterfall-Regular.ttf"
