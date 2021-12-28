266106e84f1bc8fae916783f19600bbd6919f2eb
Reno <25192197+singularitti@users.noreply.github.com>
2021-12-27 20:16:29 -0500

    Update font-iosevka-slab from 11.2.3 to 11.2.4

--- Casks/font-iosevka-slab.rb
+++ Casks/font-iosevka-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-slab" do
-  version "11.2.3"
-  sha256 "18469d240a451f62e3ff1532d9cd359d549ddb52e8fb2c1a6f0d6a6effad1a7d"
+  version "11.2.4"
+  sha256 "e2b3dbf0bdc650e5dcf364337b3624f4159cb37da0d5acb5cf389425111106b3"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-slab-#{version}.zip"
   name "Iosevka Slab"
