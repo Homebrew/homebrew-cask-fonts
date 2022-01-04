1eafa23ab903f23cdd58d6dff0caaec094e64d07
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-04 19:37:06 +0800

    Update font-roboto.rb

--- Casks/font-roboto.rb
+++ Casks/font-roboto.rb
@@ -2,9 +2,8 @@ cask "font-roboto" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/tree/main/apache/roboto",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/google/fonts/raw/main/apache/roboto",
+      verified: "github.com/google/fonts/"
   name "Roboto"
   homepage "https://fonts.google.com/specimen/Roboto"
 
