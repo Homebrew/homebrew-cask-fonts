90ca16812c684b284bef65b0e6056c6e507c2e0f
Raphael Fetzer <58503+pheraph@users.noreply.github.com>
2022-01-04 12:03:10 +0100

    Update font-roboto.rb
    URL has changed.

--- Casks/font-roboto.rb
+++ Casks/font-roboto.rb
@@ -2,7 +2,7 @@ cask "font-roboto" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/apache/roboto",
+  url "https://github.com/google/fonts/tree/main/apache/roboto",
       verified: "github.com/google/fonts/",
       using:    :svn
   name "Roboto"
