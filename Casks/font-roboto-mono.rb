b1da25a83890ed3635f4ed7cfe489a50c75e2916
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-04 19:35:27 +0800

    Update font-roboto-mono.rb

--- Casks/font-roboto-mono.rb
+++ Casks/font-roboto-mono.rb
@@ -2,9 +2,8 @@ cask "font-roboto-mono" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/tree/main/apache/robotomono",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/google/fonts/raw/main/apache/robotomono",
+      verified: "github.com/google/fonts/"
   name "Roboto Mono"
   homepage "https://fonts.google.com/specimen/Roboto+Mono"
 
