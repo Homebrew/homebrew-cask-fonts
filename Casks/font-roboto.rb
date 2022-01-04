259c58d8082b81d017f083e1cfb673c60d4a5bac
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-04 19:50:49 +0800

    Update font-roboto.rb

--- Casks/font-roboto.rb
+++ Casks/font-roboto.rb
@@ -2,9 +2,11 @@ cask "font-roboto" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/apache/roboto",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/trunk/apache/roboto",
+      verified: "github.com/google/fonts/",
+      using:    :svn
   name "Roboto"
+  desc "Font with a mechanical skeleton and the forms are largely geometric"
   homepage "https://fonts.google.com/specimen/Roboto"
 
   font "Roboto-Italic[wdth,wght].ttf"
