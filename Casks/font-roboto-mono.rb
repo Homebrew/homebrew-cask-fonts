f9425c54d69816b168a33467700f423c1313e346
Raphael Fetzer <58503+pheraph@users.noreply.github.com>
2022-01-04 12:01:17 +0100

    Update font-roboto-mono.rb
    URL has changed

--- Casks/font-roboto-mono.rb
+++ Casks/font-roboto-mono.rb
@@ -2,7 +2,7 @@ cask "font-roboto-mono" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/apache/robotomono",
+  url "https://github.com/google/fonts/tree/main/apache/robotomono",
       verified: "github.com/google/fonts/",
       using:    :svn
   name "Roboto Mono"
