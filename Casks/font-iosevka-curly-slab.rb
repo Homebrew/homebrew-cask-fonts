6fff30b433d901fedff056a7676f410482bae90d
Reno <25192197+singularitti@users.noreply.github.com>
2022-01-08 08:03:26 -0500

    Update font-iosevka-curly-slab from 11.2.4 to 11.2.5

--- Casks/font-iosevka-curly-slab.rb
+++ Casks/font-iosevka-curly-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly-slab" do
-  version "11.2.4"
-  sha256 "c2a39afb1f126734877d0e14d5c0d601b3958b40286ac6634545c5d1fd0fbc92"
+  version "11.2.5"
+  sha256 "34b99d77a64798e1c52afd6de3a0a79108dca28e205359ffa0d382052ae5e1b2"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-slab-#{version}.zip"
   name "Iosevka Curly Slab"
