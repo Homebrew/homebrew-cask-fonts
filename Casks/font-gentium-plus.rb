79e1524145c42448095a5f4e646b2d0fe67fcb01
arkn98 <20590666+arkn98@users.noreply.github.com>
2022-01-11 06:59:05 +0530

    Update font-gentium-plus from 5.000 to 6.001 (#5364)
    * Update font-gentium-plus from 5.000 to 6.001
    
    * Update font-gentium-plus.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-gentium-plus.rb
+++ Casks/font-gentium-plus.rb
@@ -1,11 +1,18 @@
 cask "font-gentium-plus" do
-  version "5.000"
-  sha256 "335911f17bd2de4e43742e1d0367cfeff19a90abf7ed604f100a42705042e154"
+  version "6.001"
+  sha256 "7589cb8fa9887c34070e14fc78108102657ffac7ed36e81bb2058bde2da9430c"
 
-  url "https://software.sil.org/downloads/r/gentium/GentiumPlus-#{version}.zip"
+  url "https://github.com/silnrsi/font-gentium/releases/download/v#{version}/GentiumPlus-#{version}.zip",
+      verified: "github.com/silnrsi/font-gentium/"
   name "Gentium"
   homepage "https://software.sil.org/gentium/"
 
-  font "GentiumPlus-5.000/GentiumPlus-I.ttf"
-  font "GentiumPlus-5.000/GentiumPlus-R.ttf"
+  font "GentiumPlus-#{version}/GentiumBookPlus-Bold.ttf"
+  font "GentiumPlus-#{version}/GentiumBookPlus-BoldItalic.ttf"
+  font "GentiumPlus-#{version}/GentiumBookPlus-Italic.ttf"
+  font "GentiumPlus-#{version}/GentiumBookPlus-Regular.ttf"
+  font "GentiumPlus-#{version}/GentiumPlus-Bold.ttf"
+  font "GentiumPlus-#{version}/GentiumPlus-BoldItalic.ttf"
+  font "GentiumPlus-#{version}/GentiumPlus-Italic.ttf"
+  font "GentiumPlus-#{version}/GentiumPlus-Regular.ttf"
 end
