2aa5ccdf4eb997dd15d0069615923087c73968c4
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-11 09:10:23 +0800

    Update font-gentium-plus.rb

--- Casks/font-gentium-plus.rb
+++ Casks/font-gentium-plus.rb
@@ -2,10 +2,17 @@ cask "font-gentium-plus" do
   version "6.001"
   sha256 "7589cb8fa9887c34070e14fc78108102657ffac7ed36e81bb2058bde2da9430c"
 
-  url "https://software.sil.org/downloads/r/gentium/GentiumPlus-#{version}.zip"
+  url "https://github.com/silnrsi/font-gentium/releases/download/v#{version}/GentiumPlus-#{version}.zip",
+      verified: "github.com/silnrsi/font-gentium/"
   name "Gentium"
   homepage "https://software.sil.org/gentium/"
 
-  font "GentiumPlus-5.000/GentiumPlus-I.ttf"
-  font "GentiumPlus-5.000/GentiumPlus-R.ttf"
+  font "GentiumPlus-#{version}/GentiumBookPlus-Bold.ttf"
+  font "GentiumPlus-#{version}/GentiumBookPlus-BoldItalic.ttf"
+  font "GentiumPlus-#{version}/GentiumBookPlus-Italic.ttf"
+  font "GentiumPlus-#{version}/GentiumBookPlus-Regular.ttf"
+  font "GentiumPlus-#{version}/GentiumPlus-Bold.ttf"
+  font "GentiumPlus-#{version}/GentiumPlus-BoldItalic.ttf"
+  font "GentiumPlus-#{version}/GentiumPlus-Italic.ttf"
+  font "GentiumPlus-#{version}/GentiumPlus-Regular.ttf"
 end
