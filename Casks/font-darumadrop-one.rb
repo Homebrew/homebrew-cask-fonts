5af8b108b1eaa635bc830268510b403593b8719a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2022-01-16 07:22:20 +0100

    Update Google Fonts (#5368)
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>

---
+++ Casks/font-darumadrop-one.rb
@@ -0,0 +1,11 @@
+cask "font-darumadrop-one" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/darumadropone/DarumadropOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
+  name "Darumadrop One"
+  homepage "https://fonts.google.com/specimen/Darumadrop+One"
+
+  font "DarumadropOne-Regular.ttf"
+end
