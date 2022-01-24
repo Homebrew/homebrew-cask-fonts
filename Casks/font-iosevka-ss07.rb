b4079645f246c4a9cfed147dcbe20b31d5e06c96
Reno <25192197+singularitti@users.noreply.github.com>
2022-01-23 23:55:35 -0500

    Update font-iosevka-ss07 from 11.2.6 to 11.2.7

--- Casks/font-iosevka-ss07.rb
+++ Casks/font-iosevka-ss07.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss07" do
-  version "11.2.6"
-  sha256 "accdcd100dd78434f765fe741721f44ac8df96e1f5f9801c71d70d790b84b87a"
+  version "11.2.7"
+  sha256 "f8aa790600ff46ab5f79a9a51a59977af9425711d44d82bd1aaf6271846536ce"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss07-#{version}.zip"
   name "Iosevka SS07"
