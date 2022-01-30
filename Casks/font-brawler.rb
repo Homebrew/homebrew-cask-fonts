79db65c83e5291253faec46751c05fcb4c614265
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2022-01-30 07:01:40 +0100

    Update Google Fonts (#5393)
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>

--- Casks/font-brawler.rb
+++ Casks/font-brawler.rb
@@ -2,10 +2,12 @@ cask "font-brawler" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/brawler/Brawler-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/trunk/ofl/brawler",
+      verified: "github.com/google/fonts/",
+      using:    :svn
   name "Brawler"
   homepage "https://fonts.google.com/specimen/Brawler"
 
+  font "Brawler-Bold.ttf"
   font "Brawler-Regular.ttf"
 end
--- Casks/font-wire-one.rb
+++ Casks/font-wire-one.rb
@@ -2,10 +2,10 @@ cask "font-wire-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/wireone/WireOne.ttf",
+  url "https://github.com/google/fonts/raw/main/ofl/wireone/WireOne-Regular.ttf",
       verified: "github.com/google/fonts/"
   name "Wire One"
   homepage "https://fonts.google.com/specimen/Wire+One"
 
-  font "WireOne.ttf"
+  font "WireOne-Regular.ttf"
 end
