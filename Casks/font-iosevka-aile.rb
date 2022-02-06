681e5c84470de430e64bedd78f7a5db565a24544
Hanlin He <hanling.he@gmail.com>
2022-02-05 18:19:13 -0800

    Update font-iosevka-aile from 11.2.7 to 11.3.0

--- Casks/font-iosevka-aile.rb
+++ Casks/font-iosevka-aile.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-aile" do
-  version "11.2.7"
-  sha256 "5cab2848aa7f01a5b44e7dc436a9e7bd86488642e13f5687e47ad5f6f2c9501e"
+  version "11.3.0"
+  sha256 "58e4985d26c71d1d68d42a8496fab70c01765236770738276eb268f8323a3802"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-aile-#{version}.zip"
   name "Iosevka Aile"
