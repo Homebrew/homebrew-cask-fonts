e889f5cbc29bbe574893d997c6e4dc8c171bf542
Hanlin He <hanling.he@gmail.com>
2022-02-05 18:33:40 -0800

    Update font-iosevka-ss01 from 11.2.7 to 11.3.0 (#5403)

--- Casks/font-iosevka-ss01.rb
+++ Casks/font-iosevka-ss01.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss01" do
-  version "11.2.7"
-  sha256 "aa79e63634cab655b02c4475b0dd5165c640fc97959804edcf38e92f06308e60"
+  version "11.3.0"
+  sha256 "02228182903bc109545bfc647f807a840e27f14878eb4788dd638fbf67df5b5a"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss01-#{version}.zip"
   name "Iosevka SS01"
