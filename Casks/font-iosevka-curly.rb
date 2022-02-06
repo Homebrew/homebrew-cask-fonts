e476d5e6152953be30006f4e104d12e045e0d237
Hanlin He <hanling.he@gmail.com>
2022-02-05 18:34:06 -0800

    Update font-iosevka-curly from 11.2.7 to 11.3.0 (#5400)

--- Casks/font-iosevka-curly.rb
+++ Casks/font-iosevka-curly.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly" do
-  version "11.2.7"
-  sha256 "30da1870f0c443e8b6e1af6259947b38f568685e33b142ccfece56726d266379"
+  version "11.3.0"
+  sha256 "5d392f22217325b623c3f5b55b606ac90f9d1e0c0e04e4743e05f571405452f1"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-#{version}.zip"
   name "Iosevka Curly"
