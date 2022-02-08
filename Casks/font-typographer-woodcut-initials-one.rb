d22549aa364b531d7f498a06ba738a2a975a9d49
Bennet Robin Fabian <bennet@bennet.dev>
2022-02-08 12:29:51 +0100

    Update "Typographer Woodcut Initials One" download URL (#5423)

--- Casks/font-typographer-woodcut-initials-one.rb
+++ Casks/font-typographer-woodcut-initials-one.rb
@@ -2,7 +2,7 @@ cask "font-typographer-woodcut-initials-one" do
   version :latest
   sha256 :no_check
 
-  url "https://dl.1001fonts.com/typographerwoodcutinitialsone.zip"
+  url "https://dl.1001fonts.com/download/typographerwoodcutinitialsone.zip"
   name "Typographer Woodcut Initials One"
   homepage "https://www.1001fonts.com/typographerwoodcutinitialsone-font.html"
 
