f4b1eec4619ed584effc01b6047b809c5a71e68c
Bennet Robin Fabian <bennet@bennet.dev>
2022-02-08 23:08:08 +0100

    Update font-ipamjmincho.rb

--- Casks/font-ipamjmincho.rb
+++ Casks/font-ipamjmincho.rb
@@ -2,10 +2,10 @@ cask "font-ipamjmincho" do
   version "006.01"
   sha256 "35494e0f2896f38b3f7369a8421a895cea6440a42c0a66ac95eab47d6ed25b68"
 
-  url "https://mojikiban.ipa.go.jp/OSCDL/IPAmjMincho/ipamjm#{version.no_dots}.zip"
+  url "https://dforest.watch.impress.co.jp/library/i/ipamjfont/10750/ipamjm#{version.no_dots}.zip"
   name "IPAmjMincho"
   name "IPAmj明朝"
-  homepage "https://mojikiban.ipa.go.jp/1300.html"
+  homepage "https://moji.or.jp/mojikiban/font/"
 
   font "ipamjm.ttf"
 end
