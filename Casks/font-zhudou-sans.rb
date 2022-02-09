cef991759f7bbe02bc705d22a1e9b9416ee6674f
singularitti <singularitti@outlook.com>
2022-02-08 20:36:07 -0500

    Add font "Casks/font-zhudou-sans.rb" v1.000

---
+++ Casks/font-zhudou-sans.rb
@@ -0,0 +1,18 @@
+cask "font-zhudou-sans" do
+  version "1.000"
+  sha256 "dae5ff79f8f1db78661a6ec0ae7c1732965c034a3c04bd95cd43baabd14996ca"
+
+  url "https://github.com/Buernia/Zhudou-Sans/releases/download/v#{version}/Zhudou.Sans.zip"
+  name "Zhudou Sans"
+  name "煮豆黑体"
+  desc "CJK symbols and punctuation derived from Noto Sans"
+  homepage "https://github.com/Buernia/Zhudou-Sans"
+
+  font "Zhudou Sans Bold.ttf"
+  font "Zhudou Sans ExtraLight.ttf"
+  font "Zhudou Sans Heavy.ttf"
+  font "Zhudou Sans Light.ttf"
+  font "Zhudou Sans Medium.ttf"
+  font "Zhudou Sans Normal.ttf"
+  font "Zhudou Sans Regular.ttf"
+end
