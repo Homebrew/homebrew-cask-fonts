0204b07e378b1c979666eaf9895262276ce7d009
Bennet Robin Fabian <bennet@bennet.dev>
2022-02-09 01:07:45 +0100

    [DONT MERGE YET] Fix "IPA MJ Mincho" download (#5427)
    * Update font-ipamjmincho.rb
    
    * Update font-ipamjmincho.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-ipamjmincho.rb
+++ Casks/font-ipamjmincho.rb
@@ -2,10 +2,16 @@ cask "font-ipamjmincho" do
   version "006.01"
   sha256 "35494e0f2896f38b3f7369a8421a895cea6440a42c0a66ac95eab47d6ed25b68"
 
-  url "https://mojikiban.ipa.go.jp/OSCDL/IPAmjMincho/ipamjm#{version.no_dots}.zip"
+  url "https://dforest.watch.impress.co.jp/library/i/ipamjfont/10750/ipamjm#{version.no_dots}.zip",
+      verified: "dforest.watch.impress.co.jp/library/i/ipamjfont/10750/"
   name "IPAmjMincho"
   name "IPAmj明朝"
-  homepage "https://mojikiban.ipa.go.jp/1300.html"
+  homepage "https://moji.or.jp/mojikiban/font/"
+
+  livecheck do
+    url :homepage
+    regex(/Ver\.v?(\d+(?:\.\d+)+)/i)
+  end
 
   font "ipamjm.ttf"
 end
