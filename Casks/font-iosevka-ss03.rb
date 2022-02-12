4601f4b8af3dcaa1ec7871a567115a4f799dce5a
Reno <25192197+singularitti@users.noreply.github.com>
2022-02-12 03:54:36 -0500

    Update font-iosevka-ss03 from 11.3.0 to 13.3.1 (#5437)

--- Casks/font-iosevka-ss03.rb
+++ Casks/font-iosevka-ss03.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss03" do
-  version "11.3.0"
-  sha256 "d061276789c571a3a46ee396bfd892c78a0b9e703276518e7ea16ee87160ee19"
+  version "13.3.1"
+  sha256 "91aec24f652cadb5ea1e46fd9214b52c398caaa23310730a61d1d0ee4222929f"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss03-#{version}.zip"
   name "Iosevka SS03"
