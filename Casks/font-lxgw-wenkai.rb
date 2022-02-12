157fbb02048c32e6c424387190b66fbbc7605362
singularitti <singularitti@outlook.com>
2022-02-12 17:13:48 -0500

    Add cask "font-lxgw-wenkai"
    Add "LXGW WenKai / 霞鹜文楷"

---
+++ Casks/font-lxgw-wenkai.rb
@@ -0,0 +1,17 @@
+cask 'font-lxgw-wenkai' do
+  version "1.120"
+  sha256 "b65a9fc64038069a5dcda06b4004572c15910266c3b6212d02f4444fb90a4e8b"
+
+  url ""
+  name "LXGW WenKai"
+  name "霞鹜文楷"
+  desc "Open-source Chinese font derived from Fontworks' Klee One"
+  homepage "https://github.com/lxgw/LxgwWenKai"
+
+  font "LXGWWenKai-Bold.ttf"
+  font "LXGWWenKai-Light.ttf"
+  font "LXGWWenKai-Regular.ttf"
+  font "LXGWWenKaiMono-Bold.ttf"
+  font "LXGWWenKaiMono-Light.ttf"
+  font "LXGWWenKaiMono-Regular.ttf"
+end
