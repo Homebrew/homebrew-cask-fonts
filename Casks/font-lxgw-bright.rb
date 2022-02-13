a4b115f94a6727ddb7ff2d8ce88186196a98d455
Reno <25192197+singularitti@users.noreply.github.com>
2022-02-13 01:10:09 -0500

    Add cask "font-lxgw-bright" v1.120
    LXGW Bright

---
+++ Casks/font-lxgw-bright.rb
@@ -0,0 +1,16 @@
+cask "font-lxgw-bright" do
+  version "1.120"
+  sha256 "076b147e4df1785ae26068821c661c8528a34efa53119fe1438d8a0824f9cb3a"
+
+  url "https://github.com/lxgw/LxgwBright/archive/refs/tags/v#{version}.zip"
+  name "LXGW Bright"
+  desc "Merged font of Ysabeau Office and LXGW WenKai"
+  homepage "https://github.com/lxgw/LxgwBright"
+
+  font "LxgwBright-#{version}/OTF/LXGWBright-Medium.otf"
+  font "LxgwBright-#{version}/OTF/LXGWBright-MediumItalic.otf"
+  font "LxgwBright-#{version}/OTF/LXGWBright-Regular.otf"
+  font "LxgwBright-#{version}/OTF/LXGWBright-Italic.otf"
+  font "LxgwBright-#{version}/OTF/LXGWBright-SemiLight.otf"
+  font "LxgwBright-#{version}/OTF/LXGWBright-SemiLightItalic.otf"
+end
