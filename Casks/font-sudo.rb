f56394ae8dc2501f03bce90811eccef6fcce4d96
Bevan Kay <email@bevankay.me>
2022-02-14 11:06:51 +1100

    Update font-sudo from 0.61 to 0.62 (#5483)

--- Casks/font-sudo.rb
+++ Casks/font-sudo.rb
@@ -1,10 +1,15 @@
 cask "font-sudo" do
-  version "0.61"
-  sha256 "08da6712a1eaedb79468078516cb3619bf2d48b795df4e931ef9ede09aa7f660"
+  version "0.62"
+  sha256 "e63d2184f65a80c21fa9a33739e98be2b21a40867254bf1cfad5d2ccee83df98"
 
   url "https://github.com/jenskutilek/sudo-font/releases/download/v#{version}/sudo.zip"
   name "Sudo"
   homepage "https://github.com/jenskutilek/sudo-font/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   font "sudo/SudoVariable.ttf"
 end
