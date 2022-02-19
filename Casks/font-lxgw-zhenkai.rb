b49cf97256a9d63273afb1227256d17ef4ec42a0
singularitti <singularitti@outlook.com>
2022-02-18 20:23:19 -0500

    Add font "LXGW ZhenKai" / "霞鹜臻楷", i.e., "font-lxgw-zhenkai"

---
+++ Casks/font-lxgw-zhenkai.rb
@@ -0,0 +1,12 @@
+cask "font-lxgw-zhenkai" do
+  version "0.5.5"
+  sha256 "35d77b71e511369f364775e594b3526e345f22a7909afe7efea2c6d7f7989fa8"
+
+  url "https://github.com/lxgw/LxgwZhenKai/releases/download/v#{version}/LXGWZhenKai.ttf"
+  name "LXGW ZhenKai"
+  name "霞鹜臻楷"
+  desc "Bolder-weight Edition of LXGW WenKai"
+  homepage "https://github.com/lxgw/LxgwZhenKai"
+
+  font "LXGWZhenKai.ttf"
+end
