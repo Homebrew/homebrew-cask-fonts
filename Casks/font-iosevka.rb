a7368a60eccaf87cd32c53501a95b21f0ea74902
singularitti <singularitti@outlook.com>
2022-02-19 10:00:33 -0500

    Update font-iosevka from 14.0.0 to 14.0.1

--- Casks/font-iosevka.rb
+++ Casks/font-iosevka.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka" do
-  version "14.0.0"
-  sha256 "6ae41f26dd50b4971b95936ef4ad27b11af39f01c0842e1f7310a507d62534a4"
+  version "14.0.1"
+  sha256 "8fdf9ac19543d754970906228e91b2fa84d30a597a9dc2246017dc4fb5a6f657"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
   name "Iosevka"
