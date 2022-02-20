f5619209ce92260bf07780b5b8eac38bb6ea5dd4
Reno <25192197+singularitti@users.noreply.github.com>
2022-02-20 18:22:53 -0500

    Add font "LXGW WenKai" / "霞鹜文楷" (#5486)
    * Add cask "font-lxgw-wenkai"
    
    Add "LXGW WenKai / 霞鹜文楷"
    
    * Fix cask "font-lxgw-wenkai"

---
+++ Casks/font-lxgw-wenkai.rb
@@ -0,0 +1,17 @@
+cask "font-lxgw-wenkai" do
+  version "1.210"
+  sha256 "6b0b45b77ef7d152268f95ea560a7c4538d0c85cbd050d54b812ad9691e868d9"
+
+  url "https://github.com/lxgw/LxgwWenKai/releases/download/v#{version}/lxgw-wenkai-v#{version}.zip"
+  name "LXGW WenKai"
+  name "霞鹜文楷"
+  desc "Open-source Chinese font derived from Fontworks' Klee One"
+  homepage "https://github.com/lxgw/LxgwWenKai"
+
+  font "lxgw-wenkai-v#{version}/LXGWWenKai-Bold.ttf"
+  font "lxgw-wenkai-v#{version}/LXGWWenKai-Light.ttf"
+  font "lxgw-wenkai-v#{version}/LXGWWenKai-Regular.ttf"
+  font "lxgw-wenkai-v#{version}/LXGWWenKaiMono-Bold.ttf"
+  font "lxgw-wenkai-v#{version}/LXGWWenKaiMono-Light.ttf"
+  font "lxgw-wenkai-v#{version}/LXGWWenKaiMono-Regular.ttf"
+end
