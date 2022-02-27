ec4328c1fcd4a7216df2db966d8f44888c87ead9
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-02-27 00:05:31 +0000

    Update Google Fonts

---
+++ Casks/font-aref-ruqaa-ink.rb
@@ -0,0 +1,14 @@
+cask "font-aref-ruqaa-ink" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/trunk/ofl/arefruqaaink",
+      verified: "github.com/google/fonts/",
+      using:    :svn
+  name "Aref Ruqaa Ink"
+  desc "Led by khaled hosny, a type designer based in cairo, egypt"
+  homepage "https://fonts.google.com/specimen/Aref+Ruqaa+Ink"
+
+  font "ArefRuqaaInk-Bold.otf"
+  font "ArefRuqaaInk-Regular.otf"
+end
