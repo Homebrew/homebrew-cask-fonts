2406db984e2050a2ac7ce537158ffc3cc402e047
Reno <25192197+singularitti@users.noreply.github.com>
2022-03-07 05:05:03 -0500

    Update font-lxgw-zhenkai from 0.5.5 to 0.5.5.1 (#5583)

--- Casks/font-lxgw-zhenkai.rb
+++ Casks/font-lxgw-zhenkai.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-zhenkai" do
-  version "0.5.5"
-  sha256 "35d77b71e511369f364775e594b3526e345f22a7909afe7efea2c6d7f7989fa8"
+  version "0.5.5.1"
+  sha256 "72b78abb4d9b046d49eca6773bd98e1d9bf509781acb2a55f0b26eb5610af5a7"
 
   url "https://github.com/lxgw/LxgwZhenKai/releases/download/v#{version}/LXGWZhenKai.ttf"
   name "LXGW ZhenKai"
