c38161316e56aad4093b96a001143f4fc6f602ba
Alexander Ronald Altman <alexanderaltman@me.com>
2022-03-29 00:22:10 -0700

    Update Casks/font-academicons.rb

--- Casks/font-academicons.rb
+++ Casks/font-academicons.rb
@@ -3,7 +3,6 @@ cask "font-academicons" do
   sha256 "6ca4bd211505da763c11e8d3abb43d2861c3399ce2c2f6482eb6a9a361eb5fc5"
 
   url "https://github.com/jpswalsh/academicons/archive/v#{version}.zip"
-  appcast "https://github.com/jpswalsh/academicons/releases.atom"
   name "Academicons"
   desc "Specialist icon font for academics"
   homepage "https://github.com/jpswalsh/academicons"
