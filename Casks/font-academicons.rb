194e4065e6e7d317ac2813680d7479cab51190f7
Alexander Ronald Altman <alexanderaltman@me.com>
2022-03-29 19:26:36 -0700

    Update font-academicons from 1.9.1 to 1.9.2 (#5682)
    * Update font-academicons from 1.9.1 to 1.9.2
    
    * Update Casks/font-academicons.rb

--- Casks/font-academicons.rb
+++ Casks/font-academicons.rb
@@ -1,9 +1,8 @@
 cask "font-academicons" do
-  version "1.9.1"
-  sha256 "f7c2c991c6d4f075bd5de688f3c4f961421e52043c480084039add989c82154f"
+  version "1.9.2"
+  sha256 "6ca4bd211505da763c11e8d3abb43d2861c3399ce2c2f6482eb6a9a361eb5fc5"
 
   url "https://github.com/jpswalsh/academicons/archive/v#{version}.zip"
-  appcast "https://github.com/jpswalsh/academicons/releases.atom"
   name "Academicons"
   desc "Specialist icon font for academics"
   homepage "https://github.com/jpswalsh/academicons"
