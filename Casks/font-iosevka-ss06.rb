f62f1cdc0d2577cee3e0315451507c9048ae1036
Reno <singularitti@outlook.com>
2022-04-16 08:16:47 -0400

    Update font-iosevka-ss06 from 15.1.0 to 15.2.0

--- Casks/font-iosevka-ss06.rb
+++ Casks/font-iosevka-ss06.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss06" do
-  version "15.1.0"
-  sha256 "7d66b66b0b7c5e7d863b9154cc4abd4b2dd2a1919863c39f18e76b978f900be5"
+  version "15.2.0"
+  sha256 "4147192876fa541aed2c2fb3be2b65c3133f91e1861dddfeb1ac068f3733db51"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss06-#{version}.zip"
   name "Iosevka SS06"
