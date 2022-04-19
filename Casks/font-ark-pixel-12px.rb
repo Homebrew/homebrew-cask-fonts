304b8b9b45efb852fd3fcf7658ca8aff0474dfbb
Reno <singularitti@outlook.com>
2022-04-19 01:58:19 -0400

    Update font-ark-pixel-12px from 2022-04-05 to 2022-04-19

--- Casks/font-ark-pixel-12px.rb
+++ Casks/font-ark-pixel-12px.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-12px" do
-  version "2022-04-05"
-  sha256 "0d22adcc91807ab91d688470e145ac0922ed50c8d8fae40bf8aceddd0f3b0556"
+  version "2022-04-19"
+  sha256 "f50b44229b56d7e4751a2fd507efb4606749c6f6ba427b3c91e3410d01ffb775"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/dev-#{version}/ark-pixel-font-12px-otf-v0.0.0-dev-#{version.no_hyphens}.zip"
   name "Ark Pixel 12px"
