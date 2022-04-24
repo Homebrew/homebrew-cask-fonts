a2de3e378da25b63f97ab2c95d5ab5443c7d858b
wktk <k.wakitani@gmail.com>
2022-04-25 06:21:19 +0900

    Add UDEV Gothic NF (#5701)

---
+++ Casks/font-udev-gothic-nf.rb
@@ -0,0 +1,26 @@
+cask "font-udev-gothic-nf" do
+  version "1.0.0"
+  sha256 "c431f9ce6762c21cdc2b2f32481b1374babb2cf50dc5e949f27a8f7101ecceeb"
+
+  url "https://github.com/yuru7/udev-gothic/releases/download/v#{version}/UDEVGothic_NF_v#{version}.zip"
+  name "UDEV Gothic NF"
+  desc "Integrate fonts from BIZ UD Gothic and JetBrains Mono"
+  homepage "https://github.com/yuru7/udev-gothic"
+
+  font "UDEVGothic_NF_v#{version}/UDEVGothic35NF-Bold.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothic35NF-BoldItalic.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothic35NF-Italic.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothic35NF-Regular.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothic35NFLG-Bold.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothic35NFLG-BoldItalic.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothic35NFLG-Italic.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothic35NFLG-Regular.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothicNF-Bold.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothicNF-BoldItalic.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothicNF-Italic.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothicNF-Regular.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothicNFLG-Bold.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothicNFLG-BoldItalic.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothicNFLG-Italic.ttf"
+  font "UDEVGothic_NF_v#{version}/UDEVGothicNFLG-Regular.ttf"
+end
