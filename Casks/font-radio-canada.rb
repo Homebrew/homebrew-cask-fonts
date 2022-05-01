d67b4621b0526cc984869d7874c6b32389f5baf3
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-05-01 00:06:10 +0000

    Update Google Fonts

--- Casks/font-radio-canada.rb
+++ Casks/font-radio-canada.rb
@@ -2,10 +2,12 @@ cask "font-radio-canada" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/radiocanada/RadioCanada%5Bwdth%2Cwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/trunk/ofl/radiocanada",
+      verified: "github.com/google/fonts/",
+      using:    :svn
   name "Radio Canada"
   homepage "https://fonts.google.com/specimen/Radio+Canada"
 
+  font "RadioCanada-Italic[wdth,wght].ttf"
   font "RadioCanada[wdth,wght].ttf"
 end
