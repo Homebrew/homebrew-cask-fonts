301830e316702f614ac9395e43b3bd3c5ebc828a
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-05-08 12:50:24 +0200

    Update font-iosevka from 15.2.0 to 15.3.0

--- Casks/font-iosevka.rb
+++ Casks/font-iosevka.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka" do
-  version "15.2.0"
-  sha256 "7bdfc02ec0afd81652a4d9b345e6850df2b93625a6289b2ee0fca278eb46df7b"
+  version "15.3.0"
+  sha256 "913d8fbad9d93777ae4818430b72719c4c59f4442e84ebbc7685de97aade005a"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
   name "Iosevka"
