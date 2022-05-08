9561e254360cbc60f9a67c9c46ef90f2e6bbda0d
Reno <singularitti@outlook.com>
2022-05-08 17:43:08 -0400

    Update font-iosevka-ss09 from 15.2.0 to 15.3.0

--- Casks/font-iosevka-ss09.rb
+++ Casks/font-iosevka-ss09.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss09" do
-  version "15.2.0"
-  sha256 "2bd082f0928218a9d44223e8c7ebd415aae4533fb72ec63db6a14d5a1ff26594"
+  version "15.3.0"
+  sha256 "c60e2c7541f61e8ecac4fe8f9e93eeda85e18abc1eaac067430c7f323e6782b8"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss09-#{version}.zip"
   name "Iosevka SS09"
