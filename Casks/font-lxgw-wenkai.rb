b4d340855725a30dbbbf77db3420f84d27b7ee9e
Reno <25192197+singularitti@users.noreply.github.com>
2022-05-10 22:31:20 -0400

    Update font-lxgw-wenkai from 1.234.5 to 1.234.56 (#5760)
    * Update font-lxgw-wenkai from 1.234.5 to 1.234.56
    
    * Update font-lxgw-wenkai.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-lxgw-wenkai.rb
+++ Casks/font-lxgw-wenkai.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-wenkai" do
-  version "1.234.5"
-  sha256 "9f2f4c61979c8953550fb3987e4e1c645ec8263bfefce0e065ec1b4a6f4bac44"
+  version "1.234.56"
+  sha256 "f5dbf2cf8cc7bc3af51363b54db6e932e0706523b7f98a0c486f4daecabd99e2"
 
   url "https://github.com/lxgw/LxgwWenKai/releases/download/v#{version}/lxgw-wenkai-v#{version}.zip"
   name "LXGW WenKai"
