11685629665d2c69a3d0879de16ee994c1c94ec5
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-11 10:24:02 +0800

    Update font-lxgw-wenkai.rb

--- Casks/font-lxgw-wenkai.rb
+++ Casks/font-lxgw-wenkai.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-wenkai" do
   version "1.234.56"
-  sha256 "75ea8c9b3acb9262f2f46a4a9b431fba93c14a35ff74fb3b1683cf237224056e"
+  sha256 "f5dbf2cf8cc7bc3af51363b54db6e932e0706523b7f98a0c486f4daecabd99e2"
 
   url "https://github.com/lxgw/LxgwWenKai/releases/download/v#{version}/lxgw-wenkai-v#{version}.zip"
   name "LXGW WenKai"
