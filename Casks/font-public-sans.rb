a27a0c1b1368f44284d64b80011537e59918ebbf
Bevan Kay <email@bevankay.me>
2022-05-12 11:10:12 +1000

    Update font-public-sans from 2.000 to 2.001

--- Casks/font-public-sans.rb
+++ Casks/font-public-sans.rb
@@ -1,6 +1,6 @@
 cask "font-public-sans" do
-  version "2.000"
-  sha256 "3498d0ce45c230428bb171b227feabf5c13acf66e8e456c628f2d1779221ad6a"
+  version "2.001"
+  sha256 "dd839c4436d4ffc4563517f64c47b8eae2da54e867aad3173da1bd7fef104597"
 
   url "https://github.com/uswds/public-sans/releases/download/v#{version}/public-sans-v#{version}.zip",
       verified: "github.com/uswds/public-sans/"
