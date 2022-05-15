1867ff95f41ca6bc59bd91e5addda3692201ed31
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-05-15 00:05:58 +0000

    Update Google Fonts

--- Casks/font-noto-sans-sc.rb
+++ Casks/font-noto-sans-sc.rb
@@ -2,17 +2,11 @@ cask "font-noto-sans-sc" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/ofl/notosanssc",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/google/fonts/raw/main/ofl/notosanssc/NotoSansSC%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans SC"
   desc "Sans-serif design for simplified chinese variant of the han ideograms"
   homepage "https://fonts.google.com/specimen/Noto+Sans+SC"
 
-  font "NotoSansSC-Black.otf"
-  font "NotoSansSC-Bold.otf"
-  font "NotoSansSC-Light.otf"
-  font "NotoSansSC-Medium.otf"
-  font "NotoSansSC-Regular.otf"
-  font "NotoSansSC-Thin.otf"
+  font "NotoSansSC[wght].ttf"
 end
--- Casks/font-noto-sans-tc.rb
+++ Casks/font-noto-sans-tc.rb
@@ -2,17 +2,11 @@ cask "font-noto-sans-tc" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/ofl/notosanstc",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/google/fonts/raw/main/ofl/notosanstc/NotoSansTC%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans TC"
   desc "Unmodulated (“sans serif”) design for languages in taiwan and macau"
   homepage "https://fonts.google.com/specimen/Noto+Sans+TC"
 
-  font "NotoSansTC-Black.otf"
-  font "NotoSansTC-Bold.otf"
-  font "NotoSansTC-Light.otf"
-  font "NotoSansTC-Medium.otf"
-  font "NotoSansTC-Regular.otf"
-  font "NotoSansTC-Thin.otf"
+  font "NotoSansTC[wght].ttf"
 end
