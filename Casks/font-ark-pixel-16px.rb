2ac61f367655f29e5bc0f27f231b95481e5eee0d
Reno <25192197+singularitti@users.noreply.github.com>
2022-05-21 06:42:29 -0400

    Update font-ark-pixel-16px from 2022-05-04 to 2022-05-20 (#5821)

--- Casks/font-ark-pixel-16px.rb
+++ Casks/font-ark-pixel-16px.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-16px" do
-  version "2022-05-04"
-  sha256 "e8ee224ae5d35bcd973ddb09f7984e63ec9b47f73f11ea4b4f807938ec31bb0b"
+  version "2022-05-20"
+  sha256 "e59d6a6a53fafdd67b1b34032d16c6a0c31883f4ea4cbac0bbe134941c537db9"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/dev-#{version}/ark-pixel-font-16px-otf-v0.0.0-dev-#{version.no_hyphens}.zip"
   name "Ark Pixel 16px"
