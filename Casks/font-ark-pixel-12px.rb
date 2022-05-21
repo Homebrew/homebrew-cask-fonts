757ce7c4598685a7da218d17e25b0d5d1b96c18d
Reno <25192197+singularitti@users.noreply.github.com>
2022-05-21 06:42:20 -0400

    Update font-ark-pixel-12px from 2022-05-04 to 2022-05-20 (#5820)

--- Casks/font-ark-pixel-12px.rb
+++ Casks/font-ark-pixel-12px.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-12px" do
-  version "2022-05-04"
-  sha256 "8ce91e766f478dc75967d80f8e7bdeb060ff1f1fad96d7e0421dbd74bd5ba7c7"
+  version "2022-05-20"
+  sha256 "bfc079400d096a1bacdfb8e797b4e6030564cfb2f001d85940b3ed4141e7f9fd"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/dev-#{version}/ark-pixel-font-12px-otf-v0.0.0-dev-#{version.no_hyphens}.zip"
   name "Ark Pixel 12px"
