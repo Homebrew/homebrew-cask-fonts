c33f4e8934ad2a3aad7d2d80d29e5c6b61378042
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-23 04:21:46 +0800

    Update font-gulzar.rb

--- Casks/font-gulzar.rb
+++ Casks/font-gulzar.rb
@@ -5,7 +5,7 @@ cask "font-gulzar" do
   url "https://github.com/google/fonts/raw/main/ofl/gulzar/Gulzar-Regular.ttf",
       verified: "github.com/google/fonts/"
   name "Gulzar"
-  desc "Contemporary urdu nasta'liq font designed by borna izadpanah and engineered by simon cozens, with a companion latin designed by alice savoie"
+  desc "Contemporary urdu nasta'liq font"
   homepage "https://fonts.google.com/specimen/Gulzar"
 
   font "Gulzar-Regular.ttf"
