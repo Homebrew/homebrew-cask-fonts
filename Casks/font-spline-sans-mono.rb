dbcf6fe06c7920346d70faca55df2a28996b9c03
BrewTestBot <ops@brew.sh>
2022-06-05 09:01:12 +0200

    Automatic update of Google Fonts (#5907)
    Update Google Fonts
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>

--- Casks/font-spline-sans-mono.rb
+++ Casks/font-spline-sans-mono.rb
@@ -2,11 +2,13 @@ cask "font-spline-sans-mono" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/splinesansmono/SplineSansMono%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/trunk/ofl/splinesansmono",
+      verified: "github.com/google/fonts/",
+      using:    :svn
   name "Spline Sans Mono"
   desc "Original typeface initiated by the spline team"
   homepage "https://fonts.google.com/specimen/Spline+Sans+Mono"
 
+  font "SplineSansMono-Italic[wght].ttf"
   font "SplineSansMono[wght].ttf"
 end
