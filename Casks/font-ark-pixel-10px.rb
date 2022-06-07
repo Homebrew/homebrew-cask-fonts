f7513b143ade0fbb9797068b723e33e0e36f6077
Reno <25192197+singularitti@users.noreply.github.com>
2022-06-07 17:54:10 -0400

    Update font-ark-pixel-10px from 2022-05-20 to 2022-06-08 (#5914)

--- Casks/font-ark-pixel-10px.rb
+++ Casks/font-ark-pixel-10px.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-10px" do
-  version "2022-05-20"
-  sha256 "db41f2041f98be1140ce86a00618530d8611fc93186717b14f602b23bbf10530"
+  version "2022-06-08"
+  sha256 "0573c7d43054bafd3a379fe79a2516643e48421fd81575530a337c5a6693badb"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/dev-#{version}/ark-pixel-font-10px-otf-v0.0.0-dev-#{version.no_hyphens}.zip"
   name "Ark Pixel 10px"
