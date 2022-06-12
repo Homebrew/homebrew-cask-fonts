267d5c2f8b30ab38411423823c06d9c79faaa667
BrewTestBot <ops@brew.sh>
2022-06-12 03:49:57 +0200

    Automatic update of Google Fonts (#5918)
    Update Google Fonts
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>

--- Casks/font-noto-serif-tc.rb
+++ Casks/font-noto-serif-tc.rb
@@ -2,18 +2,11 @@ cask "font-noto-serif-tc" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/ofl/notoseriftc",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/google/fonts/raw/main/ofl/notoseriftc/NotoSerifTC%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Serif TC"
   desc "Modulated (“serif”) design for languages in taiwan, hong kong and macau"
   homepage "https://fonts.google.com/specimen/Noto+Serif+TC"
 
-  font "NotoSerifTC-Black.otf"
-  font "NotoSerifTC-Bold.otf"
-  font "NotoSerifTC-ExtraLight.otf"
-  font "NotoSerifTC-Light.otf"
-  font "NotoSerifTC-Medium.otf"
-  font "NotoSerifTC-Regular.otf"
-  font "NotoSerifTC-SemiBold.otf"
+  font "NotoSerifTC[wght].ttf"
 end
