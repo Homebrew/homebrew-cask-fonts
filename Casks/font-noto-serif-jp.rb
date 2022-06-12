7f29240e7a2196cb201091ec70261d85f45ccba4
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-06-12 00:05:48 +0000

    Update Google Fonts

--- Casks/font-noto-serif-jp.rb
+++ Casks/font-noto-serif-jp.rb
@@ -2,18 +2,11 @@ cask "font-noto-serif-jp" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/ofl/notoserifjp",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/google/fonts/raw/main/ofl/notoserifjp/NotoSerifJP%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Serif JP"
   desc "Modulated (“serif”) design for the japanese language"
   homepage "https://fonts.google.com/specimen/Noto+Serif+JP"
 
-  font "NotoSerifJP-Black.otf"
-  font "NotoSerifJP-Bold.otf"
-  font "NotoSerifJP-ExtraLight.otf"
-  font "NotoSerifJP-Light.otf"
-  font "NotoSerifJP-Medium.otf"
-  font "NotoSerifJP-Regular.otf"
-  font "NotoSerifJP-SemiBold.otf"
+  font "NotoSerifJP[wght].ttf"
 end
--- Casks/font-noto-serif-kr.rb
+++ Casks/font-noto-serif-kr.rb
@@ -2,18 +2,11 @@ cask "font-noto-serif-kr" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/ofl/notoserifkr",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/google/fonts/raw/main/ofl/notoserifkr/NotoSerifKR%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Serif KR"
   desc "Modulated (“serif”) design for the korean language"
   homepage "https://fonts.google.com/specimen/Noto+Serif+KR"
 
-  font "NotoSerifKR-Black.otf"
-  font "NotoSerifKR-Bold.otf"
-  font "NotoSerifKR-ExtraLight.otf"
-  font "NotoSerifKR-Light.otf"
-  font "NotoSerifKR-Medium.otf"
-  font "NotoSerifKR-Regular.otf"
-  font "NotoSerifKR-SemiBold.otf"
+  font "NotoSerifKR[wght].ttf"
 end
