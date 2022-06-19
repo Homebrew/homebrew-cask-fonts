fac0719bc47b62e68c646adfef7a87cd7027198d
Reno <25192197+singularitti@users.noreply.github.com>
2022-06-19 01:27:17 -0400

    Update font-iosevka from 15.5.0 to 15.5.1 (#5924)

--- Casks/font-iosevka.rb
+++ Casks/font-iosevka.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka" do
-  version "15.5.0"
-  sha256 "8118f6466470a9a4978d495b61cf0623b1c7f34667625e40280f389807eaedb5"
+  version "15.5.1"
+  sha256 "5120ac2134b070dc141fe1db512e680bf1bcaa452474bb4f605c3b38620c6ced"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
   name "Iosevka"
