b121dd1e0ea6dcb22840214cb46a4ca226947eb5
Miccal Matthews <miccal.matthews@gmail.com>
2022-06-22 15:23:16 +0800

    Update font-edwin.rb

--- Casks/font-edwin.rb
+++ Casks/font-edwin.rb
@@ -3,8 +3,7 @@ cask "font-edwin" do
   sha256 "761a15932defe72b40eecb22e8255cba225bc9efedb208c83e2f5b0b77b7103e"
 
   url "https://github.com/MuseScoreFonts/Edwin/archive/refs/tags/v#{version}.zip"
-  appcast "https://github.com/MuseScoreFonts/Edwin/releases.atom"
-  name "FiraCode Nerd Font (Fira Code)"
+  name "Text font for musical scores"
   homepage "https://github.com/MuseScoreFonts/Edwin"
 
   font "Edwin-BdIta.otf"
