6dbaca2cf39f7d47fcf55d82167cbb3c2c250edd
ponte-vecchio <zkogdxdkur@p.monash.edu>
2022-06-23 15:10:18 +1000

    [fix] Font-edu-qld-beginner URL to the correct one

--- Casks/font-edu-qld-beginner.rb
+++ Casks/font-edu-qld-beginner.rb
@@ -1,11 +1,11 @@
-cask "font-edu-qld-beginners" do
+cask "font-edu-qld-beginner" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/eduqldbeginners/EduQLDBeginners%5Bwght%5D.ttf",
+  url "https://github.com/google/fonts/raw/main/ofl/eduqldbeginner/EduQLDBeginner%5Bwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Edu QLD Beginners"
   homepage "https://fonts.google.com/specimen/Edu+QLD+Beginners"
 
-  font "EduQLDBeginners[wght].ttf"
+  font "EduQLDBeginner[wght].ttf"
 end
