fe993366a4902e1889c3a82055d4b6439296fd0b
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-06-26 00:06:12 +0000

    Update Google Fonts

--- Casks/font-edu-qld-beginner.rb
+++ Casks/font-edu-qld-beginner.rb
@@ -4,8 +4,8 @@ cask "font-edu-qld-beginner" do
 
   url "https://github.com/google/fonts/raw/main/ofl/eduqldbeginner/EduQLDBeginner%5Bwght%5D.ttf",
       verified: "github.com/google/fonts/"
-  name "Edu QLD Beginners"
-  homepage "https://fonts.google.com/specimen/Edu+QLD+Beginners"
+  name "Edu QLD Beginner"
+  homepage "https://fonts.google.com/specimen/Edu+QLD+Beginner"
 
   font "EduQLDBeginner[wght].ttf"
 end
