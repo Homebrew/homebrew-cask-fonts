e81fe890194a9b99e5b37cae0761fcd5b90e794b
Tomy Hsieh <tomy0000000@gmail.com>
2022-07-01 08:40:02 +0800

    Add font-iansui cask

---
+++ Casks/font-iansui.rb
@@ -0,0 +1,11 @@
+cask "font-iansui" do
+  version "0.943"
+  sha256 "e730db456778638b53a4645a06c4ebc54bca3168de5725b3be1786ed9a06428b"
+
+  url "https://github.com/ButTaiwan/iansui/releases/download/v#{version}/iansui.zip"
+  appcast "https://github.com/ButTaiwan/iansui/releases.atom"
+  name "iansui"
+  homepage "https://github.com/ButTaiwan/iansui"
+
+  font "Iansui#{version.no_dots[0,3]}-Regular.ttf"
+end
