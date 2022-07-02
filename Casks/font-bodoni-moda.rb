76448d2427296c3e446e97810a15bcd7fa855395
Jacob Ford <j@cobford.com>
2022-07-03 01:38:44 +0200

    Switch Bodoni Moda to own repo, from Google Fonts (#5984)
    * Switch bodoni-moda to download directly from source repo
    
    * Add livecheck
    
    * Style fix
    
    * Update font-bodoni-moda.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-bodoni-moda.rb
+++ Casks/font-bodoni-moda.rb
@@ -1,14 +1,13 @@
 cask "font-bodoni-moda" do
-  version :latest
-  sha256 :no_check
+  version "2.3"
+  sha256 "787426889302f357b1e108fd5284fbe9d40063cb0c994d936c7b6a99816f8ccc"
 
-  url "https://github.com/google/fonts/trunk/ofl/bodonimoda",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/indestructible-type/Bodoni/releases/download/#{version}/Bodoni-master.zip",
+      verified: "github.com/indestructible-type/Bodoni/"
   name "Bodoni Moda"
   desc "Serif typeface with high contrast"
-  homepage "https://fonts.google.com/specimen/Bodoni+Moda"
+  homepage "https://indestructibletype.com/Bodoni.html"
 
-  font "BodoniModa-Italic[opsz,wght].ttf"
-  font "BodoniModa[opsz,wght].ttf"
+  font "Bodoni-master/fonts/variable/Bodoni-Italic-VF.ttf"
+  font "Bodoni-master/fonts/variable/Bodoni-VF.ttf"
 end
