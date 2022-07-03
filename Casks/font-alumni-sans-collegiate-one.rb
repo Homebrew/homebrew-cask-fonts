92e8d0a6658651393a833d589157a73212d6d000
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-07-03 00:05:48 +0000

    Update Google Fonts

--- Casks/font-alumni-sans-collegiate-one.rb
+++ Casks/font-alumni-sans-collegiate-one.rb
@@ -2,11 +2,13 @@ cask "font-alumni-sans-collegiate-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/alumnisanscollegiateone/AlumniSansCollegiateOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/trunk/ofl/alumnisanscollegiateone",
+      verified: "github.com/google/fonts/",
+      using:    :svn
   name "Alumni Sans Collegiate One"
   desc "Font inspired by Impact Black"
   homepage "https://fonts.google.com/specimen/Alumni+Sans+Collegiate+One"
 
+  font "AlumniSansCollegiateOne-Italic.ttf"
   font "AlumniSansCollegiateOne-Regular.ttf"
 end
