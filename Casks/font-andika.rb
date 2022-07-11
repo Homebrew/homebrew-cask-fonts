5f74776bae04d7e094cbf686b14c02014aedb01c
Bennet Robin Fabian <bennet@bennet.dev>
2022-07-11 23:25:51 +0200

    Update Andika font (#6000)
    * Update font-andika.rb
    
    * Add livecheck to Andika
    
    * Update font-andika.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-andika.rb
+++ Casks/font-andika.rb
@@ -1,10 +1,19 @@
 cask "font-andika" do
-  version "5.000"
-  sha256 "604b7a1194be099bdf311ef76cbce086a054fa16d2b101cfaedcf527c63df507"
+  version "6.101"
+  sha256 "8595a879054a540b24fa942a2621efcd194a9f6468d9cf2541ae693e496b8dd4"
 
   url "https://software.sil.org/downloads/r/andika/Andika-#{version}.zip"
   name "Andika"
+  desc "Sans-serif font family designed and optimized for literacy use"
   homepage "https://software.sil.org/andika/"
 
-  font "Andika-#{version}/Andika-R.ttf"
+  livecheck do
+    url "https://software.sil.org/andika/download/"
+    regex(/href=.*Andika[._-]v?(\d+(?:\.\d+)+)\.zip/i)
+  end
+
+  font "Andika-#{version}/Andika-Bold.ttf"
+  font "Andika-#{version}/Andika-BoldItalic.ttf"
+  font "Andika-#{version}/Andika-Italic.ttf"
+  font "Andika-#{version}/Andika-Regular.ttf"
 end
