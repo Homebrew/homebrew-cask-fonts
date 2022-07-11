77b15302ff9ac85f67b6ac280cf14a41b70977bf
Bennet Robin Fabian <bennet@bennet.dev>
2022-07-11 15:10:18 +0200

    Update font-andika.rb

--- Casks/font-andika.rb
+++ Casks/font-andika.rb
@@ -1,10 +1,13 @@
 cask "font-andika" do
-  version "5.000"
-  sha256 "604b7a1194be099bdf311ef76cbce086a054fa16d2b101cfaedcf527c63df507"
+  version "6.101"
+  sha256 "8595a879054a540b24fa942a2621efcd194a9f6468d9cf2541ae693e496b8dd4"
 
   url "https://software.sil.org/downloads/r/andika/Andika-#{version}.zip"
   name "Andika"
   homepage "https://software.sil.org/andika/"
 
-  font "Andika-#{version}/Andika-R.ttf"
+  font "Andika-#{version}/Andika-Bold.ttf"
+  font "Andika-#{version}/Andika-BoldItalic.ttf"
+  font "Andika-#{version}/Andika-Italic.ttf"
+  font "Andika-#{version}/Andika-Regular.ttf"
 end
