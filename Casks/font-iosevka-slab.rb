2ead25a0303d3e3cd4836181d8ec0e9559f42617
Reno <singularitti@outlook.com>
2022-07-16 23:03:39 -0400

    Update font-iosevka-slab from 15.5.2 to 15.6.0

--- Casks/font-iosevka-slab.rb
+++ Casks/font-iosevka-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-slab" do
-  version "15.5.2"
-  sha256 "e7c6f78949ffcfcdee43df08daa3578f0b0a4ba04a398a83ff761c77b448d7ba"
+  version "15.6.0"
+  sha256 "b96b095def4fee7e025eab560b64bd32272c7f66c6af42eee2f463bf111ccd02"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-slab-#{version}.zip"
   name "Iosevka Slab"
