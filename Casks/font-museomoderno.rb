280586f760f60f2ad3f49853699b4e7e019b5f1e
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-07-24 00:06:36 +0000

    Update Google Fonts

--- Casks/font-museomoderno.rb
+++ Casks/font-museomoderno.rb
@@ -2,10 +2,12 @@ cask "font-museomoderno" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/museomoderno/MuseoModerno%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/trunk/ofl/museomoderno",
+      verified: "github.com/google/fonts/",
+      using:    :svn
   name "MuseoModerno"
   homepage "https://fonts.google.com/specimen/MuseoModerno"
 
+  font "MuseoModerno-Italic[wght].ttf"
   font "MuseoModerno[wght].ttf"
 end
