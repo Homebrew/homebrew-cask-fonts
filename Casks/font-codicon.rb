44eedd9cf5c25ac28d5c196599a19c6aff5f2876
Bevan Kay <email@bevankay.me>
2022-07-30 13:07:35 +1000

    Update font-codicon.rb

--- Casks/font-codicon.rb
+++ Casks/font-codicon.rb
@@ -3,7 +3,7 @@ cask "font-codicon" do
   sha256 "157dbef05bd3ef2c0b8cab4abeddea1eef43cde412881b6046ac00193446c988"
 
   url "https://github.com/microsoft/vscode-codicons/archive/refs/tags/#{version}.zip"
-  name "codicon"
+  name "Codicon"
   desc "Icon font for Visual Studio Code"
   homepage "https://github.com/microsoft/vscode-codicons"
 
