e500592c6ba127313100a16bf36880c3e2a1c0e3
Carlo Cabrera <30379873+carlocab@users.noreply.github.com>
2022-07-31 19:51:04 +0800

    font-sono: fix description

--- Casks/font-sono.rb
+++ Casks/font-sono.rb
@@ -5,7 +5,7 @@ cask "font-sono" do
   url "https://github.com/google/fonts/raw/main/ofl/sono/Sono%5BMONO%2Cwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Sono"
-  desc "Fixed-width (and obviously not kerned), but thanks to the mono axis, you can add kerning and make the design proportional"
+  desc "Fixed-width (and obviously not kerned)"
   homepage "https://fonts.google.com/specimen/Sono"
 
   font "Sono[MONO,wght].ttf"
