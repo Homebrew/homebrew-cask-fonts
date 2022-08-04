911fa54223d49ab0190d19b851122af93814dde2
Andy Freeland <andy@andyfreeland.net>
2022-08-03 18:41:19 -0700

    Update font-codicon from 0.0.31 to 0.0.32 (#6091)

--- Casks/font-codicon.rb
+++ Casks/font-codicon.rb
@@ -1,6 +1,6 @@
 cask "font-codicon" do
-  version "0.0.31"
-  sha256 "157dbef05bd3ef2c0b8cab4abeddea1eef43cde412881b6046ac00193446c988"
+  version "0.0.32"
+  sha256 "204628efffeacab61d689d5eb469f390c9ba622df250780c77fdb343457346fc"
 
   url "https://github.com/microsoft/vscode-codicons/archive/refs/tags/#{version}.zip"
   name "Codicon"
