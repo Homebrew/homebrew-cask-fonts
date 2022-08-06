1b10861eb732c5113b3976eb997e07ccf5c07f17
Ryan <25192197+singularitti@users.noreply.github.com>
2022-08-06 03:50:45 -0400

    Update font-iosevka-ss03 from 15.6.2 to 15.6.3 (#6101)

--- Casks/font-iosevka-ss03.rb
+++ Casks/font-iosevka-ss03.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss03" do
-  version "15.6.2"
-  sha256 "c1f8e06a69ca58156f16eabb436abddc1e890425ee09c47c10f4040bac4ac53b"
+  version "15.6.3"
+  sha256 "147ff6127b2202f0b772248dcf3d244c0116fca3e0eff00f80d7fb82fd5a409a"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss03-#{version}.zip"
   name "Iosevka SS03"
