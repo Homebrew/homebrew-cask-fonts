f0032e356308bf4a8d5a24f48a164b21355a9b6e
Reno <singularitti@outlook.com>
2022-08-06 03:38:15 -0400

    Update font-iosevka-ss18 from 15.6.2 to 15.6.3

--- Casks/font-iosevka-ss18.rb
+++ Casks/font-iosevka-ss18.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss18" do
-  version "15.6.2"
-  sha256 "00711d65067ce7c92f87b6b660170c959bc3d693dc7b939ea5f0651e9f3f359a"
+  version "15.6.3"
+  sha256 "1662c3eb890b80b5bfc1c6190b3e77554b9caa98a9d4d41c50bc2ef46aa14a62"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss18-#{version}.zip"
   name "Iosevka SS18"
