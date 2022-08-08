385e32db668873c89960b2754187dca881c00c42
BrewTestBot <ops@brew.sh>
2022-08-07 21:41:42 -0400

    Automatic update of Google Fonts (#6117)
    Update Google Fonts
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>

--- Casks/font-fruktur.rb
+++ Casks/font-fruktur.rb
@@ -2,10 +2,12 @@ cask "font-fruktur" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/fruktur/Fruktur-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/trunk/ofl/fruktur",
+      verified: "github.com/google/fonts/",
+      using:    :svn
   name "Fruktur"
   homepage "https://fonts.google.com/specimen/Fruktur"
 
+  font "Fruktur-Italic.ttf"
   font "Fruktur-Regular.ttf"
 end
