e0c9b0ac4c1519fe7b42aac495102e219005396f
Hanlin He <hanling.he@gmail.com>
2022-08-20 08:14:48 -0700

    Update font-iosevka-etoile from 15.6.3 to 16.0.0

--- Casks/font-iosevka-etoile.rb
+++ Casks/font-iosevka-etoile.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-etoile" do
-  version "15.6.3"
-  sha256 "37c1474803d3921c1333393fe7cccff496844dad494618a14b857c4776b99b75"
+  version "16.0.0"
+  sha256 "30c08921679cd1eb5c99a1cc5bc5bdc17ea2ac5eed650efb52b686c495e21be3"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-etoile-#{version}.zip"
   name "Iosevka Etoile"
