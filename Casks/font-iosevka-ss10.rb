ae2153dab85b53034e8840a198a044c249c4a70b
Hanlin He <hanling.he@gmail.com>
2022-08-20 08:28:07 -0700

    Update font-iosevka-ss10 from 15.6.3 to 16.0.0 (#6139)

--- Casks/font-iosevka-ss10.rb
+++ Casks/font-iosevka-ss10.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss10" do
-  version "15.6.3"
-  sha256 "12620de5ce4088b266a89cc5fe9f396e5487e900f3e2b39874c187c7464e2894"
+  version "16.0.0"
+  sha256 "fc96468b8340d3a197b2ba91668567ac19bfcdf56caca42f5cc4e1a832322061"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss10-#{version}.zip"
   name "Iosevka SS10"
