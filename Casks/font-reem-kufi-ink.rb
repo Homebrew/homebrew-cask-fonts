65a8bb22de8dbb283ad102ae9446c0788eb3c7e4
BrewTestBot <ops@brew.sh>
2022-08-20 21:24:33 -0400

    Automatic update of Google Fonts (#6149)
    Update Google Fonts
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>

--- Casks/font-reem-kufi-ink.rb
+++ Casks/font-reem-kufi-ink.rb
@@ -2,13 +2,11 @@ cask "font-reem-kufi-ink" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/ofl/reemkufiink",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/google/fonts/raw/main/ofl/reemkufiink/ReemKufiInk-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Reem Kufi Ink"
   desc "Also the name of khaled's daughter"
   homepage "https://fonts.google.com/specimen/Reem+Kufi+Ink"
 
-  font "ReemKufiInk-Bold.ttf"
   font "ReemKufiInk-Regular.ttf"
 end
