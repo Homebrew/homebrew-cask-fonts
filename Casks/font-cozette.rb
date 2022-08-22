c18da71d27901aaf70569835cb29d0b5ea3ccefe
toastin0 <64383025+toastin0@users.noreply.github.com>
2022-08-21 22:20:24 -0400

    updated cozetteVector to 1.16.0 (#6152)
    updated font to 1.16.0

--- Casks/font-cozette.rb
+++ Casks/font-cozette.rb
@@ -1,6 +1,6 @@
 cask "font-cozette" do
-  version "1.13.0"
-  sha256 "d5d742efcb285c437fe2ff4136b11d1e846d6e260ce53f8d8452645f2559cc28"
+  version "1.16.0"
+  sha256 "a2ee65ab871ddd199ee71ada82edd587dde9b689b73239d4f69aa34f638f6826"
 
   url "https://github.com/slavfox/Cozette/releases/download/v.#{version}/CozetteVector.dfont"
   name "Cozette"
