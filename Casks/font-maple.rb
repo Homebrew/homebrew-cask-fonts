098ac48fd05d2ce701cd9e9ab953815a6207d807
Cheng Yao <kukathe@gmail.com>
2022-08-23 23:16:51 +0800

    Update font-maple.rb
    fix syntax for font-maple.rb

--- Casks/font-maple.rb
+++ Casks/font-maple.rb
@@ -1,11 +1,12 @@
 cask "font-maple" do
-  name "font Maple"
   version "5.0"
+  sha256 :no_check
+  
+  url "https://github.com/subframe7536/Maple-font/releases/download/v5.0/Maple.Mono.ttf.zip"
+  name "font Maple"
   desc "Open source monospace/Nerd Font font with round corner for IDE and command line. 开源圆角的等宽和控制台字体"
   homepage "https://github.com/subframe7536/Maple-font"
-  url "https://github.com/subframe7536/Maple-font/releases/download/v5.0/Maple.Mono.ttf.zip"
-  sha256 :no_check
-
+  
   font "MapleMono-Regular.ttf"
   font "MapleMono-Bold.ttf"
   font "MapleMono-BoldItalic.ttf"
