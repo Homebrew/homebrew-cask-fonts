101dcc47605e9e619d06e3128ceff592112f7011
Miccal Matthews <miccal.matthews@gmail.com>
2022-08-24 10:23:12 +0800

    Update font-maple.rb

--- Casks/font-maple.rb
+++ Casks/font-maple.rb
@@ -1,14 +1,14 @@
 cask "font-maple" do
   version "5.0"
-  sha256 :no_check
-  
-  url "https://github.com/subframe7536/Maple-font/releases/download/v5.0/Maple.Mono.ttf.zip"
+  sha256 "14aae22b953be17ba1fe21ec08c69e47158790a8f3bfa6db81c1c04e6ef4f05a"
+
+  url "https://github.com/subframe7536/Maple-font/releases/download/v#{version}/Maple.Mono.ttf.zip"
   name "font Maple"
-  desc "Open source monospace/Nerd Font font with round corner for IDE and command line. 开源圆角的等宽和控制台字体"
+  desc "Nerd Font font with round corners"
   homepage "https://github.com/subframe7536/Maple-font"
-  
-  font "MapleMono-Regular.ttf"
+
   font "MapleMono-Bold.ttf"
   font "MapleMono-BoldItalic.ttf"
   font "MapleMono-Italic.ttf"
+  font "MapleMono-Regular.ttf"
 end
