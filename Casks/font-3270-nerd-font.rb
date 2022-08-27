e33b0307bc61f8d07d6ed1a1e24478764215398e
Mikl Wolfe <wolfemikl@gmail.com>
2022-08-26 20:20:22 -0600

    Update font-3270-nerd-font from 2.1.0 to 2.2.1 (#6164)
    * Update font-3270-nerd-font from 2.1.0 to 2.2.1
    
    * Update font-3270-nerd-font.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-3270-nerd-font.rb
+++ Casks/font-3270-nerd-font.rb
@@ -1,9 +1,8 @@
 cask "font-3270-nerd-font" do
-  version "2.1.0"
-  sha256 "b39bb03a46bf51cacf094a53d9f07c090ede4571efa2c24600c66a25a3fee1cc"
+  version "2.2.1"
+  sha256 "161ba437475fb162058c9947a7f62e214006ea7f8b4bda1fb2a9ea19077bf9af"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/3270.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "3270 Nerd Font (3270)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
