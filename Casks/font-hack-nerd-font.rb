79c0eeaf58d1afbc8381dd778ea1172e4052323e
Moulick Aggarwal <Moulick@users.noreply.github.com>
2022-08-27 07:37:54 +0530

    Update font-hack-nerd-font from 2.1.0 to 2.2.0 (#6158)

--- Casks/font-hack-nerd-font.rb
+++ Casks/font-hack-nerd-font.rb
@@ -1,9 +1,8 @@
 cask "font-hack-nerd-font" do
-  version "2.1.0"
-  sha256 "70852e59fcffbe31d401f615625bcb9ebb6af72732c2f1fe9b9d5370c2565514"
+  version "2.2.0"
+  sha256 "09f9b0ad4ceec25f7bb81bed0ae10b4e3ceae08301a973104e07add17bc9df98"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Hack.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "Hack Nerd Font (Hack)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
