742d9ce4f45c4c9077ec652add000cc092874212
Mikl Wolfe <wolfemikl@gmail.com>
2022-08-27 00:49:18 -0600

    Update font-aurulent-sans-mono-nerd-font.rb

--- Casks/font-aurulent-sans-mono-nerd-font.rb
+++ Casks/font-aurulent-sans-mono-nerd-font.rb
@@ -3,7 +3,6 @@ cask "font-aurulent-sans-mono-nerd-font" do
   sha256 "b107076e9783928722d794635f029f8a5b1c57b177946c1dc95b9402857bc8c3"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AurulentSansMono.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "AurulentSansMono Nerd Font (Aurulent Sans Mono)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
