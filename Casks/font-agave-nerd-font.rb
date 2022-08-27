50b8bd408bfa63b2e48cb12b4b87df57e4bc94f3
Mikl Wolfe <wolfemikl@gmail.com>
2022-08-26 20:22:33 -0600

    Update font-agave-nerd-font from 2.1.0 to 2.2.1 (#6165)
    * Update font-agave-nerd-font from 2.1.0 to 2.2.1
    
    * Update font-agave-nerd-font.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-agave-nerd-font.rb
+++ Casks/font-agave-nerd-font.rb
@@ -1,9 +1,8 @@
 cask "font-agave-nerd-font" do
-  version "2.1.0"
-  sha256 "0a6a5f056553cd8fa0c95dc57b0cdf027ff2f49016d0c470386bb2ba7918f549"
+  version "2.2.1"
+  sha256 "f27ac84205ef4fcbcc5d9454b8bfae5d39d8d92a9f384d0c122f548a654f74f6"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Agave.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "Agave Nerd Font (Agave)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
