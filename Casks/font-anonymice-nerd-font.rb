a3b09c967af95088957114920ff22d8376b21d38
Mikl Wolfe <wolfemikl@gmail.com>
2022-08-26 20:54:59 -0600

    Update font-anonymice-nerd-font from 2.1.0 to 2.2.1 (#6166)
    * Update font-anonymice-nerd-font from 2.1.0 to 2.2.1
    
    * Update font-anonymice-nerd-font.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/font-anonymice-nerd-font.rb
+++ Casks/font-anonymice-nerd-font.rb
@@ -1,9 +1,8 @@
 cask "font-anonymice-nerd-font" do
-  version "2.1.0"
-  sha256 "b51b3dd9aa5bcf061240d8dfcc203e78b085eeb97a76d91f6ad8cd9473467668"
+  version "2.2.1"
+  sha256 "bf2ef8da0b76fad541261853b6281fe3705083cd5c9718bf6aea5497d33317bd"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AnonymousPro.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "Anonymice Nerd Font (Anonymous Pro)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
