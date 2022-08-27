f165fffb59fc5644afe499e75eaa55882175ee84
Evan <yif.wang@icloud.com>
2022-08-27 13:00:48 -0700

    Update font-jetbrains-mono-nerd-font from 2.1.0 to 2.2.1

--- Casks/font-jetbrains-mono-nerd-font.rb
+++ Casks/font-jetbrains-mono-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-jetbrains-mono-nerd-font" do
-  version "2.1.0"
-  sha256 "842013fa44b6896d4eb91635a81ef75244d78d7f61ff866c9dfd3315a67788cd"
+  version "2.2.1"
+  sha256 "59dd2dc001a54e6d1c6c233449c51094650c0ef1fe76c87a8524eb5def4f1db8"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/JetBrainsMono.zip"
   name "JetBrainsMono Nerd Font (JetBrains Mono)"
