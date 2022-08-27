2c16bdc75c66b33fd13811a8c6e2ffb82cf6a26b
Miccal Matthews <miccal.matthews@gmail.com>
2022-08-27 10:16:14 +0800

    Update font-agave-nerd-font.rb

--- Casks/font-agave-nerd-font.rb
+++ Casks/font-agave-nerd-font.rb
@@ -3,7 +3,6 @@ cask "font-agave-nerd-font" do
   sha256 "f27ac84205ef4fcbcc5d9454b8bfae5d39d8d92a9f384d0c122f548a654f74f6"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Agave.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "Agave Nerd Font (Agave)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
