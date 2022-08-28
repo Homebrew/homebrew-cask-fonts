d98446ec8a80321bd8e99a7939aa13553cfdc8d6
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-08-28 00:07:34 +0000

    Update Google Fonts

--- Casks/font-lateef.rb
+++ Casks/font-lateef.rb
@@ -2,10 +2,17 @@ cask "font-lateef" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/lateef/LateefRegOT.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/trunk/ofl/lateef",
+      verified: "github.com/google/fonts/",
+      using:    :svn
   name "Lateef"
   homepage "https://fonts.google.com/specimen/Lateef"
 
-  font "LateefRegOT.ttf"
+  font "Lateef-Bold.ttf"
+  font "Lateef-ExtraBold.ttf"
+  font "Lateef-ExtraLight.ttf"
+  font "Lateef-Light.ttf"
+  font "Lateef-Medium.ttf"
+  font "Lateef-Regular.ttf"
+  font "Lateef-SemiBold.ttf"
 end
--- Casks/font-nabla.rb
+++ Casks/font-nabla.rb
@@ -2,10 +2,10 @@ cask "font-nabla" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/nabla/Nabla%5Bwght%5D.ttf",
+  url "https://github.com/google/fonts/raw/main/ofl/nabla/Nabla%5BEDPT%2CEHLT%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Nabla"
   homepage "https://fonts.google.com/specimen/Nabla"
 
-  font "Nabla[wght].ttf"
+  font "Nabla[EDPT,EHLT].ttf"
 end
