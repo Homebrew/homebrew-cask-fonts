345e46210c6d324d83009e4dd940d8225d2c3cc4
Miccal Matthews <miccal.matthews@gmail.com>
2022-08-28 09:43:46 +0800

    Update font-nabla.rb

--- Casks/font-nabla.rb
+++ Casks/font-nabla.rb
@@ -3,7 +3,6 @@ cask "font-nabla" do
   sha256 :no_check
 
   url "https://github.com/google/fonts/raw/main/ofl/nabla/Nabla%5BEDPT%2CEHLT%5D.ttf",
-      verified: "github.com/google/fonts/"
   name "Nabla"
   homepage "https://github.com/justvanrossum/nabla"
 
