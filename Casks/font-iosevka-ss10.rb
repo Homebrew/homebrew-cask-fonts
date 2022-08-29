ea41969e524b29c39d95e1a90b1c2b900d559fb6
Ryan <25192197+singularitti@users.noreply.github.com>
2022-08-29 09:02:16 -0400

    Update font-iosevka-ss10 from 16.0.1 to 16.0.2 (#6213)

--- Casks/font-iosevka-ss10.rb
+++ Casks/font-iosevka-ss10.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss10" do
-  version "16.0.1"
-  sha256 "6c28e935646ad4be62c5795b7ed1fc780ba98823a0cdc867db6a04ff2ea2ea81"
+  version "16.0.2"
+  sha256 "7bd87deeb08aae31374d4830027e63792789620c4f8f4d8a169b0cf7ccfc624e"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss10-#{version}.zip"
   name "Iosevka SS10"
