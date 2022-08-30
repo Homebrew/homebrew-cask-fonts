e8b6b2bd3bf166fa78b443d3915d6aff41d3b71c
singularitti <singularitti@outlook.com>
2022-08-30 05:48:26 -0400

    Add font "font-lxgw-wenkai-tc" / "LXGW WenKai TC" / "霞鶩文楷 TC"

---
+++ Casks/font-lxgw-wenkai-tc.rb
@@ -0,0 +1,17 @@
+cask "font-lxgw-wenkai-tc" do
+  version "0.920"
+  sha256 "6f2a6d5a654af4484f80c6cbc3912fc5f9db3361ddcb7ec08cbf4d0c27159b3a"
+
+  url "https://github.com/lxgw/LxgwWenkaiTC/releases/download/v#{version}/lxgw-wenkai-tc-v#{version}.zip"
+  name "LXGW WenKai TC"
+  name "霞鶩文楷 TC"
+  desc "Open-source traditional Chinese font derived from Fontworks' Klee One"
+  homepage "https://github.com/lxgw/LxgwWenkaiTC"
+
+  font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiMonoTC-Bold.ttf"
+  font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiMonoTC-Light.ttf"
+  font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiMonoTC-Regular.ttf"
+  font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiTC-Bold.ttf"
+  font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiTC-Light.ttf"
+  font "lxgw-wenkai-tc-v#{version}/LXGWWenKaiTC-Regular.ttf"
+end
