b593f69c3dcf668f23fdc04f5bd97af28c50092c
singularitti <singularitti@outlook.com>
2022-09-02 13:10:42 -0400

    Add font "font-chenyuluoyan" / "辰宇落雁體"

---
+++ Casks/font-chenyuluoyan.rb
@@ -0,0 +1,12 @@
+cask "font-chenyuluoyan" do
+  version "1.0-motospaced"
+  sha256 "c8ffe4559c4f1d9013b58a318cba5b5c955ab7306080980e3b6a9126641e8af7"
+
+  url "https://github.com/Chenyu-otf/chenyuluoyan_thin/releases/download/v#{version}/ChenYuluoyan-Thin-Monospaced.ttf"
+  name "font-chenyuluoyan"
+  name "辰宇落雁體"
+  desc "Open-source traditional Chinese handwritten font"
+  homepage "https://github.com/Chenyu-otf/chenyuluoyan_thin"
+
+  font "ChenYuluoyan-Thin-Monospaced.ttf"
+end
