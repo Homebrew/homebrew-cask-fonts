a15793d7177186c3cb3aee44cfae9dbd4c237c7b
Joe George <joegeorge01@gmail.com>
2022-09-04 13:56:40 -0400

    Update font-caskaydia-cove-nerd-font from 2.1.0 to 2.2.1

--- Casks/font-caskaydia-cove-nerd-font.rb
+++ Casks/font-caskaydia-cove-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-caskaydia-cove-nerd-font" do
-  version "2.1.0"
-  sha256 "92b50960305c580fc90bb3bd5456e26edc9b321907dfa2578066a9b38e2a94e0"
+  version "2.2.1"
+  sha256 "06039f286c247cc5606bbc8267cdefcc01d1a1955b8da597a11c6b6fdc480c65"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/CascadiaCode.zip"
   appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
