c98821b351c6dedc20c7eac666ecc28a078e24ad
Reno <singularitti@outlook.com>
2022-09-04 13:08:29 -0400

    Update font-iosevka-ss07 from 16.0.2 to 16.1.0

--- Casks/font-iosevka-ss07.rb
+++ Casks/font-iosevka-ss07.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss07" do
-  version "16.0.2"
-  sha256 "04a8028f56ff2d3ba5735e462e5556b7da19d40f7fb7e82c083f06666e771ec1"
+  version "16.1.0"
+  sha256 "d9249a22cca484eb3ba47e1bbd836a31b5c5a540585841d71ac60eb4d7c030b8"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss07-#{version}.zip"
   name "Iosevka SS07"
