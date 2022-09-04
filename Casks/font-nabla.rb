ace25dda73d0f6280f796584700f106eb28fa745
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-09-04 00:06:23 +0000

    Update Google Fonts

--- Casks/font-nabla.rb
+++ Casks/font-nabla.rb
@@ -2,9 +2,11 @@ cask "font-nabla" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/nabla/Nabla%5BEDPT%2CEHLT%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/nabla/Nabla%5BEDPT%2CEHLT%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Nabla"
-  homepage "https://github.com/justvanrossum/nabla"
+  desc "Color font inspired by isometric computer games, built using the colrv1 format"
+  homepage "https://fonts.google.com/specimen/Nabla"
 
   font "Nabla[EDPT,EHLT].ttf"
 end
