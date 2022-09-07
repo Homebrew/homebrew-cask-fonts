a615482d40fe7510fa0e328fb5e239cebfe1cbb5
Branch Vincent <branchevincent@gmail.com>
2022-09-07 18:31:23 -0400

    Update font-meslo-lg-nerd-font from 2.2.1 to 2.2.2

--- Casks/font-meslo-lg-nerd-font.rb
+++ Casks/font-meslo-lg-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-meslo-lg-nerd-font" do
-  version "2.2.1"
-  sha256 "fa206721add4716ff6437fc2ea6e5b65c9dad8d5565117f80f13df7dd1261f98"
+  version "2.2.2"
+  sha256 "a9dc3bb2442d44bfb6a571e808a0a0528977916b5ee21f58e04c10e266684e72"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Meslo.zip"
   name "MesloLG Nerd Font families (Meslo LG)"
