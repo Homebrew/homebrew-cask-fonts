2bc3652f92d4d128ca7eb0bda6139f4361a20fdd
Moulick Aggarwal <moulickaggarwal@gmail.com>
2022-09-08 11:53:07 +0530

    Update font-fira-code-nerd-font from 2.2.1 to 2.2.2

--- Casks/font-fira-code-nerd-font.rb
+++ Casks/font-fira-code-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-fira-code-nerd-font" do
-  version "2.2.1"
-  sha256 "63410350230ad9c1873df4b1e154066d809cff2490e5bbc440f05d7967ee8e2f"
+  version "2.2.2"
+  sha256 "20182e6e7c42cf8ab479d83af3200266261ec9bd4e80cdaceb793ecd56c9a398"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraCode.zip"
   name "FiraCode Nerd Font (Fira Code)"
