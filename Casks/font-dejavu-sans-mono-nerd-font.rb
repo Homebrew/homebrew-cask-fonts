eb6484bc5c11f2a1e275bc772b661320247abeac
Josa Gesell <josa@gesell.me>
2022-09-15 02:33:40 +0200

    Update font-dejavu-sans-mono-nerd-font from 2.2.1 to 2.2.2 (#6298)
    * Update font-dejavu-sans-mono-nerd-font from 2.2.1 to 2.2.2
    
    * Update font-dejavu-sans-mono-nerd-font.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-dejavu-sans-mono-nerd-font.rb
+++ Casks/font-dejavu-sans-mono-nerd-font.rb
@@ -1,10 +1,10 @@
 cask "font-dejavu-sans-mono-nerd-font" do
-  version "2.1.0"
-  sha256 "3fbcc4904c88f68d24c8b479784a1aba37f2d78b1162d21f6fc85a58ffcc0e0f"
+  version "2.2.2"
+  sha256 "f05ebb194a76f0437bd95f3d21a5b73d6038ec09c8d4dfd6a37b748a9f548442"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DejaVuSansMono.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "DejaVuSansMono Nerd Font (DejaVu Sans Mono)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
   font "DejaVu Sans Mono Bold Nerd Font Complete Mono.ttf"
