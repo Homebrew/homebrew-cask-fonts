61f8c5153d06b36b221c839f9ff2dab130722cec
Miccal Matthews <miccal.matthews@gmail.com>
2022-09-19 11:17:36 +0800

    Update font-go-mono-nerd-font.rb

--- Casks/font-go-mono-nerd-font.rb
+++ Casks/font-go-mono-nerd-font.rb
@@ -3,8 +3,8 @@ cask "font-go-mono-nerd-font" do
   sha256 "aae3e1c8fb30afae212d1e368cefe425e0b44d9293d4bdc9ab609ce112a28c02"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Go-Mono.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "GoMono Nerd Font (Go)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
   font "Go Mono Nerd Font Complete.ttf"
