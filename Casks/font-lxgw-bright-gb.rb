8212bd59e9e310e7e0cb74567844c425a0c9af1b
singularitti <singularitti@outlook.com>
2022-09-27 19:02:35 -0400

    Update font-lxgw-bright-gb from 4.124 to 4.132.4

--- Casks/font-lxgw-bright-gb.rb
+++ Casks/font-lxgw-bright-gb.rb
@@ -1,16 +1,16 @@
 cask "font-lxgw-bright-gb" do
-  version "4.124"
-  sha256 "31ba535644f3ccc76ea4bb3e4fb8e03288aee4e9a617f95c51d8d66f1de21b0f"
+  version "4.132.4"
+  sha256 "f9655605ced1ba9af73a67f51910032d2f19fa5e673eb5dcb78ea8c39470b5a6"
 
-  url "https://github.com/lxgw/LxgwBright/archive/refs/tags/v#{version}.zip"
+  url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBrightGB.7z"
   name "LXGW Bright GB"
   desc "Merged font of Ysabeau Office and LXGW WenKai GB"
   homepage "https://github.com/lxgw/LxgwBright"
 
-  font "LxgwBright-#{version}/LXGWBrightGB/LXGWBrightGB-Medium.otf"
-  font "LxgwBright-#{version}/LXGWBrightGB/LXGWBrightGB-MediumItalic.otf"
-  font "LxgwBright-#{version}/LXGWBrightGB/LXGWBrightGB-Regular.otf"
-  font "LxgwBright-#{version}/LXGWBrightGB/LXGWBrightGB-Italic.otf"
-  font "LxgwBright-#{version}/LXGWBrightGB/LXGWBrightGB-SemiLight.otf"
-  font "LxgwBright-#{version}/LXGWBrightGB/LXGWBrightGB-SemiLightItalic.otf"
+  font "LXGWBrightGB/LXGWBrightGB-Medium.otf"
+  font "LXGWBrightGB/LXGWBrightGB-MediumItalic.otf"
+  font "LXGWBrightGB/LXGWBrightGB-Regular.otf"
+  font "LXGWBrightGB/LXGWBrightGB-Italic.otf"
+  font "LXGWBrightGB/LXGWBrightGB-SemiLight.otf"
+  font "LXGWBrightGB/LXGWBrightGB-SemiLightItalic.otf"
 end
