cff74fcacdc2ee34b0eae7ec31bac3d9adca52f3
Ryan <25192197+singularitti@users.noreply.github.com>
2022-09-27 19:01:28 -0400

    Fix download link

--- Casks/font-lxgw-bright-tc.rb
+++ Casks/font-lxgw-bright-tc.rb
@@ -2,7 +2,7 @@ cask "font-lxgw-bright-tc" do
   version "4.132.4"
   sha256 "f9655605ced1ba9af73a67f51910032d2f19fa5e673eb5dcb78ea8c39470b5a6"
 
-  url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBrightGB.7z"
+  url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBrightTC.7z"
   name "LXGW Bright TC"
   desc "Merged font of Ysabeau Office and LXGW WenKai TC"
   homepage "https://github.com/lxgw/LxgwBright"
