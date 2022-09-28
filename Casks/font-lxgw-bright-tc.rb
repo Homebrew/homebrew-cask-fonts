acadf2ba9a0e5508e7e945076eb26b7d549236ca
Ryan <25192197+singularitti@users.noreply.github.com>
2022-09-27 20:13:27 -0400

    Update font-lxgw-bright-tc from 4.124 to 4.132.4 (#6317)
    * Update font-lxgw-bright-tc from 4.124 to 4.132.4
    
    * Fix download link
    
    * Update font-lxgw-bright-tc.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-lxgw-bright-tc.rb
+++ Casks/font-lxgw-bright-tc.rb
@@ -1,16 +1,16 @@
 cask "font-lxgw-bright-tc" do
-  version "4.124"
-  sha256 "31ba535644f3ccc76ea4bb3e4fb8e03288aee4e9a617f95c51d8d66f1de21b0f"
+  version "4.132.4"
+  sha256 "a7b1eabd0f50705257adebbb995ec6d99710aef95a69f49100ab9f52b9b617d6"
 
-  url "https://github.com/lxgw/LxgwBright/archive/refs/tags/v#{version}.zip"
+  url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBrightTC.7z"
   name "LXGW Bright TC"
   desc "Merged font of Ysabeau Office and LXGW WenKai TC"
   homepage "https://github.com/lxgw/LxgwBright"
 
-  font "LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-Medium.otf"
-  font "LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-MediumItalic.otf"
-  font "LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-Regular.otf"
-  font "LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-Italic.otf"
-  font "LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-SemiLight.otf"
-  font "LxgwBright-#{version}/LXGWBrightTC/LXGWBrightTC-SemiLightItalic.otf"
+  font "LXGWBrightTC/LXGWBrightTC-Medium.otf"
+  font "LXGWBrightTC/LXGWBrightTC-MediumItalic.otf"
+  font "LXGWBrightTC/LXGWBrightTC-Regular.otf"
+  font "LXGWBrightTC/LXGWBrightTC-Italic.otf"
+  font "LXGWBrightTC/LXGWBrightTC-SemiLight.otf"
+  font "LXGWBrightTC/LXGWBrightTC-SemiLightItalic.otf"
 end
