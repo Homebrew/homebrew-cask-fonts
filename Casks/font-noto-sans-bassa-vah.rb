0797a9846c7e8b7f5e389fdfb8f2bd1a18f82073
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-10-02 00:09:55 +0000

    Update Google Fonts

--- Casks/font-noto-sans-bassa-vah.rb
+++ Casks/font-noto-sans-bassa-vah.rb
@@ -2,10 +2,10 @@ cask "font-noto-sans-bassa-vah" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosansbassavah/NotoSansBassaVah-Regular.ttf",
+  url "https://github.com/google/fonts/raw/main/ofl/notosansbassavah/NotoSansBassaVah%5Bwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Noto Sans Bassa Vah"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Bassa+Vah"
 
-  font "NotoSansBassaVah-Regular.ttf"
+  font "NotoSansBassaVah[wght].ttf"
 end
