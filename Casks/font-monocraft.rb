ac551e50f9d3c27204e4686407a05d206cb8e925
Idrees <idrees@idreesinc.com>
2022-10-06 02:31:15 -0400

    Update font-monocraft from 1.3 to 1.4 (#6375)

--- Casks/font-monocraft.rb
+++ Casks/font-monocraft.rb
@@ -1,6 +1,6 @@
 cask "font-monocraft" do
-  version "1.3"
-  sha256 "4d7d54ce08cdacf66991a683ff325f519668623404dd111c03ae4c9d54996e36"
+  version "1.4"
+  sha256 "fbdf76af2b4b72def4e6ea035fbc92aac12f093900db1b9e2c47430e3cc21030"
 
   url "https://github.com/IdreesInc/Monocraft/releases/download/v#{version}/Monocraft.otf"
   name "Monocraft"
