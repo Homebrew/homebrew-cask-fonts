9ed4c89f39d82494957a07ef041c7771ff458440
singularitti <singularitti@outlook.com>
2022-10-10 05:38:13 -0400

    font-iosevka-ss12 16.3.2
    Update font-iosevka-ss12 from 16.3.1 to 16.3.2
    
    Closes #6421.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-iosevka-ss12.rb
+++ Casks/font-iosevka-ss12.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss12" do
-  version "16.3.1"
-  sha256 "289834fd78d520a0b4f730056f3833e27af18e2484700f4bda31749c0211a5ab"
+  version "16.3.2"
+  sha256 "b92b4dd3e7a335c043b94558b1349ee874ec7eff2719928fef2d649e57598b38"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss12-#{version}.zip"
   name "Iosevka SS12"
