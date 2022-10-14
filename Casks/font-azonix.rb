bde2a9cd2e774b870f7244cf2ef3c8618845b592
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-14 23:09:51 +0800

    Update font-azonix.rb

--- Casks/font-azonix.rb
+++ Casks/font-azonix.rb
@@ -4,7 +4,7 @@ cask "font-azonix" do
 
   url "https://dl.dafont.com/dl/?f=azonix"
   name "Azonix"
-  desc "Azonix is a modern sans-serif typeface"
+  desc "Modern sans-serif typeface"
   homepage "https://www.dafont.com/azonix.font"
 
   font "Azonix.otf"
