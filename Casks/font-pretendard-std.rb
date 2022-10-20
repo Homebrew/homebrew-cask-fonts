483f51d368cfbd7e3f935844b5abf7f76472fef5
Patrick Linnane <patrick@linnane.io>
2022-10-20 08:37:18 -0700

    font-pretendard-std 1.3.6
    Update font-pretendard-std from 1.3.5 to 1.3.6
    
    Closes #6500.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-pretendard-std.rb
+++ Casks/font-pretendard-std.rb
@@ -1,6 +1,6 @@
 cask "font-pretendard-std" do
-  version "1.3.5"
-  sha256 "14ae1f7dd1ce8c41b1ec4763f3b450653f265d15c26234dc441e3746468c04b9"
+  version "1.3.6"
+  sha256 "bc7525b7046fa0748bf6573fcaed3c75f9af10d478d46ad014e55b5040d9b7f9"
 
   url "https://github.com/orioncactus/pretendard/releases/download/v#{version}/PretendardStd-#{version}.zip"
   name "Pretendard Std"
