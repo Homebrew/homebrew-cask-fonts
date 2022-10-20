e443e6956bed9e78746a7e3f0ea9be650ef7f7d9
Patrick Linnane <patrick@linnane.io>
2022-10-20 08:34:20 -0700

    font-last-resort 15.000
    Update font-last-resort from 14.000 to 15.000
    
    Closes #6492.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-last-resort.rb
+++ Casks/font-last-resort.rb
@@ -1,6 +1,6 @@
 cask "font-last-resort" do
-  version "14.000"
-  sha256 "da83a62294e74d963a10de4c3750ccf089273e3b7fc6744daef9844163ade078"
+  version "15.000"
+  sha256 "b5a6cbd70646342aca928c619d544e2c844d73ba346db256b421ebaac02c5fd1"
 
   url "https://github.com/unicode-org/last-resort-font/releases/download/#{version}/LastResort-Regular.ttf"
   name "Last Resort"
