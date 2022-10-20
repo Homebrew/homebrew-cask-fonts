d0f87b584de694b94d5737064654403e715707e6
Bevan Kay <email@bevankay.me>
2022-10-21 01:38:14 +1100

    Update font-mononoki from 1.3 to 1.4 (#6490)
    * Update font-mononoki from 1.3 to 1.4
    
    * Add desc
    
    * Update font artifcats

--- Casks/font-mononoki.rb
+++ Casks/font-mononoki.rb
@@ -1,15 +1,19 @@
 cask "font-mononoki" do
-  version "1.3"
-  sha256 "37291a9371b4127bb03e012ace7ecc79375f6eccfa688be81a36ccc5d29e3ee6"
+  version "1.4"
+  sha256 "e0b8223a524f7f4dfa6a25f1bc9b061f5303b114a3837aaad860446bca71a857"
 
   url "https://github.com/madmalik/mononoki/releases/download/#{version}/mononoki.zip",
       verified: "github.com/madmalik/mononoki/"
-  appcast "https://github.com/madmalik/mononoki/releases.atom"
   name "Mononoki"
+  desc "Programming font"
   homepage "https://madmalik.github.io/mononoki/"
 
-  font "webfont/mononoki-Bold.ttf"
-  font "webfont/mononoki-BoldItalic.ttf"
-  font "webfont/mononoki-Italic.ttf"
-  font "webfont/mononoki-Regular.ttf"
+  font "mononoki-Bold.otf"
+  font "mononoki-Bold.ttf"
+  font "mononoki-BoldItalic.otf"
+  font "mononoki-BoldItalic.ttf"
+  font "mononoki-Italic.otf"
+  font "mononoki-Italic.ttf"
+  font "mononoki-Regular.otf"
+  font "mononoki-Regular.ttf"
 end
