4afd1cc2f2a3e57edc7889eb56667fb89534851a
Patrick Linnane <patrick@linnane.io>
2022-10-20 08:34:39 -0700

    Update font-last-resort-high-efficiency from 14.000 to 15.000

--- Casks/font-last-resort-high-efficiency.rb
+++ Casks/font-last-resort-high-efficiency.rb
@@ -1,6 +1,6 @@
 cask "font-last-resort-high-efficiency" do
-  version "14.000"
-  sha256 "74507aa7ae9b02a469a6acbba53db83f978b76a89e8ccec95bb5bc14941588f9"
+  version "15.000"
+  sha256 "432a3fb6e06f0479c6fcb5bcb14032eb6c697aa9577f27f08d409baf8bc911a8"
 
   url "https://github.com/unicode-org/last-resort-font/releases/download/#{version}/LastResortHE-Regular.ttf"
   name "Last Resort High-Efficiency"
