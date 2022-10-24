0b39bf347a5038829bc21178f987597cc393eac9
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-24 08:29:56 +0800

    Update font-marhey.rb

--- Casks/font-marhey.rb
+++ Casks/font-marhey.rb
@@ -5,7 +5,7 @@ cask "font-marhey" do
   url "https://github.com/google/fonts/raw/main/ofl/marhey/Marhey%5Bwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Marhey"
-  desc "Playful display typeface, custom hand lettering with contrast strokes that makes dynamic and expressive impression"
+  desc "Custom hand lettering with contrast strokes"
   homepage "https://fonts.google.com/specimen/Marhey"
 
   font "Marhey[wght].ttf"
