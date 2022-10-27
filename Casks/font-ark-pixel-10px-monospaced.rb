cdd7973fb34dca887fb8d248c15c2e472a9d6463
TakWolf <takwolf@foxmail.com>
2022-10-28 04:29:49 +0800

    Update font-ark-pixel-10px-monospaced from 2022.07.05 to 2022.10.27

---
+++ Casks/font-ark-pixel-10px-monospaced.rb
@@ -0,0 +1,16 @@
+cask "font-ark-pixel-10px-monospaced" do
+  version "2022.10.27"
+  sha256 "6311d3f7be0dc45d96d898ce968263f87fb707c28719240a2ef26398fe0bd62e"
+
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-monospaced-otf-v#{version.no_hyphens}.zip"
+  name "Ark Pixel 10px monospaced"
+  homepage "https://github.com/TakWolf/ark-pixel-font"
+
+  font "ark-pixel-10px-monospaced-ja.otf"
+  font "ark-pixel-10px-monospaced-ko.otf"
+  font "ark-pixel-10px-monospaced-latin.otf"
+  font "ark-pixel-10px-monospaced-zh_cn.otf"
+  font "ark-pixel-10px-monospaced-zh_hk.otf"
+  font "ark-pixel-10px-monospaced-zh_tr.otf"
+  font "ark-pixel-10px-monospaced-zh_tw.otf"
+end
