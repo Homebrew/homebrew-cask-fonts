c2579cf09e33ccd445c4a7e0ce66b79de99617c2
TakWolf <takwolf@foxmail.com>
2022-10-28 11:22:04 +0800

    Change "Ark Pixel Font" cask token

---
+++ Casks/font-ark-pixel-12px-proportional.rb
@@ -0,0 +1,17 @@
+cask "font-ark-pixel-12px-proportional" do
+  version "2022.10.27"
+  sha256 "e4fb5aaa3bfe3732ed3250c89693c4aab2a5c5d2719cb8a514447dfa042bd484"
+
+  url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-12px-proportional-otf-v#{version}.zip"
+  name "Ark Pixel 12px proportional"
+  desc "Open source Pan-CJK pixel font"
+  homepage "https://github.com/TakWolf/ark-pixel-font"
+
+  font "ark-pixel-12px-proportional-ja.otf"
+  font "ark-pixel-12px-proportional-ko.otf"
+  font "ark-pixel-12px-proportional-latin.otf"
+  font "ark-pixel-12px-proportional-zh_cn.otf"
+  font "ark-pixel-12px-proportional-zh_hk.otf"
+  font "ark-pixel-12px-proportional-zh_tr.otf"
+  font "ark-pixel-12px-proportional-zh_tw.otf"
+end
