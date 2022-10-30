d88dde665dd93f518b40809b8afdab6388bdf025
Patrick Linnane <patrick@linnane.io>
2022-10-30 09:51:19 -0700

    font-iosevka-etoile 16.3.6
    Update font-iosevka-etoile from 16.3.5 to 16.3.6
    
    Closes #6540.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-iosevka-etoile.rb
+++ Casks/font-iosevka-etoile.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-etoile" do
-  version "16.3.5"
-  sha256 "2a4c22606fbe8caa18d15703fd224c3faf09d40f404bbd3a2387579aff28677b"
+  version "16.3.6"
+  sha256 "2d69b79bafad8616693064f25040637b87f1c641bb900d4610946d871ac10935"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-etoile-#{version}.zip"
   name "Iosevka Etoile"
