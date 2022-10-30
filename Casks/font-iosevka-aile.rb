b1b6bb9c0d59931bc389d994d7c491aff3133947
Patrick Linnane <patrick@linnane.io>
2022-10-30 09:50:05 -0700

    Update font-iosevka-aile from 16.3.5 to 16.3.6

--- Casks/font-iosevka-aile.rb
+++ Casks/font-iosevka-aile.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-aile" do
-  version "16.3.5"
-  sha256 "b6daa14e24c66bddc07f9b4f2ae8893a83e292dbc55f49da0727bcae07306604"
+  version "16.3.6"
+  sha256 "cb5728b432dcd70cca4a6b434b9506667b54df8cd716ec46d34a4fe3605736d4"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-aile-#{version}.zip"
   name "Iosevka Aile"
