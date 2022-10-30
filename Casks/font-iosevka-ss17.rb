ec6b0d37ac001a9557d29e0c5690fb19622b07f1
Patrick Linnane <patrick@linnane.io>
2022-10-29 21:40:18 -0700

    Update font-iosevka-ss17 from 16.3.4 to 16.3.5

--- Casks/font-iosevka-ss17.rb
+++ Casks/font-iosevka-ss17.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss17" do
-  version "16.3.4"
-  sha256 "60a5140a9856643913be31e887ee9b8c0ce2abcc544732a18562c1e628e068a9"
+  version "16.3.5"
+  sha256 "5e363f4b02eb904a4192f5f20bc2d6d69c548d288f1f775e429b29b4f419ad5a"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss17-#{version}.zip"
   name "Iosevka SS17"
