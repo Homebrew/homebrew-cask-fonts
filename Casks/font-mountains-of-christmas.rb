0faef1214d95d1ab4e14d4b958934484dd830bdb
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-11-06 22:41:04 +0000

    Update Google Fonts

--- Casks/font-mountains-of-christmas.rb
+++ Casks/font-mountains-of-christmas.rb
@@ -2,9 +2,10 @@ cask "font-mountains-of-christmas" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/apache/mountainsofchristmas",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
+      branch:    "main",
+      only_path: "apache/mountainsofchristmas"
   name "Mountains of Christmas"
   homepage "https://fonts.google.com/specimen/Mountains+of+Christmas"
 
--- Casks/font-tiro-devanagari-sanskrit.rb
+++ Casks/font-tiro-devanagari-sanskrit.rb
@@ -2,9 +2,10 @@ cask "font-tiro-devanagari-sanskrit" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/trunk/ofl/tirodevanagarisanskrit",
-      verified: "github.com/google/fonts/",
-      using:    :svn
+  url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
+      branch:    "main",
+      only_path: "ofl/tirodevanagarisanskrit"
   name "Tiro Devanagari Sanskrit"
   desc "Broader proportions, generous counters, and strong diagonal strokes"
   homepage "https://fonts.google.com/specimen/Tiro+Devanagari+Sanskrit"
