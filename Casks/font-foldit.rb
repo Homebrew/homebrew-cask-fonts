62213e6ef39450256bd5102986e59a9f81187b91
Markus Reiter <me@reitermark.us>
2022-11-07 14:27:56 +0100

    Update font-foldit.rb

--- Casks/font-foldit.rb
+++ Casks/font-foldit.rb
@@ -6,7 +6,7 @@ cask "font-foldit" do
       verified: "github.com/google/fonts/"
   name "Foldit"
   desc "Font which uses gradients to play with dimension and sense of space"
-  homepage "https://fonts.google.com/specimen/Foldit"
+  homepage "https://github.com/SophiaDesign/Foldit"
 
   font "Foldit[wght].ttf"
 end
