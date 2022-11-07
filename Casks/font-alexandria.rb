2159e491e6d771c2e899ed4d0806f7483ecfc007
Markus Reiter <me@reitermark.us>
2022-11-07 10:51:19 +0100

    Update font-alexandria.rb

--- Casks/font-alexandria.rb
+++ Casks/font-alexandria.rb
@@ -5,7 +5,7 @@ cask "font-alexandria" do
   url "https://github.com/google/fonts/raw/main/ofl/alexandria/Alexandria%5Bwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Alexandria"
-  desc "Variable font ranging from thin to black, increasing the ability to use the font in various applications, from long text using the light weights to short headlines using the heavy thick weights"
+  desc "9 weights font family made in matching to the latin type Montserrat"
   homepage "https://fonts.google.com/specimen/Alexandria"
 
   font "Alexandria[wght].ttf"
