e62767100e769f5d3b7238623117a6584dbbd6c1
Markus Reiter <me@reitermark.us>
2022-11-07 10:47:51 +0100

    Update font-noto-sans-lao-looped.rb

--- Casks/font-noto-sans-lao-looped.rb
+++ Casks/font-noto-sans-lao-looped.rb
@@ -5,7 +5,7 @@ cask "font-noto-sans-lao-looped" do
   url "https://github.com/google/fonts/raw/main/ofl/notosanslaolooped/NotoSansLaoLooped%5Bwdth%2Cwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Noto Sans Lao Looped"
-  desc "Unmodulated design in the more traditional, looped variant of the southeast asian lao script, suitable for all texts"
+  desc "Looped variant of the southeast asian lao script"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Lao+Looped"
 
   font "NotoSansLaoLooped[wdth,wght].ttf"
