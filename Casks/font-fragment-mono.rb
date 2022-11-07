09b3f60c498a842d65df8682541cbfedeace4697
Bevan Kay <email@bevankay.me>
2022-11-07 22:30:33 +1100

    Update font-fragment-mono.rb

--- Casks/font-fragment-mono.rb
+++ Casks/font-fragment-mono.rb
@@ -7,7 +7,7 @@ cask "font-fragment-mono" do
       branch:    "main",
       only_path: "ofl/fragmentmono"
   name "Fragment Mono"
-  desc "Designed by wei huang based on nimbus sans by urw design studio, based on helvetica by max miedinger"
+  desc "Monospaced coding font inspired on Helvetica"
   homepage "https://fonts.google.com/specimen/Fragment+Mono"
 
   font "FragmentMono-Italic.ttf"
