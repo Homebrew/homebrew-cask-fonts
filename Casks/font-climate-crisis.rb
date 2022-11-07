83cd64ce12340c2e78291cd3cdbba9b09db08f6e
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-11-07 14:03:34 +0000

    Update Google Fonts

--- Casks/font-climate-crisis.rb
+++ Casks/font-climate-crisis.rb
@@ -2,9 +2,10 @@ cask "font-climate-crisis" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/climatecrisis/ClimateCrisis%5BYEAR%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/climatecrisis/ClimateCrisis%5BYEAR%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Climate Crisis"
-  homepage "https://github.com/dancoull/ClimateCrisis"
+  homepage "https://fonts.google.com/specimen/Climate+Crisis"
 
   font "ClimateCrisis[YEAR].ttf"
 end
