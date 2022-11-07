f82dee6b566dd8a1454aaa834177cceb528f22a7
Bevan Kay <email@bevankay.me>
2022-11-07 22:29:42 +1100

    Update font-foldit.rb

--- Casks/font-foldit.rb
+++ Casks/font-foldit.rb
@@ -5,7 +5,7 @@ cask "font-foldit" do
   url "https://github.com/google/fonts/raw/main/ofl/foldit/Foldit%5Bwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Foldit"
-  desc "Variable-gradient colrv1 font which uses gradients to play with dimensions and give a sense of space"
+  desc "Variable-gradient font which uses gradients to play with dimensions and give a sense of space"
   homepage "https://fonts.google.com/specimen/Foldit"
 
   font "Foldit[wght].ttf"
