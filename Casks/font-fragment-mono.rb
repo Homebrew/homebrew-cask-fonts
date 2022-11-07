55862c979ef2312391f917a15b8802e86e2598cb
Markus Reiter <me@reitermark.us>
2022-11-07 14:36:14 +0100

    Update font-fragment-mono.rb

--- Casks/font-fragment-mono.rb
+++ Casks/font-fragment-mono.rb
@@ -3,12 +3,11 @@ cask "font-fragment-mono" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/fragmentmono"
   name "Fragment Mono"
   desc "Monospaced coding font inspired on Helvetica"
-  homepage "https://fonts.google.com/specimen/Fragment+Mono"
+  homepage "https://github.com/weiweihuanghuang/fragment-mono"
 
   font "FragmentMono-Italic.ttf"
   font "FragmentMono-Regular.ttf"
