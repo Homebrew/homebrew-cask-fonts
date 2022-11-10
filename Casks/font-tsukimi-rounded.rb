c292f9845b2f77c47c6934769b44101c9de8648e
Markus Reiter <me@reitermark.us>
2022-11-10 13:09:35 +0100

    Update font-tsukimi-rounded.rb

--- Casks/font-tsukimi-rounded.rb
+++ Casks/font-tsukimi-rounded.rb
@@ -3,12 +3,11 @@ cask "font-tsukimi-rounded" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/tsukimirounded"
   name "Tsukimi Rounded"
   desc "Sans-serif typeface with rounded terminals"
-  homepage "https://fonts.google.com/specimen/Tsukimi+Rounded"
+  homepage "https://github.com/mt-funa/Tsukimi-Rounded"
 
   font "TsukimiRounded-Bold.ttf"
   font "TsukimiRounded-Light.ttf"
