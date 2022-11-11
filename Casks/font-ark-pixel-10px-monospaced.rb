feae67bc64b4b2721c7f47e77277669dce463bb1
singularitti <singularitti@outlook.com>
2022-11-11 03:02:54 -0500

    Update font-ark-pixel-10px-monospaced from 2022.10.27 to 2022.11.11

--- Casks/font-ark-pixel-10px-monospaced.rb
+++ Casks/font-ark-pixel-10px-monospaced.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-10px-monospaced" do
-  version "2022.10.27"
-  sha256 "6311d3f7be0dc45d96d898ce968263f87fb707c28719240a2ef26398fe0bd62e"
+  version "2022.11.11"
+  sha256 "f0cb1932e3a98f3b1739aeea24a5f9ef101d9666193646c2760c1443082aae40"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-monospaced-otf-v#{version}.zip"
   name "Ark Pixel 10px monospaced"
