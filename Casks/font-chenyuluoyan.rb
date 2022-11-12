e1821e4cd552492c817adbfd1b2ae4dde4059ab3
Ryan <25192197+singularitti@users.noreply.github.com>
2022-11-11 23:07:41 -0500

    font-chenyuluoyan: Update name (#6598)
    font-chenyuluoyan: update name

--- Casks/font-chenyuluoyan.rb
+++ Casks/font-chenyuluoyan.rb
@@ -3,7 +3,7 @@ cask "font-chenyuluoyan" do
   sha256 "d8ecd0598634c92f0b29f90aabef8f7916f17aef19b0350883ca7b46979a5373"
 
   url "https://github.com/Chenyu-otf/chenyuluoyan_thin/releases/download/v#{version}/ChenYuluoyan-Thin-Monospaced.ttf"
-  name "font-chenyuluoyan"
+  name "Chenyuluoyan"
   name "辰宇落雁體"
   desc "Open-source traditional Chinese handwritten font"
   homepage "https://github.com/Chenyu-otf/chenyuluoyan_thin"
