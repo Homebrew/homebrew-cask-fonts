dbbd580b2f4c50e92b9055b5256939aed62e75f5
singularitti <singularitti@outlook.com>
2022-11-11 22:35:23 -0500

    Add font "font-photonico-code"

---
+++ Casks/font-photonico-code.rb
@@ -0,0 +1,11 @@
+cask "font-photonico-code" do
+  version "1.2"
+  sha256 "c4198812858dd634bc9df85aff240f5111586a911da336c0cd9b896282c21172"
+
+  url "https://github.com/Photonico/Photonico_Code/releases/download/#{version}/Photonico.#{version}.Regular.ttf"
+  name "Photonico Code"
+  desc "Free, monospace, slab-serif style font for coding"
+  homepage "https://github.com/Photonico/Photonico_Code"
+
+  font "Photonico.1.2.Regular.ttf"
+end
