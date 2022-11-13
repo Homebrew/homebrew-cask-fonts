bf8d176306fc695383b86ab474bf16d8a633608c
Markus Reiter <me@reitermark.us>
2022-11-13 22:34:37 +0100

    Update font-signika-negative-sc.rb

--- Casks/font-signika-negative-sc.rb
+++ Casks/font-signika-negative-sc.rb
@@ -8,7 +8,7 @@ cask "font-signika-negative-sc" do
       only_path: "ofl/signikanegativesc"
   name "Signika Negative SC"
   desc "Alternative version of the Signika SC font"
-  homepage "https://fonts.google.com/specimen/Signika"
+  homepage "https://fonts.google.com/specimen/Signika+Negative"
 
   font "SignikaNegativeSC-Bold.ttf"
   font "SignikaNegativeSC-Light.ttf"
