6da25fd74498a7054bc0a7268859b386e7d796cc
Bevan Kay <email@bevankay.me>
2022-11-14 11:24:27 +1100

    font-anonymous-pro: add livecheck (#6605)
    * Update font-anonymous-pro from 1.002 to 1.002
    
    * Update font-anonymous-pro.rb

--- Casks/font-anonymous-pro.rb
+++ Casks/font-anonymous-pro.rb
@@ -4,7 +4,16 @@ cask "font-anonymous-pro" do
 
   url "https://www.marksimonson.com/assets/content/fonts/AnonymousPro-#{version.dots_to_underscores}.zip"
   name "Anonymous Pro"
+  desc "Programming font"
   homepage "https://www.marksimonson.com/fonts/view/anonymous-pro"
+
+  livecheck do
+    url :homepage
+    regex(/href=.*?AnonymousPro[._-]v?(\d+(?:[._]\d+)+)\.zip/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match.first.tr("_", ".") }
+    end
+  end
 
   font "AnonymousPro-#{version}.001/Anonymous Pro B.ttf"
   font "AnonymousPro-#{version}.001/Anonymous Pro BI.ttf"
