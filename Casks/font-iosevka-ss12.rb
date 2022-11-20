20b7c819447d1e4ba471ebd972f349cd0f1729bf
Ryan <25192197+singularitti@users.noreply.github.com>
2022-11-20 18:23:44 -0500

    Update font-iosevka-ss12 from 16.3.6 to 16.4.0 (#6633)

--- Casks/font-iosevka-ss12.rb
+++ Casks/font-iosevka-ss12.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss12" do
-  version "16.3.6"
-  sha256 "58860c63c03775c6189abeba2b78f478454bcd2b7efda835daf8530ff05cea30"
+  version "16.4.0"
+  sha256 "9336b8d30654aeb240550f8723d41e09a63895d33e4bff980f069a2210a5c583"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss12-#{version}.zip"
   name "Iosevka SS12"
