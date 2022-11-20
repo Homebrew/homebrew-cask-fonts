8fca50e853574fcae23ae0d79982fbd57ea5bd9a
singularitti <singularitti@outlook.com>
2022-11-19 23:37:10 -0500

    font-ark-pixel-10px-proportional 2022.11.20
    Update font-ark-pixel-10px-proportional from 2022.11.11 to 2022.11.20
    
    Closes #6610.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-ark-pixel-10px-proportional.rb
+++ Casks/font-ark-pixel-10px-proportional.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-10px-proportional" do
-  version "2022.11.11"
-  sha256 "a88e7bc8256fa68d57771fe8fe37bab15d1ef3220230336e3de257d7a2b39ab1"
+  version "2022.11.20"
+  sha256 "23dc5e988aa2e0be3cf5f404ddf8f82f04e718ce14b58da5767ba2aea810b1f6"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-proportional-otf-v#{version}.zip"
   name "Ark Pixel 10px proportional"
