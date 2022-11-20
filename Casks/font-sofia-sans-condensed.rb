b6808061f5693fd34ce147f79e6d7278130ff142
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-11-20 00:06:31 +0000

    Update Google Fonts

---
+++ Casks/font-sofia-sans-condensed.rb
@@ -0,0 +1,14 @@
+cask "font-sofia-sans-condensed" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
+      branch:    "main",
+      only_path: "ofl/sofiasanscondensed"
+  name "Sofia Sans Condensed"
+  homepage "https://fonts.google.com/specimen/Sofia+Sans+Condensed"
+
+  font "SofiaSansCondensed-Italic[wght].ttf"
+  font "SofiaSansCondensed[wght].ttf"
+end
