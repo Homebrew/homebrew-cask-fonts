a068a2d64f2582968fe808900426d1c782fcb4de
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-11-20 00:06:43 +0000

    Update Google Fonts

--- Casks/font-alexandria.rb
+++ Casks/font-alexandria.rb
@@ -2,10 +2,11 @@ cask "font-alexandria" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/alexandria/Alexandria%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/alexandria/Alexandria%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Alexandria"
   desc "9 weights font family made in matching to the latin type Montserrat"
-  homepage "https://github.com/Gue3bara/Alexandria"
+  homepage "https://fonts.google.com/specimen/Alexandria"
 
   font "Alexandria[wght].ttf"
 end
