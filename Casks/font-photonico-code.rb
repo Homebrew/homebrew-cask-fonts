1552254fcf499c34a6b3a9ecf2a0d1c34bfb3a55
Ryan <25192197+singularitti@users.noreply.github.com>
2022-11-21 16:03:59 -0500

    Add Font Photonico Code (#6640)
    * Add font "font-photonico-code"
    
    * Bump font-photonico-code to version 1.3
    
    * Update font-photonico-code.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/font-photonico-code.rb
@@ -0,0 +1,11 @@
+cask "font-photonico-code" do
+  version "1.3"
+  sha256 "ddc539488f20a5a71ee6384b5fb227c4b746a4d0066954ff569b6005d767cbee"
+
+  url "https://github.com/Photonico/Photonico_Code/releases/download/#{version}/Photonico.#{version}.Regular.ttf"
+  name "Photonico Code"
+  desc "Free, monospace, slab-serif style font for coding"
+  homepage "https://github.com/Photonico/Photonico_Code"
+
+  font "Photonico.#{version}.Regular.ttf"
+end
