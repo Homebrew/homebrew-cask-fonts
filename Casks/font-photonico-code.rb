f924d192f75e7b0f5597c4d554949f1c9d50e9c9
singularitti <singularitti@outlook.com>
2022-11-21 15:22:20 -0500

    Bump font-photonico-code to version 1.3

--- Casks/font-photonico-code.rb
+++ Casks/font-photonico-code.rb
@@ -1,6 +1,6 @@
 cask "font-photonico-code" do
-  version "1.2"
-  sha256 "c4198812858dd634bc9df85aff240f5111586a911da336c0cd9b896282c21172"
+  version "1.3"
+  sha256 "ddc539488f20a5a71ee6384b5fb227c4b746a4d0066954ff569b6005d767cbee"
 
   url "https://github.com/Photonico/Photonico_Code/releases/download/#{version}/Photonico.#{version}.Regular.ttf"
   name "Photonico Code"
