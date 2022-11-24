421039d9292c5794fca2c1c255fbcc06537d171e
Benjamin Reich <dev@benjaminreich.de>
2022-11-24 11:42:05 +0100

    font-droid-sans-mono-nerd-font 2.2.1
    * Update font-droid-sans-mono-nerd-font from 2.1.0 to 2.2.1
    * Update font-droid-sans-mono-nerd-font.rb
    * Update font-droid-sans-mono-nerd-font.rb
    
    Closes #6647.
    
    Co-authored-by: Markus Reiter <me@reitermark.us>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-droid-sans-mono-nerd-font.rb
+++ Casks/font-droid-sans-mono-nerd-font.rb
@@ -1,11 +1,16 @@
 cask "font-droid-sans-mono-nerd-font" do
-  version "2.1.0"
-  sha256 "ffdcbbed0f95d2f4030fdc35297ed94f8e1da4e9313cd510cd900b452c5beca0"
+  version "2.2.1"
+  sha256 "14e744ab94048937c1121556ba5af42b30b08560a383d9af9ec4d19c34b457d4"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DroidSansMono.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "DroidSansMono Nerd Font (Droid Sans Mono)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   font "Droid Sans Mono Nerd Font Complete.otf"
   font "Droid Sans Mono Nerd Font Complete Mono.otf"
