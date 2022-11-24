e48ed364614239e7ec4a29dd734afa132eab2a8d
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-25 07:53:23 +0800

    Update font-cousine-nerd-font.rb

--- Casks/font-cousine-nerd-font.rb
+++ Casks/font-cousine-nerd-font.rb
@@ -4,7 +4,13 @@ cask "font-cousine-nerd-font" do
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Cousine.zip"
   name "Cousine Nerd Font (Cousine)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   font "Cousine Bold Nerd Font Complete.ttf"
   font "Cousine Bold Nerd Font Complete Mono.ttf"
