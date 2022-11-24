af7de59cca8648745293fa99c22befea96a94095
Benjamin Reich <dev@benjaminreich.de>
2022-11-24 11:43:06 +0100

    font-fira-mono-nerd-font 2.2.1
    * Update font-fira-mono-nerd-font from 2.1.0 to 2.2.1
    * Update font-fira-mono-nerd-font.rb
    * Update font-fira-mono-nerd-font.rb
    
    Closes #6649.
    
    Co-authored-by: Markus Reiter <me@reitermark.us>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-fira-mono-nerd-font.rb
+++ Casks/font-fira-mono-nerd-font.rb
@@ -1,11 +1,16 @@
 cask "font-fira-mono-nerd-font" do
-  version "2.1.0"
-  sha256 "f4e966bddbbd85826c72b5d6dfcf3c2857095f2e2819784b5babc2a95a544d38"
+  version "2.2.1"
+  sha256 "e60a9f8cd097c2cbaa54dc08dc861d6d02809cfc0d35d60410648cefb1855c23"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FiraMono.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "FiraMono Nerd Font (Fira)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   font "Fira Mono Bold Nerd Font Complete.otf"
   font "Fira Mono Medium Nerd Font Complete.otf"
