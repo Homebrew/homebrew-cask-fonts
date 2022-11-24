7c802cff806130f3e5b508ad61ca70849176fb8f
Benjamin Reich <dev@benjaminreich.de>
2022-11-24 11:42:39 +0100

    font-fantasque-sans-mono-nerd-font 2.2.1
    * Update font-fantasque-sans-mono-nerd-font from 2.1.0 to 2.2.1
    * Update font-fantasque-sans-mono-nerd-font.rb
    * Update font-fantasque-sans-mono-nerd-font.rb
    
    Closes #6648.
    
    Co-authored-by: Markus Reiter <me@reitermark.us>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-fantasque-sans-mono-nerd-font.rb
+++ Casks/font-fantasque-sans-mono-nerd-font.rb
@@ -1,11 +1,16 @@
 cask "font-fantasque-sans-mono-nerd-font" do
-  version "2.1.0"
-  sha256 "66bf67eb5031c1614e5f76b4e011860dd249a61350e5d921331c443b6609f090"
+  version "2.2.1"
+  sha256 "817965dba9610b92dfa3d1afd231d73b6e8c2b4dff349aea221dcb17b27d547b"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FantasqueSansMono.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "FantasqueSansMono Nerd Font (Fantasque Sans Mono)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   font "Fantasque Sans Mono Bold Nerd Font Complete Mono.ttf"
   font "Fantasque Sans Mono Bold Nerd Font Complete.ttf"
