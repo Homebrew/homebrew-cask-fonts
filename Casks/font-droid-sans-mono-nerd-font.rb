fed5048f3728cd657d01b9e36e2a40bfa6911e17
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-25 07:53:48 +0800

    Update font-droid-sans-mono-nerd-font.rb

--- Casks/font-droid-sans-mono-nerd-font.rb
+++ Casks/font-droid-sans-mono-nerd-font.rb
@@ -4,7 +4,13 @@ cask "font-droid-sans-mono-nerd-font" do
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DroidSansMono.zip"
   name "DroidSansMono Nerd Font (Droid Sans Mono)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   font "Droid Sans Mono Nerd Font Complete.otf"
   font "Droid Sans Mono Nerd Font Complete Mono.otf"
