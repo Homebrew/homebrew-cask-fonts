b15d56778fe0d90bac5e852da4d3e1247e9de6c7
Benjamin Reich <dev@benjaminreich.de>
2022-11-24 11:41:14 +0100

    font-blex-mono-nerd-font 2.2.1
    * Update font-blex-mono-nerd-font from 2.1.0 to 2.2.1
    * Update font-blex-mono-nerd-font.rb
    * Update font-blex-mono-nerd-font.rb
    
    Closes #6645.
    
    Co-authored-by: Markus Reiter <me@reitermark.us>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-blex-mono-nerd-font.rb
+++ Casks/font-blex-mono-nerd-font.rb
@@ -1,11 +1,16 @@
 cask "font-blex-mono-nerd-font" do
-  version "2.1.0"
-  sha256 "4fd7d9fd21cfcb7808548617628e3f2044e9eaa5871261767472e739ed9d6e76"
+  version "2.2.1"
+  sha256 "56f87ab8dfd3070105006794954fbeb5f2adcddba342231622b8f1eec6589101"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/IBMPlexMono.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "BlexMono Nerd Font (IBM Plex Mono)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/IBMPlexMono"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   font "Blex Mono Bold Italic Nerd Font Complete.ttf"
   font "Blex Mono Bold Nerd Font Complete.ttf"
