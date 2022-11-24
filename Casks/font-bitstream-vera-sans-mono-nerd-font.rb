cd5ef05ef03ac96bc276ca969b1ad1886f8fedcf
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-25 07:52:27 +0800

    Update font-bitstream-vera-sans-mono-nerd-font.rb

--- Casks/font-bitstream-vera-sans-mono-nerd-font.rb
+++ Casks/font-bitstream-vera-sans-mono-nerd-font.rb
@@ -4,7 +4,13 @@ cask "font-bitstream-vera-sans-mono-nerd-font" do
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BitstreamVeraSansMono.zip"
   name "BitstreamVeraSansMono Nerd Font (Bitstream Vera Sans Mono)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   font "Bitstream Vera Sans Mono Bold Nerd Font Complete.ttf"
   font "Bitstream Vera Sans Mono Bold Nerd Font Complete Mono.ttf"
