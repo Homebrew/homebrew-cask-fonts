8586186bdc1e6b1b6550ec129f7c54856ca94fdc
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-25 07:55:21 +0800

    Update font-gohufont-nerd-font.rb

--- Casks/font-gohufont-nerd-font.rb
+++ Casks/font-gohufont-nerd-font.rb
@@ -4,7 +4,13 @@ cask "font-gohufont-nerd-font" do
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Gohu.zip"
   name "GohuFont Nerd Font (Gohu)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   font "Gohu/11/complete/GohuFont Nerd Font Complete.ttf"
   font "Gohu/11/complete/GohuFont Nerd Font Complete Mono.ttf"
