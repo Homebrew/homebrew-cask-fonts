f15e28a4e2650df92ee7d3eedf44253d15c7bcca
Markus Reiter <me@reitermark.us>
2022-11-24 19:21:01 +0100

    Update font-bigblue-terminal-nerd-font.rb

--- Casks/font-bigblue-terminal-nerd-font.rb
+++ Casks/font-bigblue-terminal-nerd-font.rb
@@ -3,7 +3,6 @@ cask "font-bigblue-terminal-nerd-font" do
   sha256 "1c256bde692058c21c4801b3ca5293ef555cb51a451e06ae7f33d046f0fb6c2b"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/BigBlueTerminal.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "BigBlue_Terminal Nerd Font families (BigBlue Terminal)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
