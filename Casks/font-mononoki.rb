34be7b51e19ca8bac0103431a7356e63c41e284b
Matthias Tellen <1328396+madmalik@users.noreply.github.com>
2022-11-25 00:12:46 +0100

    font-mononoki: update sha256 (#6652)
    correct hash
    
    I updated the mononoki.zip for version 1.5 shorty after the release because there were some rendering issues, so the SHA hash changed. This is the correct hash of the re-released zip.

--- Casks/font-mononoki.rb
+++ Casks/font-mononoki.rb
@@ -1,6 +1,6 @@
 cask "font-mononoki" do
   version "1.5"
-  sha256 "c5bc01793b506bcbbfaefbe0a7e49021511d19077d35db934303ecf454fb3c46"
+  sha256 "2159f39e133802f48f5a13863e63a3c66bcd316ff112ca74c23d7428007f9e6b"
 
   url "https://github.com/madmalik/mononoki/releases/download/#{version}/mononoki.zip",
       verified: "github.com/madmalik/mononoki/"
