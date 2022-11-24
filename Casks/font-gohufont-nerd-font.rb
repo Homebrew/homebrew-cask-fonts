23fdbba1d18467b6f2147782724c85149c5919c6
Benjamin Reich <dev@benjaminreich.de>
2022-11-24 11:44:38 +0100

    font-gohufont-nerd-font 2.2.1
    * Update font-gohufont-nerd-font from 2.1.0 to 2.2.1
    * Update font-gohufont-nerd-font.rb
    * Update font-gohufont-nerd-font.rb
    
    Closes #6650.
    
    Co-authored-by: Markus Reiter <me@reitermark.us>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-gohufont-nerd-font.rb
+++ Casks/font-gohufont-nerd-font.rb
@@ -1,11 +1,16 @@
 cask "font-gohufont-nerd-font" do
-  version "2.1.0"
-  sha256 "41f4f0c6960fc9f5ec332150c8143e20511363afcc2aa293d1e1dbf464be93e6"
+  version "2.2.1"
+  sha256 "23f43a0b68c10818df40fc36269f969bc9f15a0d4cacd253e1d2dd9a4955dd37"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Gohu.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "GohuFont Nerd Font (Gohu)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   font "Gohu/11/complete/GohuFont Nerd Font Complete.ttf"
   font "Gohu/11/complete/GohuFont Nerd Font Complete Mono.ttf"
