b8d6ed976c091b0516b4f331656f36ab7950035b
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-25 07:54:25 +0800

    Update font-fantasque-sans-mono-nerd-font.rb

--- Casks/font-fantasque-sans-mono-nerd-font.rb
+++ Casks/font-fantasque-sans-mono-nerd-font.rb
@@ -4,7 +4,13 @@ cask "font-fantasque-sans-mono-nerd-font" do
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FantasqueSansMono.zip"
   name "FantasqueSansMono Nerd Font (Fantasque Sans Mono)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   font "Fantasque Sans Mono Bold Nerd Font Complete Mono.ttf"
   font "Fantasque Sans Mono Bold Nerd Font Complete.ttf"
