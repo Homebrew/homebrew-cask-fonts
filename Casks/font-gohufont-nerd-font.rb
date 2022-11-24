4a03dc02ba8f2e9afd298fcd8090f0312e91effb
Markus Reiter <me@reitermark.us>
2022-11-24 19:17:45 +0100

    Update font-gohufont-nerd-font.rb

--- Casks/font-gohufont-nerd-font.rb
+++ Casks/font-gohufont-nerd-font.rb
@@ -3,7 +3,6 @@ cask "font-gohufont-nerd-font" do
   sha256 "23f43a0b68c10818df40fc36269f969bc9f15a0d4cacd253e1d2dd9a4955dd37"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Gohu.zip"
-  appcast "https://github.com/ryanoasis/nerd-fonts/releases.atom"
   name "GohuFont Nerd Font (Gohu)"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
