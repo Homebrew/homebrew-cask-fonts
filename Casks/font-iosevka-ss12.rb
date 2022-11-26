9c6b7649753b75a2647cd8d710eb65241abfd34f
singularitti <singularitti@outlook.com>
2022-11-26 12:25:19 -0500

    Update font-iosevka-ss12 from 16.4.0 to 16.5.0

--- Casks/font-iosevka-ss12.rb
+++ Casks/font-iosevka-ss12.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss12" do
-  version "16.4.0"
-  sha256 "9336b8d30654aeb240550f8723d41e09a63895d33e4bff980f069a2210a5c583"
+  version "16.5.0"
+  sha256 "31bfc0049db8a691970d5c16d500bc423426dbb35239d71e4364037cb7e8f197"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss12-#{version}.zip"
   name "Iosevka SS12"
