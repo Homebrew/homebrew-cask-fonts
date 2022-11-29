56a0e7b847795874b005ee7cf8dbbefdbb0bd95a
Patrick Linnane <patrick@linnane.io>
2022-11-28 21:01:48 -0800

    Update font-fantasque-sans-mono-nerd-font from 2.2.1 to 2.2.2

--- Casks/font-fantasque-sans-mono-nerd-font.rb
+++ Casks/font-fantasque-sans-mono-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-fantasque-sans-mono-nerd-font" do
-  version "2.2.1"
-  sha256 "817965dba9610b92dfa3d1afd231d73b6e8c2b4dff349aea221dcb17b27d547b"
+  version "2.2.2"
+  sha256 "9c374c826b4735f43b628e1fe165b39d2d71bf728af668ec8e0b83f15217dce9"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/FantasqueSansMono.zip"
   name "FantasqueSansMono Nerd Font (Fantasque Sans Mono)"
