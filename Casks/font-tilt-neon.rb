c1eeb9f22f33fb749a726493fadc69795ac41bb3
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-12-04 00:06:36 +0000

    Update Google Fonts

---
+++ Casks/font-tilt-neon.rb
@@ -0,0 +1,11 @@
+cask "font-tilt-neon" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/tiltneon/TiltNeon%5BXROT%2CYROT%5D.ttf",
+      verified: "github.com/google/fonts/"
+  name "Tilt Neon"
+  homepage "https://fonts.google.com/specimen/Tilt+Neon"
+
+  font "TiltNeon[XROT,YROT].ttf"
+end
