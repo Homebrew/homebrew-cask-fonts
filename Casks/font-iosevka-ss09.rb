0050f4e6d375900c601484bcc2ea092569bf45cf
singularitti <singularitti@outlook.com>
2022-12-03 22:14:46 -0500

    font-iosevka-ss09 16.6.0
    Update font-iosevka-ss09 from 16.5.0 to 16.6.0
    
    Closes #6713.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-iosevka-ss09.rb
+++ Casks/font-iosevka-ss09.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss09" do
-  version "16.5.0"
-  sha256 "69e7dafdeff48c7f93d45a571f044da183741b4864b91e7a258bb1220adadfcc"
+  version "16.6.0"
+  sha256 "211bd207e536c2185d753289acbb68915ad50652368305b2441e30adeb3c45d5"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss09-#{version}.zip"
   name "Iosevka SS09"
