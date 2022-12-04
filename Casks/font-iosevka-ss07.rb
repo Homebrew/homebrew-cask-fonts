b04db50011ef42f8090273a56540bdb7d1c6a7d2
singularitti <singularitti@outlook.com>
2022-12-03 22:14:17 -0500

    Update font-iosevka-ss07 from 16.5.0 to 16.6.0

--- Casks/font-iosevka-ss07.rb
+++ Casks/font-iosevka-ss07.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss07" do
-  version "16.5.0"
-  sha256 "d5940bcc1bb572e0c6ce69c92aa1b6546de20b93c81b4f3156a04692406d1f14"
+  version "16.6.0"
+  sha256 "1b398a33fe43ac84c8046bd53737001a37fc06afd462cc0885891adea0a2fd14"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss07-#{version}.zip"
   name "Iosevka SS07"
