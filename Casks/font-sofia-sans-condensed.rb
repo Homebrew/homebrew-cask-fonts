1b078aafbb042761682ce9d8d2b0694ba7494a2b
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-11 13:19:33 +0800

    Update font-sofia-sans-condensed.rb

--- Casks/font-sofia-sans-condensed.rb
+++ Casks/font-sofia-sans-condensed.rb
@@ -7,7 +7,7 @@ cask "font-sofia-sans-condensed" do
       branch:    "main",
       only_path: "ofl/sofiasanscondensed"
   name "Sofia Sans Condensed"
-  homepage "https://fonts.google.com/specimen/Sofia+Sans+Condensed"
+  homepage "https://www.dfonts.org/fonts/sofia-pro-condensed-font-family/"
 
   font "SofiaSansCondensed-Italic[wght].ttf"
   font "SofiaSansCondensed[wght].ttf"
