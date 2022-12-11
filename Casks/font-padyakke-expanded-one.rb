6df5973ffbbbd3c0dbcfc0aa5f7624c9430423a8
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-11 13:32:51 +0800

    Update font-padyakke-expanded-one.rb

--- Casks/font-padyakke-expanded-one.rb
+++ Casks/font-padyakke-expanded-one.rb
@@ -2,8 +2,7 @@ cask "font-padyakke-expanded-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/padyakkeexpandedone/PadyakkeExpandedOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/padyakkeexpandedone/PadyakkeExpandedOne-Regular.ttf"
   name "Padyakke Expanded One"
   homepage "https://github.com/DunwichType/Padyakke_Libre"
 
