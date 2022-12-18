62d7132a661127ab33fef8f2c591ae55d5ff7a4a
BrewTestBot <BrewTestBot@users.noreply.github.com>
2022-12-18 00:07:52 +0000

    Update Google Fonts

--- Casks/font-biz-udmincho.rb
+++ Casks/font-biz-udmincho.rb
@@ -2,10 +2,13 @@ cask "font-biz-udmincho" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/bizudmincho/BIZUDMincho-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
+      branch:    "main",
+      only_path: "ofl/bizudmincho"
   name "BIZ UDMincho"
   homepage "https://fonts.google.com/specimen/BIZ+UDMincho"
 
+  font "BIZUDMincho-Bold.ttf"
   font "BIZUDMincho-Regular.ttf"
 end
