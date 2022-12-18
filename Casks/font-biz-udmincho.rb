849256a70954830a367e51bb78f1b528b5e8858e
BrewTestBot <ops@brew.sh>
2022-12-18 05:27:09 +0000

    Automatic update of Google Fonts (#6784)
    * Update Google Fonts
    
    * Update font-biz-udmincho.rb
    
    * Update font-biz-udpmincho.rb
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-biz-udmincho.rb
+++ Casks/font-biz-udmincho.rb
@@ -2,10 +2,14 @@ cask "font-biz-udmincho" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/bizudmincho/BIZUDMincho-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
+      branch:    "main",
+      only_path: "ofl/bizudmincho"
   name "BIZ UDMincho"
+  desc "Universal design typeface in the Japanese Mincho type tradition"
   homepage "https://fonts.google.com/specimen/BIZ+UDMincho"
 
+  font "BIZUDMincho-Bold.ttf"
   font "BIZUDMincho-Regular.ttf"
 end
--- Casks/font-biz-udpmincho.rb
+++ Casks/font-biz-udpmincho.rb
@@ -2,10 +2,14 @@ cask "font-biz-udpmincho" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/bizudpmincho/BIZUDPMincho-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
+      branch:    "main",
+      only_path: "ofl/bizudpmincho"
   name "BIZ UDPMincho"
+  desc "Universal design typeface in the Japanese Mincho type tradition"
   homepage "https://fonts.google.com/specimen/BIZ+UDPMincho"
 
+  font "BIZUDPMincho-Bold.ttf"
   font "BIZUDPMincho-Regular.ttf"
 end
