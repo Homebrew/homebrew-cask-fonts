4c93e3b907f506946687d9f210f412e50299abf6
Licardo <1014660822@qq.com>
2022-12-18 19:11:20 +0800

    Update font-smiley-sans.rb

--- Casks/font-smiley-sans.rb
+++ Casks/font-smiley-sans.rb
@@ -1,11 +1,11 @@
 cask "font-smiley-sans" do
   version "1.0.0"
   sha256 "8293c9b9fd44c9ee6b3fab696862445059e4d98b341a148d4544f91d01363fc1"
-  
+
   url "https://github.com/atelier-anchor/smiley-sans/releases/download/v#{version}/smiley-sans-v#{version}.zip",
       verified: "github.com/atelier-anchor/smiley-sans/"
   name "Smiley Sans"
-  desc "A condensed and oblique Chinese typeface seeking a visual balance between the humanist and the geometric."
+  desc "Chinese typeface seeking a visual balance between the humanist and the geometric"
   homepage "https://atelier-anchor.com/typefaces/smiley-sans/"
 
   livecheck do
