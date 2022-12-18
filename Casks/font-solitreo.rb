38f5f1c0ba56da8af7109ebebf949468db165edf
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-18 13:15:21 +0800

    Update font-solitreo.rb

--- Casks/font-solitreo.rb
+++ Casks/font-solitreo.rb
@@ -6,7 +6,7 @@ cask "font-solitreo" do
       verified: "github.com/google/fonts/"
   name "Solitreo"
   desc "Relic of the past, as most writers of the language utilize roman characters"
-  homepage "https://fonts.google.com/specimen/Solitreo"
+  homepage "https://github.com/ladinoprojects/solitreo"
 
   font "Solitreo-Regular.ttf"
 end
