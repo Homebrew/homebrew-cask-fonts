dbb8a87baf6fd934c18e099dc265d1344876a9fd
BrewTestBot <ops@brew.sh>
2022-12-18 05:29:07 +0000

    Automatic addition of Google Fonts (#6783)
    * Update Google Fonts
    
    * Update font-solitreo.rb
    
    * Update font-solitreo.rb
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/font-solitreo.rb
@@ -0,0 +1,11 @@
+cask "font-solitreo" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/solitreo/Solitreo-Regular.ttf"
+  name "Solitreo"
+  desc "Relic of the past, as most writers of the language utilize roman characters"
+  homepage "https://github.com/ladinoprojects/solitreo"
+
+  font "Solitreo-Regular.ttf"
+end
