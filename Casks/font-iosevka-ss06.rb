9d5d05962372c6dad0967c696c6e76a20b837106
Patrick Linnane <patrick@linnane.io>
2022-12-19 14:10:49 -0800

    Update font-iosevka-ss06 from 16.8.2 to 16.8.3 (#6827)

--- Casks/font-iosevka-ss06.rb
+++ Casks/font-iosevka-ss06.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss06" do
-  version "16.8.2"
-  sha256 "4e49c8b9d3a5dfc894678e74262740e11102a578563c40ccd076b007263d5626"
+  version "16.8.3"
+  sha256 "63e2ecfe22686b7b7d75eed95f28d0fb3f7c19b99669adc0015e3ff05095a0eb"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss06-#{version}.zip"
   name "Iosevka SS06"
