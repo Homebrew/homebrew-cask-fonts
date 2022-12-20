dbc0e5481abfee537b55edf9d0cd06c4c7133295
Patrick Linnane <patrick@linnane.io>
2022-12-20 14:01:03 -0800

    Update font-academicons from 1.9.2 to 1.9.3 (#6842)

--- Casks/font-academicons.rb
+++ Casks/font-academicons.rb
@@ -1,6 +1,6 @@
 cask "font-academicons" do
-  version "1.9.2"
-  sha256 "6ca4bd211505da763c11e8d3abb43d2861c3399ce2c2f6482eb6a9a361eb5fc5"
+  version "1.9.3"
+  sha256 "452e8e5b221fd291eee2e2dc0e13c66241d9568c9ffef91ab0f9d94983982322"
 
   url "https://github.com/jpswalsh/academicons/archive/v#{version}.zip"
   name "Academicons"
