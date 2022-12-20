4896b706c835eb52af44a4d74f1852132e65dd5c
Patrick Linnane <patrick@linnane.io>
2022-12-20 12:58:24 -0800

    Update font-iosevka-slab from 16.8.3 to 16.8.4

--- Casks/font-iosevka-slab.rb
+++ Casks/font-iosevka-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-slab" do
-  version "16.8.3"
-  sha256 "98aeaff57054462f6ec9955bd2e0c3490c90dabfa2b9b3bfb2bbda8dec4e1280"
+  version "16.8.4"
+  sha256 "bc5b126bf3bc8f08f287ba5f8a0f46ee54bce682a110590a19f4bc6b544a5732"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-slab-#{version}.zip"
   name "Iosevka Slab"
