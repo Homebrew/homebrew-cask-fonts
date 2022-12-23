c26590a0703c3dae7b02893a21e9634a99897844
Patrick Linnane <patrick@linnane.io>
2022-12-23 10:12:08 -0800

    font-iosevka-ss06 17.0.0
    Update font-iosevka-ss06 from 16.8.4 to 17.0.0
    
    Closes #6876.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-iosevka-ss06.rb
+++ Casks/font-iosevka-ss06.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss06" do
-  version "16.8.4"
-  sha256 "7fb1c17bf6591d60361480bed6d0f7953b8415209289b89a076a454f29ce5d53"
+  version "17.0.0"
+  sha256 "80dd671cc95e39821b3ea97b432c6bdb1864ca3836d605087d97ee705ccf5629"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss06-#{version}.zip"
   name "Iosevka SS06"
