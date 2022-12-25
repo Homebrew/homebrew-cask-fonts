6289948a1d57732742903ec0a3365c4b08177ee5
singularitti <singularitti@outlook.com>
2022-12-25 18:20:10 -0500

    font-iosevka-ss11 17.0.1
    Update font-iosevka-ss11 from 17.0.0 to 17.0.1
    
    Closes #6908.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-iosevka-ss11.rb
+++ Casks/font-iosevka-ss11.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss11" do
-  version "17.0.0"
-  sha256 "52fac2442fc4f843a49537885cafde94696425b13770d08205b31972ef001de1"
+  version "17.0.1"
+  sha256 "1da5cd8edc5599c1271c6e73864c19b730821191628ab8a59f340d0caf15d25d"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss11-#{version}.zip"
   name "Iosevka SS11"
