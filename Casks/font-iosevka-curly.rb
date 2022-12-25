1c857946d7de5197d7f22436bff6f859c1632a28
singularitti <singularitti@outlook.com>
2022-12-25 18:16:49 -0500

    font-iosevka-curly 17.0.1
    Update font-iosevka-curly from 17.0.0 to 17.0.1
    
    Closes #6895.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-iosevka-curly.rb
+++ Casks/font-iosevka-curly.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly" do
-  version "17.0.0"
-  sha256 "561d7fa1fe90ec596d1c57e5af97ce3f1f993647f62a0bf76d91cfb6a219f193"
+  version "17.0.1"
+  sha256 "261bab39070949b45316c4f1d8bc4a5ce34e1bf5ca0b730d782fa71d2645fbbf"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-#{version}.zip"
   name "Iosevka Curly"
