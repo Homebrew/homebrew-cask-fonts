be477ab05a70fd78abbe0ae042536762c2ff4579
singularitti <singularitti@outlook.com>
2022-12-27 14:58:11 -0500

    font-iosevka-ss16 17.0.2
    Update font-iosevka-ss16 from 17.0.1 to 17.0.2
    
    Closes #6939.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-iosevka-ss16.rb
+++ Casks/font-iosevka-ss16.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss16" do
-  version "17.0.1"
-  sha256 "da287ad39755163d63eb71266745bcdd2a8963f6ac76a6611810ef9c5f810f88"
+  version "17.0.2"
+  sha256 "a9acdad38361ea7f1406ee4f9266e771d3355d06c8511bdb7d98ec7230721f21"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss16-#{version}.zip"
   name "Iosevka SS16"
