89873116edae2f0419e6c29df7ae82977fa9c4a8
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-12-31 23:53:17 +0900

    font-plemol-jp-hs 1.4.0
    Update font-plemol-jp-hs from 1.3.0 to 1.4.0
    
    Closes #6953.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/font-plemol-jp-hs.rb
+++ Casks/font-plemol-jp-hs.rb
@@ -1,6 +1,6 @@
 cask "font-plemol-jp-hs" do
-  version "1.3.0"
-  sha256 "09a8e9d8c82b88ec96ee20ad8742817cf465d60de086d3429e70e74aa41004e3"
+  version "1.4.0"
+  sha256 "980be30e550e466503a3552540e565702344e47515cd504b76e67a16a5980d9d"
 
   url "https://github.com/yuru7/PlemolJP/releases/download/v#{version}/PlemolJP_HS_v#{version}.zip"
   name "PlemolJP HS"
