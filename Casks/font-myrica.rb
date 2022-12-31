4be0afac5e8b71e52595f89db3cc4aaaa1bb16d0
Patrick Linnane <patrick@linnane.io>
2022-12-30 20:22:21 -0800

    font-myrica: adjust url (#6948)

--- Casks/font-myrica.rb
+++ Casks/font-myrica.rb
@@ -2,9 +2,10 @@ cask "font-myrica" do
   version "2.006.20150301"
   sha256 "a90eb9b79885f02ad9e0e752a0b979b699847be7de13dc3b6113658f006d12bd"
 
-  url "https://codeload.github.com/tomokuni/Myrica/tar.gz/#{version}",
-      verified: "codeload.github.com/tomokuni/Myrica/"
+  url "https://github.com/tomokuni/Myrica/archive/refs/tags/#{version}.tar.gz",
+      verified: "github.com/tomokuni/Myrica/"
   name "Myrica"
+  desc "Programming font"
   homepage "https://myrica.estable.jp/"
 
   font "Myrica-#{version}/Myrica.TTC"
