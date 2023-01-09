ea84fe986f222730527278fb6afa84c4809be411
Patrick Linnane <patrick@linnane.io>
2023-01-08 16:20:27 -0800

    Update font-smiley-sans from 1.1.0 to 1.1.1 (#6963)

--- Casks/font-smiley-sans.rb
+++ Casks/font-smiley-sans.rb
@@ -1,6 +1,6 @@
 cask "font-smiley-sans" do
-  version "1.1.0"
-  sha256 "5a828523a93db22cbe8b1f1daaa70a1eb1314d876ce985ac311a0fb220c2ced5"
+  version "1.1.1"
+  sha256 "17c30d067b90ef6fb259caaf66e4339e30d70d5f197485da8f305c040d41413f"
 
   url "https://github.com/atelier-anchor/smiley-sans/releases/download/v#{version}/smiley-sans-v#{version}.zip",
       verified: "github.com/atelier-anchor/smiley-sans/"
