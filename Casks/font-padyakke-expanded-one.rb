efa13bee73fddaf8c85af2fd3bfd486d2f11b22e
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-01-14 12:49:30 +0000

    Update Google Fonts

--- Casks/font-padyakke-expanded-one.rb
+++ Casks/font-padyakke-expanded-one.rb
@@ -2,9 +2,10 @@ cask "font-padyakke-expanded-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/padyakkeexpandedone/PadyakkeExpandedOne-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/padyakkeexpandedone/PadyakkeExpandedOne-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Padyakke Expanded One"
-  homepage "https://github.com/DunwichType/Padyakke_Libre"
+  homepage "https://fonts.google.com/specimen/Padyakke+Expanded+One"
 
   font "PadyakkeExpandedOne-Regular.ttf"
 end
