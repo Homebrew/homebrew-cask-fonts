664be47d4de0f545659020e15b6e33a6b77d76ed
Miccal Matthews <miccal.matthews@gmail.com>
2023-01-17 05:13:39 +0800

    Update font-aurulent-sans-mono-nerd-font.rb

--- Casks/font-aurulent-sans-mono-nerd-font.rb
+++ Casks/font-aurulent-sans-mono-nerd-font.rb
@@ -1,11 +1,19 @@
 cask "font-aurulent-sans-mono-nerd-font" do
-  version "2.3.0-RC"
-  sha256 "c5e814ae1a930b870b38312e6132a45da3d3330808df31ba11ed0a6aaf35382c"
+  version "2.2.2"
+  sha256 "884da5c5eb6f71cdab62772d5f6b9f1c673bdc9763882838fff5eead7207111a"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AurulentSansMono.zip"
   name "AurulentSansMono Nerd Font (Aurulent Sans Mono)"
+  desc "Developer targeted fonts with a high number of glyphs"
   homepage "https://github.com/ryanoasis/nerd-fonts"
 
-  font "AurulentSansMono-Regular Nerd Font Complete.otf"
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  font "AurulentSansMono-Regular Nerd Font Complete Mono Windows Compatible.otf"
   font "AurulentSansMono-Regular Nerd Font Complete Mono.otf"
+  font "AurulentSansMono-Regular Nerd Font Complete Windows Compatible.otf"
+  font "AurulentSansMono-Regular Nerd Font Complete.otf"
 end
