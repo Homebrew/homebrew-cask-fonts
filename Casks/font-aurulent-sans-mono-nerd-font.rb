472cb703175033224b66b04c9613e72ebc47553c
Bevan Kay <email@bevankay.me>
2023-01-16 21:45:40 +1100

    Update font-aurulent-sans-mono-nerd-font from 2.2.1 to 2.3.0-RC

--- Casks/font-aurulent-sans-mono-nerd-font.rb
+++ Casks/font-aurulent-sans-mono-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-aurulent-sans-mono-nerd-font" do
-  version "2.2.1"
-  sha256 "b107076e9783928722d794635f029f8a5b1c57b177946c1dc95b9402857bc8c3"
+  version "2.3.0-RC"
+  sha256 "c5e814ae1a930b870b38312e6132a45da3d3330808df31ba11ed0a6aaf35382c"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AurulentSansMono.zip"
   name "AurulentSansMono Nerd Font (Aurulent Sans Mono)"
