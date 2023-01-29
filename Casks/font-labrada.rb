871bcb5be440ef0c3f30bda088f4ebfc0c448cf2
Miccal Matthews <miccal.matthews@gmail.com>
2023-01-29 13:30:43 +0800

    Update font-labrada.rb

--- Casks/font-labrada.rb
+++ Casks/font-labrada.rb
@@ -2,8 +2,7 @@ cask "font-labrada" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+  url "https://github.com/google/fonts.git"
       branch:    "main",
       only_path: "ofl/labrada"
   name "Labrada"
