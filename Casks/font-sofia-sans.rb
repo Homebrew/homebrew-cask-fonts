821be3eb19927663fd90386983c4bb5dcc425ffc
Miccal Matthews <miccal.matthews@gmail.com>
2023-01-29 13:07:25 +0800

    Update font-sofia-sans.rb

--- Casks/font-sofia-sans.rb
+++ Casks/font-sofia-sans.rb
@@ -7,7 +7,7 @@ cask "font-sofia-sans" do
       branch:    "main",
       only_path: "ofl/sofiasans"
   name "Sofia Sans"
-  desc "Also a feature-rich opentype family with a large character set including small caps, several figure styles, arrows, numerals in circles among others"
+  desc "Feature-rich opentype family with a large character set"
   homepage "https://fonts.google.com/specimen/Sofia+Sans"
 
   font "SofiaSans-Italic[wght].ttf"
