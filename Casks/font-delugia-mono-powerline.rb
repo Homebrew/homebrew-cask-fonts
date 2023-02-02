da0b3eead00064215646645a76a682d42f525343
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-03 00:40:08 +0800

    Update font-delugia-mono-powerline.rb

--- Casks/font-delugia-mono-powerline.rb
+++ Casks/font-delugia-mono-powerline.rb
@@ -1,8 +1,8 @@
 cask "font-delugia-mono-powerline" do
   version "2111.01.2"
-  sha256 "2a307af256b635721eeb6cec67d2365f0834590dd476e0bd62026291b1724a47"
+  sha256 "af87f367b4d23cc0b16b12c41cb1639642282b932c6d3be0e342dbbf88fddb06"
 
-  url "https://github.com/adam7/delugia-code/releases/download/v#{version}/delugia-mono-complete.zip"
+  url "https://github.com/adam7/delugia-code/releases/download/v#{version}/delugia-mono-powerline.zip"
   name "Delugia Code"
   desc "Cascadia Code + Nerd Fonts, minor difference between Caskaydia Cove Nerd Font"
   homepage "https://github.com/adam7/delugia-code"
