a5db3a80f38fb6be5d8b84be62de53fe7ed5b072
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-05 08:59:48 +0800

    Update font-phudu.rb

--- Casks/font-phudu.rb
+++ Casks/font-phudu.rb
@@ -2,11 +2,9 @@ cask "font-phudu" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/phudu/Phudu%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/phudu/Phudu%5Bwght%5D.ttf"
   name "Phudu"
-  desc "Sans-serif display typeface inspired by vietnamese hand-lettering billboards in the old days, that supports a wide range of languages by duong tran"
-  homepage "https://fonts.google.com/specimen/Phudu"
+  homepage "https://github.com/duongtrtype/DTPhudu"
 
   font "Phudu[wght].ttf"
 end
