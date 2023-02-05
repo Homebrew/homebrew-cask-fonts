6afc288db3632db6db1322b661558940add387fa
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-05 08:53:29 +0800

    Update font-gloock.rb

--- Casks/font-gloock.rb
+++ Casks/font-gloock.rb
@@ -2,11 +2,10 @@ cask "font-gloock" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/gloock/Gloock-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/gloock/Gloock-Regular.ttf"
   name "Gloock"
-  desc "The smooth relationship between the thin and thick strokes"
-  homepage "https://fonts.google.com/specimen/Gloock"
+  desc "Smooth relationship between the thin and thick strokes"
+  homepage "https://github.com/duartp/gloock"
 
   font "Gloock-Regular.ttf"
 end
