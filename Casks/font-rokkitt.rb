b2a0748c428c9b2b959a6035bcb9cd579e2eae8e
BrewTestBot <ops@brew.sh>
2023-02-05 01:44:01 +0100

    Automatic update of Google Fonts (#7078)
    * Update Google Fonts
    
    * Update font-rokkitt.rb
    
    ---------
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-rokkitt.rb
+++ Casks/font-rokkitt.rb
@@ -2,10 +2,14 @@ cask "font-rokkitt" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/rokkitt/Rokkitt%5Bwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
+      branch:    "main",
+      only_path: "ofl/rokkitt"
   name "Rokkitt"
+  desc "Slab Serif font family"
   homepage "https://fonts.google.com/specimen/Rokkitt"
 
+  font "Rokkitt-Italic[wght].ttf"
   font "Rokkitt[wght].ttf"
 end
