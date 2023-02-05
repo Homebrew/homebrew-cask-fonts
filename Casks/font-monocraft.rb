a0ef8b16f7eb2d77b821e1908add1bb839acfdac
Idrees Hassan <idrees@idreesinc.com>
2023-02-05 13:59:48 -0500

    Update font-monocraft from 2.3 to 2.4

--- Casks/font-monocraft.rb
+++ Casks/font-monocraft.rb
@@ -1,6 +1,6 @@
 cask "font-monocraft" do
-  version "2.3"
-  sha256 "2135c71e2515a2dd929944fb72bb43b607d8d25b57760fe1e2a9b913799d0130"
+  version "2.4"
+  sha256 "4b88f9bf66d325b86e8d3dc1b7c75a34dd5819860ff3354f7fd5d78ee47ae3ea"
 
   url "https://github.com/IdreesInc/Monocraft/releases/download/v#{version}/Monocraft.ttf"
   name "Monocraft"
