6005ff3c64f8f7c8f5263d218e3bebc456c3dc4d
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-05 09:01:33 +0800

    Update font-shantell-sans.rb

--- Casks/font-shantell-sans.rb
+++ Casks/font-shantell-sans.rb
@@ -3,11 +3,10 @@ cask "font-shantell-sans" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/shantellsans"
   name "Shantell Sans"
-  homepage "https://fonts.google.com/specimen/Shantell+Sans"
+  homepage "https://github.com/arrowtype/shantell-sans"
 
   font "ShantellSans-Italic[BNCE,INFM,SPAC,wght].ttf"
   font "ShantellSans[BNCE,INFM,SPAC,wght].ttf"
