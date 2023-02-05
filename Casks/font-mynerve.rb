d38645e6b0ec0f1f16a017c36840e1644d270b3b
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-05 08:58:53 +0800

    Update font-mynerve.rb

--- Casks/font-mynerve.rb
+++ Casks/font-mynerve.rb
@@ -2,11 +2,9 @@ cask "font-mynerve" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/mynerve/Mynerve-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/mynerve/Mynerve-Regular.ttf"
   name "Mynerve"
-  desc "Handwriting typeface designed to annotate and comment on documents with a fresh style"
-  homepage "https://fonts.google.com/specimen/Mynerve"
+  homepage "https://github.com/carolinashort/MyNerve"
 
   font "Mynerve-Regular.ttf"
 end
