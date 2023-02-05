2741de65a66279ead55cef14844247eba14790b3
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-05 08:52:14 +0800

    Update font-gajraj-one.rb

--- Casks/font-gajraj-one.rb
+++ Casks/font-gajraj-one.rb
@@ -2,10 +2,9 @@ cask "font-gajraj-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/gajrajone/GajrajOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/gajrajone/GajrajOne-Regular.ttf"
   name "Gajraj One"
-  homepage "https://fonts.google.com/specimen/Gajraj+One"
+  homepage "https://github.com/xconsau/GajrajOne"
 
   font "GajrajOne-Regular.ttf"
 end
