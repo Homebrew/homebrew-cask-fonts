c6710700e1dcaef6c71d036282a2853f37fa03f3
Ryan <25192197+singularitti@users.noreply.github.com>
2023-02-06 19:15:57 -0500

    Update font-lxgw-bright from 4.156 to 4.158 (#7085)

--- Casks/font-lxgw-bright.rb
+++ Casks/font-lxgw-bright.rb
@@ -1,5 +1,5 @@
 cask "font-lxgw-bright" do
-  version "4.156"
+  version "4.158"
   sha256 "3d28cd3938b9dd4fa20f7cca7fdb50b60d2d07b6b8411319a4f554b2327abeac"
 
   url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBright.7z"
