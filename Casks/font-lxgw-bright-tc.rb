b02f2e9a5488953e7000f918eea3d4f29604be83
Ryan <25192197+singularitti@users.noreply.github.com>
2023-02-06 19:27:42 -0500

    Update font-lxgw-bright-tc from 4.156 to 4.158 (#7087)

--- Casks/font-lxgw-bright-tc.rb
+++ Casks/font-lxgw-bright-tc.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-bright-tc" do
-  version "4.156"
-  sha256 "af554b31ee82733a8c1e7754cde1fc0ef265ac8b11de4af652e6519311db6576"
+  version "4.158"
+  sha256 "2ee4678c6dd4eb2bf6bdd8cfe57de5629669c87b70101fd6a7fa407c9d09044a"
 
   url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBrightTC.7z"
   name "LXGW Bright TC"
