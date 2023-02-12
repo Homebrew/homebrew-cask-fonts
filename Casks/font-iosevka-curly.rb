b310db6979c8e02f030da108abfb003e86024768
singularitti <singularitti@outlook.com>
2023-02-12 16:36:43 -0500

    Update font-iosevka-curly from 18.0.0 to 19.0.0

--- Casks/font-iosevka-curly.rb
+++ Casks/font-iosevka-curly.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly" do
-  version "18.0.0"
-  sha256 "80cc464f49b9e552399133bcb838ce5bade12e3e82c539df56250cb81fe36364"
+  version "19.0.0"
+  sha256 "7485b72d03e3cb796d9c6cd728bbcc8585e9aab49b94a494f297427cf69f22ea"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-#{version}.zip"
   name "Iosevka Curly"
