945a09d876c4f3ea4f443a62472957c49ec2ed5b
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-02-12 00:06:23 +0000

    Update Google Fonts

--- Casks/font-pontano-sans.rb
+++ Casks/font-pontano-sans.rb
@@ -2,10 +2,10 @@ cask "font-pontano-sans" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/pontanosans/PontanoSans-Regular.ttf",
+  url "https://github.com/google/fonts/raw/main/ofl/pontanosans/PontanoSans%5Bwght%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Pontano Sans"
   homepage "https://fonts.google.com/specimen/Pontano+Sans"
 
-  font "PontanoSans-Regular.ttf"
+  font "PontanoSans[wght].ttf"
 end
