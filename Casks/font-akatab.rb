e5ee67c634f612b18d9c2b9b098b33740370fd6d
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-19 12:57:37 +0800

    Update font-akatab.rb

--- Casks/font-akatab.rb
+++ Casks/font-akatab.rb
@@ -3,12 +3,11 @@ cask "font-akatab" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/akatab"
   name "Akatab"
   desc "Unicode font for rendering tifinagh characters in the tamahaq, tamashek and tawallamat languages"
-  homepage "https://fonts.google.com/specimen/Akatab"
+  homepage "https://github.com/silnrsi/font-akatab"
 
   font "Akatab-Black.ttf"
   font "Akatab-Bold.ttf"
