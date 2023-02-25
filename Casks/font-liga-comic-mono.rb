0a5ddb3c15da34494a83af60fb61d5d29548e41e
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-25 14:41:22 +0800

    Update font-liga-comic-mono.rb

--- Casks/font-liga-comic-mono.rb
+++ Casks/font-liga-comic-mono.rb
@@ -2,8 +2,7 @@ cask "font-liga-comic-mono" do
   version :latest
   sha256 :no_check
 
-  url "https://codeload.github.com/wayou/comic-mono-font/zip/master",
-      verified: "codeload.github.com/wayou/comic-mono-font/"
+  url "https://codeload.github.com/wayou/comic-mono-font/zip/master"
   name "Liga Comic Mono"
   desc "Legible monospace font with programming ligatures"
   homepage "https://github.com/wayou/comic-mono-font"
