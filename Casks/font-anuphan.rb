9fbfa511b3d7a04747ef6ae5dd1a343f092576d2
BrewTestBot <ops@brew.sh>
2023-02-26 06:59:38 +0100

    Automatic addition of Google Fonts (#7155)
    * Update Google Fonts
    
    * Update font-anuphan.rb
    
    ---------
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/font-anuphan.rb
@@ -0,0 +1,11 @@
+cask "font-anuphan" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/anuphan/Anuphan%5Bwght%5D.ttf"
+  name "Anuphan"
+  desc "Not a modification of ibm plex sans thai"
+  homepage "https://github.com/cadsondemak/Anuphan"
+
+  font "Anuphan[wght].ttf"
+end
