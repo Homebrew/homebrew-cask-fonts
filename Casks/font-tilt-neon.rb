90687b205198662fb960b9d82e206380eabfb8d7
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-02-26 00:06:36 +0000

    Update Google Fonts

--- Casks/font-tilt-neon.rb
+++ Casks/font-tilt-neon.rb
@@ -5,7 +5,7 @@ cask "font-tilt-neon" do
   url "https://github.com/google/fonts/raw/main/ofl/tiltneon/TiltNeon%5BXROT%2CYROT%5D.ttf",
       verified: "github.com/google/fonts/"
   name "Tilt Neon"
-  homepage "https://math-practice.github.io/tilt-specimen/"
+  homepage "https://fonts.google.com/specimen/Tilt+Neon"
 
   font "TiltNeon[XROT,YROT].ttf"
 end
