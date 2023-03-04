070a7cf2ae82355f0c83d2c948b6f1984bb58576
singularitti <singularitti@outlook.com>
2023-03-04 16:40:18 -0500

    Update font-iosevka-ss11 from 19.0.1 to 20.0.0

--- Casks/font-iosevka-ss11.rb
+++ Casks/font-iosevka-ss11.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss11" do
-  version "19.0.1"
-  sha256 "f4077c62033ca5a2e02fbb2b0cfdb69e9e89e0bc4aa82f568aa7925db48ad1ff"
+  version "20.0.0"
+  sha256 "d11e901af3a48de55450d45a4068537b76e3753791d5a1bb8b027527c76ae6ec"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss11-#{version}.zip"
   name "Iosevka SS11"
