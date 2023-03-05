918926920b509d733e8ed892198ca7828fd89327
BrewTestBot <ops@brew.sh>
2023-03-05 03:10:29 +0100

    Automatic addition of Google Fonts (#7189)
    * Update Google Fonts
    
    * Update font-rubik-pixels.rb
    
    ---------
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/font-rubik-pixels.rb
@@ -0,0 +1,10 @@
+cask "font-rubik-pixels" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/rubikpixels/RubikPixels-Regular.ttf"
+  name "Rubik Pixels"
+  homepage "https://github.com/NaN-xyz/Rubik-Filtered"
+
+  font "RubikPixels-Regular.ttf"
+end
