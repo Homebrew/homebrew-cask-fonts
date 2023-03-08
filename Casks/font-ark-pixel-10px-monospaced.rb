4255efa23284d0050a155a512a9d02f46865b084
Qi Zhang <25192197+singularitti@users.noreply.github.com>
2023-03-08 16:20:59 -0500

    Update font-ark-pixel-10px-monospaced from 2022.12.20 to 2023.03.08 (#7192)

--- Casks/font-ark-pixel-10px-monospaced.rb
+++ Casks/font-ark-pixel-10px-monospaced.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-10px-monospaced" do
-  version "2022.12.20"
-  sha256 "a0ffe5ac07aff5455652c16ea7e33e9b18635e3f717104ca23fae1ede6e5fbea"
+  version "2023.03.08"
+  sha256 "f2d725fbbf97fd7ebdaf4dbcdf5f53bac978b13906fe2b591a2ada123dcbd346"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-monospaced-otf-v#{version}.zip"
   name "Ark Pixel 10px monospaced"
