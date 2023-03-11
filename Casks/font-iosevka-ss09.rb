e99779ff61356dc725b85829153b3cba732265c2
Qi Zhang <25192197+singularitti@users.noreply.github.com>
2023-03-11 05:09:12 -0500

    Update font-iosevka-ss09 from 20.0.0 to 21.0.0 (#7211)

--- Casks/font-iosevka-ss09.rb
+++ Casks/font-iosevka-ss09.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss09" do
-  version "20.0.0"
-  sha256 "20fa21d2b5eb936fcdefe8c8f9438976e589c1d07d470309bbfe787a6f76b2c0"
+  version "21.0.0"
+  sha256 "d241eedd51fefd8890e6fe6356e7b637d7709fc65dd416f9e6dcb4d577896184"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss09-#{version}.zip"
   name "Iosevka SS09"
