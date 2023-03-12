bf74f277a94541b284650134da6c6041558bebf4
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-12 11:33:58 +0800

    Update font-schibsted-grotesk.rb

--- Casks/font-schibsted-grotesk.rb
+++ Casks/font-schibsted-grotesk.rb
@@ -3,12 +3,11 @@ cask "font-schibsted-grotesk" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/schibstedgrotesk"
   name "Schibsted Grotesk"
   desc "Digital-first font family crafted for user interfaces"
-  homepage "https://fonts.google.com/specimen/Schibsted+Grotesk"
+  homepage "https://github.com/schibsted/schibsted-grotesk"
 
   font "SchibstedGrotesk-Italic[wght].ttf"
   font "SchibstedGrotesk[wght].ttf"
