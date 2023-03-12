d27e25e15cda3b0bf62ac796d3451bfd9cf26ad1
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-12 11:34:36 +0800

    Update font-sigmar.rb

--- Casks/font-sigmar.rb
+++ Casks/font-sigmar.rb
@@ -2,11 +2,10 @@ cask "font-sigmar" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/sigmar/Sigmar-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/sigmar/Sigmar-Regular.ttf"
   name "Sigmar"
   desc "Based upon various fonts found in mid twentieth century pulp magazine advertising"
-  homepage "https://fonts.google.com/specimen/Sigmar"
+  homepage "https://github.com/googlefonts/sigmarone"
 
   font "Sigmar-Regular.ttf"
 end
