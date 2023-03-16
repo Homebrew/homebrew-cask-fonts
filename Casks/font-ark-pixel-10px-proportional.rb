416db81d4a521847981d78da0680e05102103570
Qi Zhang <25192197+singularitti@users.noreply.github.com>
2023-03-16 17:21:40 -0400

    Update font-ark-pixel-10px-proportional from 2023.03.08 to 2023.03.16 (#7228)

--- Casks/font-ark-pixel-10px-proportional.rb
+++ Casks/font-ark-pixel-10px-proportional.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-10px-proportional" do
-  version "2023.03.08"
-  sha256 "5c7ece51c635ad8c82768a4cf9f75728564f4dd3e6345ca82f53abeee924e368"
+  version "2023.03.16"
+  sha256 "b9705428e1d7f5533c54e30970ca69cac4f1035bafc2325795d52c8ca1d3a6f5"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-proportional-otf-v#{version}.zip"
   name "Ark Pixel 10px proportional"
