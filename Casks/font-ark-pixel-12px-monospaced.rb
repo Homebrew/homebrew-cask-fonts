ec9a86abf020db6c9c3dcf1ad8a9ae271722f249
singularitti <singularitti@outlook.com>
2023-03-16 14:58:06 -0400

    Update font-ark-pixel-12px-monospaced from 2023.03.08 to 2023.03.16

--- Casks/font-ark-pixel-12px-monospaced.rb
+++ Casks/font-ark-pixel-12px-monospaced.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-12px-monospaced" do
-  version "2023.03.08"
-  sha256 "b7c7b56a7dab4d6cc0f9a54fc48bc516f5ab2a27c5793068d0d744e97c4200d2"
+  version "2023.03.16"
+  sha256 "9a0d4261f16470a5c49ff14c36e9d58bc7866b659ae8a9cfa5430ff375ccb34c"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-12px-monospaced-otf-v#{version}.zip"
   name "Ark Pixel 12px monospaced"
