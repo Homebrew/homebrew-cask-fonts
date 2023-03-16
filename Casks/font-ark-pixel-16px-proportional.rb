ef0a2265b2216538f2d8e3dbaa5a0147230559f7
singularitti <singularitti@outlook.com>
2023-03-16 14:58:44 -0400

    Update font-ark-pixel-16px-proportional from 2023.03.08 to 2023.03.16

--- Casks/font-ark-pixel-16px-proportional.rb
+++ Casks/font-ark-pixel-16px-proportional.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-16px-proportional" do
-  version "2023.03.08"
-  sha256 "ee75403314daf88a49d1de0a9f896c64dca24e8f9d8c4617e76f9a27c9a8941e"
+  version "2023.03.16"
+  sha256 "3afa9ca7538395b8db5da1c4fc1f18cec9c2a75286fd0baa251496fa54495496"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-16px-proportional-otf-v#{version}.zip"
   name "Ark Pixel 16px proportional"
