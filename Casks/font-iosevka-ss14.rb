c625636722aba919ecf61484050b2ce7c58c3758
singularitti <singularitti@outlook.com>
2023-03-18 14:08:18 -0400

    Update font-iosevka-ss14 from 21.0.0 to 21.1.0

--- Casks/font-iosevka-ss14.rb
+++ Casks/font-iosevka-ss14.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss14" do
-  version "21.0.0"
-  sha256 "4620f105f6b978fb1df47b0ff8706e6d51ba0e28d48a31821e6fe0234e2c0786"
+  version "21.1.0"
+  sha256 "62f42c01e8a9a97b4d331f28d44c2ac9c922dc6082faa33ebebe9aca2c8abfe9"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss14-#{version}.zip"
   name "Iosevka SS14"
