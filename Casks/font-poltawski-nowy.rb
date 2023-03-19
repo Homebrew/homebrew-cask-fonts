522ce552420fa56fb8623c7246e50a80e32408e2
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-19 13:58:42 +0800

    Update font-poltawski-nowy.rb

--- Casks/font-poltawski-nowy.rb
+++ Casks/font-poltawski-nowy.rb
@@ -3,11 +3,10 @@ cask "font-poltawski-nowy" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/poltawskinowy"
   name "Poltawski Nowy"
-  homepage "https://fonts.google.com/specimen/Poltawski+Nowy"
+  homepage "https://github.com/kosmynkab/Poltawski-Nowy"
 
   font "PoltawskiNowy-Italic[wght].ttf"
   font "PoltawskiNowy[wght].ttf"
