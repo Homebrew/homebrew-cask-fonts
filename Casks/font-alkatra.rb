5f02b928c6880729301884f8f307968d577a747a
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-03-26 00:06:34 +0000

    Update Google Fonts

--- Casks/font-alkatra.rb
+++ Casks/font-alkatra.rb
@@ -2,10 +2,11 @@ cask "font-alkatra" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/alkatra/Alkatra%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/alkatra/Alkatra%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Alkatra"
   desc "Display typeface family"
-  homepage "https://github.com/suman51284/Alkatra"
+  homepage "https://fonts.google.com/specimen/Alkatra"
 
   font "Alkatra[wght].ttf"
 end
