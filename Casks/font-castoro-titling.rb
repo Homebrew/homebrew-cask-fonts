2e8b314d1c8964fcd8b2df982846d5393b520410
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-26 09:48:10 +0800

    Update font-castoro-titling.rb

--- Casks/font-castoro-titling.rb
+++ Casks/font-castoro-titling.rb
@@ -2,11 +2,9 @@ cask "font-castoro-titling" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/castorotitling/CastoroTitling-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/castorotitling/CastoroTitling-Regular.ttf"
   name "Castoro Titling"
-  desc "All-caps font in which the uppercase of the castoro text typeface has been ‘re-hung’ on the proportions of roman inscriptional letters"
-  homepage "https://fonts.google.com/specimen/Castoro+Titling"
+  homepage "https://github.com/TiroTypeworks/Castoro"
 
   font "CastoroTitling-Regular.ttf"
 end
