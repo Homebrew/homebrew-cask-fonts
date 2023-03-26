2082c0119050139d3b2b1386e1e17a7a6da8c848
BrewTestBot <ops@brew.sh>
2023-03-26 03:47:22 +0200

    Automatic update of Google Fonts (#7295)
    Update Google Fonts
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>

--- Casks/font-delicious-handrawn.rb
+++ Casks/font-delicious-handrawn.rb
@@ -2,10 +2,11 @@ cask "font-delicious-handrawn" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/delicioushandrawn/DeliciousHandrawn-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/delicioushandrawn/DeliciousHandrawn-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Delicious Handrawn"
   desc "Font inspired by agung rohmat's handwriting"
-  homepage "https://github.com/alphArtype/Delicious-Handrawn"
+  homepage "https://fonts.google.com/specimen/Delicious+Handrawn"
 
   font "DeliciousHandrawn-Regular.ttf"
 end
