6fb21890034e06544ae7ee4e472cd3f7bc2edf59
Florian Weber <fnwbr@users.noreply.github.com>
2023-03-30 22:11:10 +0200

    Add undefined medium (#7309)
    * Create font-undefined-medium
    
    Adds the cask for undefined medium as per https://github.com/andirueckel/undefined-medium
    
    * Update sha256 hash
    
    * Rename font-undefined-medium to font-undefined-medium.rb
    
    * Update homepage
    
    * Update font-undefined-medium.rb
    
    * Update font-undefined-medium.rb
    
    * Fix path to font file

---
+++ Casks/font-undefined-medium.rb
@@ -0,0 +1,11 @@
+cask "font-undefined-medium" do
+  version "1.1"
+  sha256 "e8dfbd59e2a3305a227d55ef962d5b94793aa14f1923426c59568aabbbf77aa3"
+
+  url "https://github.com/andirueckel/undefined-medium/archive/refs/tags/v#{version}.zip"
+  name "undefined medium"
+  desc "Pixel grid-based monospace typeface"
+  homepage "https://github.com/andirueckel/undefined-medium"
+
+  font "undefined-medium-#{version}/fonts/otf/undefined-medium.otf"
+end
