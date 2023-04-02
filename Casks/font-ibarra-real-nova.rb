d2c84d5ddcb0ba06d2cafd7dd20d4666585bfeea
Issy Long <me@issyl0.co.uk>
2023-04-03 00:47:08 +0100

    font-ibarra-real-nova: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-ibarra-real-nova.rb
+++ Casks/font-ibarra-real-nova.rb
@@ -3,7 +3,7 @@ cask "font-ibarra-real-nova" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/ibarrarealnova"
   name "Ibarra Real Nova"
