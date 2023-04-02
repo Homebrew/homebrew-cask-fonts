fe3588a508ee9a28c127569394e690f6561efd41
Issy Long <me@issyl0.co.uk>
2023-04-03 00:50:30 +0100

    font-srisakdi: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-srisakdi.rb
+++ Casks/font-srisakdi.rb
@@ -3,7 +3,7 @@ cask "font-srisakdi" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/srisakdi"
   name "Srisakdi"
