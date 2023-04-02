aa49c37fabcb9eb1eb110150c19424bfe9468cd7
Issy Long <me@issyl0.co.uk>
2023-04-03 00:47:55 +0100

    font-koho: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-koho.rb
+++ Casks/font-koho.rb
@@ -3,7 +3,7 @@ cask "font-koho" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/koho"
   name "KoHo"
