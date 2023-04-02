0fb4aa93be6993b7e680d4922a1c5ea69eddf00d
Issy Long <me@issyl0.co.uk>
2023-04-03 00:48:07 +0100

    font-lobster-two: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-lobster-two.rb
+++ Casks/font-lobster-two.rb
@@ -3,7 +3,7 @@ cask "font-lobster-two" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/lobstertwo"
   name "Lobster Two"
