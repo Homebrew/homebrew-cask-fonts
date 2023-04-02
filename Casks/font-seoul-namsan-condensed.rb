60d57e20fa7027174ee22d3d75c4b8a1af610987
Issy Long <me@issyl0.co.uk>
2023-04-03 00:50:05 +0100

    font-seoul-namsan-condensed: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-seoul-namsan-condensed.rb
+++ Casks/font-seoul-namsan-condensed.rb
@@ -3,7 +3,7 @@ cask "font-seoul-namsan-condensed" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/seoulnamsancondensed"
   name "Seoul Namsan Condensed"
