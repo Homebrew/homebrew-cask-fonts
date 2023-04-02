a546a80bd90286882500e14376090b0d8ad8be23
Issy Long <me@issyl0.co.uk>
2023-04-03 00:46:25 +0100

    font-expletus-sans: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-expletus-sans.rb
+++ Casks/font-expletus-sans.rb
@@ -3,7 +3,7 @@ cask "font-expletus-sans" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/expletussans"
   name "Expletus Sans"
