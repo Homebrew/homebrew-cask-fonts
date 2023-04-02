39d88503fb4a5321c14a0f6778a07bd8aa8dae66
Issy Long <me@issyl0.co.uk>
2023-04-03 00:45:48 +0100

    font-chivo-mono: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-chivo-mono.rb
+++ Casks/font-chivo-mono.rb
@@ -3,7 +3,7 @@ cask "font-chivo-mono" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/chivomono"
   name "Chivo Mono"
