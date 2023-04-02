da77ad72eab20d111c85a2e4621b9d06da955c20
Issy Long <me@issyl0.co.uk>
2023-04-03 00:51:17 +0100

    font-zen-kaku-gothic-antique: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-zen-kaku-gothic-antique.rb
+++ Casks/font-zen-kaku-gothic-antique.rb
@@ -3,7 +3,7 @@ cask "font-zen-kaku-gothic-antique" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/zenkakugothicantique"
   name "Zen Kaku Gothic Antique"
