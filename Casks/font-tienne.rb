55041c65d500177c534fc33e8fced292e827baf6
Issy Long <me@issyl0.co.uk>
2023-04-03 00:50:49 +0100

    font-tienne: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-tienne.rb
+++ Casks/font-tienne.rb
@@ -3,7 +3,7 @@ cask "font-tienne" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/tienne"
   name "Tienne"
