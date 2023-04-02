1d53dee957ede86d4e0b927567f0e2691bbfadd8
Issy Long <me@issyl0.co.uk>
2023-04-03 00:45:08 +0100

    font-b612-mono: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-b612-mono.rb
+++ Casks/font-b612-mono.rb
@@ -3,7 +3,7 @@ cask "font-b612-mono" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/b612mono"
   name "B612 Mono"
