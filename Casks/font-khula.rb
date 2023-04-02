3b471d0ce3ccb2d1f5e84c888a98076cc8839d8b
Issy Long <me@issyl0.co.uk>
2023-04-03 00:47:49 +0100

    font-khula: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-khula.rb
+++ Casks/font-khula.rb
@@ -3,7 +3,7 @@ cask "font-khula" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/khula"
   name "Khula"
