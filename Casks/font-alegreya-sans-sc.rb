ae8403593bb3e1d388206dd26e9dc666b27d0033
Issy Long <me@issyl0.co.uk>
2023-04-03 00:44:34 +0100

    font-alegreya-sans-sc: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-alegreya-sans-sc.rb
+++ Casks/font-alegreya-sans-sc.rb
@@ -3,7 +3,7 @@ cask "font-alegreya-sans-sc" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/alegreyasanssc"
   name "Alegreya Sans SC"
