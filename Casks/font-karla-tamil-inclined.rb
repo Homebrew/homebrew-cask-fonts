81bf45d616164c0a3d24051e62f87165f75eb3d9
Issy Long <me@issyl0.co.uk>
2023-04-03 00:47:45 +0100

    font-karla-tamil-inclined: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-karla-tamil-inclined.rb
+++ Casks/font-karla-tamil-inclined.rb
@@ -3,7 +3,7 @@ cask "font-karla-tamil-inclined" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/karlatamilinclined"
   name "Karla Tamil Inclined"
