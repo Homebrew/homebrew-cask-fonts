b0a10387609867ad3fae43a62354434e099af9ce
Issy Long <me@issyl0.co.uk>
2023-04-03 00:46:10 +0100

    font-dm-mono: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-dm-mono.rb
+++ Casks/font-dm-mono.rb
@@ -3,7 +3,7 @@ cask "font-dm-mono" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/dmmono"
   name "DM Mono"
