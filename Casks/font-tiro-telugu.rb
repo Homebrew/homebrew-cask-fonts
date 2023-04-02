29e36ee13ebccee4a238acd84717ab9350caa60f
Issy Long <me@issyl0.co.uk>
2023-04-03 00:50:58 +0100

    font-tiro-telugu: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-tiro-telugu.rb
+++ Casks/font-tiro-telugu.rb
@@ -3,7 +3,7 @@ cask "font-tiro-telugu" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/tirotelugu"
   name "Tiro Telugu"
