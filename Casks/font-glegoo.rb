537ab73e1252497483c9b2a5aac28f939627d236
Issy Long <me@issyl0.co.uk>
2023-04-03 00:46:49 +0100

    font-glegoo: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-glegoo.rb
+++ Casks/font-glegoo.rb
@@ -3,7 +3,7 @@ cask "font-glegoo" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/glegoo"
   name "Glegoo"
