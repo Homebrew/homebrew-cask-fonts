ff1dab3e8fa634f8488bb4a1bb295bdb590a53ee
Issy Long <me@issyl0.co.uk>
2023-04-03 00:50:54 +0100

    font-tiro-devanagari-sanskrit: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-tiro-devanagari-sanskrit.rb
+++ Casks/font-tiro-devanagari-sanskrit.rb
@@ -3,7 +3,7 @@ cask "font-tiro-devanagari-sanskrit" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/tirodevanagarisanskrit"
   name "Tiro Devanagari Sanskrit"
