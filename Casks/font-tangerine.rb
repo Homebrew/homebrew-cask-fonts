3b5b302579547fd3efa2000a2a52bbe940a91e97
Issy Long <me@issyl0.co.uk>
2023-04-03 00:50:42 +0100

    font-tangerine: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-tangerine.rb
+++ Casks/font-tangerine.rb
@@ -3,7 +3,7 @@ cask "font-tangerine" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/tangerine"
   name "Tangerine"
