6662c77ce660b5add57d4b869e4b60ba1ca39290
Issy Long <me@issyl0.co.uk>
2023-04-03 00:47:06 +0100

    font-hind-madurai: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-hind-madurai.rb
+++ Casks/font-hind-madurai.rb
@@ -3,7 +3,7 @@ cask "font-hind-madurai" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/hindmadurai"
   name "Hind Madurai"
