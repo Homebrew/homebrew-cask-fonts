1ae0a0a4d900008b7d7112182454135e4c1d5efc
Issy Long <me@issyl0.co.uk>
2023-04-03 00:49:23 +0100

    font-pt-sans-caption: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-pt-sans-caption.rb
+++ Casks/font-pt-sans-caption.rb
@@ -3,7 +3,7 @@ cask "font-pt-sans-caption" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/ptsanscaption"
   name "PT Sans Caption"
