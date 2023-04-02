e7e97cfe949ac1084a5c0dede842de814db73fed
Issy Long <me@issyl0.co.uk>
2023-04-03 00:46:33 +0100

    font-fira-sans-condensed: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-fira-sans-condensed.rb
+++ Casks/font-fira-sans-condensed.rb
@@ -3,7 +3,7 @@ cask "font-fira-sans-condensed" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/firasanscondensed"
   name "Fira Sans Condensed"
