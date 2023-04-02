b502bf9586d2137ee88388409b15ac2a15a5a5e8
Issy Long <me@issyl0.co.uk>
2023-04-03 00:47:36 +0100

    font-kadwa: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-kadwa.rb
+++ Casks/font-kadwa.rb
@@ -3,7 +3,7 @@ cask "font-kadwa" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/kadwa"
   name "Kadwa"
