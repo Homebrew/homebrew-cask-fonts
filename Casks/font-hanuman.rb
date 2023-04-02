fa79f33cd4cb1bb4a7c435e66c5867a11291c1b9
Issy Long <me@issyl0.co.uk>
2023-04-03 00:47:01 +0100

    font-hanuman: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-hanuman.rb
+++ Casks/font-hanuman.rb
@@ -3,7 +3,7 @@ cask "font-hanuman" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/hanuman"
   name "Hanuman"
