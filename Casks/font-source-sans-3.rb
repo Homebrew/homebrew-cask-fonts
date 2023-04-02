858f8dbb7b804d8c159ceed46a28b95c450dfb43
Issy Long <me@issyl0.co.uk>
2023-04-03 00:50:24 +0100

    font-source-sans-3: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-source-sans-3.rb
+++ Casks/font-source-sans-3.rb
@@ -3,7 +3,7 @@ cask "font-source-sans-3" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/sourcesans3"
   name "Source Sans 3"
