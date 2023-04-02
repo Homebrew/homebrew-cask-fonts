cbff016f68d50ca3fddce3fc0003c80c47cfb20f
Issy Long <me@issyl0.co.uk>
2023-04-03 00:50:29 +0100

    font-spline-sans-mono: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-spline-sans-mono.rb
+++ Casks/font-spline-sans-mono.rb
@@ -3,7 +3,7 @@ cask "font-spline-sans-mono" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/splinesansmono"
   name "Spline Sans Mono"
