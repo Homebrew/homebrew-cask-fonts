e1949278cb14e8121e499f7caf21a60a80cea6dc
Issy Long <me@issyl0.co.uk>
2023-04-03 00:49:15 +0100

    font-playfair-display-sc: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-playfair-display-sc.rb
+++ Casks/font-playfair-display-sc.rb
@@ -3,7 +3,7 @@ cask "font-playfair-display-sc" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/playfairdisplaysc"
   name "Playfair Display SC"
