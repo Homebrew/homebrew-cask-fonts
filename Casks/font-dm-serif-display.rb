53180d6677a23a48f15286f662b8b9ee081092d0
Issy Long <me@issyl0.co.uk>
2023-04-03 00:46:12 +0100

    font-dm-serif-display: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-dm-serif-display.rb
+++ Casks/font-dm-serif-display.rb
@@ -3,7 +3,7 @@ cask "font-dm-serif-display" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/dmserifdisplay"
   name "DM Serif Display"
