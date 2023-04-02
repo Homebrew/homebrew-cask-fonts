80dcf2cc3d604e9829b6e425eb1d5545f0ea6e9e
Issy Long <me@issyl0.co.uk>
2023-04-03 00:49:27 +0100

    font-quantico: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-quantico.rb
+++ Casks/font-quantico.rb
@@ -3,7 +3,7 @@ cask "font-quantico" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/quantico"
   name "Quantico"
