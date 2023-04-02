9525e892fc9d263715bc68c16bdb1cb299de8b0b
Issy Long <me@issyl0.co.uk>
2023-04-03 00:45:59 +0100

    font-cormorant-upright: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-cormorant-upright.rb
+++ Casks/font-cormorant-upright.rb
@@ -3,7 +3,7 @@ cask "font-cormorant-upright" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/cormorantupright"
   name "Cormorant Upright"
