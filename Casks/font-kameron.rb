f54f4eedb00a44807b6c3b87a876d18a6b0f5972
Issy Long <me@issyl0.co.uk>
2023-04-03 00:47:41 +0100

    font-kameron: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-kameron.rb
+++ Casks/font-kameron.rb
@@ -3,7 +3,7 @@ cask "font-kameron" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/kameron"
   name "Kameron"
