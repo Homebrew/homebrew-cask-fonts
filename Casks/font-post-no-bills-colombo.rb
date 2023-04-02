a2c6a69ed31910a8c1bce60089531e9d7dfd1479
Issy Long <me@issyl0.co.uk>
2023-04-03 00:49:19 +0100

    font-post-no-bills-colombo: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-post-no-bills-colombo.rb
+++ Casks/font-post-no-bills-colombo.rb
@@ -3,7 +3,7 @@ cask "font-post-no-bills-colombo" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/postnobillscolombo"
   name "Post No Bills Colombo"
