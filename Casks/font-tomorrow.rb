dd9b9a7eb28c7e258a62687cb92a7a21cb35548f
Issy Long <me@issyl0.co.uk>
2023-04-03 00:51:00 +0100

    font-tomorrow: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-tomorrow.rb
+++ Casks/font-tomorrow.rb
@@ -3,7 +3,7 @@ cask "font-tomorrow" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/tomorrow"
   name "Tomorrow"
