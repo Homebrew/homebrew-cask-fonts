ea50be56c9d6f4043cb3c75bbff4fdaacf325e29
Issy Long <me@issyl0.co.uk>
2023-04-03 00:50:03 +0100

    font-seoul-hangang-condensed: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-seoul-hangang-condensed.rb
+++ Casks/font-seoul-hangang-condensed.rb
@@ -3,7 +3,7 @@ cask "font-seoul-hangang-condensed" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/seoulhangangcondensed"
   name "Seoul Hangang Condensed"
