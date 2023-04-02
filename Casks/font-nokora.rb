8ee2e7c2caefe9bf72b1a1eb8ba63e6ed1bc87fd
Issy Long <me@issyl0.co.uk>
2023-04-03 00:48:47 +0100

    font-nokora: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-nokora.rb
+++ Casks/font-nokora.rb
@@ -3,7 +3,7 @@ cask "font-nokora" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/nokora"
   name "Nokora"
