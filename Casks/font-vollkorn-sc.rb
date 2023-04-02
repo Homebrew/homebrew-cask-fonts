738a7f1272229072c8cfa69d38c8b3f9031588ea
Issy Long <me@issyl0.co.uk>
2023-04-03 00:51:12 +0100

    font-vollkorn-sc: Autofix adding `/` to the end of the verified URL stanza

--- Casks/font-vollkorn-sc.rb
+++ Casks/font-vollkorn-sc.rb
@@ -3,7 +3,7 @@ cask "font-vollkorn-sc" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/vollkornsc"
   name "Vollkorn SC"
