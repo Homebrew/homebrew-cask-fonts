dceb430385c026a719b61d28cd8507eca96aacaf
Issy Long <me@issyl0.co.uk>
2023-04-03 17:22:38 +0100

    font-exo-2: Add trailing "/" to verified URL stanza

--- Casks/font-exo-2.rb
+++ Casks/font-exo-2.rb
@@ -3,7 +3,7 @@ cask "font-exo-2" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/exo2"
   name "Exo 2"
