8c29da30649b5d920ae6fe3805a7c239de6ff0d9
Miccal Matthews <miccal.matthews@gmail.com>
2023-04-03 08:15:53 +0800

    Update font-braah-one.rb

--- Casks/font-braah-one.rb
+++ Casks/font-braah-one.rb
@@ -2,8 +2,7 @@ cask "font-braah-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/braahone/BraahOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/braahone/BraahOne-Regular.ttf"
   name "Braah One"
   desc "Bold and playful font"
   homepage "https://github.com/artandtype/Braah"
