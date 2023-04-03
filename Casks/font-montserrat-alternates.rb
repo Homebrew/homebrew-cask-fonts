eb0022bad648529e06d16267d9f3bacbc8566c28
Issy Long <me@issyl0.co.uk>
2023-04-03 17:38:34 +0100

    font-montserrat-alternates: Add trailing "/" to verified URL stanza

--- Casks/font-montserrat-alternates.rb
+++ Casks/font-montserrat-alternates.rb
@@ -3,7 +3,7 @@ cask "font-montserrat-alternates" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/montserratalternates"
   name "Montserrat Alternates"
