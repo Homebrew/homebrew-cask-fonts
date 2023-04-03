0283970515c6db1b3bb84c53c18fab075b8bddee
Issy Long <me@issyl0.co.uk>
2023-04-03 17:37:21 +0100

    font-im-fell-double-pica: Add trailing "/" to verified URL stanza

--- Casks/font-im-fell-double-pica.rb
+++ Casks/font-im-fell-double-pica.rb
@@ -3,7 +3,7 @@ cask "font-im-fell-double-pica" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/imfelldoublepica"
   name "IM Fell Double Pica"
