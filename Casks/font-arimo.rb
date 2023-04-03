8b9ce5b117d59b7395abf7c262bf4e9f041626ac
Issy Long <me@issyl0.co.uk>
2023-04-03 17:21:05 +0100

    font-arimo: Add trailing "/" to verified URL stanza

--- Casks/font-arimo.rb
+++ Casks/font-arimo.rb
@@ -3,7 +3,7 @@ cask "font-arimo" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "apache/arimo"
   name "Arimo"
