8c67fb3f84c9d7912d627ad9bd3e514cce767cbf
Issy Long <me@issyl0.co.uk>
2023-04-03 17:21:18 +0100

    font-averia-serif-libre: Add trailing "/" to verified URL stanza

--- Casks/font-averia-serif-libre.rb
+++ Casks/font-averia-serif-libre.rb
@@ -3,7 +3,7 @@ cask "font-averia-serif-libre" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/averiaseriflibre"
   name "Averia Serif Libre"
