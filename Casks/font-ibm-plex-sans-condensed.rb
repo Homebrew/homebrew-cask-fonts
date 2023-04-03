c12a044abf936be6952dfab6cafbe8ace56f56a3
Issy Long <me@issyl0.co.uk>
2023-04-03 17:37:12 +0100

    font-ibm-plex-sans-condensed: Add trailing "/" to verified URL stanza

--- Casks/font-ibm-plex-sans-condensed.rb
+++ Casks/font-ibm-plex-sans-condensed.rb
@@ -3,7 +3,7 @@ cask "font-ibm-plex-sans-condensed" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/ibmplexsanscondensed"
   name "IBM Plex Sans Condensed"
