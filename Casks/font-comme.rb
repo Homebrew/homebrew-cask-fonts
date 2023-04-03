e6b87f6c9c637f1240e8cecbafbd57a8d38d5fd0
BrewTestBot <ops@brew.sh>
2023-04-03 02:22:38 +0200

    Automatic addition of Google Fonts (#7334)
    * Update Google Fonts
    
    * font-braah-one: add desc and update homepage
    
    * font-comme: update homepage
    
    * Update font-braah-one.rb
    
    * Update font-comme.rb
    
    ---------
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/font-comme.rb
@@ -0,0 +1,11 @@
+cask "font-comme" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/comme/Comme%5Bwght%5D.ttf"
+  name "Comme"
+  desc "Variable, with a weight axis ranging from thin to black"
+  homepage "https://github.com/googlefonts/comme"
+
+  font "Comme[wght].ttf"
+end
