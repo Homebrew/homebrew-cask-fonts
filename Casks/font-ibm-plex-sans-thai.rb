c896fbe2deb6c7531eb24560833ce7dbb2da37a0
Issy Long <me@issyl0.co.uk>
2023-04-03 17:37:18 +0100

    font-ibm-plex-sans-thai: Add trailing "/" to verified URL stanza

--- Casks/font-ibm-plex-sans-thai.rb
+++ Casks/font-ibm-plex-sans-thai.rb
@@ -3,7 +3,7 @@ cask "font-ibm-plex-sans-thai" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/ibmplexsansthai"
   name "IBM Plex Sans Thai"
