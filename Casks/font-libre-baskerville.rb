1eb7c24ce05dc05426f7a1b4b527f747e7080b14
Issy Long <me@issyl0.co.uk>
2023-04-03 17:38:04 +0100

    font-libre-baskerville: Add trailing "/" to verified URL stanza

--- Casks/font-libre-baskerville.rb
+++ Casks/font-libre-baskerville.rb
@@ -3,7 +3,7 @@ cask "font-libre-baskerville" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/librebaskerville"
   name "Libre Baskerville"
