47c2102957e73bd262a9fe3c326c630b19afa6f2
Issy Long <me@issyl0.co.uk>
2023-04-03 17:38:43 +0100

    font-museomoderno: Add trailing "/" to verified URL stanza

--- Casks/font-museomoderno.rb
+++ Casks/font-museomoderno.rb
@@ -3,7 +3,7 @@ cask "font-museomoderno" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/museomoderno"
   name "MuseoModerno"
