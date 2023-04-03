cb1067d70058f9be5d0c0111e5a4eb252606900a
Issy Long <me@issyl0.co.uk>
2023-04-03 17:39:00 +0100

    font-nunito-sans: Add trailing "/" to verified URL stanza

--- Casks/font-nunito-sans.rb
+++ Casks/font-nunito-sans.rb
@@ -3,7 +3,7 @@ cask "font-nunito-sans" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/nunitosans"
   name "Nunito Sans"
