f242d69553c223adf54199f202625824803b880b
Issy Long <me@issyl0.co.uk>
2023-04-03 17:37:15 +0100

    font-ibm-plex-sans-jp: Add trailing "/" to verified URL stanza

--- Casks/font-ibm-plex-sans-jp.rb
+++ Casks/font-ibm-plex-sans-jp.rb
@@ -3,7 +3,7 @@ cask "font-ibm-plex-sans-jp" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/ibmplexsansjp"
   name "IBM Plex Sans JP"
