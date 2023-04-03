424eb6faf1fa20a93a9529aab14cb28b0ccad293
Issy Long <me@issyl0.co.uk>
2023-04-03 17:36:49 +0100

    font-gfs-neohellenic: Add trailing "/" to verified URL stanza

--- Casks/font-gfs-neohellenic.rb
+++ Casks/font-gfs-neohellenic.rb
@@ -3,7 +3,7 @@ cask "font-gfs-neohellenic" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/gfsneohellenic"
   name "GFS Neohellenic"
