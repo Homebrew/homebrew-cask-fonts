2624716c691cf2e09a6b000edd2893e85780cbbe
Issy Long <me@issyl0.co.uk>
2023-04-03 17:36:46 +0100

    font-gentium-book-plus: Add trailing "/" to verified URL stanza

--- Casks/font-gentium-book-plus.rb
+++ Casks/font-gentium-book-plus.rb
@@ -3,7 +3,7 @@ cask "font-gentium-book-plus" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/gentiumbookplus"
   name "Gentium Book Plus"
