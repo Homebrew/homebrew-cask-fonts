970a6f03d2b1aed0d8c8d4f119f6285c20863d31
Issy Long <me@issyl0.co.uk>
2023-04-03 17:37:05 +0100

    font-hind-kochi: Add trailing "/" to verified URL stanza

--- Casks/font-hind-kochi.rb
+++ Casks/font-hind-kochi.rb
@@ -3,7 +3,7 @@ cask "font-hind-kochi" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/hindkochi"
   name "Hind Kochi"
