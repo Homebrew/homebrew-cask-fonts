e13506c017d182047a7169b54d72d9a412995bbc
Issy Long <me@issyl0.co.uk>
2023-04-03 17:37:02 +0100

    font-harmattan: Add trailing "/" to verified URL stanza

--- Casks/font-harmattan.rb
+++ Casks/font-harmattan.rb
@@ -3,7 +3,7 @@ cask "font-harmattan" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/harmattan"
   name "Harmattan"
