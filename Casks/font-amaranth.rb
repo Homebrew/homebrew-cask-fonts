ffde6c45ba365cfba1daa89fb92dfda65916837d
Issy Long <me@issyl0.co.uk>
2023-04-03 17:20:55 +0100

    font-amaranth: Add trailing "/" to verified URL stanza

--- Casks/font-amaranth.rb
+++ Casks/font-amaranth.rb
@@ -3,7 +3,7 @@ cask "font-amaranth" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/amaranth"
   name "Amaranth"
