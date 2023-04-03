c592d453929da33578503a359f6e56cc485a0987
Issy Long <me@issyl0.co.uk>
2023-04-03 17:21:23 +0100

    font-barlow-semi-condensed: Add trailing "/" to verified URL stanza

--- Casks/font-barlow-semi-condensed.rb
+++ Casks/font-barlow-semi-condensed.rb
@@ -3,7 +3,7 @@ cask "font-barlow-semi-condensed" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/barlowsemicondensed"
   name "Barlow Semi Condensed"
