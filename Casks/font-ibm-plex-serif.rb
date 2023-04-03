a65a1576b1ec7914649f24303c02a73926a82c82
Issy Long <me@issyl0.co.uk>
2023-04-03 17:37:20 +0100

    font-ibm-plex-serif: Add trailing "/" to verified URL stanza

--- Casks/font-ibm-plex-serif.rb
+++ Casks/font-ibm-plex-serif.rb
@@ -3,7 +3,7 @@ cask "font-ibm-plex-serif" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/ibmplexserif"
   name "IBM Plex Serif"
