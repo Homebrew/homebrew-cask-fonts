b9a38b8399852cecdfd61a01dc03d756a04c07a4
Issy Long <me@issyl0.co.uk>
2023-04-03 17:37:31 +0100

    font-istok-web: Add trailing "/" to verified URL stanza

--- Casks/font-istok-web.rb
+++ Casks/font-istok-web.rb
@@ -3,7 +3,7 @@ cask "font-istok-web" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
+      verified:  "github.com/google/fonts/",
       branch:    "main",
       only_path: "ofl/istokweb"
   name "Istok Web"
