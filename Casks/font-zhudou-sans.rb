3bf25814f84c4a09825d4d064f705d13043921f5
Qi Zhang <25192197+singularitti@users.noreply.github.com>
2023-04-09 10:19:44 -0400

    font-zhudou-sans 2.000 (#7366)

--- Casks/font-zhudou-sans.rb
+++ Casks/font-zhudou-sans.rb
@@ -1,6 +1,6 @@
 cask "font-zhudou-sans" do
-  version "1.000"
-  sha256 "dae5ff79f8f1db78661a6ec0ae7c1732965c034a3c04bd95cd43baabd14996ca"
+  version "2.000"
+  sha256 "1a2718aa52c98d1ac7e18d60e0f1d61057b18e558e8196a3a770104855a6fc69"
 
   url "https://github.com/Buernia/Zhudou-Sans/releases/download/v#{version}/Zhudou.Sans.zip"
   name "Zhudou Sans"
@@ -8,11 +8,11 @@ cask "font-zhudou-sans" do
   desc "CJK symbols and punctuation derived from Noto Sans"
   homepage "https://github.com/Buernia/Zhudou-Sans"
 
-  font "Zhudou Sans Bold.ttf"
-  font "Zhudou Sans ExtraLight.ttf"
-  font "Zhudou Sans Heavy.ttf"
-  font "Zhudou Sans Light.ttf"
-  font "Zhudou Sans Medium.ttf"
-  font "Zhudou Sans Normal.ttf"
-  font "Zhudou Sans Regular.ttf"
+  font "otf/ZhudouSans-Bold.otf"
+  font "otf/ZhudouSans-ExtraLight.otf"
+  font "otf/ZhudouSans-Heavy.otf"
+  font "otf/ZhudouSans-Light.otf"
+  font "otf/ZhudouSans-Medium.otf"
+  font "otf/ZhudouSans-Normal.otf"
+  font "otf/ZhudouSans-Regular.otf"
 end
