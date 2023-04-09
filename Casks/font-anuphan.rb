dd4f707d120a237ee54476d43e49fd81cb3806b5
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-04-09 00:06:07 +0000

    Update Google Fonts

--- Casks/font-anuphan.rb
+++ Casks/font-anuphan.rb
@@ -2,10 +2,11 @@ cask "font-anuphan" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/anuphan/Anuphan%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/anuphan/Anuphan%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Anuphan"
   desc "Not a modification of ibm plex sans thai"
-  homepage "https://github.com/cadsondemak/Anuphan"
+  homepage "https://fonts.google.com/specimen/Anuphan"
 
   font "Anuphan[wght].ttf"
 end
--- Casks/font-schibsted-grotesk.rb
+++ Casks/font-schibsted-grotesk.rb
@@ -3,11 +3,12 @@ cask "font-schibsted-grotesk" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/schibstedgrotesk"
   name "Schibsted Grotesk"
   desc "Digital-first font family crafted for user interfaces"
-  homepage "https://github.com/schibsted/schibsted-grotesk"
+  homepage "https://fonts.google.com/specimen/Schibsted+Grotesk"
 
   font "SchibstedGrotesk-Italic[wght].ttf"
   font "SchibstedGrotesk[wght].ttf"
