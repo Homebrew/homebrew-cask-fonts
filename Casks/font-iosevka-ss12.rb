73ef83cfd27971049424195f2a0d097e0a570009
singularitti <singularitti@outlook.com>
2023-04-15 16:57:38 -0400

    font-iosevka-ss12 22.0.2

--- Casks/font-iosevka-ss12.rb
+++ Casks/font-iosevka-ss12.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss12" do
-  version "22.0.1"
-  sha256 "d2abf0768b5f87572218085714fb72ff14d79f09504d0b7dd2d7143c8dfbf662"
+  version "22.0.2"
+  sha256 "01bb8e0c265ede2e16bb755aabbbc576d91f9f2e0cd49aa77242b93b643859ed"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss12-#{version}.zip"
   name "Iosevka SS12"
