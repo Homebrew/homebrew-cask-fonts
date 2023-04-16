6015719b4f81d1fe8d9c1c99b1e127b35cb93f10
Markus Reiter <me@reitermark.us>
2023-04-16 03:11:24 +0200

    Update font-lugrasimo.rb

--- Casks/font-lugrasimo.rb
+++ Casks/font-lugrasimo.rb
@@ -5,7 +5,7 @@ cask "font-lugrasimo" do
   url "https://github.com/google/fonts/raw/main/ofl/lugrasimo/Lugrasimo-Regular.ttf",
       verified: "github.com/google/fonts/"
   name "Lugrasimo"
-  desc "Based on fondamento, a typeface in calligraphic lettering style based on the traditional foundational hand, a basic teaching style created by edward johnston in the early 20th century"
+  desc "Typeface in calligraphic lettering style"
   homepage "https://fonts.google.com/specimen/Lugrasimo"
 
   font "Lugrasimo-Regular.ttf"
