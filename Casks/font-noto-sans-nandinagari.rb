5633e18fface7eafab267c6e7dab172c8b8c7332
Markus Reiter <me@reitermark.us>
2023-04-16 03:11:47 +0200

    Update font-noto-sans-nandinagari.rb

--- Casks/font-noto-sans-nandinagari.rb
+++ Casks/font-noto-sans-nandinagari.rb
@@ -5,7 +5,7 @@ cask "font-noto-sans-nandinagari" do
   url "https://github.com/google/fonts/raw/main/ofl/notosansnandinagari/NotoSansNandinagari-Regular.ttf",
       verified: "github.com/google/fonts/"
   name "Noto Sans Nandinagari"
-  desc "Design for the historical indic nandinagari script"
+  desc "Historical indic nandinagari script"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Nandinagari"
 
   font "NotoSansNandinagari-Regular.ttf"
