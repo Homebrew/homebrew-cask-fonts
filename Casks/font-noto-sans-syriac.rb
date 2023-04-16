b96ebde83cb6227cc9d388f852d011bdb1e4e2a8
BrewTestBot <ops@brew.sh>
2023-04-16 03:13:38 +0200

    Automatic update of Google Fonts (#7395)

--- Casks/font-noto-sans-syriac.rb
+++ Casks/font-noto-sans-syriac.rb
@@ -2,14 +2,10 @@ cask "font-noto-sans-syriac" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
-      branch:    "main",
-      only_path: "ofl/notosanssyriac"
+  url "https://github.com/google/fonts/raw/main/ofl/notosanssyriac/NotoSansSyriac%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Noto Sans Syriac"
   homepage "https://fonts.google.com/specimen/Noto+Sans+Syriac"
 
-  font "NotoSansSyriac-Black.ttf"
-  font "NotoSansSyriac-Regular.ttf"
-  font "NotoSansSyriac-Thin.ttf"
+  font "NotoSansSyriac[wght].ttf"
 end
