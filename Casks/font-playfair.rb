d7fac0793de89f0fc49a5346676a55355da2699f
Miccal Matthews <miccal.matthews@gmail.com>
2023-04-17 12:42:02 +0800

    Update font-playfair.rb

--- Casks/font-playfair.rb
+++ Casks/font-playfair.rb
@@ -3,11 +3,10 @@ cask "font-playfair" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/playfair"
   name "Playfair"
-  homepage "https://fonts.google.com/specimen/Playfair"
+  homepage "https://github.com/googlefonts/Playfair"
 
   font "Playfair-Italic[opsz,wdth,wght].ttf"
   font "Playfair[opsz,wdth,wght].ttf"
