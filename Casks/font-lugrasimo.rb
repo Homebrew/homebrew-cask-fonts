6b7b956e3cc68d9943a08f35bacba072eb5ccb19
Miccal Matthews <miccal.matthews@gmail.com>
2023-04-17 12:40:30 +0800

    Update font-lugrasimo.rb

--- Casks/font-lugrasimo.rb
+++ Casks/font-lugrasimo.rb
@@ -2,11 +2,10 @@ cask "font-lugrasimo" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/lugrasimo/Lugrasimo-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/lugrasimo/Lugrasimo-Regular.ttf"
   name "Lugrasimo"
   desc "Typeface in calligraphic lettering style"
-  homepage "https://fonts.google.com/specimen/Lugrasimo"
+  homepage "https://github.com/docrepair-fonts/lugrasimo-fonts"
 
   font "Lugrasimo-Regular.ttf"
 end
