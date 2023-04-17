21837f68118f6d76d1e327996a4617a9773d64e4
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-17 18:03:41 -0400

    font-fontawesome 6.4.0 (#7402)

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,6 +1,6 @@
 cask "font-fontawesome" do
-  version "6.3.0"
-  sha256 "1a741751025b5495ad175553eb61f94ce57fbeaac7cd4df906f24014ea794f70"
+  version "6.4.0"
+  sha256 "128b850cef31dd9c21f42dc19b028a02302408659dc0350c5111ea126f6c00ae"
 
   url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip",
       verified: "github.com/FortAwesome/Font-Awesome/"
