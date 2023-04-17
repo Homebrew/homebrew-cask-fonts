32e95583f1e5592317e0bc35404db0e0327c6e6d
Hanlin He <hanlhe@cisco.com>
2023-04-16 20:05:20 -0700

    font-iosevka-slab 22.0.2

--- Casks/font-iosevka-slab.rb
+++ Casks/font-iosevka-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-slab" do
-  version "22.0.0"
-  sha256 "86eb1ea9ff4e4b859206150ff400be560e300d32baf042e9329e5239fc1284c2"
+  version "22.0.2"
+  sha256 "315cd755ab87559fc90a326f642cb1cb06c315cc164cc0230d47b39b0a376e87"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-slab-#{version}.zip"
   name "Iosevka Slab"
