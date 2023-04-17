fcaf22158ce3d70faf7b798494c23fbe6445f6f5
Miccal Matthews <miccal.matthews@gmail.com>
2023-04-17 12:43:07 +0800

    Update font-lumanosimo.rb

--- Casks/font-lumanosimo.rb
+++ Casks/font-lumanosimo.rb
@@ -2,11 +2,10 @@ cask "font-lumanosimo" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/lumanosimo/Lumanosimo-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/lumanosimo/Lumanosimo-Regular.ttf"
   name "Lumanosimo"
   desc "Expressive typeface"
-  homepage "https://fonts.google.com/specimen/Lumanosimo"
+  homepage "https://github.com/docrepair-fonts/lumanosimo-fonts"
 
   font "Lumanosimo-Regular.ttf"
 end
