3255b5f48f635fa6775a353c5bb02a662258d2e2
Miccal Matthews <miccal.matthews@gmail.com>
2023-04-17 12:41:23 +0800

    Update font-noto-sans-nandinagari.rb

--- Casks/font-noto-sans-nandinagari.rb
+++ Casks/font-noto-sans-nandinagari.rb
@@ -2,11 +2,10 @@ cask "font-noto-sans-nandinagari" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosansnandinagari/NotoSansNandinagari-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosansnandinagari/NotoSansNandinagari-Regular.ttf"
   name "Noto Sans Nandinagari"
   desc "Historical indic nandinagari script"
-  homepage "https://fonts.google.com/specimen/Noto+Sans+Nandinagari"
+  homepage "https://github.com/notofonts/nandinagari"
 
   font "NotoSansNandinagari-Regular.ttf"
 end
