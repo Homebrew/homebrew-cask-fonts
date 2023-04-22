4f347b4ba042219bde8a8b9c72c258163b6d1acc
Miccal Matthews <miccal.matthews@gmail.com>
2023-04-22 12:49:54 +0800

    Update and rename font-monocraft-nerd.rb to font-monocraft-nerd-font.rb

--- Casks/font-monocraft-nerd-font.rb
+++ Casks/font-monocraft-nerd-font.rb
@@ -1,10 +1,10 @@
-cask "font-monocraft-nerd" do
+cask "font-monocraft-nerd-font" do
   version "2.5"
   sha256 "431329f14c1c4635b248d1d6a0d797dac5fdb5e678fad0858fe0c6e356b3b17c"
 
   url "https://github.com/IdreesInc/Monocraft/releases/download/v#{version}/Monocraft-nerd-fonts-patched.ttf"
   name "Monocraft with Nerd glyphs"
-  desc "Monospaced programming font inspired by the Minecraft typeface patched to add Nerd glyphs"
+  desc "Monospaced font inspired by the Minecraft typeface patched to add Nerd glyphs"
   homepage "https://github.com/IdreesInc/Monocraft"
 
   font "Monocraft-nerd-fonts.ttf"
