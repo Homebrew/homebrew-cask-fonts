65afbc122c7e45c8a9bb16200ac7d97543ec24fa
BrewTestBot <ops@brew.sh>
2023-04-30 01:44:10 +0100

    Automatic addition of Google Fonts (#7461)
    * Update Google Fonts
    
    * Update font-geologica.rb
    
    * Update font-kablammo.rb
    
    ---------
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/font-geologica.rb
@@ -0,0 +1,10 @@
+cask "font-geologica" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/geologica/Geologica%5BCRSV%2CSHRP%2Cslnt%2Cwght%5D.ttf"
+  name "Geologica"
+  homepage "https://github.com/googlefonts/geologica"
+
+  font "Geologica[CRSV,SHRP,slnt,wght].ttf"
+end
---
+++ Casks/font-kablammo.rb
@@ -0,0 +1,10 @@
+cask "font-kablammo" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/kablammo/Kablammo%5BMORF%5D.ttf"
+  name "Kablammo"
+  homepage "https://github.com/Vectro-Type-Foundry/kablammo"
+
+  font "Kablammo[MORF].ttf"
+end
