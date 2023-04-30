8ca6306415a1b8854981e1f1a3bcea73f80e17ec
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-04-30 00:06:02 +0000

    Update Google Fonts

--- Casks/font-foldit.rb
+++ Casks/font-foldit.rb
@@ -2,10 +2,11 @@ cask "font-foldit" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/foldit/Foldit%5Bwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/foldit/Foldit%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Foldit"
   desc "Font which uses gradients to play with dimension and sense of space"
-  homepage "https://github.com/SophiaDesign/Foldit"
+  homepage "https://fonts.google.com/specimen/Foldit"
 
   font "Foldit[wght].ttf"
 end
