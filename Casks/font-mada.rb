dd5744b54b4728c9577201d962b42df157c7273d
BrewTestBot <ops@brew.sh>
2023-04-30 01:34:13 +0100

    Automatic update of Google Fonts (#7460)
    * Update Google Fonts
    
    * Update font_casker
    
    ---------
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-mada.rb
+++ Casks/font-mada.rb
@@ -2,18 +2,10 @@ cask "font-mada" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
-      branch:    "main",
-      only_path: "ofl/mada"
+  url "https://github.com/google/fonts/raw/main/ofl/mada/Mada%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Mada"
   homepage "https://fonts.google.com/specimen/Mada"
 
-  font "Mada-Black.ttf"
-  font "Mada-Bold.ttf"
-  font "Mada-ExtraLight.ttf"
-  font "Mada-Light.ttf"
-  font "Mada-Medium.ttf"
-  font "Mada-Regular.ttf"
-  font "Mada-SemiBold.ttf"
+  font "Mada[wght].ttf"
 end
--- Casks/font-monofett.rb
+++ Casks/font-monofett.rb
@@ -2,10 +2,10 @@ cask "font-monofett" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/monofett/Monofett.ttf",
+  url "https://github.com/google/fonts/raw/main/ofl/monofett/Monofett-Regular.ttf",
       verified: "github.com/google/fonts/"
   name "Monofett"
   homepage "https://fonts.google.com/specimen/Monofett"
 
-  font "Monofett.ttf"
+  font "Monofett-Regular.ttf"
 end
