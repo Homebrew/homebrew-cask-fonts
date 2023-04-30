2b52eaff4c6f2d7c1b9939d9081e4548c5a552fa
Miccal Matthews <miccal.matthews@gmail.com>
2023-04-30 08:25:01 +0800

    Update font-geologica.rb

--- Casks/font-geologica.rb
+++ Casks/font-geologica.rb
@@ -2,11 +2,9 @@ cask "font-geologica" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/geologica/Geologica%5BCRSV%2CSHRP%2Cslnt%2Cwght%5D.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/geologica/Geologica%5BCRSV%2CSHRP%2Cslnt%2Cwght%5D.ttf"
   name "Geologica"
-  desc "Grounded in the humanist genre, but leans assertively into geometric, constructed letterforms to find its stability"
-  homepage "https://fonts.google.com/specimen/Geologica"
+  homepage "https://github.com/googlefonts/geologica"
 
   font "Geologica[CRSV,SHRP,slnt,wght].ttf"
 end
