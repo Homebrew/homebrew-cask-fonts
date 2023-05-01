efd2f0c14cd79454e0efb45c17ba6b1165516178
Bevan Kay <email@bevankay.me>
2023-05-01 12:59:34 +1000

    font-daddy-time-mono-nerd-font 3.0.0

--- Casks/font-daddy-time-mono-nerd-font.rb
+++ Casks/font-daddy-time-mono-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-daddy-time-mono-nerd-font" do
-  version "2.3.3"
-  sha256 "8d050b0f96e218fd706fdea6ee773754f147b9120cd1bac057efe1dbc9a20563"
+  version "3.0.0"
+  sha256 "2d5df5e3e05454b65c81f40322fb7392b6f18e1f8e6f2f65380a5575e592da20"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/DaddyTimeMono.zip"
   name "DaddyTimeMono Nerd Font (DaddyTimeMono)"
