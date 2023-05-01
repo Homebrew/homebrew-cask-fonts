eb0585f9db44b4765175d102037fbebd68afb974
Bevan Kay <email@bevankay.me>
2023-05-01 13:04:44 +1000

    font-inconsolata-go-nerd-font 3.0.0

--- Casks/font-inconsolata-go-nerd-font.rb
+++ Casks/font-inconsolata-go-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-inconsolata-go-nerd-font" do
-  version "2.3.3"
-  sha256 "e54a328970bb060cf94b5ad5edd7bf7e844512bb12341adf467864a10d33e732"
+  version "3.0.0"
+  sha256 "e39771fe2e9fe279511e0369d3b64b1c700d9391c8cc084db17878b512d69c8e"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/InconsolataGo.zip"
   name "InconsolataGo Nerd Font (InconsolataGo)"
