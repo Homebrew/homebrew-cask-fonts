8259f7f76d7f229e5a074d380ddaf9d6378953b2
Bevan Kay <email@bevankay.me>
2023-05-01 13:15:35 +1000

    font-terminess-ttf-nerd-font 3.0.0

--- Casks/font-terminess-ttf-nerd-font.rb
+++ Casks/font-terminess-ttf-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-terminess-ttf-nerd-font" do
-  version "2.3.3"
-  sha256 "2c60fab327da9ff6fff250a20f884ad4511e5d8edd49bf349a0d5d870b6a783a"
+  version "3.0.0"
+  sha256 "c133e1c01b8dfb636bca37d5c7b772775630fccdb2f69217adfe5b830aadab86"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/Terminus.zip"
   name "TerminessTTF Nerd Font (Terminus)"
