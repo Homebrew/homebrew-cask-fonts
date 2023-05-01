4a4b909c7e70f531af036b9268d86a697348e555
Bevan Kay <email@bevankay.me>
2023-05-01 13:03:03 +1000

    font-heavy-data-nerd-font 3.0.0

--- Casks/font-heavy-data-nerd-font.rb
+++ Casks/font-heavy-data-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-heavy-data-nerd-font" do
-  version "2.3.3"
-  sha256 "57d24ae8793a91a983022b547c7cb9bee635f1e427e9c48ed9b32449c57cf7e6"
+  version "3.0.0"
+  sha256 "00164c3331126eaf32f170322c845ef81c5f78f5a9b9bd03165e1f82df190bb4"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/HeavyData.zip"
   name "HeavyData Nerd Font (Heavy Data)"
