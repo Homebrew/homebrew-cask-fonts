dcf9b8143ee7a5925416a06181298e3bf13e5d10
Hanlin He <hanling.he@gmail.com>
2023-05-06 09:13:50 -0700

    font-iosevka-ss08 22.1.1

--- Casks/font-iosevka-ss08.rb
+++ Casks/font-iosevka-ss08.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss08" do
-  version "22.1.0"
-  sha256 "36e9652e8610d6492bf4a73915329d708b44b51195003752a16d02ea77210a7a"
+  version "22.1.1"
+  sha256 "5105dcee730dd3c36fc2808bf2b16fd2e27e1ff36ccf13a09f6c541e2a4d5ec6"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss08-#{version}.zip"
   name "Iosevka SS08"
