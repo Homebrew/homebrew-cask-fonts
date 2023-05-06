563148815f4d20bec7546d65cdf6a091ba5cd95d
Hanlin He <hanling.he@gmail.com>
2023-05-06 15:16:03 -0700

    font-iosevka 22.1.1 (#7532)

--- Casks/font-iosevka.rb
+++ Casks/font-iosevka.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka" do
-  version "22.1.0"
-  sha256 "02334f8b5059bb65387865c4b75f5e18899951998308ff754ac78c244d1b14b8"
+  version "22.1.1"
+  sha256 "12bf7672a7770217e389a7b64d96434eb9ace87a75a5da02f75ebcb3e6b13ec9"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-#{version}.zip"
   name "Iosevka"
