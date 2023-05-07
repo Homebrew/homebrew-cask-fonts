2465ade3078925de41dd51446fad3d2abc0f3fd5
Miccal Matthews <miccal.matthews@gmail.com>
2023-05-07 19:15:35 +0800

    Update font-noto-sans-chorasmian.rb

--- Casks/font-noto-sans-chorasmian.rb
+++ Casks/font-noto-sans-chorasmian.rb
@@ -2,11 +2,10 @@ cask "font-noto-sans-chorasmian" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notosanschorasmian/NotoSansChorasmian-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notosanschorasmian/NotoSansChorasmian-Regular.ttf"
   name "Noto Sans Chorasmian"
   desc "Design for the historical middle eastern chorasmian script"
-  homepage "https://fonts.google.com/specimen/Noto+Sans+Chorasmian"
+  homepage "https://github.com/notofonts/chorasmian"
 
   font "NotoSansChorasmian-Regular.ttf"
 end
