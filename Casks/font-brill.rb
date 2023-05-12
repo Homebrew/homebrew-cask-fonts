1785fd27d9b56f0f991a6be2437508f8f9b0d0d0
Stefano Coretta <stefano.coretta@gmail.com>
2023-05-12 11:21:43 +0100

    Wrong hash
    I have update Tthe hash in the font-brill rb file to reflect the actual hash.
    
    The hash for this font seems to frequently be out of sync.

--- Casks/font-brill.rb
+++ Casks/font-brill.rb
@@ -1,6 +1,6 @@
 cask "font-brill" do
   version "4.0"
-  sha256 "8312c35f9c273de4884511f60f5701c7ef6a1c8062f015b88f3c05fde66cd8dd"
+  sha256 "5575fbca12d5f8f50b8613bf01ae373f4237fcb2fdf5a379e7378f477e54352e"
 
   url "https://brill.com/fileasset/The_Brill_Typeface_Package_v_#{version.dots_to_underscores}.zip"
   name "Brill"
