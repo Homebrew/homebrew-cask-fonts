fa5156a7593915837848f030130e8e0dc0c003ab
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-18 23:01:36 -0400

    font-aurulent-sans-mono-nerd-font 3.0.1 (#7601)
    * font-aurulent-sans-mono-nerd-font 3.0.1
    
    * Update font-aurulent-sans-mono-nerd-font.rb
    
    ---------
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-aurulent-sans-mono-nerd-font.rb
+++ Casks/font-aurulent-sans-mono-nerd-font.rb
@@ -1,6 +1,6 @@
 cask "font-aurulent-sans-mono-nerd-font" do
-  version "2.3.3"
-  sha256 "963ce11f65171cc8d368c69af5ed2d1684141b35863a24f4c23f1870cc73b098"
+  version "3.0.1"
+  sha256 "3594eb6f674bd5d980454b46510de9efdf9ad6ff587fb10b0b8ca13f82a6df79"
 
   url "https://github.com/ryanoasis/nerd-fonts/releases/download/v#{version}/AurulentSansMono.zip"
   name "AurulentSansMono Nerd Font (Aurulent Sans Mono)"
@@ -12,8 +12,9 @@ cask "font-aurulent-sans-mono-nerd-font" do
     strategy :github_latest
   end
 
-  font "AurulentSansMono-Regular Nerd Font Complete Mono Windows Compatible.otf"
-  font "AurulentSansMono-Regular Nerd Font Complete Mono.otf"
-  font "AurulentSansMono-Regular Nerd Font Complete Windows Compatible.otf"
-  font "AurulentSansMono-Regular Nerd Font Complete.otf"
+  font "AurulentSansMNerdFont-Regular.otf"
+  font "AurulentSansMNerdFontMono-Regular.otf"
+  font "AurulentSansMNerdFontPropo-Regular.otf"
+
+  # No zap stanza required
 end
