c722bbddd22c59c372e182145f00a17019a0b3e3
BrewTestBot <ops@brew.sh>
2023-05-21 05:18:31 +0100

    Automatic update of Google Fonts (#7604)
    * Update Google Fonts
    
    * Update font-darker-grotesque.rb
    
    ---------
    
    Co-authored-by: BrewTestBot <BrewTestBot@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/font-darker-grotesque.rb
+++ Casks/font-darker-grotesque.rb
@@ -2,18 +2,12 @@ cask "font-darker-grotesque" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
-      branch:    "main",
-      only_path: "ofl/darkergrotesque"
+  url "https://github.com/google/fonts/raw/main/ofl/darkergrotesque/DarkerGrotesque%5Bwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Darker Grotesque"
   homepage "https://fonts.google.com/specimen/Darker+Grotesque"
 
-  font "DarkerGrotesque-Black.ttf"
-  font "DarkerGrotesque-Bold.ttf"
-  font "DarkerGrotesque-ExtraBold.ttf"
-  font "DarkerGrotesque-Light.ttf"
-  font "DarkerGrotesque-Medium.ttf"
-  font "DarkerGrotesque-Regular.ttf"
-  font "DarkerGrotesque-SemiBold.ttf"
+  font "DarkerGrotesque[wght].ttf"
+
+  # No zap stanza required
 end
