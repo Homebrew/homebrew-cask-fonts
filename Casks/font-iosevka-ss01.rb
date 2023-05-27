63f255aab4fcbfaaf569788be68a455980d68d97
singularitti <singularitti@outlook.com>
2023-05-27 12:32:26 -0400

    font-iosevka-ss01 24.0.0

--- Casks/font-iosevka-ss01.rb
+++ Casks/font-iosevka-ss01.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss01" do
-  version "23.0.0"
-  sha256 "eb274a0821e9554ed6f6c139aa2abc678c9c007b5ec220dc42d4e8208489e738"
+  version "24.0.0"
+  sha256 "a9450e799a58ceea00da19b4e9b0cda24c6cde5ff7c476db8e3836cdf9cd52d4"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss01-#{version}.zip"
   name "Iosevka SS01"
