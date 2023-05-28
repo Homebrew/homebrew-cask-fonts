979e598b1b939668d4a7e5a69f7993cc45f2d972
Miccal Matthews <miccal.matthews@gmail.com>
2023-05-28 11:28:38 +0800

    Update font-moirai-one.rb

--- Casks/font-moirai-one.rb
+++ Casks/font-moirai-one.rb
@@ -2,10 +2,11 @@ cask "font-moirai-one" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/moiraione/MoiraiOne-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/moiraione/MoiraiOne-Regular.ttf"
   name "Moirai One"
-  homepage "https://fonts.google.com/specimen/Moirai+One"
+  homepage "https://github.com/JAMO-TYPEFACE/Moirai"
 
   font "MoiraiOne-Regular.ttf"
+
+  # No zap stanza required
 end
