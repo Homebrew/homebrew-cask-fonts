f670d47e209769cbebf7d8450ec55f557e4bc3b4
Miccal Matthews <miccal.matthews@gmail.com>
2023-05-28 11:29:14 +0800

    Update font-orbit.rb

--- Casks/font-orbit.rb
+++ Casks/font-orbit.rb
@@ -2,11 +2,12 @@ cask "font-orbit" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/orbit/Orbit-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/orbit/Orbit-Regular.ttf"
   name "Orbit"
   desc "Recommended to use bright writing on a dark background, below 10pt"
-  homepage "https://fonts.google.com/specimen/Orbit"
+  homepage "https://github.com/JAMO-TYPEFACE/Orbit"
 
   font "Orbit-Regular.ttf"
+
+  # No zap stanza required
 end
