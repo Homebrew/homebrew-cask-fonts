5063bd465c6a22a47d501f1329573033a91d1a4f
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-28 19:23:58 -0400

    font-iosevka-ss16 24.1.0

--- Casks/font-iosevka-ss16.rb
+++ Casks/font-iosevka-ss16.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss16" do
-  version "24.0.0"
-  sha256 "1e682140e6d92f7e784f81c4b2fbdee7f6c3ef0b42168bd5895c3a0d8b43e5e2"
+  version "24.1.0"
+  sha256 "3b4a8a28b8aba506ff30f0e6ecbf483aa98f0b5bb0de8f4eb2aba1e1a888aa31"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss16-#{version}.zip"
   name "Iosevka SS16"
