49040fad9492f1dfdf6bf07b5bdee81b0c06f425
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-28 20:30:51 -0400

    font-iosevka-ss15 24.1.0 (#7678)

--- Casks/font-iosevka-ss15.rb
+++ Casks/font-iosevka-ss15.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss15" do
-  version "24.0.0"
-  sha256 "0b69755397fe34bc5bbe49b8861cc3cecefc7d64a1a6446c02d418dfa809ef6a"
+  version "24.1.0"
+  sha256 "7cda273e2ec3a8f7cf2e72f53186a213b488915a78c8f4d6d8165f2414d657b5"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss15-#{version}.zip"
   name "Iosevka SS15"
