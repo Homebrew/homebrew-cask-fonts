a1a3258149b27406689634bcb78276831b33af16
singularitti <singularitti@outlook.com>
2023-06-03 16:59:31 -0400

    font-iosevka-curly 24.1.1

--- Casks/font-iosevka-curly.rb
+++ Casks/font-iosevka-curly.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-curly" do
-  version "24.1.0"
-  sha256 "ffd03675a37a11daee588142e77e4ee18084942b6b482b0c61c9ac31e11f59f4"
+  version "24.1.1"
+  sha256 "197e50174a4db0ba8dc29b35c4516a01c01226576029545b1c8940dd23e906f1"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-curly-#{version}.zip"
   name "Iosevka Curly"
