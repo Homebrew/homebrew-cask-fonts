bad6c4c34e4435d58fb9e68d5fbf277a56ae095b
Miccal Matthews <miccal.matthews@gmail.com>
2023-06-04 12:26:15 +0800

    Update font-rethink-sans.rb

--- Casks/font-rethink-sans.rb
+++ Casks/font-rethink-sans.rb
@@ -3,13 +3,13 @@ cask "font-rethink-sans" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
-      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/rethinksans"
   name "Rethink Sans"
-  desc "Filled with thoughtfully turbocharged features, including: weight-specific tracking, two styles of circled numbers a simple keystroke away, and tabular lining figures right out-of-the-box"
-  homepage "https://fonts.google.com/specimen/Rethink+Sans"
+  homepage "https://github.com/hans-thiessen/Rethink-Sans"
 
   font "RethinkSans-Italic[wght].ttf"
   font "RethinkSans[wght].ttf"
+
+  # No zap stanza required
 end
