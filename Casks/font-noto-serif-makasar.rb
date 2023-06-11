17ac9b0490289ed78b58d636292a8991600ccf15
Miccal Matthews <miccal.matthews@gmail.com>
2023-06-11 13:54:25 +0800

    Update font-noto-serif-makasar.rb

--- Casks/font-noto-serif-makasar.rb
+++ Casks/font-noto-serif-makasar.rb
@@ -2,11 +2,12 @@ cask "font-noto-serif-makasar" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notoserifmakasar/NotoSerifMakasar-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notoserifmakasar/NotoSerifMakasar-Regular.ttf"
   name "Noto Serif Makasar"
   desc "Design for the historical southeast asian makasar script"
-  homepage "https://fonts.google.com/specimen/Noto+Serif+Makasar"
+  homepage "https://github.com/notofonts/makasar"
 
   font "NotoSerifMakasar-Regular.ttf"
+
+  # No zap stanza required
 end
