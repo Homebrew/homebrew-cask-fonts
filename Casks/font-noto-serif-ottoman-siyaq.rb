225ff8b644adf07f056da3f92ae5283a8dd8d8eb
Miccal Matthews <miccal.matthews@gmail.com>
2023-06-11 13:55:21 +0800

    Update font-noto-serif-ottoman-siyaq.rb

--- Casks/font-noto-serif-ottoman-siyaq.rb
+++ Casks/font-noto-serif-ottoman-siyaq.rb
@@ -2,11 +2,12 @@ cask "font-noto-serif-ottoman-siyaq" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/notoserifottomansiyaq/NotoSerifOttomanSiyaq-Regular.ttf",
-      verified: "github.com/google/fonts/"
+  url "https://github.com/google/fonts/raw/main/ofl/notoserifottomansiyaq/NotoSerifOttomanSiyaq-Regular.ttf"
   name "Noto Serif Ottoman Siyaq"
-  desc "Modulated (“serif”) design for the arabic form of the siyaq numeral system, used in iran, turkey, the arabian peninsula, and south asia for accounting and finance"
-  homepage "https://fonts.google.com/specimen/Noto+Serif+Ottoman+Siyaq"
+  desc "Modulated (“serif”) design for the arabic form of the siyaq numeral system"
+  homepage "https://github.com/notofonts/noto-project-template"
 
   font "NotoSerifOttomanSiyaq-Regular.ttf"
+
+  # No zap stanza required
 end
