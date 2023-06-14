12438e62c1e42616d3c8a7d75c6f662d16e4fcc2
TakWolf <takwolf@foxmail.com>
2023-06-14 16:49:02 +0800

    font-ark-pixel-10px-monospaced 2023.06.12

--- Casks/font-ark-pixel-10px-monospaced.rb
+++ Casks/font-ark-pixel-10px-monospaced.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-10px-monospaced" do
-  version "2023.03.27"
-  sha256 "ff6dc2e7b16c3698b326548772aeda720c23c889404fed33373804e5681cba5b"
+  version "2023.06.12"
+  sha256 "6ff986b69ee08f28c8275813239ab65ae55dd26e5b1c1a54eec93557b50410c4"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-10px-monospaced-otf-v#{version}.zip"
   name "Ark Pixel 10px monospaced"
