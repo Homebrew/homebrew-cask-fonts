7c97399ac7bd7110ddec09cb292356466395aa42
Lex.Chen <ctyytc@gmail.com>
2023-06-21 01:31:07 +0000

    Add font-lxgw-neoxihei.rb

---
+++ Casks/font-lxgw-neoxihei.rb
@@ -0,0 +1,12 @@
+cask "font-lxgw-neoxihei" do
+    version "1.101"
+    sha256 "eb371ed752ad54aa638f35e45d8cc18dfa8c139e4b46f9294b6f19adc2e8e16d"
+  
+    url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v#{version}/LXGWNeoXiHei.ttf"
+    name "LXGW NeoXiHei"
+    name "霞鹜新晰黑"
+    desc "A Chinese sans-serif font derived from IPAex Gothic."
+    homepage "https://github.com/lxgw/LxgwNeoXiHei"
+  
+    font "LXGWNeoXiHei.ttf"
+  end
