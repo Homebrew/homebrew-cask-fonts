4904d49fe4581ed9e81c854d2f24fede4bf0d7bc
Miccal Matthews <miccal.matthews@gmail.com>
2023-06-21 11:02:44 +0800

    Update font-lxgw-neoxihei.rb

--- Casks/font-lxgw-neoxihei.rb
+++ Casks/font-lxgw-neoxihei.rb
@@ -5,8 +5,10 @@ cask "font-lxgw-neoxihei" do
     url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v#{version}/LXGWNeoXiHei.ttf"
     name "LXGW NeoXiHei"
     name "霞鹜新晰黑"
-    desc "A Chinese sans-serif font derived from IPAex Gothic."
+    desc "Chinese sans-serif font derived from IPAex Gothic"
     homepage "https://github.com/lxgw/LxgwNeoXiHei"
   
     font "LXGWNeoXiHei.ttf"
+
+    # No zap stanza required
   end
