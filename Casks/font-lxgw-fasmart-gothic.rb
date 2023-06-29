1a6ecd3f46861a3961756bb9b7548ad21c69999e
singularitti <singularitti@outlook.com>
2023-06-29 11:57:26 -0400

    font-lxgw-fasmart-gothic 1.102.1

--- Casks/font-lxgw-fasmart-gothic.rb
+++ Casks/font-lxgw-fasmart-gothic.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-fasmart-gothic" do
-  version "1.102"
-  sha256 "f715cfc97808cca55d21c75a23170e1c8b1f6d79fef2f1613495b5b1433a78cf"
+  version "1.102.1"
+  sha256 "f686a25ef6a83307b3c1e01dcc0eef93f318e3e7c4b4619a5d556c008cc0a960"
 
   url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v#{version}/LXGWFasmartGothic.ttf"
   name "LXGW Fasmart Gothic"
