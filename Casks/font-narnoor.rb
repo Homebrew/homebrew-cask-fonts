f97b51a6839a5f68f8d9c9257d9ab471478fa983
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-07-02 00:06:43 +0000

    Update Google Fonts

--- Casks/font-narnoor.rb
+++ Casks/font-narnoor.rb
@@ -2,10 +2,11 @@ cask "font-narnoor" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/narnoor/Narnoor-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/narnoor/Narnoor-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Narnoor"
   desc "Actively being revived"
-  homepage "https://github.com/silnrsi/font-narnoor"
+  homepage "https://fonts.google.com/specimen/Narnoor"
 
   font "Narnoor-Regular.ttf"
 end
