e9d40fc096c21150eb1f54dc5f7ce4d4e4016251
i0ntempest <szf1234@me.com>
2023-07-09 18:03:38 -0400

    font-latin-modern-math: use https in URL

--- Casks/font-latin-modern-math.rb
+++ Casks/font-latin-modern-math.rb
@@ -2,7 +2,7 @@ cask "font-latin-modern-math" do
   version :latest
   sha256 :no_check
 
-  url "http://www.gust.org.pl/projects/e-foundry/lm-math/download/latinmodern-math-1959.zip"
+  url "https://www.gust.org.pl/projects/e-foundry/lm-math/download/latinmodern-math-1959.zip"
   name "Latin Modern Math"
   homepage "http://www.gust.org.pl/projects/e-foundry/lm-math"
 
