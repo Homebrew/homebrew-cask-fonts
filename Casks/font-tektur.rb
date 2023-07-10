509b1a3c1dc922b0ecd06683b72614baa5a1fa1d
Miccal Matthews <miccal.matthews@gmail.com>
2023-07-10 12:57:21 +0800

    Update font-tektur.rb

--- Casks/font-tektur.rb
+++ Casks/font-tektur.rb
@@ -4,7 +4,7 @@ cask "font-tektur" do
 
   url "https://github.com/google/fonts/raw/main/ofl/tektur/Tektur%5Bwdth%2Cwght%5D.ttf"
   name "Tektur"
-  desc "Set high and ascenders are aligned with the cap height allowing for compact typesetting"
+  desc "Set high allowing for compact typesetting"
   homepage "https://github.com/hyvyys/Tektur"
 
   font "Tektur[wdth,wght].ttf"
