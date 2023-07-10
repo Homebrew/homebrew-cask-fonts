a9b6760d9d5bb34f192193f206357d8d8a578881
Miccal Matthews <miccal.matthews@gmail.com>
2023-07-10 12:54:27 +0800

    Update font-borel.rb

--- Casks/font-borel.rb
+++ Casks/font-borel.rb
@@ -4,7 +4,7 @@ cask "font-borel" do
 
   url "https://github.com/google/fonts/raw/main/ofl/borel/Borel-Regular.ttf"
   name "Borel"
-  desc "French cursive primer — developed with primary school teachers and speech therapists — which aims to establish a seamless connection between cursive strokes and common typographic structure"
+  desc "French cursive primer"
   homepage "https://github.com/RosaWagner/Borel"
 
   font "Borel-Regular.ttf"
