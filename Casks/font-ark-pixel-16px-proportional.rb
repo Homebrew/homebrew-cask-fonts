91bd8012c9154299f6715925fbd94e1733287149
TakWolf <takwolf@foxmail.com>
2023-07-15 13:53:39 +0800

    font-ark-pixel-16px-proportional 2023.07.15

--- Casks/font-ark-pixel-16px-proportional.rb
+++ Casks/font-ark-pixel-16px-proportional.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-16px-proportional" do
-  version "2023.06.29"
-  sha256 "ab48b4ed4103d0145dc9b8ad33213f1ab578f9d1dd805011738f364be764a378"
+  version "2023.07.15"
+  sha256 "c61eeb21d358d82df69e85cbdbdedf02c9af8b21965dd398d3eea519d2513521"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-16px-proportional-otf-v#{version}.zip"
   name "Ark Pixel 16px Proportional"
