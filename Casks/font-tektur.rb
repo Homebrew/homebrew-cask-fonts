fbcbf78619d1c2e540b37b59b77fb32b0f1c16e2
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-07-16 00:07:34 +0000

    Update Google Fonts

--- Casks/font-tektur.rb
+++ Casks/font-tektur.rb
@@ -2,10 +2,11 @@ cask "font-tektur" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/tektur/Tektur%5Bwdth%2Cwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/tektur/Tektur%5Bwdth%2Cwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Tektur"
   desc "Set high allowing for compact typesetting"
-  homepage "https://github.com/hyvyys/Tektur"
+  homepage "https://fonts.google.com/specimen/Tektur"
 
   font "Tektur[wdth,wght].ttf"
 
