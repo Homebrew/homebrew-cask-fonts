854dd25ff91d4c6fa45ae708aa3f1ece512a954d
Hanlin He <hanling.he@gmail.com>
2023-07-23 08:48:34 -0700

    font-iosevka-ss10 25.1.1

--- Casks/font-iosevka-ss10.rb
+++ Casks/font-iosevka-ss10.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss10" do
-  version "25.1.0"
-  sha256 "c3ae21281cbfc26144b5a41d1b61df8a0c8529e4adf07b0c3d07811ab8a49b78"
+  version "25.1.1"
+  sha256 "5edea4a3bad6c7de36c0735a754fff63ed039a873f8e394f1d7a47689bfd9fe3"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss10-#{version}.zip"
   name "Iosevka SS10"
