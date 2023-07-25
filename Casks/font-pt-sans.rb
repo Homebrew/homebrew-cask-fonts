196c3e3d6530e2285a5232f7c639b1dc2baf6d0c
Bevan Kay <email@bevankay.me>
2023-07-26 09:29:37 +1000

    font-pt-sans: various updates

--- Casks/font-pt-sans.rb
+++ Casks/font-pt-sans.rb
@@ -2,18 +2,19 @@ cask "font-pt-sans" do
   version :latest
   sha256 :no_check
 
-  url "https://company.paratype.com/system/attachments/629/original/ptsans.zip"
+  url "https://api.paratype.com/api/editions/ofl/download/52212",
+      referer: "https://www.paratype.com/"
   name "PT Sans"
   homepage "https://www.paratype.com/public/"
 
-  font "PTC55F.ttf"
-  font "PTC75F.ttf"
-  font "PTN57F.ttf"
-  font "PTN77F.ttf"
-  font "PTS55F.ttf"
-  font "PTS56F.ttf"
-  font "PTS75F.ttf"
-  font "PTS76F.ttf"
+  font "PT/PT/PT-Sans/pt-sans_bold-italic.ttf"
+  font "PT/PT/PT-Sans/pt-sans_bold.ttf"
+  font "PT/PT/PT-Sans/pt-sans_caption-bold.ttf"
+  font "PT/PT/PT-Sans/pt-sans_caption-regular.ttf"
+  font "PT/PT/PT-Sans/pt-sans_italic.ttf"
+  font "PT/PT/PT-Sans/pt-sans_narrow-bold.ttf"
+  font "PT/PT/PT-Sans/pt-sans_narrow-regular.ttf"
+  font "PT/PT/PT-Sans/pt-sans_regular.ttf"
 
   # No zap stanza required
 end
