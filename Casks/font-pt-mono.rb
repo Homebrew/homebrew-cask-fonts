e323c8a7412ed56e0db2d826ac3caaeb6fc39960
Bevan Kay <email@bevankay.me>
2023-07-26 09:40:37 +1000

    font-pt-mono: various updates

--- Casks/font-pt-mono.rb
+++ Casks/font-pt-mono.rb
@@ -2,12 +2,13 @@ cask "font-pt-mono" do
   version :latest
   sha256 :no_check
 
-  url "https://company.paratype.com/system/attachments/631/original/ptmono.zip"
+  url "https://api.paratype.com/api/editions/ofl/download/52218",
+      referer: "https://www.paratype.com/"
   name "PT Mono"
   homepage "https://www.paratype.com/public/"
 
-  font "PTM55F.ttf"
-  font "PTM75F.ttf"
+  font "PT/PT/PT-Mono/pt-mono_bold.ttf"
+  font "PT/PT/PT-Mono/pt-mono_regular.ttf"
 
   # No zap stanza required
 end
