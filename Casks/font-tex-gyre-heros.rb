7f1e7135dceae1b23d554ce07e490120f2785321
Rishav Kundu <rk@rishav.io>
2023-07-30 03:51:09 +0530

    Use https links for TeX Gyre

--- Casks/font-tex-gyre-heros.rb
+++ Casks/font-tex-gyre-heros.rb
@@ -2,9 +2,9 @@ cask "font-tex-gyre-heros" do
   version "2.004"
   sha256 "755954b7349b803fc1c3d82fe9d9c4f7cf66467af718eaaf4f78ae1a09bf265d"
 
-  url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/heros/qhv#{version}otf.zip"
+  url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/heros/qhv#{version}otf.zip"
   name "TeX Gyre Heros"
-  homepage "http://www.gust.org.pl/projects/e-foundry/tex-gyre/heros"
+  homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/heros"
 
   font "texgyreheros-bold.otf"
   font "texgyreheros-bolditalic.otf"
