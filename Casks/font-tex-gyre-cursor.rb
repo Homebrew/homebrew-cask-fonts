611072a7e3ce8b59e97ea29fca02779178fc19cf
Miccal Matthews <miccal.matthews@gmail.com>
2023-08-02 11:43:57 +0800

    Update font-tex-gyre-cursor.rb

--- Casks/font-tex-gyre-cursor.rb
+++ Casks/font-tex-gyre-cursor.rb
@@ -6,6 +6,11 @@ cask "font-tex-gyre-cursor" do
   name "TeX Gyre Cursor"
   homepage "https://www.gust.org.pl/projects/e-foundry/tex-gyre/cursor"
 
+  livecheck do
+    url "https://www.gust.org.pl/projects/e-foundry/tex-gyre/whole"
+    regex(%r{Cursor</a>,\sver\.\s(\d+(?:\.\d+)+)}i)
+  end
+
   font "texgyrecursor-bold.otf"
   font "texgyrecursor-bolditalic.otf"
   font "texgyrecursor-italic.otf"
