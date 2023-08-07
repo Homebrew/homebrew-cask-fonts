90e7c674d718194243950ffca03f37259507593f
Bevan Kay <email@bevankay.me>
2023-08-07 11:08:56 +1000

    font-fontawesome 6.4.2

--- Casks/font-fontawesome.rb
+++ Casks/font-fontawesome.rb
@@ -1,6 +1,6 @@
 cask "font-fontawesome" do
-  version "6.4.0"
-  sha256 "128b850cef31dd9c21f42dc19b028a02302408659dc0350c5111ea126f6c00ae"
+  version "6.4.2"
+  sha256 "30861f49309ca47d016b35d210eed41d0b782f1d185f9211345c18594c5feade"
 
   url "https://github.com/FortAwesome/Font-Awesome/releases/download/#{version}/fontawesome-free-#{version}-desktop.zip",
       verified: "github.com/FortAwesome/Font-Awesome/"
