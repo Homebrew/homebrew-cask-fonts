889746eaa2a93eebf5be0bccb35b7468850c7545
Hanlin He <hanling.he@gmail.com>
2023-08-19 11:15:19 -0700

    font-iosevka-ss15 26.2.0

--- Casks/font-iosevka-ss15.rb
+++ Casks/font-iosevka-ss15.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss15" do
-  version "26.1.0"
-  sha256 "feb13911effd3e20bdcc9b63d723958b524806b7a205840bcf254db4b7a91d71"
+  version "26.2.0"
+  sha256 "58a1b7d3cb192f4595b2597d1cec2b319cea3f87cda8e6a53a8b27ba079cdd9d"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss15-#{version}.zip"
   name "Iosevka SS15"
