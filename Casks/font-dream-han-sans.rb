616dca6f1dbd293edd2d323150458cefc3a1f820
nmchgx <i@nmchgx.com>
2023-08-20 10:38:40 +0800

    font-dream-han-sans 2.004

---
+++ Casks/font-dream-han-sans.rb
@@ -0,0 +1,39 @@
+cask "font-dream-han-sans" do
+  version "2.004"
+  sha256 "521eda9dedb8854b915be5fd06910e58fc2ae7f5e31e3e77191a65028a8d0283"
+
+  url "https://github.com/Pal3love/dream-han-cjk/releases/download/dream-3.00-sans-#{version}-serif-2.001/DreamHanSans.zip",
+      verified: "github.com/Pal3love/dream-han-cjk/"
+  name "Dream Han Sans"
+  homepage "https://github.com/Pal3love/dream-han-cjk/"
+
+  font "DreamHanSans-W1.ttc"
+  font "DreamHanSans-W2.ttc"
+  font "DreamHanSans-W3.ttc"
+  font "DreamHanSans-W4.ttc"
+  font "DreamHanSans-W5.ttc"
+  font "DreamHanSans-W6.ttc"
+  font "DreamHanSans-W7.ttc"
+  font "DreamHanSans-W8.ttc"
+  font "DreamHanSans-W9.ttc"
+  font "DreamHanSans-W10.ttc"
+  font "DreamHanSans-W11.ttc"
+  font "DreamHanSans-W12.ttc"
+  font "DreamHanSans-W13.ttc"
+  font "DreamHanSans-W14.ttc"
+  font "DreamHanSans-W15.ttc"
+  font "DreamHanSans-W16.ttc"
+  font "DreamHanSans-W17.ttc"
+  font "DreamHanSans-W18.ttc"
+  font "DreamHanSans-W19.ttc"
+  font "DreamHanSans-W20.ttc"
+  font "DreamHanSans-W21.ttc"
+  font "DreamHanSans-W22.ttc"
+  font "DreamHanSans-W23.ttc"
+  font "DreamHanSans-W24.ttc"
+  font "DreamHanSans-W25.ttc"
+  font "DreamHanSans-W26.ttc"
+  font "DreamHanSans-W27.ttc"
+
+  # No zap stanza required
+end
