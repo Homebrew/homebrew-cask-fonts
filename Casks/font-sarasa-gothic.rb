18b619ef3780b79bf43b39d89217d866ef2958c1
Bevan Kay <email@bevankay.me>
2023-08-28 14:19:22 +1000

    font-sarasa-gothic 0.41.8

--- Casks/font-sarasa-gothic.rb
+++ Casks/font-sarasa-gothic.rb
@@ -1,6 +1,6 @@
 cask "font-sarasa-gothic" do
-  version "0.41.7"
-  sha256 "c602d28aa6b016f27385263d63e054e10860801df3f8e35c098141f4942b4f07"
+  version "0.41.8"
+  sha256 "4cb13208541f06c0821ea9d898934c24590cfa2296aa5cb7f7af73c3d01b4c54"
 
   url "https://github.com/be5invis/Sarasa-Gothic/releases/download/v#{version}/sarasa-gothic-ttc-#{version}.7z"
   name "Sarasa Gothic"
