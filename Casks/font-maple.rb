ade43ba3f479380d387baf7e0866a43be16b335e
Bevan Kay <email@bevankay.me>
2023-08-28 14:28:58 +1000

    Update font-maple.rb

--- Casks/font-maple.rb
+++ Casks/font-maple.rb
@@ -1,8 +1,8 @@
 cask "font-maple" do
   version "6.4"
-  sha256 "7f07c594d6da5971428dc9e8ecd2b7759a80cec1cb87dadd1a44aadbac5cf6ac"
+  sha256 "7f2fa17546190d6e6790c562ae1926cacded22459eccf0eb9693719d1325e165"
 
-  url "https://github.com/subframe7536/Maple-font/releases/download/v#{version}/MapleMono-otf.zip"
+  url "https://github.com/subframe7536/Maple-font/releases/download/v#{version}/MapleMono-NF.zip"
   name "Maple Mono"
   desc "Nerd Font font with round corners"
   homepage "https://github.com/subframe7536/Maple-font"
@@ -12,12 +12,12 @@ cask "font-maple" do
     strategy :github_latest
   end
 
-  font "MapleMono-Bold.otf"
-  font "MapleMono-BoldItalic.otf"
-  font "MapleMono-Italic.otf"
-  font "MapleMono-Light.otf"
-  font "MapleMono-LightItalic.otf"
-  font "MapleMono-Regular.otf"
+  font "MapleMono-NF-Bold.ttf"
+  font "MapleMono-NF-BoldItalic.ttf"
+  font "MapleMono-NF-Italic.ttf"
+  font "MapleMono-NF-Light.ttf"
+  font "MapleMono-NF-LightItalic.ttf"
+  font "MapleMono-NF-Regular.ttf"
 
   # No zap stanza required
 end
