c608c583e0f2a685a9eca12a35c64c0a48519688
Isaac Halvorson <hello@hisaac.net>
2023-09-12 06:58:40 -0500

    Update font-commit-mono.rb
    For whatever reason, the release artifact for this version of Commit Mono changed. This updates the hash.

--- Casks/font-commit-mono.rb
+++ Casks/font-commit-mono.rb
@@ -1,6 +1,6 @@
 cask "font-commit-mono" do
   version "1.136"
-  sha256 "1c31a67b5e2d1124574cfdc88d18c2f8222a060e55fb85243fcf2d4a1bb4c68a"
+  sha256 "4b7334d69c2a0d71a707d3ff52048123ce06aa076746961a968becd1f4dc883a"
 
   url "https://github.com/eigilnikolajsen/commit-mono/releases/download/#{version}/CommitMono-#{version}.zip",
       verified: "github.com/eigilnikolajsen/commit-mono/"
