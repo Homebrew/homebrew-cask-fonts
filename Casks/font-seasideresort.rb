044b3dd456a798075dd272caa76e73a1f0329e72
Andrew Vaughan <hello@andrewvaughan.io>
2023-10-17 13:27:39 -0400

    Added font SeasideResortNF

---
+++ Casks/font-seasideresort.rb
@@ -0,0 +1,12 @@
+cask "font-seasideresort" do
+  version :latest
+  sha256 :no_check
+
+  url "https://www.fontsquirrel.com/fonts/download/SeasideResortNF"
+  name "Gandhi Sans"
+  homepage "https://www.fontsquirrel.com/fonts/SeasideResortNF"
+
+  font "SEASRN__.ttf"
+
+  # No zap stanza required
+end
