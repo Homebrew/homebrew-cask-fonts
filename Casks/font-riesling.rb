c048e40997b7a571600350b2d916b33b2b0f8de9
Andrew Vaughan <hello@andrewvaughan.io>
2023-10-17 12:59:57 -0400

    Create font-riesling.rb

---
+++ Casks/font-riesling.rb
@@ -0,0 +1,12 @@
+cask "font-riesling" do
+  version :latest
+  sha256 :no_check
+
+  url "https://www.fontsquirrel.com/fonts/download/riesling"
+  name "Riesling"
+  homepage "https://www.fontsquirrel.com/fonts/riesling"
+
+  font "riesling.ttf"
+
+  # No zap stanza required
+end
