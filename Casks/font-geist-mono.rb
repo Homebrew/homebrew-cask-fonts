8c3d6fad686eb6c6583c5f254277f9a609f0c1ef
Levi Nelson <54561718+LeviticusNelson@users.noreply.github.com>
2023-10-27 17:04:54 -0700

    feat: Geist Mono

---
+++ Casks/font-geist-mono.rb
@@ -0,0 +1,26 @@
+cask "font-geist-mono" do
+  version "1.0.0"
+  sha256 "804891b853484797101878e8638391d1d3b1c7b0673a3b0e89a4423664d016d2"
+
+  url "https://github.com/vercel/geist-font/releases/download/#{version}/Geist.Mono.zip",
+      verified: "github.com/vercel/geist-font/"
+  name "Geist Mono"
+  desc "Monospaced typeface designed to be used where code is represented"
+  homepage "https://vercel.com/font/mono"
+
+  livecheck do
+    url "https://github.com/vercel/geist-font"
+    strategy :gitHub_latest
+  end
+
+  font "Geist Mono/GeistMono-Black.otf"
+  font "Geist Mono/GeistMono-Bold.otf"
+  font "Geist Mono/GeistMono-Light.otf"
+  font "Geist Mono/GeistMono-Medium.otf"
+  font "Geist Mono/GeistMono-Regular.otf"
+  font "Geist Mono/GeistMono-SemiBold.otf"
+  font "Geist Mono/GeistMono-Thin.otf"
+  font "Geist Mono/GeistMono-UltraBlack.otf"
+  font "Geist Mono/GeistMono-UltraLight.otf"
+  font "Geist Mono/GeistMonoVF.ttf"
+end
