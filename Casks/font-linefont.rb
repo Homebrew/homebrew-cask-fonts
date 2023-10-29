3b93166291e60b8f1962a520b575747738403737
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-10-29 00:06:42 +0000

    Update Google Fonts

--- Casks/font-linefont.rb
+++ Casks/font-linefont.rb
@@ -2,10 +2,11 @@ cask "font-linefont" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/linefont/Linefont%5Bwdth%2Cwght%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/linefont/Linefont%5Bwdth%2Cwght%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Linefont"
   desc "Variable font for rendering small to medium-scale line charts"
-  homepage "https://github.com/dy/linefont"
+  homepage "https://fonts.google.com/specimen/Linefont"
 
   font "Linefont[wdth,wght].ttf"
 
