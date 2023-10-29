77927ccae0edd7e341e6f2e8c9494798a7c372b2
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-10-29 00:07:00 +0000

    Update Google Fonts

---
+++ Casks/font-kalnia.rb
@@ -0,0 +1,12 @@
+cask "font-kalnia" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/kalnia/Kalnia%5Bwdth%2Cwght%5D.ttf"
+  name "Kalnia"
+  homepage "https://github.com/fridamedrano/Kalnia-Typeface"
+
+  font "Kalnia[wdth,wght].ttf"
+
+  # No zap stanza required
+end
