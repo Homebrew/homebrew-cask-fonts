d517e502b2fad18f95b4a311007d8eeb7fcfc23a
Kenny Heard <work@kennyheard.dev>
2023-11-03 21:36:22 +0000

    font-geist: updated checksum

--- Casks/font-geist.rb
+++ Casks/font-geist.rb
@@ -1,6 +1,6 @@
 cask "font-geist" do
   version "1.0.0"
-  sha256 "3d19900ade9c33ba4773f29dcf77b6563335039440de60af3d34b050911eac44"
+  sha256 "1c030f65cab740b33094be80686b01517c2072c85ec291daa8fcacfdaca05c7a"
 
   url "https://github.com/vercel/geist-font/releases/download/#{version}/Geist.zip",
       verified: "github.com/vercel/geist-font/"
