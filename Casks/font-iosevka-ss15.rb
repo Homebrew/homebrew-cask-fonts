2fbff9e0ea5c2fa3b2176628206eb3df601c0995
singularitti <singularitti@outlook.com>
2023-11-04 23:12:25 -0400

    font-iosevka-ss15 27.3.4

--- Casks/font-iosevka-ss15.rb
+++ Casks/font-iosevka-ss15.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss15" do
-  version "27.3.3"
-  sha256 "64bed66108961f0592ef895e7ce5ce861a23143bbf08b14bde872aa6138d4de9"
+  version "27.3.4"
+  sha256 "5076b8d2524b97936e434593b5c05992b1c8b32088c4579f4d0a1555d9e3161c"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss15-#{version}.zip"
   name "Iosevka SS15"
