ed48dac37a373f3166f501f242d76d39ac6815f2
Bevan Kay <email@bevankay.me>
2023-11-21 21:38:35 +1100

    font-cozette 1.23.1

--- Casks/font-cozette.rb
+++ Casks/font-cozette.rb
@@ -1,6 +1,6 @@
 cask "font-cozette" do
-  version "1.22.2"
-  sha256 "018df88ad2bb2dc3c182b94826e1475eaddb299d71ea9f8e59f59bc8f275f19d"
+  version "1.23.1"
+  sha256 "b829d4932195a6fa06b08e8705f9fe8910512f42e1aed5fdeb29130c7422d995"
 
   url "https://github.com/slavfox/Cozette/releases/download/v.#{version}/CozetteVector.dfont"
   name "Cozette"
