372e49ec642d444f46dac02b8bd72fc70bfbbb49
Bevan Kay <bevanjkay@gmail.com>
2023-11-22 09:33:27 +1100

    Update font-ia-writer-duospace.rb

--- Casks/font-ia-writer-duospace.rb
+++ Casks/font-ia-writer-duospace.rb
@@ -6,10 +6,12 @@ cask "font-ia-writer-duospace" do
   name "iA Writer Duospace"
   homepage "https://github.com/iaolo/iA-Fonts"
 
-  font "iA-Fonts-master/iA Writer Duo/Static/iAWriterDuospace-Bold.ttf"
-  font "iA-Fonts-master/iA Writer Duo/Static/iAWriterDuospace-BoldItalic.ttf"
-  font "iA-Fonts-master/iA Writer Duo/Static/iAWriterDuospace-Regular.ttf"
-  font "iA-Fonts-master/iA Writer Duo/Static/iAWriterDuospace-Italic.ttf"
+  font "iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Bold.ttf"
+  font "iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-BoldItalic.ttf"
+  font "iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Italic.ttf"
+  font "iA-Fonts-master/iA Writer Duo/Static/iAWriterDuoS-Regular.ttf"
+  font "iA-Fonts-master/iA Writer Duo/Variable/iAWriterDuoV-Italic.ttf"
+  font "iA-Fonts-master/iA Writer Duo/Variable/iAWriterDuoV.ttf"
 
   # No zap stanza required
 end
