dddcb712bbb832cb171f86846d32727a830bab81
ponte-vecchio <zkogdxdkur@p.monash.edu>
2023-11-24 10:10:07 +1100

    [fix] font source url

--- Casks/font-elice-digital-baeum.rb
+++ Casks/font-elice-digital-baeum.rb
@@ -2,7 +2,7 @@ cask "font-elice-digital-baeum" do
   version :latest
   sha256 :no_check
 
-  url "https://font.elice.io/static/archives/EliceDigitalBaeum_OTF.zip"
+  url "https://font.elice.io/static/downloads/EliceDigitalBaeum_OTF.zip"
   name "Elice Digital Baeum"
   homepage "https://font.elice.io/"
 
