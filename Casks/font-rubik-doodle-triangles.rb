c6e1b25b30fda24ec75c660cab62304864f58d21
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-11-26 00:06:32 +0000

    Update Google Fonts

---
+++ Casks/font-rubik-doodle-triangles.rb
@@ -0,0 +1,12 @@
+cask "font-rubik-doodle-triangles" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/rubikdoodletriangles/RubikDoodleTriangles-Regular.ttf"
+  name "Rubik Doodle Triangles"
+  homepage "https://github.com/NaN-xyz/Rubik-Filtered"
+
+  font "RubikDoodleTriangles-Regular.ttf"
+
+  # No zap stanza required
+end
