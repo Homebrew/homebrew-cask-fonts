af9c4010daa16ef054eba4b850df9a830f74a114
Yukai Chou <muzimuzhi@gmail.com>
2023-11-29 04:21:05 +0800

    font-noto-serif-cjk 2.002

--- Casks/font-noto-serif-cjk.rb
+++ Casks/font-noto-serif-cjk.rb
@@ -1,11 +1,16 @@
 cask "font-noto-serif-cjk" do
-  version :latest
-  sha256 :no_check
+  version "2.002"
+  sha256 "e78b23ea9f803d8376eee66d330a556e1133b713ff97014a75991d5723324f85"
 
-  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSerifCJK.ttc.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
+  url "https://github.com/notofonts/noto-cjk/releases/download/Serif#{version}/01_NotoSerifCJK.ttc.zip"
   name "Noto Serif CJK"
-  homepage "https://www.google.com/get/noto/help/cjk/"
+  desc "Static Super OTC"
+  homepage "https://github.com/notofonts/noto-cjk/tree/main/Serif"
+
+  livecheck do
+    url :stable
+    regex(/^Serif(\d+(?:\.\d+)+)$/i)
+  end
 
   font "NotoSerifCJK.ttc"
 
