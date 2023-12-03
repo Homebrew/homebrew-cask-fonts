e36757158df8c3dd3687a3d0d493cff084209e8f
BrewTestBot <BrewTestBot@users.noreply.github.com>
2023-12-03 00:06:58 +0000

    Update Google Fonts

---
+++ Casks/font-rubik-glitch-pop.rb
@@ -0,0 +1,12 @@
+cask "font-rubik-glitch-pop" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/rubikglitchpop/RubikGlitchPop-Regular.ttf"
+  name "Rubik Glitch Pop"
+  homepage "https://github.com/NaN-xyz/Rubik-Filtered"
+
+  font "RubikGlitchPop-Regular.ttf"
+
+  # No zap stanza required
+end
