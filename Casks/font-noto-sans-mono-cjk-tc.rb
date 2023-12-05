fbeba2c8ecf68e948ab6619b9626a990b83b09e2
Yukai Chou <muzimuzhi@gmail.com>
2023-12-05 22:37:01 +0800

    Add font-noto-sans-mono-cjk-tc 2.004

---
+++ Casks/font-noto-sans-mono-cjk-tc.rb
@@ -0,0 +1,19 @@
+cask "font-noto-sans-mono-cjk-tc" do
+  version "2.004"
+  sha256 "0126cbeef724edf21fbaeb113739adf392679fd90a2897c76159d1141df9e8c6"
+
+  url "https://github.com/notofonts/noto-cjk/releases/download/Sans#{version}/14_NotoSansMonoCJKtc.zip"
+  name "Noto Sans Mono CJK TC"
+  desc "Language Specific Monospace OTFs Traditional Chinese — Taiwan (繁體中文—臺灣)"
+  homepage "https://github.com/notofonts/noto-cjk/tree/main/Sans"
+
+  livecheck do
+    url :url
+    regex(/^Sans(\d+(?:\.\d+)+)$/i)
+  end
+
+  font "NotoSansMonoCJKtc-Bold.otf"
+  font "NotoSansMonoCJKtc-Regular.otf"
+
+  # No zap stanza required
+end
