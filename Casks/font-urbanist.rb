b62a1463d5622afad11adb0c965d43eee8433b20
Bevan Kay <email@bevankay.me>
2023-12-17 22:28:09 +1100

    Update font-urbanist.rb

--- Casks/font-urbanist.rb
+++ Casks/font-urbanist.rb
@@ -4,6 +4,7 @@ cask "font-urbanist" do
 
   url "https://github.com/coreyhu/Urbanist/releases/download/#{version}/Urbanist-fonts.zip"
   name "Urbanist"
+  desc "Geometric sans-serif font"
   homepage "https://github.com/coreyhu/Urbanist"
 
   font "Urbanist-fonts/fonts/otf/Urbanist-Black.otf"
@@ -24,27 +25,7 @@ cask "font-urbanist" do
   font "Urbanist-fonts/fonts/otf/Urbanist-SemiBoldItalic.otf"
   font "Urbanist-fonts/fonts/otf/Urbanist-Thin.otf"
   font "Urbanist-fonts/fonts/otf/Urbanist-ThinItalic.otf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-Black.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-BlackItalic.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-Bold.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-BoldItalic.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-ExtraBold.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-ExtraBoldItalic.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-ExtraLight.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-ExtraLightItalic.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-Italic.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-Light.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-LightItalic.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-Medium.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-MediumItalic.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-Regular.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-SemiBold.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-SemiBoldItalic.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-Thin.ttf"
-  font "Urbanist-fonts/fonts/ttf/Urbanist-ThinItalic.ttf"
-  font "Urbanist-fonts/fonts/variable/Urbanist-Italic[wght].ttf"
   font "Urbanist-fonts/fonts/variable/Urbanist[ital,wght].ttf"
-  font "Urbanist-fonts/fonts/variable/Urbanist[wght].ttf"
 
   # No zap stanza required
 end
