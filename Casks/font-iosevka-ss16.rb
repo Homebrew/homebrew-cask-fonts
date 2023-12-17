14c9075e97b84995a7d8a7f811edba9c5abb7556
singularitti <singularitti@outlook.com>
2023-12-16 21:57:54 -0500

    font-iosevka-ss16 28.0.0

--- Casks/font-iosevka-ss16.rb
+++ Casks/font-iosevka-ss16.rb
@@ -1,8 +1,8 @@
 cask "font-iosevka-ss16" do
-  version "27.3.5"
-  sha256 "9bd0518e29a4aeb1592f26028fe04a996534e5c0845e3ca8615030f84159a03e"
+  version "28.0.0"
+  sha256 "bfde348c0cb2222060d701bc17c904d9c4775e1c6a4614873029bd0829e26072"
 
-  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/ttc-iosevka-ss16-#{version}.zip"
+  url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaSS16-#{version}.zip"
   name "Iosevka SS16"
   desc "Sans-serif, slab-serif, monospace and quasi‑proportional typeface family"
   homepage "https://github.com/be5invis/Iosevka/"
@@ -12,15 +12,15 @@ cask "font-iosevka-ss16" do
     strategy :github_latest
   end
 
-  font "iosevka-ss16-bold.ttc"
-  font "iosevka-ss16-extrabold.ttc"
-  font "iosevka-ss16-extralight.ttc"
-  font "iosevka-ss16-heavy.ttc"
-  font "iosevka-ss16-light.ttc"
-  font "iosevka-ss16-medium.ttc"
-  font "iosevka-ss16-regular.ttc"
-  font "iosevka-ss16-semibold.ttc"
-  font "iosevka-ss16-thin.ttc"
+  font "IosevkaSS16-Bold.ttc"
+  font "IosevkaSS16-ExtraBold.ttc"
+  font "IosevkaSS16-ExtraLight.ttc"
+  font "IosevkaSS16-Heavy.ttc"
+  font "IosevkaSS16-Light.ttc"
+  font "IosevkaSS16-Medium.ttc"
+  font "IosevkaSS16-Regular.ttc"
+  font "IosevkaSS16-SemiBold.ttc"
+  font "IosevkaSS16-Thin.ttc"
 
   # No zap stanza required
 end
