24b8006153660aba815701cbc08b554835290dea
singularitti <singularitti@outlook.com>
2023-12-30 03:58:41 -0500

    font-iosevka-ss07 28.0.3

--- Casks/font-iosevka-ss07.rb
+++ Casks/font-iosevka-ss07.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss07" do
-  version "28.0.2"
-  sha256 "54c905373e2b3ba7545f5d00efbc16d7cbb92bec5ef2f36623599b985a5b24da"
+  version "28.0.3"
+  sha256 "c1bc560b23c9ce0b8a487c6e7888161d1aa817df87ea8bbb5d681f08dc333750"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaSS07-#{version}.zip"
   name "Iosevka SS07"
