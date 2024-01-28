094d6cadcfe038fa8dd2681fbe69223068edfc2f
BrewTestBot <BrewTestBot@users.noreply.github.com>
2024-01-28 00:06:40 +0000

    Update Google Fonts

--- Casks/font-sixtyfour.rb
+++ Casks/font-sixtyfour.rb
@@ -2,9 +2,10 @@ cask "font-sixtyfour" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/sixtyfour/Sixtyfour%5BBLED%2CSCAN%5D.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/sixtyfour/Sixtyfour%5BBLED%2CSCAN%5D.ttf",
+      verified: "github.com/google/fonts/"
   name "Sixtyfour"
-  homepage "https://github.com/jenskutilek/homecomputer-fonts"
+  homepage "https://fonts.google.com/specimen/Sixtyfour"
 
   font "Sixtyfour[BLED,SCAN].ttf"
 
