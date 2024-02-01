401584907b7eb63cbc21010438087d0a8d5b5186
sharpenedblade <sharpenedblade@proton.me>
2024-01-31 23:41:19 -0800

    Move variable noto fonts to regular cask

--- Casks/font-noto-sans-mono.rb
+++ Casks/font-noto-sans-mono.rb
@@ -1,48 +1,14 @@
-cask "font-noto-sans-mono" do
-  version :latest
+cask "font-noto-sans-mono-vf" do
+  version "2.014"
+  desc "Monospaced variable font"
   sha256 :no_check
 
-  url "https://noto-website-2.storage.googleapis.com/pkgs/NotoSansMono-unhinted.zip",
-      verified: "noto-website-2.storage.googleapis.com/"
-  name "Noto Sans Mono"
-  homepage "https://www.google.com/get/noto/#sans-mono"
+  url "https://github.com/notofonts/latin-greek-cyrillic/releases/download/NotoSansMono-v#{version}/NotoSansMono-v#{version}.zip",
+      verified: "github.com/notofonts/"
+  name "Noto Sans"
+  homepage "https://notofonts.github.io/"
 
-  font "NotoSansMono-Black.ttf"
-  font "NotoSansMono-Bold.ttf"
-  font "NotoSansMono-Condensed.ttf"
-  font "NotoSansMono-CondensedBlack.ttf"
-  font "NotoSansMono-CondensedBold.ttf"
-  font "NotoSansMono-CondensedExtraBold.ttf"
-  font "NotoSansMono-CondensedExtraLight.ttf"
-  font "NotoSansMono-CondensedLight.ttf"
-  font "NotoSansMono-CondensedMedium.ttf"
-  font "NotoSansMono-CondensedSemiBold.ttf"
-  font "NotoSansMono-CondensedThin.ttf"
-  font "NotoSansMono-ExtraBold.ttf"
-  font "NotoSansMono-ExtraCondensed.ttf"
-  font "NotoSansMono-ExtraCondensedBlack.ttf"
-  font "NotoSansMono-ExtraCondensedBold.ttf"
-  font "NotoSansMono-ExtraCondensedExtraBold.ttf"
-  font "NotoSansMono-ExtraCondensedExtraLight.ttf"
-  font "NotoSansMono-ExtraCondensedLight.ttf"
-  font "NotoSansMono-ExtraCondensedMedium.ttf"
-  font "NotoSansMono-ExtraCondensedSemiBold.ttf"
-  font "NotoSansMono-ExtraCondensedThin.ttf"
-  font "NotoSansMono-ExtraLight.ttf"
-  font "NotoSansMono-Light.ttf"
-  font "NotoSansMono-Medium.ttf"
-  font "NotoSansMono-Regular.ttf"
-  font "NotoSansMono-SemiBold.ttf"
-  font "NotoSansMono-SemiCondensed.ttf"
-  font "NotoSansMono-SemiCondensedBlack.ttf"
-  font "NotoSansMono-SemiCondensedBold.ttf"
-  font "NotoSansMono-SemiCondensedExtraBold.ttf"
-  font "NotoSansMono-SemiCondensedExtraLight.ttf"
-  font "NotoSansMono-SemiCondensedLight.ttf"
-  font "NotoSansMono-SemiCondensedMedium.ttf"
-  font "NotoSansMono-SemiCondensedSemiBold.ttf"
-  font "NotoSansMono-SemiCondensedThin.ttf"
-  font "NotoSansMono-Thin.ttf"
+  font "NotoSansMono/unhinted/variable/NotoSansMono[wdth,wght].ttf"
 
   # No zap stanza required
 end
