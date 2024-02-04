a56d7bef249db6fdbdcf75370db68d8f09c2dd2e
Hanlin He <hanling.he@gmail.com>
2024-02-04 06:03:19 -0800

    font-iosevka-ss14 28.1.0

--- Casks/font-iosevka-ss14.rb
+++ Casks/font-iosevka-ss14.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss14" do
-  version "28.0.7"
-  sha256 "29eae0a8dc20473a673c90eee8988cd740feaf757aa166256aef4edffd3faf2c"
+  version "28.1.0"
+  sha256 "480fa0b7973ef95e12be3e9e932e0a382d27430c4ba1b06d0b93a212e468efb4"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaSS14-#{version}.zip"
   name "Iosevka SS14"
