5c2cd5645146832e6a04deab84f4c55e3a943c9a
Bevan Kay <email@bevankay.me>
2024-02-08 19:36:11 +0000

    font-meslo-for-powerlevel10k: use official source
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Casks/font-meslo-for-powerlevel10k.rb
+++ Casks/font-meslo-for-powerlevel10k.rb
@@ -1,11 +1,11 @@
 cask "font-meslo-for-powerlevel10k" do
-  version "1.2.1"
-  sha256 "2581fcaa4c2dd9bb4cbb5cf6df318bfce925b51a130efad5e0d04fd68c46f4e6"
+  version "2.3.3"
+  sha256 "4b0bb4fc457df96d17aa85cfeda4fa92268dbb0651189f2923c68342ebb67cc7"
 
-  url "https://github.com/kalip2/font-meslo-for-powerlevel10k/releases/download/v#{version}/font-meslo-for-powerlevel10k.zip"
+  url "https://github.com/romkatv/powerlevel10k-media/releases/download/v#{version}/meslo-lgs-nf.tar.gz"
   name "Meslo Nerd Font for Powerlevel10k"
   desc "Monospace font customized for Powerlevel10k"
-  homepage "https://github.com/kalip2/font-meslo-for-powerlevel10k/"
+  homepage "https://github.com/romkatv/powerlevel10k-media"
 
   font "MesloLGS NF Bold Italic.ttf"
   font "MesloLGS NF Bold.ttf"
