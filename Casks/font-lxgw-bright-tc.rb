f98f1c728fe0a5601d8fafda2b956587a9d97085
Hanlin He <hanling.he@gmail.com>
2024-02-09 08:06:30 -0800

    font-lxgw-bright-tc 5.320

--- Casks/font-lxgw-bright-tc.rb
+++ Casks/font-lxgw-bright-tc.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-bright-tc" do
-  version "5.311"
-  sha256 "62459d13af1d3f77ab84c9c24a1f26c81417ffd3dc268a4d21617d15150841c5"
+  version "5.320"
+  sha256 "a69f789608d9921a2f385a7fe2c4ee57be8086d591622965517e42551d0142e7"
 
   url "https://github.com/lxgw/LxgwBright/releases/download/v#{version}/LXGWBrightTC.7z"
   name "LXGW Bright TC"
