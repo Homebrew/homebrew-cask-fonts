c986681b9974e43150ac5ff00df5da7da172452d
BrewTestBot <BrewTestBot@users.noreply.github.com>
2024-02-18 00:06:46 +0000

    Update Google Fonts

--- Casks/font-annapurna-sil.rb
+++ Casks/font-annapurna-sil.rb
@@ -3,11 +3,12 @@ cask "font-annapurna-sil" do
   sha256 :no_check
 
   url "https://github.com/google/fonts.git",
+      verified:  "github.com/google/fonts",
       branch:    "main",
       only_path: "ofl/annapurnasil"
   name "Annapurna SIL"
   desc "Unicode-based font with support for systems that use the devanagari script"
-  homepage "https://github.com/silnrsi/font-annapurna"
+  homepage "https://fonts.google.com/specimen/Annapurna+SIL"
 
   font "AnnapurnaSIL-Bold.ttf"
   font "AnnapurnaSIL-Regular.ttf"
