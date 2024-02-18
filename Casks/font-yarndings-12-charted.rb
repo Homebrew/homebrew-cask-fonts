d8d9243aadc1a056b1124efb08788d47c1f44f69
BrewTestBot <BrewTestBot@users.noreply.github.com>
2024-02-18 00:06:21 +0000

    Update Google Fonts

---
+++ Casks/font-yarndings-12-charted.rb
@@ -0,0 +1,12 @@
+cask "font-yarndings-12-charted" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/google/fonts/raw/main/ofl/yarndings12charted/Yarndings12Charted-Regular.ttf"
+  name "Yarndings 12 Charted"
+  homepage "https://github.com/scfried/soft-type-yarndings"
+
+  font "Yarndings12Charted-Regular.ttf"
+
+  # No zap stanza required
+end
