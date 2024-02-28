6536c195244ef7600afe73e674e44d1358931f37
Ocean Y <oceansyeo@gmail.com>
2024-02-28 21:09:08 +0800

    font-lxgw-neoxihei 1.120

--- Casks/font-lxgw-neoxihei.rb
+++ Casks/font-lxgw-neoxihei.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-neoxihei" do
-  version "1.110"
-  sha256 "e8a78acfc94904273fb1ce690a44b6992c0c010f17a700c83028d26d55eec87e"
+  version "1.120"
+  sha256 "ad0fa06e6518afe8969b9582512a9bfbcf9a303e49654b1b3d7674362a36725f"
 
   url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v#{version}/LXGWNeoXiHei.ttf"
   name "LXGW NeoXiHei"
