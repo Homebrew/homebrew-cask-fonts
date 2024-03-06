1fe1b393c646db9dde50a5da6268f22d8dcf8b2b
Bevan Kay <email@bevankay.me>
2024-03-06 12:43:34 +1100

    font-sudo 1.1

--- Casks/font-sudo.rb
+++ Casks/font-sudo.rb
@@ -1,6 +1,6 @@
 cask "font-sudo" do
-  version "1.0"
-  sha256 "e95efe89a6e5627f9eb875c91eb22e80e704a20ad2725894c3169655a4bff57e"
+  version "1.1"
+  sha256 "a8642658e01337827c8d8507b004f7b9d7fe52d3f6e9d0901668611f0dd25e0b"
 
   url "https://github.com/jenskutilek/sudo-font/releases/download/v#{version}/sudo.zip"
   name "Sudo"
