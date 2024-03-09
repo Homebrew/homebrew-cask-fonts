b1571e235b416aa8f134e0aa2a1052877b629f88
Klaus Hipp <khipp@users.noreply.github.com>
2024-03-09 22:34:04 +0100

    font-iosevka-ss01 29.0.0

--- Casks/font-iosevka-ss01.rb
+++ Casks/font-iosevka-ss01.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-ss01" do
-  version "28.1.0"
-  sha256 "8b6a63665137d714128237c9af530d274da3d24c4172e29e45a57ea15eaad1a8"
+  version "29.0.0"
+  sha256 "ec5f213035ecfbd13214434f18fc7c6f512e6990263dd84f2a4daa47a6ecc298"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaSS01-#{version}.zip"
   name "Iosevka SS01"
