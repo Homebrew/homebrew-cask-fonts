8635cbff14ec994cd8123e9c0d8b79677faedf2c
Hanlin He <hanling.he@gmail.com>
2024-03-25 09:39:04 -0700

    font-iosevka-slab 29.0.4

--- Casks/font-iosevka-slab.rb
+++ Casks/font-iosevka-slab.rb
@@ -1,6 +1,6 @@
 cask "font-iosevka-slab" do
-  version "29.0.3"
-  sha256 "83d5673eb582f697dbf25987c8246bc043b7c767942a7d689c9509183448ca14"
+  version "29.0.4"
+  sha256 "2344ecb57f678184985b622098221eb63aa19b744525a8692686deb59f612338"
 
   url "https://github.com/be5invis/Iosevka/releases/download/v#{version}/PkgTTC-IosevkaSlab-#{version}.zip"
   name "Iosevka Slab"
