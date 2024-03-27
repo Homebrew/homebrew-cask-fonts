e53d5e3f1a2b1f74a31c73d3d41f395afc6fc514
TakWolf <takwolf@foxmail.com>
2024-03-27 11:39:41 +0800

    font-ark-pixel-12px-proportional 2024.03.27

--- Casks/font-ark-pixel-12px-proportional.rb
+++ Casks/font-ark-pixel-12px-proportional.rb
@@ -1,6 +1,6 @@
 cask "font-ark-pixel-12px-proportional" do
-  version "2024.03.22"
-  sha256 "2cfd6acacc5ca3c0e3330e71956f2709a3d5a0f8e0649f97b2b904fe552a97a1"
+  version "2024.03.27"
+  sha256 "4a9587a15ecf14c15546d99d00c2d9c41264fd9f27eed98138b0e56e3e74121b"
 
   url "https://github.com/TakWolf/ark-pixel-font/releases/download/#{version}/ark-pixel-font-12px-proportional-otf-v#{version}.zip"
   name "Ark Pixel 12px Proportional"
