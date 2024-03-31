83b05da25cd3ba98956594be588e09c5900d85f0
Miccal Matthews <miccal.matthews@gmail.com>
2024-03-31 17:39:59 +0800

    Update font-teachers.rb

--- Casks/font-teachers.rb
+++ Casks/font-teachers.rb
@@ -6,7 +6,6 @@ cask "font-teachers" do
       branch:    "main",
       only_path: "ofl/teachers"
   name "Teachers"
-  desc "Also available in variable font format with an adjustable weight axis allowing designers more control over the display of the font"
   homepage "https://github.com/chankfonts/Teachers-fonts"
 
   font "Teachers-Italic[wght].ttf"
