8331bb915116502c6e376c5e16f12b16b328f0b6
Ocean Y <oceansyeo@gmail.com>
2024-04-27 17:31:23 +0800

    font-lxgw-fasmartgothic 1.121

--- Casks/font-lxgw-fasmartgothic.rb
+++ Casks/font-lxgw-fasmartgothic.rb
@@ -1,6 +1,6 @@
 cask "font-lxgw-fasmartgothic" do
-  version "1.120.2"
-  sha256 "21c9536a2cecefca5804de87059f28f726bc3b490626060a320ad321865fc87b"
+  version "1.121"
+  sha256 "c38f0b48d6b2c594969932717d720e643ccd45f05a00c08ac42ef90e6897fe5e"
 
   url "https://github.com/lxgw/LxgwNeoXiHei/releases/download/v#{version}/LXGWFasmartGothic.ttf"
   name "LXGW FasmartGothic"
