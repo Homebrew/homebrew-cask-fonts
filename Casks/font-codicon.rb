73563f43e23bf576cef08bc176d0deba6c2be0c1
Bevan Kay <email@bevankay.me>
2024-05-14 13:20:15 +1000

    font-codicon: update `font`, update `url`

--- Casks/font-codicon.rb
+++ Casks/font-codicon.rb
@@ -2,12 +2,12 @@ cask "font-codicon" do
   version "0.0.36"
   sha256 "06fc7ab1b9c56f5849118a8fc2355d3a6e215eda243b5b476fad4c234e668101"
 
-  url "https://github.com/microsoft/vscode-codicons/archive/refs/tags/#{version}.zip"
+  url "https://github.com/microsoft/vscode-codicons/releases/download/#{version}/codicon.ttf"
   name "Codicon"
   desc "Icon font for Visual Studio Code"
   homepage "https://github.com/microsoft/vscode-codicons"
 
-  font "vscode-codicons-#{version}/dist/codicon.ttf"
+  font "codicon.ttf"
 
   # No zap stanza required
 end
