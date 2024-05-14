3cbab22b8139d3fc0c16a2c1cb6c5f4147749fc9
BrewTestBot <BrewTestBot@users.noreply.github.com>
2024-05-14 03:42:35 +0000

    Update Google Fonts

--- Casks/font-jacquard-12.rb
+++ Casks/font-jacquard-12.rb
@@ -2,9 +2,10 @@ cask "font-jacquard-12" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/google/fonts/raw/main/ofl/jacquard12/Jacquard12-Regular.ttf"
+  url "https://github.com/google/fonts/raw/main/ofl/jacquard12/Jacquard12-Regular.ttf",
+      verified: "github.com/google/fonts/"
   name "Jacquard 12"
-  homepage "https://github.com/scfried/soft-type-jacquard"
+  homepage "https://fonts.google.com/specimen/Jacquard+12"
 
   font "Jacquard12-Regular.ttf"
 
